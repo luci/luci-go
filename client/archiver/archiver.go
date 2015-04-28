a3cc3ba7339283e54448ce82d10825eefbdc54d1
Marc-Antoine Ruel <maruel@chromium.org>
2015-04-28 19:51:26 -0400

    Change channels to use the same pointer type.
    This permits:
    - use a single allocation per object with a slightly fatter object.
    - track an object over its lifetime. This will be leveraged in a follow up.
    
    R=tandrii@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1116463003

--- client/archiver/archiver.go
+++ client/archiver/archiver.go
@@ -82,9 +82,9 @@ func New(is isolatedclient.IsolateServer) Archiver {
 		maxConcurrentContains: 16,
 		maxConcurrentUpload:   8,
 		containsBatchingDelay: 100 * time.Millisecond,
-		filesToHash:           make(chan string, 10240),
-		itemsToLookup:         make(chan *hashedItem, 10240),
-		itemsToUpload:         make(chan *lookedUpItem, 10240),
+		filesToHash:           make(chan *archiverItem, 10240),
+		itemsToLookup:         make(chan *archiverItem, 10240),
+		itemsToUpload:         make(chan *archiverItem, 10240),
 	}
 
 	a.wg.Add(1)
@@ -109,16 +109,25 @@ func New(is isolatedclient.IsolateServer) Archiver {
 
 // Private details.
 
+// archiver archives content to an Isolate server.
+//
+// Uses a 3 stages pipeline:
+// - Hashing files concurrently.
+// - Batched cache hit lookups on the server.
+// - Uploading cache misses concurrently.
+//
+// TODO(maruel): Add another pipeline stage to do a local hash cache hit to
+// skip hashing the files at all.
 type archiver struct {
 	// Immutable.
 	is                    isolatedclient.IsolateServer
-	maxConcurrentHash     int
-	maxConcurrentContains int
-	maxConcurrentUpload   int
-	containsBatchingDelay time.Duration
-	filesToHash           chan string
-	itemsToLookup         chan *hashedItem
-	itemsToUpload         chan *lookedUpItem
+	maxConcurrentHash     int                // Stage 1
+	maxConcurrentContains int                // Stage 2
+	maxConcurrentUpload   int                // Stage 3
+	containsBatchingDelay time.Duration      // Used by stage 2
+	filesToHash           chan *archiverItem // Stage 1
+	itemsToLookup         chan *archiverItem // Stage 2
+	itemsToUpload         chan *archiverItem // Stage 3
 	wg                    sync.WaitGroup
 
 	// Mutable.
@@ -127,15 +136,42 @@ type archiver struct {
 	stats Stats
 }
 
-type hashedItem struct {
-	digest isolated.DigestItem
-	path   string
+// archiverItem is an item to process.
+//
+// It is caried over from pipeline stage to stage to do processing on it.
+type archiverItem struct {
+	// Immutable.
+	path string
+
+	// Mutable.
+	lock       sync.Mutex
+	err        error               // Item specific error
+	digestItem isolated.DigestItem // Mutated by hashLoop(), used by doContains()
+
+	// Mutable but not accessible externally.
+	state *isolatedclient.PushState // Server-side push state for cache miss
 }
 
-type lookedUpItem struct {
-	state *isolatedclient.PushState
-	path  string
-	size  int64
+func newArchiverItem(path string) *archiverItem {
+	i := &archiverItem{path: path}
+	return i
+}
+
+func (i *archiverItem) setDigest(d isolated.DigestItem) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	i.digestItem = d
+}
+
+func (i *archiverItem) setErr(err error) {
+	if err == nil {
+		panic("internal error")
+	}
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	if i.err == nil {
+		i.err = err
+	}
 }
 
 // Close waits for all pending files to be done.
@@ -149,7 +185,8 @@ func (a *archiver) Close() error {
 }
 
 func (a *archiver) PushFile(path string) {
-	a.filesToHash <- path
+	item := newArchiverItem(path)
+	a.filesToHash <- item
 }
 
 func (a *archiver) Stats() *Stats {
@@ -170,6 +207,7 @@ func (a *archiver) setErr(err error) {
 	}
 }
 
+// hashLoop is stage 1.
 func (a *archiver) hashLoop() {
 	defer close(a.itemsToLookup)
 	var wg sync.WaitGroup
@@ -177,7 +215,7 @@ func (a *archiver) hashLoop() {
 
 	for file := range a.filesToHash {
 		wg.Add(1)
-		go func(f string) {
+		go func(item *archiverItem) {
 			if err := s.Wait(); err != nil {
 				return
 			}
@@ -185,23 +223,27 @@ func (a *archiver) hashLoop() {
 				s.Signal()
 				wg.Done()
 			}()
-			d, err := isolated.HashFile(f)
+
+			d, err := isolated.HashFile(item.path)
 			if err != nil {
-				a.setErr(fmt.Errorf("hash(%s) failed: %s\n", f, err))
-			} else {
-				a.itemsToLookup <- &hashedItem{d, f}
+				item.setErr(err)
+				a.setErr(fmt.Errorf("hash(%s) failed: %s\n", item.path, err))
+				return
 			}
+			item.setDigest(d)
+			a.itemsToLookup <- item
 		}(file)
 	}
 	wg.Wait()
 }
 
+// containsLoop is stage 2.
 func (a *archiver) containsLoop() {
 	defer close(a.itemsToUpload)
 	var wg sync.WaitGroup
 	s := common.NewSemaphore(a.maxConcurrentContains)
 
-	items := []*hashedItem{}
+	items := []*archiverItem{}
 	never := make(<-chan time.Time)
 	timer := never
 	loop := true
@@ -209,7 +251,7 @@ func (a *archiver) containsLoop() {
 		select {
 		case <-timer:
 			wg.Add(1)
-			go func(batch []*hashedItem) {
+			go func(batch []*archiverItem) {
 				if err := s.Wait(); err != nil {
 					return
 				}
@@ -219,7 +261,7 @@ func (a *archiver) containsLoop() {
 				}()
 				a.doContains(batch)
 			}(items)
-			items = []*hashedItem{}
+			items = []*archiverItem{}
 			timer = never
 
 		case item, ok := <-a.itemsToLookup:
@@ -236,7 +278,7 @@ func (a *archiver) containsLoop() {
 
 	if len(items) != 0 {
 		wg.Add(1)
-		go func(batch []*hashedItem) {
+		go func(batch []*archiverItem) {
 			if err := s.Wait(); err != nil {
 				return
 			}
@@ -250,13 +292,14 @@ func (a *archiver) containsLoop() {
 	wg.Wait()
 }
 
+// uploadLoop is stage 3.
 func (a *archiver) uploadLoop() {
 	var wg sync.WaitGroup
 	s := common.NewSemaphore(a.maxConcurrentUpload)
 
-	for state := range a.itemsToUpload {
+	for item := range a.itemsToUpload {
 		wg.Add(1)
-		go func(h *lookedUpItem) {
+		go func(item *archiverItem) {
 			if err := s.Wait(); err != nil {
 				return
 			}
@@ -264,24 +307,31 @@ func (a *archiver) uploadLoop() {
 				s.Signal()
 				wg.Done()
 			}()
-			a.doUpload(h)
-		}(state)
+			a.doUpload(item)
+		}(item)
 	}
 	wg.Wait()
 }
 
-func (a *archiver) doContains(items []*hashedItem) {
+// doContains is scaled by stage 2.
+func (a *archiver) doContains(items []*archiverItem) {
 	tmp := make([]*isolated.DigestItem, len(items))
-	for i, e := range items {
-		tmp[i] = &e.digest
+	// No need to lock each item at that point, no mutation occurs on
+	// archiverItem.digestItem after stage 1.
+	for i, item := range items {
+		tmp[i] = &item.digestItem
 	}
 	states, err := a.is.Contains(tmp)
 	if err != nil {
-		a.setErr(fmt.Errorf("contains(%d) failed: %s", len(items), err))
+		err = fmt.Errorf("contains(%d) failed: %s", len(items), err)
+		a.setErr(err)
+		for _, item := range items {
+			item.setErr(err)
+		}
 		return
 	}
 	for index, state := range states {
-		size := items[index].digest.Size
+		size := items[index].digestItem.Size
 		if state == nil {
 			a.lock.Lock()
 			a.stats.Hits = append(a.stats.Hits, size)
@@ -290,28 +340,32 @@ func (a *archiver) doContains(items []*hashedItem) {
 			a.lock.Lock()
 			a.stats.Misses = append(a.stats.Misses, size)
 			a.lock.Unlock()
-			a.itemsToUpload <- &lookedUpItem{state, items[index].path, size}
+			items[index].state = state
+			a.itemsToUpload <- items[index]
 		}
 	}
 	log.Printf("Looked up %d items\n", len(items))
 }
 
-func (a *archiver) doUpload(l *lookedUpItem) {
-	src, err := os.Open(l.path)
+// doUpload is scaled by stage 3.
+func (a *archiver) doUpload(item *archiverItem) {
+	src, err := os.Open(item.path)
+	if err == nil {
+		defer src.Close()
+		start := time.Now()
+		if err := a.is.Push(item.state, src); err != nil {
+			err = fmt.Errorf("push(%s) failed: %s\n", item.path, err)
+		} else {
+			duration := time.Now().Sub(start)
+			u := &UploadStat{duration, item.digestItem.Size}
+			a.lock.Lock()
+			a.stats.Pushed = append(a.stats.Pushed, u)
+			a.lock.Unlock()
+			log.Printf("Uploaded %.1fkb\n", float64(item.digestItem.Size)/1024.)
+		}
+	}
 	if err != nil {
-		a.setErr(fmt.Errorf("open(%s) failed: %s\n", l.path, err))
-		return
+		a.setErr(err)
+		item.setErr(err)
 	}
-	defer src.Close()
-	start := time.Now()
-	if err = a.is.Push(l.state, src); err != nil {
-		a.setErr(fmt.Errorf("push(%s) failed: %s\n", l.path, err))
-		return
-	}
-	duration := time.Now().Sub(start)
-	u := &UploadStat{duration, l.size}
-	a.lock.Lock()
-	a.stats.Pushed = append(a.stats.Pushed, u)
-	a.lock.Unlock()
-	log.Printf("Uploaded %.1fkb\n", float64(l.size)/1024.)
 }
--- client/archiver/archiver_test.go
+++ client/archiver/archiver_test.go
@@ -5,15 +5,49 @@
 package archiver
 
 import (
+	"io/ioutil"
+	"log"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/luci/luci-go/client/isolatedclient"
+	"github.com/luci/luci-go/client/isolatedclient/isolatedfake"
+	"github.com/luci/luci-go/common/isolated"
 	"github.com/maruel/ut"
 )
 
-func TestArchiver(t *testing.T) {
+func init() {
+	log.SetOutput(ioutil.Discard)
+}
+
+func TestArchiverEmpty(t *testing.T) {
 	t.Parallel()
-	client := isolatedclient.New("https://localhost:1", "default")
-	a := New(client)
+	a := New(isolatedclient.New("https://localhost:1", "default"))
 	ut.AssertEqual(t, &Stats{[]int64{}, []int64{}, []*UploadStat{}}, a.Stats())
+}
+
+func TestArchiverFile(t *testing.T) {
+	t.Parallel()
+	server := isolatedfake.New(t)
+	ts := httptest.NewServer(server)
+	defer ts.Close()
+	a := New(isolatedclient.New(ts.URL, "default"))
+
+	fEmpty, err := ioutil.TempFile("", "archiver")
+	ut.AssertEqual(t, nil, err)
+	a.PushFile(fEmpty.Name())
+	fFoo, err := ioutil.TempFile("", "archiver")
+	ut.AssertEqual(t, nil, err)
+	ut.AssertEqual(t, nil, ioutil.WriteFile(fFoo.Name(), []byte("foo"), 0600))
+	a.PushFile(fFoo.Name())
+	ut.AssertEqual(t, nil, a.Close())
+
+	stats := a.Stats()
+	ut.AssertEqual(t, []int64{}, stats.Hits)
+	ut.AssertEqual(t, []int64{0, 3}, stats.Misses)
+	expected := map[isolated.HexDigest][]byte{
+		"0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33": []byte("foo"),
+		"da39a3ee5e6b4b0d3255bfef95601890afd80709": {},
+	}
+	ut.AssertEqual(t, expected, server.Contents())
 }
