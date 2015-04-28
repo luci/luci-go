a8ed16c18d2e7f34c91725885dbe7932864c6189
Marc-Antoine Ruel <maruel@chromium.org>
2015-04-28 15:49:01 -0400

    Add isolated.Isolated.
    This structures represent the isolated file format.
    
    R=tandrii@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1106293002

---
+++ common/isolated/isolated.go
@@ -0,0 +1,46 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package isolated
+
+const IsolatedFormatVersion = "1.4"
+
+// ReadOnlyValue defines permissions on isolated files.
+type ReadOnlyValue int
+
+const (
+	// Writeable means that both files and directories are writeable. This should
+	// not be used.
+	Writeable ReadOnlyValue = 0
+	// FilesReadOnly means that files are read only but directories are
+	// writeable. This permits the process executed to create temporary files in
+	// the current directory. This is the recommended value and this is the
+	// default value.
+	FilesReadOnly ReadOnlyValue = 1
+	// DirsReadOnly means that both files and directories are read-only. This
+	// enforces strict no-junk policy that the process cannot create files in the
+	// temporary mapped directory.
+	DirsReadOnly ReadOnlyValue = 2
+)
+
+// File describes a single file referenced by content in a .isolated file.
+//
+// Either one of Size or Link can be set.
+type File struct {
+	Digest HexDigest `json:"h"`
+	Link   *string   `json:"l"`
+	Mode   *int      `json:"m"`
+	Size   *int64    `json:"s"`
+}
+
+// Isolated is the data from a JSON serialized .isolated file.
+type Isolated struct {
+	Algo        string          `json:"algo"` // Must be "sha-1"
+	Command     []string        `json:"command",omitempty`
+	Files       map[string]File `json:"files"`
+	Includes    []HexDigest     `json:"includes"`
+	ReadOnly    *ReadOnlyValue  `json:"read_only"`
+	RelativeCwd string          `json:"relative_cwd",omitempty`
+	Version     string          `json:"version"`
+}
