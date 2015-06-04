luci/luci-go

commit f50277f900ab4d7ae5987f597c23ff6a7bcd7dd8

Fix processing of file deps from relative includes.
R=maruel@chromium.org
BUG=496268

Review URL: https://codereview.chromium.org/1166753006

client/isolate/format.go
@@ -441,7 +441,7 @@ func (lhs *ConfigSettings) union(rhs *ConfigSettings) (*ConfigSettings, error) {
 		lFiles, rFiles = rhs.Files, lhs.Files
 	}
 
-	rebasePath, err := filepath.Rel(rRelCwd, lRelCwd)
+	rebasePath, err := filepath.Rel(lRelCwd, rRelCwd)
 	if err != nil {
 		return nil, err
 	}

client/isolate/format_test.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 	"sort"
 	"strings"
@@ -381,8 +382,6 @@ func TestLoadIsolateForConfigMissingVars(t *testing.T) {
 	ut.AssertEqualf(t, true, strings.Contains(err.Error(), "OS"), "%s", err)
 }
 
-// TODO(tandrii): make sure these tests pass on windows.
-
 func TestLoadIsolateForConfig(t *testing.T) {
 	t.Parallel()
 	// Case linux64, matches first condition.
@@ -464,14 +463,54 @@ func TestLoadIsolateAsConfigWithIncludes(t *testing.T) {
 	ut.AssertEqual(t, NotSet, ro) // first condition has no read_only specified.
 	ut.AssertEqual(t, []string{"python", "64linuxOrWin"}, cmd)
 	ut.AssertEqual(t, []string{
-		filepath.Join("..", "inc_file"),
 		"64linuxOrWin",
 		filepath.Join("<(DIR)", "inc_unittest"), // no rebasing for this.
 		filepath.Join("<(PRODUCT_DIR)", "unittest<(EXECUTABLE_SUFFIX)"),
+		filepath.Join("inc", "inc_file"),
 	}, deps)
 }
 
+func TestConfigSettingsUnion(t *testing.T) {
+	left := &ConfigSettings{
+		Command:    []string{"left takes precedence"},
+		Files:      []string{"../../le/f/t"}, // Must be POSIX.
+		IsolateDir: absPath1,
+	}
+	right := &ConfigSettings{
+		Files:      []string{"../ri/g/ht"},
+		IsolateDir: absPath2,
+	}
+
+	out, err := left.union(right)
+	ut.AssertEqual(t, nil, err)
+	ut.AssertEqual(t, left.Command, out.Command)
+	ut.AssertEqual(t, left.IsolateDir, out.IsolateDir)
+	ut.AssertEqual(t, append(getAbsoluteFilenames(left), getAbsoluteFilenames(right)...),
+		getAbsoluteFilenames(out))
+}
+
 // Helper functions.
+
+var absPath1 string
+var absPath2 string
+
+func init() {
+	absPath1 = "/tmp/bar/"
+	absPath2 = "/var/lib/"
+	if common.IsWindows() {
+		absPath1 = "E:\\tmp\\bar\\"
+		absPath2 = "X:\\var\\lib\\"
+	}
+}
+
+func getAbsoluteFilenames(c *ConfigSettings) []string {
+	files := []string{}
+	for _, f := range c.Files {
+		files = append(files, path.Join(c.IsolateDir, f))
+	}
+	sort.Strings(files)
+	return files
+}
 
 // makeVVs simplifies creating variableValue:
 // "unbound" => unbound

commit 3a9a5dcc6fe85b55504368b96273085417527aa8

Fix parsing on Windows.
The test had a regression in commit f50277f900ab4d7ae5987f597c23ff6a7bcd7dd8 but
it was actually uncovering a bug in the code.

Update tests to generate absolute paths on the fly instead of using constant,
since it's more readable.

Bump version since it's a functional change.

R=tandrii@chromium.org
BUG=

Review URL: https://codereview.chromium.org/1149563008

client/cmd/isolate/main.go
@@ -14,7 +14,7 @@ import (
 
 // version must be updated whenever functional change (behavior, arguments,
 // supported commands) is done.
-const version = "0.2"
+const version = "0.2.1"
 
 var application = &subcommands.DefaultApplication{
 	Name:  "isolate",

client/isolate/format.go
@@ -445,7 +445,7 @@ func (lhs *ConfigSettings) union(rhs *ConfigSettings) (*ConfigSettings, error) {
 	if err != nil {
 		return nil, err
 	}
-	rebasePath = strings.Replace(rebasePath, osPathSeparator, "/", 0)
+	rebasePath = strings.Replace(rebasePath, osPathSeparator, "/", -1)
 
 	filesSet := map[string]bool{}
 	for _, f := range lFiles {

client/cmd/isolate/batch_archive_test.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/luci/luci-go/client/internal/common"
@@ -60,7 +61,7 @@ func TestConvertPyToGoArchiveCMDArgs(t *testing.T) {
 }
 
 func TestInvalidArchiveCMD(t *testing.T) {
-	_, err := parseArchiveCMD([]string{}, absPath1)
+	_, err := parseArchiveCMD([]string{}, absToOS("e:", "/tmp/bar"))
 	ut.AssertEqual(t, "-isolated must be specified", err.Error())
 }
 
@@ -73,8 +74,9 @@ func TestArchiveCMDParsing(t *testing.T) {
 		"--extra-variable", "version_full=42.0.2284.0",
 		"--config-variable", "OS=linux",
 	}
-	opts, err := parseArchiveCMD(args, absPath1)
-	base := filepath.Dir(filepath.Dir(absPath1))
+	root := absToOS("e:", "/tmp/bar")
+	opts, err := parseArchiveCMD(args, root)
+	base := filepath.Dir(root)
 	ut.AssertEqual(t, filepath.Join(base, "boz", "bar.isolate"), opts.Isolate)
 	ut.AssertEqual(t, filepath.Join(base, "biz", "bar.isolated"), opts.Isolated)
 	ut.AssertEqual(t, nil, err)
@@ -90,24 +92,23 @@ func TestArchiveCMDParsing(t *testing.T) {
 // Verify that if the isolate/isolated paths are absolute, we don't
 // accidentally interpret them as relative to the cwd.
 func TestArchiveAbsolutePaths(t *testing.T) {
+	root := absToOS("e:", "/tmp/bar/")
 	args := []string{
-		"--isolated", absPath1 + "foo.isolated",
-		"--isolate", absPath1 + "foo.isolate",
+		"--isolated", root + "foo.isolated",
+		"--isolate", root + "foo.isolate",
 	}
-	opts, err := parseArchiveCMD(args, absPath2)
+	opts, err := parseArchiveCMD(args, absToOS("x:", "/var/lib"))
 	ut.AssertEqual(t, nil, err)
-	ut.AssertEqual(t, absPath1+"foo.isolate", opts.Isolate)
-	ut.AssertEqual(t, absPath1+"foo.isolated", opts.Isolated)
+	ut.AssertEqual(t, root+"foo.isolate", opts.Isolate)
+	ut.AssertEqual(t, root+"foo.isolated", opts.Isolated)
 }
 
-var absPath1 string
-var absPath2 string
+// Private stuff.
 
-func init() {
-	absPath1 = "/tmp/bar/"
-	absPath2 = "/var/lib/"
+// absToOS converts a POSIX path to OS specific format.
+func absToOS(drive, p string) string {
 	if common.IsWindows() {
-		absPath1 = "E:\\tmp\\bar\\"
-		absPath2 = "X:\\var\\lib\\"
+		return drive + strings.Replace(p, "/", "\\", -1)
 	}
+	return p
 }

client/isolate/format_test.go
@@ -9,7 +9,6 @@ import (
 	"errors"
 	"io/ioutil"
 	"os"
-	"path"
 	"path/filepath"
 	"sort"
 	"strings"
@@ -470,46 +469,52 @@ func TestLoadIsolateAsConfigWithIncludes(t *testing.T) {
 	}, deps)
 }
 
-func TestConfigSettingsUnion(t *testing.T) {
+func TestConfigSettingsUnionLeft(t *testing.T) {
 	left := &ConfigSettings{
 		Command:    []string{"left takes precedence"},
-		Files:      []string{"../../le/f/t"}, // Must be POSIX.
-		IsolateDir: absPath1,
+		Files:      []string{"../../le/f/t", "foo"}, // Must be POSIX.
+		IsolateDir: absToOS("/tmp/bar"),             // In native path.
 	}
 	right := &ConfigSettings{
 		Files:      []string{"../ri/g/ht"},
-		IsolateDir: absPath2,
+		IsolateDir: absToOS("/var/lib"),
 	}
 
 	out, err := left.union(right)
 	ut.AssertEqual(t, nil, err)
 	ut.AssertEqual(t, left.Command, out.Command)
 	ut.AssertEqual(t, left.IsolateDir, out.IsolateDir)
-	ut.AssertEqual(t, append(getAbsoluteFilenames(left), getAbsoluteFilenames(right)...),
-		getAbsoluteFilenames(out))
+	ut.AssertEqual(t, absToOS("/tmp/bar"), left.IsolateDir)
+	ut.AssertEqual(t, []string{"../../le/f/t", "../../var/ri/g/ht", "foo"}, out.Files)
+}
+
+func TestConfigSettingsUnionRight(t *testing.T) {
+	left := &ConfigSettings{
+		Files:      []string{"../../le/f/t", "foo"}, // Must be POSIX.
+		IsolateDir: absToOS("/tmp/bar"),             // In native path.
+	}
+	right := &ConfigSettings{
+		Command:    []string{"right takes precedence"},
+		Files:      []string{"../ri/g/ht"},
+		IsolateDir: absToOS("/var/lib"),
+	}
+
+	out, err := left.union(right)
+	ut.AssertEqual(t, nil, err)
+	ut.AssertEqual(t, right.Command, out.Command)
+	ut.AssertEqual(t, right.IsolateDir, out.IsolateDir)
+	ut.AssertEqual(t, absToOS("/var/lib"), right.IsolateDir)
+	ut.AssertEqual(t, []string{"../../le/f/t", "../../tmp/bar/foo", "../ri/g/ht"}, out.Files)
 }
 
 // Helper functions.
 
-var absPath1 string
-var absPath2 string
-
-func init() {
-	absPath1 = "/tmp/bar/"
-	absPath2 = "/var/lib/"
+// absToOS converts a POSIX path to OS specific format.
+func absToOS(p string) string {
 	if common.IsWindows() {
-		absPath1 = "E:\\tmp\\bar\\"
-		absPath2 = "X:\\var\\lib\\"
+		return "e:" + strings.Replace(p, "/", "\\", -1)
 	}
-}
-
-func getAbsoluteFilenames(c *ConfigSettings) []string {
-	files := []string{}
-	for _, f := range c.Files {
-		files = append(files, path.Join(c.IsolateDir, f))
-	}
-	sort.Strings(files)
-	return files
+	return p
 }
 
 // makeVVs simplifies creating variableValue:

client/isolate/isolate_test.go
@@ -61,7 +61,7 @@ func TestArchive(t *testing.T) {
 	//   /link -> /base/bar
 	// Result:
 	//   /baz.isolated
-	tmpDir, err := ioutil.TempDir("", "archiver")
+	tmpDir, err := ioutil.TempDir("", "isolate")
 	ut.AssertEqual(t, nil, err)
 	defer func() {
 		if err := os.RemoveAll(tmpDir); err != nil {