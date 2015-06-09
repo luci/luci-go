949ebae65020a7fb125fdf779e57cd80b97be89b
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2015-06-09 10:18:13 +0900

    Add data structure for AuthDB.
    Data structure to store replication.AuthDB protobuf to
    appengine.datastore.
    
    R=maruel@chromium.org, vadimsh@chromium.org
    
    Review URL: https://codereview.chromium.org/1142303006

---
+++ server/common/auth/model/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package model describes the structure used to model AuthDB relations.
+// Note that only have data structure to be replica.
+// See Also:
+// https://github.com/luci/luci-py/blob/master/appengine/components/components/auth/model.py
+package model
