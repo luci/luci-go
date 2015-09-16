76e232bc7e5f084b21ce5981d0382d3dc13e8db1
dnj <dnj@chromium.org>
2015-09-16 13:15:03 -0700

    Add Milo annotations protobuf.
    BUG=
    
    Review URL: https://codereview.chromium.org/1338423002

---
+++ common/proto/milo/generate.go
@@ -0,0 +1,17 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//go:generate protoc --go_out=. annotations.proto
+
+// Package milo contains Milo protobuf definitions.
+//
+// The package name here must match the protobuf package name, as the generated
+// files will reside in the same directory.
+package milo
+
+import (
+	"github.com/golang/protobuf/proto"
+)
+
+var _ = proto.Marshal
