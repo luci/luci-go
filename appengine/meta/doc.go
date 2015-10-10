ef3be35160080fdcc83064f73d0c9807cbe72a45
iannucci <iannucci@chromium.org>
2015-10-09 18:44:27 -0700

    Move appengine/meta lib from infra.git
    R=dnj@chromium.org, estaab@chromium.org, maruel@chromium.org, tandrii@chromium.org, vadimsh@chromium.org
    BUG=533020
    
    Review URL: https://codereview.chromium.org/1400433004

---
+++ appengine/meta/doc.go
@@ -0,0 +1,10 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package meta contains some methods for interacting with GAE's metadata APIs.
+// It only contains an implementation for those metadata APIs we've needed so
+// far, but should be extended to support new ones in the case that we use them.
+//
+// See metadata docs: https://cloud.google.com/appengine/docs/python/datastore/metadataentityclasses#EntityGroup
+package meta
