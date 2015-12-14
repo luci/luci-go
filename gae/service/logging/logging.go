7006afeb66ddd07d1c235dc902d467d06dde6a87
iannucci <iannucci@chromium.org>
2015-12-14 00:52:34 -0800

    Add logging service documentation.
    Just points to github.com/luci/luci-go/common/logging, but explains how that
    works and meshes with the rest of the service definitions.
    
    R=dnj@chromium.org, estaab@chromium.org, vadimsh@chromium.org
    BUG=https://github.com/luci/gae/issues/22
    
    Review URL: https://codereview.chromium.org/1526453002

---
+++ gae/service/logging/logging.go
@@ -0,0 +1,11 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package logging is simply documentation :)
+//
+// In order to use logging, please import and use the
+// "github.com/luci/luci-go/common/logging" package. Both
+// "github.com/luci/gae/impl/prod" and "github.com/luci/gae/impl/memory"
+// implement that service appropriately.
+package logging
