d956cd41333cf3fe16f1c3f33ee0df504ed62137
martiniss <martiniss@chromium.org>
2015-12-21 12:58:46 -0800

    MILO model definitions.
    BUG=
    
    Review URL: https://codereview.chromium.org/1510413003

---
+++ appengine/cmd/milo/model/build_test.go
@@ -0,0 +1,112 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package model
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/luci/gae/impl/memory"
+	"github.com/luci/gae/service/datastore"
+	"github.com/luci/luci-go/common/clock/testclock"
+	. "github.com/luci/luci-go/common/testing/assertions"
+	. "github.com/smartystreets/goconvey/convey"
+	"golang.org/x/net/context"
+)
+
+var _ = fmt.Print
+
+func TestBuildRoot(t *testing.T) {
+	t.Parallel()
+
+	Convey("Build", t, func() {
+		c := memory.Use(context.Background())
+		ds := datastore.Get(c)
+		buildRootIdx := datastore.IndexDefinition{
+			Kind:     "Build",
+			Ancestor: true,
+			SortBy: []datastore.IndexColumn{
+				{
+					Property:   "revisions.generation",
+					Descending: true,
+				},
+			},
+		}
+		ds.Testable().AddIndexes(&buildRootIdx)
+
+		b := GetBuildRoot(c, "master.chromium.linux", "linux_android_rel_ng")
+
+		// Subtract 7 seconds because testclock has nanoseconds, and we ignore
+		// nano-seconds in the datastore (only store milliseconds).
+		t := TimeID{
+			Time: testclock.TestTimeUTC.Add(-time.Nanosecond * 7),
+		}
+
+		build := &Build{
+			ExecutionTime: t,
+			BuildRoot:     b.Key,
+			Revisions: []revisionKey{
+				{Generation: 1},
+			},
+		}
+
+		build2 := &Build{
+			ExecutionTime: TimeID{t.Add(time.Second)},
+			BuildRoot:     b.Key,
+			Revisions: []revisionKey{
+				{Generation: 2},
+			},
+		}
+
+		So(ds.Put(build), ShouldBeNil)
+		So(ds.Put(build2), ShouldBeNil)
+		ds.Testable().CatchupIndexes()
+
+		Convey("BuildRoot", func() {
+			Convey("GetBuilds", func() {
+				q := b.GetBuilds()
+
+				Convey("returns a valid query", func() {
+					builds := make([]*Build, 0, 2)
+					So(ds.GetAll(q, &builds), ShouldBeNil)
+				})
+			})
+		})
+
+		Convey("GetBuilds", func() {
+			b2 := GetBuildRoot(c, "master.foo", "bar_baz_bam")
+
+			otherBuild := &Build{
+				ExecutionTime: t,
+				BuildRoot:     b2.Key,
+				Revisions: []revisionKey{
+					{Generation: 3},
+				},
+			}
+			So(ds.Put(otherBuild), ShouldBeNil)
+
+			sources := []BuildRoot{b, b2}
+			Convey("less than total", func() {
+				rslt, err := GetBuilds(c, sources, 1)
+				So(err, ShouldBeNil)
+
+				So(rslt, ShouldResembleV, [][]*Build{
+					{build2},
+					{otherBuild},
+				})
+			})
+
+			Convey("total", func() {
+				rslt, err := GetBuilds(c, sources, 2)
+				So(err, ShouldBeNil)
+
+				So(rslt, ShouldResembleV, [][]*Build{
+					{build2, build},
+					{otherBuild},
+				})
+			})
+		})
+	})
+}
---
+++ appengine/cmd/milo/model/revision.go
@@ -0,0 +1,85 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package model
+
+import (
+	"github.com/luci/gae/service/datastore"
+	"golang.org/x/net/context"
+)
+
+// Repository represents a repository that a Revision belongs to.
+type Repository struct {
+	datastore.Key
+}
+
+// GetRepository returns the repository object for a given repository URL.
+// TODO(martiniss): convert this to luci project name by 2016-01
+func GetRepository(c context.Context, repositoryURL string) *Repository {
+	ds := datastore.Get(c)
+	return &Repository{
+		*ds.NewKey("Repository", repositoryURL, 0, nil),
+	}
+}
+
+// GetRevision returns the corresponding Revision object for a particular
+// revision hash in this repository.
+func (r *Repository) GetRevision(c context.Context, digest string) (*Revision, error) {
+	ds := datastore.Get(c)
+
+	rev := &Revision{
+		Digest:     digest,
+		Repository: &r.Key,
+	}
+
+	err := ds.Get(rev)
+	return rev, err
+}
+
+// RevisionMetadata is the metadata associated with a particular Revision.
+type RevisionMetadata struct {
+	// Message is the commit message for a Revision.
+	Message string
+}
+
+// Revision is a immutable reference to a version of the code in a given
+// repository at a given point in time.
+//
+// Note that currently revisions have no notion of branch. This will need to
+// change once we support Chromium.
+type Revision struct {
+	// Digest is the content hash which uniquely identifies this Revision, in the
+	// context of its repository.
+	Digest string `gae:"$id"`
+
+	// Repository is the repository this Revision is associated with. See the
+	// Repository struct for more info.
+	Repository *datastore.Key `gae:"$parent"`
+
+	// Metadata is any metadata (commit message, files changed, etc.)
+	// associated with this Revision.
+	Metadata RevisionMetadata `gae:",noindex"`
+
+	// Committer is the email of the user who committed this change.
+	Committer string
+
+	// Generation is the generation number of this Revision. This is used to sort
+	// commits into a roughly time-based order, without using timestamps.
+	// See http://www.spinics.net/lists/git/msg161165.html for background.
+	Generation int
+}
+
+// revisionKey is a small struct used to refer to a particular Revision by a
+// Build. Its main use is allowing Builds to be sorted by generation number.
+type revisionKey struct {
+	Repository string `gae:"repo"`
+	Digest     string `gae:"hash,noindex"`
+	Generation int    `gae:"generation"`
+}
+
+// Get returns the corresponding Revision for this RevisionKey.
+func (r *revisionKey) GetKey(c context.Context) *datastore.Key {
+	ds := datastore.Get(c)
+	return ds.MakeKey("Revision", r.Digest, 0, GetRepository(c, r.Repository))
+}
