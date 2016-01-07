29852af6103103312029fe0d3c85e50080b7dbc3
nodir <nodir@chromium.org>
2016-01-07 09:07:18 -0800

    common/proto: add config proto files
    These .proto files are copied from
    https://github.com/luci/luci-py/tree/45f3e27/appengine/components/components/config/proto
    
    R=vadimsh@chromium.org
    BUG=#8
    
    Review URL: https://codereview.chromium.org/1559633003

---
+++ common/proto/config/generate.go
@@ -0,0 +1,19 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package config contains luci-config protobuf definitions.
+//
+// The .proto files are copied from
+// https://github.com/luci/luci-py/tree/45f3e27/appengine/components/components/config/proto
+// with package name changed to "config" because golint does not like
+// underscore in package names.
+package config
+
+import (
+	"github.com/golang/protobuf/proto"
+)
+
+var _ = proto.Marshal
+
+//go:generate protoc --go_out=. project_config.proto service_config.proto
