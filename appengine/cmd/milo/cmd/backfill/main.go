ec3a72c7e559a472cecd53bf34886d09dfc49434
martiniss <martiniss@chromium.org>
2016-01-21 12:55:33 -0800

    milo: buildbot backfiller flags and de-dup
    Adds the dryrun flag, which causes the backfiller to not change the datastore
    if it is true. Also adds the useBuildbot flag, which allows the backfiller to
    his the buildbot master directly, if CBE does not have the data.
    
    Also de duplicates the revisions for a given repository for a build.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1596523006

--- appengine/cmd/milo/cmd/backfill/main.go
+++ appengine/cmd/milo/cmd/backfill/main.go
@@ -7,19 +7,85 @@ package main
 import (
 	"os"
 
-	"github.com/luci/luci-go/appengine/cmd/milo/cmd/backfill/buildbot"
+	"github.com/luci/gae/impl/prod"
+	"github.com/luci/luci-go/appengine/cmd/milo/collectors/buildbot"
+	log "github.com/luci/luci-go/common/logging"
+	"github.com/luci/luci-go/common/logging/gologger"
 	"github.com/maruel/subcommands"
+	gol "github.com/op/go-logging"
+	"golang.org/x/net/context"
+	// Imported so you can upload this as a "module" to app engine, and access
+	// the remote API.
+	_ "google.golang.org/appengine/remote_api"
 )
 
 var application = &subcommands.DefaultApplication{
 	Name:  "backfill",
 	Title: "Backfill Build and Revision data into the milo backend from various data sources.",
 	Commands: []*subcommands.Command{
-		buildbot.Cmd,
+		buildBotCmd,
 		subcommands.CmdHelp,
 	},
 }
 
 func main() {
 	os.Exit(subcommands.Run(application, nil))
+}
+
+// commandRunBase is the base of all backfill subcommands. It defines the common
+// flags dryRun and remoteURL
+type commandRunBase struct {
+	subcommands.CommandRunBase
+	dryRun    bool
+	remoteURL string
+}
+
+func (c *commandRunBase) Init() {
+	c.Flags.StringVar(&c.remoteURL, "remote-url", "luci-milo.appspot.com", "the URL of the server to connect to via the remote API. Do NOT include the protocol (\"https://\")")
+	c.Flags.BoolVar(&c.dryRun, "dryrun", true, "if this run is a dryrun, and should make no modifications to the datastore.")
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Buildbot
+
+var buildBotCmd = &subcommands.Command{
+	UsageLine: "buildbot",
+	ShortDesc: "runs the buildbot backfiller",
+	LongDesc:  "Runs the buildbot backfiller. This hits chrome build extract, and uploads the data from that into the Cloud datastore, as backed by the remoteURL.",
+	CommandRun: func() subcommands.CommandRun {
+		c := &cmdBuildBotRun{}
+		c.Init()
+		c.Flags.StringVar(&c.master, "master", "chromium.win", "the master to upload data for")
+		c.Flags.BoolVar(&c.buildbotFallback, "buildbot-fallback", false, "if getting the data from CBE fails, get the json data directly from the buildbot master")
+		return c
+	},
+}
+
+type cmdBuildBotRun struct {
+	commandRunBase
+	master           string
+	buildbotFallback bool
+}
+
+func (c *cmdBuildBotRun) Run(a subcommands.Application, args []string) int {
+	cfg := gologger.LoggerConfig{
+		Format: "%{message}",
+		Level:  gol.INFO,
+		Out:    os.Stdout,
+	}
+	ctx := cfg.Use(context.Background())
+
+	err := prod.UseRemote(&ctx, c.remoteURL, nil)
+	if err != nil {
+		log.Errorf(ctx, "%s", err)
+		return 1
+	}
+
+	err = buildbot.PopulateMaster(ctx, c.master, c.dryRun, c.buildbotFallback)
+	if err != nil {
+		log.Errorf(ctx, "%s", err)
+		return 1
+	}
+
+	return 0
 }
--- appengine/cmd/milo/collectors/buildbot/collector.go
+++ appengine/cmd/milo/collectors/buildbot/collector.go
@@ -27,6 +27,7 @@ const workPoolSizeMultiplier = 2
 type changeJSON struct {
 	Repository string `json:"repository"`
 	Digest     string `json:"revision"`
+	UnixTime   int64  `json:"when"`
 }
 
 type sourceJSON struct {
@@ -42,45 +43,72 @@ type buildJSON struct {
 	Error       string     `json:"error"`
 	Logs        [][]string `json:"logs"`
 	SourceStamp sourceJSON `json:"sourceStamp"`
+	Number      int        `json:"number"`
+	Text        []string   `json:"text"`
+}
+
+func getRevForChange(c context.Context, change changeJSON) model.RevisionInfo {
+	repo := model.GetRepository(c, change.Repository)
+
+	generation := -1
+	if rev, err := repo.GetRevision(c, change.Digest); err == nil {
+		generation = rev.Generation
+	}
+
+	return model.RevisionInfo{
+		Repository: change.Repository,
+		Digest:     change.Digest,
+		Generation: generation,
+	}
 }
 
 func (b *buildJSON) toDS(c context.Context) *model.Build {
-	revisions := make([]model.RevisionInfo, len(b.SourceStamp.Changes))
-	parallel.FanOutIn(func(ch chan<- func() error) {
-		for i, change := range b.SourceStamp.Changes {
-			i := i
-			change := change
-			ch <- func() error {
-				repo := model.GetRepository(c, change.Repository)
-				rev, err := repo.GetRevision(c, change.Digest)
-				if err != nil {
-					// Just ignore the error, and put a revision key with a dummy
-					// generation number for now. This will be over-written later, if
-					// the revision gets put into the datastore and the backfiller is
-					// run again.
-					revisions[i] = model.RevisionInfo{
-						Repository: change.Repository,
-						Digest:     change.Digest,
-						Generation: -1,
-					}
-					return nil
-				}
-
-				revisions[i] = model.RevisionInfo{
-					Repository: rev.Repository.StringID(),
-					Digest:     rev.Digest,
-					Generation: rev.Generation,
-				}
-				return nil
+	perRepo := make(map[string]*changeJSON)
+	for _, change := range b.SourceStamp.Changes {
+		current := perRepo[change.Repository]
+		if current != nil {
+			if current.UnixTime < change.UnixTime {
+				perRepo[change.Repository] = &change
 			}
+		} else {
+			perRepo[change.Repository] = &change
 		}
-	})
+	}
+
+	revisions := make([]model.RevisionInfo, 0, len(perRepo))
+	for _, change := range perRepo {
+		revisions = append(revisions, getRevForChange(c, *change))
+	}
+
+	logKey := ""
+	if len(b.Logs) > 1 && len(b.Logs[1]) > 1 {
+		logKey = b.Logs[1][1]
+	} else {
+		log.Warningf(c, "build %d on %s master %s had weird logs\n", b.Number, b.Builder, b.Master)
+	}
+
+	userStatus := "UNKNOWN"
+	if len(b.Text) < 2 {
+		log.Warningf(c, "build %d on %s master %s had strange text\n", b.Number, b.Builder, b.Master)
+	} else {
+		if b.Text[0] == "exception" {
+			userStatus = "EXCEPTION"
+		} else if b.Text[0] == "failed" {
+			userStatus = "FAILURE"
+		} else if b.Text[1] == "successful" {
+			userStatus = "SUCCESS"
+		}
+		if userStatus == "UNKNOWN" {
+			log.Warningf(c, "build %d on %s master %s had unknown text %v\n", b.Number, b.Builder, b.Master, b.Text)
+		}
+	}
 
 	return &model.Build{
 		ExecutionTime: model.TimeID{time.Unix(int64(b.Times[0]), 0).UTC()},
 		BuildRoot:     model.GetBuildRoot(c, b.Master, b.Builder).Key,
-		BuildLogKey:   b.Logs[1][1],
+		BuildLogKey:   logKey,
 		Revisions:     revisions,
+		UserStatus:    userStatus,
 	}
 }
 
@@ -92,10 +120,19 @@ type masterJSON struct {
 	Builders map[string]builderJSON `json:"builders"`
 }
 
-func getBuild(master, builder string, build int) (*buildJSON, error) {
-	resp, err := http.Get(strings.Join(
-		[]string{BuildExtractURL, "p", master, "builders", builder, "builds", fmt.Sprintf("%d", build)},
-		"/") + "?json=1")
+func getBuild(master, builder string, build int, useCBE bool) (*buildJSON, error) {
+	resp := (*http.Response)(nil)
+	err := (error)(nil)
+	if useCBE {
+		resp, err = http.Get(strings.Join(
+			[]string{BuildExtractURL, "p", master, "builders", builder, "builds", fmt.Sprintf("%d", build)},
+			"/") + "?json=1")
+	} else {
+		resp, err = http.Get(strings.Join(
+			[]string{"https://build.chromium.org", "p", master, "json", "builders", builder, "builds", fmt.Sprintf("%d", build)},
+			"/"))
+	}
+
 	if err != nil {
 		return nil, err
 	}
@@ -119,7 +156,7 @@ func getBuild(master, builder string, build int) (*buildJSON, error) {
 // It hits CBE to grab the json, and then creates corresponding entities and
 // puts them into the datastore. It assumes that a datastore impl exists in the
 // supplied context.
-func PopulateMaster(c context.Context, master string) error {
+func PopulateMaster(c context.Context, master string, dryRun, buildbotFallback bool) error {
 	masterJSON := &masterJSON{}
 	resp, err := http.Get(strings.Join([]string{BuildExtractURL, "get_master", master}, "/"))
 	if err != nil {
@@ -137,19 +174,30 @@ func PopulateMaster(c context.Context, master string) error {
 	log.Infof(c, "Getting builds for master %s\n", master)
 
 	errors := parallel.WorkPool(workPoolSizeMultiplier*len(masterJSON.Builders), func(ch chan<- func() error) {
-		for name, builder := range masterJSON.Builders {
+		for builderName, builder := range masterJSON.Builders {
 			if len(builder.Builds) > 0 {
-				buildsToPut[name] = make([]*model.Build, len(builder.Builds))
+				buildsToPut[builderName] = make([]*model.Build, len(builder.Builds))
 
 				for ind, builderNum := range builder.Builds {
 					ind := ind
 					builderNum := builderNum
-					builderName := name
+					builderName := builderName
+
 					ch <- func() error {
-						res, err := getBuild(master, builderName, builderNum)
+						res, err := getBuild(master, builderName, builderNum, true)
 						if err != nil {
-							log.Warningf(c, "got %s", err)
-							return err
+							if !buildbotFallback {
+								log.Warningf(c, "got %s, giving up", err)
+								return err
+							}
+
+							log.Warningf(c, "got %s, retrying without CBE", err)
+							res, err = getBuild(master, builderName, builderNum, false)
+
+							if err != nil {
+								log.Warningf(c, "got %s, giving up", err)
+								return err
+							}
 						}
 
 						conv := res.toDS(c)
@@ -157,7 +205,7 @@ func PopulateMaster(c context.Context, master string) error {
 						return nil
 					}
 				}
-				log.Infof(c, "Queued builds for builder %s.", name)
+				log.Infof(c, "Queued builds for builder %s.", builderName)
 			}
 		}
 	})
@@ -196,7 +244,11 @@ func PopulateMaster(c context.Context, master string) error {
 	for builderName, toPut := range buildsToPut {
 		log.Infof(c, "%d to put for builder %s\n", len(toPut), builderName)
 		if toPut != nil {
-			ds.PutMulti(toPut)
+			if dryRun {
+				log.Infof(c, "dry run, not putting any modifications")
+			} else {
+				ds.PutMulti(toPut)
+			}
 		}
 	}
 	return nil
