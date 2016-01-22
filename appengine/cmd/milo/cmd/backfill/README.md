ef2a0c1e0ad5be713bca407611a58d37a7746a1b
estaab <estaab@chromium.org>
2016-01-22 15:05:45 -0800

    Milo backend: git Revision entity backfiller.
    BUG=
    
    Review URL: https://codereview.chromium.org/1603883005

--- appengine/cmd/milo/cmd/backfill/README.md
+++ appengine/cmd/milo/cmd/backfill/README.md
@@ -14,6 +14,15 @@ information to the build, but with a generation number of -1.
 _Note_: NOT for production use, since it depends on CBE. This will hopefully be
 deleted after we have tranisitioned off buildbot, and onto buildbucket/DM.
 
+### git
+
+The `git` subcommand reads output from git log and writes the results to the
+Revision entity group in datastore, including generation numbers. The git log
+should be generated from running:
+`git log --topo-order --reverse -z --format=format:'%H,%P,%ae,%ct,%b'`
+
+This should be done from a clone without local edits.
+
 ## Enabling remote API
 You need to deploy some module to app engine which has the remote api enabled
 in order for this backfiller to work successfully.
--- appengine/cmd/milo/cmd/backfill/main.go
+++ appengine/cmd/milo/cmd/backfill/main.go
@@ -5,10 +5,14 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/luci/gae/impl/prod"
 	"github.com/luci/luci-go/appengine/cmd/milo/collectors/buildbot"
+	"github.com/luci/luci-go/appengine/cmd/milo/collectors/git"
+	"github.com/luci/luci-go/appengine/cmd/milo/model"
 	log "github.com/luci/luci-go/common/logging"
 	"github.com/luci/luci-go/common/logging/gologger"
 	"github.com/maruel/subcommands"
@@ -24,6 +28,7 @@ var application = &subcommands.DefaultApplication{
 	Title: "Backfill Build and Revision data into the milo backend from various data sources.",
 	Commands: []*subcommands.Command{
 		buildBotCmd,
+		gitCmd,
 		subcommands.CmdHelp,
 	},
 }
@@ -83,6 +88,83 @@ func (c *cmdBuildBotRun) Run(a subcommands.Application, args []string) int {
 
 	err = buildbot.PopulateMaster(ctx, c.master, c.dryRun, c.buildbotFallback)
 	if err != nil {
+		log.Errorf(ctx, "%s", err)
+		return 1
+	}
+
+	return 0
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Git
+
+var gitCmd = &subcommands.Command{
+	UsageLine: "git",
+
+	ShortDesc: "runs the local git backfiller",
+
+	LongDesc: "Runs the local git backfiller. Reads history from a local git repo and uploads revision info to cloud datastore at remoteURL.",
+
+	CommandRun: func() subcommands.CommandRun {
+		c := &cmdGitRun{}
+		c.Init()
+		c.Flags.StringVar(&c.gitLog, "git-log", "", "file containing output of git log --topo-order --reverse -z --format=format:'%H,%P,%ae,%ct,%b'")
+		c.Flags.StringVar(&c.repoURL, "repo-url", "", "the repo URL in the Revision entity group")
+		return c
+	},
+}
+
+type cmdGitRun struct {
+	commandRunBase
+	gitLog  string
+	repoURL string
+}
+
+func (c *cmdGitRun) Run(a subcommands.Application, args []string) int {
+	cfg := gologger.LoggerConfig{
+		Format: "%{message}",
+		Level:  gol.INFO,
+		Out:    os.Stdout,
+	}
+	ctx := cfg.Use(context.Background())
+
+	if c.gitLog == "" || c.remoteURL == "" || c.repoURL == "" {
+		log.Errorf(ctx, "Flags -git-log, -remote-url, and -repo-url must all be set.")
+		return 1
+	}
+
+	// We need a gae context even in --dry-run mode to get the repository datastore key.
+	if err := prod.UseRemote(&ctx, c.remoteURL, nil); err != nil {
+		log.Errorf(ctx, "%s (remote URL: %s)", err, c.remoteURL)
+		return 1
+	}
+
+	contents, err := ioutil.ReadFile(c.gitLog)
+	if err != nil {
+		log.Errorf(ctx, "%s", err)
+		return 1
+	}
+
+	revisions, err := git.GetRevisions(string(contents))
+	if err != nil {
+		log.Errorf(ctx, "%s", err)
+		return 1
+	}
+
+	for _, revision := range revisions {
+		revision.Repository = &model.GetRepository(ctx, c.repoURL).Key
+	}
+
+	if c.dryRun {
+		log.Infof(ctx, "Running in dry run mode, writing to stdout.")
+		for _, r := range revisions {
+			fmt.Printf("%+v\n", *r)
+		}
+		return 0
+	}
+
+	log.Infof(ctx, "Saving %d revisions.", len(revisions))
+	if err := git.SaveRevisions(ctx, revisions); err != nil {
 		log.Errorf(ctx, "%s", err)
 		return 1
 	}
