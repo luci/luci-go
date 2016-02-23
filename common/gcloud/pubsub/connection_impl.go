2a67e1031bbf417c1947703a9ded15e54cad76ae
dnj <dnj@chromium.org>
2016-02-23 14:35:42 -0800

    Enforce Context cancellation in Pub/Sub layer.
    The Pub/Sub library does not bind outgoing requests to the supplied
    Context, meaning that they can block indefinitely even when the Context
    is canceled. This wraps those calls with a Context-senstive goroutine so
    that they will locally return prematurely if the Context is canceled.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1722393002

--- common/gcloud/pubsub/connection_impl.go
+++ common/gcloud/pubsub/connection_impl.go
@@ -38,8 +38,15 @@ func (p *connectionImpl) TopicExists(c context.Context, t Topic) (bool, error) {
 		return false, err
 	}
 
-	exists, err := pubsub.TopicExists(p.with(c, proj), name)
-	return exists, (err)
+	var exists bool
+	err = contextAwareCall(c, func(c context.Context) (err error) {
+		exists, err = pubsub.TopicExists(p.with(c, proj), name)
+		return
+	})
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
 }
 
 func (p *connectionImpl) SubExists(c context.Context, s Subscription) (bool, error) {
@@ -48,8 +55,15 @@ func (p *connectionImpl) SubExists(c context.Context, s Subscription) (bool, err
 		return false, err
 	}
 
-	exists, err := pubsub.SubExists(p.with(c, proj), name)
-	return exists, errors.WrapTransient(err)
+	var exists bool
+	err = contextAwareCall(c, func(c context.Context) (err error) {
+		exists, err = pubsub.SubExists(p.with(c, proj), name)
+		return
+	})
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
 }
 
 func (p *connectionImpl) Publish(c context.Context, t Topic, msgs ...*Message) ([]string, error) {
@@ -58,8 +72,21 @@ func (p *connectionImpl) Publish(c context.Context, t Topic, msgs ...*Message) (
 		return nil, err
 	}
 
-	ids, err := pubsub.Publish(p.with(c, proj), name, localMessageToPubSub(msgs)...)
-	return ids, errors.WrapTransient(err)
+	// Check if our Context has finished. Currently, the pubsub library does not
+	// interrupt calls on Context cancellation.
+	if err := c.Err(); err != nil {
+		return nil, err
+	}
+
+	var ids []string
+	err = contextAwareCall(c, func(c context.Context) (err error) {
+		ids, err = pubsub.Publish(p.with(c, proj), name, localMessageToPubSub(msgs)...)
+		return
+	})
+	if err != nil {
+		return nil, err
+	}
+	return ids, nil
 }
 
 func (p *connectionImpl) Pull(c context.Context, s Subscription, n int) ([]*Message, error) {
@@ -68,8 +95,15 @@ func (p *connectionImpl) Pull(c context.Context, s Subscription, n int) ([]*Mess
 		return nil, err
 	}
 
-	msgs, err := pubsub.Pull(p.with(c, proj), name, n)
-	return pubSubMessageToLocal(msgs), errors.WrapTransient(err)
+	var msgs []*pubsub.Message
+	err = contextAwareCall(c, func(c context.Context) (err error) {
+		msgs, err = pubsub.Pull(p.with(c, proj), name, n)
+		return
+	})
+	if err != nil {
+		return nil, err
+	}
+	return pubSubMessageToLocal(msgs), nil
 }
 
 func (p *connectionImpl) Ack(c context.Context, s Subscription, ackIDs ...string) error {
@@ -78,9 +112,40 @@ func (p *connectionImpl) Ack(c context.Context, s Subscription, ackIDs ...string
 		return err
 	}
 
-	return errors.WrapTransient(pubsub.Ack(p.with(c, proj), name, ackIDs...))
+	return contextAwareCall(c, func(c context.Context) error {
+		return pubsub.Ack(p.with(c, proj), name, ackIDs...)
+	})
 }
 
 func (p *connectionImpl) with(c context.Context, project string) context.Context {
 	return cloud.WithContext(c, project, p.client)
+}
+
+// contextAwareCall invokes the supplied function, f, and returns with either
+// f's error code or the Context's finished error code, whichever happens
+// first.
+//
+// Note that if f has side effects, they may still happen after this function
+// has returned due to Context completion, since nothing can abort f's execution
+// once executed. It is important to ensure that if this method returns an
+// error value, it is checked immediately, and that any data that f touches is
+// only consumed if this method returns nil.
+func contextAwareCall(c context.Context, f func(context.Context) error) error {
+	errC := make(chan error, 1)
+
+	go func() {
+		defer close(errC)
+		errC <- f(c)
+	}()
+
+	select {
+	case <-c.Done():
+		// Return immediately. Our "f" will finish and have its error status
+		// ignored.
+		return c.Err()
+
+	case err := <-errC:
+		// We currently treat all Pub/Sub errors as transient.
+		return errors.WrapTransient(err)
+	}
 }
