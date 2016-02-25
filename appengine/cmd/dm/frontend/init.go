6647d46444300f295b0645b59cd65d47896845dd
iannucci <iannucci@chromium.org>
2016-02-25 11:09:20 -0800

    Enable luci-config integration, as well as auth, etc.
    R=dnj@chromium.org, vadimsh@chromium.org
    BUG=550684
    
    Review URL: https://codereview.chromium.org/1739583002

--- appengine/cmd/dm/frontend/init.go
+++ appengine/cmd/dm/frontend/init.go
@@ -7,21 +7,56 @@ package frontend
 import (
 	"net/http"
 
+	"golang.org/x/net/context"
+
+	"google.golang.org/appengine"
+
 	"github.com/julienschmidt/httprouter"
 	"github.com/luci/luci-go/appengine/cmd/dm/deps"
+	gaeauthServer "github.com/luci/luci-go/appengine/gaeauth/server"
+	"github.com/luci/luci-go/appengine/gaeconfig"
+	"github.com/luci/luci-go/appengine/gaemiddleware"
 	"github.com/luci/luci-go/appengine/tumble"
+	"github.com/luci/luci-go/common/config"
+	"github.com/luci/luci-go/common/logging"
 	"github.com/luci/luci-go/server/discovery"
+	"github.com/luci/luci-go/server/middleware"
 	"github.com/luci/luci-go/server/prpc"
+	adminSettings "github.com/luci/luci-go/server/settings/admin"
 )
 
 var myTumble = tumble.DefaultConfig()
 
+func base(h middleware.Handler) httprouter.Handle {
+	newH := func(c context.Context, rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		cfg, err := gaeconfig.New(c)
+		switch err {
+		case nil:
+			c = config.Set(c, cfg)
+		case gaeconfig.ErrNotConfigured:
+			logging.Warningf(c, "luci-config service url not configured. Configure this at /admin/settings/gaeconfig.")
+		default:
+			panic(err)
+		}
+		h(c, rw, r, p)
+	}
+	if !appengine.IsDevAppServer() {
+		newH = middleware.WithPanicCatcher(newH)
+	}
+	return gaemiddleware.BaseProd(newH)
+}
+
 func init() {
 	router := httprouter.New()
+
 	svr := prpc.Server{}
 	deps.RegisterDepsServer(&svr)
 	discovery.Enable(&svr)
 
+	svr.InstallHandlers(router, base)
 	myTumble.InstallHandlers(router)
+	gaeauthServer.InstallHandlers(router, base)
+	adminSettings.InstallHandlers(router, base, &gaeauthServer.UsersAPIAuthMethod{})
+
 	http.Handle("/", router)
 }
