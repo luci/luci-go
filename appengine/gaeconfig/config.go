0c8308ed231e5925170809304e261843b21b17dc
iannucci <iannucci@chromium.org>
2016-02-25 18:44:20 -0800

    Make gaeconfig use proccache.
    R=dnj@chromium.org, nodir@chromium.org, vadimsh@chromium.org
    BUG=550684
    
    Review URL: https://codereview.chromium.org/1734253002

--- appengine/gaeconfig/config.go
+++ appengine/gaeconfig/config.go
@@ -5,17 +5,22 @@
 package gaeconfig
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"time"
 
-	mc "github.com/luci/gae/service/memcache"
+	"github.com/luci/gae/service/datastore"
+	"github.com/luci/gae/service/memcache"
+	"github.com/luci/luci-go/common/clock"
 	"github.com/luci/luci-go/common/config"
 	"github.com/luci/luci-go/common/config/filters/caching"
 	log "github.com/luci/luci-go/common/logging"
+	"github.com/luci/luci-go/server/proccache"
 	"golang.org/x/net/context"
 )
 
-// AddCacheFilter adds a memcache-caching config filter to the context.
+// AddCacheFilter adds a proccache-and-memcache-caching config filter to the context.
 //
 // A Transport from "gaeauth/client" capable of talking to the underlying
 // service should be installed prior to calling this method.
@@ -25,47 +30,79 @@ func AddCacheFilter(c context.Context, expire time.Duration) context.Context {
 	})
 }
 
-// NewCacheFilter returns memcache-caching config filter.
+// NewCacheFilter returns proccache-and-memcache-caching config filter.
 func NewCacheFilter(c context.Context, expire time.Duration) config.Filter {
 	o := caching.Options{
-		Cache: &memCache{
-			Context: c,
-		},
+		Cache:      &cache{},
 		Expiration: expire,
 	}
 	return caching.NewFilter(o)
 }
 
-type memCache struct {
-	Context context.Context
-}
+type cache struct{}
 
-func (c *memCache) Store(ctx context.Context, key string, expire time.Duration, value []byte) {
-	mi := mc.Get(ctx)
-	if err := mi.Set(mi.NewItem(c.key(key)).SetExpiration(expire).SetValue(value)); err != nil {
+type proccacheKey string
+
+func (c *cache) Store(ctx context.Context, baseKey string, expire time.Duration, value []byte) {
+	k := cacheKey(baseKey)
+
+	proccache.Put(ctx, k, value, expire)
+
+	// value in memcache is [varint(expiration_ts.Millis) ++ value]
+	// value in proccache is [value]
+	//
+	// This is because memcache doesn't populate the .Expiration field of the
+	// memcache Item on Get operations :(
+	stamp := datastore.TimeToInt(clock.Now(ctx).UTC().Add(expire))
+	buf := make([]byte, binary.MaxVarintLen64)
+	value = append(buf[:binary.PutVarint(buf, stamp)], value...)
+
+	mc := memcache.Get(ctx)
+	itm := mc.NewItem(string(k)).SetExpiration(expire).SetValue(value)
+	if err := mc.Set(itm); err != nil {
 		log.Fields{
 			log.ErrorKey: err,
-			"key":        key,
+			"key":        baseKey,
 			"expire":     expire,
 		}.Warningf(ctx, "Failed to store cache value.")
 	}
 }
 
-func (c *memCache) Retrieve(ctx context.Context, key string) []byte {
-	item, err := mc.Get(ctx).Get(c.key(key))
-	if err != nil {
-		if err != mc.ErrCacheMiss {
+func (c *cache) Retrieve(ctx context.Context, baseKey string) []byte {
+	k := cacheKey(baseKey)
+	ret, err := proccache.GetOrMake(ctx, k, func() (value interface{}, exp time.Duration, err error) {
+		item, err := memcache.Get(ctx).Get(string(k))
+		if err != nil {
+			if err != memcache.ErrCacheMiss {
+				log.Fields{
+					log.ErrorKey: err,
+					"key":        baseKey,
+				}.Warningf(ctx, "Failed to retrieve memcache value.")
+			}
+			return
+		}
+
+		buf := bytes.NewBuffer(item.Value())
+		expStamp, err := binary.ReadVarint(buf)
+		if err != nil {
 			log.Fields{
 				log.ErrorKey: err,
-				"key":        key,
-			}.Warningf(ctx, "Failed to retrieve cache value.")
+				"key":        baseKey,
+			}.Warningf(ctx, "Failed to decode stamp in memcache value.")
+			return
 		}
+
+		// proccache will ignore this value if exp is in the past
+		exp = datastore.IntToTime(expStamp).Sub(clock.Now(ctx))
+		value = buf.Bytes()
+		return
+	})
+	if err != nil {
 		return nil
 	}
-
-	return item.Value()
+	return ret.([]byte)
 }
 
-func (c *memCache) key(base string) string {
-	return fmt.Sprintf("luci-config:%s", base)
+func cacheKey(baseKey string) proccacheKey {
+	return proccacheKey(fmt.Sprintf("luci-config:v2:%s", baseKey))
 }
---
+++ appengine/gaeconfig/config_test.go
@@ -0,0 +1,99 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package gaeconfig
+
+import (
+	"testing"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"github.com/luci/gae/filter/featureBreaker"
+	"github.com/luci/gae/impl/memory"
+	"github.com/luci/gae/service/memcache"
+	"github.com/luci/luci-go/common/clock/testclock"
+	. "github.com/luci/luci-go/common/testing/assertions"
+	"github.com/luci/luci-go/server/proccache"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCache(t *testing.T) {
+	t.Parallel()
+
+	Convey("Test cache", t, func() {
+		c := context.Background()
+		c = memory.Use(c)
+
+		pc := &proccache.Cache{}
+		c = proccache.Use(c, pc)
+		c, clk := testclock.UseTime(c, testclock.TestTimeUTC)
+		_ = clk
+
+		mc := memcache.Get(c)
+		c, mcFB := featureBreaker.FilterMC(c, nil)
+
+		cache := &cache{}
+
+		Convey("Should be able to store stuff", func() {
+			Convey("memcache+proccache", func() {
+				cache.Store(c, "item", time.Second, []byte("foobar"))
+				itm, err := mc.Get(string(cacheKey("item")))
+				So(err, ShouldBeNil)
+				So(itm.Value(), ShouldResemble, []byte("\xff\xf4\xea\x9c\xf7\xd8\xde\xdc\x01foobar"))
+				val, ok := proccache.Get(c, cacheKey("item"))
+				So(ok, ShouldBeTrue)
+				So(val, ShouldResemble, []byte("foobar"))
+
+				Convey("and get it back", func() {
+					So(cache.Retrieve(c, "item"), ShouldResemble, []byte("foobar"))
+
+					Convey("unless it's expired", func() {
+						clk.Add(time.Second * 2)
+						So(cache.Retrieve(c, "item"), ShouldBeNil)
+					})
+
+					Convey("when missing from proccache", func() {
+						proccache.GetCache(c).Mutate(cacheKey("item"), func(*proccache.Entry) *proccache.Entry {
+							return nil
+						})
+						So(cache.Retrieve(c, "item"), ShouldResemble, []byte("foobar"))
+					})
+
+					Convey("unless memcache is corrupt", func() {
+						proccache.GetCache(c).Mutate(cacheKey("item"), func(*proccache.Entry) *proccache.Entry {
+							return nil
+						})
+						err := mc.Set(mc.NewItem(string(cacheKey("item"))).SetValue([]byte("\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff")))
+						So(err, ShouldBeNil)
+
+						So(cache.Retrieve(c, "item"), ShouldBeNil)
+					})
+
+				})
+			})
+
+			Convey("if memcache is down", func() {
+				mcFB.BreakFeatures(nil, "SetMulti")
+
+				cache.Store(c, "item", time.Second, []byte("foobar"))
+				_, err := mc.Get(string(cacheKey("item")))
+				So(err, ShouldErrLike, memcache.ErrCacheMiss)
+
+				val, ok := proccache.Get(c, cacheKey("item"))
+				So(ok, ShouldBeTrue)
+				So(val, ShouldResemble, []byte("foobar"))
+
+				Convey("and still get it back", func() {
+					So(cache.Retrieve(c, "item"), ShouldResemble, []byte("foobar"))
+
+					Convey("unless it's expired", func() {
+						clk.Add(time.Second * 2)
+						So(cache.Retrieve(c, "item"), ShouldBeNil)
+					})
+				})
+			})
+		})
+	})
+}
