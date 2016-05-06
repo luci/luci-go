09bd02143971653cf8fdb5bc20d0d21732d64f11
martiniss <martiniss@chromium.org>
2016-05-06 12:19:34 -0700

    common/clock: add package doc.
    BUG=
    
    Review-Url: https://codereview.chromium.org/1957733002

--- common/clock/clock.go
+++ common/clock/clock.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Package clock is an interface to system time and timers which is easy to
+// test.
 package clock
 
 import (
