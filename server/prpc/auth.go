aad4198d788acb098ea4227e4b01b30c14be2f03
Adrien Delorme <adrien.delorme@icloud.com>
2016-07-15 17:44:39 +0200

    Revert "server.prpc: RegisterDefaultAuth accepts a nil authenticator now"
    This reverts commit 353caad3d079d28ffce206672bc596026be5b68f.

--- server/prpc/auth.go
+++ server/prpc/auth.go
@@ -19,6 +19,9 @@ var defaultAuth = struct {
 // Server.Authenticator is provided.
 // Panics if a is nil or called twice.
 func RegisterDefaultAuth(a auth.Authenticator) {
+	if a == nil {
+		panic("a is nil")
+	}
 	defaultAuth.Lock()
 	defer defaultAuth.Unlock()
 	if defaultAuth.Authenticator != nil {
