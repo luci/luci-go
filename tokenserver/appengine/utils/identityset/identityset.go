6800e97e14044d27d877951d443aada8b5410063
vadimsh <vadimsh@chromium.org>
2016-10-12 16:27:06 -0700

    tokenserver: Add identityset.Set struct that will be used by delegation rules.
    The delegation rules are formulated in the language of sets ("these rules apply
    to the query iff such and such sets are subsets of the sets specified in the
    rule").
    
    They are also defined as a mix of IDs, groups and special '*' character that
    means "any identity".
    
    This struct implements the corresponding set structure and all operations that
    will be needed later.
    
    R=nodir@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2420493002

---
+++ tokenserver/appengine/utils/identityset/identityset.go
@@ -0,0 +1,233 @@
+// Copyright 2016 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package identityset implements a set-like structure for identity.Identity.
+package identityset
+
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/net/context"
+
+	"github.com/luci/luci-go/server/auth"
+	"github.com/luci/luci-go/server/auth/identity"
+)
+
+type identSet map[identity.Identity]struct{}
+type groupSet map[string]struct{}
+
+// Set is a set of identities represented as sets of IDs and groups.
+//
+// Groups are generally not expanded, but treated as different kind of items,
+// so essentially this struct represents two sets: a set of explicitly specified
+// identities, and a set of groups. The exception to this rule is IsMember
+// function that looks inside the groups.
+type Set struct {
+	All    bool     // if true, this set contains all possible identities
+	IDs    identSet // set of identity.Identity strings
+	Groups groupSet // set of group names
+}
+
+// AddIdentity adds a single identity to the set.
+//
+// The receiver must not be nil.
+func (s *Set) AddIdentity(id identity.Identity) {
+	if !s.All {
+		if s.IDs == nil {
+			s.IDs = make(identSet, 1)
+		}
+		s.IDs[id] = struct{}{}
+	}
+}
+
+// AddGroup adds a single group to the set.
+//
+// The receiver must not be nil.
+func (s *Set) AddGroup(group string) {
+	if !s.All {
+		if s.Groups == nil {
+			s.Groups = make(groupSet, 1)
+		}
+		s.Groups[group] = struct{}{}
+	}
+}
+
+// IsEmpty returns true if this set is empty.
+//
+// 'nil' receiver value is valid and represents an empty set.
+func (s *Set) IsEmpty() bool {
+	return s == nil || (!s.All && len(s.IDs) == 0 && len(s.Groups) == 0)
+}
+
+// IsMember returns true if the given identity is in the set.
+//
+// It looks inside the groups too.
+//
+// 'nil' receiver value is valid and represents an empty set.
+func (s *Set) IsMember(c context.Context, id identity.Identity) (bool, error) {
+	if s == nil {
+		return false, nil
+	}
+
+	if s.All {
+		return true, nil
+	}
+
+	if _, ok := s.IDs[id]; ok {
+		return true, nil
+	}
+
+	if len(s.Groups) != 0 {
+		db := auth.GetState(c).DB()
+		for group := range s.Groups {
+			switch found, err := db.IsMember(c, id, group); {
+			case err != nil:
+				return false, err
+			case found:
+				return true, nil
+			}
+		}
+	}
+
+	return false, nil
+}
+
+// IsSubset returns true if this set if a subset of another set.
+//
+// Two equal sets are considered subsets of each other.
+//
+// It doesn't attempt to expand groups. Compares IDs and Groups sets separately,
+// as independent kinds of entities.
+//
+// 'nil' receiver and argument values are valid and represent empty sets.
+func (s *Set) IsSubset(superset *Set) bool {
+	// An empty set is subset of any other set (including empty sets).
+	if s.IsEmpty() {
+		return true
+	}
+
+	// An empty set is not a superset of any non-empty set.
+	if superset.IsEmpty() {
+		return false
+	}
+
+	// The universal set is subset of only itself.
+	if s.All {
+		return superset.All
+	}
+
+	// The universal set is superset of any other set.
+	if superset.All {
+		return true
+	}
+
+	// Is s.IDs a subset of superset.IDs?
+	if len(superset.IDs) < len(s.IDs) {
+		return false
+	}
+	for id := range s.IDs {
+		if _, ok := superset.IDs[id]; !ok {
+			return false
+		}
+	}
+
+	// Is s.Groups a subset of superset.Groups?
+	if len(superset.Groups) < len(s.Groups) {
+		return false
+	}
+	for group := range s.Groups {
+		if _, ok := superset.Groups[group]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+// IsSuperset returns true if this set is a super set of another set.
+//
+// Two equal sets are considered supersets of each other.
+//
+// 'nil' receiver and argument values are valid and represent empty sets.
+func (s *Set) IsSuperset(subset *Set) bool {
+	return subset.IsSubset(s)
+}
+
+// FromStrings constructs a Set by parsing a slice of strings.
+//
+// Each string is either:
+//  * "<kind>:<id>" identity string.
+//  * "group:<name>" group reference.
+//  * "*" token to mean "All identities".
+func FromStrings(str []string) (*Set, error) {
+	set := &Set{}
+	for _, s := range str {
+		switch {
+		case s == "*":
+			set.All = true
+		case strings.HasPrefix(s, "group:"):
+			gr := strings.TrimPrefix(s, "group:")
+			if gr == "" {
+				return nil, fmt.Errorf("invalid entry %q", s)
+			}
+			set.AddGroup(gr)
+		default:
+			id, err := identity.MakeIdentity(s)
+			if err != nil {
+				return nil, err
+			}
+			set.AddIdentity(id)
+		}
+	}
+	// If '*' was used, separately listed IDs and groups are redundant.
+	if set.All {
+		set.Groups = nil
+		set.IDs = nil
+	}
+	return set, nil
+}
+
+// Union returns a union of a list of sets.
+func Union(sets ...*Set) *Set {
+	estimateIDs := 0
+	estimateGroups := 0
+
+	for _, s := range sets {
+		if s == nil {
+			continue
+		}
+		if s.All {
+			return &Set{All: true}
+		}
+		if len(s.IDs) > estimateIDs {
+			estimateIDs = len(s.IDs)
+		}
+		if len(s.Groups) > estimateGroups {
+			estimateGroups = len(s.Groups)
+		}
+	}
+
+	union := &Set{}
+	if estimateIDs != 0 {
+		union.IDs = make(identSet, estimateIDs)
+	}
+	if estimateGroups != 0 {
+		union.Groups = make(groupSet, estimateGroups)
+	}
+
+	for _, s := range sets {
+		if s == nil {
+			continue
+		}
+		for ident := range s.IDs {
+			union.IDs[ident] = struct{}{}
+		}
+		for group := range s.Groups {
+			union.Groups[group] = struct{}{}
+		}
+	}
+
+	return union
+}
---
+++ tokenserver/appengine/utils/identityset/identityset_test.go
@@ -0,0 +1,222 @@
+// Copyright 2016 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package identityset
+
+import (
+	"testing"
+
+	"golang.org/x/net/context"
+
+	"github.com/luci/luci-go/server/auth"
+	"github.com/luci/luci-go/server/auth/authtest"
+	"github.com/luci/luci-go/server/auth/identity"
+
+	. "github.com/luci/luci-go/common/testing/assertions"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFromStrings(t *testing.T) {
+	Convey("Empty", t, func() {
+		s, err := FromStrings(nil)
+		So(err, ShouldBeNil)
+		So(s, ShouldResemble, &Set{})
+		So(s.IsEmpty(), ShouldBeTrue)
+	})
+
+	Convey("Universal", t, func() {
+		s, err := FromStrings([]string{"*", "user:abc@example.com"})
+		So(err, ShouldBeNil)
+		So(s, ShouldResemble, &Set{All: true})
+		So(s.IsEmpty(), ShouldBeFalse)
+	})
+
+	Convey("Normal", t, func() {
+		s, err := FromStrings([]string{
+			"user:abc@example.com",
+			"user:def@example.com",
+			"user:abc@example.com",
+			"group:abc",
+			"group:def",
+			"group:abc",
+		})
+		So(err, ShouldBeNil)
+		So(s, ShouldResemble, &Set{
+			IDs: identSet{
+				"user:abc@example.com": struct{}{},
+				"user:def@example.com": struct{}{},
+			},
+			Groups: groupSet{
+				"abc": struct{}{},
+				"def": struct{}{},
+			},
+		})
+		So(s.IsEmpty(), ShouldBeFalse)
+	})
+
+	Convey("Bad group entry", t, func() {
+		s, err := FromStrings([]string{"group:"})
+		So(err, ShouldErrLike, "invalid entry")
+		So(s, ShouldEqual, nil)
+	})
+
+	Convey("Bad ID entry", t, func() {
+		s, err := FromStrings([]string{"shrug"})
+		So(err, ShouldErrLike, "bad identity string")
+		So(s, ShouldEqual, nil)
+	})
+}
+
+func TestIsMember(t *testing.T) {
+	c := context.Background()
+	c = auth.WithState(c, &authtest.FakeState{
+		Identity:       "user:abc@example.com",
+		IdentityGroups: []string{"abc"},
+	})
+
+	Convey("nil", t, func() {
+		var s *Set
+		ok, err := s.IsMember(c, identity.AnonymousIdentity)
+		So(err, ShouldBeNil)
+		So(ok, ShouldBeFalse)
+	})
+
+	Convey("All", t, func() {
+		s := Set{All: true}
+		ok, err := s.IsMember(c, identity.AnonymousIdentity)
+		So(err, ShouldBeNil)
+		So(ok, ShouldBeTrue)
+	})
+
+	Convey("Direct hit", t, func() {
+		s, _ := FromStrings([]string{"user:abc@example.com"})
+
+		ok, err := s.IsMember(c, identity.Identity("user:abc@example.com"))
+		So(err, ShouldBeNil)
+		So(ok, ShouldBeTrue)
+
+		ok, err = s.IsMember(c, identity.AnonymousIdentity)
+		So(err, ShouldBeNil)
+		So(ok, ShouldBeFalse)
+	})
+
+	Convey("Groups hit", t, func() {
+		s, _ := FromStrings([]string{"group:abc"})
+
+		ok, err := s.IsMember(c, identity.Identity("user:abc@example.com"))
+		So(err, ShouldBeNil)
+		So(ok, ShouldBeTrue)
+
+		ok, err = s.IsMember(c, identity.AnonymousIdentity)
+		So(err, ShouldBeNil)
+		So(ok, ShouldBeFalse)
+	})
+}
+
+func TestIsSubset(t *testing.T) {
+	empty := &Set{}
+	all := &Set{All: true}
+	some, _ := FromStrings([]string{
+		"user:abc@example.com",
+		"user:def@example.com",
+		"group:abc",
+		"group:def",
+	})
+	some1, _ := FromStrings([]string{
+		"user:abc@example.com",
+		"user:def@example.com",
+	})
+	some2, _ := FromStrings([]string{
+		"group:abc",
+		"group:def",
+	})
+	some3, _ := FromStrings([]string{
+		"user:abc@example.com",
+		"group:abc",
+	})
+	some4, _ := FromStrings([]string{
+		"user:xxx@example.com",
+		"user:yyy@example.com",
+		"group:xxx",
+		"group:yyy",
+	})
+
+	Convey("empty", t, func() {
+		So(empty.IsSubset(empty), ShouldBeTrue)
+		So(empty.IsSubset(all), ShouldBeTrue)
+		So(empty.IsSubset(some), ShouldBeTrue)
+
+		So(empty.IsSuperset(empty), ShouldBeTrue) // for code coverage
+	})
+
+	Convey("all", t, func() {
+		So(all.IsSubset(empty), ShouldBeFalse)
+		So(all.IsSubset(all), ShouldBeTrue)
+		So(all.IsSubset(some), ShouldBeFalse)
+	})
+
+	Convey("some", t, func() {
+		So(some.IsSubset(empty), ShouldBeFalse)
+		So(some.IsSubset(all), ShouldBeTrue)
+		So(some.IsSubset(some), ShouldBeTrue)
+
+		So(some1.IsSubset(some), ShouldBeTrue)
+		So(some2.IsSubset(some), ShouldBeTrue)
+		So(some3.IsSubset(some), ShouldBeTrue)
+
+		So(some1.IsSubset(some2), ShouldBeFalse)
+		So(some1.IsSubset(some3), ShouldBeFalse)
+		So(some2.IsSubset(some1), ShouldBeFalse)
+		So(some3.IsSubset(some1), ShouldBeFalse)
+
+		So(some1.IsSubset(some3), ShouldBeFalse)
+		So(some1.IsSubset(some4), ShouldBeFalse)
+
+		So(some2.IsSubset(some3), ShouldBeFalse)
+		So(some2.IsSubset(some4), ShouldBeFalse)
+	})
+}
+
+func TestUnion(t *testing.T) {
+	empty := &Set{}
+	all := &Set{All: true}
+	some, _ := FromStrings([]string{
+		"user:abc@example.com",
+		"user:def@example.com",
+		"group:abc",
+		"group:def",
+	})
+	some1, _ := FromStrings([]string{
+		"user:abc@example.com",
+		"user:def@example.com",
+	})
+	some2, _ := FromStrings([]string{
+		"group:abc",
+		"group:def",
+	})
+	some3, _ := FromStrings([]string{
+		"user:abc@example.com",
+		"group:abc",
+	})
+
+	Convey("empty", t, func() {
+		So(Union(), ShouldResemble, empty)
+		So(Union(empty, nil), ShouldResemble, empty)
+	})
+
+	Convey("one", t, func() {
+		So(Union(some), ShouldResemble, some)
+		So(Union(some, empty), ShouldResemble, some)
+	})
+
+	Convey("many", t, func() {
+		So(Union(some1, some2, some3, empty), ShouldResemble, some)
+	})
+
+	Convey("all", t, func() {
+		So(Union(all), ShouldResemble, all)
+		So(Union(all, empty), ShouldResemble, all)
+		So(Union(all, some1), ShouldResemble, all)
+	})
+}
