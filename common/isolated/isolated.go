588f364b852ca2e30314976a21b2522e37b48e0d
djd <djd@chromium.org>
2016-11-16 18:43:28 -0800

    common/isolated: add FileType to File.
    This brings the structure of the isolated struct closer in-line with the
    python library [1].
    
    In addition, add helper methods to quickly construct the common File
    structs to make sure the correct fields are set in concert.
    
    Issue luci/luci-go#9
    
    [1] https://github.com/luci/luci-py/blob/master/appengine/isolate/doc/Design.md#file-types
    
    BUG=59899
    R=maruel@chromium.org, tansell@chromium.org
    
    Review-Url: https://codereview.chromium.org/2506243002

--- common/isolated/isolated.go
+++ common/isolated/isolated.go
@@ -28,14 +28,57 @@ const (
 // Algorithm is the value for Algo.
 const Algorithm = "sha-1"
 
+// FileType describes the type of file being isolated.
+type FileType string
+
+const (
+	// Basic represents normal files. It is the default type.
+	Basic FileType = "basic"
+
+	// ArArchive represents an ar archive containing a large number of small files.
+	ArArchive FileType = "ar"
+
+	// TarArchive represents a tar archive containing a large number of small files.
+	TarArchive FileType = "tar"
+)
+
 // File describes a single file referenced by content in a .isolated file.
 //
-// Either one of Size or Link can be set.
+// For regular files, the Digest, Mode, and Size fields should be set, and the
+// Type field should be set for non-basic files.
+// For symbolic links, only the Link field should be set.
 type File struct {
 	Digest HexDigest `json:"h,omitempty"`
 	Link   *string   `json:"l,omitempty"`
 	Mode   *int      `json:"m,omitempty"`
 	Size   *int64    `json:"s,omitempty"`
+	Type   FileType  `json:"t,omitempty"`
+}
+
+// BasicFile returns a File populated for a basic file.
+func BasicFile(d HexDigest, mode int, size int64) File {
+	return File{
+		Digest: d,
+		Mode:   &mode,
+		Size:   &size,
+	}
+}
+
+// SymLink returns a File populated for a symbolic link.
+func SymLink(link string) File {
+	return File{
+		Link: &link,
+	}
+}
+
+// TarFile returns a file populated for a tar archive file.
+func TarFile(d HexDigest, mode int, size int64) File {
+	return File{
+		Digest: d,
+		Mode:   &mode,
+		Size:   &size,
+		Type:   TarArchive,
+	}
 }
 
 // Isolated is the data from a JSON serialized .isolated file.
