dd65e8b8494aedf0eafff0ac6894a0c99e0a62ba
iannucci <iannucci@chromium.org>
2016-11-28 18:23:24 -0800

    Add base128 encoding library.
    This will be used to safely encode datastore Key string ids, which are
    supposedly UTF8 encoded. This base128 implementation only ever uses the lower 7
    bits, and so should always roundtrip through UTF8 without any drama.
    
    This is also designed to retain sort-order between sorted and unsorted forms
    (that is: the data encoded and unencoded should memcmp sort the same way).
    Unfortunately other schemes (such as base64/ascii85) don't preserve this
    property.
    
    R=dnj@chromium.org, vadimsh@chromium.org
    BUG=669308
    
    Review-Url: https://codereview.chromium.org/2538553003

---
+++ common/data/base128/base128.go
@@ -0,0 +1,108 @@
+// Copyright 2016 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package base128 implements base128 encoding and decoding.
+//
+// Encoded results are UTF8-safe, and will retain the same memcmp sorting
+// properties of the input data.
+package base128
+
+import "errors"
+
+var (
+	// ErrLength is returned from the Decode* methods if the input has an
+	// impossible length.
+	ErrLength = errors.New("base128: invalid length base128 string")
+
+	// ErrBit is returned from the Decode* methods if the input has a byte with
+	// the high-bit set (e.g. 0x80). This will never be the case for strings
+	// produced from the Encode* methods in this package.
+	ErrBit = errors.New("base128: high bit set in base128 string")
+)
+
+// Encode encodes src into EncodedLen(len(src)) bytes of dst. As a convenience,
+// it returns the number of bytes written to dst, but this value is always
+// EncodedLen(len(src)).
+//
+// Encode implements base128 encoding.
+func Encode(dst, src []byte) int {
+	ret := EncodedLen(len(src))
+	if len(dst) < ret {
+		panic("dst has insufficient length")
+	}
+	dst = dst[:0]
+	whichByte := uint(1)
+	bufByte := byte(0)
+	for _, v := range src {
+		dst = append(dst, bufByte|(v>>whichByte))
+		bufByte = (v&(1<<whichByte) - 1) << (7 - whichByte)
+		if whichByte == 7 {
+			dst = append(dst, bufByte)
+			bufByte = 0
+			whichByte = 0
+		}
+		whichByte++
+	}
+	dst = append(dst, bufByte)
+	return ret
+}
+
+// Decode decodes src into DecodedLen(len(src)) bytes, returning the actual
+// number of bytes written to dst.
+//
+// If Decode encounters invalid input, it returns an error describing the
+// failure.
+func Decode(dst, src []byte) (int, error) {
+	dLen := DecodedLen(len(src))
+	if EncodedLen(dLen) != len(src) {
+		return 0, ErrLength
+	}
+	if len(dst) < dLen {
+		panic("dst has insufficient length")
+	}
+	dst = dst[:0]
+	whichByte := uint(1)
+	bufByte := byte(0)
+	for _, v := range src {
+		if (v & 0x80) != 0 {
+			return len(dst), ErrBit
+		}
+		if whichByte > 1 {
+			dst = append(dst, bufByte|(v>>(8-whichByte)))
+		}
+		bufByte = v << whichByte
+		if whichByte == 8 {
+			whichByte = 0
+		}
+		whichByte++
+	}
+	return len(dst), nil
+}
+
+// DecodeString returns the bytes represented by the base128 string s.
+func DecodeString(s string) ([]byte, error) {
+	src := []byte(s)
+	dst := make([]byte, DecodedLen(len(src)))
+	if _, err := Decode(dst, src); err != nil {
+		return nil, err
+	}
+	return dst, nil
+}
+
+// DecodedLen returns the number of bytes `encLen` encoded bytes decodes to.
+func DecodedLen(encLen int) int {
+	return (encLen * 7 / 8)
+}
+
+// EncodedLen returns the number of bytes that `dataLen` bytes will encode to.
+func EncodedLen(dataLen int) int {
+	return (((dataLen * 8) + 6) / 7)
+}
+
+// EncodeToString returns the base128 encoding of src.
+func EncodeToString(src []byte) string {
+	dst := make([]byte, EncodedLen(len(src)))
+	Encode(dst, src)
+	return string(dst)
+}
---
+++ common/data/base128/base128_test.go
@@ -0,0 +1,91 @@
+// Copyright 2016 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package base128
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestBase128(t *testing.T) {
+	t.Parallel()
+
+	Convey("base128", t, func() {
+		Convey("lengths", func() {
+			cases := []struct {
+				dec, enc int
+			}{
+				{0, 0},
+				{1, 2},
+				{6, 7},
+				{7, 8},
+				{8, 10},
+				{9, 11},
+			}
+			for _, c := range cases {
+				Convey(fmt.Sprintf("enc:%d dec:%d", c.enc, c.dec), func() {
+					So(DecodedLen(c.enc), ShouldEqual, c.dec)
+					So(EncodedLen(c.dec), ShouldEqual, c.enc)
+					So(EncodedLen(DecodedLen(c.enc)), ShouldEqual, c.enc)
+					So(DecodedLen(EncodedLen(c.dec)), ShouldEqual, c.dec)
+				})
+			}
+		})
+
+		Convey("errors", func() {
+			Convey("bad length", func() {
+				_, err := Decode(nil, []byte{0xff})
+				So(err, ShouldEqual, ErrLength)
+
+				_, err = Decode(nil, bytes.Repeat([]byte{0xff}, 9))
+				So(err, ShouldEqual, ErrLength)
+
+				So(func() { Decode(nil, []byte{0xff, 0xff}) }, ShouldPanic)
+			})
+
+			Convey("bad byte", func() {
+				_, err := Decode([]byte{0}, []byte{0x7f, 0xff})
+				So(err, ShouldEqual, ErrBit)
+			})
+		})
+
+		Convey("bytes", func() {
+			cases := []struct {
+				dec, enc []byte
+			}{
+				{[]byte{}, []byte{}},
+				{[]byte{0xff}, []byte{0x7f, 0x40}},
+				{
+					bytes.Repeat([]byte{0xff}, 6),
+					append(bytes.Repeat([]byte{0x7f}, 6), 0x7e),
+				},
+				{
+					bytes.Repeat([]byte{0xff}, 7),
+					bytes.Repeat([]byte{0x7f}, 8),
+				},
+				{
+					bytes.Repeat([]byte{0xff}, 8),
+					append(bytes.Repeat([]byte{0x7f}, 9), 0x40),
+				},
+			}
+			for _, c := range cases {
+				Convey(fmt.Sprintf("%q -> %q", c.dec, c.enc), func() {
+					buf := make([]byte, len(c.enc))
+					So(Encode(buf, c.dec), ShouldEqual, len(buf))
+					So(buf, ShouldResemble, c.enc)
+
+					buf = make([]byte, len(c.dec))
+					n, err := Decode(buf, c.enc)
+					So(err, ShouldBeNil)
+					So(n, ShouldEqual, len(buf))
+					So(buf, ShouldResemble, c.dec)
+				})
+			}
+		})
+	})
+}
