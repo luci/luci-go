b1a0590aa40265cd286f439d820133a3bcd00a3d
djd <djd@chromium.org>
2016-12-14 18:22:56 -0800

    client/isolate: make tar construction simpler
    This introduces a new way of constructing tar archives from a set of
    individual files (*Items) in a way that will reduce complexity.
    
    At the moment, I haven't removed the existing code or wired this new
    approach up yet.
    
    R=mcgreevy@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2574913003

--- client/cmd/isolate/tar_archiver.go
+++ client/cmd/isolate/tar_archiver.go
@@ -20,6 +20,97 @@ var osOpen = func(name string) (io.ReadCloser, error) {
 	return os.Open(name)
 }
 
+// ItemBundle is a slice of *Items that will be archived together.
+type ItemBundle struct {
+	Items []*Item
+	// ItemSize is the total size (in bytes) of the constituent files. It will be
+	// smaller than the resultant tar.
+	ItemSize int64
+	// TarSize is the size (in bytes) of the resultant tar.
+	TarSize int64
+}
+
+// ShardItems shards the provided items into ItemBundles, using the provided
+// threshold as the maximum size the resultant tars should be.
+//
+// ShardItems does not access the filesystem to determine
+func ShardItems(items []*Item, threshold int64) []*ItemBundle {
+	var (
+		bundles []*ItemBundle
+		bundle  *ItemBundle
+	)
+
+	for len(items) > 0 {
+		bundle, items = oneBundle(items, threshold)
+		bundles = append(bundles, bundle)
+	}
+	return bundles
+}
+
+func oneBundle(items []*Item, threshold int64) (*ItemBundle, []*Item) {
+	bundle := &ItemBundle{
+		TarSize: 1024, // two trailing blank 512-byte records.
+	}
+
+	for i, item := range items {
+		// The in-tar size of the file (512 header + rounded up to nearest 512).
+		tarSize := (item.Size + 1023) & ^511
+
+		if i > 0 && bundle.TarSize+tarSize > threshold {
+			return bundle, items[i:]
+		}
+
+		bundle.Items = items[:i+1]
+		bundle.ItemSize += item.Size
+		bundle.TarSize += tarSize
+	}
+	return bundle, nil
+}
+
+// Digest returns the hash of the tar constructed from the bundle's items.
+func (b *ItemBundle) Digest() (isolated.HexDigest, error) {
+	h := sha1.New()
+	if err := b.writeTar(h); err != nil {
+		return "", err
+	}
+	return isolated.Sum(h), nil
+}
+
+// Contents returns an io.ReadCloser containing the tar's contents.
+func (b *ItemBundle) Contents() (io.ReadCloser, error) {
+	pr, pw := io.Pipe()
+	go func() {
+		pw.CloseWithError(b.writeTar(pw))
+	}()
+	return pr, nil
+}
+
+func (b *ItemBundle) writeTar(w io.Writer) error {
+	tw := tar.NewWriter(w)
+
+	for _, item := range b.Items {
+		if err := tw.WriteHeader(&tar.Header{
+			Name:     item.RelPath,
+			Mode:     int64(item.Mode),
+			Typeflag: tar.TypeReg,
+			Size:     item.Size,
+		}); err != nil {
+			return err
+		}
+
+		file, err := osOpen(item.Path)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(tw, file)
+		file.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return tw.Close()
+}
+
 // Tar represents a completed tar archive.
 type Tar struct {
 	Content []byte
--- client/cmd/isolate/tar_archiver_test.go
+++ client/cmd/isolate/tar_archiver_test.go
@@ -9,12 +9,14 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"reflect"
 	"strconv"
 	"strings"
 	"testing"
 	"testing/iotest"
 	"time"
 
+	"github.com/kr/pretty"
 	"github.com/luci/luci-go/common/isolated"
 )
 
@@ -128,5 +130,172 @@ func TestTarArchiver_Error(t *testing.T) {
 		Size: 42,
 	}); err == nil {
 		t.Errorf("ta.AddItem(%q) got nil error, want non-nil", "/err/open")
+	}
+}
+
+func TestItemBundle(t *testing.T) {
+	oldOpen := osOpen
+	osOpen = fakeOsOpen
+	defer func() { osOpen = oldOpen }()
+
+	// All tests use an archive threshold of 5000 bytes.
+	const threshold = 5000
+
+	testCases := []struct {
+		desc        string
+		sizes       []int64
+		wantBundles []int                // The number of items in each bundle.
+		wantDigests []isolated.HexDigest // The hash of each bundle's tar.
+		wantSize    []int                // The size of each bundle's tar.
+	}{
+		{
+			desc:        "one bundle",
+			sizes:       []int64{100, 200, 300},
+			wantBundles: []int{3},
+			wantSize:    []int{1024 + 1024 + 1024 + 1024},
+			wantDigests: []isolated.HexDigest{
+				"003afa69f9b267a2cf267cc5ff715968cb05354b",
+			},
+		},
+		{
+			desc:        "multi bundle",
+			sizes:       []int64{100, 200, 300, 400, 500, 2000},
+			wantBundles: []int{3, 2, 1},
+			wantSize: []int{
+				1024 + 1024 + 1024 + 1024,
+				1024 + 1024 + 1024,
+				1024 + 2560,
+			},
+			wantDigests: []isolated.HexDigest{
+				"003afa69f9b267a2cf267cc5ff715968cb05354b",
+				"7f8cedcc12dc7ddcbe0849a46628b2748f5e84aa",
+				"e4a390dbfa081e564fe61368bac77bb9ef30a4e7",
+			},
+		},
+		{
+			desc:        "file below boundary",
+			sizes:       []int64{511},
+			wantBundles: []int{1},
+			wantSize:    []int{2048},
+			wantDigests: []isolated.HexDigest{
+				"6103d5cc5fc494f9490107d61a952c6ce1e48be6",
+			},
+		},
+		{
+			desc:        "file on boundary",
+			sizes:       []int64{512},
+			wantBundles: []int{1},
+			wantSize:    []int{2048},
+			wantDigests: []isolated.HexDigest{
+				"8c63e33b6264ade5f62ab62b322de8d8ac5eeb8b",
+			},
+		},
+		{
+			desc:        "all items over threshold",
+			sizes:       []int64{5000, 6000, 4500},
+			wantBundles: []int{1, 1, 1},
+			wantSize:    []int{6656, 7680, 6144},
+			wantDigests: []isolated.HexDigest{
+				"2cd4cb7a965d9ca34cbb692e3885f7074f28a4a4",
+				"925b04df322c98d013b470727df5644644d93e70",
+				"1f576aa4d44aea793c751bf03de2d513a2dd65eb",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		// Construct the input items and expected output.
+		var items []*Item
+		for _, size := range tc.sizes {
+			items = append(items, &Item{
+				RelPath: fmt.Sprintf("./%d", size),
+				Path:    fmt.Sprintf("/size/%d", size),
+				Size:    size,
+			})
+		}
+		var wantBundles []*ItemBundle
+		temp := items
+		for i, bSize := range tc.wantBundles {
+			bundle := &ItemBundle{
+				Items:   temp[:bSize],
+				TarSize: int64(tc.wantSize[i]),
+			}
+			wantBundles = append(wantBundles, bundle)
+			temp = temp[bSize:]
+			for _, item := range bundle.Items {
+				bundle.ItemSize += item.Size
+			}
+		}
+
+		bundles := ShardItems(items, threshold)
+		if !reflect.DeepEqual(bundles, wantBundles) {
+			t.Errorf("%s:\ngot  %s\nwant %s", tc.desc, pretty.Sprint(bundles), pretty.Sprint(wantBundles))
+		}
+
+		for i, bundle := range bundles {
+			digest, err := bundle.Digest()
+			if err != nil {
+				t.Errorf("%s: bundle[%d].Digest gave err %v, want nil", tc.desc, i, err)
+				continue
+			}
+			if digest != tc.wantDigests[i] {
+				t.Errorf("%s: bundle[%d].Digest() = %q, want %q", tc.desc, i, digest, tc.wantDigests[i])
+			}
+
+			rc, err := bundle.Contents()
+			if err != nil {
+				t.Errorf("%s: bundle[%d].Contents gave err %v, want nil", tc.desc, i, err)
+				continue
+			}
+			tar, err := ioutil.ReadAll(rc)
+			rc.Close()
+			if err != nil {
+				t.Errorf("%s: reading bundle[%d].Contents gave err %v, want nil", tc.desc, i, err)
+			}
+			if got := len(tar); got != tc.wantSize[i] {
+				t.Errorf("%s: bundle[%d].Contents had len %d, want %d", tc.desc, i, got, tc.wantSize[i])
+			}
+		}
+	}
+}
+
+func TestItemBundle_Errors(t *testing.T) {
+	oldOpen := osOpen
+	osOpen = fakeOsOpen
+	defer func() { osOpen = oldOpen }()
+
+	testItems := []*Item{
+		{
+			// File that fails to open.
+			RelPath: "./open",
+			Path:    "/err/open",
+			Size:    123,
+		},
+		{
+			// File that fails to read.
+			RelPath: "./read",
+			Path:    "/err/read",
+			Size:    123,
+		},
+	}
+
+	bundles := ShardItems(testItems, 0)
+	if len(bundles) != 2 {
+		t.Errorf("len(bundles) = %d, want 2", len(bundles))
+	}
+	for _, bundle := range bundles {
+		if _, err := bundle.Digest(); err == nil {
+			t.Errorf("Path %q, bundle.Digest gave nil error; want some error", bundle.Items[0].Path)
+		}
+		rc, err := bundle.Contents()
+		if err != nil {
+			t.Errorf("Path %q, bundle.Contents gave error %v; want nil error", bundle.Items[0].Path, err)
+			continue
+		}
+		_, err = ioutil.ReadAll(rc)
+		rc.Close()
+		if err == nil {
+			t.Errorf("Path %q, reading contents gave nil error, want some error", bundle.Items[0].Path)
+		}
 	}
 }
