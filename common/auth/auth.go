luci/luci-go

commit 971a040ec0d831b53ed8d5e565ca312c8ad71c5a

Refactor homedir use for auth secrets directory.
The homedir package is a third party dependency which provides access to
a user home directory without a dependency on cgo. The home directory is
used by the auth package as a location to create a cache for tokens.

This change refactors the secret directory to be associated with the
authcli package instead of the top-level common/auth package.

R=maruel@chromium.org
BUG=

Review-Url: https://codereview.chromium.org/2644403002

common/auth/auth.go
@@ -41,8 +41,6 @@ import (
 
 	"cloud.google.com/go/compute/metadata"
 
-	"github.com/mitchellh/go-homedir"
-
 	"github.com/luci/luci-go/common/auth/internal"
 	"github.com/luci/luci-go/common/clock"
 	"github.com/luci/luci-go/common/logging"
@@ -168,10 +166,8 @@ type Options struct {
 	// Default: "default" account.
 	GCEAccountName string
 
-	// SecretsDir can be used to override a path to a directory where tokens
+	// SecretsDir can be used to set the path to a directory where tokens
 	// are cached and default service account key is located.
-	//
-	// If not set, SecretsDir() will be used.
 	SecretsDir string
 
 	// DisableMonitoring can be used to disable the monitoring instrumentation.
@@ -517,15 +513,12 @@ func (a *Authenticator) ensureInitialized() error {
 			return a.err
 		}
 	} else {
-		secretsDir, err := a.opts.secretsDir()
-		if err != nil {
-			// Note: in observed cases the 'err' here is some confusing nonsense like
-			// "exit status 1", this is fine.
-			logging.Warningf(a.ctx, "Disabling token cache, can't find HOME (the inner error is %q)", err)
+		if a.opts.SecretsDir == "" {
+			logging.Warningf(a.ctx, "Disabling token cache. Not configured.")
 			a.cache = &memoryCache{}
 		} else {
 			a.cache = &tokenFileCache{
-				path: filepath.Join(secretsDir, cacheName+".tok"),
+				path: filepath.Join(a.opts.SecretsDir, cacheName+".tok"),
 				ctx:  a.ctx,
 			}
 		}
@@ -824,14 +817,6 @@ func (c *memoryCache) Clear() error {
 ////////////////////////////////////////////////////////////////////////////////
 // Utility functions.
 
-// secretsDir returns directory with token cache files.
-func (opts *Options) secretsDir() (string, error) {
-	if opts.SecretsDir != "" {
-		return opts.SecretsDir, nil
-	}
-	return SecretsDir()
-}
-
 // cacheEntryName constructs a name of cache entry from data that identifies
 // requested credential, to allow multiple differently configured instances of
 // Authenticator to coexist.
@@ -915,14 +900,4 @@ func DefaultClient() (clientID string, clientSecret string) {
 	clientID = "446450136466-2hr92jrq8e6i4tnsa56b52vacp7t3936.apps.googleusercontent.com"
 	clientSecret = "uBfbay2KCy9t4QveJ-dOqHtp"
 	return
-}
-
-// SecretsDir returns an absolute path to a directory (in $HOME) to keep secret
-// files in or an error if $HOME can't be determined.
-func SecretsDir() (string, error) {
-	home, err := homedir.Dir()
-	if err != nil {
-		return "", err
-	}
-	return filepath.Join(home, ".config", "chrome_infra", "auth"), nil
 }

buildbucket/client/cmd/buildbucket/base_command.go
@@ -14,6 +14,7 @@ import (
 
 	"net/url"
 
+	"github.com/luci/luci-go/client/authcli"
 	"github.com/luci/luci-go/common/auth"
 	"github.com/luci/luci-go/common/lhttp"
 	"github.com/luci/luci-go/common/logging"
@@ -21,21 +22,18 @@ import (
 
 type baseCommandRun struct {
 	subcommands.CommandRunBase
-	serviceAccountJSONPath string
-	host                   string
+	authFlags      authcli.Flags
+	parsedAuthOpts auth.Options
+	host           string
 }
 
 func (r *baseCommandRun) SetDefaultFlags() {
-	r.Flags.StringVar(
-		&r.serviceAccountJSONPath,
-		"service-account-json",
-		"",
-		"path to service account json file.")
 	r.Flags.StringVar(
 		&r.host,
 		"host",
 		"cr-buildbucket.appspot.com",
 		"host for the buildbucket service instance.")
+	r.authFlags.Register(&r.Flags, auth.Options{})
 }
 
 func (r *baseCommandRun) createClient(ctx context.Context) (*client, error) {
@@ -45,16 +43,18 @@ func (r *baseCommandRun) createClient(ctx context.Context) (*client, error) {
 	if strings.ContainsRune(r.host, '/') {
 		return nil, fmt.Errorf("invalid host %q", r.host)
 	}
+	var err error
+	if r.parsedAuthOpts, err = r.authFlags.Options(); err != nil {
+		return nil, err
+	}
 
 	loginMode := auth.OptionalLogin
-	if r.serviceAccountJSONPath != "" {
+	if r.parsedAuthOpts.ServiceAccountJSONPath != "" {
 		// if service account is specified, the request MUST be authenticated
 		// otherwise it is optional.
 		loginMode = auth.SilentLogin
 	}
-	authenticator := auth.NewAuthenticator(ctx, loginMode, auth.Options{
-		ServiceAccountJSONPath: r.serviceAccountJSONPath,
-	})
+	authenticator := auth.NewAuthenticator(ctx, loginMode, r.parsedAuthOpts)
 
 	httpClient, err := authenticator.Client()
 	if err != nil {

client/authcli/authcli.go
@@ -46,11 +46,13 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"sort"
 	"strings"
 	"time"
 
 	"github.com/maruel/subcommands"
+	homedir "github.com/mitchellh/go-homedir"
 	"golang.org/x/net/context"
 	"golang.org/x/net/context/ctxhttp"
 
@@ -104,6 +106,9 @@ func (fl *Flags) Options() (auth.Options, error) {
 	if fl.registerScopesFlag {
 		opts.Scopes = strings.Split(fl.scopes, " ")
 		sort.Strings(opts.Scopes)
+	}
+	if opts.SecretsDir == "" {
+		opts.SecretsDir, _ = secretsDir()
 	}
 	return opts, nil
 }
@@ -431,4 +436,14 @@ func reportIdentity(ctx context.Context, a *auth.Authenticator) error {
 	}
 
 	return nil
+}
+
+// secretsDir returns an absolute path to a directory (in $HOME) to keep secret
+// files in or an error if $HOME can't be determined.
+func secretsDir() (string, error) {
+	home, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, ".config", "chrome_infra", "auth"), nil
 }

client/cmd/isolated/common.go
@@ -11,6 +11,7 @@ import (
 	"github.com/maruel/subcommands"
 	"golang.org/x/net/context"
 
+	"github.com/luci/luci-go/client/authcli"
 	"github.com/luci/luci-go/client/internal/common"
 	"github.com/luci/luci-go/common/auth"
 	"github.com/luci/luci-go/common/isolatedclient"
@@ -23,24 +24,32 @@ func init() {
 
 type commonFlags struct {
 	subcommands.CommandRunBase
-	defaultFlags  common.Flags
-	isolatedFlags isolatedclient.Flags
+	defaultFlags   common.Flags
+	isolatedFlags  isolatedclient.Flags
+	authFlags      authcli.Flags
+	parsedAuthOpts auth.Options
 }
 
 func (c *commonFlags) Init() {
 	c.defaultFlags.Init(&c.Flags)
 	c.isolatedFlags.Init(&c.Flags)
+	c.authFlags.Register(&c.Flags, auth.Options{})
 }
 
 func (c *commonFlags) Parse() error {
-	if err := c.defaultFlags.Parse(); err != nil {
+	var err error
+	if err = c.defaultFlags.Parse(); err != nil {
 		return err
 	}
-	return c.isolatedFlags.Parse()
+	if err = c.isolatedFlags.Parse(); err != nil {
+		return err
+	}
+	c.parsedAuthOpts, err = c.authFlags.Options()
+	return err
 }
 
 func (c *commonFlags) createClient() *http.Client {
 	ctx := gologger.StdConfig.Use(context.Background())
-	client, _ := auth.NewAuthenticator(ctx, auth.SilentLogin, auth.Options{}).Client()
+	client, _ := auth.NewAuthenticator(ctx, auth.SilentLogin, c.parsedAuthOpts).Client()
 	return client
 }

commit 14b8f6eef59bde5631f4c9b80ea927744b537b37

Cache access tokens used by tsmon.
They used to be cached, but this has regressed in
https://github.com/luci/luci-go/commit/971a040.

There are many short-lived processes that send metrics to tsmon. Making them
go through OAuth flow each time is too expensive.

R=maruel@chromium.org, dsansome@chromium.org
BUG=

Review-Url: https://codereview.chromium.org/2661493002

common/auth/auth.go
@@ -167,7 +167,13 @@ type Options struct {
 	GCEAccountName string
 
 	// SecretsDir can be used to set the path to a directory where tokens
-	// are cached and default service account key is located.
+	// are cached.
+	//
+	// If not set, tokens will be cached only in the process memory. For refresh
+	// tokens it means the user would have to go through the login process each
+	// time process is started. For service account tokens it means there'll be
+	// HTTP round trip to OAuth backend to generate access token each time the
+	// process is started.
 	SecretsDir string
 
 	// DisableMonitoring can be used to disable the monitoring instrumentation.

common/tsmon/config.go
@@ -7,9 +7,11 @@ package tsmon
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 	"runtime"
 
 	"github.com/luci/luci-go/common/errors"
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 // config is the representation of a tsmon JSON config file.
@@ -56,4 +58,12 @@ func defaultConfigFilePath() string {
 		return "C:\\chrome-infra\\ts-mon.json"
 	}
 	return "/etc/chrome-infra/ts-mon.json"
+}
+
+func tokenCachePath() (string, error) {
+	home, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, ".config", "chrome_infra", "auth"), nil
 }

common/tsmon/iface.go
@@ -223,8 +223,14 @@ func initMonitor(c context.Context, config config) (monitor.Monitor, error) {
 
 // newAuthenticator returns a new authenticator for PubSub and HTTP requests.
 func newAuthenticator(ctx context.Context, credentials string, scopes []string) *auth.Authenticator {
+	tokenCache, err := tokenCachePath()
+	if err != nil {
+		// Empty "tokenCache" just disables the token cache. We shouldn't abort.
+		logging.WithError(err).Warningf(ctx, "Failed to resolve path to token cache dir")
+	}
 	authOpts := auth.Options{
-		Scopes: scopes,
+		Scopes:     scopes,
+		SecretsDir: tokenCache,
 	}
 	switch credentials {
 	case GCECredentials: