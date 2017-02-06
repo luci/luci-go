33bd26b5c4159b0a3b8471ac059b64ae62aa5ad3
ryanmartens <ryanmartens@google.com>
2017-02-06 13:48:49 -0800

    Remove some dependencies on maruel/ut package.
    This is the first of several commits to remove the depedency on the
    maruel/ut package and instead use smartystreets/goconvey.
    
    Splitting this up to make sure there are no issues before broader
    changes are applied.
    
    R=maruel@chromium.org, vadimsh@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2668843002

--- client/archiver/archiver_test.go
+++ client/archiver/archiver_test.go
@@ -18,7 +18,8 @@ import (
 	"github.com/luci/luci-go/common/isolated"
 	"github.com/luci/luci-go/common/isolatedclient"
 	"github.com/luci/luci-go/common/isolatedclient/isolatedfake"
-	"github.com/maruel/ut"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func init() {
@@ -27,114 +28,124 @@ func init() {
 
 func TestArchiverEmpty(t *testing.T) {
 	t.Parallel()
-	a := New(isolatedclient.New(nil, nil, "https://localhost:1", isolatedclient.DefaultNamespace, nil), nil)
-	stats := a.Stats()
-	ut.AssertEqual(t, 0, stats.TotalHits())
-	ut.AssertEqual(t, 0, stats.TotalMisses())
-	ut.AssertEqual(t, units.Size(0), stats.TotalBytesHits())
-	ut.AssertEqual(t, units.Size(0), stats.TotalBytesPushed())
-	ut.AssertEqual(t, nil, a.Close())
+	Convey(`An empty archiver should produce sane output.`, t, func() {
+		a := New(isolatedclient.New(nil, nil, "https://localhost:1", isolatedclient.DefaultNamespace, nil), nil)
+		stats := a.Stats()
+		So(stats.TotalHits(), ShouldResemble, 0)
+		So(stats.TotalMisses(), ShouldResemble, 0)
+		So(stats.TotalBytesHits(), ShouldResemble, units.Size(0))
+		So(stats.TotalBytesPushed(), ShouldResemble, units.Size(0))
+		So(a.Close(), ShouldBeNil)
+	})
 }
 
 func TestArchiverFile(t *testing.T) {
 	t.Parallel()
-	server := isolatedfake.New()
-	ts := httptest.NewServer(server)
-	defer ts.Close()
-	a := New(isolatedclient.New(nil, nil, ts.URL, isolatedclient.DefaultNamespace, nil), nil)
+	Convey(`An archiver should handle file archival operations.`, t, func() {
+		server := isolatedfake.New()
+		ts := httptest.NewServer(server)
+		defer ts.Close()
+		a := New(isolatedclient.New(nil, nil, ts.URL, isolatedclient.DefaultNamespace, nil), nil)
 
-	fEmpty, err := ioutil.TempFile("", "archiver")
-	ut.AssertEqual(t, nil, err)
-	item1 := a.PushFile(fEmpty.Name(), fEmpty.Name(), 0)
-	ut.AssertEqual(t, fEmpty.Name(), item1.DisplayName)
-	fFoo, err := ioutil.TempFile("", "archiver")
-	ut.AssertEqual(t, nil, err)
-	ut.AssertEqual(t, nil, ioutil.WriteFile(fFoo.Name(), []byte("foo"), 0600))
-	item2 := a.PushFile(fFoo.Name(), fFoo.Name(), 0)
-	// Push the same file another time. It'll get linked to the first.
-	item3 := a.PushFile(fFoo.Name(), fFoo.Name(), 0)
-	item1.WaitForHashed()
-	item2.WaitForHashed()
-	item3.WaitForHashed()
-	ut.AssertEqual(t, nil, a.Close())
+		fEmpty, err := ioutil.TempFile("", "archiver")
+		So(err, ShouldBeNil)
+		item1 := a.PushFile(fEmpty.Name(), fEmpty.Name(), 0)
+		So(item1.DisplayName, ShouldResemble, fEmpty.Name())
+		fFoo, err := ioutil.TempFile("", "archiver")
+		So(err, ShouldBeNil)
+		So(ioutil.WriteFile(fFoo.Name(), []byte("foo"), 0600), ShouldBeNil)
+		item2 := a.PushFile(fFoo.Name(), fFoo.Name(), 0)
+		// Push the same file another time. It'll get linked to the first.
+		item3 := a.PushFile(fFoo.Name(), fFoo.Name(), 0)
+		item1.WaitForHashed()
+		item2.WaitForHashed()
+		item3.WaitForHashed()
+		So(a.Close(), ShouldBeNil)
 
-	stats := a.Stats()
-	ut.AssertEqual(t, 0, stats.TotalHits())
-	// Only 2 lookups, not 3.
-	ut.AssertEqual(t, 2, stats.TotalMisses())
-	ut.AssertEqual(t, units.Size(0), stats.TotalBytesHits())
-	ut.AssertEqual(t, units.Size(3), stats.TotalBytesPushed())
-	expected := map[isolated.HexDigest][]byte{
-		"0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33": []byte("foo"),
-		"da39a3ee5e6b4b0d3255bfef95601890afd80709": {},
-	}
-	ut.AssertEqual(t, expected, server.Contents())
-	ut.AssertEqual(t, isolated.HexDigest("da39a3ee5e6b4b0d3255bfef95601890afd80709"), item1.Digest())
-	ut.AssertEqual(t, nil, item1.Error())
-	ut.AssertEqual(t, isolated.HexDigest("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"), item2.Digest())
-	ut.AssertEqual(t, nil, item2.Error())
-	ut.AssertEqual(t, isolated.HexDigest("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"), item3.Digest())
-	ut.AssertEqual(t, nil, item3.Error())
-	ut.AssertEqual(t, nil, server.Error())
+		stats := a.Stats()
+		So(stats.TotalHits(), ShouldResemble, 0)
+		// Only 2 lookups, not 3.
+		So(stats.TotalMisses(), ShouldResemble, 2)
+		So(stats.TotalBytesHits(), ShouldResemble, units.Size(0))
+		So(stats.TotalBytesPushed(), ShouldResemble, units.Size(3))
+		expected := map[isolated.HexDigest][]byte{
+			"0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33": []byte("foo"),
+			"da39a3ee5e6b4b0d3255bfef95601890afd80709": {},
+		}
+		So(server.Contents(), ShouldResemble, expected)
+		So(item1.Digest(), ShouldResemble, isolated.HexDigest("da39a3ee5e6b4b0d3255bfef95601890afd80709"))
+		So(item1.Error(), ShouldBeNil)
+		So(item2.Digest(), ShouldResemble, isolated.HexDigest("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"))
+		So(item2.Error(), ShouldBeNil)
+		So(item3.Digest(), ShouldResemble, isolated.HexDigest("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"))
+		So(item3.Error(), ShouldBeNil)
+		So(server.Error(), ShouldBeNil)
+	})
 }
 
 func TestArchiverFileHit(t *testing.T) {
 	t.Parallel()
-	server := isolatedfake.New()
-	ts := httptest.NewServer(server)
-	defer ts.Close()
-	a := New(isolatedclient.New(nil, nil, ts.URL, isolatedclient.DefaultNamespace, nil), nil)
-	server.Inject([]byte("foo"))
-	item := a.Push("foo", isolatedclient.NewBytesSource([]byte("foo")), 0)
-	item.WaitForHashed()
-	ut.AssertEqual(t, isolated.HexDigest("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"), item.Digest())
-	ut.AssertEqual(t, nil, a.Close())
+	Convey(`An archiver should correctly cache an archived file.`, t, func() {
+		server := isolatedfake.New()
+		ts := httptest.NewServer(server)
+		defer ts.Close()
+		a := New(isolatedclient.New(nil, nil, ts.URL, isolatedclient.DefaultNamespace, nil), nil)
+		server.Inject([]byte("foo"))
+		item := a.Push("foo", isolatedclient.NewBytesSource([]byte("foo")), 0)
+		item.WaitForHashed()
+		So(item.Digest(), ShouldResemble, isolated.HexDigest("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"))
+		So(a.Close(), ShouldBeNil)
 
-	stats := a.Stats()
-	ut.AssertEqual(t, 1, stats.TotalHits())
-	ut.AssertEqual(t, 0, stats.TotalMisses())
-	ut.AssertEqual(t, units.Size(3), stats.TotalBytesHits())
-	ut.AssertEqual(t, units.Size(0), stats.TotalBytesPushed())
+		stats := a.Stats()
+		So(stats.TotalHits(), ShouldResemble, 1)
+		So(stats.TotalMisses(), ShouldResemble, 0)
+		So(stats.TotalBytesHits(), ShouldResemble, units.Size(3))
+		So(stats.TotalBytesPushed(), ShouldResemble, units.Size(0))
+	})
 }
 
 func TestArchiverCancel(t *testing.T) {
 	t.Parallel()
-	server := isolatedfake.New()
-	ts := httptest.NewServer(server)
-	defer ts.Close()
-	a := New(isolatedclient.New(nil, nil, ts.URL, isolatedclient.DefaultNamespace, nil), nil)
+	Convey(`A cancelled archiver should produce sane output.`, t, func() {
+		server := isolatedfake.New()
+		ts := httptest.NewServer(server)
+		defer ts.Close()
+		a := New(isolatedclient.New(nil, nil, ts.URL, isolatedclient.DefaultNamespace, nil), nil)
 
-	tmpDir, err := ioutil.TempDir("", "archiver")
-	ut.AssertEqual(t, nil, err)
-	defer func() {
-		if err := os.RemoveAll(tmpDir); err != nil {
-			t.Fail()
+		tmpDir, err := ioutil.TempDir("", "archiver")
+		So(err, ShouldBeNil)
+		defer func() {
+			if err := os.RemoveAll(tmpDir); err != nil {
+				t.Fail()
+			}
+		}()
+
+		// This will trigger an eventual Cancel().
+		nonexistent := filepath.Join(tmpDir, "nonexistent")
+		item1 := a.PushFile("foo", nonexistent, 0)
+		So(item1.DisplayName, ShouldResemble, "foo")
+
+		fileName := filepath.Join(tmpDir, "existent")
+		So(ioutil.WriteFile(fileName, []byte("foo"), 0600), ShouldBeNil)
+		item2 := a.PushFile("existent", fileName, 0)
+		item1.WaitForHashed()
+		item2.WaitForHashed()
+		osErr := "no such file or directory"
+		if common.IsWindows() {
+			osErr = "The system cannot find the file specified."
 		}
-	}()
-
-	// This will trigger an eventual Cancel().
-	nonexistent := filepath.Join(tmpDir, "nonexistent")
-	item1 := a.PushFile("foo", nonexistent, 0)
-	ut.AssertEqual(t, "foo", item1.DisplayName)
-
-	fileName := filepath.Join(tmpDir, "existent")
-	ut.AssertEqual(t, nil, ioutil.WriteFile(fileName, []byte("foo"), 0600))
-	item2 := a.PushFile("existent", fileName, 0)
-	item1.WaitForHashed()
-	item2.WaitForHashed()
-	osErr := "no such file or directory"
-	if common.IsWindows() {
-		osErr = "The system cannot find the file specified."
-	}
-	expected := fmt.Errorf("source(foo) failed: open %s: %s\n", nonexistent, osErr)
-	ut.AssertEqual(t, expected, <-a.Channel())
-	ut.AssertEqual(t, expected, a.Close())
-	ut.AssertEqual(t, nil, server.Error())
+		expected := fmt.Errorf("source(foo) failed: open %s: %s\n", nonexistent, osErr)
+		So(<-a.Channel(), ShouldResemble, expected)
+		So(a.Close(), ShouldResemble, expected)
+		So(server.Error(), ShouldBeNil)
+	})
 }
 
 func TestArchiverPushClosed(t *testing.T) {
 	t.Parallel()
-	a := New(nil, nil)
-	ut.AssertEqual(t, nil, a.Close())
-	ut.AssertEqual(t, (*Item)(nil), a.PushFile("ignored", "ignored", 0))
+	Convey(`A closed archiver should ignore additional input.`, t, func() {
+		a := New(nil, nil)
+		So(a.Close(), ShouldBeNil)
+		So(a.PushFile("ignored", "ignored", 0), ShouldBeNil)
+	})
 }
--- client/archiver/directory_test.go
+++ client/archiver/directory_test.go
@@ -19,96 +19,101 @@ import (
 	"github.com/luci/luci-go/common/isolated"
 	"github.com/luci/luci-go/common/isolatedclient"
 	"github.com/luci/luci-go/common/isolatedclient/isolatedfake"
-	"github.com/maruel/ut"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestWalkBadRegexp(t *testing.T) {
-	ch := make(chan *walkItem)
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		defer close(ch)
-		walk("inexistent", []string{"a["}, ch)
-	}()
-	item := <-ch
-	ut.AssertEqual(t, &walkItem{err: errors.New("bad blacklist pattern \"a[\"")}, item)
-	item, ok := <-ch
-	ut.AssertEqual(t, (*walkItem)(nil), item)
-	ut.AssertEqual(t, false, ok)
-	wg.Wait()
+	Convey(`A bad regexp should fail when walking a directory.`, t, func() {
+		ch := make(chan *walkItem)
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			defer close(ch)
+			walk("inexistent", []string{"a["}, ch)
+		}()
+		item := <-ch
+		So(item, ShouldResemble, &walkItem{err: errors.New("bad blacklist pattern \"a[\"")})
+		item, ok := <-ch
+		So(item, ShouldBeNil)
+		So(ok, ShouldBeFalse)
+		wg.Wait()
+	})
 }
 
 func TestPushDirectory(t *testing.T) {
 	// Uploads a real directory. 2 times the same file.
 	t.Parallel()
-	server := isolatedfake.New()
-	ts := httptest.NewServer(server)
-	defer ts.Close()
-	a := New(isolatedclient.New(nil, nil, ts.URL, isolatedclient.DefaultNamespace, nil), nil)
+	Convey(`Pushing a real directory should upload the directory.`, t, func() {
+		server := isolatedfake.New()
+		ts := httptest.NewServer(server)
+		defer ts.Close()
+		a := New(isolatedclient.New(nil, nil, ts.URL, isolatedclient.DefaultNamespace, nil), nil)
 
-	// Setup temporary directory.
-	tmpDir, err := ioutil.TempDir("", "archiver")
-	ut.AssertEqual(t, nil, err)
-	defer func() {
-		if err := os.RemoveAll(tmpDir); err != nil {
-			t.Fail()
+		// Setup temporary directory.
+		tmpDir, err := ioutil.TempDir("", "archiver")
+		So(err, ShouldBeNil)
+		defer func() {
+			if err := os.RemoveAll(tmpDir); err != nil {
+				t.Fail()
+			}
+		}()
+		baseDir := filepath.Join(tmpDir, "base")
+		ignoredDir := filepath.Join(tmpDir, "ignored1")
+		So(os.Mkdir(baseDir, 0700), ShouldBeNil)
+		So(ioutil.WriteFile(filepath.Join(baseDir, "bar"), []byte("foo"), 0600), ShouldBeNil)
+		So(ioutil.WriteFile(filepath.Join(baseDir, "bar_dupe"), []byte("foo"), 0600), ShouldBeNil)
+		if !common.IsWindows() {
+			So(os.Symlink("bar", filepath.Join(baseDir, "link")), ShouldBeNil)
 		}
-	}()
-	baseDir := filepath.Join(tmpDir, "base")
-	ignoredDir := filepath.Join(tmpDir, "ignored1")
-	ut.AssertEqual(t, nil, os.Mkdir(baseDir, 0700))
-	ut.AssertEqual(t, nil, ioutil.WriteFile(filepath.Join(baseDir, "bar"), []byte("foo"), 0600))
-	ut.AssertEqual(t, nil, ioutil.WriteFile(filepath.Join(baseDir, "bar_dupe"), []byte("foo"), 0600))
-	if !common.IsWindows() {
-		ut.AssertEqual(t, nil, os.Symlink("bar", filepath.Join(baseDir, "link")))
-	}
-	ut.AssertEqual(t, nil, ioutil.WriteFile(filepath.Join(baseDir, "ignored2"), []byte("ignored"), 0600))
-	ut.AssertEqual(t, nil, os.Mkdir(ignoredDir, 0700))
-	ut.AssertEqual(t, nil, ioutil.WriteFile(filepath.Join(ignoredDir, "really"), []byte("ignored"), 0600))
+		So(ioutil.WriteFile(filepath.Join(baseDir, "ignored2"), []byte("ignored"), 0600), ShouldBeNil)
+		So(os.Mkdir(ignoredDir, 0700), ShouldBeNil)
+		So(ioutil.WriteFile(filepath.Join(ignoredDir, "really"), []byte("ignored"), 0600), ShouldBeNil)
 
-	item := PushDirectory(a, tmpDir, "", []string{"ignored1", filepath.Join("*", "ignored2")})
-	ut.AssertEqual(t, filepath.Base(tmpDir)+".isolated", item.DisplayName)
-	item.WaitForHashed()
-	ut.AssertEqual(t, nil, a.Close())
+		item := PushDirectory(a, tmpDir, "", []string{"ignored1", filepath.Join("*", "ignored2")})
+		So(item.DisplayName, ShouldResemble, filepath.Base(tmpDir)+".isolated")
+		item.WaitForHashed()
+		So(a.Close(), ShouldBeNil)
 
-	mode := 0600
-	if common.IsWindows() {
-		mode = 0666
-	}
-	isolatedData := isolated.Isolated{
-		Algo: "sha-1",
-		Files: map[string]isolated.File{
-			filepath.Join("base", "bar"):      isolated.BasicFile("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33", mode, 3),
-			filepath.Join("base", "bar_dupe"): isolated.BasicFile("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33", mode, 3),
-		},
-		Version: isolated.IsolatedFormatVersion,
-	}
-	if !common.IsWindows() {
-		isolatedData.Files[filepath.Join("base", "link")] = isolated.SymLink("bar")
-	}
-	encoded, err := json.Marshal(isolatedData)
-	ut.AssertEqual(t, nil, err)
-	isolatedEncoded := string(encoded) + "\n"
-	isolatedHash := isolated.HashBytes([]byte(isolatedEncoded))
+		mode := 0600
+		if common.IsWindows() {
+			mode = 0666
+		}
+		isolatedData := isolated.Isolated{
+			Algo: "sha-1",
+			Files: map[string]isolated.File{
+				filepath.Join("base", "bar"):      isolated.BasicFile("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33", mode, 3),
+				filepath.Join("base", "bar_dupe"): isolated.BasicFile("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33", mode, 3),
+			},
+			Version: isolated.IsolatedFormatVersion,
+		}
+		if !common.IsWindows() {
+			isolatedData.Files[filepath.Join("base", "link")] = isolated.SymLink("bar")
+		}
+		encoded, err := json.Marshal(isolatedData)
+		So(err, ShouldBeNil)
+		isolatedEncoded := string(encoded) + "\n"
+		isolatedHash := isolated.HashBytes([]byte(isolatedEncoded))
 
-	expected := map[string]string{
-		"0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33": "foo",
-		string(isolatedHash):                       isolatedEncoded,
-	}
-	actual := map[string]string{}
-	for k, v := range server.Contents() {
-		actual[string(k)] = string(v)
-	}
-	ut.AssertEqual(t, expected, actual)
-	ut.AssertEqual(t, isolatedHash, item.Digest())
+		expected := map[string]string{
+			"0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33": "foo",
+			string(isolatedHash):                       isolatedEncoded,
+		}
+		actual := map[string]string{}
+		for k, v := range server.Contents() {
+			actual[string(k)] = string(v)
+		}
+		So(actual, ShouldResemble, expected)
+		So(item.Digest(), ShouldResemble, isolatedHash)
 
-	stats := a.Stats()
-	ut.AssertEqual(t, 0, stats.TotalHits())
-	// There're 3 cache misses even if the same content is looked up twice.
-	ut.AssertEqual(t, 3, stats.TotalMisses())
-	ut.AssertEqual(t, units.Size(0), stats.TotalBytesHits())
-	ut.AssertEqual(t, units.Size(3+3+len(isolatedEncoded)), stats.TotalBytesPushed())
+		stats := a.Stats()
+		So(stats.TotalHits(), ShouldResemble, 0)
+		// There're 3 cache misses even if the same content is looked up twice.
+		So(stats.TotalMisses(), ShouldResemble, 3)
+		So(stats.TotalBytesHits(), ShouldResemble, units.Size(0))
+		So(stats.TotalBytesPushed(), ShouldResemble, units.Size(3+3+len(isolatedEncoded)))
 
-	ut.AssertEqual(t, nil, server.Error())
+		So(server.Error(), ShouldBeNil)
+	})
 }
