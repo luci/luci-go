13ab95970b130ff86fc02b0f6118b97527ab5ce2
vadimsh <vadimsh@chromium.org>
2017-03-14 19:05:02 -0700

    token-server: Move cron handlers into 'backend' module.
    There'll be more stuff in 'backend' soon (like upload of BigQuery rows). This
    change also makes load on 'default' module more uniform: it's only token minting
    RPCs now.
    
    R=iannucci@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2750973003

---
+++ tokenserver/appengine/backend/main.go
@@ -0,0 +1,119 @@
+// Copyright 2017 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package backend implements HTTP server that handles requests to 'backend'
+// module.
+//
+// Handlers here are called only via GAE cron or task queues.
+package backend
+
+import (
+	"net/http"
+	"sync"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+
+	"github.com/luci/gae/service/info"
+	"github.com/luci/luci-go/appengine/gaemiddleware"
+	"github.com/luci/luci-go/common/logging"
+	"github.com/luci/luci-go/server/router"
+
+	"github.com/luci/luci-go/tokenserver/api/admin/v1"
+
+	"github.com/luci/luci-go/tokenserver/appengine/impl/services/admin/adminsrv"
+	"github.com/luci/luci-go/tokenserver/appengine/impl/services/admin/certauthorities"
+)
+
+var (
+	caServer    = certauthorities.NewServer()
+	adminServer = adminsrv.NewServer()
+)
+
+func init() {
+	r := router.New()
+	basemw := gaemiddleware.BaseProd()
+
+	gaemiddleware.InstallHandlers(r, basemw)
+
+	r.GET("/internal/cron/read-config", basemw.Extend(gaemiddleware.RequireCron), readConfigCron)
+	r.GET("/internal/cron/fetch-crl", basemw.Extend(gaemiddleware.RequireCron), fetchCRLCron)
+
+	http.DefaultServeMux.Handle("/", r)
+}
+
+// readConfigCron is handler for /internal/cron/read-config GAE cron task.
+func readConfigCron(c *router.Context) {
+	// Don't override manually imported configs with 'nil' on devserver.
+	if info.IsDevAppServer(c.Context) {
+		c.Writer.WriteHeader(http.StatusOK)
+		return
+	}
+
+	wg := sync.WaitGroup{}
+	var errs [2]error
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		_, errs[0] = adminServer.ImportCAConfigs(c.Context, nil)
+		if errs[0] != nil {
+			logging.Errorf(c.Context, "ImportCAConfigs failed - %s", errs[0])
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		_, errs[1] = adminServer.ImportDelegationConfigs(c.Context, nil)
+		if errs[1] != nil {
+			logging.Errorf(c.Context, "ImportDelegationConfigs failed - %s", errs[1])
+		}
+	}()
+
+	wg.Wait()
+
+	// Retry cron job only on transient errors. On fatal errors let it rerun one
+	// minute later, as usual, to avoid spamming logs with errors.
+	c.Writer.WriteHeader(statusFromErrs(errs[:]))
+}
+
+// fetchCRLCron is handler for /internal/cron/fetch-crl GAE cron task.
+func fetchCRLCron(c *router.Context) {
+	list, err := caServer.ListCAs(c.Context, nil)
+	if err != nil {
+		panic(err) // let panic catcher deal with it
+	}
+
+	// Fetch CRL of each active CA in parallel. In practice there are very few
+	// CAs there (~= 1), so the risk of OOM is small.
+	wg := sync.WaitGroup{}
+	errs := make([]error, len(list.Cn))
+	for i, cn := range list.Cn {
+		wg.Add(1)
+		go func(i int, cn string) {
+			defer wg.Done()
+			_, err := caServer.FetchCRL(c.Context, &admin.FetchCRLRequest{Cn: cn})
+			if err != nil {
+				logging.Errorf(c.Context, "FetchCRL(%q) failed - %s", cn, err)
+				errs[i] = err
+			}
+		}(i, cn)
+	}
+	wg.Wait()
+
+	// Retry cron job only on transient errors. On fatal errors let it rerun one
+	// minute later, as usual, to avoid spamming logs with errors.
+	c.Writer.WriteHeader(statusFromErrs(errs))
+}
+
+// statusFromErrs returns 500 if any of gRPC errors is codes.Internal.
+func statusFromErrs(errs []error) int {
+	for _, err := range errs {
+		if grpc.Code(err) == codes.Internal {
+			return http.StatusInternalServerError
+		}
+	}
+	return http.StatusOK
+}
--- tokenserver/appengine/frontend/main.go
+++ tokenserver/appengine/frontend/main.go
@@ -2,22 +2,18 @@
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-// Package frontend implements HTTP server that handles requests to default
+// Package frontend implements HTTP server that handles requests to 'default'
 // module.
-//
-// It stitches together all the code.
 package frontend
 
 import (
 	"net/http"
-	"sync"
 
 	"github.com/golang/protobuf/proto"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 
-	"github.com/luci/gae/service/info"
 	"github.com/luci/luci-go/appengine/gaeauth/server"
 	"github.com/luci/luci-go/appengine/gaemiddleware"
 	"github.com/luci/luci-go/common/logging"
@@ -33,32 +29,6 @@ import (
 	"github.com/luci/luci-go/tokenserver/appengine/impl/services/admin/adminsrv"
 	"github.com/luci/luci-go/tokenserver/appengine/impl/services/admin/certauthorities"
 	"github.com/luci/luci-go/tokenserver/appengine/impl/services/minter/tokenminter"
-)
-
-var (
-	// caServerWithoutAuth implements admin.CertificateAuthorities RPC interface.
-	caServerWithoutAuth = certauthorities.NewServer()
-
-	// caServerWithAuth adds admin check to caServerWithoutAuth.
-	caServerWithAuth = &admin.DecoratedCertificateAuthorities{
-		Service: caServerWithoutAuth,
-		Prelude: adminPrelude("admin.CertificateAuthorities"),
-	}
-
-	// adminServerWithoutAuth implements admin.Admin RPC interface.
-	adminServerWithoutAuth = adminsrv.NewServer()
-
-	// adminServerWithAuth adds admin check to adminServerWithoutAuth.
-	adminServerWithAuth = &admin.DecoratedAdmin{
-		Service: adminServerWithoutAuth,
-		Prelude: adminPrelude("admin.Admin"),
-	}
-
-	// tokenMinterServer implements minter.TokenMinter RPC interface.
-	//
-	// It is main public API of the token server. It doesn't require any external
-	// authentication (it happens inside), and so it's installed as is.
-	tokenMinterServerWithoutAuth = tokenminter.NewServer()
 )
 
 // adminPrelude returns a prelude that authorizes only administrators.
@@ -91,10 +61,6 @@ func init() {
 	r.GET("/_ah/warmup", basemw, warmupHandler)
 	r.GET("/_ah/start", basemw, warmupHandler)
 
-	// Backend routes used for cron and task queues.
-	r.GET("/internal/cron/read-config", basemw.Extend(gaemiddleware.RequireCron), readConfigCron)
-	r.GET("/internal/cron/fetch-crl", basemw.Extend(gaemiddleware.RequireCron), fetchCRLCron)
-
 	// Install all RPC servers.
 	api := prpc.Server{
 		Authenticator: auth.Authenticator{
@@ -102,9 +68,15 @@ func init() {
 		},
 		UnaryServerInterceptor: grpcmon.NewUnaryServerInterceptor(nil),
 	}
-	admin.RegisterCertificateAuthoritiesServer(&api, caServerWithAuth)
-	admin.RegisterAdminServer(&api, adminServerWithAuth)
-	minter.RegisterTokenMinterServer(&api, tokenMinterServerWithoutAuth) // auth inside
+	admin.RegisterCertificateAuthoritiesServer(&api, &admin.DecoratedCertificateAuthorities{
+		Service: certauthorities.NewServer(),
+		Prelude: adminPrelude("admin.CertificateAuthorities"),
+	})
+	admin.RegisterAdminServer(&api, &admin.DecoratedAdmin{
+		Service: adminsrv.NewServer(),
+		Prelude: adminPrelude("admin.Admin"),
+	})
+	minter.RegisterTokenMinterServer(&api, tokenminter.NewServer()) // auth inside
 	discovery.Enable(&api)
 	api.InstallHandlers(r, basemw)
 
@@ -112,87 +84,10 @@ func init() {
 	http.DefaultServeMux.Handle("/", r)
 }
 
-/// Routes.
-
 // warmupHandler warms in-memory caches.
 func warmupHandler(c *router.Context) {
 	if err := server.Warmup(c.Context); err != nil {
 		panic(err) // let panic catcher deal with it
 	}
 	c.Writer.WriteHeader(http.StatusOK)
-}
-
-// readConfigCron is handler for /internal/cron/read-config GAE cron task.
-func readConfigCron(c *router.Context) {
-	// Don't override manually imported configs with 'nil' on devserver.
-	if info.IsDevAppServer(c.Context) {
-		c.Writer.WriteHeader(http.StatusOK)
-		return
-	}
-
-	wg := sync.WaitGroup{}
-	var errs [2]error
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		_, errs[0] = adminServerWithoutAuth.ImportCAConfigs(c.Context, nil)
-		if errs[0] != nil {
-			logging.Errorf(c.Context, "ImportCAConfigs failed - %s", errs[0])
-		}
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		_, errs[1] = adminServerWithoutAuth.ImportDelegationConfigs(c.Context, nil)
-		if errs[1] != nil {
-			logging.Errorf(c.Context, "ImportDelegationConfigs failed - %s", errs[1])
-		}
-	}()
-
-	wg.Wait()
-
-	// Retry cron job only on transient errors. On fatal errors let it rerun one
-	// minute later, as usual, to avoid spamming logs with errors.
-	c.Writer.WriteHeader(statusFromErrs(errs[:]))
-}
-
-// fetchCRLCron is handler for /internal/cron/fetch-crl GAE cron task.
-func fetchCRLCron(c *router.Context) {
-	list, err := caServerWithoutAuth.ListCAs(c.Context, nil)
-	if err != nil {
-		panic(err) // let panic catcher deal with it
-	}
-
-	// Fetch CRL of each active CA in parallel. In practice there are very few
-	// CAs there (~= 1), so the risk of OOM is small.
-	wg := sync.WaitGroup{}
-	errs := make([]error, len(list.Cn))
-	for i, cn := range list.Cn {
-		wg.Add(1)
-		go func(i int, cn string) {
-			defer wg.Done()
-			_, err := caServerWithoutAuth.FetchCRL(c.Context, &admin.FetchCRLRequest{Cn: cn})
-			if err != nil {
-				logging.Errorf(c.Context, "FetchCRL(%q) failed - %s", cn, err)
-				errs[i] = err
-			}
-		}(i, cn)
-	}
-	wg.Wait()
-
-	// Retry cron job only on transient errors. On fatal errors let it rerun one
-	// minute later, as usual, to avoid spamming logs with errors.
-	c.Writer.WriteHeader(statusFromErrs(errs))
-}
-
-// statusFromErrs returns 500 if any of gRPC errors is codes.Internal.
-func statusFromErrs(errs []error) int {
-	for _, err := range errs {
-		if grpc.Code(err) == codes.Internal {
-			return http.StatusInternalServerError
-		}
-	}
-	return http.StatusOK
 }
