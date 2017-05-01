8c2e2129068fd3649910e95c51943d65175599a2
vadimsh <vadimsh@chromium.org>
2017-05-01 14:23:51 -0700

    Always present account chooser when signing in users via GAE Users API.
    This is same hack as used by Monorail:
    https://chromium.googlesource.com/infra/infra/+/15b9f61d90423bcb2d240736e4a9401efddba42f
    https://chromium.googlesource.com/infra/infra/+/0c68c1aec12aa7b0442e532eb86d1a5ca2d10728
    
    R=hinoka@chromium.org, iannucci@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2851263003

--- appengine/gaeauth/server/cookies.go
+++ appengine/gaeauth/server/cookies.go
@@ -6,10 +6,13 @@ package server
 
 import (
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/context"
 
+	"github.com/luci/gae/service/info"
 	"github.com/luci/gae/service/user"
+	"github.com/luci/luci-go/common/logging"
 	"github.com/luci/luci-go/server/auth"
 	"github.com/luci/luci-go/server/auth/identity"
 )
@@ -35,10 +38,30 @@ func (m UsersAPIAuthMethod) Authenticate(c context.Context, r *http.Request) (*a
 	}, nil
 }
 
+const (
+	// serviceLoginURL is expected URL prefix for LoginURLs returned by prod GAE.
+	serviceLoginURL = "https://www.google.com/accounts/ServiceLogin?"
+	// accountChooserURL is what we use instead.
+	accountChooserURL = "https://www.google.com/accounts/AccountChooser?"
+)
+
 // LoginURL returns a URL that, when visited, prompts the user to sign in,
 // then redirects the user to the URL specified by dest.
 func (m UsersAPIAuthMethod) LoginURL(c context.Context, dest string) (string, error) {
-	return user.LoginURL(c, dest)
+	url, err := user.LoginURL(c, dest)
+	if err != nil {
+		return "", err
+	}
+	if !strings.HasPrefix(url, serviceLoginURL) {
+		if !info.IsDevAppServer(c) {
+			logging.Warningf(c, "Unexpected login URL: %q", url)
+		}
+		return url, nil
+	}
+	// Give the user a choice of existing accounts in their session or the option
+	// to add an account, even if they are currently signed in to exactly one
+	// account.
+	return accountChooserURL + url[len(serviceLoginURL):], nil
 }
 
 // LogoutURL returns a URL that, when visited, signs the user out,
