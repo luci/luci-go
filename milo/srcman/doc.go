acb7ae023851772341bcf1e077c130b7249cf971
Robert Iannucci <iannucci@chromium.org>
2017-08-17 19:17:18 -0700

    [milo] Add source manifest proto to milo.
    This just adds the bare proto and a new package `srcman` which will be
    used for ingesting, querying, diffing, caching, etc. source manifests.
    
    R=dnj@chromium.org, hinoka@chromium.org, mkynszek@google.com, nodir@chromium.org
    
    Bug: 468053
    Change-Id: I78c86ab12a9ea189a3bb913552cb0abeef7376b3

---
+++ milo/srcman/doc.go
@@ -0,0 +1,5 @@
+// Package srcman contains all functions pertaining to the storage, retrieveal
+// and aggregation of Source Manifest protos in Milo.
+//
+// (See go.chromium.org/luci/milo/api/source_manifest for the proto)
+package srcman
