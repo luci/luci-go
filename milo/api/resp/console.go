89032a357b3b5447e9a1a0847df90bfa93a33b78
Michael Anthony Knyszek <mknyszek@google.com>
2017-08-29 13:01:35 -0700

    [milo] Improve console user interface.
    Bug:
    Change-Id: Iac40ff25a754fdfe8be906fe28e2639ed812dfa7
    Reviewed-on: https://chromium-review.googlesource.com/638842
    Commit-Queue: Michael Knyszek <mknyszek@google.com>
    Reviewed-by: Ryan Tseng <hinoka@chromium.org>

--- milo/api/resp/console.go
+++ milo/api/resp/console.go
@@ -15,42 +15,274 @@
 package resp
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
+
 	"go.chromium.org/luci/milo/common/model"
 )
 
 // This file contains the structures for defining a Console view.
 // Console: The main entry point and the overall struct for a console page.
+// Category: A column in the console representing a builder category that may contain
+//   subcategories (other columns) as well as builders  References a commit with a list
+//   of build summaries.
 // BuilderRef: Used both as an input to request a builder and headers for the console.
-// CommitBuild: A row in the console.  References a commit with a list of build summaries.
-// ConsoleBuild: A cell in the console. Contains all information required to render the cell.
 
-// Console represents a console view.  Commit contains the full matrix of
-// Commits x Builder, and BuilderRef contains information on how to render
-// the header.  The two structs are expected to be consistent.  IE len(Console.[]BuilderRef)
-// Should equal len(commit.Build) for all commit in Console.Commit.
+// This file also contains an interface through which to render the console "tree."
+// ConsoleElement: Represents a renderable unit of the console. In this case a unit refers
+//   to either a builder (via a BuilderRef) or a category (via a Category).
+
+// Console represents a console view.  Commit contains a list of commits to be displayed
+// in this console. Table contains a tree of categories whose leaves are builders.
+// The builders maintain information regarding the builds for the corresponding commits
+// in Commit. MaxDepth is a useful piece of metadata for adding in empty rows in the
+// console's header.
 type Console struct {
 	Name string
 
-	Commit []CommitBuild
+	// Commit is a list of commits representing the list of commits to the left of the
+	// console.
+	Commit []Commit
 
-	BuilderRef []BuilderRef
+	// Table is a tree of builder categories used to generate the console's main table.
+	//
+	// Leaf nodes must always be of concrete type BuilderRef, interior nodes must
+	// always be of type Category. The root node is a dummy node without a name
+	// to simplify the implementation.
+	Table Category
+
+	// MaxDepth represents the maximum tree depth of Table.
+	MaxDepth int
 }
 
-// BuilderRef is an unambiguous reference to a builder, along with metadata on how
-// to lay it out for rendering.
+// ConsoleElement represents a single renderable console element.
+type ConsoleElement interface {
+	// Writes HTML into the given byte buffer.
+	//
+	// The two integer parameters represent useful pieces of metadata in
+	// rendering: current depth, and maximum depth.
+	RenderHTML(*bytes.Buffer, int, int)
+}
+
+// Category represents an interior node in a category tree for builders.
+//
+// Implements ConsoleElement.
+type Category struct {
+	Name string
+
+	// The node's children, which can be any console element.
+	Children []ConsoleElement
+
+	// The node's children in a map to simplify insertion.
+	childrenMap map[string]ConsoleElement
+}
+
+// NewCategory allocates a new Category struct with no children.
+func NewCategory(name string) *Category {
+	return &Category{
+		Name:        name,
+		childrenMap: make(map[string]ConsoleElement),
+		Children:    make([]ConsoleElement, 0),
+	}
+}
+
+// AddBuilder inserts the builder into this Category tree.
+//
+// AddBuilder will create new subcategories as a chain of Category structs
+// as needed until there are no categories remaining. The builder is then
+// made a child of the deepest such Category.
+func (c *Category) AddBuilder(categories []string, builder *BuilderRef) {
+	current := c
+	for _, category := range categories {
+		if child, ok := current.childrenMap[category]; ok {
+			original := child.(*Category)
+			current = original
+		} else {
+			newChild := NewCategory(category)
+			current.childrenMap[category] = ConsoleElement(newChild)
+			current.Children = append(current.Children, ConsoleElement(newChild))
+			current = newChild
+		}
+	}
+	current.childrenMap[builder.Name] = ConsoleElement(builder)
+	current.Children = append(current.Children, ConsoleElement(builder))
+}
+
+// BuilderRef is an unambiguous reference to a builder.
+//
+// It represents a single column of builds in the console view.
+//
+// Implements ConsoleElement.
 type BuilderRef struct {
 	// Name is the canonical reference to a specific builder.
 	Name string
-	// Category is a pipe "|" deliminated list of short strings used to catagorize
-	// and organize builders.  Adjacent builders with common categories will be
-	// merged on the header.
-	Category []string
 	// ShortName is a string of length 1-3 used to label the builder.
 	ShortName string
+	// The most recent build summaries for this builder.
+	Build []*model.BuildSummary
 }
 
-// CommitBuild is a row in the console.  References a commit with a list of build summaries.
-type CommitBuild struct {
-	Commit
-	Build []*model.BuildSummary
+// Convenience function for writing to bytes.Buffer: in our case, the
+// writes into the buffer should _never_ fail. It is a catastrophic error
+// if it does.
+func must(_ int, err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// State machine states for rendering builds.
+const (
+	empty = iota
+	top
+	middle
+	bottom
+	cell
+)
+
+// RenderHTML renders a BuilderRef as HTML with its builds in a column.
+func (br BuilderRef) RenderHTML(buffer *bytes.Buffer, depth int, maxDepth int) {
+	must(buffer.WriteString(`<div class="console-builder-column" style="flex-grow: 1">`))
+	// Add spaces if we haven't hit maximum depth to keep the grid consistent.
+	for i := 0; i < (maxDepth - depth); i++ {
+		must(buffer.WriteString(`<div class="console-space"></div>`))
+	}
+	// Render builder link
+	must(buffer.WriteString(`<div>`))
+	must(fmt.Fprintf(
+		buffer, `<a class="console-builder-item" href="/%s" title="%s">%s</a>`,
+		template.HTMLEscapeString(br.Name),
+		template.HTMLEscapeString(br.Name),
+		template.HTMLEscapeString(br.ShortName)))
+	must(buffer.WriteString(`</div>`))
+
+	must(buffer.WriteString(`<div class="console-build-column">`))
+
+	status := "None"
+	link := "#"
+	class := ""
+
+	// Below is a state machine for rendering a single builder's column.
+	// In essence, the state machine takes 3 inputs: the current state, and
+	// the if the next 2 builds exist. It uses this information to choose the
+	// next state.
+	//
+	// Each iteration, the state machine writes out the state's corresponding element,
+	// either a lone cell, the top of a long cell, the bottom of a long cell, the
+	// middle of a long cell, or an empty space.
+	//
+	// The ultimate goal of this state machine is to visually extend a single
+	// build down to the next known build for this builder by commit.
+
+	// Initialize state machine state.
+	//
+	// Could equivalently be implemented using a "start" state, but
+	// that requires a no-render special case which would make the
+	// state machine less clean.
+	var state int
+	switch {
+	case len(br.Build) == 1:
+		switch {
+		case br.Build[0] != nil:
+			state = cell
+		case br.Build[0] == nil:
+			state = empty
+		}
+	case len(br.Build) > 1:
+		switch {
+		case br.Build[0] != nil && br.Build[1] != nil:
+			state = cell
+		case br.Build[0] != nil && br.Build[1] == nil:
+			state = top
+		case br.Build[0] == nil:
+			state = empty
+		}
+	default:
+		panic("Rendering zero builds should have been caught earlier.")
+	}
+	// Execute state machine for determining cell type.
+	for i, build := range br.Build {
+		nextBuild := false
+		if i < len(br.Build)-1 {
+			nextBuild = br.Build[i+1] != nil
+		}
+		nextNextBuild := false
+		if i < len(br.Build)-2 {
+			nextNextBuild = br.Build[i+2] != nil
+		}
+
+		var nextState int
+		switch state {
+		case empty:
+			class = "empty-cell"
+			switch {
+			case nextBuild && nextNextBuild:
+				nextState = cell
+			case nextBuild && !nextNextBuild:
+				nextState = top
+			case !nextBuild:
+				nextState = empty
+			}
+		case top:
+			class = "cell-top"
+			status = build.Summary.Status.String()
+			link = build.SelfLink
+			switch {
+			case nextNextBuild:
+				nextState = bottom
+			case !nextNextBuild:
+				nextState = middle
+			}
+		case middle:
+			class = "cell-middle"
+			switch {
+			case nextNextBuild:
+				nextState = bottom
+			case !nextNextBuild:
+				nextState = middle
+			}
+		case bottom:
+			class = "cell-bottom"
+			switch {
+			case nextNextBuild:
+				nextState = cell
+			case !nextNextBuild:
+				nextState = top
+			}
+		case cell:
+			class = "cell"
+			status = build.Summary.Status.String()
+			link = build.SelfLink
+			switch {
+			case nextNextBuild:
+				nextState = cell
+			case !nextNextBuild:
+				nextState = top
+			}
+		default:
+			panic("Unrecognized state")
+		}
+		// Write current state's information.
+		must(fmt.Fprintf(buffer,
+			`<div><a class="console-%s status-%s" href="%s"></a></div>`,
+			class, status, link))
+
+		// Update state.
+		state = nextState
+	}
+	must(buffer.WriteString(`</div></div>`))
+}
+
+// RenderHTML renders the Category struct and its children as HTML into a buffer.
+func (c Category) RenderHTML(buffer *bytes.Buffer, depth int, maxDepth int) {
+	must(buffer.WriteString(`<div class="console-column">`))
+	must(fmt.Fprintf(buffer, `<div class="console-top-item">%s</div>
+				  <div class="console-top-row">`,
+		template.HTMLEscapeString(c.Name),
+	))
+	for _, child := range c.Children {
+		child.RenderHTML(buffer, depth+1, maxDepth)
+	}
+	must(buffer.WriteString(`</div></div>`))
 }
---
+++ milo/api/resp/console_test.go
@@ -0,0 +1,104 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resp
+
+import (
+	"testing"
+
+	"go.chromium.org/luci/milo/common/model"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+type testBuilder struct {
+	Builder  *BuilderRef
+	Category []string
+}
+
+// Test helpers
+func buildVerifyRoot(name string, builders []testBuilder, expectChildren int) *Category {
+	root := NewCategory(name)
+	for _, builder := range builders {
+		root.AddBuilder(builder.Category, builder.Builder)
+	}
+	So(len(root.Children), ShouldEqual, expectChildren)
+	So(root.Name, ShouldEqual, name)
+	return root
+}
+
+func verifyCategory(e ConsoleElement, expectChildren int, expectName string) *Category {
+	cat := e.(*Category)
+	So(len(cat.Children), ShouldEqual, expectChildren)
+	So(cat.Name, ShouldEqual, expectName)
+	return cat
+}
+
+func TestCategory(t *testing.T) {
+	Convey("Category structure", t, func() {
+		// Test structures
+		var emptycat []string
+		cat1 := []string{"66__bbl"}
+		cat2 := []string{"test.data"}
+		deepcat := []string{"Hi", "Goodbye"}
+		br1 := &BuilderRef{
+			Name:      "test 1",
+			ShortName: "t1",
+			Build:     []*model.BuildSummary{},
+		}
+		br2 := &BuilderRef{
+			Name:      "test 2",
+			ShortName: "t2",
+			Build:     []*model.BuildSummary{},
+		}
+
+		// Tests
+		Convey("Root category", func() {
+			buildVerifyRoot("root", []testBuilder{}, 0)
+		})
+
+		Convey("With builder", func() {
+			root := buildVerifyRoot("_root_", []testBuilder{{br1, emptycat}}, 1)
+			So(root.Children[0].(*BuilderRef).Name, ShouldEqual, br1.Name)
+		})
+
+		Convey("With nested categories", func() {
+			root := buildVerifyRoot("o_o", []testBuilder{{br1, deepcat}}, 1)
+			child1 := verifyCategory(root.Children[0], 1, deepcat[0])
+			child2 := verifyCategory(child1.Children[0], 1, deepcat[1])
+			So(child2.Children[0].(*BuilderRef).Name, ShouldEqual, br1.Name)
+		})
+
+		Convey("Multiple categories", func() {
+			root := buildVerifyRoot("@_@", []testBuilder{
+				{br1, cat1},
+				{br2, cat2},
+			}, 2)
+			child1 := verifyCategory(root.Children[0], 1, cat1[0])
+			So(child1.Children[0].(*BuilderRef).Name, ShouldEqual, br1.Name)
+			child2 := verifyCategory(root.Children[1], 1, cat2[0])
+			So(child2.Children[0].(*BuilderRef).Name, ShouldEqual, br2.Name)
+		})
+
+		Convey("Reusing existing categories", func() {
+			root := buildVerifyRoot("rut", []testBuilder{
+				{br1, cat1},
+				{br2, cat1},
+			}, 1)
+			child := verifyCategory(root.Children[0], 2, cat1[0])
+			So(child.Children[0].(*BuilderRef).Name, ShouldEqual, br1.Name)
+			So(child.Children[1].(*BuilderRef).Name, ShouldEqual, br2.Name)
+		})
+	})
+}
