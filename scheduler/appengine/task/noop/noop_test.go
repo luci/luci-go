0d3fead53c439a92f33949be4267add06fe8c1c5
Andrii Shyshkalov <tandrii@chromium.org>
2017-09-08 12:33:42 -0700

    scheduler: test that all tasks implement task.Manager interface.
    Non functional change, but speeds up iterations.
    
    Change-Id: Iba9adafd02fc70a68b4e7c79dee3f3b1c3c0ee5d
    Reviewed-on: https://chromium-review.googlesource.com/651009
    Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

---
+++ scheduler/appengine/task/noop/noop_test.go
@@ -0,0 +1,45 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package noop
+
+import (
+	"testing"
+	"time"
+
+	"go.chromium.org/gae/impl/memory"
+	"go.chromium.org/luci/scheduler/appengine/messages"
+	"go.chromium.org/luci/scheduler/appengine/task"
+	"go.chromium.org/luci/scheduler/appengine/task/utils/tasktest"
+	"golang.org/x/net/context"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+var _ task.Manager = (*TaskManager)(nil)
+
+func TestFullFlow(t *testing.T) {
+	Convey("Noop", t, func() {
+		c := memory.Use(context.Background())
+		mgr := TaskManager{sleepDuration: time.Nanosecond}
+		ctl := &tasktest.TestController{
+			TaskMessage:  &messages.NoopTask{},
+			SaveCallback: func() error { return nil },
+		}
+		So(mgr.LaunchTask(c, ctl), ShouldBeNil)
+		So(ctl.TaskState, ShouldResemble, task.State{
+			Status: task.StatusSucceeded,
+		})
+	})
+}
