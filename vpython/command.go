ff45a9d9bfe0d0a2b48a62498e09055d38e48149
Dan Jacques <dnj@google.com>
2017-09-13 06:05:24 -0700

    [vpython] Use execve on Linux/Mac.
    Use "execve" to launch Python on Linux/Mac. This causes "vpython" to
    *become* the target Python rather than simply host it, increasing the
    accuracy of the wrapper's Python-like behavior.
    
    We want to be non-invasive, meaning we don't want to modify the target
    Python, including injecting the shared lock file descriptor.
    Accordingly, we spawn a monitor process alongside the Python environment
    to ensure that its lock is held. This monitor process holds its own
    version of the lock and periodically polls the Python process to see if
    it exists. If it does not, the monitor process terminates.
    
    BUG=None
    TEST=local
    
    Change-Id: I0cedeb22e703f5206a7fe70ac8b761d29b80046f

---
+++ vpython/command.go
@@ -0,0 +1,103 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package vpython
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/system/environ"
+
+	"golang.org/x/net/context"
+)
+
+const (
+	// ApplicationCommandENV is an enviornment varible which, if present,
+	// instructs vpython jump to a command routine instead of its default
+	// interactive behavior.
+	ApplicationCommandENV = "_VPYTHON_APPLICATION_COMMAND"
+)
+
+// RunCommand runs the named application command, returning an application
+// return code.
+func RunCommand(c context.Context, v string, env environ.Env) int {
+	parts := strings.SplitN(v, ":", 2)
+	name, val := parts[0], ""
+	if len(parts) == 2 {
+		val = parts[1]
+	}
+
+	if rc, ok := runSystemCommand(c, name, val, env); ok {
+		return rc
+	}
+
+	logging.Errorf(c, "Unknown application command: %q", name)
+	return 1
+}
+
+func createCommand(c context.Context, name string, val interface{}, env environ.Env) (*exec.Cmd, error) {
+	self, err := os.Executable()
+	if err != nil {
+		return nil, errors.Annotate(err, "could not get executable").Err()
+	}
+
+	env = env.Clone()
+	if err := setCommand(env, name, val); err != nil {
+		return nil, err
+	}
+
+	// Start our monitoring process.
+	cmd := exec.Command(self)
+	cmd.Env = env.Sorted()
+	cmd.Stderr = os.Stderr
+	return cmd, nil
+}
+
+func setCommand(env environ.Env, name string, val interface{}) error {
+	if val != nil {
+		valEnc, err := envValueEncode(val)
+		if err != nil {
+			return err
+		}
+		name = fmt.Sprintf("%s:%s", name, valEnc)
+	}
+	env.Set(ApplicationCommandENV, name)
+	return nil
+}
+
+func envValueEncode(obj interface{}) (string, error) {
+	data, err := json.Marshal(obj)
+	if err != nil {
+		return "", errors.Annotate(err, "failed to marshal").Err()
+	}
+	return base64.StdEncoding.EncodeToString(data), nil
+}
+
+func envValueDecode(v string, obj interface{}) error {
+	data, err := base64.StdEncoding.DecodeString(v)
+	if err != nil {
+		return errors.Annotate(err, "failed to base64 decode").Err()
+	}
+	if err := json.Unmarshal(data, obj); err != nil {
+		return errors.Annotate(err, "failed to unmarshal").Err()
+	}
+	return nil
+}
--- vpython/system_windows.go
+++ vpython/system_windows.go
@@ -16,13 +16,71 @@ package vpython
 
 import (
 	"os"
+	"os/signal"
 	"syscall"
+
+	"go.chromium.org/luci/vpython/venv"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/system/environ"
+
+	"golang.org/x/net/context"
 )
 
-// forwardedSignals is the list of signals that should be forwarded to the
-// Python subproess.
-var forwardedSignals = []os.Signal{
-	syscall.SIGHUP,
-	syscall.SIGINT,
-	syscall.SIGQUIT,
+// systemSpecificLaunch launches the process described by "cmd" while ensuring
+// that the VirtualEnv lock is held throughout its duration (best effort).
+//
+// Only the Args, Env, and Dir fields of "cmd" are examined.
+//
+// On Windows, we launch it as a child process and interpret any signal that we
+// receive as terminal, cancelling the child.
+func systemSpecificLaunch(c context.Context, ve *venv.Env, args []string, env environ.Env, dir string) error {
+	c, cancelFunc := context.WithCancel(c)
+
+	cmd := ve.Interpreter().IsolatedCommand(c, args...)
+	cmd.Dir = dir
+	cmd.Env = env.Sorted()
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	signalC := make(chan os.Signal, 1)
+	signalDoneC := make(chan struct{})
+	signal.Notify(signalC, syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT)
+	defer func() {
+		signal.Stop(signalC)
+
+		close(signalC)
+		<-signalDoneC
+	}()
+
+	if err := cmd.Start(); err != nil {
+		return errors.Annotate(err, "failed to start process").Err()
+	}
+
+	logging.Fields{
+		"pid": cmd.Process.Pid,
+	}.Debugf(c, "Python subprocess has started!")
+
+	// Start our signal forwarding goroutine, now that the process is running.
+	go func() {
+		defer func() {
+			close(signalDoneC)
+		}()
+
+		for sig := range signalC {
+			logging.Debugf(c, "Received signal %v, terminating child...", sig)
+			cancelFunc()
+		}
+	}()
+
+	err := cmd.Wait()
+	logging.Debugf(c, "Python subprocess has terminated: %v", err)
+	if err != nil {
+		return errors.Annotate(err, "").Err()
+	}
+	return nil
 }
+
+func runSystemCommand(context.Context, string, string, environ.Env) (int, bool) { return 0, false }
