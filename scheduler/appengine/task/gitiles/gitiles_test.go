1256f7655bb7c9998862924313e6af1f97096079
Andrii Shyshkalov <tandrii@chromium.org>
2017-09-12 18:42:47 -0700

    [scheduler] gitiles task now emits triggers.
    Emitted triggers carry (repo, ref, revision). They will be
    consumed by buildbucket tasks in follow up CL.
    
    Bug: 646067
    Change-Id: I2aa8a5e2f758d678a4a37b8435103f34503a807e

--- scheduler/appengine/task/gitiles/gitiles_test.go
+++ scheduler/appengine/task/gitiles/gitiles_test.go
@@ -15,148 +15,130 @@
 package gitiles
 
 import (
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"golang.org/x/net/context"
 
 	"go.chromium.org/gae/impl/memory"
 	ds "go.chromium.org/gae/service/datastore"
-	"go.chromium.org/gae/service/urlfetch"
+	"go.chromium.org/luci/scheduler/appengine/internal"
 	"go.chromium.org/luci/scheduler/appengine/messages"
 	"go.chromium.org/luci/scheduler/appengine/task"
 	"go.chromium.org/luci/scheduler/appengine/task/utils/tasktest"
 
+	"github.com/golang/protobuf/proto"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 var _ task.Manager = (*TaskManager)(nil)
 
 func TestTriggerBuild(t *testing.T) {
-	Convey("LaunchTask triggers builds for new commits", t, func(ctx C) {
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			resp := ""
-			switch r.URL.Path {
-			case "/+refs":
-				resp = `)]}'
-{
-  "refs/heads/master": {
-    "value": "baddcafe"
-  }
-}`
-			case "/+log/refs/heads/master~..refs/heads/master":
-				resp = `)]}'
-{
-  "log": [
-    {
-      "commit": "baddcafe",
-      "tree": "deadfeed",
-      "parents": [
-        "b0000000"
-      ],
-      "author": {
-        "name": "First Last",
-        "email": "firstlast@example.com",
-        "time": "Mon Jan 01 12:00:00 1970 -0000"
-      },
-      "committer": {
-        "name": "First Last",
-        "email": "firstlast@example.com",
-        "time": "Mon Jan 01 12:00:00 1970 -0000"
-      },
-      "message": "test\n"
-    }
-  ]
-}`
-			default:
-				ctx.Printf("Unknown URL: %s\n", r.URL.Path)
-				w.WriteHeader(400)
-				return
-			}
-			w.WriteHeader(200)
-			w.Header().Set("Content-Type", "application/json")
-			io.WriteString(w, resp)
-		}))
-		defer ts.Close()
 
+	Convey("LaunchTask Triggers Jobs", t, func() {
 		c := memory.Use(context.Background())
-		c = urlfetch.Set(c, http.DefaultTransport)
-
-		m := TaskManager{}
+		cfg := &messages.GitilesTask{
+			Repo: "https://a.googlesource.com/b.git",
+			Refs: []string{"refs/heads/master", "refs/heads/branch", "refs/branch-heads/1.2.3"},
+		}
+		loadSavedRepo := func() *Repository {
+			r := &Repository{ID: "proj/gitiles:https://a.googlesource.com/b.git"}
+			So(ds.Get(c, r), ShouldBeNil)
+			return r
+		}
 		ctl := &tasktest.TestController{
-			TaskMessage: &messages.GitilesTask{
-				Repo: ts.URL,
-				Refs: []string{"refs/heads/master"},
-			},
+			TaskMessage:   cfg,
 			Client:        http.DefaultClient,
 			SaveCallback:  func() error { return nil },
-			OverrideJobID: "proj/job",
+			OverrideJobID: "proj/gitiles",
 		}
+		gitilesMock := &mockGitilesClient{}
+		m := TaskManager{mockGitilesClient: gitilesMock}
 
-		// Launch.
-		So(m.LaunchTask(c, ctl, nil), ShouldBeNil)
-		So(ctl.Log[0], ShouldEqual, "Trigger build for commit baddcafe")
-		r := Repository{ID: "proj/job:" + ts.URL}
-		So(ds.Get(c, &r), ShouldBeNil)
-		So(r, ShouldResemble, Repository{
-			ID: "proj/job:" + ts.URL,
-			References: []Reference{
-				{Name: "refs/heads/master", Revision: "baddcafe"},
-			},
+		Convey("new refs are discovered", func() {
+			gitilesMock.refs = func(ctx context.Context, repo string, path string) (map[string]string, error) {
+				return map[string]string{
+					"refs/heads/master": "deadbeef0",
+					"refs/weird":        "123456789",
+				}, nil
+			}
+			So(m.LaunchTask(c, ctl, nil), ShouldBeNil)
+			So(loadSavedRepo(), ShouldResemble, &Repository{
+				ID: "proj/gitiles:https://a.googlesource.com/b.git",
+				References: []Reference{
+					{Name: "refs/heads/master", Revision: "deadbeef0"},
+				},
+			})
+			So(ctl.Triggers, ShouldHaveLength, 1)
+			So(ctl.Triggers[0].ID, ShouldEqual, "https://a.googlesource.com/b.git/+/refs/heads/master@deadbeef0")
+			p := internal.TriggerPayload{}
+			So(proto.Unmarshal(ctl.Triggers[0].Payload, &p), ShouldBeNil)
+			So(p, ShouldResemble, internal.TriggerPayload{
+				Gitiles: &internal.GitilesTrigger{
+					Repo:     "https://a.googlesource.com/b.git",
+					Ref:      "refs/heads/master",
+					Revision: "deadbeef0",
+				},
+			})
+		})
+
+		Convey("do nothing if there are no new commits", func() {
+			ds.Put(c, &Repository{
+				ID: "proj/gitiles:https://a.googlesource.com/b.git",
+				References: []Reference{
+					{Name: "refs/heads/master", Revision: "deadbeef0"},
+				},
+			})
+			gitilesMock.refs = func(ctx context.Context, repo string, path string) (map[string]string, error) {
+				return map[string]string{
+					"refs/heads/master": "deadbeef0",
+					"refs/weird":        "123456789",
+				}, nil
+			}
+			So(m.LaunchTask(c, ctl, nil), ShouldBeNil)
+			So(ctl.Triggers, ShouldBeNil)
+			So(loadSavedRepo(), ShouldResemble, &Repository{
+				ID: "proj/gitiles:https://a.googlesource.com/b.git",
+				References: []Reference{
+					{Name: "refs/heads/master", Revision: "deadbeef0"},
+				},
+			})
+		})
+
+		Convey("Updated, not changed and deleted refs", func() {
+			ds.Put(c, &Repository{
+				ID: "proj/gitiles:https://a.googlesource.com/b.git",
+				References: []Reference{
+					{Name: "refs/heads/master", Revision: "deadbeef0"},
+					{Name: "refs/heads/branch", Revision: "123456789"},
+					{Name: "refs/was/watched", Revision: "098765432"},
+				},
+			})
+			gitilesMock.refs = func(ctx context.Context, repo string, path string) (map[string]string, error) {
+				return map[string]string{
+					"refs/heads/master":       "deadbeef1",
+					"refs/branch-heads/1.2.3": "baadcafe0",
+				}, nil
+			}
+			So(m.LaunchTask(c, ctl, nil), ShouldBeNil)
+			So(loadSavedRepo(), ShouldResemble, &Repository{
+				ID: "proj/gitiles:https://a.googlesource.com/b.git",
+				References: []Reference{
+					{Name: "refs/branch-heads/1.2.3", Revision: "baadcafe0"},
+					{Name: "refs/heads/master", Revision: "deadbeef1"},
+				},
+			})
+			So(ctl.Triggers, ShouldHaveLength, 2)
+			So(ctl.Triggers[0].ID, ShouldEqual, "https://a.googlesource.com/b.git/+/refs/branch-heads/1.2.3@baadcafe0")
+			So(ctl.Triggers[1].ID, ShouldEqual, "https://a.googlesource.com/b.git/+/refs/heads/master@deadbeef1")
 		})
 	})
+}
 
-	Convey("LaunchTask doesn't do anything if there are no new commits", t, func(ctx C) {
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			resp := ""
-			switch r.URL.Path {
-			case "/+refs":
-				resp = `)]}'
-{
-  "refs/heads/master": {
-    "value": "baddcafe"
-  }
-}`
-			default:
-				ctx.Printf("Unknown URL: %s\n", r.URL.Path)
-				w.WriteHeader(400)
-				return
-			}
-			w.WriteHeader(200)
-			w.Header().Set("Content-Type", "application/json")
-			io.WriteString(w, resp)
-		}))
-		defer ts.Close()
+type mockGitilesClient struct {
+	refs func(ctx context.Context, repoURL, refsPath string) (map[string]string, error)
+}
 
-		c := memory.Use(context.Background())
-		c = urlfetch.Set(c, http.DefaultTransport)
-
-		m := TaskManager{}
-		ctl := &tasktest.TestController{
-			TaskMessage: &messages.GitilesTask{
-				Repo: ts.URL,
-				Refs: []string{"refs/heads/master"},
-			},
-			Client:        http.DefaultClient,
-			SaveCallback:  func() error { return nil },
-			OverrideJobID: "proj/job",
-		}
-
-		// Launch.
-		So(ds.Put(c,
-			&Repository{
-				ID: "proj/job:" + ts.URL,
-				References: []Reference{
-					{Name: "refs/heads/master", Revision: "baddcafe"},
-				},
-			},
-		), ShouldBeNil)
-
-		So(m.LaunchTask(c, ctl, nil), ShouldBeNil)
-		So(len(ctl.Log), ShouldEqual, 0)
-	})
+func (m *mockGitilesClient) Refs(c context.Context, repo string, path string) (map[string]string, error) {
+	return m.refs(c, repo, path)
 }
