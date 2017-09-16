5a90ac1c13e5ef0cb8d67a29c10da1b42cf1a1b6
smut <smut@google.com>
2017-09-15 18:31:45 -0700

    Port Machine Provider Agent from luci-py
    Port of the Python Machine Provider agent to Go. Source:
    https://chromium.googlesource.com/infra/luci/luci-py/+/master/appengine/gce-backend/agent/
    
    Bug: 765882
    Change-Id: Ibd73196ce90409f11c1833a92866ac14ee11b907

---
+++ mp/cmd/mpagent/agent_linux.go
@@ -0,0 +1,132 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+	"os/exec"
+	"os/user"
+	"strconv"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+
+	"golang.org/x/net/context"
+)
+
+type LinuxStrategy struct {
+}
+
+// chown changes ownership of a path.
+func (LinuxStrategy) chown(ctx context.Context, username, path string) error {
+	user, err := user.Lookup(username)
+	if err != nil {
+		return err
+	}
+	uid, err := strconv.Atoi(user.Uid)
+	if err != nil {
+		return err
+	}
+	gid, err := strconv.Atoi(user.Gid)
+	if err != nil {
+		return err
+	}
+	return os.Chown(path, uid, gid)
+}
+
+// reboot reboots the machine.
+func (LinuxStrategy) reboot(ctx context.Context) error {
+	return exec.Command("/sbin/shutdown", "-r", "now").Run()
+}
+
+type SystemdStrategy struct {
+	LinuxStrategy
+}
+
+// start starts the agent.
+func (SystemdStrategy) start(ctx context.Context, _ string) error {
+	if err := exec.Command("systemctl", "daemon-reload").Run(); err != nil {
+		return err
+	}
+	if err := exec.Command("systemctl", "enable", "machine-provider-agent").Run(); err != nil {
+		return err
+	}
+	return exec.Command("systemctl", "start", "machine-provider-agent").Run()
+}
+
+// stop stops all instances of the agent.
+func (SystemdStrategy) stop(ctx context.Context) error {
+	return exec.Command("systemctl", "stop", "machine-provider-agent").Run()
+}
+
+type UpstartStrategy struct {
+	LinuxStrategy
+}
+
+// start starts the agent.
+func (UpstartStrategy) start(ctx context.Context, _ string) error {
+	if err := exec.Command("initctl", "reload-configuration").Run(); err != nil {
+		return err
+	}
+	return exec.Command("start", "machine-provider-agent").Run()
+}
+
+// stop stops all instances of the agent.
+func (UpstartStrategy) stop(ctx context.Context) error {
+	return exec.Command("stop", "machine-provider-agent").Run()
+}
+
+// systemdFound returns true iff systemd is supported on this machine.
+func systemdFound() bool {
+	return exec.Command("which", "systemctl").Run() == nil
+}
+
+// upstartFound returns true iff upstart is supported on this machine.
+func upstartFound() bool {
+	return exec.Command("init", "--version").Run() == nil
+}
+
+// getAgent returns an agent which runs on Linux, depending on supported init systems.
+func getAgent(ctx context.Context) (*Agent, error) {
+	if systemdFound() {
+		agent := Agent{
+			agentAutoStartPath:        "/etc/systemd/system/machine-provider-agent.service",
+			agentAutoStartTemplate:    "machine-provider-agent.service.tmpl",
+			logsDir:                   "/var/log/machine-provider-agent",
+			swarmingAutoStartPath:     "/etc/systemd/system/swarming-start-bot.service",
+			swarmingAutoStartTemplate: "swarming-start-bot.service.tmpl",
+			swarmingBotDir:            "/b/s",
+			strategy:                  SystemdStrategy{},
+		}
+		logging.Infof(ctx, "Using systemd Linux agent.")
+		return &agent, nil
+	}
+
+	if upstartFound() {
+		agent := Agent{
+			agentAutoStartPath:        "/etc/init/machine-provider-agent.conf",
+			agentAutoStartTemplate:    "machine-provider-agent.conf.tmpl",
+			logsDir:                   "/var/log/messages/machine-provider-agent",
+			swarmingAutoStartPath:     "/etc/init/swarming-start-bot.conf",
+			swarmingAutoStartTemplate: "swarming-start-bot.conf.tmpl",
+			swarmingBotDir:            "/b/s",
+			strategy:                  UpstartStrategy{},
+		}
+		logging.Infof(ctx, "Using Upstart Linux agent.")
+		return &agent, nil
+	}
+
+	return nil, errors.New("unsupported init system, expected systemd or Upstart")
+}
---
+++ mp/cmd/mpagent/agent_windows.go
@@ -0,0 +1,71 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os/exec"
+	"syscall"
+
+	"go.chromium.org/luci/common/logging"
+
+	"golang.org/x/net/context"
+)
+
+type WindowsStrategy struct {
+}
+
+// chown changes ownership of a path.
+func (WindowsStrategy) chown(ctx context.Context, username, path string) error {
+	// TODO(smut): Determine if this is necessary on Windows.
+	return nil
+}
+
+// reboot reboots the machine.
+func (WindowsStrategy) reboot(ctx context.Context) error {
+	return exec.Command("shutdown", "/f", "/r", "/t", "0").Run()
+}
+
+// start starts the agent.
+func (WindowsStrategy) start(ctx context.Context, path string) error {
+	cmd := exec.Command(path)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		// https://msdn.microsoft.com/en-us/library/windows/desktop/ms684863.aspx
+		// CREATE_NEW_PROCESS_GROUP: 	0x200
+		// DETACHED_PROCESS: 		0x008
+		CreationFlags: 0x200 | 0x8,
+	}
+	return cmd.Start()
+}
+
+// stop stops all instances of the agent.
+func (WindowsStrategy) stop(ctx context.Context) error {
+	// TODO(smut): Stop the agent.
+	return nil
+}
+
+// getAgent returns an agent which runs on Windows.
+func getAgent(ctx context.Context) (*Agent, error) {
+	agent := Agent{
+		agentAutoStartPath:        "C:\\Users\\{{.User}}\\Start Menu\\Programs\\Startup\\machine-provider-agent.bat",
+		agentAutoStartTemplate:    "machine-provider-agent.bat.tmpl",
+		logsDir:                   "C:\\logs",
+		swarmingAutoStartPath:     "C:\\Users\\{{.User}}\\Start Menu\\Programs\\Startup\\swarming-start-bot.bat",
+		swarmingAutoStartTemplate: "swarming-start-bot.bat.tmpl",
+		swarmingBotDir:            "C:\\b\\s",
+		strategy:                  WindowsStrategy{},
+	}
+	logging.Infof(ctx, "Using Windows agent.")
+	return &agent, nil
+}
