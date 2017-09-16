4b6d9ce1d2bbb2bec08486949e9f258d30abc4f2
Vadim Shtayura <vadimsh@chromium.org>
2017-09-15 18:45:02 -0700

    scheduler: Store triggers in the Invocation entity.
    Display them in UI while at it.
    
    R=tandrii@chromium.org
    BUG=764043, 646067
    
    Change-Id: I64bf9f4c669fe1224d8f231cb4f792b2e7e920c4

--- scheduler/appengine/task/task.go
+++ scheduler/appengine/task/task.go
@@ -102,13 +102,26 @@ type Traits struct {
 
 // Trigger is passed from a triggering job to a triggered job.
 type Trigger struct {
-	// ID must uniquely identify a Trigger in time. It is used to deduplicate and
-	// hence provide idempotency for adding a trigger.
+	// ID must uniquely identify a Trigger in time.
+	//
+	// It is used to deduplicate and hence provide idempotency for adding
+	// a trigger.
 	ID string
+
+	// Created is when this trigger was created, used in UI only.
+	Created time.Time
+
+	// Title is a user friendly name for this trigger that shows up in UI.
+	//
+	// Doesn't have to be unique.
+	Title string
+
+	// URL is optional HTTP link to display in UI.
+	URL string
 
 	// Payload stores data passed from a triggering Job to a triggered Job.
 	// It is opaque to the engine and could be empty. It is up to respective
-	// TaskManager instances to interprete it.
+	// TaskManager instances to interpret it.
 	//
 	// Payload should be fairly small in order for all outstanding triggers to fit
 	// into 1 datastore entry.
