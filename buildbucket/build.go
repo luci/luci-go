0d31363ecb89421f0ce72ad82baaf890097339e8
Nodir Turakulov <nodir@google.com>
2017-09-21 03:24:02 -0700

    [buildbucket] add build parsing
    Add buildbucket.Build type that is more convenient than
    buildbucket.ApiCommonBuildMessage that a lot of places use.
    
    Add buildbucket.ParseBuild that converts *buildbucket.ApiCommonBuildMessage
    to buildbucket.Build.
    
    Bug:
    Change-Id: I0d889972453d011da00cea5c9ee03a8fdf87faa9

---
+++ buildbucket/build.go
@@ -0,0 +1,209 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buildbucket
+
+import (
+	"encoding/json"
+	"strings"
+	"time"
+
+	"fmt"
+
+	"go.chromium.org/luci/common/api/buildbucket/buildbucket/v1"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/server/auth/identity"
+)
+
+// CanaryPreference specifies whether a build should use canary of the build
+// infrastructure.
+type CanaryPreference int
+
+const (
+	// NoCanaryPreference specifies that either prod or canary version of build
+	// infrastructure can be used for this build.
+	NoCanaryPreference CanaryPreference = iota
+	// Prod specifies that the prod version of build infrastructure
+	// is preferred for this build.
+	Prod
+	// Canary specifies that the canary version of build infrastructure
+	// is preferred for this build.
+	Canary
+)
+
+// Build is a buildbucket build.
+// It is a more type-safe version of buildbucket.ApiCommonBuildMessage.
+type Build struct {
+
+	// fields set at the build creation time
+
+	ID           int64
+	CreationTime time.Time
+	// TODO: move identity package to //common/auth/identity
+	CreatedBy        identity.Identity
+	Bucket           string
+	Builder          string
+	Tags             Tags
+	Input            Input
+	CanaryPreference CanaryPreference
+
+	// fields that can change during build lifetime
+
+	Status           Status
+	StatusChangeTime time.Time
+	URL              string
+	UpdateTime       time.Time
+	Canary           bool
+
+	// fields set on build completion
+
+	CompletionTime time.Time
+	Output         Output
+}
+
+// Output is build input.
+type Input struct {
+	Properties map[string]interface{}
+
+	// TODO(nodir): add support for changes
+	// https://chromium.googlesource.com/chromium/tools/build/+/master/scripts/master/buildbucket/README.md#build-parameters
+}
+
+//// Change
+//type Change struct {
+//	ID string `json:"id"`
+//	Revision string `json:"revision"`
+//	Author struct {
+//		Email string `json:"email"`
+//	} `json:"author"`
+//	Message string  `json:"message"`
+//	Branch string  `json:"branch"`
+//	URL string  `json:"url"`
+//	Files []struct {
+//		Path string `json:"path"`
+//	} `json:"files"`
+//
+//	// TODO: maybe add the rest of the fields
+//	// https://chromium.googlesource.com/chromium/tools/build/+/master/scripts/master/buildbucket/README.md#build-parameters
+//}
+
+// Output is build output.
+type Output struct {
+	Properties map[string]interface{}
+
+	// TODO(nodir): add support for changes
+	// https://chromium.googlesource.com/chromium/tools/build/+/master/scripts/master/buildbucket/README.md#build-parameters
+}
+
+// ParseBuild parses a build message to Build.
+func ParseBuild(raw *buildbucket.ApiCommonBuildMessage) (*Build, error) {
+	status, err := ParseStatus(raw)
+	if err != nil {
+		return nil, err
+	}
+
+	createdBy, err := identity.MakeIdentity(raw.CreatedBy)
+	if err != nil {
+		return nil, err
+	}
+
+	tags := ParseTags(raw.Tags)
+	builder := tags.Get(TagBuilder)
+	tags.Del(TagBuilder)
+
+	b := &Build{
+		ID:           raw.Id,
+		CreationTime: ParseTimestamp(raw.CreatedTs),
+		CreatedBy:    createdBy,
+		Bucket:       raw.Bucket,
+		Builder:      builder,
+		Tags:         tags,
+
+		Status:           status,
+		StatusChangeTime: ParseTimestamp(raw.StatusChangedTs),
+		URL:              raw.Url,
+		UpdateTime:       ParseTimestamp(raw.UpdatedTs),
+		Canary:           raw.Canary,
+
+		CompletionTime: ParseTimestamp(raw.CompletedTs),
+	}
+
+	switch b.CanaryPreference {
+	case "PROD":
+		b.CanaryPreference = Prod
+	case "CANARY":
+		b.CanaryPreference = Canary
+	}
+
+	if err := parseJSON(raw.ParametersJson, &b.Input); err != nil {
+		return nil, errors.Annotate(err, "invalid raw.ParametersJson").Err()
+	}
+
+	if err := parseJSON(raw.ResultDetailsJson, &b.Output); err != nil {
+		return nil, errors.Annotate(err, "invalid raw.ResultDetailsJson").Err()
+	}
+
+	return b, nil
+}
+
+// Message converts b to a build creation request.
+func (b *Build) Message() (*buildbucket.ApiPutRequestMessage, error) {
+	msg := &buildbucket.ApiPutRequestMessage{
+		Bucket: b.Bucket,
+		Tags:   b.Tags.Format(),
+	}
+
+	switch b.CanaryPreference {
+	case Prod:
+		msg.CanaryPreference = "PROD"
+	case Canary:
+		msg.CanaryPreference = "CANARY"
+	case NoCanaryPreference:
+		// that's default
+	default:
+		return nil, fmt.Errorf("invalid CanaryPreference %v", b.CanaryPreference)
+	}
+
+	parameters := map[string]interface{}{
+		"builder_name": b.Builder,
+		"properties":   b.Input.Properties,
+	}
+	if data, err := json.Marshal(parameters); err != nil {
+		return nil, err
+	} else {
+		msg.ParametersJson = string(data)
+	}
+
+	return msg, nil
+}
+
+func parseJSON(data string, v interface{}) error {
+	if data == "" {
+		return nil
+	}
+	return json.NewDecoder(strings.NewReader(data)).Decode(v)
+}
+
+// ParseTimestamp parses a buildbucket timestamp.
+func ParseTimestamp(usec int64) time.Time {
+	if usec == 0 {
+		return time.Time{}
+	}
+	return time.Unix(usec/1e6, (usec%1e6)*1e3)
+}
+
+// FormatTimestamp t converts to a buildbucket timestamp.
+func FormatTimestamp(t time.Time) int64 {
+	return t.UnixNano() / 1000
+}
---
+++ buildbucket/doc.go
@@ -0,0 +1,3 @@
+// Package buildbucket defines Go types that are more convenient than the API
+// types in "go.chromium.org/luci/common/api/buildbucket/buildbucket/v1"
+package buildbucket
