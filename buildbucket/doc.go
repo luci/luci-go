da8c4582fb4eae0903be433ed5c85bc486ff248f
Nodir Turakulov <nodir@google.com>
2017-09-29 11:16:04 -0700

    [buildbucket] add an idiomatic Build type
    All *buildbucket.ApiCommonBuildMessage users have to do a lot of parsing
    because of int64 for timestamps, JSON-encoded fields for properties,
    stringly created_by, []string for tags, untyped buildset string tag
    and a 3 level status field.
    
    Add a more convenient idiomatic buildbucket.Build type.
    The goal is to be as Go-native as possible.
    
    Add (*Build).ParseMessage that converts *buildbucket.ApiCommonBuildMessage
    to buildbucket.Build.
    
    If this successful, we can design buildbucket API v2 based on this.
    
    Bug:
    Change-Id: I0d889972453d011da00cea5c9ee03a8fdf87faa9

---
+++ buildbucket/doc.go
@@ -0,0 +1,17 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package buildbucket defines Go types that are more convenient than the API
+// types in "go.chromium.org/luci/common/api/buildbucket/buildbucket/v1"
+package buildbucket
