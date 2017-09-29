08cf3585764b0e215589c9c34773201f7f9525bf
Nodir Turakulov <nodir@google.com>
2017-09-29 12:37:19 -0700

    [milo] include completed builds in builder tests
    buildbucket/builder_test.go ignores test data of completed builds.
    Include them in tests
    
    R=iannucci@chromium.org
    Bug: 769338
    Change-Id: I19325060550c80a2b065be3ac0b55354a7111030
    Reviewed-on: https://chromium-review.googlesource.com/692578
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

--- milo/buildsource/buildbucket/builder_test.go
+++ milo/buildsource/buildbucket/builder_test.go
@@ -57,7 +57,7 @@ func TestBuilder(t *testing.T) {
 				err := os.MkdirAll(filepath.Dir(expectationFilePath), 0777)
 				So(err, ShouldBeNil)
 
-				actual, err := GetBuilder(c, tc.bucket, tc.builder, 0)
+				actual, err := GetBuilder(c, tc.bucket, tc.builder, 20)
 				So(err, ShouldBeNil)
 				actualJSON, err := json.MarshalIndent(actual, "", "  ")
 				So(err, ShouldBeNil)
