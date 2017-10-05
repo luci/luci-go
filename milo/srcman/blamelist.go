6b1c6094d65c4ee81f74278586f099a1f99ae028
Robert Iannucci <iannucci@chromium.org>
2017-10-05 16:25:31 -0700

    [milo] Add backend code for blamelist calculation and caching.
    R=nodir@chromium.org, tandrii@chromium.org
    
    Bug: 758007
    Change-Id: I231ad98e94e5c4a2183e6369b36803eb7df8f101

---
+++ milo/srcman/blamelist.go
@@ -0,0 +1,296 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package srcman
+
+import (
+	"bytes"
+	"compress/gzip"
+	"crypto/sha256"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	"golang.org/x/net/context"
+
+	"go.chromium.org/gae/service/memcache"
+	"go.chromium.org/luci/common/api/gitiles"
+	"go.chromium.org/luci/common/data/base128"
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/proto/milo"
+	"go.chromium.org/luci/common/sync/parallel"
+	"go.chromium.org/luci/server/auth"
+)
+
+// DiffOptions allows the caller of DiffWithBlamelist to opt-in to including git
+// history. If history is opted for, the caller may also opt to include
+// tree-diff information with WithFiles.
+type DiffOptions struct {
+	WithHistory bool
+	WithFiles   bool
+}
+
+func (d *DiffOptions) String() string {
+	ret := ""
+	if d.WithHistory {
+		ret += "H"
+	}
+	if d.WithFiles {
+		ret += "F"
+	}
+	return ret
+}
+
+func decodeMD(data []byte) (*milo.ManifestDiff, error) {
+	r, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	newData, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	ret := &milo.ManifestDiff{}
+	if err := proto.Unmarshal(newData, ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+func encodeMD(d *milo.ManifestDiff) ([]byte, error) {
+	raw, err := proto.Marshal(d)
+	if err != nil {
+		return nil, err
+	}
+	buf := &bytes.Buffer{}
+	w := gzip.NewWriter(buf)
+	if _, err = w.Write(raw); err != nil {
+		return nil, err
+	}
+	if err = w.Close(); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func manifestDiffMCItem(c context.Context, hashA, hashB []byte, options *DiffOptions) (memcache.Item, bool) {
+	if len(hashA) == sha256.Size && len(hashB) == sha256.Size {
+		return memcache.NewItem(c, fmt.Sprintf("DiffWithBlamelist(%s,%s,%s)",
+			options,
+			base128.EncodeToString(hashA),
+			base128.EncodeToString(hashB))), true
+	}
+	return nil, false
+}
+
+// HACK(iannucci,hinoka,tandrii) - Until Milo properly supports ACLs for
+// blamelists, we have a hack; if the git repo being diff'd is in this list, use
+// `auth.AsSelf`. Otherwise use `auth.Anonymous`.
+//
+// The reason to do this is that we currently do blamelist calculation in the
+// backend, so we can't accurately determine if the requesting user has access
+// to these repos or not. For now, we use this whitelist to indicate domains
+// that we know have full public read-access so that we can use milo's
+// credentials (instead of anonymous) in order to avoid hitting gitiles'
+// anonymous quota limits.
+var whitelistDomains = stringset.NewFromSlice(
+	"chromium.googlesource.com",
+)
+
+type historyWorkItem struct {
+	c      context.Context
+	client *gitiles.Client
+
+	dirname   string
+	withFiles bool
+	treeish   string
+
+	toUpdate *milo.ManifestDiff_GitCheckout
+	ok       bool
+}
+
+func (i *historyWorkItem) process() error {
+	opts := []gitiles.LogOption{gitiles.Limit(100)}
+	if i.withFiles {
+		opts = append(opts, gitiles.WithTreeDiff)
+	}
+	log, err := i.client.Log(i.c, i.toUpdate.RepoUrl, i.treeish, opts...)
+	if err != nil {
+		logging.WithError(err).Warningf(i.c, "fetching log - %q", i.dirname)
+		return nil
+	}
+
+	i.toUpdate.History, err = gitiles.LogProto(log)
+	if err != nil {
+		logging.WithError(err).Warningf(i.c, "protoizing log - %q", i.dirname)
+		return nil
+	}
+
+	i.ok = true
+	return nil
+}
+
+// PopulateHistory attempts to populate the git histories for any DIFF-status
+// GitCheckouts in the ManifestDiff.
+//
+// This is only able to populate histories from googlesource.com-based git
+// repos.
+//
+// Any errors fetching the histories will be logged, but the errors will not be
+// returned.
+//
+// This function returns true iff all git histories that COULD be filled in,
+// were actually successfully filled in (and thus, should the resulting diff
+// object be cached).
+func PopulateHistory(c context.Context, diff *milo.ManifestDiff, withFiles bool) (ok bool) {
+	logging.Infof(c, "populating git history in ManifestDiff")
+
+	workList := []*historyWorkItem{}
+
+	var noauthClient *gitiles.Client
+	if t, err := auth.GetRPCTransport(c, auth.NoAuth); err != nil {
+		logging.WithError(err).Errorf(c, "getting NoAuth transport")
+	} else {
+		noauthClient = &gitiles.Client{Client: &http.Client{Transport: t}}
+	}
+
+	var selfClient *gitiles.Client
+	if t, err := auth.GetRPCTransport(c, auth.AsSelf); err != nil {
+		logging.WithError(err).Errorf(c, "getting AsSelf transport")
+	} else {
+		selfClient = &gitiles.Client{Client: &http.Client{Transport: t}}
+	}
+
+	if selfClient == nil && noauthClient == nil {
+		logging.Warningf(c, "could not build any gitiles.Clients, aborting")
+		return
+	}
+
+	for dirname, dir := range diff.Directories {
+		gitCheckout := dir.GetGitCheckout()
+		if gitCheckout.Overall != milo.ManifestDiff_DIFF {
+			continue
+		}
+
+		u, err := url.Parse(gitCheckout.RepoUrl)
+		if err != nil {
+			logging.WithError(err).Warningf(c, "could not parse RepoUrl %q / %q", dirname, gitCheckout.RepoUrl)
+			continue
+		}
+
+		if !strings.HasSuffix(u.Host, "googlesource.com") {
+			logging.WithError(err).Warningf(c, "unsupported git host %q / %q", dirname, gitCheckout.RepoUrl)
+			continue
+		}
+
+		item := &historyWorkItem{
+			c,
+			noauthClient,
+
+			dirname,
+			withFiles,
+			fmt.Sprintf(
+				"%s..%s",
+				diff.Old.Directories[dirname].GitCheckout.Revision,
+				diff.New.Directories[dirname].GitCheckout.Revision,
+			),
+
+			gitCheckout,
+			false,
+		}
+		if whitelistDomains.Has(u.Host) && selfClient != nil {
+			item.client = selfClient
+		}
+
+		workList = append(workList, item)
+	}
+
+	switch len(workList) {
+	case 0:
+		logging.Infof(c, "no work items to process")
+	case 1:
+		logging.Infof(c, "one work item to process")
+		_ = workList[0].process() // always returns nil, logs all errors itself
+	default:
+		logging.Infof(c, "%d work items to process", len(workList))
+		limit := 8
+		if len(workList) < limit {
+			limit = len(workList)
+		}
+		// always returns RunMulti's error, which is nil, because process returns
+		// nil.
+		_ = parallel.RunMulti(c, limit, func(mr parallel.MultiRunner) error {
+			return mr.RunMulti(func(ch chan<- func() error) {
+				for _, wi := range workList {
+					ch <- wi.process
+				}
+			})
+		})
+	}
+
+	for _, wi := range workList {
+		if !wi.ok {
+			return false
+		}
+	}
+	return true
+}
+
+// DiffWithBlamelist retrieves the manifests, computes a diff, then adds git
+// history logs for git checkouts with a status of "DIFF".
+//
+// These operations are cached in memcache (not datastore).
+func DiffWithBlamelist(c context.Context, a, b *milo.ManifestLink, options *DiffOptions) (*milo.ManifestDiff, error) {
+	if mi, ok := manifestDiffMCItem(c, a.Sha256, b.Sha256, options); ok {
+		if err := memcache.Get(c, mi); err != nil {
+			ret, err := decodeMD(mi.Value())
+			if err != nil {
+				logging.WithError(err).Warningf(c, "error decoding cached diff")
+			} else {
+				return ret, nil
+			}
+		}
+	}
+
+	// need to fetch these for real.
+	mans, hashes, err := GetMulti(c, a, b)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := mans[0].Diff(mans[1])
+	shouldCache := true
+
+	if options.WithHistory {
+		shouldCache = PopulateHistory(c, ret, options.WithFiles)
+	}
+
+	if shouldCache {
+		if mi, ok := manifestDiffMCItem(c, hashes[0], hashes[1], options); ok {
+			data, err := encodeMD(ret)
+			if err != nil {
+				logging.WithError(err).Warningf(c, "error encoding diff cache value")
+			} else {
+				if err := memcache.Set(c, mi.SetValue(data)); err != nil {
+					logging.WithError(err).Warningf(c, "error writing diff cache value")
+				}
+			}
+		}
+	}
+	return ret, nil
+}
