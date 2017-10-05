99fcb12df81757745ab8cf362c202e1971a03305
Michael Anthony Knyszek <mknyszek@google.com>
2017-10-05 15:31:27 -0700

    [luci-notify] Use commits for "previous" build.
    This alters how luci-notify determines a "previous" build for the
    on_change notification. If commit information is available for a
    given build from its buildset, then luci-notify will start to use
    that (the combination of commit time and revision) to track changes.
    If at any point that information is unavailable, it will fall back
    to build creation time as before.
    
    Change-Id: I72666f8e3cb718cff303f99d5a8d9f67f5660dc9

--- luci_notify/notify/builder.go
+++ luci_notify/notify/builder.go
@@ -21,6 +21,7 @@ import (
 
 	"go.chromium.org/gae/service/datastore"
 	"go.chromium.org/luci/buildbucket"
+	"go.chromium.org/luci/common/api/gitiles"
 	"go.chromium.org/luci/common/logging"
 )
 
@@ -35,26 +36,37 @@ type Builder struct {
 	Status buildbucket.Status
 
 	// StatusTime can be used to decide whether Status should be updated.
-	// It is computed as the creation time of the build that caused
-	// a change of Status.
+	// It is computed as either the creation time of the build that caused
+	// a change of Status, or the commit time of the revision associated
+	// with the that build. The former definition is used if
+	// StatusRevision==nil, meaning this builder has no one repository
+	// associated with it.
 	StatusTime time.Time
-}
 
-// NewBuilder constructs a new Builder.
-//
-// This is intended to maintain a consistent interface to datastore models and
-// mimics the behavior of NewProject and NewNotifier.
-func NewBuilder(id string, build *buildbucket.Build) *Builder {
-	return &Builder{
-		ID:         id,
-		Status:     build.Status,
-		StatusTime: build.CreationTime,
-	}
+	// StatusRevision can be used to decided whether a Status should be updated.
+	// It is the revision of the codebase that's associated with the build
+	// that last caused a change of Status.
+	StatusRevision string
 }
 
 // StatusUnknown is used in the LookupBuilder return value
 // if builder status is unknown.
 var StatusUnknown buildbucket.Status = -1
+
+// NewBuilder creates a new builder from an ID, a build, and optionally a commit.
+func NewBuilder(id string, build *buildbucket.Build, commit *gitiles.Commit) *Builder {
+	builder := &Builder{
+		ID:     id,
+		Status: build.Status,
+	}
+	if commit != nil {
+		builder.StatusTime = time.Time(commit.Committer.Time)
+		builder.StatusRevision = commit.Commit
+	} else {
+		builder.StatusTime = build.CreationTime
+	}
+	return builder
+}
 
 // LookupBuilder returns a "previous" builder state.
 //
@@ -63,7 +75,7 @@ var StatusUnknown buildbucket.Status = -1
 // have never recorded information about this builder before.
 //
 // It will also update the Builder in the datastore according to build.
-func LookupBuilder(c context.Context, id string, build *buildbucket.Build) (*Builder, error) {
+func LookupBuilder(c context.Context, id string, build *buildbucket.Build, commit *gitiles.Commit) (*Builder, error) {
 	prev := &Builder{ID: id}
 	err := datastore.RunInTransaction(c, func(c context.Context) error {
 		switch err := datastore.Get(c, prev); {
@@ -75,13 +87,25 @@ func LookupBuilder(c context.Context, id string, build *buildbucket.Build) (*Bui
 		case err != nil:
 			return err
 
-		case prev.StatusTime.After(build.CreationTime):
+		// If we have revision information, but the revision isn't new...
+		//
+		// ...if it's the same revision...
+		case commit != nil && commit.Commit == prev.StatusRevision:
+			return nil
+
+		// ...or if it's an older revision.
+		case commit != nil && commit.Commit != prev.StatusRevision && prev.StatusTime.After(time.Time(commit.Committer.Time)):
+			logging.Debugf(c, "build %d (%s) from commit at %s, is not new", build.ID, build.Status, commit.Committer.Time)
+			return nil
+
+		// If we're in the fallback case (i.e. no revision information) check against creation time.
+		case commit == nil && prev.StatusRevision == "" && prev.StatusTime.After(build.CreationTime):
 			// Don't update the datastore if the new build is not newer.
 			logging.Debugf(c, "build %d (%s) created at %s, is not new", build.ID, build.Status, build.CreationTime)
 			return nil
 		}
 
-		return datastore.Put(c, NewBuilder(prev.ID, build))
+		return datastore.Put(c, NewBuilder(id, build, commit))
 	}, nil)
 	if err != nil {
 		return nil, err
