978ac2430a7cec3da0b1032a703557f1884bc92a
Michael Anthony Knyszek <mknyszek@google.com>
2017-10-05 16:49:45 -0700

    [luci-notify] Use commits for "previous" build.
    This alters how luci-notify determines a "previous" build for the
    on_change notification. If commit information is available for a
    given build from its buildset, then luci-notify will start to use
    that (the combination of commit time and revision) to track changes.
    If at any point that information is unavailable, it will fall back
    to build creation time as before.
    
    Also, fix api/gitiles times to be typed (time.Time vs. string).
    
    Bug:
    Change-Id: I72666f8e3cb718cff303f99d5a8d9f67f5660dc9

--- common/api/gitiles/proto.go
+++ common/api/gitiles/proto.go
@@ -30,15 +30,8 @@ func (u *User) Proto() (ret *git.Commit_User, err error) {
 		Name:  u.Name,
 		Email: u.Email,
 	}
-	if u.Time != "" {
-		var ts time.Time
-		if ts, err = u.GetTime(); err != nil {
-			err = errors.Annotate(err, "decoding time").Err()
-			return
-		}
-		ret.Time, err = ptypes.TimestampProto(ts)
-		err = errors.Annotate(err, "encoding time").Err()
-	}
+	ret.Time, err = ptypes.TimestampProto(time.Time(u.Time))
+	err = errors.Annotate(err, "encoding time").Err()
 	return
 }
 
