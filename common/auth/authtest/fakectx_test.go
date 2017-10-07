b8d38dffdf0ba1f32ae3e0bf3681482109f57dfb
Vadim Shtayura <vadimsh@chromium.org>
2017-10-06 17:59:08 -0700

    auth: Add 'authtest' testing helpers package.
    Will be used to setup fake authentication context for various integration tests,
    in particular ones in Kitchen (that use git).
    
    R=iannucci@chromium.org
    BUG=756224
    
    Change-Id: Ic96d3786feac92f861e07a5256b1ece898824621

---
+++ common/auth/authtest/fakectx_test.go
@@ -0,0 +1,56 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package authtest
+
+import (
+	"testing"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/common/auth"
+	"go.chromium.org/luci/common/clock/testclock"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFakeContext(t *testing.T) {
+	t.Parallel()
+
+	Convey("Works", t, func() {
+		ctx, _ := testclock.UseTime(context.Background(), testclock.TestRecentTimeUTC)
+
+		fake := FakeContext{}
+		ctx, err := fake.Start(ctx)
+		So(err, ShouldBeNil)
+		defer fake.Stop(ctx)
+
+		auth := auth.NewAuthenticator(ctx, auth.SilentLogin, auth.Options{})
+
+		email, err := auth.GetEmail()
+		So(err, ShouldBeNil)
+		So(email, ShouldEqual, DefaultFakeEmail)
+
+		tok, err := auth.GetAccessToken(time.Minute)
+		So(err, ShouldBeNil)
+		So(tok.AccessToken, ShouldEqual, DefaultFakeToken)
+
+		// Expiry is rounded to integer number of seconds, since that's the
+		// granularity of OAuth token expiration. Compare int unix timestamps to
+		// account for that.
+		So(tok.Expiry.Unix(), ShouldEqual,
+			testclock.TestRecentTimeUTC.Add(DefaultFakeLifetime).Unix())
+	})
+}
