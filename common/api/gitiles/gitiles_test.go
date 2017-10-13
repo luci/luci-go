21025efc1f6d83d2ae6d447c11459b8b6a3370b7
Michael Anthony Knyszek <mknyszek@google.com>
2017-10-13 15:50:52 -0700

    [gitiles] Expose mock environment for testing.
    Bug:
    Change-Id: Id55d7287f1aa151d161610ab1c7511e873eb2235

--- common/api/gitiles/gitiles_test.go
+++ common/api/gitiles/gitiles_test.go
@@ -18,7 +18,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/http/httptest"
 	"strings"
 	"testing"
 	"time"
@@ -26,6 +25,41 @@ import (
 	"golang.org/x/net/context"
 
 	. "github.com/smartystreets/goconvey/convey"
+)
+
+var (
+	fakeCommit1Str = `{
+		"commit": "0b2c5409e58a71c691b05454b55cc5580cc822d1",
+		"tree": "3c6f95bc757698cd6aca3c49f88f640fd145ea69",
+		"parents": [ "dc1dbf1aa56e4dd4cbfaab61c4d30a35adce5f40" ],
+		"author": {
+			"name": "Author 1",
+			"email": "author1@example.com",
+			"time": "Mon Jul 17 15:02:43 2017 -0800"
+		},
+		"committer": {
+			"name": "Commit Bot",
+			"email": "commit-bot@chromium.org",
+			"time": "Mon Jul 17 15:02:43 2017 +0000"
+		},
+		"message": "Import wpt@d96d68ed964f9bfc2bb248c2d2fab7a8870dc685\\n\\nCr-Commit-Position: refs/heads/master@{#487078}"
+	}`
+	fakeCommit2Str = `{
+		"commit": "dc1dbf1aa56e4dd4cbfaab61c4d30a35adce5f40",
+		"tree": "1ba2335c07915c31597b97a8d824aecc85a996f6",
+		"parents": ["8de6836858c99e48f3c58164ab717bda728e95dd"],
+		"author": {
+			"name": "Author 2",
+			"email": "author-2@example.com",
+			"time": "Mon Jul 17 15:01:13 2017"
+		},
+		"committer": {
+			"name": "Commit Bot",
+			"email": "commit-bot@chromium.org",
+			"time": "Mon Jul 17 15:01:13 2017"
+		},
+		"message": "[Web Payments] User weak ptr in Payment Request\u0027s error callback\\n\\nBug: 742329\\nReviewed-on: https://chromium-review.googlesource.com/570982\\nCr-Commit-Position: refs/heads/master@{#487077}"
+  }`
 )
 
 func TestTime(t *testing.T) {
@@ -86,16 +120,14 @@ func TestLog(t *testing.T) {
 	ctx := context.Background()
 
 	Convey("Log with bad URL", t, func() {
-		srv, c := newMockClient(func(w http.ResponseWriter, r *http.Request) {})
+		srv, c := NewBadMockEnvironment()
 		defer srv.Close()
 		_, err := c.Log(ctx, "bad://repo.git/", "master", Limit(10))
 		So(err, ShouldNotBeNil)
 	})
 
 	Convey("Log w/o pages", t, func() {
-		srv, c := newMockClient(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(200)
-			w.Header().Set("Content-Type", "application/json")
+		srv, c := NewMockEnvironment(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, ")]}'\n{\"log\": [%s, %s]}\n", fakeCommit1Str, fakeCommit2Str)
 		})
 		defer srv.Close()
@@ -120,9 +152,7 @@ func TestLog(t *testing.T) {
 
 	Convey("Log Paging", t, func() {
 		cursorSent := ""
-		srv, c := newMockClient(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(200)
-			w.Header().Set("Content-Type", "application/json")
+		srv, c := NewMockEnvironment(func(w http.ResponseWriter, r *http.Request) {
 			if next := r.URL.Query().Get("s"); next == "" {
 				fmt.Fprintf(w, ")]}'\n{\"log\": [%s], \"next\": \"next_cursor_value\"}\n", fakeCommit1Str)
 			} else {
@@ -158,16 +188,14 @@ func TestLogForward(t *testing.T) {
 	ctx := context.Background()
 
 	Convey("LogForward with bad URL", t, func() {
-		srv, c := newMockClient(func(w http.ResponseWriter, r *http.Request) {})
+		srv, c := NewMockEnvironment(func(w http.ResponseWriter, r *http.Request) {})
 		defer srv.Close()
 		_, err := c.LogForward(ctx, "bad://repo.git/", "8de6836858c99e48f3c58164ab717bda728e95dd", "master")
 		So(err, ShouldNotBeNil)
 	})
 
 	Convey("LogForward w/o pages", t, func() {
-		srv, c := newMockClient(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(200)
-			w.Header().Set("Content-Type", "application/json")
+		srv, c := NewMockEnvironment(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, ")]}'\n{\"log\": [%s, %s]}\n", fakeCommit1Str, fakeCommit2Str)
 		})
 		defer srv.Close()
@@ -183,9 +211,7 @@ func TestLogForward(t *testing.T) {
 	})
 
 	Convey("LogForward Paging", t, func() {
-		srv, c := newMockClient(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(200)
-			w.Header().Set("Content-Type", "application/json")
+		srv, c := NewMockEnvironment(func(w http.ResponseWriter, r *http.Request) {
 			next := r.URL.Query().Get("s")
 			logEntries := make([]string, 100)
 			for i := 0; i < 100; i++ {
@@ -227,9 +253,7 @@ func TestRefs(t *testing.T) {
 	})
 
 	Convey("Refs All", t, func() {
-		srv, c := newMockClient(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(200)
-			w.Header().Set("Content-Type", "application/json")
+		srv, c := NewMockEnvironment(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, `)]}'
 				{
 					"refs/heads/master": { "value": "deadbeef" },
@@ -255,9 +279,7 @@ func TestRefs(t *testing.T) {
 		})
 	})
 	Convey("Refs heads", t, func() {
-		srv, c := newMockClient(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(200)
-			w.Header().Set("Content-Type", "application/json")
+		srv, c := NewMockEnvironment(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, `)]}'
 				{
 					"master": { "value": "deadbeef" },
@@ -273,48 +295,4 @@ func TestRefs(t *testing.T) {
 			"refs/heads/infra/config": "0000beef",
 		})
 	})
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-var (
-	fakeCommit1Str = `{
-		"commit": "0b2c5409e58a71c691b05454b55cc5580cc822d1",
-		"tree": "3c6f95bc757698cd6aca3c49f88f640fd145ea69",
-		"parents": [ "dc1dbf1aa56e4dd4cbfaab61c4d30a35adce5f40" ],
-		"author": {
-			"name": "Author 1",
-			"email": "author1@example.com",
-			"time": "Mon Jul 17 15:02:43 2017 -0800"
-		},
-		"committer": {
-			"name": "Commit Bot",
-			"email": "commit-bot@chromium.org",
-			"time": "Mon Jul 17 15:02:43 2017 +0000"
-		},
-		"message": "Import wpt@d96d68ed964f9bfc2bb248c2d2fab7a8870dc685\\n\\nCr-Commit-Position: refs/heads/master@{#487078}"
-	}`
-	fakeCommit2Str = `{
-		"commit": "dc1dbf1aa56e4dd4cbfaab61c4d30a35adce5f40",
-		"tree": "1ba2335c07915c31597b97a8d824aecc85a996f6",
-		"parents": ["8de6836858c99e48f3c58164ab717bda728e95dd"],
-		"author": {
-			"name": "Author 2",
-			"email": "author-2@example.com",
-			"time": "Mon Jul 17 15:01:13 2017"
-		},
-		"committer": {
-			"name": "Commit Bot",
-			"email": "commit-bot@chromium.org",
-			"time": "Mon Jul 17 15:01:13 2017"
-		},
-		"message": "[Web Payments] User weak ptr in Payment Request\u0027s error callback\\n\\nBug: 742329\\nReviewed-on: https://chromium-review.googlesource.com/570982\\nCr-Commit-Position: refs/heads/master@{#487077}"
-  }`
-)
-
-////////////////////////////////////////////////////////////////////////////////
-
-func newMockClient(handler func(w http.ResponseWriter, r *http.Request)) (*httptest.Server, *Client) {
-	srv := httptest.NewServer(http.HandlerFunc(handler))
-	return srv, &Client{Client: http.DefaultClient, mockRepoURL: srv.URL}
 }
