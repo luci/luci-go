74366d3656b1a44c3a2daad3fe6bb8356fadc2d2
Robert Iannucci <iannucci@chromium.org>
2017-10-20 14:27:03 -0700

    [milo] remove swarming_run_id, implicit log_location.
    swarming_run_id is not used for any current builds, and all builds which
    use logdog specify the log_location swarming tag.
    
    R=hinoka@chromium.org, nodir@chromium.org
    
    Bug: 758007, 718191
    Change-Id: Id43924603ebe3465bf0baccfd39ef91adfea0fce
    Reviewed-on: https://chromium-review.googlesource.com/727545
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- milo/buildsource/swarming/buildinfo.go
+++ milo/buildsource/swarming/buildinfo.go
@@ -15,11 +15,6 @@
 package swarming
 
 import (
-	"strconv"
-	"strings"
-	"unicode/utf8"
-
-	swarming "go.chromium.org/luci/common/api/swarming/swarming/v1"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 	miloProto "go.chromium.org/luci/common/proto/milo"
@@ -27,7 +22,6 @@ import (
 	"go.chromium.org/luci/logdog/common/types"
 	"go.chromium.org/luci/luci_config/common/cfgtypes"
 	milo "go.chromium.org/luci/milo/api/proto"
-	"go.chromium.org/luci/swarming/tasktemplate"
 
 	"golang.org/x/net/context"
 )
@@ -68,11 +62,7 @@ func (p *BuildInfoProvider) GetBuildInfo(c context.Context, req *milo.BuildInfoR
 	host := sf.getHost()
 	logging.Infof(c, "Loading build info for Swarming host %s, task %s.", host, req.Task)
 
-	fetchParams := swarmingFetchParams{
-		fetchReq: true,
-		fetchRes: true,
-	}
-	fr, err := swarmingFetch(c, sf, req.Task, fetchParams)
+	fr, err := swarmingFetch(c, sf, req.Task, swarmingFetchParams{})
 	if err != nil {
 		if err == ErrNotMiloJob {
 			logging.Warningf(c, "User requested nonexistent task or does not have permissions.")
@@ -86,7 +76,7 @@ func (p *BuildInfoProvider) GetBuildInfo(c context.Context, req *milo.BuildInfoR
 	// Determine the LogDog annotation stream path for this Swarming task.
 	//
 	// On failure, will return a gRPC error.
-	stream, err := resolveLogDogAnnotations(c, fr.req, projectHint, host, req.Task, fr.res.TryNumber)
+	stream, err := resolveLogDogAnnotations(c, fr.res.Tags)
 	if err != nil {
 		logging.WithError(err).Warningf(c, "Failed to get annotation stream parameters.")
 		return nil, err
@@ -132,92 +122,16 @@ func (p *BuildInfoProvider) GetBuildInfo(c context.Context, req *milo.BuildInfoR
 // parameters.
 //
 // This will return a gRPC error on failure.
-//
-// This function is messy and implementation-specific. That's the point of this
-// endpoint, though. All of the nastiness here should be replaced with something
-// more elegant once that becomes available. In the meantime...
-func resolveLogDogAnnotations(c context.Context, sr *swarming.SwarmingRpcsTaskRequest, projectHint cfgtypes.ProjectName,
-	host, taskID string, tryNumber int64) (*types.StreamAddr, error) {
-
-	// Try and resolve from explicit tags (preferred).
-	tags := swarmingTags(sr.Tags)
-	addr, err := resolveLogDogStreamAddrFromTags(tags, taskID, tryNumber)
-	if err == nil {
-		return addr, nil
-	}
-	logging.WithError(err).Debugf(c, "Could not resolve stream address from tags.")
-
-	// If this is a Kitchen command, maybe we can infer our LogDog project from
-	// the command-line.
-	if sr.Properties == nil {
-		logging.Warningf(c, "No request properties, can't infer annotation stream path.")
-		return nil, grpcutil.NotFound
-	}
-
-	addr = &types.StreamAddr{}
-	var isKitchen bool
-	if addr.Project, isKitchen = getLogDogProjectFromKitchen(sr.Properties.Command); !isKitchen {
-		logging.Warningf(c, "Not a Kitchen CLI, can't infer annotation stream path.")
-		return nil, grpcutil.NotFound
-	}
-
-	if addr.Project == "" {
-		addr.Project = projectHint
-	}
-	if addr.Project == "" {
-		logging.Warningf(c, "Don't know how to get annotation stream path.")
-		return nil, grpcutil.NotFound
-	}
-
-	// This is a Kitchen run, and it has a project! Construct the annotation
-	// stream path.
-	//
-	// This is an implementation of:
-	// https://chromium.googlesource.com/infra/infra/+/a7032e3e240d4b81a1912bfaf29a20d02f665cc1/go/src/infra/tools/kitchen/cook_logdog.go#129
-	runID, err := getRunID(taskID, tryNumber)
+func resolveLogDogAnnotations(c context.Context, tagsRaw []string) (*types.StreamAddr, error) {
+	addr, err := resolveLogDogStreamAddrFromTags(swarmingTags(tagsRaw))
 	if err != nil {
-		logging.Fields{
-			logging.ErrorKey: err,
-			"taskID":         taskID,
-			"tryNumber":      tryNumber,
-		}.Errorf(c, "Failed to get Run ID for task/try.")
-		return nil, grpcutil.Internal
+		logging.WithError(err).Debugf(c, "Could not resolve stream address from tags.")
+		return nil, grpcutil.NotFound
 	}
-
-	prefix, err := types.MakeStreamName("", "swarm", host, runID)
-	if err != nil {
-		logging.WithError(err).Errorf(c, "Failed to generate Swarming prefix.")
-		return nil, grpcutil.Internal
-	}
-	addr.Path = prefix.Join("annotations")
 	return addr, nil
 }
 
-func getLogDogProjectFromKitchen(cmd []string) (proj cfgtypes.ProjectName, isKitchen bool) {
-	// Is this a Kitchen command?
-	switch {
-	case len(cmd) == 0:
-		return
-	case !strings.HasPrefix(cmd[0], "kitchen"):
-		return
-	}
-	isKitchen = true
-	cmd = cmd[1:]
-
-	// Scan through for the "-logdog-project" argument.
-	for i, arg := range cmd {
-		if arg == "-logdog-project" {
-			if i < len(cmd)-2 {
-				proj = cfgtypes.ProjectName(cmd[i+1])
-				return
-			}
-			break
-		}
-	}
-	return
-}
-
-func resolveLogDogStreamAddrFromTags(tags map[string]string, taskID string, tryNumber int64) (*types.StreamAddr, error) {
+func resolveLogDogStreamAddrFromTags(tags map[string]string) (*types.StreamAddr, error) {
 	// If we don't have a LUCI project, abort.
 	luciProject, logLocation := tags["luci_project"], tags["log_location"]
 	switch {
@@ -225,18 +139,6 @@ func resolveLogDogStreamAddrFromTags(tags map[string]string, taskID string, tryN
 		return nil, errors.New("no 'luci_project' tag")
 	case logLocation == "":
 		return nil, errors.New("no 'log_location' tag")
-	}
-
-	// Gather our Swarming task template parameters and perform a substitution.
-	runID, err := getRunID(taskID, tryNumber)
-	if err != nil {
-		return nil, errors.Annotate(err, "").Err()
-	}
-	p := tasktemplate.Params{
-		SwarmingRunID: runID,
-	}
-	if logLocation, err = p.Resolve(logLocation); err != nil {
-		return nil, errors.Annotate(err, "failed to resolve swarming task templating in 'log_location'").Err()
 	}
 
 	addr, err := types.ParseURL(logLocation)
@@ -250,30 +152,4 @@ func resolveLogDogStreamAddrFromTags(tags map[string]string, taskID string, tryN
 	}
 
 	return addr, nil
-}
-
-// getRunID converts a Swarming task ID and try number into a Swarming Run ID.
-//
-// The run ID is a permutation of the last four bits of the Swarming Task ID.
-// Therefore, we chop it off of the string, mutate it, and then add it back.
-//
-// TODO(dnj): Replace this with Swarming API call once finished.
-func getRunID(taskID string, tryNumber int64) (string, error) {
-	// Slice off the last character form the task ID.
-	if len(taskID) == 0 {
-		return "", errors.New("swarming task ID is empty")
-	}
-
-	// Parse "tryNumber" as a hex string.
-	if tryNumber < 0 || tryNumber > 0x0F {
-		return "", errors.Reason("try number %d exceeds 4 bits", tryNumber).Err()
-	}
-
-	lastChar, lastCharSize := utf8.DecodeLastRuneInString(taskID)
-	v, err := strconv.ParseUint(string(lastChar), 16, 8)
-	if err != nil {
-		return "", errors.Annotate(err, "failed to parse hex from rune: %r", lastChar).Err()
-	}
-
-	return taskID[:len(taskID)-lastCharSize] + strconv.FormatUint((v|uint64(tryNumber)), 16), nil
 }
--- milo/buildsource/swarming/buildinfo_test.go
+++ milo/buildsource/swarming/buildinfo_test.go
@@ -15,13 +15,9 @@
 package swarming
 
 import (
-	"fmt"
 	"testing"
 
 	swarming "go.chromium.org/luci/common/api/swarming/swarming/v1"
-	miloProto "go.chromium.org/luci/common/proto/milo"
-	"go.chromium.org/luci/grpc/grpcutil"
-	"go.chromium.org/luci/logdog/api/endpoints/coordinator/logs/v1"
 	"go.chromium.org/luci/logdog/client/coordinator"
 	milo "go.chromium.org/luci/milo/api/proto"
 
@@ -106,16 +102,6 @@ func TestBuildInfo(t *testing.T) {
 			},
 		}
 
-		logdogStep := miloProto.Step{
-			Command: &miloProto.Step_Command{
-				CommandLine: []string{"foo", "bar", "baz"},
-			},
-			Text: []string{"test step"},
-			Property: []*miloProto.Step_Property{
-				{Name: "bar", Value: "log-bar"},
-			},
-		}
-
 		biReq := milo.BuildInfoRequest{
 			Build: &milo.BuildInfoRequest_Swarming_{
 				Swarming: &milo.BuildInfoRequest_Swarming{
@@ -131,41 +117,6 @@ func TestBuildInfo(t *testing.T) {
 			So(err, ShouldBeRPCNotFound)
 		})
 
-		Convey("Can load a build, inferring project from Kitchen CLI.", func() {
-			testClient.resp = datagramGetResponse("testproject", "swarm/swarming.example.com/12341", &logdogStep)
-
-			resp, err := bip.GetBuildInfo(c, biReq.GetSwarming(), "")
-			So(err, ShouldBeNil)
-			So(testClient.req, ShouldResemble, &logdog.TailRequest{
-				Project: "testproject",
-				Path:    "swarm/swarming.example.com/12341/+/annotations",
-				State:   true,
-			})
-			So(resp, ShouldResemble, &milo.BuildInfoResponse{
-				Project: "testproject",
-				Step: &miloProto.Step{
-					Command: &miloProto.Step_Command{
-						CommandLine: []string{"foo", "bar", "baz"},
-					},
-					Text: []string{"test step"},
-					Link: &miloProto.Link{
-						Label: "Task 12340",
-						Value: &miloProto.Link_Url{
-							Url: "https://swarming.example.com/task?id=12340&show_raw=1&wide_logs=true",
-						},
-					},
-					Property: []*miloProto.Step_Property{
-						{Name: "bar", Value: "log-bar"},
-					},
-				},
-				AnnotationStream: &miloProto.LogdogStream{
-					Server: "example.com",
-					Prefix: "swarm/swarming.example.com/12341",
-					Name:   "annotations",
-				},
-			})
-		})
-
 		Convey("Will fail to load Kitchen without LogDog and no project hint.", func() {
 			testSvc.req.Properties.Command = []string{"kitchen"}
 
@@ -173,87 +124,5 @@ func TestBuildInfo(t *testing.T) {
 			So(err, ShouldBeRPCNotFound)
 		})
 
-		Convey("Will load Kitchen without LogDog if there is a project hint.", func() {
-			biReq.ProjectHint = "testproject"
-			testSvc.req.Properties.Command = []string{"kitchen"}
-			testClient.resp = datagramGetResponse("testproject", "swarm/swarming.example.com/12341", &logdogStep)
-
-			resp, err := bip.GetBuildInfo(c, biReq.GetSwarming(), "testproject")
-			So(err, ShouldBeNil)
-			So(testClient.req, ShouldResemble, &logdog.TailRequest{
-				Project: "testproject",
-				Path:    "swarm/swarming.example.com/12341/+/annotations",
-				State:   true,
-			})
-			So(resp, ShouldResemble, &milo.BuildInfoResponse{
-				Project: "testproject",
-				Step: &miloProto.Step{
-					Command: &miloProto.Step_Command{
-						CommandLine: []string{"foo", "bar", "baz"},
-					},
-					Text: []string{"test step"},
-					Link: &miloProto.Link{
-						Label: "Task 12340",
-						Value: &miloProto.Link_Url{
-							Url: "https://swarming.example.com/task?id=12340&show_raw=1&wide_logs=true",
-						},
-					},
-					Property: []*miloProto.Step_Property{
-						{Name: "bar", Value: "log-bar"},
-					},
-				},
-				AnnotationStream: &miloProto.LogdogStream{
-					Server: "example.com",
-					Prefix: "swarm/swarming.example.com/12341",
-					Name:   "annotations",
-				},
-			})
-
-			Convey("Will return NotFound if the build is internal", func() {
-				testSvc.res.Tags = testSvc.res.Tags[1:]
-				testSvc.req.Tags = testSvc.req.Tags[1:]
-				_, err := bip.GetBuildInfo(c, biReq.GetSwarming(), "testproject")
-				So(err, ShouldResemble, grpcutil.NotFound)
-			})
-		})
-	})
-}
-
-func TestGetRunID(t *testing.T) {
-	t.Parallel()
-
-	successes := []struct {
-		taskID    string
-		tryNumber int64
-		runID     string
-	}{
-		{"3442825749e6e110", 1, "3442825749e6e111"},
-	}
-
-	failures := []struct {
-		taskID    string
-		tryNumber int64
-		err       string
-	}{
-		{"", 1, "swarming task ID is empty"},
-		{"3442825749e6e110", 16, "exceeds 4 bits"},
-		{"3442825749e6e11M", 1, "failed to parse hex from rune"},
-	}
-
-	Convey("Testing BuildInfo's getRunID", t, func() {
-		for _, tc := range successes {
-			Convey(fmt.Sprintf("Successfully processes %q / %q => %q", tc.taskID, tc.tryNumber, tc.runID), func() {
-				v, err := getRunID(tc.taskID, tc.tryNumber)
-				So(err, ShouldBeNil)
-				So(v, ShouldEqual, tc.runID)
-			})
-		}
-
-		for _, tc := range failures {
-			Convey(fmt.Sprintf("Failes to parse %q / %q (%s)", tc.taskID, tc.tryNumber, tc.err), func() {
-				_, err := getRunID(tc.taskID, tc.tryNumber)
-				So(err, ShouldErrLike, tc.err)
-			})
-		}
 	})
 }
