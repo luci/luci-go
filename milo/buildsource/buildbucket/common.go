db144b1cda5d39a4f21d3a6dbaac8a2e6fdda64d
Nodir Turakulov <nodir@google.com>
2017-10-23 13:13:39 -0700

    [milo] use buildbucket package in builder.go
    in buildbucket/builder.go, use buildbucket package and simplify overall
    / delete useless code.
    
    Also:
    - delete buildbot support in buildbucket package.
      Nobody uses buildbucket views for buildbot builds.
    - delete Rietveld support
    - improve/simplify tests
    - add copyright headers in some files
    - use bbapi package alias for buildbucket api package
    
    Bug:
    Change-Id: Id81d5fcc14ec466587a52e89beea63a8c2ba6180

--- milo/buildsource/buildbucket/common.go
+++ milo/buildsource/buildbucket/common.go
@@ -17,40 +17,16 @@ package buildbucket
 import (
 	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
 	"golang.org/x/net/context"
 
-	"go.chromium.org/luci/common/api/buildbucket/buildbucket/v1"
+	"go.chromium.org/luci/buildbucket"
+	bbapi "go.chromium.org/luci/common/api/buildbucket/buildbucket/v1"
 	"go.chromium.org/luci/common/api/buildbucket/swarmbucket/v1"
-	"go.chromium.org/luci/milo/api/resp"
 	"go.chromium.org/luci/milo/common/model"
 	"go.chromium.org/luci/server/auth"
 )
-
-const (
-	// StatusScheduled means a build is pending.
-	StatusScheduled = "SCHEDULED"
-	// StatusStarted means a build is executing.
-	StatusStarted = "STARTED"
-	// StatusCompleted means a build is completed (successfully or not).
-	StatusCompleted = "COMPLETED"
-)
-
-// ParseTags parses buildbucket build tags to a map.
-// Ignores tags that doesn't have colon (we don't have them in practice because
-// buildbucket validates tags).
-func ParseTags(tags []string) map[string]string {
-	result := make(map[string]string, len(tags))
-	for _, t := range tags {
-		parts := strings.SplitN(t, ":", 2)
-		if len(parts) == 2 {
-			result[parts[0]] = parts[1]
-		}
-	}
-	return result
-}
 
 func newSwarmbucketClient(c context.Context, server string) (*swarmbucket.Service, error) {
 	c, _ = context.WithTimeout(c, time.Minute)
@@ -66,13 +42,13 @@ func newSwarmbucketClient(c context.Context, server string) (*swarmbucket.Servic
 	return client, nil
 }
 
-func newBuildbucketClient(c context.Context, server string) (*buildbucket.Service, error) {
+func newBuildbucketClient(c context.Context, server string) (*bbapi.Service, error) {
 	c, _ = context.WithTimeout(c, time.Minute)
 	t, err := auth.GetRPCTransport(c, auth.AsUser)
 	if err != nil {
 		return nil, err
 	}
-	client, err := buildbucket.New(&http.Client{Transport: t})
+	client, err := bbapi.New(&http.Client{Transport: t})
 	if err != nil {
 		return nil, err
 	}
@@ -80,82 +56,23 @@ func newBuildbucketClient(c context.Context, server string) (*buildbucket.Servic
 	return client, nil
 }
 
-// parseStatus converts a buildbucket build status to resp.Status.
-func parseStatus(build *buildbucket.ApiCommonBuildMessage) (model.Status, error) {
-	switch build.Status {
-	case StatusScheduled:
-		return model.NotRun, nil
-
-	case StatusStarted:
-		return model.Running, nil
-
-	case StatusCompleted:
-		switch build.Result {
-		case "SUCCESS":
-			return model.Success, nil
-
-		case "FAILURE":
-			switch build.FailureReason {
-			case "BUILD_FAILURE":
-				return model.Failure, nil
-			default:
-				return model.InfraFailure, nil
-			}
-
-		case "CANCELED":
-			return model.InfraFailure, nil
-
-		default:
-			return 0, fmt.Errorf("unexpected buildbucket build result %q", build.Result)
-		}
-
-	default:
-		return 0, fmt.Errorf("unexpected buildbucket build status %q", build.Status)
-	}
+// statusMap maps buildbucket status to milo status.
+// Buildbucket statuses not in the map must be treated
+// as InfraFailure.
+var statusMap = map[buildbucket.Status]model.Status{
+	buildbucket.StatusScheduled: model.NotRun,
+	buildbucket.StatusStarted:   model.Running,
+	buildbucket.StatusSuccess:   model.Success,
+	buildbucket.StatusFailure:   model.Failure,
+	buildbucket.StatusError:     model.InfraFailure,
+	buildbucket.StatusCancelled: model.Cancelled,
+	buildbucket.StatusTimeout:   model.Expired,
 }
 
-// getChangeList tries to extract CL information from a buildbucket build.
-func getChangeList(
-	build *buildbucket.ApiCommonBuildMessage, params *buildParameters,
-	resultDetails *resultDetails) (result *resp.Commit) {
-
-	prop := &params.Properties
-	switch prop.PatchStorage {
-	case "rietveld":
-		if prop.RietveldURL != "" && prop.Issue != 0 {
-			result = &resp.Commit{
-				RequestRevision: resp.NewLink(prop.Revision, ""),
-				Changelist: resp.NewLink(
-					fmt.Sprintf("Rietveld CL %d", prop.Issue),
-					fmt.Sprintf("%s/%d/#ps%d", prop.RietveldURL, prop.Issue, prop.PatchSet),
-				),
-			}
-			if resultDetails.Properties.GotRevision != "" {
-				// TODO(hinoka): Figure out the full URL for these revisions, add it
-				// to the URL field.
-				result.Revision = resp.NewLink(resultDetails.Properties.GotRevision, "")
-			}
-		}
-
-	case "gerrit":
-		if prop.GerritPatchURL != "" && prop.GerritPatchIssue != 0 {
-			path := fmt.Sprintf("%d", prop.GerritPatchIssue)
-			if prop.GerritPatchSet != 0 {
-				path = fmt.Sprintf("%d/%d", prop.GerritPatchIssue, prop.GerritPatchSet)
-			}
-			result = &resp.Commit{
-				Changelist: resp.NewLink(
-					fmt.Sprintf("Gerrit CL %d", prop.GerritPatchIssue),
-					fmt.Sprintf("%s/c/%s", prop.GerritPatchURL, path),
-				),
-			}
-		}
+// parseStatus converts a buildbucket status to model.Status.
+func parseStatus(status buildbucket.Status) model.Status {
+	if st, ok := statusMap[status]; ok {
+		return st
 	}
-
-	if result != nil && len(params.Changes) != 0 {
-		// tryjobs have one change and it is the CL author
-		result.AuthorEmail = params.Changes[0].Author.Email
-	}
-
-	return
+	return model.InfraFailure
 }
