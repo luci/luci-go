6367be3403f60ab7ba46abc5752e9110f80fd931
Dan Jacques <dnj@chromium.org>
2017-10-25 10:48:40 -0400

    [logdog] Have default service export all endpoints
    Have the "default" service export all LogDog endpoints. This is mostly
    inconsequential, since pRPC traffic is routed to specific services.
    However, this allows it to represent the full set of services in RPC
    explorer and pRPC discovery.
    
    Also install "grpcmon" into services endpoints.
    
    BUG=None
    TEST=dev
    
    Change-Id: Ief8266a7c8f9cfde421814b2e49c4bc4ae1c27ad

---
+++ logdog/appengine/cmd/coordinator/default/dummy_logs_service.go
@@ -0,0 +1,42 @@
+// Copyright 2015 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package module
+
+import (
+	"go.chromium.org/luci/grpc/grpcutil"
+	logsPb "go.chromium.org/luci/logdog/api/endpoints/coordinator/logs/v1"
+
+	"github.com/golang/protobuf/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+)
+
+// dummyLogsService is a logsPb.Service implementation that always returns
+// a "codes.FailedPrecondition" error.
+//
+// This is implemented this way so that the Logs service can be made
+// discoverable without actually hosting an implementation of that service.
+// The actual implementation is hosted in the "logs" GAE service, and RPCs
+// directed at this dummy service are automatically routed to the "logs" service
+// through "dispatch.yaml".
+var dummyLogsService = &logsPb.DecoratedLogs{
+	Service: nil, // No backing implementation.
+	Prelude: func(c context.Context, methodName string, req proto.Message) (context.Context, error) {
+		return nil, grpcutil.Errf(codes.FailedPrecondition,
+			"Logs traffic cannot be handled by the default service. It must be routed to the logs "+
+				"service. This error indicates that the routing is not working as intended. Please report "+
+				"this error to the maintainers.")
+	},
+}
