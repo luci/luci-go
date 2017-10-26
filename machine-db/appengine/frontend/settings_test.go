fdee5bbedff60e332fb325d9a49feb74f65137a5
smut <smut@google.com>
2017-10-26 15:01:47 -0700

    [Machine Database] Create database protos and config
    Bug: 778865
    Change-Id: Ifa6d18bcc649aecc5d3b28b413859998c943f6dc

---
+++ machine-db/appengine/frontend/settings_test.go
@@ -0,0 +1,64 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package frontend
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"go.chromium.org/luci/server/settings"
+
+	"golang.org/x/net/context"
+)
+
+func TestDatabaseSettings(t *testing.T) {
+	t.Parallel()
+
+	Convey("NewDatabaseSettings works", t, func() {
+		s := NewSettings(context.Background())
+		So(s, ShouldNotBeNil)
+		So(s.Server, ShouldEqual, "")
+		So(s.Username, ShouldEqual, "")
+		So(s.Password, ShouldEqual, "")
+	})
+
+	Convey("GetUncachedSettings returns defaults", t, func() {
+		s, e := GetUncachedSettings(context.Background())
+		So(s, ShouldNotBeNil)
+		So(s.Server, ShouldEqual, "")
+		So(s.Username, ShouldEqual, "")
+		So(s.Password, ShouldEqual, "")
+		So(e, ShouldBeNil)
+	})
+
+	Convey("GetUncachedSettings returns settings", t, func() {
+		c := settings.Use(context.Background(), settings.New(&settings.MemoryStorage{}))
+		expected := &DatabaseSettings{
+			Server:   "server",
+			Username: "username",
+			Password: "password",
+		}
+		e := settings.Set(c, settingsKey, expected, "whoever", "for whatever reason")
+		So(e, ShouldBeNil)
+
+		actual, e := GetUncachedSettings(c)
+		So(actual, ShouldNotBeNil)
+		So(actual.Server, ShouldEqual, expected.Server)
+		So(actual.Username, ShouldEqual, expected.Username)
+		So(actual.Password, ShouldEqual, expected.Password)
+		So(e, ShouldBeNil)
+	})
+}
