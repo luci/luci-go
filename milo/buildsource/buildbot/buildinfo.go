f6259ae167ae426d5bf895758ee7323712055def
Robert Iannucci <iannucci@chromium.org>
2017-11-01 16:35:46 -0700

    [milo] Remove deprecated sources of logdog annotation stream.
    R=hinoka@chromium.org, jchinlee@chromium.org
    
    Bug:
    Change-Id: Ifd1b5fbeb50696a4ad41339f2b65c7c99f98f9a0

--- milo/buildsource/buildbot/buildinfo.go
+++ milo/buildsource/buildbot/buildinfo.go
@@ -16,9 +16,6 @@ package buildbot
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
@@ -27,7 +24,6 @@ import (
 	"go.chromium.org/luci/common/logging"
 	miloProto "go.chromium.org/luci/common/proto/milo"
 	"go.chromium.org/luci/grpc/grpcutil"
-	"go.chromium.org/luci/logdog/client/coordinator"
 	"go.chromium.org/luci/logdog/common/types"
 	"go.chromium.org/luci/luci_config/common/cfgtypes"
 	"go.chromium.org/luci/milo/api/buildbot"
@@ -68,37 +64,19 @@ func GetBuildInfo(c context.Context, req *milo.BuildInfoRequest_BuildBot,
 			req.BuildNumber, req.MasterName, req.BuilderName)
 	}
 
-	// Create a new LogDog client.
-	client, err := rawpresentation.NewClient(c, "")
-	if err != nil {
-		logging.WithError(err).Errorf(c, "Failed to create LogDog client.")
-		return nil, grpcutil.Internal
-	}
-
 	// Identify the LogDog annotation stream from the build.
 	//
 	// This will return a gRPC error on failure.
-	addr, err := getLogDogAnnotationAddr(c, client, build, projectHint)
+	addr, err := getLogDogAnnotationAddr(c, build)
 	if err != nil {
 		return nil, err
 	}
 	logging.Infof(c, "Resolved annotation stream: %s / %s", addr.Project, addr.Path)
 
-	// Load the annotation protobuf.
-	as := rawpresentation.AnnotationStream{
-		Client:  client,
-		Path:    addr.Path,
-		Project: addr.Project,
-	}
-	if err := as.Normalize(); err != nil {
-		logging.WithError(err).Errorf(c, "Failed to normalize annotation stream.")
-		return nil, grpcutil.Internal
-	}
-
-	step, err := as.Fetch(c)
+	step, err := rawpresentation.ReadAnnotations(c, addr)
 	if err != nil {
 		logging.WithError(err).Errorf(c, "Failed to load annotation stream.")
-		return nil, grpcutil.Errf(codes.Internal, "failed to load LogDog annotation stream from: %s", as.Path)
+		return nil, grpcutil.Errf(codes.Internal, "failed to load LogDog annotation stream from: %s", addr.Path)
 	}
 
 	// Merge the information together.
@@ -107,135 +85,24 @@ func GetBuildInfo(c context.Context, req *milo.BuildInfoRequest_BuildBot,
 		return nil, grpcutil.Errf(codes.Internal, "failed to merge annotation and build data")
 	}
 
-	prefix, name := as.Path.Split()
+	prefix, name := addr.Path.Split()
 	return &milo.BuildInfoResponse{
-		Project: string(as.Project),
+		Project: string(addr.Project),
 		Step:    step,
 		AnnotationStream: &miloProto.LogdogStream{
-			Server: client.Host,
+			Server: addr.Host,
 			Prefix: string(prefix),
 			Name:   string(name),
 		},
 	}, nil
 }
 
-// Resolve BuildBot and LogDog build information. We do this
-//
-// This returns an AnnotationStream instance with its project and path
-// populated.
-//
-// This function is messy and implementation-specific. That's the point of this
-// endpoint, though. All of the nastiness here should be replaced with something
-// more elegant once that becomes available. In the meantime...
-func getLogDogAnnotationAddr(c context.Context, client *coordinator.Client, build *buildbot.Build,
-	projectHint cfgtypes.ProjectName) (*types.StreamAddr, error) {
-
+// Resolve LogDog annotation stream for this build.
+func getLogDogAnnotationAddr(c context.Context, build *buildbot.Build) (*types.StreamAddr, error) {
 	if v, ok := build.PropertyValue("log_location").(string); ok && v != "" {
-		addr, err := types.ParseURL(v)
-		if err == nil {
-			return addr, nil
-		}
-
-		logging.Fields{
-			logging.ErrorKey: err,
-			"log_location":   v,
-		}.Debugf(c, "'log_location' property did not parse as LogDog URL.")
+		return types.ParseURL(v)
 	}
-
-	// logdog_annotation_url (if present, must be valid)
-	if v, ok := build.PropertyValue("logdog_annotation_url").(string); ok && v != "" {
-		addr, err := types.ParseURL(v)
-		if err != nil {
-			logging.Fields{
-				logging.ErrorKey: err,
-				"url":            v,
-			}.Errorf(c, "Failed to parse 'logdog_annotation_url' property.")
-			return nil, grpcutil.Errf(codes.FailedPrecondition, "build has invalid annotation URL")
-		}
-
-		return addr, nil
-	}
-
-	// Modern builds will have this information in their build properties.
-	var addr types.StreamAddr
-	prefix, _ := build.PropertyValue("logdog_prefix").(string)
-	project, _ := build.PropertyValue("logdog_project").(string)
-	if prefix != "" && project != "" {
-		// Construct the full annotation path.
-		addr.Project = cfgtypes.ProjectName(project)
-		addr.Path = types.StreamName(prefix).Join("annotations")
-
-		logging.Debugf(c, "Resolved path/project from build properties.")
-		return &addr, nil
-	}
-
-	// From here on out, we will need a project hint.
-	if projectHint == "" {
-		return nil, grpcutil.Errf(codes.NotFound, "annotation stream not found")
-	}
-	addr.Project = projectHint
-
-	// Execute a LogDog service query to see if we can identify the stream.
-	err := func() error {
-		var annotationStream *coordinator.LogStream
-		err := client.Query(c, addr.Project, "", coordinator.QueryOptions{
-			Tags: map[string]string{
-				"buildbot.master":      build.Master,
-				"buildbot.builder":     build.Buildername,
-				"buildbot.buildnumber": strconv.Itoa(build.Number),
-			},
-			ContentType: miloProto.ContentTypeAnnotations,
-		}, func(ls *coordinator.LogStream) bool {
-			// Only need the first (hopefully only?) result.
-			annotationStream = ls
-			return false
-		})
-		if err != nil {
-			logging.WithError(err).Errorf(c, "Failed to issue log stream query.")
-			return grpcutil.Internal
-		}
-
-		if annotationStream != nil {
-			addr.Path = annotationStream.Path
-		}
-		return nil
-	}()
-	if err != nil {
-		return nil, err
-	}
-	if addr.Path != "" {
-		logging.Debugf(c, "Resolved path/project via tag query.")
-		return &addr, nil
-	}
-
-	// Last-ditch effort: generate a prefix based on the build properties. This
-	// re-implements the "_build_prefix" function in:
-	// https://chromium.googlesource.com/chromium/tools/build/+/2d23e5284cc31f31c6bc07aa1d3fc5b1c454c3b4/scripts/slave/logdog_bootstrap.py#363
-	isAlnum := func(r rune) bool {
-		return ((r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9'))
-	}
-	normalize := func(v string) string {
-		v = strings.Map(func(r rune) rune {
-			if isAlnum(r) {
-				return r
-			}
-			switch r {
-			case ':', '_', '-', '.':
-				return r
-			default:
-				return '_'
-			}
-		}, v)
-		if r, _ := utf8.DecodeRuneInString(v); r == utf8.RuneError || !isAlnum(r) {
-			v = "s_" + v
-		}
-		return v
-	}
-	addr.Path = types.StreamPath(fmt.Sprintf("bb/%s/%s/%s/+/annotations",
-		normalize(build.Master), normalize(build.Buildername), normalize(strconv.Itoa(build.Number))))
-
-	logging.Debugf(c, "Generated path/project algorithmically.")
-	return &addr, nil
+	return nil, grpcutil.Errf(codes.NotFound, "annotation stream not found")
 }
 
 // mergeBuildInfoIntoAnnotation merges BuildBot-specific build informtion into
