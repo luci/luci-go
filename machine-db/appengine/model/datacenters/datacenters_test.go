02b188767994808365fee70351cceea36ddbf776
smut <smut@google.com>
2017-11-02 15:13:51 -0700

    [Machine Database] Import datacenters config, handle datacenters RPC
    Bug: 778865
    Change-Id: I0ae02808850ccaf16b7f1bc561368d1e72e6666c

---
+++ machine-db/appengine/model/datacenters/datacenters_test.go
@@ -0,0 +1,121 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datacenters
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+
+	"go.chromium.org/luci/machine-db/api/config/v1"
+	"go.chromium.org/luci/machine-db/appengine/database"
+
+	"golang.org/x/net/context"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestValidateDatacenters(t *testing.T) {
+	columns := []string{"id", "name"}
+
+	Convey("EnsureDatacenters: empty", t, func() {
+		db, m, e := sqlmock.New()
+		defer db.Close()
+		So(db, ShouldNotBeNil)
+		So(m, ShouldNotBeNil)
+		So(e, ShouldBeNil)
+
+		datacenters := map[string]*config.DatacenterConfig{}
+
+		m.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows(columns))
+		m.ExpectPrepare("DELETE")
+		m.ExpectPrepare("INSERT")
+		m.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows(columns))
+
+		e = EnsureDatacenters(database.With(context.Background(), db), datacenters)
+		So(e, ShouldBeNil)
+	})
+
+	Convey("EnsureDatacenters: insert", t, func() {
+		db, m, e := sqlmock.New()
+		defer db.Close()
+		So(db, ShouldNotBeNil)
+		So(m, ShouldNotBeNil)
+		So(e, ShouldBeNil)
+
+		datacenters := map[string]*config.DatacenterConfig{
+			"datacenter1.cfg": {
+				Name:        "datacenter 1",
+				Description: "description",
+			},
+			"datacenter2.cfg": {
+				Name: "datacenter 2",
+			},
+		}
+
+		m.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows(columns))
+		m.ExpectPrepare("DELETE")
+		m.ExpectPrepare("INSERT")
+		m.ExpectPrepare("INSERT")
+		m.ExpectExec("INSERT").WithArgs(datacenters["datacenter1.cfg"].Name, datacenters["datacenter1.cfg"].Description).WillReturnResult(sqlmock.NewResult(1, 1))
+		m.ExpectExec("INSERT").WithArgs(datacenters["datacenter2.cfg"].Name, datacenters["datacenter2.cfg"].Description).WillReturnResult(sqlmock.NewResult(2, 1))
+
+		e = EnsureDatacenters(database.With(context.Background(), db), datacenters)
+		So(e, ShouldBeNil)
+	})
+
+	Convey("EnsureDatacenters: delete", t, func() {
+		db, m, e := sqlmock.New()
+		defer db.Close()
+		So(db, ShouldNotBeNil)
+		So(m, ShouldNotBeNil)
+		So(e, ShouldBeNil)
+
+		datacenters := map[string]*config.DatacenterConfig{}
+
+		m.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows(columns).AddRow(1, "datacenter"))
+		m.ExpectPrepare("DELETE")
+		m.ExpectPrepare("INSERT")
+		m.ExpectPrepare("DELETE")
+		m.ExpectExec("DELETE").WithArgs(1).WillReturnResult(sqlmock.NewResult(1, 1))
+
+		e = EnsureDatacenters(database.With(context.Background(), db), datacenters)
+		So(e, ShouldBeNil)
+	})
+
+	Convey("EnsureDatacenters: ok", t, func() {
+		db, m, e := sqlmock.New()
+		defer db.Close()
+		So(db, ShouldNotBeNil)
+		So(m, ShouldNotBeNil)
+		So(e, ShouldBeNil)
+
+		datacenters := map[string]*config.DatacenterConfig{
+			"datacenter.cfg": {
+				Name:        "datacenter",
+				Description: "description",
+			},
+		}
+
+		m.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows(columns).AddRow(1, datacenters["datacenter.cfg"].Name))
+		m.ExpectPrepare("DELETE")
+		m.ExpectPrepare("INSERT")
+		m.ExpectPrepare("INSERT")
+		m.ExpectExec("INSERT").WithArgs(datacenters["datacenter.cfg"].Name, datacenters["datacenter.cfg"].Description).WillReturnResult(sqlmock.NewResult(1, 1))
+
+		e = EnsureDatacenters(database.With(context.Background(), db), datacenters)
+		So(e, ShouldBeNil)
+	})
+}
