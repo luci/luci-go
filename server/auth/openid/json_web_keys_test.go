f734ccaed42a093769e76e2807abd7132b4145aa
Vadim Shtayura <vadimsh@chromium.org>
2017-11-06 21:49:26 -0800

    openid: Add a method to verify and parse ID token.
    Extract the user information from the ID token instead of hitting the user info
    endpoint.
    
    R=tandrii@chromium.org
    BUG=712506
    
    Change-Id: I669ee7eb9849496a41840a1dd4c1dd7af00e72b9

--- server/auth/openid/json_web_keys_test.go
+++ server/auth/openid/json_web_keys_test.go
@@ -32,91 +32,86 @@ func TestParseJSONWebKeySet(t *testing.T) {
 	t.Parallel()
 
 	Convey("Happy path", t, func() {
-		keys, err := ParseJSONWebKeySet([]byte(`{
-			"keys": [
+		keys, err := NewJSONWebKeySet(&JSONWebKeySetStruct{
+			Keys: []JSONWebKeyStruct{
 				{
-					"kty": "RSA",
-					"alg": "RS256",
-					"use": "sig",
-					"kid": "key-1",
-					"n": "sJ46htmsgvo99FyibDKBh6zHGr2hldxfHt-6y9VTrDo3jfuFeA8xFG3Lb9LkJE4qlCvC9QRv3S0TWhQcTg9uGN2mY7B7ccIRaD2AL2hh87mFAkQzoRPxg5lvS6QisgiFQcH-oea-4iBYlBHyMemoXR8pRSwTeQKxzvZ2zohl5c-yE77vhrmDV9_n3viJH1YBYJk2weAj9Pqcfj7_cLF6tR1vd4voTL29WrWJwkO2KG6FcXrc0DQQba8nXPc-TFJd1Z9aSyzxS91rxnj8wnMbPdXPfG2jryT1Dg_LhWtDjIG7m2ZVN-_wL1hHhxC6m7yl4jSIaAO5rjKMToq5wrlgVQ",
-					"e": "AQAB"
+					Kty: "RSA",
+					Alg: "RS256",
+					Use: "sig",
+					Kid: "key-1",
+					N:   "sJ46htmsgvo99FyibDKBh6zHGr2hldxfHt-6y9VTrDo3jfuFeA8xFG3Lb9LkJE4qlCvC9QRv3S0TWhQcTg9uGN2mY7B7ccIRaD2AL2hh87mFAkQzoRPxg5lvS6QisgiFQcH-oea-4iBYlBHyMemoXR8pRSwTeQKxzvZ2zohl5c-yE77vhrmDV9_n3viJH1YBYJk2weAj9Pqcfj7_cLF6tR1vd4voTL29WrWJwkO2KG6FcXrc0DQQba8nXPc-TFJd1Z9aSyzxS91rxnj8wnMbPdXPfG2jryT1Dg_LhWtDjIG7m2ZVN-_wL1hHhxC6m7yl4jSIaAO5rjKMToq5wrlgVQ",
+					E:   "AQAB",
 				},
 				{
-					"kty": "SOMETHING-ELSE"
-				}
-			]
-		}`))
+					Kty: "SOMETHING-ELSE",
+				},
+			},
+		})
 		So(err, ShouldBeNil)
 		So(len(keys.keys), ShouldEqual, 1)
 		So(keys.keys["key-1"].E, ShouldEqual, 65537)
 	})
 
-	Convey("Bad json", t, func() {
-		_, err := ParseJSONWebKeySet([]byte("not json"))
-		So(err, ShouldErrLike, "failed to parse JSON web key")
-	})
-
 	Convey("No key ID", t, func() {
-		_, err := ParseJSONWebKeySet([]byte(`{
-			"keys": [
+		_, err := NewJSONWebKeySet(&JSONWebKeySetStruct{
+			Keys: []JSONWebKeyStruct{
 				{
-					"kty": "RSA",
-					"alg": "RS256",
-					"use": "sig",
-					"n": "sJ46htmsgvo99FyibDKBh6zHGr2hldxfHt",
-					"e": "AQAB"
-				}
-			]
-		}`))
+					Kty: "RSA",
+					Alg: "RS256",
+					Use: "sig",
+					N:   "sJ46htmsgvo99FyibD",
+					E:   "AQAB",
+				},
+			},
+		})
 		So(err, ShouldErrLike, "missing 'kid' field")
 	})
 
 	Convey("Bad e", t, func() {
-		_, err := ParseJSONWebKeySet([]byte(`{
-			"keys": [
+		_, err := NewJSONWebKeySet(&JSONWebKeySetStruct{
+			Keys: []JSONWebKeyStruct{
 				{
-					"kty": "RSA",
-					"alg": "RS256",
-					"use": "sig",
-					"kid": "key-1",
-					"n": "sJ46htmsgvo99FyibDKBh6zHGr2hldxfHt",
-					"e": "???"
-				}
-			]
-		}`))
+					Kty: "RSA",
+					Alg: "RS256",
+					Use: "sig",
+					Kid: "key-1",
+					N:   "sJ46htmsgvo99FyibD",
+					E:   "????",
+				},
+			},
+		})
 		So(err, ShouldErrLike, "bad exponent encoding")
 	})
 
 	Convey("Bad n", t, func() {
-		_, err := ParseJSONWebKeySet([]byte(`{
-			"keys": [
+		_, err := NewJSONWebKeySet(&JSONWebKeySetStruct{
+			Keys: []JSONWebKeyStruct{
 				{
-					"kty": "RSA",
-					"alg": "RS256",
-					"use": "sig",
-					"kid": "key-1",
-					"n": "???",
-					"e": "AQAB"
-				}
-			]
-		}`))
+					Kty: "RSA",
+					Alg: "RS256",
+					Use: "sig",
+					Kid: "key-1",
+					N:   "????",
+					E:   "AQAB",
+				},
+			},
+		})
 		So(err, ShouldErrLike, "bad modulus encoding")
 	})
 
 	Convey("No signing keys", t, func() {
-		_, err := ParseJSONWebKeySet([]byte(`{
-			"keys": [
+		_, err := NewJSONWebKeySet(&JSONWebKeySetStruct{
+			Keys: []JSONWebKeyStruct{
 				{
-					"kty": "RSA",
-					"alg": "RS256",
-					"use": "encrypt",
-					"kid": "key-1",
-					"n": "sJ46htmsgvo99FyibDKBh6zHGr2hldxfHt",
-					"e": "AQAB"
-				}
-			]
-		}`))
+					Kty: "RSA",
+					Alg: "RS256",
+					Use: "encrypt",
+					Kid: "key-1",
+					N:   "sJ46htmsgvo99FyibD",
+					E:   "AQAB",
+				},
+			},
+		})
 		So(err, ShouldErrLike, "didn't have any signing keys")
 	})
 }
