d79a3a5f29b892608f1c07d07d5ce92f7c1c68b1
Robert Iannucci <iannucci@chromium.org>
2017-11-08 19:39:01 -0800

    [milo] Console caching cleanup.
      * Adds per-request caching for common.GetAllConsoles()
      * Removes ad-hoc `BuildSummary.consoles` cache thingy
      * Simplifies manifest key interface
      * Makes BuilderSummary extract console IDs from ManifestKeys
      * Parallelizes all tests
      * Fix test inter-dependency by moving context initialization inside
        Convey (previously tests were persisting state between tests).
    
    R=hinoka@chromium.org, jchinlee@chromium.org, nodir@chromium.org
    
    Bug: 782489, 782874
    Change-Id: I8c84482a7b0f6f8584c86349872cc26546432527

--- milo/buildsource/buildbot/builder_test.go
+++ milo/buildsource/buildbot/builder_test.go
@@ -23,83 +23,89 @@ import (
 	"go.chromium.org/luci/common/clock/testclock"
 	"go.chromium.org/luci/milo/api/buildbot"
 	"go.chromium.org/luci/milo/buildsource/buildbot/buildstore"
+	"go.chromium.org/luci/server/caching"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestBuilder(t *testing.T) {
-	c := memory.UseWithAppID(context.Background(), "dev~luci-milo")
-	c, _ = testclock.UseTime(c, testclock.TestTimeUTC)
-	fakeTime := unixTime(123)
+	t.Parallel()
 
-	Convey(`A test Environment`, t, func() {
-		// Seed a builder with 10 builds.
-		for i := 0; i < 10; i++ {
+	Convey(`TestBuilder`, t, func() {
+		c := memory.UseWithAppID(context.Background(), "dev~luci-milo")
+		c, _ = testclock.UseTime(c, testclock.TestTimeUTC)
+		c = caching.WithRequestCache(c)
+		fakeTime := unixTime(123)
+
+		Convey(`A test Environment`, func() {
+			// Seed a builder with 10 builds.
+			for i := 0; i < 10; i++ {
+				importBuild(c, &buildbot.Build{
+					Master:      "fake",
+					Buildername: "fake",
+					Number:      i,
+					Internal:    false,
+					Times:       buildbot.MkTimeRange(fakeTime, fakeTime),
+					Sourcestamp: &buildbot.SourceStamp{},
+					Results:     buildbot.Success,
+					Finished:    true,
+				})
+			}
+			// Failed build
 			importBuild(c, &buildbot.Build{
 				Master:      "fake",
 				Buildername: "fake",
-				Number:      i,
+				Number:      10,
 				Internal:    false,
 				Times:       buildbot.MkTimeRange(fakeTime, fakeTime),
 				Sourcestamp: &buildbot.SourceStamp{},
-				Results:     buildbot.Success,
+				Results:     buildbot.Failure,
 				Finished:    true,
+				Text:        []string{"failed", "stuff"},
 			})
-		}
-		// Failed build
-		importBuild(c, &buildbot.Build{
-			Master:      "fake",
-			Buildername: "fake",
-			Number:      10,
-			Internal:    false,
-			Times:       buildbot.MkTimeRange(fakeTime, fakeTime),
-			Sourcestamp: &buildbot.SourceStamp{},
-			Results:     buildbot.Failure,
-			Finished:    true,
-			Text:        []string{"failed", "stuff"},
-		})
-		buildstore.SaveMaster(c, &buildbot.Master{
-			Name:     "fake",
-			Builders: map[string]*buildbot.Builder{"fake": {}},
-		}, false, nil)
-		datastore.GetTestable(c).Consistent(true)
-		datastore.GetTestable(c).AutoIndex(true)
-		datastore.GetTestable(c).CatchupIndexes()
+			buildstore.SaveMaster(c, &buildbot.Master{
+				Name:     "fake",
+				Builders: map[string]*buildbot.Builder{"fake": {}},
+			}, false, nil)
+			datastore.GetTestable(c).Consistent(true)
+			datastore.GetTestable(c).AutoIndex(true)
+			datastore.GetTestable(c).CatchupIndexes()
 
-		Convey(`Invalid builder`, func() {
-			_, err := GetBuilder(c, "fake", "not real builder", 2, "")
-			So(err.Error(), ShouldResemble, "Cannot find builder \"not real builder\" in master \"fake\".\nAvailable builders: \nfake")
-		})
-		Convey(`Basic 3 build builder`, func() {
-			Convey(`Fetch 2`, func() {
-				response, err := GetBuilder(c, "fake", "fake", 2, "")
-				So(err, ShouldBeNil)
-				So(len(response.FinishedBuilds), ShouldEqual, 2)
-				So(response.NextCursor, ShouldEqual, "-9") // numbers < 9
-				So(response.PrevCursor, ShouldEqual, "")   // no prev cursor for a non-cursor query
-				So(response.FinishedBuilds[0].Link.Label, ShouldEqual, "#10")
-				So(response.FinishedBuilds[0].Text, ShouldResemble, []string{"failed stuff"})
-
-				Convey(`Fetch another 2`, func() {
-					response2, err := GetBuilder(c, "fake", "fake", 2, response.NextCursor)
+			Convey(`Invalid builder`, func() {
+				_, err := GetBuilder(c, "fake", "not real builder", 2, "")
+				So(err.Error(), ShouldResemble, "Cannot find builder \"not real builder\" in master \"fake\".\nAvailable builders: \nfake")
+			})
+			Convey(`Basic 3 build builder`, func() {
+				Convey(`Fetch 2`, func() {
+					response, err := GetBuilder(c, "fake", "fake", 2, "")
 					So(err, ShouldBeNil)
-					So(len(response2.FinishedBuilds), ShouldEqual, 2)
-					So(response2.PrevCursor, ShouldEqual, "9")  // numbers >= 9
-					So(response2.NextCursor, ShouldEqual, "-7") // numbers < 7
+					So(len(response.FinishedBuilds), ShouldEqual, 2)
+					So(response.NextCursor, ShouldEqual, "-9") // numbers < 9
+					So(response.PrevCursor, ShouldEqual, "")   // no prev cursor for a non-cursor query
+					So(response.FinishedBuilds[0].Link.Label, ShouldEqual, "#10")
+					So(response.FinishedBuilds[0].Text, ShouldResemble, []string{"failed stuff"})
 
 					Convey(`Fetch another 2`, func() {
-						response3, err := GetBuilder(c, "fake", "fake", 2, response2.NextCursor)
+						response2, err := GetBuilder(c, "fake", "fake", 2, response.NextCursor)
 						So(err, ShouldBeNil)
-						So(len(response3.FinishedBuilds), ShouldEqual, 2)
-						So(response3.PrevCursor, ShouldEqual, "7")  // numbers >= 7
-						So(response3.NextCursor, ShouldEqual, "-5") // numbers < 5
+						So(len(response2.FinishedBuilds), ShouldEqual, 2)
+						So(response2.PrevCursor, ShouldEqual, "9")  // numbers >= 9
+						So(response2.NextCursor, ShouldEqual, "-7") // numbers < 7
 
-						Convey(`Fetch the rest`, func() {
-							response4, err := GetBuilder(c, "fake", "fake", 20, response3.NextCursor)
+						Convey(`Fetch another 2`, func() {
+							response3, err := GetBuilder(c, "fake", "fake", 2, response2.NextCursor)
 							So(err, ShouldBeNil)
-							So(len(response4.FinishedBuilds), ShouldEqual, 5)
-							So(response4.PrevCursor, ShouldEqual, "5") // numbers >= 5
-							So(response4.NextCursor, ShouldEqual, "")  // EOF
+							So(len(response3.FinishedBuilds), ShouldEqual, 2)
+							So(response3.PrevCursor, ShouldEqual, "7")  // numbers >= 7
+							So(response3.NextCursor, ShouldEqual, "-5") // numbers < 5
+
+							Convey(`Fetch the rest`, func() {
+								response4, err := GetBuilder(c, "fake", "fake", 20, response3.NextCursor)
+								So(err, ShouldBeNil)
+								So(len(response4.FinishedBuilds), ShouldEqual, 5)
+								So(response4.PrevCursor, ShouldEqual, "5") // numbers >= 5
+								So(response4.NextCursor, ShouldEqual, "")  // EOF
+							})
 						})
 					})
 				})
