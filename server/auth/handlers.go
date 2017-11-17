f8b81f81b58bdfefbbd0886655f7e5d1ed95c561
Vadim Shtayura <vadimsh@chromium.org>
2017-11-16 20:02:09 -0800

    [tsmon] Refactor appengine/tsmon in preparation for splitting it.
    There are 3 functional components in this package currently:
      1) Middleware that manages tsmon state and captures request metrics.
      2) A housekeeping cron that assigns task numbers to instances.
      3) Global metrics (also reported from the housekeeping cron).
    
    (1) has very little actual ties to GAE and can be extracted into a package
    reusable from Flex or GKE or wherever. This CL is preparation for this.
    
    It introduces TaskNumAllocator interface that formalizes relation between
    the tsmon middleware and the housekeeping cron. GAE standard and GAE Flex will
    have slightly different implementations of TaskNumAllocator.
    
    R=tandrii@chromium.org
    BUG=785049
    
    Change-Id: Ibf9074b940166f4af5aa61e06ce9b8b77bd8d402

--- server/auth/handlers.go
+++ server/auth/handlers.go
@@ -19,10 +19,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"golang.org/x/net/context"
-
 	"go.chromium.org/luci/common/logging"
-	"go.chromium.org/luci/server/auth/signing"
 	"go.chromium.org/luci/server/router"
 )
 
@@ -39,7 +36,7 @@ func InstallHandlers(r *router.Router, base router.MiddlewareChain) {
 
 // certsHandler servers public certificates of the signer in the context.
 func certsHandler(c *router.Context) {
-	s := getSigner(c.Context)
+	s := GetSigner(c.Context)
 	if s == nil {
 		httpReplyError(c, http.StatusNotFound, "No Signer instance available")
 		return
@@ -54,7 +51,7 @@ func certsHandler(c *router.Context) {
 
 // infoHandler returns information about the current service identity.
 func infoHandler(c *router.Context) {
-	s := getSigner(c.Context)
+	s := GetSigner(c.Context)
 	if s == nil {
 		httpReplyError(c, http.StatusNotFound, "No Signer instance available")
 		return
@@ -68,13 +65,6 @@ func infoHandler(c *router.Context) {
 }
 
 ////
-
-func getSigner(c context.Context) signing.Signer {
-	if cfg := getConfig(c); cfg != nil {
-		return cfg.Signer
-	}
-	return nil
-}
 
 func httpReply(c *router.Context, code int, out interface{}) {
 	c.Writer.Header().Set("Content-Type", "application/json")
