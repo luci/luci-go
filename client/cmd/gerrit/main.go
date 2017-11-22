460f1af9fff13d2c542f42d204d21c6a99e74750
Petr Hosek <phosek@google.com>
2017-11-21 18:59:24 -0800

    gerrit: Minimal Gerrit client
    This is a minimal Gerrit client built on top of the existing common
    Gerrit API which also supports authentication context and can be
    used to query Gerrit API on bots.
    
    Change-Id: I2e2959a9d93ce70675a9df3f0157e56b7f8e7ec7
    Reviewed-on: https://chromium-review.googlesource.com/780689
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Petr Hosek <phosek@chromium.org>

---
+++ client/cmd/gerrit/main.go
@@ -0,0 +1,59 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/maruel/subcommands"
+
+	"go.chromium.org/luci/client/authcli"
+	"go.chromium.org/luci/client/versioncli"
+	"go.chromium.org/luci/common/api/gitiles"
+	"go.chromium.org/luci/common/auth"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+
+	"go.chromium.org/luci/hardcoded/chromeinfra"
+)
+
+// version must be updated whenever functional change (behavior, arguments,
+// supported commands) is done.
+const version = "0.1"
+
+func GetApplication(defaultAuthOpts auth.Options) *subcommands.DefaultApplication {
+	defaultAuthOpts.Scopes = []string{auth.OAuthScopeEmail, gitiles.OAuthScope}
+	return &subcommands.DefaultApplication{
+		Name:  "gerrit",
+		Title: "gerrit client",
+		// Keep in alphabetical order of their name.
+		Commands: []*subcommands.Command{
+			cmdChangeDetail(defaultAuthOpts),
+			cmdChangeQuery(defaultAuthOpts),
+			subcommands.CmdHelp,
+			authcli.SubcommandInfo(defaultAuthOpts, "whoami", false),
+			authcli.SubcommandLogin(defaultAuthOpts, "login", false),
+			authcli.SubcommandLogout(defaultAuthOpts, "logout", false),
+			versioncli.CmdVersion(version),
+		},
+	}
+}
+
+func main() {
+	log.SetFlags(log.Lmicroseconds)
+	mathrand.SeedRandomly()
+	app := GetApplication(chromeinfra.DefaultAuthOptions())
+	os.Exit(subcommands.Run(app, nil))
+}
