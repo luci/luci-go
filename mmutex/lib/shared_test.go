3518c3bc92e6a68506d822c72d0376fc0476bf9a
Charlie Andrews <charliea@chromium.org>
2017-12-04 15:58:05 -0500

    Fix two flaky time-based tests in mmutex package
    Both of these tests assumed that context-based deadlines will be
    enforced for the *exact* amount of time that they're specified. For
    various reasons, including the overhead of the previous lines of
    execution, it's likely that the time will differ slightly, so just
    checking with the context expired is likely to be a lot less
    error-prone.
    
    Bug: 790881
    Change-Id: I142c568c175a90c8f5245c493d0ebcf25cc6eb4e
    Reviewed-on: https://chromium-review.googlesource.com/806744
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Charlie Andrews <charliea@chromium.org>

--- mmutex/lib/shared_test.go
+++ mmutex/lib/shared_test.go
@@ -61,10 +61,9 @@ func TestShared(t *testing.T) {
 			So(err, ShouldBeNil)
 			defer handle.Unlock()
 
-			ctx, _ = context.WithTimeout(ctx, 5*time.Millisecond)
-			start := time.Now()
+			ctx, _ = context.WithTimeout(ctx, time.Millisecond)
 			So(RunShared(ctx, env, fnThatReturns(nil)), ShouldErrLike, "fslock: lock is held")
-			So(time.Now(), ShouldHappenOnOrAfter, start.Add(5*time.Millisecond))
+			So(ctx.Err(), ShouldErrLike, context.DeadlineExceeded)
 		})
 
 		Convey("uses context parameter as basis for new context", func() {
@@ -73,7 +72,7 @@ func TestShared(t *testing.T) {
 			err := RunShared(ctx, env, func(ctx context.Context) error {
 				return clock.Sleep(ctx, time.Millisecond).Err
 			})
-			So(err, ShouldErrLike, "context canceled")
+			So(err, ShouldErrLike, context.Canceled)
 		})
 
 		Convey("executes the command if shared lock already held", func() {
