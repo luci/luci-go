b6c3d5521472baca32d8ba1e1a96dba04dc719b8
smut <smut@google.com>
2017-12-07 14:37:13 -0800

    [Machine Database] Move RPC service into a separate package
    Bug: 778865
    Change-Id: I72eeb0e958a3d19e6154153893dd49bae155ce1f
    Reviewed-on: https://chromium-review.googlesource.com/815288
    Commit-Queue: smut <smut@google.com>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

---
+++ machine-db/appengine/rpc/crimson_test.go
@@ -0,0 +1,112 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"fmt"
+	"testing"
+
+	"golang.org/x/net/context"
+
+	"github.com/DATA-DOG/go-sqlmock"
+
+	"go.chromium.org/luci/common/data/stringset"
+
+	"go.chromium.org/luci/machine-db/api/crimson/v1"
+	"go.chromium.org/luci/machine-db/appengine/database"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestGetDatacenters(t *testing.T) {
+	Convey("getDatacenters", t, func() {
+		db, m, _ := sqlmock.New()
+		defer db.Close()
+		c := database.With(context.Background(), db)
+		selectStmt := `^SELECT name, description FROM datacenters$`
+		columns := []string{"name", "description"}
+		rows := sqlmock.NewRows(columns)
+
+		Convey("query failed", func() {
+			names := stringset.NewFromSlice([]string{"datacenter"}...)
+			m.ExpectQuery(selectStmt).WillReturnError(fmt.Errorf("error"))
+			dcs, err := getDatacenters(c, names)
+			So(dcs, ShouldHaveLength, 0)
+			So(err, ShouldErrLike, "failed to fetch datacenters")
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("empty", func() {
+			names := stringset.NewFromSlice([]string{"datacenter"}...)
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			dcs, err := getDatacenters(c, names)
+			So(dcs, ShouldHaveLength, 0)
+			So(err, ShouldBeNil)
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("no matches", func() {
+			names := stringset.NewFromSlice([]string{"datacenter"}...)
+			rows.AddRow("datacenter 1", "description 1")
+			rows.AddRow("datacenter 2", "description 2")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			dcs, err := getDatacenters(c, names)
+			So(dcs, ShouldHaveLength, 0)
+			So(err, ShouldBeNil)
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+		Convey("matches", func() {
+			names := stringset.NewFromSlice([]string{"datacenter 2", "datacenter 3"}...)
+			rows.AddRow("datacenter 1", "description 1")
+			rows.AddRow("datacenter 2", "description 2")
+			rows.AddRow("datacenter 3", "description 3")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			dcs, err := getDatacenters(c, names)
+			So(dcs, ShouldResemble, []*crimson.Datacenter{
+				{
+					Name:        "datacenter 2",
+					Description: "description 2",
+				},
+				{
+					Name:        "datacenter 3",
+					Description: "description 3",
+				},
+			})
+			So(err, ShouldBeNil)
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("ok", func() {
+			names := stringset.NewFromSlice([]string{}...)
+			rows.AddRow("datacenter 1", "description 1")
+			rows.AddRow("datacenter 2", "description 2")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			dcs, err := getDatacenters(c, names)
+			So(dcs, ShouldResemble, []*crimson.Datacenter{
+				{
+					Name:        "datacenter 1",
+					Description: "description 1",
+				},
+				{
+					Name:        "datacenter 2",
+					Description: "description 2",
+				},
+			})
+			So(err, ShouldBeNil)
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+	})
+}
