2137b3a9aced8586950358f4ca0f76b379a54ced
Michael Anthony Knyszek <mknyszek@google.com>
2017-12-13 16:06:09 -0800

    [cloudkms] Add CLI client with encrypt.
    Change-Id: I198f4be7d1c9f2f46bbf6fc85f819da74528aa17
    Reviewed-on: https://chromium-review.googlesource.com/825961
    Commit-Queue: Michael Knyszek <mknyszek@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

---
+++ client/cmd/cloudkms/common.go
@@ -0,0 +1,90 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/maruel/subcommands"
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/client/authcli"
+	"go.chromium.org/luci/common/auth"
+	"go.chromium.org/luci/common/errors"
+)
+
+type commonFlags struct {
+	subcommands.CommandRunBase
+	authFlags      authcli.Flags
+	parsedAuthOpts auth.Options
+	output         string
+}
+
+func (c *commonFlags) Init(authOpts auth.Options) {
+	c.authFlags.Register(&c.Flags, authOpts)
+	c.Flags.StringVar(&c.output, "output", "", "Path to write operation results to (use '-' for stdout).")
+}
+
+func (c *commonFlags) Parse() error {
+	var err error
+	c.parsedAuthOpts, err = c.authFlags.Options()
+	return err
+}
+
+func (c *commonFlags) createAuthClient(ctx context.Context) (*http.Client, error) {
+	return auth.NewAuthenticator(ctx, auth.SilentLogin, c.parsedAuthOpts).Client()
+}
+
+func readInput(file string) ([]byte, error) {
+	if file == "-" {
+		return ioutil.ReadAll(os.Stdin)
+	}
+	return ioutil.ReadFile(file)
+}
+
+func writeOutput(file string, data []byte) error {
+	if file == "-" {
+		_, err := os.Stdout.Write(data)
+		return err
+	}
+	return ioutil.WriteFile(file, data, 0664)
+}
+
+var kmsPathComponents = []string{
+	"projects",
+	"locations",
+	"keyRings",
+	"cryptoKeys",
+	"cryptoKeyVersions",
+}
+
+func parseKMSPath(path string) (string, error) {
+	components := strings.Split(path, "/")
+	if len(components) < 2 {
+		return "", errors.New("a KMS path needs at least a project and a location")
+	}
+	if len(components) > 5 {
+		return "", errors.New("a KMS path will never have more than 5 components")
+	}
+	full := make([]string, 0, 2*len(components))
+	for i, c := range components {
+		full = append(full, kmsPathComponents[i])
+		full = append(full, c)
+	}
+	return strings.Join(full, "/"), nil
+}
