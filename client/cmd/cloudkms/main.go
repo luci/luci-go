f006013960c5f23a871876f56ab81db17b58c69f
Michael Anthony Knyszek <mknyszek@google.com>
2017-12-13 16:06:09 -0800

    [cloudkms] Add CLI client with encrypt.
    Change-Id: I198f4be7d1c9f2f46bbf6fc85f819da74528aa17

---
+++ client/cmd/cloudkms/main.go
@@ -0,0 +1,64 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+
+	"github.com/maruel/subcommands"
+
+	"golang.org/x/net/context"
+
+	cloudkms "google.golang.org/api/cloudkms/v1"
+
+	"go.chromium.org/luci/client/authcli"
+	"go.chromium.org/luci/client/versioncli"
+	"go.chromium.org/luci/common/auth"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+	"go.chromium.org/luci/common/logging/gologger"
+
+	"go.chromium.org/luci/hardcoded/chromeinfra"
+)
+
+// version must be updated whenever functional change (behavior, arguments,
+// supported commands) is done.
+const version = "0.1"
+
+func GetApplication(defaultAuthOpts auth.Options) *cli.Application {
+	defaultAuthOpts.Scopes = []string{auth.OAuthScopeEmail, cloudkms.CloudPlatformScope}
+	return &cli.Application{
+		Name:  "cloudkms",
+		Title: "Client for interfacing with Google Cloud Key Management Service",
+		Context: func(ctx context.Context) context.Context {
+			return gologger.StdConfig.Use(ctx)
+		},
+		// Keep in alphabetical order of their name.
+		Commands: []*subcommands.Command{
+			subcommands.CmdHelp,
+			cmdEncrypt(defaultAuthOpts),
+			authcli.SubcommandInfo(defaultAuthOpts, "whoami", false),
+			authcli.SubcommandLogin(defaultAuthOpts, "login", false),
+			authcli.SubcommandLogout(defaultAuthOpts, "logout", false),
+			versioncli.CmdVersion(version),
+		},
+	}
+}
+
+func main() {
+	mathrand.SeedRandomly()
+	app := GetApplication(chromeinfra.DefaultAuthOptions())
+	os.Exit(subcommands.Run(app, nil))
+}
