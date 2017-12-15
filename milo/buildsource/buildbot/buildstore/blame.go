57c55e157c0d3b0ee4c0af1e1869f17162a638ef
Nodir Turakulov <nodir@google.com>
2017-12-15 11:49:24 -0800

    [milo] cap getPrevRev recursive call
    When switching LUCI to prod, we also bump next build number. This causes
    buildstore.getPrevRev fall into the slowest code path where it recursively
    goes back by build numbers trying to find prev build, which takes a lot of
    time if the build number was bumped by a large number (1000x),
    and ultimately fails with the request timeout.
    
    Cap getPrevRev recursion.
    
    Bug: 793514
    Change-Id: Ifca3db1d89b7c82b8643c02f39d9ceebdc736dd0

--- milo/buildsource/buildbot/buildstore/blame.go
+++ milo/buildsource/buildbot/buildstore/blame.go
@@ -107,7 +107,7 @@ func fetchChanges(c context.Context, b *buildbot.Build) error {
 		return nil
 	}
 
-	prevRev, err := getPrevRev(c, b)
+	prevRev, err := getPrevRev(c, b, 100)
 	switch {
 	case err != nil:
 		return errors.Annotate(err, "failed to get prev revision for build %q", b).Err()
@@ -167,10 +167,20 @@ func changeFromGitiles(repoURL, branch string, commit gitiles.Commit) buildbot.C
 // getPrevRev returns revision of the closest previous build with a commit
 // hash, or "" if not found.
 // Memcaches results.
-func getPrevRev(c context.Context, b *buildbot.Build) (string, error) {
-	if b.Number == 0 {
+func getPrevRev(c context.Context, b *buildbot.Build, maxRecursionDepth int) (string, error) {
+	// note: we cannot use exponential scan here because there may be build
+	// number gaps anywhere, for example given build numbers 10 20 30 40,
+	// if we check 25 while scanning [20, 40), we don't know if we should continue
+	// scanning in [20, 25) or (25, 40).
+
+	switch {
+	case b.Number == 0:
+		return "", nil
+	case maxRecursionDepth <= 0:
+		logging.Warningf(c, "reached maximum recursion depth; giving up")
 		return "", nil
 	}
+
 	prev := &buildbot.Build{
 		Master:      b.Master,
 		Buildername: b.Buildername,
@@ -204,7 +214,7 @@ func getPrevRev(c context.Context, b *buildbot.Build) (string, error) {
 
 		// This is a recursive call of itself.
 		// The results are memcached along the stack though.
-		if prevRev, err = getPrevRev(c, b); err != nil {
+		if prevRev, err = getPrevRev(c, b, maxRecursionDepth-1); err != nil {
 			return "", err
 		}
 	}
