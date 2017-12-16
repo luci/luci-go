1ea3f61ac9288ea66f56ac10972a3ac7a3ca3215
Michael Anthony Knyszek <mknyszek@google.com>
2017-12-15 17:37:12 -0800

    [milo] Add buildbucket access ACL functions.
    Change-Id: I848a97ae7d4ed34b9751c61f4d3a8c992fe2f04c

--- milo/common/acl.go
+++ milo/common/acl.go
@@ -15,8 +15,17 @@
 package common
 
 import (
+	"net/http"
+	"time"
+
 	"golang.org/x/net/context"
 
+	"go.chromium.org/gae/service/memcache"
+
+	bbAccess "go.chromium.org/luci/buildbucket/access"
+	accessProto "go.chromium.org/luci/common/proto/access"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/luci_config/common/cfgtypes"
 	"go.chromium.org/luci/luci_config/server/cfgclient/access"
 	"go.chromium.org/luci/luci_config/server/cfgclient/backend"
@@ -52,4 +61,110 @@ func IsAllowed(c context.Context, project string) (bool, error) {
 func IsAdmin(c context.Context) (bool, error) {
 	// TODO(nodir): unhardcode group name to config file if there is a need
 	return auth.IsMember(c, "administrators")
+}
+
+// AccessClient is a high-level interface for getting bucket permissions from buildbucket.
+//
+// The main purpose of this interface is to stub out bbAccess.BucketPermissions for testing,
+// rather than relying on the low-level accessProto.AccessClient interface.
+type AccessClient interface {
+	bucketPermissions(c context.Context, buckets []string) (bbAccess.Permissions, time.Duration, error)
+}
+
+type accessClientImpl struct {
+	accessProto.AccessClient
+}
+
+func (a *accessClientImpl) bucketPermissions(c context.Context, buckets []string) (bbAccess.Permissions, time.Duration, error) {
+	return bbAccess.BucketPermissions(c, a.AccessClient, buckets)
+}
+
+// BuildbucketClient returns a new AccessClient for talking to this milo instance's buildbucket instance.
+// The only pRPC service exposed by buildbucket currently is the access API.
+func NewAccessClient(c context.Context) (AccessClient, error) {
+	settings := GetSettings(c)
+	if settings.Buildbucket == nil {
+		return nil, errors.Reason("no buildbucket config found").Err()
+	}
+	t, err := auth.GetRPCTransport(c, auth.AsUser)
+	if err != nil {
+		return nil, errors.Annotate(err, "getting RPC Transport").Err()
+	}
+	return &accessClientImpl{
+		AccessClient: bbAccess.NewClient(settings.Buildbucket.Host, &http.Client{Transport: t}),
+	}, nil
+}
+
+// BucketPermissions gets permissions for the current identity for all given buckets.
+func BucketPermissions(c context.Context, client AccessClient, buckets ...string) (bbAccess.Permissions, error) {
+	perms := make(bbAccess.Permissions, len(buckets))
+
+	// Create cache entries for each bucket.
+	entries := make([]memcache.Item, len(buckets))
+	identityString := string(auth.CurrentIdentity(c))
+	for i, bucket := range buckets {
+		item := memcache.NewItem(c, identityString+"|"+bucket)
+		entries[i] = item
+	}
+
+	// Check the cache.
+	if err := memcache.Get(c, entries...); err != nil && err != memcache.ErrCacheMiss {
+		if merr, ok := err.(errors.MultiError); ok {
+			for _, err := range merr {
+				if err != nil && err != memcache.ErrCacheMiss {
+					logging.WithError(err).Warningf(c, "memcache get")
+					break
+				}
+			}
+		} else {
+			logging.WithError(err).Warningf(c, "memcache get")
+		}
+	}
+
+	// Collect uncached buckets, if any. Also put cached buckets into perms.
+	var bucketsToCache []string
+	var itemsToUpdate []memcache.Item
+	for i, bucket := range buckets {
+		action := bbAccess.Action(0)
+		err := action.UnmarshalBinary(entries[i].Value())
+		if err != nil {
+			bucketsToCache = append(bucketsToCache, bucket)
+			itemsToUpdate = append(itemsToUpdate, entries[i])
+			continue
+		}
+		perms[bucket] = action
+	}
+
+	// Finish early if all of the buckets were in the cache.
+	if len(bucketsToCache) == 0 {
+		return perms, nil
+	}
+
+	// Make an RPC to get uncached buckets from buildbucket.
+	newPerms, validTime, err := client.bucketPermissions(c, bucketsToCache)
+	if err != nil {
+		return nil, err
+	}
+
+	// Update items, collect them, and put their values into perms.
+	for i, bucket := range bucketsToCache {
+		action, ok := newPerms[bucket]
+		if !ok {
+			continue
+		}
+		bytes, err := action.MarshalBinary()
+		if err != nil {
+			return nil, errors.New("failed to marshal Action")
+		}
+		itemsToUpdate[i].SetValue(bytes)
+		itemsToUpdate[i].SetExpiration(validTime)
+		perms[bucket] = action 
+	}
+
+	// Update the cache.
+	if err := memcache.Set(c, itemsToUpdate...); err != nil {
+		logging.WithError(err).Warningf(c, "memcache set")
+	}
+
+	return perms, nil
 }
