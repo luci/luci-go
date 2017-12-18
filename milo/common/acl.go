cad8bb0d4fee9b53ddd15011c63dc684124f144f
Michael Anthony Knyszek <mknyszek@google.com>
2017-12-18 14:46:17 -0800

    [milo] Add buildbucket access ACL functions.
    Change-Id: I848a97ae7d4ed34b9751c61f4d3a8c992fe2f04c

--- milo/common/acl.go
+++ milo/common/acl.go
@@ -15,8 +15,17 @@
 package common
 
 import (
+	"net/http"
+
 	"golang.org/x/net/context"
 
+	"go.chromium.org/gae/service/info"
+	"go.chromium.org/gae/service/memcache"
+
+	bbAccess "go.chromium.org/luci/buildbucket/access"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	accessProto "go.chromium.org/luci/common/proto/access"
 	"go.chromium.org/luci/luci_config/common/cfgtypes"
 	"go.chromium.org/luci/luci_config/server/cfgclient/access"
 	"go.chromium.org/luci/luci_config/server/cfgclient/backend"
@@ -52,4 +61,97 @@ func IsAllowed(c context.Context, project string) (bool, error) {
 func IsAdmin(c context.Context) (bool, error) {
 	// TODO(nodir): unhardcode group name to config file if there is a need
 	return auth.IsMember(c, "administrators")
+}
+
+// NewAccessClient returns a new AccessClient for talking to this milo instance's buildbucket instance.
+// The only pRPC service exposed by buildbucket currently is the access API.
+func NewAccessClient(c context.Context) (accessProto.AccessClient, error) {
+	settings := GetSettings(c)
+	if settings.Buildbucket.GetHost() == "" {
+		return nil, errors.Reason("no buildbucket done found").Err()
+	}
+	t, err := auth.GetRPCTransport(c, auth.AsUser)
+	if err != nil {
+		return nil, errors.Annotate(err, "getting RPC Transport").Err()
+	}
+	return bbAccess.NewClient(settings.Buildbucket.Host, &http.Client{Transport: t}), nil
+}
+
+// BucketPermissions gets permissions for the current identity for all given buckets.
+func BucketPermissions(c context.Context, client accessProto.AccessClient, buckets ...string) (bbAccess.Permissions, error) {
+	perms := make(bbAccess.Permissions, len(buckets))
+
+	// Set the namespace in the context for memcache.
+	// TODO(mknyszek): Derive this namespace from the buildbucket host we're calling to.
+	c, err := info.Namespace(c, "buildbucket-access")
+	if err != nil {
+		return nil, err
+	}
+
+	// Create cache entries for each bucket.
+	entries := make([]memcache.Item, len(buckets))
+	identityString := string(auth.CurrentIdentity(c))
+	for i, bucket := range buckets {
+		entries[i] = memcache.NewItem(c, identityString+"|"+bucket)
+	}
+
+	// Check the cache.
+	if err := memcache.Get(c, entries...); err != nil && err != memcache.ErrCacheMiss {
+		if merr, ok := err.(errors.MultiError); ok {
+			for i, err := range merr {
+				if err != nil && err != memcache.ErrCacheMiss {
+					logging.WithError(err).Warningf(c, "memcache get %s", entries[i])
+				}
+			}
+		} else {
+			logging.WithError(err).Warningf(c, "memcache get")
+		}
+	}
+
+	// Collect uncached buckets, if any. Also put cached buckets into perms.
+	var bucketsToCache []string
+	var itemsToUpdate []memcache.Item
+	for i, bucket := range buckets {
+		action := bbAccess.Action(0)
+		err := action.UnmarshalBinary(entries[i].Value())
+		if err != nil {
+			bucketsToCache = append(bucketsToCache, bucket)
+			itemsToUpdate = append(itemsToUpdate, entries[i])
+			continue
+		}
+		perms[bucket] = action
+	}
+
+	// Finish early if all of the buckets were in the cache.
+	if len(bucketsToCache) == 0 {
+		return perms, nil
+	}
+
+	// Make an RPC to get uncached buckets from buildbucket.
+	newPerms, validTime, err := bbAccess.BucketPermissions(c, client, bucketsToCache)
+	if err != nil {
+		return nil, err
+	}
+
+	// Update items, collect them, and put their values into perms.
+	for i, bucket := range bucketsToCache {
+		action, ok := newPerms[bucket]
+		if !ok {
+			continue
+		}
+		bytes, err := action.MarshalBinary()
+		if err != nil {
+			return nil, errors.Annotate(err, "failed to marshal Action").Err()
+		}
+		itemsToUpdate[i].SetValue(bytes)
+		itemsToUpdate[i].SetExpiration(validTime)
+		perms[bucket] = action
+	}
+
+	// Update the cache.
+	if err := memcache.Set(c, itemsToUpdate...); err != nil {
+		logging.WithError(err).Warningf(c, "memcache set")
+	}
+
+	return perms, nil
 }
