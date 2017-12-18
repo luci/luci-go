bcccc58be0ea1609025fc824eb0d9090d9627216
Michael Anthony Knyszek <mknyszek@google.com>
2017-12-18 14:44:35 -0800

    [buildbucket] Add conversion methods for Permissions.
    This change adds conversion methods for Permissions called ToProto
    and FromProto which convert to and from the access API's
    PermittedActionsResponse message.
    
    Change-Id: I289610e3562faa18bfb52cf7f8579ee0ccffe344

--- buildbucket/access/acl.go
+++ buildbucket/access/acl.go
@@ -16,6 +16,7 @@ package access
 
 import (
 	"net/http"
+	"sort"
 	"time"
 
 	"github.com/golang/protobuf/ptypes"
@@ -35,6 +36,45 @@ type Permissions map[string]Action
 // Can checks whether an Action is allowed for a given bucket.
 func (p Permissions) Can(bucket string, action Action) bool {
 	return (p[bucket] & action) == action
+}
+
+// FromProto populates a Permissions from an access.PermittedActionsResponse.
+func (p Permissions) FromProto(resp *access.PermittedActionsResponse) error {
+	// Clean up p before we populate it.
+	for bucket := range p {
+		delete(p, bucket)
+	}
+	for bucket, actions := range resp.Permitted {
+		newActions := Action(0)
+		for _, action := range actions.Actions {
+			newAction, err := ParseAction(action)
+			if err != nil {
+				return err
+			}
+			newActions |= newAction
+		}
+		p[bucket] = newActions
+	}
+	return nil
+}
+
+// ToProto converts a Permissions into a PermittedActionsResponse.
+func (p Permissions) ToProto(validTime time.Duration) *access.PermittedActionsResponse {
+	perms := make(map[string]*access.PermittedActionsResponse_ResourcePermissions, len(p))
+	for bucket, action := range p {
+		var actions []string
+		for a, name := range actionToName {
+			if action&a == a {
+				actions = append(actions, name)
+			}
+		}
+		sort.Strings(actions)
+		perms[bucket] = &access.PermittedActionsResponse_ResourcePermissions{Actions: actions}
+	}
+	return &access.PermittedActionsResponse{
+		Permitted:        perms,
+		ValidityDuration: ptypes.DurationProto(validTime),
+	}
 }
 
 // NewClient returns a new AccessClient which can be used to talk to
@@ -61,17 +101,9 @@ func BucketPermissions(c context.Context, client access.AccessClient, buckets []
 	}
 
 	// Parse proto into a convenient format.
-	perms := make(map[string]Action, len(resp.Permitted))
-	for bucket, actions := range resp.Permitted {
-		newActions := Action(0)
-		for _, action := range actions.Actions {
-			newAction, err := ParseAction(action)
-			if err != nil {
-				return nil, 0, err
-			}
-			newActions |= newAction
-		}
-		perms[bucket] = newActions
+	perms := make(Permissions, len(resp.Permitted))
+	if err := perms.FromProto(resp); err != nil {
+		return nil, 0, err
 	}
 	dur, err := ptypes.Duration(resp.ValidityDuration)
 	if err != nil {
