b8de57f2a580efa296520b257f7e34f8564333d0
Michael Anthony Knyszek <mknyszek@google.com>
2017-12-18 14:46:17 -0800

    [milo] Add buildbucket access ACL functions.
    Change-Id: I848a97ae7d4ed34b9751c61f4d3a8c992fe2f04c

--- milo/common/acl.go
+++ milo/common/acl.go
@@ -15,8 +15,18 @@
 package common
 
 import (
+	"fmt"
+	"net/http"
+
 	"golang.org/x/net/context"
 
+	"go.chromium.org/gae/service/info"
+	"go.chromium.org/gae/service/memcache"
+
+	bbAccess "go.chromium.org/luci/buildbucket/access"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	accessProto "go.chromium.org/luci/common/proto/access"
 	"go.chromium.org/luci/luci_config/common/cfgtypes"
 	"go.chromium.org/luci/luci_config/server/cfgclient/access"
 	"go.chromium.org/luci/luci_config/server/cfgclient/backend"
@@ -52,4 +62,125 @@ func IsAllowed(c context.Context, project string) (bool, error) {
 func IsAdmin(c context.Context) (bool, error) {
 	// TODO(nodir): unhardcode group name to config file if there is a need
 	return auth.IsMember(c, "administrators")
+}
+
+var accessClientKey = "access client key"
+
+// AccessClient wraps an accessProto.AccessClient and exports its Host.
+type AccessClient struct {
+	accessProto.AccessClient
+	Host string
+}
+
+// NewAccessClient creates a new AccessClient for talking to this milo instance's buildbucket instance.
+func NewAccessClient(c context.Context) (*AccessClient, error) {
+	settings := GetSettings(c)
+	if settings.Buildbucket.GetHost() == "" {
+		return nil, errors.Reason("no buildbucket host found").Err()
+	}
+	t, err := auth.GetRPCTransport(c, auth.AsUser)
+	if err != nil {
+		return nil, errors.Annotate(err, "getting RPC Transport").Err()
+	}
+	return &AccessClient{
+		AccessClient: bbAccess.NewClient(settings.Buildbucket.Host, &http.Client{Transport: t}),
+		Host:         settings.Buildbucket.Host,
+	}, nil
+}
+
+// WithAccessClient attaches an AccessClient to the given context.
+func WithAccessClient(c context.Context, a *AccessClient) context.Context {
+	return context.WithValue(c, &accessClientKey, a)
+}
+
+// GetAccessClient retrieves an AccessClient from the given context.
+func GetAccessClient(c context.Context) *AccessClient {
+	if client, ok := c.Value(&accessClientKey).(*AccessClient); !ok {
+		panic("access client not found in context")
+	} else {
+		return client
+	}
+}
+
+// BucketPermissions gets permissions for the current identity for all given buckets.
+//
+// TODO(mknyszek): If a cache entry expires, then there could be QPS issues if all
+// instances query buildbucket for an update simultaneously. Evaluate whether there's
+// an issue in practice, and if so, consider expiring cache entries randomly.
+func BucketPermissions(c context.Context, buckets ...string) (bbAccess.Permissions, error) {
+	perms := make(bbAccess.Permissions, len(buckets))
+
+	// Set the namespace in the context for memcache.
+	client := GetAccessClient(c)
+	c, err := info.Namespace(c, fmt.Sprintf("buildbucket-access-%s", client.Host))
+	if err != nil {
+		return nil, err
+	}
+
+	// Create cache entries for each bucket.
+	entries := make([]memcache.Item, len(buckets))
+	identityString := string(auth.CurrentIdentity(c))
+	for i, bucket := range buckets {
+		entries[i] = memcache.NewItem(c, identityString+"|"+bucket)
+	}
+
+	// Check the cache.
+	if err := memcache.Get(c, entries...); err != nil && err != memcache.ErrCacheMiss {
+		if merr, ok := err.(errors.MultiError); ok {
+			for i, err := range merr {
+				if err != nil && err != memcache.ErrCacheMiss {
+					logging.WithError(err).Warningf(c, "memcache get %s", entries[i].Key())
+				}
+			}
+		} else {
+			logging.WithError(err).Warningf(c, "memcache get")
+		}
+	}
+
+	// Collect uncached buckets, if any. Also put cached buckets into perms.
+	var bucketsToCache []string
+	var itemsToUpdate []memcache.Item
+	for i, bucket := range buckets {
+		action := bbAccess.Action(0)
+		err := action.UnmarshalBinary(entries[i].Value())
+		if err != nil {
+			bucketsToCache = append(bucketsToCache, bucket)
+			itemsToUpdate = append(itemsToUpdate, entries[i])
+			continue
+		}
+		perms[bucket] = action
+	}
+
+	// Finish early if all of the buckets were in the cache.
+	if len(bucketsToCache) == 0 {
+		return perms, nil
+	}
+
+	// Make an RPC to get uncached buckets from buildbucket.
+	newPerms, validTime, err := bbAccess.BucketPermissions(c, client, bucketsToCache)
+	if err != nil {
+		return nil, err
+	}
+
+	// Update items, collect them, and put their values into perms.
+	for i, bucket := range bucketsToCache {
+		action, ok := newPerms[bucket]
+		if !ok {
+			continue
+		}
+		bytes, err := action.MarshalBinary()
+		if err != nil {
+			return nil, errors.Annotate(err, "failed to marshal Action").Err()
+		}
+		itemsToUpdate[i].SetValue(bytes)
+		itemsToUpdate[i].SetExpiration(validTime)
+		perms[bucket] = action
+	}
+
+	// Update the cache.
+	if err := memcache.Set(c, itemsToUpdate...); err != nil {
+		logging.WithError(err).Warningf(c, "memcache set")
+	}
+
+	return perms, nil
 }
--- milo/common/acl_test.go
+++ milo/common/acl_test.go
@@ -16,9 +16,12 @@ package common
 
 import (
 	"testing"
+	"time"
 
 	"go.chromium.org/gae/impl/memory"
+	"go.chromium.org/luci/buildbucket/access"
 	"go.chromium.org/luci/common/auth/identity"
+	"go.chromium.org/luci/common/clock/testclock"
 	memcfg "go.chromium.org/luci/common/config/impl/memory"
 	"go.chromium.org/luci/common/logging/gologger"
 	"go.chromium.org/luci/luci_config/server/cfgclient/backend/testconfig"
@@ -28,6 +31,13 @@ import (
 
 	. "github.com/smartystreets/goconvey/convey"
 )
+
+func withTestAccessClient(c context.Context, client *access.TestClient) context.Context {
+	return WithAccessClient(c, &AccessClient{
+		AccessClient: client,
+		Host:         "buildbucket.example.com",
+	})
+}
 
 func TestACL(t *testing.T) {
 	t.Parallel()
@@ -85,6 +95,107 @@ func TestACL(t *testing.T) {
 					So(ok, ShouldEqual, false)
 					So(err, ShouldBeNil)
 				})
+			})
+		})
+
+		Convey("Set up buildbucket client", func() {
+			client := access.TestClient{}
+			helloPermissions := access.Permissions{
+				"hello": access.AccessBucket,
+			}
+
+			Convey("With anonymous identity", func() {
+				c = auth.WithState(c, &authtest.FakeState{
+					Identity:       identity.AnonymousIdentity,
+					IdentityGroups: []string{"all"},
+				})
+
+				Convey("Get bucket permissions uncached", func() {
+					c = withTestAccessClient(c, &client)
+
+					// Uncached call.
+					client.PermittedActionsResponse = helloPermissions.ToProto(0)
+					perms, err := BucketPermissions(c, "hello")
+					So(err, ShouldBeNil)
+					So(perms, ShouldResemble, helloPermissions)
+
+					// Cached call. Since perms is nil and we haven't advanced the clock,
+					// this would error if the value isn't cached.
+					client.PermittedActionsResponse = (access.Permissions{}).ToProto(0)
+					perms, err = BucketPermissions(c, "hello")
+					So(err, ShouldBeNil)
+					So(perms["hello"], ShouldEqual, access.AccessBucket)
+				})
+
+				Convey("Get bucket permissions cache duration", func() {
+					c = withTestAccessClient(c, &client)
+					c, clk := testclock.UseTime(c, testclock.TestRecentTimeLocal)
+
+					// Uncached call.
+					client.PermittedActionsResponse = helloPermissions.ToProto(1 * time.Second)
+					perms, err := BucketPermissions(c, "hello")
+					So(err, ShouldBeNil)
+					So(perms, ShouldResemble, helloPermissions)
+
+					// Move time forward.
+					clk.Add(2 * time.Second)
+
+					// Also uncached call.
+					expectPermissions := access.Permissions{
+						"hello": access.ViewBuild,
+					}
+					client.PermittedActionsResponse = expectPermissions.ToProto(0)
+					perms, err = BucketPermissions(c, "hello")
+					So(err, ShouldBeNil)
+					So(perms, ShouldResemble, expectPermissions)
+				})
+
+				Convey("Get bucket permissions for cached and uncached buckets", func() {
+					c = withTestAccessClient(c, &client)
+
+					// Uncached call.
+					client.PermittedActionsResponse = helloPermissions.ToProto(0)
+					perms, err := BucketPermissions(c, "hello")
+					So(err, ShouldBeNil)
+					So(perms, ShouldResemble, helloPermissions)
+
+					// Cached call for hello, but uncached call for goodbye.
+					// Since perms is nil and we haven't advanced the clock,
+					// this should fail if there's no caching happening.
+					goodbyePermissions := access.Permissions{
+						"goodbye": access.AccessBucket,
+					}
+					client.PermittedActionsResponse = goodbyePermissions.ToProto(0)
+					perms, err = BucketPermissions(c, "hello", "goodbye")
+					So(err, ShouldBeNil)
+					So(perms["hello"], ShouldEqual, access.AccessBucket)
+					So(perms["goodbye"], ShouldEqual, access.AccessBucket)
+				})
+			})
+
+			Convey("Make sure cache doesn't share identity", func() {
+				c = withTestAccessClient(c, &client)
+
+				cABob := auth.WithState(c, &authtest.FakeState{
+					Identity:       "user:alicebob@google.com",
+					IdentityGroups: []string{"googlers", "all"},
+				})
+				cEve := auth.WithState(c, &authtest.FakeState{
+					Identity:       "user:eve@notgoogle.com",
+					IdentityGroups: []string{"all"},
+				})
+				// Uncached call.
+				client.PermittedActionsResponse = helloPermissions.ToProto(0)
+				perms, err := BucketPermissions(cEve, "hello")
+				So(err, ShouldBeNil)
+				So(perms, ShouldResemble, helloPermissions)
+
+				// Eve's result should now be cached, but anon should make an RPC
+				// for themselves, and Eve's result should not be used.
+				client.PermittedActionsResponse = (access.Permissions{}).ToProto(0)
+				perms, err = BucketPermissions(cABob, "hello")
+				So(err, ShouldBeNil)
+				So(len(perms), ShouldEqual, 0)
 			})
 		})
 	})
