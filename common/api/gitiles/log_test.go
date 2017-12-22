e4d953b04b238e6b320ed7ab915122f39e428faf
Nodir Turakulov <nodir@google.com>
2017-12-22 14:22:27 -0800

    [gitiles] add gitiles service as a proto
    Declare Gitiles service in proto language.
    Next CL will rewrite gitiles package to provide its functionality as an
    implementation of the proto service, in the same way as monorail client is
    implemented:
    https://chromium.googlesource.com/infra/infra/+/9a5d9f98b2dd859726933a23c985c516641e4710/go/src/infra/monorail/monorail.proto
    https://chromium.googlesource.com/infra/infra/+/9a5d9f98b2dd859726933a23c985c516641e4710/go/src/infra/monorail/endpoints.go
    
    gitiles package will contain helper functions for LogForward and auto-paging Log.
    
    Bug:
    Change-Id: I7d94d1b9edffe1662ea673bc1b4b3bc621a49cb5

---
+++ common/api/gitiles/log_test.go
@@ -0,0 +1,79 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gitiles
+
+import (
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/common/proto/gitiles"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestPagingLog(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	Convey("PagingLog", t, func() {
+		var reqs []http.Request
+		srv, c := newMockClient(func(w http.ResponseWriter, r *http.Request) {
+			reqs = append(reqs, *r)
+
+			w.Header().Set("Content-Type", "application/json")
+			fmt.Fprintf(w, ")]}'\n")
+			if r.FormValue("s") == "" {
+				fmt.Fprintf(w, `{"log": [%s], "next": "next_cursor_value"}`, fakeCommit1Str)
+			} else {
+				fmt.Fprintf(w, `{"log": [%s]}`, fakeCommit2Str)
+			}
+		})
+		defer srv.Close()
+
+		Convey("Page till no cursor", func() {
+			req := gitiles.LogRequest{
+				Project:  "repo",
+				Ancestor: "master",
+				Treeish:  "8de6836858c99e48f3c58164ab717bda728e95dd",
+			}
+			commits, err := PagingLog(ctx, c, req, 10)
+			So(err, ShouldBeNil)
+			So(reqs, ShouldHaveLength, 10)
+			So(reqs[0].FormValue("s"), ShouldEqual, "")
+			So(reqs[1].FormValue("s"), ShouldEqual, "next_cursor_value")
+			So(reqs[9].FormValue("s"), ShouldEqual, "next_cursor_value")
+			So(len(commits), ShouldEqual, 10)
+			So(commits[0].Author.Name, ShouldEqual, "Author 1")
+			So(hex.EncodeToString(commits[1].Id), ShouldEqual, "dc1dbf1aa56e4dd4cbfaab61c4d30a35adce5f40")
+		})
+
+		Convey("Page till limit", func() {
+			req := gitiles.LogRequest{
+				Project: "repo",
+				Treeish: "master",
+			}
+			commits, err := PagingLog(ctx, c, req, 1)
+			So(err, ShouldBeNil)
+			So(reqs, ShouldHaveLength, 1)
+			So(reqs[0].FormValue("n"), ShouldEqual, "1")
+			So(len(commits), ShouldEqual, 1)
+			So(commits[0].Author.Name, ShouldEqual, "Author 1")
+		})
+	})
+}
