eef5eef1ecc46c7bc8a11b58143372fe9928a601
smut <smut@google.com>
2017-12-21 16:05:06 -0800

    [Machine Database] Move rpcs into separate files
    Bug: 778865
    Change-Id: I0fcecccffd16364c42324d7cc287f188875d1b30

---
+++ machine-db/appengine/rpc/datacenters.go
@@ -0,0 +1,61 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+
+	"go.chromium.org/luci/machine-db/api/crimson/v1"
+	"go.chromium.org/luci/machine-db/appengine/database"
+)
+
+// GetDatacenters handles a request to retrieve datacenters.
+func (*Service) GetDatacenters(c context.Context, req *crimson.DatacentersRequest) (*crimson.DatacentersResponse, error) {
+	datacenters, err := getDatacenters(c, stringset.NewFromSlice(req.Names...))
+	if err != nil {
+		return nil, internalError(c, err)
+	}
+	return &crimson.DatacentersResponse{
+		Datacenters: datacenters,
+	}, nil
+}
+
+// getDatacenters returns a slice of datacenters in the database.
+func getDatacenters(c context.Context, names stringset.Set) ([]*crimson.Datacenter, error) {
+	db := database.Get(c)
+	rows, err := db.QueryContext(c, `
+		SELECT d.name, d.description
+		FROM datacenters d
+	`)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to fetch datacenters").Err()
+	}
+	defer rows.Close()
+
+	var datacenters []*crimson.Datacenter
+	for rows.Next() {
+		dc := &crimson.Datacenter{}
+		if err = rows.Scan(&dc.Name, &dc.Description); err != nil {
+			return nil, errors.Annotate(err, "failed to fetch datacenter").Err()
+		}
+		if matches(dc.Name, names) {
+			datacenters = append(datacenters, dc)
+		}
+	}
+	return datacenters, nil
+}
---
+++ machine-db/appengine/rpc/racks_test.go
@@ -0,0 +1,126 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"fmt"
+	"testing"
+
+	"golang.org/x/net/context"
+
+	"github.com/DATA-DOG/go-sqlmock"
+
+	"go.chromium.org/luci/common/data/stringset"
+
+	"go.chromium.org/luci/machine-db/api/crimson/v1"
+	"go.chromium.org/luci/machine-db/appengine/database"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestGetRacks(t *testing.T) {
+	Convey("getRacks", t, func() {
+		db, m, _ := sqlmock.New()
+		defer db.Close()
+		c := database.With(context.Background(), db)
+		selectStmt := `
+			^SELECT r.name, r.description, d.name
+			FROM racks r, datacenters d
+			WHERE r.datacenter_id = d.id$
+		`
+		columns := []string{"r.name", "r.description", "d.name"}
+		rows := sqlmock.NewRows(columns)
+
+		Convey("query failed", func() {
+			names := stringset.NewFromSlice("rack")
+			dcs := stringset.NewFromSlice("datacenter")
+			m.ExpectQuery(selectStmt).WillReturnError(fmt.Errorf("error"))
+			racks, err := getRacks(c, names, dcs)
+			So(err, ShouldErrLike, "failed to fetch racks")
+			So(racks, ShouldBeEmpty)
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("empty", func() {
+			names := stringset.NewFromSlice("rack")
+			dcs := stringset.NewFromSlice("datacenter")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			racks, err := getRacks(c, names, dcs)
+			So(err, ShouldBeNil)
+			So(racks, ShouldBeEmpty)
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("no matches", func() {
+			names := stringset.NewFromSlice("rack")
+			dcs := stringset.NewFromSlice("datacenter")
+			rows.AddRow("rack 1", "description 1", "datacenter 1")
+			rows.AddRow("rack 2", "description 2", "datacenter 2")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			racks, err := getRacks(c, names, dcs)
+			So(err, ShouldBeNil)
+			So(racks, ShouldBeEmpty)
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("matches", func() {
+			names := stringset.NewFromSlice("rack 2", "rack 3")
+			dcs := stringset.NewFromSlice("datacenter 2", "datacenter 3")
+			rows.AddRow("rack 1", "description 1", "datacenter 1")
+			rows.AddRow("rack 2", "description 2", "datacenter 2")
+			rows.AddRow("rack 3", "description 3", "datacenter 3")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			racks, err := getRacks(c, names, dcs)
+			So(err, ShouldBeNil)
+			So(racks, ShouldResemble, []*crimson.Rack{
+				{
+					Name:        "rack 2",
+					Description: "description 2",
+					Datacenter:  "datacenter 2",
+				},
+				{
+					Name:        "rack 3",
+					Description: "description 3",
+					Datacenter:  "datacenter 3",
+				},
+			})
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("ok", func() {
+			names := stringset.New(0)
+			dcs := stringset.New(0)
+			rows.AddRow("rack 1", "description 1", "datacenter 1")
+			rows.AddRow("rack 2", "description 2", "datacenter 2")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			racks, err := getRacks(c, names, dcs)
+			So(err, ShouldBeNil)
+			So(racks, ShouldResemble, []*crimson.Rack{
+				{
+					Name:        "rack 1",
+					Description: "description 1",
+					Datacenter:  "datacenter 1",
+				},
+				{
+					Name:        "rack 2",
+					Description: "description 2",
+					Datacenter:  "datacenter 2",
+				},
+			})
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+	})
+}
