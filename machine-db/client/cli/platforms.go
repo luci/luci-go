e7e8e68ecbea92fa1dcdb442e324db88c8561dd8
smut <smut@google.com>
2017-12-22 14:46:25 -0800

    [Machine Database] Add command to retrieve platforms
    Bug: 778865
    Change-Id: Id4d8e55b3b4fd645547f480f7b91bb7b595ae0c8
    Reviewed-on: https://chromium-review.googlesource.com/843482
    Commit-Queue: smut <smut@google.com>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

---
+++ machine-db/client/cli/platforms.go
@@ -0,0 +1,65 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cli
+
+import (
+	"fmt"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/maruel/subcommands"
+
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/flag/stringlistflag"
+
+	"go.chromium.org/luci/machine-db/api/crimson/v1"
+)
+
+// GetPlatformsCmd is the command to get platforms.
+type GetPlatformsCmd struct {
+	subcommands.CommandRunBase
+	names stringlistflag.Flag
+}
+
+// Run runs the command to get platforms.
+func (c *GetPlatformsCmd) Run(app subcommands.Application, args []string, env subcommands.Env) int {
+	ctx := cli.GetContext(app, c, env)
+	req := &crimson.PlatformsRequest{
+		Names: c.names,
+	}
+	client := getClient(ctx)
+	resp, err := client.GetPlatforms(ctx, req)
+	if err != nil {
+		errors.Log(ctx, err)
+		return 1
+	}
+	// TODO(smut): Format this response.
+	fmt.Print(proto.MarshalTextString(resp))
+	return 0
+}
+
+// getPlatformsCmd returns a command to get platforms.
+func getPlatformsCmd() *subcommands.Command {
+	return &subcommands.Command{
+		UsageLine: "get-platforms [-name <name>]...",
+		ShortDesc: "retrieves platforms",
+		LongDesc:  "Retrieves platforms matching the given names, or all platforms if names are omitted.",
+		CommandRun: func() subcommands.CommandRun {
+			cmd := &GetPlatformsCmd{}
+			cmd.Flags.Var(&cmd.names, "name", "Name of a platform to filter by. Can be specified multiple times.")
+			return cmd
+		},
+	}
+}
