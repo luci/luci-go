bd276d71f95f4e8d12b7660da041afe319de4684
smut <smut@google.com>
2017-12-21 16:05:55 -0800

    [Machine Database] Move rpcs into separate files
    Bug: 778865
    Change-Id: I0fcecccffd16364c42324d7cc287f188875d1b30

---
+++ machine-db/appengine/rpc/switches.go
@@ -0,0 +1,63 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+
+	"go.chromium.org/luci/machine-db/api/crimson/v1"
+	"go.chromium.org/luci/machine-db/appengine/database"
+)
+
+// GetSwitches handles a request to retrieve switches.
+func (*Service) GetSwitches(c context.Context, req *crimson.SwitchesRequest) (*crimson.SwitchesResponse, error) {
+	switches, err := getSwitches(c, stringset.NewFromSlice(req.Names...), stringset.NewFromSlice(req.Racks...), stringset.NewFromSlice(req.Datacenters...))
+	if err != nil {
+		return nil, internalError(c, err)
+	}
+	return &crimson.SwitchesResponse{
+		Switches: switches,
+	}, nil
+}
+
+// getSwitches returns a slice of switches in the database.
+func getSwitches(c context.Context, names, racks, datacenters stringset.Set) ([]*crimson.Switch, error) {
+	db := database.Get(c)
+	rows, err := db.QueryContext(c, `
+		SELECT s.name, s.description, s.ports, r.name, d.name
+		FROM switches s, racks r, datacenters d
+		WHERE s.rack_id = r.id
+			AND r.datacenter_id = d.id
+	`)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to fetch switches").Err()
+	}
+	defer rows.Close()
+
+	var switches []*crimson.Switch
+	for rows.Next() {
+		s := &crimson.Switch{}
+		if err = rows.Scan(&s.Name, &s.Description, &s.Ports, &s.Rack, &s.Datacenter); err != nil {
+			return nil, errors.Annotate(err, "failed to fetch switch").Err()
+		}
+		if matches(s.Name, names) && matches(s.Rack, racks) && matches(s.Datacenter, datacenters) {
+			switches = append(switches, s)
+		}
+	}
+	return switches, nil
+}
---
+++ machine-db/appengine/rpc/switches_test.go
@@ -0,0 +1,140 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"fmt"
+	"testing"
+
+	"golang.org/x/net/context"
+
+	"github.com/DATA-DOG/go-sqlmock"
+
+	"go.chromium.org/luci/common/data/stringset"
+
+	"go.chromium.org/luci/machine-db/api/crimson/v1"
+	"go.chromium.org/luci/machine-db/appengine/database"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestGetSwitches(t *testing.T) {
+	Convey("getSwitches", t, func() {
+		db, m, _ := sqlmock.New()
+		defer db.Close()
+		c := database.With(context.Background(), db)
+		selectStmt := `
+			^SELECT s.name, s.description, s.ports, r.name, d.name
+			FROM switches s, racks r, datacenters d
+			WHERE s.rack_id = r.id AND r.datacenter_id = d.id$
+		`
+		columns := []string{"s.name", "s.description", "s.ports", "r.name", "d.name"}
+		rows := sqlmock.NewRows(columns)
+
+		Convey("query failed", func() {
+			names := stringset.NewFromSlice("switch")
+			racks := stringset.NewFromSlice("rack")
+			dcs := stringset.NewFromSlice("datacenter")
+			m.ExpectQuery(selectStmt).WillReturnError(fmt.Errorf("error"))
+			switches, err := getSwitches(c, names, racks, dcs)
+			So(err, ShouldErrLike, "failed to fetch switches")
+			So(switches, ShouldBeEmpty)
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("empty", func() {
+			names := stringset.NewFromSlice("switch")
+			racks := stringset.NewFromSlice("rack")
+			dcs := stringset.NewFromSlice("datacenter")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			switches, err := getSwitches(c, names, racks, dcs)
+			So(err, ShouldBeNil)
+			So(switches, ShouldBeEmpty)
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("no matches", func() {
+			names := stringset.NewFromSlice("switch")
+			racks := stringset.NewFromSlice("rack")
+			dcs := stringset.NewFromSlice("datacenter")
+			rows.AddRow("switch 1", "description 1", 10, "rack 1", "datacenter 1")
+			rows.AddRow("switch 2", "description 2", 20, "rack 2", "datacenter 2")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			switches, err := getSwitches(c, names, racks, dcs)
+			So(err, ShouldBeNil)
+			So(switches, ShouldBeEmpty)
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("matches", func() {
+			names := stringset.NewFromSlice("switch 3", "switch 4")
+			racks := stringset.NewFromSlice("rack 3", "rack 4")
+			dcs := stringset.NewFromSlice("datacenter 2", "datacenter 3", "datacenter 4")
+			rows.AddRow("switch 1", "description 1", 10, "rack 1", "datacenter 1")
+			rows.AddRow("switch 2", "description 2", 20, "rack 2", "datacenter 2")
+			rows.AddRow("switch 3", "description 3", 30, "rack 3", "datacenter 3")
+			rows.AddRow("switch 4", "description 4", 40, "rack 4", "datacenter 4")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			switches, err := getSwitches(c, names, racks, dcs)
+			So(err, ShouldBeNil)
+			So(switches, ShouldResemble, []*crimson.Switch{
+				{
+					Name:        "switch 3",
+					Description: "description 3",
+					Ports:       30,
+					Rack:        "rack 3",
+					Datacenter:  "datacenter 3",
+				},
+				{
+					Name:        "switch 4",
+					Description: "description 4",
+					Ports:       40,
+					Rack:        "rack 4",
+					Datacenter:  "datacenter 4",
+				},
+			})
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("ok", func() {
+			names := stringset.New(0)
+			racks := stringset.New(0)
+			dcs := stringset.New(0)
+			rows.AddRow("switch 1", "description 1", 10, "rack 1", "datacenter 1")
+			rows.AddRow("switch 2", "description 2", 20, "rack 2", "datacenter 2")
+			m.ExpectQuery(selectStmt).WillReturnRows(rows)
+			switches, err := getSwitches(c, names, racks, dcs)
+			So(err, ShouldBeNil)
+			So(switches, ShouldResemble, []*crimson.Switch{
+				{
+					Name:        "switch 1",
+					Description: "description 1",
+					Ports:       10,
+					Rack:        "rack 1",
+					Datacenter:  "datacenter 1",
+				},
+				{
+					Name:        "switch 2",
+					Description: "description 2",
+					Ports:       20,
+					Rack:        "rack 2",
+					Datacenter:  "datacenter 2",
+				},
+			})
+			So(m.ExpectationsWereMet(), ShouldBeNil)
+		})
+	})
+}
