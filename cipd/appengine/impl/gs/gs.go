6f693b13ede59a3e92dfba3af3195f122af76683
Vadim Shtayura <vadimsh@chromium.org>
2018-01-17 20:00:41 -0800

    [cipd] Google Storage helper package.
    It contains some niche methods not exposed (and not needed) elsewhere, like
    a method for conditional copying, ReaderAt implementation and most importantly
    Resumable uploads initiation. None of the existing high level Google Storage
    wrappers provide them.
    
    R=iannucci@chromium.org, nodir@chromium.org
    BUG=801423
    
    Change-Id: I9bd4b8e788dc7cb88273a0f549280bc484c666b3

---
+++ cipd/appengine/impl/gs/gs.go
@@ -0,0 +1,314 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gs
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"sync"
+
+	"golang.org/x/net/context"
+	"golang.org/x/net/context/ctxhttp"
+	"google.golang.org/api/googleapi"
+	"google.golang.org/api/storage/v1"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/retry/transient"
+	"go.chromium.org/luci/server/auth"
+)
+
+// GoogleStorage is a wrapper over raw Google Cloud Storage JSON API.
+//
+// All paths are expected to be in format "/<bucket>/<object>", method would
+// panic otherwise. Use ValidatePath prior to calling GoogleStorage methods
+// if necessary.
+//
+// Errors returned by GoogleStorage are annotated with transient tag and with
+// HTTP status codes of corresponding Google Storage JSON API replies (if
+// available). Use StatusCode(err) to extract them.
+//
+// Logs all calls to the info log.
+type GoogleStorage struct {
+	c context.Context
+
+	o      sync.Once
+	err    error            // initialization error, if any
+	client *http.Client     // authenticating HTTP client
+	srv    *storage.Service // the raw Cloud Storage API service
+}
+
+// Get returns Google Storage JSON API wrapper.
+//
+// Its guts are lazily initializes on first use, to simplify error handling.
+//
+// All methods are bound to the given context, thus this object should not be
+// reused between requests.
+func Get(c context.Context) *GoogleStorage {
+	return &GoogleStorage{c: c}
+}
+
+// init lazily initializes GoogleStorage.
+func (gs *GoogleStorage) init() error {
+	gs.o.Do(func() {
+		tr, err := auth.GetRPCTransport(gs.c, auth.AsSelf, auth.WithScopes(storage.CloudPlatformScope))
+		if err != nil {
+			gs.err = errors.Annotate(err, "failed to get authenticating transport").
+				Tag(transient.Tag).Err()
+			return
+		}
+		gs.client = &http.Client{Transport: tr}
+		gs.srv, err = storage.New(gs.client)
+		if err != nil {
+			gs.err = errors.Annotate(err, "failed to construct storage.Service").Err()
+		}
+	})
+	return gs.err
+}
+
+// Exists checks whether given Google Storage file exists.
+func (gs *GoogleStorage) Exists(c context.Context, path string) (exists bool, err error) {
+	logging.Infof(c, "gs: Exists(path=%q)", path)
+	if err := gs.init(); err != nil {
+		return false, err
+	}
+
+	call := gs.srv.Objects.Get(SplitPath(path)).Context(c)
+
+	switch err := withRetry(c, func() error { _, err = call.Do(); return err }); {
+	case err == nil:
+		return true, nil
+	case StatusCode(err) == 404:
+		return false, nil
+	default:
+		return false, err
+	}
+}
+
+// Copy copies a file at 'src' to 'dst'.
+//
+// Applies ifSourceGenerationMatch and ifGenerationMatch preconditions if
+// srcGen or dstGen are non-negative.
+func (gs *GoogleStorage) Copy(c context.Context, src, dst string, srcGen, dstGen int64) error {
+	logging.Infof(c, "gs: Copy(src=%q, dst=%q, srcGen=%d, dstGen=%d)", src, dst, srcGen, dstGen)
+	if err := gs.init(); err != nil {
+		return err
+	}
+
+	srcBucket, srcPath := SplitPath(src)
+	dstBucket, dstPath := SplitPath(dst)
+
+	call := gs.srv.Objects.Copy(srcBucket, srcPath, dstBucket, dstPath, nil).Context(c)
+	if srcGen >= 0 {
+		call.IfSourceGenerationMatch(srcGen)
+	}
+	if dstGen >= 0 {
+		call.IfGenerationMatch(dstGen)
+	}
+
+	return withRetry(c, func() error { _, err := call.Do(); return err })
+}
+
+// Delete removes a file.
+//
+// Missing file is not an error.
+func (gs *GoogleStorage) Delete(c context.Context, path string) error {
+	logging.Infof(c, "gs: Delete(path=%q)", path)
+	if err := gs.init(); err != nil {
+		return err
+	}
+
+	call := gs.srv.Objects.Delete(SplitPath(path)).Context(c)
+
+	err := withRetry(c, func() error { return call.Do() })
+	if err == nil || StatusCode(err) == 404 {
+		return nil
+	}
+	return err
+}
+
+// Publish implements conditional copy operation with some caveats, making it
+// useful for moving upload files from temporary storage area after they have
+// been verified.
+//
+// 'src' will be copied to 'dst' only if source generation number matches
+// 'srcGen'. If 'srcGen' is negative or zero, the generation check is not
+// performed. Also assumes 'dst' is ether missing, or already contains data
+// matching 'src' (so 'dst' should usually be a content-addressed path). This
+// allows Publish operation to be safely retried even if it failed midway
+// before.
+//
+// Note that it keeps 'src' intact.
+func (gs *GoogleStorage) Publish(c context.Context, src, dst string, srcGen int64) error {
+	switch err := gs.Copy(c, src, dst, srcGen, -1); {
+	case StatusCode(err) == http.StatusNotFound:
+		// 'src' is missing. Maybe we attempt to publish already and this is a
+		// retry. If so, 'dst' should exist already, and we assume it matches 'src'
+		// per the method contract. Note that there's no efficient way to check
+		// this.
+		//
+		// If 'dst' is also missing, then we are not retying failed move (it would
+		// have left either 'src' or 'dst' or both present), and 'src' is genuinely
+		// missing.
+		switch exists, dstErr := gs.Exists(c, dst); {
+		case dstErr != nil:
+			return errors.Annotate(dstErr, "failed to check the destination object presence").Err()
+		case !exists:
+			return errors.Annotate(err, "the source object is missing").Err()
+		}
+		return nil // 'dst' exists, Publish is considered successful
+	case StatusCode(err) == http.StatusPreconditionFailed:
+		return errors.Annotate(err, "the source object has unexpected generation number").Err()
+	case err != nil:
+		return errors.Annotate(err, "failed to copy the object").Err()
+	}
+	return nil
+}
+
+// StartUpload opens a new resumable upload session to a given path.
+//
+// Returns URL to use in Resumable Upload protocol. It contains uploadId that
+// acts as an authentication token, treat the URL as a secret.
+func (gs *GoogleStorage) StartUpload(c context.Context, path string) (uploadURL string, err error) {
+	logging.Infof(c, "gs: StartUpload(path=%q)", path)
+	if err := gs.init(); err != nil {
+		return "", err
+	}
+
+	// Unfortunately gs.srv.Objects.Insert doesn't allow to start a resumable
+	// upload session here, but finish it elsewhere. Also it gives no access to
+	// the upload URL and demands an io.Reader with all the data right away. So we
+	// have to resort to the rawest API possible :(
+	//
+	// See:
+	//   https://cloud.google.com/storage/docs/json_api/v1/objects/insert
+	//   https://cloud.google.com/storage/docs/json_api/v1/how-tos/resumable-upload
+	bucket, name := SplitPath(path)
+	u := &url.URL{
+		Scheme: "https",
+		Host:   "www.googleapis.com",
+		Path:   "/upload/storage/v1/b/{bucket}/o",
+		RawQuery: (url.Values{
+			"name":       {name},
+			"uploadType": {"resumable"},
+		}).Encode(),
+	}
+	googleapi.Expand(u, map[string]string{"bucket": bucket})
+
+	err = withRetry(c, func() error {
+		req, _ := http.NewRequest("POST", u.String(), nil)
+		resp, err := ctxhttp.Do(c, gs.client, req)
+		if err != nil {
+			return err
+		}
+		defer googleapi.CloseBody(resp)
+		if err := googleapi.CheckResponse(resp); err != nil {
+			return err
+		}
+		uploadURL = resp.Header.Get("Location")
+		return nil
+	})
+
+	if err != nil {
+		return "", errors.Annotate(err, "failed to open the resumable upload session").Err()
+	}
+
+	return uploadURL, nil
+}
+
+// Reader returns io.ReaderAt implementation to read contents of a file at
+// a specific generation (if 'gen' is positive) or at the current live one.
+//
+// The reader is bound to the given context.
+func (gs *GoogleStorage) Reader(c context.Context, path string, gen int64) (*Reader, error) {
+	logging.Infof(c, "gs: Reader(path=%q, gen=%d)", path, gen)
+	if err := gs.init(); err != nil {
+		return nil, err
+	}
+
+	call := gs.srv.Objects.Get(SplitPath(path)).Context(c)
+	if gen > 0 {
+		call.Generation(gen)
+	}
+
+	var obj *storage.Object
+	var err error
+	err = withRetry(c, func() error { obj, err = call.Do(); return err })
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to grab the object size and generation").Err()
+	}
+
+	return &Reader{c, gs, path, int64(obj.Size), obj.Generation}, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Reader can read chunks of Google Storage files.
+//
+// Use GoogleStorage.Reader to get the reader.
+type Reader struct {
+	ctx  context.Context
+	gs   *GoogleStorage
+	path string
+	size int64
+	gen  int64
+}
+
+// Size is the total file size.
+func (r *Reader) Size() int64 { return r.size }
+
+// Generation is generation number of the content we are reading.
+func (r *Reader) Generation() int64 { return r.gen }
+
+// ReadAt reads len(p) bytes into p starting at offset off in the underlying
+// input source. It returns the number of bytes read (0 <= n <= len(p)) and any
+// error encountered.
+func (r *Reader) ReadAt(p []byte, off int64) (n int, err error) {
+	toRead := int64(len(p))
+	if off+toRead > r.size {
+		toRead = r.size - off
+	}
+	if toRead == 0 {
+		return 0, io.EOF
+	}
+
+	logging.Infof(r.ctx, "gs: ReadAt(path=%q, offset=%d, length=%d, gen=%d)", r.path, off, toRead, r.gen)
+	if err := r.gs.init(); err != nil {
+		return 0, err
+	}
+
+	call := r.gs.srv.Objects.Get(SplitPath(r.path)).Context(r.ctx).Generation(r.gen)
+	call.Header().Set("Range", fmt.Sprintf("bytes=%d-%d", off, off+toRead-1))
+
+	err = withRetry(r.ctx, func() error {
+		resp, err := call.Download()
+		if err != nil {
+			return err
+		}
+		defer googleapi.CloseBody(resp)
+		n, err = io.ReadAtLeast(resp.Body, p, int(toRead))
+		if err != nil {
+			return errors.Annotate(err, "failed to read the response").Tag(transient.Tag).Err()
+		}
+		return nil
+	})
+
+	if err == nil && off+toRead == r.size {
+		err = io.EOF
+	}
+	return
+}
