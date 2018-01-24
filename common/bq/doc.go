be09ce4c06fb4c6f59ce5c03a4847985fa0f0849
Katie Thomas <katthomas@google.com>
2018-01-24 10:55:19 -0800

    [luci/common/bq] add doc.go
    Bug: 798430
    Change-Id: I5916bf13b2d4255aa6263df044d4543edacb39b8
    Reviewed-on: https://chromium-review.googlesource.com/878503
    Commit-Queue: Katie Thomas <katthomas@google.com>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

---
+++ common/bq/doc.go
@@ -0,0 +1,45 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package bq is a library for working with BigQuery.
+//
+// Limits
+//
+// Please see BigQuery docs:
+// https://cloud.google.com/bigquery/quotas#streaminginserts for the most
+// updated limits for streaming inserts. It is expected that the client is
+// responsible for ensuring their usage will not exceed these limits through
+// bq usage. A note on maximum rows per request: Put() batches rows per request,
+// ensuring that no more than 10,000 rows are sent per request, and allowing for
+// custom batch size. BigQuery recommends using 500 as a practical limit (so we
+// use this as a default), and experimenting with your specific schema and data
+// sizes to determine the batch size with the ideal balance of throughput and
+// latency for your use case.
+//
+// Authentication
+//
+// Authentication for the Cloud projects happens
+// during client creation: https://godoc.org/cloud.google.com/go#pkg-examples.
+// What form this takes depends on the application.
+//
+// Monitoring
+//
+// You can use tsmon (https://godoc.org/go.chromium.org/luci/common/tsmon) to
+// track upload latency and errors.
+//
+// Uploader has a public field, UploadsMetricName, which will automatically
+// create a counter metric to track successes and failures. It does not
+// automatically create a metric for tracking latency (a cumulative distribution
+// metric is recommended), but could be modified to do so.
+package bq
