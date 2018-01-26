8a3df62e98490c591288f2f122a118d7cc140993
smut <smut@google.com>
2018-01-25 17:20:59 -0800

    [Machine Database] Add commands to create, retrieve physical hosts
    Bug: 778865
    Change-Id: I95552aa5954f66a2cd9dbe64881a73c467048289

--- machine-db/client/cli/cli.go
+++ machine-db/client/cli/cli.go
@@ -105,6 +105,10 @@ func New(params *Parameters) *cli.Application {
 			// Machines.
 			addMachineCmd(),
 			getMachinesCmd(),
+
+			// Physical hosts.
+			addPhysicalHostCmd(),
+			getPhysicalHostsCmd(),
 		},
 	}
 }
