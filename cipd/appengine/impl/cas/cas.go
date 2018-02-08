f3606b737fc5164bfb287deeec19a99ee092b3b0
Vadim Shtayura <vadimsh@chromium.org>
2018-02-07 16:04:12 -0800

    [cipd] Implement BeginUpload RPC method.
    It initiates a Google Storage resumable upload session and returns it to the
    caller.
    
    R=nodir@chromium.org, iannucci@chromium.org
    BUG=801423
    
    Change-Id: I501caaf96e66d6f67ff10d97674bf3cca52dec0a

--- cipd/appengine/impl/cas/cas.go
+++ cipd/appengine/impl/cas/cas.go
@@ -15,16 +15,21 @@
 package cas
 
 import (
+	"fmt"
 	"strings"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"go.chromium.org/gae/service/datastore"
+	"go.chromium.org/luci/common/clock"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/grpc/grpcutil"
+	"go.chromium.org/luci/server/auth"
 
 	api "go.chromium.org/luci/cipd/api/cipd/v1"
+	"go.chromium.org/luci/cipd/appengine/impl/cas/upload"
 	"go.chromium.org/luci/cipd/appengine/impl/common"
 	"go.chromium.org/luci/cipd/appengine/impl/gs"
 	"go.chromium.org/luci/cipd/appengine/impl/settings"
@@ -36,6 +41,7 @@ import (
 // done.
 func Internal() api.StorageServer {
 	return &storageImpl{
+		getGS:        gs.Get,
 		settings:     settings.Get,
 		getSignedURL: getSignedURL,
 	}
@@ -46,6 +52,7 @@ func Internal() api.StorageServer {
 // Doesn't do any ACL checks.
 type storageImpl struct {
 	// Mocking points for tests. See Internal() for real implementations.
+	getGS        func(c context.Context) gs.GoogleStorage
 	settings     func(c context.Context) (*settings.Settings, error)
 	getSignedURL func(c context.Context, gsPath, filename string, signer signerFactory, gs gs.GoogleStorage) (string, error)
 }
@@ -77,7 +84,7 @@ func (s *storageImpl) GetObjectURL(c context.Context, r *api.GetObjectURLRequest
 		}
 	}
 
-	url, err := s.getSignedURL(c, cfg.ObjectPath(r.Object), r.DownloadFilename, sigFactory, gs.Get(c))
+	url, err := s.getSignedURL(c, cfg.ObjectPath(r.Object), r.DownloadFilename, sigFactory, s.getGS(c))
 	if err != nil {
 		return nil, errors.Annotate(err, "failed to get signed URL").Err()
 	}
@@ -107,11 +114,77 @@ func (s *storageImpl) BeginUpload(c context.Context, r *api.BeginUploadRequest) 
 		hashAlgo = r.HashAlgo
 	}
 
-	// TODO(vadimsh): Implement.
-	_ = hashAlgo
-	_ = hexDigest
+	gs := s.getGS(c)
+	cfg, err := s.settings(c)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, status.Errorf(codes.Unimplemented, "not implemented")
+	// If we know the name of the object being uploaded, check we don't have it
+	// in the store already to avoid wasting time uploading it. Note that it is
+	// always fine to "overwrite" objects, so if the object appears while the
+	// client is still uploading, nothing catastrophic happens, just some time
+	// gets wasted.
+	if r.Object != nil {
+		switch yes, err := gs.Exists(c, cfg.ObjectPath(r.Object)); {
+		case err != nil:
+			return nil, errors.Annotate(err, "failed to check the object's presence").
+				Tag(grpcutil.InternalTag).Err()
+		case yes:
+			return nil, status.Errorf(codes.AlreadyExists, "the object is already in the store")
+		}
+	}
+
+	// Grab new unique ID for the upload operation, it is used in GS filenames.
+	opID, err := upload.NewOpID(c)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to allocate upload operation ID").
+			Tag(grpcutil.InternalTag).Err()
+	}
+
+	// Attach HMAC to it, to be returned to the client to make sure clients can't
+	// access sessions they don't own. Do it early, to avoid storing stuff in
+	// the datastore and GS if WrapOpID fails.
+	caller := auth.CurrentIdentity(c)
+	wrappedOpID, err := upload.WrapOpID(c, opID, caller)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to HMAC-tag upload operation ID").
+			Tag(grpcutil.InternalTag).Err()
+	}
+
+	// GS path to which the client will upload the data. Prefix it with the
+	// current timestamp to make bucket listing sorted by time.
+	now := clock.Now(c)
+	tempGSPath := fmt.Sprintf("%s/%d_%d", cfg.TempGSPath, now.Unix(), opID)
+
+	// Initiate Google Storage resumable upload session to this path. The returned
+	// URL can be accessed unauthenticated. The client will use it directly to
+	// upload the data. If left open, the GS session eventually expires, so it's
+	// not big deal if we loose it (e.g due to a crash before returning).
+	uploadURL, err := gs.StartUpload(c, tempGSPath)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to start resumable upload").
+			Tag(grpcutil.InternalTag).Err()
+	}
+
+	// Save the operation. It is accessed in FinishUpload.
+	op := upload.Operation{
+		ID:         opID,
+		Status:     api.UploadStatus_UPLOADING,
+		TempGSPath: tempGSPath,
+		UploadURL:  uploadURL,
+		HashAlgo:   hashAlgo,
+		HexDigest:  hexDigest, // may be empty, means the server should calculate it
+		CreatedBy:  caller,
+		CreatedTS:  now.UTC(),
+		UpdatedTS:  now.UTC(),
+	}
+	if err = datastore.Put(c, &op); err != nil {
+		return nil, errors.Annotate(err, "failed to persist upload operation").
+			Tag(grpcutil.InternalTag).Err()
+	}
+
+	return op.ToProto(wrappedOpID), nil
 }
 
 // FinishUpload implements the corresponding RPC method, see the proto doc.
