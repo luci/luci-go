b2250d6ed1dc5e2170d3c4a60529144da859cce8
Ryan Tseng <hinoka@google.com>
2018-02-14 18:47:01 -0800

    gkelogger - A simple JSON logger for GKE
    This outputs logs to stdout in a format that the GKE (fluentd) + Stackdriver
    stack expects.
    
    Bug: 812474
    Change-Id: I8d4f679cf00abbe8a5fcd38d6f496b86b6cd19bd

---
+++ common/logging/gkelogger/doc.go
@@ -0,0 +1,30 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package gkelogger is a simple logger that outputs all log entries as single-
+// line JSON objects to stdout in a format that GKE + Stackdriver understands.
+//
+// Default usage (logging to stderr):
+//
+//   import (
+//     "go.chromium.org/luci/common/logging"
+//     "go.chromium.org/luci/common/logging/gkelogger"
+//   )
+//
+//   ...
+//
+//   ctx := context.Background()
+//   ctx = gkelogger.Use(ctx)
+//   logging.Infof(ctx, "Hello %s", "world")
+package gkelogger
