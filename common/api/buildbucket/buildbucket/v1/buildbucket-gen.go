0bc991eedcb9ccd0b23ef586c5e88b8e0b2b50af
smut <smut@google.com>
2018-02-16 14:59:07 -0800

    Add machine_provider 'catalog' API client; regenerate other API clients
    Bug: 813318
    Change-Id: Ic576ab6461756ce2859721b07294faf87ea3ff88

--- common/api/buildbucket/buildbucket/v1/buildbucket-gen.go
+++ common/api/buildbucket/buildbucket/v1/buildbucket-gen.go
@@ -1,4 +1,4 @@
-// Copyright 2017 The LUCI Authors.
+// Copyright 2018 The LUCI Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -328,6 +328,8 @@ type ApiCommonBuildMessage struct {
 
 	RetryOf int64 `json:"retry_of,omitempty,string"`
 
+	ServiceAccount string `json:"service_account,omitempty"`
+
 	StartedTs int64 `json:"started_ts,omitempty,string"`
 
 	// Possible values:
@@ -646,52 +648,6 @@ func (s *ApiLeaseRequestBodyMessage) MarshalJSON() ([]byte, error) {
 	type NoMethod ApiLeaseRequestBodyMessage
 	raw := NoMethod(*s)
 	return gensupport.MarshalJSON(raw, s.ForceSendFields, s.NullFields)
-}
-
-type ApiLongestPendingTimeResponse struct {
-	Error *ApiErrorMessage `json:"error,omitempty"`
-
-	LongestPendingTimeSec float64 `json:"longest_pending_time_sec,omitempty"`
-
-	// ServerResponse contains the HTTP response code and headers from the
-	// server.
-	googleapi.ServerResponse `json:"-"`
-
-	// ForceSendFields is a list of field names (e.g. "Error") to
-	// unconditionally include in API requests. By default, fields with
-	// empty values are omitted from API requests. However, any non-pointer,
-	// non-interface field appearing in ForceSendFields will be sent to the
-	// server regardless of whether the field is empty or not. This may be
-	// used to include empty fields in Patch requests.
-	ForceSendFields []string `json:"-"`
-
-	// NullFields is a list of field names (e.g. "Error") to include in API
-	// requests with the JSON null value. By default, fields with empty
-	// values are omitted from API requests. However, any field with an
-	// empty value appearing in NullFields will be sent to the server as
-	// null. It is an error if a field in this list has a non-empty value.
-	// This may be used to include null fields in Patch requests.
-	NullFields []string `json:"-"`
-}
-
-func (s *ApiLongestPendingTimeResponse) MarshalJSON() ([]byte, error) {
-	type NoMethod ApiLongestPendingTimeResponse
-	raw := NoMethod(*s)
-	return gensupport.MarshalJSON(raw, s.ForceSendFields, s.NullFields)
-}
-
-func (s *ApiLongestPendingTimeResponse) UnmarshalJSON(data []byte) error {
-	type NoMethod ApiLongestPendingTimeResponse
-	var s1 struct {
-		LongestPendingTimeSec gensupport.JSONFloat64 `json:"longest_pending_time_sec"`
-		*NoMethod
-	}
-	s1.NoMethod = (*NoMethod)(s)
-	if err := json.Unmarshal(data, &s1); err != nil {
-		return err
-	}
-	s.LongestPendingTimeSec = float64(s1.LongestPendingTimeSec)
-	return nil
 }
 
 type ApiPauseResponse struct {
@@ -2286,146 +2242,6 @@ func (c *LeaseCall) Do(opts ...googleapi.CallOption) (*ApiBuildResponseMessage, 
 	//   },
 	//   "response": {
 	//     "$ref": "ApiBuildResponseMessage"
-	//   },
-	//   "scopes": [
-	//     "https://www.googleapis.com/auth/userinfo.email"
-	//   ]
-	// }
-
-}
-
-// method id "buildbucket.longest_pending_time":
-
-type LongestPendingTimeCall struct {
-	s            *Service
-	urlParams_   gensupport.URLParams
-	ifNoneMatch_ string
-	ctx_         context.Context
-	header_      http.Header
-}
-
-// LongestPendingTime: Returns longest pending time among all SCHEDULED
-// builds of a builder.
-func (s *Service) LongestPendingTime(bucket string, builder string) *LongestPendingTimeCall {
-	c := &LongestPendingTimeCall{s: s, urlParams_: make(gensupport.URLParams)}
-	c.urlParams_.Set("bucket", bucket)
-	c.urlParams_.Set("builder", builder)
-	return c
-}
-
-// Fields allows partial responses to be retrieved. See
-// https://developers.google.com/gdata/docs/2.0/basics#PartialResponse
-// for more information.
-func (c *LongestPendingTimeCall) Fields(s ...googleapi.Field) *LongestPendingTimeCall {
-	c.urlParams_.Set("fields", googleapi.CombineFields(s))
-	return c
-}
-
-// IfNoneMatch sets the optional parameter which makes the operation
-// fail if the object's ETag matches the given value. This is useful for
-// getting updates only after the object has changed since the last
-// request. Use googleapi.IsNotModified to check whether the response
-// error from Do is the result of In-None-Match.
-func (c *LongestPendingTimeCall) IfNoneMatch(entityTag string) *LongestPendingTimeCall {
-	c.ifNoneMatch_ = entityTag
-	return c
-}
-
-// Context sets the context to be used in this call's Do method. Any
-// pending HTTP request will be aborted if the provided context is
-// canceled.
-func (c *LongestPendingTimeCall) Context(ctx context.Context) *LongestPendingTimeCall {
-	c.ctx_ = ctx
-	return c
-}
-
-// Header returns an http.Header that can be modified by the caller to
-// add HTTP headers to the request.
-func (c *LongestPendingTimeCall) Header() http.Header {
-	if c.header_ == nil {
-		c.header_ = make(http.Header)
-	}
-	return c.header_
-}
-
-func (c *LongestPendingTimeCall) doRequest(alt string) (*http.Response, error) {
-	reqHeaders := make(http.Header)
-	for k, v := range c.header_ {
-		reqHeaders[k] = v
-	}
-	reqHeaders.Set("User-Agent", c.s.userAgent())
-	if c.ifNoneMatch_ != "" {
-		reqHeaders.Set("If-None-Match", c.ifNoneMatch_)
-	}
-	var body io.Reader = nil
-	c.urlParams_.Set("alt", alt)
-	urls := googleapi.ResolveRelative(c.s.BasePath, "metrics/longest-pending-time")
-	urls += "?" + c.urlParams_.Encode()
-	req, _ := http.NewRequest("GET", urls, body)
-	req.Header = reqHeaders
-	return gensupport.SendRequest(c.ctx_, c.s.client, req)
-}
-
-// Do executes the "buildbucket.longest_pending_time" call.
-// Exactly one of *ApiLongestPendingTimeResponse or error will be
-// non-nil. Any non-2xx status code is an error. Response headers are in
-// either *ApiLongestPendingTimeResponse.ServerResponse.Header or (if a
-// response was returned at all) in error.(*googleapi.Error).Header. Use
-// googleapi.IsNotModified to check whether the returned error was
-// because http.StatusNotModified was returned.
-func (c *LongestPendingTimeCall) Do(opts ...googleapi.CallOption) (*ApiLongestPendingTimeResponse, error) {
-	gensupport.SetOptions(c.urlParams_, opts...)
-	res, err := c.doRequest("json")
-	if res != nil && res.StatusCode == http.StatusNotModified {
-		if res.Body != nil {
-			res.Body.Close()
-		}
-		return nil, &googleapi.Error{
-			Code:   res.StatusCode,
-			Header: res.Header,
-		}
-	}
-	if err != nil {
-		return nil, err
-	}
-	defer googleapi.CloseBody(res)
-	if err := googleapi.CheckResponse(res); err != nil {
-		return nil, err
-	}
-	ret := &ApiLongestPendingTimeResponse{
-		ServerResponse: googleapi.ServerResponse{
-			Header:         res.Header,
-			HTTPStatusCode: res.StatusCode,
-		},
-	}
-	target := &ret
-	if err := gensupport.DecodeResponse(target, res); err != nil {
-		return nil, err
-	}
-	return ret, nil
-	// {
-	//   "description": "Returns longest pending time among all SCHEDULED builds of a builder.",
-	//   "httpMethod": "GET",
-	//   "id": "buildbucket.longest_pending_time",
-	//   "parameterOrder": [
-	//     "bucket",
-	//     "builder"
-	//   ],
-	//   "parameters": {
-	//     "bucket": {
-	//       "location": "query",
-	//       "required": true,
-	//       "type": "string"
-	//     },
-	//     "builder": {
-	//       "location": "query",
-	//       "required": true,
-	//       "type": "string"
-	//     }
-	//   },
-	//   "path": "metrics/longest-pending-time",
-	//   "response": {
-	//     "$ref": "ApiLongestPendingTimeResponse"
 	//   },
 	//   "scopes": [
 	//     "https://www.googleapis.com/auth/userinfo.email"
