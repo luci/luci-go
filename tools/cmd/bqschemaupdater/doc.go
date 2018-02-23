b9c727724a42db5f6c5c0c5302feb57b1a696b87
Nodir Turakulov <nodir@google.com>
2018-02-23 14:15:07 -0800

    [bqschemaupdater] refine doc
    Refine doc explaining proto->bq schema mapping.
    
    TBR=katthomas@google.com
    Bug: 788034
    Change-Id: I04e264ff51244bd90e236e7d43f1dcdf239b7016
    Reviewed-on: https://chromium-review.googlesource.com/935663
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

--- tools/cmd/bqschemaupdater/doc.go
+++ tools/cmd/bqschemaupdater/doc.go
@@ -5,17 +5,17 @@
 // Command bqschemaupdater accepts location and schema of a BigQuery table and
 // creates or updates the table.
 //
-// When converting a proto message to BigQuery schema:
+// When converting a proto message to BigQuery schema, in the order of
+// precedence:
 //
-//   - each message field becomes a BigQuery field
+//   - one message field becomes at most one BigQuery field
 //   - if a field has leading comments, common indentation is trimmed
 //     and the result becomes the BigQuery field description
 //   - if a field is of enum type, the BigQuery type is string
 //     and valid values are appended to the BigQuery field description
 //   - if a field is google.protobuf.Timestamp, the BigQuery type is TIMESTAMP
 //   - if a field is google.protobuf.Struct, is is persisted as a JSONPB string.
-//   - if a field is of message type without subfields of supported types,
-//     it is ignored.
 //   - if a field is of message type, the BigQuery type is RECORD
-//     with schema corresponding to the proto field type. Recursively.
+//     with schema corresponding to the proto field type, recursively.
+//     However, if the resulting RECORD schema is empty, the field is omitted.
 package main
