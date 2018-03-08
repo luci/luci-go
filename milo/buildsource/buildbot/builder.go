11f178f86f55cb1aed2c962724360b6ca3c84b4d
Ryan Tseng <hinoka@google.com>
2018-03-08 13:34:35 -0800

    Refactor BuildSummary to more closely resemble MiloBuild
    That way BuildSummary can be more cleanly derived directly from MiloBuild
    
    This is in preparation for the buildbucket build refactor.
    
    Of note:
    * Interval timing standarized to ui.NewInterval().
    * Added pending interval to builds, to match BuildSummary
    * Made build labels links, to match BuildSummary
    
    Bug: 774345
    Change-Id: I1cc73ebf49906b3131e4a771bc477457316ac577

--- milo/buildsource/buildbot/builder.go
+++ milo/buildsource/buildbot/builder.go
@@ -78,24 +78,19 @@ func mergeText(text []string) []string {
 	return result
 }
 
-func getBuildSummary(b *buildbot.Build, linkParams url.Values) *ui.BuildSummary {
-	linkURL := fmt.Sprintf("%d", b.Number)
+// getBuildSummary takes a buildbot build a returns the ui.MiloBuild rendering
+// of the build.  It is similar to renderBuild, but does not process steps or
+// properties.
+func getBuildSummary(c context.Context, b *buildbot.Build, linkParams url.Values) *ui.MiloBuild {
+	mb := &ui.MiloBuild{
+		Trigger: sourcestamp(c, b),
+		Summary: summary(c, b),
+		Blame:   blame(b),
+	}
 	if len(linkParams) > 0 {
-		linkURL += "?" + linkParams.Encode()
+		mb.Summary.Label.URL += "?" + linkParams.Encode()
 	}
-	return &ui.BuildSummary{
-		Link: ui.NewLink(fmt.Sprintf("#%d", b.Number), linkURL,
-			fmt.Sprintf("build number %d on builder %s", b.Number, b.Buildername)),
-		Status: b.Status(),
-		ExecutionTime: ui.Interval{
-			Started:  b.Times.Start.Time,
-			Finished: b.Times.Finish.Time,
-			Duration: b.Times.Duration(),
-		},
-		Text:     mergeText(b.Text),
-		Blame:    blame(b),
-		Revision: b.Sourcestamp.Revision,
-	}
+	return mb
 }
 
 func summarizeSlavePool(
@@ -170,17 +165,14 @@ func GetBuilder(c context.Context, masterName, builderName string, limit int, cu
 	}
 
 	// Extract pending builds out of the master.
-	result.PendingBuilds = make([]*ui.BuildSummary, len(builder.PendingBuildStates))
+	result.PendingBuilds = make([]*ui.MiloBuild, len(builder.PendingBuildStates))
 	result.PendingBuildNum = builder.PendingBuilds
 	logging.Debugf(c, "Number of pending builds: %d", len(builder.PendingBuildStates))
 	for i, pb := range builder.PendingBuildStates {
 		start := time.Unix(int64(pb.SubmittedAt), 0).UTC()
-		result.PendingBuilds[i] = &ui.BuildSummary{
-			PendingTime: ui.Interval{
-				Started:  start,
-				Duration: clock.Now(c).UTC().Sub(start),
-			},
-			Blame: make([]*ui.Commit, len(pb.Source.Changes)),
+		result.PendingBuilds[i] = &ui.MiloBuild{
+			Summary: ui.BuildComponent{PendingTime: ui.NewInterval(c, start, time.Time{})},
+			Blame:   make([]*ui.Commit, len(pb.Source.Changes)),
 		}
 		for j, cm := range pb.Source.Changes {
 			result.PendingBuilds[i].Blame[j] = &ui.Commit{
@@ -221,9 +213,9 @@ func GetBuilder(c context.Context, masterName, builderName string, limit int, cu
 			}
 			result.NextCursor = res.NextCursor
 			result.PrevCursor = res.PrevCursor
-			result.FinishedBuilds = make([]*ui.BuildSummary, len(res.Builds))
+			result.FinishedBuilds = make([]*ui.MiloBuild, len(res.Builds))
 			for i, b := range res.Builds {
-				result.FinishedBuilds[i] = getBuildSummary(b, linkParams)
+				result.FinishedBuilds[i] = getBuildSummary(c, b, linkParams)
 			}
 			return err
 		}
@@ -234,10 +226,10 @@ func GetBuilder(c context.Context, masterName, builderName string, limit int, cu
 			if err != nil {
 				return err
 			}
-			result.CurrentBuilds = make([]*ui.BuildSummary, len(res.Builds))
+			result.CurrentBuilds = make([]*ui.MiloBuild, len(res.Builds))
 			for i, b := range res.Builds {
 				// currentBuilds is presented in reversed order, so flip it
-				result.CurrentBuilds[len(res.Builds)-i-1] = getBuildSummary(b, linkParams)
+				result.CurrentBuilds[len(res.Builds)-i-1] = getBuildSummary(c, b, linkParams)
 			}
 			return err
 		}
