luci/luci-go

commit 7ed42e5cd8886a738295152f9840f04d6a6eb17b

CIPD: add option to preserve mtime and writable bit
BUG=803158
R=vadimsh@chromium.org

Change-Id: Ib6921d922ccb0855e9a66e6bb3ea5f5e1071d501
Reviewed-on: https://chromium-review.googlesource.com/920751
Commit-Queue: Sergey Berezin <sergeyberezin@chromium.org>
Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

cipd/client/cipd/local/builder.go
@@ -35,6 +35,8 @@ import (
 
 // BuildInstanceOptions defines options for BuildInstance function.
 type BuildInstanceOptions struct {
+	ScanOptions
+
 	// Input is a list of files to add to the package.
 	Input []File
 
@@ -97,12 +99,12 @@ func BuildInstance(ctx context.Context, opts BuildInstanceOptions) error {
 	}
 
 	// Write the final zip file.
-	return zipInputFiles(ctx, files, opts.Output, opts.CompressionLevel)
+	return zipInputFiles(ctx, files, opts.Output, opts.CompressionLevel, opts.PreserveWritable)
 }
 
 // zipInputFiles deterministically builds a zip archive out of input files and
 // writes it to the writer. Files are written in the order given.
-func zipInputFiles(ctx context.Context, files []File, w io.Writer, level int) error {
+func zipInputFiles(ctx context.Context, files []File, w io.Writer, level int, preserveWritable bool) error {
 	logging.Infof(ctx, "About to zip %d files with compression level %d", len(files), level)
 
 	writer := zip.NewWriter(w)
@@ -129,8 +131,9 @@ func zipInputFiles(ctx context.Context, files []File, w io.Writer, level int) er
 			return err
 		}
 
-		// Intentionally do not add timestamp or file mode to make zip archive
-		// deterministic. See also zip.FileInfoHeader() implementation.
+		// Intentionally do not add file mode to make zip archive
+		// deterministic. Timestamps sometimes need to be preserved, but normally
+		// are zero valued. See also zip.FileInfoHeader() implementation.
 		fh := zip.FileHeader{
 			Name:   in.Name(),
 			Method: zip.Deflate,
@@ -139,14 +142,22 @@ func zipInputFiles(ctx context.Context, files []File, w io.Writer, level int) er
 			fh.Method = zip.Store
 		}
 
-		mode := os.FileMode(0600)
+		mode := os.FileMode(0400)
 		if in.Executable() {
 			mode |= 0100
+		}
+		if in.Writable() {
+			mode |= 0200
 		}
 		if in.Symlink() {
 			mode |= os.ModeSymlink
 		}
 		fh.SetMode(mode)
+
+		if !in.ModTime().IsZero() {
+			fh.SetModTime(in.ModTime())
+		}
+
 		fh.ExternalAttrs |= uint32(in.WinAttrs())
 
 		dst, err := writer.CreateHeader(&fh)
@@ -282,6 +293,8 @@ type manifestFile []byte
 func (m *manifestFile) Name() string       { return manifestName }
 func (m *manifestFile) Size() uint64       { return uint64(len(*m)) }
 func (m *manifestFile) Executable() bool   { return false }
+func (m *manifestFile) Writable() bool     { return false }
+func (m *manifestFile) ModTime() time.Time { return time.Time{} }
 func (m *manifestFile) Symlink() bool      { return false }
 func (m *manifestFile) WinAttrs() WinAttrs { return 0 }
 

cipd/client/cipd/local/json_descs.go
@@ -34,7 +34,7 @@ const (
 	manifestName = packageServiceDir + "/manifest.json"
 
 	// manifestFormatVersion is a version to write to the manifest file.
-	manifestFormatVersion = "1"
+	manifestFormatVersion = "1.1"
 )
 
 // InstallMode defines how to install a package.

cipd/client/cipd/local/reader.go
@@ -35,7 +35,7 @@ import (
 	"go.chromium.org/luci/common/logging"
 )
 
-// VerificationMode is defines whether to verify hash or not.
+// VerificationMode defines whether to verify hash or not.
 type VerificationMode int
 
 const (
@@ -43,11 +43,12 @@ const (
 	// compare it to the given instanceID.
 	VerifyHash VerificationMode = 0
 
-	// SkipVerification instructs OpenPackage to skip the hash verification and
-	// trust that the given instanceID matches the package.
+	// SkipHashVerification instructs OpenPackage to skip the hash verification
+	// and trust that the given instanceID matches the package.
 	SkipHashVerification VerificationMode = 1
 )
 
+// ErrHashMismatch is an error when package hash doesn't match.
 var ErrHashMismatch = errors.New("package hash mismatch")
 
 // PackageInstance represents a binary CIPD package file (with manifest inside).
@@ -67,7 +68,7 @@ type InstanceFile interface {
 	io.ReadSeeker
 
 	// Close is a bit non-standard, and can be used to indicate to the storage
-	// (filesystem and/or cache) layer that this instance is actaully bad. The
+	// (filesystem and/or cache) layer that this instance is actually bad. The
 	// storage layer can then evict/revoke, etc. the bad file.
 	Close(ctx context.Context, corrupt bool) error
 }
@@ -180,7 +181,7 @@ func ExtractInstance(ctx context.Context, inst PackageInstance, dest Destination
 			}
 			manifest.Files = append(manifest.Files, fi)
 		}
-		out, err := dest.CreateFile(ctx, f.Name(), false, 0)
+		out, err := dest.CreateFile(ctx, f.Name(), CreateFileOptions{})
 		if err != nil {
 			return err
 		}
@@ -203,7 +204,12 @@ func ExtractInstance(ctx context.Context, inst PackageInstance, dest Destination
 
 	extractRegularFile := func(f File) (err error) {
 		defer progress.advance(f)
-		out, err := dest.CreateFile(ctx, f.Name(), f.Executable(), f.WinAttrs())
+		out, err := dest.CreateFile(ctx, f.Name(), CreateFileOptions{
+			Executable: f.Executable(),
+			Writable:   f.Writable(),
+			ModTime:    f.ModTime(),
+			WinAttrs:   f.WinAttrs(),
+		})
 		if err != nil {
 			return err
 		}
@@ -425,6 +431,21 @@ func (inst *packageInstance) open(instanceID string, v VerificationMode) error {
 		inst.files[i] = fiz
 	}
 
+	// Version "1" (legacy format) used to set the writable mode bit (0200) in
+	// zipped files, and then ignored it when unpacking. Newer versions respect
+	// the writable mode bit.  Strip it off for the version "1", to preserve
+	// the old semantics.
+	if inst.manifest.FormatVersion == "1" {
+		for _, f := range inst.files {
+			zf, ok := f.(*fileInZip)
+			if !ok {
+				return fmt.Errorf("file %s is not a fileInZip type", f.Name())
+			}
+
+			zf.z.SetMode(zf.z.Mode() & 0757)
+		}
+	}
+
 	// Generate version_file if needed.
 	if inst.manifest.VersionFile != "" {
 		vf, err := makeVersionFile(inst.manifest.VersionFile, VersionFile{
@@ -511,6 +532,8 @@ type blobFile struct {
 func (b *blobFile) Name() string                   { return b.name }
 func (b *blobFile) Size() uint64                   { return uint64(len(b.blob)) }
 func (b *blobFile) Executable() bool               { return false }
+func (b *blobFile) Writable() bool                 { return false }
+func (b *blobFile) ModTime() time.Time             { return time.Time{} }
 func (b *blobFile) Symlink() bool                  { return false }
 func (b *blobFile) SymlinkTarget() (string, error) { return "", nil }
 func (b *blobFile) WinAttrs() WinAttrs             { return 0 }
@@ -552,6 +575,18 @@ func (f *fileInZip) Executable() bool {
 		return false
 	}
 	return (f.z.Mode() & 0100) != 0
+}
+
+func (f *fileInZip) Writable() bool {
+	return (f.z.Mode() & 0200) != 0
+}
+
+func (f *fileInZip) ModTime() time.Time {
+	var t time.Time
+	if f.z.ModifiedTime != 0 || f.z.ModifiedDate != 0 {
+		t = f.z.ModTime()
+	}
+	return t
 }
 
 func (f *fileInZip) Size() uint64 {

cipd/client/cli/main.go
@@ -387,9 +387,11 @@ type inputOptions struct {
 	vars       packageVars
 
 	// Alternative to 'pkg-def'.
-	packageName string
-	inputDir    string
-	installMode local.InstallMode
+	packageName      string
+	inputDir         string
+	installMode      local.InstallMode
+	preserveModTime  bool
+	preserveWritable bool
 
 	// Deflate compression level (if [1-9]) or 0 to disable compression.
 	//
@@ -409,6 +411,10 @@ func (opts *inputOptions) registerFlags(f *flag.FlagSet) {
 	f.StringVar(&opts.inputDir, "in", "", "Path to a directory with files to package (unused with -pkg-def).")
 	f.Var(&opts.installMode, "install-mode",
 		"How the package should be installed: \"copy\" or \"symlink\" (unused with -pkg-def).")
+	f.BoolVar(&opts.preserveModTime, "preserve-mtime", false,
+		"Preserve file's modification time (unused with -pkg-def).")
+	f.BoolVar(&opts.preserveWritable, "preserve-writable", false,
+		"Preserve file's writable permission bit (unused with -pkg-def).")
 
 	// Options for the builder.
 	f.IntVar(&opts.compressionLevel, "compression-level", 5,
@@ -423,6 +429,11 @@ func (opts *inputOptions) prepareInput() (local.BuildInstanceOptions, error) {
 
 	if opts.compressionLevel < 0 || opts.compressionLevel > 9 {
 		return empty, makeCLIError("invalid -compression-level: must be in [0-9] set")
+	}
+
+	scanOpts := local.ScanOptions{
+		PreserveModTime:  opts.preserveModTime,
+		PreserveWritable: opts.preserveWritable,
 	}
 
 	// Handle -name and -in if defined. Do not allow -pkg-def and -pkg-var in that case.
@@ -443,11 +454,12 @@ func (opts *inputOptions) prepareInput() (local.BuildInstanceOptions, error) {
 		}
 
 		// Simply enumerate files in the directory.
-		files, err := local.ScanFileSystem(opts.inputDir, opts.inputDir, nil)
+		files, err := local.ScanFileSystem(opts.inputDir, opts.inputDir, nil, scanOpts)
 		if err != nil {
 			return empty, err
 		}
 		return local.BuildInstanceOptions{
+			ScanOptions:      scanOpts,
 			Input:            files,
 			PackageName:      packageName,
 			InstallMode:      opts.installMode,
@@ -461,7 +473,13 @@ func (opts *inputOptions) prepareInput() (local.BuildInstanceOptions, error) {
 			return empty, makeCLIError("-pkg-def and -name can not be used together")
 		}
 		if opts.installMode != "" {
-			return empty, makeCLIError("-install-mode is ignored if -pkd-def is used")
+			return empty, makeCLIError("-install-mode is ignored if -pkg-def is used")
+		}
+		if opts.preserveModTime {
+			return empty, makeCLIError("-preserve-mtime is ignored if -pkg-def is used")
+		}
+		if opts.preserveWritable {
+			return empty, makeCLIError("-preserve-writable is ignored if -pkg-def is used")
 		}
 
 		// Parse the file, perform variable substitution.
@@ -483,6 +501,7 @@ func (opts *inputOptions) prepareInput() (local.BuildInstanceOptions, error) {
 			return empty, err
 		}
 		return local.BuildInstanceOptions{
+			ScanOptions:      scanOpts,
 			Input:            files,
 			PackageName:      pkgDef.Package,
 			VersionFile:      pkgDef.VersionFile(),

commit 15bca8cf0f5c22f211ba716c3ce0457c24b53bc3

[cipd] Fix handling of file mode flags for packages with format "1".
It has regressed in https://github.com/luci/luci-go/commit/7ed42e5cd88. This
CL introduced package format "1.1" (with slightly different semantics for +w bit
and modification time propagation), but it never tested that packages with
format "1" still are unpacked correctly. They aren't.

R=iannucci@chromium.org, sergeyberezin@chromium.org
BUG=803158

Change-Id: Id968310353680a740e95675333e99ea9db9623b4

cipd/client/cipd/local/builder.go
@@ -35,8 +35,6 @@ import (
 
 // BuildInstanceOptions defines options for BuildInstance function.
 type BuildInstanceOptions struct {
-	ScanOptions
-
 	// Input is a list of files to add to the package.
 	Input []File
 
@@ -54,6 +52,12 @@ type BuildInstanceOptions struct {
 
 	// CompressionLevel defines deflate compression level in range [0-9].
 	CompressionLevel int
+
+	// overrideFormatVersion, if set, will override the default format version put
+	// into the manifest file.
+	//
+	// This is useful for testing.
+	overrideFormatVersion string
 }
 
 // BuildInstance builds a new package instance.
@@ -99,12 +103,12 @@ func BuildInstance(ctx context.Context, opts BuildInstanceOptions) error {
 	}
 
 	// Write the final zip file.
-	return zipInputFiles(ctx, files, opts.Output, opts.CompressionLevel, opts.PreserveWritable)
+	return zipInputFiles(ctx, files, opts.Output, opts.CompressionLevel)
 }
 
 // zipInputFiles deterministically builds a zip archive out of input files and
 // writes it to the writer. Files are written in the order given.
-func zipInputFiles(ctx context.Context, files []File, w io.Writer, level int, preserveWritable bool) error {
+func zipInputFiles(ctx context.Context, files []File, w io.Writer, level int) error {
 	logging.Infof(ctx, "About to zip %d files with compression level %d", len(files), level)
 
 	writer := zip.NewWriter(w)
@@ -315,9 +319,13 @@ func makeManifestFile(opts BuildInstanceOptions) (File, error) {
 	if err := ValidateInstallMode(opts.InstallMode); err != nil {
 		return nil, err
 	}
+	formatVer := manifestFormatVersion
+	if opts.overrideFormatVersion != "" {
+		formatVer = opts.overrideFormatVersion
+	}
 	buf := &bytes.Buffer{}
 	err := writeManifest(&Manifest{
-		FormatVersion: manifestFormatVersion,
+		FormatVersion: formatVer,
 		PackageName:   opts.PackageName,
 		VersionFile:   opts.VersionFile,
 		InstallMode:   opts.InstallMode,

cipd/client/cipd/local/json_descs.go
@@ -77,6 +77,15 @@ type FileInfo struct {
 	// Only used for Linux\Mac archives. False for symlinks.
 	Executable bool `json:"executable,omitempty"`
 
+	// Writable is true if the file is user-writable.
+	Writable bool `json:"writable,omitempty"`
+
+	// ModTime is Unix timestamp with modification time of the file as it is set
+	// inside CIPD package.
+	//
+	// Maybe 0 if the package was built without preserving the modification times.
+	ModTime int64 `json:"modtime,omitempty"`
+
 	// WinAttrs is a string representation of extra windows file attributes.
 	//
 	// Only used for Win archives.

cipd/client/cipd/local/reader.go
@@ -170,7 +170,11 @@ func ExtractInstance(ctx context.Context, inst PackageInstance, dest Destination
 				Name:       file.Name(),
 				Size:       file.Size(),
 				Executable: file.Executable(),
+				Writable:   file.Writable(),
 				WinAttrs:   file.WinAttrs().String(),
+			}
+			if modTime := file.ModTime(); !modTime.IsZero() {
+				fi.ModTime = modTime.Unix()
 			}
 			if file.Symlink() {
 				target, err := file.SymlinkTarget()
@@ -441,8 +445,7 @@ func (inst *packageInstance) open(instanceID string, v VerificationMode) error {
 			if !ok {
 				return fmt.Errorf("file %s is not a fileInZip type", f.Name())
 			}
-
-			zf.z.SetMode(zf.z.Mode() & 0757)
+			zf.z.SetMode(zf.z.Mode() &^ 0222)
 		}
 	}
 

cipd/client/cli/main.go
@@ -431,11 +431,6 @@ func (opts *inputOptions) prepareInput() (local.BuildInstanceOptions, error) {
 		return empty, makeCLIError("invalid -compression-level: must be in [0-9] set")
 	}
 
-	scanOpts := local.ScanOptions{
-		PreserveModTime:  opts.preserveModTime,
-		PreserveWritable: opts.preserveWritable,
-	}
-
 	// Handle -name and -in if defined. Do not allow -pkg-def and -pkg-var in that case.
 	if opts.inputDir != "" {
 		if opts.packageName == "" {
@@ -454,12 +449,14 @@ func (opts *inputOptions) prepareInput() (local.BuildInstanceOptions, error) {
 		}
 
 		// Simply enumerate files in the directory.
-		files, err := local.ScanFileSystem(opts.inputDir, opts.inputDir, nil, scanOpts)
+		files, err := local.ScanFileSystem(opts.inputDir, opts.inputDir, nil, local.ScanOptions{
+			PreserveModTime:  opts.preserveModTime,
+			PreserveWritable: opts.preserveWritable,
+		})
 		if err != nil {
 			return empty, err
 		}
 		return local.BuildInstanceOptions{
-			ScanOptions:      scanOpts,
 			Input:            files,
 			PackageName:      packageName,
 			InstallMode:      opts.installMode,
@@ -501,7 +498,6 @@ func (opts *inputOptions) prepareInput() (local.BuildInstanceOptions, error) {
 			return empty, err
 		}
 		return local.BuildInstanceOptions{
-			ScanOptions:      scanOpts,
 			Input:            files,
 			PackageName:      pkgDef.Package,
 			VersionFile:      pkgDef.VersionFile(),

cipd/client/cipd/local/builder_test.go
@@ -166,10 +166,6 @@ func TestBuildInstance(t *testing.T) {
 		testMTime := time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC)
 		makeOpts := func(out io.Writer, level int) BuildInstanceOptions {
 			return BuildInstanceOptions{
-				ScanOptions: ScanOptions{
-					PreserveModTime:  true,
-					PreserveWritable: true,
-				},
 				Input: []File{
 					NewTestFile("testing/qwerty", "12345", TestFileOpts{}),
 					NewTestFile("abc", "duh", TestFileOpts{Executable: true}),

cipd/client/cipd/local/reader_test.go
@@ -162,9 +162,6 @@ func TestPackageReading(t *testing.T) {
 	})
 
 	Convey("ExtractInstance works", t, func() {
-		// Add a bunch of files to a package.
-		out := bytes.Buffer{}
-
 		testMTime := time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC)
 
 		inFiles := []File{
@@ -183,10 +180,8 @@ func TestPackageReading(t *testing.T) {
 			)
 		}
 
+		out := bytes.Buffer{}
 		err := BuildInstance(ctx, BuildInstanceOptions{
-			ScanOptions: ScanOptions{
-				PreserveWritable: true,
-			},
 			Input:            inFiles,
 			Output:           &out,
 			PackageName:      "testing",
@@ -237,34 +232,28 @@ func TestPackageReading(t *testing.T) {
 			})
 		}
 
-		var zeroTime time.Time
-
-		So(string(dest.files[0].Bytes()), ShouldEqual, "12345")
-		So(dest.files[1].executable, ShouldBeTrue)
-		So(dest.files[1].writable, ShouldBeFalse)
-		So(dest.files[2].writable, ShouldBeTrue)
-		So(dest.files[2].modtime, ShouldEqual, zeroTime)
-
-		So(dest.files[3].name, ShouldEqual, "timestamped")
-		So(dest.files[3].modtime, ShouldEqual, testMTime)
-		So(dest.files[4].symlinkTarget, ShouldEqual, "abc")
-		So(dest.files[5].symlinkTarget, ShouldEqual, "/abc/def")
+		So(string(dest.fileByName("testing/qwerty").Bytes()), ShouldEqual, "12345")
+		So(dest.fileByName("abc").executable, ShouldBeTrue)
+		So(dest.fileByName("abc").writable, ShouldBeFalse)
+		So(dest.fileByName("writable").writable, ShouldBeTrue)
+		So(dest.fileByName("writable").modtime.IsZero(), ShouldBeTrue)
+		So(dest.fileByName("timestamped").modtime, ShouldEqual, testMTime)
+		So(dest.fileByName("rel_symlink").symlinkTarget, ShouldEqual, "abc")
+		So(dest.fileByName("abs_symlink").symlinkTarget, ShouldEqual, "/abc/def")
 
 		// Verify version file is correct.
-		verFileIdx := 6
 		goodVersionFile := `{
 			"instance_id": "284036fec1eaf6492a4e75d1db0920be5e8b3fd7",
 			"package_name": "testing"
 		}`
 		if runtime.GOOS == "windows" {
-			verFileIdx = 8
 			goodVersionFile = `{
 				"instance_id": "7745210cceeb08f108cbb81122f5dbe3a415aaa2",
 				"package_name": "testing"
 			}`
 		}
-		So(dest.files[verFileIdx].name, ShouldEqual, "subpath/version.json")
-		So(string(dest.files[verFileIdx].Bytes()), shouldBeSameJSONDict, goodVersionFile)
+		So(string(dest.fileByName("subpath/version.json").Bytes()),
+			shouldBeSameJSONDict, goodVersionFile)
 
 		// Verify manifest file is correct.
 		goodManifest := `{
@@ -283,9 +272,11 @@ func TestPackageReading(t *testing.T) {
 				},
 				{
 					"name": "writable",
-					"size": 8
+					"size": 8,
+					"writable": true
 				},
 				{
+					"modtime":1514764800,
 					"name": "timestamped",
 					"size": 7
 				},
@@ -305,9 +296,7 @@ func TestPackageReading(t *testing.T) {
 				}
 			]
 		}`
-		manifestIdx := 0
 		if runtime.GOOS == "windows" {
-			manifestIdx = 9
 			goodManifest = fmt.Sprintf(goodManifest, `,{
 				"name": "secret",
 				"size": 5,
@@ -319,11 +308,147 @@ func TestPackageReading(t *testing.T) {
 				"win_attrs": "S"
 			}`)
 		} else {
-			manifestIdx = 7
 			goodManifest = fmt.Sprintf(goodManifest, "")
 		}
-		So(dest.files[manifestIdx].name, ShouldEqual, ".cipdpkg/manifest.json")
-		So(string(dest.files[manifestIdx].Bytes()), shouldBeSameJSONDict, goodManifest)
+		So(string(dest.fileByName(".cipdpkg/manifest.json").Bytes()),
+			shouldBeSameJSONDict, goodManifest)
+	})
+
+	Convey("ExtractInstance handles v1 packages correctly", t, func() {
+		// ZipInfos in packages with format_version "1" always have the writable bit
+		// set, and always have 0 timestamp. During the extraction of such package,
+		// the writable bit should be cleared, and the timestamp should not be reset
+		// to 0 (it will be set to whatever the current time is).
+		inFiles := []File{
+			NewTestFile("testing/qwerty", "12345", TestFileOpts{Writable: true}),
+			NewTestFile("abc", "duh", TestFileOpts{Executable: true, Writable: true}),
+			NewTestFile("bad_dir/pkg/0/description.json", "{}", TestFileOpts{Writable: true}),
+			NewTestSymlink("rel_symlink", "abc"),
+			NewTestSymlink("abs_symlink", "/abc/def"),
+		}
+		if runtime.GOOS == "windows" {
+			inFiles = append(inFiles,
+				NewWinTestFile("secret", "ninja", WinAttrHidden),
+				NewWinTestFile("system", "machine", WinAttrSystem),
+			)
+		}
+
+		out := bytes.Buffer{}
+		err := BuildInstance(ctx, BuildInstanceOptions{
+			Input:                 inFiles,
+			Output:                &out,
+			PackageName:           "testing",
+			VersionFile:           "subpath/version.json",
+			CompressionLevel:      5,
+			overrideFormatVersion: "1",
+		})
+		So(err, ShouldBeNil)
+
+		// Extract files.
+		inst, err := OpenInstance(ctx, bytesFile(out.Bytes()), "", VerifyHash)
+		So(err, ShouldBeNil)
+		dest := &testDestination{}
+		err = ExtractInstance(ctx, inst, dest, func(f File) bool {
+			return strings.HasPrefix(f.Name(), "bad_dir/")
+		})
+		So(err, ShouldBeNil)
+		So(dest.beginCalls, ShouldEqual, 1)
+		So(dest.endCalls, ShouldEqual, 1)
+
+		// Verify file list, file data and flags are correct.
+		names := make([]string, len(dest.files))
+		for i, f := range dest.files {
+			names[i] = f.name
+		}
+		if runtime.GOOS != "windows" {
+			So(names, ShouldResemble, []string{
+				"testing/qwerty",
+				"abc",
+				"rel_symlink",
+				"abs_symlink",
+				"subpath/version.json",
+				".cipdpkg/manifest.json",
+			})
+		} else {
+			So(names, ShouldResemble, []string{
+				"testing/qwerty",
+				"abc",
+				"rel_symlink",
+				"abs_symlink",
+				"secret",
+				"system",
+				"subpath/version.json",
+				".cipdpkg/manifest.json",
+			})
+		}
+
+		So(string(dest.fileByName("testing/qwerty").Bytes()), ShouldEqual, "12345")
+		So(dest.fileByName("abc").executable, ShouldBeTrue)
+		So(dest.fileByName("abc").writable, ShouldBeFalse)
+		So(dest.fileByName("rel_symlink").symlinkTarget, ShouldEqual, "abc")
+		So(dest.fileByName("abs_symlink").symlinkTarget, ShouldEqual, "/abc/def")
+
+		// Verify version file is correct.
+		goodVersionFile := `{
+			"instance_id": "2caf604d611300332f4f5d97dc2e3e1b40b9fc10",
+			"package_name": "testing"
+		}`
+		if runtime.GOOS == "windows" {
+			goodVersionFile = `{
+				"instance_id": "7745210cceeb08f108cbb81122f5dbe3a415aaa2",
+				"package_name": "testing"
+			}`
+		}
+		So(string(dest.fileByName("subpath/version.json").Bytes()),
+			shouldBeSameJSONDict, goodVersionFile)
+
+		// Verify manifest file is correct.
+		goodManifest := `{
+			"format_version": "1",
+			"package_name": "testing",
+			"version_file": "subpath/version.json",
+			"files": [
+				{
+					"name": "testing/qwerty",
+					"size": 5
+				},
+				{
+					"name": "abc",
+					"size": 3,
+					"executable": true
+				},
+				{
+					"name": "rel_symlink",
+					"size": 0,
+					"symlink": "abc"
+				},
+				{
+					"name": "abs_symlink",
+					"size": 0,
+					"symlink": "/abc/def"
+				}%s,
+				{
+					"name": "subpath/version.json",
+					"size": 92
+				}
+			]
+		}`
+		if runtime.GOOS == "windows" {
+			goodManifest = fmt.Sprintf(goodManifest, `,{
+				"name": "secret",
+				"size": 5,
+				"win_attrs": "H"
+			},
+			{
+				"name": "system",
+				"size": 7,
+				"win_attrs": "S"
+			}`)
+		} else {
+			goodManifest = fmt.Sprintf(goodManifest, "")
+		}
+		So(string(dest.fileByName(".cipdpkg/manifest.json").Bytes()),
+			shouldBeSameJSONDict, goodManifest)
 	})
 }
 
@@ -375,5 +500,14 @@ func (d *testDestination) CreateSymlink(ctx context.Context, name string, target
 
 func (d *testDestination) End(ctx context.Context, success bool) error {
 	d.endCalls++
+	return nil
+}
+
+func (d *testDestination) fileByName(name string) *testDestinationFile {
+	for _, f := range d.files {
+		if f.name == name {
+			return f
+		}
+	}
 	return nil
 }