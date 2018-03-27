2fe9d5fee6a55e97a25e737116427808bd5ff2f8
Don Garrett <dgarrett@google.com>
2018-03-27 14:53:31 -0700

    Create configuration for luci-notify email templates.
    This creates the concept of email templates in the luci-notify
    configuration files, but doesn't do anything with them yet.
    
    An arbitrary number of named templates can be defined and used by name
    from both build requests in parameters_json, and from notifier
    defitions.
    
    The first template defined will be used as the default for all project
    notifications not otherwise specified.
    
    Bug: chromium:816652
    Change-Id: I5f6d1320358b6d4c6ec97f1576c9b4f8529fb60a

--- luci_notify/config/notifier.go
+++ luci_notify/config/notifier.go
@@ -42,6 +42,9 @@ type NotificationConfig struct {
 
 	// EmailRecipients is a list of email recipients to notify.
 	EmailRecipients []string `json:"email_recipients"`
+
+	// EmailRecipients is a list of email recipients to notify.
+	Template string `json:"template"`
 }
 
 func newNotificationConfig(cfg *notifyConfig.Notification) NotificationConfig {
@@ -54,6 +57,7 @@ func newNotificationConfig(cfg *notifyConfig.Notification) NotificationConfig {
 		OnFailure:       cfg.OnFailure,
 		OnChange:        cfg.OnChange,
 		EmailRecipients: recipients,
+		Template:        cfg.Template,
 	}
 }
 
