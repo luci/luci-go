32c925331b99a4040db23b70baf3f70a6e3356d7
Nodir Turakulov <nodir@google.com>
2018-03-29 11:26:09 -0700

    [buildbucket] Move address functions to buildbucket/v1
    The concept of build address does not propagate to v2, so move build address
    functions to buildbucket v1.
    
    R=hinoka@chromium.org, jchinlee@chromium.org
    
    Bug: 826006
    Change-Id: Ie71f41a937c00b19341c74c9a3fdb4f6658bfc93

---
+++ common/api/buildbucket/buildbucket/v1/address.go
@@ -0,0 +1,111 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buildbucket
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/common/data/strpair"
+)
+
+// FormatBuildAddress returns a value for TagBuildAddress tag.
+// If number is positive, returns "<bucket>/<builder>/<number>",
+// otherwise returns "<id>"
+//
+// See also ParseBuildAddress.
+func FormatBuildAddress(id int64, bucket, builder string, number int) string {
+	if number > 0 {
+		return fmt.Sprintf("%s/%s/%d", bucket, builder, number)
+	}
+
+	return strconv.FormatInt(id, 10)
+}
+
+// ParseBuildAddress parses a value of a TagBuildAddress tag.
+// See also FormatBuildAddress.
+//
+// If id is non-zero, project, bucket and builder are zero.
+// If bucket is non-zero, id is zero.
+func ParseBuildAddress(address string) (id int64, project, bucket, builder string, number int, err error) {
+	parts := strings.Split(address, "/")
+	switch len(parts) {
+	case 3:
+		var numberStr string
+		bucket, builder, numberStr = parts[0], parts[1], parts[2]
+		project = ProjectFromBucket(bucket)
+		number, err = strconv.Atoi(numberStr)
+	case 1:
+		id, err = strconv.ParseInt(parts[0], 10, 64)
+	default:
+		err = fmt.Errorf("unrecognized build address format %q", address)
+	}
+	return
+}
+
+// ValidateBuildAddress returns an error if the build address is invalid.
+func ValidateBuildAddress(address string) error {
+	_, _, _, _, _, err := ParseBuildAddress(address)
+	return err
+}
+
+// GetByAddress fetches a build by its address.
+// Returns (nil, nil) if build is not found.
+func GetByAddress(c context.Context, client *Service, address string) (*ApiCommonBuildMessage, error) {
+	id, _, _, _, _, err := ParseBuildAddress(address)
+	if err != nil {
+		return nil, err
+	}
+
+	if id != 0 {
+		res, err := client.Get(id).Context(c).Do()
+		switch {
+		case err != nil:
+			return nil, err
+		case res.Error != nil && res.Error.Reason == ReasonNotFound:
+			return nil, nil
+		default:
+			return res.Build, nil
+		}
+	}
+
+	msgs, err := client.Search().
+		Context(c).
+		Tag(strpair.Format(TagBuildAddress, address)).
+		Fetch(1, nil)
+	switch {
+	case err != nil:
+		return nil, err
+	case len(msgs) == 0:
+		return nil, nil
+	default:
+		return msgs[0], nil
+	}
+}
+
+// ProjectFromBucket tries to retrieve project id from bucket name.
+// Returns "" on failure.
+func ProjectFromBucket(bucket string) string {
+	// Buildbucket guarantees that buckets that start with "luci."
+	// have "luci.<project id>." prefix.
+	parts := strings.Split(bucket, ".")
+	if len(parts) >= 3 && parts[0] == "luci" {
+		return parts[1]
+	}
+	return ""
+}
