3c4b3207ed2f783448bbd673e2f8c443228d60d7
smut <smut@google.com>
2018-03-29 14:35:20 -0700

    [Machine Database] Add manufacturer to platforms
    Bug: 778865
    Change-Id: Ie8ac91e3ed35c2b19fc2e0c069f8e64d07ff9033

--- machine-db/appengine/rpc/platforms.go
+++ machine-db/appengine/rpc/platforms.go
@@ -17,7 +17,8 @@ package rpc
 import (
 	"golang.org/x/net/context"
 
-	"go.chromium.org/luci/common/data/stringset"
+	"github.com/Masterminds/squirrel"
+
 	"go.chromium.org/luci/common/errors"
 
 	"go.chromium.org/luci/machine-db/api/crimson/v1"
@@ -26,9 +27,9 @@ import (
 
 // ListPlatforms handles a request to retrieve platforms.
 func (*Service) ListPlatforms(c context.Context, req *crimson.ListPlatformsRequest) (*crimson.ListPlatformsResponse, error) {
-	platforms, err := listPlatforms(c, stringset.NewFromSlice(req.Names...))
+	platforms, err := listPlatforms(c, req)
 	if err != nil {
-		return nil, internalError(c, err)
+		return nil, err
 	}
 	return &crimson.ListPlatformsResponse{
 		Platforms: platforms,
@@ -36,26 +37,29 @@ func (*Service) ListPlatforms(c context.Context, req *crimson.ListPlatformsReque
 }
 
 // listPlatforms returns a slice of platforms in the database.
-func listPlatforms(c context.Context, names stringset.Set) ([]*crimson.Platform, error) {
-	db := database.Get(c)
-	rows, err := db.QueryContext(c, `
-		SELECT name, description
-		FROM platforms
-	`)
+func listPlatforms(c context.Context, req *crimson.ListPlatformsRequest) ([]*crimson.Platform, error) {
+	stmt := squirrel.Select("name", "description", "manufacturer").From("platforms")
+	stmt = selectInString(stmt, "names", req.Names)
+	stmt = selectInString(stmt, "manufacturer", req.Manufacturers)
+	query, args, err := stmt.ToSql()
 	if err != nil {
-		return nil, errors.Annotate(err, "failed to fetch platforms").Err()
+		return nil, internalError(c, errors.Annotate(err, "failed to generate statement").Err())
+	}
+
+	db := database.Get(c)
+	rows, err := db.QueryContext(c, query, args...)
+	if err != nil {
+		return nil, internalError(c, errors.Annotate(err, "failed to fetch platforms").Err())
 	}
 	defer rows.Close()
 
 	var platforms []*crimson.Platform
 	for rows.Next() {
 		p := &crimson.Platform{}
-		if err = rows.Scan(&p.Name, &p.Description); err != nil {
-			return nil, errors.Annotate(err, "failed to fetch platform").Err()
+		if err = rows.Scan(&p.Name, &p.Description, &p.Manufacturer); err != nil {
+			return nil, internalError(c, errors.Annotate(err, "failed to fetch platform").Err())
 		}
-		if matches(p.Name, names) {
-			platforms = append(platforms, p)
-		}
+		platforms = append(platforms, p)
 	}
 	return platforms, nil
 }
