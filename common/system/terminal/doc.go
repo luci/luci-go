08ee08d72b340c19838e8abe0ae5504bde6d52a4
Robert Iannucci <iannucci@chromium.org>
2018-03-30 22:33:12 -0700

    [common/system/terminal] Just forward IsTerminal upstream.
    Not only is this simpler, but it should work a lot better!
    
    R=nodir@chromium.org, vadimsh@chromium.org
    
    Change-Id: I67835abfc2dcac327b859e8871626be18b967b57

--- common/system/terminal/doc.go
+++ common/system/terminal/doc.go
@@ -1,8 +1,21 @@
-// Copyright 2011 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package terminal is cherry-picked code from golang.org/x/crypto/ssh.
+// Copyright 2018 The LUCI Authors.
 //
-// Is it under a different license. The only exception is util_appengine.go.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package terminal is just a way to forward
+//
+//   "golang.org/x/crypto/ssh/terminal".IsTerminal(fd int)
+//
+// In a way that will work on appengine (specifically; it doesn't break
+// appengine's compilation).
 package terminal
