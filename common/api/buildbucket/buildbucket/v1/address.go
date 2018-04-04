38a60732a977b5d9544f53c1af75d5bb43b02bf9
Ryan Tseng <hinoka@google.com>
2018-04-04 11:02:00 -0700

    [buildbucket v1] Always include experimental when fetching by address
    If a caller is fetching a buildbucket build by address, it shouldn't matter
    if it was experimental or not, the API should return the build.
    
    Change-Id: I0a2f68fa7995abd45200b42c7ddf639f44ea7046

--- common/api/buildbucket/buildbucket/v1/address.go
+++ common/api/buildbucket/buildbucket/v1/address.go
@@ -87,6 +87,7 @@ func GetByAddress(c context.Context, client *Service, address string) (*ApiCommo
 	msgs, err := client.Search().
 		Context(c).
 		Tag(strpair.Format(TagBuildAddress, address)).
+		IncludeExperimental(true).
 		Fetch(1, nil)
 	switch {
 	case err != nil:
