ab59c73016b9a3ff300f110dc23996ab73dd73ae
Vadim Shtayura <vadimsh@chromium.org>
2018-04-17 18:50:04 -0700

    [scheduler] Implement ListInvocations for v2.
    R=tandrii@chromium.org
    BUG=764043
    
    Change-Id: Ibb5e751bd1e66161150756a789772678c92e32cb

--- scheduler/appengine/engine/engine.go
+++ scheduler/appengine/engine/engine.go
@@ -377,62 +377,54 @@ func (e *engineImpl) GetVisibleJobBatch(c context.Context, jobIDs []string) (map
 //
 // Supports both v1 and v2 invocations.
 func (e *engineImpl) ListInvocations(c context.Context, job *Job, opts ListInvocationsOpts) ([]*Invocation, string, error) {
-	// TODO(vadimsh): Implement FinishedOnly and ActiveOnly
-	if opts.ActiveOnly {
-		return nil, "", nil
+	if opts.ActiveOnly && opts.FinishedOnly {
+		return nil, "", fmt.Errorf("using both ActiveOnly and FinishedOnly is not allowed")
 	}
 
-	jobID := job.JobID
-
-	if opts.PageSize == 0 || opts.PageSize > 500 {
+	if opts.PageSize <= 0 || opts.PageSize > 500 {
 		opts.PageSize = 500
 	}
 
-	// Deserialize the cursor.
-	var cursor invocationsCursor
-	if opts.Cursor != "" {
-		var err error
-		cursor, err = decodeInvocationsCursor(c, opts.Cursor)
+	// Deserialize the incoming cursor.
+	var cursor internal.InvocationsCursor
+	if err := decodeInvCursor(opts.Cursor, &cursor); err != nil {
+		return nil, "", err
+	}
+
+	out := make([]*Invocation, 0, opts.PageSize)
+	lastScanned := cursor.LastScanned
+	done := false
+
+	// Build the full page out of potentially incomplete (due to post-filtering)
+	// smaller pages. Note that most of the time 'fetchInvsPage' will return the
+	// full page right away.
+	for opts.PageSize > 0 && !done {
+		page, err := fetchInvsPage(c, job, opts, lastScanned)
 		if err != nil {
 			return nil, "", err
 		}
+		if len(page.invs) > opts.PageSize {
+			panic("fetchInvsPage returned unexpectedly more results")
+		}
+		out = append(out, page.invs...)
+		opts.PageSize -= len(page.invs)
+		lastScanned = page.lastScanned
+		done = page.final
 	}
 
-	// Prepare the query. Fetch 'pageSize' worth of entities as a single batch.
-	q := ds.NewQuery("Invocation")
-	if e.isV2Job(jobID) {
-		q = q.Eq("IndexedJobID", jobID)
-	} else {
-		q = q.Ancestor(ds.NewKey(c, "Job", jobID, 0, nil))
-	}
-	q = q.Order("__key__").Limit(int32(opts.PageSize))
-	if cursor.QueryCursor != nil {
-		q = q.Start(cursor.QueryCursor)
+	// Return empty cursor if fetched all available invocations, this signals the
+	// caller that the query has finished.
+	if done {
+		return out, "", nil
 	}
 
-	// Fetch pageSize worth of invocations, then grab the cursor.
-	out := make([]*Invocation, 0, opts.PageSize)
-	newCursor := invocationsCursor{}
-	err := ds.Run(c, q, func(obj *Invocation, getCursor ds.CursorCB) error {
-		out = append(out, obj)
-		if len(out) < opts.PageSize {
-			return nil
-		}
-		var err error
-		if newCursor.QueryCursor, err = getCursor(); err != nil {
-			return err
-		}
-		return ds.Stop
+	// Otherwise prepare the next cursor.
+	cursorStr, err := encodeInvCursor(&internal.InvocationsCursor{
+		LastScanned: lastScanned,
 	})
-	if err != nil {
-		return nil, "", transient.Tag.Apply(err)
-	}
-
-	cursorStr, err := newCursor.Serialize()
 	if err != nil {
 		return nil, "", errors.Annotate(err, "failed to serialize the cursor").Err()
 	}
-
 	return out, cursorStr, nil
 }
 
