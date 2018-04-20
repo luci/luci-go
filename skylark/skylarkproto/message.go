4e4739f76867f5b34bdb26576e1651510830a4d8
Vadim Shtayura <vadimsh@chromium.org>
2018-04-19 20:19:39 -0700

    [skylark] Add skylarkproto package.
    Has basic functionality of making proto messages available as skylark values.
    Currently only int64 and message-typed fields are supported (as POC). More
    features will be added later in follow up commits.
    
    R=smut@chromium.org, tandrii@chromium.org
    BUG=833946
    
    Change-Id: Ia30551bb458d34faf9f124ef260931230c33cd43

---
+++ skylark/skylarkproto/message.go
@@ -0,0 +1,154 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package skylarkproto
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/google/skylark"
+)
+
+// Message is a skylark value that implements a struct-like type structured
+// like a protobuf message.
+type Message struct {
+	typ    *MessageType       // type information
+	fields skylark.StringDict // populated fields, keyed by proto field name
+}
+
+// NewMessage instantiates a message of the given type.
+func NewMessage(typ *MessageType) *Message {
+	return &Message{
+		typ:    typ,
+		fields: skylark.StringDict{},
+	}
+}
+
+// Public API used by the hosting environment.
+
+// MessageType returns detailed type information about the message.
+func (m *Message) MessageType() *MessageType { return m.typ }
+
+// ToProto returns a new populated proto message of an appropriate type.
+//
+// Returns an error if the data inside the skylark representation of the message
+// has a wrong type.
+func (m *Message) ToProto() (proto.Message, error) {
+	ptr := reflect.New(m.typ.Type().Elem()) // ~ ptr := &ProtoMessage{}
+	msg := ptr.Elem()                       // ~ msg := *ptr (a reference)
+
+	for name, val := range m.fields {
+		fd, ok := m.typ.fields[name]
+		if !ok {
+			panic("should not happen, SetField and Attr checks the structure already")
+		}
+		if err := assign(fd.value(msg), val); err != nil {
+			return nil, fmt.Errorf("bad value for field %q of %q - %s", name, m.Type(), err)
+		}
+	}
+
+	return ptr.Interface().(proto.Message), nil
+}
+
+// Basic skylark.Value interface.
+
+func (m *Message) String() string {
+	msg, err := m.ToProto()
+	if err != nil {
+		return fmt.Sprintf("<!Bad %s: %s!>", m.Type(), err)
+	}
+	return msg.String()
+}
+
+func (m *Message) Type() string { return m.typ.name }
+
+func (m *Message) Freeze() {} // TODO
+
+func (m *Message) Truth() skylark.Bool { return skylark.True }
+
+func (m *Message) Hash() (uint32, error) {
+	return 0, fmt.Errorf("proto message %q is not hashable", m.Type())
+}
+
+// HasAttrs and HasSetField interfaces that make the message look like a struct.
+
+func (m *Message) Attr(name string) (skylark.Value, error) {
+	// A method?
+	if builtin := messageMethods[name]; builtin != nil {
+		return builtin.BindReceiver(m), nil
+	}
+
+	// The field was already set?
+	val, ok := m.fields[name]
+	if ok {
+		return val, nil
+	}
+
+	// The field wasn't set, but it is defined by the proto schema? Need to
+	// generate and return the default value then.
+	if fd, ok := m.typ.fields[name]; ok {
+		def, err := newDefaultValue(fd.typ)
+		if err != nil {
+			return nil, err
+		}
+		m.fields[name] = def
+		return def, nil
+	}
+
+	return nil, fmt.Errorf("proto message %q has no field %q", m.Type(), name)
+}
+
+func (m *Message) AttrNames() []string {
+	// Note: AttrNames is called only by dir(...), it is OK that it is slow.
+	out := make([]string, 0, len(m.typ.fields)+len(messageMethods))
+	for k := range m.typ.fields {
+		out = append(out, k)
+	}
+	for k := range messageMethods { // see methods.go
+		out = append(out, k)
+	}
+	sort.Strings(out)
+	return out
+}
+
+func (m *Message) SetField(name string, val skylark.Value) error {
+	// Overriding methods is not allowed.
+	if messageMethods[name] != nil {
+		return fmt.Errorf("can't overwrite method field %q in message %q", name, m.Type())
+	}
+
+	fd, ok := m.typ.fields[name]
+	if !ok {
+		return fmt.Errorf("proto message %q has no field %q", m.Type(), name)
+	}
+
+	// Setting a field to None removes it completely.
+	if val == skylark.None {
+		delete(m.fields, name)
+		return nil
+	}
+
+	// Do the type checking, but do not actually assign anything, since at this
+	// point we do not yet have Go values to assign to, everything is represented
+	// by Skylark values stored in 'fields'.
+	if err := checkAssignable(fd.typ, val); err != nil {
+		return fmt.Errorf("can't assign value of type %q to field %q in proto %q - %s", val.Type(), name, m.Type(), err)
+	}
+	m.fields[name] = val
+
+	return nil
+}
