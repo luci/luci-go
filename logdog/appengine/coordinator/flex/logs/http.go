2f68bb8fbd84adc18be94f01885172ec07ea59ec
Ryan Tseng <hinoka@google.com>
2018-04-26 17:15:28 -0700

    [logdog] Plaintext Endpoint on Flex
    This CL implements the bare minimum feature set of a plaintext endpoint.
    The rest of the features (html, auth redirection, etc) will come later.
    
    This doesn't use the fetcher/renderer paradigm because:
    * It requires copying a lot of code from the existing log server renderer anyways.
    * We want more control on how to render text lines.  In theory we could
    augment renderer with a "WriteLine" interface that takes a LogLine and
    outputs a string.  But at that point, it feels like the layer of abstraction
    is causing more code to be written, rather than less.
    
    Change-Id: I2a5b8c80c176ca59a08093ddfd2128a1b6ee0fa5

---
+++ logdog/appengine/coordinator/flex/logs/http.go
@@ -0,0 +1,118 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package logs
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"go.chromium.org/luci/common/logging"
+	v1 "go.chromium.org/luci/logdog/api/endpoints/coordinator/logs/v1"
+	"go.chromium.org/luci/logdog/api/logpb"
+	"go.chromium.org/luci/server/router"
+	"golang.org/x/net/context"
+)
+
+// maxBuffer specifies that maximum number of requests we can serve at a time
+// before we start waiting.  Each request has a max byte size of 16MB,
+// as specified in getBytesLimit.
+var maxBuffer = 2
+
+// logResp is the response object passed from the fetch routine back to the rendering routine.
+type logResp struct {
+	getResp *v1.GetResponse
+	err     error
+}
+
+// fetch is a goroutine that fetches log chunks at path and sends data back
+// through ch.
+func fetch(c context.Context, ch chan<- logResp, project, path string) {
+	// Get the server instance.  Test instances can be installed in the context,
+	// the production instance is defined as the server struct in service.go.
+	server := GetServer(c)
+	// Fetch logs until done.
+	req := &v1.GetRequest{
+		Project: project,
+		Path:    path,
+		State:   true,
+		Index:   0,
+	}
+	var err error
+	for {
+		var resp *v1.GetResponse
+		resp, err = server.Get(c, req)
+		if err != nil {
+			break
+		}
+		if t := resp.GetDesc().GetStreamType(); t != logpb.StreamType_TEXT {
+			err = errors.New("Not a text stream")
+			break
+		}
+		ch <- logResp{resp, nil}
+
+		terminalIndex := resp.State.TerminalIndex
+		lastLog := resp.Logs[len(resp.Logs)-1]
+		lastIndex := int64(lastLog.StreamIndex)
+		if lastIndex == terminalIndex {
+			break
+		}
+		req.Index = lastIndex + 1
+	}
+	ch <- logResp{nil, err}
+}
+
+// HttpHandler is a plaintext HTTP handler for retrieving logs.
+// Fetching happens concurrently with serving.
+func HttpHandler(ctx *router.Context) {
+	path := ctx.Params.ByName("path")
+	logging.Infof(ctx.Context, "Got request for %s", path)
+	parts := strings.SplitN(path, "/", 3)
+	if len(parts) != 3 {
+		ctx.Writer.WriteHeader(400)
+		fmt.Fprintf(ctx.Writer, "Missing path")
+		return
+	}
+	first := true
+
+	ch := make(chan logResp, maxBuffer)
+	// Start the fetcher and wait for fetched logs to arrive.
+	go fetch(ctx.Context, ch, parts[1], parts[2])
+	for {
+		logResp := <-ch
+		resp, err := logResp.getResp, logResp.err
+		switch {
+		case resp == nil && err == nil:
+			return // Done!
+		case err != nil:
+			ctx.Writer.WriteHeader(500)
+			fmt.Fprintf(ctx.Writer, "Encountered error while loading: %s", err)
+			return
+		case resp != nil:
+			if first {
+				first = false
+				ctx.Writer.WriteHeader(200)
+				ctx.Writer.Header().Set("Content-Type", "text/plain")
+			}
+			for _, log := range resp.Logs {
+				for _, lines := range log.GetText().GetLines() {
+					fmt.Fprintf(ctx.Writer, "%s\n", lines.GetValue())
+				}
+			}
+		default:
+			panic("this should never happen")
+		}
+	}
+}
