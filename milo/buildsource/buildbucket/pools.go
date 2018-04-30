e33162b8d9e3bb8dd2deb02d3ed266517dd00d54
Ryan Tseng <hinoka@google.com>
2018-04-30 16:09:18 -0700

    [milo] Add cron job to update machine pool for luci builders from swarming
    This adds a cron job to update a couple new entities:
    * BotPool: Keyed on Swarming Host + Dimensions.  Contains list of bots and statuses.
    * BuilderPool: Child of BuilderSummary.  References BotPool via PoolDescriptor.
    
    These combined are enough to display bot information like we do in buildbot.
    
    Bug: 687335
    Change-Id: Ib725c26dad77ebf85c98fe1bb9f17a2123af64aa

---
+++ milo/buildsource/buildbucket/pools.go
@@ -0,0 +1,164 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buildbucket
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"go.chromium.org/gae/service/datastore"
+	swarmbucketAPI "go.chromium.org/luci/common/api/buildbucket/swarmbucket/v1"
+	swarmingAPI "go.chromium.org/luci/common/api/swarming/swarming/v1"
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/sync/parallel"
+	"go.chromium.org/luci/milo/buildsource/swarming"
+	"go.chromium.org/luci/milo/common/model"
+	"go.chromium.org/luci/server/auth"
+)
+
+// processBuilders parses out all of the builder pools from the Swarmbucket get_builders response,
+// and saves the BuilderPool information into the datastore.
+// It returns a list of PoolDescriptors that needs to be fetched and saved.
+func processBuilders(c context.Context, r *swarmbucketAPI.SwarmingSwarmbucketApiGetBuildersResponseMessage) ([]model.PoolDescriptor, error) {
+	var builderPools []model.BuilderPool
+	var descriptors []model.PoolDescriptor
+	seen := stringset.New(0)
+	for _, bucket := range r.Buckets {
+		for _, builder := range bucket.Builders {
+			id := fmt.Sprintf("buildbucket/%s/%s", bucket.Name, builder.Name)
+			descriptor := model.NewPoolDescriptor(bucket.SwarmingHostname, builder.SwarmingDimensions)
+			dID := descriptor.PoolID()
+			builderPools = append(builderPools, model.BuilderPool{
+				BuilderID: datastore.MakeKey(c, model.BuilderSummaryKind, id),
+				PoolKey:   datastore.MakeKey(c, model.BotPoolKind, dID),
+			})
+			if added := seen.Add(dID); added {
+				descriptors = append(descriptors, descriptor)
+			}
+		}
+	}
+	return descriptors, datastore.Put(c, builderPools)
+}
+
+// parseBot parses a Swarming BotInfo response into the structure we will
+// save into the datastore.  Since BotInfo doesn't have an explicit status
+// field that matches Milo's abstraction of a Bot, the status is inferred:
+// * A bot with TaskID is Busy
+// * A bot that is dead or quarantined is Offline
+// * Otherwise, it is implicitly connected and Idle.
+func parseBot(c context.Context, botInfo *swarmingAPI.SwarmingRpcsBotInfo) (*model.Bot, error) {
+	lastSeen, err := time.Parse(swarming.SwarmingTimeLayout, botInfo.LastSeenTs)
+	if err != nil {
+		return nil, err
+	}
+	result := &model.Bot{
+		Name:     botInfo.BotId,
+		LastSeen: lastSeen,
+	}
+
+	switch {
+	case botInfo.TaskId != "":
+		result.Status = model.Busy
+	case botInfo.IsDead || botInfo.Quarantined:
+		result.Status = model.Offline
+	default:
+		// Defaults to idle.
+	}
+	return result, nil
+}
+
+// processBot retrieves the Bot pool details from Swarming for a given set of
+// dimensions for its respective Swarming host, and saves the data into datastore.
+func processBot(c context.Context, desc model.PoolDescriptor) error {
+	t, err := auth.GetRPCTransport(c, auth.AsSelf)
+	if err != nil {
+		return err
+	}
+	sc, err := swarmingAPI.New(&http.Client{Transport: t})
+	if err != nil {
+		return err
+	}
+	sc.BasePath = fmt.Sprintf("https://%s/_ah/api/swarming/v1/", desc.Host())
+
+	botList, err := sc.Bots.List().Dimensions(desc.Dimensions().Format()...).Do()
+	if err != nil {
+		return err
+	}
+	bots := make([]model.Bot, 0, len(botList.Items))
+	for _, botInfo := range botList.Items {
+		// Ignore deleted bots.
+		if botInfo.Deleted {
+			continue
+		}
+		bot, err := parseBot(c, botInfo)
+		if err != nil {
+			return err
+		}
+		bots = append(bots, *bot)
+	}
+	// This is a large RPC, don't try to batch it.
+	return datastore.Put(c, &model.BotPool{
+		PoolID:     desc.PoolID(),
+		Descriptor: desc,
+		Bots:       bots,
+		LastUpdate: clock.Now(c),
+	})
+}
+
+// fetchBotPools resolves the descriptors into actual BotPool information.
+// The input is a list of descriptors to fetch from swarming.
+// Basically this just runs processBot() a bunch of times.
+func processBots(c context.Context, descriptors []model.PoolDescriptor) error {
+	return parallel.WorkPool(8, func(ch chan<- func() error) {
+		for _, desc := range descriptors {
+			desc := desc
+			ch <- func() error {
+				return processBot(c, desc)
+			}
+		}
+	})
+}
+
+// UpdatePools is a cron job endpoint that:
+// 1. Fetches all the builders from our associated Swarmbucket instance.
+// 2. Consolidates all known descriptors (host+dimensions), saves BuilderPool.
+// 3. Fetches and saves BotPool data from swarming for all known descriptors.
+func UpdatePools(c context.Context) error {
+	host, err := getHost(c)
+	if err != nil {
+		return err
+	}
+	sc, err := newSwarmbucketClient(c, host)
+	if err != nil {
+		return err
+	}
+	r, err := sc.GetBuilders().Do()
+	if err != nil {
+		return err
+	}
+	// Process builders and save them.  We get back the descriptors that we have
+	// to fetch next.
+	descriptors, err := processBuilders(c, r)
+	if err != nil {
+		return errors.Annotate(err, "processing builders").Err()
+	}
+	// And now also fetch and save the BotPools.
+	return processBots(c, descriptors)
+}
---
+++ milo/buildsource/buildbucket/pools_test.go
@@ -0,0 +1,126 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buildbucket
+
+import (
+	"testing"
+
+	"go.chromium.org/gae/service/datastore"
+	"go.chromium.org/luci/appengine/gaetesting"
+	"go.chromium.org/luci/auth/identity"
+	swarmbucket "go.chromium.org/luci/common/api/buildbucket/swarmbucket/v1"
+	sv1 "go.chromium.org/luci/common/api/swarming/swarming/v1"
+	"go.chromium.org/luci/common/clock/testclock"
+	memcfg "go.chromium.org/luci/config/impl/memory"
+	"go.chromium.org/luci/config/server/cfgclient/backend/testconfig"
+	"go.chromium.org/luci/milo/buildsource/swarming"
+	"go.chromium.org/luci/milo/common/model"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/auth/authtest"
+	"go.chromium.org/luci/server/caching"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestPools(t *testing.T) {
+	t.Parallel()
+	Convey(`Test Pool Information`, t, func() {
+		c := gaetesting.TestingContextWithAppID("luci-milo-dev")
+		datastore.GetTestable(c).Consistent(true)
+		c, _ = testclock.UseTime(c, RefTime)
+		c = testconfig.WithCommonClient(c, memcfg.New(bktConfigFull))
+		c = auth.WithState(c, &authtest.FakeState{
+			Identity:       identity.AnonymousIdentity,
+			IdentityGroups: []string{"all"},
+		})
+		dim1 := []string{"egg:white", "pants:green"}
+		dim2 := []string{"egg:blue", "pants:green"}
+		getBuilderMsg := &swarmbucket.SwarmingSwarmbucketApiGetBuildersResponseMessage{
+			Buckets: []*swarmbucket.SwarmingSwarmbucketApiBucketMessage{
+				{
+					SwarmingHostname: "swarming.example.com",
+					Name:             "foobucket",
+					Builders: []*swarmbucket.SwarmingSwarmbucketApiBuilderMessage{
+						{
+							Name:               "foobuilder",
+							SwarmingDimensions: dim1,
+						},
+						{
+							Name:               "foobuilder2",
+							SwarmingDimensions: dim1,
+						},
+						{
+							Name:               "foobuilder3",
+							SwarmingDimensions: dim2,
+						},
+					},
+				},
+			},
+		}
+
+		md1 := model.NewPoolDescriptor("swarming.example.com", dim1)
+		md2 := model.NewPoolDescriptor("swarming.example.com", dim2)
+		c = caching.WithRequestCache(c)
+		Convey(`Unit Tests`, func() {
+			Convey(`Parsing Builders`, func() {
+				descriptors, err := processBuilders(c, getBuilderMsg)
+				So(err, ShouldBeNil)
+				So(len(descriptors), ShouldEqual, 2)
+				Convey(`And builders should be there`, func() {
+					b1 := model.BuilderPool{
+						BuilderID: datastore.MakeKey(c, "BuilderSummary", "buildbucket/foobucket/foobuilder"),
+					}
+					So(datastore.Get(c, &b1), ShouldBeNil)
+					So(b1.PoolKey.StringID(), ShouldEqual, md1.PoolID())
+					b2 := model.BuilderPool{
+						BuilderID: datastore.MakeKey(c, "BuilderSummary", "buildbucket/foobucket/foobuilder3"),
+					}
+					So(datastore.Get(c, &b2), ShouldBeNil)
+					So(b2.PoolKey.StringID(), ShouldEqual, md2.PoolID())
+				})
+				// TODO(hinoka): Test that swarming pools load and save correctly.
+			})
+
+			Convey(`Parsing Bot Status`, func() {
+				bot := &sv1.SwarmingRpcsBotInfo{
+					LastSeenTs: RefTime.Format(swarming.SwarmingTimeLayout),
+				}
+				Convey(`Empty is Idle`, func() {
+					s, err := parseBot(c, bot)
+					So(err, ShouldBeNil)
+					So(s.Status, ShouldEqual, model.Idle)
+				})
+				Convey(`With TaskID is Busy`, func() {
+					bot.TaskId = "someID"
+					s, err := parseBot(c, bot)
+					So(err, ShouldBeNil)
+					So(s.Status, ShouldEqual, model.Busy)
+				})
+				Convey(`Died is Offline`, func() {
+					bot.IsDead = true
+					s, err := parseBot(c, bot)
+					So(err, ShouldBeNil)
+					So(s.Status, ShouldEqual, model.Offline)
+				})
+				Convey(`Quarantined is Offline`, func() {
+					bot.Quarantined = true
+					s, err := parseBot(c, bot)
+					So(err, ShouldBeNil)
+					So(s.Status, ShouldEqual, model.Offline)
+				})
+			})
+		})
+	})
+}
