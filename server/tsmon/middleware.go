8af19840c928cf64b2355a1adb50175ca7af5413
Andrew McRae <amcrae@chromium.org>
2018-05-01 14:31:43 +1000

    Use initial retry of 5 seconds. Add flush back-off.
    Bug: 836028
    Change-Id: I92030b2db0d2c143801878abc88198d8d8b78e34

--- server/tsmon/middleware.go
+++ server/tsmon/middleware.go
@@ -76,10 +76,12 @@ type State struct {
 	state        *tsmon.State
 	lastSettings tsmonSettings
 
-	instanceID  string    // cached result of InstanceID() call
-	flushingNow bool      // true if some goroutine is flushing right now
-	nextFlush   time.Time // next time we should do the flush
-	lastFlush   time.Time // last successful flush
+	instanceID   string        // cached result of InstanceID() call
+	flushingNow  bool          // true if some goroutine is flushing right now
+	nextFlush    time.Time     // next time we should do the flush
+	lastFlush    time.Time     // last successful flush
+	flushTimeout time.Duration // flush timeout
+	flushRetry   time.Duration // flush retry delay
 
 	testingMonitor  monitor.Monitor // mocked in unit tests
 	testingSettings *tsmonSettings  // mocked in unit tests
@@ -90,8 +92,15 @@ const (
 	// the last successful flush (if ever) was too long ago.
 	noFlushErrorThreshold = 5 * time.Minute
 
-	// flushTimeout defines a deadline for the flush operation.
-	flushTimeout = 5 * time.Second
+	// flushInitialTimeout defines a minimum deadline for the flush operation.
+	flushInitialTimeout = 5 * time.Second
+
+	// flushMaxRetry defines the maximum delay between flush retries.
+	flushMaxRetry = 10 * time.Minute
+
+	// flushInitialRetry defines the initial retry delay. This
+	// is doubled every retry, up to flushMaxRetry.
+	flushInitialRetry = 5 * time.Second
 )
 
 // Middleware is a middleware that collects request metrics and triggers metric
@@ -185,6 +194,9 @@ func (s *State) enableTsMon(c context.Context) {
 	// invalid logging that the last flush was long time ago.
 	s.nextFlush = clock.Now(c)
 	s.lastFlush = s.nextFlush
+	// Set initial values for timeout and delay.
+	s.flushTimeout = flushInitialTimeout
+	s.flushRetry = flushInitialRetry
 }
 
 // disableTsMon puts nil metrics store in the context's tsmon state.
@@ -227,7 +239,7 @@ func (s *State) flushIfNeeded(c context.Context, req *http.Request, state *tsmon
 	}
 
 	// The flush must be fast. Limit it by some timeout.
-	c, cancel := clock.WithTimeout(c, flushTimeout)
+	c, cancel := clock.WithTimeout(c, s.flushTimeout)
 	defer cancel()
 
 	// Report per-process statistic.
@@ -236,19 +248,30 @@ func (s *State) flushIfNeeded(c context.Context, req *http.Request, state *tsmon
 		runtimestats.Report(c)
 	}
 
-	// Unset 'flushingNow' no matter what (even on panics). Update 'nextFlush'
-	// only on successful flush or when we are still waiting for the task number.
-	// Unsuccessful flush thus will be retried ASAP.
+	// Unset 'flushingNow' no matter what (even on panics).
+	// If flush has failed, retry with back off to avoid
+	// hammering the receiver.
 	var err error
 	defer func() {
 		s.lock.Lock()
 		defer s.lock.Unlock()
 		s.flushingNow = false
 		if err == nil || err == ErrNoTaskNumber {
+			// Reset timeout and retry delay.
+			s.flushTimeout = flushInitialTimeout
+			s.flushRetry = flushInitialRetry
 			s.nextFlush = now.Add(time.Duration(settings.FlushIntervalSec) * time.Second)
 			if err == nil {
 				s.lastFlush = now
 			}
+		} else {
+			// Flush has failed, back off the next flush,
+			// and increase the timeout.
+			if s.flushRetry < flushMaxRetry {
+				s.flushRetry *= 2
+				s.flushTimeout += time.Second
+			}
+			s.nextFlush = now.Add(s.flushRetry)
 		}
 	}()
 
