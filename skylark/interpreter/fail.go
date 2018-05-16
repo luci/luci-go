d5002387142b1a3aebf8aa7d3022804a157d6ba0
Vadim Shtayura <vadimsh@chromium.org>
2018-05-15 21:03:08 -0700

    [skylark] Implement an interpreter that can load modular scripts.
    Raw skylark.ExecFile and skylark.Thread facilities are limited to working with
    only one file. This is not sufficient for any non-trivial scripts.
    
    This CL introduces Interpreter struct that knows how work with a tree of skylark
    scripts that import each other, as well as protobuf definition files.
    
    It also has a support for a separate tree of script that constitute a "standard
    library", with an entry point being in init.sky. All scripts executed by the
    interpreter have access to symbols exported by init.sky. The standard library
    can be compiled into the interpreter executable, to make it completely
    standalone (this will be used in next CLs).
    
    R=smut@google.com
    BUG=833946
    
    Change-Id: I1d2b1a8d622abcf14c0f6eaf82111c7058f29ec4

---
+++ skylark/interpreter/fail.go
@@ -0,0 +1,42 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package interpreter
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/google/skylark"
+)
+
+// failImpl implements fail() builtin.
+//
+// It aborts the script with a fatal error.
+func failImpl(thread *skylark.Thread, _ *skylark.Builtin, args skylark.Tuple, kwargs []skylark.Tuple) (skylark.Value, error) {
+	if len(kwargs) != 0 {
+		return nil, fmt.Errorf("'fail' doesn't accept kwargs")
+	}
+	if len(args) != 1 {
+		return nil, fmt.Errorf("'fail' got %d arguments, wants 1", len(args))
+	}
+	msg, ok := skylark.AsString(args[0])
+	if !ok {
+		msg = args[0].String()
+	}
+	// Note: skylark.ExecFile returns *EvalError on errors which have the original
+	// error flattened into a string, so returning a structured error here makes
+	// no difference.
+	return nil, errors.New(msg)
+}
