ff28d054f9fc92c150e8808819dfeadfb6f9d5ab
Nodir Turakulov <nodir@google.com>
2018-05-17 22:50:01 -0700

    [luci-notify] Replicate email templates in the datastore
    Code that runs frequently must not contact luci-config directly because
    luci-config is not designed for this scale and it is slower.
    
    In the existing cron job that fetches project config from luci-config, use
    new ListFiles API to fetch names of all email templates of the project,
    then fetch them and save in a new EmailTemplate datastore entity.
    It happens in the same Datastore transaction as the luci-notify.cfg file.
    The root entity is the project itself.
    
    Bug: 816652
    Change-Id: Ie8e856cd9a2bb67de7a2e7855e4a9bd4c5722c84

--- luci_notify/config/emailtemplate.go
+++ luci_notify/config/emailtemplate.go
@@ -20,12 +20,24 @@ import (
 	"regexp"
 	"strings"
 	text "text/template"
+
+	"golang.org/x/net/context"
+
+	"go.chromium.org/gae/service/datastore"
+	"go.chromium.org/gae/service/info"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	configInterface "go.chromium.org/luci/config"
 )
 
-// emailTemplateFilenameRegexp is a regular expression for email template file
-// names.
-// The first path component is a app id. We do not validate it here.
-var emailTemplateFilenameRegexp = regexp.MustCompile(`^[^/]+/email-templates/[a-z][a-z0-9_]*\.template$`)
+// emailTemplateFilenameRegexp returns a regular expression for email template
+// file names.
+func emailTemplateFilenameRegexp(c context.Context) *regexp.Regexp {
+	appId := info.AppID(c)
+	pattern := fmt.Sprintf(`^%s+/email-templates/([a-z][a-z0-9_]*)\.template$`, regexp.QuoteMeta(appId))
+	return regexp.MustCompile(pattern)
+}
 
 // ParsedEmailTemplate is a parsed email template file.
 type ParsedEmailTemplate struct {
@@ -33,36 +45,105 @@ type ParsedEmailTemplate struct {
 	Body    *html.Template
 }
 
-// Parse parses an email template file contents.
+// Parse parses email subject and body templates.
+func (t *ParsedEmailTemplate) Parse(subject, body string) error {
+	subjectTemplate, err := text.New("subject").Parse(subject)
+	if err != nil {
+		return err // error includes template name
+	}
+
+	bodyTemplate, err := html.New("body").Parse(body)
+	// Due to luci-config limitation, we cannot detect an invalid reference to
+	// a sub-template defined in a different file.
+	if err != nil {
+		return err // error includes template name
+	}
+
+	t.Subject = subjectTemplate
+	t.Body = bodyTemplate
+	return nil
+}
+
+// EmailTemplate is a Datastore entity directly under Project entity that
+// represents an email template.
+// It is managed by the cron job that ingests configs.
+type EmailTemplate struct {
+	// ProjectKey is a datastore key of the LUCI project containing this email
+	// template.
+	ProjectKey *datastore.Key `gae:"$parent"`
+
+	// Name identifies the email template. It is unique within the project.
+	Name string `gae:"$id"`
+
+	// SubjectTextTemplate is a text.Template of the email subject.
+	SubjectTextTemplate string `gae:",noindex"`
+
+	// BodyHTMLTemplate is a html.Template of the email body.
+	BodyHTMLTemplate string `gae:",noindex"`
+}
+
+// fetchAllEmailTemplates fetches all valid email templates of the project.
+// Returned EmailTemplate entities do not have ProjectKey set.
+func fetchAllEmailTemplates(c context.Context, configService configInterface.Interface, projectId string) (map[string]*EmailTemplate, error) {
+	configSet := configInterface.ProjectSet(projectId)
+	files, err := configService.ListFiles(c, configSet)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := map[string]*EmailTemplate{}
+
+	// This runs in a cron job. It is not performance critical, so we don't have
+	// to fetch files concurrently.
+	filenameRegexp := emailTemplateFilenameRegexp(c)
+	for _, f := range files {
+		m := filenameRegexp.FindStringSubmatch(f)
+		if m == nil {
+			// Not a template file or a template of another instance of luci-notify.
+			continue
+		}
+		templateName := m[1]
+
+		logging.Infof(c, "fetching email template from %s:%s", configSet, f)
+		config, err := configService.GetConfig(c, configSet, f, false)
+		if err != nil {
+			return nil, errors.Annotate(err, "failed to fetch %q", f).Err()
+		}
+
+		subject, body, err := splitEmailTemplateFile(config.Content)
+		if err != nil {
+			// Should not happen. luci-config should not have passed this commit in
+			// because luci-notify exposes its validation code to luci-conifg.
+			logging.Warningf(c, "invalid email template content in %q: %s", f, err)
+			continue
+		}
+
+		ret[templateName] = &EmailTemplate{
+			Name:                templateName,
+			SubjectTextTemplate: subject,
+			BodyHTMLTemplate:    body,
+		}
+	}
+	return ret, nil
+}
+
+// splitEmailTemplateFile splits an email template file into subject and body.
+// Does not validate their syntaxes.
 // See notify.proto for file format.
-func (t *ParsedEmailTemplate) Parse(content string) error {
+func splitEmailTemplateFile(content string) (subject, body string, err error) {
 	if len(content) == 0 {
-		return fmt.Errorf("empty file")
+		return "", "", fmt.Errorf("empty file")
 	}
 
 	parts := strings.SplitN(content, "\n", 3)
 	switch {
 	case len(parts) < 3:
-		return fmt.Errorf("less than three lines")
+		return "", "", fmt.Errorf("less than three lines")
 
 	case len(strings.TrimSpace(parts[1])) > 0:
-		return fmt.Errorf("second line is not blank: %q", parts[1])
+		return "", "", fmt.Errorf("second line is not blank: %q", parts[1])
 
 	default:
-		subject, err := text.New("subject").Parse(strings.TrimSpace(parts[0]))
-		if err != nil {
-			return err // error includes template name
-		}
-
-		body, err := html.New("body").Parse(strings.TrimSpace(parts[2]))
-		// Due to luci-config limitation, we cannot detect an invalid reference to
-		// a sub-template defined in a different file.
-		if err != nil {
-			return err // error includes template name
-		}
-
-		t.Subject = subject
-		t.Body = body
-		return nil
+		return strings.TrimSpace(parts[0]), strings.TrimSpace(parts[2]), nil
 	}
 }
