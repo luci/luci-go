55dca5c7b21a7efbd1b057fc0e8b8f08bbe7d3d9
Andrii Shyshkalov <tandrii@chromium.org>
2018-05-17 18:07:48 -0700

    [milo] validate whitelist of read permission grants on git/gerrit repos.
    Bug: 796317
    Change-Id: Iba511a220a4c27e25dc77a208f650b3c58a36321

---
+++ milo/git/acls.go
@@ -0,0 +1,219 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package git
+
+import (
+	"net/url"
+	"sort"
+	"strings"
+
+	"go.chromium.org/luci/auth/identity"
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/config/validation"
+	"go.chromium.org/luci/milo/api/config"
+)
+
+// ACLsFromConfig returns ACLs if config is valid.
+func ACLsFromConfig(cfg []*config.Settings_SourceAcls) (*ACLs, error) {
+	ACLs := &ACLs{}
+	var ctx validation.Context
+	ACLs.load(&ctx, cfg)
+	if err := ctx.Finalize(); err != nil {
+		return nil, err
+	}
+	return ACLs, nil
+}
+
+// ValidateACLsConfig passes all validation errors through validation context.
+func ValidateACLsConfig(ctx *validation.Context, cfg []*config.Settings_SourceAcls) {
+	var ACLs ACLs
+	ACLs.load(ctx, cfg)
+}
+
+// ACLs define readers for git repositories and Gerrits CLs.
+type ACLs struct {
+	hosts map[string]*hostACLs
+}
+
+// private implementation.
+
+type itemACLs struct {
+	definedIn int // used for validation only.
+	readers   []string
+}
+
+type hostACLs struct {
+	itemACLs
+	projects map[string]*itemACLs // key is project name starting with '/'.
+}
+
+// load validates and loads ACLs from config.
+func (a *ACLs) load(ctx *validation.Context, cfg []*config.Settings_SourceAcls) {
+	a.hosts = map[string]*hostACLs{}
+	for i, group := range cfg {
+		ctx.Enter("source_acls #%d", i)
+
+		if len(group.Readers) == 0 {
+			ctx.Errorf("at least 1 reader required")
+		}
+		if len(group.Hosts)+len(group.Projects) == 0 {
+			ctx.Errorf("at least 1 host or project required")
+		}
+
+		gReaders := a.loadReaders(ctx, group.Readers)
+		for _, hostURL := range group.Hosts {
+			ctx.Enter("host %q", hostURL)
+			a.loadHost(ctx, i, hostURL, gReaders)
+			ctx.Exit()
+		}
+		for _, projectURL := range group.Projects {
+			ctx.Enter("project %q", projectURL)
+			a.loadProject(ctx, i, projectURL, gReaders)
+			ctx.Exit()
+		}
+		ctx.Exit()
+	}
+}
+
+// loadReaders validates readers and returns slice of valid identities.
+func (a *ACLs) loadReaders(ctx *validation.Context, readers []string) []string {
+	known := stringset.New(len(readers))
+	for _, r := range readers {
+		id := r
+		switch {
+		case strings.HasPrefix(r, "group:"):
+			if r[len("group:"):] == "" {
+				ctx.Errorf("invalid readers %q: needs a group name", r)
+			}
+		case !strings.ContainsRune(r, ':'):
+			id = "user:" + r
+			fallthrough
+		default:
+			if _, err := identity.MakeIdentity(id); err != nil {
+				ctx.Error(errors.Annotate(err, "invalid readers %q", r).Err())
+				continue
+			}
+		}
+		if !known.Add(id) {
+			ctx.Errorf("duplicate readers %q", r)
+		}
+	}
+	res := known.ToSlice()
+	sort.Strings(res)
+	return res
+}
+
+func (a *ACLs) loadHost(ctx *validation.Context, blockId int, hostURL string, readers []string) {
+	u, valid := validateURL(ctx, hostURL)
+	if !valid && u == nil {
+		return // Can't validate any more.
+	}
+	u.Path = strings.TrimRight(u.Path, "/")
+	if u.Path != "" || u.Fragment != "" {
+		ctx.Errorf("shouldn't have path or fragment components")
+		valid = false
+	}
+	switch ha, dup := a.hosts[u.Host]; {
+	case dup && ha.definedIn > -1:
+		ctx.Errorf("has already been defined in source_acl #%d", ha.definedIn)
+	case !valid:
+		return
+	case dup:
+		ha.definedIn = blockId
+		ha.readers = readers
+	default:
+		a.hosts[u.Host] = &hostACLs{itemACLs: itemACLs{definedIn: blockId, readers: readers}}
+	}
+}
+func (a *ACLs) loadProject(ctx *validation.Context, blockId int, projectURL string, readers []string) {
+	u, valid := validateURL(ctx, projectURL)
+	if !valid && u == nil {
+		return // Can't validate any more.
+	}
+	u.Path = strings.TrimRight(u.Path, "/")
+	if u.Path == "" {
+		ctx.Errorf("should not be just a host")
+		valid = false
+	}
+	if strings.HasPrefix(u.Path, "/a/") {
+		ctx.Errorf("should not contain '/a' path prefix")
+		valid = false
+	}
+	if strings.HasSuffix(u.Path, ".git") {
+		ctx.Errorf("should not contain '.git'")
+		valid = false
+	}
+	if u.Fragment != "" {
+		ctx.Errorf("shouldn't have fragment components")
+		valid = false
+	}
+	if !valid {
+		return
+	}
+
+	// u.Path at this point starts with '/' and will be key in projects map.
+	hACLs, knownHost := a.hosts[u.Host]
+	switch {
+	case knownHost && hACLs.definedIn == blockId:
+		ctx.Errorf("redundant because already covered by its host in the same source_acls block")
+		return
+	case !knownHost:
+		hACLs = &hostACLs{itemACLs: itemACLs{definedIn: -1}}
+		a.hosts[u.Host] = hACLs
+		fallthrough
+	case hACLs.projects == nil:
+		// Less optimal, but more readable.
+		hACLs.projects = make(map[string]*itemACLs, 1)
+	}
+
+	switch projACLs, known := hACLs.projects[u.Path]; {
+	case known && projACLs.definedIn == blockId:
+		ctx.Errorf("duplicate, already defined in the same source_acls block")
+	case known:
+		projACLs.definedIn = blockId
+		projACLs.readers = stringset.NewFromSlice(append(projACLs.readers, readers...)...).ToSlice()
+		sort.Strings(projACLs.readers)
+	default:
+		hACLs.projects[u.Path] = &itemACLs{definedIn: blockId, readers: readers}
+	}
+}
+
+// validateURL returns parsed URL and whether it has passed validation.
+func validateURL(ctx *validation.Context, s string) (*url.URL, bool) {
+	// url.Parse considers "example.com/a/b" to be a path, so ensure a scheme.
+	if !strings.HasPrefix(s, "https://") {
+		s = "https://" + s
+	}
+	u, err := url.Parse(s)
+	if err != nil {
+		ctx.Error(errors.Annotate(err, "not a valid URL").Err())
+		return nil, false
+	}
+	valid := true
+	if !strings.HasSuffix(u.Host, ".googlesource.com") {
+		ctx.Errorf("isn't at *.googlesource.com %q %q", u.Host)
+		valid = false
+	}
+	if u.Scheme != "" && u.Scheme != "https" {
+		ctx.Errorf("scheme must be https")
+		valid = false
+	}
+	if strings.HasSuffix(u.Host, "-review.googlesource.com") {
+		ctx.Errorf("must not be a Gerrit host (try without '-review')")
+		valid = false
+	}
+	return u, valid
+}
---
+++ milo/git/acls_test.go
@@ -0,0 +1,240 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package git
+
+import (
+	"fmt"
+	"testing"
+
+	"go.chromium.org/luci/config/validation"
+	"go.chromium.org/luci/milo/api/config"
+	"golang.org/x/net/context"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestConfigValidation(t *testing.T) {
+	t.Parallel()
+	c := context.Background()
+
+	Convey("Validation works", t, func() {
+		validate := func(cfg ...*config.Settings_SourceAcls) error {
+			ctx := validation.Context{Context: c}
+			ctx.SetFile("settings.cfg")
+			ValidateACLsConfig(&ctx, cfg)
+			return ctx.Finalize()
+		}
+		mustError := func(cfg ...*config.Settings_SourceAcls) multiError {
+			err := validate(cfg...)
+			So(err, ShouldNotBeNil)
+			return multiError(err.(*validation.Error).Errors)
+		}
+
+		valid := config.Settings_SourceAcls{
+			Hosts:    []string{"a.googlesource.com"},
+			Projects: []string{"https://b.googlesource.com/c"},
+			Readers:  []string{"group:g", "user@example.com"},
+		}
+		So(validate(&valid), ShouldBeNil)
+
+		mustError(&config.Settings_SourceAcls{}).with(
+			"at least 1 reader required",
+			"at least 1 host or project required",
+		)
+
+		Convey("readers", func() {
+			mod := valid
+			mod.Readers = []string{"bad:kind", "group:", "user", "group:a", "group:a"}
+			mustError(&mod).with(
+				`invalid readers "bad:kind"`,
+				`invalid readers "group:": needs a group name`,
+				`invalid readers "user"`,
+				`duplicate`,
+			)
+		})
+
+		Convey("hosts", func() {
+			second := config.Settings_SourceAcls{
+				Hosts: []string{
+					valid.Hosts[0],
+					"example.com",
+					"repo.googlesource.com/repo",
+					"b.googlesource.com", // valid.Project was from here, and it's OK.
+				},
+				Readers: []string{"group:a"},
+			}
+			mustError(&valid, &second).with(
+				`host "a.googlesource.com"): has already been defined in source_acl #0`,
+				`isn't at *.googlesource.com`,
+				"shouldn't have path or fragment components",
+			)
+		})
+
+		Convey("projects", func() {
+			second := config.Settings_SourceAcls{
+				Hosts: []string{"r.googlesource.com"},
+				Projects: []string{
+					valid.Projects[0], // dups of prev blocks are OK.
+					"r.googlesource.com/redundant",
+					"not-repo.googlesource.com",
+					"c.googlesource.com/a/repo.git#123",
+					"c-review.googlesource.com/src",
+					"https://\\meh",
+					valid.Projects[0], // dups of projects in this block is not OK.
+				},
+				Readers: []string{"group:b"},
+			}
+			mustError(&valid, &second).with(
+				`redundant because already covered by its host in the same source_acls block`,
+				`project "not-repo.googlesource.com"): should not be just a host`,
+				`should not contain '/a' path prefix`,
+				`should not contain '.git'`,
+				`shouldn't have fragment components`,
+				`must not be a Gerrit host (try without '-review')`,
+				`not a valid URL`,
+				`duplicate, already defined in the same source_acls block`,
+			)
+		})
+	})
+}
+
+func TestConfigLoading(t *testing.T) {
+	t.Parallel()
+
+	Convey("Loading works", t, func() {
+		load := func(cfg ...*config.Settings_SourceAcls) *ACLs {
+			a, err := ACLsFromConfig(cfg)
+			if err != nil {
+				panic(err) // for stacktrace.
+			}
+			return a
+		}
+
+		So(load(
+			&config.Settings_SourceAcls{
+				Hosts: []string{"first.googlesource.com"},
+				Projects: []string{
+					"second.googlesource.com/y1",
+					"third.googlesource.com/z",
+				},
+				Readers: []string{"user@example.com"},
+			}),
+			ShouldResemble,
+			&ACLs{
+				hosts: map[string]*hostACLs{
+					"first.googlesource.com": {
+						itemACLs: itemACLs{
+							definedIn: 0,
+							readers:   []string{"user:user@example.com"},
+						},
+					},
+					"second.googlesource.com": {
+						itemACLs: itemACLs{definedIn: -1},
+						projects: map[string]*itemACLs{
+							"/y1": {
+								definedIn: 0,
+								readers:   []string{"user:user@example.com"},
+							},
+						},
+					},
+					"third.googlesource.com": {
+						itemACLs: itemACLs{definedIn: -1},
+						projects: map[string]*itemACLs{
+							"/z": {
+								definedIn: 0,
+								readers:   []string{"user:user@example.com"},
+							},
+						},
+					},
+				},
+			})
+
+		So(load(
+			&config.Settings_SourceAcls{
+				Hosts: []string{"first.googlesource.com"},
+				Projects: []string{
+					"second.googlesource.com/y1",
+					"third.googlesource.com/z",
+				},
+				Readers: []string{"user@example.com"},
+			},
+			&config.Settings_SourceAcls{
+				Hosts: []string{"third.googlesource.com"},
+				Projects: []string{
+					"first.googlesource.com/x",
+					"second.googlesource.com/y1",
+					"second.googlesource.com/y2",
+				},
+				Readers: []string{"group:g"},
+			}),
+			ShouldResemble,
+			&ACLs{
+				hosts: map[string]*hostACLs{
+					"first.googlesource.com": {
+						itemACLs: itemACLs{
+							definedIn: 0,
+							readers:   []string{"user:user@example.com"},
+						},
+						projects: map[string]*itemACLs{
+							"/x": {
+								definedIn: 1,
+								readers:   []string{"group:g"},
+							},
+						},
+					},
+					"second.googlesource.com": {
+						itemACLs: itemACLs{definedIn: -1},
+						projects: map[string]*itemACLs{
+							"/y1": {
+								definedIn: 1,
+								readers:   []string{"group:g", "user:user@example.com"},
+							},
+							"/y2": {
+								definedIn: 1,
+								readers:   []string{"group:g"},
+							},
+						},
+					},
+					"third.googlesource.com": {
+						itemACLs: itemACLs{
+							definedIn: 1,
+							readers:   []string{"group:g"},
+						},
+						projects: map[string]*itemACLs{
+							"/z": {
+								definedIn: 0,
+								readers:   []string{"user:user@example.com"},
+							},
+						},
+					},
+				},
+			})
+	})
+}
+
+type multiError []error
+
+func (m multiError) with(substrings ...string) {
+	for i, err := range m {
+		if i >= len(substrings) {
+			So(fmt.Errorf("extra errors produced: %q", m[i:]), ShouldBeNil)
+		} else {
+			So(err.Error(), ShouldContainSubstring, substrings[i])
+		}
+	}
+	if len(substrings) > len(m) {
+		panic(fmt.Errorf("not produced errors: %q", substrings[len(m):]))
+	}
+}
