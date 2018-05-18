b18d5a8e5882e8613e717a05dc1cdb622593f8bf
Nodir Turakulov <nodir@google.com>
2018-05-17 22:55:10 -0700

    [luci-notify] Replicate email templates in the datastore
    Code that runs frequently must not contact luci-config directly because
    luci-config is not designed for this scale and it is slower.
    
    In the existing cron job that fetches project config from luci-config, use
    new ListFiles API to fetch names of all email templates of the project,
    then fetch them and save in a new EmailTemplate datastore entity.
    It happens in the same Datastore transaction as the luci-notify.cfg file.
    The root entity is the project itself.
    
    Bug: 816652
    Change-Id: Ie8e856cd9a2bb67de7a2e7855e4a9bd4c5722c84

--- luci_notify/config/emailtemplate_test.go
+++ luci_notify/config/emailtemplate_test.go
@@ -17,53 +17,88 @@ package config
 import (
 	"testing"
 
+	"go.chromium.org/luci/appengine/gaetesting"
+	"go.chromium.org/luci/config"
+	"go.chromium.org/luci/config/impl/memory"
+
 	. "github.com/smartystreets/goconvey/convey"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/logging/gologger"
 	. "go.chromium.org/luci/common/testing/assertions"
 )
 
 func TestEmailTemplate(t *testing.T) {
 	t.Parallel()
 
-	Convey(`ParsedEmailTemplate.Parse`, t, func() {
-		var t ParsedEmailTemplate
-
+	Convey(`splitEmailTemplateFile`, t, func() {
 		Convey(`valid template`, func() {
-			content := `subject
+			_, _, err := splitEmailTemplateFile(`subject
 
-        body`
-			So(t.Parse(content), ShouldBeNil)
+        body`)
+			So(err, ShouldBeNil)
 		})
 
 		Convey(`empty`, func() {
-			So(t.Parse(""), ShouldErrLike, "empty")
+			_, _, err := splitEmailTemplateFile(``)
+			So(err, ShouldErrLike, "empty")
 		})
 
 		Convey(`less than three lines`, func() {
-			content := `subject
-        body`
-			So(t.Parse(content), ShouldErrLike, "less than three lines")
+			_, _, err := splitEmailTemplateFile(`subject
+        body`)
+			So(err, ShouldErrLike, "less than three lines")
 		})
 
 		Convey(`no blank line`, func() {
-			content := `subject
+			_, _, err := splitEmailTemplateFile(`subject
         body
         second line
-        `
-			So(t.Parse(content), ShouldErrLike, "second line is not blank")
+        `)
+			So(err, ShouldErrLike, "second line is not blank")
 		})
+	})
+
+	Convey(`ParsedEmailTemplate.Parse`, t, func() {
+		var t ParsedEmailTemplate
 
 		Convey(`invalid subject template`, func() {
-			content := `subject{{}}}
-
-        body`
-			So(t.Parse(content), ShouldErrLike, "missing value for command")
+			So(t.Parse("{{}}}", ""), ShouldErrLike, "subject:1: missing value for command")
 		})
 
 		Convey(`invalid body template`, func() {
-			content := `subject
+			So(t.Parse("", "{{!}}"), ShouldErrLike, `body:1: unexpected "!" in command`)
+		})
+	})
 
-        body {{!}}`
-			So(t.Parse(content), ShouldErrLike, `unexpected "!" in command`)
+	Convey("fetchAllEmailTemplates", t, func() {
+		c := gaetesting.TestingContextWithAppID("luci-config")
+		c = gologger.StdConfig.Use(c)
+		c = logging.SetLevel(c, logging.Debug)
+
+		cfgService := memory.New(map[config.Set]memory.Files{
+			"projects/x": {
+				"luci-config/email-templates/a.template":            "aSubject\n\naBody",
+				"luci-config/email-templates/b.template":            "bSubject\n\nbBody",
+				"luci-config/email-templates/invalid name.template": "subject\n\nbody",
+			},
+			"projects/y": {
+				"luci-config/email-templates/c.template": "cSubject\n\ncBody",
+			},
+		})
+		templates, err := fetchAllEmailTemplates(c, cfgService, "x")
+		So(err, ShouldBeNil)
+
+		So(templates, ShouldResemble, map[string]*EmailTemplate{
+			"a": {
+				Name:                "a",
+				SubjectTextTemplate: "aSubject",
+				BodyHTMLTemplate:    "aBody",
+			},
+			"b": {
+				Name:                "b",
+				SubjectTextTemplate: "bSubject",
+				BodyHTMLTemplate:    "bBody",
+			},
 		})
 	})
 }
