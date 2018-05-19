c7374ca27835ecc2ac412ba4de0d7e464467f855
Vadim Shtayura <vadimsh@chromium.org>
2018-05-18 18:11:20 -0700

    [skylark] Add 'mutable' helper function.
    It allows modules to keep a mutable internal state. Unlike Python (where module
    dict is always mutable), Skylark freezes module dicts after module loading. With
    'mutable' a module can get a handle to a value that doesn't get frozen.
    
    This would allow to prototype config generation mostly entirely in Skylark. Once
    we get to a final state we can move all mutable state into the Go part of the
    interpreter (this is how other Skylark embedders deal with it).
    
    R=smut@google.com
    BUG=833946
    
    Change-Id: Iff2ee6eb8ca6c8977841f13e90b94f8b9b112309

---
+++ skylark/interpreter/mutable_test.go
@@ -0,0 +1,63 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package interpreter
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMutable(t *testing.T) {
+	t.Parallel()
+
+	Convey("Works", t, func() {
+		_, logs, err := runIntr(intrParams{
+			stdlib: map[string]string{
+				"init.sky": `
+					load("builtin:imported.sky", "append", "collect")
+					append(1)
+					append(2)
+					print(collect())
+				`,
+				"imported.sky": `
+					_state = mutable([])
+					def append(i):
+						_state.get().append(i)
+					def collect():
+						return _state.get()
+				`,
+			},
+		})
+		So(err, ShouldBeNil)
+		So(logs, ShouldResemble, []string{"[builtin:init.sky:5] [1, 2]"})
+	})
+
+	Convey("Setter and boolean cast work", t, func() {
+		So(runScript(`
+			def test():
+				s = mutable()
+				if s:
+					fail("should be falsish")
+				s.set(123)
+				if s.get() != 123:
+					fail("should be 123")
+				if not s:
+					fail("should be trueish")
+
+			test()  # skylark doesn't allow 'if' in global scope, need a function
+		`), ShouldBeNil)
+	})
+}
