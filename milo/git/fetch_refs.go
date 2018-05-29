65a5f0ca66949ece6729084e3cf620696b1fa75a
Sergiy Byelozyorov <sergiyb@chromium.org>
2018-05-29 09:09:47 -0700

    Add support for regexp ref specification in milo
    R=hinoka@chromium.org, nodir@chromium.org
    
    Bug: 836232
    Change-Id: Iab421316f254b166b917675d5f18661268b6126b

---
+++ milo/git/fetch_refs.go
@@ -0,0 +1,123 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package git
+
+import (
+	"regexp"
+	"strings"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.chromium.org/luci/common/errors"
+	gitpb "go.chromium.org/luci/common/proto/git"
+	gitilespb "go.chromium.org/luci/common/proto/gitiles"
+	"go.chromium.org/luci/common/sync/parallel"
+)
+
+// Log implements Client interface.
+func (p *implementation) FetchRefs(c context.Context, host, project, startRef, refNamespace string, refSuffix *regexp.Regexp, limit int) (commits []*gitpb.Commit, err error) {
+	defer func() { err = errors.Annotate(tagError(c, err), "gitiles.FetchRefs").Err() }()
+
+	if refSuffix == nil {
+		err = errors.Reason("refSuffix is required").Err()
+		return
+	}
+
+	allowed, err := p.acls.IsAllowed(c, host, project)
+	switch {
+	case err != nil:
+		return
+	case !allowed:
+		err = status.Errorf(codes.NotFound, "not found")
+		return
+	}
+
+	// Step 1. Request all refs in refNamespace.
+	client, err := p.gitilesClient(c, host)
+	if err != nil {
+		return
+	}
+
+	req := gitilespb.RefsRequest{Project: project, RefsPath: refNamespace}
+	res, err := client.Refs(c, &req)
+	if err != nil {
+		return
+	}
+
+	// Step 2. Filter out those that match refSuffix.
+	matchingRefs := map[string]string{}
+	for ref, commit := range res.Revisions {
+		suffix := strings.TrimPrefix(ref, refNamespace)
+		if refSuffix.MatchString(suffix) {
+			matchingRefs[ref] = commit
+		}
+	}
+
+	// Step 3. Fetch commits from all matching refs using p.log(c, host, project,
+	//         ref, startRef, &LogOptins{Limit: limit})
+	// TODO(sergiyb): Implement caching in memcache, i.e. store (ref+commit) ->
+	// (list of commits) mapping and call Gitiles only if ref moves.
+	logs := map[string][]*gitpb.Commit{}
+	if err = parallel.FanOutIn(func(ch chan<- func() error) {
+		for ref, commit := range matchingRefs {
+			ch <- func() (err error) {
+				logs[ref], err = p.log(
+					c, host, project, commit, startRef, &LogOptions{Limit: limit})
+				return
+			}
+		}
+	}); err != nil {
+		return
+	}
+
+	// Step 4. Merge commits from all refs sorted by time into a single
+	//         mergedCommits list up to a limit.
+	commits = make([]*gitpb.Commit, limit)
+	for {
+		if len(commits) == limit {
+			return
+		}
+
+		commitTime := func(commit *gitpb.Commit) uint64 {
+			if commit == nil {
+				return 0
+			}
+
+			// This should not overflow for the next 500 years or so.
+			return uint64(commit.Committer.Time.Seconds)*1e9 + uint64(commit.Committer.Time.Nanos)
+		}
+
+		var c *gitpb.Commit = nil
+		var refName string
+		for ref, commits := range logs {
+			if len(commits) > 0 && commitTime(commits[0]) > commitTime(c) {
+				c = commits[0]
+				refName = ref
+			}
+		}
+
+		// We ran out of commits on all refs.
+		if c == nil {
+			return
+		}
+
+		logs[refName] = logs[refName][1:]
+		commits = append(commits, c)
+	}
+
+	return
+}
---
+++ milo/git/fetch_refs_test.go
@@ -0,0 +1,106 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package git
+
+import (
+	"encoding/hex"
+	"regexp"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"golang.org/x/net/context"
+
+	"go.chromium.org/gae/impl/memory"
+	"go.chromium.org/luci/auth/identity"
+	gitpb "go.chromium.org/luci/common/proto/git"
+	gitilespb "go.chromium.org/luci/common/proto/gitiles"
+	"go.chromium.org/luci/milo/api/config"
+	"go.chromium.org/luci/milo/git/gitacls"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/auth/authtest"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFetchRefs(t *testing.T) {
+	t.Parallel()
+
+	Convey("FetchRefs", t, func() {
+		c := memory.Use(context.Background())
+
+		ctl := gomock.NewController(t)
+		defer ctl.Finish()
+		gitilesMock := gitilespb.NewMockGitilesClient(ctl)
+
+		host := "limited.googlesource.com"
+		acls, err := gitacls.FromConfig(c, []*config.Settings_SourceAcls{
+			{Hosts: []string{host}, Readers: []string{"allowed@example.com"}},
+		})
+		So(err, ShouldBeNil)
+		impl := implementation{mockGitiles: gitilesMock, acls: acls}
+		c = Use(c, &impl)
+		cAllowed := auth.WithState(c, &authtest.FakeState{Identity: "user:allowed@example.com"})
+		cDenied := auth.WithState(c, &authtest.FakeState{Identity: identity.AnonymousIdentity})
+
+		Convey("refSuffix must be required", func() {
+			_, err := impl.FetchRefs(
+				cAllowed, host, "project", "refs/heads/master", "refs/branch-heads/", nil, 50)
+			So(err, ShouldNotBeNil)
+		})
+
+		fakeCommits := make([]*gitpb.Commit, 255)
+		commitID := make([]byte, 20)
+		commitID[0] = 255
+		for i := range fakeCommits {
+			fakeCommits[i] = &gitpb.Commit{Id: hex.EncodeToString(commitID)}
+			if i%10 > 0 { // every 10th commit has no parent (another branch)
+				fakeCommits[i-1].Parents = []string{fakeCommits[i].Id}
+			}
+
+			commitID[0]--
+		}
+
+		suffixRegexp, err := regexp.Compile(`\d+\.\d+`)
+		So(err, ShouldBeNil)
+
+		Convey("ACLs respected", func() {
+			_, err := impl.FetchRefs(
+				cDenied, host, "project", "refs/heads/master", "refs/branch-heads/",
+				suffixRegexp, 50)
+			So(err.Error(), ShouldContainSubstring, "not found")
+		})
+
+		Convey("one ref matches", func() {
+			gitilesMock.EXPECT().Refs(gomock.Any(), &gitilespb.RefsRequest{
+				Project: "project", RefsPath: "refs/branch-heads/",
+			}).Return(&gitilespb.RefsResponse{
+				Revisions: map[string]string{"refs/branch-heads/1.1": fakeCommits[0].Id},
+			}, nil)
+
+			gitilesMock.EXPECT().Log(gomock.Any(), &gitilespb.LogRequest{
+				Project: "project", Treeish: "refs/branch-heads/1.1",
+				PageSize: 100, Ancestor: "refs/heads/master", TreeDiff: false,
+			}).Return(&gitilespb.LogResponse{
+				Log: fakeCommits[0:5],
+			}, nil)
+
+			commits, err := impl.FetchRefs(
+				cAllowed, host, "project", "refs/heads/master", "refs/branch-heads/",
+				suffixRegexp, 50)
+			So(err, ShouldBeNil)
+			So(commits, ShouldResemble, fakeCommits[0:5])
+		})
+	})
+}
