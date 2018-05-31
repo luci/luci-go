655c20427e3866cc093daf1414025b0c7bbed39f
Michael Anthony Knyszek <mknyszek@google.com>
2018-05-30 17:19:28 -0700

    [luci_notify] Compute blamelist from source manifests
    This change allows luci-notify to compute a blamelist from source
    manifests by querying LogDog by content type. It assumes that a given
    build has at most one source manifest.
    
    Change-Id: I1d93dffd24678b9b405b9e3200f85adade89e4a3

---
+++ luci_notify/notify/srcman.go
@@ -0,0 +1,101 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package notify
+
+import (
+	"bytes"
+	"net/http"
+	"path"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/common/api/gitiles"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/proto/srcman"
+	"go.chromium.org/luci/grpc/prpc"
+	"go.chromium.org/luci/logdog/client/coordinator"
+	"go.chromium.org/luci/logdog/common/renderer"
+	"go.chromium.org/luci/logdog/common/types"
+	"go.chromium.org/luci/server/auth"
+)
+
+func getCommits(c context.Context, build *Build) (map[string]string, error) {
+	transport, err := auth.GetRPCTransport(c, auth.AsSelf, nil)
+	if err != nil {
+		return nil, errors.Annotate(err, "getting RPC Transport").Err()
+	}
+	client := coordinator.NewClient(&prpc.Client{
+		C:       &http.Client{Transport: transport},
+		Host:    build.Infra.Logdog.Hostname,
+		Options: prpc.DefaultOptions(),
+	})
+	qo := coordinator.QueryOptions{
+		ContentType: srcman.ContentTypeSourceManifest,
+	}
+	logProject := types.ProjectName(build.Infra.Logdog.Project)
+	logPath := path.Join(build.Infra.Logdog.Prefix, "*")
+
+	// Perform the query, capturing exactly one log stream and erroring otherwise.
+	var log *coordinator.LogStream
+	err = client.Query(c, logProject, logPath, qo, func(s *coordinator.LogStream) bool {
+		log = s
+		return false
+	})
+	if err == nil {
+		return nil, err
+	}
+
+	// Read the source manifest from the log stream.
+	var buf bytes.Buffer
+	_, err = buf.ReadFrom(&renderer.Renderer{
+		Source: client.Stream(log.Project, log.Path).Fetcher(c, nil),
+		Raw:    true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the source manifest from the bytes.
+	var manifest srcman.Manifest
+	if err := proto.Unmarshal(buf.Bytes(), &manifest); err != nil {
+		return nil, err
+	}
+
+	results := make(map[string]string)
+	for dirname, dir := range manifest.Directories {
+		dirname := dirname
+
+		gitCheckout := dir.GetGitCheckout()
+		if gitCheckout == nil {
+			continue
+		}
+
+		_, host, err := gitiles.ParseRepoURL(gitCheckout.RepoUrl)
+		if err != nil {
+			logging.WithError(err).Warningf(c, "could not parse RepoURL %q for dir %q", gitCheckout.RepoUrl, dirname)
+			continue
+		}
+
+		if !strings.HasSuffix(host, ".googlesource.com") {
+			logging.WithError(err).Warningf(c, "unsupported git host %q for dir %q", gitCheckout.RepoUrl, dirname)
+			continue
+		}
+		results[gitCheckout.RepoUrl] = gitCheckout.Revision
+	}
+	return results, nil
+}
