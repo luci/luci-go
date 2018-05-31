f26275fb0a0211e94a3cd8099a3b2de2d6d43dc0
smut <smut@google.com>
2018-05-30 17:05:41 -0700

    [Isolate] Update client to hit /_ah/api instead of /api
    The webapp2 adapter is now serving at /_ah/api, the old Endpoints v1 path.
    
    Bug: 805543
    Change-Id: I286de3b75d14ad051b4c96722f77607bb2eebf01
    Reviewed-on: https://chromium-review.googlesource.com/1080231
    Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>

--- common/isolatedclient/isolatedclient.go
+++ common/isolatedclient/isolatedclient.go
@@ -120,7 +120,7 @@ func New(anonClient, authClient *http.Client, host, namespace string, rFn retry.
 // ServerCapabilities returns the server details.
 func (i *Client) ServerCapabilities(c context.Context) (*isolateservice.HandlersEndpointsV1ServerDetails, error) {
 	out := &isolateservice.HandlersEndpointsV1ServerDetails{}
-	if err := i.postJSON(c, "/api/isolateservice/v1/server_details", nil, map[string]string{}, out); err != nil {
+	if err := i.postJSON(c, "/_ah/api/isolateservice/v1/server_details", nil, map[string]string{}, out); err != nil {
 		return nil, err
 	}
 	return out, nil
@@ -136,7 +136,7 @@ func (i *Client) Contains(c context.Context, items []*isolateservice.HandlersEnd
 	in := isolateservice.HandlersEndpointsV1DigestCollection{Items: items, Namespace: &isolateservice.HandlersEndpointsV1Namespace{}}
 	in.Namespace.Namespace = i.namespace
 	data := &isolateservice.HandlersEndpointsV1UrlCollection{}
-	if err = i.postJSON(c, "/api/isolateservice/v1/preupload", nil, in, data); err != nil {
+	if err = i.postJSON(c, "/_ah/api/isolateservice/v1/preupload", nil, in, data); err != nil {
 		return nil, err
 	}
 	out = make([]*PushState, len(items))
@@ -174,7 +174,7 @@ func (i *Client) Push(c context.Context, state *PushState, source Source) (err e
 		// stored files).
 		in := isolateservice.HandlersEndpointsV1FinalizeRequest{UploadTicket: state.status.UploadTicket}
 		headers := map[string]string{"Cache-Control": "public, max-age=31536000"}
-		if err = i.postJSON(c, "/api/isolateservice/v1/finalize_gs_upload", headers, in, nil); err != nil {
+		if err = i.postJSON(c, "/_ah/api/isolateservice/v1/finalize_gs_upload", headers, in, nil); err != nil {
 			log.Printf("Push(%s) (finalize) failed: %s\n%#v", state.digest, err, state)
 			return
 		}
@@ -186,7 +186,7 @@ func (i *Client) Push(c context.Context, state *PushState, source Source) (err e
 // Fetch downloads an item from the server.
 func (i *Client) Fetch(c context.Context, digest isolated.HexDigest, dest io.Writer) error {
 	// Perform initial request.
-	url := i.url + "/api/isolateservice/v1/retrieve"
+	url := i.url + "/_ah/api/isolateservice/v1/retrieve"
 	in := &isolateservice.HandlersEndpointsV1RetrieveRequest{
 		Digest: string(digest),
 		Namespace: &isolateservice.HandlersEndpointsV1Namespace{
@@ -266,7 +266,7 @@ func (i *Client) doPushDB(c context.Context, state *PushState, reader io.Reader)
 		UploadTicket: state.status.UploadTicket,
 		Content:      base64.StdEncoding.EncodeToString(buf.Bytes()),
 	}
-	return i.postJSON(c, "/api/isolateservice/v1/store_inline", nil, in, nil)
+	return i.postJSON(c, "/_ah/api/isolateservice/v1/store_inline", nil, in, nil)
 }
 
 // defaultGCSHandler implements the default Fetch and Push handlers for
