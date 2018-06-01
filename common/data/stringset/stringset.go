b93923db528bba219a1c2e3763951769057d869a
Marc-Antoine Ruel <maruel@chromium.org>
2018-06-01 17:53:14 -0400

    stringset: switch Set to a concrete type
    There had been a plan to make a concurrent safe Set implementation but it's not
    happening. Replace the interface Set with the concrete type set, which turns the
    method through an interface to direct calls, which can be inlined by the
    compiler.
    
    Because of https://github.com/golang/go/issues/19348, inline all the functions
    (it doesn't make the code longer!) to ensure that most methods can be inlined.
    
    Change-Id: Ib8dd36a16ab57c1383edd236ad62b78b4a10091c
    Reviewed-on: https://chromium-review.googlesource.com/1083531
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>

---
+++ common/data/stringset/stringset.go
@@ -0,0 +1,161 @@
+// Copyright 2015 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package stringset
+
+// Set is the base type. make(Set) can be used too.
+type Set map[string]struct{}
+
+// New returns a new string Set implementation.
+func New(sizeHint int) Set {
+	return make(Set, sizeHint)
+}
+
+// NewFromSlice returns a new string Set implementation,
+// initialized with the values in the provided slice.
+func NewFromSlice(vals ...string) Set {
+	ret := make(Set, len(vals))
+	for _, k := range vals {
+		ret[k] = struct{}{}
+	}
+	return ret
+}
+
+// Has returns true iff the Set contains value.
+func (s Set) Has(value string) bool {
+	_, ret := s[value]
+	return ret
+}
+
+// Add ensures that Set contains value, and returns true if it was added (i.e.
+// it returns false if the Set already contained the value).
+func (s Set) Add(value string) bool {
+	if _, ok := s[value]; ok {
+		return false
+	}
+	s[value] = struct{}{}
+	return true
+}
+
+// Del removes value from the set, and returns true if it was deleted (i.e. it
+// returns false if the Set did not already contain the value).
+func (s Set) Del(value string) bool {
+	if _, ok := s[value]; !ok {
+		return false
+	}
+	delete(s, value)
+	return true
+}
+
+// Peek returns an arbitrary element from the set. If the set was empty, this
+// returns ("", false).
+func (s Set) Peek() (string, bool) {
+	for k := range s {
+		return k, true
+	}
+	return "", false
+}
+
+// Peek removes and returns an arbitrary element from the set. If the set was
+// empty, this returns ("", false).
+func (s Set) Pop() (string, bool) {
+	for k := range s {
+		delete(s, k)
+		return k, true
+	}
+	return "", false
+}
+
+// Iter calls `cb` for each item in the set. If `cb` returns false, the
+// iteration stops.
+func (s Set) Iter(cb func(string) bool) {
+	for k := range s {
+		if !cb(k) {
+			break
+		}
+	}
+}
+
+// Len returns the number of items in this set.
+func (s Set) Len() int {
+	return len(s)
+}
+
+// Dup returns a duplicate set.
+func (s Set) Dup() Set {
+	ret := make(Set, len(s))
+	for k := range s {
+		ret[k] = struct{}{}
+	}
+	return ret
+}
+
+// ToSlice renders this set to a slice of all values.
+func (s Set) ToSlice() []string {
+	ret := make([]string, 0, len(s))
+	for k := range s {
+		ret = append(ret, k)
+	}
+	return ret
+}
+
+// Intersect returns a new Set which is the intersection of this set with the
+// other set.
+//
+// `other` must have the same underlying type as the current set, or this will
+// panic.
+func (s Set) Intersect(other Set) Set {
+	smallLen := len(s)
+	if lo := len(other); lo < smallLen {
+		smallLen = lo
+	}
+	ret := make(Set, smallLen)
+	for k := range s {
+		if _, ok := other[k]; ok {
+			ret[k] = struct{}{}
+		}
+	}
+	return ret
+}
+
+// Difference returns a new Set which is this set with all elements from other
+// removed (i.e. `self - other`).
+//
+// `other` must have the same underlying type as the current set, or this will
+// panic.
+func (s Set) Difference(other Set) Set {
+	ret := make(Set)
+	for k := range s {
+		if _, ok := other[k]; !ok {
+			ret[k] = struct{}{}
+		}
+	}
+	return ret
+}
+
+// Union returns a new Set which contains all element from this set, as well
+// as all elements from the other set.
+//
+// `other` must have the same underlying type as the current set, or this will
+// panic.
+func (s Set) Union(other Set) Set {
+	ret := make(Set, len(s))
+	for k := range s {
+		ret[k] = struct{}{}
+	}
+	for k := range other {
+		ret[k] = struct{}{}
+	}
+	return ret
+}
