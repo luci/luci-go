d194eb2be0f6f4c7fe282dbaff05a74e31145b75
Vadim Shtayura <vadimsh@chromium.org>
2018-06-05 16:41:45 -0700

    [cipd] Add VisitMetadata function to metadata.Storage interface.
    Only mock implementation for now. Also add more tests and cleanup the mock
    implementation a little bit (by de-adhocing handling or the root).
    
    It will be used to discover a visible subset of the package space when listing
    packages.
    
    R=tandrii@chromium.org, nodir@chromium.org
    BUG=801423
    
    Change-Id: I613cdb8493c966a3ad4633de6d6c4b7ddf8971e4

--- cipd/appengine/impl/metadata/metadata.go
+++ cipd/appengine/impl/metadata/metadata.go
@@ -20,11 +20,20 @@ import (
 	api "go.chromium.org/luci/cipd/api/cipd/v1"
 )
 
+// Visiter is a callback passed to VisitMetadata.
+//
+// It decides whether to continue exploring this metadata subtree or not.
+type Visiter func(prefix string, md []*api.PrefixMetadata) (cont bool, err error)
+
 // Storage knows how to store, fetch and update prefix metadata, as well as
 // how to calculate its fingerprint.
 //
-// Doesn't try to understand what metadata means, just fingerprints and stores
-// it.
+// The metadata is organizes in a forest-like structure, where each node is
+// associated with some package prefix (e.g. has a name "a/b/c"). The single
+// root ("") may or may not be present, depending on the implementation.
+//
+// Doesn't try to understand what metadata means, just fingerprints, stores and
+// enumerates it.
 //
 // This functionality is organized into an interface to simplify mocking. Use
 // GetStorage to grab a real implementation.
@@ -51,6 +60,22 @@ type Storage interface {
 	// Returns a fatal error if the prefix is malformed, all other errors are
 	// transient.
 	GetMetadata(c context.Context, prefix string) ([]*api.PrefixMetadata, error)
+
+	// VisitMetadata enumerates the metadata in breadth-first order.
+	//
+	// Can be used to fetch all metadata items at and under the given prefix.
+	//
+	// The callback is called for each visited node (starting from 'prefix' itself
+	// if metadata for it exists), receiving same metadata list as if GetMetadata
+	// was used to fetch it. The callback can decide whether to proceed with the
+	// enumeration of the corresponding subtree or skip it (by returning either
+	// true or false).
+	//
+	// Abort the traversal on a first error from the callback.
+	//
+	// Returns either a transient error if fetching failed, or whatever error the
+	// callback returned.
+	VisitMetadata(c context.Context, prefix string, cb Visiter) error
 
 	// UpdateMetadata transactionally updates or creates metadata of some
 	// prefix and returns it.
--- cipd/appengine/impl/testutil/metadata.go
+++ cipd/appengine/impl/testutil/metadata.go
@@ -15,6 +15,7 @@
 package testutil
 
 import (
+	"container/list"
 	"sort"
 	"strings"
 	"sync"
@@ -28,9 +29,12 @@ import (
 )
 
 // MetadataStore implements metadata.Storage using memory, for tests.
+//
+// Not terribly efficient, thus shouldn't be used with large number of entries.
 type MetadataStore struct {
-	l     sync.Mutex
-	metas map[string]*api.PrefixMetadata // prefix with trailing '/' => metadata
+	l      sync.Mutex
+	metas  map[string]*api.PrefixMetadata // prefix with trailing '/' => metadata
+	sorted []string                       // sorted list of keys in 'metas'
 }
 
 // Populate adds a metadata entry to the storage.
@@ -55,42 +59,140 @@ func (s *MetadataStore) Populate(prefix string, m *api.PrefixMetadata) *api.Pref
 //
 // Panics if the prefix is bad. Purging missing metadata is noop.
 func (s *MetadataStore) Purge(prefix string) {
-	prefix, err := common.ValidatePackagePrefix(prefix)
+	prefix, err := normPrefix(prefix)
 	if err != nil {
 		panic(err)
 	}
-	prefix += "/"
 
 	s.l.Lock()
 	defer s.l.Unlock()
 
-	delete(s.metas, prefix)
+	if _, ok := s.metas[prefix]; ok {
+		delete(s.metas, prefix)
+		s.rebuildSorted()
+	}
+}
+
+// rebuildSorted recalculates s.sorted based on s.metas.
+//
+// Should be called under the lock.
+func (s *MetadataStore) rebuildSorted() {
+	s.sorted = s.sorted[:0]
+	for k := range s.metas {
+		s.sorted = append(s.sorted, k)
+	}
+	sort.Strings(s.sorted)
 }
 
 // GetMetadata fetches metadata associated with the given prefix and all
 // parent prefixes.
 func (s *MetadataStore) GetMetadata(c context.Context, prefix string) ([]*api.PrefixMetadata, error) {
-	prefix, err := common.ValidatePackagePrefix(prefix)
+	prefix, err := normPrefix(prefix)
 	if err != nil {
 		return nil, err
 	}
-	prefix += "/"
 
 	s.l.Lock()
 	defer s.l.Unlock()
 
 	var metas []*api.PrefixMetadata
-	for p, meta := range s.metas {
-		// Note: "/" is indicator of the root metadata.
-		if p == "/" || strings.HasPrefix(prefix, p) {
-			metas = append(metas, cloneMetadata(meta))
+	for _, p := range s.sorted {
+		if strings.HasPrefix(prefix, p) {
+			metas = append(metas, cloneMetadata(s.metas[p]))
+		}
+	}
+	return metas, nil
+}
+
+// VisitMetadata performs breadth-first enumeration of the metadata graph.
+func (s *MetadataStore) VisitMetadata(c context.Context, prefix string, cb metadata.Visiter) error {
+	prefix, err := normPrefix(prefix)
+	if err != nil {
+		return err
+	}
+
+	// Start with the requested prefix if it exists, otherwise start with all
+	// its direct descendants.
+	var next stringDeque
+	if s.exists(prefix) {
+		next.PushBack(prefix)
+	} else {
+		for _, p := range s.directDescendants(prefix) {
+			next.PushBack(p)
 		}
 	}
 
-	sort.Slice(metas, func(i, j int) bool {
-		return metas[i].Prefix < metas[j].Prefix
-	})
-	return metas, nil
+	// Note that we don't hold the lock here, to avoid deadlocking if 'cb' calls
+	// other MetadataStore methods.
+	for next.Len() > 0 {
+		pfx := next.PopFront()
+
+		// Convert the prefix back to the form expected by the public API.
+		clean := strings.TrimSuffix(strings.TrimPrefix(pfx, "/"), "/")
+
+		md, err := s.GetMetadata(c, clean)
+		if err != nil {
+			return err
+		}
+
+		switch cont, err := cb(clean, md); {
+		case err != nil:
+			return err
+		case cont:
+			// Asked to explore this subtree, add the children to the queue of nodes
+			// to visit.
+			for _, p := range s.directDescendants(pfx) {
+				next.PushBack(p)
+			}
+		}
+	}
+	return nil
+}
+
+// exists returns true if metadata for prefix 'p' is registered.
+//
+// 'p' assumed to be already normalized via normPrefix.
+func (s *MetadataStore) exists(p string) bool {
+	s.l.Lock()
+	defer s.l.Unlock()
+	_, ok := s.metas[p]
+	return ok
+}
+
+// directDescendants returns registered prefixes that are directly under 'p'.
+//
+// "Directly under" here means that if A is "directly under" B, then B is a
+// prefix of A, and there's no such C that A is a prefix of C and C is a prefix
+// of B.
+//
+// Both 'p' and returned prefixes assumed to be in the normalized form (see
+// normPrefix). The returned list is sorted already.
+func (s *MetadataStore) directDescendants(p string) []string {
+	s.l.Lock()
+	defer s.l.Unlock()
+
+	// Note: this can be optimized using a smarter data structures and algorithms,
+	// but we don't care since MetadataStore is for unit tests only.
+	var out []string
+	for _, pfx := range s.sorted {
+		if p == pfx || !strings.HasPrefix(pfx, p) {
+			continue
+		}
+
+		// Maybe we've discovered a smaller prefix already. If so, 'pfx' is not a
+		// direct descendant and we should skip it.
+		keep := true
+		for _, discovered := range out {
+			if strings.HasPrefix(pfx, discovered) {
+				keep = false
+				break
+			}
+		}
+		if keep {
+			out = append(out, pfx)
+		}
+	}
+	return out
 }
 
 // UpdateMetadata transactionally updates or creates metadata of some
@@ -104,9 +206,9 @@ func (s *MetadataStore) UpdateMetadata(c context.Context, prefix string, cb func
 	s.l.Lock()
 	defer s.l.Unlock()
 
-	key := prefix + "/"
-	before := s.metas[key] // the metadata before the callback
-	if before == nil {
+	key, _ := normPrefix(prefix)
+	before, existed := s.metas[key] // the metadata before the callback
+	if !existed {
 		before = &api.PrefixMetadata{Prefix: prefix}
 	}
 
@@ -123,7 +225,7 @@ func (s *MetadataStore) UpdateMetadata(c context.Context, prefix string, cb func
 	// No changes at all? Return nil if the metadata didn't exist and wasn't
 	// created by the callback. Otherwise return the existing metadata.
 	if proto.Equal(before, meta) {
-		if before.Fingerprint == "" {
+		if !existed {
 			return nil, nil
 		}
 		return meta, nil
@@ -135,9 +237,54 @@ func (s *MetadataStore) UpdateMetadata(c context.Context, prefix string, cb func
 		s.metas = make(map[string]*api.PrefixMetadata, 1)
 	}
 	s.metas[key] = cloneMetadata(meta)
+
+	// Rebuild 'sorted'. Note that this can be optimized by using sort.Search and
+	// insertion into the existing array. But we don't care, len(metas) should be
+	// tiny.
+	if !existed {
+		s.rebuildSorted()
+	}
+
 	return meta, nil
+}
+
+// normPrefix takes "a/b/c" and returns "/a/b/c/".
+//
+// For "" returns "/".
+func normPrefix(p string) (string, error) {
+	p, err := common.ValidatePackagePrefix(p)
+	if err != nil {
+		return "", err
+	}
+	if p == "" {
+		return "/", nil
+	}
+	return "/" + p + "/", nil
 }
 
 func cloneMetadata(m *api.PrefixMetadata) *api.PrefixMetadata {
 	return proto.Clone(m).(*api.PrefixMetadata)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type stringDeque struct {
+	l list.List
+}
+
+func (d *stringDeque) Len() int {
+	return d.l.Len()
+}
+
+func (d *stringDeque) PushBack(s string) {
+	d.l.PushBack(s)
+}
+
+func (d *stringDeque) PopFront() string {
+	e := d.l.Front()
+	if e == nil {
+		panic("popping from an empty stringDeque")
+	}
+	d.l.Remove(e)
+	return e.Value.(string)
 }
