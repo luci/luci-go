ca0cbe7a71b5ad72d7db419719b86225f0826045
Vadim Shtayura <vadimsh@chromium.org>
2018-06-05 17:04:29 -0700

    [cipd] Add VisitMetadata function to metadata.Storage interface.
    Only mock implementation for now. Also add more tests and cleanup the mock
    implementation a little bit (by de-adhocing handling or the root).
    
    It will be used to discover a visible subset of the package space when listing
    packages.
    
    R=tandrii@chromium.org, nodir@chromium.org
    BUG=801423
    
    Change-Id: I613cdb8493c966a3ad4633de6d6c4b7ddf8971e4

--- cipd/appengine/impl/testutil/metadata_test.go
+++ cipd/appengine/impl/testutil/metadata_test.go
@@ -29,6 +29,22 @@ func TestMetadataStore(t *testing.T) {
 
 	ctx := context.Background()
 
+	populateMD := func(s *MetadataStore, prefixes []string) {
+		for _, p := range prefixes {
+			s.Populate(p, &api.PrefixMetadata{UpdateUser: "user:someone@example.com"})
+		}
+	}
+
+	getMD := func(s *MetadataStore, pfx string) []string {
+		md, err := s.GetMetadata(ctx, pfx)
+		So(err, ShouldBeNil)
+		out := make([]string, len(md))
+		for i, m := range md {
+			out[i] = m.Prefix
+		}
+		return out
+	}
+
 	Convey("Works", t, func() {
 		s := MetadataStore{}
 
@@ -167,5 +183,103 @@ func TestMetadataStore(t *testing.T) {
 		metas, err = s.GetMetadata(ctx, "a/b/c")
 		So(err, ShouldBeNil)
 		So(metas, ShouldResemble, []*api.PrefixMetadata{rootMeta, abMeta})
+	})
+
+	Convey("GetMetadata filters by prefix correctly", t, func() {
+		s := MetadataStore{}
+		populateMD(&s, []string{"", "a", "ab", "a/b", "b", "a/b/c"})
+
+		So(getMD(&s, ""), ShouldResemble, []string{""})
+		So(getMD(&s, "a"), ShouldResemble, []string{"", "a"})
+		So(getMD(&s, "a/b"), ShouldResemble, []string{"", "a", "a/b"})
+		So(getMD(&s, "a/b/c"), ShouldResemble, []string{"", "a", "a/b", "a/b/c"})
+	})
+
+	Convey("Purge works", t, func() {
+		s := MetadataStore{}
+		populateMD(&s, []string{"", "a", "a/b", "a/b/c"})
+
+		s.Purge("a/b")
+		So(getMD(&s, "a/b/c"), ShouldResemble, []string{"", "a", "a/b/c"})
+
+		s.Purge("")
+		So(getMD(&s, "a/b/c"), ShouldResemble, []string{"a", "a/b/c"})
+
+		s.Purge("a/b/c")
+		So(getMD(&s, "a/b/c"), ShouldResemble, []string{"a"})
+
+		s.Purge("a")
+		So(getMD(&s, "a/b/c"), ShouldResemble, []string{})
+	})
+
+	Convey("directDescendants works", t, func() {
+		s := MetadataStore{}
+		populateMD(&s, []string{
+			"", "a", "ab", "a/b", "a/b/c",
+			"b", "b/c/d/e", "b/c/d/f",
+		})
+
+		// Note: unlike public methods, directDescendants uses normalized prefixes
+		// that have leading and trailing '/'.
+		So(s.directDescendants("/"), ShouldResemble, []string{"/a/", "/ab/", "/b/"})
+		So(s.directDescendants("/a/"), ShouldResemble, []string{"/a/b/"})
+		So(s.directDescendants("/a/b/"), ShouldResemble, []string{"/a/b/c/"})
+		So(s.directDescendants("/a/b/c/"), ShouldResemble, []string(nil))
+		So(s.directDescendants("/b/"), ShouldResemble, []string{"/b/c/d/e/", "/b/c/d/f/"})
+	})
+
+	Convey("VisitMetadata visits everything", t, func() {
+		s := MetadataStore{}
+		populateMD(&s, []string{
+			"", "a", "ab", "a/b", "a/b/c",
+			"b", "b/c/d/e", "b/c/d/f",
+		})
+
+		var visited []string
+		s.VisitMetadata(ctx, "", func(p string, md []*api.PrefixMetadata) (bool, error) {
+			visited = append(visited, p)
+			return true, nil
+		})
+
+		So(visited, ShouldResemble, []string{
+			"", "a", "ab", "b",
+			"a/b", "b/c/d/e", "b/c/d/f",
+			"a/b/c",
+		})
+	})
+
+	Convey("VisitMetadata visits direct descendants if pfx is missing", t, func() {
+		s := MetadataStore{}
+		populateMD(&s, []string{"a", "ab", "a/b", "a/b/c", "a/c"})
+
+		var visited []string
+		s.VisitMetadata(ctx, "", func(p string, md []*api.PrefixMetadata) (bool, error) {
+			visited = append(visited, p)
+			return true, nil
+		})
+
+		So(visited, ShouldResemble, []string{
+			"a", "ab", "a/b", "a/c", "a/b/c",
+		})
+	})
+
+	Convey("VisitMetadata respects callback return value", t, func() {
+		s := MetadataStore{}
+		populateMD(&s, []string{
+			"1", "1/a", "1/a/b", "1/a/b/c",
+			"2", "2/a", "2/a/b", "2/a/b/c",
+		})
+
+		var visited []string
+		s.VisitMetadata(ctx, "", func(p string, md []*api.PrefixMetadata) (bool, error) {
+			visited = append(visited, p)
+			return len(md) <= 2, nil // explore no deeper than 3 levels
+		})
+
+		So(visited, ShouldResemble, []string{
+			"1", "2",
+			"1/a", "2/a",
+			"1/a/b", "2/a/b",
+		})
 	})
 }
