e653d6c2cf4c936a23533c478a7c5ce0a2fdbec0
Ryan Tseng <hinoka@google.com>
2018-06-07 15:42:22 -0700

    [cloudlogger] Add Stackdriver logging as a logger
    Change-Id: I51b0efaebdd2fad23a26b55b54b9f9391c9f637b

---
+++ common/logging/cloudlogger/config.go
@@ -0,0 +1,32 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudlogger
+
+import (
+	cloudLogging "cloud.google.com/go/logging"
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/common/logging"
+)
+
+// Use registers cloud loggers into the context.
+// Use takes two cloudLogging.Loggers, main and lines.
+// Main is required, but lines is optional.
+// * If main is specified, and lines is nil: All log entries will be sent to main.
+// * If both main and lines are specified: Logs with RequestKey in fields will go to main,
+//   and all other logs will go to lines.  This is required for combined request logs.
+func Use(c context.Context, main, lines *cloudLogging.Logger) context.Context {
+	return logging.SetFactory(c, GetFactory(main, lines))
+}
