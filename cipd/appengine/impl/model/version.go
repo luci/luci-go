ca8cdddbbba12a477467622ab2bffd51bf713bc3
Vadim Shtayura <vadimsh@chromium.org>
2018-06-15 02:32:29 +0000

    [cipd] Implement ResolveVersion helper function.
    It takes an instance ID, a tag or a ref and returns *Instance entity with this
    version.
    
    R=tandrii@chromium.org, nodir@chromium.org
    BUG=801423
    
    Change-Id: I30b76bb955addfee7dbddfc1f71fb7c3a9e55145
    Reviewed-on: https://chromium-review.googlesource.com/1102107
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

---
+++ cipd/appengine/impl/model/version.go
@@ -0,0 +1,74 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package model
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/grpc/grpcutil"
+
+	"go.chromium.org/luci/cipd/common"
+)
+
+// ResolveVersion takes a version identifier (an instance ID, a ref or a tag)
+// and resolves it into a concrete package instance, ensuring it exists.
+//
+// Returns gRPC-tagged errors:
+//   InvalidArgument if the version string format is invalid.
+//   NotFound if there's no such package or such version.
+//   FailedPrecondition if the tag resolves to multiple instances.
+func ResolveVersion(c context.Context, pkg, version string) (*Instance, error) {
+	// Pick a resolution method based on the format of the version string.
+	var iid string
+	var err error
+	switch {
+	case common.ValidateInstanceID(version) == nil:
+		iid = version
+	case common.ValidatePackageRef(version) == nil:
+		var ref *Ref
+		if ref, err = GetRef(c, pkg, version); err == nil {
+			iid = ref.InstanceID
+		}
+	case common.ValidateInstanceTag(version) == nil:
+		iid, err = ResolveTag(c, pkg, common.MustParseInstanceTag(version))
+	default:
+		return nil, errors.Reason("not a valid version identifier").Tag(grpcutil.InvalidArgumentTag).Err()
+	}
+
+	if err != nil {
+		// If there's no such ref or tag, maybe the package is missing completely.
+		if grpcutil.Code(err) == codes.NotFound {
+			if pkgErr := CheckPackageExists(c, pkg); pkgErr != nil {
+				return nil, pkgErr
+			}
+		}
+		return nil, err
+	}
+
+	// Verify the instance exists and fetch its details. This is particularly
+	// important for the case when 'version' is raw instance ID already. For
+	// cases of tags and refs, the instance MUST exist per checks we do in SetRef
+	// and AttachTags, but being defensive here doesn't hurt.
+	inst := &Instance{
+		InstanceID: iid,
+		Package:    PackageKey(c, pkg),
+	}
+	if err := CheckInstanceExists(c, inst); err != nil {
+		return nil, err
+	}
+	return inst, nil
+}
---
+++ cipd/appengine/impl/model/version_test.go
@@ -0,0 +1,169 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package model
+
+import (
+	"strings"
+	"testing"
+
+	"google.golang.org/grpc/codes"
+
+	"go.chromium.org/gae/service/datastore"
+	"go.chromium.org/luci/appengine/gaetesting"
+	"go.chromium.org/luci/grpc/grpcutil"
+
+	api "go.chromium.org/luci/cipd/api/cipd/v1"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestResolveVersion(t *testing.T) {
+	t.Parallel()
+
+	Convey("With datastore", t, func() {
+		ctx := gaetesting.TestingContext()
+		datastore.GetTestable(ctx).AutoIndex(true)
+
+		pkg := &Package{Name: "pkg"}
+		inst1 := &Instance{
+			InstanceID:   strings.Repeat("1", 40),
+			Package:      PackageKey(ctx, "pkg"),
+			RegisteredBy: "user:1@example.com",
+		}
+		inst2 := &Instance{
+			InstanceID:   strings.Repeat("2", 40),
+			Package:      PackageKey(ctx, "pkg"),
+			RegisteredBy: "user:2@example.com",
+		}
+		missing := &Instance{
+			InstanceID:   strings.Repeat("3", 40),
+			Package:      PackageKey(ctx, "pkg"),
+			RegisteredBy: "user:3@example.com",
+		}
+
+		So(datastore.Put(ctx,
+			pkg, inst1, inst2, // note: do not store 'missing' here
+			&Ref{
+				Name:       "latest",
+				Package:    PackageKey(ctx, "pkg"),
+				InstanceID: inst2.InstanceID,
+			},
+			&Ref{
+				Name:       "broken",
+				Package:    PackageKey(ctx, "pkg"),
+				InstanceID: missing.InstanceID,
+			},
+			&Tag{
+				ID:       TagID(&api.Tag{Key: "ver", Value: "1"}),
+				Instance: datastore.KeyForObj(ctx, inst1),
+				Tag:      "ver:1",
+			},
+			&Tag{
+				ID:       TagID(&api.Tag{Key: "ver", Value: "ambiguous"}),
+				Instance: datastore.KeyForObj(ctx, inst1),
+				Tag:      "ver:ambiguous",
+			},
+			&Tag{
+				ID:       TagID(&api.Tag{Key: "ver", Value: "ambiguous"}),
+				Instance: datastore.KeyForObj(ctx, inst2),
+				Tag:      "ver:ambiguous",
+			},
+			&Tag{
+				ID:       TagID(&api.Tag{Key: "ver", Value: "broken"}),
+				Instance: datastore.KeyForObj(ctx, missing),
+				Tag:      "ver:broken",
+			},
+		), ShouldBeNil)
+
+		Convey("Resolves instance ID", func() {
+			inst, err := ResolveVersion(ctx, "pkg", inst1.InstanceID)
+			So(err, ShouldBeNil)
+			So(inst, ShouldResemble, inst1)
+		})
+
+		Convey("Resolves ref", func() {
+			inst, err := ResolveVersion(ctx, "pkg", "latest")
+			So(err, ShouldBeNil)
+			So(inst, ShouldResemble, inst2)
+		})
+
+		Convey("Resolves tag", func() {
+			inst, err := ResolveVersion(ctx, "pkg", "ver:1")
+			So(err, ShouldBeNil)
+			So(inst, ShouldResemble, inst1)
+		})
+
+		Convey("Fails on unrecognized version format", func() {
+			_, err := ResolveVersion(ctx, "pkg", "::::")
+			So(grpcutil.Code(err), ShouldEqual, codes.InvalidArgument)
+			So(err, ShouldErrLike, "not a valid version identifier")
+		})
+
+		Convey("Handles missing instance", func() {
+			Convey("Via instance ID", func() {
+				_, err := ResolveVersion(ctx, "pkg", missing.InstanceID)
+				So(grpcutil.Code(err), ShouldEqual, codes.NotFound)
+				So(err, ShouldErrLike, "no such instance")
+			})
+			Convey("Via broken ref", func() {
+				_, err := ResolveVersion(ctx, "pkg", "broken")
+				So(grpcutil.Code(err), ShouldEqual, codes.NotFound)
+				So(err, ShouldErrLike, "no such instance")
+			})
+			Convey("Via broken tag", func() {
+				_, err := ResolveVersion(ctx, "pkg", "ver:broken")
+				So(grpcutil.Code(err), ShouldEqual, codes.NotFound)
+				So(err, ShouldErrLike, "no such instance")
+			})
+		})
+
+		Convey("Handles missing package", func() {
+			Convey("Via instance ID", func() {
+				_, err := ResolveVersion(ctx, "pkg2", inst1.InstanceID)
+				So(grpcutil.Code(err), ShouldEqual, codes.NotFound)
+				So(err, ShouldErrLike, "no such package")
+			})
+			Convey("Via ref", func() {
+				_, err := ResolveVersion(ctx, "pkg2", "latest")
+				So(grpcutil.Code(err), ShouldEqual, codes.NotFound)
+				So(err, ShouldErrLike, "no such package")
+			})
+			Convey("Via tag", func() {
+				_, err := ResolveVersion(ctx, "pkg2", "ver:1")
+				So(grpcutil.Code(err), ShouldEqual, codes.NotFound)
+				So(err, ShouldErrLike, "no such package")
+			})
+		})
+
+		Convey("Missing ref", func() {
+			_, err := ResolveVersion(ctx, "pkg", "missing")
+			So(grpcutil.Code(err), ShouldEqual, codes.NotFound)
+			So(err, ShouldErrLike, "no such ref")
+		})
+
+		Convey("Missing tag", func() {
+			_, err := ResolveVersion(ctx, "pkg", "ver:missing")
+			So(grpcutil.Code(err), ShouldEqual, codes.NotFound)
+			So(err, ShouldErrLike, "no such tag")
+		})
+
+		Convey("Ambiguous tag", func() {
+			_, err := ResolveVersion(ctx, "pkg", "ver:ambiguous")
+			So(grpcutil.Code(err), ShouldEqual, codes.FailedPrecondition)
+			So(err, ShouldErrLike, "ambiguity when resolving the tag")
+		})
+	})
+}
