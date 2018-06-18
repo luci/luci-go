4d483a2d769aea1b6559ae84c66dd62bfb66350a
Vadim Shtayura <vadimsh@chromium.org>
2018-06-18 09:16:24 -0700

    [cipd] Implement SearchInstances RPC.
    R=tandrii@chromium.org, nodir@chromium.org
    BUG=801423
    
    Change-Id: Id459d049918653e4a4aabaf3d15c06f4ca37517d

--- cipd/appengine/impl/model/query.go
+++ cipd/appengine/impl/model/query.go
@@ -15,13 +15,15 @@
 package model
 
 import (
-	"fmt"
-
 	"golang.org/x/net/context"
 
 	"go.chromium.org/gae/service/datastore"
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/retry/transient"
 
 	api "go.chromium.org/luci/cipd/api/cipd/v1"
+	"go.chromium.org/luci/cipd/common"
 )
 
 // SearchInstances lists instances of a package with all given tags attached.
@@ -30,5 +32,188 @@ import (
 // entity exists. Returns up to pageSize entities, plus non-nil cursor (if
 // there are more results). 'pageSize' must be positive.
 func SearchInstances(c context.Context, pkg string, tags []*api.Tag, pageSize int32, cursor datastore.Cursor) (out []*Instance, nextCur datastore.Cursor, err error) {
-	return nil, nil, fmt.Errorf("not implemented")
+	switch {
+	case len(tags) == 0:
+		panic("tags must not be empty")
+	case pageSize <= 0:
+		panic("pageSize must be positive")
+	}
+
+	// Due to the way our entities organized, we can't efficiently query by all
+	// tags at once. Instead we query by the first tag only, and then filter the
+	// result to keep only instances that have the rest of tags attached. We do it
+	// chunk by chunk, until we assemble the full page.
+
+	seen := stringset.New(len(tags))
+
+	// The principal tag we query by.
+	mainTag := common.JoinInstanceTag(tags[0])
+	seen.Add(mainTag)
+
+	// Rest of the tags, dedupped, in the original order (the order influences the
+	// query efficiency, see below).
+	auxTags := make([]*api.Tag, 0, len(tags)-1)
+	for i := 1; i < len(tags); i++ {
+		if t := common.JoinInstanceTag(tags[i]); !seen.Has(t) {
+			auxTags = append(auxTags, tags[i])
+			seen.Add(t)
+		}
+	}
+
+	for int32(len(out)) < pageSize {
+		// Iterate over query on mainTag. This returns keys of instances that have
+		// mainTag attached.
+		var page []*datastore.Key
+		page, cursor, err = queryByTag(c, pkg, mainTag, cursor, pageSize-int32(len(out)))
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// Filter out instances that don't have any of remaining tags[1:] tags
+		// attached.
+		//
+		// Note that we can potentially filter by all tags at once here, hitting
+		// len(page)*(len(tags)-1) entities in parallel. Instead we assume each
+		// additional tag noticeably limits number of results, so we sequentially
+		// filter the result tag by tag, eventually making fewer requests total.
+		//
+		// Assuming N = len(page), T = len(tags)-1, and k is a portion of a page
+		// left after filtering by one tag (e.g. k = 0.7 if 7 out of 10 results
+		// survive filtering by a tag):
+		//
+		// For parallel filtering we hit the following number of entities:
+		//    N * T.
+		//
+		// For sequential filtering we hit:
+		//    N * (1 + k + k^2 + ... + k^(T-1)) = N * (1.0 - k^(T-1)) / (1.0 - k).
+		//
+		// For N = 100, T = 4, k = 0.7 this results in:
+		//    400 instances hit via 1 RPC vs ~220 instances hit via 4 RPCs.
+		//
+		// The real life difference of course dramatically depends on k. But in
+		// general we expect the sequential strategy will be cheaper overall (in
+		// terms of combined Datastore + CPU cost). This hasn't been confirmed by
+		// hard numbers though.
+		for _, tag := range auxTags {
+			if len(page) == 0 {
+				break
+			}
+			page, err = filterByTag(c, page, tag)
+			if err != nil {
+				return nil, nil, err
+			}
+		}
+
+		// Fetch actual instance bodies. It is possible (though highly improbable),
+		// that some instances are gone already, so len(instances) may be less than
+		// len(page).
+		instances, err := fetchExistingInstances(c, page)
+		if err != nil {
+			return nil, nil, err
+		}
+		out = append(out, instances...)
+
+		if cursor == nil {
+			break // listed everything we could
+		}
+	}
+
+	return
+}
+
+// queryByTag returns keys of Instances that have the given tag attached.
+//
+// Returns up to 'pageSize' of results, along with a cursor to continue the
+// query or nil if it was the end of it.
+func queryByTag(c context.Context, pkg, tag string, cursor datastore.Cursor, pageSize int32) (
+	out []*datastore.Key,
+	next datastore.Cursor,
+	err error) {
+
+	// TODO(vadimsh): 'registered_ts' here is when the tag was attached. The
+	// callers likely expect results ordered by instance registration time. This
+	// is not possible currently. We'll need to copy Instance.registered_ts into
+	// InstanceTag entities, so we can order by instance registration time.
+
+	q := datastore.NewQuery("InstanceTag").
+		Ancestor(PackageKey(c, pkg)).
+		Eq("tag", tag).
+		Order("-registered_ts").
+		Limit(pageSize).
+		KeysOnly(true)
+	if cursor != nil {
+		q = q.Start(cursor)
+	}
+
+	err = datastore.Run(c, q, func(k *datastore.Key, cb datastore.CursorCB) error {
+		out = append(out, k.Parent())
+		if len(out) >= int(pageSize) {
+			if next, err = cb(); err != nil {
+				return err
+			}
+			return datastore.Stop
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, nil, errors.Annotate(err, "failed to query by tag %q", tag).Tag(transient.Tag).Err()
+	}
+	return
+}
+
+// filterByTag takes keys of Instance entities and keeps only ones that have the
+// given tag attached (preserving the order).
+func filterByTag(c context.Context, page []*datastore.Key, tag *api.Tag) ([]*datastore.Key, error) {
+	tagID := TagID(tag)
+	tagKeys := make([]*datastore.Key, len(page))
+	for i, inst := range page {
+		tagKeys[i] = datastore.NewKey(c, "InstanceTag", tagID, 0, inst)
+	}
+	// TODO(vadimsh): Need to actually fetch entity bodies and double check that
+	// Tag field matches 'tag' as a precaution against SHA1 collision on TagID.
+	exists, err := datastore.Exists(c, tagKeys)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed by filter by tag %q", tag).Tag(transient.Tag).Err()
+	}
+	filtered := page[:0]
+	for i, inst := range page {
+		if exists.Get(0, i) {
+			filtered = append(filtered, inst)
+		}
+	}
+	return filtered, nil
+}
+
+// fetchExistingInstances fetches Instance entities given their keys.
+//
+// Skips missing ones.
+func fetchExistingInstances(c context.Context, keys []*datastore.Key) ([]*Instance, error) {
+	instances := make([]*Instance, len(keys))
+	for i, k := range keys {
+		instances[i] = &Instance{
+			InstanceID: k.StringID(),
+			Package:    k.Parent(),
+		}
+	}
+
+	err := datastore.Get(c, instances)
+	if err == nil {
+		return instances, nil
+	}
+
+	merr, ok := err.(errors.MultiError)
+	if !ok {
+		return nil, errors.Annotate(err, "failed to fetch instances").Tag(transient.Tag).Err()
+	}
+
+	existing := instances[:0]
+	for i, inst := range instances {
+		switch err := merr[i]; {
+		case err == nil:
+			existing = append(existing, inst)
+		case err != datastore.ErrNoSuchEntity:
+			return nil, errors.Annotate(err, "failed to fetch instance %q", inst.InstanceID).Tag(transient.Tag).Err()
+		}
+	}
+	return existing, nil
 }
