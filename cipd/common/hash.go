c99ee16d07083fb1dfc15f734cad2f21c08910b1
Vadim Shtayura <vadimsh@chromium.org>
2018-07-17 19:57:36 -0700

    [cipd] Introduce SHA256 into the protocol.
    To preserve visual space in various UIs we encode SHA256 digests using base64
    encoding, resulting in instance IDs that are only 3 chars longer than
    SHA1-based.
    
    For example: qUiQTy8PR5uPgZdpSzAYSw0u0cHNKh7A-4XSmaGSpEc.
    
    After this change, the server is capable of processing SHA256 instances, but
    client is still operating only with SHA1 ones.
    
    R=iannucci@chromium.org, nodir@chromium.org
    BUG=821194
    
    Change-Id: Ie36ba38c6b99a64238bca39d19eb7feae01fa189

--- cipd/common/hash.go
+++ cipd/common/hash.go
@@ -16,7 +16,10 @@ package common
 
 import (
 	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
+	"fmt"
 	"hash"
 
 	"go.chromium.org/luci/common/errors"
@@ -32,13 +35,76 @@ import (
 // allowed to use some other hash algo.
 const DefaultHashAlgo = api.HashAlgo_SHA1
 
-// Supported algo => its digest length (in hex encoding) + factory function.
-var supportedAlgos = []struct {
-	digestLen int
-	hash      func() hash.Hash
+// Supported algo => its properties, a factory function and converters.
+//
+// All iidLen MUST be different. We use them to differentiate between different
+// hash algos when parsing IIDs in ValidateInstanceID and InstanceIDToObjectRef.
+var supportedAlgos = []*struct {
+	hash           func() hash.Hash
+	hexDigestLen   int                              // len of hex(digest blob) string
+	iidLen         int                              // len of IID produced by hexDigestToIID
+	hexDigestToIID func(hex string) (string, error) // note: when called, len(hex) == hexDigestLen
+	iidToHexDigest func(iid string) (string, error) // note: when called, len(iid) == iidLen
 }{
 	api.HashAlgo_HASH_ALGO_UNSPECIFIED: {},
-	api.HashAlgo_SHA1:                  {40, sha1.New},
+
+	api.HashAlgo_SHA1: {
+		hash:         sha1.New,
+		hexDigestLen: 40,
+		iidLen:       40,
+		hexDigestToIID: func(h string) (string, error) {
+			return h, nil
+		},
+		iidToHexDigest: func(iid string) (string, error) {
+			if err := checkIsHex(iid); err != nil {
+				return "", err
+			}
+			return iid, nil
+		},
+	},
+
+	api.HashAlgo_SHA256: {
+		hash:         sha256.New,
+		hexDigestLen: 64,
+		iidLen:       43,
+		hexDigestToIID: func(h string) (string, error) {
+			blob, err := hex.DecodeString(h)
+			if err != nil {
+				return "", err
+			}
+			return base64.RawURLEncoding.EncodeToString(blob), nil
+		},
+		iidToHexDigest: func(iid string) (string, error) {
+			blob, err := base64.RawURLEncoding.DecodeString(iid)
+			if err != nil {
+				return "", err
+			}
+			return hex.EncodeToString(blob), nil
+		},
+	},
+}
+
+// Maps len(instance ID string) to api.HashAlgo used to derive it.
+var algoByIIDLen map[int]api.HashAlgo
+
+func init() {
+	algoByIIDLen = make(map[int]api.HashAlgo, len(supportedAlgos)-1)
+	for i := 1; i < len(supportedAlgos); i++ { // skip UNSPECIFIED
+		props := supportedAlgos[i]
+		if algoByIIDLen[props.iidLen] != 0 {
+			panic(fmt.Sprintf("can't have two hashes with same IID length %d", props.iidLen))
+		}
+		algoByIIDLen[props.iidLen] = api.HashAlgo(i)
+	}
+}
+
+func checkIsHex(s string) error {
+	for _, c := range s {
+		if !((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f')) {
+			return fmt.Errorf("bad lowercase hex string %q, wrong char %c", s, c)
+		}
+	}
+	return nil
 }
 
 // NewHash returns a hash implementation or an error if the algo is unknown.
@@ -68,7 +134,7 @@ func ValidateHashAlgo(h api.HashAlgo) error {
 	case h == api.HashAlgo_HASH_ALGO_UNSPECIFIED:
 		return errors.Reason("the hash algorithm is not specified").
 			Tag(grpcutil.InvalidArgumentTag).Err()
-	case int(h) >= len(supportedAlgos) || supportedAlgos[h].digestLen == 0:
+	case int(h) >= len(supportedAlgos) || supportedAlgos[h].hexDigestLen == 0:
 		return errors.Reason("unsupported hash algorithm %d", h).
 			Tag(grpcutil.InvalidArgumentTag).Err()
 	}
@@ -89,17 +155,15 @@ func ValidateObjectRef(ref *api.ObjectRef) error {
 		return err
 	}
 
-	digestLen := supportedAlgos[ref.HashAlgo].digestLen
-	if len(ref.HexDigest) != digestLen {
-		return errors.Reason("invalid %s digest: expecting %d chars, got %d", ref.HashAlgo, digestLen, len(ref.HexDigest)).
+	hexDigestLen := supportedAlgos[ref.HashAlgo].hexDigestLen
+	if len(ref.HexDigest) != hexDigestLen {
+		return errors.Reason("invalid %s digest: expecting %d chars, got %d", ref.HashAlgo, hexDigestLen, len(ref.HexDigest)).
 			Tag(grpcutil.InvalidArgumentTag).Err()
 	}
 
-	for _, c := range ref.HexDigest {
-		if !((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f')) {
-			return errors.Reason("invalid %s digest: wrong char %c", ref.HashAlgo, c).
-				Tag(grpcutil.InvalidArgumentTag).Err()
-		}
+	if err := checkIsHex(ref.HexDigest); err != nil {
+		return errors.Annotate(err, "invalid %s digest", ref.HashAlgo).
+			Tag(grpcutil.InvalidArgumentTag).Err()
 	}
 
 	return nil
