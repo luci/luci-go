fc7a45e9ca10ad4931b46183ee404acaaf45f82d
Vadim Shtayura <vadimsh@chromium.org>
2018-08-02 16:50:38 -0700

    [cipd] Rename cipd.Verifier to cipd.Resolver, simplify and speed it up a bit.
    Its API is reduced to only 2 functions:
      * ResolvePackage
      * ResolveEnsureFile
    
    'ensure' package API has also been shrunk a bit.
    
    The version resolution is now also done in parallel (even in 'cipd ensure').
    
    Another speed up comes from skipping DescribeInstance calls for pins returned
    by ResolveVersion: the backend guarantees they are already valid.
    
    R=iannucci@chromium.org
    BUG=870166
    
    Change-Id: I3733ba1372669fb10db3ab38b9ab8ad91a758d54

--- cipd/client/cipd/ensure/file.go
+++ cipd/client/cipd/ensure/file.go
@@ -22,10 +22,12 @@ import (
 	"net/url"
 	"sort"
 	"strings"
+	"sync"
 	"unicode"
 
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/iotools"
+	"go.chromium.org/luci/common/sync/parallel"
 
 	"go.chromium.org/luci/cipd/client/cipd/template"
 	"go.chromium.org/luci/cipd/common"
@@ -104,12 +106,7 @@ func ParseFile(r io.Reader) (*File, error) {
 			settingsAllowed = false
 			pkg := PackageDef{tok1, tok2, lineNo}
 
-			_, err := pkg.Resolve(func(pkg, vers string) (common.Pin, error) {
-				return common.Pin{
-					PackageName: pkg,
-					InstanceID:  vers,
-				}, common.ValidateInstanceVersion(vers)
-			}, template.DefaultExpander())
+			_, err := pkg.Expand(template.DefaultExpander())
 			if err != nil && err != template.ErrSkipTemplate {
 				return nil, err
 			}
@@ -123,6 +120,22 @@ func ParseFile(r io.Reader) (*File, error) {
 
 	return ret, nil
 }
+
+// VersionResolver transforms a {PackageName, Version} tuple (corresponding to
+// the given `def`) into a resolved pin.
+//
+//  - `pkg` is guaranteed to pass common.ValidatePackageName
+//  - `vers` is guaranteed to pass common.ValidateInstanceVersion
+//
+// The returned pin will NOT be checked (so that you can provide a pass-through
+// resolver, if you like). Returning a zero Pin causes File.ResolveWith to
+// silently omit the package from the resolved file.
+//
+// `def` is given as FYI for error messages and logging.
+//
+// VersionResolver should expect to be called concurrently from multiple
+// goroutines.
+type VersionResolver func(pkg, vers string, def PackageDef) (common.Pin, error)
 
 // ResolvedFile only contains valid, fully-resolved information and is the
 // result of calling File.Resolve.
@@ -151,16 +164,10 @@ func (f *ResolvedFile) Serialize(w io.Writer) (int, error) {
 }
 
 // Resolve takes the current unresolved File and expands all package templates
-// using common.DefaultPackageNameExpander(), and also resolves all versions
-// with the provided VersionResolver.
-func (f *File) Resolve(rslv VersionResolver) (*ResolvedFile, error) {
-	return f.ResolveWith(rslv, template.DefaultExpander())
-}
-
-// ResolveWith takes the current unresolved File and expands all package
-// templates using the provided values of arch and os, and also resolves
-// all versions with the provided VersionResolver.
-func (f *File) ResolveWith(rslv VersionResolver, expander template.Expander) (*ResolvedFile, error) {
+// using the provided expander (usually template.DefaultExpander()), and also
+// resolves all versions with the provided VersionResolver, calling it
+// concurrently from multiple goroutines.
+func (f *File) Resolve(rslv VersionResolver, expander template.Expander) (*ResolvedFile, error) {
 	ret := &ResolvedFile{}
 
 	if f.ServiceURL != "" {
@@ -183,10 +190,19 @@ func (f *File) ResolveWith(rslv VersionResolver, expander template.Expander) (*R
 		return ret, nil
 	}
 
-	// subdir -> pkg -> orig_lineno
-	resolvedPkgDupList := map[string]map[string]int{}
+	type resolveWorkItem struct {
+		idx    int        // index in the definition, to preserve the ordering
+		subdir string     // expanded
+		pkg    string     // expanded
+		def    PackageDef // original
 
-	ret.PackagesBySubdir = common.PinSliceBySubdir{}
+		pin common.Pin // resolved
+		err error      // resolution error
+	}
+
+	// Collect a list of package defs we want to resolve, expanding the templates
+	// right away.
+	var toResolve []resolveWorkItem
 	for subdir, pkgs := range f.PackagesBySubdir {
 		realSubdir, err := expander.Expand(subdir)
 		switch err {
@@ -201,29 +217,80 @@ func (f *File) ResolveWith(rslv VersionResolver, expander template.Expander) (*R
 		if err := common.ValidateSubdir(realSubdir); err != nil {
 			return nil, errors.Annotate(err, "normalizing %q", subdir).Err()
 		}
-		for _, pkg := range pkgs {
-			pin, err := pkg.Resolve(rslv, expander)
-			if err == template.ErrSkipTemplate {
+
+		for _, def := range pkgs {
+			switch realPkg, err := def.Expand(expander); {
+			case err == template.ErrSkipTemplate:
 				continue
+			case err != nil:
+				return nil, err // the error is already properly annotated
+			default:
+				toResolve = append(toResolve, resolveWorkItem{
+					idx:    len(toResolve),
+					subdir: realSubdir,
+					pkg:    realPkg,
+					def:    def,
+				})
 			}
-			if err != nil {
-				return nil, errors.Annotate(err, "resolving package").Err()
-			}
-
-			if origLineNo, ok := resolvedPkgDupList[realSubdir][pin.PackageName]; ok {
-				return nil, errors.
-					Reason("duplicate package in subdir %q: %q: defined on line %d and %d",
-						realSubdir, pin.PackageName, origLineNo, pkg.LineNo).Err()
-			}
-			if resolvedPkgDupList[realSubdir] == nil {
-				resolvedPkgDupList[realSubdir] = map[string]int{}
-			}
-			resolvedPkgDupList[realSubdir][pin.PackageName] = pkg.LineNo
-
-			ret.PackagesBySubdir[realSubdir] = append(ret.PackagesBySubdir[realSubdir], pin)
 		}
 	}
 
+	// Resolve versions into instance IDs in parallel. Errors are collected
+	// through 'resolved'.
+	resolved := make([]*resolveWorkItem, len(toResolve)) // index -> resolved or nil if skipped
+	parallel.FanOutIn(func(tasks chan<- func() error) {
+		mu := sync.Mutex{}
+		for _, p := range toResolve {
+			p := p
+			tasks <- func() error {
+				p.pin, p.err = rslv(p.pkg, p.def.UnresolvedVersion, p.def)
+				switch {
+				case p.err != nil:
+					p.err = errors.Annotate(p.err, "failed to resolve package version (line %d)", p.def.LineNo).Err()
+				case p.pin == (common.Pin{}):
+					return nil // the resolver asked to skip this package
+				}
+				mu.Lock()
+				resolved[p.idx] = &p
+				mu.Unlock()
+				return nil
+			}
+		}
+	})
+
+	// subdir -> pkg -> orig_lineno
+	resolvedPkgDupList := map[string]map[string]int{}
+
+	// Check and split the result.
+	ret.PackagesBySubdir = common.PinSliceBySubdir{}
+	var merr errors.MultiError
+	for _, p := range resolved {
+		switch {
+		case p == nil:
+			continue // was skipped by the resolved
+		case p.err != nil:
+			merr = append(merr, p.err)
+			continue
+		}
+
+		if origLineNo, ok := resolvedPkgDupList[p.subdir][p.pin.PackageName]; ok {
+			merr = append(merr, errors.
+				Reason("duplicate package in subdir %q: %q: defined on line %d and %d",
+					p.subdir, p.pin.PackageName, origLineNo, p.def.LineNo).Err())
+			continue
+		}
+
+		if resolvedPkgDupList[p.subdir] == nil {
+			resolvedPkgDupList[p.subdir] = map[string]int{}
+		}
+		resolvedPkgDupList[p.subdir][p.pin.PackageName] = p.def.LineNo
+
+		ret.PackagesBySubdir[p.subdir] = append(ret.PackagesBySubdir[p.subdir], p.pin)
+	}
+
+	if len(merr) != 0 {
+		return nil, merr
+	}
 	return ret, nil
 }
 
---
+++ cipd/client/cipd/resolver.go
@@ -0,0 +1,162 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cipd
+
+import (
+	"fmt"
+	"sort"
+	"sync"
+
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/sync/promise"
+
+	"go.chromium.org/luci/cipd/client/cipd/ensure"
+	"go.chromium.org/luci/cipd/client/cipd/template"
+	"go.chromium.org/luci/cipd/common"
+)
+
+// Resolver resolves versions of packages in an ensure file into concrete
+// instance IDs.
+//
+// For versions that are already defined as instance IDs, it verifies they
+// actually exist.
+//
+// The instance of Resolver is stateful. It caches result of resolution and
+// verification, so subsequent attempts to resolve/verify same pins are fast.
+//
+// Resolver can be safely used concurrently.
+type Resolver struct {
+	// Client is the CIPD client to use for resolving versions.
+	Client Client
+
+	resolving promise.Map // unresolvedPkg => (common.Pin, error)
+	verifying promise.Map // common.Pin => (nil, error)
+}
+
+type unresolvedPkg struct {
+	pkg string
+	ver string
+}
+
+func (p *unresolvedPkg) String() string { return fmt.Sprintf("%s@%s", p.pkg, p.ver) }
+
+// ResolvePackage resolves the package's version into a concrete instance ID
+// and verifies it exists.
+func (r *Resolver) ResolvePackage(ctx context.Context, pkg, ver string) (common.Pin, error) {
+	if err := common.ValidatePackageName(pkg); err != nil {
+		return common.Pin{}, err
+	}
+	if err := common.ValidateInstanceVersion(ver); err != nil {
+		return common.Pin{}, err
+	}
+
+	// If the version already looks like an instance ID just verify it exists.
+	if common.ValidateInstanceID(ver, common.AnyHash) == nil {
+		pin := common.Pin{PackageName: pkg, InstanceID: ver}
+		if err := r.verifyPin(ctx, pin); err != nil {
+			return common.Pin{}, err
+		}
+		return pin, nil
+	}
+
+	// Otherwise resolve it into the IID. Note that ResolveVersion verifies that
+	// the resolved pin exists, so skip 'verifyPin' step for such resolved pins.
+	return r.resolveVersion(ctx, pkg, ver)
+}
+
+// ResolveEnsureFile resolves versions of all packages in the ensure file.
+//
+// Succeeds only if all packages have been successfully resolved and verified.
+// Names of packages that failed the resolution are logged and also returned
+// as part of the multi-error.
+func (r *Resolver) ResolveEnsureFile(ctx context.Context, file *ensure.File, expander template.Expander) (*ensure.ResolvedFile, error) {
+	// Even on a resolution failure, we don't return an error so the resolver
+	// continues to try the remaining packages. However, we'll track the error
+	// and return it externally.
+	type fail struct {
+		def ensure.PackageDef
+		err error
+	}
+	var fails []fail
+	var mu sync.Mutex
+
+	ret, err := file.Resolve(func(pkg, ver string, def ensure.PackageDef) (common.Pin, error) {
+		pin, err := r.ResolvePackage(ctx, pkg, ver)
+		if err != nil {
+			mu.Lock()
+			fails = append(fails, fail{def, err})
+			mu.Unlock()
+			return common.Pin{}, nil
+		}
+		return pin, nil
+	}, expander)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// For determinism, we sort the returned errors by the definition line number.
+	if len(fails) > 0 {
+		sort.Slice(fails, func(i, j int) bool {
+			return fails[i].def.LineNo < fails[j].def.LineNo
+		})
+		merr := make(errors.MultiError, len(fails))
+		for i, fail := range fails {
+			merr[i] = fail.err
+		}
+		return nil, merr
+	}
+
+	return ret, nil
+}
+
+// resolveVersion returns a resolved pin for the given package or an error.
+func (r *Resolver) resolveVersion(ctx context.Context, pkg, ver string) (common.Pin, error) {
+	unresolved := unresolvedPkg{pkg, ver}
+
+	promise := r.resolving.Get(ctx, unresolved, func(ctx context.Context) (interface{}, error) {
+		logging.Debugf(ctx, "Resolving package %s ...", unresolved)
+		pin, err := r.Client.ResolveVersion(ctx, unresolved.pkg, unresolved.ver)
+		if err != nil {
+			logging.Errorf(ctx, "Failed to resolve package %s: %s", unresolved, err)
+		} else {
+			logging.Debugf(ctx, "Resolved package %s => %s", unresolved, pin)
+		}
+		return pin, err
+	})
+
+	pin, err := promise.Get(ctx)
+	if err != nil {
+		return common.Pin{}, err
+	}
+	return pin.(common.Pin), nil
+}
+
+// verifyPin returns nil if the given pin exists on the backend.
+func (r *Resolver) verifyPin(ctx context.Context, pin common.Pin) error {
+	promise := r.verifying.Get(ctx, pin, func(ctx context.Context) (interface{}, error) {
+		logging.Debugf(ctx, "Validating pin %s...", pin)
+		_, err := r.Client.DescribeInstance(ctx, pin, nil)
+		if err != nil {
+			logging.Errorf(ctx, "Failed to resolve instance info for %s: %s", pin, err)
+		}
+		return nil, err
+	})
+	_, err := promise.Get(ctx)
+	return err
+}
