0d491f60755cc5fe7800cb7b6a55e958c416b65d
Vadim Shtayura <vadimsh@chromium.org>
2018-08-02 16:23:06 -0700

    [common/sync] Add promise.Map type.
    It's a map of promised, for when we need to wait for a keyed things. Will be
    used in a following CL that refactors cipd.Verifier to be simpler and more
    parallel.
    
    R=iannucci@chromium.org, nodir@chromium.org
    BUG=870166
    
    Change-Id: Iac563f74a3f0d663bf37a7d25b3b311fb7604dc3

---
+++ common/sync/promise/map_test.go
@@ -0,0 +1,44 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package promise
+
+import (
+	"testing"
+
+	"golang.org/x/net/context"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMap(t *testing.T) {
+	t.Parallel()
+
+	Convey(`Works`, t, func() {
+		c := context.Background()
+		m := Map{}
+
+		p1 := m.Get(c, 1, func(context.Context) (interface{}, error) {
+			return "hello", nil
+		})
+		p2 := m.Get(c, 1, func(context.Context) (interface{}, error) {
+			panic("must not be called")
+		})
+		So(p1, ShouldEqual, p2)
+
+		res, err := p1.Get(c)
+		So(err, ShouldBeNil)
+		So(res.(string), ShouldEqual, "hello")
+	})
+}
