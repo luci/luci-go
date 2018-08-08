32d5fd3dd76281da86960883474a710acaff5ffd
Bev Cristobal <bevc@google.com>
2018-08-08 22:22:48 +0000

    Add DRACs, KVMs, NICs, OSes, Racks, Switches, VLANs, and VMs to Crimson UI
    Bug: 869644
    Change-Id: Id62b9ad389e3689008661816ad2f4fc0073619bb
    Reviewed-on: https://chromium-review.googlesource.com/1164386
    Commit-Queue: Bev Cristobal <bevc@chromium.org>
    Reviewed-by: smut <smut@google.com>

---
+++ machine-db/appengine/ui/racks.go
@@ -0,0 +1,33 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ui
+
+import (
+	"go.chromium.org/luci/server/router"
+	"go.chromium.org/luci/server/templates"
+
+	"go.chromium.org/luci/machine-db/api/crimson/v1"
+)
+
+func racksPage(c *router.Context) {
+	resp, err := server(c.Context).ListRacks(c.Context, &crimson.ListRacksRequest{})
+	if err != nil {
+		renderErr(c, err)
+		return
+	}
+	templates.MustRender(c.Context, c.Writer, "pages/racks.html", map[string]interface{}{
+		"Racks": resp.Racks,
+	})
+}
