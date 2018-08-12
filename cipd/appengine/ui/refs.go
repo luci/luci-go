aabf0787124df11bc334a163e1867a1f6f5e3cb4
Vadim Shtayura <vadimsh@chromium.org>
2018-08-12 16:00:41 -0700

    [cipd] Add a page with basic information about package instances.
    R=nodir@chromium.org
    
    Change-Id: If7b4d9a1b832db2c0b4ee0af7bcb57103a911a80

---
+++ cipd/appengine/ui/refs.go
@@ -0,0 +1,46 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ui
+
+import (
+	"strings"
+	"time"
+
+	"github.com/dustin/go-humanize"
+
+	"go.chromium.org/luci/common/proto/google"
+
+	api "go.chromium.org/luci/cipd/api/cipd/v1"
+)
+
+type refItem struct {
+	Title string
+	Href  string
+	User  string
+	Age   string
+}
+
+func refsListing(refs []*api.Ref, pkg string, now time.Time) []refItem {
+	l := make([]refItem, len(refs))
+	for i, r := range refs {
+		l[i] = refItem{
+			Title: r.Name,
+			Href:  instancePageURL(pkg, r.Name),
+			User:  strings.TrimPrefix(r.ModifiedBy, "user:"),
+			Age:   humanize.RelTime(google.TimeFromProto(r.ModifiedTs), now, "", ""),
+		}
+	}
+	return l
+}
