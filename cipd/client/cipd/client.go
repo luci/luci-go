930e1ac5dd787bb24fc8d579a0446afc3ac69cbf
Vadim Shtayura <vadimsh@chromium.org>
2018-08-14 18:48:25 +0000

    [cipd] Move *.digests generation and checks into selfupdate-roll subcommand.
    Also teach it to write to the version file, so to roll a pinned client all
    we have to do is to run:
    
      cipd selfupdate-roll -version-file <path> -version <new-version>
    
    The primary reason we do it at once is that in depot_tools (and probably other
    places where the client is pinned), modifying <version-file> manually makes
    'cipd' invocation try to self-update to this new version next time the client
    runs. So when running 'cipd selfupdate ... -generate-digests' to regenerate
    *.digests we hit the self-update code path with OLD *.digests file, and the
    self-update rightfully fails.
    
    R=nodir@chromium.org, iannucci@chromium.org
    BUG=870166
    
    Change-Id: I18a57a12fd6b34d45f62ea52036d31575d36f091
    Reviewed-on: https://chromium-review.googlesource.com/1173728
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

--- cipd/client/cipd/client.go
+++ cipd/client/cipd/client.go
@@ -144,7 +144,7 @@ var (
 	// ClientPackage is a package with the CIPD client. Used during self-update.
 	ClientPackage = "infra/tools/cipd/${platform}"
 	// UserAgent is HTTP user agent string for CIPD client.
-	UserAgent = "cipd 2.2.4"
+	UserAgent = "cipd 2.2.5"
 )
 
 func init() {
