51cf1452dc19d6e28fd6959495b957f094d8bbc9
Julie Hockett <juliehockett@google.com>
2018-08-24 10:04:32 -0700

    [auth integration] Add a firebase auth shim server
    So that kitchen can set the FIREBASE_TOKEN env variable. This is used to
    allow a recipe to deploy to a firebase-hosted site from the service
    account. Fuchsia is planning to use this to host generated API
    documentation.
    
    Change-Id: I8086dc5ed2dde2c271d0062994677fb4b505e617

---
+++ auth/integration/firebase/firebase_test.go
@@ -0,0 +1,76 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package firebase
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+	"time"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/clock/testclock"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestProtocol(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	ctx, _ = testclock.UseTime(ctx, testclock.TestRecentTimeUTC)
+	token := "tok1"
+
+	Convey("With server", t, func(c C) {
+		s := Server{
+			Source: oauth2.StaticTokenSource(&oauth2.Token{
+				AccessToken: token,
+				Expiry:      clock.Now(ctx).Add(30 * time.Minute),
+			}),
+		}
+		addr, err := s.Start(ctx)
+		So(err, ShouldBeNil)
+		defer s.Stop(ctx)
+
+		call := func(refreshTok string) (*http.Response, error) {
+			form := url.Values{}
+			form.Add("grant_type", "refresh_token")
+			form.Add("refresh_token", refreshTok)
+			form.Add("client_id", "fake_client_id")
+			form.Add("client_secret", "fake_client_secret")
+			return http.Post(addr, "application/x-www-form-urlencoded", strings.NewReader(form.Encode()))
+		}
+
+		Convey("Happy path", func() {
+			resp, err := call(token)
+			So(err, ShouldBeNil)
+			defer resp.Body.Close()
+			tok := map[string]interface{}{}
+			So(resp.StatusCode, ShouldEqual, 200)
+			So(resp.Header.Get("Content-Type"), ShouldEqual, "application/json")
+			So(json.NewDecoder(resp.Body).Decode(&tok), ShouldBeNil)
+			So(tok, ShouldResemble, map[string]interface{}{
+				"access_token": "tok1",
+				"expires_in":   1800.0,
+				"token_type":   "Bearer",
+			})
+		})
+	})
+}
