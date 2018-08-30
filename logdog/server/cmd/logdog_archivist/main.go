ad3019406c2ab788d148da5fba73b2277a631562
Ryan Tseng <hinoka@google.com>
2018-08-30 15:50:39 -0700

    [logdog] Set MaxOutstandingMessages to 16
    We do actually nack messages now, so we can set a limit for outstanding messages.
    
    Bug: 795156
    Change-Id: I35d5938974300d0df17a8039696a5270a8f78f57

--- logdog/server/cmd/logdog_archivist/main.go
+++ logdog/server/cmd/logdog_archivist/main.go
@@ -109,12 +109,13 @@ func (a *application) runArchivist(c context.Context) error {
 	}
 	sub := psClient.Subscription(psSubscriptionName)
 	sub.ReceiveSettings = pubsub.ReceiveSettings{
-		// These must be -1 (unlimited), otherwise the flow controller will saturate
-		// since we do not Nack messages.  PubSub performs poorly as a Task Queue otherwise.
+		// These must be -1 (unlimited), otherwise the flow controller will saturate.
+		// PubSub performs poorly as a Task Queue otherwise.
 		// https://github.com/GoogleCloudPlatform/google-cloud-go/issues/919#issuecomment-372403175
-		MaxExtension:           -1,
-		MaxOutstandingMessages: -1,
-		MaxOutstandingBytes:    -1,
+		MaxExtension:        -1,
+		MaxOutstandingBytes: -1,
+
+		MaxOutstandingMessages: 16,
 		NumGoroutines:          8,
 	}
 
