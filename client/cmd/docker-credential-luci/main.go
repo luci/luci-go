3276718b44a0232680aa67c1732150540a23c659
Petr Hosek <phosek@google.com>
2018-09-17 21:34:19 +0000

    docker-credential-luci: Docker credential helper
    This enables integration of Docker with LUCI service accounts.
    
    Change-Id: I04ce2afa701274ef305b4216614bcec445143507
    Reviewed-on: https://chromium-review.googlesource.com/1226232
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Petr Hosek <phosek@chromium.org>

---
+++ client/cmd/docker-credential-luci/main.go
@@ -0,0 +1,115 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Command docker-credential-luci is a Docker credential helper.
+//
+// The protocol used for communication between Docker and the credential
+// helper is heavily inspired by Git, but it differs in the information
+// shared:
+//
+// https://docs.docker.com/engine/reference/commandline/login/#credential-helper-protocol
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/auth"
+	"go.chromium.org/luci/auth/client/authcli"
+	"go.chromium.org/luci/common/logging/gologger"
+
+	"go.chromium.org/luci/hardcoded/chromeinfra"
+)
+
+var (
+	flags    authcli.Flags
+	lifetime time.Duration
+)
+
+func init() {
+	defaults := chromeinfra.DefaultAuthOptions()
+	defaults.Scopes = []string{auth.OAuthScopeEmail, "https://www.googleapis.com/auth/cloud-platform"}
+	flags.RegisterScopesFlag = true
+	flags.Register(flag.CommandLine, defaults)
+	flag.DurationVar(
+		&lifetime, "lifetime", time.Minute,
+		"Minimum token lifetime. If existing token expired and refresh token or service account is not present, returns nothing.",
+	)
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: docker-credential-luci command\n")
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		fmt.Fprintln(os.Stderr, "invalid number of arguments")
+		os.Exit(1)
+	}
+
+	opts, err := flags.Options()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if lifetime > 45*time.Minute {
+		fmt.Fprintln(os.Stderr, "lifetime cannot exceed 45m")
+		os.Exit(1)
+	}
+
+	ctx := gologger.StdConfig.Use(context.Background())
+	auth := auth.NewAuthenticator(ctx, auth.SilentLogin, opts)
+
+	// We only use the command and ignore the payload.
+	switch flag.Args()[0] {
+	case "get":
+		t, err := auth.GetAccessToken(lifetime)
+		if err != nil {
+			printErr("cannot get access token", err)
+			os.Exit(1)
+		}
+		response := map[string]string{
+			"Username": "oauth2accesstoken",
+			"Secret":   t.AccessToken,
+		}
+		if err := json.NewEncoder(os.Stdout).Encode(&response); err != nil {
+			printErr("cannot encode payload", err)
+			os.Exit(1)
+		}
+	case "erase":
+		if err := auth.PurgeCredentialsCache(); err != nil {
+			printErr("cannot erase cache", err)
+			os.Exit(1)
+		}
+	default:
+		// We don't support the "store" command and hence ignore it.
+	}
+}
+
+func printErr(prefix string, err error) {
+	switch {
+	case err == auth.ErrLoginRequired:
+		fmt.Fprintln(os.Stderr, "not running with a service account and not logged it")
+	case err != nil:
+		fmt.Fprintf(os.Stderr, "%s: %v\n", prefix, err)
+	}
+}
