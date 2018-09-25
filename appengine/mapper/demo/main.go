c5d00f79b9050986c0331db259ae2f37fdebcd03
Vadim Shtayura <vadimsh@chromium.org>
2018-09-25 15:55:58 +0000

    [mapper] Add initial framework.
    It does very little currently. Just sets up some structure for more future
    functionality.
    
    R=iannucci@chromium.org
    
    Change-Id: I1b5b38ae5e58caa5f2d2de7d60c9ed1e5099a62c
    Reviewed-on: https://chromium-review.googlesource.com/1239209
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

---
+++ appengine/mapper/demo/main.go
@@ -0,0 +1,92 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package demo contains minimal demo for 'mapper' package.
+package demo
+
+import (
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/context"
+
+	"go.chromium.org/gae/service/datastore"
+
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/server/router"
+
+	"go.chromium.org/luci/appengine/gaemiddleware/standard"
+	"go.chromium.org/luci/appengine/mapper"
+	"go.chromium.org/luci/appengine/tq"
+)
+
+type dumpingMapper struct{}
+
+func (dumpingMapper) MapperID() mapper.ID {
+	return "dumping/v1"
+}
+
+func (dumpingMapper) Process(c context.Context, params mapper.Params, keys []*datastore.Key) error {
+	logging.Infof(c, "Got %d keys:", len(keys))
+	for _, k := range keys {
+		logging.Infof(c, "%s", k)
+	}
+	return nil
+}
+
+type TestEntity struct {
+	ID int64 `gae:"$id"`
+}
+
+func init() {
+
+	r := router.New()
+	base := standard.Base()
+	standard.InstallHandlers(r)
+
+	tasks := tq.Dispatcher{}
+
+	mappers := mapper.Controller{}
+	mappers.RegisterMapper(dumpingMapper{})
+	mappers.Install(&tasks)
+
+	tasks.InstallRoutes(r, base)
+
+	// Populate creates a bunch of entities.
+	r.GET("/populate", base, func(c *router.Context) {
+		ents := make([]TestEntity, 1024)
+		if err := datastore.Put(c.Context, ents); err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(c.Writer, "Done")
+	})
+
+	// Launch launches the mapper job.
+	r.GET("/launch", base, func(c *router.Context) {
+		jobID, err := mappers.LaunchJob(c.Context, &mapper.JobConfig{
+			Query: mapper.Query{
+				Kind: "TestEntity",
+			},
+			Mapper:     (dumpingMapper{}).MapperID(),
+			ShardCount: 8,
+			PageSize:   64,
+		})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(c.Writer, "Launched job %d", jobID)
+	})
+
+	http.DefaultServeMux.Handle("/", r)
+}
