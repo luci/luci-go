9dc66e204745dc8f5b8838649a9487fb381a7305
Vadim Shtayura <vadimsh@chromium.org>
2018-10-10 02:05:26 +0000

    [cipd] Add boilerplate for cipd.Admin API.
    It's an RPC API accessible only to service administrators.
    
    It will be used (at least initially) to kick off mapper jobs. Later it can grow
    various adhoc methods useful when debugging issues or managing the service.
    
    R=tandrii@chromium.org, iannucci@chromium.org, nodir@chromium.org
    BUG=875064
    
    Change-Id: I6eb7bc296d5adb8a226a5526e422bb14cc7a60cb
    Reviewed-on: https://chromium-review.googlesource.com/c/1271923
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

---
+++ cipd/appengine/impl/admin/acl.go
@@ -0,0 +1,58 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package admin
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.chromium.org/luci/appengine/tq"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/server/auth"
+
+	api "go.chromium.org/luci/cipd/api/admin/v1"
+)
+
+// AdminGroup is a name of a group with accounts that can use Admin API.
+const AdminGroup = "administrators"
+
+// AdminAPI returns an ACL-protected implementation of cipd.AdminServer that can
+// be exposed as a public API (i.e. admins can use it via external RPCs).
+func AdminAPI(d *tq.Dispatcher) api.AdminServer {
+	impl := &adminImpl{tq: d}
+	impl.registerTasks()
+	return &api.DecoratedAdmin{
+		Service: impl,
+		Prelude: checkAdminPrelude,
+	}
+}
+
+// checkAdminPrelude is called before each RPC to check the caller is in
+// "administrators" group.
+func checkAdminPrelude(c context.Context, method string, req proto.Message) (context.Context, error) {
+	switch yep, err := auth.IsMember(c, AdminGroup); {
+	case err != nil:
+		logging.WithError(err).Errorf(c, "IsMember(%q) failed", AdminGroup)
+		return nil, status.Errorf(codes.Internal, "failed to check ACL")
+	case !yep:
+		logging.Warningf(c, "Denying access to %q for %q, not in %q group", method, auth.CurrentIdentity(c), AdminGroup)
+		return nil, status.Errorf(codes.PermissionDenied, "not allowed")
+	}
+	logging.Infof(c, "Admin %q is calling %q", auth.CurrentIdentity(c), method)
+	return c, nil
+}
