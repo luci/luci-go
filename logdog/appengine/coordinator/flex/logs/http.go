43d838d1017f2fae4f1633b9cb726c16550b53ba
Ryan Tseng <hinoka@google.com>
2018-10-19 10:32:38 -0700

    [logdog] Escape lines in lite mode
    This changes the behavior to make lite mode and raw mode
    behave slightly differently.
    Lite mode: Escape lines then emit them, with \n
    Raw mode: Emit lines, and emit the original deliminator.
    
    Bug: 897017
    Change-Id: I4c6a81d830ddeb5d3e150b2b58ee9a2395eacf46

--- logdog/appengine/coordinator/flex/logs/http.go
+++ logdog/appengine/coordinator/flex/logs/http.go
@@ -685,12 +685,16 @@ func serve(c context.Context, data logData, w http.ResponseWriter) (err error) {
 		}
 
 		for i, line := range log.GetText().GetLines() {
-			// For html full mode, we wrap each line with a div.
-			// For html lite and raw mode, we just regurgitate the lines.
-			if data.options.format == formatHTMLFull {
+			// For html full mode, we escape and wrap each line with a div.
+			// For html lite mode, just escape the line.
+			// For raw mode, we just regurgitate the line.
+			var ierr error
+			switch data.options.format {
+			case formatHTMLFull:
 				lt := logLineStruct{
 					// Note: We want to use PrefixIndex because we might be viewing more than 1 stream.
-					ID:   fmt.Sprintf("L%d_%d", log.PrefixIndex, i),
+					ID: fmt.Sprintf("L%d_%d", log.PrefixIndex, i),
+					// The templating engine below escapes the lines for us.
 					Text: string(line.GetValue()),
 				}
 				// Add in timestamp information, if available.
@@ -699,10 +703,16 @@ func serve(c context.Context, data logData, w http.ResponseWriter) (err error) {
 					lt.DurationInfo = durationInfo(prevDuration, duration)
 					prevDuration = duration
 				}
-				if ierr := lineTemplate.Execute(w, lt); ierr != nil {
-					merr = append(merr, ierr)
-				}
-			} else if _, ierr := fmt.Fprintf(w, "%s\n", line.GetValue()); ierr != nil {
+				ierr = lineTemplate.Execute(w, lt)
+			case formatHTMLLite:
+				text := template.HTMLEscapeString(string(line.GetValue()))
+				_, ierr = fmt.Fprintf(w, "%s\n", text)
+			case formatRAW:
+				_, ierr = fmt.Fprintf(w, "%s%s", line.GetValue(), line.GetDelimiter())
+			default:
+				panic("Impossible")
+			}
+			if ierr != nil {
 				merr = append(merr, ierr)
 			}
 		}
