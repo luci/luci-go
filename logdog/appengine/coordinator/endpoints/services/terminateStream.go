ce0fb0ce49ca553e6624954abe5d43d3a2d8d47b
Ryan Tseng <hinoka@google.com>
2018-10-31 13:38:01 -0700

    [logdog] Split out archival delay and max stream age
    Previously ArchivalDelayMax determines both:
    * When the pessimistic log archival will be scheduled.
    * How long a archival request will thrash in the PubSub queue before forcefully getting serviced.
    
    This CL splits up these settings into a new setting:
    * ArchivalDelayMax: How long a archival request will thrash in the PubSub queue before forcefully getting serviced.
    * MaxStreamAge: When the pessimistic log archival will be scheduled.
    
    Bug: 900148, 900712
    Change-Id: I2e24b8af800dde395ece1abe1ae0f39953535655

--- logdog/appengine/coordinator/endpoints/services/terminateStream.go
+++ logdog/appengine/coordinator/endpoints/services/terminateStream.go
@@ -159,6 +159,6 @@ func (s *server) TerminateStream(c context.Context, req *logdog.TerminateStreamR
 func standardArchivalParams(cfg *config.Config, pcfg *svcconfig.ProjectConfig) *coordinator.ArchivalParams {
 	return &coordinator.ArchivalParams{
 		SettleDelay:    google.DurationFromProto(cfg.Coordinator.ArchiveSettleDelay),
-		CompletePeriod: endpoints.MinDuration(cfg.Coordinator.ArchiveDelayMax, pcfg.MaxStreamAge),
+		CompletePeriod: endpoints.MinDuration(cfg.Coordinator.MaxStreamAge, pcfg.MaxStreamAge),
 	}
 }
