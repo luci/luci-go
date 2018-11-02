83bf97807e4c38d157af45aa5fa4bf36714537b2
smut <smut@google.com>
2018-11-01 20:23:12 -0700

    [GCE] Create config for GCE service
    Bug: 897355
    Change-Id: I250dd83c48b63fed4b3e64cb0c8b2e46d8258130

---
+++ gce/api/config/v1/kinds_test.go
@@ -0,0 +1,69 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"context"
+	"testing"
+
+	"go.chromium.org/luci/config/validation"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestValidateKinds(t *testing.T) {
+	t.Parallel()
+
+	Convey("validate", t, func() {
+		c := &validation.Context{Context: context.Background()}
+
+		Convey("empty", func() {
+			k := &Kinds{}
+			k.Validate(c)
+			So(c.Finalize(), ShouldBeNil)
+		})
+
+		Convey("names", func() {
+			Convey("missing", func() {
+				k := &Kinds{
+					Kind: []*Kind{
+						{},
+					},
+				}
+				k.Validate(c)
+				So(c.Finalize(), ShouldErrLike, "name is required")
+			})
+
+			Convey("duplicate", func() {
+				k := &Kinds{
+					Kind: []*Kind{
+						{
+							Name: "duplicated",
+						},
+						{
+							Name: "unique",
+						},
+						{
+							Name: "duplicated",
+						},
+					},
+				}
+				k.Validate(c)
+				So(c.Finalize(), ShouldErrLike, "duplicate name")
+			})
+		})
+	})
+}
