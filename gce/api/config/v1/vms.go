62c6d9d862076d1c3e18817fd0683c39732590b6
smut <smut@google.com>
2018-11-02 17:11:42 -0700

    [GCE] Create config for GCE service
    Bug: 897355
    Change-Id: I250dd83c48b63fed4b3e64cb0c8b2e46d8258130

---
+++ gce/api/config/v1/vms.go
@@ -0,0 +1,47 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"strings"
+
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/config/validation"
+)
+
+// Validate validates VMs blocks given the allowable kinds blocks may reference.
+func (vms *VMs) Validate(c *validation.Context, kinds stringset.Set) {
+	prefixes := make([]string, 0, len(vms.Vms))
+	for i, block := range vms.Vms {
+		c.Enter("vms block %d", i)
+		if block.Prefix == "" {
+			c.Errorf("prefix is required")
+		}
+		// Ensure no prefix is a prefix of any other prefix. Building a prefix tree
+		// and waiting until the end to check this is faster, but config validation
+		// isn't particularly time sensitive since configs are processed asynchronously.
+		for _, p := range prefixes {
+			switch {
+			case strings.HasPrefix(p, block.Prefix):
+				c.Errorf("prefix %q is a prefix of %q", block.Prefix, p)
+			case strings.HasPrefix(block.Prefix, p):
+				c.Errorf("prefix %q is a prefix of %q", p, block.Prefix)
+			}
+		}
+		prefixes = append(prefixes, block.Prefix)
+		block.Validate(c, kinds)
+		c.Exit()
+	}
+}
