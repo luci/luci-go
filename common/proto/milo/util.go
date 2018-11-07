e75bbfdf1a5ced21258ab86b5cd1ed662095fc70
Nodir Turakulov <nodir@google.com>
2018-11-07 19:09:27 +0000

    [annotation] Add ExtractProperties
    Add a function that extracts properties from annotation step to a
    google.protobuf.Struct.
    It will be used by kitchen to stream updated output properties.
    
    Bug: 843714
    Change-Id: I7c9cbeb13f949a77d6072c26bedf3bb93c78bc30
    Reviewed-on: https://chromium-review.googlesource.com/c/1320929
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- common/proto/milo/util.go
+++ common/proto/milo/util.go
@@ -14,5 +14,44 @@
 
 package milo
 
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/ptypes/struct"
+)
+
 // ContentTypeAnnotations is a stream content type for annotation streams.
 const ContentTypeAnnotations = "text/x-chrome-infra-annotations; version=2"
+
+// ExtractProperties returns a flat list of properties from a tree of steps.
+// If multiple step nodes have a property of the same name, the last one in the
+// preorder traversal wins.
+func ExtractProperties(s *Step) (*structpb.Struct, error) {
+	finals := map[string]json.RawMessage{}
+
+	var extract func(s *Step)
+	extract = func(s *Step) {
+		for _, p := range s.Property {
+			finals[p.Name] = json.RawMessage(p.Value)
+		}
+		for _, substep := range s.GetSubstep() {
+			if ss := substep.GetStep(); ss != nil {
+				extract(ss)
+			}
+		}
+	}
+	extract(s)
+
+	buf, err := json.Marshal(finals)
+	if err != nil {
+		panic("failed to marshal in-memory map to JSON")
+	}
+
+	ret := &structpb.Struct{}
+	if err := jsonpb.Unmarshal(bytes.NewReader(buf), ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
