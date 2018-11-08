f87361867b908fab8810eef5ed3a9d15ba114984
smut <smut@google.com>
2018-11-08 01:32:39 +0000

    [GCE] Support writing VMs blocks to the datastore
    Bug: 897355
    Change-Id: Id8b4ab09f93cc28564b6ec01e5f15ca46de80b3d

--- gce/api/config/v1/block.go
+++ gce/api/config/v1/block.go
@@ -15,9 +15,27 @@
 package config
 
 import (
+	"github.com/golang/protobuf/proto"
+
+	"go.chromium.org/gae/service/datastore"
 	"go.chromium.org/luci/common/data/stringset"
 	"go.chromium.org/luci/config/validation"
 )
+
+// Ensure Block implements datastore.PropertyConverter.
+// This allows VMs blocks to be read from and written to the datastore.
+var _ datastore.PropertyConverter = &Block{}
+
+// FromProperty implements datastore.PropertyConverter.
+func (b *Block) FromProperty(p datastore.Property) error {
+	return proto.UnmarshalText(p.Value().(string), b)
+}
+
+// ToProperty implements datastore.PropertyConverter.
+func (b *Block) ToProperty() (datastore.Property, error) {
+	p := datastore.Property{}
+	return p, p.SetValue(proto.MarshalTextString(b), false)
+}
 
 // Validate validates this VMs block given the allowable kinds the block may reference.
 func (b *Block) Validate(c *validation.Context, kinds stringset.Set) {
