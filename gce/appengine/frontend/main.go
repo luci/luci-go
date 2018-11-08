a0ea1e4d6acd1b6fe8e77d7e2ae84a31fc724eb1
smut <smut@google.com>
2018-11-08 01:32:39 +0000

    [GCE] Create RPC service framework
    For development and app-internal use only.
    Configs will be imported from the config service.
    
    Bug: 897355
    Change-Id: I9107f9dc94c4d6d0573c7c8d15df5b691d39a9d3
    Reviewed-on: https://chromium-review.googlesource.com/c/1324818
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: smut <smut@google.com>

---
+++ gce/appengine/frontend/main.go
@@ -0,0 +1,43 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package frontend
+
+import (
+	"net/http"
+
+	"go.chromium.org/luci/appengine/gaemiddleware/standard"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+	"go.chromium.org/luci/grpc/discovery"
+	"go.chromium.org/luci/grpc/grpcmon"
+	"go.chromium.org/luci/grpc/prpc"
+	"go.chromium.org/luci/server/router"
+
+	"go.chromium.org/luci/gce/api/config/v1"
+	"go.chromium.org/luci/gce/appengine/rpc"
+)
+
+func init() {
+	mathrand.SeedRandomly()
+	r := router.New()
+	standard.InstallHandlers(r)
+
+	api := prpc.Server{UnaryServerInterceptor: grpcmon.NewUnaryServerInterceptor(nil)}
+	srv := rpc.New()
+	config.RegisterConfigServer(&api, srv)
+	discovery.Enable(&api)
+	api.InstallHandlers(r, standard.Base())
+
+	http.DefaultServeMux.Handle("/", r)
+}
