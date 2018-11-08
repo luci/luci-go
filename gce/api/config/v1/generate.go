94be20c73ab4b10c4d331664557ad36777776b8f
smut <smut@google.com>
2018-11-07 16:46:37 -0800

    [GCE] Create RPC service framework
    Bug: 897355
    Change-Id: I9107f9dc94c4d6d0573c7c8d15df5b691d39a9d3

--- gce/api/config/v1/generate.go
+++ gce/api/config/v1/generate.go
@@ -14,5 +14,6 @@
 
 package config
 
-//go:generate go install go.chromium.org/luci/grpc/cmd/cproto
+//go:generate go install go.chromium.org/luci/grpc/cmd/cproto go.chromium.org/luci/grpc/cmd/svcdec
 //go:generate cproto
+//go:generate svcdec -type ConfigServer
