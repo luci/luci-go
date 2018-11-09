d63f13bfba746b2db86f200343910dab5ae1244e
Nodir Turakulov <nodir@google.com>
2018-11-09 08:53:01 -0800

    [buildbucket] add BuildTokenHeader const
    R=jchinlee@chromium.org
    
    Bug: 843714
    Change-Id: I30307054869b7632f7fcca968f637068f39e8a1c

---
+++ buildbucket/proto.go
@@ -0,0 +1,11 @@
+package buildbucket
+
+// This file contains helper functions for buildbucketpb package.
+// TODO(nodir): move existing helpers from buildbucketpb to this file.
+
+// BuildTokenHeader is the name of gRPC metadata header indicating the build
+// token (see BuildSecrets.BuildToken).
+// It is required in UpdateBuild RPC.
+// Defined in
+// https://chromium.googlesource.com/infra/infra/+/c189064/appengine/cr-buildbucket/v2/api.py#35
+const BuildTokenHeader = "x-build-token"
