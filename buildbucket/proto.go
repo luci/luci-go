d5e685e170f936c7ddaad0b8c32e9ba8ce944ed0
Nodir Turakulov <nodir@google.com>
2018-11-09 08:53:33 -0800

    [buildbucket] add BuildTokenHeader const
    R=jchinlee@chromium.org
    
    Bug: 843714
    Change-Id: I30307054869b7632f7fcca968f637068f39e8a1c

---
+++ buildbucket/proto.go
@@ -0,0 +1,25 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buildbucket
+
+// This file contains helper functions for buildbucketpb package.
+// TODO(nodir): move existing helpers from buildbucketpb to this file.
+
+// BuildTokenHeader is the name of gRPC metadata header indicating the build
+// token (see BuildSecrets.BuildToken).
+// It is required in UpdateBuild RPC.
+// Defined in
+// https://chromium.googlesource.com/infra/infra/+/c189064/appengine/cr-buildbucket/v2/api.py#35
+const BuildTokenHeader = "x-build-token"
