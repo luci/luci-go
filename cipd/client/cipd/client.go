388ad2854434b2ab7ed22ce1694830d53f30560a
Robert Iannucci <iannucci@chromium.org>
2018-11-15 11:29:29 -0800

    [cipd] Allow long paths on windows.
    This should enable CIPD to install cipd packages which have 'long' path
    names in them.
    
    R=vadimsh@chromium.org
    
    Change-Id: I61c1402d39e74e16204f28d47d43811f6a5ee005

--- cipd/client/cipd/client.go
+++ cipd/client/cipd/client.go
@@ -129,7 +129,7 @@ var (
 	// ClientPackage is a package with the CIPD client. Used during self-update.
 	ClientPackage = "infra/tools/cipd/${platform}"
 	// UserAgent is HTTP user agent string for CIPD client.
-	UserAgent = "cipd 2.2.10"
+	UserAgent = "cipd 2.2.11"
 )
 
 func init() {
