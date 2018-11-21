4b859bb1ebb2737b7cbc999204f4a619ee0e1745
Vadim Shtayura <vadimsh@chromium.org>
2018-11-21 10:03:26 -0800

    [lucicfg] Framework for the config generator.
    This CL just implements the barebone structure without any meat on top of it.
    
    High-level configs are defined in LUCI.star file (which may load other Starlark
    files via '//<path>').
    
    There's a standard library (presently embedded into the interpreter) that
    exposes a bunch of global functions that become our public API. There are
    also portions of the standard library that can be explicitly loaded via
    load("@stdlib//...") or some other TBD package alias (e.g. "@lowlevel") to
    access more advanced public APIs which we tolerate but discourage (and for that
    reason hide deeper).
    
    Thus API surfaces are expressed in Starlark. This would allow us, for example,
    to consistently generate API docs based on Starlark code ala bazelbuild/skydoc.
    Or eventually move them to an external package versioned separately from the
    interpreter binary.
    
    To interface with the interpreter guts, stdlib functions use '__native__' object
    which is a bundle of all private functions implemented in Go. They may observe
    and mutate interpreter state (for example, create config sets and populate them
    with protos, though not in this CL). At the end of execution interpreter's State
    defines what ends up in generated configs.
    
    R=iannucci@chromium.org
    BUG=833946
    
    Change-Id: I004278cf4ad0e8337903f9cd897c87715ec2e753

---
+++ lucicfg/native.go
@@ -0,0 +1,85 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lucicfg
+
+import (
+	"context"
+	"fmt"
+
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
+)
+
+// nativeCall carries arguments for nativeFunc to avoid ridiculously long
+// func declarations.
+type nativeCall struct {
+	Ctx    context.Context
+	State  *State
+	Thread *starlark.Thread
+	Fn     *starlark.Builtin
+	Args   starlark.Tuple
+	Kwargs []starlark.Tuple
+}
+
+// unpack unpacks the positional arguments into corresponding variables.
+//
+// See starlark.UnpackPositionalArgs for more info.
+func (c *nativeCall) unpack(min int, vars ...interface{}) error {
+	return starlark.UnpackPositionalArgs(c.Fn.Name(), c.Args, c.Kwargs, min, vars...)
+}
+
+// nativeFunc is callable from Starlark side via __native__.<name>(...).
+//
+// It gets the state of the generator and can potentially examine or mutate it.
+// It must not depend on any other global state.
+type nativeFunc func(call nativeCall) (starlark.Value, error)
+
+// All native functions exposed to the starlark code.
+//
+// Populated during init() time via declNative(...) calls.
+var nativeFuncs = map[string]nativeFunc{}
+
+// declNative registers a native function during init() time.
+//
+// Panics if such function has already been registered.
+func declNative(name string, f nativeFunc) {
+	if nativeFuncs[name] != nil {
+		panic(fmt.Sprintf("native function %q is already registered", name))
+	}
+	nativeFuncs[name] = f
+}
+
+// native returns a starlark struct with all registered native functions.
+//
+// This struct is put into the global starlark namespace as '__native__'. It is
+// bound to the given state and context: all native functions will receive them
+// through corresponding arguments.
+func native(ctx context.Context, s *State) starlark.Value {
+	dict := make(starlark.StringDict, len(nativeFuncs))
+	for name, cb := range nativeFuncs {
+		cb := cb
+		dict[name] = starlark.NewBuiltin(name, func(th *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+			return cb(nativeCall{
+				Ctx:    ctx,
+				State:  s,
+				Thread: th,
+				Fn:     fn,
+				Args:   args,
+				Kwargs: kwargs,
+			})
+		})
+	}
+	return starlarkstruct.FromStringDict(starlark.String("__native__"), dict)
+}
