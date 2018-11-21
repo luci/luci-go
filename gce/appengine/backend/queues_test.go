f2c756532e096829b3c47c9f9ee9c2959ef3a4d4
smut <smut@google.com>
2018-11-20 18:50:34 -0800

    [GCE] Create VM entities
    Bug: 897355
    Change-Id: Id7fef6703637bc06923b38bf16cd771bf2a7ee6b

--- gce/appengine/backend/queues_test.go
+++ gce/appengine/backend/queues_test.go
@@ -19,11 +19,13 @@ import (
 	"testing"
 
 	"go.chromium.org/gae/impl/memory"
+	"go.chromium.org/gae/service/datastore"
 	"go.chromium.org/luci/appengine/tq"
 	"go.chromium.org/luci/appengine/tq/tqtesting"
 
 	"go.chromium.org/luci/gce/api/config/v1"
 	"go.chromium.org/luci/gce/api/tasks/v1"
+	"go.chromium.org/luci/gce/appengine/model"
 	rpc "go.chromium.org/luci/gce/appengine/rpc/memory"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -33,7 +35,7 @@ import (
 func TestQueues(t *testing.T) {
 	t.Parallel()
 
-	Convey("expand", t, func() {
+	Convey("queues", t, func() {
 		dsp := &tq.Dispatcher{}
 		registerTasks(dsp)
 		srv := &rpc.Config{}
@@ -41,47 +43,146 @@ func TestQueues(t *testing.T) {
 		tqt := tqtesting.GetTestable(c, dsp)
 		tqt.CreateQueues()
 
-		Convey("invalid", func() {
-			Convey("nil", func() {
-				err := expand(c, nil)
-				So(err, ShouldErrLike, "unexpected payload")
-				So(tqt.GetScheduledTasks(), ShouldBeEmpty)
+		Convey("ensure", func() {
+			Convey("invalid", func() {
+				Convey("nil", func() {
+					err := ensure(c, nil)
+					So(err, ShouldErrLike, "unexpected payload")
+				})
+
+				Convey("empty", func() {
+					err := ensure(c, &tasks.Ensure{})
+					So(err, ShouldErrLike, "ID is required")
+				})
 			})
 
-			Convey("empty", func() {
-				err := expand(c, &tasks.Expand{})
-				So(err, ShouldErrLike, "ID is required")
-				So(tqt.GetScheduledTasks(), ShouldBeEmpty)
+			Convey("valid", func() {
+				Convey("nil", func() {
+					err := ensure(c, &tasks.Ensure{
+						Id: "id",
+					})
+					So(err, ShouldBeNil)
+					err = datastore.Get(c, &model.VM{
+						ID: "id",
+					})
+					So(err, ShouldBeNil)
+				})
+
+				Convey("empty", func() {
+					err := ensure(c, &tasks.Ensure{
+						Id:         "id",
+						Attributes: &config.VM{},
+					})
+					So(err, ShouldBeNil)
+					err = datastore.Get(c, &model.VM{
+						ID: "id",
+					})
+					So(err, ShouldBeNil)
+				})
+
+				Convey("non-empty", func() {
+					err := ensure(c, &tasks.Ensure{
+						Id: "id",
+						Attributes: &config.VM{
+							Disk: []*config.Disk{
+								{
+									Image: "image",
+								},
+							},
+						},
+					})
+					So(err, ShouldBeNil)
+					v := &model.VM{
+						ID: "id",
+					}
+					err = datastore.Get(c, v)
+					So(err, ShouldBeNil)
+					So(v, ShouldResemble, &model.VM{
+						ID: "id",
+						Attributes: config.VM{
+							Disk: []*config.Disk{
+								{
+									Image: "image",
+								},
+							},
+						},
+					})
+				})
+
+				Convey("updated", func() {
+					datastore.Put(c, &model.VM{
+						ID: "id",
+						Attributes: config.VM{
+							Zone: "zone",
+						},
+					})
+					err := ensure(c, &tasks.Ensure{
+						Id: "id",
+						Attributes: &config.VM{
+							Project: "project",
+						},
+					})
+					So(err, ShouldBeNil)
+					v := &model.VM{
+						ID: "id",
+					}
+					err = datastore.Get(c, v)
+					So(err, ShouldBeNil)
+					So(v, ShouldResemble, &model.VM{
+						ID: "id",
+						Attributes: config.VM{
+							Project: "project",
+						},
+					})
+				})
 			})
 		})
 
-		Convey("valid", func() {
-			Convey("missing", func() {
-				err := expand(c, &tasks.Expand{Id: "id"})
-				So(err, ShouldErrLike, "failed to get VMs block")
-				So(tqt.GetScheduledTasks(), ShouldBeEmpty)
+		Convey("expand", func() {
+			Convey("invalid", func() {
+				Convey("nil", func() {
+					err := expand(c, nil)
+					So(err, ShouldErrLike, "unexpected payload")
+					So(tqt.GetScheduledTasks(), ShouldBeEmpty)
+				})
+
+				Convey("empty", func() {
+					err := expand(c, &tasks.Expand{})
+					So(err, ShouldErrLike, "ID is required")
+					So(tqt.GetScheduledTasks(), ShouldBeEmpty)
+				})
 			})
 
-			Convey("zero", func() {
-				srv.EnsureVMs(c, &config.EnsureVMsRequest{
-					Id:  "id",
-					Vms: &config.Block{},
+			Convey("valid", func() {
+				Convey("missing", func() {
+					err := expand(c, &tasks.Expand{
+						Id: "id",
+					})
+					So(err, ShouldErrLike, "failed to get VMs block")
+					So(tqt.GetScheduledTasks(), ShouldBeEmpty)
 				})
-				err := expand(c, &tasks.Expand{Id: "id"})
-				So(err, ShouldBeNil)
-				So(tqt.GetScheduledTasks(), ShouldBeEmpty)
-			})
 
-			Convey("one", func() {
-				srv.EnsureVMs(c, &config.EnsureVMsRequest{
-					Id: "id",
-					Vms: &config.Block{
-						Amount: 1,
-					},
+				Convey("zero", func() {
+					srv.EnsureVMs(c, &config.EnsureVMsRequest{
+						Id:  "id",
+						Vms: &config.Block{},
+					})
+					err := expand(c, &tasks.Expand{Id: "id"})
+					So(err, ShouldBeNil)
+					So(tqt.GetScheduledTasks(), ShouldBeEmpty)
 				})
-				err := expand(c, &tasks.Expand{Id: "id"})
-				So(err, ShouldBeNil)
-				So(tqt.GetScheduledTasks(), ShouldHaveLength, 1)
+
+				Convey("one", func() {
+					srv.EnsureVMs(c, &config.EnsureVMsRequest{
+						Id: "id",
+						Vms: &config.Block{
+							Amount: 1,
+						},
+					})
+					err := expand(c, &tasks.Expand{Id: "id"})
+					So(err, ShouldBeNil)
+					So(tqt.GetScheduledTasks(), ShouldHaveLength, 1)
+				})
 			})
 		})
 	})
--- gce/appengine/model/model.go
+++ gce/appengine/model/model.go
@@ -20,10 +20,11 @@ import (
 	"go.chromium.org/luci/gce/api/config/v1"
 )
 
-// VMsKind is the VMs entities' kind in the datastore.
+// VMsKind is a VMs entity's kind in the datastore.
 const VMsKind = "VMs"
 
 // VMs is a root entity representing a configured block of VMs.
+// VM entities should be created for each VMs entity.
 type VMs struct {
 	// _extra is where unknown properties are put into memory.
 	// Extra properties are not written to the datastore.
@@ -34,4 +35,21 @@ type VMs struct {
 	ID string `gae:"$id"`
 	// Config is the config.Block representation of this entity.
 	Config config.Block `gae:"config"`
+}
+
+// VMKind is a VM entity's kind in the datastore.
+const VMKind = "VM"
+
+// VM is a root entity representing a configured VM.
+// GCE instances should be created for each VM entity.
+type VM struct {
+	// _extra is where unknown properties are put into memory.
+	// Extra properties are not written to the datastore.
+	_extra datastore.PropertyMap `gae:"-,extra"`
+	// _kind is the entity's kind in the datastore.
+	_kind string `gae:"$kind,VM"`
+	// ID is the unique identifier for this VM.
+	ID string `gae:"$id"`
+	// Attributes is the config.VM describing the GCE instance to create.
+	Attributes config.VM `gae:"attributes"`
 }
