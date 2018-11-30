188e2fc044cc2f072ef0c09e5d137a3ff769dfcb
smut <smut@google.com>
2018-11-30 03:41:05 +0000

    [GCE] Create GCE instances
    Bug: 897355
    Change-Id: I22c4337523857ff51161b85f136985587cd89372
    Reviewed-on: https://chromium-review.googlesource.com/c/1355224
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: smut <smut@google.com>

---
+++ gce/appengine/testing/roundtripper/roundtripper.go
@@ -0,0 +1,63 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package roundtripper contains an http.RoundTripper implementation suitable for testing.
+package roundtripper
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+// Ensure *JSONRoundTripper implements http.RoundTripper.
+var _ http.RoundTripper = (*JSONRoundTripper)(nil)
+
+// JSONRoundTripper implements http.RoundTripper to handle *http.Requests with a JSON body.
+type JSONRoundTripper struct {
+	// Handler is called by RoundTrip with the unmarshalled JSON from an *http.Request.
+	// Returns an interface{} to marshal as JSON in an *http.Response.
+	Handler func(interface{}) interface{}
+	// Type is the reflect.Type to unmarshal *http.Request.Body into.
+	Type reflect.Type
+}
+
+// RoundTrip handles an *http.Request with a JSON body, returning an *http.Response with a JSON body.
+// Panics on error. Implements http.RoundTripper.
+func (t *JSONRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	// Unmarshal *http.Request.Body.
+	val := reflect.New(t.Type).Interface()
+	err := json.NewDecoder(req.Body).Decode(&val)
+	req.Body.Close()
+	if err != nil {
+		panic(errors.Annotate(err, "failed to unmarshal *http.Request.Body").Err())
+	}
+
+	// Marshal the returned value from t.Handler into *http.Response.Body.
+	if t.Handler == nil {
+		panic("handler is required")
+	}
+	b, err := json.Marshal(t.Handler(val))
+	if err != nil {
+		panic(errors.Annotate(err, "failed to marshal *http.Response.Body").Err())
+	}
+	return &http.Response{
+		Body:       ioutil.NopCloser(bytes.NewReader(b)),
+		StatusCode: 200,
+	}, nil
+}
