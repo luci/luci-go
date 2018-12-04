7bbfca73d2bb2cd56e0284c0014011ca6a8d6d39
David Burger <dburger@chromium.org>
2018-12-04 23:47:04 +0000

    Add timeline visualization to milo build page
    Add a timeline visualization to the milo build page using the vis
    timeline component (http://visjs.org/docs/timeline/). A custom
    build of vis is added which includes only DataSet and Timeline
    (hammer and moment are removed, moment is already included as a
    separate resource).
    
    Bug: 903369
    Change-Id: I8b30a43185132a17913435a387715a56d95ef9ba
    Reviewed-on: https://chromium-review.googlesource.com/c/1342666
    Commit-Queue: David Burger <dburger@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Ryan Tseng <hinoka@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- milo/frontend/view_build.go
+++ milo/frontend/view_build.go
@@ -5,10 +5,13 @@
 package frontend
 
 import (
+	"encoding/json"
 	"fmt"
+	"html"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	bbv1 "go.chromium.org/luci/common/api/buildbucket/buildbucket/v1"
 	"go.chromium.org/luci/common/data/strpair"
@@ -25,6 +28,7 @@ import (
 	"go.chromium.org/luci/milo/buildsource/rawpresentation"
 	"go.chromium.org/luci/milo/buildsource/swarming"
 	"go.chromium.org/luci/milo/common"
+	"go.chromium.org/luci/milo/common/model"
 	"go.chromium.org/luci/milo/frontend/ui"
 )
 
@@ -160,8 +164,129 @@ func renderBuild(c *router.Context, build *ui.MiloBuild, err error) error {
 
 	build.StepDisplayPref = getStepDisplayPrefCookie(c)
 	build.Fix(c.Context)
+
+	timelineJSON, err := timelineData(build)
+	if err != nil {
+		return err
+	}
+
 	templates.MustRender(c.Context, c.Writer, "pages/build.html", templates.Args{
-		"Build": build,
+		"Build":        build,
+		"TimelineJSON": timelineJSON,
 	})
 	return nil
+}
+
+// timelineData returns the timelineJSON for a vis timeline timeline
+// as a JSON.parse parseable string that will contain the necessary
+// Groups and Items.
+func timelineData(build *ui.MiloBuild) (string, error) {
+	// stepData is extra data to deliver with the groups and items (see below) for the
+	// Javascript vis Timeline component.
+	type stepData struct {
+		Label           string       `json:"label"`
+		Text            []string     `json:"text"`
+		Duration        string       `json:"duration"`
+		MainLink        ui.LinkSet   `json:"mainLink"`
+		SubLink         []ui.LinkSet `json:"subLink"`
+		StatusClassName string       `json:"statusClassName"`
+	}
+
+	// group corresponds to, and matches the shape of, a Group for the Javascript
+	// vis Timeline component http://visjs.org/docs/timeline/#groups. Data
+	// rides along as an extra property (unused by vis Timeline itself) used
+	// in client side rendering. Each Group is rendered as its own row in the
+	// timeline component on to which Items are rendered. Currently we only render
+	// one Item per Group, that is one thing per row.
+	type group struct {
+		ID   string   `json:"id"`
+		Data stepData `json:"data"`
+	}
+
+	// item corresponds to, and matches the shape of, an Item for the Javascript
+	// vis Timeline component http://visjs.org/docs/timeline/#items. Data
+	// rides along as an extra property (unused by vis Timeline itself) used
+	// in client side rendering. Each Item is rendered to a Group which corresponds
+	// to a row. Currently we only render one Item per Group, that is one thing per
+	// row.
+	type item struct {
+		ID        string   `json:"id"`
+		Group     string   `json:"group"`
+		Start     int64    `json:"start"`
+		End       int64    `json:"end"`
+		Type      string   `json:"type"`
+		ClassName string   `json:"className"`
+		Data      stepData `json:"data"`
+	}
+
+	groups := make([]group, len(build.Components))
+	items := make([]item, len(build.Components))
+	for index, comp := range build.Components {
+		groupID := strconv.Itoa(index)
+		statusClassName := fmt.Sprintf("status-%s", comp.Status)
+		data := stepData{
+			Label:           html.EscapeString(comp.Label.Label),
+			Text:            sanitize(comp.TextBR()),
+			Duration:        humanDuration(comp.ExecutionTime.Duration),
+			MainLink:        sanitizeLinkSet(comp.MainLink),
+			SubLink:         sanitizeLinkSets(comp.SubLink),
+			StatusClassName: statusClassName,
+		}
+		groups[index] = group{groupID, data}
+		items[index] = item{
+			ID:        groupID,
+			Group:     groupID,
+			Start:     milliseconds(comp.ExecutionTime.Started),
+			End:       milliseconds(comp.ExecutionTime.Finished),
+			Type:      "range",
+			ClassName: statusClassName,
+			Data:      data,
+		}
+	}
+
+	timeline, err := json.Marshal(map[string]interface{}{
+		"groups": groups,
+		"items":  items,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return string(timeline), nil
+}
+
+func sanitize(values []string) []string {
+	result := make([]string, len(values))
+	for i, value := range values {
+		result[i] = html.EscapeString(value)
+	}
+	return result
+}
+
+func sanitizeLinkSet(linkSet ui.LinkSet) ui.LinkSet {
+	result := make(ui.LinkSet, len(linkSet))
+	for i, link := range linkSet {
+		result[i] = &ui.Link{
+			Link: model.Link{
+				Label: html.EscapeString(link.Label),
+				URL:   html.EscapeString(link.URL),
+			},
+			AriaLabel: html.EscapeString(link.AriaLabel),
+			Img:       html.EscapeString(link.Img),
+			Alt:       html.EscapeString(link.Alt),
+		}
+	}
+	return result
+}
+
+func sanitizeLinkSets(linkSets []ui.LinkSet) []ui.LinkSet {
+	result := make([]ui.LinkSet, len(linkSets))
+	for i, linkSet := range linkSets {
+		result[i] = sanitizeLinkSet(linkSet)
+	}
+	return result
+}
+
+func milliseconds(time time.Time) int64 {
+	return time.UnixNano() / 1e6
 }
--- server/analytics/analytics.go
+++ server/analytics/analytics.go
@@ -43,6 +43,7 @@ func Snippet(c context.Context) template.HTML {
 	}
 	return template.HTML(fmt.Sprintf(`
 <script>
+setTimeout(function() {
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -50,6 +51,7 @@ func Snippet(c context.Context) template.HTML {
 
 	ga('create', '%s', 'auto');
 	ga('send', 'pageview');
+}, 0);
 </script>
 `, id))
 }
