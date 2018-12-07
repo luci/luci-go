c956196a0c6ffcc5efe4a5c9fafff279facd0f50
Ryan Tseng <hinoka@google.com>
2018-12-06 16:44:22 -0800

    [milo] Re-run goimports
    This runs:
    goimports -w .
    in the /milo directory
    
    Change-Id: I609b4c17c62acb688ff993dc4accd8176234a8a5

--- milo/buildsource/buildbot/buildstore/build.go
+++ milo/buildsource/buildbot/buildstore/build.go
@@ -27,7 +27,7 @@ import (
 	"go.chromium.org/gae/service/datastore"
 
 	"go.chromium.org/luci/buildbucket"
-	"go.chromium.org/luci/buildbucket/proto"
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
 	bbv1 "go.chromium.org/luci/common/api/buildbucket/buildbucket/v1"
 	"go.chromium.org/luci/common/data/stringset"
 	"go.chromium.org/luci/common/data/strpair"
