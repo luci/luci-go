fdee5e57ed7d4aef697fcddc34e10528a43dd7b3
Vadim Shtayura <vadimsh@chromium.org>
2018-12-10 19:57:09 -0800

    [starlark] Add gensym(...) and ctor(...) builtins.
    They can be used to create structs "tagged" with a symbol. It's not quite
    like a typed struct (each individual object still can have arbitrary fields),
    but pretty close:
    
       mystruct = gensym("mystruct")
       s = mystruct(a=1, b=2)
       assert.eq(ctor(s), mystruct)
       assert.eq(ctor(1), None)
       assert.eq(ctor(struct(a=1, b=2)), "struct")
    
    Can be used to add some sort of type safety to Starlark libraries. Note that
    using type(strct) is insufficient, since it always returns "struct".
    
    R=nodir@chromium.org, iannucci@chromium.org
    BUG=833946
    
    Change-Id: I65abc0594441367a7ecd3c0faa05c06499d61c30

--- starlark/builtins/struct.go
+++ starlark/builtins/struct.go
@@ -15,12 +15,66 @@
 package builtins
 
 import (
+	"fmt"
+
 	"go.starlark.net/starlark"
 	"go.starlark.net/starlarkstruct"
 )
 
-// Struct is struct(**kwargs) builtin.
-//
-//  def struct(**kwargs):
-//    """Returns an immutable object with fields set to given values."""
-var Struct = starlark.NewBuiltin("struct", starlarkstruct.Make)
+var (
+	// Struct is struct(**kwargs) builtin.
+	//
+	//  def struct(**kwargs):
+	//    """Returns an immutable object with fields set to given values."""
+	Struct = starlark.NewBuiltin("struct", starlarkstruct.Make)
+
+	// GenSym is gensym(name) builtin.
+	//
+	//  def gensym(name):
+	//    """Returns a callable constructor for "branded" struct instances."""
+	GenSym = starlark.NewBuiltin("gensym", func(_ *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		var name string
+		if err := starlark.UnpackArgs("gensym", args, kwargs, "name", &name); err != nil {
+			return nil, err
+		}
+		return &symbol{name: name}, nil
+	})
+
+	// Ctor is ctor(obj) builtin.
+	//
+	//  def ctor(obj):
+	//    """Returns a constructor used to construct this struct or None."""
+	Ctor = starlark.NewBuiltin("ctor", func(_ *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		var obj starlark.Value
+		if err := starlark.UnpackArgs("ctor", args, kwargs, "obj", &obj); err != nil {
+			return nil, err
+		}
+		if st, ok := obj.(*starlarkstruct.Struct); ok {
+			return st.Constructor(), nil
+		}
+		return starlark.None, nil
+	})
+)
+
+// Note: the implementation below is copied from
+// https://github.com/google/starlark-go/blob/master/starlarkstruct/struct_test.go
+
+// A symbol is a distinct value that acts as a constructor of "branded"
+// struct instances, like a class symbol in Python or a "provider" in Bazel.
+type symbol struct{ name string }
+
+var _ starlark.Callable = (*symbol)(nil)
+
+func (sym *symbol) Name() string          { return sym.name }
+func (sym *symbol) String() string        { return sym.name }
+func (sym *symbol) Type() string          { return "symbol" }
+func (sym *symbol) Freeze()               {} // immutable
+func (sym *symbol) Truth() starlark.Bool  { return starlark.True }
+func (sym *symbol) Hash() (uint32, error) { return 0, fmt.Errorf("unhashable: %s", sym.Type()) }
+
+func (sym *symbol) CallInternal(thread *starlark.Thread, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	if len(args) > 0 {
+		return nil, fmt.Errorf("%s: unexpected positional arguments", sym)
+	}
+	return starlarkstruct.FromKeywords(sym, kwargs), nil
+}
