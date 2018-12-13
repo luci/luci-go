ca19125404ffce2c066a97bccf72e616af2cc153
Felix Matenaar <fmatenaar@chromium.org>
2018-12-12 17:04:57 -0800

    [Auth] Implement project-scoped service accounts.
    Project-scoped service accounts will extend LUCI tokenserver to
    issue service accounts tied to a particular LUCI project. This will
    reduce the ambient authority under which a LUCI service performs
    a set of actions in order to reduce potential issues in regard to
    the "confused deputy" problem.
    
    R=vadimsh@chromium.org
    
    Bug: 911811
    Change-Id: I7263ce4d2b2d2d49f1e4c064aa6f818e1a3f0e2a

---
+++ tokenserver/appengine/impl/serviceaccounts/rpc_create_project_scope_service_account_test.go
@@ -0,0 +1,102 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package serviceaccounts
+
+import (
+	"context"
+
+	"go.chromium.org/luci/appengine/gaetesting"
+	"go.chromium.org/luci/tokenserver/api/admin/v1"
+	"go.chromium.org/luci/tokenserver/appengine/impl/utils/projectscope"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCreateProjectScopedServiceAccount(t *testing.T) {
+	serviceToProjectMap := map[string]string{
+		"svc":  "bar",
+		"svc3": "baz",
+	}
+
+	serviceAccountMap := map[string]bool{}
+
+	ctx := gaetesting.TestingContext()
+	rpc := CreateProjectScopedServiceAccountRPC{
+		Rules:   nil,
+		Storage: projectscope.Get(),
+		GcpProjectResolver: func(service string) string {
+			return serviceToProjectMap[service]
+		},
+		ServiceAccountCreator: func(ctx context.Context, project, accountId string) (bool, error) {
+			if _, found := serviceAccountMap[accountId]; found {
+				return false, nil
+			}
+			serviceAccountMap[accountId] = true
+			return true, nil
+		},
+	}
+
+	req := &admin.CreateProjectScopedServiceAccountRequest{
+		Service:           "svc",
+		Project:           "project",
+		OverrideAccountId: "",
+	}
+	generatedAccountId, err := projectscope.GenerateAccountId(req.Service, req.Project)
+
+	Convey("Happy path", t, func() {
+
+		// Create a first account
+		So(err, ShouldBeNil)
+		resp, err := rpc.CreateProjectScopedServiceAccount(ctx, req)
+		So(err, ShouldBeNil)
+		So(resp, ShouldResemble, &admin.CreateProjectScopedServiceAccountResponse{
+			AccountId:    generatedAccountId,
+			GcpProjectId: "bar",
+		})
+
+		// Create another account, but redefine req since it won't be used again
+		req := &admin.CreateProjectScopedServiceAccountRequest{
+			Service:           "svc3",
+			Project:           "project3",
+			OverrideAccountId: "",
+		}
+		generatedAccountId, err := projectscope.GenerateAccountId(req.Service, req.Project)
+		resp, err = rpc.CreateProjectScopedServiceAccount(ctx, req)
+		So(err, ShouldBeNil)
+		So(resp, ShouldResemble, &admin.CreateProjectScopedServiceAccountResponse{
+			AccountId:    generatedAccountId,
+			GcpProjectId: "baz",
+		})
+
+	})
+
+	Convey("Account already exists", t, func() {
+		_, err = rpc.CreateProjectScopedServiceAccount(ctx, req)
+		So(err, ShouldNotBeNil)
+		So(err, ShouldResemble, status.Errorf(codes.AlreadyExists, "service account already exists"))
+	})
+
+	Convey("Explicit override for account which already exists", t, func() {
+		req.Service = "svc2"
+		req.OverrideAccountId = generatedAccountId
+		_, err = rpc.CreateProjectScopedServiceAccount(ctx, req)
+		So(err, ShouldNotBeNil)
+		So(err, ShouldResemble, status.Errorf(codes.AlreadyExists, "service account id already taken"))
+	})
+}
