7f7ad0409c88d357124c24fbccda8db661fa1dcc
Gary Miguel <garymm@google.com>
2018-12-26 15:02:15 -0800

    Implement lucicfg validate command.
    This will be useful for users of LUCI who don't use depot_tools, and
    thus can't use the presubmit check that exists in depot_tools.
    This new code is largely based on the existing depot_tools code:
    https://chromium.googlesource.com/chromium/tools/depot_tools.git/+/f28ef9887ee23597d4a0b89d068f7b8e377fc876/presubmit_canned_checks.py#1246
    
    Fuchsia-Bug: https://fuchsia.atlassian.net/browse/IN-855
    Change-Id: I34ec2ed53898e832797d88f7fbd32258e177cb23

--- lucicfg/cli/validate.go
+++ lucicfg/cli/validate.go
@@ -16,11 +16,16 @@ package cli
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	"github.com/maruel/subcommands"
-
+	config "go.chromium.org/luci/common/api/luci_config/config/v1"
 	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/logging"
 )
 
 // TODO(vadimsh): If the config set is not provided, try to guess it from the
@@ -30,60 +35,113 @@ import (
 
 func cmdValidate(params Parameters) *subcommands.Command {
 	return &subcommands.Command{
-		UsageLine: "validate",
-		ShortDesc: "sends *.cfg files to LUCI Config service for validation",
+		UsageLine: "validate -config-set CONFIG_SET [CONFIG_DIR]",
+		ShortDesc: "sends files under CONFIG_DIR (or CWD if not set) to LUCI Config service for validation",
 		CommandRun: func() subcommands.CommandRun {
-			c := &validateRun{}
-			c.init(params, true)
-			c.Flags.StringVar(&c.configSet, "config-set", "<name>", "Name of the config set to validate against.")
-			return c
+			vr := &validateRun{}
+			vr.init(params, true)
+			vr.Flags.StringVar(&vr.configSet, "config-set", "<name>", "Name of the config set to validate against.")
+			return vr
 		},
 	}
 }
 
 type validateRun struct {
 	subcommand
-
 	configSet string
+	configDir string
 }
 
 type validateResult struct {
-	// Note: this is just an example.
-	Files []string `json:"files"`
+	ErrorMessages []*config.ComponentsConfigEndpointValidationMessage `json:"error_messages"`
 }
 
-func (c *validateRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
-	if !c.checkArgs(args, 0, 0) {
+func (vr *validateRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	if !vr.checkArgs(args, 0, 1) {
 		return 1
 	}
-	ctx := cli.GetContext(a, c, env)
-	return c.done(c.run(ctx))
+	if len(args) == 1 {
+		vr.configDir = args[0]
+	} else {
+		configDir, err := os.Getwd()
+		if err != nil {
+			return vr.done(nil, err)
+		}
+		vr.configDir = configDir
+	}
+	ctx := cli.GetContext(a, vr, env)
+	// Construct the service outside run to improve testability.
+	svc, err := vr.configService(ctx)
+	if err != nil {
+		return vr.done(nil, err)
+	}
+	return vr.done(vr.run(ctx, svc))
 }
 
-func (c *validateRun) run(ctx context.Context) (*validateResult, error) {
-	svc, err := c.configService(ctx)
+// run recursively searches vr.configDir for config files, calls svc.ValidateConfig() on them
+// and aggregates the results.
+func (vr *validateRun) run(ctx context.Context, svc *config.Service) (*validateResult, error) {
+	validateRequest, err := vr.constructRequest()
 	if err != nil {
 		return nil, err
 	}
+	resp, err := svc.ValidateConfig(validateRequest).Context(ctx).Do()
+	return processResponse(ctx, resp, err)
+}
 
-	// TODO: implement. For now it just lists files in an existing config set
-	// as an example of how to make RPCs.
-
-	resp, err := svc.GetConfigSets().
-		ConfigSet(c.configSet).
-		IncludeFiles(true).
-		Context(ctx).Do()
+// processResponses produces a validateResult that contains all the messages from resp.
+// Returns an error if any files were invalid or failed to be checked.
+func processResponse(ctx context.Context, resp *config.LuciConfigValidateConfigResponseMessage, err error) (*validateResult, error) {
 	if err != nil {
-		return nil, err
+		return &validateResult{}, fmt.Errorf("error validating configs: %v", err)
 	}
-
-	result := validateResult{}
-	for _, cs := range resp.ConfigSets {
-		for _, f := range cs.Files {
-			fmt.Println(f.Path)
-			result.Files = append(result.Files, f.Path)
+	if len(resp.Messages) == 0 {
+		return &validateResult{}, nil
+	}
+	for _, message := range resp.Messages {
+		lvl := logging.Info
+		if message.Severity == "WARNING" {
+			lvl = logging.Warning
+		} else if message.Severity == "ERROR" || message.Severity == "CRITICAL" {
+			lvl = logging.Error
 		}
+		logging.Logf(ctx, lvl, "%s: %s", message.Path, message.Text)
+	}
+	return &validateResult{resp.Messages}, fmt.Errorf("Some files were invalid")
+}
+
+// constructRequest searches the vr.configDir for config files and constructs a
+// LuciConfigValidateConfigRequestMessage.
+func (vr *validateRun) constructRequest() (*config.LuciConfigValidateConfigRequestMessage, error) {
+	var configFiles []*config.LuciConfigValidateConfigRequestMessageFile
+	err := filepath.Walk(vr.configDir,
+		func(p string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() { // Walk will handle recursion
+				return nil
+			}
+			content, err := ioutil.ReadFile(p)
+			if err != nil {
+				return err
+			}
+			relPath, err := filepath.Rel(vr.configDir, p)
+			if err != nil {
+				return err
+			}
+			configFiles = append(configFiles, &config.LuciConfigValidateConfigRequestMessageFile{
+				Content: base64.StdEncoding.EncodeToString(content),
+				Path:    filepath.ToSlash(relPath),
+			})
+			return nil
+		})
+	if err != nil {
+		return nil, err
 	}
 
-	return &result, nil
+	return &config.LuciConfigValidateConfigRequestMessage{
+		ConfigSet: vr.configSet,
+		Files:     configFiles,
+	}, nil
 }
