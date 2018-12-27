1455d2ba9b23f74bca68e2f1dc9d078a05c232c4
Andrii Shyshkalov <tandrii@chromium.org>
2018-12-26 17:35:27 -0800

    cq: add v1 and v2 cq.cfg protos.
    v1 is to replicate existing v1 validation. It'll be removed soon.
    Both v1 and v2 protos here are the new source of truth,
    they are removed from internal codebase in internal counterpart CL:
      https://chrome-internal-review.googlesource.com/746407
    
    Bug: 884556, 907334, 774159
    Change-Id: I369bbaef3b6f9ec5685ca742ca35554224b96759

---
+++ cq/api/admin/v2/gen.go
@@ -0,0 +1,20 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:generate cproto
+
+// Package admin contains CQ administrative and Config API.
+//
+// Services defined here are used by service administrators.
+package admin
