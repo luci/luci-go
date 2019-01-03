8f20b70edb7d57e1fb618b36fc51e5add64cb6fc
Vadim Shtayura <vadimsh@chromium.org>
2019-01-03 22:35:30 +0000

    [lucicfg] Add a mode for tests in which error(...) immediately aborts execution.
    Note that by default error(...) just collects the error without aborting the
    script, all collected errors are reported at the end.
    
    fail_on_errors() mode is useful for writing smallish unit tests that check error
    detection logic.
    
    R=tandrii@chromium.org, nodir@chromium.org
    BUG=833946
    
    Change-Id: I0ca06978871ec1733ba12d9104fb6675eec99467
    Reviewed-on: https://chromium-review.googlesource.com/c/1394190
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

--- lucicfg/errors.go
+++ lucicfg/errors.go
@@ -65,17 +65,35 @@ func (e *Error) Backtrace() string {
 
 func init() {
 	// emit_error(msg, stack) adds the given error to the list of errors in the
-	// state, to be returned at the end of generation.
+	// state, to be returned at the end of generation (in the default mode), or
+	// immediately aborts the execution if 'fail_on_errors' has been called
+	// before.
 	declNative("emit_error", func(call nativeCall) (starlark.Value, error) {
 		var msg starlark.String
 		var stack *builtins.CapturedStacktrace
 		if err := call.unpack(2, &msg, &stack); err != nil {
 			return nil, err
 		}
-		call.State.err(&Error{
+		err := &Error{
 			Msg:   msg.GoString(),
 			Stack: stack,
-		})
+		}
+		if call.State.failOnErrs {
+			return nil, err
+		}
+		call.State.err(err)
+		return starlark.None, nil
+	})
+
+	// fail_on_errors() enables a mode in which emit_error(...) immediately aborts
+	// the execution instead of just recording the error. This is useful in
+	// tests to check what errors are emitted (using assert.fails). A call to
+	// clear_state() resets this mode.
+	declNative("fail_on_errors", func(call nativeCall) (starlark.Value, error) {
+		if err := call.unpack(0); err != nil {
+			return nil, err
+		}
+		call.State.failOnErrs = true
 		return starlark.None, nil
 	})
 }
