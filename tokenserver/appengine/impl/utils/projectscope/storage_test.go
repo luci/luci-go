4cc31fade8407802c899dddcc6328b74081b4d65
Felix Matenaar <fmatenaar@chromium.org>
2019-01-04 16:24:01 -0800

    [Auth] Implement project-scoped service accounts.
    Project-scoped service accounts will extend LUCI tokenserver to
    issue service accounts tied to a particular LUCI project. This will
    reduce the ambient authority under which a LUCI service performs
    a set of actions in order to reduce potential issues in regard to
    the "confused deputy" problem.
    
    R=vadimsh@chromium.org
    
    Bug: 911811
    Change-Id: I7263ce4d2b2d2d49f1e4c064aa6f818e1a3f0e2a

---
+++ tokenserver/appengine/impl/utils/projectscope/storage_test.go
@@ -0,0 +1,79 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package projectscope
+
+import (
+	"testing"
+
+	"go.chromium.org/luci/appengine/gaetesting"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestScopedServiceAccountStorage(t *testing.T) {
+
+	Convey("Test persistent storage state handling", t, func() {
+		ctx := gaetesting.TestingContext()
+		storage := &persistentIdentityManager{}
+
+		_, err := storage.Get(ctx, "service1", "project1")
+		So(err, ShouldNotBeNil)
+
+		identity, created, err := storage.GetOrCreate(ctx, "service1", "project1", "", nil)
+		So(err, ShouldBeNil)
+		So(created, ShouldResemble, true)
+		So(identity.AccountId, ShouldResemble, "ps-600a8066dfcb40a0ee2d9036563")
+
+		identity, err = storage.Get(ctx, "service1", "project1")
+		So(err, ShouldBeNil)
+		So(identity.AccountId, ShouldResemble, "ps-600a8066dfcb40a0ee2d9036563")
+
+		identity, created, err = storage.GetOrCreate(ctx, "service1", "project1", "", nil)
+		So(err, ShouldBeNil)
+		So(created, ShouldResemble, false)
+		So(identity.AccountId, ShouldResemble, "ps-600a8066dfcb40a0ee2d9036563")
+
+		identity, created, err = storage.GetOrCreate(ctx, "service2", "project2", "", nil)
+		So(err, ShouldBeNil)
+		So(created, ShouldResemble, true)
+		So(identity.AccountId, ShouldResemble, "ps-30b131968fa6e7134042d4f3bd3")
+
+		identity, created, err = storage.GetOrCreate(ctx, "service2", "project2", "", nil)
+		So(err, ShouldBeNil)
+		So(created, ShouldResemble, false)
+		So(identity.AccountId, ShouldResemble, "ps-30b131968fa6e7134042d4f3bd3")
+	})
+
+	Convey("Test Lookup functionality", t, func() {
+		service := "servicefoo"
+		project := "projectbar"
+		ctx := gaetesting.TestingContext()
+		storage := &persistentIdentityManager{}
+
+		identity, created, err := storage.GetOrCreate(ctx, service, project, "", nil)
+		So(err, ShouldBeNil)
+		So(created, ShouldResemble, true)
+		So(identity.AccountId, ShouldResemble, GenerateAccountId(service, project))
+
+		scopedIdentity, err := storage.Lookup(ctx, identity.AccountId)
+		So(err, ShouldBeNil)
+		So(scopedIdentity, ShouldResemble, &ScopedIdentity{
+			AccountId: identity.AccountId,
+			Service:   service,
+			Project:   project,
+		})
+	})
+
+}
