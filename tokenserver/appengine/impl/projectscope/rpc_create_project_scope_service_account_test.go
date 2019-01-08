78f7dfb154767354cdecedaf62072cb829b5ecff
Felix Matenaar <fmatenaar@chromium.org>
2019-01-07 17:20:43 -0800

    [Auth] Implement project-scoped service accounts.
    Project-scoped service accounts will extend LUCI tokenserver to
    issue service accounts tied to a particular LUCI project. This will
    reduce the ambient authority under which a LUCI service performs
    a set of actions in order to reduce potential issues in regard to
    the "confused deputy" problem.
    
    R=vadimsh@chromium.org
    
    Bug: 911811
    Change-Id: I7263ce4d2b2d2d49f1e4c064aa6f818e1a3f0e2a

---
+++ tokenserver/appengine/impl/projectscope/rpc_create_project_scope_service_account_test.go
@@ -0,0 +1,152 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package projectscope
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strconv"
+
+	"go.chromium.org/luci/common/gcloud/iam"
+
+	"go.chromium.org/luci/appengine/gaetesting"
+	"go.chromium.org/luci/tokenserver/api/admin/v1"
+	"go.chromium.org/luci/tokenserver/appengine/impl/utils/projectscope"
+
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func newTestGcpProjectResolver() *testGcpProjectResolver {
+	return &testGcpProjectResolver{
+		m: make(map[string]string),
+	}
+}
+
+type testGcpProjectResolver struct {
+	m map[string]string
+}
+
+func (s *testGcpProjectResolver) Resolve(service string) (string, error) {
+	if project, ok := s.m[service]; ok {
+		return project, nil
+	}
+	return "", fmt.Errorf("not found")
+}
+
+func (s *testGcpProjectResolver) Add(service string, gcpProject string) {
+	s.m[service] = gcpProject
+}
+
+func (s *testGcpProjectResolver) Remove(service string) {
+	delete(s.m, service)
+}
+
+func newServiceAccount(luciService string, luciProject string, gcpProject string) *iam.ServiceAccount {
+	accountId := projectscope.GenerateAccountId(luciService, luciProject)
+	email := fmt.Sprintf("%s@%s.iam.gserviceaccount.com", accountId, luciProject)
+	return &iam.ServiceAccount{
+		Name:        fmt.Sprintf("projects/%s/serviceAccounts/%s", luciProject, email),
+		ProjectId:   gcpProject,
+		UniqueId:    strconv.Itoa(rand.Int()),
+		Email:       email,
+		DisplayName: accountId,
+	}
+}
+
+func testServiceAccountCreator(ctx context.Context, gcpProject string, identity *projectscope.ScopedIdentity, client projectscope.ServiceAccountClient) (*iam.ServiceAccount, bool, error) {
+	m := map[string]*iam.ServiceAccount{
+		projectscope.GenerateAccountId("svc", "project"):   newServiceAccount("svc", "project", "bar"),
+		projectscope.GenerateAccountId("svc3", "project3"): newServiceAccount("svc3", "project3", "baz"),
+	}
+
+	fmt.Printf("Looking for identity.AccountId = %s", identity.AccountId)
+	if sa, found := m[identity.AccountId]; found {
+		return sa, true, nil
+	}
+	return nil, false, fmt.Errorf("unable to create service account")
+}
+
+func TestCreateProjectScopedServiceAccount(t *testing.T) {
+	ctx := gaetesting.TestingContext()
+	rpc := CreateProjectScopedServiceAccountRPC{
+		ScopedIdentities: projectscope.ScopedIdentities,
+		Rules: func(c context.Context) (*Rules, error) {
+			return &Rules{
+				revision: "fake-revision",
+				rulesPerService: map[string]*Rule{
+					"svc": {
+						CloudProjectID: "bar",
+						Rule: &admin.ServiceConfig{
+							Service:        "svc",
+							CloudProjectId: "bar",
+						},
+						Revision: "fake-revision",
+					},
+					"svc3": {
+						CloudProjectID: "baz",
+						Rule: &admin.ServiceConfig{
+							Service:        "svc3",
+							CloudProjectId: "baz",
+						},
+						Revision: "fake-revision",
+					},
+				},
+			}, nil
+		},
+		CreateServiceAccount: testServiceAccountCreator,
+	}
+
+	req := &admin.CreateProjectScopedServiceAccountRequest{
+		Service: "svc",
+		Project: "project",
+	}
+	generatedAccountId := projectscope.GenerateAccountId(req.Service, req.Project)
+
+	Convey("Happy path", t, func() {
+
+		// Create a first account
+		resp, err := rpc.CreateProjectScopedServiceAccount(ctx, req)
+		So(err, ShouldBeNil)
+		So(resp, ShouldResemble, &admin.CreateProjectScopedServiceAccountResponse{
+			AccountId: generatedAccountId,
+		})
+
+		// Create another account, but redefine req since it won't be used again
+		req := &admin.CreateProjectScopedServiceAccountRequest{
+			Service: "svc3",
+			Project: "project3",
+		}
+		generatedAccountId := projectscope.GenerateAccountId(req.Service, req.Project)
+		resp, err = rpc.CreateProjectScopedServiceAccount(ctx, req)
+		So(err, ShouldBeNil)
+		So(resp, ShouldResemble, &admin.CreateProjectScopedServiceAccountResponse{
+			AccountId: generatedAccountId,
+		})
+
+	})
+
+	Convey("Account already exists", t, func() {
+		_, err := rpc.CreateProjectScopedServiceAccount(ctx, req)
+		So(err, ShouldNotBeNil)
+	})
+
+	Convey("Test invalid input", t, func() {
+		_, err := rpc.CreateProjectScopedServiceAccount(ctx, &admin.CreateProjectScopedServiceAccountRequest{})
+		So(err, ShouldNotBeNil)
+	})
+}
---
+++ tokenserver/appengine/impl/projectscope/rpc_mint_service_oauth_token.go
@@ -0,0 +1,228 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package projectscope
+
+import (
+	"context"
+	"time"
+
+	"go.chromium.org/gae/service/info"
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/proto/google"
+	"go.chromium.org/luci/common/retry/transient"
+	"go.chromium.org/luci/server/auth/authdb"
+	"go.chromium.org/luci/tokenserver/appengine/impl/serviceaccounts"
+	"go.chromium.org/luci/tokenserver/appengine/impl/utils"
+	"go.chromium.org/luci/tokenserver/appengine/impl/utils/projectscope"
+
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/auth/signing"
+	"golang.org/x/oauth2"
+
+	"github.com/golang/protobuf/jsonpb"
+	"go.chromium.org/luci/auth/identity"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/tokenserver/api/minter/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// MintOAuthTokenViaGrantRPC implements TokenMinter.MintOAuthTokenViaGrant
+// method.
+type MintServiceOAuthTokenRPC struct {
+	// Signer is mocked in tests.
+	//
+	// In prod it is gaesigner.Signer.
+	Signer signing.Signer
+
+	// Rules returns service account rules to use for the request.
+	//
+	// In prod it is GlobalRulesCache.Rules.
+	Rules func(context.Context) (*Rules, error)
+
+	// MintAccessToken produces an OAuth token for a service account.
+	//
+	// In prod it is auth.MintAccessTokenForServiceAccount.
+	MintAccessToken func(context.Context, auth.MintAccessTokenParams) (*oauth2.Token, error)
+
+	// LogOAuthToken is mocked in tests.
+	//
+	// In prod it is LogOAuthToken from oauth_token_bigquery_log.go.
+	LogOAuthToken func(context.Context, serviceaccounts.LoggableOAuthTokenInfo) error
+
+	// Storage manages project scoped identities.
+	//
+	// In  prod it is projectscope.persistentIdentityManager
+	Storage projectscope.ScopedIdentityManager
+
+	// CreateServiceAccount can be set to use in tests
+	CreateServiceAccount projectscope.ServiceAccountCreator
+}
+
+// MinServiceOAuthToken mints a project-scoped service account token.
+//
+// Project-scoped service accounts are identities tied to an individual LUCI project.
+// Therefore they provide a way to safely interact with LUCI APIs and prevent accidental
+// cross-project operations.
+func (r *MintServiceOAuthTokenRPC) MintServiceOAuthToken(c context.Context, req *minter.MintServiceOAuthTokenRequest) (*minter.MintServiceOAuthTokenResponse, error) {
+	state := auth.GetState(c)
+
+	callerId := state.User().Identity
+	if callerId != state.PeerIdentity() {
+		logging.Errorf(c, "Trying to use delegation, it's forbidden")
+		return nil, status.Errorf(codes.PermissionDenied, "delegation is forbidden for this API call")
+	}
+
+	rule, err := r.validateRequest(c, req, callerId)
+	if err != nil {
+		return nil, err
+	}
+	if req.MinValidityDuration == 0 {
+		if rule.Rule.MaxValidityDuration > maxAllowedMaxGrantValidityDuration {
+			req.MinValidityDuration = maxAllowedMaxGrantValidityDuration
+		} else {
+			req.MinValidityDuration = rule.Rule.MaxValidityDuration
+		}
+	}
+
+	// Scoped identity should not exist yet, and we'll try to eagerly create the IAM service account
+	scopedIdentity, created, err := r.Storage.GetOrCreate(c, string(callerId), req.LuciProject, rule.CloudProjectID, r.CreateServiceAccount)
+	if err != nil {
+		logging.WithError(err)
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+
+	accessTok, err := r.MintAccessToken(c, auth.MintAccessTokenParams{
+		ServiceAccount: scopedIdentity.AccountId,
+		Scopes:         req.OauthScope,
+		MinTTL:         time.Duration(int64(time.Second) * req.MinValidityDuration),
+	})
+
+	// Minting token might fail because of the Cloud IAM is not ready to issue a token
+	// right after service account creation. If this is the case, a transient error is indicated
+	// to the client which is encouraged to retry.
+	// All other errors are non-transient.
+	if err != nil {
+		logging.WithError(err).Errorf(c, "Failed to mint scoped oauth token for service %q in project %q, created=%t", callerId, req.LuciProject, created)
+		msg := "failed to mint scoped oauth token"
+		if created {
+			code := codes.ResourceExhausted
+			return nil, transient.Tag.Apply(status.Errorf(code, msg))
+		}
+		code := codes.Internal
+		return nil, status.Errorf(code, msg)
+	}
+
+	if err != nil && created {
+		logging.WithError(err).Errorf(c, "Failed to mint scoped oauth token for service %q in project %q, ")
+	}
+	if err != nil {
+		logging.WithError(err).Errorf(c, "Failed to mint scoped oauth token for service %q in project %q", callerId, req.LuciProject)
+		code := codes.InvalidArgument
+		if transient.Tag.In(err) {
+			code = codes.Internal
+		}
+		return nil, status.Errorf(code, "failed to mint scoped oauth token for service %q in project %q", callerId, req.LuciProject)
+	}
+
+	serviceVer, err := utils.ServiceVersion(c, r.Signer)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "can't grab service version - %s", err)
+	}
+
+	resp := &minter.MintServiceOAuthTokenResponse{
+		ServiceAccount: scopedIdentity.ServiceAccount.Email,
+		AccessToken:    accessTok.AccessToken,
+		Expiry:         google.NewTimestamp(accessTok.Expiry),
+		ServiceVersion: serviceVer,
+	}
+
+	// Log it to BigQuery
+	if r.LogOAuthToken != nil {
+		// Errors during logging are considered not fatal. bqlog library has
+		// a monitoring counter that tracks number of errors, so they are not
+		// totally invisible.
+		info := serviceaccounts.MintedServiceOAuthTokenInfo{
+			Request:  req,
+			Response: resp,
+			Rule:     rule.Rule,
+			OAuthTokenInfo: serviceaccounts.OAuthTokenInfo{
+				RequestedAt: clock.Now(c),
+				ConfigRev:   rule.Revision,
+				PeerIP:      state.PeerIP(),
+				RequestID:   info.RequestID(c),
+				AuthDBRev:   authdb.Revision(state.DB()),
+			},
+		}
+		if logErr := r.LogOAuthToken(c, &info); logErr != nil {
+			logging.WithError(logErr).Errorf(c, "Failed to insert the oauth token into the BigQuery log")
+		}
+	}
+	return resp, nil
+}
+
+// validateRequest performs basic request validation and pulls the configuration context according to the caller identity.
+func (r *MintServiceOAuthTokenRPC) validateRequest(c context.Context, req *minter.MintServiceOAuthTokenRequest, caller identity.Identity) (*Rule, error) {
+	r.logRequest(c, req, caller)
+
+	// Check required proto fields.
+	if req.LuciProject == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "luci project must be specified")
+	}
+	if len(req.OauthScope) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "oauth scopes must be specified")
+	}
+
+	// Pick the proper configuration for the calling service.
+	rules, err := r.Rules(c)
+	if err != nil {
+		return nil, err
+	}
+	rule, found := rules.rulesPerService[string(caller)]
+	if !found {
+		return nil, status.Errorf(codes.NotFound, "service %s not registered", caller)
+	}
+	return rule, nil
+}
+
+// logRequest logs the body of the request.
+func (r *MintServiceOAuthTokenRPC) logRequest(c context.Context, req *minter.MintServiceOAuthTokenRequest, caller identity.Identity) {
+	if !logging.IsLogging(c, logging.Debug) {
+		return
+	}
+	m := jsonpb.Marshaler{Indent: " "}
+	dump, _ := m.MarshalToString(req)
+	logging.Debugf(c, "Identity: %s", caller)
+	logging.Debugf(c, "MintServiceOAuthTokenRequest:\n%s", dump)
+}
+
+// checkRules verifies the requested token is allowed to be issued according to the rules.
+func (r *MintServiceOAuthTokenRPC) checkRules(c context.Context, req *minter.MintServiceOAuthTokenRequest, caller identity.Identity) (*Rule, error) {
+	rules, err := r.Rules(c)
+	if err != nil {
+		logging.WithError(err).Errorf(c, "Failed to load project-scoped service account rules")
+		return nil, status.Errorf(codes.Internal, "failed to load project-scoped service account rules")
+	}
+	rule, found := rules.rulesPerService[string(caller)]
+	if !found {
+		return nil, status.Errorf(codes.NotFound, "service %s not registered", caller)
+	}
+
+	if req.MinValidityDuration > rule.Rule.MaxValidityDuration {
+		logging.Errorf(c, "Requested validity is larger than allowed: %d > %d", req.MinValidityDuration, rule.Rule.MaxValidityDuration)
+		return nil, status.Errorf(codes.InvalidArgument, "per service config %q the validity duration should be <= %d", rule.Rule.Service, rule.Rule.MaxValidityDuration)
+	}
+	return rule, nil
+}
