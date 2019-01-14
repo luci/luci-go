4fdae58fe9585256b6679d32af29f3bb008c73d0
Andrii Shyshkalov <tandrii@chromium.org>
2019-01-14 19:15:33 +0000

    [validation] delete spurious godoc line.
    doc.go already contains a more detailed godoc for this package.
    
    R=vadimsh
    
    Change-Id: Iac8cb9ab10d8f54e24f81a48edd13d63fc7dcdd1
    Reviewed-on: https://chromium-review.googlesource.com/c/1409765
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>

--- config/validation/net_util.go
+++ config/validation/net_util.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package validation provides a helper for performing config validations.
 package validation
 
 import (
