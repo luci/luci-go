1e5a1afe3c51f2ee898e35015fa498fbbc95f638
David Burger <dburger@chromium.org>
2019-01-17 17:45:31 -0700

    Restore timeline functionality for new build page
    Restore the timeline functionality that was briefly present for build
    pages and now has been adjusted to work with the new build page approach.
    
    Note that some time handling functions previously in middleware.go were
    moved to a common/time.go location to avoid a cyclic dependency problem
    when trying to use them in the build.go page directly. I believe this
    cyclic dependency situation is eliminated when view_build_legacy.go is
    retired.
    
    BUG=903369
    
    Change-Id: I92f211a7a68bf487edae31293ca3be64630b1ace

--- milo/api/config/project.pb.go
+++ milo/api/config/project.pb.go
@@ -31,16 +31,21 @@ type Project struct {
 	// This field is optional. The logo URL must have a host of
 	// storage.googleapis.com.
 	LogoUrl string `protobuf:"bytes,4,opt,name=logo_url,json=logoUrl,proto3" json:"logo_url,omitempty"`
-	// BugTemplate is the template for making a custom feedback link for
+	// BuildBugTemplate is the template for making a custom feedback link for
 	// the build page. This field is optional. Supported interpolations
 	// for the "summary" and "description" fields in this context are:
 	//
+	// Some populated fields of the standard build proto:
 	// {{.Build.Builder.Project}}
 	// {{.Build.Builder.Bucket}}
 	// {{.Build.Builder.Builder}}
 	//
-	// If any specified placeholder cannot be satisfied then a default URL
-	// is rendered for the feedback link.
+	// Others:
+	// {{.MiloBuildUrl}}
+	// {{.MiloBuilderUrl}}
+	//
+	// If any specified placeholder cannot be satisfied then no URL
+	// is rendered for the build page feedback link.
 	BuildBugTemplate     *BugTemplate `protobuf:"bytes,5,opt,name=build_bug_template,json=buildBugTemplate,proto3" json:"build_bug_template,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}     `json:"-"`
 	XXX_unrecognized     []byte       `json:"-"`
