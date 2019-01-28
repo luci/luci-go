c652e627266ee85821712900166bd93dc23e0cba
David Burger <dburger@chromium.org>
2019-01-28 07:30:09 -0700

    Add build page feedback links for LUCI build pages
    Add the build page feedback links available in the legacy view build
    pages to the LUCI build pages.
    
    Bug:903385
    Change-Id: I0ca24331a90f62f30e179be8ebeae0ffba51e673

--- milo/buildsource/buildbucket/build.go
+++ milo/buildsource/buildbucket/build.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"reflect"
 	"strconv"
 
 	"github.com/golang/protobuf/ptypes"
@@ -32,11 +33,12 @@ import (
 	"go.chromium.org/luci/common/logging"
 	gitpb "go.chromium.org/luci/common/proto/git"
 	"go.chromium.org/luci/grpc/prpc"
-	"go.chromium.org/luci/server/auth"
-
+	"go.chromium.org/luci/milo/api/config"
 	"go.chromium.org/luci/milo/common"
 	"go.chromium.org/luci/milo/common/model"
 	"go.chromium.org/luci/milo/frontend/ui"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/router"
 )
 
 var ErrNotFound = errors.Reason("Build not found").Tag(common.CodeNotFound).Err()
@@ -202,24 +204,51 @@ var fullBuildMask = &field_mask.FieldMask{
 	},
 }
 
+// getBugLink attempts to formulate and return the build page feedback link
+// for the given build.
+func getBugLink(c *router.Context, b *buildbucketpb.Build) (string, error) {
+	project, err := common.GetProject(c.Context, b.Builder.GetProject())
+	if err != nil || reflect.DeepEqual(project.BuildBugTemplate, config.BugTemplate{}) {
+		return "", err
+	}
+
+	builderPath := fmt.Sprintf("/p/%s/builders/%s/%s", b.Builder.GetProject(), b.Builder.GetBucket(), b.Builder.GetBuilder())
+	buildURL, err := c.Request.URL.Parse(builderPath + "/" + c.Params.ByName("numberOrId"))
+	if err != nil {
+		return "", errors.Annotate(err, "Unable to make build URL for build bug link.").Err()
+	}
+	builderURL, err := c.Request.URL.Parse(builderPath)
+	if err != nil {
+		return "", errors.Annotate(err, "Unable to make builder URL for build bug link.").Err()
+	}
+
+	return MakeBuildBugLink(&project.BuildBugTemplate, map[string]interface{}{
+		"Build":          b,
+		"MiloBuildUrl":   buildURL,
+		"MiloBuilderUrl": builderURL,
+	})
+}
+
 // GetBuildPage fetches the full set of information for a Milo build page from Buildbucket.
 // Including the blamelist and other auxiliary information.
-func GetBuildPage(c context.Context, br buildbucketpb.GetBuildRequest) (*ui.BuildPage, error) {
+func GetBuildPage(c *router.Context, br buildbucketpb.GetBuildRequest) (*ui.BuildPage, error) {
 	br.Fields = fullBuildMask
-	host, err := getHost(c)
+	host, err := getHost(c.Context)
 	if err != nil {
 		return nil, err
 	}
-	b, err := GetBuild(c, host, br)
+	b, err := GetBuild(c.Context, host, br)
 	if err != nil {
 		return nil, err
 	}
-	blame, err := getBlame(c, host, b)
+	blame, err := getBlame(c.Context, host, b)
 	if err != nil {
 		return nil, err
 	}
+	link, err := getBugLink(c, b)
 	return &ui.BuildPage{
-		Build: *b,
-		Blame: blame,
+		Build:             *b,
+		Blame:             blame,
+		BuildBugLink: link,
 	}, nil
 }
