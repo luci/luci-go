c7659e7ccc9380e511eab1939b956441c4c1cc58
Ryan Tseng <hinoka@google.com>
2019-01-30 16:16:55 -0800

    [milo] Fix timeline not rendering for large builds
    Bug: 927086
    Change-Id: I425e0bb59e42159cb86a25058224c5b8eb754721

--- milo/frontend/appengine/static/common/js/build.js
+++ milo/frontend/appengine/static/common/js/build.js
@@ -96,6 +96,17 @@ $(document).ready(function() {
       // activate event hack).
       if (ui.newPanel.attr('id') === 'timeline-tab' && timeline === null) {
         timeline = createTimeline();
+        // HACK(hinoka): Sometimes the timeline renders but stays hidden.
+        // This appears to be a vis bug.
+        // We detect this condition because initialRangeChangeDone wouldn't
+        // have been set (Even if the range it set).
+        // Manually flip the flag and force a re-render if this happens.
+        setTimeout(() => {
+          if (!timeline.initialRangeChangeDone) {
+            timeline.initialRangeChangeDone = true;
+            timeline.redraw();
+          }
+        }, 0);
       }
     },
   }).show();
