6e7ac4213990fed5a8a7ac5a9855e0b5e1380b95
Felix Matenaar <fmatenaar@chromium.org>
2019-02-11 11:43:34 -0800

    [luci-token-server] Implement project-scoped service accounts, server side.
    Project-scoped service accounts will extend LUCI tokenserver to
    issue service accounts tied to a particular LUCI project. This will
    reduce the ambient authority under which a LUCI service performs
    a set of actions in order to reduce potential issues in regard to
    the "confused deputy" problem.
    
    Bug: 911811
    Change-Id: I923bcd46a0f4c6fda8e742d13b06d37bfa5b0553

--- tokenserver/appengine/impl/services/minter/tokenminter/service.go
+++ tokenserver/appengine/impl/services/minter/tokenminter/service.go
@@ -20,6 +20,8 @@ package tokenminter
 import (
 	"go.chromium.org/luci/appengine/gaeauth/server/gaesigner"
 	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/tokenserver/appengine/impl/projectscope"
+	projectscopeutils "go.chromium.org/luci/tokenserver/appengine/impl/utils/projectscope"
 
 	"go.chromium.org/luci/tokenserver/appengine/impl/certchecker"
 	"go.chromium.org/luci/tokenserver/appengine/impl/delegation"
@@ -38,6 +40,7 @@ type serverImpl struct {
 	delegation.MintDelegationTokenRPC
 	serviceaccounts.MintOAuthTokenGrantRPC
 	serviceaccounts.MintOAuthTokenViaGrantRPC
+	projectscope.MintProjectTokenRPC
 }
 
 // NewServer returns prod TokenMinterServer implementation.
@@ -65,6 +68,12 @@ func NewServer() minter.TokenMinterServer {
 			Rules:           serviceaccounts.GlobalRulesCache.Rules,
 			MintAccessToken: auth.MintAccessTokenForServiceAccount,
 			LogOAuthToken:   serviceaccounts.LogOAuthToken,
+		},
+		MintProjectTokenRPC: projectscope.MintProjectTokenRPC{
+			Signer:            gaesigner.Signer{},
+			MintAccessToken:   auth.MintAccessTokenForServiceAccount,
+			LogOAuthToken:     serviceaccounts.LogOAuthToken,
+			ProjectIdentities: projectscopeutils.ProjectIdentities,
 		},
 	}
 }
---
+++ tokenserver/appengine/impl/utils/projectscope/storage.go
@@ -0,0 +1,130 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package projectscope
+
+import (
+	"context"
+	"errors"
+
+	"go.chromium.org/luci/common/gcloud/iam"
+	"go.chromium.org/luci/common/retry/transient"
+
+	ds "go.chromium.org/gae/service/datastore"
+)
+
+var (
+	// ErrAlreadyExists indicated that the entity already exists in the storage.
+	ErrAlreadyExists = errors.New("already exists")
+
+	// ErrNotFound indicates that the entity which was queried does not exist in the storage.
+	ErrNotFound = errors.New("not found")
+)
+
+// projectIdentities is the default storage for all scoped identities.
+var projectIdentities = &persistentIdentityManager{}
+
+// ProjectIdentities returns the global scoped identity storage.
+func ProjectIdentities(ctx context.Context) ProjectIdentityManager {
+	return projectIdentities
+}
+
+// ProjectIdentityManager interface declares the interface to the scoped identity storage.
+type ProjectIdentityManager interface {
+
+	// Create an identity based on a (project, email) combination
+	Create(c context.Context, project, email string, serviceAccount iam.ServiceAccount) (*ProjectIdentity, error)
+
+	// Delete an identity from the storage.
+	Delete(c context.Context, identity *ProjectIdentity) error
+
+	// Lookup an identity from the storage.
+	Lookup(c context.Context, identity *ProjectIdentity) (*ProjectIdentity, error)
+
+	// LookupByProject performs a lookup by project name.
+	LookupByProject(c context.Context, project string) (*ProjectIdentity, error)
+}
+
+// ProjectIdentity defines a scoped identity in the storage.
+type ProjectIdentity struct {
+	_kind          string `gae:"$kind,ScopedIdentity"`
+	Project        string `gae:"$id"`
+	Email          string
+	ServiceAccount iam.ServiceAccount `gae:",noindex"`
+}
+
+// persistentIdentityManager implements ScopedIdentityManager.
+type persistentIdentityManager struct {
+}
+
+func (s *persistentIdentityManager) lookupInternal(c context.Context, identity *ProjectIdentity) error {
+	if err := ds.Get(c, identity); err != nil {
+		switch {
+		case err == ds.ErrNoSuchEntity:
+			return ErrNotFound
+		case err != nil:
+			return transient.Tag.Apply(err)
+		}
+	}
+	return nil
+}
+
+// LookupByProject returns the project identity stored for a given project.
+func (s *persistentIdentityManager) LookupByProject(c context.Context, project string) (*ProjectIdentity, error) {
+	return s.Lookup(c, &ProjectIdentity{Project: project})
+}
+
+// Lookup reads an identity from the storage based on what fields are set in the identity struct.
+func (s *persistentIdentityManager) Lookup(c context.Context, identity *ProjectIdentity) (*ProjectIdentity, error) {
+	if err := s.lookupInternal(c, identity); err != nil {
+		return nil, err
+	}
+	return identity, nil
+}
+
+// Delete removes an identity from the storage.
+func (s *persistentIdentityManager) Delete(c context.Context, identity *ProjectIdentity) error {
+	return ds.RunInTransaction(c, func(c context.Context) error {
+		err := s.lookupInternal(c, identity)
+		if err != nil {
+			return err
+		}
+		return ds.Delete(c, identity)
+	}, nil)
+
+}
+
+// Create stores a new entry for a project identity.
+func (s *persistentIdentityManager) Create(c context.Context, project, email string, serviceAccount iam.ServiceAccount) (*ProjectIdentity, error) {
+	identity := &ProjectIdentity{
+		Project:        project,
+		Email:          email,
+		ServiceAccount: serviceAccount,
+	}
+	err := ds.RunInTransaction(c, func(c context.Context) error {
+		err := s.lookupInternal(c, identity)
+		switch {
+		case err == nil:
+			return ErrAlreadyExists
+		case err == ErrNotFound:
+			return ds.Put(c, identity)
+		default:
+			return err
+		}
+	}, nil)
+	if err != nil {
+		return nil, err
+	}
+	return identity, nil
+}
