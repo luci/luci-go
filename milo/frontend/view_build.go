c38f78163ef50d952ebcaebfdecaa35717960cf7
Ryan Tseng <hinoka@google.com>
2019-02-13 18:51:33 -0800

    [milo] Change internal build links to BBv2
    So the point to the build endpoint for v2.
    
    Bug: 850113
    Change-Id: I6f60b712c74a0377df6e3d44125b026ced8debe6

--- milo/frontend/view_build.go
+++ milo/frontend/view_build.go
@@ -113,7 +113,9 @@ func redirectLUCIBuild(c *router.Context) error {
 	for _, t := range build.Tags {
 		switch k, v := strpair.Parse(t); k {
 		case bbv1.TagBuildAddress:
-			_, project, bucket, builder, number, _ := bbv1.ParseBuildAddress(v)
+			_, project, v1bucket, builder, number, _ := bbv1.ParseBuildAddress(v)
+			// Use V2 bucket
+			_, bucket := bb.BucketNameToV2(v1bucket)
 			if number > 0 {
 				u.Path = fmt.Sprintf("/p/%s/builders/%s/%s/%d", project, bucket, builder, number)
 				http.Redirect(c.Writer, c.Request, u.String(), http.StatusMovedPermanently)
@@ -128,7 +130,9 @@ func redirectLUCIBuild(c *router.Context) error {
 		return errors.Reason("build %s does not have a builder", idStr).Tag(common.CodeParameterError).Err()
 	}
 
-	u.Path = fmt.Sprintf("/p/%s/builders/%s/%s/b%d", build.Project, build.Bucket, builder, build.Id)
+	// Use V2 bucket
+	_, bucket := bb.BucketNameToV2(build.Bucket)
+	u.Path = fmt.Sprintf("/p/%s/builders/%s/%s/b%d", build.Project, bucket, builder, build.Id)
 	http.Redirect(c.Writer, c.Request, u.String(), http.StatusMovedPermanently)
 	return nil
 }
