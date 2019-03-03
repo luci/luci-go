56f33eb1818c6a35b9ba090c48e1c4c1d141a479
Nodir Turakulov <nodir@google.com>
2019-03-02 16:02:23 -0800

    [buildbucket] Move fields from Build.Output
    Build.input and Build.output were not well defined, and that lead to
    arbitrary/bad decisions whether to put a field to Build.{input,output} or to
    Build.
    
    Define their scope more clearly and move some fields while we can:
    - criticality isn't used by anything else yet.
      It is not necessarily returned by a recipe.
      It is easy to image that it is set in a builder def.
    - summary_markdown is set not only by a recipe.
      For example, buildbucket sets it in some cases of INFRA_FAILURE today.
      The field is used only by Milo, which is updated in this CL.
      Current Milo impl will backfill the summary markdown if it is missing,
      with a list of failed steps, so it is reasonably safe to make this breaking
      change.
    
    Corresponding update in buildbucket impl:
    https://chromium-review.googlesource.com/c/infra/infra/+/1497826
    
    Change-Id: I91494e9512b578801b12d927999c79f455d0a9db

--- buildbucket/proto/rpc.mock.pb.go
+++ buildbucket/proto/rpc.mock.pb.go
@@ -36,11 +36,13 @@ func (m *MockisBatchRequest_Request_Request) EXPECT() *MockisBatchRequest_Reques
 
 // isBatchRequest_Request_Request mocks base method
 func (m *MockisBatchRequest_Request_Request) isBatchRequest_Request_Request() {
+	m.ctrl.T.Helper()
 	m.ctrl.Call(m, "isBatchRequest_Request_Request")
 }
 
 // isBatchRequest_Request_Request indicates an expected call of isBatchRequest_Request_Request
 func (mr *MockisBatchRequest_Request_RequestMockRecorder) isBatchRequest_Request_Request() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "isBatchRequest_Request_Request", reflect.TypeOf((*MockisBatchRequest_Request_Request)(nil).isBatchRequest_Request_Request))
 }
 
@@ -69,11 +71,13 @@ func (m *MockisBatchResponse_Response_Response) EXPECT() *MockisBatchResponse_Re
 
 // isBatchResponse_Response_Response mocks base method
 func (m *MockisBatchResponse_Response_Response) isBatchResponse_Response_Response() {
+	m.ctrl.T.Helper()
 	m.ctrl.Call(m, "isBatchResponse_Response_Response")
 }
 
 // isBatchResponse_Response_Response indicates an expected call of isBatchResponse_Response_Response
 func (mr *MockisBatchResponse_Response_ResponseMockRecorder) isBatchResponse_Response_Response() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "isBatchResponse_Response_Response", reflect.TypeOf((*MockisBatchResponse_Response_Response)(nil).isBatchResponse_Response_Response))
 }
 
@@ -102,6 +106,7 @@ func (m *MockBuildsClient) EXPECT() *MockBuildsClientMockRecorder {
 
 // GetBuild mocks base method
 func (m *MockBuildsClient) GetBuild(ctx context.Context, in *GetBuildRequest, opts ...grpc.CallOption) (*Build, error) {
+	m.ctrl.T.Helper()
 	varargs := []interface{}{ctx, in}
 	for _, a := range opts {
 		varargs = append(varargs, a)
@@ -114,12 +119,14 @@ func (m *MockBuildsClient) GetBuild(ctx context.Context, in *GetBuildRequest, op
 
 // GetBuild indicates an expected call of GetBuild
 func (mr *MockBuildsClientMockRecorder) GetBuild(ctx, in interface{}, opts ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetBuild", reflect.TypeOf((*MockBuildsClient)(nil).GetBuild), varargs...)
 }
 
 // SearchBuilds mocks base method
 func (m *MockBuildsClient) SearchBuilds(ctx context.Context, in *SearchBuildsRequest, opts ...grpc.CallOption) (*SearchBuildsResponse, error) {
+	m.ctrl.T.Helper()
 	varargs := []interface{}{ctx, in}
 	for _, a := range opts {
 		varargs = append(varargs, a)
@@ -132,12 +139,14 @@ func (m *MockBuildsClient) SearchBuilds(ctx context.Context, in *SearchBuildsReq
 
 // SearchBuilds indicates an expected call of SearchBuilds
 func (mr *MockBuildsClientMockRecorder) SearchBuilds(ctx, in interface{}, opts ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SearchBuilds", reflect.TypeOf((*MockBuildsClient)(nil).SearchBuilds), varargs...)
 }
 
 // UpdateBuild mocks base method
 func (m *MockBuildsClient) UpdateBuild(ctx context.Context, in *UpdateBuildRequest, opts ...grpc.CallOption) (*Build, error) {
+	m.ctrl.T.Helper()
 	varargs := []interface{}{ctx, in}
 	for _, a := range opts {
 		varargs = append(varargs, a)
@@ -150,12 +159,14 @@ func (m *MockBuildsClient) UpdateBuild(ctx context.Context, in *UpdateBuildReque
 
 // UpdateBuild indicates an expected call of UpdateBuild
 func (mr *MockBuildsClientMockRecorder) UpdateBuild(ctx, in interface{}, opts ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateBuild", reflect.TypeOf((*MockBuildsClient)(nil).UpdateBuild), varargs...)
 }
 
 // ScheduleBuild mocks base method
 func (m *MockBuildsClient) ScheduleBuild(ctx context.Context, in *ScheduleBuildRequest, opts ...grpc.CallOption) (*Build, error) {
+	m.ctrl.T.Helper()
 	varargs := []interface{}{ctx, in}
 	for _, a := range opts {
 		varargs = append(varargs, a)
@@ -168,12 +179,14 @@ func (m *MockBuildsClient) ScheduleBuild(ctx context.Context, in *ScheduleBuildR
 
 // ScheduleBuild indicates an expected call of ScheduleBuild
 func (mr *MockBuildsClientMockRecorder) ScheduleBuild(ctx, in interface{}, opts ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ScheduleBuild", reflect.TypeOf((*MockBuildsClient)(nil).ScheduleBuild), varargs...)
 }
 
 // CancelBuild mocks base method
 func (m *MockBuildsClient) CancelBuild(ctx context.Context, in *CancelBuildRequest, opts ...grpc.CallOption) (*Build, error) {
+	m.ctrl.T.Helper()
 	varargs := []interface{}{ctx, in}
 	for _, a := range opts {
 		varargs = append(varargs, a)
@@ -186,12 +199,14 @@ func (m *MockBuildsClient) CancelBuild(ctx context.Context, in *CancelBuildReque
 
 // CancelBuild indicates an expected call of CancelBuild
 func (mr *MockBuildsClientMockRecorder) CancelBuild(ctx, in interface{}, opts ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CancelBuild", reflect.TypeOf((*MockBuildsClient)(nil).CancelBuild), varargs...)
 }
 
 // Batch mocks base method
 func (m *MockBuildsClient) Batch(ctx context.Context, in *BatchRequest, opts ...grpc.CallOption) (*BatchResponse, error) {
+	m.ctrl.T.Helper()
 	varargs := []interface{}{ctx, in}
 	for _, a := range opts {
 		varargs = append(varargs, a)
@@ -204,6 +219,7 @@ func (m *MockBuildsClient) Batch(ctx context.Context, in *BatchRequest, opts ...
 
 // Batch indicates an expected call of Batch
 func (mr *MockBuildsClientMockRecorder) Batch(ctx, in interface{}, opts ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Batch", reflect.TypeOf((*MockBuildsClient)(nil).Batch), varargs...)
 }
@@ -233,6 +249,7 @@ func (m *MockBuildsServer) EXPECT() *MockBuildsServerMockRecorder {
 
 // GetBuild mocks base method
 func (m *MockBuildsServer) GetBuild(arg0 context.Context, arg1 *GetBuildRequest) (*Build, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetBuild", arg0, arg1)
 	ret0, _ := ret[0].(*Build)
 	ret1, _ := ret[1].(error)
@@ -241,11 +258,13 @@ func (m *MockBuildsServer) GetBuild(arg0 context.Context, arg1 *GetBuildRequest)
 
 // GetBuild indicates an expected call of GetBuild
 func (mr *MockBuildsServerMockRecorder) GetBuild(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetBuild", reflect.TypeOf((*MockBuildsServer)(nil).GetBuild), arg0, arg1)
 }
 
 // SearchBuilds mocks base method
 func (m *MockBuildsServer) SearchBuilds(arg0 context.Context, arg1 *SearchBuildsRequest) (*SearchBuildsResponse, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "SearchBuilds", arg0, arg1)
 	ret0, _ := ret[0].(*SearchBuildsResponse)
 	ret1, _ := ret[1].(error)
@@ -254,11 +273,13 @@ func (m *MockBuildsServer) SearchBuilds(arg0 context.Context, arg1 *SearchBuilds
 
 // SearchBuilds indicates an expected call of SearchBuilds
 func (mr *MockBuildsServerMockRecorder) SearchBuilds(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SearchBuilds", reflect.TypeOf((*MockBuildsServer)(nil).SearchBuilds), arg0, arg1)
 }
 
 // UpdateBuild mocks base method
 func (m *MockBuildsServer) UpdateBuild(arg0 context.Context, arg1 *UpdateBuildRequest) (*Build, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "UpdateBuild", arg0, arg1)
 	ret0, _ := ret[0].(*Build)
 	ret1, _ := ret[1].(error)
@@ -267,11 +288,13 @@ func (m *MockBuildsServer) UpdateBuild(arg0 context.Context, arg1 *UpdateBuildRe
 
 // UpdateBuild indicates an expected call of UpdateBuild
 func (mr *MockBuildsServerMockRecorder) UpdateBuild(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateBuild", reflect.TypeOf((*MockBuildsServer)(nil).UpdateBuild), arg0, arg1)
 }
 
 // ScheduleBuild mocks base method
 func (m *MockBuildsServer) ScheduleBuild(arg0 context.Context, arg1 *ScheduleBuildRequest) (*Build, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ScheduleBuild", arg0, arg1)
 	ret0, _ := ret[0].(*Build)
 	ret1, _ := ret[1].(error)
@@ -280,11 +303,13 @@ func (m *MockBuildsServer) ScheduleBuild(arg0 context.Context, arg1 *ScheduleBui
 
 // ScheduleBuild indicates an expected call of ScheduleBuild
 func (mr *MockBuildsServerMockRecorder) ScheduleBuild(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ScheduleBuild", reflect.TypeOf((*MockBuildsServer)(nil).ScheduleBuild), arg0, arg1)
 }
 
 // CancelBuild mocks base method
 func (m *MockBuildsServer) CancelBuild(arg0 context.Context, arg1 *CancelBuildRequest) (*Build, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CancelBuild", arg0, arg1)
 	ret0, _ := ret[0].(*Build)
 	ret1, _ := ret[1].(error)
@@ -293,11 +318,13 @@ func (m *MockBuildsServer) CancelBuild(arg0 context.Context, arg1 *CancelBuildRe
 
 // CancelBuild indicates an expected call of CancelBuild
 func (mr *MockBuildsServerMockRecorder) CancelBuild(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CancelBuild", reflect.TypeOf((*MockBuildsServer)(nil).CancelBuild), arg0, arg1)
 }
 
 // Batch mocks base method
 func (m *MockBuildsServer) Batch(arg0 context.Context, arg1 *BatchRequest) (*BatchResponse, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Batch", arg0, arg1)
 	ret0, _ := ret[0].(*BatchResponse)
 	ret1, _ := ret[1].(error)
@@ -306,5 +333,6 @@ func (m *MockBuildsServer) Batch(arg0 context.Context, arg1 *BatchRequest) (*Bat
 
 // Batch indicates an expected call of Batch
 func (mr *MockBuildsServerMockRecorder) Batch(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Batch", reflect.TypeOf((*MockBuildsServer)(nil).Batch), arg0, arg1)
 }
