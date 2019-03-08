ee0235a925bb294719f06fa4135ed9586b331827
Felix Matenaar <fmatenaar@chromium.org>
2019-03-08 15:27:02 -0800

    [luci-token-server] Ingesting config identities from luci-config/projects.cfg
    Tokenserver needs to populate its internal project->identity mapping for
    project scoped identities. This is done by ingesting projects.cfg from
    luci-config service, either by explicitly calling the
    ImportProjectIdentityConfigs API or triggered by recurring cron job.
    
    Bug: 911811
    Change-Id: I3013f31530e017327d26d6091758a94b586b7cfd

--- tokenserver/appengine/impl/utils/policy/config_fetcher.go
+++ tokenserver/appengine/impl/utils/policy/config_fetcher.go
@@ -28,23 +28,30 @@ import (
 	"go.chromium.org/luci/config/server/cfgclient/textproto"
 )
 
-// luciConfigFetcher implements ConfigFetcher interface via LUCI Config client.
+// LuciConfigFetcher implements ConfigFetcher interface via LUCI Config client.
 //
 // It fetches all config files at a single revision: the first config file
 // is fetched at HEAD, and the rest of them are fetched at the same revision.
-type luciConfigFetcher struct {
-	m   sync.Mutex
-	rev string
+type LuciConfigFetcher struct {
+	m    sync.Mutex
+	revs map[config.Set]string
 }
 
-func (f *luciConfigFetcher) Revision() string {
+// NewLuciConfigFetcher creates a new LUCI config fetcher.
+func NewLuciConfigFetcher() *LuciConfigFetcher {
+	return &LuciConfigFetcher{revs: make(map[config.Set]string, 0)}
+}
+
+// Revision returns the revision associated with the current config set.
+func (f *LuciConfigFetcher) Revision(c context.Context) string {
+	configSet := cfgclient.CurrentServiceConfigSet(c)
 	f.m.Lock()
 	defer f.m.Unlock()
-	return f.rev
+	return f.revs[configSet]
 }
 
-func (f *luciConfigFetcher) FetchTextProto(c context.Context, path string, out proto.Message) error {
-	configSet := cfgclient.CurrentServiceConfigSet(c)
+// FetchTextProtoFromService fetches a config via particular config set.
+func (f *LuciConfigFetcher) FetchTextProtoFromService(c context.Context, configSet config.Set, path string, out proto.Message) error {
 	logging.Infof(c, "Reading %q from config set %q", path, configSet)
 	c, cancel := context.WithTimeout(c, 40*time.Second) // URL fetch deadline
 	defer cancel()
@@ -58,17 +65,22 @@ func (f *luciConfigFetcher) FetchTextProto(c context.Context, path string, out p
 	defer f.m.Unlock()
 
 	switch {
-	case f.rev == "":
-		f.rev = meta.Revision
-	case f.rev != meta.Revision:
+	case f.revs[configSet] == "":
+		f.revs[configSet] = meta.Revision
+	case f.revs[configSet] != meta.Revision:
 		// TODO(vadimsh): Lock all subsequent 'FetchTextProto' calls to the revision
 		// we fetched during the first call. Unfortunately cfgclient doesn't support
 		// fetching a specific revision. So we just fail if a wrong revision is
 		// fetched, assuming the state will "normalize" later.
 		return fmt.Errorf(
 			"expected config %q to be at rev %s, but got %s",
-			path, f.rev, meta.Revision)
+			path, f.revs[configSet], meta.Revision)
 	}
 
 	return nil
+}
+
+// FetchTextProto fetches a config from the current config set.
+func (f *LuciConfigFetcher) FetchTextProto(c context.Context, path string, out proto.Message) error {
+	return f.FetchTextProtoFromService(c, cfgclient.CurrentServiceConfigSet(c), path, out)
 }
