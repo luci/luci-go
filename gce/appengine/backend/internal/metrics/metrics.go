ccafc2d124fb3a513611d9dc10d1e111a042c52e
smut <smut@google.com>
2019-03-08 18:26:43 -0800

    [GCE] Export instance creation failures, remove connected metric
    Connected metric is incorrect since cached values vary per instance of
    the app but global callbacks only run on one instance. That means only
    metrics recorded by the same instance where the global callback runs get
    flushed each minute. This problem would be magnified at scale when there
    are thousands of GCE instances, and thus tens of app instances to handle
    them.
    
    Bug: 897355
    Change-Id: I726aa5d9a9e5d688af413a767b58d2a851af46ab

--- gce/appengine/backend/internal/metrics/metrics.go
+++ gce/appengine/backend/internal/metrics/metrics.go
@@ -17,15 +17,28 @@ package metrics
 
 import (
 	"context"
-	"fmt"
-	"sync"
 
-	"go.chromium.org/luci/common/tsmon"
 	"go.chromium.org/luci/common/tsmon/field"
 	"go.chromium.org/luci/common/tsmon/metric"
 
 	"go.chromium.org/luci/gce/appengine/model"
 )
+
+var (
+	creationFailures = metric.NewCounter(
+		"gce/failures/creation",
+		"The number of failures during GCE instance creation.",
+		nil,
+		field.String("prefix"),
+		field.String("project"),
+		field.String("zone"),
+	)
+)
+
+// UpdateFailures increments failure counters.
+func UpdateFailures(c context.Context, creations int, vm *model.VM) {
+	creationFailures.Add(c, int64(creations), vm.Prefix, vm.Attributes.GetProject(), vm.Attributes.GetZone())
+}
 
 var (
 	quotaLimit = metric.NewFloat(
@@ -61,67 +74,4 @@ func UpdateQuota(c context.Context, limit, usage float64, metric, project, regio
 	quotaLimit.Set(c, limit, metric, project, region)
 	quotaRemaining.Set(c, limit-usage, metric, project, region)
 	quotaUsage.Set(c, usage, metric, project, region)
-}
-
-var (
-	instanceConnected = metric.NewBool(
-		"gce/instance/connected",
-		"Whether a GCE instance is connected to its Swarming server.",
-		nil,
-		field.String("hostname"),
-		field.String("prefix"),
-		field.String("project"),
-		field.String("server"),
-		field.String("zone"),
-	)
-
-	// cache is a map of hostname to *connInst to report instanceConnected for.
-	cache = make(map[string]*connInst)
-
-	// cacheLock is the lock under which the cache should be accessed.
-	cacheLock = &sync.Mutex{}
-)
-
-type connInst struct {
-	connected bool
-	prefix    string
-	project   string
-	server    string
-	zone      string
-}
-
-// UpdateInstance sets GCE instance metrics.
-func UpdateInstance(c context.Context, connected bool, vm *model.VM) {
-	inst := &connInst{
-		connected: connected,
-		prefix:    vm.Prefix,
-		project:   vm.Attributes.GetProject(),
-		server:    vm.Swarming,
-		zone:      vm.Attributes.GetZone(),
-	}
-	cacheLock.Lock()
-	defer cacheLock.Unlock()
-	cache[vm.Hostname] = inst
-}
-
-// autogen returns the field value to use when the given field value is
-// automatically generated.
-func autogen(s string) string {
-	// Generally metric fields should have a known set of possible values.
-	// autogen: prefix tells tsmon that these values are generated at random.
-	return fmt.Sprintf("autogen:%s", s)
-}
-
-// setCached reports cached values to tsmon.
-func setCached(c context.Context) {
-	cacheLock.Lock()
-	defer cacheLock.Unlock()
-	for hostname, inst := range cache {
-		instanceConnected.Set(c, inst.connected, autogen(hostname), inst.prefix, inst.project, inst.server, inst.zone)
-		delete(cache, hostname)
-	}
-}
-
-func init() {
-	tsmon.RegisterGlobalCallback(setCached, instanceConnected)
 }
