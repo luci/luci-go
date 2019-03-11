9072e0b0709cc8324df9339aca0408c800b24795
Ryan Tseng <hinoka@google.com>
2019-03-11 21:35:30 +0000

    [logdog] Change the archivist to pull from appengine taskqueues
    Change logdog_archivist entrypoint to read from TaskQueue instead of PubSub.
    
    archivist.go is the main implementation, refactoring include:
    * Remove AssertLease(), which isn't actually used
    * Replace UniqueID() of the task with a random int
    * Remove ArchivalKey check, since TaskQueue now manages the task lifecycle.
    * Remove SettleDelay/CompletePeriod check, TaskQueue now manages those.
    * archiveTaskImpl() returns err for transient errors (need retries),
      and nil for success or permanent errors (don't retry).
      The legacy pipeline continues to use task.Consume() as the signaling mechanism.
    
    main.go:
    * Calls coordinator's LeaseTasks and DeleteTasks instead of PubSub,
      Each loop processes 10 tasks.
    
    Others:
    * Remove retryServicesClient, since it wraps the pRPC client,
      which already has built in retries.
    * Use a forever loop instead of retry.Retry.
      retry.Retry has confusing semantics and is difficult for a reader to
      ascertain correctness, considering that retry.Retry has not been
      used anywhere else in the luci-go codebase to be a forever loop.
      It wasn't used as a forever loop in the previous implementation either.
      It was just an extra retry catcher on top of the main PubSub forever loop.
      In the interest of simplification, I thought it was better to
      replace that library with ~10 lines of code.
    
    Bug: 923557
    Change-Id: Id1ab6a0e9894efc92282264fb3efd57c5d6d1042
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1510510
    Commit-Queue: Ryan Tseng <hinoka@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- logdog/server/archivist/archivist_test.go
+++ logdog/server/archivist/archivist_test.go
@@ -27,7 +27,7 @@ import (
 	"go.chromium.org/luci/common/gcloud/gs"
 	"go.chromium.org/luci/common/proto/google"
 	"go.chromium.org/luci/common/retry/transient"
-	"go.chromium.org/luci/logdog/api/endpoints/coordinator/services/v1"
+	logdog "go.chromium.org/luci/logdog/api/endpoints/coordinator/services/v1"
 	"go.chromium.org/luci/logdog/api/logpb"
 	"go.chromium.org/luci/logdog/common/storage"
 	"go.chromium.org/luci/logdog/common/storage/memory"
@@ -61,14 +61,6 @@ func (t *testTask) Task() *logdog.ArchiveTask {
 
 func (t *testTask) Consume() {
 	t.consumed = true
-}
-
-func (t *testTask) AssertLease(context.Context) error {
-	if err := t.assertLeaseErr; err != nil {
-		return err
-	}
-	t.assertCount++
-	return nil
 }
 
 // testServicesClient implements logdog.ServicesClient sufficient for testing
@@ -367,7 +359,7 @@ func TestHandleArchive(t *testing.T) {
 		Convey(`Will consume task and refrain from archiving if the stream is already archived.`, func() {
 			stream.State.Archived = true
 
-			So(ar.archiveTaskImpl(c, task), ShouldErrLike, "log stream is archived")
+			So(ar.archiveTaskImpl(c, task), ShouldBeNil)
 			So(task.consumed, ShouldBeTrue)
 			So(archiveRequest, ShouldBeNil)
 		})
@@ -375,7 +367,7 @@ func TestHandleArchive(t *testing.T) {
 		Convey(`Will consume task and refrain from archiving if the stream is purged.`, func() {
 			stream.State.Purged = true
 
-			So(ar.archiveTaskImpl(c, task), ShouldErrLike, "log stream is purged")
+			So(ar.archiveTaskImpl(c, task), ShouldBeNil)
 			So(task.consumed, ShouldBeTrue)
 			So(archiveRequest, ShouldBeNil)
 		})
@@ -402,7 +394,7 @@ func TestHandleArchive(t *testing.T) {
 			stream.Age = google.NewDuration(expired)
 			stream.ArchivalKey = []byte("non-matching archival key")
 
-			So(ar.archiveTaskImpl(c, task), ShouldErrLike, "superfluous archival request")
+			So(ar.archiveTaskImpl(c, task), ShouldBeNil)
 			So(task.consumed, ShouldBeTrue)
 			So(archiveRequest, ShouldBeNil)
 		})
@@ -629,14 +621,14 @@ func TestHandleArchive(t *testing.T) {
 		Convey(`With an empty project name, will fail and consume the task.`, func() {
 			archiveTask.Project = ""
 
-			So(ar.archiveTaskImpl(c, task), ShouldErrLike, "invalid project name")
+			So(ar.archiveTaskImpl(c, task), ShouldBeNil)
 			So(task.consumed, ShouldBeTrue)
 		})
 
 		Convey(`With an invalid project name, will fail and consume the task.`, func() {
 			archiveTask.Project = "!!! invalid project name !!!"
 
-			So(ar.archiveTaskImpl(c, task), ShouldErrLike, "invalid project name")
+			So(ar.archiveTaskImpl(c, task), ShouldBeNil)
 			So(task.consumed, ShouldBeTrue)
 		})
 
