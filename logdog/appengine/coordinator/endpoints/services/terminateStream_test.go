b9220efa42cce8b3bc5bad34f7232f6f3547198f
Ryan Tseng <hinoka@google.com>
2019-03-15 17:03:39 -0700

    [logdog] Remove old archival tasking pipeline
    As of https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1524458
    The old pipeline no longer tasks archival.
    This removes all the code associated with both the old tasking pipeline
    as well as the semantics associated with the old pipeline.
    
    Some tests are updated to reflect new semantics (All tasks are archived by archivist)
    
    Bug: 923557
    Change-Id: Ia74861fda41def859f4d1f293f8b02cbc9bf37e0

--- logdog/appengine/coordinator/endpoints/services/terminateStream_test.go
+++ logdog/appengine/coordinator/endpoints/services/terminateStream_test.go
@@ -28,7 +28,6 @@ import (
 	logdog "go.chromium.org/luci/logdog/api/endpoints/coordinator/services/v1"
 	"go.chromium.org/luci/logdog/appengine/coordinator"
 	ct "go.chromium.org/luci/logdog/appengine/coordinator/coordinatorTest"
-	"go.chromium.org/luci/logdog/appengine/coordinator/endpoints"
 
 	"go.chromium.org/luci/common/proto/google"
 
@@ -163,48 +162,6 @@ func TestTerminateStream(t *testing.T) {
 			Convey(`Will fail if the stream is not registered.`, func() {
 				_, err := svr.TerminateStream(c, &req)
 				So(err, ShouldBeRPCNotFound, "is not registered")
-			})
-		})
-
-		Convey(`Will choose the correct archival delay`, func() {
-			getParams := func() *coordinator.ArchivalParams {
-				svc := endpoints.GetServices(c)
-				cfg, err := svc.Config(c)
-				if err != nil {
-					panic(err)
-				}
-
-				pcfg, err := svc.ProjectConfig(c, "proj-foo")
-				if err != nil {
-					panic(err)
-				}
-
-				return standardArchivalParams(cfg, pcfg)
-			}
-
-			Convey(`When there is no project config delay.`, func() {
-				env.ModProjectConfig(c, "proj-foo", func(pcfg *svcconfig.ProjectConfig) {
-					pcfg.MaxStreamAge = nil
-				})
-
-				So(getParams(), ShouldResemble, &coordinator.ArchivalParams{
-					SettleDelay:    10 * time.Second,
-					CompletePeriod: 24 * time.Hour,
-				})
-			})
-
-			Convey(`When there is no service or project config delay.`, func() {
-				env.ModServiceConfig(c, func(cfg *svcconfig.Config) {
-					cfg.Coordinator.ArchiveDelayMax = nil
-				})
-				env.ModProjectConfig(c, "proj-foo", func(pcfg *svcconfig.ProjectConfig) {
-					pcfg.MaxStreamAge = nil
-				})
-
-				So(getParams(), ShouldResemble, &coordinator.ArchivalParams{
-					SettleDelay:    10 * time.Second,
-					CompletePeriod: 0,
-				})
 			})
 		})
 	})
