c1508bf086792333880ec644e7ee43af7e7b7f6b
Ryan Tseng <hinoka@google.com>
2019-03-18 13:26:52 -0700

    [milo] Move blamelist behind a tab, add option to force loading blamelist.
    Change-Id: I2b640a886b82e919d35ab92311923df383348a90

--- milo/frontend/appengine/static/common/js/build.js
+++ milo/frontend/appengine/static/common/js/build.js
@@ -97,6 +97,12 @@ $(document).ready(function() {
       if (ui.newPanel.attr('id') === 'timeline-tab' && timeline === null) {
         timeline = createTimeline();
       }
+      if (ui.newPanel.attr('id') === 'blamelist-tab' && !blamelistLoaded) {
+        let l = window.location;
+        let params = (new URL(l)).searchParams;
+        params.set("blamelist", "1");
+        document.location = `${l.protocol}//${l.host}${l.pathname}?${params.toString()}#blamelist-tab`;
+      }
     },
   }).show();
 });
