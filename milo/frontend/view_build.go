2f9529ed5bb47b6b276ad9fb4e391057e5e049a6
Ryan Tseng <hinoka@google.com>
2019-03-18 17:53:29 -0700

    [milo] Move blamelist behind a tab, add option to force loading blamelist.
    Bug: 942239
    Change-Id: I2b640a886b82e919d35ab92311923df383348a90

--- milo/frontend/view_build.go
+++ milo/frontend/view_build.go
@@ -49,6 +49,7 @@ func handleLUCIBuild(c *router.Context) error {
 	bucket := c.Params.ByName("bucket")
 	buildername := c.Params.ByName("builder")
 	numberOrId := c.Params.ByName("numberOrId")
+	forceBlamelist := c.Request.FormValue("blamelist") != ""
 
 	if _, v2Bucket := bb.BucketNameToV2(bucket); v2Bucket != "" {
 		// Params bucket is a v1 bucket, so call the legacy endpoint.
@@ -76,7 +77,7 @@ func handleLUCIBuild(c *router.Context) error {
 		}
 	}
 
-	bp, err := buildbucket.GetBuildPage(c, br)
+	bp, err := buildbucket.GetBuildPage(c, br, forceBlamelist)
 	return renderBuild(c, bp, err)
 }
 
