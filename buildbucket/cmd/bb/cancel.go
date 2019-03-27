b7e6bfed04408dd03ef0385b6facf37a6df0afe4
Nodir Turakulov <nodir@google.com>
2019-03-27 15:09:04 -0700

    [bb] Modernize cancel
    Accept any non-negative number of build ids
    Print builds in JSON or human-readable format.
    
    Bug: 946422
    Change-Id: Iae0225cfbc284690a6e7da5a530f87a308b62897

--- buildbucket/cmd/bb/cancel.go
+++ buildbucket/cmd/bb/cancel.go
@@ -16,21 +16,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/golang/protobuf/jsonpb"
 	"github.com/maruel/subcommands"
+	"google.golang.org/grpc/codes"
 
 	"go.chromium.org/luci/auth"
 	"go.chromium.org/luci/common/cli"
+
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
 )
 
 func cmdCancel(defaultAuthOpts auth.Options) *subcommands.Command {
 	return &subcommands.Command{
-		UsageLine: `cancel [flags] <build id>`,
-		ShortDesc: "cancel a build",
-		LongDesc:  "Cancel a build.",
+		UsageLine: `cancel [flags] <build id> [<build id>...]`,
+		ShortDesc: "cancel builds",
+		LongDesc:  "Cancel builds.",
 		CommandRun: func() subcommands.CommandRun {
 			r := &cancelRun{}
 			r.SetDefaultFlags(defaultAuthOpts)
+			r.Flags.StringVar(&r.summary, "summary", "", "reason of cancelation; required")
 			return r
 		},
 	}
@@ -38,15 +44,65 @@ func cmdCancel(defaultAuthOpts auth.Options) *subcommands.Command {
 
 type cancelRun struct {
 	baseCommandRun
-	buildIDArg
+	buildFieldFlags
+	summary string
 }
 
 func (r *cancelRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
 	ctx := cli.GetContext(a, r, env)
 
-	if err := r.parseArgs(args); err != nil {
+	if r.summary == "" {
+		return r.done(ctx, fmt.Errorf("-summary is required"))
+	}
+
+	buildIDs, err := parseBuildIDArgs(args)
+	if err != nil {
 		return r.done(ctx, err)
 	}
 
-	return r.callAndDone(ctx, "POST", fmt.Sprintf("builds/%d/cancel", r.buildID), nil)
+	req := &buildbucketpb.BatchRequest{}
+	for _, id := range buildIDs {
+		req.Requests = append(req.Requests, &buildbucketpb.BatchRequest_Request{
+			Request: &buildbucketpb.BatchRequest_Request_CancelBuild{
+				CancelBuild: &buildbucketpb.CancelBuildRequest{
+					Id:              id,
+					SummaryMarkdown: r.summary,
+					Fields:          r.FieldMask(),
+				},
+			},
+		})
+	}
+
+	client, err := r.newClient(ctx)
+	if err != nil {
+		return r.done(ctx, err)
+	}
+
+	res, err := client.Batch(ctx, req)
+	if err != nil {
+		return r.done(ctx, err)
+	}
+
+	hasErr := false
+	m := jsonpb.Marshaler{}
+	p := newStdoutPrinter()
+	for i, subres := range res.Responses {
+		error := subres.GetError()
+		build := subres.GetCancelBuild()
+		switch {
+		case error != nil:
+			hasErr = true
+			fmt.Fprintf(os.Stderr, "Failed to cancel build %d: %s: %s\n", buildIDs[i], codes.Code(error.Code), error.Message)
+		case r.json:
+			if err := m.Marshal(os.Stdout, build); err != nil {
+				panic(err)
+			}
+		default:
+			p.Build(build)
+		}
+	}
+	if hasErr {
+		return 1
+	}
+	return 0
 }
