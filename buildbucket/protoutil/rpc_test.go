aaebbad59e0addf8efbb7910bd11378786e61a9d
Nodir Turakulov <nodir@google.com>
2019-03-27 11:13:57 -0700

    [bb] Add support for build number
    Implement getting a build by a build number
    
    R=smut@google.com
    
    Bug: 946422
    Change-Id: Icc9f06070594ee7afa4bd894c561c55c5db0dc0f

---
+++ buildbucket/protoutil/rpc_test.go
@@ -0,0 +1,49 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protoutil
+
+import (
+	"testing"
+
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestParseGetBuildRequest(t *testing.T) {
+	t.Parallel()
+
+	Convey("ParseGetBuildRequest", t, func() {
+		Convey("build id", func() {
+			req, err := ParseGetBuildRequest("1234567890")
+			So(err, ShouldBeNil)
+			So(req, ShouldResembleProto, &buildbucketpb.GetBuildRequest{Id: 1234567890})
+		})
+
+		Convey("build number", func() {
+			req, err := ParseGetBuildRequest("chromium/ci/linux-rel/1")
+			So(err, ShouldBeNil)
+			So(req, ShouldResembleProto, &buildbucketpb.GetBuildRequest{
+				Builder: &buildbucketpb.BuilderID{
+					Project: "chromium",
+					Bucket:  "ci",
+					Builder: "linux-rel",
+				},
+				BuildNumber: 1,
+			})
+		})
+	})
+}
