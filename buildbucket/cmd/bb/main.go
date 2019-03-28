39c2a90c033a99a7dce42c6ab9c744eab4dc8708
Nodir Turakulov <nodir@google.com>
2019-03-28 15:49:31 -0700

    [bb] Add log subcommand
    Subcommand log prints logs of a step.
    
    By defaults, prints stdout and stderr, if present.
    
    Prints stderr log to its own stderr.
    
    Bug: 946422
    
    R=hinoka@chromium.org
    
    Change-Id: I564763e5644c53fac09d1b39c225807f18370915

--- buildbucket/cmd/bb/main.go
+++ buildbucket/cmd/bb/main.go
@@ -46,6 +46,7 @@ func GetApplication(defaultAuthOpts auth.Options) *cli.Application {
 		Commands: []*subcommands.Command{
 			cmdAdd(defaultAuthOpts),
 			cmdGet(defaultAuthOpts),
+			cmdLog(defaultAuthOpts),
 			cmdCancel(defaultAuthOpts),
 			cmdBatch(defaultAuthOpts),
 			cmdCollect(defaultAuthOpts),
