6468d012c21961c5098a709d675f615f5d5b156c
Nodir Turakulov <nodir@google.com>
2019-03-28 20:09:27 +0000

    [bb] put: add -cl flag
    Add -cl flag to `bb put`. It can accept one or more strings that
    indicate a Gerrit CL. It can be a Gerrit CL URL, or something more
    noisy, e.g. URL of a file within a CL.
    
    Include Gerrit OAuth scope.
    
    R=tandrii@chromium.org
    
    Bug: 946422
    Change-Id: I598e471719f7768e423d676b5aa8a86efeedc89d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1544434
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

---
+++ buildbucket/cmd/bb/parse.go
@@ -0,0 +1,73 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+)
+
+func parseBuildIDArgs(args []string) ([]int64, error) {
+	buildIDs := make([]int64, len(args))
+	for i, arg := range args {
+		id, err := strconv.ParseInt(arg, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("invalid build id %s, expected an int64", arg)
+		}
+		buildIDs[i] = id
+	}
+
+	return buildIDs, nil
+}
+
+var regexCL = regexp.MustCompile(`(\w+-review\.googlesource\.com)/c/(([^\+]+)/\+/)?(\d+)(/(\d+))?`)
+
+// parseCL tries to retrieve a CL info from a string.
+//
+// It is not strict and can consume noisy strings, e.g.
+// https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1541677/7/buildbucket/cmd/bb/base_command.go
+// or incomplete strings, e.g.
+// https://chromium-review.googlesource.com/c/1541677
+// If err is nil, returned change is guaranteed to have Host and Change.
+func parseCL(s string) (*buildbucketpb.GerritChange, error) {
+	m := regexCL.FindStringSubmatch(s)
+	if m == nil {
+		return nil, fmt.Errorf("does not match regexp %q", regexCL)
+	}
+	ret := &buildbucketpb.GerritChange{
+		Host:    m[1],
+		Project: m[3],
+	}
+	change := m[4]
+	patchSet := m[6]
+
+	var err error
+	ret.Change, err = strconv.ParseInt(change, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("invalid change %q: %s", change, err)
+	}
+
+	if patchSet != "" {
+		ret.Patchset, err = strconv.ParseInt(patchSet, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("invalid patchset %q: %s", patchSet, err)
+		}
+	}
+
+	return ret, nil
+}
