9424f807d10e126f53dd13cfd67b7c021101b244
Nodir Turakulov <nodir@google.com>
2019-03-28 17:38:02 -0700

    [bb] Add log subcommand
    Subcommand log prints logs of a step.
    
    By defaults, prints stdout and stderr, if present.
    
    Prints stderr log to its own stderr.
    
    Bug: 946422
    
    R=hinoka@chromium.org
    
    Change-Id: I564763e5644c53fac09d1b39c225807f18370915

---
+++ buildbucket/cmd/bb/log.go
@@ -0,0 +1,352 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"container/heap"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/maruel/subcommands"
+	"google.golang.org/genproto/protobuf/field_mask"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.chromium.org/luci/auth"
+	"go.chromium.org/luci/buildbucket/protoutil"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/sync/parallel"
+	"go.chromium.org/luci/grpc/prpc"
+
+	"go.chromium.org/luci/logdog/client/coordinator"
+	"go.chromium.org/luci/logdog/common/fetcher"
+	"go.chromium.org/luci/logdog/common/types"
+
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+	logpb "go.chromium.org/luci/logdog/api/logpb"
+)
+
+func cmdLog(defaultAuthOpts auth.Options) *subcommands.Command {
+	return &subcommands.Command{
+		UsageLine: `log [flags] <BUILD> <STEP> [<LOG>...]`,
+		ShortDesc: "prints step logs",
+		LongDesc: `Prints step logs.
+
+Argument BUILD can be an int64 build id or a string
+<project>/<bucket>/<builder>/<build_number>, e.g. chromium/ci/linux-rel/1
+
+Argument STEP is a build step name, e.g. "bot_update".
+Use | as parent-child separator, e.g. "parent|child".
+
+Arguments LOG is one ore more log names of the STEP. They will be multiplexed
+by time. Defaults to stdout and stderr.
+`,
+		CommandRun: func() subcommands.CommandRun {
+			r := &logRun{}
+			r.SetDefaultFlags(defaultAuthOpts)
+			return r
+		},
+	}
+}
+
+type logRun struct {
+	baseCommandRun
+
+	// args
+
+	build    string
+	getBuild *buildbucketpb.GetBuildRequest
+	step     string
+	logs     stringset.Set
+}
+
+func (r *logRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	ctx := cli.GetContext(a, r, env)
+
+	if err := r.parseArgs(args); err != nil {
+		return r.done(ctx, err)
+	}
+
+	// Find the step.
+	steps, err := r.getSteps(ctx)
+	if err != nil {
+		return r.done(ctx, err)
+	}
+	var step *buildbucketpb.Step
+	for _, s := range steps {
+		if s.Name == r.step {
+			step = s
+			break
+		}
+	}
+	if step == nil {
+		fmt.Fprintf(os.Stderr, "step %q not found in build %q. Available steps:\n", r.step, r.build)
+		for _, s := range steps {
+			fmt.Fprintf(os.Stderr, "  %s\n", s.Name)
+		}
+		return 1
+	}
+
+	// Find the logs.
+	logByName := map[string]*buildbucketpb.Step_Log{}
+	for _, l := range step.Logs {
+		logByName[l.Name] = l
+	}
+	var logs []*buildbucketpb.Step_Log
+	if len(r.logs) == 0 {
+		logs = r.defaultLogs(logByName)
+	} else {
+		logs = make([]*buildbucketpb.Step_Log, 0, len(r.logs))
+		for name := range r.logs {
+			l, ok := logByName[name]
+			if !ok {
+				fmt.Fprintf(os.Stderr, "log %q not found in step %q of build %q. Available logs:\n", name, r.step, r.build)
+				for _, l := range step.Logs {
+					fmt.Fprintf(os.Stderr, "  %s\n", l.Name)
+				}
+				return 1
+			}
+			logs = append(logs, l)
+		}
+	}
+
+	return r.done(ctx, r.printLogs(ctx, logs))
+}
+
+func (r *logRun) parseArgs(args []string) error {
+	if len(args) < 2 {
+		return fmt.Errorf("usage: bb log <BUILD> <STEP> [LOG...]")
+	}
+
+	r.build = args[0]
+	r.step = args[1]
+	r.logs = stringset.NewFromSlice(args[2:]...)
+	var err error
+	r.getBuild, err = protoutil.ParseGetBuildRequest(r.build)
+	return err
+}
+
+func (r *logRun) defaultLogs(available map[string]*buildbucketpb.Step_Log) []*buildbucketpb.Step_Log {
+	logs := make([]*buildbucketpb.Step_Log, 0, 2)
+	if log, ok := available["stdout"]; ok {
+		logs = append(logs, log)
+	}
+	if log, ok := available["stderr"]; ok {
+		logs = append(logs, log)
+	}
+	return logs
+}
+
+// getSteps fetches steps of the build.
+func (r *logRun) getSteps(ctx context.Context) ([]*buildbucketpb.Step, error) {
+	client, err := r.newClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	r.getBuild.Fields = &field_mask.FieldMask{Paths: []string{"steps"}}
+	build, err := client.GetBuild(ctx, r.getBuild, prpc.ExpectedCode(codes.NotFound))
+	switch status.Code(err) {
+	case codes.OK:
+		return build.Steps, nil
+	case codes.NotFound:
+		return nil, fmt.Errorf("build not found")
+	default:
+		return nil, err
+	}
+}
+
+// printLogs fetches and prints the logs to io.Stdout/io.Stderr.
+// Entries from different logs are multiplexed by timestamps.
+func (r *logRun) printLogs(ctx context.Context, logs []*buildbucketpb.Step_Log) error {
+	if len(logs) == 0 {
+		return nil
+	}
+
+	// Parse Log URLs.
+	addrs := make([]*types.StreamAddr, len(logs))
+	for i, l := range logs {
+		var err error
+		addrs[i], err = types.ParseURL(l.Url)
+		if err != nil {
+			return fmt.Errorf("log %q has unsupported URL %q", l.Name, l.Url)
+		}
+
+		if addrs[0].Host != addrs[i].Host {
+			// unrealistic
+			return fmt.Errorf("multiple different logdog hosts are not supported")
+		}
+	}
+
+	// Create a client.
+	httpClient, err := r.createHTTPClient(ctx)
+	if err != nil {
+		return err
+	}
+	client := coordinator.NewClient(&prpc.Client{
+		C:    httpClient,
+		Host: addrs[0].Host,
+	})
+
+	// Fetch descriptors and create fetchers.
+	fetchers := make([]*fetcher.Fetcher, len(addrs))
+	chans := make([]logChan, len(addrs))
+	err = parallel.FanOutIn(func(work chan<- func() error) {
+		for i, addr := range addrs {
+			i := i
+			stream := client.Stream(addr.Project, addr.Path)
+			fetchers[i] = stream.Fetcher(ctx, nil)
+			work <- func() error {
+				s, err := stream.State(ctx)
+				if err != nil {
+					return err
+				}
+				if s.Desc.StreamType != logpb.StreamType_TEXT {
+					return fmt.Errorf("log %q is not a text stream; only text streams are currently supported", logs[i].Name)
+				}
+				chans[i].start, err = ptypes.Timestamp(s.Desc.Timestamp)
+				return err
+			}
+		}
+	})
+	if err != nil {
+		return err
+	}
+
+	// Start fetching.
+	for i := range chans {
+		i := i
+		c := make(chan logAndErr, 256)
+		chans[i].c = c
+		go func() {
+			defer close(c)
+			for {
+				log, err := fetchers[i].NextLogEntry()
+				c <- logAndErr{log, err}
+				if err != nil {
+					break
+				}
+			}
+		}()
+	}
+
+	return multiplexLogs(chans, func(logIndex int, log *logpb.LogEntry) {
+		out := os.Stdout
+		if logs[logIndex].Name == "stderr" {
+			out = os.Stderr
+		}
+		for _, line := range log.GetText().GetLines() {
+			fmt.Fprintf(out, "%s", line.Value)
+			fmt.Fprintf(out, line.Delimiter)
+		}
+	})
+}
+
+type logChan struct {
+	start time.Time      // base time for log entries
+	c     chan logAndErr // channel of log entries
+}
+
+type logAndErr struct {
+	*logpb.LogEntry
+	err error
+}
+
+type logHeap []logHeapEntry
+type logHeapEntry struct {
+	*logpb.LogEntry
+	time      time.Time
+	chanIndex int
+}
+
+func (h logHeap) Len() int           { return len(h) }
+func (h logHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h logHeap) Less(i, j int) bool { return h[i].time.Before(h[j].time) }
+
+func (h *logHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(logHeapEntry))
+}
+
+func (h *logHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// multiplexLogs receives from chans and calls f in the order of log
+// timestamps.
+func multiplexLogs(chans []logChan, f func(chanIndex int, log *logpb.LogEntry)) error {
+	// heap of log entries, sorted by time.
+	h := make(logHeap, len(chans))
+
+	recv := func(chanIndex int, initializing bool) error {
+		switch l := <-chans[chanIndex].c; {
+		case l.err == io.EOF:
+			// do not add to heap
+
+		case l.err != nil:
+			// fail fastt
+			return l.err
+
+		default:
+			// add to the heap
+			e := logHeapEntry{
+				LogEntry:  l.LogEntry,
+				time:      chans[chanIndex].start,
+				chanIndex: chanIndex,
+			}
+			if e.TimeOffset != nil {
+				offset, err := ptypes.Duration(e.TimeOffset)
+				if err != nil {
+					return err
+				}
+				e.time = e.time.Add(offset)
+			}
+			if initializing {
+				h[chanIndex] = e
+			} else {
+				heap.Push(&h, e)
+			}
+		}
+		return nil
+	}
+
+	// Populate the heap.
+	for i := range chans {
+		if err := recv(i, true); err != nil {
+			return err
+		}
+	}
+	heap.Init(&h)
+
+	// Consume the channels.
+	for len(h) > 0 {
+		log := heap.Pop(&h).(logHeapEntry)
+		f(log.chanIndex, log.LogEntry)
+		if err := recv(log.chanIndex, false); err != nil {
+			return err
+		}
+	}
+	return nil
+}
