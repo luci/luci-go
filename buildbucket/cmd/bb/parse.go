f6cda5bca9d2f8f34bc4271d809ddecdfd31d5b0
Nodir Turakulov <nodir@google.com>
2019-03-29 17:08:04 -0700

    [bb] Add -commit flag
    Accept -commit flag in add subcommand.
    It accepts a lax string indicating a commit or ref
    
    R=tandrii@chromium.org
    
    Bug: 946422
    Change-Id: Idb0279348d2a4a69631ca2e3ffb58cedefbb56a5

--- buildbucket/cmd/bb/parse.go
+++ buildbucket/cmd/bb/parse.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 
 	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
 )
@@ -57,4 +58,51 @@ func parseCL(s string) (*buildbucketpb.GerritChange, error) {
 	}
 
 	return ret, nil
+}
+
+var regexCommit = regexp.MustCompile(`(\w+\.googlesource\.com)/([^\+]+)/\+/(([a-f0-9]{40})|([\w\-/]+))`)
+
+// parseCommit tries to retrieve a Gitiles Commit from a string.
+//
+// It is not strict or precise, and can consume noisy strings, e.g.
+// https://chromium.googlesource.com/chromium/src/+/34e1ee99cc34fa86a1c2699977e223a13eb5f96c/chrome/test/chromedriver/test/run_py_tests.py
+// If err is nil, returned commit is guaranteed to have Host, Project and
+// either Ref or Id.
+//
+// If mustConfirmRef is true, commit.Ref needs to be confirmed.
+func parseCommit(s string) (commit *buildbucketpb.GitilesCommit, mustConfirmRef bool, err error) {
+	m := regexCommit.FindStringSubmatch(s)
+	if m == nil {
+		return nil, false, fmt.Errorf("does not match regexp %q", regexCommit)
+	}
+	commit = &buildbucketpb.GitilesCommit{
+		Host:    m[1],
+		Project: m[2],
+		Id:      m[4],
+		Ref:     m[5],
+	}
+
+	ref := strings.Split(commit.Ref, "/")
+	// Apply heuristic.
+	switch {
+	case commit.Id != "":
+		// This is a precise commit.
+
+	case strings.HasPrefix(commit.Ref, "refs/heads/"):
+		// Take one component after refs/heads/
+		commit.Ref = strings.Join(ref[:3], "/")
+		mustConfirmRef = len(ref) > 3
+
+	case !strings.HasPrefix(commit.Ref, "refs/"):
+		// Suspect a branch.
+		commit.Ref = "refs/heads/" + ref[0]
+		mustConfirmRef = true
+
+	default:
+		// The user has specified something with prefix "refs/", but without
+		// prefix "refs/heads/". They must know what they is doing.
+		mustConfirmRef = true
+
+	}
+	return
 }
