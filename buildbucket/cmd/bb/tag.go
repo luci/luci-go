ab0eae7d0f96f326b0ad223739477abb7f8d8414
Nodir Turakulov <nodir@google.com>
2019-04-01 13:04:10 -0700

    [buildbucket] Add "-t" flag
    Add -t flag to ls and add subcommands.
    The flag accepts one or more tags.
    
    Bug: 946422
    Change-Id: I32aaa5fa6df47465ddd7ae66815e42485ae74155

---
+++ buildbucket/cmd/bb/tag.go
@@ -0,0 +1,38 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+
+	"go.chromium.org/luci/buildbucket/protoutil"
+	"go.chromium.org/luci/common/data/strpair"
+	luciflag "go.chromium.org/luci/common/flag"
+
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+)
+
+type tagsFlag struct {
+	tags strpair.Map
+}
+
+func (f *tagsFlag) Register(fs *flag.FlagSet, help string) {
+	f.tags = strpair.Map{}
+	fs.Var(luciflag.StringPairs(f.tags), "t", help)
+}
+
+func (f *tagsFlag) Tags() []*buildbucketpb.StringPair {
+	return protoutil.StringPairs(f.tags)
+}
