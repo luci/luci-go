9cbb50c4e912d198d09457f7ed2a4c145e2101fd
smut <smut@google.com>
2019-04-05 15:29:29 -0700

    [GCE] Cache VM counts in the datastore
    VM counts need to be reported globally, they should not be reported by
    arbitrary GAE app instances. However, computing these counts is costly.
    Therefore, let arbitrary app instances compute the counts and cache them
    in the datastore, then install a tsmon global callback to read and
    report the counts from the precomputations. To avoid zeroing counts when
    deleting configs, store a time with each count and only transmit metrics
    if the count was computed recently.
    
    Additionally, increase rate of precomputations, since it's currently
    slower than the VM creation rate (so VM deletion and recreation may be
    missed) for certain types of VMs.
    
    Bug: 897355
    Change-Id: Iec66083304c5bbd3a3326e21e59228b6e213adf1

--- gce/appengine/backend/internal/metrics/metrics.go
+++ gce/appengine/backend/internal/metrics/metrics.go
@@ -17,7 +17,13 @@ package metrics
 
 import (
 	"context"
+	"time"
 
+	"go.chromium.org/gae/service/datastore"
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/tsmon"
 	"go.chromium.org/luci/common/tsmon/field"
 	"go.chromium.org/luci/common/tsmon/metric"
 
@@ -69,15 +75,118 @@ var (
 	)
 )
 
+// instanceCount encapsulates a count of connected and created VMs.
+type instanceCount struct {
+	Connected int
+	Created   int
+	Project   string
+	Server    string
+	Zone      string
+}
+
+// InstanceCounter tracks counts of connected and created VMs for a common
+// prefix. Create a new one with make(InstanceCounter).
+type InstanceCounter map[string]map[string]map[string]*instanceCount
+
+// get returns the *instanceCount associated with the given project, server, and
+// zone, creating it if it doesn't exist.
+func (ic InstanceCounter) get(project, server, zone string) *instanceCount {
+	if _, ok := ic[project]; !ok {
+		ic[project] = make(map[string]map[string]*instanceCount)
+	}
+	if _, ok := ic[project][server]; !ok {
+		ic[project][server] = make(map[string]*instanceCount)
+	}
+	if _, ok := ic[project][server][zone]; !ok {
+		ic[project][server][zone] = &instanceCount{
+			Project: project,
+			Server:  server,
+			Zone:    zone,
+		}
+	}
+	return ic[project][server][zone]
+}
+
+// Connected increments by one the count of connected VMs for the given project,
+// server, and zone.
+func (ic InstanceCounter) Connected(project, server, zone string) {
+	ic.get(project, server, zone).Connected++
+}
+
+// Created increments by one the count of created VMs for the given project,
+// server, and zone.
+func (ic InstanceCounter) Created(project, server, zone string) {
+	ic.get(project, server, zone).Created++
+}
+
+// Update updates metrics for all known counts of VMs for the given prefix.
+func (ic InstanceCounter) Update(c context.Context, prefix string) error {
+	n := &InstanceCount{
+		// Prefixes are globally unique, so we can use them as IDs.
+		ID:       prefix,
+		Computed: clock.Now(c).UTC(),
+		Counts:   make([]instanceCount, 0),
+		Prefix:   prefix,
+	}
+	for _, srvs := range ic {
+		for _, zones := range srvs {
+			for _, count := range zones {
+				n.Counts = append(n.Counts, *count)
+			}
+		}
+	}
+	if err := datastore.Put(c, n); err != nil {
+		return errors.Annotate(err, "failed to store count").Err()
+	}
+	return nil
+}
+
+// InstanceCount is a root entity representing a count for instances with a
+// common prefix.
+type InstanceCount struct {
+	// _extra is where unknown properties are put into memory.
+	// Extra properties are not written to the datastore.
+	_extra datastore.PropertyMap `gae:"-,extra"`
+	// _kind is the entity's kind in the datastore.
+	_kind string `gae:"$kind,InstanceCount"`
+	// ID is the unique identifier for this count.
+	ID string `gae:"$id"`
+	// Prefix is the prefix for this count.
+	Prefix string `gae:"prefix"`
+	// Computed is the time this count was computed.
+	Computed time.Time `gae:"computed"`
+	// Counts is a slice of instanceCounts.
+	Counts []instanceCount `gae:"counts,noindex"`
+}
+
 // UpdateConfiguredInstances sets configured GCE instance metrics.
 func UpdateConfiguredInstances(c context.Context, configured int, prefix, project string) {
 	configuredInstances.Set(c, int64(configured), prefix, project)
 }
 
-// UpdateInstances sets GCE instance metrics.
-func UpdateInstances(c context.Context, connected, created int, prefix, project, server, zone string) {
-	connectedInstances.Set(c, int64(connected), prefix, project, server, zone)
-	createdInstances.Set(c, int64(created), prefix, project, zone)
+// updateInstances sets GCE instance metrics.
+func updateInstances(c context.Context) {
+	now := clock.Now(c)
+	q := datastore.NewQuery("InstanceCount").Order("computed")
+	if err := datastore.Run(c, q, func(n *InstanceCount) {
+		if now.Sub(n.Computed) > 10*time.Minute {
+			logging.Debugf(c, "deleting outdated count %q", n.Prefix)
+			if err := datastore.Delete(c, n); err != nil {
+				logging.Errorf(c, "%s", err)
+			}
+			return
+		}
+		for _, count := range n.Counts {
+			connectedInstances.Set(c, int64(count.Connected), n.Prefix, count.Project, count.Server, count.Zone)
+			createdInstances.Set(c, int64(count.Created), n.Prefix, count.Project, count.Zone)
+		}
+	}); err != nil {
+		errors.Log(c, errors.Annotate(err, "failed to fetch counts").Err())
+	}
+}
+
+func init() {
+	tsmon.RegisterGlobalCallback(updateInstances, connectedInstances, createdInstances)
 }
 
 var (
