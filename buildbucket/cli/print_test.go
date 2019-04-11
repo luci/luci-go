7216b013e428361b8e91a90592e84bc027d2e68b
Nodir Turakulov <nodir@google.com>
2019-04-11 09:51:21 -0700

    [bb] Truncate durations
    E.g. 6.2s instead of 6.2342423s
    
    R=sergiyb@chromium.org
    
    Change-Id: I1285910a56a70e2a6e4c231d8c6009efd4955b2c

--- buildbucket/cli/print_test.go
+++ buildbucket/cli/print_test.go
@@ -156,7 +156,7 @@ var buildJSON = `
 const expectedBuildPrintedTemplate = `<green+u>http://ci.chromium.org/b/8917899588926498064<reset><green> SUCCESS   'chromium/try/linux-rel/1'<reset>
 <white+b>Summary<reset>: it was ok
 <white+b>Experimental<reset>
-<white+b>Created<reset> on 2019-03-26 at 18:33:47, <white+b>waited<reset> 4.488841s, <white+b>started<reset> at 18:33:52, <white+b>ran<reset> for 3m21.444617s, <white+b>ended<reset> at 18:37:13
+<white+b>Created<reset> on 2019-03-26 at 18:33:47, <white+b>waited<reset> 4.5s, <white+b>started<reset> at 18:33:52, <white+b>ran<reset> for 3m21s, <white+b>ended<reset> at 18:37:13
 <white+b>By<reset>: user:5071639625-1lppvbtck1morgivc6sq4dul7klu27sd@developer.gserviceaccount.com
 <white+b>Commit<reset>: <white+u>https://chromium.googlesource.com/infra/luci/luci-go/+/deadbeef<reset> on refs/heads/master
 <white+b>CL<reset>: <white+u>https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1539021/1<reset>
@@ -183,10 +183,10 @@ const expectedBuildPrintedTemplate = `<green+u>http://ci.chromium.org/b/89178995
   },
   "bot_id": "build234-m9"
 }
-<green>Step "first"    SUCCESS   58.9ms    Logs: "stdout", "stderr"<reset>
+<green>Step "first"    SUCCESS   59ms      Logs: "stdout", "stderr"<reset>
   first summary markdown
   summary second line
-<green>Step "second"   SUCCESS   58.9ms    Logs: "stdout", "stderr"<reset>
+<green>Step "second"   SUCCESS   59ms      Logs: "stdout", "stderr"<reset>
 `
 
 func TestPrint(t *testing.T) {
