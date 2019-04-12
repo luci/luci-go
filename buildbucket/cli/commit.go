0f32b1f4db9e46ff70fe1b94615f695c92f2ed9d
Nodir Turakulov <nodir@google.com>
2019-04-12 10:57:49 -0700

    [bb] Add crrev.com support for commits
    When parsing a "-commit crrev.com/blah", ask crrev what is blah
    
    R=sergiyb@chromium.org, tandrii@chromium.org
    
    Change-Id: I26132f2c488a95879407a9eb26467b5f2ede8ce9

--- buildbucket/cli/commit.go
+++ buildbucket/cli/commit.go
@@ -15,10 +15,16 @@
 package cli
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"net/http"
+	"net/url"
 	"regexp"
 	"strings"
+
+	"golang.org/x/net/context/ctxhttp"
 
 	pb "go.chromium.org/luci/buildbucket/proto"
 )
@@ -31,15 +37,29 @@ func (f *commitFlag) Register(fs *flag.FlagSet, help string) {
 	fs.StringVar(&f.commit, "commit", "", help)
 }
 
+var regexCommitCRRev = regexp.MustCompile(`crrev\.com/([a-f0-9]+)`)
+
 // retrieveCommit retrieves a GitilesCommit from f.commit.
-// Interacts with the user if necessary/
-func (f *commitFlag) retrieveCommit() (*pb.GitilesCommit, error) {
-	if f.commit == "" {
+// Might make RPCs.
+// Interacts with the user if necessary.
+func (f *commitFlag) retrieveCommit(ctx context.Context, client *http.Client) (*pb.GitilesCommit, error) {
+	s := f.commit
+	if s == "" {
 		return nil, nil
 	}
 
-	commit, mustConfirmRef, err := parseCommit(f.commit)
+	if m := regexCommitCRRev.FindStringSubmatch(s); m != nil {
+		redirect, err := crrevRedirect(ctx, client, m[1])
+		if err != nil {
+			return nil, fmt.Errorf("crrev.com response for %q: %s", m[1], err)
+		}
+		s = redirect
+	}
+
+	commit, mustConfirmRef, err := parseCommit(s)
 	switch {
+	case err == errMalformedCommitString:
+		return nil, fmt.Errorf("does not match r%q or r%q", regexCommit, regexCommitCRRev)
 	case err != nil:
 		return nil, fmt.Errorf("invalid -commit: %s", err)
 
@@ -52,10 +72,41 @@ func (f *commitFlag) retrieveCommit() (*pb.GitilesCommit, error) {
 			commit.Ref = reply
 		}
 	}
+
 	return commit, nil
 }
 
 var regexCommit = regexp.MustCompile(`(\w+\.googlesource\.com)/([^\+]+)/\+/(([a-f0-9]{40})|([\w\-/]+))`)
+
+// crrevRedirect returns a URL that crrev.com redirects the query to.
+func crrevRedirect(ctx context.Context, client *http.Client, query string) (redirect string, err error) {
+	// API Explorer for this API:
+	// https://cr-rev.appspot.com/_ah/api/explorer#p/crrev/v1/crrev.redirect.get
+	u := "https://cr-rev.appspot.com/_ah/api/crrev/v1/redirect/" + url.PathEscape(query)
+	res, err := ctxhttp.Get(ctx, client, u)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("%s", res.Status)
+	}
+
+	var body struct {
+		RedirectURL string `json:"redirect_url"`
+	}
+	switch err := json.NewDecoder(res.Body).Decode(&body); {
+	case err != nil:
+		return "", fmt.Errorf("decoding crrev.com response: %s", err)
+	case body.RedirectURL == "":
+		return "", fmt.Errorf("crrev.com redirected to nothing")
+	default:
+		return body.RedirectURL, nil
+	}
+}
+
+var errMalformedCommitString = fmt.Errorf("does not match regexp %q", regexCommit)
 
 // parseCommit tries to retrieve a Gitiles Commit from a string.
 //
@@ -68,7 +119,7 @@ var regexCommit = regexp.MustCompile(`(\w+\.googlesource\.com)/([^\+]+)/\+/(([a-
 func parseCommit(s string) (commit *pb.GitilesCommit, mustConfirmRef bool, err error) {
 	m := regexCommit.FindStringSubmatch(s)
 	if m == nil {
-		return nil, false, fmt.Errorf("does not match regexp %q", regexCommit)
+		return nil, false, errMalformedCommitString
 	}
 	commit = &pb.GitilesCommit{
 		Host:    m[1],
