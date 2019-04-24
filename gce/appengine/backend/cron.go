06db3345fe90a5ebe4908e2a793e23b8e46a29c4
smut <smut@google.com>
2019-04-24 23:09:15 +0000

    [GCE] Deduplicate pattern used by cron handlers
    Reduces code duplication by factoring out the pattern of "trigger one
    task per datastore entity" from each cron handler into a generic
    function. Reduces memory utilization by replacing datastore.GetAll with
    datastore.Run in cron handlers.
    
    Bug: 897355
    Change-Id: I69561d50f6fa956d9b39525f1fe730d8b4576169
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1580999
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: smut <smut@google.com>

--- gce/appengine/backend/cron.go
+++ gce/appengine/backend/cron.go
@@ -17,6 +17,9 @@ package backend
 import (
 	"context"
 	"net/http"
+	"reflect"
+
+	"github.com/golang/protobuf/proto"
 
 	"go.chromium.org/gae/service/datastore"
 	"go.chromium.org/luci/appengine/tq"
@@ -43,138 +46,66 @@ func newHTTPHandler(f func(c context.Context) error) router.Handler {
 	}
 }
 
-// countVMsAsync schedules task queue tasks to count VMs for each config.
-func countVMsAsync(c context.Context) error {
-	var keys []*datastore.Key
-	if err := datastore.GetAll(c, datastore.NewQuery(model.ConfigKind), &keys); err != nil {
-		return errors.Annotate(err, "failed to fetch configs").Err()
+// payloadFn is a function which receives an ID and returns a proto.Message to
+// use as the Payload in a *tq.Task.
+type payloadFn func(string) proto.Message
+
+// payloadFactory returns a payloadFn which can be called to return a
+// proto.Message to use as the Payload in a *tq.Task.
+func payloadFactory(t tasks.Task) payloadFn {
+	rt := reflect.TypeOf(t).Elem()
+	return func(id string) proto.Message {
+		p := reflect.New(rt)
+		p.Elem().FieldByName("Id").SetString(id)
+		return p.Interface().(proto.Message)
 	}
-	t := make([]*tq.Task, len(keys))
-	for i, k := range keys {
-		id := k.StringID()
-		t[i] = &tq.Task{
-			Payload: &tasks.CountVMs{
-				Id: id,
-			},
-		}
+}
+
+// trigger triggers a task queue task for each key returned by the given query.
+func trigger(c context.Context, t tasks.Task, q *datastore.Query) error {
+	tasks := make([]*tq.Task, 0)
+	newPayload := payloadFactory(t)
+	addTask := func(k *datastore.Key) {
+		tasks = append(tasks, &tq.Task{
+			Payload: newPayload(k.StringID()),
+		})
 	}
-	logging.Debugf(c, "found %d configs", len(t))
-	if err := getDispatcher(c).AddTask(c, t...); err != nil {
+	if err := datastore.Run(c, q, addTask); err != nil {
+		return errors.Annotate(err, "failed to fetch keys").Err()
+	}
+	logging.Debugf(c, "scheduling %d tasks", len(tasks))
+	if err := getDispatcher(c).AddTask(c, tasks...); err != nil {
 		return errors.Annotate(err, "failed to schedule tasks").Err()
 	}
 	return nil
+}
+
+// countVMsAsync schedules task queue tasks to count VMs for each config.
+func countVMsAsync(c context.Context) error {
+	return trigger(c, &tasks.CountVMs{}, datastore.NewQuery(model.ConfigKind))
 }
 
 // createInstancesAsync schedules task queue tasks to create each GCE instance.
 func createInstancesAsync(c context.Context) error {
-	var keys []*datastore.Key
-	q := datastore.NewQuery(model.VMKind).Eq("url", "")
-	if err := datastore.GetAll(c, q, &keys); err != nil {
-		return errors.Annotate(err, "failed to fetch VMs").Err()
-	}
-	t := make([]*tq.Task, len(keys))
-	for i, k := range keys {
-		id := k.StringID()
-		t[i] = &tq.Task{
-			Payload: &tasks.CreateInstance{
-				Id: id,
-			},
-		}
-	}
-	logging.Debugf(c, "found %d VMs", len(t))
-	if err := getDispatcher(c).AddTask(c, t...); err != nil {
-		return errors.Annotate(err, "failed to schedule tasks").Err()
-	}
-	return nil
+	return trigger(c, &tasks.CreateInstance{}, datastore.NewQuery(model.VMKind).Eq("url", ""))
 }
 
 // drainVMsAsync schedules task queue tasks to drain each VM.
 func drainVMsAsync(c context.Context) error {
-	var keys []*datastore.Key
-	q := datastore.NewQuery(model.VMKind)
-	if err := datastore.GetAll(c, q, &keys); err != nil {
-		return errors.Annotate(err, "failed to fetch VMs").Err()
-	}
-	t := make([]*tq.Task, len(keys))
-	for i, k := range keys {
-		id := k.StringID()
-		t[i] = &tq.Task{
-			Payload: &tasks.DrainVM{
-				Id: id,
-			},
-		}
-	}
-	logging.Debugf(c, "found %d VMs", len(t))
-	if err := getDispatcher(c).AddTask(c, t...); err != nil {
-		return errors.Annotate(err, "failed to schedule tasks").Err()
-	}
-	return nil
+	return trigger(c, &tasks.DrainVM{}, datastore.NewQuery(model.VMKind))
 }
 
 // expandConfigsAsync schedules task queue tasks to expand each config.
 func expandConfigsAsync(c context.Context) error {
-	var keys []*datastore.Key
-	if err := datastore.GetAll(c, datastore.NewQuery(model.ConfigKind), &keys); err != nil {
-		return errors.Annotate(err, "failed to fetch configs").Err()
-	}
-	t := make([]*tq.Task, len(keys))
-	for i, k := range keys {
-		id := k.StringID()
-		t[i] = &tq.Task{
-			Payload: &tasks.ExpandConfig{
-				Id: id,
-			},
-		}
-	}
-	logging.Debugf(c, "found %d configs", len(t))
-	if err := getDispatcher(c).AddTask(c, t...); err != nil {
-		return errors.Annotate(err, "failed to schedule tasks").Err()
-	}
-	return nil
+	return trigger(c, &tasks.ExpandConfig{}, datastore.NewQuery(model.ConfigKind))
 }
 
 // manageBotsAsync schedules task queue tasks to manage each Swarming bot.
 func manageBotsAsync(c context.Context) error {
-	var keys []*datastore.Key
-	// TODO(smut): Fetch in batches.
-	q := datastore.NewQuery(model.VMKind).Gt("url", "")
-	if err := datastore.GetAll(c, q, &keys); err != nil {
-		return errors.Annotate(err, "failed to fetch VMs").Err()
-	}
-	t := make([]*tq.Task, len(keys))
-	for i, k := range keys {
-		id := k.StringID()
-		t[i] = &tq.Task{
-			Payload: &tasks.ManageBot{
-				Id: id,
-			},
-		}
-	}
-	logging.Debugf(c, "found %d VMs", len(t))
-	if err := getDispatcher(c).AddTask(c, t...); err != nil {
-		return errors.Annotate(err, "failed to schedule tasks").Err()
-	}
-	return nil
+	return trigger(c, &tasks.ManageBot{}, datastore.NewQuery(model.VMKind).Gt("url", ""))
 }
 
 // reportQuotasAsync schedules task queue tasks to report quota in each project.
 func reportQuotasAsync(c context.Context) error {
-	var keys []*datastore.Key
-	if err := datastore.GetAll(c, datastore.NewQuery(model.ProjectKind), &keys); err != nil {
-		return errors.Annotate(err, "failed to fetch project").Err()
-	}
-	t := make([]*tq.Task, len(keys))
-	for i, k := range keys {
-		id := k.StringID()
-		t[i] = &tq.Task{
-			Payload: &tasks.ReportQuota{
-				Id: id,
-			},
-		}
-	}
-	logging.Debugf(c, "found %d projects", len(t))
-	if err := getDispatcher(c).AddTask(c, t...); err != nil {
-		return errors.Annotate(err, "failed to schedule tasks").Err()
-	}
-	return nil
+	return trigger(c, &tasks.ReportQuota{}, datastore.NewQuery(model.ProjectKind))
 }
