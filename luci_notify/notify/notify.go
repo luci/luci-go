82aac5f666c19976391bc7eef86e86fbc699b23e
Nodir Turakulov <nodir@google.com>
2019-05-06 23:22:00 +0000

    [luci_notify] Reply to yourself
    Use the recepient email address as reply-to
    
    R=tandrii@chromium.org
    
    Change-Id: I0b00f09d0ea8a1922a132c06ea7ef31a6cc32873
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1597698
    Auto-Submit: Nodir Turakulov <nodir@chromium.org>
    Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>

--- luci_notify/notify/notify.go
+++ luci_notify/notify/notify.go
@@ -220,5 +220,6 @@ func SendEmail(c context.Context, task proto.Message) error {
 		To:       emailTask.Recipients,
 		Subject:  emailTask.Subject,
 		HTMLBody: body,
+		ReplyTo:  emailTask.Recipients[0],
 	})
 }
