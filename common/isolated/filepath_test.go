d49bb93ed6217ac07af337d42afb67557135a2c2
Takuto Ikuta <tikuta@chromium.org>
2019-05-15 23:24:50 +0900

    [isolated] add filepath.go
    This CL adds functions used for change_tree_read_only
    https://cs.chromium.org/chromium/infra/luci/client/run_isolated.py?l=235&rcl=fed355224b4742cfba34fc5df27213ad20d88193
    
    Bug: 962804
    Change-Id: Ie6e12bfa916a15a6cb4b168482635119fe538946

---
+++ common/isolated/filepath_test.go
@@ -0,0 +1,62 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package isolated
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSetReadOnly(t *testing.T) {
+	t.Parallel()
+
+	Convey("Simple tests for SetReadOnly", t, func() {
+		tmpdir, err := ioutil.TempDir("", "set_read_only_")
+		So(err, ShouldBeNil)
+		defer func() {
+			So(os.RemoveAll(tmpdir), ShouldBeNil)
+		}()
+
+		f, err := ioutil.TempFile(tmpdir, "tmp")
+		So(err, ShouldBeNil)
+		tmpfile := f.Name()
+		So(f.Close(), ShouldBeNil)
+
+		// make tmpfile readonly
+		So(SetReadOnly(tmpfile, true), ShouldBeNil)
+
+		_, err = os.Create(tmpfile)
+		So(err, ShouldNotBeNil)
+
+		f, err = os.Open(tmpfile)
+		So(err, ShouldBeNil)
+		So(f.Close(), ShouldBeNil)
+
+		// make tmpfile writable
+		So(SetReadOnly(tmpfile, false), ShouldBeNil)
+
+		f, err = os.Create(tmpfile)
+		So(err, ShouldBeNil)
+		So(f.Close(), ShouldBeNil)
+
+		f, err = os.Open(tmpfile)
+		So(err, ShouldBeNil)
+		So(f.Close(), ShouldBeNil)
+	})
+
+}
