bbe87f083fe34cf624360712ad1db3237bdbb868
Vadim Shtayura <vadimsh@chromium.org>
2019-05-16 15:07:42 -0700

    [server] Implement google-fluentd compatible logging.
    The hardest part is collecting various useful information about the request:
      * End-client IP address: extracted from X-Forwarded-For header.
      * Cloud Trace ID: extracted from X-Cloud-Trace-Context header.
      * Request URL: reconstructed based on request headers.
      * Log severity: calculated based on max observed severity inside the handler.
      * Response status and size: calculated by substituting http.ResponseWriter.
    
    All this stuff surfaces in Stackdriver Logs UI.
    
    R=jchinlee@chromium.org, nodir@chromium.org
    BUG=959427
    
    Change-Id: I820df2dbb644bbe28d9f99ab3c235769177c3da9

--- server/server_test.go
+++ server/server_test.go
@@ -15,12 +15,18 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
+	"sync"
 	"testing"
+	"time"
 
+	"go.chromium.org/luci/common/clock/testclock"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/logging/gkelogger"
 	"go.chromium.org/luci/server/router"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -30,9 +36,20 @@ func TestServer(t *testing.T) {
 	t.Parallel()
 
 	Convey("Works", t, func() {
+		ctx, tc := testclock.UseTime(context.Background(), testclock.TestRecentTimeUTC)
+
+		stdoutLogs := logsRecorder{}
+		stderrLogs := logsRecorder{}
+
 		srv := New(Options{
+			Prod:      true,
 			HTTPAddr:  "main_addr",
 			AdminAddr: "admin_addr",
+
+			testCtx:    ctx,
+			testSeed:   1,
+			testStdout: &stdoutLogs,
+			testStderr: &stderrLogs,
 
 			// Bind to auto-assigned ports.
 			testListeners: map[string]net.Listener{
@@ -45,6 +62,10 @@ func TestServer(t *testing.T) {
 		mainAddr := fmt.Sprintf("http://127.0.0.1:%d", mainPort)
 
 		srv.Routes.GET("/test", router.MiddlewareChain{}, func(c *router.Context) {
+			logging.Infof(c.Context, "Info log")
+			tc.Add(time.Second)
+			logging.Warningf(c.Context, "Warn log")
+			c.Writer.WriteHeader(404)
 			c.Writer.Write([]byte("Hello, world"))
 		})
 
@@ -53,9 +74,53 @@ func TestServer(t *testing.T) {
 		go func() { done <- srv.ListenAndServe() }()
 
 		// Call "/test" to verify the server can serve successfully.
-		resp, err := httpGet(mainAddr + "/test")
+		resp, err := httpGet(mainAddr+"/test", map[string]string{
+			"User-Agent":            "Test-user-agent",
+			"X-Cloud-Trace-Context": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/00001;trace=TRUE",
+			"X-Forwarded-For":       "1.1.1.1,2.2.2.2,3.3.3.3",
+		})
 		So(err, ShouldBeNil)
 		So(resp, ShouldEqual, "Hello, world")
+
+		// Stderr log captures details about the request.
+		So(stderrLogs.Last(1), ShouldResemble, []gkelogger.LogEntry{
+			{
+				Severity: "warning",
+				Time:     "1454472307.7",
+				TraceID:  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+				RequestInfo: &gkelogger.RequestInfo{
+					Method:       "GET",
+					URL:          mainAddr + "/test",
+					Status:       404,
+					RequestSize:  "0",
+					ResponseSize: "12", // len("Hello, world")
+					UserAgent:    "Test-user-agent",
+					RemoteIP:     "2.2.2.2",
+					Latency:      "1.000000s",
+				},
+			},
+		})
+		// Stdout log captures individual log lines.
+		So(stdoutLogs.Last(2), ShouldResemble, []gkelogger.LogEntry{
+			{
+				Severity: "info",
+				Message:  "Info log",
+				Time:     "1454472306.7",
+				TraceID:  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+				Operation: &gkelogger.Operation{
+					ID: "9566c74d10037c4d7bbb0407d1e2c649",
+				},
+			},
+			{
+				Severity: "warning",
+				Message:  "Warn log",
+				Time:     "1454472307.7",
+				TraceID:  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+				Operation: &gkelogger.Operation{
+					ID: "9566c74d10037c4d7bbb0407d1e2c649",
+				},
+			},
+		})
 
 		// Make sure Shutdown works.
 		srv.Shutdown()
@@ -71,12 +136,38 @@ func setupListener() net.Listener {
 	return l
 }
 
-func httpGet(addr string) (resp string, err error) {
-	res, err := http.Get(addr)
+func httpGet(addr string, headers map[string]string) (resp string, err error) {
+	req, err := http.NewRequest("GET", addr, nil)
+	if err != nil {
+		return
+	}
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return
 	}
 	defer res.Body.Close()
 	blob, err := ioutil.ReadAll(res.Body)
 	return string(blob), err
+}
+
+type logsRecorder struct {
+	m    sync.Mutex
+	logs []gkelogger.LogEntry
+}
+
+func (r *logsRecorder) Write(e *gkelogger.LogEntry) {
+	r.m.Lock()
+	r.logs = append(r.logs, *e)
+	r.m.Unlock()
+}
+
+func (r *logsRecorder) Last(n int) []gkelogger.LogEntry {
+	entries := make([]gkelogger.LogEntry, n)
+	r.m.Lock()
+	copy(entries, r.logs[len(r.logs)-n:])
+	r.m.Unlock()
+	return entries
 }
