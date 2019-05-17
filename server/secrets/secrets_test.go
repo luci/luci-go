55ca5eded1d9b2e31f1e9bde85f7701de5d33895
Vadim Shtayura <vadimsh@chromium.org>
2019-05-16 21:04:35 -0700

    [secrets] Simplify secrets API.
    In particular:
      * Replace NamedBlob with just []byte. Names weren't actually used.
      * Replace secrets.Key with just 'string'.
      * Remove Clone(), trust callers not to mutate secrets.
    
    R=jchinlee@chromium.org, nodir@chromium.org
    BUG=959427
    
    Change-Id: I7378d57c58d16f204babb281e956798433b9de55

--- server/secrets/secrets_test.go
+++ server/secrets/secrets_test.go
@@ -21,35 +21,19 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestClone(t *testing.T) {
-	Convey("Clone works", t, func() {
-		s1 := Secret{
-			Current: NamedBlob{"id1", []byte{1, 2, 3}},
-			Previous: []NamedBlob{
-				{"id2", []byte{4, 5, 6}},
-				{"id3", []byte{7, 8, 9}},
-			},
-		}
-		s2 := s1.Clone()
-		So(s1, ShouldResemble, s2)
-		So(s1.Current.Blob, ShouldNotEqual, s2.Current.Blob)
-		So(s1.Previous[0].Blob, ShouldNotEqual, s2.Previous[0].Blob)
-	})
-}
-
 func TestBlobs(t *testing.T) {
 	Convey("Blobs works", t, func() {
 		s := Secret{
-			Current: NamedBlob{"id1", nil},
-			Previous: []NamedBlob{
-				{"id2", nil},
-				{"id3", nil},
+			Current: []byte("s1"),
+			Previous: [][]byte{
+				[]byte("s2"),
+				[]byte("s3"),
 			},
 		}
-		So(s.Blobs(), ShouldResemble, []NamedBlob{
-			{"id1", nil},
-			{"id2", nil},
-			{"id3", nil},
+		So(s.Blobs(), ShouldResemble, [][]byte{
+			[]byte("s1"),
+			[]byte("s2"),
+			[]byte("s3"),
 		})
 	})
 }
@@ -57,11 +41,11 @@ func TestBlobs(t *testing.T) {
 func TestContext(t *testing.T) {
 	Convey("Works", t, func() {
 		c := Set(context.Background(), StaticStore{
-			"key": Secret{Current: NamedBlob{ID: "secret"}},
+			"key": Secret{Current: []byte("secret")},
 		})
 		s, err := GetSecret(c, "key")
 		So(err, ShouldBeNil)
-		So(s.Current.ID, ShouldEqual, "secret")
+		So(s.Current, ShouldResemble, []byte("secret"))
 
 		_, err = GetSecret(c, "missing")
 		So(err, ShouldEqual, ErrNoSuchSecret)
