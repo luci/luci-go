2fdae2867c2baa987771dbb036030eeb1e858df8
smut <smut@google.com>
2019-05-22 21:50:29 +0000

    [GCE] Remove prefix requirement when listing instances
    Because it makes sense to be able to supply a filter without a prefix to
    search for all instances using a particular image. Adds page size to
    prevent empty requests (which were previously rejected outright) from
    fetching thousands of results.
    
    Bug: 951483
    Change-Id: Ib04d12d438244a3fb3f1d8716fbd3dfc93ddfbd4
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1623159
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: smut <smut@google.com>

---
+++ gce/appengine/rpc/instances_test.go
@@ -0,0 +1,195 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"context"
+	"testing"
+
+	"go.chromium.org/gae/impl/memory"
+	"go.chromium.org/gae/service/datastore"
+
+	"go.chromium.org/luci/gce/api/instances/v1"
+	"go.chromium.org/luci/gce/appengine/model"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestInstances(t *testing.T) {
+	t.Parallel()
+
+	Convey("Instances", t, func() {
+		srv := &Instances{}
+		c := memory.Use(context.Background())
+		datastore.GetTestable(c).AutoIndex(true)
+		datastore.GetTestable(c).Consistent(true)
+
+		Convey("List", func() {
+			Convey("invalid", func() {
+				Convey("nil", func() {
+					_, err := srv.List(c, nil)
+					So(err, ShouldErrLike, "request must not be nil")
+				})
+
+				Convey("filter", func() {
+					req := &instances.ListRequest{
+						Filter: "filter",
+					}
+					_, err := srv.List(c, req)
+					So(err, ShouldErrLike, "invalid filter expression")
+				})
+
+				Convey("page token", func() {
+					req := &instances.ListRequest{
+						PageToken: "token",
+					}
+					_, err := srv.List(c, req)
+					So(err, ShouldErrLike, "invalid page token")
+				})
+			})
+
+			Convey("valid", func() {
+				Convey("empty", func() {
+					Convey("none", func() {
+						req := &instances.ListRequest{}
+						rsp, err := srv.List(c, req)
+						So(err, ShouldBeNil)
+						So(rsp.Instances, ShouldBeEmpty)
+					})
+
+					Convey("one", func() {
+						vm := &model.VM{
+							ID: "id",
+						}
+						So(datastore.Put(c, vm), ShouldBeNil)
+
+						req := &instances.ListRequest{}
+						rsp, err := srv.List(c, req)
+						So(err, ShouldBeNil)
+						So(rsp.Instances, ShouldHaveLength, 1)
+						So(rsp.Instances[0].Id, ShouldEqual, "id")
+					})
+				})
+
+				Convey("pages", func() {
+					So(datastore.Put(c, &model.VM{ID: "id1"}), ShouldBeNil)
+					So(datastore.Put(c, &model.VM{ID: "id2"}), ShouldBeNil)
+					So(datastore.Put(c, &model.VM{ID: "id3"}), ShouldBeNil)
+
+					Convey("default", func() {
+						req := &instances.ListRequest{}
+						rsp, err := srv.List(c, req)
+						So(err, ShouldBeNil)
+						So(rsp.Instances, ShouldNotBeEmpty)
+					})
+
+					Convey("one", func() {
+						req := &instances.ListRequest{
+							PageSize: 1,
+						}
+						rsp, err := srv.List(c, req)
+						So(err, ShouldBeNil)
+						So(rsp.Instances, ShouldHaveLength, 1)
+						So(rsp.Instances[0].Id, ShouldEqual, "id1")
+
+						req.PageToken = rsp.NextPageToken
+						rsp, err = srv.List(c, req)
+						So(err, ShouldBeNil)
+						So(rsp.Instances, ShouldHaveLength, 1)
+						So(rsp.Instances[0].Id, ShouldEqual, "id2")
+
+						req.PageToken = rsp.NextPageToken
+						rsp, err = srv.List(c, req)
+						So(err, ShouldBeNil)
+						So(rsp.Instances, ShouldHaveLength, 1)
+						So(rsp.Instances[0].Id, ShouldEqual, "id3")
+					})
+
+					Convey("two", func() {
+						req := &instances.ListRequest{
+							PageSize: 2,
+						}
+						rsp, err := srv.List(c, req)
+						So(err, ShouldBeNil)
+						So(rsp.Instances, ShouldHaveLength, 2)
+
+						req.PageToken = rsp.NextPageToken
+						rsp, err = srv.List(c, req)
+						So(err, ShouldBeNil)
+						So(rsp.Instances, ShouldHaveLength, 1)
+					})
+
+					Convey("many", func() {
+						req := &instances.ListRequest{
+							PageSize: 200,
+						}
+						rsp, err := srv.List(c, req)
+						So(err, ShouldBeNil)
+						So(rsp.Instances, ShouldHaveLength, 3)
+
+						req.PageToken = rsp.NextPageToken
+						rsp, err = srv.List(c, req)
+						So(err, ShouldBeNil)
+						So(rsp.Instances, ShouldBeEmpty)
+					})
+				})
+
+				Convey("filter", func() {
+					vm := &model.VM{
+						ID: "id",
+						AttributesIndexed: []string{
+							"disk.image:image2",
+						},
+					}
+					So(datastore.Put(c, vm), ShouldBeNil)
+
+					req := &instances.ListRequest{
+						Filter: "disks.image=image1",
+					}
+					rsp, err := srv.List(c, req)
+					So(err, ShouldBeNil)
+					So(rsp.Instances, ShouldBeEmpty)
+
+					req.Filter = "disks.image=image2"
+					rsp, err = srv.List(c, req)
+					So(err, ShouldBeNil)
+					So(rsp.Instances, ShouldHaveLength, 1)
+				})
+
+				Convey("prefix", func() {
+					vm := &model.VM{
+						ID:     "id1",
+						Prefix: "prefix1",
+					}
+					So(datastore.Put(c, vm), ShouldBeNil)
+					vm = &model.VM{
+						ID:     "id2",
+						Prefix: "prefix2",
+					}
+					So(datastore.Put(c, vm), ShouldBeNil)
+
+					req := &instances.ListRequest{
+						Prefix: "prefix1",
+					}
+					rsp, err := srv.List(c, req)
+					So(err, ShouldBeNil)
+					So(rsp.Instances, ShouldHaveLength, 1)
+					So(rsp.Instances[0].Id, ShouldEqual, "id1")
+				})
+			})
+		})
+	})
+}
