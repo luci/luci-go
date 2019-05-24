2a673ef77172da4a42e765cde185f5dd3cf4776c
Takuto Ikuta <tikuta@chromium.org>
2019-05-24 15:23:56 +0000

    [exec2] add exec2 package supporting better process management
    This supports Wait with timeout.
    I will do windows support later.
    
    This will be used for run_command
    https://cs.chromium.org/chromium/infra/luci/client/run_isolated.py?l=425&rcl=851d2d0d1642e24bae3797aaabb546e230663d53
    
    Bug: 962804
    Change-Id: I8f034f40ba3c13eaa7004626c97200180c89521b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1627443
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>

---
+++ common/system/exec2/exec2_unix.go
@@ -0,0 +1,37 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !windows
+
+package exec2
+
+import (
+	"syscall"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+func (c *Cmd) setupCmd() {
+	c.SysProcAttr = &syscall.SysProcAttr{
+		// Use process group to kill all child processes.
+		Setpgid: true,
+	}
+}
+
+func (c *Cmd) terminate() error {
+	if err := c.Process.Signal(syscall.SIGTERM); err != nil {
+		return errors.Annotate(err, "failed to send SIGTERM").Err()
+	}
+	return nil
+}
