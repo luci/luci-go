35a6439a46cef86d41dcddc2dd31712738442889
Takuto Ikuta <tikuta@chromium.org>
2019-05-24 23:35:29 +0900

    [exec2] add exec2 package supporting better process management
    This supports Wait with timeout.
    I will do windows support later.
    
    This will be used for run_command
    https://cs.chromium.org/chromium/infra/luci/client/run_isolated.py?l=425&rcl=851d2d0d1642e24bae3797aaabb546e230663d53
    
    Bug: 962804
    Change-Id: I8f034f40ba3c13eaa7004626c97200180c89521b

---
+++ common/system/exec2/exec2.go
@@ -0,0 +1,70 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package exec2
+
+import (
+	"context"
+	"os/exec"
+	"sync"
+	"time"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+// ErrTimeout is error for timeout.
+var ErrTimeout = errors.Reason("timeout").Err()
+
+// Cmd is like exec.Cmd, but has fields for timeout support.
+type Cmd struct {
+	*exec.Cmd
+
+	waitCh chan error
+	once   sync.Once
+}
+
+// CommandContext is like exec.CommandContext, but it uses process group by
+// default and supports timeout in Wait function.
+func CommandContext(ctx context.Context, name string, arg ...string) *Cmd {
+	cmd := &Cmd{
+		Cmd: exec.CommandContext(ctx, name, arg...),
+	}
+
+	cmd.setupCmd()
+
+	return cmd
+}
+
+// Wait waits for timeout.
+func (c *Cmd) Wait(timeout time.Duration) error {
+	c.once.Do(func() {
+		c.waitCh = make(chan error)
+		go func() {
+			c.waitCh <- c.Cmd.Wait()
+			close(c.waitCh)
+		}()
+	})
+
+	select {
+	case <-time.After(timeout):
+		return ErrTimeout
+	case err := <-c.waitCh:
+		return err
+	}
+}
+
+// Terminate sends SIGTERM.
+func (c *Cmd) Terminate() error {
+	return c.terminate()
+}
