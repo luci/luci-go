13178f2b6c55940347bf3646292ceca9bc33e7d3
smut <smut@google.com>
2019-05-24 20:02:47 -0700

    [GCE] Support page size and page tokens for all list RPCs
    Bug: 897355
    Change-Id: I3dd2ba37865e11ca3894707d076daeefe9ae6653

--- gce/appengine/rpc/projects_test.go
+++ gce/appengine/rpc/projects_test.go
@@ -21,34 +21,26 @@ import (
 	"go.chromium.org/gae/impl/memory"
 	"go.chromium.org/gae/service/datastore"
 
-	"go.chromium.org/luci/gce/api/instances/v1"
+	"go.chromium.org/luci/gce/api/projects/v1"
 	"go.chromium.org/luci/gce/appengine/model"
 
 	. "github.com/smartystreets/goconvey/convey"
 	. "go.chromium.org/luci/common/testing/assertions"
 )
 
-func TestInstances(t *testing.T) {
+func TestProjects(t *testing.T) {
 	t.Parallel()
 
-	Convey("Instances", t, func() {
-		srv := &Instances{}
+	Convey("Projects", t, func() {
+		srv := &Projects{}
 		c := memory.Use(context.Background())
 		datastore.GetTestable(c).AutoIndex(true)
 		datastore.GetTestable(c).Consistent(true)
 
 		Convey("List", func() {
 			Convey("invalid", func() {
-				Convey("filter", func() {
-					req := &instances.ListRequest{
-						Filter: "filter",
-					}
-					_, err := srv.List(c, req)
-					So(err, ShouldErrLike, "invalid filter expression")
-				})
-
 				Convey("page token", func() {
-					req := &instances.ListRequest{
+					req := &projects.ListRequest{
 						PageToken: "token",
 					}
 					_, err := srv.List(c, req)
@@ -61,148 +53,100 @@ func TestInstances(t *testing.T) {
 					Convey("none", func() {
 						rsp, err := srv.List(c, nil)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldBeEmpty)
+						So(rsp.Projects, ShouldBeEmpty)
 					})
 
 					Convey("one", func() {
-						vm := &model.VM{
+						p := &model.Project{
 							ID: "id",
 						}
-						So(datastore.Put(c, vm), ShouldBeNil)
+						So(datastore.Put(c, p), ShouldBeNil)
 
 						rsp, err := srv.List(c, nil)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldHaveLength, 1)
-						So(rsp.Instances[0].Id, ShouldEqual, "id")
+						So(rsp.Projects, ShouldHaveLength, 1)
 					})
 				})
 
 				Convey("empty", func() {
 					Convey("none", func() {
-						req := &instances.ListRequest{}
+						req := &projects.ListRequest{}
 						rsp, err := srv.List(c, req)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldBeEmpty)
+						So(rsp.Projects, ShouldBeEmpty)
 					})
 
 					Convey("one", func() {
-						vm := &model.VM{
+						p := &model.Project{
 							ID: "id",
 						}
-						So(datastore.Put(c, vm), ShouldBeNil)
+						So(datastore.Put(c, p), ShouldBeNil)
 
-						req := &instances.ListRequest{}
+						req := &projects.ListRequest{}
 						rsp, err := srv.List(c, req)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldHaveLength, 1)
-						So(rsp.Instances[0].Id, ShouldEqual, "id")
+						So(rsp.Projects, ShouldHaveLength, 1)
 					})
 				})
 
 				Convey("pages", func() {
-					So(datastore.Put(c, &model.VM{ID: "id1"}), ShouldBeNil)
-					So(datastore.Put(c, &model.VM{ID: "id2"}), ShouldBeNil)
-					So(datastore.Put(c, &model.VM{ID: "id3"}), ShouldBeNil)
+					So(datastore.Put(c, &model.Project{ID: "id1"}), ShouldBeNil)
+					So(datastore.Put(c, &model.Project{ID: "id2"}), ShouldBeNil)
+					So(datastore.Put(c, &model.Project{ID: "id3"}), ShouldBeNil)
 
 					Convey("default", func() {
-						req := &instances.ListRequest{}
+						req := &projects.ListRequest{}
 						rsp, err := srv.List(c, req)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldNotBeEmpty)
+						So(rsp.Projects, ShouldNotBeEmpty)
 					})
 
 					Convey("one", func() {
-						req := &instances.ListRequest{
+						req := &projects.ListRequest{
 							PageSize: 1,
 						}
 						rsp, err := srv.List(c, req)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldHaveLength, 1)
-						So(rsp.Instances[0].Id, ShouldEqual, "id1")
+						So(rsp.Projects, ShouldHaveLength, 1)
 
 						req.PageToken = rsp.NextPageToken
 						rsp, err = srv.List(c, req)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldHaveLength, 1)
-						So(rsp.Instances[0].Id, ShouldEqual, "id2")
+						So(rsp.Projects, ShouldHaveLength, 1)
 
 						req.PageToken = rsp.NextPageToken
 						rsp, err = srv.List(c, req)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldHaveLength, 1)
-						So(rsp.Instances[0].Id, ShouldEqual, "id3")
+						So(rsp.Projects, ShouldHaveLength, 1)
 					})
 
 					Convey("two", func() {
-						req := &instances.ListRequest{
+						req := &projects.ListRequest{
 							PageSize: 2,
 						}
 						rsp, err := srv.List(c, req)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldHaveLength, 2)
+						So(rsp.Projects, ShouldHaveLength, 2)
 
 						req.PageToken = rsp.NextPageToken
 						rsp, err = srv.List(c, req)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldHaveLength, 1)
+						So(rsp.Projects, ShouldHaveLength, 1)
 					})
 
 					Convey("many", func() {
-						req := &instances.ListRequest{
+						req := &projects.ListRequest{
 							PageSize: 200,
 						}
 						rsp, err := srv.List(c, req)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldHaveLength, 3)
+						So(rsp.Projects, ShouldHaveLength, 3)
 
 						req.PageToken = rsp.NextPageToken
 						rsp, err = srv.List(c, req)
 						So(err, ShouldBeNil)
-						So(rsp.Instances, ShouldBeEmpty)
+						So(rsp.Projects, ShouldBeEmpty)
 					})
-				})
-
-				Convey("filter", func() {
-					vm := &model.VM{
-						ID: "id",
-						AttributesIndexed: []string{
-							"disk.image:image2",
-						},
-					}
-					So(datastore.Put(c, vm), ShouldBeNil)
-
-					req := &instances.ListRequest{
-						Filter: "disks.image=image1",
-					}
-					rsp, err := srv.List(c, req)
-					So(err, ShouldBeNil)
-					So(rsp.Instances, ShouldBeEmpty)
-
-					req.Filter = "disks.image=image2"
-					rsp, err = srv.List(c, req)
-					So(err, ShouldBeNil)
-					So(rsp.Instances, ShouldHaveLength, 1)
-				})
-
-				Convey("prefix", func() {
-					vm := &model.VM{
-						ID:     "id1",
-						Prefix: "prefix1",
-					}
-					So(datastore.Put(c, vm), ShouldBeNil)
-					vm = &model.VM{
-						ID:     "id2",
-						Prefix: "prefix2",
-					}
-					So(datastore.Put(c, vm), ShouldBeNil)
-
-					req := &instances.ListRequest{
-						Prefix: "prefix1",
-					}
-					rsp, err := srv.List(c, req)
-					So(err, ShouldBeNil)
-					So(rsp.Instances, ShouldHaveLength, 1)
-					So(rsp.Instances[0].Id, ShouldEqual, "id1")
 				})
 			})
 		})
