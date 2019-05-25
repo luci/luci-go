33f8dd469280b740274b08e30d16ecd8efe580de
Andrii Shyshkalov <tandrii@chromium.org>
2019-05-24 17:49:56 -0700

    cq: add proto to be used as input of CQ recipe_module.
    R=iannucci
    
    Bug: 966979
    Change-Id: I66cd4ca1954e1c58db0fb4196902050ed639ea8d

---
+++ cq/api/recipe/v1/gen.go
@@ -0,0 +1,21 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:generate cproto
+
+// Package recipe contains CQ Recipe input proto.
+package recipe
+
+// Until CQ service isn't rewritten in Go, the generated protos aren't actually
+// used for anything but verifying that .proto file in this dir is valid.
