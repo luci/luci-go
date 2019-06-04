97ab13965116727e069c78280303e421d8c8c2b0
Vadim Shtayura <vadimsh@chromium.org>
2019-06-04 21:43:01 +0000

    [cipd] Make DeletePackage work with packages with lots of metadata.
    Will be used to delete infra/cipd_recipe_test/* packages that complicate
    tags => metadata migration.
    
    R=tandrii@chromium.org
    
    Change-Id: I3aa82043aeb79d475863526d085d61da84756381
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1642354
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

--- cipd/appengine/impl/model/deletion.go
+++ cipd/appengine/impl/model/deletion.go
@@ -16,6 +16,7 @@ package model
 
 import (
 	"context"
+	"strings"
 
 	"go.chromium.org/gae/service/datastore"
 
@@ -27,116 +28,50 @@ import (
 	api "go.chromium.org/luci/cipd/api/cipd/v1"
 )
 
-var (
-	// List of entity kinds to delete when removing a package. We purposefully
-	// do not delete the entire entity group (using kindless datastore query) to
-	// make sure that whoever adds more entity types there is cognizant of the
-	// deletion procedure. Additionally, we want to preserve event log entities,
-	// that live in the same entity group.
-	kindsToDelete = []string{
-		"InstanceTag",
-		"PackageInstance",
-		"PackageRef",
-		"ProcessingResult",
-	}
-	// Number of keys to delete at once in DeletePackage. Replaced in tests.
-	deletionBatchSize = 256
-)
-
 // DeletePackage deletes all entities associated with a package.
+//
+// Deleting everything under a single transaction is generally not feasible,
+// since deleting packages with lots of metadata exceeds transaction limits.
+//
+// Instead we delete most of the stuff non-transactionally first, and then
+// delete the rest inside the transaction (to make sure nothing stays, even if
+// it was created while we were deleting stuff).
 //
 // Returns grpc-tagged errors (in particular NotFound if there's no such
 // package).
 func DeletePackage(c context.Context, pkg string) error {
-	// TODO(vadimsh): This transaction can become too big for packages with a lot
-	// of instances. If this becomes a problem, we can delete most of the stuff
-	// non-transactionally first, and then delete the rest inside the transaction
-	// (to make sure nothing stays, even if it was created while we were deleting
-	// stuff).
+	if err := CheckPackageExists(c, pkg); err != nil {
+		return err
+	}
+
+	// Note that to maintain data model consistency during the deletion, we delete
+	// various metadata first, and PackageInstance entities second (to make sure
+	// we don't have metadata with dangling pointers to deleted instances).
+	err := deleteEntityKinds(c, pkg, []string{
+		"InstanceTag",
+		"PackageRef",
+		"ProcessingResult",
+	})
+	if err != nil {
+		return err
+	}
+	if err := deleteEntityKinds(c, pkg, []string{"PackageInstance"}); err != nil {
+		return nil
+	}
+
+	// Cleanup whatever remains. It contains entities that were created while we
+	// were deleting stuff above. There should be very few (usually 0) such
+	// entities, so it's OK to delete them transactionally.
 	return Txn(c, "DeletePackage", func(c context.Context) error {
-		if err := CheckPackageExists(c, pkg); err != nil {
-			return err
-		}
-
-		err := parallel.WorkPool(len(kindsToDelete)+1, func(tasks chan<- func() error) {
-			// A channel that receives keys to delete. Set some arbitrary buffer
-			// size to parallelize work a bit better.
-			keys := make(chan *datastore.Key, 1024)
-
-			// Launch queries that fetch keys to delete, and feed them to the channel.
-			// Each query enqueues nil when it is done to let the consumer know.
-			for _, kind := range kindsToDelete {
-				kind := kind
-				tasks <- func() error {
-					q := datastore.NewQuery(kind).
-						Ancestor(PackageKey(c, pkg)).
-						KeysOnly(true)
-
-					count := 0
-					err := datastore.Run(c, q, func(k *datastore.Key, _ datastore.CursorCB) error {
-						count += 1
-						keys <- k
-						return nil
-					})
-
-					keys <- nil // put "we are done" signal
-					if err == nil {
-						logging.Infof(c, "Found %d %q entities to be deleted", count, kind)
-					} else {
-						logging.WithError(err).Errorf(c, "Found %d %q entities and then failed", count, kind)
-					}
-					return err
-				}
-			}
-
-			// Delete keys in batches. Stop when we get all "we are done" signals from
-			// all len(kindsToDelete) queries. Carry on on errors, to make sure to
-			// drain the channel completely (otherwise enqueue ops will get blocked).
-			tasks <- func() error {
-				var batch []*datastore.Key
-				var errs errors.MultiError
-
-				// flush deletes all keys recorded in 'batch'.
-				flush := func() {
-					logging.Infof(c, "Deleting %d entities...", len(batch))
-					if err := datastore.Delete(c, batch); err != nil {
-						logging.WithError(err).Errorf(c, "Failed to delete %d entities", len(batch))
-						errs = append(errs, err)
-					}
-					batch = batch[:0]
-				}
-
-				stillRunning := len(kindsToDelete)
-				for k := range keys {
-					if k != nil {
-						if batch = append(batch, k); len(batch) >= deletionBatchSize {
-							flush()
-						}
-						continue
-					}
-
-					// Got "we are done" signal from some query.
-					if stillRunning -= 1; stillRunning == 0 {
-						break // all queries are done
-					}
-				}
-
-				// Flush the last incomplete batch.
-				if len(batch) > 0 {
-					flush()
-				}
-				if len(errs) != 0 {
-					return errs
-				}
-				return nil
-			}
+		err := deleteEntityKinds(c, pkg, []string{
+			"InstanceTag",
+			"PackageRef",
+			"ProcessingResult",
+			"PackageInstance",
 		})
 		if err != nil {
-			return transient.Tag.Apply(err)
+			return err
 		}
-
-		// Finally, delete the package entity itself. Its absence is a marker that
-		// the package is completely gone (as checked in CheckPackageExists).
 		if err := datastore.Delete(c, PackageKey(c, pkg)); err != nil {
 			return transient.Tag.Apply(err)
 		}
@@ -145,4 +80,87 @@ func DeletePackage(c context.Context, pkg string) error {
 			Package: pkg,
 		})
 	})
+}
+
+var (
+	// Number of keys to delete at once in deleteKinds. Replaced in tests.
+	deletionBatchSize = 256
+)
+
+// deleteEntityKinds deletes all entities of given kinds under given root.
+func deleteEntityKinds(c context.Context, pkg string, kindsToDelete []string) error {
+	logging.Infof(c, "Deleting %s...", strings.Join(kindsToDelete, ", "))
+	return transient.Tag.Apply(parallel.WorkPool(len(kindsToDelete)+1, func(tasks chan<- func() error) {
+		// A channel that receives keys to delete. Set some arbitrary buffer size to
+		// parallelize work a bit better.
+		keys := make(chan *datastore.Key, 1024)
+
+		// Launch queries that fetch keys to delete, and feed them to the channel.
+		// Each query enqueues nil when it is done to let the consumer know.
+		for _, kind := range kindsToDelete {
+			kind := kind
+			tasks <- func() error {
+				q := datastore.NewQuery(kind).
+					Ancestor(PackageKey(c, pkg)).
+					KeysOnly(true)
+
+				count := 0
+				err := datastore.Run(c, q, func(k *datastore.Key, _ datastore.CursorCB) error {
+					count += 1
+					keys <- k
+					return nil
+				})
+
+				keys <- nil // put "we are done" signal
+				if err == nil {
+					logging.Infof(c, "Found %d %q entities to be deleted", count, kind)
+				} else {
+					logging.WithError(err).Errorf(c, "Found %d %q entities and then failed", count, kind)
+				}
+				return err
+			}
+		}
+
+		// Delete keys in batches. Stop when we get all "we are done" signals from
+		// all len(kindsToDelete) queries. Carry on on errors, to make sure to
+		// drain the channel completely (otherwise enqueue ops will get blocked).
+		tasks <- func() error {
+			var batch []*datastore.Key
+			var errs errors.MultiError
+
+			// flush deletes all keys recorded in 'batch'.
+			flush := func() {
+				logging.Infof(c, "Deleting %d entities...", len(batch))
+				if err := datastore.Delete(c, batch); err != nil {
+					logging.WithError(err).Errorf(c, "Failed to delete %d entities", len(batch))
+					errs = append(errs, err)
+				}
+				batch = batch[:0]
+			}
+
+			stillRunning := len(kindsToDelete)
+			for k := range keys {
+				if k != nil {
+					if batch = append(batch, k); len(batch) >= deletionBatchSize {
+						flush()
+					}
+					continue
+				}
+
+				// Got "we are done" signal from some query.
+				if stillRunning -= 1; stillRunning == 0 {
+					break // all queries are done
+				}
+			}
+
+			// Flush the last incomplete batch.
+			if len(batch) > 0 {
+				flush()
+			}
+			if len(errs) != 0 {
+				return errs
+			}
+			return nil
+		}
+	}))
 }
