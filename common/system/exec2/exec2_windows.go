3dbf295f39d825f3171c679d4ac00e67a80f1865
Takuto Ikuta <tikuta@chromium.org>
2019-06-04 14:24:08 +0900

    [exec2] support windows
    This is basic support for windows.
    
    On windows, we need to use job object.
    But go's standard library does not allow to get thread handle via
    exec.Cmd. So this CL uses CreateProcess instead of (*exec.Cmd).Start().
    
    Most of code in startprocess_windows.go is copied from
    https://github.com/golang/go/blob/3a1b4e75f8b6c1b57db73bccf7ca871bf1a97ca9/src/syscall/exec_windows.go
    with a few modifications.
    
    I will do some TODO in following CLs.
    
    Bug: 962804
    Change-Id: I7c66be6cb062bb267f974d73f15442fdce4e6c7b

--- common/system/exec2/exec2_windows.go
+++ common/system/exec2/exec2_windows.go
@@ -14,11 +14,149 @@
 
 package exec2
 
+import (
+	"os"
+	"sync"
+	"syscall"
+
+	"go.chromium.org/luci/common/errors"
+	"golang.org/x/sys/windows"
+)
+
 // TODO(tikuta): support windows.
 
+type attr struct {
+	job      windows.Handle
+	process  windows.Handle
+	exitCode int
+}
+
 func (c *Cmd) setupCmd() {
+	c.Cmd.SysProcAttr = &syscall.SysProcAttr{
+		CreationFlags: windows.CREATE_SUSPENDED,
+	}
+}
+
+func createJobObject() (windows.Handle, error) {
+	job, err := windows.CreateJobObject(nil, nil)
+	if err != nil {
+		return 0, errors.Annotate(err, "failed to create job object").Err()
+	}
+
+	// TODO(tikuta): use SetInformationJobObject
+
+	return job, nil
+}
+
+var devnull *os.File
+var devnullonce sync.Once
+
+func (c *Cmd) start() error {
+	// TODO(tikuta): use os/exec package if https://github.com/golang/go/issues/32404 is fixed.
+	sysattr := &syscall.ProcAttr{
+		Dir: c.Dir,
+		Env: c.Env,
+		Sys: c.SysProcAttr,
+	}
+
+	if sysattr.Env == nil {
+		sysattr.Env = os.Environ()
+	}
+
+	devnullonce.Do(func() {
+		devnull, _ = os.Open(os.DevNull)
+	})
+
+	sysattr.Files = append(sysattr.Files, devnull.Fd(), os.Stdout.Fd(), os.Stderr.Fd())
+
+	lp, err := lookExtensions(c.Path, c.Dir)
+	if err != nil {
+		return errors.Annotate(err, "failed to call lookExtensions").Err()
+	}
+	c.Path = lp
+	process, thread, err := startProcess(c.Path, c.Args, sysattr)
+	if err != nil {
+		return errors.Annotate(err, "failed to call startProcess").Err()
+	}
+	defer windows.CloseHandle(thread)
+	c.attr.process = process
+
+	success := false
+
+	defer func() {
+		if !success {
+			c.kill()
+			c.wait()
+		}
+	}()
+
+	// need for ResumeThread
+	// https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-resumethread
+	const PROCESS_SUSPEND_RESUME = 0x0800
+
+	// need for AssignProcessToJobObject
+	// https://docs.microsoft.com/en-us/windows/desktop/api/jobapi2/nf-jobapi2-assignprocesstojobobject
+	const PROCESS_SET_QUOTA = 0x0100
+
+	job, err := createJobObject()
+	if err != nil {
+		return errors.Annotate(err, "failed to create job object").Err()
+	}
+
+	defer func() {
+		if !success {
+			windows.CloseHandle(job)
+		}
+	}()
+	c.attr.job = job
+
+	if err := windows.AssignProcessToJobObject(job, process); err != nil {
+		return errors.Annotate(err, "failed to assing process to job object").Err()
+	}
+
+	if _, err := windows.ResumeThread(thread); err != nil {
+		return errors.Annotate(err, "failed to resume thread").Err()
+	}
+
+	success = true
+	return nil
 }
 
 func (c *Cmd) terminate() error {
+	// TODO(tikuta): use GenerateConsoleCtrlEvent
+	return c.kill()
+}
+
+func (c *Cmd) killprocess() error {
+	return windows.TerminateProcess(c.attr.process, 1)
+}
+
+func (c *Cmd) wait() error {
+	e, err := windows.WaitForSingleObject(c.attr.process, windows.INFINITE)
+	if err != nil {
+		return errors.Annotate(err, "failed to call WaitForSingleObject").Err()
+	}
+
+	if e != windows.WAIT_OBJECT_0 {
+		return errors.Reason("unknown return value from WaitForSingleObject: %d", e).Err()
+	}
+
+	var ec uint32
+	if err := windows.GetExitCodeProcess(c.attr.process, &ec); err != nil {
+		return errors.Annotate(err, "failed to call GetExitCodeProcess").Err()
+	}
+	c.attr.exitCode = int(ec)
 	return nil
+}
+
+func (c *Cmd) kill() error {
+	if err := c.killprocess(); err != nil {
+		return errors.Annotate(err, "failed to call killprocess").Err()
+	}
+
+	return windows.TerminateJobObject(c.attr.job, 0)
+}
+
+func (c *Cmd) exitCode() int {
+	return c.attr.exitCode
 }
