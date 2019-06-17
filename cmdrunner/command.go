ee78b7ef5d506d83e8e0f5e89409c463bfe08022
Takuto Ikuta <tikuta@chromium.org>
2019-06-17 17:12:06 +0900

    [cmdrunner] add runCommand function
    This is re-implementation of run_command function.
    https://cs.chromium.org/chromium/infra/luci/client/run_isolated.py?l=425&rcl=4cc258d5e00dd678caa94cef53a01f1e13c31f58
    
    I will address TODO in later CL or patchset.
    
    Bug: 962804
    Change-Id: Ib0a937d185a109432bab48c4e0ac5fd1c52cfa15

--- cmdrunner/command.go
+++ cmdrunner/command.go
@@ -12,18 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package isolated
+package cmdrunner
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
+	"time"
 
+	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/system/environ"
+	"go.chromium.org/luci/common/system/exec2"
 )
 
 const (
@@ -45,7 +48,7 @@ func replaceParameters(ctx context.Context, arg, outDir, botFile string) (string
 
 	if strings.Contains(arg, isolatedOutdirParameter) {
 		if outDir == "" {
-			return "", errors.New("output directory is requested in command or env var, but not provided; please specify one")
+			return "", errors.Reason("output directory is requested in command or env var, but not provided; please specify one").Err()
 		}
 		arg = strings.Replace(arg, isolatedOutdirParameter, outDir, -1)
 		replaceSlash = true
@@ -158,4 +161,71 @@ func getCommandEnv(ctx context.Context, tmpDir string, cipdInfo *cipdInfo, runDi
 	}
 
 	return out, nil
+}
+
+// Run runs the command.
+func Run(ctx context.Context, command []string, cwd string, env environ.Env, hardTimeout time.Duration, gracePeriod time.Duration, lowerPriority bool, containment bool) (int, bool, error) {
+	logging.Infof(ctx, "runCommand(%s, %s, %s, %s, %s, %s, %s)", command, cwd, env, hardTimeout, gracePeriod, lowerPriority, containment)
+	exitCode := 1
+	hadHardTimeout := false
+
+	cmd := exec2.CommandContext(ctx, command[0], command[1:]...)
+	cmd.Env = env.Sorted()
+	cmd.Dir = cwd
+	// TODO(tikuta): handle STOP_SIGNALS
+
+	failure := func(err error, hadHardTimeout bool) (int, bool, error) {
+		fmt.Fprintf(os.Stderr, "failed to run command %s: %v\n", command, err)
+		fmt.Fprint(os.Stderr, `<The executable does not exist or a dependent library is missing>
+<Check for missing .so/.dll in the .isolate or GN file>
+`)
+		if _, ok := environ.System().Get("SWARMING_TASK_ID"); ok {
+			fmt.Fprint(os.Stderr, `<See the task's page for commands to help diagnose this issue by reproducing the task locally>
+`)
+		}
+		return 1, hadHardTimeout, err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return 0, false, errors.Annotate(err, "failed to start command").Err()
+	}
+
+	err := cmd.Wait(hardTimeout)
+	if err != nil && err != exec2.ErrTimeout {
+		return failure(err, hadHardTimeout)
+	}
+
+	if err == nil {
+		exitCode = cmd.ProcessState.ExitCode()
+		return exitCode, false, nil
+	}
+
+	if err != exec2.ErrTimeout {
+		return failure(err, hadHardTimeout)
+	}
+
+	hadHardTimeout = true
+	if err := cmd.Terminate(); err != nil {
+		logging.Warningf(ctx, "failed to call Terminate: %v", err)
+	}
+
+	err = cmd.Wait(gracePeriod)
+	if err == nil {
+		// Process exited fast enough after a nudge. Happy path.
+		exitCode = cmd.ProcessState.ExitCode()
+		return exitCode, hadHardTimeout, nil
+	}
+
+	if err != exec2.ErrTimeout {
+		// Process didn't exit in time after a nudge, try to kill it.
+		cmd.Kill()
+		return failure(err, hadHardTimeout)
+	}
+
+	if err := cmd.Kill(); err != nil {
+		return failure(err, hadHardTimeout)
+	}
+
+	exitCode = cmd.ProcessState.ExitCode()
+	return exitCode, hadHardTimeout, nil
 }
