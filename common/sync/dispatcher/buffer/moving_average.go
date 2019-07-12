7ebfc161f7c3176256d8db507bb468d2ca672638
Robert Iannucci <iannucci@chromium.org>
2019-07-12 01:31:52 +0000

    [sync/dispatcher/buffer] Add moving average implementation.
    R=tandrii@chromium.org
    
    Change-Id: If51094baa3b6e7c06471bc0696af775014feb216
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1697422
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>

---
+++ common/sync/dispatcher/buffer/moving_average.go
@@ -0,0 +1,63 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buffer
+
+import (
+	"errors"
+)
+
+// movingAverage tracks a moving average of integers.
+//
+// It is not goroutine-safe.
+type movingAverage struct {
+	values []int // used as a circular buffer
+	curIdx int
+	curSum int64
+}
+
+// newMovingAverage makes a new movingAverage
+//
+// Args:
+//   window - The number of values to track.; if this is <= 0, panics.
+//   seed - A seed value.
+func newMovingAverage(window, seed int) *movingAverage {
+	if window <= 0 {
+		panic(errors.New("window must be positive"))
+	}
+
+	values := make([]int, window)
+	curSum := int64(0)
+	for i := range values {
+		values[i] = seed
+		curSum += int64(seed)
+	}
+
+	return &movingAverage{values, 0, curSum}
+}
+
+// get retrieves the current average value rounded up to the next integer value.
+func (m *movingAverage) get() int {
+	numValues := int64(len(m.values))
+	// Do this to round up to the next integer value.
+	return int((m.curSum + numValues - 1) / numValues)
+}
+
+// adds a new value to track.
+func (m *movingAverage) record(value int) {
+	old := m.values[m.curIdx]
+	m.values[m.curIdx] = value
+	m.curSum += int64(value - old)
+	m.curIdx = (m.curIdx + 1) % len(m.values)
+}
