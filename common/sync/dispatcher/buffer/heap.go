b085aa4855bc93f5a2e35641e63896f6da8491f9
Robert Iannucci <iannucci@chromium.org>
2019-07-24 17:37:54 -0700

    [dispatcher/buffer] Ensure its possible to do in-order transmission of data.
    This allows dispatcher.Channel to guarantee that it sends all accumulated
    data in FIFO order. Test case should ensure that this continues to be the
    case.
    
    R=tandrii@chromium.org
    
    Bug: 939368
    Change-Id: I92a42a886f7c62f4cd8de6e54e087d50fc92aaec

--- common/sync/dispatcher/buffer/heap.go
+++ common/sync/dispatcher/buffer/heap.go
@@ -18,23 +18,42 @@ import (
 	"container/heap"
 )
 
-// batchHeap maintains sorted order based on (nextSend, id)
-type batchHeap []*Batch
+// batchHeap maintains sorted order based on
+//   * (nextSend, id) if onlyID is false
+//   * (id,) if onlyID is true
+type batchHeap struct {
+	onlyID bool
+	data   []*Batch
+}
 
 var _ heap.Interface = &batchHeap{}
 
 // Implements sort.Interface.
-func (h batchHeap) Len() int           { return len(h) }
-func (h batchHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h batchHeap) Less(i, j int) bool { return h[i].Less(h[j]) }
+func (h batchHeap) Len() int      { return len(h.data) }
+func (h batchHeap) Swap(i, j int) { h.data[i], h.data[j] = h.data[j], h.data[i] }
+func (h batchHeap) Less(i, j int) bool {
+	a, b := h.data[i], h.data[j]
+
+	if !h.onlyID {
+		if a.nextSend.Before(b.nextSend) {
+			return true
+		}
+		if b.nextSend.Before(a.nextSend) {
+			return false
+		}
+	}
+	return a.id < b.id
+}
 
 // Implements heap.Interface.
-func (h *batchHeap) Push(itm interface{}) { *h = append(*h, itm.(*Batch)) }
+func (h *batchHeap) Push(itm interface{}) {
+	h.data = append(h.data, itm.(*Batch))
+}
 func (h *batchHeap) Pop() interface{} {
-	old := *h
+	old := h.data
 	n := len(old)
 	x := old[n-1]
-	*h = old[:n-1]
+	h.data = old[:n-1]
 	return x
 }
 
@@ -53,7 +72,7 @@ func (h *batchHeap) PopBatch() *Batch {
 // If not found, the returned idx will be -1
 func (h *batchHeap) Oldest() (oldest *Batch, idx int) {
 	idx = -1
-	for i, batch := range *h {
+	for i, batch := range h.data {
 		if oldest == nil || batch.id < oldest.id {
 			oldest, idx = batch, i
 		}
@@ -65,4 +84,12 @@ func (h *batchHeap) Oldest() (oldest *Batch, idx int) {
 // conjunction with OldestID.
 func (h *batchHeap) RemoveAt(idx int) {
 	heap.Remove(h, idx)
+}
+
+// Peek returns the lowest-ordered Batch, or nil if the heap is empty.
+func (h batchHeap) Peek() *Batch {
+	if len(h.data) == 0 {
+		return nil
+	}
+	return h.data[0]
 }
