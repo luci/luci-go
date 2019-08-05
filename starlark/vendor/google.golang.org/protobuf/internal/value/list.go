4f1994ad7336c6c117822fab46ba65a7564df3b5
Vadim Shtayura <vadimsh@chromium.org>
2019-08-05 11:31:03 -0700

    [starlarkproto] Vendor google.golang.org/protobuf for now.
    It is not officially released yet, its API may be unstable. Additionally it
    makes some interesting choices (like forced randomization of text proto
    encoding) that makes usage of text protos in lucicfg unnecessarily hard.
    
    See https://github.com/golang/protobuf/issues/920
    
    Hopefully, the randomization will become optional in the future (when more
    people discover it and find it inconvenient). If it is still there in the
    official release, we'll have to reimplement the stable text encoding
    ourselves.
    
    For now, our vendored google.golang.org/protobuf will be used as internal
    implementation detail of new starlarkprotov2 package that implements
    Starlark<->Proto interop in a "self contained" way.
    
    R=iannucci@chromium.org
    BUG=986972
    
    Change-Id: I5dc1d274f8a7bc9239e6cf6e2852f1c193fb0a39

---
+++ starlark/vendor/google.golang.org/protobuf/internal/value/list.go
@@ -0,0 +1,52 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package value
+
+import (
+	"reflect"
+
+	pref "google.golang.org/protobuf/reflect/protoreflect"
+)
+
+// ListOf returns a protoreflect.List view of p, which must be a *[]T.
+// If p is nil, this returns an empty, read-only list.
+func ListOf(p interface{}, c Converter) interface {
+	pref.List
+	Unwrapper
+} {
+	// TODO: Validate that p is a *[]T?
+	rv := reflect.ValueOf(p)
+	return &listReflect{rv, c}
+}
+
+type listReflect struct {
+	v    reflect.Value // *[]T
+	conv Converter
+}
+
+func (ls *listReflect) Len() int {
+	if ls.v.IsNil() {
+		return 0
+	}
+	return ls.v.Elem().Len()
+}
+func (ls *listReflect) Get(i int) pref.Value {
+	return ls.conv.PBValueOf(ls.v.Elem().Index(i))
+}
+func (ls *listReflect) Set(i int, v pref.Value) {
+	ls.v.Elem().Index(i).Set(ls.conv.GoValueOf(v))
+}
+func (ls *listReflect) Append(v pref.Value) {
+	ls.v.Elem().Set(reflect.Append(ls.v.Elem(), ls.conv.GoValueOf(v)))
+}
+func (ls *listReflect) Truncate(i int) {
+	ls.v.Elem().Set(ls.v.Elem().Slice(0, i))
+}
+func (ls *listReflect) NewMessage() pref.Message {
+	return ls.conv.NewMessage()
+}
+func (ls *listReflect) ProtoUnwrap() interface{} {
+	return ls.v.Interface()
+}
