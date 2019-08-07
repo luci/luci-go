a7046daf79211fe8e623f0923bbd08d9a69069d5
Vadim Shtayura <vadimsh@chromium.org>
2019-08-07 15:38:07 -0700

    [starlarkproto] Implement v2 via google.golang.org/protobuf.
    This is a fully dynamic implementation based on protobuf reflection (not Go
    reflection). It doesn't rely on proto type information embedded into the go
    binary and instead loads types from external descriptors (prepared by 'protoc').
    
    It is a completely standalone (re)implementation of starlarkproto package,
    which allows both of them to be used simultaneously during the migration period.
    Note that there's no interoperatability between them (starlark values produced
    by starlarkproto package aren't recognized by starlarkprotov2 and vice versa).
    
    Semantically it is almost identical to starlarkproto (as confirmed by tests).
    Differences are:
      * Message types are instantiated through a `proto.loader()` instance instead
        of being imported from an ether. This allows to populate the loader with
        descriptors prior to starting using protos. This also allows to have
        multiple loaders in the same process (using, perhaps, different versions
        of protos).
      * Text marshaller appends\removes trailing '\n' somewhat differently.
      * Bytes fields are represented as str, not as []uint8{...}.
      * proto.to_jsonpb doesn't have 'emit_defaults' kwarg (it is always False).
      * Better support for proto2 messages.
    
    R=iannucci@chromium.org
    BUG=986972
    
    Change-Id: Ice38752e82a1a4833acf8552d4a535803cd75066

---
+++ starlark/starlarkprotov2/loader.go
@@ -0,0 +1,328 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package starlarkprotov2
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"sync/atomic"
+
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/reflect/protoregistry"
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+// Loader can instantiate Starlark values that correspond to proto messages.
+//
+// Holds a pool of descriptors that describe all available proto types.
+//
+// Use AddDescriptor to seed it. Once seeded, use Module to get a Starlark
+// module with symbols defined in some `*.proto` file.
+//
+// Loader is also a Starlark value itself, with the following methods:
+//   * add_descriptor_set(bytes) - see AddDescriptorSet.
+//   * module(path) - see Module.
+//
+// Can be used concurrently.
+type Loader struct {
+	m sync.RWMutex
+
+	files *protoregistry.Files
+	types *protoregistry.Types
+
+	mtypes  map[protoreflect.MessageDescriptor]*MessageType
+	modules map[string]*starlarkstruct.Module // *.proto file => its top-level symbols
+
+	hash uint32 // unique within the process value, used by Hash()
+}
+
+// loaderHash is used to give each instance of *Loader its own unique hash
+// value for Hash() method.
+//
+// We could probably use a pointer address instead, but it would have resulted
+// in unsafe code.
+var loaderHash uint32 = 1000
+
+// NewLoader instantiates a new loader with empty proto registry.
+func NewLoader() *Loader {
+	return &Loader{
+		files:   protoregistry.NewFiles(),
+		types:   protoregistry.NewTypes(),
+		mtypes:  make(map[protoreflect.MessageDescriptor]*MessageType, 0),
+		modules: make(map[string]*starlarkstruct.Module, 0),
+		hash:    atomic.AddUint32(&loaderHash, 1),
+	}
+}
+
+// AddDescriptorSet ingests all descriptors from a serialized FileDescriptorSet.
+//
+// This set is the result of running "protoc --descriptor_set_out ...".
+//
+// There should be no unresolvable references: either all referenced descriptors
+// are in the added set, or they were added in some previous set.
+//
+// Additionally there should be no redeclarations. If some file and some type
+// were declared previously, it can't be redeclared again.
+func (l *Loader) AddDescriptorSet(raw []byte) error {
+	fds := &descriptorpb.FileDescriptorSet{}
+	if err := proto.Unmarshal(raw, fds); err != nil {
+		return fmt.Errorf("unmarshaling FileDescriptorSet: %s", err)
+	}
+
+	l.m.Lock()
+	defer l.m.Unlock()
+
+	for _, fd := range fds.File {
+		// Load the file descriptor resolving all references through 'res' which
+		// will capture unresolved references.
+		res := &resolver{r: l.files}
+		f, err := protodesc.NewFile(fd, res)
+		if err != nil {
+			return fmt.Errorf("resolving imports in %s: %s", fd.GetName(), err)
+		}
+
+		// Note: per comments in protodesc/desc.go, there would be an option to tell
+		// protodesc.NewFile to make this check natively.
+		switch {
+		case len(res.files) != 0:
+			return fmt.Errorf(
+				"compiled proto file %s refers to undefined files: %s",
+				fd.GetName(), strings.Join(res.files, ", "))
+		case len(res.descs) != 0:
+			return fmt.Errorf(
+				"compiled proto file %s refers to undefined descriptors: %s",
+				fd.GetName(), strings.Join(res.descs, ", "))
+		}
+
+		if err := l.files.Register(f); err != nil {
+			return fmt.Errorf("registering %s: %s", fd.GetName(), err)
+		}
+	}
+
+	// TODO(vadimsh): Populate l.types somehow. It is used by encoders/decoders
+	// to handle google.protobuf.Any fields (which we currently do not support).
+
+	return nil
+}
+
+// resolver wraps protodesc.Resolver by capturing unresolved references.
+type resolver struct {
+	r protodesc.Resolver
+
+	files []string // unresolvable files
+	descs []string // unresolvable descriptors
+}
+
+func (r *resolver) FindFileByPath(p string) (protoreflect.FileDescriptor, error) {
+	d, err := r.r.FindFileByPath(p)
+	if err == protoregistry.NotFound {
+		r.files = append(r.files, p)
+	}
+	return d, err
+}
+
+func (r *resolver) FindDescriptorByName(n protoreflect.FullName) (protoreflect.Descriptor, error) {
+	d, err := r.r.FindDescriptorByName(n)
+	if err == protoregistry.NotFound {
+		r.descs = append(r.descs, string(n))
+	}
+	return d, err
+}
+
+// Module returns a module with top-level definitions from some *.proto file.
+//
+// The descriptor of this proto file should be registered already via
+// AddDescriptorSet. 'path' here is matched to what's in the descriptor, which
+// is a path to *.proto EXACTLY as it was given to 'protoc'.
+func (l *Loader) Module(path string) (*starlarkstruct.Module, error) {
+	// Lookup in the cache under the reader lock.
+	mod, desc, err := func() (*starlarkstruct.Module, protoreflect.FileDescriptor, error) {
+		l.m.RLock()
+		defer l.m.RUnlock()
+		if mod := l.modules[path]; mod != nil {
+			return mod, nil, nil
+		}
+		desc, err := l.files.FindFileByPath(path)
+		if err != nil {
+			return nil, nil, fmt.Errorf("loading %s: %s", path, err)
+		}
+		return nil, desc, nil
+	}()
+	if mod != nil || err != nil {
+		return mod, err
+	}
+
+	l.m.Lock()
+	defer l.m.Unlock()
+
+	// Populate the module dict with top-level symbols in the file.
+	mod = &starlarkstruct.Module{
+		Name:    string(desc.Package()),
+		Members: starlark.StringDict{},
+	}
+	l.injectMessageTypes(mod.Members, desc.Messages())
+	l.injectEnumValues(mod.Members, desc.Enums())
+
+	l.modules[path] = mod
+	return mod, nil
+}
+
+// MessageType instances new (or returns existing) MessageType.
+func (l *Loader) MessageType(desc protoreflect.MessageDescriptor) *MessageType {
+	l.m.RLock()
+	mt := l.mtypes[desc]
+	l.m.RUnlock()
+	if mt != nil {
+		return mt
+	}
+
+	l.m.Lock()
+	defer l.m.Unlock()
+	return l.initMessageType(desc)
+}
+
+// initMessageType creates *MessageType if it didn't exist before.
+//
+// Called with l.m locked.
+func (l *Loader) initMessageType(desc protoreflect.MessageDescriptor) *MessageType {
+	if typ := l.mtypes[desc]; typ != nil {
+		return typ
+	}
+
+	typ := &MessageType{
+		loader: l,
+		desc:   desc,
+		attrs:  starlark.StringDict{},
+	}
+	typ.init()
+
+	// Constructor function that uses the type to instantiate the messages.
+	typ.Builtin = starlark.NewBuiltin(typ.Type(), func(_ *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		if len(args) != 0 {
+			return nil, fmt.Errorf("proto message constructors accept only keyword arguments")
+		}
+		msg := typ.NewMessage()
+		for _, kv := range kwargs {
+			if err := msg.SetField(string(kv[0].(starlark.String)), kv[1]); err != nil {
+				return nil, err
+			}
+		}
+		return msg, nil
+	})
+
+	// Inject nested symbols.
+	l.injectMessageTypes(typ.attrs, desc.Messages())
+	l.injectEnumValues(typ.attrs, desc.Enums())
+
+	l.mtypes[desc] = typ
+	return typ
+}
+
+// injectMessageTypes instantiates constructors for messages in 'msgs' and adds
+// them to the dict 'd'.
+//
+// Called with l.m locked.
+func (l *Loader) injectMessageTypes(d starlark.StringDict, msgs protoreflect.MessageDescriptors) {
+	for i := 0; i < msgs.Len(); i++ {
+		desc := msgs.Get(i)
+		// map<...> fields are represented by magical map message types. We do not
+		// expose them on Starlark level and represent maps as dicts instead.
+		if !desc.IsMapEntry() {
+			d[string(desc.Name())] = l.initMessageType(desc)
+		}
+	}
+}
+
+// injectEnumValues takes enum constants defined in 'enums' and puts them
+// directly into the given dict as integers.
+//
+// Called with l.m locked.
+func (l *Loader) injectEnumValues(d starlark.StringDict, enums protoreflect.EnumDescriptors) {
+	for i := 0; i < enums.Len(); i++ {
+		vals := enums.Get(i).Values()
+		for j := 0; j < vals.Len(); j++ {
+			val := vals.Get(j)
+			d[string(val.Name())] = starlark.MakeInt(int(val.Number()))
+		}
+	}
+}
+
+// Implementation of starlark.Value and starlark.HasAttrs.
+
+// String returns str(...) representation of the loader.
+func (l *Loader) String() string {
+	return fmt.Sprintf("proto.loader(%d)", l.hash)
+}
+
+// Type return "proto.loader".
+func (l *Loader) Type() string {
+	return "proto.loader"
+}
+
+// Freeze is noop.
+func (l *Loader) Freeze() {}
+
+// Truth returns true.
+func (l *Loader) Truth() starlark.Bool {
+	return starlark.True
+}
+
+// Hash is important if the loader is used as a dict key.
+func (l *Loader) Hash() (uint32, error) {
+	return l.hash, nil
+}
+
+// AtrrNames is available attributes.
+func (l *Loader) AttrNames() []string {
+	return []string{"add_descriptor_set", "module"}
+}
+
+// Attr returns an attribute given its name or nil if not present.
+func (l *Loader) Attr(name string) (starlark.Value, error) {
+	switch name {
+	case "add_descriptor_set":
+		return addDescSetBuiltin.BindReceiver(l), nil
+	case "module":
+		return moduleBuiltin.BindReceiver(l), nil
+	default:
+		return nil, nil
+	}
+}
+
+// Shims for calling Loader methods from Starlark.
+
+var addDescSetBuiltin = starlark.NewBuiltin("add_descriptor_set", func(_ *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var raw string
+	if err := starlark.UnpackPositionalArgs("add_descriptor_set", args, kwargs, 1, &raw); err != nil {
+		return nil, err
+	}
+	l := b.Receiver().(*Loader)
+	return starlark.None, l.AddDescriptorSet([]byte(raw))
+})
+
+var moduleBuiltin = starlark.NewBuiltin("module", func(_ *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var path string
+	if err := starlark.UnpackPositionalArgs("module", args, kwargs, 1, &path); err != nil {
+		return nil, err
+	}
+	l := b.Receiver().(*Loader)
+	return l.Module(path)
+})
---
+++ starlark/starlarkprotov2/testdata/imported_msg.star
@@ -0,0 +1,39 @@
+# Copyright 2018 The LUCI Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+l = proto.new_loader(read('./testprotos/all.pb'))
+testprotos = l.module('go.chromium.org/luci/starlark/starlarkprotov2/testprotos/test.proto')
+
+m = testprotos.RefsOtherProtos()
+
+# Indirectly using messages imported from elsewhere works.
+m.another_msg.i = 123
+m.ts.seconds = 456
+assert.eq(proto.to_textpb(m), """another_msg: <
+  i: 123
+>
+ts: <
+  seconds: 456
+>
+""")
+
+# To instantiate them directly, need to load the corresponding proto module
+# first. To avoid clashing with already imported 'testprotos' symbol, import it
+# under a different name.
+another_pb = l.module('go.chromium.org/luci/starlark/starlarkprotov2/testprotos/another.proto')
+assert.eq(proto.to_textpb(another_pb.AnotherMessage(i=123)), "i: 123\n")
+
+# Can import modules with dotted names too.
+google_pb = l.module('google/protobuf/timestamp.proto')
+assert.eq(proto.to_textpb(google_pb.Timestamp(seconds=456)), "seconds: 456\n")
