bd0cac02da7f1949b7a1f27e6d26a94ef6c6ef1f
Vadim Shtayura <vadimsh@chromium.org>
2019-08-16 14:59:26 -0700

    [lucicfg] Compare textproto files semantically in `validate`.
    This means we ignore any formatting differences, comments, etc. when comparing
    generated configs to what's on the disk right now. This will be useful to avoid
    noisy diffs when we release a new lucicfg with new protobuf library which uses
    slightly different format of textproto.
    
    Note that we don't transform the structure of proto messages in any way, e.g. we
    don't try to unroll Buildbucket mixins or anything like that.
    
    R=iannucci@chromium.org
    BUG=986972
    
    Change-Id: Ic2f48197066230b26c39d2aff8e03203b4a482ad

--- lucicfg/cli/cmds/validate/validate.go
+++ lucicfg/cli/cmds/validate/validate.go
@@ -43,11 +43,13 @@ flag, it is required in this mode.
 
 If the first positional argument is a Starlark file, it is interpreted (as with
 'generate' subcommand) and the resulting generated configs are compared to
-what's already on disk in -config-dir directory. If they are different, the
-subcommand exits with non-zero exit code (indicating files on disk are stale).
-Otherwise the configs are sent to LUCI Config service for validation.
-Partitioning into config sets is specified in the Starlark code in this case,
--config-set flag is rejected if given.
+what's already on disk in -config-dir directory using semantic comparison (i.e.
+config files on disk are deserialized and compared to the generated files as
+objects rather than as byte blobs). If they are different, the subcommand exits
+with non-zero exit code (indicating the files on disk are stale). Otherwise the
+configs are sent to LUCI Config service for validation. Partitioning into config
+sets is specified in the Starlark code in this case, -config-set flag is
+rejected if given.
 
 When interpreting Starlark script, flags like -config-dir and -fail-on-warnings
 work as overrides for values declared in the script via lucicfg.config(...)
@@ -166,8 +168,9 @@ func (vr *validateRun) validateGenerated(ctx context.Context, path string) (*val
 				result.Stale = append(result.Stale, f)
 			}
 		}
-		// Find files that are newer in the output or do not exist on disk.
-		changed, _, err := output.Compare(meta.ConfigDir)
+		// Find files that are newer in the output or do not exist on disk. Do
+		// semantic comparison for protos.
+		changed, _, err := output.Compare(meta.ConfigDir, true)
 		if err != nil {
 			return result, err
 		}
