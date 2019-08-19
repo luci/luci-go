ff4ecebcf946d51b2da4bb8ce0dfb9bbedfbeb4c
Vadim Shtayura <vadimsh@chromium.org>
2019-08-19 23:18:02 +0000

    [lucicfg] Use proto options to specify documentation URL.
    This is the correct way to associate custom metadata with proto files. Using it
    will allow to somewhat simplify Starlark API for registering proto descriptors.
    
    R=iannucci@chromium.org
    BUG=986972
    
    Change-Id: I4e6db07000f7fd7f7f38180779b5ba8bce702e6c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1761507
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- common/proto/doc.go
+++ common/proto/doc.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The LUCI Authors.
+// Copyright 2019 The LUCI Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,16 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-syntax = "proto3";
+// Package proto contains protobuf-related utilities.
+package proto
 
-package testproto;
-
-import "go.chromium.org/luci/common/proto/options.proto";
-
-option (luci.file_metadata) = {
-  doc_url: "https://example.com/proto-doc";
-};
-
-message Msg {
-  int64 i = 1;
-}
+//go:generate cproto
