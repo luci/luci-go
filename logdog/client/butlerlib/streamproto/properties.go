da0f66e2613bccd7ed194705bedf16d024e61978
Robert Iannucci <iannucci@chromium.org>
2019-08-22 16:01:32 -0700

    [logdog/butlerlib] Make client typesafe and correctly work for subrocs.
    This removes the weird `Stream` union type in favor of `io.WriteCloser`
    and its alter-ego `DatagramStream`. This also removes the need for the
    client to handle the odd `streamproto.Flags` type, in favor of
    well-documented Option functions.
    
    Adds a single well-known (and tested) in-memory `fake` protocol, as well
    as a `null` protocol. There are now no external implementations of Client
    (or its new `dialer` type).
    
    As a bonus, this adds full testing for all client protocols, on all
    platforms, including ensuring that `ForProcess` file descriptors actually
    are the correct types and correctly work for subprocesses.
    
    R=vadimsh@chromium.org
    
    Change-Id: I1eeb5e775f4763fc00a3ed9031c4ff75fef92a86

--- logdog/client/butlerlib/streamproto/properties.go
+++ logdog/client/butlerlib/streamproto/properties.go
@@ -15,9 +15,14 @@
 package streamproto
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"time"
 
 	"go.chromium.org/luci/common/clock/clockflag"
+	"go.chromium.org/luci/common/data/recordio"
+	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/proto/google"
 	"go.chromium.org/luci/logdog/api/logpb"
 	"go.chromium.org/luci/logdog/common/types"
@@ -50,4 +55,40 @@ func (f *Flags) Descriptor() *logpb.LogStreamDescriptor {
 		Timestamp:   google.NewTimestamp(time.Time(f.Timestamp)),
 		Tags:        f.Tags,
 	}
+}
+
+// WriteHandshake writes the butler protocol header handshake on the given
+// Writer.
+func (f *Flags) WriteHandshake(w io.Writer) error {
+	data, err := json.Marshal(f)
+	if err != nil {
+		return errors.Annotate(err, "marshaling flags").Err()
+	}
+	if _, err := w.Write(ProtocolFrameHeaderMagic); err != nil {
+		return errors.Annotate(err, "writing magic number").Err()
+	}
+	if _, err := recordio.WriteFrame(w, data); err != nil {
+		return errors.Annotate(err, "writing properties").Err()
+	}
+	return nil
+}
+
+// FromHandshake reads the butler protocol header handshake from the given
+// Reader.
+func (f *Flags) FromHandshake(r io.Reader) error {
+	header := make([]byte, len(ProtocolFrameHeaderMagic))
+	_, err := r.Read(header)
+	if err != nil {
+		return errors.Annotate(err, "reading magic number").Err()
+	}
+	if !bytes.Equal(header, ProtocolFrameHeaderMagic) {
+		return errors.Reason(
+			"magic number mismatch: got(%q) expected(%q)",
+			header, ProtocolFrameHeaderMagic).Err()
+	}
+	flagData, err := recordio.NewReader(r, 1024*1024).ReadFrameAll()
+	if err != nil {
+		return errors.Annotate(err, "reading property frame").Err()
+	}
+	return errors.Annotate(json.Unmarshal(flagData, f), "parsing flag JSON").Err()
 }
