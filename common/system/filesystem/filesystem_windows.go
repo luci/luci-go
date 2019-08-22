89115ad81630387dbeda480715282427bf48307f
Takuto Ikuta <tikuta@chromium.org>
2019-08-22 18:29:05 +0900

    [filesystem] add enableSymlink
    This is Go version of
    https://cs.chromium.org/chromium/infra/luci/client/utils/file_path.py?l=358&rcl=9f4ccecdd7564371d941fe46ed8221227b886c4e
    
    This will be used for test in
    https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1763607
    
    Bug: 962804
    Change-Id: I40b9eb03ce36230d96ccc11b66b9b736f0283a58

--- common/system/filesystem/filesystem_windows.go
+++ common/system/filesystem/filesystem_windows.go
@@ -17,6 +17,10 @@ package filesystem
 import (
 	"os"
 	"syscall"
+
+	"golang.org/x/sys/windows"
+
+	"go.chromium.org/luci/common/errors"
 )
 
 func umask(mask int) int {
@@ -25,4 +29,58 @@ func umask(mask int) int {
 
 func addReadMode(mode os.FileMode) os.FileMode {
 	return mode | syscall.S_IRUSR
+}
+
+func getProcessToken() (windows.Token, error) {
+	var token windows.Token
+	process, err := windows.GetCurrentProcess()
+	if err != nil {
+		return token, errors.Annotate(err, "failed to get current process").Err()
+	}
+
+	if err := windows.OpenProcessToken(process, windows.TOKEN_ALL_ACCESS, &token); err != nil {
+		return token, errors.Annotate(err, "failed to open process token").Err()
+	}
+	return token, nil
+}
+
+func getLUID(name string) (windows.LUID, error) {
+	var luid windows.LUID
+	utf16, err := syscall.UTF16PtrFromString(name)
+	if err != nil {
+		return luid, errors.Annotate(err, `failed to convert "%s" to utf16`, name).Err()
+	}
+
+	if err := windows.LookupPrivilegeValue(nil, utf16, &luid); err != nil {
+		return luid, errors.Annotate(err, "failed to lookup privilege value for %s", name).Err()
+	}
+	return luid, nil
+}
+
+func enableSymlink() error {
+	luid, err := getLUID("SeCreateSymbolicLinkPrivilege")
+	if err != nil {
+		return errors.Annotate(err, "failed to get LUID for SeCreateSymbolicLinkPrivilege").Err()
+	}
+	privileges := windows.Tokenprivileges{
+		PrivilegeCount: 1,
+		Privileges: [1]windows.LUIDAndAttributes{
+			{
+				Luid:       luid,
+				Attributes: windows.SE_PRIVILEGE_ENABLED,
+			},
+		},
+	}
+
+	token, err := getProcessToken()
+	if err != nil {
+		return errors.Annotate(err, "failed to get process token").Err()
+	}
+	defer windows.CloseHandle(windows.Handle(token))
+
+	if err := windows.AdjustTokenPrivileges(token, false, &privileges, 0, nil, nil); err != nil {
+		return errors.Annotate(err, "failed to adjust token privileges").Err()
+	}
+
+	return nil
 }
