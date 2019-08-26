e8c8e405ac3aa58406619bf7b3275110f692642c
Vadim Shtayura <vadimsh@chromium.org>
2019-08-25 19:15:11 -0700

    [server] Add server.Main(...) function to reduce boilerplate.
    R=iannucci@chromium.org, jchinlee@chromium.org
    BUG=959427
    
    Change-Id: I647185b3d9d4c60f6bfeb02382f8af4b3f8e6e33

--- results/recorder/main.go
+++ results/recorder/main.go
@@ -15,41 +15,23 @@
 package main
 
 import (
-	"flag"
-
 	"go.chromium.org/luci/common/logging"
 
 	"go.chromium.org/luci/server"
 	"go.chromium.org/luci/server/router"
 
-	"go.chromium.org/luci/hardcoded/chromeinfra"
-
 	resultspb "go.chromium.org/luci/results/proto/v1"
 )
 
 func main() {
-	opts := server.Options{
-		ClientAuth: chromeinfra.DefaultAuthOptions(),
-	}
-	opts.Register(flag.CommandLine)
-	flag.Parse()
-
-	srv, err := server.New(opts)
-	if err != nil {
-		srv.Fatal(err)
-	}
-
-	srv.Routes.GET("/", router.MiddlewareChain{}, func(c *router.Context) {
-		logging.Debugf(c.Context, "DEV Hello debug world")
-		logging.Infof(c.Context, "DEV Hello info world")
-		logging.Warningf(c.Context, "DEV Hello warning world")
-		c.Writer.Write([]byte("DEV Hello, world"))
+	server.Main(nil, func(srv *server.Server) error {
+		srv.Routes.GET("/", router.MiddlewareChain{}, func(c *router.Context) {
+			logging.Debugf(c.Context, "DEV Hello debug world")
+			logging.Infof(c.Context, "DEV Hello info world")
+			logging.Warningf(c.Context, "DEV Hello warning world")
+			c.Writer.Write([]byte("DEV Hello, world"))
+		})
+		resultspb.RegisterRecorderServer(srv.PRPC, &RecorderServer{})
+		return nil
 	})
-
-	// Register pRPC services.
-	resultspb.RegisterRecorderServer(srv.PRPC, &RecorderServer{})
-
-	if err := srv.ListenAndServe(); err != nil {
-		srv.Fatal(err)
-	}
 }
