9ad6305e1424958356a0d68b1b8c0d20d4b5b921
Vadim Shtayura <vadimsh@chromium.org>
2019-08-26 19:22:57 +0000

    [rpcexplorer] Serve RPC Explorer static assets from Go.
    Hardcore path at "/rpcexplorer" for uniformity across services.
    
    R=iannucci@chromium.org
    
    Change-Id: I6d29de666db66b21852812adddcd5716e431f999
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1771002
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- web/gowrappers/rpcexplorer/rpcexplorer.go
+++ web/gowrappers/rpcexplorer/rpcexplorer.go
@@ -17,4 +17,38 @@
 // Linking to this package will add 7MB to your binary.
 package rpcexplorer
 
-// TODO(vadimsh): Implement public API.
+import (
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"go.chromium.org/luci/server/router"
+	"go.chromium.org/luci/web/gowrappers/rpcexplorer/internal"
+)
+
+// Install adds routes to serve RPCExplorer web app from "/rpcexplorer".
+func Install(r *router.Router) {
+	r.GET("/rpcexplorer", router.MiddlewareChain{}, func(c *router.Context) {
+		http.Redirect(c.Writer, c.Request, "/rpcexplorer/", http.StatusMovedPermanently)
+	})
+
+	r.GET("/rpcexplorer/*path", router.MiddlewareChain{}, func(c *router.Context) {
+		path := strings.TrimPrefix(c.Params.ByName("path"), "/")
+		if path == "" {
+			path = "index.html"
+		}
+
+		hash := internal.GetAssetSHA256(path)
+		if hash == nil {
+			http.Error(c.Writer, "404 page not found", http.StatusNotFound)
+			return
+		}
+
+		c.Writer.Header().Set("ETag", fmt.Sprintf("%q", hex.EncodeToString(hash)))
+		http.ServeContent(
+			c.Writer, c.Request, path, time.Time{},
+			strings.NewReader(internal.GetAssetString(path)))
+	})
+}
