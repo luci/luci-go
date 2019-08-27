9027fac0f623c9862f0a4f822a79b0db99e077ec
Takuto Ikuta <tikuta@chromium.org>
2019-08-27 14:31:25 +0900

    [cmdrunner] add linkOutputsToOutdir
    This is Go version of
    https://cs.chromium.org/chromium/infra/luci/client/run_isolated.py?l=519&rcl=9f4ccecdd7564371d941fe46ed8221227b886c4e
    
    Bug: 962804
    Change-Id: I3b3ab5124a3d9e2bdd667305f5ec7309acf54106

--- cmdrunner/command_test.go
+++ cmdrunner/command_test.go
@@ -23,6 +23,7 @@ import (
 	"time"
 
 	"go.chromium.org/luci/common/system/environ"
+	"go.chromium.org/luci/common/testing/testfs"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -168,4 +169,32 @@ func TestRun(t *testing.T) {
 
 		// TODO(tikuta): have test for error cases.
 	})
+}
+
+func TestLinkOutputsToOutdir(t *testing.T) {
+	t.Parallel()
+
+	Convey("LinkOutputsToOutdir", t, testfs.MustWithTempDir(t, "", func(dir string) {
+		rundir := filepath.Join(dir, "rundir")
+
+		So(testfs.Build(rundir, map[string]string{
+			"a/b":   "ab",
+			"a/c/d": "acd",
+			"e":     "e",
+		}), ShouldBeNil)
+
+		outdir := filepath.Join(dir, "outdir")
+		So(linkOutputsToOutdir(rundir, outdir, []string{
+			filepath.Join("a", "b"),
+			"e",
+			// do not link a/c/d here.
+		}), ShouldBeNil)
+
+		layout, err := testfs.Collect(outdir)
+		So(err, ShouldBeNil)
+		So(layout, ShouldResemble, map[string]string{
+			"a/b": "ab",
+			"e":   "e",
+		})
+	}))
 }
