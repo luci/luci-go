adb5928f7c4ec544cafae7799967f1142a1bf4b0
Vadim Shtayura <vadimsh@chromium.org>
2019-08-29 20:30:53 -0700

    [lucicfg] Add proto.default_loader(), expose built-in descriptor sets.
    This CL moves initialization of the proto loader into stdlib *.star code,
    allowing external packages to do roughly same.
    
    Additionally, misc proto descriptors (with messages not used by lucicfg itself)
    are now loaded lazily only if some load('@proto//...') reference can't be
    resolved. This allows to transition to external proto descriptors in a backward
    compatible way.
    
    R=iannucci@chromium.org
    BUG=986972
    
    Change-Id: Ic855b0a4bf60e4db96fbca64c6d8ba3b9f56f501

--- lucicfg/protos_test.go
+++ lucicfg/protos_test.go
@@ -16,30 +16,51 @@ package lucicfg
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
 
 	"go.starlark.net/starlark"
 
+	"go.chromium.org/luci/starlark/protohacks"
 	"go.chromium.org/luci/starlark/starlarkproto"
-
-	_ "go.chromium.org/luci/lucicfg/testproto"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-// Register protos used exclusively from tests.
-func init() {
-	miscProtos = append(miscProtos, "go.chromium.org/luci/lucicfg/testproto/test.proto")
-}
+// testProtoLoader is a proto.Loader populated with misc/support/test.proto
+// descriptor and its dependencies. Used by testMessage().
+var testProtoLoader *starlarkproto.Loader
 
-// testMessage returns new testproto.Msg as a Starlark value to be used from
-// tests.
-func testMessage(i int) *starlarkproto.Message {
-	testproto, _, err := protoLoader()("go.chromium.org/luci/lucicfg/testproto/test.proto")
+func init() {
+	// See testdata/gen.go for where this file is generated.
+	blob, err := ioutil.ReadFile("testdata/misc/support/test_descpb.bin")
 	if err != nil {
 		panic(err)
 	}
-	msgT, err := testproto["testproto"].(starlark.HasAttrs).Attr("Msg")
+	dspb, err := protohacks.UnmarshalFileDescriptorSet(blob)
+	if err != nil {
+		panic(err)
+	}
+	ds, err := starlarkproto.NewDescriptorSet("test", dspb.GetFile(), []*starlarkproto.DescriptorSet{
+		luciTypesDescSet, // for "go.chromium.org/luci/common/proto/options.proto"
+	})
+	if err != nil {
+		panic(err)
+	}
+	testProtoLoader = starlarkproto.NewLoader()
+	if err := testProtoLoader.AddDescriptorSet(ds); err != nil {
+		panic(err)
+	}
+}
+
+// testMessage returns new testproto.Msg as a Starlark value to be used from
+// tests (grabs it via testProtoLoader).
+func testMessage(i int) *starlarkproto.Message {
+	testproto, err := testProtoLoader.Module("misc/support/test.proto")
+	if err != nil {
+		panic(err)
+	}
+	msgT, err := testproto.Attr("Msg")
 	if err != nil {
 		panic(err)
 	}
@@ -53,16 +74,19 @@ func testMessage(i int) *starlarkproto.Message {
 func TestProtos(t *testing.T) {
 	t.Parallel()
 
-	loader := protoLoader()
-
 	// Note: imports of standard and LUCI protos are tested by more high-level
 	// lucicfg tests that actually generate configs. Misc protos are not involved
 	// in them, so we test they can be imported separately here.
+	//
+	// TODO(vadimsh): Get rid of this.
+	loader := starlarkproto.NewLoader()
+	if err := loader.AddDescriptorSet(miscTypesDescSet); err != nil {
+		panic(err)
+	}
 	for _, path := range miscProtos {
 		Convey(fmt.Sprintf("%q is importable", path), t, func(c C) {
-			dict, _, err := loader(path)
+			_, err := loader.Module(path)
 			So(err, ShouldBeNil)
-			So(len(dict), ShouldEqual, 1)
 		})
 	}
 
@@ -79,6 +103,6 @@ func TestProtos(t *testing.T) {
 	Convey("Doc URL works", t, func() {
 		name, doc := protoMessageDoc(testMessage(123))
 		So(name, ShouldEqual, "Msg")
-		So(doc, ShouldEqual, "https://example.com/proto-doc") // see testproto/test.proto
+		So(doc, ShouldEqual, "https://example.com/proto-doc") // see misc/support/test.proto
 	})
 }
