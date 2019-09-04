e1dd9ee71e1ea96b5c25bf7c1a48f09ee2c28f8c
Robert Iannucci <iannucci@chromium.org>
2019-09-04 01:15:34 +0000

    [luciexe/host] Implement auth.
    R=tandrii@chromium.org
    
    Bug: 939368
    Change-Id: I0015f5791fb4ce1b4dfe0cdd9a4f5cf6de18c1ef
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1783624
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

---
+++ luciexe/host/cleanup.go
@@ -0,0 +1,48 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package host
+
+import (
+	"os"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/system/environ"
+)
+
+type cleanupFn func() error
+type cleanupSlice []cleanupFn
+
+func (c cleanupSlice) run() {
+	merr := errors.NewLazyMultiError(len(c))
+	for i, fn := range c {
+		merr.Assign(i, fn())
+	}
+	if err := merr.Get(); err != nil {
+		panic(err)
+	}
+}
+
+func (c *cleanupSlice) add(cleanup cleanupSlice, err error) error {
+	*c = append(*c, cleanup...)
+	return err
+}
+
+func restoreEnv() cleanupFn {
+	origEnv := environ.System()
+	return func() error {
+		os.Clearenv()
+		return errors.Annotate(origEnv.Iter(os.Setenv), "restoring original env").Err()
+	}
+}
