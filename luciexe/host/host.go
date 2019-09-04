01b951f30f7bbd04a1001e6a1ff84bc6ad419ea4
Robert Iannucci <iannucci@chromium.org>
2019-09-03 17:29:13 -0700

    [luciexe/host] Implement auth.
    R=tandrii@chromium.org
    
    Bug: 939368
    Change-Id: I0015f5791fb4ce1b4dfe0cdd9a4f5cf6de18c1ef

--- luciexe/host/host.go
+++ luciexe/host/host.go
@@ -22,9 +22,71 @@ package host
 
 import (
 	"context"
+	"os"
 
 	bbpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/system/environ"
+	"go.chromium.org/luci/lucictx"
 )
+
+type cleanupSlice []func() error
+
+func (c cleanupSlice) run() {
+	merr := errors.NewLazyMultiError(len(c))
+	for i, fn := range c {
+		merr.Assign(i, fn())
+	}
+	if err := merr.Get(); err != nil {
+		panic(err)
+	}
+}
+
+func (c *cleanupSlice) add(cleanup cleanupSlice, err error) error {
+	*c = append(*c, cleanup...)
+	return err
+}
+
+func startAuthServices(ctx context.Context, opts *Options) (cleanupSlice, error) {
+	var envCleanups cleanupSlice
+	defer envCleanups.run()
+
+	env := environ.New(nil)
+
+	if err := opts.ExeAuth.Launch(ctx, opts.authDir); err != nil {
+		return nil, errors.Annotate(err, "setting up task auth").Err()
+	}
+	opts.ExeAuth.Report(ctx)
+	ctx = opts.ExeAuth.Export(ctx, env)
+	envCleanups = append(envCleanups, func() error {
+		opts.ExeAuth.Close(ctx)
+		return nil
+	})
+
+	exported, err := lucictx.ExportInto(ctx, opts.lucictxDir)
+	if err != nil {
+		return nil, errors.Annotate(err, "exporting LUCI_CONTEXT").Err()
+	}
+	envCleanups = append(envCleanups, exported.Close)
+	exported.SetInEnviron(env)
+
+	if err := env.Iter(os.Setenv); err != nil {
+		return nil, errors.Annotate(err, "setting up environment").Err()
+	}
+
+	retCleanups := envCleanups
+	envCleanups = nil
+
+	return retCleanups, nil
+}
+
+func restoreEnv() func() error {
+	origEnv := environ.System()
+	return func() error {
+		os.Clearenv()
+		return errors.Annotate(origEnv.Iter(os.Setenv), "restoring original env").Err()
+	}
+}
 
 // Run executes `cb` in a "luciexe" host environment.
 //
@@ -37,7 +99,46 @@ import (
 // When the callback function completes, Run closes the returned channel.
 //
 // Blocking the returned channel may block the execution of `cb`.
+//
+// NOTE: This modifies the environment (i.e. with os.Setenv) while `cb` is
+// running. Be careful when using Run concurrently with other code.
 func Run(ctx context.Context, options *Options, cb func(context.Context) error) (<-chan *bbpb.Build, error) {
-	// TODO(iannucci): implement
-	return nil, nil
+	var opts Options
+	if options != nil {
+		opts = *options
+	}
+	if err := opts.initialize(); err != nil {
+		return nil, err
+	}
+
+	// cleanup will accumulate all of the cleanup functions as we set up the
+	// environment. If an error occurs before we can start the user code (`cb`),
+	// the defer below will run them all. Otherwise they'll be transferred to the
+	// goroutine.
+	var cleanup cleanupSlice
+	defer cleanup.run()
+
+	// First, capture the entire env to restore it later.
+	cleanup = append(cleanup, restoreEnv())
+
+	// Startup auth services
+	if err := cleanup.add(startAuthServices(ctx, &opts)); err != nil {
+		return nil, err
+	}
+
+	// TODO(iannucci): implement logdog butler
+	builds := make(chan *bbpb.Build)
+
+	// Transfer ownership of cleanups to goroutine
+	userCleanup := cleanup
+	cleanup = nil
+	go func() {
+		defer close(builds)
+		defer userCleanup.run()
+
+		// TODO(iannucci): do something with retval of cb
+		cb(ctx)
+	}()
+
+	return builds, nil
 }
