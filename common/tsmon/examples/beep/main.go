59f500fed7b8d884f318cd143eb6d27c1695804e
Scott Lee <ddoman@chromium.org>
2019-09-11 20:41:28 +1000

    [tsmon] add dummy_project.proto and implement the target interface
    WIP, because I need to add unit tests, but please feel free to leave review
    comments.
    -------------------
    
    This CL adds an example proto message, dummy_project.proto, and implements
    types.Target interface for the proto message. Metrics can be created with
    DummyProject, and report metric values with an instance of DummyProject.
    
    e.g.,
    
    ```
    rpcMetric = NewCounterWithTargetType(
      "/chops/rpc/server/count", target.DummyProjectType,
      "# of RPC req processed by the dummy project.", nil,
      field.String("status"),
      field.String("failure_reason"),
    )
    
    func ProcessRPCReq(ctx context.Context, req *api.ProcessRPCRequest) error {
      prj := model.FindProject(req)
    
      # The data model of Project may have the exactly same set of fields with
      # monitoring.DummyProject, but not all the cases. Thus, implementing such
      # a function like ToMonitoringTarget() would be necessary.
      tctx := target.Set(prj.ToMonitoringTarget())
      err := dispatch(tctx, prj, req)
    
      if err == nil {
        rpcMetric.Add(tctx, str(NoError.code), "")
      } else {
        rpcMetric.Add(tctx, str(err.code), err.reason)
      }
      return err
    }
    ```
    
    Bug: 941798
    Change-Id: I3a9049b06f78e3bbb6556cd362bba07219272f90

---
+++ common/tsmon/examples/beep/main.go
@@ -0,0 +1,81 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// package dummy_project implements a demo application that populates monitoring
+// data for a dummy_project.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"context"
+	"os"
+
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/logging/gologger"
+	"go.chromium.org/luci/common/tsmon"
+	"go.chromium.org/luci/common/tsmon/field"
+	"go.chromium.org/luci/common/tsmon/metric"
+	"go.chromium.org/luci/common/tsmon/target"
+
+	"go.chromium.org/luci/common/tsmon/examples/beep/dummy_project"
+)
+
+var presence = metric.NewBoolWithTargetType(
+	"test/tsmon/examples/beep",
+	(*dummy_project.DummyProject)(nil).Type(),
+	"A always-true heart-beating metric.",
+	nil,
+	field.Int("num"),
+)
+
+// initialize initializes flags and tsmon, and returns the context.
+func initialize() context.Context {
+	fs := flag.NewFlagSet("", flag.ExitOnError)
+	tsmonFlags := tsmon.NewFlags()
+	tsmonFlags.Flush = "manual"
+
+	// The generated proto message will be displayed in stderr.
+	tsmonFlags.Endpoint = "file://"
+	tsmonFlags.Register(fs)
+
+	loggingConfig := logging.Config{Level: logging.Info}
+	loggingConfig.AddFlags(fs)
+	fs.Parse(os.Args[1:])
+
+	c := context.Background()
+	c = gologger.StdConfig.Use(c)
+	c = loggingConfig.Set(c)
+
+	if err := tsmon.InitializeFromFlags(c, &tsmonFlags); err != nil {
+		panic(fmt.Sprintf("failed to initialize tsmon: %s", err))
+	}
+	return c
+}
+
+func main() {
+	c := initialize()
+	for i := 0; i < 4; i++ {
+		// Create a context with a dummy project target.
+		tc := target.Set(c, &dummy_project.DummyProject{
+			Project: fmt.Sprintf("MyProject-%d", i),
+			Location: "MyComputer",
+			IsStaging: false,
+		})
+		presence.Set(tc, true, i)
+	}
+
+	// The output should contain 4 MetricCollections for the 4 project targets.
+	tsmon.Flush(c)
+}
