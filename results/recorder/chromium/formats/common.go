5edb197e1e995e2a356e450bcea9b8b68ced05e8
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-09-11 17:12:51 -0700

    [results] Add initial conversion of GTest results to Invocation.
    Change-Id: If7b8d7f11f79ad69c56f2e648c2518a1e55b6f34

--- results/recorder/chromium/formats/common.go
+++ results/recorder/chromium/formats/common.go
@@ -13,3 +13,66 @@
 // limitations under the License.
 
 package formats
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"sort"
+	"strings"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+
+	resultspb "go.chromium.org/luci/results/proto/v1"
+)
+
+
+// VariantDefMap contains the key:val pairs that define a Variant.
+//
+// It is the "Def" part of a VariantDef proto.
+type VariantDefMap map[string]string
+
+// GetID returns an ID for the provided variant.
+func (d VariantDefMap) GetID() string {
+	// TODO: Implement.
+	return "variant ID"
+}
+
+// GetProto converts the VariantDefMap to a fully fledged resultspb.VariantDef proto.
+func (d VariantDefMap) GetProto(sortedKeys []string) *resultspb.VariantDef {
+	// If we didn't build up a sorted key list already, do so.
+	if len(sortedKeys) <= 0 {
+		for k := range d {
+			sortedKeys = append(sortedKeys, k)
+		}
+		sort.Strings(sortedKeys)
+	}
+
+	// TODO: Handle too many pairs/too long keys.
+
+	pairs := make([]string, len(d))
+	for i, k := range sortedKeys {
+		pairs[i] = fmt.Sprintf("%s:%s", k, d[k])
+	}
+
+	h := sha256.New()
+	h.Write([]byte(strings.Join(pairs, "\n")))
+	digest := string(h.Sum(nil))
+
+	return &resultspb.VariantDef{
+		Def:    d,
+		Digest: digest,
+	}
+}
+
+// secondsToTimestamp converts a UTC float64 timestamp to a ptypes Timestamp.
+func secondsToTimestamp(t float64) *timestamp.Timestamp {
+	if t < 0 {
+		panic("negative time given in secondsToTimestamp")
+	}
+
+	return &timestamp.Timestamp{
+		Seconds: int64(t),
+		Nanos:   int32(1e9 * (t - math.Floor(t))),
+	}
+}
---
+++ results/recorder/chromium/formats/common_test.go
@@ -0,0 +1,73 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package formats
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	resultspb "go.chromium.org/luci/results/proto/v1"
+)
+
+func TestTimeConversion(t *testing.T) {
+	Convey(`Works`, t, func() {
+		Convey(`with whole seconds`, func() {
+			ts := secondsToTimestamp(float64(1567740569))
+			So(ts, ShouldResemble, &timestamp.Timestamp{Seconds: 1567740569})
+		})
+
+		Convey(`with fractions of seconds`, func() {
+			ts := secondsToTimestamp(float64(1567740569.147629))
+			So(ts.Seconds, ShouldEqual, 1567740569)
+			So(ts.Nanos, ShouldAlmostEqual, 147629000, 100)
+		})
+	})
+}
+
+func TestVariantUtils(t *testing.T) {
+	Convey(`Map conversion works`, t, func() {
+		def := VariantDefMap{ //map[string]string{
+			"k2": "v2",
+			"k3": "v3",
+			"k1": "v1",
+		}
+
+		Convey(`with presorted keys`, func() {
+			varpb := def.GetProto([]string{"k1", "k2", "k3"})
+			So(varpb, ShouldResemble, &resultspb.VariantDef{
+				Def: map[string]string{
+					"k1": "v1",
+					"k2": "v2",
+					"k3": "v3",
+				},
+				Digest: string([]byte{200, 131, 176, 65, 74, 65, 110, 8, 188, 253, 143, 179, 55, 140, 121, 192, 193, 199, 233, 91, 35, 178, 42, 240, 92, 156, 189, 85, 237, 163, 5, 89}),
+			})
+		})
+		Convey(`without presorted keys`, func() {
+			varpb := def.GetProto([]string{})
+			So(varpb, ShouldResemble, &resultspb.VariantDef{
+				Def: map[string]string{
+					"k1": "v1",
+					"k2": "v2",
+					"k3": "v3",
+				},
+				Digest: string([]byte{200, 131, 176, 65, 74, 65, 110, 8, 188, 253, 143, 179, 55, 140, 121, 192, 193, 199, 233, 91, 35, 178, 42, 240, 92, 156, 189, 85, 237, 163, 5, 89}),
+			})
+		})
+	})
+}
