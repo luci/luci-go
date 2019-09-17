24221df916e3082b89346d1a4ee2c9db7562ea03
Robert Iannucci <iannucci@chromium.org>
2019-09-16 18:40:40 -0700

    [sync/dispatcher] Miscellaneous features/fixes.
    Adds:
      * Channel.Close() method (non-panic'ing `close(Channel.C)`)
      * Channel.SafeSend() method (non-panic'ing `Channel.C <- item`)
      * Channel.IsDrained() method (convenient check for closed `Channel.DrainC`)
      * Options.DrainedFn callback (after processing loop dies, but before DrainC
        is closed)
    
    R=tandrii@chromium.org
    
    Fixes:
      * If context was canceled, Channel.C wouldn't close, but its consumer would
        die. Now when the consumer dies, it closes Channel.C (in a non-panic'ing
        way) to unblock SafeSend users/panic unaware senders.
    Change-Id: I635e6c101da1dbfa24b7ad8caf51ef685cd5b4d9

--- common/sync/dispatcher/channel.go
+++ common/sync/dispatcher/channel.go
@@ -33,17 +33,39 @@ type Channel struct {
 	DrainC <-chan struct{}
 }
 
+// Close is a convenience function which closes C (and swallows panic if
+// already closed).
+func (c Channel) Close() {
+	defer func() { recover() }()
+	close(c.C)
+}
+
 // CloseAndDrain is a convenience function which closes C (and swallows panic if
 // already closed) and then blocks on DrainC/ctx.Done().
 func (c Channel) CloseAndDrain(ctx context.Context) {
-	func() {
-		defer func() { recover() }()
-		close(c.C)
-	}()
-
+	c.Close()
 	select {
 	case <-ctx.Done():
 	case <-c.DrainC:
+	}
+}
+
+// SafeSend attempts to send the item `itm` to the Channel, which may be closed.
+//
+// On successful send, returns true, otherwise false.
+func (c Channel) SafeSend(itm interface{}) (ok bool) {
+	defer func() { recover() }()
+	c.C <- itm
+	return true
+}
+
+// IsDrained returns true iff the Channel is closed and drained.
+func (c Channel) IsDrained() bool {
+	select {
+	case <-c.DrainC:
+		return true
+	default:
+		return false
 	}
 }
 
@@ -108,6 +130,5 @@ func NewChannel(ctx context.Context, opts *Options, send SendFn) (Channel, error
 	}
 
 	go cstate.run(ctx, send)
-
 	return Channel{C: itemCh, DrainC: drainCh}, nil
 }
