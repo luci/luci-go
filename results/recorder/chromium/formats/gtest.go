8d70352befb3de557ad81cb794f4e15355df0b6e
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-09-19 12:47:27 -0700

    [results] Initial parsing for GTest format.
    Change-Id: I84ce7e89a2d0c269fbf8d89e1ec1296296a2007b

---
+++ results/recorder/chromium/formats/gtest.go
@@ -0,0 +1,73 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package formats
+
+import (
+	"encoding/json"
+	"io"
+
+	"golang.org/x/net/context"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+// GTestResults represents the structure as described to be generated in
+// https://cs.chromium.org/chromium/src/base/test/launcher/test_results_tracker.h?l=83&rcl=96020cfd447cb285acfa1a96c37a67ed22fa2499
+// (base::TestResultsTracker::SaveSummaryAsJSON)
+//
+// Fields not used by Test Results are omitted.
+type GTestResults struct {
+	AllTests   []string `json:"all_tests"`
+	GlobalTags []string `json:"global_tags"`
+
+	// PerIterationData is a vector of run iterations, each mapping test names to a list of test data.
+	PerIterationData []map[string][]*GTestRunResult `json:"per_iteration_data"`
+
+	// TestLocations maps test names to their location in code.
+	TestLocations map[string]*Location `json:"test_locations"`
+}
+
+// GTestRunResult represents the per_iteration_data as described in
+// https://cs.chromium.org/chromium/src/base/test/launcher/test_results_tracker.h?l=83&rcl=96020cfd447cb285acfa1a96c37a67ed22fa2499
+// (base::TestResultsTracker::SaveSummaryAsJSON)
+//
+// Fields not used by Test Results are omitted.
+type GTestRunResult struct {
+	Status        string `json:"status"`
+	ElapsedTimeMs int    `json:"elapsed_time_ms"`
+
+	OutputSnippet       string `json:"output_snippet"`
+	LosslessSnippet     bool   `json:"losless_snippet"`
+	OutputSnippetBase64 string `json:"output_snippit_base64"`
+}
+
+// Location describes a code location.
+type Location struct {
+	File string `json:"file"`
+	Line int    `json:"line"`
+}
+
+// ConvertFromJSON reads the provided reader into the receiver.
+func (r *GTestResults) ConvertFromJSON(ctx context.Context, reader io.Reader) error {
+	if err := json.NewDecoder(reader).Decode(r); err != nil {
+		return err
+	}
+
+	if len(r.AllTests) == 0 {
+		return errors.Reason(`missing "all_tests" field in JSON`).Err()
+	}
+
+	return nil
+}
