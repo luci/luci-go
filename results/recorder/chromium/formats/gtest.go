42bd59287080de96e900f5aee1cfb73ae532380a
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-09-19 20:45:49 -0700

    [results] Add initial conversion of GTest results to Invocation.
    Change-Id: If7b8d7f11f79ad69c56f2e648c2518a1e55b6f34

--- results/recorder/chromium/formats/gtest.go
+++ results/recorder/chromium/formats/gtest.go
@@ -15,12 +15,20 @@
 package formats
 
 import (
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"io"
+	"sort"
+	"strconv"
 
 	"golang.org/x/net/context"
 
 	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+
+	resultspb "go.chromium.org/luci/results/proto/v1"
+	"go.chromium.org/luci/results/util"
 )
 
 // GTestResults represents the structure as described to be generated in
@@ -48,9 +56,8 @@ type GTestRunResult struct {
 	Status        string `json:"status"`
 	ElapsedTimeMs int    `json:"elapsed_time_ms"`
 
-	OutputSnippet       string `json:"output_snippet"`
 	LosslessSnippet     bool   `json:"losless_snippet"`
-	OutputSnippetBase64 string `json:"output_snippit_base64"`
+	OutputSnippetBase64 string `json:"output_snippet_base64"`
 }
 
 // Location describes a code location.
@@ -70,4 +77,164 @@ func (r *GTestResults) ConvertFromJSON(ctx context.Context, reader io.Reader) er
 	}
 
 	return nil
+}
+
+// ToInvocation converts a GTestResults to a (partial) resultspb.Invocation.
+func (r *GTestResults) ToInvocation(ctx context.Context, req *resultspb.DeriveInvocationFromSwarmingRequest) (*resultspb.Invocation, error) {
+	// testsToVariants maps GTest base name to the variant IDs present to Results.
+	// We store these maps to handle the same test and variants appearing in multiple iterations.
+	testsToVariants := map[string]map[string][]*resultspb.Result{}
+	variantDefs := map[string]*resultspb.VariantDef{}
+
+	// In theory, we can have multiple iterations. This seems rare in practice, so log if we do see
+	// more than one to confirm and track.
+	if len(r.PerIterationData) > 1 {
+		logging.Infof(ctx,
+			"Got %d iterations for task %s on %s", len(r.PerIterationData), req.Task.Id, req.Task.Hostname)
+	}
+
+	// Assume the invocation is complete for now; if any results are NOTRUN, we'll mark as otherwise.
+	incomplete := false
+
+	for _, data := range r.PerIterationData {
+		for name, results := range data {
+			baseName, params := r.extractParameters(name)
+
+			// Derive the variant definition from base variant and params.
+			variantDef := MergeTestVariantMaps(req.BaseTestVariant.Def, params).Proto()
+			varID := variantDef.Digest
+			if _, ok := variantDefs[varID]; !ok {
+				variantDefs[varID] = variantDef
+			}
+
+			// Generate individual result protos.
+			rpbs := make([]*resultspb.Result, len(results))
+			for i, result := range results {
+				rpbs[i] = r.convertRunResult(ctx, name, result)
+
+				// TODO: Verify that it's indeed the case that getting NOTRUN results in the final results
+				// indicates the task was incomplete.
+				if result.Status == "NOTRUN" {
+					incomplete = true
+				}
+			}
+
+			// Store the processed test result into the correct part of the overall map.
+			testPath := req.TestPathPrefix + baseName
+			if _, ok := testsToVariants[testPath]; !ok {
+				testsToVariants[testPath] = map[string][]*resultspb.Result{}
+			}
+			testsToVariants[testPath][varID] = append(testsToVariants[testPath][varID], rpbs...)
+		}
+	}
+
+	// Now that we have all the results mapped by test names and variant, populate the invocation.
+	inv := &resultspb.Invocation{}
+
+	for name, variants := range testsToVariants {
+		testpb := &resultspb.Invocation_Test{Name: name}
+
+		for id, results := range variants {
+			testpb.Variants = append(testpb.Variants, &resultspb.Invocation_TestVariant{
+				VariantId: id,
+				Results:   results,
+			})
+		}
+
+		inv.Tests = append(inv.Tests, testpb)
+	}
+	sort.Slice(inv.Tests, func(i, j int) bool { return inv.Tests[i].Name < inv.Tests[j].Name })
+
+	inv.VariantDefs = variantDefs
+
+	inv.Incomplete = incomplete
+
+	// Populate the tags.
+	inv.Tags = append(inv.Tags, util.StringPair("test_framework", "gtest"))
+	for _, tag := range r.GlobalTags {
+		inv.Tags = append(inv.Tags, util.StringPair("global_tag", tag))
+	}
+
+	return inv, nil
+}
+
+// status derives a resultspb.Status from the given GTest status and annotating info as tags if any.
+func (r *GTestResults) status(s string) (status resultspb.Status, tags []*resultspb.StringPair) {
+	switch s {
+	case "SUCCESS":
+		return resultspb.Status_PASS, nil
+	case "FAILURE":
+		return resultspb.Status_FAIL, nil
+	case "FAILURE_ON_EXIT":
+		return resultspb.Status_FAIL, []*resultspb.StringPair{
+			util.StringPair("gtest_status", "FAILURE_ON_EXIT"),
+		}
+	case "CRASH":
+		return resultspb.Status_CRASH, nil
+	case "TIMEOUT":
+		return resultspb.Status_ABORT, nil
+	case "SKIPPED":
+		return resultspb.Status_SKIP, nil
+	case "EXCESSIVE_OUTPUT":
+		return resultspb.Status_FAIL, []*resultspb.StringPair{
+			util.StringPair("gtest_status", "EXCESSIVE_OUTPUT"),
+		}
+	case "NOTRUN":
+		return resultspb.Status_SKIP, nil
+	default:
+		return resultspb.Status_STATUS_UNSPECIFIED, []*resultspb.StringPair{
+			util.StringPair("gtest_status", s),
+		}
+	}
+}
+
+// extractParameters extracts parameters from a test path as a mapping with "param/" keys.
+func (r *GTestResults) extractParameters(testPath string) (basePath string, params VariantDefMap) {
+	// TODO: Implement.
+	basePath = testPath
+	params = VariantDefMap{}
+	return
+}
+
+func (r *GTestResults) convertRunResult(ctx context.Context, name string, result *GTestRunResult) *resultspb.Result {
+	rpb := &resultspb.Result{}
+
+	status, statusTags := r.status(result.Status)
+	rpb.Status = status
+	rpb.Tags = append(rpb.Tags, statusTags...)
+
+	rpb.Duration = secondsToDuration(1e-6 * float64(result.ElapsedTimeMs))
+
+	// Write the summary.
+	if len(result.OutputSnippetBase64) > 0 {
+		bytes, err := base64.StdEncoding.DecodeString(result.OutputSnippetBase64)
+		if err != nil {
+			// Log the error, but we shouldn't fail to convert an entire invocation just because we can't
+			// convert a summary.
+			logging.Errorf(ctx, "Failed to convert OutputSnippetBase64 %q", result.OutputSnippetBase64)
+		} else {
+			summary := fmt.Sprintf("%q", bytes)
+
+			// %q double quotes a Go-escaped string, so strip the surrounding quotation marks.
+			rpb.Summary = &resultspb.Markdown{
+				Text: fmt.Sprintf("```\n%s\n```", summary[1:len(summary)-1]),
+			}
+		}
+	}
+
+	// Store the correct output snippet.
+	rpb.Tags = append(
+		rpb.Tags,
+		util.StringPair("lossless_snippet", strconv.FormatBool(result.LosslessSnippet)),
+	)
+
+	// Store the test code location.
+	if loc, ok := r.TestLocations[name]; ok {
+		rpb.Tags = append(rpb.Tags,
+			util.StringPair("file", loc.File),
+			util.StringPair("line", strconv.Itoa(loc.Line)),
+		)
+	}
+
+	return rpb
 }
--- results/recorder/chromium/formats/gtest_test.go
+++ results/recorder/chromium/formats/gtest_test.go
@@ -17,63 +17,69 @@ package formats
 import (
 	"bytes"
 	"context"
+	"sort"
 	"testing"
+
+	resultspb "go.chromium.org/luci/results/proto/v1"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestGTestJsonConversion(t *testing.T) {
+func TestGTestConversions(t *testing.T) {
 	ctx := context.Background()
-	buf := []byte(
-		`{
-			"all_tests": [
-				"FooTest.TestDoBar",
-				"FooTest.TestDoBaz"
-			],
-      "global_tags": ["CPU_64_BITS","MODE_RELEASE","OS_WIN"],
-			"per_iteration_data": [{
-				"FooTest.TestDoBar": [
-					{
-						"elapsed_time_ms": 1837,
-						"losless_snippet": false,
-						"output_snippet": "[ RUN      ] FooTest.TestDoBar",
-						"status": "CRASH"
-					},
-					{
-						"elapsed_time_ms": 1856,
-						"losless_snippet": false,
-						"output_snippet": "[ RUN      ] FooTest.TestDoBar",
-						"status": "FAIL"
-					}
-				],
-				"FooTest.TestDoBaz": [
-					{
-						"elapsed_time_ms": 837,
-						"losless_snippet": false,
-						"output_snippet": "[ RUN      ] FooTest.TestDoBaz",
-						"status": "SUCCESS"
-					},
-					{
-						"elapsed_time_ms": 856,
-						"losless_snippet": false,
-						"output_snippet": "[ RUN      ] FooTest.TestDoBaz",
-						"status": "SUCCESS"
-					}
-				]
-			}],
-			"test_locations": {
-				"FooTest.TestDoBar": {
-					"file": "../../chrome/browser/foo/test.cc",
-					"line": 287
-				},
-				"FooTest.TestDoBaz": {
-					"file": "../../chrome/browser/foo/test.cc",
-					"line": 293
-				}
-			}
-		}`)
 
-	Convey(`Works`, t, func() {
+	Convey(`From JSON works`, t, func() {
+		buf := []byte(
+			`{
+				"all_tests": [
+					"FooTest.TestDoBar",
+					"FooTest.TestDoBaz"
+				],
+				"global_tags": ["CPU_64_BITS","MODE_RELEASE","OS_WIN"],
+				"per_iteration_data": [{
+					"FooTest.TestDoBar": [
+						{
+							"elapsed_time_ms": 1837,
+							"losless_snippet": true,
+							"output_snippet": "[ RUN      ] FooTest.TestDoBar",
+							"output_snippet_base64": "WyBSVU4gICAgICBdIEZvb1Rlc3QuVGVzdERvQmFy",
+							"status": "CRASH"
+						},
+						{
+							"elapsed_time_ms": 1856,
+							"losless_snippet": false,
+							"output_snippet_base64": "c29tZSBkYXRhIHdpdGggACBhbmQg77u/",
+							"status": "FAIL"
+						}
+					],
+					"FooTest.TestDoBaz": [
+						{
+							"elapsed_time_ms": 837,
+							"losless_snippet": true,
+							"output_snippet": "[ RUN      ] FooTest.TestDoBaz",
+							"output_snippet_base64": "WyBSVU4gICAgICBdIEZvb1Rlc3QuVGVzdERvQmF6",
+							"status": "SUCCESS"
+						},
+						{
+							"elapsed_time_ms": 856,
+							"losless_snippet": false,
+							"output_snippet_base64": "c29tZSBkYXRhIHdpdGggACBhbmQg77u/",
+							"status": "SUCCESS"
+						}
+					]
+				}],
+				"test_locations": {
+					"FooTest.TestDoBar": {
+						"file": "../../chrome/browser/foo/test.cc",
+						"line": 287
+					},
+					"FooTest.TestDoBaz": {
+						"file": "../../chrome/browser/foo/test.cc",
+						"line": 293
+					}
+				}
+			}`)
+
 		results := &GTestResults{}
 		err := results.ConvertFromJSON(ctx, bytes.NewReader(buf))
 		So(err, ShouldBeNil)
@@ -83,26 +89,28 @@ func TestGTestJsonConversion(t *testing.T) {
 			{
 				"FooTest.TestDoBar": {
 					{
-						Status:        "CRASH",
-						ElapsedTimeMs: 1837,
-						OutputSnippet: "[ RUN      ] FooTest.TestDoBar",
+						Status:              "CRASH",
+						ElapsedTimeMs:       1837,
+						LosslessSnippet:     true,
+						OutputSnippetBase64: "WyBSVU4gICAgICBdIEZvb1Rlc3QuVGVzdERvQmFy",
 					},
 					{
-						Status:        "FAIL",
-						ElapsedTimeMs: 1856,
-						OutputSnippet: "[ RUN      ] FooTest.TestDoBar",
+						Status:              "FAIL",
+						ElapsedTimeMs:       1856,
+						OutputSnippetBase64: "c29tZSBkYXRhIHdpdGggACBhbmQg77u/",
 					},
 				},
 				"FooTest.TestDoBaz": {
 					{
-						Status:        "SUCCESS",
-						ElapsedTimeMs: 837,
-						OutputSnippet: "[ RUN      ] FooTest.TestDoBaz",
+						Status:              "SUCCESS",
+						ElapsedTimeMs:       837,
+						LosslessSnippet:     true,
+						OutputSnippetBase64: "WyBSVU4gICAgICBdIEZvb1Rlc3QuVGVzdERvQmF6",
 					},
 					{
-						Status:        "SUCCESS",
-						ElapsedTimeMs: 856,
-						OutputSnippet: "[ RUN      ] FooTest.TestDoBaz",
+						Status:              "SUCCESS",
+						ElapsedTimeMs:       856,
+						OutputSnippetBase64: "c29tZSBkYXRhIHdpdGggACBhbmQg77u/",
 					},
 				},
 			},
@@ -111,5 +119,179 @@ func TestGTestJsonConversion(t *testing.T) {
 			"FooTest.TestDoBar": {File: "../../chrome/browser/foo/test.cc", Line: 287},
 			"FooTest.TestDoBaz": {File: "../../chrome/browser/foo/test.cc", Line: 293},
 		})
+	})
+
+	Convey(`To Invocation works`, t, func() {
+		results := &GTestResults{
+			AllTests:   []string{"test1", "test2", "test3"},
+			GlobalTags: []string{"tag1", "tag2"},
+			PerIterationData: []map[string][]*GTestRunResult{
+				{
+					"test1": {
+						{
+							Status:              "SUCCESS",
+							ElapsedTimeMs:       10,
+							LosslessSnippet:     true,
+							OutputSnippetBase64: "WyBSVU4gICAgICBdIEZvb1Rlc3QuVGVzdERvQmFyCigxMCBtcyk=",
+						},
+						{
+							Status:              "SUCCESS",
+							ElapsedTimeMs:       12,
+							LosslessSnippet:     true,
+							OutputSnippetBase64: "WyBSVU4gICAgICBdIEZvb1Rlc3QuVGVzdERvQmFyCigxMiBtcyk=",
+						},
+					},
+					"test2": {
+						{
+							Status:              "FAILURE",
+							ElapsedTimeMs:       20,
+							OutputSnippetBase64: "c29tZSBkYXRhIHdpdGggACBhbmQg77u/",
+						},
+						{
+							Status:              "FAILURE",
+							ElapsedTimeMs:       22,
+							OutputSnippetBase64: "c29tZSBkYXRhIHdpdGggACBhbmQg77u/",
+						},
+					},
+					"test3": {
+						{Status: "FAILURE", ElapsedTimeMs: 30},
+						{Status: "SUCCESS", ElapsedTimeMs: 32},
+					},
+				},
+				{
+					"test1": {
+						{
+							Status:              "SUCCESS",
+							ElapsedTimeMs:       14,
+							LosslessSnippet:     true,
+							OutputSnippetBase64: "WyBSVU4gICAgICBdIEZvb1Rlc3QuVGVzdERvQmFyCigxNCBtcyk=",
+						},
+						{
+							Status:              "SUCCESS",
+							ElapsedTimeMs:       16,
+							LosslessSnippet:     true,
+							OutputSnippetBase64: "WyBSVU4gICAgICBdIEZvb1Rlc3QuVGVzdERvQmFyCigxNiBtcyk=",
+						},
+					},
+					"test2": {
+						{
+							Status:              "FAILURE",
+							ElapsedTimeMs:       24,
+							OutputSnippetBase64: "c29tZSBkYXRhIHdpdGggACBhbmQg77u/",
+						},
+						{
+							Status:              "FAILURE",
+							ElapsedTimeMs:       26,
+							OutputSnippetBase64: "c29tZSBkYXRhIHdpdGggACBhbmQg77u/",
+						},
+					},
+					"test3": {
+						{Status: "CRASH", ElapsedTimeMs: 34},
+						{Status: "NOTRUN", ElapsedTimeMs: 36},
+					},
+				},
+			},
+			TestLocations: map[string]*Location{
+				"test1": {File: "fileA.cc", Line: 12},
+				"test2": {File: "fileA.cc", Line: 24},
+				"test3": {File: "fileB.cc", Line: 36},
+			},
+		}
+
+		req := &resultspb.DeriveInvocationFromSwarmingRequest{
+			Task: &resultspb.DeriveInvocationFromSwarmingRequest_SwarmingTask{
+				Hostname: "host-swarming",
+				Id:       "123",
+			},
+			TestPathPrefix: "prefix/",
+			BaseTestVariant: &resultspb.VariantDef{Def: map[string]string{
+				"bucket":     "bkt",
+				"builder":    "blder",
+				"test_suite": "foo_unittests",
+			}},
+		}
+
+		inv, err := results.ToInvocation(ctx, req)
+		So(err, ShouldBeNil)
+		So(inv.Incomplete, ShouldBeTrue)
+
+		// Check tests. They all have the same variant so far.
+		expectedTestStatuses := map[string][]resultspb.Status{
+			"prefix/test1": {
+				resultspb.Status_PASS, resultspb.Status_PASS, resultspb.Status_PASS, resultspb.Status_PASS,
+			},
+			"prefix/test2": {
+				resultspb.Status_FAIL, resultspb.Status_FAIL, resultspb.Status_FAIL, resultspb.Status_FAIL,
+			},
+			"prefix/test3": {
+				resultspb.Status_FAIL, resultspb.Status_PASS, resultspb.Status_CRASH, resultspb.Status_SKIP,
+			},
+		}
+		expectedTestDurationsNs := map[string][]int32{
+			"prefix/test1": {10000, 12000, 14000, 16000},
+			"prefix/test2": {20000, 22000, 24000, 26000},
+			"prefix/test3": {30000, 32000, 34000, 36000},
+		}
+		expectedTestSummaries := map[string][]string{
+			"prefix/test1": {
+				"```\n[ RUN      ] FooTest.TestDoBar\\n(10 ms)\n```",
+				"```\n[ RUN      ] FooTest.TestDoBar\\n(12 ms)\n```",
+				"```\n[ RUN      ] FooTest.TestDoBar\\n(14 ms)\n```",
+				"```\n[ RUN      ] FooTest.TestDoBar\\n(16 ms)\n```",
+			},
+			"prefix/test2": {
+				"```\nsome data with \\x00 and \\ufeff\n```",
+				"```\nsome data with \\x00 and \\ufeff\n```",
+				"```\nsome data with \\x00 and \\ufeff\n```",
+				"```\nsome data with \\x00 and \\ufeff\n```",
+			},
+		}
+		expectedTestTags := map[string][]*resultspb.StringPair{
+			"prefix/test1": {
+				{Key: "lossless_snippet", Value: "true"},
+				{Key: "file", Value: "fileA.cc"},
+				{Key: "line", Value: "12"},
+			},
+			"prefix/test2": {
+				{Key: "lossless_snippet", Value: "false"},
+				{Key: "file", Value: "fileA.cc"},
+				{Key: "line", Value: "24"},
+			},
+			"prefix/test3": {
+				{Key: "lossless_snippet", Value: "false"},
+				{Key: "file", Value: "fileB.cc"},
+				{Key: "line", Value: "36"},
+			},
+		}
+		for _, tpb := range inv.Tests {
+			So(expectedTestStatuses, ShouldContainKey, tpb.Name)
+			So(len(tpb.Variants), ShouldEqual, 1)
+
+			for i, res := range tpb.Variants[0].Results {
+				So(res.Tags, ShouldResemble, expectedTestTags[tpb.Name])
+				So(res.Status, ShouldResemble, expectedTestStatuses[tpb.Name][i])
+
+				So(res.Duration.Seconds, ShouldEqual, 0)
+				So(res.Duration.Nanos, ShouldAlmostEqual, expectedTestDurationsNs[tpb.Name][i], 100)
+
+				if expectedSummaries, ok := expectedTestSummaries[tpb.Name]; ok {
+					So(res.Summary, ShouldNotBeNil)
+					So(res.Summary.Text, ShouldResemble, expectedSummaries[i])
+				} else {
+					So(res.Summary, ShouldBeNil)
+				}
+			}
+		}
+
+		sort.Slice(inv.Tags, func(i, j int) bool { return inv.Tags[i].Key < inv.Tags[j].Key })
+		So(
+			inv.Tags,
+			ShouldResemble,
+			[]*resultspb.StringPair{
+				{Key: "global_tag", Value: "tag1"},
+				{Key: "global_tag", Value: "tag2"},
+				{Key: "test_framework", Value: "gtest"},
+			},
+		)
 	})
 }
