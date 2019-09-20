dc6b82fd44e85d16d0e1933a6a9eb78f5dfaa635
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-09-20 11:01:28 -0700

    [results] Add initial conversion of GTest results to Invocation.
    Change-Id: If7b8d7f11f79ad69c56f2e648c2518a1e55b6f34

---
+++ results/recorder/chromium/formats/common.go
@@ -0,0 +1,112 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package formats
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"math"
+	"sort"
+
+	"github.com/golang/protobuf/ptypes/duration"
+	"github.com/golang/protobuf/ptypes/timestamp"
+
+	resultspb "go.chromium.org/luci/results/proto/v1"
+)
+
+// VariantDefMap contains the key:val pairs that define a Variant.
+//
+// It is the "Def" part of a VariantDef proto.
+type VariantDefMap map[string]string
+
+// Validates the receiver VariantDefMap by checking characters and length of the keys and values.
+func (d VariantDefMap) Validate() error {
+	// TODO: Implement.
+	return nil
+}
+
+// ID returns a hex SHA256 hash of newline-joined "<key>:<val>" strings from the variant as an ID.
+func (d VariantDefMap) ID() string {
+	h := sha256.New()
+	for _, k := range d.sortedKeys() {
+		io.WriteString(h, k)
+		io.WriteString(h, ":")
+		io.WriteString(h, d[k])
+		io.WriteString(h, "\n")
+	}
+
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// Proto converts the VariantDefMap to a fully fledged resultspb.VariantDef proto.
+func (d VariantDefMap) Proto() *resultspb.VariantDef {
+	return &resultspb.VariantDef{
+		Def:    d,
+		Digest: d.ID(),
+	}
+}
+
+func (d VariantDefMap) sortedKeys() []string {
+	keys := make([]string, 0, len(d))
+	for k := range d {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+// MergeTestVariantMaps gets the test variant def from merging the input maps.
+//
+// If multiple maps define the same key, the last one wins.
+func MergeTestVariantMaps(maps ...VariantDefMap) VariantDefMap {
+	def := VariantDefMap{}
+	for _, m := range maps {
+		for k, v := range m {
+			def[k] = v
+		}
+	}
+	return def
+}
+
+// SortTagsInPlace sorts in-place the tags slice lexicographically by key, then value.
+func SortTagsInPlace(tags []*resultspb.StringPair) {
+	sort.Slice(tags, func(i, j int) bool {
+		if tags[i].Key != tags[j].Key {
+			return tags[i].Key < tags[j].Key
+		}
+		return tags[i].Value < tags[j].Value
+	})
+}
+
+// secondsToTimestamp converts a UTC float64 timestamp to a ptypes Timestamp.
+func secondsToTimestamp(t float64) *timestamp.Timestamp {
+	if t < 0 {
+		panic("negative time given in secondsToTimestamp")
+	}
+	seconds, nanos := splitSeconds(t)
+	return &timestamp.Timestamp{Seconds: seconds, Nanos: nanos}
+}
+
+func secondsToDuration(t float64) *duration.Duration {
+	seconds, nanos := splitSeconds(t)
+	return &duration.Duration{Seconds: seconds, Nanos: nanos}
+}
+
+func splitSeconds(t float64) (seconds int64, nanos int32) {
+	seconds = int64(t)
+	nanos = int32(1e9 * (t - math.Floor(t)))
+	return
+}
