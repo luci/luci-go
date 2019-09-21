ea6747a5d0a16462715eb88bdcd892d80605f382
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-09-21 00:30:12 -0700

    [results] Add initial conversion of JSON Test Result-formatted results to Invocation.
    Change-Id: I68fa68c66f3b080eac8b94c0167f0965605b5084

--- results/recorder/chromium/formats/json.go
+++ results/recorder/chromium/formats/json.go
@@ -18,11 +18,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"sort"
+	"strings"
 
+	wrappers "github.com/golang/protobuf/ptypes/wrappers"
 	"golang.org/x/net/context"
 
 	"go.chromium.org/luci/common/errors"
+
+	rdb "go.chromium.org/luci/results"
+	resultspb "go.chromium.org/luci/results/proto/v1"
+	"go.chromium.org/luci/results/util"
 )
+
+const testNamePrefixKey = "test_name_prefix"
 
 // JSONTestResults represents the structure in
 // https://chromium.googlesource.com/chromium/src/+/master/docs/testing/json_test_results_format.md
@@ -33,8 +42,6 @@ type JSONTestResults struct {
 
 	PathDelimiter string `json:"path_delimiter"`
 
-	SecondsSinceEpoch float64 `json:"seconds_since_epoch"`
-
 	TestsRaw json.RawMessage `json:"tests"`
 	Tests    map[string]*TestFields
 
@@ -43,7 +50,12 @@ type JSONTestResults struct {
 	ArtifactTypes map[string]string `json:"artifact_types"`
 
 	BuildNumber string `json:"build_number"`
-	BuilderName string `json:"builder_number"`
+	BuilderName string `json:"builder_name"`
+
+	// Metadata associated with results, which may include a list of expectation_files, or
+	// test_name_prefix e.g. in GPU tests (distinct from test_path_prefix passed in the recorder API
+	// request).
+	Metadata map[string]json.RawMessage `json:"metadata"`
 }
 
 // TestFields represents the test fields structure in
@@ -62,19 +74,91 @@ type TestFields struct {
 
 // ConvertFromJSON converts a JSON of test results in the JSON Test Results
 // format to the internal struct format.
-//
-// TODO: convert to resultspb.Invocation.
-func ConvertFromJSON(ctx context.Context, reader io.Reader) (*JSONTestResults, error) {
-	results := &JSONTestResults{}
-	if err := json.NewDecoder(reader).Decode(results); err != nil {
-		return nil, err
+func (r *JSONTestResults) ConvertFromJSON(ctx context.Context, reader io.Reader) error {
+	if err := json.NewDecoder(reader).Decode(r); err != nil {
+		return err
 	}
 
 	// Convert Tests and return.
-	if err := results.convertTests("", results.TestsRaw); err != nil {
+	if err := r.convertTests("", r.TestsRaw); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ToInvocation converts a JSONTestResults to a (partial) resultspb.Invocation.
+func (r *JSONTestResults) ToInvocation(ctx context.Context, req *resultspb.DeriveInvocationFromSwarmingRequest) (*resultspb.Invocation, error) {
+	if r.Version != 3 {
+		return nil, errors.Reason("unknown JSON Test Results version %d", r.Version).Err()
+	}
+
+	// testsToVariants maps GTest base name to the variant IDs present to Results.
+	// We store these maps to handle the same test and variants appearing in multiple iterations.
+	testsToVariants := map[string]map[string][]*resultspb.TestResult{}
+	variantDefs := map[string]*resultspb.VariantDef{}
+
+	for name, fields := range r.Tests {
+		baseName, params := extractJSONTestResultParameters(name)
+
+		// Derive the variant definition from base variant and params.
+		variantDefMap := rdb.MergeTestVariantMaps(req.BaseTestVariant.Def, params)
+		if err := variantDefMap.Validate(); err != nil {
+			return nil, errors.Annotate(err, "test %s failed to validate variant def", name).Err()
+		}
+		variantDef := variantDefMap.Proto()
+		varID := variantDef.Digest
+		if _, ok := variantDefs[varID]; !ok {
+			variantDefs[varID] = variantDef
+		}
+
+		// Generate results associated with this test path.
+		rpbs, err := fields.toProtos(name)
+		if err != nil {
+			return nil, errors.Annotate(err, "test %s failed to convert run fields", name).Err()
+		}
+
+		// Store the processed test result into the correct part of the overall map.
+		if _, ok := testsToVariants[baseName]; !ok {
+			testsToVariants[baseName] = map[string][]*resultspb.TestResult{}
+		}
+		testsToVariants[baseName][varID] = append(testsToVariants[baseName][varID], rpbs...)
+	}
+
+	// Now that we have all the results mapped by test path and variant, populate the invocation.
+	inv := &resultspb.Invocation{
+		Incomplete:  r.Interrupted,
+		VariantDefs: variantDefs,
+		Tags:        []*resultspb.StringPair{util.StringPair("test_framework", "json")},
+	}
+
+	for baseName, variants := range testsToVariants {
+		testpb := &resultspb.Invocation_Test{Path: req.TestPathPrefix + baseName}
+
+		for id, results := range variants {
+			testpb.Variants = append(testpb.Variants, &resultspb.Invocation_TestVariant{
+				VariantId: id,
+				Results:   results,
+			})
+		}
+
+		inv.Tests = append(inv.Tests, testpb)
+	}
+	sort.Slice(inv.Tests, func(i, j int) bool { return inv.Tests[i].Path < inv.Tests[j].Path })
+
+	// Get tags from metadata if any.
+	tags, err := r.extractTags()
+	if err != nil {
 		return nil, err
 	}
-	return results, nil
+	for _, tag := range tags {
+		inv.Tags = append(inv.Tags, util.StringPair("json_format_tag", tag))
+	}
+	if r.BuildNumber != "" {
+		inv.Tags = append(inv.Tags, util.StringPair("build_number", r.BuildNumber))
+	}
+	SortTagsInPlace(inv.Tags)
+
+	return inv, nil
 }
 
 // convertTests converts the trie of tests.
@@ -99,8 +183,17 @@ func (r *JSONTestResults) convertTests(curPath string, curNode json.RawMessage) 
 		if r.PathDelimiter != "" {
 			delim = r.PathDelimiter
 		}
+
 		if curPath != "" {
 			testPath = fmt.Sprintf("%s%s%s", curPath, delim, key)
+		} else {
+			if prefixJson, ok := r.Metadata[testNamePrefixKey]; ok {
+				var prefix string
+				if err := json.Unmarshal(prefixJson, &prefix); err != nil {
+					return errors.Annotate(err, "%s not string, got %v", testNamePrefixKey, prefixJson).Err()
+				}
+				testPath = fmt.Sprintf("%s%s", prefix, key)
+			}
 		}
 
 		// No error from unmarshalling to TestFields might mean that the RawMessage
@@ -121,4 +214,101 @@ func (r *JSONTestResults) convertTests(curPath string, curNode json.RawMessage) 
 	}
 
 	return nil
+}
+
+// extractTags tries to read the optional "tags" field in "metadata" as a slice of strings.
+func (r *JSONTestResults) extractTags() ([]string, error) {
+	maybeTags, ok := r.Metadata["tags"]
+	if !ok {
+		return nil, nil
+	}
+
+	var tags []string
+	if err := json.Unmarshal(maybeTags, &tags); err != nil {
+		return nil, errors.Annotate(err, "tags not []string, got %v", maybeTags).Err()
+	}
+
+	return tags, nil
+}
+
+func statusExpected(status string, expected []string) bool {
+	for _, exp := range expected {
+		if status == exp {
+			return true
+		}
+	}
+	return false
+}
+
+func ofJSONStatus(s string) (resultspb.Status, error) {
+	switch s {
+	case "CRASH":
+		return resultspb.Status_CRASH, nil
+	case "FAIL":
+		return resultspb.Status_FAIL, nil
+	case "PASS":
+		return resultspb.Status_PASS, nil
+	case "SKIP":
+		return resultspb.Status_SKIP, nil
+	case "TIMEOUT":
+		return resultspb.Status_ABORT, nil
+	default:
+		// There are a number of web test-specific statuses not handled here as they are deprecated.
+		return resultspb.Status_STATUS_UNSPECIFIED, errors.Reason(
+			"unknown JSON Test Format status %s", s).Err()
+	}
+}
+
+// extractJSONTestResultParameters extracts parameters from a test path as a mapping with "param/" keys.
+func extractJSONTestResultParameters(testPath string) (basePath string, params rdb.VariantDefMap) {
+	// TODO(jchinlee): Implement.
+	basePath = testPath
+	params = rdb.VariantDefMap{}
+	return
+}
+
+// convertTestFields converts the TestFields into a slice of resultspb.Result consisting of the contained runs.
+func (f *TestFields) toProtos(name string) ([]*resultspb.TestResult, error) {
+	// Process statuses.
+	statuses := strings.Split(f.Actual, " ")
+	expectedSet := strings.Split(f.Expected, " ")
+
+	// Process times.
+	// Durations are optional, but if they exist, the number should match that of runs.
+	durations := make([]float64, 0, len(f.Times)+1)
+	if f.Time > 0 {
+		durations = append(durations, f.Time)
+	}
+	durations = append(durations, f.Times...)
+
+	// Times are optional, but there should be one per run if any.
+	if len(durations) > 0 && len(statuses) != len(durations) {
+		return nil, errors.Reason(
+			"test %s had %d durations populated but has %d test statuses; should match",
+			name, len(durations), len(statuses)).Err()
+	}
+
+	rpbs := make([]*resultspb.TestResult, len(statuses))
+	for i, runStatus := range statuses {
+		status, err := ofJSONStatus(runStatus)
+		if err != nil {
+			return nil, err
+		}
+
+		rpbs[i] = &resultspb.TestResult{
+			Expected: &wrappers.BoolValue{
+				Value: statusExpected(runStatus, expectedSet),
+			},
+			Status: status,
+			Tags:   []*resultspb.StringPair{util.StringPair("json_format_status", runStatus)},
+		}
+
+		if len(durations) > 0 {
+			rpbs[i].Duration = secondsToDuration(durations[i])
+		}
+
+		// TODO(jchinlee): Populate artifacts.
+	}
+
+	return rpbs, nil
 }
--- results/recorder/chromium/formats/json_test.go
+++ results/recorder/chromium/formats/json_test.go
@@ -17,71 +17,205 @@ package formats
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"sort"
 	"testing"
 
+	resultspb "go.chromium.org/luci/results/proto/v1"
+
 	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
 )
 
-func TestConversion(t *testing.T) {
+func TestJSONConversions(t *testing.T) {
+	t.Parallel()
 	ctx := context.Background()
-	buf := []byte(
-		`{
-				"version": 3,
-				"interrupted": false,
-				"path_delimiter": "::",
-				"seconds_since_epoch": 1565504423,
-				"tests": {
-					"c1": {
+
+	Convey(`From JSON works`, t, func() {
+		buf := []byte(
+			`{
+					"version": 3,
+					"interrupted": false,
+					"path_delimiter": "::",
+					"metadata": {"test_name_prefix": "prefix."},
+					"builder_name": "Linux Tests",
+					"build_number": "82046",
+					"tests": {
+						"c1": {
+							"c2": {
+								"t1.html": {
+									"actual": "PASS PASS PASS",
+									"expected": "PASS",
+									"time": 0.3,
+									"times": [ 0.2, 0.1 ]
+								},
+								"t2.html": {
+									"actual": "PASS FAIL PASS",
+									"expected": "PASS FAIL",
+									"times": [ 0.05 ]
+								}
+							}
+						},
 						"c2": {
-							"t1.html": {
-								"actual": "PASS",
-								"expected": "PASS",
-								"times": [ 0.2, 0.1 ]
-							},
-							"t2.html": {
-								"actual": "PASS",
-								"expected": "FAIL",
-								"times": [ 0.05 ]
+							"t3.html": {
+								"actual": "FAIL",
+								"expected": "PASS"
 							}
 						}
-					},
-					"c2": {
-						"t3.html": {
-							"actual": "FAIL",
-							"expected": "PASS",
-							"times": [ 0.3, 0.4 ]
-						}
 					}
-				}
-			}`)
+				}`)
 
-	Convey(`Works`, t, func() {
-		results, err := ConvertFromJSON(ctx, bytes.NewReader(buf))
-		So(err, ShouldBeNil)
-		So(results, ShouldNotBeNil)
-		So(results.Version, ShouldEqual, 3)
-		So(results.Interrupted, ShouldEqual, false)
-		So(results.PathDelimiter, ShouldEqual, "::")
-		So(results.SecondsSinceEpoch, ShouldEqual, 1565504423)
-		So(results.Tests, ShouldResemble, map[string]*TestFields{
-			"c1::c2::t1.html": {Actual: "PASS", Expected: "PASS", Times: []float64{0.2, 0.1}},
-			"c1::c2::t2.html": {Actual: "PASS", Expected: "FAIL", Times: []float64{0.05}},
-			"c2::t3.html":     {Actual: "FAIL", Expected: "PASS", Times: []float64{0.3, 0.4}},
-		})
-
-		Convey(`with default path delimiter`, func() {
-			// Clear the delimiter and already processed and flattened tests.
-			results.PathDelimiter = ""
-			results.Tests = make(map[string]*TestFields)
-
-			err := results.convertTests("", results.TestsRaw)
+		Convey(`Works`, func() {
+			results := &JSONTestResults{}
+			err := results.ConvertFromJSON(ctx, bytes.NewReader(buf))
 			So(err, ShouldBeNil)
 			So(results, ShouldNotBeNil)
+			So(results.Version, ShouldEqual, 3)
+			So(results.Interrupted, ShouldEqual, false)
+			So(results.PathDelimiter, ShouldEqual, "::")
+			So(results.BuildNumber, ShouldEqual, "82046")
+			So(results.BuilderName, ShouldEqual, "Linux Tests")
 			So(results.Tests, ShouldResemble, map[string]*TestFields{
-				"c1/c2/t1.html": {Actual: "PASS", Expected: "PASS", Times: []float64{0.2, 0.1}},
-				"c1/c2/t2.html": {Actual: "PASS", Expected: "FAIL", Times: []float64{0.05}},
-				"c2/t3.html":    {Actual: "FAIL", Expected: "PASS", Times: []float64{0.3, 0.4}},
+				"prefix.c1::c2::t1.html": {
+					Actual:   "PASS PASS PASS",
+					Expected: "PASS",
+					Time:     0.3,
+					Times:    []float64{0.2, 0.1},
+				},
+				"prefix.c1::c2::t2.html": {
+					Actual:   "PASS FAIL PASS",
+					Expected: "PASS FAIL",
+					Times:    []float64{0.05},
+				},
+				"prefix.c2::t3.html": {
+					Actual:   "FAIL",
+					Expected: "PASS",
+				},
 			})
+
+			Convey(`with default path delimiter`, func() {
+				// Clear the delimiter and already processed and flattened tests.
+				results.PathDelimiter = ""
+				results.Tests = make(map[string]*TestFields)
+
+				err := results.convertTests("", results.TestsRaw)
+				So(err, ShouldBeNil)
+				So(results, ShouldNotBeNil)
+
+				paths := make([]string, 0, len(results.Tests))
+				for path := range results.Tests {
+					paths = append(paths, path)
+				}
+				sort.Slice(paths, func(i, j int) bool { return paths[i] < paths[j] })
+				So(paths, ShouldResemble, []string{
+					"prefix.c1/c2/t1.html",
+					"prefix.c1/c2/t2.html",
+					"prefix.c2/t3.html",
+				})
+			})
+		})
+	})
+
+	Convey(`To Invocation works`, t, func() {
+		results := &JSONTestResults{
+			Interrupted: true,
+			Metadata: map[string]json.RawMessage{
+				"tags": json.RawMessage(`["linux", "ubuntu", "desktop"]`),
+			},
+			Version: 3,
+			Tests: map[string]*TestFields{
+				"c1/c2/t1.html": {
+					Actual:   "PASS PASS PASS",
+					Expected: "PASS",
+					Time:     0.3,
+					Times:    []float64{0.2, 0.1},
+				},
+				"c1/c2/t2.html": {
+					Actual:   "PASS FAIL PASS CRASH",
+					Expected: "PASS FAIL",
+					Times:    []float64{0.05, 0.07, 0.06, 0.01},
+				},
+				"c2/t3.html": {
+					Actual:   "FAIL",
+					Expected: "PASS",
+				},
+			},
+		}
+
+		req := &resultspb.DeriveInvocationFromSwarmingRequest{
+			Task: &resultspb.DeriveInvocationFromSwarmingRequest_SwarmingTask{
+				Hostname: "host-swarming",
+				Id:       "123",
+			},
+			TestPathPrefix: "prefix/",
+			BaseTestVariant: &resultspb.VariantDef{Def: map[string]string{
+				"bucket":     "bkt",
+				"builder":    "blder",
+				"test_suite": "foo_unittests",
+			}},
+		}
+
+		inv, err := results.ToInvocation(ctx, req)
+		So(err, ShouldBeNil)
+		So(inv.Incomplete, ShouldBeTrue)
+		So(
+			inv.Tags,
+			ShouldResemble,
+			[]*resultspb.StringPair{
+				{Key: "json_format_tag", Value: "desktop"},
+				{Key: "json_format_tag", Value: "linux"},
+				{Key: "json_format_tag", Value: "ubuntu"},
+				{Key: "test_framework", Value: "json"},
+			},
+		)
+
+		So(len(inv.Tests), ShouldEqual, 3)
+
+		Convey(`grouping by test name works`, func() {
+			paths := make([]string, len(inv.Tests))
+			for i, t := range inv.Tests {
+				paths[i] = t.Path
+			}
+			So(paths, ShouldResemble, []string{
+				"prefix/c1/c2/t1.html", "prefix/c1/c2/t2.html", "prefix/c2/t3.html"})
+		})
+
+		Convey(`grouping by variant works`, func() {
+			// They all have one and the same variant so far.
+			So(len(inv.Tests[0].Variants), ShouldEqual, 1)
+			So(len(inv.Tests[1].Variants), ShouldEqual, 1)
+			So(len(inv.Tests[2].Variants), ShouldEqual, 1)
+		})
+
+		Convey(`collecting runs per tests works`, func() {
+			So(len(inv.Tests[0].Variants[0].Results), ShouldEqual, 3)
+			So(len(inv.Tests[1].Variants[0].Results), ShouldEqual, 4)
+			So(len(inv.Tests[2].Variants[0].Results), ShouldEqual, 1)
+		})
+
+		Convey(`durations captured correctly`, func() {
+			testResults := inv.Tests[0].Variants[0].Results
+			expectedDurationsNs := []int32{3e8, 2e8, 1e8}
+			So(len(testResults), ShouldEqual, len(expectedDurationsNs))
+			for i, r := range testResults {
+				So(r.Duration.Seconds, ShouldEqual, 0)
+				So(r.Duration.Nanos, ShouldAlmostEqual, expectedDurationsNs[i], 100)
+			}
+
+			Convey(`fails with mismatched runs and durations`, func() {
+				results.Tests["c1/c2/t2.html"].Actual = "PASS FAIL PASS"
+				_, err := results.ToInvocation(ctx, req)
+				So(err, ShouldErrLike, "had 4 durations populated but has 3 test statuses")
+			})
+		})
+
+		Convey(`expectations captured correctly`, func() {
+			testResults := inv.Tests[1].Variants[0].Results
+			expectations := make([]bool, len(testResults))
+			for i, r := range testResults {
+				expectations[i] = r.Expected.Value
+			}
+			So(expectations, ShouldResemble, []bool{true, true, true, false})
 		})
 	})
 }
