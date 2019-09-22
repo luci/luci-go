a6f3ce470c5fe3c9cc8405cd635c34cb80de33af
Vadim Shtayura <vadimsh@chromium.org>
2019-09-22 09:37:13 +0000

    [auth] Extract token server cert bundle caching into a separate struct.
    Also slightly refactor it to account for the current situation of trusting only
    the token server.
    
    R=tandrii@chromium.org
    
    Change-Id: I16fb4d14210bb1d845d2217763941db4f33a0daf
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1809154
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

---
+++ server/auth/authdb/internal/certs/certs.go
@@ -0,0 +1,68 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package certs knows how to fetch certificate bundles of trusted services.
+package certs
+
+import (
+	"context"
+	"time"
+
+	"go.chromium.org/luci/auth/identity"
+	"go.chromium.org/luci/common/data/caching/lazyslot"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/server/auth/signing"
+)
+
+// Bundle is a lazy-loaded cert bundle of some LUCI service.
+type Bundle struct {
+	ServiceURL string        // root URL of the service to fetch the bundle from
+	certs      lazyslot.Slot // stores fetchedBundle with the lazily fetched bundle
+}
+
+type fetchedBundle struct {
+	id    identity.Identity
+	certs *signing.PublicCertificates
+}
+
+// GetCerts fetches (perhaps from cache) cert bundles of the service.
+//
+// Returns the service identity as well.
+func (b *Bundle) GetCerts(ctx context.Context) (identity.Identity, *signing.PublicCertificates, error) {
+	fetched, err := b.certs.Get(ctx, func(interface{}) (interface{}, time.Duration, error) {
+		fetched, err := b.fetch(ctx)
+		return fetched, time.Hour, err
+	})
+	if err != nil {
+		return "", nil, err
+	}
+	fetchedBundle := fetched.(*fetchedBundle)
+	return fetchedBundle.id, fetchedBundle.certs, nil
+}
+
+// fetch actually fetches the cert bundle.
+func (b *Bundle) fetch(ctx context.Context) (*fetchedBundle, error) {
+	certs, err := signing.FetchCertificatesFromLUCIService(ctx, b.ServiceURL)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to fetch certs from %s", b.ServiceURL).Err()
+	}
+	if certs.ServiceAccountName == "" {
+		return nil, errors.Reason("service %s didn't provide its service account name", b.ServiceURL).Err()
+	}
+	id, err := identity.MakeIdentity("user:" + certs.ServiceAccountName)
+	if err != nil {
+		return nil, errors.Reason("invalid service_account_name %q in certificates bundle from %s", certs.ServiceAccountName, b.ServiceURL).Err()
+	}
+	return &fetchedBundle{id, certs}, nil
+}
---
+++ server/auth/authdb/internal/certs/certs_test.go
@@ -0,0 +1,85 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package certs
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"testing"
+	"time"
+
+	"go.chromium.org/luci/common/clock/testclock"
+	"go.chromium.org/luci/server/auth/internal"
+	"go.chromium.org/luci/server/auth/signing"
+	"go.chromium.org/luci/server/auth/signing/signingtest"
+	"go.chromium.org/luci/server/caching"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestWorks(t *testing.T) {
+	Convey("GetCertificates works", t, func(c C) {
+		tokenService := signingtest.NewSigner(&signing.ServiceInfo{
+			AppID:              "token-server",
+			ServiceAccountName: "token-server-account@example.com",
+		})
+
+		ctx, tc := testclock.UseTime(context.Background(), time.Time{})
+		ctx = caching.WithEmptyProcessCache(ctx)
+
+		calls := 0
+
+		ctx = internal.WithTestTransport(ctx, func(r *http.Request, body string) (int, string) {
+			calls++
+			if r.URL.String() != "http://token-server/auth/api/v1/server/certificates" {
+				return 404, "Wrong URL"
+			}
+			certs, err := tokenService.Certificates(ctx)
+			if err != nil {
+				panic(err)
+			}
+			blob, err := json.Marshal(certs)
+			if err != nil {
+				panic(err)
+			}
+			return 200, string(blob)
+		})
+
+		bundle := Bundle{ServiceURL: "http://token-server"}
+
+		id, certs, err := bundle.GetCerts(ctx)
+		So(err, ShouldBeNil)
+		So(id, ShouldEqual, "user:token-server-account@example.com")
+		So(certs, ShouldNotBeNil)
+		So(calls, ShouldEqual, 1)
+
+		// Reuses stuff from cache.
+		id, certs, err = bundle.GetCerts(ctx)
+		So(err, ShouldBeNil)
+		So(id, ShouldEqual, "user:token-server-account@example.com")
+		So(certs, ShouldNotBeNil)
+		So(calls, ShouldEqual, 1)
+
+		tc.Add(time.Hour + 5*time.Minute)
+
+		// Until it expires.
+		id, certs, err = bundle.GetCerts(ctx)
+		So(err, ShouldBeNil)
+		So(id, ShouldEqual, "user:token-server-account@example.com")
+		So(certs, ShouldNotBeNil)
+		So(calls, ShouldEqual, 2)
+	})
+}
