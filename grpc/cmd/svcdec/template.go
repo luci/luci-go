04460dfbf9e04f7a6b5bdd645c5f249ba71def40
Marc-Antoine Ruel <maruel@chromium.org>
2019-09-23 16:44:56 -0400

    svcdec: use 'ctx' instead of 'c' for context.Context variable name
    First, updated svcdec template to use 'ctx' instead of 'c'.
    
    Then regenerated the relevant packages with this command:
    
      git grep --name-only 'Code generated by svcdec' | \
        xargs -n 1 dirname | uniq | sed 's/\(.*\)/.\/\1/' | \
        xargs -n 1 go generate
    
    Bug: 1006921
    Change-Id: I8f01daecb3bd81a9c30dd52ef66eef5dfcdbf00f

--- grpc/cmd/svcdec/template.go
+++ grpc/cmd/svcdec/template.go
@@ -43,29 +43,29 @@ type {{$StructName}} struct {
 	// Prelude is called for each method before forwarding the call to Service.
 	// If Prelude returns an error, then the call is skipped and the error is
 	// processed via the Postlude (if one is defined), or it is returned directly.
-	Prelude func(c context.Context, methodName string, req proto.Message) (context.Context, error)
+	Prelude func(ctx context.Context, methodName string, req proto.Message) (context.Context, error)
 	// Postlude is called for each method after Service has processed the call, or
 	// after the Prelude has returned an error. This takes the the Service's
 	// response proto (which may be nil) and/or any error. The decorated
 	// service will return the response (possibly mutated) and error that Postlude
 	// returns.
-	Postlude func(c context.Context, methodName string, rsp proto.Message, err error) error
+	Postlude func(ctx context.Context, methodName string, rsp proto.Message, err error) error
 }
 
 {{range .Methods}}
-func (s *{{$StructName}}) {{.Name}}(c context.Context, req {{.InputType}}) (rsp {{.OutputType}}, err error) {
+func (s *{{$StructName}}) {{.Name}}(ctx context.Context, req {{.InputType}}) (rsp {{.OutputType}}, err error) {
 	if s.Prelude != nil {
 		var newCtx context.Context
-		newCtx, err = s.Prelude(c, "{{.Name}}", req)
+		newCtx, err = s.Prelude(ctx, "{{.Name}}", req)
 		if err == nil {
-			c = newCtx
+			ctx = newCtx
 		}
 	}
 	if err == nil {
-		rsp, err = s.Service.{{.Name}}(c, req)
+		rsp, err = s.Service.{{.Name}}(ctx, req)
 	}
 	if s.Postlude != nil {
-		err = s.Postlude(c, "{{.Name}}", rsp, err)
+		err = s.Postlude(ctx, "{{.Name}}", rsp, err)
 	}
 	return
 }
--- logdog/api/endpoints/coordinator/registration/v1/registrationserver_dec.go
+++ logdog/api/endpoints/coordinator/registration/v1/registrationserver_dec.go
@@ -14,28 +14,28 @@ type DecoratedRegistration struct {
 	// Prelude is called for each method before forwarding the call to Service.
 	// If Prelude returns an error, then the call is skipped and the error is
 	// processed via the Postlude (if one is defined), or it is returned directly.
-	Prelude func(c context.Context, methodName string, req proto.Message) (context.Context, error)
+	Prelude func(ctx context.Context, methodName string, req proto.Message) (context.Context, error)
 	// Postlude is called for each method after Service has processed the call, or
 	// after the Prelude has returned an error. This takes the the Service's
 	// response proto (which may be nil) and/or any error. The decorated
 	// service will return the response (possibly mutated) and error that Postlude
 	// returns.
-	Postlude func(c context.Context, methodName string, rsp proto.Message, err error) error
+	Postlude func(ctx context.Context, methodName string, rsp proto.Message, err error) error
 }
 
-func (s *DecoratedRegistration) RegisterPrefix(c context.Context, req *RegisterPrefixRequest) (rsp *RegisterPrefixResponse, err error) {
+func (s *DecoratedRegistration) RegisterPrefix(ctx context.Context, req *RegisterPrefixRequest) (rsp *RegisterPrefixResponse, err error) {
 	if s.Prelude != nil {
 		var newCtx context.Context
-		newCtx, err = s.Prelude(c, "RegisterPrefix", req)
+		newCtx, err = s.Prelude(ctx, "RegisterPrefix", req)
 		if err == nil {
-			c = newCtx
+			ctx = newCtx
 		}
 	}
 	if err == nil {
-		rsp, err = s.Service.RegisterPrefix(c, req)
+		rsp, err = s.Service.RegisterPrefix(ctx, req)
 	}
 	if s.Postlude != nil {
-		err = s.Postlude(c, "RegisterPrefix", rsp, err)
+		err = s.Postlude(ctx, "RegisterPrefix", rsp, err)
 	}
 	return
 }
