4292b68c7f15fcc50aa135607895378a9dff931f
Robert Iannucci <iannucci@chromium.org>
2019-09-24 14:12:36 -0700

    [buildbucket/cmd/bbagent] Replace luci_runner+luciexe/runner with bbagent.
    This is a probably-not-totally-correct implementation of bbagent, but it
    should be enough to get started.
    
    R=tandrii@chromium.org
    
    Bug: 939368
    Change-Id: I93dea98b92ad0f38dfaca84424bcb1b85b1d40dd

---
+++ buildbucket/cmd/bbagent/logdog_output.go
@@ -0,0 +1,37 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+
+	"go.chromium.org/luci/auth"
+
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/logdog/client/butler/output"
+	"go.chromium.org/luci/logdog/client/butler/output/logdog"
+	"go.chromium.org/luci/logdog/common/types"
+)
+
+func mkLogdogOutput(ctx context.Context, opts *bbpb.BuildInfra_LogDog) (output.Output, error) {
+	return (&logdog.Config{
+		Auth: auth.NewAuthenticator(ctx, auth.SilentLogin, auth.Options{
+			MonitorAs: "bbagent/logdog",
+		}),
+		Host:    opts.Hostname,
+		Project: opts.Project,
+		Prefix:  types.StreamName(opts.Prefix),
+	}).Register(ctx)
+}
