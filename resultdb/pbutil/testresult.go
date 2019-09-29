0a19443189db5d405be37b887fb3a036642c2dee
Nodir Turakulov <nodir@google.com>
2019-09-29 07:17:17 -0700

    [resultdb] Consolidate protobuf utility functions
    Currently we have util package that has functions to work with
    StringPair message. We also have Normalize* functions that work with
    other protobuf message, but they live in the top-level resultdb package.
    Next CL will add functions to parse/generate resource names (e.g.
    Invocation name). We need a place for all utility functions that work
    with ResultDB protobuf messages.
    
    Consolidate all existing functions to util package and rename it to
    pbutil.
    
    Do not move VariantDefMap just yet. It is unclear whether it will remain
    the same given the recent changes in the protobuf messages.
    
    R=jchinlee@chromium.org
    
    Change-Id: I8583f74f3ac576ca8f97e20922e22824992cf3ec

---
+++ resultdb/pbutil/testresult.go
@@ -0,0 +1,41 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pbutil
+
+import (
+	"sort"
+
+	pb "go.chromium.org/luci/resultdb/proto/v1"
+)
+
+// NormalizeTestResult converts inv to the canonical form.
+func NormalizeTestResult(tr *pb.TestResult) {
+	sortStringPairs(tr.Tags)
+}
+
+// NormalizeTestResultSlice converts trs to the canonical form.
+func NormalizeTestResultSlice(trs []*pb.TestResult) {
+	for _, tr := range trs {
+		NormalizeTestResult(tr)
+	}
+	sort.Slice(trs, func(i, j int) bool {
+		a := trs[i]
+		b := trs[j]
+		if a.TestPath != b.TestPath {
+			return a.TestPath < b.TestPath
+		}
+		return a.Name < b.Name
+	})
+}
