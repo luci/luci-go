a6a0cae894b15e6759ad034dcb0e02e76769171b
Robert Iannucci <iannucci@chromium.org>
2019-10-01 20:47:04 -0700

    [butler] Make CloseNamespace wait until drain.
    Previously CloseNamespace would return as soon as all data had been read
    from that namespace into the client, but hadn't yet (necessarially) been
    processed through the callbacks or flushed to the Output.
    
    Also fix a bug with the callbacks; apparently "bundler" handles streams
    which have recieved data asymmetrically from streams which have read
    NO data. This is why streams which are opened, but have no data written
    to them, appear as `404 Not Found` on the server, as opposed to showing
    up as a log with no data.
    
    I can't dig that deeply into it right now, but at least make callbacks
    behave correctly.
    
    R=tandrii@chromium.org
    
    Bug: 910369
    Change-Id: Iec32c786d8dca55f340123b4a25570f53ac75762

--- logdog/client/butler/callback.go
+++ logdog/client/butler/callback.go
@@ -17,6 +17,7 @@ package butler
 import (
 	"github.com/golang/protobuf/proto"
 	"go.chromium.org/luci/logdog/api/logpb"
+	"go.chromium.org/luci/logdog/common/types"
 )
 
 // StreamRegistrationCallback is a callback to invoke when a new stream is
@@ -102,17 +103,26 @@ func (b *Butler) runCallbacks(bundle *logpb.ButlerLogBundle) {
 	for _, entry := range bundle.Entries {
 		name := entry.Desc.Name
 		cb := cbsCopy[name]
-		if cb == nil {
-			continue
-		}
-		for _, log := range entry.Logs {
-			cb(log)
+		if cb != nil {
+			for _, log := range entry.Logs {
+				cb(log)
+			}
 		}
 		if entry.Terminal {
-			cb(nil)
-			b.streamCallbacksMu.Lock()
-			delete(b.streamCallbacks, name)
-			b.streamCallbacksMu.Unlock()
+			b.finalCallback(name)
 		}
 	}
+}
+
+func (b *Butler) finalCallback(name string) {
+	b.streamCallbacksMu.Lock()
+	cb, ok := b.streamCallbacks[name]
+	if ok {
+		delete(b.streamCallbacks, name)
+	}
+	b.streamCallbacksMu.Unlock()
+	if cb != nil {
+		cb(nil)
+	}
+	b.streams.DrainedStream(types.StreamName(name))
 }
