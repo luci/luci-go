349b9c01dcf017e20435e7d386f6caf0016741fd
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-10-07 19:56:15 +0000

    [resultdb] Check whether to process Swarming task at all.
    Adds derivation for Invocation ID from Swarming task, and
    decision of whether to write and how to parse.
    
    Change-Id: I7dfea818741a01afc251f4618e25fd36318bf04d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1829975
    Commit-Queue: Jao-ke Chin-Lee <jchinlee@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- resultdb/recorder/chromium/swarming.go
+++ resultdb/recorder/chromium/swarming.go
@@ -16,17 +16,29 @@ package chromium
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/golang/protobuf/proto"
 	"golang.org/x/net/context"
+	"google.golang.org/api/googleapi"
 
 	swarmingAPI "go.chromium.org/luci/common/api/swarming/swarming/v1"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/isolated"
 	"go.chromium.org/luci/common/isolatedclient"
 	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/retry/transient"
+	"go.chromium.org/luci/grpc/grpcutil"
+
+	"go.chromium.org/luci/resultdb"
+	"go.chromium.org/luci/resultdb/internal"
+	"go.chromium.org/luci/resultdb/pbutil"
+	pb "go.chromium.org/luci/resultdb/proto/v1"
+	"go.chromium.org/luci/resultdb/recorder/chromium/formats"
 )
 
 const (
@@ -34,29 +46,109 @@ const (
 	swarmingAPIEndpoint = "_ah/api/swarming/v1/"
 )
 
-// fetchOutputJSON fetches the output.json from the given task on the given host.
+// DeriveInvocation derives the invocation associated with the given swarming task.
 //
-// TODO: convert the bytes.Buffer to a pb.Invocation.
-func fetchOutputJSON(ctx context.Context, cl *http.Client, swarmingURL, taskID string) ([]byte, error) {
-	// Set up swarming service for getting task info.
+// If the task is a dedup of another task, the invocation returned is the underlying one; otherwise,
+// the invocation returned is associated with the swarming task itself.
+func DeriveInvocation(ctx context.Context, req *pb.DeriveInvocationRequest) (*pb.Invocation, error) {
+	swarmingURL := "https://" + req.SwarmingTask.Hostname
+	taskID := req.SwarmingTask.Id
+	cl := internal.HTTPClient(ctx)
+
+	swarmSvc, err := getSwarmSvc(cl, swarmingURL)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the swarming task.
+	task, err := getSwarmingTask(ctx, taskID, swarmSvc)
+	if err != nil {
+		return nil, err
+	}
+	if task.State == "PENDING" || task.State == "RUNNING" {
+		return nil, errors.Reason(
+			"cannot write Invocation of incomplete task %s on %s (%s)",
+			taskID, swarmingURL, task.State).Err()
+	}
+
+	// Check if we even need to write this invocation: is it finalized?
+	// TODO(jchinlee): Actually use returned Invocation ID.
+	_, err = getInvocationID(ctx, task, swarmSvc, req)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO(jchinlee): Get Invocation from Spanner.
+	var inv *pb.Invocation
+	if inv != nil {
+		if !pbutil.IsFinalized(inv.State) {
+			return nil, errors.Reason(
+				"attempting to derive an existing non-finalized invocation").Err()
+		}
+
+		return inv, nil
+	}
+
+	// We may need to write the invocation, so fetch the output JSON.
+	outputJSON, err := fetchOutputJSON(ctx, task)
+	if err != nil {
+		return nil, errors.Annotate(err, "task %s (%s)", taskID, swarmingURL).Err()
+	}
+
+	// Get invocation and test results using the correct format.
+	inv, _, err = convertOutputJSON(ctx, req, outputJSON)
+	if err != nil {
+		return nil, err
+	}
+
+	return inv, nil
+}
+
+func getSwarmSvc(cl *http.Client, swarmingURL string) (*swarmingAPI.Service, error) {
 	swarmSvc, err := swarmingAPI.New(cl)
 	if err != nil {
 		return nil, err
 	}
-	swarmSvc.BasePath = fmt.Sprintf("%s/%s", swarmingURL, swarmingAPIEndpoint)
 
+	swarmSvc.BasePath = fmt.Sprintf("%s/%s", swarmingURL, swarmingAPIEndpoint)
+	return swarmSvc, nil
+}
+
+// getSwarmingTask fetches the task from swarming, annotating errors with gRPC codes as needed.
+func getSwarmingTask(ctx context.Context, taskID string, swarmSvc *swarmingAPI.Service) (*swarmingAPI.SwarmingRpcsTaskResult, error) {
+	task, err := swarmSvc.Task.Result(taskID).Context(ctx).Do()
+	if err, ok := err.(*googleapi.Error); ok {
+		switch {
+		case err.Code == http.StatusUnauthorized:
+			return nil, errors.Annotate(err, "").Tag(grpcutil.UnauthenticatedTag).Err()
+		case err.Code == http.StatusForbidden:
+			return nil, errors.Annotate(err, "").Tag(grpcutil.PermissionDeniedTag).Err()
+		case err.Code == http.StatusNotFound:
+			return nil, errors.Annotate(err, "swarming task not found").Tag(grpcutil.NotFoundTag).Err()
+		case err.Code >= 500:
+			return nil, errors.Annotate(err, "swarming unavailable").Tag(
+				transient.Tag, grpcutil.InternalTag).Err()
+		}
+	}
+
+	return task, errors.Annotate(err, "").Tag(grpcutil.InternalTag).Err()
+}
+
+// fetchOutputJSON fetches the output.json from the given task on the given host.
+func fetchOutputJSON(ctx context.Context, task *swarmingAPI.SwarmingRpcsTaskResult) ([]byte, error) {
 	// Get the ref for the isolated outs of the given task.
-	ref, err := getOutputsRef(ctx, swarmSvc, taskID)
+	ref, err := getOutputsRef(ctx, task)
 	if err != nil {
 		return nil, err
 	}
 
 	// Get isolated client for getting isolated objects.
+	cl := internal.HTTPClient(ctx)
 	isoClient := isolatedclient.New(nil, cl, ref.Isolatedserver, ref.Namespace, nil, nil)
 
 	// Fetch the isolate.
 	logging.Infof(
-		ctx, "Fetching %s for isolated outs of task %s in %s", ref.Isolated, taskID, swarmingURL)
+		ctx, "Fetching %s for isolated outs of task %s", ref.Isolated, task.TaskId)
 	buf := &bytes.Buffer{}
 	if err := isoClient.Fetch(ctx, isolated.HexDigest(ref.Isolated), buf); err != nil {
 		return nil, err
@@ -70,13 +162,13 @@ func fetchOutputJSON(ctx context.Context, cl *http.Client, swarmingURL, taskID s
 	outputFile, ok := isolates.Files[outputJSONFileName]
 	if !ok {
 		return nil, errors.Reason(
-			"missing expected output %s in isolated outputs of task %s in %s",
-			outputJSONFileName, taskID, swarmingURL).Err()
+			"missing expected output %s in isolated outputs",
+			outputJSONFileName).Err()
 	}
 
 	// Now fetch (from the same server and namespace) the output file by digest.
 	logging.Infof(
-		ctx, "Fetching %s for output of task %s in %s", outputFile.Digest, taskID, swarmingURL)
+		ctx, "Fetching %s for output of task %s", outputFile.Digest, task.TaskId)
 	buf = &bytes.Buffer{}
 	if err := isoClient.Fetch(ctx, outputFile.Digest, buf); err != nil {
 		return nil, err
@@ -87,16 +179,71 @@ func fetchOutputJSON(ctx context.Context, cl *http.Client, swarmingURL, taskID s
 
 // getOutputsRef gets the data needed to fetch the isolated outputs given a task and swarming
 // service.
-func getOutputsRef(ctx context.Context, swarmSvc *swarmingAPI.Service, taskID string) (*swarmingAPI.SwarmingRpcsFilesRef, error) {
-	logging.Infof(ctx, "Fetching outputs ref for task %s (%s)", taskID, swarmSvc.BasePath)
-	taskResult, err := swarmSvc.Task.Result(taskID).Context(ctx).Do()
-	if err != nil {
-		return nil, err
+func getOutputsRef(ctx context.Context, task *swarmingAPI.SwarmingRpcsTaskResult) (*swarmingAPI.SwarmingRpcsFilesRef, error) {
+	// TODO(jchinlee): Handle BOT_DIED, which may have the below state but shouldn't error internally.
+	if task.OutputsRef == nil || task.OutputsRef.Isolated == "" {
+		return nil, errors.Reason("no isolated outputs").Err()
 	}
 
-	if taskResult.OutputsRef == nil || taskResult.OutputsRef.Isolated == "" {
-		return nil, errors.Reason("Task %s (%s) had no isolated outputs", taskID, swarmSvc.BasePath).Err()
+	return task.OutputsRef, nil
+}
+
+// getInvocationID gets the ID of the invocation associated with a task and swarming service.
+func getInvocationID(ctx context.Context, task *swarmingAPI.SwarmingRpcsTaskResult, swarmSvc *swarmingAPI.Service, req *pb.DeriveInvocationRequest) (string, error) {
+	// If the task was deduped, then the invocation associated with it is just the one associated
+	// to the task from which it was deduped.
+	var err error
+	for task.DedupedFrom != "" {
+		task, err = getSwarmingTask(ctx, task.DedupedFrom, swarmSvc)
+		if err != nil {
+			return "", err
+		}
 	}
 
-	return taskResult.OutputsRef, nil
+	// Get request information to include in ID.
+	canonicalReq, _ := proto.Clone(req).(*pb.DeriveInvocationRequest)
+	canonicalReq.SwarmingTask.Id = task.RunId
+	h := sha256.New()
+	if err := json.NewEncoder(h).Encode(canonicalReq); err != nil {
+		return "", err
+	}
+	return "from_swarming_" + hex.EncodeToString(h.Sum(nil)), nil
+}
+
+// convertOutputJSON converts the given data to Invocation and TestResults.
+//
+// It tries to convert to JSON Test Results format, then GTest format.
+func convertOutputJSON(ctx context.Context, req *pb.DeriveInvocationRequest, data []byte) (*pb.Invocation, []*pb.TestResult, error) {
+	// Validate BaseTestVariant passed in the request.
+	if err := resultdb.VariantDefMap(req.BaseTestVariant.Def).Validate(); err != nil {
+		return nil, nil, errors.Annotate(
+			err, "invalid base test variant %q", req.BaseTestVariant.Def).
+			Tag(grpcutil.InvalidArgumentTag).Err()
+	}
+
+	inv := &pb.Invocation{BaseTestVariantDef: req.BaseTestVariant}
+
+	// Try to convert the buffer treating its format as the JSON Test Results Format.
+	jsonFormat := &formats.JSONTestResults{}
+	jsonErr := jsonFormat.ConvertFromJSON(ctx, bytes.NewReader(data))
+	if jsonErr == nil {
+		results, err := jsonFormat.ToProtos(ctx, req, inv)
+		if err != nil {
+			return nil, nil, errors.Annotate(err, "converting as JSON Test Results Format").Err()
+		}
+		return inv, results, nil
+	}
+
+	// Try to convert the buffer treating its format as that of GTests.
+	gtestFormat := &formats.GTestResults{}
+	gtestErr := gtestFormat.ConvertFromJSON(ctx, bytes.NewReader(data))
+	if gtestErr == nil {
+		results, err := gtestFormat.ToProtos(ctx, req, inv)
+		if err != nil {
+			return nil, nil, errors.Annotate(err, "converting as GTest results format").Err()
+		}
+		return inv, results, nil
+	}
+
+	return nil, nil, errors.NewMultiError(jsonErr, gtestErr)
 }
--- resultdb/recorder/chromium/swarming_test.go
+++ resultdb/recorder/chromium/swarming_test.go
@@ -21,20 +21,28 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
+
+	"google.golang.org/grpc/codes"
 
 	"go.chromium.org/luci/common/isolated"
 	"go.chromium.org/luci/common/isolatedclient/isolatedfake"
+	"go.chromium.org/luci/grpc/grpcutil"
+
+	"go.chromium.org/luci/resultdb/internal"
+	"go.chromium.org/luci/resultdb/pbutil"
+	pb "go.chromium.org/luci/resultdb/proto/v1"
 
 	. "github.com/smartystreets/goconvey/convey"
 	. "go.chromium.org/luci/common/testing/assertions"
 )
 
 func TestFetchingOutputJson(t *testing.T) {
-	Convey(`Getting output JSON file`, t, func() {
-		ctx := context.Background()
+	t.Parallel()
+	ctx := context.Background()
+	ctx = internal.WithHTTPClient(ctx, http.DefaultClient)
 
+	Convey(`Getting output JSON file`, t, func() {
 		// Set up fake isolatedserver.
 		isoFake := isolatedfake.New()
 		isoServer := httptest.NewServer(isoFake)
@@ -62,37 +70,201 @@ func TestFetchingOutputJson(t *testing.T) {
 			path := r.URL.Path
 
 			var resp string
-			switch {
-			case strings.Contains(path, "successful-task"):
+			switch path {
+			case fmt.Sprintf("/%stask/successful-task/result", swarmingAPIEndpoint):
 				resp = fmt.Sprintf(
 					`{"outputs_ref": {"isolatedserver": "%s", "namespace": "ns", "isolated": "%s"}}`,
 					isoServer.URL, outputsDigest)
-			case strings.Contains(path, "no-output-file-task"):
+			case fmt.Sprintf("/%stask/no-output-file-task/result", swarmingAPIEndpoint):
 				resp = fmt.Sprintf(
 					`{"outputs_ref": {"isolatedserver": "%s", "namespace": "ns", "isolated": "%s"}}`,
 					isoServer.URL, badOutputsDigest)
 			default:
-				resp = fmt.Sprintf(`{"outputs_ref": {}}`)
+				resp = `{"outputs_ref": {}}`
 			}
 
 			io.WriteString(w, resp)
 		}))
 		defer swarmingFake.Close()
 
+		swarmSvc, err := getSwarmSvc(http.DefaultClient, swarmingFake.URL)
+		So(err, ShouldBeNil)
+
 		Convey(`successfully gets output file`, func() {
-			buf, err := fetchOutputJSON(ctx, http.DefaultClient, swarmingFake.URL, "successful-task")
+			task, err := swarmSvc.Task.Result("successful-task").Context(ctx).Do()
+			So(err, ShouldBeNil)
+
+			buf, err := fetchOutputJSON(ctx, task)
 			So(err, ShouldBeNil)
 			So(buf, ShouldResemble, []byte("f00df00d"))
 		})
 
 		Convey(`errs if no outputs`, func() {
-			_, err := fetchOutputJSON(ctx, http.DefaultClient, swarmingFake.URL, "no-outputs-task")
-			So(err, ShouldErrLike, "had no isolated outputs")
+			task, err := swarmSvc.Task.Result("no-outputs-task").Context(ctx).Do()
+			So(err, ShouldBeNil)
+
+			_, err = fetchOutputJSON(ctx, task)
+			So(err, ShouldErrLike, "no isolated outputs")
 		})
 
 		Convey(`errs if no output file`, func() {
-			_, err := fetchOutputJSON(ctx, http.DefaultClient, swarmingFake.URL, "no-output-file-task")
+			task, err := swarmSvc.Task.Result("no-output-file-task").Context(ctx).Do()
+			So(err, ShouldBeNil)
+
+			_, err = fetchOutputJSON(ctx, task)
 			So(err, ShouldErrLike, "missing expected output output.json in isolated outputs")
+		})
+	})
+
+	Convey(`handles Swarming errors`, t, func() {
+		swarmingFake := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			path := r.URL.Path
+
+			switch path {
+			case fmt.Sprintf("/%stask/200-task/result", swarmingAPIEndpoint):
+				w.WriteHeader(http.StatusOK)
+				io.WriteString(w, `{"outputs_ref": {}}`)
+			case fmt.Sprintf("/%stask/5xx-task/result", swarmingAPIEndpoint):
+				w.WriteHeader(http.StatusInternalServerError)
+			default:
+				w.WriteHeader(http.StatusNotFound)
+			}
+
+		}))
+		defer swarmingFake.Close()
+
+		swarmSvc, err := getSwarmSvc(http.DefaultClient, swarmingFake.URL)
+		So(err, ShouldBeNil)
+
+		Convey(`does not error for 200`, func() {
+			_, err := getSwarmingTask(ctx, "200-task", swarmSvc)
+			So(err, ShouldBeNil)
+		})
+
+		Convey(`tags with gRPC NotFound for 404`, func() {
+			_, err := getSwarmingTask(ctx, "404-task", swarmSvc)
+			So(err, ShouldNotBeNil)
+			So(grpcutil.Code(err), ShouldEqual, codes.NotFound)
+		})
+
+		Convey(`tags with gRPC Internal for 5xx`, func() {
+			_, err := getSwarmingTask(ctx, "5xx-task", swarmSvc)
+			So(err, ShouldNotBeNil)
+			So(grpcutil.Code(err), ShouldEqual, codes.Internal)
+		})
+	})
+
+	req := &pb.DeriveInvocationRequest{
+		SwarmingTask: &pb.DeriveInvocationRequest_SwarmingTask{
+			Hostname: "host-swarming",
+			Id:       "123",
+		},
+		TestPathPrefix: "prefix/",
+		BaseTestVariant: &pb.VariantDef{Def: map[string]string{
+			"bucket":     "bkt",
+			"builder":    "blder",
+			"test_suite": "foo_unittests",
+		}},
+	}
+
+	Convey(`Getting Invocation ID`, t, func() {
+		swarmingFake := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			path := r.URL.Path
+
+			var resp string
+			switch path {
+			case fmt.Sprintf("/%stask/deduped-task/result", swarmingAPIEndpoint):
+				resp = `{"deduped_from" : "first-task", "run_id": "123410"}`
+			case fmt.Sprintf("/%stask/first-task/result", swarmingAPIEndpoint):
+				resp = `{"run_id": "abcd12"}`
+			default:
+				resp = `{}`
+			}
+
+			io.WriteString(w, resp)
+		}))
+		defer swarmingFake.Close()
+
+		swarmSvc, err := getSwarmSvc(http.DefaultClient, swarmingFake.URL)
+		So(err, ShouldBeNil)
+
+		Convey(`for non-deduped task`, func() {
+			req.SwarmingTask.Id = "first-task"
+			task, err := swarmSvc.Task.Result(req.SwarmingTask.Id).Context(ctx).Do()
+			So(err, ShouldBeNil)
+
+			id, err := getInvocationID(ctx, task, swarmSvc, req)
+			So(err, ShouldBeNil)
+			So(id, ShouldEqual,
+				"from_swarming_1273df315bb3d4933c67a1bdc23164d6bb17222b5ff487cf1ce6aa4e7c6369da")
+		})
+
+		Convey(`for deduped task`, func() {
+			req.SwarmingTask.Id = "deduped-task"
+			task, err := swarmSvc.Task.Result(req.SwarmingTask.Id).Context(ctx).Do()
+			So(err, ShouldBeNil)
+
+			id, err := getInvocationID(ctx, task, swarmSvc, req)
+			So(err, ShouldBeNil)
+			So(id, ShouldEqual,
+				"from_swarming_1273df315bb3d4933c67a1bdc23164d6bb17222b5ff487cf1ce6aa4e7c6369da")
+		})
+	})
+
+	Convey(`Converting output JSON`, t, func() {
+		Convey(`chooses JSON Test Results Format correctly`, func() {
+			buf := []byte(
+				`{
+					"version": 3,
+					"tests": {
+						"c1": {
+							"c2": {
+								"t1.html": {
+									"actual": "PASS PASS PASS",
+									"expected": "PASS",
+									"time": 0.3,
+									"times": [ 0.3, 0.2, 0.1 ]
+								}
+							}
+						}
+					}
+				}`)
+			inv, _, err := convertOutputJSON(ctx, req, buf)
+			So(err, ShouldBeNil)
+			So(inv, ShouldNotBeNil)
+			So(inv.Tags, ShouldResembleProto, pbutil.StringPairs("test_framework", "json"))
+		})
+
+		Convey(`chooses GTest format correctly`, func() {
+			buf := []byte(
+				`{
+					"all_tests": [ "FooTest.TestDoBar" ],
+					"per_iteration_data": [{
+						"FooTest.TestDoBar": [
+							{
+								"elapsed_time_ms": 1837,
+								"losless_snippet": true,
+								"output_snippet": "[ RUN      ] FooTest.TestDoBar",
+								"output_snippet_base64": "WyBSVU4gICAgICBdIEZvb1Rlc3QuVGVzdERvQmFy",
+								"status": "CRASH"
+							}
+						]
+					}]
+				}`)
+			inv, _, err := convertOutputJSON(ctx, req, buf)
+			So(err, ShouldBeNil)
+			So(inv, ShouldNotBeNil)
+			So(inv.Tags, ShouldResembleProto, pbutil.StringPairs("test_framework", "gtest"))
+		})
+
+		Convey(`errors on neither`, func() {
+			buf := []byte(
+				`{
+					"all_tests": "not GTest format",
+					"version": "not JSON Test Results format"
+				}`)
+			_, _, err := convertOutputJSON(ctx, req, buf)
+			So(err, ShouldErrLike, `(and 1 other error)`)
 		})
 	})
 }
