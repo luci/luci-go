2cc2813c8c205820cd0bcbe028058d4c9ce80970
Xixuan Wu <xixuan@google.com>
2019-10-07 17:56:49 -0700

    gerrit: set Content-Type: application/json for deleting request.
    Gerrit call get unexpected HTTP 400 response:
    https://screenshot.googleplex.com/eDwh8steCrg
    
    It's probably because
    https://gerrit-review.googlesource.com/Documentation/rest-api.html#_400_bad_request,
    json input is expected but we pass in others.
    
    Switch to use call() to force input content of header is application/json.
    
    BUG=chromium:1008442
    TEST=None
    
    Change-Id: I090eed9a897742bc7a6e9fe541bb71fb5e0c7327

--- common/api/gerrit/rest.go
+++ common/api/gerrit/rest.go
@@ -248,7 +248,9 @@ func (c *client) ChangeEditFileContent(ctx context.Context, req *gerritpb.Change
 
 func (c *client) DeleteEditFileContent(ctx context.Context, req *gerritpb.DeleteEditFileContentRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
 	path := fmt.Sprintf("/changes/%s/edit/%s", gerritChangeIDForRouting(req.Number, req.Project), url.PathEscape(req.FilePath))
-	if _, _, err := c.callRaw(ctx, "DELETE", path, url.Values{}, textInputHeaders(), []byte{}, http.StatusNoContent); err != nil {
+	var data struct{}
+	var resp changeInfo
+	if _, err := c.call(ctx, "DELETE", path, url.Values{}, &data, &resp, http.StatusNoContent); err != nil {
 		return nil, errors.Annotate(err, "delete edit file content").Err()
 	}
 	return &empty.Empty{}, nil
