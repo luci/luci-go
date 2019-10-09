db3a8ee770853304a866b7fdd6f36796733d60fb
Nodir Turakulov <nodir@google.com>
2019-10-09 16:31:11 -0700

    [resultdb] Implement OverrideInclusion RPC
    This is probably the most complicated RPC that we have,
    it has a lot of edge cases, which are all handled in this CL:
    - any of invocations do not exist
    - the including invocation is already finalized
    - the inclusion being overridden is already overridden
    - the inclusion being overridden is already overridden by the same
      inclusion we are trying to override
    - compute "Ready" of both inclusions
    - create the new inclusion if it does not exist yet
    
    R=chanli@chromium.org, jchinlee@chromium.org
    
    Change-Id: Id7cfba2421c214b764cc72f91b85207a2b178610

---
+++ resultdb/recorder/override_invocation.go
@@ -0,0 +1,148 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"golang.org/x/sync/errgroup"
+
+	"cloud.google.com/go/spanner"
+	"google.golang.org/grpc/codes"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/grpc/grpcutil"
+
+	"go.chromium.org/luci/resultdb/internal/span"
+	"go.chromium.org/luci/resultdb/pbutil"
+	pb "go.chromium.org/luci/resultdb/proto/v1"
+)
+
+var errRepeatedRequest = fmt.Errorf("this request was handled before")
+
+// OverrideInclusion implements pb.RecorderServer.
+func (s *RecorderServer) OverrideInclusion(ctx context.Context, in *pb.OverrideInclusionRequest) (*pb.OverrideInclusionResponse, error) {
+	if err := pbutil.ValidateOverrideInclusionRequest(in); err != nil {
+		return nil, errors.Annotate(err, "bad request").Tag(grpcutil.InvalidArgumentTag).Err()
+	}
+	var (
+		includingInvID  = pbutil.MustParseInvocationName(in.IncludingInvocation)
+		overridingInvID = pbutil.MustParseInvocationName(in.OverridingIncludedInvocation)
+		overriddenInvID = pbutil.MustParseInvocationName(in.OverriddenIncludedInvocation)
+	)
+
+	// Check permissions before opening a RW txn.
+	client := span.Client(ctx)
+	if err := mayMutateInvocation(ctx, client.Single(), includingInvID); err != nil {
+		return nil, err
+	}
+
+	ret := &pb.OverrideInclusionResponse{
+		OverriddenInclusion: &pb.Inclusion{
+			Name:               pbutil.InclusionName(includingInvID, overriddenInvID),
+			IncludedInvocation: in.OverriddenIncludedInvocation,
+		},
+		OverridingInclusion: &pb.Inclusion{
+			Name:               pbutil.InclusionName(includingInvID, overridingInvID),
+			IncludedInvocation: in.OverridingIncludedInvocation,
+		},
+	}
+	_, err := span.Client(ctx).ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		eg, ctx := errgroup.WithContext(ctx)
+
+		// Check invocation state again.
+		eg.Go(func() error {
+			return mayMutateInvocation(ctx, txn, includingInvID)
+		})
+
+		// Compute ret.{Overriding,Overridden}Inclusion.Ready
+		computeReady := func(inc *pb.Inclusion, invID string) {
+			eg.Go(func() error {
+				state, err := readInvocationState(ctx, txn, invID)
+				if err != nil {
+					return err
+				}
+				inc.Ready = pbutil.IsFinalized(state)
+				return nil
+			})
+		}
+		computeReady(ret.OverriddenInclusion, overriddenInvID)
+		computeReady(ret.OverridingInclusion, overridingInvID)
+
+		// Ensure the overridden inclusion exists and not overridden already.
+		eg.Go(func() error {
+			return checkOverridingInclusion(ctx, txn, includingInvID, overriddenInvID, overridingInvID)
+		})
+
+		switch err := eg.Wait(); {
+		case err == errRepeatedRequest:
+			// No need to write anything.
+			return nil
+
+		case err != nil:
+			return err
+		}
+
+		return txn.BufferWrite([]*spanner.Mutation{
+			// Mark the existing inclusion as overridden.
+			spanner.UpdateMap("Inclusions", map[string]interface{}{
+				"InvocationID":                     includingInvID,
+				"IncludedInvocationID":             overriddenInvID,
+				"OverriddenByIncludedInvocationID": overridingInvID,
+				"Ready":                            ret.OverriddenInclusion.Ready,
+			}),
+			// Ensure the overriding inclusion exists.
+			spanner.InsertOrUpdateMap("Inclusions", map[string]interface{}{
+				"InvocationID":         includingInvID,
+				"IncludedInvocationID": overridingInvID,
+				"Ready":                ret.OverridingInclusion.Ready,
+			}),
+		})
+	})
+	return ret, err
+}
+
+// checkOverridingInclusion checks whether the inclusion being overridden already exists
+// and it is not overridden by any other inclusion.
+// If it is already overridden by overridingInvID, returns errRepeatedRequest.
+func checkOverridingInclusion(ctx context.Context, txn *spanner.ReadWriteTransaction, includingInvID, overriddenInvID, overridingInvID string) error {
+	var actualOverridingInvID spanner.NullString
+	switch err := span.ReadRow(ctx, txn, "Inclusions", spanner.Key{includingInvID, overriddenInvID}, map[string]interface{}{"OverriddenByIncludedInvocationID": &actualOverridingInvID}); {
+
+	case spanner.ErrCode(err) == codes.NotFound:
+		return errors.Reason("inclusion %q not found", pbutil.InclusionName(includingInvID, overriddenInvID)).
+			InternalReason("%s", err).
+			Tag(grpcutil.NotFoundTag).
+			Err()
+
+	case err != nil:
+		return err
+
+	case !actualOverridingInvID.Valid:
+		// The inclusion is not overridden.
+		return nil
+
+	case actualOverridingInvID.StringVal == overridingInvID:
+		// This makes this request idempotent.
+		return errRepeatedRequest
+
+	default:
+		return errors.
+			Reason("inclusion %q is already overridden by %q", pbutil.InclusionName(includingInvID, overriddenInvID), pbutil.InclusionName(includingInvID, actualOverridingInvID.StringVal)).
+			Tag(grpcutil.FailedPreconditionTag).
+			Err()
+	}
+}
