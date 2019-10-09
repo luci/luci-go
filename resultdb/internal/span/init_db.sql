7b8e844beb1242d3a8c6de88ea11b4098bbad7d3
Nodir Turakulov <nodir@google.com>
2019-10-09 21:26:35 +0000

    [resultdb] Add a Spanner testing framework
    Add common/spantest package that helps testing applications against
    a real Spanner database. Its primary API for now is creation and
    destruction of a temporary database.
    
    Accordingly, add resultdb-specific resultdb/internal/testutil package
    which creates a temp database with all ResultDB tables on test binary
    startup, as well as a spanner client, runs tests and destroys the
    database. Database creation takes ~5s, the first test takes long time,
    but the rest are <=0.2s. Tests run sequentially and cleanup database
    on startup.
    
    Tests that interact with spanner are executed only if INTEGRATION_TEST=1
    env var is present. Otherwise these tests skip themselves.
    
    Extend hardcoded/chromeinfra/chromeinfra.go to include the name of the chops-wide
    spanner instance designed for testing.
    
    Add resultdb/internal/span/init_db.sql which keeps database schema
    under source control. The script is tested because it is used to
    create the database.
    
    Bug: 1009519
    Change-Id: I8e9a1c68b2b823cd5e0d5252e25b74de2cdf57a1
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1832385
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Jao-ke Chin-Lee <jchinlee@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

---
+++ resultdb/internal/span/init_db.sql
@@ -0,0 +1,186 @@
+-- Copyright 2019 The LUCI Authors.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+--------------------------------------------------------------------------------
+-- This script initializes a ResultDB Spanner database.
+
+-- Stores the invocations.
+-- This is the root table for much of the other data and tables, which define the
+-- hierarchy (dependency graph, subsets of interest) for invocations.
+CREATE TABLE Invocations (
+  -- Users provide their own IDs, which are hashed and concatenated with the
+  -- origin ID to improve sharding.
+  InvocationId STRING(MAX) NOT NULL,
+
+  -- Invocation state, see InvocationState in invocation.proto
+  State INT64 NOT NULL,
+
+  -- Security realm this invocation belongs to.
+  Realm STRING(64) NOT NULL,  -- used to enforce ACLs
+
+  -- When to delete the invocation from the table.
+  InvocationExpirationTime TIMESTAMP NOT NULL,
+
+  -- InvocationExpirationTime truncated to a week.
+  InvocationExpirationWeek TIMESTAMP NOT NULL,  -- used for indexing
+
+  -- When to delete expected test results from this invocation.
+  ExpectedTestResultsExpirationTime TIMESTAMP NOT NULL,
+
+  -- ExpectedTestResultsExpirationTime truncated to a week.
+  ExpectedTestResultsExpirationWeek TIMESTAMP NOT NULL,  -- used for indexing
+
+  -- Secret token that a client must provide to mutate this or any of the
+  -- interleaved rows.
+  UpdateToken STRING(64),
+
+  -- When the invocation was created.
+  CreateTime TIMESTAMP NOT NULL,
+
+  -- When the invocation was finalized.
+  FinalizeTime TIMESTAMP,
+
+  -- When to force invocation finalization with state INTERRUPTED.
+  Deadline TIMESTAMP,
+
+  -- Variant definition pairs common for all test results in this invocation.
+  -- Each string is a colon-separated key-value pair.
+  -- A particular test result can have additional key-value pairs.
+  BaseTestVariantDef ARRAY<STRING(MAX)>,
+
+  -- List of colon-separated key-value tags.
+  -- Corresponds to Invocation.tags in invocation.proto.
+  -- For each tag here, there is a row in the InvocationsByTag table.
+  Tags ARRAY<STRING(MAX)>
+) PRIMARY KEY (InvocationId);
+
+-- Index of invocations by expiration week.
+-- Used by a cron job that periodically removes expired invocations.
+CREATE INDEX InvocationsByInvocationExpirationWeek ON Invocations (InvocationExpirationWeek, InvocationId);
+
+-- Index of invocations by expected test result expiration week.
+-- Used by a cron job that periodically removes expected test results.
+CREATE INDEX InvocationsByExpectedTestResultsExpirationWeek ON Invocations (ExpectedTestResultsExpirationWeek, InvocationId);
+
+-- Index of invocations by a tag.
+CREATE TABLE InvocationsByTag (
+  -- Format: "${sha256_hex(tag)}_${key}:${value}".
+  TagId STRING(MAX) NOT NULL,
+
+  -- FK to Invocations.InvocationId.
+  InvocationId STRING(MAX) NOT NULL
+) PRIMARY KEY (TagId, InvocationId);
+
+-- Stores "inclusion" edges between invocations.
+-- Interleaved in Invocations table.
+CREATE TABLE Inclusions (
+  -- ID of the including invocation, the "source" node of the edge.
+  InvocationId STRING(MAX) NOT NULL,
+
+  -- ID of the included invocation, the "target" node of the edge.
+  IncludedInvocationId STRING(MAX) NOT NULL,
+
+  -- If not NULL, this inclusion is overridden by inclusion
+  -- (InvocationId, OverridenByIncludedInvocationID).
+  OverridenByIncludedInvocationID STRING (MAX),
+
+  -- Whether the included invocation is finalized before the including one.
+  -- See also Inclusion.ready in invocation.proto
+  Ready BOOL,
+) PRIMARY KEY (InvocationId, IncludedInvocationId),
+  INTERLEAVE IN PARENT Invocations ON DELETE CASCADE;
+
+-- Stores test results. Interleaved in Invocations.
+CREATE TABLE TestResults (
+  -- ID of the parent Invocations row.
+  InvocationId STRING(MAX) NOT NULL,
+
+  -- Unique identifier of the test,
+  -- see also TestResult.test_path in test_result.proto.
+  TestPath STRING(MAX) NOT NULL,
+
+  -- A suffix for PK to allow multiple test results for the same test path in
+  -- a given invocation.
+  -- Generated on the server.
+  ResultId STRING(MAX) NOT NULL,
+
+  -- Extra variant pairs, in addition to parent Invocation.BaseTestVariantDef.
+  -- See also TestResult.extra_variant_pairs in test_result.proto.
+  ExtraVariantPairs ARRAY<STRING(MAX)>,
+
+  -- Last time this row was modified.
+  -- Given that we only create and delete row, for an existing row this equals
+  -- row creation time.
+  CommitTimestamp TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true),
+
+  -- Whether the test status was unexpected
+  -- MUST either be either NULL or False, to keep null-filtered index below
+  -- thin.
+  IsUnexpected BOOL,
+
+  -- Test status, see TestStatus in test_result.proto.
+  Status INT64 NOT NULL,
+
+  -- Summary of the test result for humans, in Markdown.
+  SummaryMarkdown STRING(MAX),
+
+  -- When the test execution started.
+  StartTime TIMESTAMP,
+
+  -- How long the test execution took, in microseconds.
+  RunDurationUsec INT64,
+
+  -- Tags associated with the test result, for example GTest-specific test
+  -- status.
+  Tags ARRAY<STRING(MAX)>,
+
+  -- Input artifacts, see also TestResult.input_artifacts in test_result.proto.
+  -- Each element is Artifact message encoded in bytes.
+  InputArtifacts ARRAY<BYTES(MAX)>,
+
+  -- Output artifacts, see also TestResult.output_artifacts in test_result.proto.
+  -- Each element is Artifact message encoded in bytes.
+  OutputArtifacts ARRAY<BYTES(MAX)>
+) PRIMARY KEY (InvocationId, TestPath, ResultId),
+  INTERLEAVE IN PARENT Invocations ON DELETE CASCADE;
+
+-- Unexpected test results for each invocation.
+-- It is significantly smaller (<2%) than TestResult table and should be used
+-- for most queries.
+-- It includes TestPath to be able to find all unexpected test result with a
+-- given test path or a test path prefix.
+CREATE NULL_FILTERED INDEX UnexpectedTestResults ON TestResults (InvocationId, TestPath, IsUnexpected),
+  INTERLEAVE IN Invocations;
+
+
+-- Stores test exonerations, see TestExoneration in test_result.proto
+CREATE TABLE TestExonerations (
+  -- ID of the parent Invocations row.
+  InvocationId STRING(MAX) NOT NULL,
+
+  -- The exoneration applies only to test results with this exact test path.
+  -- This is a foreign key to TestResults.TestPath column.
+  TestPath STRING(MAX) NOT NULL,
+
+  -- Server-generated exoneration ID, to allow multiple test exonerations per
+  -- test path in a given invocation.
+  ExonerationId INT64 NOT NULL,
+
+  -- The exoneration applies only to test results with this exact test variant.
+  VariantDef ARRAY<STRING(MAX)> NOT NULL,
+
+  -- Explanation of the exoneration for humans, in Markdown.
+  ExplanationMarkdown STRING(MAX),
+) PRIMARY KEY (InvocationId, TestPath, ExonerationId),
+  INTERLEAVE IN PARENT Invocations ON DELETE CASCADE;
---
+++ resultdb/internal/testutil/spantest.go
@@ -0,0 +1,166 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testutil
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/spanner"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/spantest"
+	"go.chromium.org/luci/resultdb/internal/span"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+// IntegrationTestEnvVar is the name of the environment variable which controls
+// whether spanner tests are executed.
+// The value must be "1" for integration tests to run.
+const IntegrationTestEnvVar = "INTEGRATION_TESTS"
+
+// RunIntegrationTests returns true if integration tests should run.
+func RunIntegrationTests() bool {
+	return os.Getenv(IntegrationTestEnvVar) == "1"
+}
+
+var spannerClient *spanner.Client
+
+// SpannerTestContext returns a context for testing code that talks to Spanner.
+// Skips the test if integration tests are not enabled.
+//
+// Tests that use Spanner must not call t.Parallel().
+func SpannerTestContext(t *testing.T) context.Context {
+	switch {
+	case !RunIntegrationTests():
+		t.Skipf("env var %s=1 is missing", IntegrationTestEnvVar)
+	case spannerClient == nil:
+		t.Fatalf("spanner client is not initialized; forgot to call SpannerTestMain?")
+	}
+
+	ctx := context.Background()
+	err := cleanupDatabase(ctx, spannerClient)
+	So(err, ShouldBeNil)
+
+	return span.WithClient(ctx, spannerClient)
+}
+
+// findInitScript returns path //resultdb/internal/span/init_db.sql.
+func findInitScript() (string, error) {
+	ancestor, err := filepath.Abs(".")
+	if err != nil {
+		return "", err
+	}
+
+	for {
+		scriptPath := filepath.Join(ancestor, "internal", "span", "init_db.sql")
+		_, err := os.Stat(scriptPath)
+		if os.IsNotExist(err) {
+			parent := filepath.Dir(ancestor)
+			if parent == ancestor {
+				return "", errors.Reason("init_db.sql not found").Err()
+			}
+			ancestor = parent
+			continue
+		}
+
+		return scriptPath, err
+	}
+}
+
+// SpannerTestMain is a test main function for packages that have tests that
+// talk to spanner. It creates/destroys a temporary spanner database
+// before/after running tests.
+//
+// This function never returns. Instead it calls os.Exit with the value returned
+// by m.Run().
+func SpannerTestMain(m *testing.M) {
+	exitCode, err := spannerTestMain(m)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	os.Exit(exitCode)
+}
+
+func spannerTestMain(m *testing.M) (exitCode int, err error) {
+	testing.Init()
+
+	if RunIntegrationTests() {
+		// Find init_db.sql
+		initScriptPath, err := findInitScript()
+		if err != nil {
+			return 0, err
+		}
+
+		// Create a Spanner database.
+		ctx := context.Background()
+		start := time.Now()
+		db, err := spantest.NewTempDB(ctx, spantest.TempDBConfig{InitScriptPath: initScriptPath})
+		if err != nil {
+			return 0, errors.Annotate(err, "failed to create a temporary Spanner database").Err()
+		}
+		fmt.Printf("created a temporary Spanner database %s in %s\n", db.Name, time.Since(start))
+
+		defer func() {
+			switch dropErr := db.Drop(ctx); {
+			case dropErr == nil:
+
+			case err == nil:
+				err = dropErr
+
+			default:
+				fmt.Fprintf(os.Stderr, "failed to drop the database: %s\n", dropErr)
+			}
+		}()
+
+		// Create a global Spanner client.
+		spannerClient, err = db.Client(ctx)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return m.Run(), nil
+}
+
+// cleanupDatabase deletes all data from all tables.
+func cleanupDatabase(ctx context.Context, client *spanner.Client) error {
+	_, err := client.Apply(ctx, []*spanner.Mutation{
+		// All other tables are interleaved in Invocations table.
+		spanner.Delete("Invocations", spanner.AllKeys()),
+	})
+	return err
+}
+
+// MustApply applies the mutations to the spanner client in the context.
+// Asserts that application succeeds.
+func MustApply(ctx context.Context, ms ...*spanner.Mutation) {
+	_, err := span.Client(ctx).Apply(ctx, ms)
+	So(err, ShouldBeNil)
+}
+
+func fatalIf(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
