13cda57379ad2a9b80d4dc7af84acc6eec897609
Nodir Turakulov <nodir@google.com>
2019-10-09 09:08:36 -0700

    [resultdb] Add a Spanner testing framework
    Add common/spantest package that helps testing applications against
    a real Spanner database. Its primary API for now is creation and
    destruction of a temporary database.
    
    Accordingly, add resultdb-specific resultdb/internal/testutil package
    which creates a temp database with all ResultDB tables on test binary
    startup, as well as a spanner client, runs tests and destroys the
    database. Database creation takes ~5s, the first test takes long time,
    but the rest are <=0.2s. Tests run sequentially and cleanup database
    on startup.
    
    Tests that interact with spanner are executed only if INTEGRATION_TEST=1
    env var is present. Otherwise these tests skip themselves.
    
    Extend hardcoded/chromeinfra/chromeinfra.go to include the name of the chops-wide
    spanner instance designed for testing.
    
    Add resultdb/internal/span/init_db.sql which keeps database schema
    under source control. The script is tested because it is used to
    create the database.
    
    Bug: 1009519
    Change-Id: I8e9a1c68b2b823cd5e0d5252e25b74de2cdf57a1

---
+++ common/spantest/spantest.go
@@ -0,0 +1,182 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package spantest implements creation/destruction of a temporary Spanner
+// database.
+package spantest
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"os/user"
+	"regexp"
+	"strings"
+
+	"cloud.google.com/go/spanner"
+	spandb "cloud.google.com/go/spanner/admin/database/apiv1"
+	"golang.org/x/oauth2"
+	"google.golang.org/api/option"
+	dbpb "google.golang.org/genproto/googleapis/spanner/admin/database/v1"
+
+	"go.chromium.org/luci/auth"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/hardcoded/chromeinfra"
+)
+
+// TempDBConfig specifies how to create a temporary database.
+type TempDBConfig struct {
+	// InstanceName is the name of Spannner instance where to create the
+	// temporary database.
+	// Format: projects/{project}/instances/{instance}.
+	// Defaults to chromeinfra.TestSpannerInstance.
+	InstanceName string
+
+	// TokenSource will be used to authenticate to Spanner.
+	// If nil, auth.Authenticator with SilentLogin and chrome-infra auth options
+	// will be used.
+	// This means that that the user may have to login with luci-auth tool.
+	TokenSource oauth2.TokenSource
+
+	// InitScriptPath is a path to a DDL script to initialize the database.
+	//
+	// In lieu of a proper DDL parser, it is parsed using regexes.
+	// Therefore the script MUST:
+	//   - Use `#`` and/or `--`` for comments. No block comments.
+	//   - Separate DDL statements with `;\n`.
+	//
+	// If empty, the database is created with no tables.
+	InitScriptPath string
+}
+
+func (cfg *TempDBConfig) tokenSource(ctx context.Context) (oauth2.TokenSource, error) {
+	if cfg.TokenSource != nil {
+		return cfg.TokenSource, nil
+	}
+
+	opts := chromeinfra.DefaultAuthOptions()
+	opts.Scopes = spandb.DefaultAuthScopes()
+	a := auth.NewAuthenticator(ctx, auth.SilentLogin, opts)
+	if err := a.CheckLoginRequired(); err != nil {
+		return nil, errors.Annotate(err, "please login with `luci-auth login -scopes %q`", strings.Join(opts.Scopes, " ")).Err()
+	}
+	return a.TokenSource()
+}
+
+var ddlStatementSepRe = regexp.MustCompile(`;\s*\n`)
+var commentRe = regexp.MustCompile(`(--|#)[^\n]*`)
+
+// readDDLStatements read the file at cfg.InitScriptPath as a sequence of DDL
+// statements. If the path is empty, returns (nil, nil).
+func (cfg *TempDBConfig) readDDLStatements() ([]string, error) {
+	if cfg.InitScriptPath == "" {
+		return nil, nil
+	}
+
+	contents, err := ioutil.ReadFile(cfg.InitScriptPath)
+	if err != nil {
+		return nil, err
+	}
+
+	statements := ddlStatementSepRe.Split(string(contents), -1)
+	ret := statements[:0]
+	for _, stmt := range statements {
+		stmt = commentRe.ReplaceAllString(stmt, "")
+		stmt = strings.TrimSpace(stmt)
+		if stmt != "" {
+			ret = append(ret, stmt)
+		}
+	}
+	return ret, nil
+}
+
+// TempDB is a temporary Spanner database.
+type TempDB struct {
+	Name string
+	ts   oauth2.TokenSource
+}
+
+// Client returns a spanner client connected to the database.
+func (db *TempDB) Client(ctx context.Context) (*spanner.Client, error) {
+	return spanner.NewClient(ctx, db.Name, option.WithTokenSource(db.ts))
+}
+
+// Drop deletes the database.
+func (db *TempDB) Drop(ctx context.Context) error {
+	client, err := spandb.NewDatabaseAdminClient(ctx, option.WithTokenSource(db.ts))
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	return client.DropDatabase(ctx, &dbpb.DropDatabaseRequest{
+		Database: db.Name,
+	})
+}
+
+// NewTempDB creates a temporary database with a random name.
+// The caller is responsible for calling Drop on the returned TempDB to
+// cleanup resources after usage.
+func NewTempDB(ctx context.Context, cfg TempDBConfig) (*TempDB, error) {
+	instanceName := cfg.InstanceName
+	if instanceName == "" {
+		instanceName = chromeinfra.TestSpannerInstance
+	}
+
+	ts, err := cfg.tokenSource(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	initStatements, err := cfg.readDDLStatements()
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to read %q", cfg.InitScriptPath).Err()
+	}
+
+	client, err := spandb.NewDatabaseAdminClient(ctx, option.WithTokenSource(ts))
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	// Generate a random database name.
+	var random uint32
+	if err := binary.Read(rand.Reader, binary.LittleEndian, &random); err != nil {
+		panic(err)
+	}
+	dbName := fmt.Sprintf("test%d", random)
+	if u, err := user.Current(); err == nil && u.Username != "" {
+		dbName += "_by_" + u.Username
+	}
+
+	dbOp, err := client.CreateDatabase(ctx, &dbpb.CreateDatabaseRequest{
+		Parent:          instanceName,
+		CreateStatement: "CREATE DATABASE " + dbName,
+		ExtraStatements: initStatements,
+	})
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to create database").Err()
+	}
+	db, err := dbOp.Wait(ctx)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to create database").Err()
+	}
+
+	return &TempDB{
+		Name: db.Name,
+		ts:   ts,
+	}, nil
+}
