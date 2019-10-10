55c3437961e77248f76ef9cf837306da27771971
Andrii Shyshkalov <tandrii@google.com>
2019-10-10 01:56:11 -0700

    buildbucket: Update helper script to save time to future self and others.
    R=nodir
    
    Change-Id: I2135a116eddc06787a4302b286e663b9b620a143

--- buildbucket/proto/update_tables.sh
+++ buildbucket/proto/update_tables.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+set -e
+
 # This little script is just to remember the incantation to update the bigquery
 # schema. If you don't know what this is, you don't need to run it (and likely
 # don't have permission to anyhow).
-bqschemaupdater -message buildbucket.v2.Build -table cr-buildbucket.raw.completed_builds
+
+# Fist, make sure you run up-to-date bqschemaupdater,
+# since it's not installed by default in Infra's Go Env.
+go install go.chromium.org/luci/tools/cmd/bqschemaupdater
+
+# Actually update BQ schemas, -dev before prod.
 bqschemaupdater -message buildbucket.v2.Build -table cr-buildbucket-dev.raw.completed_builds
+bqschemaupdater -message buildbucket.v2.Build -table cr-buildbucket.raw.completed_builds
