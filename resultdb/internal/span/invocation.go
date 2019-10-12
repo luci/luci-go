177271394f8526a55d8c5d1abbe19a57420a1192
Nodir Turakulov <nodir@chromium.org>
2019-10-11 18:55:16 -0700

    [resultdb] Implement Include RPC
    Use the new spanner testing framework.
    
    Also add a few spanner helper functions and a fix a typo in init_db.sql
    
    R=chanli@chromium.org, jchinlee@chromium.org
    
    Change-Id: Id7cfba2421c214b764cc72f91b85207a2b178610

---
+++ resultdb/internal/span/invocation.go
@@ -0,0 +1,48 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package span
+
+import (
+	"context"
+
+	"cloud.google.com/go/spanner"
+	"google.golang.org/grpc/codes"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/grpc/grpcutil"
+
+	"go.chromium.org/luci/resultdb/pbutil"
+)
+
+// ReadInvocation reads one invocation within the transaction.
+// If the invocation does not exist, the returned error is annotated with
+// NotFound GRPC code.
+// For ptrMap see ReadRow comment in util.go.
+func ReadInvocation(ctx context.Context, txn Txn, invID string, ptrMap map[string]interface{}) error {
+	err := ReadRow(ctx, txn, "Invocations", spanner.Key{invID}, ptrMap)
+	switch {
+	case spanner.ErrCode(err) == codes.NotFound:
+		return errors.Reason("%q not found", pbutil.InvocationName(invID)).
+			InternalReason("%s", err).
+			Tag(grpcutil.NotFoundTag).
+			Err()
+
+	case err != nil:
+		return errors.Annotate(err, "failed to fetch %q", pbutil.InvocationName((invID))).Err()
+
+	default:
+		return nil
+	}
+}
