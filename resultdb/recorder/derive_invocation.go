27878fa2da17a10f009a4bf978f598e24ed5b4b6
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-10-18 19:55:05 -0700

    [resultdb] Write DeriveInvocation protos to Spanner.
    Change-Id: I6d3015b8cda5771b1d30ef1e04739d13f30a3d3d

--- resultdb/recorder/derive_invocation.go
+++ resultdb/recorder/derive_invocation.go
@@ -15,13 +15,23 @@
 package main
 
 import (
-	"golang.org/x/net/context"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
 	"strings"
+	"time"
+
+	"cloud.google.com/go/spanner"
+	"github.com/golang/protobuf/ptypes"
+	"google.golang.org/grpc/codes"
 
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/grpc/grpcutil"
 
 	"go.chromium.org/luci/resultdb/internal"
+	"go.chromium.org/luci/resultdb/internal/span"
 	"go.chromium.org/luci/resultdb/pbutil"
 	pb "go.chromium.org/luci/resultdb/proto/v1"
 	"go.chromium.org/luci/resultdb/recorder/chromium"
@@ -49,7 +59,7 @@ func validateDeriveInvocationRequest(req *pb.DeriveInvocationRequest) error {
 		return errors.Reason("swarming_task.id missing").Err()
 	}
 
-	if err := pbutil.ValidateVariantDef(req.GetBaseTestVariant()); err != nil {
+	if err := pbutil.ValidateVariantDef(req.BaseTestVariant); err != nil {
 		return errors.Annotate(err, "base_test_variant").Err()
 	}
 
@@ -80,26 +90,171 @@ func (s *recorderServer) DeriveInvocation(ctx context.Context, in *pb.DeriveInvo
 	if task, err = chromium.GetOriginTask(ctx, task, swarmSvc); err != nil {
 		return nil, err
 	}
-
-	// Check if we even need to write this invocation: is it finalized?
-	// TODO(jchinlee): Get Invocation from Spanner.
-	var inv *pb.Invocation
-	if inv != nil {
-		if !pbutil.IsFinalized(inv.State) {
-			return nil, errors.Reason(
-				"attempting to derive an existing non-finalized invocation").
-				Tag(grpcutil.InternalTag).Err()
-		}
-
-		return inv, nil
-	}
-
-	inv, _, err = chromium.DeriveProtosForWriting(ctx, task, in)
+	invID, err := chromium.GetInvocationID(ctx, task, in)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO(jchinlee): Write Invocation and TestResults to Spanner.
+	client := span.Client(ctx)
+
+	// Check if we even need to write this invocation: is it finalized?
+	skipWrite, err := shouldSkipWriteInvocation(ctx, client.Single(), invID)
+	if err != nil {
+		return nil, err
+	}
+	if skipWrite {
+		readTxn, err := client.BatchReadOnlyTransaction(ctx, spanner.StrongRead())
+		defer readTxn.Close()
+		if err != nil {
+			return nil, err
+		}
+		return span.ReadInvocationFull(ctx, readTxn, invID)
+	}
+
+	// Otherwise, get the protos and write them to Spanner.
+	inv, results, err := chromium.DeriveProtosForWriting(ctx, task, in)
+	if err != nil {
+		return nil, err
+	}
+	inv.Deadline = inv.FinalizeTime
+
+	// TODO(jchinlee): Validate invocation and results.
+
+	// Get expiration times.
+	exp := getExpirations(time.Now())
+
+	_, err = client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		// Check invocation state again.
+		skipWrite, err := shouldSkipWriteInvocation(ctx, txn, invID)
+		if err != nil {
+			return err
+		}
+		if skipWrite {
+			return nil
+		}
+
+		// Get Invocation mutation.
+		invMap := map[string]interface{}{
+			"InvocationId": invID,
+			"State":        int(inv.State),
+			"Realm":        "chromium",
+
+			"InvocationExpirationTime":          exp.InvocationTime,
+			"InvocationExpirationWeek":          exp.InvocationWeek,
+			"ExpectedTestResultsExpirationTime": exp.ResultsTime,
+			"ExpectedTestResultsExpirationWeek": exp.ResultsWeek,
+
+			"UpdateToken": "",
+
+			"BaseTestVariantDef": pbutil.VariantDefPairs(inv.BaseTestVariantDef),
+			"Tags":               pbutil.StringPairsToStrings(inv.Tags...),
+		}
+
+		if invMap["CreateTime"], err = ptypes.Timestamp(inv.CreateTime); err != nil {
+			return errors.Annotate(err, "inv.CreateTime").Err()
+		}
+		if invMap["Deadline"], err = ptypes.Timestamp(inv.Deadline); err != nil {
+			return errors.Annotate(err, "inv.Deadline").Err()
+		}
+		if inv.FinalizeTime != nil {
+			finalizeTime, err := ptypes.Timestamp(inv.FinalizeTime)
+			if err != nil {
+				return errors.Annotate(err, "inv.FinalizeTime").Err()
+			}
+			invMap["FinalizeTime"] = finalizeTime
+		}
+
+		muts := []*spanner.Mutation{spanner.InsertMap("Invocations", invMap)}
+
+		// Create InvocationByTags mutations.
+		for _, tag := range inv.Tags {
+			muts = append(muts, spanner.InsertMap("InvocationsByTag", map[string]interface{}{
+				"TagId":        tagID(tag),
+				"InvocationId": invID,
+			}))
+		}
+
+		// Get TestResult mutations.
+		for i, tr := range results {
+			trMap := map[string]interface{}{
+				"InvocationId": invID,
+				"TestPath":     tr.TestPath,
+				"ResultId":     generateTestResultID(tr.TestPath, i),
+
+				"ExtraVariantPairs": pbutil.VariantDefPairs(tr.ExtraVariantPairs),
+
+				"CommitTimestamp": spanner.CommitTimestamp,
+
+				"Status":          int(tr.Status),
+				"SummaryMarkdown": tr.SummaryMarkdown,
+				"RunDurationUsec": 1e6*tr.Duration.Seconds + int64(1e-3*float64(tr.Duration.Nanos)),
+				"Tags":            pbutil.StringPairsToStrings(tr.Tags...),
+			}
+
+			if tr.StartTime != nil {
+				startTime, err := ptypes.Timestamp(tr.StartTime)
+				if err != nil {
+					return errors.Annotate(err, "test result #%d %q", i, tr.TestPath).Err()
+				}
+				trMap["StartTime"] = startTime
+			}
+
+			var err error
+			if trMap["InputArtifacts"], err = pbutil.ArtifactsToByteArrays(tr.InputArtifacts); err != nil {
+				return errors.Annotate(err, "test result #%d %q", i, tr.TestPath).Err()
+			}
+			if trMap["OutputArtifacts"], err = pbutil.ArtifactsToByteArrays(tr.OutputArtifacts); err != nil {
+				return errors.Annotate(err, "test result #%d %q", i, tr.TestPath).Err()
+			}
+
+			// Populate IsUnexpected /only/ if true, to keep the index thin.
+			if !tr.Expected {
+				trMap["IsUnexpected"] = true
+			}
+
+			muts = append(muts, spanner.InsertMap("TestResults", trMap))
+		}
+
+		// Write mutations.
+		return txn.BufferWrite(muts)
+	})
 
 	return inv, err
+}
+
+func shouldSkipWriteInvocation(ctx context.Context, txn span.Txn, invID string) (bool, error) {
+	state, err := readInvocationState(ctx, txn, invID)
+	switch {
+	case grpcutil.Code(err) == codes.NotFound:
+		// No such invocation found means we may have to write it, so proceed.
+		return false, nil
+
+	case err != nil:
+		return true, err
+
+	case !pbutil.IsFinalized(state):
+		return true, errors.Reason(
+			"attempting to derive an existing non-finalized invocation").Err()
+	}
+
+	// The invocation exists and is finalized, so no need to write it.
+	return true, nil
+}
+
+// generateTestResultID returns the ID of the TestResult with given path and index in invocation.
+// Has format "${sha256_hex(${testPath}:${index})}".
+func generateTestResultID(testPath string, i int) string {
+	h := sha256.New()
+	io.WriteString(h, testPath)
+	io.WriteString(h, ":")
+	io.WriteString(h, string(i))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// tagID returns the ID of the StringPair tag, with format "${sha256_hex(tag)}_${key}:${value}".
+func tagID(tag *pb.StringPair) string {
+	tagStr := pbutil.StringPairToString(tag)
+	h := sha256.New()
+	io.WriteString(h, tagStr)
+	return fmt.Sprintf("%s_%s", hex.EncodeToString(h.Sum(nil)), tagStr)
 }
