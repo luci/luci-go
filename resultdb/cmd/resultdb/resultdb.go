e92fbc7ba087af4de1f2a28d13b39e40aa0afc98
Nodir Turakulov <nodir@chromium.org>
2019-10-23 01:22:42 +0000

    [resultdb] Add empty resultdb server
    None of RPCs are implemented.
    
    R=chanli@chromium.org, jchinlee@chromium.org
    
    Bug: 1009519
    Change-Id: Ia3310316d2a3a6a3391af26e196c6dcde92651be
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1874094
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Auto-Submit: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Jao-ke Chin-Lee <jchinlee@chromium.org>

---
+++ resultdb/cmd/resultdb/resultdb.go
@@ -0,0 +1,62 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+
+	"go.chromium.org/luci/grpc/grpcutil"
+
+	"go.chromium.org/luci/resultdb/internal"
+	pb "go.chromium.org/luci/resultdb/proto/v1"
+)
+
+// resultDBServer implements pb.ResultDBServer.
+//
+// It does not return gRPC-native errors. NewResultDBServer takes care of that.
+type resultDBServer struct {
+}
+
+// NewResultDBServer creates an implementation of resultDBServer.
+func NewResultDBServer() pb.ResultDBServer {
+	return &pb.DecoratedResultDB{
+		Service:  &resultDBServer{},
+		Postlude: internal.UnwrapGrpcCodePostlude,
+	}
+}
+
+func (s *resultDBServer) GetInvocation(ctx context.Context, in *pb.GetInvocationRequest) (*pb.Invocation, error) {
+	return nil, grpcutil.Unimplemented
+}
+
+func (s *resultDBServer) GetTestResult(ctx context.Context, in *pb.GetTestResultRequest) (*pb.TestResult, error) {
+	return nil, grpcutil.Unimplemented
+}
+
+func (s *resultDBServer) ListTestResults(ctx context.Context, in *pb.ListTestResultsRequest) (*pb.ListTestResultsResponse, error) {
+	return nil, grpcutil.Unimplemented
+}
+
+func (s *resultDBServer) GetTestExoneration(ctx context.Context, in *pb.GetTestExonerationRequest) (*pb.TestExoneration, error) {
+	return nil, grpcutil.Unimplemented
+}
+
+func (s *resultDBServer) ListTestExonerations(ctx context.Context, in *pb.ListTestExonerationsRequest) (*pb.ListTestExonerationsResponse, error) {
+	return nil, grpcutil.Unimplemented
+}
+
+func (s *resultDBServer) QueryTestResults(ctx context.Context, in *pb.QueryTestResultsRequest) (*pb.QueryTestResultsResponse, error) {
+	return nil, grpcutil.Unimplemented
+}
