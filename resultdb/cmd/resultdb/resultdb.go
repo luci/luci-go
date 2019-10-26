c03ab928be0e9671b324c279967e83d0e975dd05
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-10-25 19:18:20 -0700

    [resultdb] Implement GetTestResult.
    Change-Id: If3020dfb2846d004661d394d2d42cac4052390d8

--- resultdb/cmd/resultdb/resultdb.go
+++ resultdb/cmd/resultdb/resultdb.go
@@ -37,10 +37,6 @@ func NewResultDBServer() pb.ResultDBServer {
 	}
 }
 
-func (s *resultDBServer) GetTestResult(ctx context.Context, in *pb.GetTestResultRequest) (*pb.TestResult, error) {
-	return nil, grpcutil.Unimplemented
-}
-
 func (s *resultDBServer) ListTestResults(ctx context.Context, in *pb.ListTestResultsRequest) (*pb.ListTestResultsResponse, error) {
 	return nil, grpcutil.Unimplemented
 }
