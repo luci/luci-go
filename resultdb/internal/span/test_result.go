607358159d8090cc92e74f6e73048b29db65152b
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-10-29 15:22:26 -0700

    [resultdb] Implement ListTestResults RPC.
    Change-Id: Idf1cd9fa77c72ee44c0ecb5da5345de26033a5c6

--- resultdb/internal/span/test_result.go
+++ resultdb/internal/span/test_result.go
@@ -16,16 +16,20 @@ package span
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"cloud.google.com/go/spanner"
 	"github.com/golang/protobuf/ptypes"
 	durpb "github.com/golang/protobuf/ptypes/duration"
+	"google.golang.org/api/iterator"
 	"google.golang.org/grpc/codes"
 
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/grpc/grpcutil"
 
+	"go.chromium.org/luci/resultdb/internal"
+	"go.chromium.org/luci/resultdb/internal/proto"
 	"go.chromium.org/luci/resultdb/pbutil"
 	pb "go.chromium.org/luci/resultdb/proto/rpc/v1"
 )
@@ -65,14 +69,126 @@ func ReadTestResult(ctx context.Context, txn Txn, name string) (*pb.TestResult, 
 		return nil, errors.Annotate(err, "failed to fetch %q", name).Err()
 	}
 
-	if maybeUnexpected.Valid {
-		tr.Expected = !maybeUnexpected.Bool
+	populateExpectedField(tr, maybeUnexpected)
+	populateDurationField(tr, micros)
+	return tr, nil
+}
+
+// ReadTestResults reads all test results, if any, of an invocation within the transaction.
+func ReadTestResults(ctx context.Context, txn Txn, name string, excludeExpected bool, cursor *internalpb.Cursor, pageSize int) ([]*pb.TestResult, *internalpb.Cursor, error) {
+	// Construct query for test results.
+	invID := pbutil.MustParseInvocationName(name)
+	queryParams := map[string]interface{}{
+		"invID": invID,
+		"limit": pageSize + 1, // request one more so we know if we've exhaustetd the collection
 	}
+
+	positionClause := ""
+	key := cursor.GetPosition()
+	if key != nil {
+		if len(key) != 2 {
+			return nil, nil, errors.Reason(
+				"expected string slice of {TestPath, ResultId} for TestResults cursor position, got %q",
+				key).Err()
+		}
+
+		positionClause = `AND (
+			(TestPath > @testPath)
+			OR (TestPath = @testPath AND ResultId >= @resultId))`
+		queryParams["testPath"] = key[0]
+		queryParams["resultId"] = key[1]
+	}
+
+	expectedClause := ""
+	if excludeExpected {
+		expectedClause = "AND IsUnexpected"
+	}
+
+	query := spanner.NewStatement(fmt.Sprintf(`
+		SELECT
+			TestPath,
+			ResultId,
+			ExtraVariantPairs,
+			IsUnexpected,
+			Status,
+			SummaryMarkdown,
+			StartTime,
+			RunDurationUsec,
+			Tags,
+			InputArtifacts,
+			OutputArtifacts
+		FROM TestResults
+		WHERE InvocationId=@invID
+			%s
+			%s
+		ORDER BY CommitTimestamp, TestPath, ResultId ASC
+		LIMIT @limit
+		`, positionClause, expectedClause))
+	query.Params = queryParams
+
+	// Fetch the TestResults.
+	it := txn.Query(ctx, query)
+	defer it.Stop()
+
+	trs := []*pb.TestResult{}
+	for {
+		row, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, nil, err
+		}
+
+		var maybeUnexpected spanner.NullBool
+		var micros int64
+
+		tr := &pb.TestResult{}
+		err = FromSpanner(row,
+			&tr.TestPath,
+			&tr.ResultId,
+			&tr.ExtraVariantPairs,
+			&maybeUnexpected,
+			&tr.Status,
+			&tr.SummaryMarkdown,
+			&tr.StartTime,
+			&micros,
+			&tr.Tags,
+			&tr.InputArtifacts,
+			&tr.OutputArtifacts,
+		)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		tr.Name = pbutil.TestResultName(invID, tr.TestPath, tr.ResultId)
+		populateExpectedField(tr, maybeUnexpected)
+		populateDurationField(tr, micros)
+
+		trs = append(trs, tr)
+	}
+
+	// If we got fewer than pageSize+1, then we've exhausted the collection, so return everything,
+	// with a nil-positioned cursor.
+	if len(trs) < pageSize+1 {
+		return trs, internal.NewCursor(nil), nil
+	}
+
+	// Otherwise, construct the next cursor.
+	trLast := trs[pageSize]
+	return trs[:pageSize], internal.NewCursor([]string{trLast.TestPath, trLast.ResultId}), nil
+}
+
+func populateDurationField(tr *pb.TestResult, micros int64) {
 	if micros > 0 {
 		tr.Duration = FromMicros(micros)
 	}
+}
 
-	return tr, nil
+func populateExpectedField(tr *pb.TestResult, maybeUnexpected spanner.NullBool) {
+	if maybeUnexpected.Valid {
+		tr.Expected = !maybeUnexpected.Bool
+	}
 }
 
 // ToMicros converts a duration.Duration proto to microseconds.
