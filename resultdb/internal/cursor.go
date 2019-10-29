213d416e13b5b996a5722b49168c1178fe1c521e
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-10-28 18:04:43 -0700

    [resultdb] Add basic Cursor.
    Change-Id: I2b97040288bdeca27a9119d3363351541b790da1

---
+++ resultdb/internal/cursor.go
@@ -0,0 +1,79 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"bytes"
+	"encoding/base64"
+
+	"github.com/golang/protobuf/proto"
+
+	"go.chromium.org/luci/common/errors"
+
+	"go.chromium.org/luci/resultdb/internal/proto"
+)
+
+const curVersion = 0
+
+// NewCursor creates a cursor in position 0 for the specified set of pagination
+// requests and given page size.
+func NewCursor(reqBytes []byte, pageSize int32) *internalpb.Cursor {
+	if pageSize <= 0 {
+		panic("nonpositive cursor page size")
+	}
+
+	return &internalpb.Cursor{
+		Request: reqBytes,
+		PageSize: pageSize,
+	}
+}
+
+// Cursor creates a cursor from the given cursor token.
+func Cursor(token string) (*internalpb.Cursor, error) {
+	tokBytes, err := base64.StdEncoding.DecodeString(token)
+	if err != nil {
+		return nil, err
+	}
+	buf := proto.NewBuffer(tokBytes)
+
+	switch version, err := buf.DecodeVarint(); {
+	case err != nil:
+		return nil, errors.Annotate(err, "version").Err()
+	case version != 0:
+		return nil, errors.Reason("unknown cursor version %d", version).Err()
+	}
+
+	cursor := &internalpb.Cursor{}
+	if err := buf.DecodeMessage(cursor); err != nil {
+		return nil, err
+	}
+	return cursor, nil
+}
+
+// Token converts an internally represented cursor to an opaque token string.
+func Token(c *internalpb.Cursor) (string, error) {
+	buf := proto.NewBuffer(nil)
+	buf.EncodeVarint(curVersion)
+	if err := buf.EncodeMessage(c); err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(buf.Bytes()), nil
+}
+
+// ValidateForRequest indicates whether this cursor may be used with the request
+// represented by the given bytes.
+func ValidateForRequest(c *internalpb.Cursor, req []byte) bool {
+	return bytes.Compare(c.Request, req) == 0
+}
---
+++ resultdb/internal/cursor_test.go
@@ -0,0 +1,54 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"testing"
+
+	"go.chromium.org/luci/resultdb/internal/proto"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestCursor(t *testing.T) {
+	t.Parallel()
+
+	Convey(`Token works`, t, func() {
+		c := &internalpb.Cursor{PageSize: 5, Offset: 10}
+		tok, err := Token(c)
+		So(err, ShouldBeNil)
+		So(tok, ShouldResemble, "AAQQBRgK")
+
+		c, err = Cursor("AAQQBRgK")
+		So(err, ShouldBeNil)
+		So(c, ShouldResembleProto, &internalpb.Cursor{PageSize: 5, Offset: 10})
+
+		Convey(`and errors with wrong version`, func() {
+			_, err = Cursor("AQQQBRgK")
+			So(err, ShouldErrLike, "unknown cursor version 1")
+		})
+	})
+
+	Convey(`Fresh cursor works`, t, func() {
+		c := NewCursor([]byte("req_0"), 5)
+		So(c.Offset, ShouldEqual, 0)
+
+		Convey(`for validating against the correct request`, func() {
+			So(ValidateForRequest(c, []byte("req_0")), ShouldBeTrue)
+			So(ValidateForRequest(c, []byte("req_1")), ShouldBeFalse)
+		})
+	})
+}
