d79541115f769554c10455d4c06a59f4683d4d60
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-10-29 15:16:39 -0700

    [resultdb] Add basic Cursor.
    Change-Id: I2b97040288bdeca27a9119d3363351541b790da1

---
+++ resultdb/internal/cursor.go
@@ -0,0 +1,68 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"encoding/base64"
+
+	"github.com/golang/protobuf/proto"
+
+	"go.chromium.org/luci/common/errors"
+
+	"go.chromium.org/luci/resultdb/internal/proto"
+)
+
+const curVersion = 0
+
+// NewCursor creates a cursor starting at the given Key (inclusive).
+func NewCursor(pos []string) *internalpb.Cursor {
+	return &internalpb.Cursor{Version: curVersion, Position: pos}
+}
+
+// Cursor creates a cursor from the given cursor token.
+func Cursor(token string) (*internalpb.Cursor, error) {
+	if token == "" {
+		return NewCursor(nil), nil
+	}
+
+	tokBytes, err := base64.StdEncoding.DecodeString(token)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := proto.NewBuffer(tokBytes)
+	cursor := &internalpb.Cursor{}
+	if err := buf.DecodeMessage(cursor); err != nil {
+		return nil, errors.Annotate(err, "cursor contents %q", buf.Bytes()).Err()
+	}
+
+	if cursor.Version != 0 {
+		return nil, errors.Reason("unknown cursor version %d", cursor.Version).Err()
+	}
+	return cursor, nil
+}
+
+// Token converts an internally represented cursor to an opaque token string.
+func Token(c *internalpb.Cursor) (string, error) {
+	if c.GetPosition() == nil {
+		return "", nil
+	}
+
+	buf := proto.NewBuffer(nil)
+	if err := buf.EncodeMessage(c); err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(buf.Bytes()), nil
+}
