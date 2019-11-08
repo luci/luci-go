7df51279b33059f340bcf35ea287cf587d11da98
Nodir Turakulov <nodir@chromium.org>
2019-11-08 01:19:50 -0800

    [resultdb] Implement ListTestExonerations RPC
    Use (TestPath, ExonerationID) as cursor.
    
    Bug: 1009519
    Change-Id: I2f2d3b9a10446c09cc747aa8bdf38de4b94d4ebb

---
+++ resultdb/cmd/resultdb/list_test_exonerations.go
@@ -0,0 +1,59 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/grpc/grpcutil"
+
+	"go.chromium.org/luci/resultdb/internal/span"
+	"go.chromium.org/luci/resultdb/pbutil"
+	pb "go.chromium.org/luci/resultdb/proto/rpc/v1"
+)
+
+func validateListTestExonerationsRequest(req *pb.ListTestExonerationsRequest) error {
+	if err := pbutil.ValidateInvocationName(req.GetInvocation()); err != nil {
+		return errors.Annotate(err, "invocation").Err()
+	}
+
+	if err := pbutil.ValidatePageSize(req.GetPageSize()); err != nil {
+		return errors.Annotate(err, "page_size").Err()
+	}
+
+	return nil
+}
+
+// ListTestExonerations implements pb.ResultDBServer.
+func (s *resultDBServer) ListTestExonerations(ctx context.Context, in *pb.ListTestExonerationsRequest) (*pb.ListTestExonerationsResponse, error) {
+	if err := validateListTestExonerationsRequest(in); err != nil {
+		return nil, errors.Annotate(err, "bad request").Tag(grpcutil.InvalidArgumentTag).Err()
+	}
+	invID := span.MustParseInvocationName(in.Invocation)
+
+	txn := span.Client(ctx).ReadOnlyTransaction()
+	defer txn.Close()
+
+	tes, tok, err := span.ReadTestExonerations(ctx, txn, invID, in.GetPageToken(), adjustPageSize(in.GetPageSize()))
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ListTestExonerationsResponse{
+		TestExonerations: tes,
+		NextPageToken:    tok,
+	}, nil
+}
