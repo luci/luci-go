11aa44c40d17c98f843ca08c7179ee54fde25992
Nodir Turakulov <nodir@chromium.org>
2019-11-10 07:47:20 -0800

    [filegraph] Add skeleton
    This defines main.go with no subcommands, and adds a README.md
    with a brief description of the tool.
    
    Subsequent CLs will implement the tool.
    
    Change-Id: I7681ad48a729ae63cb42b42cbe0420c0c901290e

---
+++ tools/filegraph/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/maruel/subcommands"
+
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/flag/fixflagpos"
+	"go.chromium.org/luci/common/logging/gologger"
+)
+
+var logCfg = gologger.LoggerConfig{
+	Format: `%{message}`,
+	Out:    os.Stderr,
+}
+
+type baseCommandRun struct {
+	subcommands.CommandRunBase
+}
+
+func (r *baseCommandRun) done(err error) int {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return 1
+	}
+	return 0
+}
+
+func main() {
+	app := &cli.Application{
+		Name:  "filegraph",
+		Title: "Filegraph",
+		Context: func(ctx context.Context) context.Context {
+			return logCfg.Use(ctx)
+		},
+		Commands: []*subcommands.Command{
+			// TODO(nodir): add subcommands.
+
+			{},
+			subcommands.CmdHelp,
+		},
+	}
+
+	os.Exit(subcommands.Run(app, fixflagpos.FixSubcommands(os.Args[1:])))
+}
