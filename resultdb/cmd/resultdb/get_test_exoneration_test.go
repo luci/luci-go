d8ad4721450a58d8cbbe6db826d77fb484562c49
Nodir Turakulov <nodir@chromium.org>
2019-11-11 20:28:08 -0800

    [resultdb] Implement GetTestExoneration RPC
    R=liaoyuke@chromium.org
    
    Bug: 1009519
    Change-Id: I14cbfd346583d5ae7b15fcd5d70de79405cf0b7c

---
+++ resultdb/cmd/resultdb/get_test_exoneration_test.go
@@ -0,0 +1,80 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"testing"
+
+	"go.chromium.org/luci/common/clock/testclock"
+
+	"go.chromium.org/luci/resultdb/internal/span"
+	"go.chromium.org/luci/resultdb/internal/testutil"
+	"go.chromium.org/luci/resultdb/pbutil"
+	pb "go.chromium.org/luci/resultdb/proto/rpc/v1"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestValidateGetTestExonerationRequest(t *testing.T) {
+	t.Parallel()
+	Convey(`ValidateGetTestExonerationRequest`, t, func() {
+		Convey(`Valid`, func() {
+			req := &pb.GetTestExonerationRequest{Name: "invocations/a/tests/gn:%2F%2Fchrome%2Ftest:foo_tests%2FBarTest.DoBaz/exonerations/id"}
+			So(validateGetTestExonerationRequest(req), ShouldBeNil)
+		})
+
+		Convey(`Invalid name`, func() {
+			req := &pb.GetTestExonerationRequest{}
+			So(validateGetTestExonerationRequest(req), ShouldErrLike, "unspecified")
+		})
+	})
+}
+
+func TestGetTestExoneration(t *testing.T) {
+	Convey(`GetTestExoneration`, t, func() {
+		ctx := testutil.SpannerTestContext(t)
+
+		ct := testclock.TestRecentTimeUTC
+		ctx, _ = testclock.UseTime(ctx, ct)
+
+		srv := NewResultDBServer()
+
+		invID := span.InvocationID("inv_0")
+		// Insert a TestExoneration.
+		testutil.MustApply(ctx,
+			testutil.InsertInvocation("inv_0", pb.Invocation_ACTIVE, "", ct),
+			span.InsertMap("TestExonerations", map[string]interface{}{
+				"InvocationId":        invID,
+				"TestPath":            "gn://chrome/test:foo_tests/BarTest.DoBaz",
+				"ExonerationId":       "id",
+				"Variant":             pbutil.Variant("k1", "v1", "k2", "v2"),
+				"ExplanationMarkdown": "broken",
+			}))
+
+		req := &pb.GetTestExonerationRequest{Name: "invocations/inv_0/tests/gn:%2F%2Fchrome%2Ftest:foo_tests%2FBarTest.DoBaz/exonerations/id"}
+		tr, err := srv.GetTestExoneration(ctx, req)
+		So(err, ShouldBeNil)
+		So(tr, ShouldResembleProto, &pb.TestExoneration{
+			Name:          "invocations/inv_0/tests/gn:%2F%2Fchrome%2Ftest:foo_tests%2FBarTest.DoBaz/exonerations/id",
+			ExonerationId: "id",
+			TestVariant: &pb.TestVariant{
+				TestPath: "gn://chrome/test:foo_tests/BarTest.DoBaz",
+				Variant:  pbutil.Variant("k1", "v1", "k2", "v2"),
+			},
+			ExplanationMarkdown: "broken",
+		})
+	})
+}
