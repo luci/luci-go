60f132296c17906fa70024f3353c64a6da5b635e
Nodir Turakulov <nodir@chromium.org>
2019-11-21 10:15:45 -0800

    [resultdb] Implement rdb-derive
    Parse args, call DeriveInvocation concurrently. Then print results.
    
    Fix invalid domain name.
    
    R=liaoyuke@chromium.org
    
    Bug: 1025779
    Change-Id: I64dcc8ef1ab4fa5daed8108c1d75af50b08057b0

--- resultdb/cli/app.go
+++ resultdb/cli/app.go
@@ -19,6 +19,7 @@ import (
 	"os"
 
 	"github.com/maruel/subcommands"
+	"google.golang.org/grpc/metadata"
 
 	"go.chromium.org/luci/auth"
 	"go.chromium.org/luci/auth/client/authcli"
@@ -45,6 +46,10 @@ func application(p Params) *cli.Application {
 		Name:  "rdb",
 		Title: "A CLI client for ResultDB.",
 		Context: func(ctx context.Context) context.Context {
+			// TODO(crbug.com/1021849): remove, when this domain name points to the
+			// associated IP address.
+			ctx = metadata.AppendToOutgoingContext(ctx, "host", "nodir.results.api.cr.dev")
+
 			return logCfg.Use(ctx)
 		},
 		Commands: []*subcommands.Command{
