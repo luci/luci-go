0878632ad77b57599c2c2ace282b6256bbbf9b66
Nodir Turakulov <nodir@chromium.org>
2019-11-21 15:00:04 -0800

    [resultdb] Implement rdb-derive
    Parse args, call DeriveInvocation concurrently. Then print results.
    
    Fix invalid domain name.
    
    R=liaoyuke@chromium.org
    
    Bug: 1025779
    Change-Id: I64dcc8ef1ab4fa5daed8108c1d75af50b08057b0

--- resultdb/cli/cmd_derive.go
+++ resultdb/cli/cmd_derive.go
@@ -15,15 +15,24 @@
 package cli
 
 import (
+	"context"
+	"strings"
+
 	"github.com/maruel/subcommands"
+	"golang.org/x/sync/errgroup"
 
 	"go.chromium.org/luci/common/cli"
 	"go.chromium.org/luci/common/data/text"
+	"go.chromium.org/luci/common/errors"
+
+	pb "go.chromium.org/luci/resultdb/proto/rpc/v1"
 )
+
+const deriveUsage = `derive [flags] SWARMING_HOST TASK_ID [TASK_ID]...`
 
 func cmdDerive(p Params) *subcommands.Command {
 	return &subcommands.Command{
-		UsageLine: `derive [flags] SWARMING_HOST TASK_ID [TASK_ID]...`,
+		UsageLine: deriveUsage,
 		ShortDesc: "derive results from Chromium swarming tasks and query them",
 		LongDesc: text.Doc(`
 			Derives Invocation(s) from Chromium Swarming task(s) and prints results,
@@ -36,6 +45,8 @@ func cmdDerive(p Params) *subcommands.Command {
 		CommandRun: func() subcommands.CommandRun {
 			r := &deriveRun{}
 			r.queryRun.registerFlags(p)
+			// TODO(crbug.com/1021849): add -base-test-variant flag.
+			// TODO(crbug.com/1021849): add -base-test-path-prefix flag.
 			return r
 		},
 	}
@@ -43,12 +54,29 @@ func cmdDerive(p Params) *subcommands.Command {
 
 type deriveRun struct {
 	queryRun
+	swarmingHost string
+	taskIDs      []string
+}
+
+func (r *deriveRun) parseArgs(args []string) error {
+	if len(args) < 2 {
+		return errors.Reason("usage: %s", deriveUsage).Err()
+	}
+
+	r.swarmingHost = args[0]
+	r.taskIDs = args[1:]
+
+	if strings.Contains(r.swarmingHost, "/") {
+		return errors.Reason("invalid swarming host %q", r.swarmingHost).Err()
+	}
+
+	return r.queryRun.validate()
 }
 
 func (r *deriveRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
 	ctx := cli.GetContext(a, r, env)
 
-	if err := r.queryRun.validate(); err != nil {
+	if err := r.parseArgs(args); err != nil {
 		return r.done(err)
 	}
 
@@ -56,7 +84,36 @@ func (r *deriveRun) Run(a subcommands.Application, args []string, env subcommand
 		return r.done(err)
 	}
 
-	// TODO(crbug.com/1021849): parse arguments.
-	// TODO(crbug.com/1021849): derive invocations and call queryAndPrint.
-	panic("unimplemented")
+	invNames, err := r.deriveInvocations(ctx)
+	if err != nil {
+		return r.done(err)
+	}
+
+	return r.done(r.queryAndPrint(ctx, &pb.InvocationPredicate{Names: invNames}))
+}
+
+// deriveInvocations derives invocations from the swarming tasks and returns
+// invocation names.
+func (r *deriveRun) deriveInvocations(ctx context.Context) ([]string, error) {
+	eg, ctx := errgroup.WithContext(ctx)
+	ret := make([]string, len(r.taskIDs))
+	for i, tid := range r.taskIDs {
+		i := i
+		tid := tid
+		eg.Go(func() error {
+			res, err := r.recorder.DeriveInvocation(ctx, &pb.DeriveInvocationRequest{
+				SwarmingTask: &pb.DeriveInvocationRequest_SwarmingTask{
+					Hostname: r.swarmingHost,
+					Id:       tid,
+				},
+			})
+			if err != nil {
+				return err
+			}
+
+			ret[i] = res.Name
+			return nil
+		})
+	}
+	return ret, eg.Wait()
 }
