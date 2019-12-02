88b92757b6a45dc548915e5291445789c7ad2e78
Nodir Turakulov <nodir@chromium.org>
2019-12-02 13:25:21 -0800

    [resultdb] Isolate gtest snippets
    GTest snippets are very large. Store them in isolate instead.
    
    R=jchinlee@chromium.org
    
    Bug: 1027922
    Change-Id: Ib6dd49d66fe181e2b8e0a2eb8bcc04499b25a539

--- resultdb/cmd/recorder/chromium/formats/gtest_test.go
+++ resultdb/cmd/recorder/chromium/formats/gtest_test.go
@@ -160,7 +160,13 @@ func TestGTestConversions(t *testing.T) {
 					OutputSnippetBase64: "WyBSVU4gICAgICBdIEZvb1Rlc3QuVGVzdERvQmFyCigxMCBtcyk=",
 				})
 				So(err, ShouldBeNil)
-				So(tr.SummaryMarkdown, ShouldEqual, "[ RUN      ] FooTest.TestDoBar\n(10 ms)")
+				So(tr.OutputArtifacts, ShouldResembleProto, []*pb.Artifact{
+					{
+						Name:        "gtest_snippet.txt",
+						ContentType: "text/plain",
+						Contents:    []byte("[ RUN      ] FooTest.TestDoBar\n(10 ms)"),
+					},
+				})
 			})
 
 			Convey("invalid does not cause a fatal error", func() {
@@ -170,7 +176,7 @@ func TestGTestConversions(t *testing.T) {
 					OutputSnippetBase64: "invalid base64",
 				})
 				So(err, ShouldBeNil)
-				So(tr.SummaryMarkdown, ShouldEqual, "")
+				So(tr.OutputArtifacts, ShouldHaveLength, 0)
 			})
 		})
 
