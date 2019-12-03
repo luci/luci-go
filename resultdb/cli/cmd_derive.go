89e0da7dd03cf17655fceb7185e567e5444edc79
Nodir Turakulov <nodir@chromium.org>
2019-12-03 20:22:56 +0000

    [resultdb][cli] Invocation ids as args or stdin
    Accept invocation ids via positional args or stdin.
    
    R=sajjadm@chromium.org
    
    Bug: 1025779
    Change-Id: Ib49a47942dc02cc366faddd2a75d6a33f4db4729
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1948181
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

--- resultdb/cli/cmd_derive.go
+++ resultdb/cli/cmd_derive.go
@@ -25,6 +25,7 @@ import (
 	"go.chromium.org/luci/common/data/text"
 	"go.chromium.org/luci/common/errors"
 
+	"go.chromium.org/luci/resultdb/pbutil"
 	pb "go.chromium.org/luci/resultdb/proto/rpc/v1"
 )
 
@@ -82,16 +83,16 @@ func (r *deriveRun) Run(a subcommands.Application, args []string, env subcommand
 		return r.done(err)
 	}
 
-	invNames, err := r.deriveInvocations(ctx)
+	invIDs, err := r.deriveInvocations(ctx)
 	if err != nil {
 		return r.done(err)
 	}
 
-	return r.done(r.queryAndPrint(ctx, invNames))
+	return r.done(r.queryAndPrint(ctx, invIDs))
 }
 
 // deriveInvocations derives invocations from the swarming tasks and returns
-// invocation names.
+// invocation ids.
 func (r *deriveRun) deriveInvocations(ctx context.Context) ([]string, error) {
 	eg, ctx := errgroup.WithContext(ctx)
 	ret := make([]string, len(r.taskIDs))
@@ -109,8 +110,8 @@ func (r *deriveRun) deriveInvocations(ctx context.Context) ([]string, error) {
 				return err
 			}
 
-			ret[i] = res.Name
-			return nil
+			ret[i], err = pbutil.ParseInvocationName(res.Name)
+			return err
 		})
 	}
 	return ret, eg.Wait()
--- resultdb/cli/cmd_ls.go
+++ resultdb/cli/cmd_ls.go
@@ -15,33 +15,39 @@
 package cli
 
 import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+
 	"github.com/maruel/subcommands"
 
 	"go.chromium.org/luci/common/cli"
 	"go.chromium.org/luci/common/data/text"
 	"go.chromium.org/luci/common/errors"
-	luciflag "go.chromium.org/luci/common/flag"
 
 	"go.chromium.org/luci/resultdb/pbutil"
 )
 
 func cmdLs(p Params) *subcommands.Command {
 	return &subcommands.Command{
-		UsageLine: `ls [flags]`,
+		UsageLine: `ls [flags] [INVOCATION_ID]...`,
 		ShortDesc: "query results",
+		LongDesc: text.Doc(`
+			Query results.
+
+			If no invocation ids are specified on the command line, read them from
+			stdin separated by newline. Example:
+			  bb chromium/ci/linux-rel -status failure -inv -10 | rdb ls
+		`),
 		CommandRun: func() subcommands.CommandRun {
 			r := &lsRun{}
 			r.queryRun.registerFlags(p)
-			r.Flags.Var(luciflag.StringSlice(&r.invIDs), "inv", text.Doc(`
-				Retrieve results from the invocation with this ID.
-
-				May be specified multiple times.
-			`))
-
-			// TODO(crbug.com/1021849): add flag -cl
 			// TODO(crbug.com/1021849): add flag -var
 			// TODO(crbug.com/1021849): add flag -watch
-
 			return r
 		},
 	}
@@ -53,12 +59,18 @@ type lsRun struct {
 }
 
 func (r *lsRun) parseArgs(args []string) error {
+	r.invIDs = args
 	if len(r.invIDs) == 0 {
-		return errors.Reason("-inv is required").Err()
+		var err error
+		if r.invIDs, err = readStdin(); err != nil {
+			return err
+		}
 	}
 
-	if len(args) != 0 {
-		return errors.Reason("unexpected positional arguments").Err()
+	for _, id := range r.invIDs {
+		if err := pbutil.ValidateInvocationID(id); err != nil {
+			return errors.Annotate(err, "invocation id %q", id).Err()
+		}
 	}
 
 	return r.queryRun.validate()
@@ -75,13 +87,33 @@ func (r *lsRun) Run(a subcommands.Application, args []string, env subcommands.En
 		return r.done(err)
 	}
 
-	return r.done(r.queryAndPrint(ctx, r.InvocationNames()))
+	return r.done(r.queryAndPrint(ctx, r.invIDs))
 }
 
-func (r *lsRun) InvocationNames() []string {
-	names := make([]string, len(r.invIDs))
-	for i, id := range r.invIDs {
-		names[i] = pbutil.InvocationName(id)
+// readStdin reads all lines from os.Stdin.
+func readStdin() ([]string, error) {
+	// This context is used only to cancel the goroutine below.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go func() {
+		select {
+		case <-time.After(time.Second):
+			fmt.Fprintln(os.Stderr, "expecting invocation ids on stdin...")
+		case <-ctx.Done():
+		}
+	}()
+
+	var ret []string
+	stdin := bufio.NewReader(os.Stdin)
+	for {
+		line, err := stdin.ReadString('\n')
+		if err == io.EOF {
+			return ret, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, strings.TrimSuffix(line, "\n"))
+		cancel() // do not print the warning since we got something.
 	}
-	return names
 }
