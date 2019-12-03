3dfb543607eb35a13cb2d59d71cce48fc266300d
Takuto Ikuta <tikuta@chromium.org>
2019-12-03 16:11:22 +0900

    cache: propagate error from library without wrap
    HardLink() currently cannot return wrapped error.
    Caller refers returned error without wrapping.
    crrev.com/90339b0b3ed3296ee280b036c91f3314dcc6e6c8/client/downloader/downloader.go#444
    
    This is fix for
    https://chromium-swarm-dev.appspot.com/task?id=48e3486deeb62910
    introduced by
    crrev.com/b265971d2cfd995f9c6803f8e5871c5a06ca6a5a
    
    Bug: 932396
    Change-Id: Ida956a2c61aa0e76f80ff29d4cb277e75e58bcd6

--- common/data/caching/cache/cache.go
+++ common/data/caching/cache/cache.go
@@ -19,7 +19,6 @@ import (
 	"crypto"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -375,13 +374,9 @@ func (d *disk) Hardlink(digest isolated.HexDigest, dest string, perm os.FileMode
 	//
 	// - On any other (sane) OS, if dest exists, it is silently overwritten.
 	if err := os.Link(src, dest); err != nil {
-		return fmt.Errorf("failed to call os.Link(%s, %s): %v", src, dest, err)
+		return err
 	}
-
-	if err := os.Chmod(dest, perm); err != nil {
-		return fmt.Errorf("failed to call os.Chmod(%s, %#o): %v", dest, perm, err)
-	}
-	return nil
+	return os.Chmod(dest, perm)
 }
 
 func (d *disk) GetAdded() []int64 {
