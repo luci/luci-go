5fad56e2bff4aecfd88e889f3d14285d20a25035
Quinten Yearsley <qyearsley@chromium.org>
2019-12-06 14:20:02 -0800

    Update CQ Attempt BigQuery row proto
    This updates the proto message based on feedback so far.
    
    In this change:
    
     - Rename start and stop to start_time and end_time,
       these are standard names as described in
       https://cloud.google.com/apis/design/standard_fields
     - Rename attempt_key to key, "attempt_" seems redundant.
     - Add other fields that we're pretty sure we want:
       list of CLs, list of builds, etc.
    
    Bug: 804289
    Change-Id: Ia7bb09dde26808758f7b922feb7e0855249d7bae

--- cq/api/bigquery/attempt.pb.go
+++ cq/api/bigquery/attempt.pb.go
@@ -21,78 +21,109 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-// Whether the attempt is a dry run or full run. If constituent CLs have
-// different modes, then the mode is disagreement.
-type Attempt_Mode int32
+type Mode int32
 
 const (
-	Attempt_DISAGREEMENT Attempt_Mode = 0
-	Attempt_DRY_RUN      Attempt_Mode = 1
-	Attempt_FULL_RUN     Attempt_Mode = 2
+	// Default, never set.
+	Mode_MODE_UNSPECIFIED Mode = 0
+	// Run all tests but do not submit.
+	Mode_DRY_RUN Mode = 1
+	// Run all tests and potentially submit.
+	Mode_FULL_RUN Mode = 2
 )
 
-var Attempt_Mode_name = map[int32]string{
-	0: "DISAGREEMENT",
+var Mode_name = map[int32]string{
+	0: "MODE_UNSPECIFIED",
 	1: "DRY_RUN",
 	2: "FULL_RUN",
 }
 
-var Attempt_Mode_value = map[string]int32{
-	"DISAGREEMENT": 0,
-	"DRY_RUN":      1,
-	"FULL_RUN":     2,
+var Mode_value = map[string]int32{
+	"MODE_UNSPECIFIED": 0,
+	"DRY_RUN":          1,
+	"FULL_RUN":         2,
 }
 
-func (x Attempt_Mode) String() string {
-	return proto.EnumName(Attempt_Mode_name, int32(x))
+func (x Mode) String() string {
+	return proto.EnumName(Mode_name, int32(x))
 }
 
-func (Attempt_Mode) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_8792fe122a6ce934, []int{0, 0}
+func (Mode) EnumDescriptor() ([]byte, []int) {
+	return fileDescriptor_8792fe122a6ce934, []int{0}
 }
 
-// Pass or fail state of the attempt. Pending attempts shouldn't be included
-// in the completed attempts table.
-type Attempt_SubmitState int32
+type AttemptStatus int32
 
 const (
-	Attempt_QUEUED    Attempt_SubmitState = 0
-	Attempt_FAILED    Attempt_SubmitState = 1
-	Attempt_SUBMITTED Attempt_SubmitState = 2
+	// Default, never set.
+	AttemptStatus_STATUS_UNSPECIFIED AttemptStatus = 0
+	// Started but not completed. Used by CQ API, TBD.
+	AttemptStatus_STARTED AttemptStatus = 1
+	// Ready to submit, all checks passed.
+	AttemptStatus_SUCCESS AttemptStatus = 2
+	// Attempt stopped before completion. This may happen when CQ vote
+	// is removed, a new patchset is uploaded, a CL is deleted, etc.
+	AttemptStatus_ABORTED AttemptStatus = 3
+	// Completed and failed some check. This may happen when a build failed,
+	// footer syntax was incorrect, or CL was not approved.
+	AttemptStatus_FAILURE AttemptStatus = 4
+	// Failure in CQ itself caused the Attempt to be dropped.
+	AttemptStatus_INFRA_FAILURE AttemptStatus = 5
 )
 
-var Attempt_SubmitState_name = map[int32]string{
-	0: "QUEUED",
-	1: "FAILED",
-	2: "SUBMITTED",
+var AttemptStatus_name = map[int32]string{
+	0: "STATUS_UNSPECIFIED",
+	1: "STARTED",
+	2: "SUCCESS",
+	3: "ABORTED",
+	4: "FAILURE",
+	5: "INFRA_FAILURE",
 }
 
-var Attempt_SubmitState_value = map[string]int32{
-	"QUEUED":    0,
-	"FAILED":    1,
-	"SUBMITTED": 2,
+var AttemptStatus_value = map[string]int32{
+	"STATUS_UNSPECIFIED": 0,
+	"STARTED":            1,
+	"SUCCESS":            2,
+	"ABORTED":            3,
+	"FAILURE":            4,
+	"INFRA_FAILURE":      5,
 }
 
-func (x Attempt_SubmitState) String() string {
-	return proto.EnumName(Attempt_SubmitState_name, int32(x))
+func (x AttemptStatus) String() string {
+	return proto.EnumName(AttemptStatus_name, int32(x))
 }
 
-func (Attempt_SubmitState) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_8792fe122a6ce934, []int{0, 1}
+func (AttemptStatus) EnumDescriptor() ([]byte, []int) {
+	return fileDescriptor_8792fe122a6ce934, []int{1}
 }
 
-// Attempt includes the state CQ attempt.
-// For thecompleted
+// Attempt includes the state of one CQ attempt.
+//
+// An attempt involves doing checks for one or more CLs that could
+// potentially be submitted together.
 type Attempt struct {
-	// Time when the attempt started and stopped (TODO: define more precisely)
-	Start                *timestamp.Timestamp `protobuf:"bytes,1,opt,name=start,proto3" json:"start,omitempty"`
-	Stop                 *timestamp.Timestamp `protobuf:"bytes,2,opt,name=stop,proto3" json:"stop,omitempty"`
-	Mode                 Attempt_Mode         `protobuf:"varint,3,opt,name=mode,proto3,enum=bigquery.Attempt_Mode" json:"mode,omitempty"`
-	State                Attempt_SubmitState  `protobuf:"varint,4,opt,name=state,proto3,enum=bigquery.Attempt_SubmitState" json:"state,omitempty"`
-	AttemptKey           string               `protobuf:"bytes,5,opt,name=attempt_key,json=attemptKey,proto3" json:"attempt_key,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}             `json:"-"`
-	XXX_unrecognized     []byte               `json:"-"`
-	XXX_sizecache        int32                `json:"-"`
+	// The opaque key unique to this Attempt.
+	Key string `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`
+	// The LUCI project that this attempt belongs to.
+	Project string `protobuf:"bytes,2,opt,name=project,proto3" json:"project,omitempty"`
+	// An opaque key that is unique for a given set of Gerrit change patchsets
+	// (or, equivalently, buildsets). The same cl_group_key will be used if
+	// another attempt is made for the same set of changes at a different time.
+	ClGroupKey string `protobuf:"bytes,6,opt,name=cl_group_key,json=clGroupKey,proto3" json:"cl_group_key,omitempty"`
+	// Time when the attempt started (trigger time of the last CL triggered)
+	// and ended (released by CQ).
+	StartTime *timestamp.Timestamp `protobuf:"bytes,3,opt,name=start_time,json=startTime,proto3" json:"start_time,omitempty"`
+	EndTime   *timestamp.Timestamp `protobuf:"bytes,4,opt,name=end_time,json=endTime,proto3" json:"end_time,omitempty"`
+	// Gerrit changes, with specific patchsets, in this Attempt.
+	// There should be one or more.
+	GerritChanges []*GerritChange `protobuf:"bytes,5,rep,name=gerrit_changes,json=gerritChanges,proto3" json:"gerrit_changes,omitempty"`
+	// Builds checked as part of this attempt, whether triggered or reused.
+	Builds []*Build `protobuf:"bytes,7,rep,name=builds,proto3" json:"builds,omitempty"`
+	// Final status of the Attempt.
+	Status               AttemptStatus `protobuf:"varint,8,opt,name=status,proto3,enum=bigquery.AttemptStatus" json:"status,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}      `json:"-"`
+	XXX_unrecognized     []byte        `json:"-"`
+	XXX_sizecache        int32         `json:"-"`
 }
 
 func (m *Attempt) Reset()         { *m = Attempt{} }
@@ -120,45 +151,253 @@ func (m *Attempt) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Attempt proto.InternalMessageInfo
 
-func (m *Attempt) GetStart() *timestamp.Timestamp {
+func (m *Attempt) GetKey() string {
 	if m != nil {
-		return m.Start
-	}
-	return nil
-}
-
-func (m *Attempt) GetStop() *timestamp.Timestamp {
-	if m != nil {
-		return m.Stop
-	}
-	return nil
-}
-
-func (m *Attempt) GetMode() Attempt_Mode {
-	if m != nil {
-		return m.Mode
-	}
-	return Attempt_DISAGREEMENT
-}
-
-func (m *Attempt) GetState() Attempt_SubmitState {
-	if m != nil {
-		return m.State
-	}
-	return Attempt_QUEUED
-}
-
-func (m *Attempt) GetAttemptKey() string {
-	if m != nil {
-		return m.AttemptKey
+		return m.Key
 	}
 	return ""
 }
 
+func (m *Attempt) GetProject() string {
+	if m != nil {
+		return m.Project
+	}
+	return ""
+}
+
+func (m *Attempt) GetClGroupKey() string {
+	if m != nil {
+		return m.ClGroupKey
+	}
+	return ""
+}
+
+func (m *Attempt) GetStartTime() *timestamp.Timestamp {
+	if m != nil {
+		return m.StartTime
+	}
+	return nil
+}
+
+func (m *Attempt) GetEndTime() *timestamp.Timestamp {
+	if m != nil {
+		return m.EndTime
+	}
+	return nil
+}
+
+func (m *Attempt) GetGerritChanges() []*GerritChange {
+	if m != nil {
+		return m.GerritChanges
+	}
+	return nil
+}
+
+func (m *Attempt) GetBuilds() []*Build {
+	if m != nil {
+		return m.Builds
+	}
+	return nil
+}
+
+func (m *Attempt) GetStatus() AttemptStatus {
+	if m != nil {
+		return m.Status
+	}
+	return AttemptStatus_STATUS_UNSPECIFIED
+}
+
+// GerritChange represents one revision (patchset) of one Gerrit change
+// in an attempt.
+//
+// See also: GerritChange in buildbucket/proto/common.proto.
+type GerritChange struct {
+	// Gerrit hostname, e.g. "chromium-review.googlesource.com".
+	Host string `protobuf:"bytes,1,opt,name=host,proto3" json:"host,omitempty"`
+	// Gerrit project, e.g. "chromium/src".
+	Project string `protobuf:"bytes,2,opt,name=project,proto3" json:"project,omitempty"`
+	// Change number, e.g. 12345.
+	Change int64 `protobuf:"varint,3,opt,name=change,proto3" json:"change,omitempty"`
+	// Patch set number, e.g. 1.
+	Patchset int64 `protobuf:"varint,4,opt,name=patchset,proto3" json:"patchset,omitempty"`
+	// The earliest patchset of the CL that is considered
+	// equivalent to the patchset above.
+	EarliestEquivalentPatchset int64 `protobuf:"varint,5,opt,name=earliest_equivalent_patchset,json=earliestEquivalentPatchset,proto3" json:"earliest_equivalent_patchset,omitempty"`
+	// The time that the CQ was triggered for this CL in this attempt.
+	TriggerTime *timestamp.Timestamp `protobuf:"bytes,6,opt,name=trigger_time,json=triggerTime,proto3" json:"trigger_time,omitempty"`
+	// CQ Mode for this CL, e.g. dry run or full run.
+	Mode Mode `protobuf:"varint,7,opt,name=mode,proto3,enum=bigquery.Mode" json:"mode,omitempty"`
+	// True if the change was submitted.
+	//
+	// That is, the status of the change in Gerrit was changed to "merged".
+	// Submission of Gerrit CLs in an Attempt is not an atomic operation,
+	// so it's possible that only some of the GerritChanges are submitted.
+	Submitted            bool     `protobuf:"varint,8,opt,name=submitted,proto3" json:"submitted,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *GerritChange) Reset()         { *m = GerritChange{} }
+func (m *GerritChange) String() string { return proto.CompactTextString(m) }
+func (*GerritChange) ProtoMessage()    {}
+func (*GerritChange) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8792fe122a6ce934, []int{1}
+}
+
+func (m *GerritChange) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GerritChange.Unmarshal(m, b)
+}
+func (m *GerritChange) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GerritChange.Marshal(b, m, deterministic)
+}
+func (m *GerritChange) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GerritChange.Merge(m, src)
+}
+func (m *GerritChange) XXX_Size() int {
+	return xxx_messageInfo_GerritChange.Size(m)
+}
+func (m *GerritChange) XXX_DiscardUnknown() {
+	xxx_messageInfo_GerritChange.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GerritChange proto.InternalMessageInfo
+
+func (m *GerritChange) GetHost() string {
+	if m != nil {
+		return m.Host
+	}
+	return ""
+}
+
+func (m *GerritChange) GetProject() string {
+	if m != nil {
+		return m.Project
+	}
+	return ""
+}
+
+func (m *GerritChange) GetChange() int64 {
+	if m != nil {
+		return m.Change
+	}
+	return 0
+}
+
+func (m *GerritChange) GetPatchset() int64 {
+	if m != nil {
+		return m.Patchset
+	}
+	return 0
+}
+
+func (m *GerritChange) GetEarliestEquivalentPatchset() int64 {
+	if m != nil {
+		return m.EarliestEquivalentPatchset
+	}
+	return 0
+}
+
+func (m *GerritChange) GetTriggerTime() *timestamp.Timestamp {
+	if m != nil {
+		return m.TriggerTime
+	}
+	return nil
+}
+
+func (m *GerritChange) GetMode() Mode {
+	if m != nil {
+		return m.Mode
+	}
+	return Mode_MODE_UNSPECIFIED
+}
+
+func (m *GerritChange) GetSubmitted() bool {
+	if m != nil {
+		return m.Submitted
+	}
+	return false
+}
+
+// Build represents one tryjob Buildbucket build.
+//
+// See also: Build in buildbucket/proto/build.proto.
+type Build struct {
+	// Buildbucket build ID, unique per Buildbucket instance.
+	Id int64 `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	// Buildbucket host, e.g. "cr-buildbucket.appspot.com".
+	Host string `protobuf:"bytes,2,opt,name=host,proto3" json:"host,omitempty"`
+	// True if this build was pre-existing, or false if it was triggered as
+	// part of this CQ attempt.
+	Reused bool `protobuf:"varint,3,opt,name=reused,proto3" json:"reused,omitempty"`
+	// YES if this build must pass in order for the CLs to be considered
+	// ready to submit; NO if the build status should not be used to assess
+	// correctness the CLs in the Attempt.
+	Critical             bool     `protobuf:"varint,4,opt,name=critical,proto3" json:"critical,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Build) Reset()         { *m = Build{} }
+func (m *Build) String() string { return proto.CompactTextString(m) }
+func (*Build) ProtoMessage()    {}
+func (*Build) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8792fe122a6ce934, []int{2}
+}
+
+func (m *Build) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Build.Unmarshal(m, b)
+}
+func (m *Build) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Build.Marshal(b, m, deterministic)
+}
+func (m *Build) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Build.Merge(m, src)
+}
+func (m *Build) XXX_Size() int {
+	return xxx_messageInfo_Build.Size(m)
+}
+func (m *Build) XXX_DiscardUnknown() {
+	xxx_messageInfo_Build.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Build proto.InternalMessageInfo
+
+func (m *Build) GetId() int64 {
+	if m != nil {
+		return m.Id
+	}
+	return 0
+}
+
+func (m *Build) GetHost() string {
+	if m != nil {
+		return m.Host
+	}
+	return ""
+}
+
+func (m *Build) GetReused() bool {
+	if m != nil {
+		return m.Reused
+	}
+	return false
+}
+
+func (m *Build) GetCritical() bool {
+	if m != nil {
+		return m.Critical
+	}
+	return false
+}
+
 func init() {
-	proto.RegisterEnum("bigquery.Attempt_Mode", Attempt_Mode_name, Attempt_Mode_value)
-	proto.RegisterEnum("bigquery.Attempt_SubmitState", Attempt_SubmitState_name, Attempt_SubmitState_value)
+	proto.RegisterEnum("bigquery.Mode", Mode_name, Mode_value)
+	proto.RegisterEnum("bigquery.AttemptStatus", AttemptStatus_name, AttemptStatus_value)
 	proto.RegisterType((*Attempt)(nil), "bigquery.Attempt")
+	proto.RegisterType((*GerritChange)(nil), "bigquery.GerritChange")
+	proto.RegisterType((*Build)(nil), "bigquery.Build")
 }
 
 func init() {
@@ -166,25 +405,42 @@ func init() {
 }
 
 var fileDescriptor_8792fe122a6ce934 = []byte{
-	// 317 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x90, 0x4f, 0x6b, 0xfa, 0x30,
-	0x1c, 0xc6, 0x6d, 0x7f, 0xf5, 0xdf, 0xb7, 0xfe, 0x46, 0xc9, 0x61, 0x14, 0x61, 0x28, 0x9e, 0x64,
-	0x87, 0x64, 0xe8, 0xde, 0x80, 0xa3, 0x71, 0xc8, 0x54, 0x58, 0xda, 0x1e, 0x76, 0x92, 0x56, 0xb3,
-	0x2e, 0xcc, 0x90, 0x5a, 0xd3, 0x83, 0xef, 0x74, 0x2f, 0x67, 0x98, 0xb6, 0x30, 0xd8, 0x61, 0xb7,
-	0xe4, 0xe1, 0xf3, 0xe1, 0xfb, 0xf0, 0xc0, 0x2c, 0x53, 0x78, 0xff, 0x51, 0x28, 0x29, 0x4a, 0x89,
-	0x55, 0x91, 0x91, 0x63, 0xb9, 0x17, 0x64, 0x7f, 0x22, 0x49, 0x2e, 0x48, 0x2a, 0xb2, 0x53, 0xc9,
-	0x8b, 0x0b, 0x49, 0xb4, 0xe6, 0x32, 0xd7, 0x38, 0x2f, 0x94, 0x56, 0xa8, 0xd7, 0xe4, 0xc3, 0x51,
-	0xa6, 0x54, 0x76, 0xe4, 0xc4, 0xe4, 0x69, 0xf9, 0x4e, 0xb4, 0x90, 0xfc, 0xac, 0x13, 0x99, 0x57,
-	0xe8, 0xe4, 0xcb, 0x86, 0xee, 0xa2, 0x92, 0xd1, 0x03, 0xb4, 0xcf, 0x3a, 0x29, 0xb4, 0x6f, 0x8d,
-	0xad, 0xa9, 0x3b, 0x1b, 0xe2, 0x4a, 0xc6, 0x8d, 0x8c, 0xa3, 0x46, 0x66, 0x15, 0x88, 0x30, 0x38,
-	0x67, 0xad, 0x72, 0xdf, 0xfe, 0x53, 0x30, 0x1c, 0xba, 0x07, 0x47, 0xaa, 0x03, 0xf7, 0xff, 0x8d,
-	0xad, 0xe9, 0xcd, 0xec, 0x16, 0x37, 0x3d, 0x71, 0x5d, 0x01, 0x6f, 0xd4, 0x81, 0x33, 0xc3, 0xa0,
-	0xb9, 0x69, 0xa3, 0xb9, 0xef, 0x18, 0xf8, 0xee, 0x37, 0x1c, 0x96, 0xa9, 0x14, 0x3a, 0xbc, 0x42,
-	0xac, 0x62, 0xd1, 0x08, 0xdc, 0x7a, 0x8a, 0xdd, 0x27, 0xbf, 0xf8, 0xed, 0xb1, 0x35, 0xed, 0x33,
-	0xa8, 0xa3, 0x17, 0x7e, 0x99, 0xcc, 0xc1, 0xb9, 0xde, 0x40, 0x1e, 0x0c, 0x82, 0x55, 0xb8, 0x78,
-	0x66, 0x94, 0x6e, 0xe8, 0x36, 0xf2, 0x5a, 0xc8, 0x85, 0x6e, 0xc0, 0xde, 0x76, 0x2c, 0xde, 0x7a,
-	0x16, 0x1a, 0x40, 0x6f, 0x19, 0xaf, 0xd7, 0xe6, 0x67, 0x4f, 0x1e, 0xc1, 0xfd, 0x71, 0x0b, 0x01,
-	0x74, 0x5e, 0x63, 0x1a, 0xd3, 0xc0, 0x6b, 0x5d, 0xdf, 0xcb, 0xc5, 0x6a, 0x4d, 0x03, 0xcf, 0x42,
-	0xff, 0xa1, 0x1f, 0xc6, 0x4f, 0x9b, 0x55, 0x14, 0xd1, 0xc0, 0xb3, 0xd3, 0x8e, 0x99, 0x61, 0xfe,
-	0x1d, 0x00, 0x00, 0xff, 0xff, 0xfa, 0xc7, 0x3f, 0x5c, 0xc2, 0x01, 0x00, 0x00,
+	// 584 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x91, 0x4f, 0x8f, 0x9a, 0x50,
+	0x14, 0xc5, 0x2b, 0x28, 0x32, 0x57, 0xc7, 0xd2, 0x97, 0xc6, 0x12, 0x33, 0x49, 0x8d, 0x9b, 0x9a,
+	0x59, 0x40, 0x62, 0xd3, 0x34, 0x5d, 0x4c, 0x52, 0x47, 0x71, 0x62, 0x3a, 0xff, 0xf2, 0xd0, 0x45,
+	0x57, 0x04, 0xe1, 0x15, 0x5f, 0x0b, 0xc2, 0x3c, 0x1e, 0x4d, 0xe6, 0x73, 0xf5, 0x1b, 0xf5, 0x93,
+	0x34, 0x5c, 0x41, 0xa7, 0x69, 0xd2, 0xd9, 0x71, 0xee, 0x39, 0x07, 0x2e, 0xbf, 0x0b, 0x93, 0x28,
+	0xb5, 0x82, 0xad, 0x48, 0x13, 0x5e, 0x24, 0x56, 0x2a, 0x22, 0x3b, 0x2e, 0x02, 0x6e, 0x07, 0x0f,
+	0xb6, 0x9f, 0x71, 0x7b, 0xc3, 0xa3, 0x87, 0x82, 0x89, 0x47, 0xdb, 0x97, 0x92, 0x25, 0x99, 0xb4,
+	0x32, 0x91, 0xca, 0x94, 0xe8, 0xf5, 0x7c, 0xf0, 0x36, 0x4a, 0xd3, 0x28, 0x66, 0x36, 0xce, 0x37,
+	0xc5, 0x37, 0x5b, 0xf2, 0x84, 0xe5, 0xd2, 0x4f, 0xb2, 0x7d, 0x74, 0xf4, 0x5b, 0x81, 0xf6, 0x74,
+	0x5f, 0x26, 0x06, 0xa8, 0x3f, 0xd8, 0xa3, 0xd9, 0x18, 0x36, 0xc6, 0x27, 0xb4, 0x7c, 0x24, 0x26,
+	0xb4, 0x33, 0x91, 0x7e, 0x67, 0x81, 0x34, 0x15, 0x9c, 0xd6, 0x92, 0x0c, 0xa1, 0x1b, 0xc4, 0x5e,
+	0x24, 0xd2, 0x22, 0xf3, 0xca, 0x92, 0x86, 0x36, 0x04, 0xf1, 0x55, 0x39, 0xfa, 0xc2, 0x1e, 0xc9,
+	0x27, 0x80, 0x5c, 0xfa, 0x42, 0x7a, 0xe5, 0x27, 0x4d, 0x75, 0xd8, 0x18, 0x77, 0x26, 0x03, 0x6b,
+	0xbf, 0x8f, 0x55, 0xef, 0x63, 0xad, 0xea, 0x7d, 0xe8, 0x09, 0xa6, 0x4b, 0x4d, 0x3e, 0x80, 0xce,
+	0x76, 0xe1, 0xbe, 0xd8, 0x7c, 0xb6, 0xd8, 0x66, 0xbb, 0x10, 0x6b, 0x17, 0xd0, 0x8b, 0x98, 0x10,
+	0x5c, 0x7a, 0xc1, 0xd6, 0xdf, 0x45, 0x2c, 0x37, 0x5b, 0x43, 0x75, 0xdc, 0x99, 0xf4, 0xad, 0x9a,
+	0x87, 0x75, 0x85, 0xfe, 0x0c, 0x6d, 0x7a, 0x1a, 0x3d, 0x51, 0x39, 0x79, 0x07, 0xda, 0xa6, 0xe0,
+	0x71, 0x98, 0x9b, 0x6d, 0xac, 0xbd, 0x3c, 0xd6, 0x2e, 0xcb, 0x39, 0xad, 0x6c, 0x62, 0x83, 0x96,
+	0x4b, 0x5f, 0x16, 0xb9, 0xa9, 0x0f, 0x1b, 0xe3, 0xde, 0xe4, 0xcd, 0x31, 0x58, 0xa1, 0x74, 0xd1,
+	0xa6, 0x55, 0x6c, 0xf4, 0x4b, 0x81, 0xee, 0xd3, 0x2f, 0x13, 0x02, 0xcd, 0x6d, 0x9a, 0xcb, 0x0a,
+	0x35, 0x3e, 0xff, 0x87, 0x75, 0x1f, 0xb4, 0xfd, 0x0f, 0x21, 0x45, 0x95, 0x56, 0x8a, 0x0c, 0x40,
+	0xcf, 0x7c, 0x19, 0x6c, 0x73, 0x26, 0x11, 0x93, 0x4a, 0x0f, 0x9a, 0x7c, 0x86, 0x33, 0xe6, 0x8b,
+	0x98, 0xb3, 0x5c, 0x7a, 0xec, 0xa1, 0xe0, 0x3f, 0xfd, 0x98, 0xed, 0xa4, 0x77, 0xc8, 0xb7, 0x30,
+	0x3f, 0xa8, 0x33, 0xce, 0x21, 0x72, 0x5f, 0xbf, 0xe1, 0x02, 0xba, 0x52, 0xf0, 0x28, 0x62, 0x62,
+	0x7f, 0x08, 0xed, 0xd9, 0x43, 0x74, 0xaa, 0x3c, 0x1e, 0x63, 0x04, 0xcd, 0x24, 0x0d, 0x99, 0xd9,
+	0x46, 0x44, 0xbd, 0x23, 0xa2, 0x9b, 0x34, 0x64, 0x14, 0x3d, 0x72, 0x06, 0x27, 0x79, 0xb1, 0x49,
+	0xb8, 0x94, 0x2c, 0x44, 0x96, 0x3a, 0x3d, 0x0e, 0x46, 0x1e, 0xb4, 0x90, 0x3b, 0xe9, 0x81, 0xc2,
+	0x43, 0x64, 0xa5, 0x52, 0x85, 0x87, 0x07, 0x7a, 0xca, 0x13, 0x7a, 0x7d, 0xd0, 0x04, 0x2b, 0x72,
+	0x16, 0x22, 0x23, 0x9d, 0x56, 0xaa, 0x64, 0x14, 0x08, 0x2e, 0x79, 0xe0, 0xc7, 0xc8, 0x48, 0xa7,
+	0x07, 0x7d, 0xfe, 0x11, 0x9a, 0xe5, 0x32, 0xe4, 0x35, 0x18, 0x37, 0x77, 0x73, 0xc7, 0x5b, 0xdf,
+	0xba, 0xf7, 0xce, 0x6c, 0xb9, 0x58, 0x3a, 0x73, 0xe3, 0x05, 0xe9, 0x40, 0x7b, 0x4e, 0xbf, 0x7a,
+	0x74, 0x7d, 0x6b, 0x34, 0x48, 0x17, 0xf4, 0xc5, 0xfa, 0xfa, 0x1a, 0x95, 0x72, 0xbe, 0x83, 0xd3,
+	0xbf, 0x0e, 0x4d, 0xfa, 0x40, 0xdc, 0xd5, 0x74, 0xb5, 0x76, 0xff, 0x7d, 0x87, 0xbb, 0x9a, 0xd2,
+	0x95, 0x33, 0x37, 0x1a, 0x28, 0xd6, 0xb3, 0x99, 0xe3, 0xba, 0x86, 0x52, 0x8a, 0xe9, 0xe5, 0x1d,
+	0x3a, 0x6a, 0x29, 0x16, 0xd3, 0xe5, 0xf5, 0x9a, 0x3a, 0x46, 0x93, 0xbc, 0x82, 0xd3, 0xe5, 0xed,
+	0x82, 0x4e, 0xbd, 0x7a, 0xd4, 0xda, 0x68, 0x08, 0xfb, 0xfd, 0x9f, 0x00, 0x00, 0x00, 0xff, 0xff,
+	0xe0, 0xb4, 0x9e, 0x99, 0x0c, 0x04, 0x00, 0x00,
 }
