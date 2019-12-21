801358f1421cd81cb9b70046dc0706a392840ee1
Jao-ke Chin-Lee <jchinlee@chromium.org>
2019-12-20 22:23:56 -0500

    [resultdb] Support GCS artifacts.
    Change-Id: Ib79a7f0e9ef59a00c77f9e17590a13aa3b0eaf91

---
+++ resultdb/cmd/recorder/chromium/util/gcs.go
@@ -0,0 +1,37 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+import (
+	"fmt"
+	"regexp"
+
+	pb "go.chromium.org/luci/resultdb/proto/rpc/v1"
+)
+
+var gcsPathRe = regexp.MustCompile("^gs://([a-z0-9_.-]+)/(.+)$")
+
+// AsGCSArtifact returns an artifact corresponding to the path provided if treated as a GCS object.
+func AsGCSArtifact(name, url string) *pb.Artifact {
+	if m := gcsPathRe.FindStringSubmatch(url); m != nil {
+		return &pb.Artifact{
+			Name:     name,
+			FetchUrl: url,
+			ViewUrl: fmt.Sprintf(
+				"https://console.developers.google.com/m/cloudstorage/b/%s/o/%s", m[1], m[2]),
+		}
+	}
+	return nil
+}
