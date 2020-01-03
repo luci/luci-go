8a2acded152f3b57d07fea9839a5ec61eb8140d5
Vadim Shtayura <vadimsh@chromium.org>
2020-01-03 01:41:40 +0000

    [server] Add a simple limiter for number of concurrently handled RPCs.
    The focus of this CL is to come up with a good API for the limiter (including
    metrics!), so that we can teach it more tricks later without significantly
    changing the callers.
    
    Another caveat is that we install the limiter only on gRPC handlers for now
    (since that's what we need). Installing the limiter on all HTTP routes on the
    generic server.Server level is complicated because pRPC requests are also routed
    through the default HTTP port and we want configs and metrics for pRPC requests
    to be represented in terms of gRPC concepts, not in terms of HTTP requests.
    
    R=tandrii@chromium.org, nodir@chromium.org
    BUG=1033758
    
    Change-Id: Ie014ed6d218d6483348d3c2f348925cd19dc19c5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1984803
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

---
+++ server/limiter/peer.go
@@ -0,0 +1,39 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package limiter
+
+import (
+	"context"
+
+	"go.chromium.org/luci/auth/identity"
+	"go.chromium.org/luci/server/auth"
+)
+
+// PeerLabelFromAuthState looks at the auth.State in the context and derives
+// a peer label from it.
+//
+// Currently returns one of "unknown", "anonymous", "authenticated".
+//
+// TODO(vadimsh): Have a small group with a whitelist of identities that are OK
+// to use as peer label directly.
+func PeerLabelFromAuthState(ctx context.Context) string {
+	if s := auth.GetState(ctx); s != nil {
+		if s.PeerIdentity() == identity.AnonymousIdentity {
+			return "anonymous"
+		}
+		return "authenticated"
+	}
+	return "unknown"
+}
