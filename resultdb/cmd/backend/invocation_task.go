9acf664cd98b42ad9d3127f340e9a0da81999adc
Chan Li <chanli@chromium.org>
2020-01-13 14:53:09 -0800

    [resultdb] BigQuery Export: Run invocationTasks in background
    Bug: 1030355
    Change-Id: I0fb7bd98a00c9667c150262765d5a83f13fe9ac5

--- resultdb/cmd/backend/invocation_task.go
+++ resultdb/cmd/backend/invocation_task.go
@@ -100,3 +100,29 @@ func dispatchInvocationTasks(ctx context.Context, taskKeys []*span.TaskKey) erro
 		}
 	})
 }
+
+// runInvocationTasks gets invocation tasks and dispatch the tasks to workers.
+func runInvocationTasks(ctx context.Context) {
+	// TODO(chanli): Add alert on failures.
+	attempt := 0
+	for {
+		taskKeys, err := span.SampleInvocationTasks(ctx, time.Now(), 100)
+		if err != nil {
+			logging.Errorf(ctx, "Failed to query invocation tasks: %s", err)
+
+			attempt++
+			sleep := time.Duration(attempt) * time.Second
+			if sleep > 5*time.Second {
+				sleep = 5 * time.Second
+			}
+			time.Sleep(sleep)
+
+			continue
+		}
+
+		attempt = 0
+		if err = dispatchInvocationTasks(ctx, taskKeys); err != nil {
+			logging.Errorf(ctx, "Failed to run invocation tasks: %s", err)
+		}
+	}
+}
