f4c6e7eca22649a8ceb8f27431aabd8a95892634
Nodir Turakulov <nodir@chromium.org>
2020-01-15 11:30:48 -0800

    [resultdb] Serve isolate content
    Context: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/1993278
    
    Add two items:
    - Add a function to generate a signed URL to fetch isolate content
    - Add a plain HTTP URL handler to check the token and serve isolate
      content
    
    Verify user content request Host header to prevent serving user content
    from an unexpected host accidentally.
    
    R=chanli@chromium.org, vadimsh@chromium.org
    
    Bug: 1042261
    Change-Id: I2a55408beab1d33c975b2c885eb052e9dd729119

---
+++ resultdb/internal/usercontent/signing.go
@@ -0,0 +1,79 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package usercontent
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"time"
+
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/server/router"
+	"go.chromium.org/luci/server/tokens"
+)
+
+var pathTokenKind = tokens.TokenKind{
+	Algo:       tokens.TokenAlgoHmacSHA256,
+	Expiration: time.Hour,
+	SecretKey:  "user_content",
+	Version:    1,
+}
+
+// generateSignedURL generates a signed HTTPS URL back to this server.
+// The token works only with the urlPath, after calling path.Clean.
+// The function can be used for any kind of user content URLs.
+func (s *Server) generateSignedURL(ctx context.Context, urlPath string) (string, error) {
+	urlPath = path.Clean(urlPath)
+
+	state := []byte(urlPath)
+	tok, err := pathTokenKind.Generate(ctx, state, nil, time.Hour)
+	if err != nil {
+		return "", err
+	}
+
+	q := url.Values{}
+	q.Set("token", tok)
+	u := &url.URL{
+		Scheme:   "https",
+		Host:     s.Hostname,
+		Path:     urlPath,
+		RawQuery: q.Encode(),
+	}
+	return u.String(), nil
+}
+
+// validateToken validates token query parameter.
+// It can be used as a router middleware.
+func validateToken(ctx *router.Context, next router.Handler) {
+	token := ctx.Request.URL.Query().Get("token")
+	if token == "" {
+		ctx.Writer.WriteHeader(http.StatusUnauthorized)
+		io.WriteString(ctx.Writer, "missing token query parameters")
+		return
+	}
+
+	state := []byte(path.Clean(ctx.Request.URL.Path))
+	if _, err := pathTokenKind.Validate(ctx.Context, token, state); err != nil {
+		logging.Warningf(ctx.Context, "invalid token: %s", err)
+		ctx.Writer.WriteHeader(http.StatusForbidden)
+		io.WriteString(ctx.Writer, "invalid token")
+		return
+	}
+
+	next(ctx)
+}
