dda84b466f5c5132cab160eb181bc4d6522cd3fc
Takuto Ikuta <tikuta@chromium.org>
2020-01-20 09:57:28 +0000

    [cache] use python compatible cache state format
    This is to avoid crrev.com/c/2000354
    
    * update version from 1 to 3 with crrev.com/c/2009949
    * add 'lastAccess' to entry
    
    Bug: 932396
    Change-Id: If73e4d65bdb52ff7b434fc5ce4cd38787ce934ac
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2009533
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>

--- common/data/caching/cache/lru_test.go
+++ common/data/caching/cache/lru_test.go
@@ -18,17 +18,20 @@ import (
 	"math"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestEntryJSON(t *testing.T) {
 	for _, e := range []entry{
 		{
-			key:   "da39a3ee5e6b4b0d3255bfef95601890afd80709",
-			value: 0,
+			key:        "da39a3ee5e6b4b0d3255bfef95601890afd80709",
+			value:      0,
+			lastAccess: time.Now().Unix(),
 		},
 		{
-			key:   "46c5964fc9911cf02d6353d04ddff98aebb56ced",
-			value: math.MaxInt32 + 1,
+			key:        "46c5964fc9911cf02d6353d04ddff98aebb56ced",
+			value:      math.MaxInt32 + 1,
+			lastAccess: time.Now().Unix(),
 		},
 	} {
 		got, err := e.MarshalJSON()
@@ -42,7 +45,29 @@ func TestEntryJSON(t *testing.T) {
 		}
 
 		if !reflect.DeepEqual(unmarshal, e) {
-			t.Errorf("%v != UnMarshalJSON(%v.MarshalJSON())", unmarshal, e)
+			t.Errorf("%#v != UnMarshalJSON(%#v.MarshalJSON())", unmarshal, e)
+		}
+	}
+}
+
+func TestEntryUnmarshal(t *testing.T) {
+	for _, data := range []string{
+		`["key", [1, 1]]`,
+		`["key", [1.0, 1.0]]`,
+	} {
+		var e entry
+		if err := e.UnmarshalJSON([]byte(data)); err != nil {
+			t.Fatalf("failed to unmarshal `%s`: %v", data, err)
+		}
+
+		if e.key != "key" {
+			t.Errorf("got %s for key, expected `key`", e.key)
+		}
+		if e.value != 1 {
+			t.Errorf("got %d for value, expected 1.0", e.value)
+		}
+		if e.lastAccess != 1 {
+			t.Errorf("got %d for lastAccess, expected 1.0", e.lastAccess)
 		}
 	}
 }
