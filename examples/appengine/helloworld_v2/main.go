6a6be2c24e94fa3288f70d9b23d961847102b364
Vadim Shtayura <vadimsh@chromium.org>
2020-01-21 17:25:05 -0800

    [server] Add minimal GAE v2 support.
    Mostly works, except:
      * Logging is almost totally broken.
      * Tracing is broken.
      * Auth is broken 7% of the time (~2 min each 30 min).
      * The end user IP address detection is broken.
      * The health checking is useless (/healthz is not routable).
      * The admin port doesn't exist.
      * Deployment-specific parameters are hardcoded in the app.yaml.
    
    All these issues are relatively easily fixable (except the last two)
    and will be fixed one-by-one in follow up CLs.
    
    Purposefully keep the helloworld sample code absolutely identical to
    the GKE version (using the magic of symlinks), to assert that
    GAE v2 and GKE server environments are mostly identical.
    
    R=iannucci@chromium.org, nodir@chromium.org
    
    Change-Id: I5cd45c3e63c77993e091e6539eba2cb8f811f75e

---
+++ examples/appengine/helloworld_v2/main.go
@@ -0,0 +1,89 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/gomodule/redigo/redis"
+
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/trace"
+
+	"go.chromium.org/luci/server"
+	"go.chromium.org/luci/server/redisconn"
+	"go.chromium.org/luci/server/router"
+
+	"go.chromium.org/luci/examples/k8s/helloworld/apipb"
+)
+
+func main() {
+	server.Main(nil, func(srv *server.Server) error {
+		// pRPC example.
+		apipb.RegisterGreeterServer(srv.PRPC, &greeterServer{})
+
+		// Logging and tracing example.
+		srv.Routes.GET("/", router.MiddlewareChain{}, func(c *router.Context) {
+			logging.Debugf(c.Context, "Hello debug world")
+
+			ctx, span := trace.StartSpan(c.Context, "Testing")
+			logging.Infof(ctx, "Hello info world")
+			time.Sleep(100 * time.Millisecond)
+			span.End(nil)
+
+			logging.Warningf(c.Context, "Hello warning world")
+			c.Writer.Write([]byte("Hello, world"))
+		})
+
+		// Redis example.
+		//
+		// To run Redis for tests locally (in particular on OSX):
+		//   docker run --name redis -p 6379:6379 --restart always --detach redis
+		//
+		// Then launch the example with "... -redis-addr :6379".
+		//
+		// Note that it makes Redis port available on 0.0.0.0. This is a necessity
+		// when using Docker-for-Mac. Don't put any sensitive stuff there (or make
+		// sure your firewall is configured to block external connections).
+		srv.Routes.GET("/redis", router.MiddlewareChain{}, func(c *router.Context) {
+			conn, err := redisconn.Get(c.Context)
+			if err != nil {
+				http.Error(c.Writer, err.Error(), 500)
+				return
+			}
+			defer conn.Close()
+			n, err := redis.Int(conn.Do("INCR", "testKey"))
+			if err != nil {
+				http.Error(c.Writer, err.Error(), 500)
+				return
+			}
+			fmt.Fprintf(c.Writer, "%d", n)
+		})
+
+		return nil
+	})
+}
+
+type greeterServer struct{}
+
+func (*greeterServer) SayHi(ctx context.Context, _ *empty.Empty) (*empty.Empty, error) {
+	logging.Infof(ctx, "Hi")
+	time.Sleep(100 * time.Millisecond)
+	return &empty.Empty{}, nil
+}
