820161359c47da3335b4e3b27e0c45505c596a77
Scott Lee <ddoman@chromium.org>
2020-01-24 22:23:50 +1100

    [resultdb] add validators for TestResult and Artifacts
    R=nodir
    Bug: 1017288
    Change-Id: Id94c54a49edfc45e5fa5dd085d55e908d82444f3

--- resultdb/pbutil/test_result_test.go
+++ resultdb/pbutil/test_result_test.go
@@ -15,15 +15,85 @@
 package pbutil
 
 import (
+	"fmt"
 	"strings"
 	"testing"
+	"time"
 
+	"go.chromium.org/luci/common/clock/testclock"
+	pb "go.chromium.org/luci/resultdb/proto/rpc/v1"
+	typepb "go.chromium.org/luci/resultdb/proto/type"
+
+	"github.com/golang/protobuf/ptypes"
 	. "github.com/smartystreets/goconvey/convey"
 	. "go.chromium.org/luci/common/testing/assertions"
 )
 
+// validArtifacts returns two valid Artifact samples.
+func validArtifacts(now time.Time) (*pb.Artifact, *pb.Artifact) {
+	et, _ := ptypes.TimestampProto(now.Add(24 * time.Hour))
+	art1 := &pb.Artifact{
+		Name:               "this is artifact 1",
+		FetchUrl:           "https://foo/bar",
+		FetchUrlExpiration: et,
+		ContentType:        "text/plain",
+		Size:               1024,
+	}
+	art2 := &pb.Artifact{
+		Name:               "this is artifact 2",
+		FetchUrl:           "https://foo/bar/log.png",
+		FetchUrlExpiration: et,
+		ContentType:        "image/png",
+		Size:               1024,
+	}
+	return art1, art2
+}
+
+// invalidArtifacts returns two invalid Artifact samples.
+func invalidArtifacts(now time.Time) (*pb.Artifact, *pb.Artifact) {
+	et, _ := ptypes.TimestampProto(now.Add(24 * time.Hour))
+	art1 := &pb.Artifact{
+		Name:               " this is a bad artifact name.",
+		FetchUrl:           "https://foo/bar",
+		FetchUrlExpiration: et,
+		ContentType:        "text/plain",
+		Size:               1024,
+	}
+	art2 := &pb.Artifact{
+		Name:               "this has a bad fetch url",
+		FetchUrl:           "isolate://foo/bar/log.png",
+		FetchUrlExpiration: et,
+		ContentType:        "image/png",
+		Size:               1024,
+	}
+	return art1, art2
+}
+
+var now = testclock.TestRecentTimeUTC
+
+// validTestResult returns a valid TestResult sample.
+func validTestResult(now time.Time) *pb.TestResult {
+	st, _ := ptypes.TimestampProto(now.Add(-2 * time.Minute))
+	art1, art2 := validArtifacts(now)
+	return &pb.TestResult{
+		Name:            "invocations/a/tests/invocation_id1/results/result_id1",
+		TestId:          "this is testID",
+		ResultId:        "result_id1",
+		Variant:         Variant("a", "b"),
+		Expected:        true,
+		Status:          pb.TestStatus_PASS,
+		SummaryHtml:     "HTML summary",
+		StartTime:       st,
+		Duration:        ptypes.DurationProto(time.Minute),
+		Tags:            StringPairs("k1", "v1"),
+		InputArtifacts:  []*pb.Artifact{art1},
+		OutputArtifacts: []*pb.Artifact{art2},
+	}
+}
+
 func TestTestResultName(t *testing.T) {
 	t.Parallel()
+
 	Convey("ParseTestResultName", t, func() {
 		Convey("Parse", func() {
 			invID, testID, resultID, err := ParseTestResultName(
@@ -38,7 +108,7 @@ func TestTestResultName(t *testing.T) {
 			Convey(`has slashes`, func() {
 				_, _, _, err := ParseTestResultName(
 					"invocations/inv/tests/ninja://test/results/result1")
-				So(err, ShouldErrLike, "does not match")
+				So(err, ShouldErrLike, doesNotMatch(testResultNameRe))
 			})
 
 			Convey(`bad unescape`, func() {
@@ -50,7 +120,7 @@ func TestTestResultName(t *testing.T) {
 			Convey(`unescaped unprintable`, func() {
 				_, _, _, err := ParseTestResultName(
 					"invocations/a/tests/unprintable_%07/results/result1")
-				So(err, ShouldErrLike, "does not match")
+				So(err, ShouldErrLike, doesNotMatch(testIDRe))
 			})
 		})
 
@@ -62,33 +132,321 @@ func TestTestResultName(t *testing.T) {
 	})
 }
 
-func TestValidateArtifactName(t *testing.T) {
+func TestValidateTestResult(t *testing.T) {
 	t.Parallel()
-	Convey("Successes with good names", t, func() {
-		gn := []string{
-			"n", "name", "foo bar", "Chrome - Test #1",
-			"ab12-3cda-9b8b-dd75", "1111-2222-3333-4444",
-		}
-		for _, n := range gn {
-			So(ValidateArtifactName(n), ShouldBeNil)
-		}
-	})
 
-	Convey("Fails", t, func() {
-		Convey("With an empty name", func() {
-			So(ValidateArtifactName(""), ShouldErrLike, "unspecified")
+	Convey("Succeeds", t, func() {
+		msg := validTestResult(now)
+		So(ValidateTestResult(now, msg), ShouldBeNil)
+
+		Convey("with invalid Name", func() {
+			// ValidateTestResult should skip validating TestResult.Name.
+			msg.Name = "this is not a valid name for TestResult.Name"
+			expected := doesNotMatch(testResultNameRe)
+			So(ValidateTestResultName(msg.Name), ShouldErrLike, expected)
+			So(ValidateTestResult(now, msg), ShouldBeNil)
 		})
 
-		Convey("With bad names", func() {
-			bn := []string{" name", "name ", "name ##", "name ?", "name 1@"}
-			for _, n := range bn {
-				So(ValidateArtifactName(n), ShouldErrLike, "does not match")
+		Convey("with no variant", func() {
+			msg.Variant = nil
+			So(ValidateTestResult(now, msg), ShouldBeNil)
+		})
+
+		Convey("with valid summary", func() {
+			goodInputs := []string{
+				strings.Repeat("1", maxLenSummaryHTML),
+				strings.Repeat("☕", maxLenSummaryHTML),
+			}
+			for _, in := range goodInputs {
+				msg.SummaryHtml = in
+				So(ValidateSummaryHTML(in), ShouldBeNil)
+				So(ValidateTestResult(now, msg), ShouldBeNil)
 			}
 		})
 
-		Convey("With a too-long name", func() {
-			n := strings.Repeat("n", 256+1)
-			So(ValidateArtifactName(n), ShouldErrLike, "does not match")
+		Convey("with empty tags", func() {
+			msg.Tags = nil
+			So(ValidateTestResult(now, msg), ShouldBeNil)
+		})
+
+		Convey("with empty artifacts", func() {
+			Convey("in InputArtifacts", func() {
+				msg.InputArtifacts = nil
+				So(ValidateTestResult(now, msg), ShouldBeNil)
+			})
+
+			Convey("in OutputArtifacts", func() {
+				msg.OutputArtifacts = nil
+				So(ValidateTestResult(now, msg), ShouldBeNil)
+			})
+
+			// or both
+			msg.InputArtifacts = nil
+			msg.OutputArtifacts = nil
+			So(ValidateTestResult(now, msg), ShouldBeNil)
+		})
+
+	})
+
+	Convey("Fails", t, func() {
+		msg := validTestResult(now)
+		Convey("with nil", func() {
+			So(ValidateTestResult(now, nil), ShouldErrLike, unspecified())
+		})
+
+		Convey("with empty TestID", func() {
+			msg.TestId = ""
+			So(ValidateTestID(""), ShouldErrLike, unspecified())
+			So(ValidateTestResult(now, msg), ShouldErrLike, "test_id")
+		})
+
+		Convey("with invalid TestID", func() {
+			badInputs := []string{
+				strings.Repeat("1", 256+1),
+				// [[:print:]] matches with [ -~] and [[:graph:]]
+				string(163),
+			}
+			expected := doesNotMatch(testIDRe)
+			for _, in := range badInputs {
+				msg.TestId = in
+				So(ValidateTestID(msg.TestId), ShouldErrLike, expected)
+				So(ValidateTestResult(now, msg), ShouldErrLike, "test_id")
+			}
+		})
+
+		Convey("with empty ResultID", func() {
+			msg.ResultId = ""
+			So(ValidateResultID(""), ShouldErrLike, unspecified())
+			So(ValidateTestResult(now, msg), ShouldErrLike, "result_id")
+		})
+
+		Convey("with invalid ResultID", func() {
+			badInputs := []string{
+				strings.Repeat("1", 32+1),
+				// [[:ascii:]] matches with a char in [\x00-\x7F]
+				string(163),
+			}
+			expected := doesNotMatch(resultIDRe)
+			for _, in := range badInputs {
+				msg.ResultId = in
+				So(ValidateResultID(msg.ResultId), ShouldErrLike, expected)
+				So(ValidateTestResult(now, msg), ShouldErrLike, "result_id")
+			}
+		})
+
+		Convey("with invalid Variant", func() {
+			badInputs := []*typepb.Variant{
+				Variant("", ""),
+				Variant("", "val"),
+			}
+			expected := "key: " + unspecified().Error()
+			for _, in := range badInputs {
+				msg.Variant = in
+				So(ValidateVariant(msg.Variant), ShouldErrLike, expected)
+				So(ValidateTestResult(now, msg), ShouldErrLike, "variant")
+			}
+		})
+
+		Convey("with invalid Status", func() {
+			msg.Status = pb.TestStatus(len(pb.TestStatus_name) + 1)
+			So(ValidateTestResult(now, msg), ShouldErrLike, "status")
+		})
+
+		Convey("with valid summary", func() {
+			badInputs := []string{
+				strings.Repeat("1", maxLenSummaryHTML+1),
+				strings.Repeat("☕", maxLenSummaryHTML+1),
+			}
+			expected := fmt.Sprintf("exceeds the maximum length of %d", maxLenSummaryHTML)
+			for _, in := range badInputs {
+				msg.SummaryHtml = in
+				So(ValidateSummaryHTML(in), ShouldErrLike, expected)
+				So(ValidateTestResult(now, msg), ShouldErrLike, "summary_html")
+			}
+		})
+
+		Convey("with invalid StartTime and Duration", func() {
+			Convey("because start_time is in the future", func() {
+				future, _ := ptypes.TimestampProto(now.Add(time.Hour))
+				msg.StartTime = future
+				expected := "cannot be in the future"
+				So(ValidateStartTimeWithDuration(now, msg.StartTime, msg.Duration), ShouldErrLike, expected)
+				So(ValidateTestResult(now, msg), ShouldErrLike, "start_time")
+			})
+
+			Convey("because duration is < 0", func() {
+				nd := ptypes.DurationProto(-1 * time.Minute)
+				msg.Duration = nd
+				expected := "is < 0"
+				So(ValidateStartTimeWithDuration(now, msg.StartTime, msg.Duration), ShouldErrLike, expected)
+				So(ValidateTestResult(now, msg), ShouldErrLike, "duration")
+			})
+
+			Convey("because (start_time + duration) is future", func() {
+				st, _ := ptypes.TimestampProto(now.Add(-1 * time.Hour))
+				msg.StartTime = st
+				msg.Duration = ptypes.DurationProto(2 * time.Hour)
+				expected := "is invalid"
+				So(ValidateStartTimeWithDuration(now, msg.StartTime, msg.Duration), ShouldErrLike, expected)
+				So(ValidateTestResult(now, msg), ShouldErrLike, "duration")
+			})
+		})
+
+		Convey("with invalid StringPairs", func() {
+			msg.Tags = StringPairs("", "")
+			So(ValidateTestResult(now, msg), ShouldErrLike, unspecified())
+		})
+
+		Convey("with invalid artifacts", func() {
+			bart1, bart2 := invalidArtifacts(now)
+			expected := doesNotMatch(artifactNameRe)
+
+			Convey("in InputArtifacts", func() {
+				msg.InputArtifacts = []*pb.Artifact{bart1}
+				So(ValidateArtifacts(msg.InputArtifacts), ShouldErrLike, expected)
+				So(ValidateTestResult(now, msg), ShouldErrLike, "input_artifacts")
+			})
+
+			Convey("in OutputArtifacts", func() {
+				msg.OutputArtifacts = []*pb.Artifact{bart1}
+				So(ValidateArtifacts(msg.OutputArtifacts), ShouldErrLike, expected)
+				So(ValidateTestResult(now, msg), ShouldErrLike, "output_artifacts")
+			})
+
+			// or both
+			msg.InputArtifacts = []*pb.Artifact{bart1}
+			msg.OutputArtifacts = []*pb.Artifact{bart2}
+			So(ValidateTestResult(now, msg), ShouldErrLike, expected)
+		})
+	})
+}
+
+func TestValidateArtifacts(t *testing.T) {
+	t.Parallel()
+	art1, art2 := validArtifacts(now)
+	bart1, bart2 := invalidArtifacts(now)
+
+	Convey("Succeeds", t, func() {
+		Convey("with no artifact", func() {
+			in := []*pb.Artifact{}
+			So(ValidateArtifacts(in), ShouldBeNil)
+		})
+
+		Convey("with an artifact", func() {
+			in := []*pb.Artifact{art1}
+			So(ValidateArtifacts(in), ShouldBeNil)
+		})
+		Convey("with multiple artifacts", func() {
+			in := []*pb.Artifact{art1, art2}
+			So(ValidateArtifacts(in), ShouldBeNil)
+		})
+	})
+
+	Convey("Fails", t, func() {
+		expected := doesNotMatch(artifactNameRe)
+
+		Convey("with duplicate names", func() {
+			art2.Name = art1.Name
+			in := []*pb.Artifact{art1, art2}
+			So(ValidateArtifacts(in), ShouldErrLike, "duplicate Artifact.Name")
+		})
+
+		Convey("with an invalid artifact", func() {
+			in := []*pb.Artifact{bart1}
+			So(ValidateArtifacts(in), ShouldErrLike, expected)
+		})
+
+		Convey("with multiple invalid artifacts", func() {
+			in := []*pb.Artifact{bart1, bart2}
+			So(ValidateArtifacts(in), ShouldErrLike, expected)
+		})
+
+		Convey("with a mix of valid and invalid artifacts", func() {
+			in := []*pb.Artifact{art1, bart1}
+			So(ValidateArtifacts(in), ShouldErrLike, expected)
+		})
+	})
+}
+
+func TestValidateArtifact(t *testing.T) {
+	t.Parallel()
+
+	Convey("Succeeds", t, func() {
+		msg, _ := validArtifacts(now)
+		So(ValidateArtifact(msg), ShouldBeNil)
+
+		Convey("with no FetchUrlExpiration", func() {
+			msg.FetchUrlExpiration = nil
+			So(ValidateArtifact(msg), ShouldBeNil)
+		})
+
+		Convey("with empty ContentType", func() {
+			msg.ContentType = ""
+			So(ValidateArtifact(msg), ShouldBeNil)
+		})
+
+		Convey("with 0 in Size", func() {
+			msg.Size = 0
+			So(ValidateArtifact(msg), ShouldBeNil)
+		})
+	})
+
+	Convey("Fails", t, func() {
+		msg, _ := validArtifacts(now)
+
+		Convey("with nil", func() {
+			So(ValidateArtifact(nil), ShouldErrLike, unspecified())
+		})
+
+		Convey("with empty Name", func() {
+			msg.Name = ""
+			So(ValidateArtifactName(""), ShouldErrLike, unspecified())
+			So(ValidateArtifact(msg), ShouldErrLike, "name")
+		})
+
+		Convey("with invalid Name", func() {
+			badInputs := []string{
+				" name", "name ", "name ##", "name ?", "name 1@",
+				strings.Repeat("n", 256+1),
+			}
+			expected := doesNotMatch(artifactNameRe)
+			for _, in := range badInputs {
+				msg.Name = in
+				So(ValidateArtifactName(in), ShouldErrLike, expected)
+				So(ValidateArtifact(msg), ShouldErrLike, "name")
+			}
+		})
+
+		Convey("with empty FetchUrl", func() {
+			msg.FetchUrl = ""
+			So(ValidateArtifactFetchURL(""), ShouldErrLike, "empty")
+			So(ValidateArtifact(msg), ShouldErrLike, "fetch_url")
+		})
+
+		Convey("with invalid URI", func() {
+			msg.FetchUrl = "foo/bar"
+			expected := "invalid URI for request"
+			So(ValidateArtifactFetchURL("foo/bar"), ShouldErrLike, expected)
+			So(ValidateArtifact(msg), ShouldErrLike, "fetch_url")
+
+		})
+
+		Convey("with unsupported Scheme", func() {
+			badInputs := []string{
+				"/my_test", "http://host/page", "isolate://foo/bar",
+			}
+			expected := "the URL scheme is not HTTPS"
+			for _, in := range badInputs {
+				msg.FetchUrl = in
+				So(ValidateArtifactFetchURL(in), ShouldErrLike, expected)
+				So(ValidateArtifact(msg), ShouldErrLike, "fetch_url")
+			}
+		})
+
+		Convey("without host", func() {
+			msg.FetchUrl = "https://"
+			So(ValidateArtifactFetchURL("https://"), ShouldErrLike, "missing host")
+			So(ValidateArtifact(msg), ShouldErrLike, "fetch_url")
 		})
 	})
 }
