bbd526e6137fe50b45c682e6310342b28d635ef3
Roberto Carrillo <robertocn@google.com>
2020-01-24 00:05:24 +0000

    [ResultDB] Drop old rows for variants with only expected tests results.
    Periodically
    - Sample invocations with expiring test results in a random shard
    - Using a worker pool, delete test results for the variants in each
      invocation that only have expected test results, using a partitioned
      statement.
    - Unset the expiration for the invocation.
    
    R=nodir,chanli
    
    Bug: 1040301
    Change-Id: Ia9aa563d051e97614bac539f6d511cbbb2702c42
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2006788
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

---
+++ resultdb/cmd/backend/purge_expired_results.go
@@ -0,0 +1,202 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"time"
+
+	"cloud.google.com/go/spanner"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/sync/parallel"
+
+	"go.chromium.org/luci/resultdb/internal/span"
+)
+
+// maxPurgeTestResultsWorkers is the number of concurrent invocations to purge of expired
+// results.
+const maxPurgeTestResultsWorkers = 30
+
+// maxTestVariantsToFilter is the maximum number of test variants to include
+// in the exclusion clause of the paritioned delete statement used to purge
+// expired test results. Invocations that have more than this number of test
+// variant combinations with unexpected results will not be purged, until
+// the whole invocation expires.
+const maxTestVariantsToFilter = 1000
+
+func unsetInvocationResultsExpiration(ctx context.Context, id span.InvocationID) error {
+
+	_, err := span.Client(ctx).Apply(ctx, []*spanner.Mutation{
+		span.UpdateMap("Invocations", map[string]interface{}{
+			"InvocationID":                      id,
+			"ExpectedTestResultsExpirationTime": nil,
+		}),
+	})
+	return err
+}
+
+// purgeOneInvocation finds test variants with unexpected results and drops the
+// complement, if there aren't too many of them.
+func purgeOneInvocation(ctx context.Context, id span.InvocationID) error {
+
+	// Get the test variants that have one or more unexpected results (up to limit + 1).
+	testVariantsToKeep, err := queryTestVariantsWithUnexpectedResults(ctx, id, maxTestVariantsToFilter+1)
+	if err != nil {
+		return err
+	}
+
+	if len(testVariantsToKeep) > maxTestVariantsToFilter {
+		logging.Warningf(ctx, "Too many test variants with unexpected test results for %s, not purging", id.Name())
+	} else if err := deleteTestResults(ctx, id, testVariantsToKeep); err != nil {
+		return err
+	}
+
+	// Set the invocation's result expiration to null
+	return unsetInvocationResultsExpiration(ctx, id)
+}
+
+// dispatchExpiredResultDeletionTasks uses a pool of workers for purging results
+// for several invocations in parallel.
+func dispatchExpiredResultDeletionTasks(ctx context.Context, invIDs []span.InvocationID) error {
+	return parallel.FanOutIn(func(workC chan<- func() error) {
+		for _, id := range invIDs {
+			id := id
+			workC <- func() error {
+				return purgeOneInvocation(ctx, id)
+			}
+		}
+	})
+}
+
+// purgeExpiredResults is a loop that repeatedly polls a random shard and purges
+// expired test results for a number of its invocations.
+func purgeExpiredResults(ctx context.Context) {
+	processingLoop(ctx, 5*time.Second, 5*time.Second, func(ctx context.Context) error {
+		expiredResultsInvocationIds, err := sampleExpiredResultsInvocations(ctx, clock.Now(ctx), maxPurgeTestResultsWorkers)
+		if err != nil {
+			return err
+		}
+		if len(expiredResultsInvocationIds) == 0 {
+			return nil
+		}
+		return dispatchExpiredResultDeletionTasks(ctx, expiredResultsInvocationIds)
+	})
+}
+
+// sampleExpiredResultsInvocations selects a random set of invocations that have
+// expired test results.
+func sampleExpiredResultsInvocations(ctx context.Context, expirationTime time.Time, sampleSize int64) ([]span.InvocationID, error) {
+	var maxShard int64
+	row, err := span.Client(ctx).Single().Query(ctx, spanner.NewStatement(`
+		SELECT ShardId
+		FROM Invocations@{FORCE_INDEX=InvocationsByInvocationExpiration}
+		ORDER BY ShardID DESC
+		LIMIT 1
+	`)).Next()
+	if err != nil {
+		return nil, err
+	}
+	if err := row.Columns(&maxShard); err != nil {
+		return nil, err
+	}
+	st := spanner.NewStatement(`
+		WITH expiringInvocations AS (
+			SELECT InvocationId
+			FROM Invocations@{FORCE_INDEX=InvocationsByExpectedTestResultsExpiration}
+			WHERE ShardId = @shardId
+			AND ExpectedTestResultsExpirationTime IS NOT NULL
+			AND ExpectedTestResultsExpirationTime <= CURRENT_TIMESTAMP())
+		SELECT *
+		FROM expiringInvocations
+		TABLESAMPLE RESERVOIR (@sampleSize ROWS)
+	`)
+	st.Params["shardId"] = mathrand.Int63n(ctx, maxShard+1)
+	st.Params["sampleSize"] = sampleSize
+	ret := make([]span.InvocationID, 0, sampleSize)
+	var b span.Buffer
+	err = span.Query(ctx, "Sample expired results invocations", span.Client(ctx).Single(), st, func(row *spanner.Row) error {
+		var id span.InvocationID
+		if err := b.FromSpanner(row, &id); err != nil {
+			return err
+		}
+		ret = append(ret, id)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+// testVariantID is a simple pair-of-strings struct with no spanner columnnames.
+//
+// It is used to pass these pairs as sql params in the UNNEST function in the
+// statement composed by deleteTestResults below.
+type testVariantID struct {
+	TestId      string `spanner:""`
+	VariantHash string `spanner:""`
+}
+
+// deleteTestResults composes and executes a partitioned delete to drop
+// all the test results in a given invocation except those matching any of the given
+// test variant combination given in except.
+func deleteTestResults(ctx context.Context, id span.InvocationID, except []testVariantID) error {
+	st := spanner.NewStatement(`
+		DELETE FROM TestResults
+		WHERE InvocationId = @invocationId
+		AND (TestId, VariantHash) NOT IN UNNEST(@exceptTestVariants)
+	`)
+	st.Params["invocationId"] = span.ToSpanner(id)
+	st.Params["exceptTestVariants"] = except
+	count, err := span.Client(ctx).PartitionedUpdate(ctx, st)
+	if err != nil {
+		return err
+	}
+	logging.Infof(ctx, "Deleted %d expired test result rows in %s", count, id.Name())
+	return nil
+}
+
+// queryTestVariantsWithUnexpectedResults finds up to `limit` test variant
+// combinations that have at least one unexpected test result for a given
+// invocation.
+func queryTestVariantsWithUnexpectedResults(ctx context.Context, id span.InvocationID, limit int) ([]testVariantID, error) {
+	ret := []testVariantID{}
+	st := spanner.NewStatement(`
+		SELECT DISTINCT TestId, VariantHash
+		FROM TestResults@{FORCE_INDEX=UnexpectedTestResults}
+		WHERE InvocationId = @invocationId
+		AND IsUnexpected = TRUE
+		LIMIT @limit
+	`)
+
+	st.Params["invocationId"] = id
+	st.Params["limit"] = limit
+	err := span.Query(ctx, "Query test variant combinations with expired test results", span.Client(ctx).Single(), st, func(row *spanner.Row) error {
+		tv := testVariantID{}
+		if err := row.Columns(&tv.TestId, &tv.VariantHash); err != nil {
+			return err
+		}
+		ret = append(ret, tv)
+		return nil
+
+	})
+	if err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
---
+++ resultdb/cmd/backend/purge_expired_results_test.go
@@ -0,0 +1,124 @@
+// Copyright 2019 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/spanner"
+	durpb "github.com/golang/protobuf/ptypes/duration"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/resultdb/internal/span"
+	"go.chromium.org/luci/resultdb/internal/testutil"
+	"go.chromium.org/luci/resultdb/pbutil"
+	pb "go.chromium.org/luci/resultdb/proto/rpc/v1"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+// makeTestResultsWithVariants creates test results with a number of passing/failing variants.
+//
+// There'll be two rows for each variant. If the variant is a passing variant, both results
+// will have a passing status, otherwise the first will be passing, and the second faled.
+func makeTestResultsWithVariants(invID, testID string, nPassingVariants, nFailedVariants int) []*pb.TestResult {
+	nVariants := nPassingVariants + nFailedVariants
+	// For every variant we'll generate two rows.
+	statuses := []pb.TestStatus{pb.TestStatus_PASS, pb.TestStatus_PASS}
+	trs := make([]*pb.TestResult, nVariants*len(statuses))
+	for v := 0; v < nVariants; v++ {
+		// We'll generate all passing variants first, so we only have to change the
+		// list of statuses once.
+		if v == nPassingVariants {
+			statuses[len(statuses)-1] = pb.TestStatus_FAIL
+		}
+		for s, status := range statuses {
+			rIndex := v*len(statuses) + s
+			resultID := fmt.Sprintf("%d", rIndex)
+			trs[rIndex] = &pb.TestResult{
+				Name:     pbutil.TestResultName(invID, testID, resultID),
+				TestId:   testID,
+				ResultId: resultID,
+				Variant:  pbutil.Variant("k1", "v1", "k2", fmt.Sprintf("v%d", v)),
+				Expected: status == pb.TestStatus_PASS,
+				Status:   status,
+				Duration: &durpb.Duration{Seconds: int64(rIndex), Nanos: 234567000},
+			}
+		}
+	}
+	return trs
+}
+
+func insertInvocationWithTestResults(ctx context.Context, invID span.InvocationID, nTests, nPassingVariants, nFailedVariants int) span.InvocationID {
+	now := clock.Now(ctx)
+
+	// Insert an invocation,
+	extraArgs := map[string]interface{}{"ExpectedTestResultsExpirationTime": now.Add(-time.Minute)}
+	testutil.MustApply(ctx, testutil.InsertInvocation(invID, pb.Invocation_FINALIZED, now.Add(-time.Hour), extraArgs))
+
+	inserts := []*spanner.Mutation{}
+	for i := 0; i < nTests; i++ {
+		results := makeTestResultsWithVariants(string(invID), fmt.Sprintf("Test%d", i), nPassingVariants, nFailedVariants)
+		inserts = append(inserts, testutil.InsertTestResults(results)...)
+	}
+	testutil.MustApply(ctx, testutil.CombineMutations(inserts)...)
+	return invID
+}
+
+func countTestResults(ctx context.Context, invID span.InvocationID) int64 {
+	st := spanner.NewStatement(`
+		SELECT COUNT(*) FROM TestResults
+		WHERE InvocationId = @invocationId`)
+	st.Params["invocationId"] = span.ToSpanner(invID)
+	var res int64
+	row, err := span.Client(ctx).Single().Query(ctx, st).Next()
+	So(err, ShouldBeNil)
+	err = row.Columns(&res)
+	So(err, ShouldBeNil)
+	return res
+}
+
+func TestPurgeExpiredResults(t *testing.T) {
+	Convey(`TestPurgeExpiredResults`, t, func() {
+		ctx := testutil.SpannerTestContext(t)
+		// Insert three invocations with their data.
+		invocations := []span.InvocationID{
+			insertInvocationWithTestResults(ctx, "inv-some-unexpected", 10, 9, 1),
+			insertInvocationWithTestResults(ctx, "inv-no-unexpected", 10, 10, 0),
+			insertInvocationWithTestResults(ctx, "inv-too-many-unexpected", 1, 1, 1001)}
+
+		expiredResultsInvocationIds, err := sampleExpiredResultsInvocations(ctx, clock.Now(ctx), 100)
+		So(err, ShouldBeNil)
+		So(len(expiredResultsInvocationIds), ShouldEqual, 3)
+
+		// Purge expired data.
+		So(dispatchExpiredResultDeletionTasks(ctx, expiredResultsInvocationIds), ShouldBeNil)
+
+		// Count remaining results
+		// 10 tests * 1 variant with unexpected results * 2 results per test variant
+		So(countTestResults(ctx, invocations[0]), ShouldEqual, 20)
+
+		// 10 tests * 0 variants with unexpected results * 2 results per test variant
+		So(countTestResults(ctx, invocations[1]), ShouldEqual, 0)
+
+		// Too many test variants with unexpected results (1001), so no deletions happen.
+		// 1 test * 1002 variants * 2 results per test variants
+		So(countTestResults(ctx, invocations[2]), ShouldEqual, 2004)
+
+	})
+}
