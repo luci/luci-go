84e70b5361965059e5dc8114e480e0d78a82029c
Roberto Carrillo <robertocn@google.com>
2020-01-27 16:27:23 -0800

    [ResultDB] Add a heartbeat log to backend loops.
    R=chanli,nodir
    
    Change-Id: I391c762768e0045fcbf2388cb586eba5141aa78f

--- resultdb/internal/backend/backend.go
+++ resultdb/internal/backend/backend.go
@@ -17,6 +17,7 @@ package backend
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"go.chromium.org/luci/common/clock"
@@ -26,12 +27,51 @@ import (
 	"go.chromium.org/luci/server"
 )
 
+// ThrottledLogger emits a log item at most every MinHeartbeatInterval.
+//
+// It also holds a format string for the heartbeat message and a logging level.
+//
+// Its zero-value emits a debug-level log every time Beat() is called and
+// includes all the args to the Beat function in the output.
+type ThrottledLogger struct {
+	MinHeartbeatInterval time.Duration
+	Level                logging.Level
+	Format               string
+	lastHeartbeat        time.Time
+	mut                  sync.Mutex
+}
+
+// Beat emits a log item if lastHeartbeat was longer than MinHeartbeatInterval
+// ago.
+//
+// It uses a format string if set, otherwise it simply concatenates the args
+// with fmt.Sprint().
+func (tl ThrottledLogger) Beat(ctx context.Context, args ...interface{}) {
+	start := clock.Now(ctx)
+	tl.mut.Lock()
+	if start.Sub(tl.lastHeartbeat) > tl.MinHeartbeatInterval {
+		if tl.Format == "" {
+			logging.Logf(ctx, tl.Level, fmt.Sprintf("Heartbeat: %s", fmt.Sprint(args...)))
+		} else {
+			logging.Logf(ctx, tl.Level, tl.Format, args...)
+		}
+		tl.lastHeartbeat = start
+	}
+	tl.mut.Unlock()
+}
+
 // processingLoop runs f repeatedly, until the context is cancelled.
 // Ensures f is not called too often (minInterval) and backs off linearly
 // on errors.
-func processingLoop(ctx context.Context, minInterval, maxSleep time.Duration, f func(context.Context) error) {
+func processingLoop(ctx context.Context, activityName string, minInterval, maxSleep time.Duration, f func(context.Context) error) {
 	attempt := 0
+	var iterationCounter int64
+	tl := ThrottledLogger{}
+	tl.Format = fmt.Sprintf("%s has ran %%d iterations since start-up", activityName)
+	tl.MinHeartbeatInterval = time.Duration(5) * time.Minute
 	for ctx.Err() == nil {
+		iterationCounter++
+		tl.Beat(ctx, iterationCounter)
 		start := clock.Now(ctx)
 		if err := f(ctx); err == nil {
 			attempt = 0
@@ -49,6 +89,7 @@ func processingLoop(ctx context.Context, minInterval, maxSleep time.Duration, f 
 			time.Sleep(sleep)
 		}
 	}
+	logging.Warningf(ctx, "Exiting loop for %s due to %v", activityName, ctx.Err())
 }
 
 // Options is backend server configuration.
