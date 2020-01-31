c9d6f04a1adfceb8f44bc972dd2f7af967065921
Nodir Turakulov <nodir@chromium.org>
2020-01-31 10:22:54 -0800

    [resultdb] Fix usage of clock.Now in inv creation
    1) Use Spanner commit timestamp for for Invocation.create_time
    2) Use UTC when we use clock.Now
    
    Change-Id: Ibb37ba3b924e3004ecc1eda46bbb3c69a8e0378b

--- resultdb/internal/recorder/update_invocation.go
+++ resultdb/internal/recorder/update_invocation.go
@@ -30,7 +30,7 @@ import (
 )
 
 // validateUpdateInvocationRequest returns non-nil error if req is invalid.
-func validateUpdateInvocationRequest(req *pb.UpdateInvocationRequest, now time.Time) error {
+func validateUpdateInvocationRequest(req *pb.UpdateInvocationRequest, nowUTC time.Time) error {
 	if err := pbutil.ValidateInvocationName(req.Invocation.GetName()); err != nil {
 		return errors.Annotate(err, "invocation: name").Err()
 	}
@@ -45,7 +45,7 @@ func validateUpdateInvocationRequest(req *pb.UpdateInvocationRequest, now time.T
 		// similar switch statement in UpdateInvocation implementaiton.
 
 		case "deadline":
-			if err := validateInvocationDeadline(req.Invocation.GetDeadline(), now); err != nil {
+			if err := validateInvocationDeadline(req.Invocation.GetDeadline(), nowUTC); err != nil {
 				return errors.Annotate(err, "invocation: deadline").Err()
 			}
 
@@ -59,7 +59,7 @@ func validateUpdateInvocationRequest(req *pb.UpdateInvocationRequest, now time.T
 
 // UpdateInvocation implements pb.RecorderServer.
 func (s *recorderServer) UpdateInvocation(ctx context.Context, in *pb.UpdateInvocationRequest) (*pb.Invocation, error) {
-	if err := validateUpdateInvocationRequest(in, clock.Now(ctx)); err != nil {
+	if err := validateUpdateInvocationRequest(in, clock.Now(ctx).UTC()); err != nil {
 		return nil, appstatus.BadRequest(err)
 	}
 
