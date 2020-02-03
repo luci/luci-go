0181b19a1522228047d7a0c496af28b5dc17700e
Takuto Ikuta <tikuta@chromium.org>
2020-02-03 08:48:07 +0000

    isolate: fix conditional
    archive command does not require isolated flag
    
    Change-Id: I4da4be79d4f3b142ee1ecad83c9da971ceeffd0d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2032651
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>

--- client/cmd/isolate/archive.go
+++ client/cmd/isolate/archive.go
@@ -81,7 +81,7 @@ func (c *archiveRun) Parse(a subcommands.Application, args []string) error {
 	if err != nil {
 		return err
 	}
-	if err := c.isolateFlags.Parse(cwd, RequireIsolateFile&RequireIsolatedFile); err != nil {
+	if err := c.isolateFlags.Parse(cwd, RequireIsolateFile); err != nil {
 		return err
 	}
 	if len(args) != 0 {
