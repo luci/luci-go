de3e0d9f278ec4f40b4ff3f5e7de4735978bac41
Roberto Carrillo <robertocn@chromium.org>
2020-02-05 12:24:18 -0800

    [ResultDB] Validate UpdateIncludedInvocationsRequest
    R=nodir,chanli
    
    Bug: 1035707
    Change-Id: I157f858d38f95fd50dd787c5ff525d4921823b21

--- resultdb/internal/recorder/update_included_invocations.go
+++ resultdb/internal/recorder/update_included_invocations.go
@@ -22,6 +22,7 @@ import (
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc/codes"
 
+	"go.chromium.org/luci/common/data/stringset"
 	"go.chromium.org/luci/common/errors"
 
 	"go.chromium.org/luci/resultdb/internal/appstatus"
@@ -81,7 +82,36 @@ func (s *recorderServer) Include(ctx context.Context, in *pb.IncludeRequest) (*e
 	return &empty.Empty{}, err
 }
 
+// validateUpdateIncludedInvocationsRequest returns a non-nil error if req is
+// determined to be invalid.
+func validateUpdateIncludedInvocationsRequest(req *pb.UpdateIncludedInvocationsRequest) error {
+	if _, err := pbutil.ParseInvocationName(req.IncludingInvocation); err != nil {
+		return errors.Annotate(err, "including_invocation: %q", req.IncludingInvocation).Err()
+	}
+	for _, name := range req.AddInvocations {
+		if name == req.IncludingInvocation {
+			return errors.Reason("cannot include itself").Err()
+		}
+		if _, err := pbutil.ParseInvocationName(name); err != nil {
+			return errors.Annotate(err, "add_invocations: %q", name).Err()
+		}
+	}
+	addedNames := stringset.NewFromSlice(req.AddInvocations...)
+	for _, name := range req.RemoveInvocations {
+		if _, err := pbutil.ParseInvocationName(name); err != nil {
+			return errors.Annotate(err, "remove_invocations: %q", name).Err()
+		}
+		if addedNames.Has(name) {
+			return errors.Reason("cannot add and remove the same invocation at the same time: %q", name).Err()
+		}
+	}
+	return nil
+}
+
 // UpdateIncludedInvocations implements pb.RecorderServer.
 func (s *recorderServer) UpdateIncludedInvocations(ctx context.Context, in *pb.UpdateIncludedInvocationsRequest) (*empty.Empty, error) {
+	if err := validateUpdateIncludedInvocationsRequest(in); err != nil {
+		return nil, appstatus.BadRequest(err)
+	}
 	return nil, appstatus.Errorf(codes.Unimplemented, "RPC is not implemented yet")
 }
