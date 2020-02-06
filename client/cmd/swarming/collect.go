db46d743e7322518ef51ed0dc9b4594aa495a98a
Marc-Antoine Ruel <maruel@google.com>
2020-02-06 16:54:39 -0500

    [swarming] Make swarming collect return non zero in case of failure
    In case of communication failure, the error was ignored and nil was returned.
    Make sure the first RPC failure is returned so the exit code is 1.
    
    Bug: 1049756
    Change-Id: I001110b8bfd9ee10b81237fdb700741d90c8e7a5

--- client/cmd/swarming/collect.go
+++ client/cmd/swarming/collect.go
@@ -382,7 +382,11 @@ func (c *collectRun) main(a subcommands.Application, taskIDs []string) error {
 		ctx, cancel = clock.WithTimeout(ctx, c.timeout)
 		defer cancel()
 	}
+	return c.mainInner(ctx, service, taskIDs)
+}
 
+// mainInner is extracted out to help with testing.
+func (c *collectRun) mainInner(ctx context.Context, service swarmingService, taskIDs []string) error {
 	// Aggregate results by polling and fetching across multiple goroutines.
 	results := make([]taskResult, len(taskIDs))
 	var wg sync.WaitGroup
@@ -405,10 +409,15 @@ func (c *collectRun) main(a subcommands.Application, taskIDs []string) error {
 			return err
 		}
 	}
+	var err error
 	for _, result := range results {
-		if c.taskOutput.includesConsole() || result.err != nil {
+		if result.err != nil {
+			err = result.err
+			continue
+		}
+		if c.taskOutput.includesConsole() {
 			result.Print(os.Stdout)
 		}
 	}
-	return nil
+	return err
 }
