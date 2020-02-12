68118daa6a7c55b5ac02fae8510c7534d13ac438
Nodir Turakulov <nodir@chromium.org>
2020-02-12 11:27:27 -0800

    [resultdb] Remove cron iteration timeout
    It is challenging to come up with a good default for cron iteration
    timeout. We lease 100 tasks and each has up to 10m. Which means a whole
    iteration can take up to 1000 minutes which is more than an hour.
    
    Remove cron iteartion timeout. If a particular cron job really needs it,
    it can limits its own context itself.
    
    R=chanli@chromium.org
    
    Change-Id: If1ceef96eb42b0e25d12d77a135d237f64cf7798

--- resultdb/internal/backend/backend.go
+++ resultdb/internal/backend/backend.go
@@ -58,10 +58,6 @@ type backend struct {
 
 	// taskWorkers is the number of goroutines that process invocation tasks.
 	taskWorkers int
-
-	// cronIterationTimeout is the timeout for each cron() iteration.
-	// Ignored if <=0.
-	cronIterationTimeout time.Duration
 }
 
 // cronGroup runs multiple cron jobs concurrently.
@@ -94,11 +90,6 @@ func (b *backend) cron(ctx context.Context, minInterval time.Duration, f func(co
 		defer paniccatcher.Catch(func(p *paniccatcher.Panic) {
 			logging.Errorf(ctx, "Caught panic: %s\n%s", p.Reason, p.Stack)
 		})
-		if b.cronIterationTimeout > 0 {
-			var cancel context.CancelFunc
-			ctx, cancel = context.WithTimeout(ctx, b.cronIterationTimeout)
-			defer cancel()
-		}
 		return f(ctx)
 	}
 
