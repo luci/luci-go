6ba6b5498f1e0a8d57e51e620bb3d3bdbd9e86ec
Scott Lee <ddoman@chromium.org>
2020-02-27 01:19:15 +1100

    [resultdb] move parseArgs() into flags.go and add initLog()
    This CL contains the following changes.
    (1) replace --port with --sink-server
    (2) remove --result-file
    SinkServer in sink.proto doesn't define ReportTestResultsFromFile() yet.
    The flag will be added back when the SinkServer is ready to process the file.
    (3) rename --invocation-tag with --test-result-tag
    (4) add --test-base-variant
    (5) add --log-level from logging/config.go
    
    Change-Id: Idfa2db016ab38d550796ae5a45b3fc777db82a3a

---
+++ resultdb/cmd/result_sink_wrapper/flags.go
@@ -0,0 +1,102 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"os"
+
+	"go.chromium.org/luci/common/data/strpair"
+	"go.chromium.org/luci/common/errors"
+	luciflag "go.chromium.org/luci/common/flag"
+	"go.chromium.org/luci/common/logging"
+)
+
+// sinkWrapperFlags defines command line flags related to result_sink_wrapper.
+// Use NewWrapperFlags() to get a sinkWrapperFlags struct with sensible default values.
+type sinkWrapperFlags struct {
+	// flags for the test results
+	InvocationIDFile string
+	TestIDPrefix     string
+	TestBaseVariants strpair.Map
+	TestResultTags   strpair.Map
+
+	CompleteInvocationExitCodes []int32
+
+	// flags for the SinkServer
+	SinkAddress string
+	// TODO(ddoman): move this to sink/flags.go so that the flags become available
+	// to whichever binaries importing SinkServer.
+	Recorder string
+
+	// flags for logging
+	LogFile       string
+	LoggingConfig logging.Config
+}
+
+// NewFlags returns a Flags struct with sensible default values.
+func newFlags() *sinkWrapperFlags {
+	return &sinkWrapperFlags{
+		CompleteInvocationExitCodes: []int32{0},
+
+		// TODO(ddoman): Set the default value with sink.DefaultAddress.
+		SinkAddress: "localhost:62115",
+		// TODO(ddoman): Set the default recorder address.
+		Recorder: "",
+
+		LoggingConfig: logging.Config{Level: logging.Debug},
+	}
+}
+
+// Register adds sinkWrapper related flags to a FlagSet.
+func (f *sinkWrapperFlags) register(fs *flag.FlagSet) {
+	// flags for the test results
+	fs.StringVar(&f.InvocationIDFile, "invocation-id-file", f.InvocationIDFile,
+		"Path to write the generated invocation ID")
+	fs.StringVar(&f.TestIDPrefix, "test-id-prefix", f.TestIDPrefix,
+		"Prefix to prepepend before the test id of every test result")
+	fs.Var(luciflag.StringPairs(f.TestBaseVariants), "test-base-variant",
+		"Base for variant def specified for test result, of form key:value, may be set multiple times")
+	fs.Var(luciflag.StringPairs(f.TestResultTags), "test-result-tag",
+		"Tag to add to all test results, of form key:value, may be set multiple times")
+
+	fs.Var(CommaCodeList(&f.CompleteInvocationExitCodes),
+		"complete-invocation-exit-codes",
+		"Comma-separated list of exit codes from the subprocess that mean the Invocation should be marked non-interrupted")
+
+	// flags for the SinkServer
+	fs.StringVar(&f.SinkAddress, "sink-address", f.SinkAddress,
+		"TCP address for the SinkServer to listen on")
+	fs.StringVar(&f.Recorder, "recorder", f.Recorder,
+		"Hostname of the Recorder service that the server should upload results to")
+
+	// flags for logs
+	fs.StringVar(&f.LogFile, "log-file", f.LogFile, "the logoutput")
+	f.LoggingConfig.AddFlags(fs)
+}
+
+func parseFlags() (*sinkWrapperFlags, []string, error) {
+	fs := flag.NewFlagSet("", flag.ContinueOnError)
+	wrapperFlags := newFlags()
+	wrapperFlags.register(fs)
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		return nil, nil, err
+	}
+	args := fs.Args()
+	if len(args) < 1 {
+		return nil, nil, errors.Reason("must pass command to run in wrapper").Err()
+	}
+	return wrapperFlags, args, nil
+}
--- resultdb/cmd/result_sink_wrapper/main.go
+++ resultdb/cmd/result_sink_wrapper/main.go
@@ -16,14 +16,10 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"os"
-	"strconv"
 
-	"go.chromium.org/luci/common/data/strpair"
 	"go.chromium.org/luci/common/errors"
-	luciflag "go.chromium.org/luci/common/flag"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/logging/gologger"
 	"go.chromium.org/luci/common/system/exec2"
@@ -36,114 +32,11 @@ const (
 	wrapperErrorCode = 1001
 )
 
-type wrapperArgs struct {
-	port                        int
-	recorder                    string
-	invocationIDFile            string
-	logFile                     string
-	resultFiles                 strpair.Map
-	testIDPrefix                string
-	invocationTags              strpair.Map
-	baseTestVariant             map[string]string
-	completeInvocationExitCodes []int
-}
-
-func parseArgs() (wrapperArgs, error) {
-	args := wrapperArgs{
-		invocationTags: make(strpair.Map),
-		resultFiles:    make(strpair.Map),
-	}
-
-	flag.IntVar(&args.port, "port", 0, "TCP port to listen on, will be arbitrarily selected if unset")
-	// TODO(sajjadm): Set the default recorder once it exists.
-	flag.StringVar(&args.recorder, "recorder", "",
-		"Hostname of the Recorder service that the server should upload results to")
-
-	flag.StringVar(&args.invocationIDFile, "invocation-id-file", "",
-		"Path to write the generated invocation ID")
-
-	flag.StringVar(&args.logFile, "log-file", "", "File to log to")
-
-	flag.Var(luciflag.StringPairs(args.resultFiles), "result-file",
-		"Files to read and upload after running the subprocess, of form format:path, may be set more than once. Valid formats are luci, chromium_jtr, and chromium_gtest")
-
-	flag.StringVar(&args.testIDPrefix, "test-id-prefix", "",
-		"Prefix to prepepend before the test id of every test result")
-
-	flag.Var(luciflag.StringPairs(args.invocationTags), "invocation-tag",
-		"Tag to add to the Invocation, of form key:value, may be set more than once")
-
-	// TODO(sajjadm): Add new function to flag package that decodes to a map[string]string and
-	// enforces unique keys, then use that function to implement a -base-test-variant flag.
-	// The description for the flag will be:
-	// "Variant definition pairs common for all test results in this Invocation, of form key:value"
-	// It will decode into wrapperArgs.baseTestVariant
-
-	var rawExitCodes []string
-	flag.Var(luciflag.CommaList(&rawExitCodes),
-		"complete-invocation-exit-codes",
-		"Comma-separated list of exit codes from the subprocess that mean the Invocation should be marked non-interrupted")
-
-	flag.Parse()
-
-	args.completeInvocationExitCodes = make([]int, len(rawExitCodes))
-	for i, rawCode := range rawExitCodes {
-		code, err := strconv.Atoi(rawCode)
-		if err != nil {
-			return args, errors.Annotate(err, "must pass integers to -complete-invocation-exit-codes").Err()
-		}
-		args.completeInvocationExitCodes[i] = code
-	}
-
-	if len(args.completeInvocationExitCodes) == 0 {
-		args.completeInvocationExitCodes = []int{0}
-	}
-
-	return args, nil
-}
-
 type wrapper struct {
 	serverCfg     sink.ServerConfig
-	logFile       *os.File
-	logCfg        gologger.LoggerConfig
 	cmdName       string
 	cmdArgs       []string
 	childExitCode int
-}
-
-func (w *wrapper) init() error {
-	args, err := parseArgs()
-	if err != nil {
-		return err
-	}
-
-	if flag.NArg() < 1 {
-		return errors.Reason("must pass command to run in wrapper").Err()
-	}
-	w.cmdName = flag.Arg(0)
-	w.cmdArgs = flag.Args()[1:]
-
-	w.serverCfg.Port = args.port
-
-	if args.logFile == "" {
-		w.logCfg.Out = os.Stderr
-	} else {
-		f, err := os.OpenFile(args.logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			return err
-		}
-		w.logFile = f
-		w.logCfg.Out = w.logFile
-	}
-	// TODO(sajjadm): Parse remaining args into meaningful structures
-
-	return nil
-}
-
-func (w *wrapper) close() {
-	if w.logFile != nil {
-		w.logFile.Close()
-	}
 }
 
 func (w *wrapper) run(ctx context.Context) (int, error) {
@@ -194,24 +87,46 @@ func (w *wrapper) main(ctx context.Context) error {
 	return nil
 }
 
-func main() {
-	var w wrapper
-	if err := w.init(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(wrapperErrorCode)
+func initLogs(flags *sinkWrapperFlags) (context.Context, func(), error) {
+	lcfg := gologger.LoggerConfig{Out: os.Stderr}
+	cleanup := func() {}
+	if flags.LogFile != "" {
+		o, err := os.OpenFile(flags.LogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return nil, cleanup, err
+		}
+		lcfg.Out = o
+		cleanup = func() {
+			o.Sync()
+			o.Close()
+		}
 	}
+	return flags.LoggingConfig.Set(lcfg.Use(context.Background())), cleanup, nil
+}
 
-	ctx := w.logCfg.Use(context.Background())
-	ctx = logging.SetLevel(ctx, logging.Debug)
+func fatalf(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(wrapperErrorCode)
+}
+
+func main() {
+	flags, args, err := parseFlags()
+	if err != nil {
+		fatalf(err)
+	}
+	ctx, cleanup, err := initLogs(flags)
+	if err != nil {
+		fatalf(err)
+	}
+	defer cleanup()
 
 	var exitCode int
+	w := wrapper{cmdName: args[0], cmdArgs: args[1:]}
 	if err := w.main(ctx); err != nil {
 		logging.Errorf(ctx, "FATAL: %s", err)
 		exitCode = wrapperErrorCode
 	} else {
 		exitCode = w.childExitCode
 	}
-
-	w.close()
 	os.Exit(exitCode)
 }
