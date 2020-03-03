b53641fd13197b84fdb4e1a6017e927f4c918b16
smut <smut@google.com>
2020-03-03 15:38:15 -0800

    [buildbucket] Validate GetBuild requests
    Validates requests but doesn't fully implement fetching results.
    
    Bug: 1042991
    Change-Id: Iafb55c795e8751cbed54b5daf5e8ab7cafb234e1

---
+++ buildbucket/appengine/model/model.go
@@ -0,0 +1,34 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package model
+
+import "go.chromium.org/gae/service/datastore"
+
+// Build is representation of a build in the datastore.
+type Build struct {
+	_extra datastore.PropertyMap `gae:"-,extra"`
+	_kind  string                `gae:"$kind,Build"`
+	ID     int64                 `gae:"$id"`
+
+	Project string `gae:"project"`
+
+	// <project>/<bucket>. Bucket is in v2 format.
+	// e.g. chromium/try (never chromium/luci.chromium.try).
+	BucketID string `gae:"bucket_id"`
+
+	// <project>/<bucket>/<builder>. Bucket is in v2 format.
+	// e.g. chromium/try/linux-rel.
+	BuilderID string `gae:"builder_id"`
+}
