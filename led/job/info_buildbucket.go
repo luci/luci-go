d7be7d3d205f393345a3a7c836379c1414fcb9b9
Robert Iannucci <iannucci@chromium.org>
2020-03-05 09:25:17 -0800

    [led] Add info buildbucket skeleton.
    Includes 'obvious' one-liners.
    
    R=smut@chromium.org, tandrii@chromium.org
    
    Bug: 1017818
    Change-Id: Ibd4e794717c739781a29fc01e302d7703e2cc656

---
+++ led/job/info_buildbucket.go
@@ -0,0 +1,66 @@
+// Copyright 2020 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package job
+
+import (
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+	swarmingpb "go.chromium.org/luci/swarming/proto/api"
+)
+
+type bbInfo struct {
+	*Buildbucket
+
+	userPayload *swarmingpb.CASTree
+}
+
+var _ Info = bbInfo{}
+
+func (b bbInfo) SwarmingHostname() string {
+	return b.GetBbagentArgs().GetBuild().GetInfra().GetSwarming().GetHostname()
+}
+
+func (b bbInfo) TaskName() string {
+	return b.GetName()
+}
+
+func (b bbInfo) CurrentIsolated() (*swarmingpb.CASTree, error) {
+	return b.userPayload, nil
+}
+
+func (b bbInfo) Env() (ret map[string]string, err error) {
+	panic("implement me")
+}
+
+func (b bbInfo) Priority() int32 {
+	return b.GetBbagentArgs().GetBuild().GetInfra().GetSwarming().GetPriority()
+}
+
+func (b bbInfo) PrefixPathEnv() (ret []string, err error) {
+	panic("implement me")
+}
+
+func (b bbInfo) Tags() (ret []string) {
+	panic("implement me")
+}
+
+func (b bbInfo) Experimental() bool {
+	return b.GetBbagentArgs().GetBuild().GetInput().GetExperimental()
+}
+
+func (b bbInfo) Properties() (ret map[string]string, err error) {
+	panic("implement me")
+}
+
+func (b bbInfo) GerritChanges() (ret []*bbpb.GerritChange) {
+	panic("implement me")
+}
+
+func (b bbInfo) GitilesCommit() (ret *bbpb.GitilesCommit) {
+	panic("implement me")
+}
+
+func (b bbInfo) TaskPayload() (cipdPkg, cipdVers string, pathInTask string) {
+	panic("implement me")
+}
