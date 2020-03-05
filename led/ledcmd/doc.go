d2d9a0b4b1eccaa1118b141f58e6195cefac974f
Robert Iannucci <iannucci@chromium.org>
2020-03-05 11:40:38 -0800

    [ledcmd] Just the doc.
    R=smut@chromium.org, tandrii@chromium.org
    
    Bug: 1017818
    Change-Id: I0f0fc878845d8a991019416e7e6c0635a62f1df0

---
+++ led/ledcmd/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2017 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package ledcmd implements the subcommands for the `led` command line tool
+// which interact with external services.
+package ledcmd
