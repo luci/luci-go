4a13fa6cfcad0d5208da83a88b57c6a13429ddc5
Weiwei Lin <weiweilin@google.com>
2020-03-05 11:51:44 +1100

    [test-results-ui] first commit of test results ui
    Setup app engine config file, build script, test script, etc.
    Implement routing, 404 page, login page & trivial invocation page.
    
    Bug: 1044351
    Change-Id: I97e763b93d8fac8ec905300baffbd4112d94b923

---
+++ milo/frontend/test-results-ui/webpack.config.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, '../appengine/test-results-ui/static/dist/scripts/'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '../appengine/test-results-ui/'),
+    publicPath: '/static/dist/scripts/',
+    historyApiFallback: true,
+  }
+};
+
+// Default export is required by webpack.
+// tslint:disable-next-line: no-default-export
+export default config;
