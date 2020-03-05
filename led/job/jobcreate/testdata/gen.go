6d588c0a1a060f5e003fc9d6275d11d8c5f0c9e9
Robert Iannucci <iannucci@chromium.org>
2020-03-05 13:58:27 -0800

    [led] Add ability to create Buildbucket job Definitions from swarming.
    R=smut@chromium.org, tandrii@chromium.org
    
    Bug: 1017818
    Change-Id: I54afc827367e8cbd085ad5d12aa7038fc5a75c84

---
+++ led/job/jobcreate/testdata/gen.go
@@ -0,0 +1,20 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testdata
+
+// Running `go generate` will regenerate the '.job.json' files. When doing this
+// make sure to inspect the diff to ensure that the changes are correct.
+
+//go:generate go test .. -train
