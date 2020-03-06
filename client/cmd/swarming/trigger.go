1c7f9f065f5aa42f57c96162932aaedff0d273a6
Takuto Ikuta <tikuta@chromium.org>
2020-03-06 18:28:11 +0900

    swarming: show task url in trigger command
    Change-Id: I06ebc573ea454dbfd8927cf04f84afc1956ee84a

--- client/cmd/swarming/trigger.go
+++ client/cmd/swarming/trigger.go
@@ -292,6 +292,11 @@ func (c *triggerRun) main(a subcommands.Application, args []string, env subcomma
 		fmt.Println()
 	}
 
+	if !c.defaultFlags.Quiet {
+		fmt.Println("You can also see task status in")
+		fmt.Printf("  https://%s/task?id=%s\n", c.serverURL, result.TaskId)
+	}
+
 	duration := time.Since(start)
 	log.Printf("Duration: %s\n", units.Round(duration, time.Millisecond))
 	return nil
