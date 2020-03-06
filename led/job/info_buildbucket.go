83385d736fd3c94766e36a7303e79bb920bae7d0
Robert Iannucci <iannucci@chromium.org>
2020-03-06 11:37:07 -0800

    [led] Add info buildbucket skeleton.
    Includes 'obvious' one-liners.
    
    R=smut@chromium.org, tandrii@chromium.org
    
    Bug: 1017818
    Change-Id: Ibd4e794717c739781a29fc01e302d7703e2cc656

---
+++ led/job/info_buildbucket.go
@@ -0,0 +1,76 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package job
+
+import (
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+	swarmingpb "go.chromium.org/luci/swarming/proto/api"
+)
+
+type bbInfo struct {
+	*Buildbucket
+
+	userPayload *swarmingpb.CASTree
+}
+
+var _ Info = bbInfo{}
+
+func (b bbInfo) SwarmingHostname() string {
+	return b.GetBbagentArgs().GetBuild().GetInfra().GetSwarming().GetHostname()
+}
+
+func (b bbInfo) TaskName() string {
+	return b.GetName()
+}
+
+func (b bbInfo) CurrentIsolated() (*swarmingpb.CASTree, error) {
+	return b.userPayload, nil
+}
+
+func (b bbInfo) Env() (ret map[string]string, err error) {
+	panic("implement me")
+}
+
+func (b bbInfo) Priority() int32 {
+	return b.GetBbagentArgs().GetBuild().GetInfra().GetSwarming().GetPriority()
+}
+
+func (b bbInfo) PrefixPathEnv() (ret []string, err error) {
+	panic("implement me")
+}
+
+func (b bbInfo) Tags() (ret []string) {
+	panic("implement me")
+}
+
+func (b bbInfo) Experimental() bool {
+	return b.GetBbagentArgs().GetBuild().GetInput().GetExperimental()
+}
+
+func (b bbInfo) Properties() (ret map[string]string, err error) {
+	panic("implement me")
+}
+
+func (b bbInfo) GerritChanges() (ret []*bbpb.GerritChange) {
+	panic("implement me")
+}
+
+func (b bbInfo) GitilesCommit() (ret *bbpb.GitilesCommit) {
+	panic("implement me")
+}
+
+func (b bbInfo) TaskPayload() (cipdPkg, cipdVers string, pathInTask string) {
+	panic("implement me")
+}
