c1c0f82220418e485d021077b219f74058d28be5
Robert Iannucci <iannucci@chromium.org>
2020-03-07 01:56:51 +0000

    [led] Add initial tests for Info() methods.
    Includes swarming implementation of CurrentIsolated as well.
    
    R=smut@chromium.org, tandrii@chromium.org
    
    Bug: 1017818
    Change-Id: I85af923c3915afe725a2f9af07639ce1730bf4d9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2090118
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>

--- led/job/info_swarming.go
+++ led/job/info_swarming.go
@@ -15,6 +15,7 @@
 package job
 
 import (
+	"go.chromium.org/luci/common/errors"
 	swarmingpb "go.chromium.org/luci/swarming/proto/api"
 )
 
@@ -34,7 +35,27 @@ func (s swInfo) TaskName() string {
 }
 
 func (s swInfo) CurrentIsolated() (*swarmingpb.CASTree, error) {
-	panic("implement me")
+	isolatedOptions := map[string]*swarmingpb.CASTree{}
+	if up := s.userPayload; up != nil {
+		isolatedOptions[up.Digest] = up
+	}
+
+	if sw := s.Swarming; sw != nil {
+		for _, slc := range sw.GetTask().GetTaskSlices() {
+			input := slc.GetProperties().GetCasInputs()
+			if input != nil {
+				isolatedOptions[input.Digest] = input
+			}
+		}
+	}
+	if len(isolatedOptions) > 1 {
+		return nil, errors.Reason(
+			"Definition contains multiple isolateds: %v", isolatedOptions).Err()
+	}
+	for _, v := range isolatedOptions {
+		return v, nil
+	}
+	return nil, nil
 }
 
 func (s swInfo) Env() (ret map[string]string, err error) {
