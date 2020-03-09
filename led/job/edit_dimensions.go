fa219b680c5e695ea0743d09b7ff76d14dc4acec
Robert Iannucci <iannucci@chromium.org>
2020-03-09 13:13:49 -0700

    [led] Implement dimension info+editing.
    R=smut@chromium.org, tandrii@chromium.org
    
    Bug: 1017818
    Change-Id: I0f970b3b0ddde565652057af71af084df11f7478

---
+++ led/job/edit_dimensions.go
@@ -0,0 +1,315 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package job
+
+import (
+	fmt "fmt"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	durpb "github.com/golang/protobuf/ptypes/duration"
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+)
+
+// ExpiringValue represents a tuple of dimension value, plus an expiration time.
+//
+// If Expiration is zero, it counts as "no expiration".
+type ExpiringValue struct {
+	Value      string
+	Expiration time.Duration
+}
+
+// DimensionEditCommand is instruction on how to process the values in the task
+// associated with a swarming dimension.
+//
+// The fields are processed in order:
+//   * if SetValues is non-nil, the dimension values are set to this set
+//     (including empty).
+//   * if RemoveValues is non-empty, these values will be removed from the
+//     dimension values.
+//   * if AddValues is non-empty, these values will ber added to the dimension
+//     values.
+//
+// If the set of values at the end of this process is empty, the dimension will
+// be removed from the task. Otherwise the dimension will be set to the sorted
+// remaining values.
+type DimensionEditCommand struct {
+	SetValues    []ExpiringValue
+	RemoveValues []string
+	AddValues    []ExpiringValue
+}
+
+// DimensionEditCommands is a mapping of dimension name to a set of commands to
+// apply to the values of that dimension.
+type DimensionEditCommands map[string]*DimensionEditCommand
+
+func split2(s, sep string) (a, b string, ok bool) {
+	idx := strings.Index(s, sep)
+	if idx == -1 {
+		return s, "", false
+	}
+	return s[:idx], s[idx+1:], true
+}
+
+func rsplit2(s, sep string) (a, b string) {
+	idx := strings.LastIndex(s, sep)
+	if idx == -1 {
+		return s, ""
+	}
+	return s[:idx], s[idx+1:]
+}
+
+func parseDimensionEditCmd(cmd string) (dim, op, val string, exp time.Duration, err error) {
+	dim, valueExp, ok := split2(cmd, "=")
+	if !ok {
+		err = errors.Reason("expected $key$op$value, but op was missing").Err()
+		return
+	}
+
+	switch dim[len(dim)-1] {
+	case '-':
+		op = "-="
+		dim = dim[:len(dim)-1]
+	case '+':
+		op = "+="
+		dim = dim[:len(dim)-1]
+	default:
+		op = "="
+	}
+
+	val, expStr := rsplit2(valueExp, "@")
+	if expStr != "" {
+		var expSec int
+		if expSec, err = strconv.Atoi(expStr); err != nil {
+			err = errors.Annotate(err, "parsing expiration %q", expStr).Err()
+			return
+		}
+		exp = time.Second * time.Duration(expSec)
+	}
+
+	if val == "" && op != "=" {
+		err = errors.Reason("empty value not allowed for operator %q: %q", op, cmd).Err()
+	}
+	if exp != 0 && op == "-=" {
+		err = errors.Reason("expiration seconds not allowed for operator %q: %q", op, cmd).Err()
+	}
+
+	return
+}
+
+// MakeDimensionEditCommands takes a slice of commands in the form of:
+//
+//   dimension=
+//   dimension=value
+//   dimension=value@1234
+//
+//   dimension-=value
+//
+//   dimension+=value
+//   dimension+=value@1234
+//
+// Logically:
+//   * dimension_name - The name of the dimension to modify
+//   * operator
+//     * "=" - Add value to SetValues. If empty, ensures that SetValues is
+//       non-nil (i.e. clear all values for this dimension).
+//     * "-=" - Add value to RemoveValues.
+//     * "+=" - Add value to AddValues.
+//   * value - The dimension value for the operand
+//   * expiration seconds - The time at which this value should expire.
+//
+// All equivalent operations for the same dimension will be grouped into
+// a single DimensionEditCommand in the order they appear in `commands`.
+func MakeDimensionEditCommands(commands []string) (DimensionEditCommands, error) {
+	if len(commands) == 0 {
+		return nil, nil
+	}
+
+	ret := DimensionEditCommands{}
+	for _, command := range commands {
+		dimension, operator, value, expiration, err := parseDimensionEditCmd(command)
+		if err != nil {
+			return nil, errors.Annotate(err, "parsing %q", command).Err()
+		}
+		editCmd := ret[dimension]
+		if editCmd == nil {
+			editCmd = &DimensionEditCommand{}
+			ret[dimension] = editCmd
+		}
+		switch operator {
+		case "=":
+			// explicitly setting SetValues takes care of the 'dimension=' case.
+			if editCmd.SetValues == nil {
+				editCmd.SetValues = []ExpiringValue{}
+			}
+			if value != "" {
+				editCmd.SetValues = append(editCmd.SetValues, ExpiringValue{
+					Value: value, Expiration: expiration,
+				})
+			}
+		case "-=":
+			editCmd.RemoveValues = append(editCmd.RemoveValues, value)
+		case "+=":
+			editCmd.AddValues = append(editCmd.AddValues, ExpiringValue{
+				Value: value, Expiration: expiration,
+			})
+		}
+	}
+	return ret, nil
+}
+
+// Applies the DimensionEditCommands to the given logicalDimensions.
+func (dimEdits DimensionEditCommands) apply(dimMap logicalDimensions, minExp time.Duration) {
+	if len(dimEdits) == 0 {
+		return
+	}
+
+	shouldApply := func(eVal ExpiringValue) bool {
+		return eVal.Expiration == 0 || minExp == 0 || eVal.Expiration >= minExp
+	}
+
+	for dim, edits := range dimEdits {
+		if edits.SetValues != nil {
+			dimMap[dim] = make(dimValueExpiration, len(edits.SetValues))
+			for _, expVal := range edits.SetValues {
+				if shouldApply(expVal) {
+					dimMap[dim][expVal.Value] = expVal.Expiration
+				}
+			}
+		}
+		for _, value := range edits.RemoveValues {
+			delete(dimMap[dim], value)
+		}
+		for _, expVal := range edits.AddValues {
+			if shouldApply(expVal) {
+				expValMap := dimMap[dim]
+				if expValMap == nil {
+					expValMap = dimValueExpiration{}
+					dimMap[dim] = expValMap
+				}
+				expValMap[expVal.Value] = expVal.Expiration
+			}
+		}
+	}
+	toRemove := stringset.New(len(dimMap))
+	for dim, valExps := range dimMap {
+		if len(valExps) == 0 {
+			toRemove.Add(dim)
+		}
+	}
+	toRemove.Iter(func(dim string) bool {
+		delete(dimMap, dim)
+		return true
+	})
+}
+
+// ExpiringDimensions is a map from dimension name to a list of values
+// corresponding to that dimension.
+//
+// When retrieved from a led library, the values will be sorted by expiration
+// time, followed by value. Expirations of 0 (i.e. "infinite") are sorted last.
+type ExpiringDimensions map[string][]ExpiringValue
+
+func (e ExpiringDimensions) String() string {
+	bits := []string{}
+	for key, values := range e {
+		for _, value := range values {
+			if value.Expiration == 0 {
+				bits = append(bits, fmt.Sprintf("%s=%s", key, value.Value))
+			} else {
+				bits = append(bits, fmt.Sprintf(
+					"%s=%s@%d", key, value.Value, value.Expiration/time.Second))
+			}
+		}
+	}
+	return strings.Join(bits, ", ")
+}
+
+func (e ExpiringDimensions) toLogical() logicalDimensions {
+	ret := logicalDimensions{}
+	for key, expVals := range e {
+		dve := ret[key]
+		if dve == nil {
+			dve = dimValueExpiration{}
+			ret[key] = dve
+		}
+		for _, expVal := range expVals {
+			dve[expVal.Value] = expVal.Expiration
+		}
+	}
+	return ret
+}
+
+type dimValueExpiration map[string]time.Duration
+
+func (valExps dimValueExpiration) toSlice() []string {
+	ret := make([]string, 0, len(valExps))
+	for value := range valExps {
+		ret = append(ret, value)
+	}
+	sort.Strings(ret)
+	return ret
+}
+
+// A multimap of dimension to values.
+type logicalDimensions map[string]dimValueExpiration
+
+func (dims logicalDimensions) update(dim, value string, expiration *durpb.Duration) {
+	var exp time.Duration
+	if expiration != nil {
+		var err error
+		if exp, err = ptypes.Duration(expiration); err != nil {
+			panic(err)
+		}
+	}
+	dims.updateDuration(dim, value, exp)
+}
+
+func (dims logicalDimensions) updateDuration(dim, value string, exp time.Duration) {
+	if dims[dim] == nil {
+		dims[dim] = dimValueExpiration{}
+	}
+	dims[dim][value] = exp
+}
+
+func expLess(a, b time.Duration) bool {
+	if a == 0 { // b is either infinity (==a) or finite (<a)
+		return false
+	} else if b == 0 { // b is infinity, a is finite
+		return true
+	}
+	return a < b
+}
+
+func (dims logicalDimensions) toExpiringDimensions() ExpiringDimensions {
+	ret := ExpiringDimensions{}
+	for key, dve := range dims {
+		for dim, expiration := range dve {
+			ret[key] = append(ret[key], ExpiringValue{dim, expiration})
+		}
+		sort.Slice(ret[key], func(i, j int) bool {
+			a, b := ret[key][i], ret[key][j]
+			if a.Expiration == b.Expiration {
+				return a.Value < b.Value
+			}
+			return expLess(a.Expiration, b.Expiration)
+		})
+	}
+	return ret
+}
---
+++ led/job/edit_dimensions_test.go
@@ -0,0 +1,393 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package job
+
+import (
+	fmt "fmt"
+	"testing"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	. "github.com/smartystreets/goconvey/convey"
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+	. "go.chromium.org/luci/common/testing/assertions"
+	api "go.chromium.org/luci/swarming/proto/api"
+)
+
+func TestMakeDimensionEditCommands(t *testing.T) {
+	t.Parallel()
+
+	var testCases = []struct {
+		name   string
+		cmds   []string
+		err    string
+		expect DimensionEditCommands
+	}{
+		{name: "empty"},
+		{
+			name: "err no op",
+			cmds: []string{"bad"},
+			err:  "op was missing",
+		},
+		{
+			name: "err no value",
+			cmds: []string{"bad+="},
+			err:  "empty value not allowed for operator",
+		},
+		{
+			name: "err bad expiration",
+			cmds: []string{"bad=value@dogs"},
+			err:  `parsing expiration "dogs"`,
+		},
+		{
+			name: "err unwanted expiration",
+			cmds: []string{"bad-=value@123"},
+			err:  "expiration seconds not allowed",
+		},
+		{
+			name: "reset",
+			cmds: []string{
+				"key=",
+			},
+			expect: DimensionEditCommands{
+				"key": &DimensionEditCommand{
+					SetValues: []ExpiringValue{},
+				},
+			},
+		},
+		{
+			name: "set",
+			cmds: []string{
+				"key=value",
+				"key=other_value@123",
+				"something=value", // ignored on 'apply', but will show up here
+				"something=value@123",
+			},
+			expect: DimensionEditCommands{
+				"key": &DimensionEditCommand{
+					SetValues: []ExpiringValue{
+						{Value: "value"},
+						{Value: "other_value", Expiration: 123 * time.Second},
+					},
+				},
+				"something": &DimensionEditCommand{
+					SetValues: []ExpiringValue{
+						{Value: "value"},
+						{Value: "value", Expiration: 123 * time.Second},
+					},
+				},
+			},
+		},
+		{
+			name: "add",
+			cmds: []string{
+				"key+=value",
+				"key+=other_value@123",
+				"something+=value", // ignored on 'apply', but will show up here
+				"something+=value@123",
+			},
+			expect: DimensionEditCommands{
+				"key": &DimensionEditCommand{
+					AddValues: []ExpiringValue{
+						{Value: "value"},
+						{Value: "other_value", Expiration: 123 * time.Second},
+					},
+				},
+				"something": &DimensionEditCommand{
+					AddValues: []ExpiringValue{
+						{Value: "value"},
+						{Value: "value", Expiration: 123 * time.Second},
+					},
+				},
+			},
+		},
+		{
+			name: "remove",
+			cmds: []string{
+				"key-=value",
+				"key-=other_value",
+			},
+			expect: DimensionEditCommands{
+				"key": &DimensionEditCommand{
+					RemoveValues: []string{"value", "other_value"},
+				},
+			},
+		},
+	}
+
+	Convey(`MakeDimensionEditCommands`, t, func() {
+		for _, tc := range testCases {
+			tc := tc
+			Convey(tc.name, func() {
+				dec, err := MakeDimensionEditCommands(tc.cmds)
+				if tc.err == "" {
+					So(err, ShouldBeNil)
+					So(dec, ShouldResemble, tc.expect)
+				} else {
+					So(err, ShouldErrLike, tc.err)
+				}
+			})
+		}
+	})
+}
+
+func TestSetDimensions(t *testing.T) {
+	t.Parallel()
+
+	runCases(t, "SetDimensions", []testCase{
+		{
+			name: "nil",
+			fn: func(jd *Definition) {
+				SoEdit(jd, func(je Editor) {
+					je.SetDimensions(nil)
+				})
+				So(mustGetDimensions(jd), ShouldBeEmpty)
+			},
+		},
+
+		{
+			name:        "add",
+			skipSWEmpty: true,
+			fn: func(jd *Definition) {
+				baselineDims(jd)
+
+				So(mustGetDimensions(jd).String(), ShouldResemble, ExpiringDimensions{
+					"key": []ExpiringValue{
+						{Value: "A", Expiration: swSlice1Exp},
+						{Value: "AA", Expiration: swSlice1Exp},
+						{Value: "B", Expiration: swSlice2Exp},
+						{Value: "C", Expiration: swSlice3Exp},
+						{Value: "Z", Expiration: swSlice3Exp},
+					},
+				}.String())
+
+				if sw := jd.GetSwarming(); sw != nil {
+					// ensure dimensions show up in ALL slices which they ought to.
+					So(sw.Task.TaskSlices[0].Properties.Dimensions, ShouldResemble, []*api.StringListPair{
+						{
+							Key:    "key",
+							Values: []string{"A", "AA", "B", "C", "Z"},
+						},
+					})
+					So(sw.Task.TaskSlices[1].Properties.Dimensions, ShouldResemble, []*api.StringListPair{
+						{
+							Key:    "key",
+							Values: []string{"B", "C", "Z"},
+						},
+					})
+					So(sw.Task.TaskSlices[2].Properties.Dimensions, ShouldResemble, []*api.StringListPair{
+						{
+							Key:    "key",
+							Values: []string{"C", "Z"},
+						},
+					})
+				} else {
+					rdims := jd.GetBuildbucket().BbagentArgs.Build.Infra.Swarming.TaskDimensions
+					So(rdims, ShouldResemble, []*bbpb.RequestedDimension{
+						{Key: "key", Value: "A", Expiration: ptypes.DurationProto(swSlice1Exp)},
+						{Key: "key", Value: "AA", Expiration: ptypes.DurationProto(swSlice1Exp)},
+						{Key: "key", Value: "B", Expiration: ptypes.DurationProto(swSlice2Exp)},
+						{Key: "key", Value: "C", Expiration: ptypes.DurationProto(swSlice3Exp)},
+						{Key: "key", Value: "Z"},
+					})
+				}
+			},
+		},
+
+		{
+			name:        "replace",
+			skipSWEmpty: true,
+			fn: func(jd *Definition) {
+				baselineDims(jd)
+
+				SoEdit(jd, func(je Editor) {
+					je.SetDimensions(ExpiringDimensions{
+						"key": []ExpiringValue{
+							{Value: "norp", Expiration: swSlice1Exp},
+						},
+					})
+				})
+
+				So(mustGetDimensions(jd), ShouldResemble, ExpiringDimensions{
+					"key": []ExpiringValue{
+						{Value: "norp", Expiration: swSlice1Exp},
+					},
+				})
+			},
+		},
+
+		{
+			name:        "delete",
+			skipSWEmpty: true,
+			fn: func(jd *Definition) {
+				baselineDims(jd)
+
+				SoEdit(jd, func(je Editor) {
+					je.SetDimensions(nil)
+				})
+
+				So(mustGetDimensions(jd), ShouldResemble, ExpiringDimensions{})
+			},
+		},
+
+		{
+			name:        "bad expiration",
+			skipSWEmpty: true,
+			skipBB:      true,
+			fn: func(jd *Definition) {
+				err := jd.Edit(func(je Editor) {
+					je.SetDimensions(ExpiringDimensions{
+						"key": []ExpiringValue{
+							{Value: "narp", Expiration: time.Second * 10},
+						},
+					})
+				})
+				So(err, ShouldErrLike,
+					"key=narp@10 has invalid expiration time: "+
+						"current slices expire at [0 60 240 600]")
+			},
+		},
+	})
+
+}
+
+func editDims(jd *Definition, cmds ...string) {
+	editCmds, err := MakeDimensionEditCommands(cmds)
+	So(err, ShouldBeNil)
+	err = jd.Edit(func(je Editor) {
+		je.EditDimensions(editCmds)
+	})
+	So(err, ShouldBeNil)
+}
+
+func TestEditDimensions(t *testing.T) {
+	t.Parallel()
+
+	runCases(t, "EditDimensions", []testCase{
+		{
+			name: "nil (empty)",
+			fn: func(jd *Definition) {
+				editDims(jd) // no edit commands
+				So(mustGetDimensions(jd), ShouldResemble, ExpiringDimensions{})
+			},
+		},
+
+		{
+			name:        "nil (existing)",
+			skipSWEmpty: true,
+			fn: func(jd *Definition) {
+				base := baselineDims(jd)
+				So(mustGetDimensions(jd), ShouldResemble, base)
+
+				editDims(jd) // no edit commands
+
+				So(mustGetDimensions(jd), ShouldResemble, base)
+			},
+		},
+
+		{
+			name:        "add",
+			skipSWEmpty: true,
+			fn: func(jd *Definition) {
+				editDims(jd,
+					fmt.Sprintf("key+=value@%d", swSlice1ExpSecs),
+					fmt.Sprintf("key+=other_value@%d", swSlice3ExpSecs),
+					"other-=bogus",
+					"reset=everything",
+					"reset=else",
+				)
+
+				So(mustGetDimensions(jd), ShouldResemble, ExpiringDimensions{
+					"key": []ExpiringValue{
+						{Value: "value", Expiration: swSlice1Exp},
+						{Value: "other_value", Expiration: swSlice3Exp},
+					},
+					"reset": []ExpiringValue{
+						{Value: "else", Expiration: swSlice3Exp},
+						{Value: "everything", Expiration: swSlice3Exp},
+					},
+				})
+
+				if sw := jd.GetSwarming(); sw != nil {
+					// ensure dimensions show up in ALL slices which they ought to.
+					So(sw.Task.TaskSlices[0].Properties.Dimensions, ShouldResemble, []*api.StringListPair{
+						{
+							Key:    "key",
+							Values: []string{"other_value", "value"},
+						},
+						{
+							Key:    "reset",
+							Values: []string{"else", "everything"},
+						},
+					})
+					So(sw.Task.TaskSlices[1].Properties.Dimensions, ShouldResemble, []*api.StringListPair{
+						{
+							Key:    "key",
+							Values: []string{"other_value"},
+						},
+						{
+							Key:    "reset",
+							Values: []string{"else", "everything"},
+						},
+					})
+					So(sw.Task.TaskSlices[2].Properties.Dimensions, ShouldResemble, []*api.StringListPair{
+						{
+							Key:    "key",
+							Values: []string{"other_value"},
+						},
+						{
+							Key:    "reset",
+							Values: []string{"else", "everything"},
+						},
+					})
+				} else {
+					rdims := jd.GetBuildbucket().BbagentArgs.Build.Infra.Swarming.TaskDimensions
+					So(rdims, ShouldResemble, []*bbpb.RequestedDimension{
+						{Key: "key", Value: "other_value", Expiration: ptypes.DurationProto(swSlice3Exp)},
+						{Key: "key", Value: "value", Expiration: ptypes.DurationProto(swSlice1Exp)},
+						{Key: "reset", Value: "else"},
+						{Key: "reset", Value: "everything"},
+					})
+				}
+			},
+		},
+
+		{
+			name:        "remove",
+			skipSWEmpty: true,
+			fn: func(jd *Definition) {
+				editDims(jd,
+					fmt.Sprintf("key+=value@%d", swSlice1ExpSecs),
+					fmt.Sprintf("key+=other_value@%d", swSlice3ExpSecs),
+					"reset=everything",
+					"reset=else",
+				)
+
+				editDims(jd, "key-=other_value")
+
+				So(mustGetDimensions(jd), ShouldResemble, ExpiringDimensions{
+					"key": []ExpiringValue{
+						{Value: "value", Expiration: swSlice1Exp},
+					},
+					"reset": []ExpiringValue{
+						{Value: "else", Expiration: swSlice3Exp},
+						{Value: "everything", Expiration: swSlice3Exp},
+					},
+				})
+
+			},
+		},
+	})
+}
