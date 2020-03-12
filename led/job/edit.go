91d019ddada93730f5c12afe39a0b395b24cdf16
Robert Iannucci <iannucci@chromium.org>
2020-03-12 18:31:42 +0000

    [led] Add Editor and HighLevelEditor interfaces.
    These allow the rest of led to manipulate the job Definition without
    having to do type switching or protobuf exploration on every line.
    
    R=smut@chromium.org, tandrii@chromium.org
    
    Bug: 1017818
    Change-Id: I0b525c9e77b802fa4e608f937da2cb624d149633
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2090119
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: smut <smut@google.com>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>

---
+++ led/job/edit.go
@@ -0,0 +1,151 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package job
+
+import (
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/errors"
+)
+
+// Editor represents low-level mutations you can make on a job.Definition.
+//
+// You may edit a job.Definition by calling its Edit method.
+type Editor interface {
+	// ClearCurrentIsolated removes all isolateds from this Definition.
+	ClearCurrentIsolated()
+
+	// ClearDimensions removes all dimensions.
+	ClearDimensions()
+
+	// SetDimensions sets the full set of dimensions.
+	// IMPLEMENT: SetDimensions(dims ExpiringDimensions)
+
+	// EditDimensions edits the swarming dimensions.
+	// IMPLEMENT: EditDimensions(dimEdits DimensionEditCommands)
+
+	// CIPDPkgs allows you to edit the cipd packages. The mapping is in the form
+	// of:
+	//
+	//    "subdir:name/of/package" -> "version"
+	//
+	// If version is empty, this package will be removed (if it's present).
+	// IMPLEMENT: CIPDPkgs(cipdPkgs CIPDPkgs)
+
+	// Env edits the swarming environment variables (i.e. those set before the
+	// user payload runs).
+	//
+	// The map given is a map of environment variable to its value; If the value
+	// is "", then the environment variable is removed.
+	Env(env map[string]string)
+
+	// PrefixPathEnv controls swarming's env_prefix mapping for $PATH.
+	//
+	// Values prepended with '!' will remove them from the existing list of values
+	// (if present). Otherwise these values will be appended to the current list
+	// of PATH-prefix-envs.
+	PrefixPathEnv(values []string)
+
+	// Priority edits the swarming task priority.
+	Priority(priority int32)
+
+	// SwarmingHostname allows you to modify the current SwarmingHostname used by
+	// this led pipeline.
+	SwarmingHostname(host string)
+
+	// Tags appends the given values to the task's tags.
+	Tags(values []string)
+}
+
+// HighLevelEditor represents high-level mutations you can make on
+// a job.Definition.
+//
+// Currently only Buildbucket job.Definitions support high level edits.
+//
+// You may edit a job.Definition by calling its HighLevelEdit method.
+type HighLevelEditor interface {
+	Editor
+
+	// Experimental sets the task to either be marked 'experimental' or not.
+	Experimental(isExperimental bool)
+
+	// Properties edits the recipe properties.
+	//
+	// `props` should be a mapping of the top-level property to JSON-encoded data
+	// for the value of this property. If the value is "", then the top-level
+	// property will be deleted.
+	//
+	// If `auto` is true, then values which do not decode as JSON will be treated
+	// as literal strings. For example, given a value `literal string` with
+	// auto=true, this would be equivalent to passing the value `"literal string"`
+	// with auto=false.
+	Properties(props map[string]string, auto bool)
+
+	// Sets the task executable payload.
+	//
+	// If cipdPkg and cipdVers are provided, they indicate that the task payload
+	// is obtained from this location; it will be installed to `dirInTask`.
+	//
+	// If cipdPkg and cipdVers are omitted, they indicate that the task payload is
+	// obtained from the UserPayload isolate source. The job will assume that the
+	// payload executable is `${dirInTask}/luciexe`.
+	TaskPayload(cipdPkg, cipdVers string, dirInTask string)
+
+	// AddGerritChange ensures the GerritChange is in the set of input CLs.
+	AddGerritChange(cl *bbpb.GerritChange)
+
+	// RemoveGerritChange ensures the GerritChange is not in the set of input CLs.
+	RemoveGerritChange(cl *bbpb.GerritChange)
+
+	// GitilesCommit sets the GitilesCommit.
+	//
+	// If `commit` is nil, removes the commit.
+	GitilesCommit(commit *bbpb.GitilesCommit)
+}
+
+// Edit runs a mutator function with an Editor and returns the first mutation
+// error, if any.
+func (jd *Definition) Edit(cb func(m Editor)) error {
+	var m interface {
+		Editor
+		Close() error
+	}
+
+	if jd.GetBuildbucket() != nil {
+		panic("implement me")
+	} else {
+		panic("implement me")
+	}
+
+	cb(m)
+	return m.Close()
+}
+
+// HighLevelEdit runs a mutator function with an Editor and returns the first
+// mutation error, if any.
+func (jd *Definition) HighLevelEdit(cb func(m HighLevelEditor)) error {
+	var m interface {
+		HighLevelEditor
+		Close() error
+	}
+
+	if jd.GetBuildbucket() != nil {
+		panic("implement me")
+	} else {
+		return errors.New("job.HighLevelEdit not supported for this Job")
+	}
+
+	cb(m)
+	return m.Close()
+}
