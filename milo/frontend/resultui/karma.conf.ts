c01f59ada0a45b12a4f2c4edfd9e26ae1a42dc58
Weiwei Lin <weiweilin@google.com>
2020-03-12 16:11:06 +1100

    [resultui] show invocation details on invocation page
    Bug:1044351
    Change-Id: I033b3315187a6b1f3d29e177ef8f74f51ef9f9b5

--- milo/frontend/resultui/karma.conf.ts
+++ milo/frontend/resultui/karma.conf.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {Config} from 'karma';
+import { Config } from 'karma';
 import webpackConfig from './webpack.config';
 
 module.exports = (config: Config) => {
---
+++ milo/frontend/resultui/src/components/status_bar.ts
@@ -0,0 +1,76 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { MobxLitElement } from '@adobe/lit-mobx';
+import { css, customElement, html } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
+import { styleMap } from 'lit-html/directives/style-map';
+import { observable } from 'mobx';
+
+export interface Component {
+  color: string;
+  weight: number;
+}
+
+@customElement('tr-status-bar')
+export class StatusBarElement extends MobxLitElement {
+  @observable.ref
+  components: Component[] = [];
+
+  @observable.ref
+  loading = false;
+
+  private renderComponent(component: Component) {
+    return html`
+      <span
+        class=${classMap({'loading': this.loading})}
+        style=${styleMap({
+          'flex-grow': component.weight.toString(),
+          'background-color': component.color,
+        })}
+      >
+      </span>
+    `;
+  }
+
+  protected render() {
+    return html`
+      <div id="container">
+        ${this.components.map((c) => this.renderComponent(c))}
+      </div>
+    `;
+  }
+
+  static styles = css`
+    #container {
+      display: flex;
+      height: 5px;
+    }
+
+    .loading {
+      background-image: linear-gradient(45deg,rgba(255,255,255,.15) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.15) 50%,rgba(255,255,255,.15) 75%,transparent 75%,transparent);
+      background-size: 1rem 1rem;
+      animation: progress-bar-stripes 1s linear infinite;
+    }
+
+    @keyframes progress-bar-stripes {
+      0% {
+        background-position: 1rem 0;
+      }
+      100% {
+        background-position: 0 0;
+      }
+    }
+  `;
+}
