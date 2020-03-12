c4afa21ddc7dc6f923e5ce1f23437699ebde0b99
Roberto Carrillo <robertocn@google.com>
2020-03-12 01:35:19 -0700

    Add update-inclusions command to rdb cli tool.
    R=chanli,nodir
    
    Bug: 1059492
    Change-Id: I40007d3a9e0e843dca5a09de383c723993ed3dbd

--- resultdb/cli/app.go
+++ resultdb/cli/app.go
@@ -51,6 +51,7 @@ func application(p Params) *cli.Application {
 			cmdContext(),
 			cmdLs(p),
 			cmdDerive(p),
+			cmdUpdateInclusions(p),
 			// TODO(crbug.com/1021849): add subcommand upload
 			// TODO(crbug.com/1021849): add subcommand run
 
