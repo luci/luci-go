005739af97314617d274a0e067ab417859b10b22
Roberto Carrillo <robertocn@google.com>
2020-03-12 00:36:45 -0700

    Implement ctx subcommand for rdb cli tool.
    This puts resultdb-specifc parameters from the command line into
    LUCI_CONTEXT, and then executes the given command in an environment
    where this has been set.
    
    R=nodir,chanli
    
    Bug: 1060468
    Change-Id: I4e215025b94d466e9bf916298d07ae25b1dd9e8d

--- resultdb/cli/app.go
+++ resultdb/cli/app.go
@@ -48,6 +48,7 @@ func application(p Params) *cli.Application {
 			return logCfg.Use(ctx)
 		},
 		Commands: []*subcommands.Command{
+			cmdContext(),
 			cmdLs(p),
 			cmdDerive(p),
 			// TODO(crbug.com/1021849): add subcommand upload
