3b74af583fa96fe1ed7e8caf2ef06278ff2be148
Roberto Carrillo <robertocn@google.com>
2020-03-17 11:35:53 -0700

    Implement BatchCreateInvocations
    R=nodir,chanli
    
    Bug: 1059344
    Change-Id: If6b642714ec110da4717c85dacfdfa817968e1f0

--- resultdb/internal/services/recorder/create_invocation.go
+++ resultdb/internal/services/recorder/create_invocation.go
@@ -101,89 +101,192 @@ func validateCreateInvocationRequest(req *pb.CreateInvocationRequest, now time.T
 
 // CreateInvocation implements pb.RecorderServer.
 func (s *recorderServer) CreateInvocation(ctx context.Context, in *pb.CreateInvocationRequest) (*pb.Invocation, error) {
-	now := clock.Now(ctx).UTC()
-
-	allowCustomID, err := auth.IsMember(ctx, customIdGroup)
+	invs, tokens, err := s.createInvocations(ctx, []*pb.CreateInvocationRequest{in}, in.RequestId)
 	if err != nil {
 		return nil, err
 	}
-	if err := validateCreateInvocationRequest(in, now, allowCustomID); err != nil {
-		return nil, appstatus.BadRequest(err)
+	if len(invs) != 1 || len(tokens) != 1 {
+		panic("createInvocations did not return either an error or a valid invocation/token pair")
 	}
+	md := metadata.MD{}
+	md.Set(UpdateTokenMetadataKey, tokens...)
+	prpc.SetHeader(ctx, md)
+	return invs[0], nil
+}
 
-	invID := span.InvocationID(in.InvocationId)
-
-	// Prepare the invocation we will return.
-	inv := &pb.Invocation{
-		Name:            invID.Name(),
-		State:           pb.Invocation_ACTIVE,
-		Deadline:        in.Invocation.GetDeadline(),
-		Tags:            in.Invocation.GetTags(),
-		BigqueryExports: in.Invocation.GetBigqueryExports(),
-	}
-
-	// Ensure the invocation has a deadline.
-	if inv.Deadline == nil {
-		inv.Deadline = pbutil.MustTimestampProto(now.Add(defaultInvocationDeadlineDuration))
-	}
-
-	pbutil.NormalizeInvocation(inv)
-
-	commitTimestamp, err := span.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
-		// Dedupe the request if possible.
-		if in.RequestId != "" {
-			var curRequestID spanner.NullString
-			err := span.ReadInvocation(ctx, txn, invID, map[string]interface{}{
-				"CreateRequestId": &curRequestID,
-			})
-			st, _ := appstatus.Get(err)
-			switch {
-			case st.Code() == codes.NotFound:
-				// Continue to creation.
-
-			case err != nil:
-				return err
-
-			case curRequestID.Valid && curRequestID.StringVal == in.RequestId:
-				// Dedupe the request.
-				inv, err = span.ReadInvocationFull(ctx, txn, invID)
-				return err
-
-			default:
-				return invocationAlreadyExists(invID)
-			}
-		}
-
-		return txn.BufferWrite([]*spanner.Mutation{
-			span.InsertMap("Invocations", s.rowOfInvocation(ctx, inv, in.RequestId)),
-		})
-	})
-	switch {
-	case spanner.ErrCode(err) == codes.AlreadyExists:
-		return nil, invocationAlreadyExists(invID)
-
-	case err != nil:
-		return nil, err
-	}
-
-	if inv.CreateTime == nil {
-		// The request was not deduped.
-		inv.CreateTime = pbutil.MustTimestampProto(commitTimestamp)
-		span.IncRowCount(ctx, 1, span.Invocations, span.Inserted)
-	}
-
-	// Return an update token to the client, only if the creation was successful
-	// or it is a valid deduplication.
-	// Generate a token with the invocation id as the state, no embedded data.
-	updateToken, err := generateInvocationToken(ctx, invID)
+// BatchCreateInvocations implements pb.RecorderServer.
+func (s *recorderServer) BatchCreateInvocations(ctx context.Context, in *pb.BatchCreateInvocationsRequest) (*pb.BatchCreateInvocationsResponse, error) {
+	invs, tokens, err := s.createInvocations(ctx, in.Requests, in.RequestId)
 	if err != nil {
 		return nil, err
 	}
-	prpc.SetHeader(ctx, metadata.Pairs(UpdateTokenMetadataKey, updateToken))
-
-	return inv, nil
+	md := metadata.MD{}
+	md.Set(UpdateTokenMetadataKey, tokens...)
+	prpc.SetHeader(ctx, md)
+	return &pb.BatchCreateInvocationsResponse{Invocations: invs}, nil
 }
 
 func invocationAlreadyExists(id span.InvocationID) error {
 	return appstatus.Errorf(codes.AlreadyExists, "%s already exsts", id.Name())
+}
+
+func (s *recorderServer) createInvocations(ctx context.Context, reqs []*pb.CreateInvocationRequest, requestID string) ([]*pb.Invocation, []string, error) {
+	now := clock.Now(ctx).UTC()
+
+	allowCustomID, err := auth.IsMember(ctx, customIdGroup)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	idSet, err := s.validateMultipleCreateInvocationRequests(now, reqs, requestID, allowCustomID)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	muts := s.createInvocationsRequestsToMutations(ctx, now, reqs, requestID)
+
+	deduped := false
+	_, err = span.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		deduped, err := s.deduplicateCreateInvocations(ctx, idSet, requestID, txn)
+		if err != nil {
+			return err
+		}
+		if !deduped {
+			return txn.BufferWrite(muts)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	if !deduped {
+		span.IncRowCount(ctx, len(reqs), span.Invocations, span.Inserted)
+	}
+
+	return s.getCreatedInvocationsAndUpdateTokens(ctx, idSet, reqs)
+}
+
+// validateMultipleCreateInvocationRequests checks that the individual requests
+// are valid, that they match the batch request requestID and that their names
+// are not repeated.
+func (s *recorderServer) validateMultipleCreateInvocationRequests(
+	now time.Time, reqs []*pb.CreateInvocationRequest, requestID string, allowCustomID bool) (span.InvocationIDSet, error) {
+	idSet := make(span.InvocationIDSet, len(reqs))
+	for _, req := range reqs {
+		if err := validateCreateInvocationRequest(req, now, allowCustomID); err != nil {
+			return nil, appstatus.BadRequest(err)
+		}
+
+		// If there's multiple `CreateInvocationRequest`s their request id
+		// must either be empty or match the one in the batch request.
+		if req.RequestId != "" && req.RequestId != requestID {
+			return nil, appstatus.BadRequest(errors.Reason("request_id %q does not match %s", req.RequestId, requestID).Err())
+		}
+
+		invID := span.InvocationID(req.InvocationId)
+		if idSet.Has(invID) {
+			return nil, appstatus.BadRequest(
+				errors.Reason("duplicate invocation id %s in request", req.InvocationId).Err())
+		}
+		idSet.Add(invID)
+	}
+	return idSet, nil
+}
+
+// createInvocationsRequestsToMutations computes a database mutation for
+// inserting a row for each invocation creation requested.
+func (s *recorderServer) createInvocationsRequestsToMutations(ctx context.Context, now time.Time, reqs []*pb.CreateInvocationRequest, requestID string) []*spanner.Mutation {
+
+	muts := make([]*spanner.Mutation, len(reqs))
+	// Compute mutations
+	for i, req := range reqs {
+
+		// Prepare the invocation we will save to spanner.
+		inv := &pb.Invocation{
+			Name:            span.InvocationID(req.InvocationId).Name(),
+			State:           pb.Invocation_ACTIVE,
+			Deadline:        req.Invocation.GetDeadline(),
+			Tags:            req.Invocation.GetTags(),
+			BigqueryExports: req.Invocation.GetBigqueryExports(),
+		}
+
+		// Ensure the invocation has a deadline.
+		if inv.Deadline == nil {
+			inv.Deadline = pbutil.MustTimestampProto(now.Add(defaultInvocationDeadlineDuration))
+		}
+
+		pbutil.NormalizeInvocation(inv)
+		// Create a mutation to create the invocation.
+		muts[i] = span.InsertMap("Invocations", s.rowOfInvocation(ctx, inv, requestID))
+	}
+	return muts
+}
+
+// getCreatedInvocationsAndUpdateTokens reads the full details of the
+// invocations just created in a separate read-only transaction, and
+// generates an update token for each.
+func (s *recorderServer) getCreatedInvocationsAndUpdateTokens(ctx context.Context, idSet span.InvocationIDSet, reqs []*pb.CreateInvocationRequest) ([]*pb.Invocation, []string, error) {
+	txn := span.Client(ctx).ReadOnlyTransaction()
+	defer txn.Close()
+	invMap, err := span.ReadInvocationsFull(ctx, txn, idSet)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Arrange them in same order as the incoming requests.
+	// Ordering is important to match the tokens.
+	invs := make([]*pb.Invocation, len(reqs))
+	for i, req := range reqs {
+		invs[i] = invMap[span.InvocationID(req.InvocationId)]
+	}
+
+	tokens, err := generateTokens(ctx, invs)
+	if err != nil {
+		return nil, nil, err
+	}
+	return invs, tokens, nil
+}
+
+// deduplicateCreateInvocations checks if the invocations have already been
+// created with the given requestID. Returns a true if they have.
+func (s *recorderServer) deduplicateCreateInvocations(ctx context.Context, idSet span.InvocationIDSet, requestID string, txn span.Txn) (bool, error) {
+	invCount := 0
+	err := txn.Read(ctx, "Invocations", idSet.Keys(), []string{"InvocationId", "CreateRequestId"}).Do(func(r *spanner.Row) error {
+		var rowRequestID spanner.NullString
+		var invID span.InvocationID
+		if err := span.FromSpanner(r, &invID, &rowRequestID); err != nil {
+			return errors.Annotate(err, "failed to parse InvocationId and CreateRequestId from %s", r).Err()
+		}
+		if rowRequestID.IsNull() || rowRequestID.String() != requestID {
+			return invocationAlreadyExists(invID)
+		}
+		invCount += 1
+		return nil
+	})
+	if err != nil {
+		return false, err
+	}
+	if invCount == len(idSet) {
+		// All invocations were previously created with this request id.
+		return true, nil
+	} else if invCount == 0 {
+		// None of the invocations exist already.
+		return false, nil
+	}
+	// Could happen if someone sent two different but overlapping batch create
+	// requests, but reused the request_id.
+	return false, appstatus.Errorf(codes.AlreadyExists, "some of the invocations already created with this request id")
+}
+
+// generateTokens generates an update token for each invocation.
+func generateTokens(ctx context.Context, invs []*pb.Invocation) ([]string, error) {
+	ret := make([]string, len(invs))
+	for i, inv := range invs {
+		updateToken, err := generateInvocationToken(ctx, span.MustParseInvocationName(inv.Name))
+		if err != nil {
+			return nil, err
+		}
+		ret[i] = updateToken
+	}
+	return ret, nil
 }
