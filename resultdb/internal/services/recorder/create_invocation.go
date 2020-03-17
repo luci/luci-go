4c8d97197c2e40f1ccc21b3c67d369b120a24a4c
Roberto Carrillo <robertocn@google.com>
2020-03-16 17:04:32 -0700

    Implement BatchCreateInvocations
    R=nodir,chanli
    
    Bug: 1059344
    Change-Id: If6b642714ec110da4717c85dacfdfa817968e1f0

--- resultdb/internal/services/recorder/create_invocation.go
+++ resultdb/internal/services/recorder/create_invocation.go
@@ -101,89 +101,162 @@ func validateCreateInvocationRequest(req *pb.CreateInvocationRequest, now time.T
 
 // CreateInvocation implements pb.RecorderServer.
 func (s *recorderServer) CreateInvocation(ctx context.Context, in *pb.CreateInvocationRequest) (*pb.Invocation, error) {
-	now := clock.Now(ctx).UTC()
-
-	allowCustomID, err := auth.IsMember(ctx, customIdGroup)
+	invs, tokens, err := s.createInvocations(ctx, []*pb.CreateInvocationRequest{in}, in.RequestId)
 	if err != nil {
 		return nil, err
 	}
-	if err := validateCreateInvocationRequest(in, now, allowCustomID); err != nil {
-		return nil, appstatus.BadRequest(err)
+	if len(invs) != 1 || len(tokens) != 1 {
+		panic("createInvocations did not return either an error or a valid invocation/token pair")
 	}
+	md := metadata.MD{}
+	md.Set(UpdateTokenMetadataKey, tokens...)
+	prpc.SetHeader(ctx, md)
+	return invs[0], nil
+}
 
-	invID := span.InvocationID(in.InvocationId)
-
-	// Prepare the invocation we will return.
-	inv := &pb.Invocation{
-		Name:            invID.Name(),
-		State:           pb.Invocation_ACTIVE,
-		Deadline:        in.Invocation.GetDeadline(),
-		Tags:            in.Invocation.GetTags(),
-		BigqueryExports: in.Invocation.GetBigqueryExports(),
-	}
-
-	// Ensure the invocation has a deadline.
-	if inv.Deadline == nil {
-		inv.Deadline = pbutil.MustTimestampProto(now.Add(defaultInvocationDeadlineDuration))
-	}
-
-	pbutil.NormalizeInvocation(inv)
-
-	commitTimestamp, err := span.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
-		// Dedupe the request if possible.
-		if in.RequestId != "" {
-			var curRequestID spanner.NullString
-			err := span.ReadInvocation(ctx, txn, invID, map[string]interface{}{
-				"CreateRequestId": &curRequestID,
-			})
-			st, _ := appstatus.Get(err)
-			switch {
-			case st.Code() == codes.NotFound:
-				// Continue to creation.
-
-			case err != nil:
-				return err
-
-			case curRequestID.Valid && curRequestID.StringVal == in.RequestId:
-				// Dedupe the request.
-				inv, err = span.ReadInvocationFull(ctx, txn, invID)
-				return err
-
-			default:
-				return invocationAlreadyExists(invID)
-			}
-		}
-
-		return txn.BufferWrite([]*spanner.Mutation{
-			span.InsertMap("Invocations", s.rowOfInvocation(ctx, inv, in.RequestId)),
-		})
-	})
-	switch {
-	case spanner.ErrCode(err) == codes.AlreadyExists:
-		return nil, invocationAlreadyExists(invID)
-
-	case err != nil:
-		return nil, err
-	}
-
-	if inv.CreateTime == nil {
-		// The request was not deduped.
-		inv.CreateTime = pbutil.MustTimestampProto(commitTimestamp)
-		span.IncRowCount(ctx, 1, span.Invocations, span.Inserted)
-	}
-
-	// Return an update token to the client, only if the creation was successful
-	// or it is a valid deduplication.
-	// Generate a token with the invocation id as the state, no embedded data.
-	updateToken, err := generateInvocationToken(ctx, invID)
+// BatchCreateInvocations implements pb.RecorderServer.
+func (s *recorderServer) BatchCreateInvocations(ctx context.Context, in *pb.BatchCreateInvocationsRequest) (*pb.BatchCreateInvocationsResponse, error) {
+	invs, tokens, err := s.createInvocations(ctx, in.Requests, in.RequestId)
 	if err != nil {
 		return nil, err
 	}
-	prpc.SetHeader(ctx, metadata.Pairs(UpdateTokenMetadataKey, updateToken))
-
-	return inv, nil
+	md := metadata.MD{}
+	md.Set(UpdateTokenMetadataKey, tokens...)
+	prpc.SetHeader(ctx, md)
+	return &pb.BatchCreateInvocationsResponse{Invocations: invs}, nil
 }
 
 func invocationAlreadyExists(id span.InvocationID) error {
 	return appstatus.Errorf(codes.AlreadyExists, "%s already exsts", id.Name())
+}
+
+func (s *recorderServer) createInvocations(ctx context.Context, reqs []*pb.CreateInvocationRequest, requestID string) ([]*pb.Invocation, []string, error) {
+	now := clock.Now(ctx).UTC()
+
+	allowCustomID, err := auth.IsMember(ctx, customIdGroup)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	muts := make([]*spanner.Mutation, 0, len(reqs))
+	idSet := make(span.InvocationIDSet, len(reqs))
+	// Validate Requests
+	for _, req := range reqs {
+		if err := validateCreateInvocationRequest(req, now, allowCustomID); err != nil {
+			return nil, nil, appstatus.BadRequest(err)
+		}
+		// If there's multiple `CreateInvocationRequest`s their request id
+		// must either be empty or match the one in the batch request.
+		if req.RequestId != "" && req.RequestId != requestID {
+			return nil, nil, appstatus.BadRequest(
+				errors.Reason("request_id %s does not match %s", req.RequestId, requestID).Err())
+		}
+
+		invID := span.InvocationID(req.InvocationId)
+		if idSet.Has(invID) {
+			return nil, nil, appstatus.BadRequest(
+				errors.Reason("duplicate invocation id %s in request", req.InvocationId).Err())
+		}
+		idSet.Add(invID)
+
+		// Prepare the invocation we will save to spanner.
+		inv := &pb.Invocation{
+			Name:            invID.Name(),
+			State:           pb.Invocation_ACTIVE,
+			Deadline:        req.Invocation.GetDeadline(),
+			Tags:            req.Invocation.GetTags(),
+			BigqueryExports: req.Invocation.GetBigqueryExports(),
+		}
+
+		// Ensure the invocation has a deadline.
+		if inv.Deadline == nil {
+			inv.Deadline = pbutil.MustTimestampProto(now.Add(defaultInvocationDeadlineDuration))
+		}
+
+		pbutil.NormalizeInvocation(inv)
+		// Create a mutation to create the invocation.
+		muts = append(muts, span.InsertMap("Invocations", s.rowOfInvocation(ctx, inv, requestID)))
+	}
+
+	deduped := false
+	_, err = span.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		deduped, err := s.deduplicateCreateInvocations(ctx, idSet, requestID, txn)
+		if err != nil {
+			return err
+		}
+		if !deduped {
+			return txn.BufferWrite(muts)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	if !deduped {
+		span.IncRowCount(ctx, len(reqs), span.Invocations, span.Inserted)
+	}
+
+	// Read the full details of the invocations just created or deduplicated
+	// in a separate read-only transaction.
+	txn := span.Client(ctx).ReadOnlyTransaction()
+	defer txn.Close()
+	invMap, err := span.ReadInvocationsFull(ctx, txn, idSet)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Arrange them in same order as the incoming requests.
+	// Ordering is important to match the tokens.
+	invs := make([]*pb.Invocation, len(reqs))
+	for i, req := range reqs {
+		invs[i] = invMap[span.InvocationID(req.InvocationId)]
+	}
+
+	tokens, err := generateTokens(ctx, invs)
+	if err != nil {
+		return nil, nil, err
+	}
+	return invs, tokens, nil
+}
+
+// deduplicateCreateInvocations checks if the invocations have already been
+// created with the given requestID. Returns a true if they have.
+func (s *recorderServer) deduplicateCreateInvocations(ctx context.Context, idSet span.InvocationIDSet, requestID string, txn span.Txn) (bool, error) {
+	invCount := 0
+	err := txn.Read(ctx, "Invocations", idSet.Keys(), []string{"InvocationId", "CreateRequestId"}).Do(func(r *spanner.Row) error {
+		var invID span.InvocationID
+		var rowRequestID spanner.NullString
+		if err := span.FromSpanner(r, &invID, &rowRequestID); err != nil {
+			return errors.Annotate(err, "failed to fetch %s", idSet).Err()
+		}
+		if rowRequestID.IsNull() || rowRequestID.String() != requestID {
+			return invocationAlreadyExists(invID)
+		}
+		invCount += 1
+		return nil
+	})
+	if err != nil {
+		return false, err
+	}
+	if invCount == len(idSet) {
+		return true, nil
+	} else if invCount != 0 {
+		// Some invocations already exist and have the right request id but some are
+		// missing, but not all.
+		panic("while deduplicating a request to create invocations at least one was unexpectedly missing")
+	}
+	return false, nil
+}
+
+// generateTokens generates an update token for each invocation.
+func generateTokens(ctx context.Context, invs []*pb.Invocation) ([]string, error) {
+	ret := make([]string, 0, len(invs))
+	for _, inv := range invs {
+		updateToken, err := generateInvocationToken(ctx, span.MustParseInvocationName(inv.Name))
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, updateToken)
+	}
+	return ret, nil
 }
