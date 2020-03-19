eb44c96b192ec098cbac949fd4a5d50d45d41cc0
Yiwei Zhang <yiwzhang@google.com>
2020-03-19 20:34:56 +0000

    [mask] Implement path parsing functionality
    This cl depends on: https://crrev.com/c/2107577
    
    Equivalent python implementation:
      https://chromium.googlesource.com/infra/luci/luci-py/+/master/appengine/components/components/protoutil/field_masks.py
    
    R=nodir
    
    Bug: 964590
    Change-Id: Iaa24dbf07a13d26d34c00f1899a3cbac8a15cede
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2107187
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

---
+++ common/proto/mask/parse.go
@@ -0,0 +1,287 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mask
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+// path models the parsed path which consists of a slice of segments
+type path []string
+
+const pathDelimiter = '.'
+
+// parsePath parses a path string to a slice of segments (See grammar in pkg
+// doc). It also removes the trailing stars, e.g. parses ["a.*"] as ["a"].
+//
+// If isJSONName is true, parsing the field name using JSON field name instead
+// of its canonical form. However, the result segments in path will use
+// canonical field name.
+func parsePath(rawPath string, descriptor protoreflect.MessageDescriptor, isJSONName bool) (path, error) {
+	t := &tokenizer{
+		path:      rawPath,
+		delimiter: pathDelimiter,
+	}
+	ctx := &parseCtx{
+		curDescriptor: descriptor,
+	}
+	ret := path{}
+	for t.hasMoreTokens() {
+		if tok, err := t.nextToken(); err != nil {
+			return nil, err
+		} else {
+			seg, err := parseSegment(tok, isJSONName, ctx)
+			if err != nil {
+				return nil, err
+			}
+			ret = append(ret, seg)
+		}
+	}
+
+	if n := len(ret); n > 0 && ret[n-1] == "*" {
+		return ret[:n-1], nil
+	}
+	return ret, nil
+}
+
+// parseCtx defines context during path parsing
+type parseCtx struct {
+	curDescriptor protoreflect.MessageDescriptor
+	isList        bool
+	mustBeLast    bool
+}
+
+// advanceToField advances the context to the next field of current message.
+// Returns the canonical form of field name. Returns error when the supplied
+// field doesn't exist in message or the current message descriptor is nil
+//(meaning scalar field).
+//
+// If isJSONName is true, we will assume the given field name is JSON name and
+// look up the JSON name instead of the field name.
+func (ctx *parseCtx) advanceToField(fieldName string, isJSONName bool) (string, error) {
+	msgDesc := ctx.curDescriptor
+	if msgDesc == nil {
+		return "", fmt.Errorf("can't advance to field when current descriptor is nil")
+	}
+	var fieldDesc protoreflect.FieldDescriptor
+	if isJSONName {
+		fieldDesc = msgDesc.Fields().ByJSONName(fieldName)
+	} else {
+		fieldDesc = msgDesc.Fields().ByName(protoreflect.Name(fieldName))
+	}
+	if fieldDesc == nil {
+		return "", fmt.Errorf("field %q does not exist in message %s", fieldName, msgDesc.Name())
+	}
+	ctx.curDescriptor = fieldDesc.Message()
+	ctx.isList = fieldDesc.IsList()
+	return string(fieldDesc.Name()), nil
+}
+
+// mapKeyKindToTokenType defines the mapping between the kind of mapkey
+// and the expected token type of the token in path string.
+var mapKeyKindToTokenType = map[protoreflect.Kind]tokenType{
+	protoreflect.Int32Kind:    intLiteral,
+	protoreflect.Int64Kind:    intLiteral,
+	protoreflect.Sint32Kind:   intLiteral,
+	protoreflect.Sint64Kind:   intLiteral,
+	protoreflect.Uint32Kind:   intLiteral,
+	protoreflect.Uint64Kind:   intLiteral,
+	protoreflect.Sfixed32Kind: intLiteral,
+	protoreflect.Fixed32Kind:  intLiteral,
+	protoreflect.Sfixed64Kind: intLiteral,
+	protoreflect.Fixed64Kind:  intLiteral,
+	protoreflect.BoolKind:     boolLiteral,
+	protoreflect.StringKind:   strLiteral,
+}
+
+// parseSegment parses a token to a segment string and updates the prase context
+// accordingly.
+//
+// If isJSONName is true, the token value is expected to be JSON name of
+// a field of a message instead of canonical name. However, the return segment
+// will always be canonical name.
+func parseSegment(tok token, isJSONName bool, ctx *parseCtx) (string, error) {
+	switch desc := ctx.curDescriptor; {
+	case ctx.mustBeLast:
+		return "", fmt.Errorf("expected end of string; got token: %q", tok.value)
+	case ctx.isList:
+		// The current segment corresponds to a list field (non-map entry repeated
+		// field). Only star is allowed
+		if tok.typ != star {
+			return "", fmt.Errorf("expected a star following a repeated field; got token: %q", tok.value)
+		}
+		ctx.isList = false
+		return "*", nil
+
+	case desc == nil:
+		return "", fmt.Errorf("scalar field cannot have subfield: %q", tok.value)
+
+	case desc.IsMapEntry():
+		if tok.typ != star {
+			keyKind := desc.Fields().ByName(protoreflect.Name("key")).Kind()
+			if expectTokenType, found := mapKeyKindToTokenType[keyKind]; !found {
+				return "", fmt.Errorf("unexpected map key kind %s", keyKind)
+			} else if expectTokenType != tok.typ {
+				return "", fmt.Errorf("expected map key kind %s; got token: %q", keyKind, tok.value)
+			}
+		}
+
+		if _, err := ctx.advanceToField("value", false); err != nil {
+			return "", err
+		}
+		return tok.value, nil
+
+	case tok.typ == star:
+		// a star cannot be followed by any subfields if it does not corresponds to
+		// a repeated field
+		ctx.mustBeLast = true
+		return "*", nil
+
+	case tok.typ != strLiteral:
+		return "", fmt.Errorf("expected a field name of type string; got token: %q", tok.value)
+
+	default:
+		return ctx.advanceToField(tok.value, isJSONName)
+	}
+}
+
+// tokenizer breaks a path string into tokens(segments)
+type tokenizer struct {
+	path      string
+	delimiter byte
+	pos       int
+}
+
+// token is a composite of token type and the raw string value. It represents
+// a segment in the path
+type token struct {
+	typ   tokenType
+	value string
+}
+
+// tokenType models different types of segment defined in grammar (see pkg doc).
+// Note that, quoted string will also be treated as string literal.
+type tokenType int8
+
+const (
+	star tokenType = iota
+	strLiteral
+	boolLiteral
+	intLiteral
+)
+
+// hasMoreTokens tests if there are more tokens available from the path string
+func (t tokenizer) hasMoreTokens() bool {
+	return t.pos < len(t.path)
+}
+
+// nextToken returns the next token in the path string. Always call
+// hasMoreTokens before calling this function. Otherwise, This function call
+// will panic with index out of range when there is no more token available.
+func (t *tokenizer) nextToken() (token, error) {
+	if t.pos > 0 {
+		// if not reading the first token, expecting a delimiter
+		if t.path[t.pos] != t.delimiter {
+			return token{}, fmt.Errorf("expected delimiter: %c; got %c", t.delimiter, t.path[t.pos])
+		}
+		t.pos++ // swallow the delimiter
+		if t.pos == len(t.path) {
+			return token{}, fmt.Errorf("path can't end with delimiter: %c", t.delimiter)
+		}
+	}
+
+	switch b, pathLen := t.path[t.pos], len(t.path); {
+	case b == '`':
+		t.pos++ // swallow the starting backtick
+		sb := &strings.Builder{}
+		for {
+			nextBacktickRel := strings.IndexRune(t.path[t.pos:], '`')
+			if nextBacktickRel == -1 {
+				sb.WriteString(t.path[t.pos:])
+				return token{}, fmt.Errorf("a quoted string is never closed; got: %q", sb)
+			}
+			nextBacktickAbs := t.pos + nextBacktickRel
+			sb.WriteString(t.path[t.pos:nextBacktickAbs])
+			t.pos = nextBacktickAbs + 1 // Swallow the discovered backtick as well
+			if t.pos >= pathLen || t.path[t.pos] != '`' {
+				// Stop if eof or the discovered backtick is not for escaping
+				break
+			}
+			sb.WriteByte('`')
+			t.pos++ // Swallow the escaped backtick
+		}
+		return token{
+			typ:   strLiteral,
+			value: sb.String(),
+		}, nil
+
+	case b == '*':
+		t.pos++ // swallow the star
+		return token{
+			typ:   star,
+			value: "*",
+		}, nil
+
+	// Check if '-' is the last character or look ahead to see if it is followed
+	// by a digit
+	case b == '-' && (t.pos+1 == pathLen || !unicode.IsDigit(rune(t.path[t.pos+1]))):
+		return token{}, fmt.Errorf("expected digit following minus sign for negative numbers; got minus sign only")
+
+	case b == '-' || unicode.IsDigit(rune(b)):
+		start := t.pos
+		t.pos++ // swallow the first digit or minus sign felled through
+		n := strings.IndexFunc(t.path[t.pos:], func(r rune) bool { return !unicode.IsDigit(r) })
+		if n == -1 {
+			t.pos = pathLen
+		} else {
+			t.pos += n
+		}
+		return token{
+			typ:   intLiteral,
+			value: t.path[start:t.pos],
+		}, nil
+
+	case b == '_' || unicode.IsLetter(rune(b)):
+		start := t.pos
+		t.pos++ // swallow the underscore or first letter
+		n := strings.IndexFunc(t.path[t.pos:], isInvalidStringChar)
+		if n == -1 {
+			t.pos = pathLen
+		} else {
+			t.pos += n
+		}
+		typ, val := strLiteral, t.path[start:t.pos]
+		if val == "true" || val == "false" {
+			typ = boolLiteral
+		}
+		return token{
+			typ:   typ,
+			value: val,
+		}, nil
+
+	default:
+		return token{}, fmt.Errorf("unexpected token: %c", b)
+	}
+}
+
+// isInvalidStringChar tells whether the given rune represents an invalid
+// character in a string literal according to the grammar.
+func isInvalidStringChar(r rune) bool {
+	return !unicode.IsLetter(r) && !unicode.IsDigit(r) && r != '_'
+}
---
+++ common/proto/mask/parse_test.go
@@ -0,0 +1,161 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mask
+
+import (
+	"fmt"
+	"testing"
+
+	"go.chromium.org/luci/common/proto/internal/testingpb"
+
+	"google.golang.org/protobuf/runtime/protoimpl"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+type testMsg = testingpb.Full
+
+var testMsgDescriptor = protoimpl.X.MessageDescriptorOf(&testMsg{})
+
+func TestParsePath(t *testing.T) {
+	Convey("Expect path parsing", t, func() {
+		Convey("succeeds when parsing scalar field", func() {
+			tryParsePath("str").andExpectPath("str")
+			tryParsePath("num").andExpectPath("num")
+		})
+		Convey("fails when given subfield for a scalar field", func() {
+			tryParsePath("str.str").andExpectErrorLike("scalar field cannot have subfield: \"str\"")
+		})
+		Convey("fails for invalid delimiter", func() {
+			tryParsePath("str@").andExpectErrorLike("expected delimiter: .; got @")
+		})
+		Convey("succeeds when parsing repeated field", func() {
+			tryParsePath("strs").andExpectPath("strs")
+			tryParsePath("nums").andExpectPath("nums")
+			tryParsePath("msgs").andExpectPath("msgs")
+		})
+		Convey("succeeds when parsing repeated field and path ends with star", func() {
+			// trailing star will be removed
+			tryParsePath("strs.*").andExpectPath("strs")
+			tryParsePath("nums.*").andExpectPath("nums")
+			tryParsePath("msgs.*").andExpectPath("msgs")
+		})
+		Convey("fails when parsing repeated field and path ends with index", func() {
+			tryParsePath("strs.1").andExpectErrorLike("expected a star following a repeated field; got token: \"1\"")
+			tryParsePath("nums.2").andExpectErrorLike("expected a star following a repeated field; got token: \"2\"")
+			tryParsePath("msgs.a").andExpectErrorLike("expected a star following a repeated field; got token: \"a\"")
+		})
+		Convey("succeeds when parsing map field (key type integer, scalar value)", func() {
+			tryParsePath("map_num_str.1").andExpectPath("map_num_str", "1")
+			tryParsePath("map_num_str.-1").andExpectPath("map_num_str", "-1")
+			// trailing star will be removed
+			tryParsePath("map_num_str.*").andExpectPath("map_num_str")
+		})
+		Convey("succeeds when parsing map field (key type string, scalar value)", func() {
+			// unquoted
+			tryParsePath("map_str_num.abcd").andExpectPath("map_str_num", "abcd")
+			tryParsePath("map_str_num._ab_cd").andExpectPath("map_str_num", "_ab_cd")
+			// quoted
+			tryParsePath("map_str_num.`abcd`").andExpectPath("map_str_num", "abcd")
+			tryParsePath("map_str_num.`_ab.cd`").andExpectPath("map_str_num", "_ab.cd")
+			tryParsePath("map_str_num.`ab``cd`").andExpectPath("map_str_num", "ab`cd")
+			// trailing star will be removed
+			tryParsePath("map_str_num.*").andExpectPath("map_str_num")
+		})
+		Convey("succeeds when parsing map field (key type boolean, scalar value)", func() {
+			tryParsePath("map_bool_str.false").andExpectPath("map_bool_str", "false")
+			tryParsePath("map_bool_str.true").andExpectPath("map_bool_str", "true")
+			tryParsePath("map_bool_str.*").andExpectPath("map_bool_str")
+		})
+		Convey("fails when parsing map field with incompatible key type", func() {
+			tryParsePath("map_num_str.a").andExpectErrorLike("expected map key kind int32; got token: \"a\"")
+			tryParsePath("map_str_num.1").andExpectErrorLike("expected map key kind string; got token: \"1\"")
+			tryParsePath("map_bool_str.not_a_bool").andExpectErrorLike("expected map key kind bool; got token: \"not_a_bool\"")
+		})
+		Convey("succeeds when parsing map field (value type message)", func() {
+			tryParsePath("map_str_msg.some_key.str").andExpectPath("map_str_msg",
+				"some_key", "str")
+			tryParsePath("map_str_msg.*.str").andExpectPath("map_str_msg", "*", "str")
+		})
+		Convey("succeeds when parsing message field", func() {
+			tryParsePath("msg.str").andExpectPath("msg", "str")
+			tryParsePath("msg.*").andExpectPath("msg")
+			tryParsePath("msg.msg.msg.*").andExpectPath("msg", "msg", "msg")
+		})
+		Convey("fails when parsing message field and given a non-string field name", func() {
+			tryParsePath("msg.123").andExpectErrorLike("expected a field name of type string; got token: \"123\"")
+		})
+		Convey("fails when parsing message field and star is not the last token", func() {
+			tryParsePath("msg.*.str").andExpectErrorLike("expected end of string; got token: \"str\"")
+		})
+		Convey("fails when parsing message field with unknown subfield", func() {
+			tryParsePath("msg.unknown_field").andExpectErrorLike(fmt.Sprintf("field \"unknown_field\" does not exist in message %s", testMsgDescriptor.Name()))
+			tryParsePath("msg.msg.unknown_field").andExpectErrorLike(fmt.Sprintf("field \"unknown_field\" does not exist in message %s", testMsgDescriptor.Name()))
+		})
+		Convey("succeeds when parsing repeated message fields given subfield", func() {
+			tryParsePath("msgs.*.str").andExpectPath("msgs", "*", "str")
+		})
+		Convey("fails when ends delimiter", func() {
+			tryParsePath("msg.").andExpectErrorLike("path can't end with delimiter: .")
+		})
+		Convey("fails when quoted string is not closed", func() {
+			tryParsePath("`quoted``str").andExpectErrorLike("a quoted string is never closed; got: \"quoted`str\"")
+		})
+		Convey("fails when an integer literal has only minus sign", func() {
+			tryParsePath("map_num_str.-").andExpectErrorLike("expected digit following minus sign for negative numbers; got minus sign only")
+		})
+		Convey("fails when multiple delimiters", func() {
+			tryParsePath("msg..str").andExpectErrorLike("unexpected token: .")
+			tryParsePath("msg..").andExpectErrorLike("unexpected token: .")
+		})
+		Convey("succeeds for json name", func() {
+			p, err := parsePath("jsonName", testMsgDescriptor, true)
+			So(err, ShouldBeNil)
+			So(p, ShouldResemble, path{"json_name"})
+			p, err = parsePath("another_json_name", testMsgDescriptor, true)
+			So(err, ShouldBeNil)
+			So(p, ShouldResemble, path{"json_name_option"})
+
+		})
+	})
+}
+
+// parseResult is a helper struct to enable descriptive test for path parsing
+type parseResult struct {
+	p   path
+	err error
+}
+
+func tryParsePath(rawPath string) parseResult {
+	p, err := parsePath(rawPath, testMsgDescriptor, false)
+	return parseResult{
+		p:   p,
+		err: err,
+	}
+}
+
+func (res parseResult) andExpectErrorLike(errorSubstring string) {
+	So(res.err, ShouldErrLike, errorSubstring)
+}
+
+func (res parseResult) andExpectPath(segments ...string) {
+	So(res.err, ShouldBeNil)
+	expectedPath := make(path, len(segments))
+	for i, seg := range segments {
+		expectedPath[i] = seg
+	}
+	So(res.p, ShouldResemble, expectedPath)
+}
