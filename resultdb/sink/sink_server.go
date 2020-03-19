b2d6ab85ce77599f4607e9d66f6d9c288964af2f
Scott Lee <ddoman@chromium.org>
2020-03-19 21:59:32 +1100

    [resultdb] add dispatcher channel for recorder requests
    Bug: 1017299
    Change-Id: I08bad3283535ae6517eee974b678392edfca512f

--- resultdb/sink/sink_server.go
+++ resultdb/sink/sink_server.go
@@ -16,30 +16,151 @@ package sink
 import (
 	"context"
 	"fmt"
+	"os"
+	"sort"
+	"time"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/google/uuid"
+	"golang.org/x/time/rate"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 
 	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/sync/dispatcher"
+	"go.chromium.org/luci/common/sync/dispatcher/buffer"
 
 	"go.chromium.org/luci/resultdb/pbutil"
+	rpcpb "go.chromium.org/luci/resultdb/proto/rpc/v1"
 	sinkpb "go.chromium.org/luci/resultdb/proto/sink/v1"
 )
 
+// channel options for Recorder.
+func recorderDispatcherOpts() *dispatcher.Options {
+	return &dispatcher.Options{
+		QPSLimit: rate.NewLimiter(1, 1),
+		Buffer: buffer.Options{
+			BatchSize:     20,
+			MaxLeases:     4,
+			BatchDuration: 10 * time.Second,
+			FullBehavior:  &buffer.BlockNewItems{MaxItems: 1000},
+		},
+		// retry on transient errors
+		ErrorFn: dispatcher.ErrorFnQuiet,
+	}
+}
+
 // sinkServer implements sinkpb.SinkServer.
 type sinkServer struct {
-	cfg ServerConfig
+	cfg     ServerConfig
+	closeFn func()
+
+	recCh    *dispatcher.Channel
+	recorder rpcpb.RecorderClient
 }
 
 func newSinkServer(ctx context.Context, cfg ServerConfig) (sinkpb.SinkServer, error) {
+	ss := &sinkServer{cfg: cfg}
+	if err := ss.initChannels(ctx); err != nil {
+		return nil, err
+	}
 	return &sinkpb.DecoratedSink{
-		Service: &sinkServer{
-			cfg: cfg,
-		},
+		Service: ss,
 		Prelude: authTokenPrelude(cfg.AuthToken),
 	}, nil
+}
+
+func (s *sinkServer) initChannels(ctx context.Context) error {
+	// install a dispatcher channel for Recorder
+	recSendFn := func(b *buffer.Batch) error {
+		req := prepareReportTestResultsRequest(ctx, s.cfg.Invocation, b)
+		_, err := s.cfg.Recorder.BatchCreateTestResults(ctx, req)
+		return err
+	}
+	rc, err := dispatcher.NewChannel(ctx, recorderDispatcherOpts(), recSendFn)
+	if err != nil {
+		return err
+	}
+	s.recCh = &rc
+
+	// TODO(crbug.com/1017288) - add gs channel
+	return nil
+}
+
+func prepareReportTestResultsRequest(ctx context.Context, inv string, b *buffer.Batch) *rpcpb.BatchCreateTestResultsRequest {
+	// retried batch?
+	if b.Meta != nil {
+		return b.Meta.(*rpcpb.BatchCreateTestResultsRequest)
+	}
+	req := &rpcpb.BatchCreateTestResultsRequest{
+		Invocation: inv,
+		// a random UUID
+		RequestId: uuid.New().String(),
+	}
+	for _, d := range b.Data {
+		tr := d.(*sinkpb.TestResult)
+		req.Requests = append(req.Requests, &rpcpb.CreateTestResultRequest{
+			TestResult: &rpcpb.TestResult{
+				TestId:          tr.GetTestId(),
+				ResultId:        tr.GetResultId(),
+				Variant:         tr.GetVariant(),
+				Expected:        tr.GetExpected(),
+				SummaryHtml:     tr.GetSummaryHtml(),
+				StartTime:       tr.GetStartTime(),
+				Duration:        tr.GetDuration(),
+				Tags:            tr.GetTags(),
+				InputArtifacts:  sinkArtsToRpcArts(ctx, tr.GetInputArtifacts()),
+				OutputArtifacts: sinkArtsToRpcArts(ctx, tr.GetOutputArtifacts()),
+			},
+		})
+	}
+	b.Meta = req
+	return req
+}
+
+func sinkArtsToRpcArts(ctx context.Context, sArts map[string]*sinkpb.Artifact) (rArts []*rpcpb.Artifact) {
+	for name, sart := range sArts {
+		var size int64 = -1
+		fn := sart.GetFilePath()
+		cnts := sart.GetContents()
+
+		switch {
+		case fn != "":
+			if info, err := os.Stat(fn); err == nil {
+				size = info.Size()
+			} else {
+				logging.Errorf(ctx, "%s: %s - %s", name, fn, err)
+			}
+		case cnts != nil:
+			size = int64(len(cnts))
+		default:
+			// This should never be reached. pbutil.ValidateSinkArtifact() should
+			// filter out invalid artifacts.
+			panic(fmt.Sprintf("%s: neither file_path nor contents were given", name))
+		}
+
+		rArts = append(rArts, &rpcpb.Artifact{
+			Name: name,
+			// TODO(ddoman): set fetch_url and fetch_url_expiration
+			// skip `FetchUrl`
+			// skip `FetchUrlExpiration`
+			ContentType: sart.GetContentType(),
+			Size:        size,
+		})
+	}
+	sort.Slice(rArts, func(i, j int) bool {
+		return rArts[i].Name < rArts[j].Name
+	})
+	return
+}
+
+// closeSinkServer closes the dispatcher channels and blocks until they are fully drained,
+// or the context is cancelled.
+func closeSinkServer(ctx context.Context, s sinkpb.SinkServer) {
+	ss := s.(*sinkpb.DecoratedSink).Service.(*sinkServer)
+	ss.recCh.CloseAndDrain(ctx)
 }
 
 // authTokenValue returns the value of the Authorization HTTP header that all requests must
@@ -80,5 +201,9 @@ func (s *sinkServer) ReportTestResults(ctx context.Context, in *sinkpb.ReportTes
 			return nil, status.Errorf(codes.InvalidArgument, "bad request: %s", err)
 		}
 	}
+	for _, tr := range in.TestResults {
+		s.recCh.C <- tr
+	}
+	// TODO(1017288) - set `TestResultNames` in the response
 	return &sinkpb.ReportTestResultsResponse{}, nil
 }
