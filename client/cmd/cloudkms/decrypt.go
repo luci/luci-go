c4ed609c78729d156e1deb29e09551e7e8529d88
Jason D. Clinton <jclinton@chromium.org>
2020-03-25 02:39:35 +0000

    cloudkms: Port to new Cloud KMS APIs
    Bug: 1059973
    Change-Id: Ibc71bf097dd3383f4da774158b0046d18fecbad9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2113952
    Auto-Submit: Jason Clinton <jclinton@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

--- client/cmd/cloudkms/decrypt.go
+++ client/cmd/cloudkms/decrypt.go
@@ -16,42 +16,29 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
-	"time"
 
 	"github.com/maruel/subcommands"
 
-	cloudkms "google.golang.org/api/cloudkms/v1"
+	cloudkms "cloud.google.com/go/kms/apiv1"
+	kmspb "google.golang.org/genproto/googleapis/cloud/kms/v1"
 
 	"go.chromium.org/luci/auth"
 	"go.chromium.org/luci/common/logging"
-	"go.chromium.org/luci/common/retry"
-	"go.chromium.org/luci/common/retry/transient"
 )
 
-func doDecrypt(ctx context.Context, service *cloudkms.Service, input []byte, keyPath string) ([]byte, error) {
+func doDecrypt(ctx context.Context, client *cloudkms.KeyManagementClient, input []byte, keyPath string) ([]byte, error) {
 	// Set up request, using the ciphertext directly (should already be base64 encoded).
-	req := cloudkms.DecryptRequest{
-		Ciphertext: base64.StdEncoding.EncodeToString(input),
+	req := &kmspb.DecryptRequest{
+		Name:       keyPath,
+		Ciphertext: input,
 	}
 
-	var resp *cloudkms.DecryptResponse
-	err := retry.Retry(ctx, transient.Only(retry.Default), func() error {
-		var err error
-		resp, err = service.
-			Projects.
-			Locations.
-			KeyRings.
-			CryptoKeys.
-			Decrypt(keyPath, &req).Context(ctx).Do()
-		return err
-	}, func(err error, d time.Duration) {
-		logging.Warningf(ctx, "Transient error while making request, retrying in %s...", d)
-	})
+	resp, err := client.Decrypt(ctx, req)
 	if err != nil {
+		logging.Errorf(ctx, "Error while making request")
 		return nil, err
 	}
-	return base64.StdEncoding.DecodeString(resp.Plaintext)
+	return resp.Plaintext, nil
 }
 
 func cmdDecrypt(authOpts auth.Options) *subcommands.Command {
