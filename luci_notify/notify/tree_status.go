7a9635d7fc2661c7f9334f6ce256c04fcd5df74d
Owen Rodley <orodley@chromium.org>
2020-04-02 16:39:49 +1100

    Fetch TreeClosers, and compute the new status.
    In this CL the remote tree status instance is stubbed out with an
    interface. In the test we use a fake, in-memory implementation, and in
    the real code we just use a dummy implementation, for now.
    
    Bug: 1051371
    Change-Id: I80709645e1decf68a331093ab28bfd75e88f2cdd

--- luci_notify/notify/tree_status.go
+++ luci_notify/notify/tree_status.go
@@ -16,12 +16,43 @@ package notify
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
+	"go.chromium.org/gae/service/datastore"
 	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/sync/parallel"
+	"go.chromium.org/luci/luci_notify/config"
 	"go.chromium.org/luci/server/router"
 )
+
+const botUsername = "luci-notify@chromium.org"
+const legacyBotUsername = "buildbot@chromium.org"
+
+type TreeStatus struct {
+	username string
+	message  string
+	key      string
+	status   config.TreeCloserStatus
+}
+
+type TreeStatusClient interface {
+	getStatus(host string) (*TreeStatus, error)
+	putStatus(host, message, prevKey string) error
+}
+
+// TODO: Write a real implementation.
+type DummyTreeStatusClient struct{}
+
+// For the dummy impl we return a closed status, set by someone else. We won't
+// even attempt to re-open in this case.
+func (dummy *DummyTreeStatusClient) getStatus(host string) (*TreeStatus, error) {
+	return &TreeStatus{"someone.else", "", "", config.Closed}, nil
+}
+func (dummy *DummyTreeStatusClient) putStatus(host, message, prevKey string) error {
+	return nil
+}
 
 // UpdateTreeStatus is the HTTP handler triggered by cron when it's time to
 // check tree closers and update tree status if necessary.
@@ -30,7 +61,7 @@ func UpdateTreeStatus(c *router.Context) {
 	ctx, cancel := context.WithTimeout(ctx, time.Minute)
 	defer cancel()
 
-	if err := updateTrees(ctx); err != nil {
+	if err := updateTrees(ctx, &DummyTreeStatusClient{}); err != nil {
 		logging.WithError(err).Errorf(ctx, "error while updating tree status")
 		w.WriteHeader(http.StatusInternalServerError)
 	} else {
@@ -40,8 +71,77 @@ func UpdateTreeStatus(c *router.Context) {
 
 // updateTrees fetches all TreeClosers from datastore, uses this to determine if
 // any trees should be opened or closed, and makes the necessary updates.
-//
-// TODO: Or at least it will - it's just a placeholder for now.
-func updateTrees(c context.Context) error {
-	return nil
+func updateTrees(c context.Context, ts TreeStatusClient) error {
+	var treeClosers []*config.TreeCloser
+	if err := datastore.GetAll(c, datastore.NewQuery("TreeCloser"), &treeClosers); err != nil {
+		return err
+	}
+
+	return parallel.FanOutIn(func(ch chan<- func() error) {
+		for host, treeClosers := range groupTreeClosers(treeClosers) {
+			host, treeClosers := host, treeClosers
+			ch <- func() error { return updateHost(ts, host, treeClosers) }
+		}
+	})
+}
+
+func groupTreeClosers(treeClosers []*config.TreeCloser) map[string][]*config.TreeCloser {
+	byHost := map[string][]*config.TreeCloser{}
+	for _, tc := range treeClosers {
+		slice, exists := byHost[tc.TreeStatusHost]
+		if !exists {
+			slice = []*config.TreeCloser{}
+		}
+
+		byHost[tc.TreeStatusHost] = append(slice, tc)
+	}
+
+	fmt.Printf("groupTreeClosers(%+v) -> %+v\n", treeClosers, byHost)
+	return byHost
+}
+
+func updateHost(ts TreeStatusClient, host string, treeClosers []*config.TreeCloser) error {
+	var oldestClosed *config.TreeCloser = nil
+	for _, tc := range treeClosers {
+		if tc.Status == config.Closed && (oldestClosed == nil || tc.Timestamp.Before(oldestClosed.Timestamp)) {
+			oldestClosed = tc
+		}
+	}
+
+	var overallStatus config.TreeCloserStatus
+	if oldestClosed == nil {
+		overallStatus = config.Open
+	} else {
+		overallStatus = config.Closed
+	}
+
+	treeStatus, err := ts.getStatus(host)
+	if err != nil {
+		return err
+	}
+	if treeStatus.status == overallStatus {
+		// Nothing to do, status is already correct.
+		return nil
+	}
+	if treeStatus.status == config.Closed && treeStatus.username != botUsername && treeStatus.username != legacyBotUsername {
+		// Don't reopen the tree if it wasn't automatically closed.
+		return nil
+	}
+
+	var message string
+	if overallStatus == config.Open {
+		message = fmt.Sprintf("Tree is open (Automatic: %s)", randomEmoji())
+	} else {
+		// TODO: We actually want to render the template from oldestClosed.
+		// We can't do this yet as we don't store the necessary info in the
+		// TreeCloser config struct.
+		message = fmt.Sprintf("Tree is closed (Automatic: %s)", "TODO")
+	}
+	fmt.Printf("Putting status %q for host %s\n", message, host)
+	return ts.putStatus(host, message, treeStatus.key)
+}
+
+func randomEmoji() string {
+	// TODO: Import the emojis from Gatekeeper.
+	return "Yes!"
 }
