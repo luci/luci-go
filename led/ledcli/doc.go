f117e5e048d7b097390e6752923b3279d319b7ae
Robert Iannucci <iannucci@chromium.org>
2020-04-07 05:50:29 +0000

    [ledcli] Add ledcli package docs.
    R=smut@google.com, tandrii@chromium.org
    
    Bug: 1017818
    Change-Id: I7cf6665effb25f89973d834a09f434814960ad28
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2091282
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>

---
+++ led/ledcli/doc.go
@@ -0,0 +1,28 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package ledcli implements the subcommands for the `led` command line tool.
+//
+// Each file here roughly corresponds to one of the subcommands of `led`. A
+// `led` subcommand manipulates a `job definition` object (a proto, see the
+// `job` subpackage).
+//
+// The `get*` subcommands writes a job definition from some external source.
+// The `launch` subcommand reads a job definition and launches it in Swarming.
+// The `edit*` subcommands reads a job definition, manipulates it, then writes
+//   the evolved job definition out.
+//
+// led subcommands are meant to be connected in a UNIX pipeline, starting with
+// a 'get' subcommand, and ending with the 'launch' subcommand.
+package ledcli
