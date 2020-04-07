ba3c1bac1d135941f65b9ff7b4ec4009a68adf80
Robert Iannucci <iannucci@chromium.org>
2020-04-07 05:16:09 +0000

    [ledcmd] Add get_build functionality.
    This creates a job.Definition from a Buildbucket build.
    
    R=smut@google.com, tandrii@chromium.org
    
    Bug: 1017818
    Change-Id: Ie92838f3120e988aa50f8936fc1529774a1b8b68
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2091839
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: smut <smut@google.com>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>

---
+++ led/ledcmd/get_build.go
@@ -0,0 +1,81 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ledcmd
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/led/job"
+	"google.golang.org/genproto/protobuf/field_mask"
+)
+
+// GetBuildOpts are the options for GetBuild.
+type GetBuildOpts struct {
+	BuildbucketHost string
+	BuildID         int64
+	PinBotID        bool
+}
+
+// GetBuild retrieves a job Definition from a Buildbucket build.
+func GetBuild(ctx context.Context, authClient *http.Client, opts GetBuildOpts) (*job.Definition, error) {
+	logging.Infof(ctx, "getting build definition")
+	bbucket := newBuildbucketClient(authClient, opts.BuildbucketHost)
+
+	answer, err := bbucket.GetBuild(ctx, &bbpb.GetBuildRequest{
+		Id: opts.BuildID,
+		Fields: &field_mask.FieldMask{
+			Paths: []string{"tags"},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	logging.Infof(ctx, "getting build definition: done")
+
+	swarmingTaskID := ""
+	swarmingHostname := ""
+	for _, t := range answer.Tags {
+		key, value := t.Key, t.Value
+		switch key {
+		case "swarming_task_id":
+			swarmingTaskID = value
+		case "swarming_hostname":
+			swarmingHostname = value
+		}
+		if swarmingTaskID != "" && swarmingHostname != "" {
+			break
+		}
+	}
+
+	if swarmingTaskID == "" {
+		return nil, errors.New("unable to find swarming task ID on buildbucket task")
+	}
+	if swarmingHostname == "" {
+		return nil, errors.New("unable to find swarming hostname on buildbucket task")
+	}
+
+	return GetFromSwarmingTask(ctx, authClient, GetFromSwarmingTaskOpts{
+		SwarmingHost: swarmingHostname,
+		TaskID:       swarmingTaskID,
+		PinBotID:     opts.PinBotID,
+		Name:         fmt.Sprintf("get-build %d", opts.BuildID),
+	})
+}
