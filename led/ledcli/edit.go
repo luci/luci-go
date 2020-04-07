0dbbda1b5f92b1c6f34d88253230eb218843a03e
Robert Iannucci <iannucci@chromium.org>
2020-04-07 17:37:25 +0000

    [ledcli] Add `led edit` subcommand.
    R=smut@google.com, tandrii@chromium.org
    
    Bug: 1017818
    Change-Id: I396bab3d2911d5979d05a173fa78c9a8170f9a59
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2091287
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>

---
+++ led/ledcli/edit.go
@@ -0,0 +1,135 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ledcli
+
+import (
+	"net/http"
+
+	"golang.org/x/net/context"
+
+	"github.com/maruel/subcommands"
+
+	"go.chromium.org/luci/common/flag/stringlistflag"
+	"go.chromium.org/luci/common/flag/stringmapflag"
+	"go.chromium.org/luci/led/job"
+)
+
+func editCmd(opts cmdBaseOptions) *subcommands.Command {
+	return &subcommands.Command{
+		UsageLine: "edit [options]",
+		ShortDesc: "edits the userland of a JobDescription",
+		LongDesc: `Allows common manipulations to a JobDescription.
+
+Example:
+
+led get-builder ... |
+  led edit -d os=Linux -p something=[100] |
+  led launch
+`,
+
+		CommandRun: func() subcommands.CommandRun {
+			ret := &cmdEdit{}
+			ret.initFlags(opts)
+			return ret
+		},
+	}
+}
+
+type cmdEdit struct {
+	cmdBase
+
+	dimensions     stringlistflag.Flag
+	properties     stringmapflag.Value
+	propertiesAuto stringmapflag.Value
+	recipeName     string
+	experimental   string
+
+	processedDimensions job.DimensionEditCommands
+
+	swarmingHost string
+}
+
+func (c *cmdEdit) initFlags(opts cmdBaseOptions) {
+	c.Flags.Var(&c.dimensions, "d",
+		"(repeatable) edit a dimension. "+
+			"This takes a parameter of `dimension{=,-=,+=}[value[@expiration_secs]]`. "+
+			"Specifying '=[value[@expiration_secs]]' will Reset the dimension to the"+
+			" Set of values specified with = (repeating this adds to the Set."+
+			" To clear the dimension, specify `dimension=`). "+
+			"Specifying '-=value' will Delete the value from the dimension. "+
+			"Specifying '+=value[@expiration_secs]' will Add that value to the dimension (expiration). "+
+			"Operations are applied as Resets, Deletions, Additions. "+
+			"If expiration_secs are omitted, all slices will have the dimension.")
+
+	c.Flags.Var(&c.properties, "p",
+		"(repeatable) override a recipe property. This takes a parameter of `property_name=json_value`. "+
+			"Providing an empty json_value will remove that property.")
+
+	c.Flags.Var(&c.propertiesAuto, "pa",
+		"(repeatable) override a recipe property, using the recipe engine autoconvert rule. "+
+			"This takes a parameter of `property_name=json_value_or_string`. If json_value_or_string "+
+			"cannot be decoded as JSON, it will be used verbatim as the property value. "+
+			"Providing an empty json_value will remove that property.")
+
+	c.Flags.StringVar(&c.recipeName, "r", "",
+		"override the `recipe` to run.")
+
+	c.Flags.StringVar(&c.swarmingHost, "S", "",
+		"override the swarming `host` to launch the task on (i.e. chromium-swarm.appspot.com).")
+
+	c.Flags.StringVar(&c.experimental, "exp", "",
+		"set to `true` or `false` to change the Build.Input.Experimental value. `led` jobs, "+
+			"by default, always start as experimental.")
+
+	c.cmdBase.initFlags(opts)
+}
+
+func (c *cmdEdit) positionalRange() (min, max int) { return 0, 0 }
+func (c *cmdEdit) jobInput() bool                  { return true }
+
+func (c *cmdEdit) validateFlags(ctx context.Context, _ []string, _ subcommands.Env) (err error) {
+	c.processedDimensions, err = job.MakeDimensionEditCommands(c.dimensions)
+	if err != nil {
+		return err
+	}
+
+	return
+}
+
+func (c *cmdEdit) execute(ctx context.Context, _ *http.Client, inJob *job.Definition) (out interface{}, err error) {
+	err = inJob.Edit(func(je job.Editor) {
+		je.EditDimensions(c.processedDimensions)
+		if host := c.swarmingHost; host != "" {
+			je.SwarmingHostname(c.swarmingHost)
+		}
+	})
+	if err == nil {
+		err = inJob.HighLevelEdit(func(je job.HighLevelEditor) {
+			je.Properties(c.properties, false)
+			je.Properties(c.propertiesAuto, true)
+			if c.recipeName != "" {
+				je.Properties(map[string]string{"recipe": c.recipeName}, true)
+			}
+			if c.experimental != "" {
+				je.Experimental(c.experimental == "true")
+			}
+		})
+	}
+	return inJob, err
+}
+
+func (c *cmdEdit) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	return c.doContextExecute(a, c, args, env)
+}
