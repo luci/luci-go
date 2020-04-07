055cb361c484632500a8ddda8ef1a23b4e867ca8
Owen Rodley <orodley@chromium.org>
2020-04-07 02:06:17 +0000

    Change from a dummy TreeStatusClient to a read-only impl.
    Bug: 1051371
    Change-Id: I321608dde1fa6e6230527ab6d906652a31098919
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2135092
    Commit-Queue: Owen Rodley <orodley@chromium.org>
    Auto-Submit: Owen Rodley <orodley@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- luci_notify/notify/tree_status.go
+++ luci_notify/notify/tree_status.go
@@ -16,14 +16,18 @@ package notify
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
 	"go.chromium.org/gae/service/datastore"
+	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/sync/parallel"
 	"go.chromium.org/luci/luci_notify/config"
+	"go.chromium.org/luci/server/auth"
 	"go.chromium.org/luci/server/router"
 )
 
@@ -33,26 +37,90 @@ const legacyBotUsername = "buildbot@chromium.org"
 type treeStatus struct {
 	username  string
 	message   string
-	key       string
+	key       int64
 	status    config.TreeCloserStatus
 	timestamp time.Time
 }
 
 type treeStatusClient interface {
 	getStatus(c context.Context, host string) (*treeStatus, error)
-	putStatus(c context.Context, host, message, prevKey string) error
+	putStatus(c context.Context, host, message string, prevKey int64) error
 }
 
-// TODO: Write a real implementation.
-type dummyTreeStatusClient struct{}
-
-// For the dummy impl we return a closed status, set by someone else. We won't
-// even attempt to re-open in this case.
-func (dummy *dummyTreeStatusClient) getStatus(c context.Context, host string) (*treeStatus, error) {
-	return &treeStatus{"someone.else", "", "", config.Closed, time.Now()}, nil
+type readOnlyTreeStatusClient struct {
+	fetchFunc func(context.Context, string) ([]byte, error)
 }
-func (dummy *dummyTreeStatusClient) putStatus(c context.Context, host, message, prevKey string) error {
+
+func (ts *readOnlyTreeStatusClient) getStatus(c context.Context, host string) (*treeStatus, error) {
+	respJSON, err := ts.fetchFunc(c, fmt.Sprintf("https://%s/current?format=json", host))
+	if err != nil {
+		return nil, err
+	}
+
+	var r struct {
+		Username        string
+		CanCommitFreely bool `json:"can_commit_freely"`
+		Key             int64
+		Date            string
+		Message         string
+	}
+	if err = json.Unmarshal(respJSON, &r); err != nil {
+		return nil, errors.Annotate(err, "failed to unmarshal JSON").Err()
+	}
+
+	var status config.TreeCloserStatus = config.Closed
+	if r.CanCommitFreely {
+		status = config.Open
+	}
+
+	// Similar to RFC3339, but not quite the same. No time zone is specified,
+	// so this will default to UTC, which is correct here.
+	const dateFormat = "2006-01-02 15:04:05.999999"
+	t, err := time.Parse(dateFormat, r.Date)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to parse date from tree status").Err()
+	}
+
+	return &treeStatus{
+		username:  r.Username,
+		message:   r.Message,
+		key:       r.Key,
+		status:    status,
+		timestamp: t,
+	}, nil
+}
+
+// TODO: Make this actually update the tree status, once we're confident it's
+// doing the right thing.
+func (ts *readOnlyTreeStatusClient) putStatus(c context.Context, host, message string, prevKey int64) error {
+	logging.Infof(c, "Updating status for %s: %s", host, message)
 	return nil
+}
+
+func fetchHttp(c context.Context, url string) ([]byte, error) {
+	transport, err := auth.GetRPCTransport(c, auth.AsSelf)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(c)
+
+	response, err := (&http.Client{Transport: transport}).Do(req)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to get data from %q", url).Err()
+	}
+
+	defer response.Body.Close()
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to read response body from %q", url).Err()
+	}
+
+	return bytes, nil
 }
 
 // UpdateTreeStatus is the HTTP handler triggered by cron when it's time to
@@ -62,7 +130,7 @@ func UpdateTreeStatus(c *router.Context) {
 	ctx, cancel := context.WithTimeout(ctx, time.Minute)
 	defer cancel()
 
-	if err := updateTrees(ctx, &dummyTreeStatusClient{}); err != nil {
+	if err := updateTrees(ctx, &readOnlyTreeStatusClient{fetchHttp}); err != nil {
 		logging.WithError(err).Errorf(ctx, "error while updating tree status")
 		w.WriteHeader(http.StatusInternalServerError)
 	} else {
