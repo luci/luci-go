e0989f893e9b4ab2ca26cd7370b94ddd554aac5c
Robert Iannucci <iannucci@chromium.org>
2020-04-07 22:02:55 +0000

    [led] Add pure luci-go main executable for `led`.
    This version doesn't have kitchen support, but that's OK; Once we drop
    kitchen, this version will be the one that lives on.
    
    infra.git will have a different entrypoint which supplies kitchen
    functionality (since it needs to link against 'infra/*' libraries to
    parse the kitchen cook flags). We'll ship the infra.git version as the
    first replacement during the transition.
    
    R=smut@google.com, tandrii@chromium.org
    
    Change-Id: I7bd89fb24ad859337dc5ed616b664ae64411f497
    Bug: 1017818
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2065982
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: smut <smut@google.com>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>

---
+++ led/main.go
@@ -0,0 +1,36 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Command led implements a 'led' binary WITHOUT support for "kitchen" based
+// tasks. If you need support for kitchen based tasks, see 'infra/tools/led2'.
+//
+// TODO(crbug.com/1015181) Hopefully kitchen isn't long for this world and will
+// be gone soon (~mid 2020).
+//
+// Subpackages include:
+//   * ledcli        - The implementation of the CLI executable, paramaterized
+//                     with an object to handle kitchen jobs.
+//   * ledcmd        - Implementation of 'heavyweight' led subcommands, usually
+//                     those which interact with external services.
+//   * job           - The job definition which is passed between subcommands,
+//                     as well as structured editing APIs.
+//   * job/jobcreate - Library for generating jobs from external sources
+//   * job/jobexport - Library for exporting jobs to external sinks
+package main
+
+import "go.chromium.org/luci/led/ledcli"
+
+func main() {
+	ledcli.Main(nil)
+}
