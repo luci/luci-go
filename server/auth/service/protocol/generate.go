d9943f49d18dc1bfe85abcd9536e942da5cf342e
Vadim Shtayura <vadimsh@chromium.org>
2020-04-10 03:21:35 +0000

    [auth] Copy realms.proto from luci-py.
    Since these *.proto files now use `import ...` we need some hacks
    to make them be able to find each other.
    
    Also add a script to automate this a bit for the future.
    
    R=tandrii@chromium.org
    BUG=1051724
    
    Change-Id: Ibf5983b859381d0f05277f492ad224698b2f3ac2
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2145098
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

--- server/auth/service/protocol/generate.go
+++ server/auth/service/protocol/generate.go
@@ -1,4 +1,4 @@
-// Copyright 2015 The LUCI Authors.
+// Copyright 2020 The LUCI Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,13 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate cproto
+//go:generate cproto -proto-path . components/auth/proto
 
 package protocol
 
 // Files copied from https://chromium.googlesource.com/infra/luci/luci-py:
+//   appengine/components/components/auth/proto/realms.proto
 //   appengine/components/components/auth/proto/replication.proto
 //   appengine/components/components/auth/proto/security_config.proto
 //
-// Commit: 16689396b8c039960af4f5ab40b3e1a20eb1f259
+// Commit: 0a090525210dc130a7dc9e40ec967eb197b8f9d7
 // Modifications: None
