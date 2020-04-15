8bc1f365287080a86082a7b8e2f381b77f2da63e
Yiwei Zhang <yiwzhang@google.com>
2020-04-15 15:04:52 -0700

    [led] Instructs bbagent not to upload build to buildbucket
    R=iannucci
    
    Bug: 1017818
    Change-Id: I292a2f9066e6dc821374eb4e4b1aa2b0dbdd0a81

--- led/job/jobcreate/create.go
+++ led/job/jobcreate/create.go
@@ -82,12 +82,11 @@ func FromNewTaskRequest(ctx context.Context, r *swarming.SwarmingRpcsNewTaskRequ
 	}
 
 	if bb := ret.GetBuildbucket(); err == nil && bb != nil {
-		bb.BbagentArgs.Build.Id = 0
-		bb.BbagentArgs.Build.Infra.Logdog.Prefix = ""
-		bb.BbagentArgs.Build.Infra.Swarming.TaskId = ""
-
 		// set all buildbucket type tasks to experimental by default.
 		bb.BbagentArgs.Build.Input.Experimental = true
+
+		// Don't upload build to buildbucket as there's no associated buildbucket build
+		bb.BbagentArgs.Build.Infra.Buildbucket.Hostname = "no-upload"
 
 		// bump priority by default
 		bb.BbagentArgs.Build.Infra.Swarming.Priority += 10
@@ -96,11 +95,14 @@ func FromNewTaskRequest(ctx context.Context, r *swarming.SwarmingRpcsNewTaskRequ
 		bb.BbagentArgs.Build.CreatedBy = ""
 		bb.BbagentArgs.Build.CanceledBy = ""
 		bb.BbagentArgs.Build.CreateTime = nil
-		bb.BbagentArgs.Build.UpdateTime = nil
 		bb.BbagentArgs.Build.Id = 0
 		bb.BbagentArgs.Build.Number = 0
-		bb.BbagentArgs.Build.Infra.Buildbucket.RequestedProperties = nil
 		bb.BbagentArgs.Build.Status = 0
+		bb.BbagentArgs.Build.UpdateTime = nil
+
+		bb.BbagentArgs.Build.Infra.Buildbucket.RequestedProperties = nil
+		bb.BbagentArgs.Build.Infra.Logdog.Prefix = ""
+		bb.BbagentArgs.Build.Infra.Swarming.TaskId = ""
 
 		// drop the executable package; it's canonically represented by
 		// out.BBAgentArgs.Build.Exe.
