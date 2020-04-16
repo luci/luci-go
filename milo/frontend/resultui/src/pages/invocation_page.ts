6d9e554849bf52a73a135a5ae431923ff5cc4df3
Weiwei Lin <weiweilin@google.com>
2020-04-16 14:20:14 +1000

    [resultui] implement test selection
    Bug:1044351
    Change-Id: I115fb6a2ebad3762c46097155626297ba842429e

--- milo/frontend/resultui/src/pages/invocation_page.ts
+++ milo/frontend/resultui/src/pages/invocation_page.ts
@@ -90,11 +90,7 @@ export class InvocationPageElement extends MobxLitElement implements BeforeEnter
     return ret;
   }
 
-  @computed
-  private get selectedTests(): readonly ReadonlyTest[] {
-    // TODO(weiweilin): implement this.selectedTests()
-    return this.testLoader.node.allTests;
-  }
+  @observable.ref selectedNode = this.testLoader.node;
 
   @computed
   private get rootName(): string {
@@ -117,12 +113,15 @@ export class InvocationPageElement extends MobxLitElement implements BeforeEnter
       () => this.invocationReq.get().tag === 'err',
       () => Router.go('/error'),
     ));
-    // Load the first batch whenever this.testLoader is updated.
+    // Load more tests when there are more tests to be displayed but not loaded.
     this.disposers.push(reaction(
-      () => this.testLoader,
       () => {
-        this.testLoader.loadMore();
+        if (this.selectedNode.fullyLoaded || this.testLoader.isLoading) {
+          return 0;
+        }
+        return Math.max(this.pageLength - this.selectedNode.allTests.length, 0);
       },
+      (numOfTests) => this.testLoader.loadMore(numOfTests),
       {fireImmediately: true},
     ));
   }
@@ -171,7 +170,10 @@ export class InvocationPageElement extends MobxLitElement implements BeforeEnter
           id="left-panel"
           style=${styleMap({display: this.leftPanelExpanded ? '' : 'none'})}
         >
-          <tr-test-nav-tree .testLoader=${this.testLoader}></tr-test-nav-tree>
+          <tr-test-nav-tree
+            .testLoader=${this.testLoader}
+            .onSelectedNodeChanged=${(node: TestNode) => this.selectedNode = node}
+          ></tr-test-nav-tree>
         </div>
         <div id="test-result-view">
           <div id="test-result-header">
@@ -181,24 +183,20 @@ export class InvocationPageElement extends MobxLitElement implements BeforeEnter
             <span id="root-name" title="common test ID prefix">${this.rootName}</span>
           </div>
           <div id="test-result-content">
-            ${repeat(this.selectedTests.slice(0, this.pageLength), (t) => t.id, (t, i) => html`
+            ${repeat(this.selectedNode.allTests.slice(0, this.pageLength), (t) => t.id, (t, i) => html`
             <tr-test-entry
               .test=${t}
               .rootName=${this.rootName}
-              .prevTestId=${(this.selectedTests[i-1]?.id || this.rootName)}
-              .expanded=${this.selectedTests.length === 1}
+              .prevTestId=${(this.selectedNode.allTests[i-1]?.id || this.rootName)}
+              .expanded=${this.selectedNode.allTests.length === 1}
             ></tr-test-entry>
             `)}
             <div id="list-tail">
-              <span>Showing ${Math.min(this.selectedTests.length, this.pageLength)}/${this.selectedTests.length} tests.</span>
+              <span>Showing ${Math.min(this.selectedNode.allTests.length, this.pageLength)}/${this.selectedNode.allTests.length}${this.selectedNode.fullyLoaded ? '' : '+'} tests.</span>
               <span
                 id="load-more"
-                style=${styleMap({'display': this.pageLength >= this.selectedTests.length ? 'none' : ''})}
-                @click=${() => {
-                  // TODO(weiweilin): trigger this.testLoader.loadMore() when
-                  // needed.
-                  this.pageLength += 100;
-                }}
+                style=${styleMap({'display': this.pageLength >= this.selectedNode.allTests.length && this.selectedNode.fullyLoaded ? 'none' : ''})}
+                @click=${() => this.pageLength += 100}
               >
                 Load More
               </span>
@@ -294,5 +292,7 @@ export class InvocationPageElement extends MobxLitElement implements BeforeEnter
 }
 
 customElement('tr-invocation-page')(
-  contextConsumer('appState')(InvocationPageElement),
+  contextConsumer('appState')(
+    InvocationPageElement,
+  ),
 );
