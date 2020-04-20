17e4caa60a0a087d3a25a9864c28450fb16cd4cb
Joey Scarr <jsca@google.com>
2020-04-20 17:35:49 +1000

    [milo] Remove support for viewing buildbot builds.
    Bug: 998337
    Change-Id: Iac00774ecc371281dba99a7fc999ebeec0ef7082

--- milo/buildsource/builder.go
+++ milo/buildsource/builder.go
@@ -1,32 +1,34 @@
-// Copyright 2017 The LUCI Authors. All rights reserved.
-// Use of this source code is governed under the Apache License, Version 2.0
-// that can be found in the LICENSE file.
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package buildsource
 
 import (
-	"context"
-	"fmt"
-	"sort"
 	"strings"
 
 	"go.chromium.org/luci/common/errors"
-	"go.chromium.org/luci/common/sync/parallel"
 	"go.chromium.org/luci/grpc/grpcutil"
 
-	"go.chromium.org/luci/milo/buildsource/buildbot"
-	"go.chromium.org/luci/milo/common"
 	"go.chromium.org/luci/milo/common/model"
-	"go.chromium.org/luci/milo/frontend/ui"
 )
 
 // BuilderID is the universal ID of a builder, and has the form:
 //   buildbucket/bucket/builder
-//   buildbot/master/builder   (deprecated)
 type BuilderID string
 
 // Split breaks the BuilderID into pieces.
-//   - backend is either 'buildbot' or 'buildbucket'
+//   - backend is always 'buildbucket'
 //   - backendGroup is either the bucket or master name
 //   - builderName is the builder name.
 //
@@ -40,81 +42,19 @@ func (b BuilderID) Split() (backend, backendGroup, builderName string, err error
 		return
 	}
 	backend, backendGroup, builderName = toks[0], toks[1], toks[2]
-	switch backend {
-	case "buildbot", "buildbucket":
-	default:
+	switch {
+	case backend != "buildbucket":
 		err = errors.Reason("bad BuilderID: unknown backend %q", backend).
 			Tag(grpcutil.InvalidArgumentTag).Err()
-		return
-	}
-	if backendGroup == "" {
+	case backendGroup == "":
 		err = errors.New("bad BuilderID: empty backendGroup", grpcutil.InvalidArgumentTag)
-		return
-	}
-	if builderName == "" {
+	case builderName == "":
 		err = errors.New("bad BuilderID: empty builderName", grpcutil.InvalidArgumentTag)
-		return
 	}
 	return
-}
-
-// GetLegacy allows you to obtain the resp.Builder that corresponds with this
-// BuilderID.
-func (b BuilderID) GetLegacy(c context.Context, limit int, cursor string) (*ui.BuilderLegacy, error) {
-	// TODO(iannucci): replace these implementations with a BuildSummary query.
-	source, group, builderName, err := b.Split()
-	if err != nil {
-		return nil, err
-	}
-
-	var builder *ui.BuilderLegacy
-	var consoles []*common.Console
-	err = parallel.FanOutIn(func(work chan<- func() error) {
-		work <- func() (err error) {
-			switch source {
-			case "buildbot":
-				builder, err = buildbot.GetBuilder(c, group, builderName, limit, cursor)
-			default:
-				panic(fmt.Errorf("unexpected build source %q", source))
-			}
-			return
-		}
-		work <- func() (err error) {
-			consoles, err = common.GetAllConsoles(c, string(b))
-			return
-		}
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	builder.Groups = make([]*ui.Link, len(consoles))
-	for i, c := range consoles {
-		builder.Groups[i] = ui.NewLink(
-			fmt.Sprintf("%s / %s", c.ProjectID(), c.ID),
-			fmt.Sprintf("/p/%s/g/%s", c.ProjectID(), c.ID),
-			fmt.Sprintf("builder group %s in project %s", c.ID, c.ProjectID()))
-	}
-	sort.Slice(builder.Groups, func(i, j int) bool {
-		return builder.Groups[i].Label < builder.Groups[j].Label
-	})
-
-	for _, b := range builder.FinishedBuilds {
-		if len(b.Blame) > 0 {
-			builder.HasBlamelist = true
-			break
-		}
-	}
-
-	return builder, nil
 }
 
 // SelfLink returns LUCI URL of the builder.
 func (b BuilderID) SelfLink(project string) string {
 	return model.BuilderIDLink(string(b), project)
-}
-
-// Buildbot returns true iff this BuilderID originates from a buildbot builder.
-func (b BuilderID) Buildbot() bool {
-	return strings.HasPrefix(string(b), "buildbot/")
 }
