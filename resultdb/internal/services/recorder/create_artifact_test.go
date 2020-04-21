00e50964ec8485a0bea1fdac681227ae1fef2f98
Andrii Shyshkalov <tandrii@google.com>
2020-04-20 19:02:51 -0700

    cq: remove actual config values from luci_gae_vars.
    CQ GAE app automation is now defining this in infradata/gae repo.
    
    R=vadimsh
    
    Bug: 1070418
    Change-Id: Iba9b348cf678b3d576d8c31185d21c77e5e1becd

---
+++ resultdb/internal/services/recorder/create_artifact_test.go
@@ -0,0 +1,104 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package recorder
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+
+	"go.chromium.org/luci/server/router"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/resultdb/internal/testutil"
+)
+
+func TestCreateArtifact(t *testing.T) {
+	Convey(`CreateArtifact`, t, func() {
+		ctx := SpannerTestContext(t)
+
+		send := func(artifact, hash string, size int64, updateToken string) *httptest.ResponseRecorder {
+			rec := httptest.NewRecorder()
+			c := &router.Context{
+				Context: ctx,
+				Params:  httprouter.Params{{Key: "artifact", Value: artifact}},
+				Request: &http.Request{Header: http.Header{}},
+				Writer:  rec,
+			}
+			if hash != "" {
+				c.Request.Header.Set(artifactContentHashHeaderKey, hash)
+			}
+			if size != -1 {
+				c.Request.Header.Set(artifactContentSizeHeaderKey, strconv.FormatInt(size, 10))
+			}
+			if updateToken != "" {
+				c.Request.Header.Set(UpdateTokenMetadataKey, updateToken)
+			}
+			handleArtifactCreation(c)
+			return rec
+		}
+
+		Convey(`Artifact name is malformed`, func() {
+			rec := send("artifact", "", 0, "")
+			So(rec.Code, ShouldEqual, http.StatusBadRequest)
+			So(rec.Body.String(), ShouldContainSubstring, "bad artifact name: does not match")
+		})
+
+		Convey(`Hash is missing`, func() {
+			rec := send("invocations/inv/artifacts/a", "", 0, "")
+			So(rec.Code, ShouldEqual, http.StatusBadRequest)
+			So(rec.Body.String(), ShouldContainSubstring, "Content-Hash header is missing")
+		})
+
+		Convey(`Hash is malformed`, func() {
+			rec := send("invocations/inv/artifacts/a", "a", 0, "")
+			So(rec.Code, ShouldEqual, http.StatusBadRequest)
+			So(rec.Body.String(), ShouldContainSubstring, "Content-Hash header value does not match")
+		})
+
+		Convey(`Size is missing`, func() {
+			rec := send("invocations/inv/artifacts/a", "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", -1, "")
+			So(rec.Code, ShouldEqual, http.StatusBadRequest)
+			So(rec.Body.String(), ShouldContainSubstring, "Content-Length header is missing")
+		})
+
+		Convey(`Size is negative`, func() {
+			rec := send("invocations/inv/artifacts/a", "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", -100, "")
+			So(rec.Code, ShouldEqual, http.StatusBadRequest)
+			So(rec.Body.String(), ShouldContainSubstring, "Content-Length header must be a value between 0 and 68719476736")
+		})
+
+		Convey(`Size is too large`, func() {
+			rec := send("invocations/inv/artifacts/a", "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", 1<<40, "")
+			So(rec.Code, ShouldEqual, http.StatusBadRequest)
+			So(rec.Body.String(), ShouldContainSubstring, "Content-Length header must be a value between 0 and 68719476736")
+		})
+
+		Convey(`Update token is missing`, func() {
+			rec := send("invocations/inv/artifacts/a", "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", 0, "")
+			So(rec.Code, ShouldEqual, http.StatusUnauthorized)
+			So(rec.Body.String(), ShouldContainSubstring, "Update-Token header is missing")
+		})
+
+		Convey(`Update token is invalid`, func() {
+			rec := send("invocations/inv/artifacts/a", "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", 0, "x")
+			So(rec.Code, ShouldEqual, http.StatusForbidden)
+			So(rec.Body.String(), ShouldContainSubstring, "invalid Update-Token header value")
+		})
+	})
+}
