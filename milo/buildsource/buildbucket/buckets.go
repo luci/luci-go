c2f0faf1269a43f382214a15f9f56a0e75ca2603
Joey Scarr <jsca@google.com>
2020-04-21 16:24:37 +1000

    [milo] Compress builder cache values using zlib.
    The responses were getting larger than 1MB and being rejected by
    memcache.
    
    On dev, this brings the load time of /search from ~4.5s down to ~1s.
    
    Bug: 1071316
    Change-Id: I1fc31092d3c689ee0a8d02e2ef50873f539d7a81

--- milo/buildsource/buildbucket/buckets.go
+++ milo/buildsource/buildbucket/buckets.go
@@ -15,10 +15,13 @@
 package buildbucket
 
 import (
+	"bytes"
+	"compress/zlib"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"strings"
 	"time"
 
@@ -41,14 +44,15 @@ func GetBuilders(c context.Context) (*swarmbucket.LegacySwarmbucketApiGetBuilder
 }
 
 func getBuilders(c context.Context, host string) (*swarmbucket.LegacySwarmbucketApiGetBuildersResponseMessage, error) {
-	mc := memcache.NewItem(c, fmt.Sprintf("swarmbucket-builders-%q-%q", host, auth.CurrentIdentity(c)))
+	key := fmt.Sprintf("swarmbucket-builders-%q-%q", host, auth.CurrentIdentity(c))
+	mc := memcache.NewItem(c, key)
 	switch err := memcache.Get(c, mc); {
 	case err == memcache.ErrCacheMiss:
 	case err != nil:
 		logging.WithError(err).Warningf(c, "memcache.get failed while loading swarmbucket builders")
 	default:
 		var res swarmbucket.LegacySwarmbucketApiGetBuildersResponseMessage
-		if err := json.Unmarshal(mc.Value(), &res); err != nil {
+		if err := deserializeBuildersResponse(mc.Value(), &res); err != nil {
 			logging.WithError(err).Warningf(c, "corrupted swarmbucket builders cache")
 		} else {
 			return &res, nil
@@ -65,18 +69,66 @@ func getBuilders(c context.Context, host string) (*swarmbucket.LegacySwarmbucket
 		return nil, err
 	}
 
-	if data, err := json.Marshal(res); err == nil {
+	if data, err := serializeBuildersResponse(res); err == nil {
 		// Large expiration is unfortunate as this slows down propagation of ACL
 		// changes limiting builder's visibility.
 		// TODO(crbug/1071316): switch to faster V2 Buildbucket API once available
 		// and reduce expiration to 10 minutes.
 		mc.SetValue(data).SetExpiration(12 * time.Hour)
 		if err := memcache.Set(c, mc); err != nil {
-			logging.WithError(err).Warningf(c, "failed to cache swarmbucket builders")
+			logging.WithError(err).Warningf(c, "failed to cache swarmbucket builder list of size %d with key %q", len(data), key)
 		}
 	}
 
 	return res, nil
+}
+
+// serializeBuildersResponse converts a response message into compressed JSON form, suitable for storing in memcache.
+func serializeBuildersResponse(message *swarmbucket.LegacySwarmbucketApiGetBuildersResponseMessage) ([]byte, error) {
+	// Convert to JSON.
+	data, err := json.Marshal(message);
+	if err != nil {
+		return nil, err
+	}
+
+	// Compress using zlib.
+	var b bytes.Buffer
+	w := zlib.NewWriter(&b)
+	_, err = w.Write(data)
+	if err != nil {
+		return nil, err
+	}
+	err = w.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+// deserializeBuildersResponse converts a response message back from compressed JSON form.
+func deserializeBuildersResponse(serialized []byte, res *swarmbucket.LegacySwarmbucketApiGetBuildersResponseMessage) error {
+	// Decompress using zlib.
+	r, err := zlib.NewReader(bytes.NewReader(serialized))
+	if err != nil {
+		return err
+	}
+	var jsonMessage []byte
+	jsonMessage, err = ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	err = r.Close()
+	if err != nil {
+		return err
+	}
+
+	// Convert from JSON.
+	err = json.Unmarshal(jsonMessage, &res);
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // CIService returns a *ui.CIService containing all known buckets and builders.
