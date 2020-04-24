2e88d7a9b41b5aa18c344319ac7598cb0b45ca1d
Nodir Turakulov <nodir@chromium.org>
2020-04-24 10:36:56 -0700

    [resultdb] Serve artifact contents
    Change-Id: I4d4d90b61df524c3d89184244d7f309af310fc3b

--- resultdb/internal/services/resultdb/main_test.go
+++ resultdb/internal/services/resultdb/main_test.go
@@ -16,7 +16,6 @@ package resultdb
 
 import (
 	"context"
-	"fmt"
 	"net/url"
 	"testing"
 	"time"
@@ -31,11 +30,11 @@ func TestMain(m *testing.M) {
 
 func newTestResultDBService() *resultDBServer {
 	return &resultDBServer{
-		generateIsolateURL: func(ctx context.Context, host, ns, digest string) (u *url.URL, expiration time.Time, err error) {
+		generateArtifactURL: func(ctx context.Context, artifactName string) (u *url.URL, expiration time.Time, err error) {
 			u = &url.URL{
 				Scheme: "http",
 				Host:   "results.usercontent.example.com",
-				Path:   fmt.Sprintf("/isolate/%s/%s/%s", host, ns, digest),
+				Path:   "/" + artifactName,
 			}
 			expiration = clock.Now(ctx).UTC().Add(time.Hour)
 			return
