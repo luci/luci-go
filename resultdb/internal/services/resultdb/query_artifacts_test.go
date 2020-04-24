0d98c472e919451cca9d8519caeb660a4ee125fb
Nodir Turakulov <nodir@chromium.org>
2020-04-24 16:17:23 -0700

    [resultdb] QueryArtifacts: test id regexp
    Implement test result artifact filtering by test id regexp.
    Do not necessarily join with TestResults table because it is not
    required.
    
    Bug: 1071258
    Change-Id: Iabb0df5863b91c733fde864718a53040c78e7a3c

--- resultdb/internal/services/resultdb/query_artifacts_test.go
+++ resultdb/internal/services/resultdb/query_artifacts_test.go
@@ -122,6 +122,23 @@ func TestQueryArtifacts(t *testing.T) {
 			So(actual, ShouldResemble, []string{"invocations/inv1/artifacts/a"})
 		})
 
+		Convey(`Test ID regexp`, func() {
+			MustApply(ctx,
+				InsertInvocationArtifact("inv1", "a", nil),
+				InsertTestResultArtifact("inv1", "t00", "r", "a", nil),
+				InsertTestResultArtifact("inv1", "t10", "r", "a", nil),
+				InsertTestResultArtifact("inv1", "t11", "r", "a", nil),
+				InsertTestResultArtifact("inv1", "t20", "r", "a", nil),
+			)
+			req.TestResultPredicate.TestIdRegexp = "t1.+"
+			actual := mustQueryNames(req)
+			So(actual, ShouldResemble, []string{
+				"invocations/inv1/artifacts/a",
+				"invocations/inv1/tests/t10/results/r/artifacts/a",
+				"invocations/inv1/tests/t11/results/r/artifacts/a",
+			})
+		})
+
 		Convey(`Paging`, func() {
 			MustApply(ctx,
 				InsertInvocationArtifact("inv1", "a0", nil),
