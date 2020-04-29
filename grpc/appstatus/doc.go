7b2b7a0d28ebc5941acbc0ad51972c223c767016
Nodir Turakulov <nodir@chromium.org>
2020-04-29 10:32:52 -0700

    Expand appstatus godoc
    Add examples and reasoning.
    
    Change-Id: If446d17653b0612dc3e7c17686eec7e7bdedb58e

--- grpc/appstatus/doc.go
+++ grpc/appstatus/doc.go
@@ -15,4 +15,48 @@
 // Package appstatus can attach/reterieve an application-specific response
 // status to/from an error. It designed to prevent accidental exposure of
 // internal statuses to RPC clients, for example Spanner's statuses.
+//
+// Attaching a status
+//
+// Use ToError, Error and Errorf to create new status-annotated errors.
+// Use Attach and Attachf to annotate existing errors with a status.
+//
+//   if req.PageSize < 0  {
+//      return appstatus.Errorf(codes.InvalidArgument, "page size cannot be negative")
+//   }
+//   if err := checkState(); err != nil {
+//     return appstatus.Attachf(err, codes.PreconditionFailed, "invalid state")
+//   }
+//
+// This may be done deep in the function call hierarchy.
+//
+// Do not use appstatus in code where you don't explicitly intend to return a
+// specific status. This is unnecessary because any unrecognized error is
+// treated as internal and because it is explicitly prohibited to attach a
+// status to an error chain multiple times. When a status is attached, the
+// package supports its propagation all the way to the requester
+// unless there is code that explicitly throws it away.
+//
+// Returning a status
+//
+// Use GRPCifyAndLog right before returning the error from a gRPC method
+// handler. Usually it is done in a Postlude of a service decorator, see
+// ../cmd/svcdec.
+//
+//   func NewMyServer() pb.MyServer {
+//     return &pb.DecoratedMyServer{
+//       Service:  &actualImpl{},
+//       Postlude: func(ctx context.Context, methodName string, rsp proto.Message, err error) error {
+//         return appstatus.GRPCifyAndLog(ctx, err)
+//       },
+//     }
+//   }
+//
+// It recognizes only appstatus-annotated errors and treats any other error as
+// internal. This behavior is important to avoid accidentally returning errors
+// from Spanner or other client library that also uses grpc/status package to
+// communicate status code from *other* services. For example, if a there is a
+// typo in Spanner SQL statement, spanner package may return a status-annotated
+// errors with code NotFound. In this case, our RPC must respond with internal
+// error and not NotFound.
 package appstatus
