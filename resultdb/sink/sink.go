7547672ba1ca47486a1b3d46fcce1a1703695c48
Scott Lee <ddoman@chromium.org>
2020-04-30 09:24:29 +0000

    [resultdb] Run is a global function under package sink
    The intent of this change is to protect Run from misuse of other functions
    under sink. If sink.Run() is used, there is no need to invoke Start(), Close(),
    Shutdown(), Done(), and Err().
    
    If a user of sink.Run wants to terminate Run in the middle of the execution,
    the user should just send a signal to the callback to voluntarily finish.
    
    Change-Id: I51789ccc7e94caee1943bc9059a9d3dc94b2d6d9
    Bug:1017288
    R=nodir@chromium.org

--- resultdb/sink/sink.go
+++ resultdb/sink/sink.go
@@ -74,6 +74,9 @@ type ServerConfig struct {
 
 	// TestIDPrefix will be prepended to the test_id of each TestResult.
 	TestIDPrefix string
+
+	// Listener for tests
+	testListener net.Listener
 }
 
 func (c *ServerConfig) Validate() error {
@@ -101,9 +104,6 @@ type Server struct {
 
 	// 1 indicates that the server is starting or has started. 0, otherwise.
 	started int32
-
-	// Listener for tests
-	testListener net.Listener
 
 	mu  sync.Mutex // protects err
 	err error
@@ -159,35 +159,39 @@ func (s *Server) Err() error {
 	return s.err
 }
 
-// Run invokes callback in a context where the server is running.
+// Run starts a server and run callback in a context where the server is running.
 //
 // The context passed to callback will be cancelled if the server has stopped due to
 // critical errors or Close being invoked. The context also has the server's information
-// exported into it. If callback finishes, Run will return the error callback returned.
-func (s *Server) Run(ctx context.Context, callback func(context.Context) error) (err error) {
-	// TODO(ddoman): make Run as a global function under package sink
-	if err := s.Start(ctx); err != nil {
+// exported into it. If callback finishes, Run will stop the server and return the error
+// callback returned.
+func Run(srvCtx context.Context, cfg ServerConfig, callback func(context.Context, ServerConfig) error) (err error) {
+	s, err := NewServer(srvCtx, cfg)
+	if err != nil {
+		return err
+	}
+	if err := s.Start(srvCtx); err != nil {
 		return err
 	}
 	defer func() {
 		// Run returns nil IFF both of the callback and Shutdown returned nil.
-		sErr := s.Shutdown(ctx)
+		sErr := s.Shutdown(srvCtx)
 		if err == nil {
 			err = sErr
 		}
 	}()
 
-	ctx, cancel := context.WithCancel(s.Export(ctx))
+	cbCtx, cancel := context.WithCancel(s.Export(srvCtx))
 	defer cancel()
 	go func() {
 		select {
 		case <-s.Done():
 			// cancel the context if the server terminates before callback finishes.
 			cancel()
-		case <-ctx.Done():
+		case <-srvCtx.Done():
 		}
 	}()
-	return callback(ctx)
+	return callback(cbCtx, cfg)
 }
 
 // Start runs the server.
@@ -222,12 +226,12 @@ func (s *Server) Start(ctx context.Context) error {
 		}()
 
 		var err error
-		if s.testListener == nil {
+		if s.cfg.testListener == nil {
 			// err will be written to s.err after the channel fully drained.
 			err = s.httpSrv.ListenAndServe()
 		} else {
 			// In test mode, the the listener MUST be prepared already.
-			err = s.httpSrv.Serve(s.testListener)
+			err = s.httpSrv.Serve(s.cfg.testListener)
 		}
 
 		// if the reason of the server stopped was due to s.Close or s.Shutdown invoked,
