2894eee45749d03ba34df6d29da7b2465e3b9dbe
Nodir Turakulov <nodir@chromium.org>
2020-05-04 19:45:19 -0700

    [resultdb] Implement ListArtifacts RPC
    This a complete implementation. It reuses span.QueryArtifacts.
    
    Bug: 1071258
    Change-Id: I9c065d614f286c93ad0f45306994962f7e4efc67

---
+++ resultdb/internal/services/resultdb/list_artifacts.go
@@ -0,0 +1,78 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resultdb
+
+import (
+	"context"
+	"regexp"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/grpc/appstatus"
+
+	"go.chromium.org/luci/resultdb/internal/pagination"
+	"go.chromium.org/luci/resultdb/internal/span"
+	"go.chromium.org/luci/resultdb/pbutil"
+	pb "go.chromium.org/luci/resultdb/proto/rpc/v1"
+)
+
+func validateListArtifactsRequest(req *pb.ListArtifactsRequest) error {
+	if pbutil.ValidateInvocationName(req.Parent) != nil && pbutil.ValidateTestResultName(req.Parent) != nil {
+		return errors.Reason("parent: neither valid invocation name nor valid test result name").Err()
+	}
+
+	if err := pagination.ValidatePageSize(req.GetPageSize()); err != nil {
+		return errors.Annotate(err, "page_size").Err()
+	}
+
+	return nil
+}
+
+// ListArtifacts implements pb.ResultDBServer.
+func (s *resultDBServer) ListArtifacts(ctx context.Context, in *pb.ListArtifactsRequest) (*pb.ListArtifactsResponse, error) {
+	if err := validateListArtifactsRequest(in); err != nil {
+		return nil, appstatus.BadRequest(err)
+	}
+
+	// Prepare the query.
+	q := span.ArtifactQuery{
+		PageSize:  pagination.AdjustPageSize(in.PageSize),
+		PageToken: in.PageToken,
+	}
+	if invIDStr, err := pbutil.ParseInvocationName(in.Parent); err == nil {
+		q.InvocationIDs = span.NewInvocationIDSet(span.InvocationID(invIDStr))
+		// Fetch only invocation-level artifacts. They have empty ParentId.
+		q.ParentIDRegexp = "^$"
+	} else {
+		// in.Parent must be a test result name.
+		invID, testID, resultID := span.MustParseTestResultName(in.Parent)
+		q.InvocationIDs = span.NewInvocationIDSet(invID)
+		q.ParentIDRegexp = regexp.QuoteMeta(span.ArtifactParentID(testID, resultID))
+	}
+
+	// Read artifacts.
+	arts, token, err := q.Fetch(ctx, span.Client(ctx).Single())
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.populateFetchURLs(ctx, arts...); err != nil {
+		return nil, err
+	}
+
+	return &pb.ListArtifactsResponse{
+		Artifacts:     arts,
+		NextPageToken: token,
+	}, nil
+}
