571002639546784b060cc00306e860dd39ef0f94
Weiwei Lin <weiweilin@google.com>
2020-05-06 16:52:44 +1000

    [resultui] serve ResultDB host string from the server
    1. inject settings with a js file generated by the server.
    2. add handler to webpack-dev-server to support local development.
    
    Alternatives:
    1. load settings through HTTP request
      * slower first paint
    2. inject settings into bundle at build time
      * requires a new build/deployment everytime the setting is changed.
    3. inject settings into HTML data attribute
      * a bit more complicated to support local development.
    
    
    Bug: 1073321
    Change-Id: I6a872faca52ba082ab1d39a949af42135cb6a5fd

--- milo/frontend/resultui/main.go
+++ milo/frontend/resultui/main.go
@@ -15,22 +15,55 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 
 	"google.golang.org/appengine"
 
 	"go.chromium.org/luci/appengine/gaemiddleware/standard"
+	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/server/router"
 )
 
 func main() {
-	// Install only default LUCI routes (admin routes and some auth stuff).
-	//
 	// This tiny server is needed for two reasons:
 	//   1. To serve /auth/api/v1/server/client_id for the frontend.
-	//   2. GAE modules can't have static files only, they need some Go code.
+	//   2. Provide configs.js, which includes dynamically injected configs.
 	r := router.New()
 	standard.InstallHandlers(r)
+
+	baseMW := standard.Base()
+	r.GET("/configs.js", baseMW, handleError(configsHandler))
 	http.DefaultServeMux.Handle("/", r)
 	appengine.Main()
+}
+
+type configs struct {
+	ResultDB resultDBConfigs
+}
+
+type resultDBConfigs struct {
+	Host string
+}
+
+func configsHandler(c *router.Context) error {
+	template, err := template.ParseFiles("configs.template.js")
+	if err != nil {
+		return err
+	}
+	c.Writer.Header().Set("content-type", "application/javascript")
+	// TODO(weiweilin): read the host name from luci-config.
+	return template.Execute(c.Writer, configs{resultDBConfigs{"staging.results.api.cr.dev"}})
+}
+
+// handleError is a wrapper for a handler so that the handler can return an
+// error.
+// If the wrapped handler returns an error, log the error and respond 500.
+func handleError(handler func(c *router.Context) error) func(c *router.Context) {
+	return func(c *router.Context) {
+		if err := handler(c); err != nil {
+			errors.Log(c.Context, err)
+			http.Error(c.Writer, "unexpected error", 500)
+		}
+	}
 }
