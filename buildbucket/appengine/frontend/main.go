c304a9a6e8140047b39bc72aa7d6adb06baa8103
smut <smut@google.com>
2020-05-05 17:29:29 -0700

    [buildbucket] Proxy GetBuild requests to the Python service
    Since dispatch.yaml is global configuration, splitting a portion of
    traffic over to the Go service is impossible. Instead, whenever the Go
    service receives a request, proxy requests to the Python service and
    return its response instead. This allows switching over fully to this
    service safely. In subsequent CLs, this code will be modified to allow a
    portion of requests through to the Go service, achieving a programmatic
    traffic split. Once 100% of traffic is served by Go, remove the proxy.
    
    Bug: 1042991
    Change-Id: I00dffab6322d4921f6e66cc85a55983364475cef

--- buildbucket/appengine/frontend/main.go
+++ buildbucket/appengine/frontend/main.go
@@ -16,10 +16,17 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/proto/access"
 	"go.chromium.org/luci/server"
 	"go.chromium.org/luci/server/gaeemulation"
 	"go.chromium.org/luci/server/module"
+	"go.chromium.org/luci/server/router"
 
 	"go.chromium.org/luci/buildbucket/appengine/rpc"
 	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
@@ -31,8 +38,34 @@ func main() {
 	}
 
 	server.Main(nil, mods, func(srv *server.Server) error {
+		// Proxy buildbucket.v2.Builds pRPC requests back to the Python
+		// service in order to achieve a programmatic traffic split.
+		// Because of the way dispatch routes work, requests are proxied
+		// to a copy of the Python service hosted at a different path.
+		// TODO(crbug/1042991): Remove the proxy once the go service handles all traffic.
+		pythonURL, err := url.Parse(fmt.Sprintf("https://default-dot-%s.appspot.com/python", srv.Options.CloudProject))
+		if err != nil {
+			panic(err)
+		}
+		prx := httputil.NewSingleHostReverseProxy(pythonURL)
+		prx.Director = func(req *http.Request) {
+			// According to net.Request documentation, setting Host is unnecessary
+			// because URL.Host is supposed to be used for outbound requests.
+			// However, on GAE, it seems that req.Host is incorrectly used.
+			req.Host = pythonURL.Host
+			req.URL.Scheme = pythonURL.Scheme
+			req.URL.Host = pythonURL.Host
+			req.URL.Path = fmt.Sprintf("%s%s", pythonURL.Path, req.URL.Path)
+		}
+
 		access.RegisterAccessServer(srv.PRPC, &access.UnimplementedAccessServer{})
 		buildbucketpb.RegisterBuildsServer(srv.PRPC, rpc.New())
+		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "GetBuild", func(ctx *router.Context) bool {
+			logging.Debugf(ctx.Context, "proxying request to %s", pythonURL)
+			prx.ServeHTTP(ctx.Writer, ctx.Request)
+			// TODO(crbug/1042991): Split some portion of traffic to the Go service.
+			return false
+		})
 		return nil
 	})
 }
