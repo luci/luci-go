8f0ba206e9e59a56ab245e572de77affb00c4ab8
Robert Iannucci <iannucci@chromium.org>
2020-05-16 16:53:00 -0700

    [luciexe] Some refinements to the protocol before launch.
    R=yiwzhang@google.com
    
    Bug: 1015181
    Change-Id: I9080673f189c1f31edd96ae2070f7d5c93cb776c

--- led/job/jobcreate/create.go
+++ led/job/jobcreate/create.go
@@ -17,6 +17,7 @@ package jobcreate
 import (
 	"context"
 	"path"
+	"strings"
 
 	"go.chromium.org/luci/buildbucket/cmd/bbagent/bbinput"
 	swarming "go.chromium.org/luci/common/api/swarming/swarming/v1"
@@ -106,7 +107,15 @@ func FromNewTaskRequest(ctx context.Context, r *swarming.SwarmingRpcsNewTaskRequ
 
 		// drop the executable package; it's canonically represented by
 		// out.BBAgentArgs.Build.Exe.
-		dropRecipePackage(&bb.CipdPackages, path.Dir(bb.BbagentArgs.ExecutablePath))
+		if exePath := bb.BbagentArgs.ExecutablePath; exePath != "" {
+			// convert to new mode
+			payload, arg := path.Split(exePath)
+			bb.BbagentArgs.ExecutablePath = ""
+			bb.BbagentArgs.PayloadPath = strings.TrimSuffix(payload, "/")
+			bb.BbagentArgs.Build.Exe.Cmd = []string{arg}
+		}
+
+		dropRecipePackage(&bb.CipdPackages, bb.BbagentArgs.PayloadPath)
 
 		// drop legacy recipe fields
 		if recipe := bb.BbagentArgs.Build.Infra.Recipe; recipe != nil {
