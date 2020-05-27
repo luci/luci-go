80bbfbea71f116474a602cfb75323551a917cefe
Owen Rodley <orodley@chromium.org>
2020-05-27 16:11:25 +1000

    Return all failing steps if there is no filter.
    We still want to be able to display which steps failed even if we don't
    filter to any particular set of step names.
    
    Bug: 1051371
    Change-Id: I47c959b239a355b9781c1416f4375d949de4a1e9

--- luci_notify/notify/notify.go
+++ luci_notify/notify/notify.go
@@ -311,11 +311,6 @@ func matchingSteps(build *buildbucketpb.Build, failedStepRegexp, failedStepRegex
 		excludeRegex = regexp.MustCompile(fmt.Sprintf("^%s$", failedStepRegexpExclude))
 	}
 
-	// Don't scan steps if we have no regexes.
-	if includeRegex == nil && excludeRegex == nil {
-		return true, nil
-	}
-
 	var steps []*buildbucketpb.Step
 	for _, step := range build.Steps {
 		if step.Status == buildbucketpb.Status_FAILURE {
@@ -326,7 +321,9 @@ func matchingSteps(build *buildbucketpb.Build, failedStepRegexp, failedStepRegex
 		}
 	}
 
-	if len(steps) > 0 {
+	// If there are no regex filters, we return true regardless of whether any
+	// steps matched.
+	if len(steps) > 0 || (includeRegex == nil && excludeRegex == nil) {
 		return true, steps
 	}
 	return false, nil
