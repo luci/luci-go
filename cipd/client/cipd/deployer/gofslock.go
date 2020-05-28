e356851cacb2db9a2ebe33cf2e3eb44bdd41358b
Vadim Shtayura <vadimsh@chromium.org>
2020-05-27 18:33:52 -0700

    [auth] Make NewLUCITSTokenProvider optional.
    It (and its dependencies) can be excluded from the build by
    excluding internal/luci_ts.go file (e.g. via a build tag or by
    not exporting it in the first place).
    
    Add `copybara` build tag to exclude such files from `go list` output.
    Mark few existing excluded files with that tag as well.
    
    R=tandrii@chromium.org, yekuang@google.com
    
    Change-Id: I9d724cc9482e84d8b47361c67357add8c6b8ac10

--- cipd/client/cipd/deployer/gofslock.go
+++ cipd/client/cipd/deployer/gofslock.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build !copybara
+
 package deployer
 
 // Note: FS locking functionality is moved into a separate file for easier
