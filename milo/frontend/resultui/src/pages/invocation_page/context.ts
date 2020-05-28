c74968c361863cc4e28e29b495fda8619b575de1
Weiwei Lin <weiweilin@google.com>
2020-05-28 23:21:21 +1000

    [resultui] cache iterators to avoid re-fetching data

--- milo/frontend/resultui/src/pages/invocation_page/context.ts
+++ milo/frontend/resultui/src/pages/invocation_page/context.ts
@@ -20,7 +20,7 @@ import { AppState } from '../../context/app_state_provider';
 import { consumeContext, provideContext } from '../../libs/context';
 import * as iter from '../../libs/iter_utils';
 import { streamTestBatches, streamTestExonerationBatches, streamTestResultBatches, streamVariantBatches, TestLoader } from '../../models/test_loader';
-import { ReadonlyTest, TestNode, VariantStatus } from '../../models/test_node';
+import { TestNode, VariantStatus } from '../../models/test_node';
 import { Expectancy, Invocation } from '../../services/resultdb';
 
 /**
@@ -57,31 +57,45 @@ export class InvocationPageState {
   @observable.ref showExpected = false;
   @observable.ref showExonerated = true;
 
-  @computed
-  private get testIter(): AsyncIterableIterator<ReadonlyTest[]> {
+  @computed private get testResultBatchIterFn() {
     if (!this.appState?.resultDb) {
-      return (async function*() {})();
+      return async function*() {};
     }
-    let variantBatches = streamVariantBatches(
-      streamTestResultBatches(
-        {
-          invocations: [this.invocationName],
-          predicate: {
-            expectancy: this.showExpected ? Expectancy.All : Expectancy.VariantsWithUnexpectedResults,
-          },
+    return iter.teeAsync(streamTestResultBatches(
+      {
+        invocations: [this.invocationName],
+        predicate: {
+          expectancy: this.showExpected ? Expectancy.All : Expectancy.VariantsWithUnexpectedResults,
         },
-        this.appState.resultDb,
-      ),
-      streamTestExonerationBatches({invocations: [this.invocationName]}, this.appState.resultDb),
+      },
+      this.appState.resultDb,
+    ));
+  }
+
+  @computed private get testExonerationBatchIterFn() {
+    if (!this.appState?.resultDb) {
+      return async function*() {};
+    }
+    return iter.teeAsync(streamTestExonerationBatches(
+      {invocations: [this.invocationName]},
+      this.appState.resultDb,
+    ));
+  }
+
+  @computed
+  private get testIterFn() {
+    let variantBatches = streamVariantBatches(
+      this.testResultBatchIterFn(),
+      this.testExonerationBatchIterFn(),
     );
     variantBatches = this.showExonerated ?
       variantBatches :
       iter.mapAsync(variantBatches, (batch) => batch.filter((v) => v.status !== VariantStatus.Exonerated));
-    return streamTestBatches(variantBatches);
+    return iter.teeAsync(streamTestBatches(variantBatches));
   }
 
   @computed({keepAlive: true})
-  get testLoader() { return new TestLoader(TestNode.newRoot(), this.testIter); }
+  get testLoader() { return new TestLoader(TestNode.newRoot(), this.testIterFn()); }
 }
 
 export const consumePageState = consumeContext<'pageState', InvocationPageState>('pageState');
