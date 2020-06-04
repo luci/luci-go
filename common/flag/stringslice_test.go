79a2fdc6e8ae93cdbefef8b64bd7c8e5549b7207
Ye Kuang <yekuang@google.com>
2020-06-04 15:34:42 +0900

    Add package comment
    Bug: 1086813
    Change-Id: Idf0e81ad2e63a46da0d0f82099a1a018277704a4

--- common/flag/stringslice_test.go
+++ common/flag/stringslice_test.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package flag provides the utilities for implementing command line flags.
 package flag
 
 import (
