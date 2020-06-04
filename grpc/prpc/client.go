6b2d2fa8ba21fc36f123e63433b28907bc80d72f
Chan Li <chanli@chromium.org>
2020-06-04 00:00:36 -0700

    [prpc] Use req.GetBody to get body
    This is a follow-up of change https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2229766
    
    Bug: 1081225
    Change-Id: I8b64c9985831f9e426b371337c03432d6ed0834d

--- grpc/prpc/client.go
+++ grpc/prpc/client.go
@@ -259,10 +259,10 @@ func (c *Client) call(ctx context.Context, serviceName, methodName string, in []
 			}
 
 			// Send the request.
-			req.Body = ioutil.NopCloser(bytes.NewReader(in))
 			req.GetBody = func() (io.ReadCloser, error) {
 				return ioutil.NopCloser(bytes.NewReader(in)), nil
 			}
+			req.Body, _ = req.GetBody()
 			res, err := ctxhttp.Do(ctx, c.getHTTPClient(), req)
 			if res != nil && res.Body != nil {
 				defer res.Body.Close()
