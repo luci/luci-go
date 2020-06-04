83dd60f245163a428147f637c99266f7d251a22b
Ye Kuang <yekuang@google.com>
2020-06-04 07:06:01 +0000

    Revert "Add package comment"
    This reverts commit 108ecc540aaf945f9ad5baf9d877f68c4b87276f.
    
    Reason for revert: should add a comment in only one of the files
    
    Original change's description:
    > Add package comment
    > 
    > Bug: 1086813
    > Change-Id: Idf0e81ad2e63a46da0d0f82099a1a018277704a4
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2229905
    > Reviewed-by: Junji Watanabe <jwata@google.com>
    > Commit-Queue: Ye Kuang <yekuang@google.com>
    
    TBR=tikuta@chromium.org,jwata@google.com,yekuang@google.com,infra-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: Iafff7072ebb13108783fe6dfe00dc6b9daafc483
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1086813
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2230398
    Reviewed-by: Ye Kuang <yekuang@google.com>
    Commit-Queue: Ye Kuang <yekuang@google.com>

--- common/flag/time_test.go
+++ common/flag/time_test.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package flag provides the utilities for implementing command line flags.
 package flag_test
 
 import (
