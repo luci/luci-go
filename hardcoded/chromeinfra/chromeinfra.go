76b426832881c780579280155cf9044944407a75
Ye Kuang <yekuang@google.com>
2020-06-04 09:41:19 +0000

    Replace homedir.Dir() with os.UserHomeDir()
    Bug: 1086813
    Change-Id: Ide429a2defd80e617f2639d006a9d4c7cf9dff2e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2230401
    Commit-Queue: Ye Kuang <yekuang@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Ye Kuang <yekuang@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>

--- hardcoded/chromeinfra/chromeinfra.go
+++ hardcoded/chromeinfra/chromeinfra.go
@@ -25,7 +25,6 @@ import (
 	"path/filepath"
 	"sync"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"go.chromium.org/luci/auth"
 )
 
@@ -111,7 +110,7 @@ var secrets struct {
 // cache).
 func SecretsDir() string {
 	secrets.once.Do(func() {
-		home, err := homedir.Dir()
+		home, err := os.UserHomeDir()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Can't resolve $HOME: %s", err)
 		} else {
