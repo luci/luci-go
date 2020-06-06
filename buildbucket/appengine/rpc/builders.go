d13f48e40ba303a2e309e59f9e887512ae82448c
Nodir Turakulov <nodir@chromium.org>
2020-06-06 15:51:40 -0700

    [buildbucket] Add unimplemented Builders service
    Hook up an unimplemented Builders service.
    
    Bug: 1091489
    Change-Id: Ie1e00a0826098e526c5ee654b01886b833694f31

---
+++ buildbucket/appengine/rpc/builders.go
@@ -0,0 +1,63 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"regexp"
+	"strings"
+
+	"go.chromium.org/luci/common/errors"
+
+	pb "go.chromium.org/luci/buildbucket/proto"
+)
+
+var (
+	projRegex    = regexp.MustCompile(`^[a-z0-9\-_]+$`)
+	bucketRegex  = regexp.MustCompile(`^[a-z0-9\-_.]{1,100}$`)
+	builderRegex = regexp.MustCompile(`^[a-zA-Z0-9\-_.\(\) ]{1,128}$`)
+)
+
+// validateBuilderID validates the given builder ID.
+// Bucket and Builder are optional and only validated if specified.
+func validateBuilderID(b *pb.BuilderID) error {
+	switch parts := strings.Split(b.GetBucket(), "."); {
+	case !projRegex.MatchString(b.GetProject()):
+		return errors.Reason("project must match %q", projRegex).Err()
+	case b.GetBucket() != "" && !bucketRegex.MatchString(b.Bucket):
+		return errors.Reason("bucket must match %q", bucketRegex).Err()
+	case b.GetBuilder() != "" && !builderRegex.MatchString(b.Builder):
+		return errors.Reason("builder must match %q", builderRegex).Err()
+	case b.GetBucket() != "" && parts[0] == "luci" && len(parts) > 2:
+		return errors.Reason("invalid use of v1 bucket in v2 API (hint: try %q)", parts[2]).Err()
+	default:
+		return nil
+	}
+}
+
+// Builders implements pb.BuildersServer.
+type Builders struct {
+	pb.UnimplementedBuildersServer
+}
+
+// Ensure Builds implements projects.BuildersServer.
+var _ pb.BuildersServer = &Builders{}
+
+// NewBuilders returns a new pb.BuildersServer.
+func NewBuilders() pb.BuildersServer {
+	return &pb.DecoratedBuilders{
+		Prelude: logDetails,
+		Service: &Builders{},
+	}
+}
