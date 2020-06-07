df0b37eb308d4239c9bade95cfc3bd08543283e6
Chan Li <chanli@chromium.org>
2020-06-07 04:48:18 +0000

    [ResultDB] Implement QueryTestResultStatistics RPC
    Bug:1034881
    Change-Id: I2415dec5d9dfcf4f573bf6e560a8fe7bff712719
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2214042
    Commit-Queue: Chan Li <chanli@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

---
+++ resultdb/internal/services/resultdb/query_test_result_statistics.go
@@ -0,0 +1,58 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resultdb
+
+import (
+	"context"
+
+	"cloud.google.com/go/spanner"
+	"github.com/golang/protobuf/ptypes"
+
+	"go.chromium.org/luci/grpc/appstatus"
+
+	"go.chromium.org/luci/resultdb/internal/invocations"
+	"go.chromium.org/luci/resultdb/internal/span"
+	pb "go.chromium.org/luci/resultdb/proto/rpc/v1"
+)
+
+// QueryTestResultStatistics implements pb.ResultDBServer.
+func (s *resultDBServer) QueryTestResultStatistics(ctx context.Context, in *pb.QueryTestResultStatisticsRequest) (*pb.QueryTestResultStatisticsResponse, error) {
+	if err := validateQueryRequest(in); err != nil {
+		return nil, appstatus.BadRequest(err)
+	}
+
+	// Open a transaction.
+	txn := span.Client(ctx).ReadOnlyTransaction()
+	defer txn.Close()
+	if in.MaxStaleness != nil {
+		st, _ := ptypes.Duration(in.MaxStaleness)
+		txn.WithTimestampBound(spanner.MaxStaleness(st))
+	}
+
+	// Get the transitive closure.
+	invs, err := invocations.Reachable(ctx, txn, maxInvocationGraphSize, invocations.MustParseNames(in.Invocations))
+	if err != nil {
+		return nil, err
+	}
+
+	totalNum, err := invocations.ReadTestResultCount(ctx, txn, invs)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.QueryTestResultStatisticsResponse{
+		TotalTestResults: totalNum,
+	}, nil
+}
