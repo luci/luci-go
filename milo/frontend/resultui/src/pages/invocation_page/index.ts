aa35210c29b0137bca95f3cc1a08ce149fddb6ba
Weiwei Lin <weiweilin@google.com>
2020-06-10 03:19:51 +0000

    [resultui] improve the error message when invocation is not found
    1. Pass source URL and reason HTML to the error page via search params.
    2. Renders a source URL and reasonHTML on the error page.
    
    
    Bug:1084860
    Change-Id: I7ad926583a7e3bcb0b240150ccecd276bc466b67
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2235438
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Joey Scarr <jsca@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>

--- milo/frontend/resultui/src/pages/invocation_page/index.ts
+++ milo/frontend/resultui/src/pages/invocation_page/index.ts
@@ -62,7 +62,16 @@ export class InvocationPageElement extends MobxLitElement implements BeforeEnter
     ));
     this.disposers.push(when(
       () => this.pageState.invocationReq.state === 'rejected',
-      () => Router.go(router.urlForName('error')),
+      () => {
+        const searchParams = new URLSearchParams({
+          reason: `${this.pageState.invocationReq.value}`,
+          sourceUrl: window.location.toString(),
+        });
+        Router.go({
+          pathname: router.urlForName('error'),
+          search: '?' + searchParams.toString(),
+        });
+      },
     ));
   }
   disconnectedCallback() {
