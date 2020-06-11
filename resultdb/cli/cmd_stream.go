5a583ba848780f71bbb8f60b3f7f4c9ad1893b3d
Scott Lee <ddoman@chromium.org>
2020-06-11 20:38:51 +0000

    [resultdb] login is optional in rdb coommand
    rdb authenticates incoming requests with update token instead of auth,
    unless it's to create an invocation.
    
    This CL makes login optional for rdb-stream unless it needs
    to create an invocation.
    
    Bug: 1084843
    Change-Id: Ib9be2b616dec6294eb5677108f779912c4d879d5
    R=chanli@chromium.org,nodir@chromium.org,vadimsh@chromium.org

--- resultdb/cli/cmd_stream.go
+++ resultdb/cli/cmd_stream.go
@@ -28,6 +28,7 @@ import (
 	"github.com/maruel/subcommands"
 	"google.golang.org/grpc/metadata"
 
+	"go.chromium.org/luci/auth"
 	"go.chromium.org/luci/common/cli"
 	"go.chromium.org/luci/common/data/rand/mathrand"
 	"go.chromium.org/luci/common/data/text"
@@ -111,9 +112,19 @@ func (r *streamRun) Run(a subcommands.Application, args []string, env subcommand
 	if err := r.validate(ctx, args); err != nil {
 		return r.done(err)
 	}
-	if err := r.initClients(ctx); err != nil {
+
+	loginMode := auth.OptionalLogin
+	// login is required only if it creates a new invocation.
+	if r.isNew {
+		loginMode = auth.SilentLogin
+	}
+	if err := r.initClients(ctx, loginMode); err != nil {
 		return r.done(err)
 	}
+
+	// login should be optional, unless it needs to create an invocation.
+	r.prpcClient.C = r.noLoginHttp
+	r.resultdb = pb.NewResultDBPRPCClient(r.prpcClient)
 
 	// if -new is passed, create a new invocation. If not, use the existing one set in
 	// lucictx.
