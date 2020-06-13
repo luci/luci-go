c270b401efaf478ce21fb08fc768a48eb3f25000
Scott Lee <ddoman@chromium.org>
2020-06-13 10:31:06 +0000

    [resultdb] add artifactChannel and rename trChan to testResultChannel
    (1) add artifact Channel
    It handles uploading tasks for artifacts. Detail implementations will be added
    in follow-up CLs.
    
    (2) rename trChan to testResultChannel
    Local var names with short abbreviations are preferred, because the vars
    are always references in the same place where they are used. However,
    if struct/functions are referenced in other files. even if it was private,
    more descriptive names seem better choices, otherwise the abbreviation
    used is well known.
    
    test_result_channel.go and testResultChannel{} look more intuitive than
    trchan.go and trChan{}.
    
    Bug:1087955
    Change-Id: Icbd76ad2e384988d0edeb87ce2cca20e738afb97

---
+++ resultdb/sink/artifact_channel.go
@@ -0,0 +1,75 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sink
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"golang.org/x/time/rate"
+
+	"go.chromium.org/luci/common/sync/dispatcher"
+	"go.chromium.org/luci/common/sync/dispatcher/buffer"
+)
+
+type artifactChannel struct {
+	ch  *dispatcher.Channel
+	cfg ServerConfig
+
+	// wgActive indicates if there are active goroutines invoking reportTestResults.
+	//
+	// reportTestResults can be invoked by multiple goroutines in parallel. wgActive is used
+	// to ensure that all active goroutines finish enqueuing messages to the channel before
+	// closeAndDrain closes and drains the channel.
+	wgActive sync.WaitGroup
+
+	// 1 indicates that artifactChannel started the process of closing and draining
+	// the channel. 0, otherwise.
+	closed int32
+}
+
+func (ac *artifactChannel) init(ctx context.Context) error {
+	chOpts := &dispatcher.Options{
+		QPSLimit: rate.NewLimiter(1, 1),
+		Buffer: buffer.Options{
+			BatchSize:     1,
+			MaxLeases:     4,
+			BatchDuration: 10 * time.Second,
+			FullBehavior:  &buffer.BlockNewItems{MaxItems: 2000},
+		},
+	}
+
+	ch, err := dispatcher.NewChannel(ctx, chOpts, func(b *buffer.Batch) error {
+		// TODO(crbug/1087955) - process
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	ac.ch = &ch
+	return nil
+}
+
+func (ac *artifactChannel) closeAndDrain(ctx context.Context) {
+	// annonuce that it is in the process of closeAndDrain.
+	if !atomic.CompareAndSwapInt32(&ac.closed, 0, 1) {
+		return
+	}
+	// wait for all the active sessions to finish enquing tests results to the channel
+	ac.wgActive.Wait()
+	ac.ch.CloseAndDrain(ctx)
+}
