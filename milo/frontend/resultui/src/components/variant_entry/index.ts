7a946e74a7175c5cb7b9c399bb0459d700951469
Weiwei Lin <weiweilin@google.com>
2020-06-15 23:51:39 +1000

    [resultui] flatten test variants
    Change-Id: Ic717efb5e082e15301d2220f4a85aec8aa9dbd54
    Bug:1085638

---
+++ milo/frontend/resultui/src/components/variant_entry/index.ts
@@ -0,0 +1,270 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { MobxLitElement } from '@adobe/lit-mobx';
+import '@material/mwc-icon';
+import { css, customElement, html } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
+import { repeat } from 'lit-html/directives/repeat';
+import { styleMap } from 'lit-html/directives/style-map';
+import takeWhile from 'lodash-es/takeWhile';
+import { computed, observable } from 'mobx';
+
+import { ID_SEG_REGEX, ReadonlyVariant, VariantStatus } from '../../models/test_node';
+import './exoneration_entry';
+import './result_entry';
+
+
+const STATUS_CLASS_MAP = {
+  [VariantStatus.Exonerated]: 'exonerated',
+  [VariantStatus.Expected]: 'expected',
+  [VariantStatus.Unexpected]: 'unexpected',
+  [VariantStatus.Flaky]: 'flaky',
+};
+
+const STATUS_ICON_MAP = Object.freeze({
+  // TODO(weiweilin): find an appropriate icon for exonerated
+  [VariantStatus.Exonerated]: 'check',
+  [VariantStatus.Expected]: 'check',
+  [VariantStatus.Flaky]: 'warning',
+  [VariantStatus.Unexpected]: 'error',
+});
+
+// This list defines the order in which variant def keys should be displayed.
+// Any unrecognized keys will be listed after the ones defined below.
+const ORDERED_VARIANT_DEF_KEYS = Object.freeze([
+  'bucket',
+  'builder',
+  'test_suite',
+]);
+
+/**
+ * Renders an expandable entry of the given test variant.
+ */
+@customElement('tr-variant-entry')
+export class VariantEntryElement extends MobxLitElement {
+  @observable.ref variant!: ReadonlyVariant;
+  @observable.ref prevTestId = '';
+
+  @observable.ref private _expanded = false;
+  @computed get expanded() {
+    return this._expanded;
+  }
+  set expanded(newVal: boolean) {
+    this._expanded = newVal;
+    this.wasExpanded = this.wasExpanded || newVal;
+  }
+
+  // Always render the children once it was expanded so the children's state
+  // don't get reset after the node is collapsed.
+  @observable.ref private wasExpanded = false;
+
+  @observable.ref private variantDefExpanded = false;
+
+  /**
+   * Common prefix between this.test.id and this.prevTestId.
+   */
+  @computed
+  private get commonTestIdPrefix() {
+    const prevSegs = this.prevTestId.match(ID_SEG_REGEX)!;
+    const currentSegs = this.variant.testId.match(ID_SEG_REGEX)!;
+    return takeWhile(prevSegs, (seg, i) => currentSegs[i] === seg).join('');
+  }
+
+  @computed
+  private get hasSingleChild() {
+    return (this.variant!.results.length + this.variant!.exonerations.length) === 1;
+  }
+
+  @computed
+  private get variantDef() {
+    const def = this.variant!.variant.def;
+    const res = [];
+    const seen = new Set();
+    for (const key of ORDERED_VARIANT_DEF_KEYS) {
+      if (def.hasOwnProperty(key)) {
+        res.push([key, def[key]]);
+        seen.add(key);
+      }
+    }
+    for (const [key, value] of Object.entries(def)) {
+      if (!seen.has(key)) {
+        res.push([key, value]);
+      }
+    }
+    return res;
+  }
+
+  private renderVariantDef() {
+    if (this.variantDef.length === 0) {
+      return html``;
+    }
+
+    return html`
+      <div
+        class="expandable-header"
+        @click=${() => this.variantDefExpanded = !this.variantDefExpanded}
+      >
+        <mwc-icon class="expand-toggle">${this.variantDefExpanded ? 'expand_more' : 'chevron_right'}</mwc-icon>
+        <span id="variant-def" class="one-line-content">
+          definition:
+          <span class="light" style=${styleMap({display: this.variantDefExpanded ? 'none': ''})}>
+            ${this.variantDef.map(([k, v]) => html`
+            <span class="kv-key">${k}</span>
+            <span class="kv-value">${v}</span>
+            `)}
+          </span>
+        </span>
+      </div>
+      <table id="def-table" border="0" style=${styleMap({display: this.variantDefExpanded ? '': 'none'})}>
+        ${this.variantDef.map(([k, v]) => html`
+        <tr>
+          <td>${k}:</td>
+          <td>${v}</td>
+        </tr>
+        `)}
+      </table>
+    `;
+  }
+
+  protected render() {
+    return html`
+      <div>
+        <div
+          class="test-identifier expandable-header"
+          @click=${() => this.expanded = !this.expanded}
+        >
+          <mwc-icon id="expand-toggle">${this.expanded ? 'expand_more' : 'chevron_right'}</mwc-icon>
+          <div id="header" class="one-line-content">
+            <mwc-icon
+              id="status-indicator"
+              class=${classMap({[STATUS_CLASS_MAP[this.variant.status]]: true})}
+            >${STATUS_ICON_MAP[this.variant.status]}</mwc-icon>
+            <div id="test-identifier">
+              <span class="light">${this.commonTestIdPrefix}</span>${this.variant.testId.slice(this.commonTestIdPrefix.length)}
+              <tr-copy-to-clipboard
+                .textToCopy=${this.variant.testId}
+                @click=${(e: Event) => e.stopPropagation()}
+                title="copy test ID to clipboard"
+              ></tr-copy-to-clipboard>
+            </div>
+          </div>
+        </div>
+        <div id="body">
+          <div id="content-ruler"></div>
+          <div id="content" style=${styleMap({display: this.expanded ? '' : 'none'})}>
+            ${this.renderVariantDef()}
+            ${repeat(this.wasExpanded ? this.variant!.results : [], (r) => r.resultId, (r, i) => html`
+            <tr-result-entry
+              .id=${i + 1}
+              .testResult=${r}
+              .expanded=${this.hasSingleChild || !r.expected}
+            ></tr-result-entry>
+            `)}
+            ${repeat(this.wasExpanded ? this.variant!.exonerations : [], (e) => e.exonerationId, (e, i) => html`
+            <tr-exoneration-entry
+              .id=${this.variant!.results.length + i + 1}
+              .testExoneration=${e}
+              .expanded=${this.hasSingleChild}
+            ></tr-exoneration-entry>
+            `)}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  // TODO(weiweilin): extract the color scheme to a separate stylesheet.
+  static styles = css`
+    .expandable-header {
+      display: grid;
+      grid-template-columns: 24px 1fr;
+      grid-template-rows: 24px;
+      letter-spacing: 0.15px;
+      grid-gap: 5px;
+      cursor: pointer;
+      user-select: none;
+    }
+    .expandable-header .expand-toggle {
+      grid-row: 1;
+      grid-column: 1;
+    }
+    .expandable-header .one-line-content {
+      grid-row: 1;
+      grid-column: 2;
+      line-height: 24px;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+    #test-identifier {
+      font-size: 16px;
+    }
+
+    #header {
+      display: grid;
+      grid-template-columns: 24px 1fr;
+      grid-template-rows: 24px;
+      grid-gap: 5px;
+    }
+
+    #status-indicator {
+      grid-row: 1;
+      grid-column: 1;
+    }
+    #status-indicator.exonerated {
+      color: #ff33d2;
+    }
+    #status-indicator.expected {
+      color: #33ac71;
+    }
+    #status-indicator.unexpected {
+      color: #d23f31;
+    }
+    #status-indicator.flaky {
+      color: #f5a309;
+    }
+
+    #body {
+      display: grid;
+      grid-template-columns: 24px 1fr;
+      grid-gap: 5px;
+    }
+    #content-ruler {
+      border-left: 1px solid #DDDDDD;
+      width: 0px;
+      margin-left: 11.5px;
+    }
+
+    #variant-def {
+      font-weight: 500;
+    }
+    .kv-key::after {
+      content: ':';
+    }
+    .kv-value::after {
+      content: ',';
+    }
+    .kv-value:last-child::after {
+      content: '';
+    }
+    #def-table {
+      margin-left: 29px;
+    }
+
+    .light {
+      color: grey;
+    }
+  `;
+}
