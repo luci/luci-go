6c91fe876e84b2c765b78fd52a3fb2a1210377d3
Nodir Turakulov <nodir@chromium.org>
2020-06-16 08:52:32 +0000

    [buildbucket] Remove dupe tests
    Remove CancelBuild tests from search_test.go
    
    Change-Id: Ie0a2814e17c22086b8510ec002c8327b14ba9c86
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2234497
    Reviewed-by: smut <smut@google.com>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

--- buildbucket/appengine/rpc/search_builds_test.go
+++ buildbucket/appengine/rpc/search_builds_test.go
@@ -15,16 +15,8 @@
 package rpc
 
 import (
-	"context"
 	"testing"
 
-	"go.chromium.org/gae/impl/memory"
-	"go.chromium.org/gae/service/datastore"
-	"go.chromium.org/luci/auth/identity"
-	"go.chromium.org/luci/server/auth"
-	"go.chromium.org/luci/server/auth/authtest"
-
-	"go.chromium.org/luci/buildbucket/appengine/model"
 	pb "go.chromium.org/luci/buildbucket/proto"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -33,103 +25,6 @@ import (
 
 func TestSearchBuilds(t *testing.T) {
 	t.Parallel()
-
-	Convey("CancelBuild", t, func() {
-		srv := &Builds{}
-		ctx := memory.Use(context.Background())
-		datastore.GetTestable(ctx).AutoIndex(true)
-		datastore.GetTestable(ctx).Consistent(true)
-
-		Convey("id", func() {
-			Convey("not found", func() {
-				req := &pb.CancelBuildRequest{
-					Id:              1,
-					SummaryMarkdown: "summary",
-				}
-				rsp, err := srv.CancelBuild(ctx, req)
-				So(err, ShouldErrLike, "not found")
-				So(rsp, ShouldBeNil)
-			})
-
-			Convey("permission denied", func() {
-				ctx = auth.WithState(ctx, &authtest.FakeState{
-					Identity: identity.Identity("user:user"),
-				})
-				So(datastore.Put(ctx, &model.Bucket{
-					ID:     "bucket",
-					Parent: model.ProjectKey(ctx, "project"),
-					Proto: pb.Bucket{
-						Acls: []*pb.Acl{
-							{
-								Identity: "user:user",
-								Role:     pb.Acl_READER,
-							},
-						},
-					},
-				}), ShouldBeNil)
-				So(datastore.Put(ctx, &model.Build{
-					Proto: pb.Build{
-						Id: 1,
-						Builder: &pb.BuilderID{
-							Project: "project",
-							Bucket:  "bucket",
-							Builder: "builder",
-						},
-					},
-				}), ShouldBeNil)
-				req := &pb.CancelBuildRequest{
-					Id:              1,
-					SummaryMarkdown: "summary",
-				}
-				rsp, err := srv.CancelBuild(ctx, req)
-				So(err, ShouldErrLike, "does not have permission")
-				So(rsp, ShouldBeNil)
-			})
-
-			Convey("found", func() {
-				ctx = auth.WithState(ctx, &authtest.FakeState{
-					Identity: identity.Identity("user:user"),
-				})
-				So(datastore.Put(ctx, &model.Bucket{
-					ID:     "bucket",
-					Parent: model.ProjectKey(ctx, "project"),
-					Proto: pb.Bucket{
-						Acls: []*pb.Acl{
-							{
-								Identity: "user:user",
-								Role:     pb.Acl_WRITER,
-							},
-						},
-					},
-				}), ShouldBeNil)
-				So(datastore.Put(ctx, &model.Build{
-					Proto: pb.Build{
-						Id: 1,
-						Builder: &pb.BuilderID{
-							Project: "project",
-							Bucket:  "bucket",
-							Builder: "builder",
-						},
-					},
-				}), ShouldBeNil)
-				req := &pb.CancelBuildRequest{
-					Id:              1,
-					SummaryMarkdown: "summary",
-				}
-				rsp, err := srv.CancelBuild(ctx, req)
-				So(err, ShouldBeNil)
-				So(rsp, ShouldResembleProto, &pb.Build{
-					Id: 1,
-					Builder: &pb.BuilderID{
-						Project: "project",
-						Bucket:  "bucket",
-						Builder: "builder",
-					},
-					Input: &pb.Build_Input{},
-				})
-			})
-		})
-	})
 
 	Convey("validateChange", t, func() {
 		Convey("nil", func() {
