93d8999d5258089c5812f09e69b6040396eb0239
Weiwei Lin <weiweilin@google.com>
2020-06-18 00:41:07 +0000

    [resultui] hide flaky variants by default
    Given the sad state of flakiness in chrome, hide flakes by default.
    We can revert this once chrome tests become stable enough.
    
    Bug:1084390
    Change-Id: I64fb56ba6064230d6b6da172553bcaa704f82eb6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2249437
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>

--- milo/frontend/resultui/src/components/test_filter.ts
+++ milo/frontend/resultui/src/components/test_filter.ts
@@ -33,7 +33,7 @@ export class TestFilterElement extends MobxLitElement {
 
   @observable.ref showExpected = false;
   @observable.ref showExonerated = true;
-  @observable.ref showFlaky = true;
+  @observable.ref showFlaky = false;
 
   private disposer = () => {};
   connectedCallback() {
