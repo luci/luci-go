9be47869fb2783e01f44b0acedde4d87fba73819
Vadim Shtayura <vadimsh@chromium.org>
2020-06-19 15:22:07 -0700

    [lucicfg] Add `fmt` subcommand.
    It uses Bazel buildifier rules for non-BUILD files to format Starlark
    code. They appear to be generic enough to apply to lucicfg Starlark
    files too.
    
    R=tandrii@chromium.org, iannucci@chromium.org
    BUG=1096936
    
    Change-Id: I206a18f9f4c0e582d9f0c46daaa8cfd4c5ae3b94

--- lucicfg/cli/app.go
+++ lucicfg/cli/app.go
@@ -31,6 +31,7 @@ import (
 	"go.chromium.org/luci/lucicfg"
 	"go.chromium.org/luci/lucicfg/cli/base"
 	"go.chromium.org/luci/lucicfg/cli/cmds/diff"
+	"go.chromium.org/luci/lucicfg/cli/cmds/fmt"
 	"go.chromium.org/luci/lucicfg/cli/cmds/generate"
 	"go.chromium.org/luci/lucicfg/cli/cmds/validate"
 )
@@ -72,6 +73,7 @@ func GetApplication(params base.Parameters) *cli.Application {
 			subcommands.Section("Config generation\n"),
 			generate.Cmd(params),
 			validate.Cmd(params),
+			fmt.Cmd(params),
 
 			subcommands.Section("Aiding in the migration\n"),
 			diff.Cmd(params),
