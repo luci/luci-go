e8eca849bf979f4291161b949c77fa7d55f1565b
Yuanjun Huang <yuanjunh@google.com>
2020-06-20 19:29:32 -0700

    [buildbucket] Convert SearchBuildsRequest to the intermediate SearchQuery Object
    Bug:1090540
    Change-Id: If50e825d9a300aa472982e2d60a48e7df3a962eb

--- buildbucket/protoutil/tag.go
+++ buildbucket/protoutil/tag.go
@@ -53,6 +53,24 @@ func SortStringPairs(pairs []*pb.StringPair) {
 	})
 }
 
+func ParseStringPair(p *pb.StringPair) string {
+	if p == nil {
+		return ""
+	}
+	return fmt.Sprintf("%s:%s", p.Key, p.Value)
+}
+
+func ParseStringPairs(p []*pb.StringPair) []string {
+	if p == nil {
+		return []string{}
+	}
+	strs := make([]string, len(p))
+	for i, pair := range p {
+		strs[i] = ParseStringPair(pair)
+	}
+	return strs
+}
+
 // BuildSets returns all of the buildsets of the build.
 func BuildSets(b *pb.Build) []string {
 	var result []string
