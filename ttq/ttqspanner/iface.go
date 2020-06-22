86d45a3d1a9189f8f911d4cda2bce95f8e7a5493
Andrii Shyshkalov <tandrii@google.com>
2020-06-21 21:50:22 -0700

    ttq: define and document public Options.
    Bug: 1080423
    Change-Id: Id2781f3a80bb26a192df09c71d0e0f37b6e77642

--- ttq/ttqspanner/iface.go
+++ ttq/ttqspanner/iface.go
@@ -45,12 +45,14 @@ type TTQ struct {
 
 // New creates a new TTQ for Spanner.
 // You must also call InstallRoutes in at least one of your app's microservices.
-func New(c *cloudtasks.Client, opts ttq.Options, sp *spanner.Client) *TTQ {
-	// TODO(tandrii): implement.
+func New(c *cloudtasks.Client, opts ttq.Options, sp *spanner.Client) (*TTQ, error) {
+	if err := opts.Validate(); err != nil {
+		return nil, err
+	}
 	return &TTQ{impl: internal.Impl{
 		Options: opts,
 		DB:      &db{client: sp}},
-	}
+	}, nil
 }
 
 // InstallRoutes installs handlers for sweeping to ensure correctness.
