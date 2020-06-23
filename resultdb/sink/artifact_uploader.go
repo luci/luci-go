f633df2cd7aceda5b1e82269f3e2b0dc47bbccae
Scott Lee <ddoman@chromium.org>
2020-06-23 03:56:28 +0000

    [resultdb] add ArtifactUploader
    ArtifactUploader provides methods for uploading artifacts.
    Please visit https://crrev.com/c/2253562 to find how it is used in rdb.
    
    Change-Id: I6ff7f05490c9f7043b36ee8c781b417f82ba340c
    Bug: 1087955
    R=chanli@chromium.org,iannucci@chromium.org,nodir@chromium.org

---
+++ resultdb/sink/artifact_uploader.go
@@ -0,0 +1,165 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sink
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/retry"
+	"go.chromium.org/luci/common/retry/transient"
+)
+
+// MaxInMemoryFileSize is the maximum size of an artifact file that can be loaded into
+// memory.
+//
+// ArtifactUploader reads the entire contents of a file twice; one for calculating the hash
+// and one for transmitting the contents over network. If the size is equal to or smaller
+// than MaxInMemoryFileSize, ArtifactUploader dumps the entire contents into a memory buffer
+// to minimize disk I/O.
+const MaxInMemoryFileSize = 64 * 1024
+
+// ArtifactUploader provides functions for uploading artifacts to ResultDB.
+type ArtifactUploader struct {
+	// Client is an HTTP client used for uploading artifacts to ResultDB.
+	Client *http.Client
+	// Host is the host of a ResultDB instance to upload artifacts to.
+	Host string
+}
+
+// NewArtifactUploader returns a new ArtifactUploader.
+func NewArtifactUploader(c *http.Client, host string) *ArtifactUploader {
+	return &ArtifactUploader{Client: c, Host: host}
+}
+
+func (u *ArtifactUploader) newRequest(ctx context.Context, name, contentType string, input io.ReadSeeker, updateToken string) (*http.Request, error) {
+	req, err := http.NewRequestWithContext(ctx, "PUT", fmt.Sprintf("https://%s/%s", u.Host, name), input)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to create a request").Err()
+	}
+	hash, err := calculateHash(input)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to calcualte hash").Err()
+	}
+	// rewind the current position back to the beginning so that the request body can be
+	// re-read by HTTPClient.Do.
+	if _, err := input.Seek(0, io.SeekStart); err != nil {
+		return nil, errors.Annotate(err, "failed to reset the stream cursor").Err()
+	}
+
+	req.Header.Add("Content-Hash", hash)
+	if contentType != "" {
+		req.Header.Add("Content-Type", contentType)
+	}
+	req.Header.Add("Update-Token", updateToken)
+	return req, nil
+}
+
+// Upload uploads an artifact from a given input stream.
+//
+// `name` is the artifact name, which uniquely identifies the artifact globally. It must
+// conform to the format described in the name property of message Artifact at
+// https://chromium.googlesource.com/infra/luci/luci-go/+/master/resultdb/proto/v1/artifact.proto
+//
+// If the input source is a file, it's highly recommended to use UploadFromFile, instead.
+func (u *ArtifactUploader) Upload(ctx context.Context, name, contentType string, input io.ReadSeeker, updateToken string) error {
+	req, err := u.newRequest(ctx, name, contentType, input, updateToken)
+	if err != nil {
+		return errors.Annotate(err, "artifact %q", name).Err()
+	}
+	if err = u.send(ctx, req); err != nil {
+		return errors.Annotate(err, "artifact %q", name).Err()
+	}
+	return nil
+}
+
+// Upload uploads an artifact from a given input stream.
+//
+// `name` is the artifact name, which uniquely identifies the artifact globally. It must
+// conform to the format described in the name property of message Artifact at
+// https://chromium.googlesource.com/infra/luci/luci-go/+/master/resultdb/proto/v1/artifact.proto
+func (u *ArtifactUploader) UploadFromFile(ctx context.Context, name, contentType, path, updateToken string) error {
+	var input io.ReadSeeker
+	st, err := os.Stat(path)
+	if err != nil {
+		return errors.Annotate(err, "artifact %q: failed to query the file status", name).Err()
+	}
+	// small file?
+	if st.Size() <= MaxInMemoryFileSize {
+		contents, err := ioutil.ReadFile(path)
+		if err != nil {
+			return errors.Annotate(err, "artifact %q: failed to read file contents", name).Err()
+		}
+		return u.Upload(ctx, name, contentType, bytes.NewReader(contents), updateToken)
+	}
+
+	if input, err = os.Open(path); err != nil {
+		return errors.Annotate(err, "artifact %q: failed to open file", name).Err()
+	}
+	req, err := u.newRequest(ctx, name, contentType, input, updateToken)
+	if err != nil {
+		return errors.Annotate(err, "artifact %q", name).Err()
+	}
+	req.GetBody = func() (io.ReadCloser, error) {
+		s, err := os.Open(path)
+		if err != nil {
+			return nil, errors.Annotate(err, "artifact %q: failed to re-open file", name).Err()
+		}
+		return s, err
+	}
+	req.ContentLength = st.Size()
+	if err := u.send(ctx, req); err != nil {
+		return errors.Annotate(err, "artifact %q", name).Err()
+	}
+	return nil
+}
+
+func (u *ArtifactUploader) send(ctx context.Context, req *http.Request) error {
+	return retry.Retry(ctx, transient.Only(retry.Default), func() error {
+		resp, err := u.Client.Do(req)
+		if err != nil {
+			return errors.Annotate(err, "failed to send HTTP request").Err()
+		}
+
+		code := resp.StatusCode
+		// ResultDB returns StatusNoContent on success.
+		if code == http.StatusNoContent {
+			return nil
+		}
+
+		// Tag the error as an transient error, if retriable.
+		hErr := errors.Reason("http request failed(%d): %s", resp.StatusCode, resp.Status)
+		if code == http.StatusRequestTimeout || code == http.StatusTooManyRequests {
+			hErr = hErr.Tag(transient.Tag)
+		}
+		return errors.Annotate(hErr.Err(), "a non-transient error response returned").Err()
+	}, nil)
+}
+
+func calculateHash(input io.Reader) (string, error) {
+	hash := sha256.New()
+	if _, err := io.Copy(hash, input); err != nil {
+		return "", err
+	}
+	return "sha256:" + hex.EncodeToString(hash.Sum(nil)), nil
+}
