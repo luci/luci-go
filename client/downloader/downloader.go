7f24e8963165f49a3ee28dd06ece9948aa6f4ef6
Takuto Ikuta <tikuta@chromium.org>
2020-06-23 05:42:25 +0000

    downloader: make default file mode more limitted
    Security folks recommended to set more strict permission for the files.
    
    file       : 0666 -> 0644
    executable : 0777 -> 0744
    
    Bug: 1087760
    Change-Id: I4231436251acabbe4507171c1e93088286920b9e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2259812
    Commit-Queue: Ye Kuang <yekuang@google.com>
    Reviewed-by: Ye Kuang <yekuang@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>

--- client/downloader/downloader.go
+++ client/downloader/downloader.go
@@ -433,7 +433,7 @@ func (d *Downloader) scheduleFileJob(filename, name string, details *isolated.Fi
 			return
 		}
 
-		mode := 0666
+		mode := 0644
 		// Ignore mode other than executable bit.
 		if details.Mode != nil {
 			mode |= *details.Mode & 0100
@@ -600,7 +600,7 @@ func (d *Downloader) scheduleTarballJob(tarname string, details *isolated.File) 
 			filename := filepath.Join(d.outputDir, name)
 
 			// Igonre mode other than executable bit.
-			mode := 0666 | (int(hdr.Mode) & 0100)
+			mode := 0644 | (int(hdr.Mode) & 0100)
 
 			if err := d.ensureDir(filepath.Dir(filename)); err != nil {
 				d.addError(tarType, string(hash)+":"+filename, err)
