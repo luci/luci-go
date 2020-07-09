230572ad0c4930cbfe27b50c27e1d1bfab665542
Scott Lee <ddoman@chromium.org>
2020-07-09 07:24:16 +0000

    [resultdb] fix flaky test in globalmetrics/metrics_test.go
    R=nodir@chromium.org
    
    Bug: 1103544
    Change-Id: I06baab6b28c7388577a16c8ae9f3bfe62d0bdfe1

--- resultdb/internal/services/globalmetrics/metrics_test.go
+++ resultdb/internal/services/globalmetrics/metrics_test.go
@@ -15,8 +15,11 @@
 package globalmetrics
 
 import (
+	"sort"
 	"testing"
+	"time"
 
+	"cloud.google.com/go/spanner"
 	"go.chromium.org/luci/common/clock"
 	"go.chromium.org/luci/common/tsmon"
 	"go.chromium.org/luci/resultdb/internal/tasks"
@@ -38,6 +41,24 @@ func TestUpdateTaskStats(t *testing.T) {
 			tasks.Enqueue(tasks.TryFinalizeInvocation, "task5", "inv", "payload", now),
 		)
 
+		// find the actual CreateTime, which is the spanner CommitTimestamp.
+		findCT := func(key spanner.Key, storage *time.Time) {
+			testutil.MustReadRow(ctx, "InvocationTasks", key, map[string]interface{}{
+				"CreateTime": storage,
+			})
+		}
+
+		bqcts := make([]time.Time, 2)
+		findCT(tasks.BQExport.Key("task1"), &bqcts[0])
+		findCT(tasks.BQExport.Key("task2"), &bqcts[1])
+		sort.Slice(bqcts, func(i, j int) bool { return bqcts[i].Before(bqcts[j]) })
+
+		tfcts := make([]time.Time, 3)
+		findCT(tasks.TryFinalizeInvocation.Key("task3"), &tfcts[0])
+		findCT(tasks.TryFinalizeInvocation.Key("task4"), &tfcts[1])
+		findCT(tasks.TryFinalizeInvocation.Key("task5"), &tfcts[2])
+		sort.Slice(tfcts, func(i, j int) bool { return tfcts[i].Before(tfcts[j]) })
+
 		Convey(`reports count metric`, func() {
 			So(updateTaskStats(ctx), ShouldBeNil)
 			So(taskCountMetric.Get(ctx, string(tasks.BQExport)), ShouldEqual, 2)
@@ -55,21 +76,21 @@ func TestUpdateTaskStats(t *testing.T) {
 
 		Convey("reports oldest-task-age metric", func() {
 			So(updateTaskStats(ctx), ShouldBeNil)
-			So(oldestTaskMetric.Get(ctx, string(tasks.BQExport)), ShouldEqual, now.Unix())
-			So(oldestTaskMetric.Get(ctx, string(tasks.TryFinalizeInvocation)), ShouldEqual, now.Unix())
+			So(oldestTaskMetric.Get(ctx, string(tasks.BQExport)), ShouldEqual, bqcts[0].Unix())
+			So(oldestTaskMetric.Get(ctx, string(tasks.TryFinalizeInvocation)), ShouldEqual, tfcts[0].Unix())
 		})
 
 		Convey("stops reporting oldest-task-age, if task-count == 0", func() {
 			So(updateTaskStats(ctx), ShouldBeNil)
-			So(oldestTaskMetric.Get(ctx, string(tasks.BQExport)), ShouldEqual, now.Unix())
-			So(oldestTaskMetric.Get(ctx, string(tasks.TryFinalizeInvocation)), ShouldEqual, now.Unix())
+			So(oldestTaskMetric.Get(ctx, string(tasks.BQExport)), ShouldEqual, bqcts[0].Unix())
+			So(oldestTaskMetric.Get(ctx, string(tasks.TryFinalizeInvocation)), ShouldEqual, tfcts[0].Unix())
 
 			tasks.Delete(ctx, tasks.TryFinalizeInvocation, "task3")
 			tasks.Delete(ctx, tasks.TryFinalizeInvocation, "task4")
 			tasks.Delete(ctx, tasks.TryFinalizeInvocation, "task5")
 
 			So(updateTaskStats(ctx), ShouldBeNil)
-			So(oldestTaskMetric.Get(ctx, string(tasks.BQExport)), ShouldEqual, now.Unix())
+			So(oldestTaskMetric.Get(ctx, string(tasks.BQExport)), ShouldEqual, bqcts[0].Unix())
 			// metric.Get() returns the zero value of the metric value type, if there is
 			// no matching cell for the fields. Thus, tsmon.Store.Get() should be used
 			// to find whether the cell no longer exists or not.
