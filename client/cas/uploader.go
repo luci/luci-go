b82b08305eeeac59b4ddd9789248fbb3b25ec311
Ye Kuang <yekuang@google.com>
2020-07-15 13:20:07 +0900

    [isolate] Add a CAS uploader
    Again, please let me know your opinion on this before i add tests.
    
    * https://crrev.com/c/2291502 is an example for how this could be used. I was able to use that to upload to CAS.
    * Shall we make Upload asynchronous, so that batch_archive can feed all the isolates to uploader at once?
    
    Bug: 924625
    Change-Id: I0a9581d82267ac8bfa07c3ada6ba2361b3b2d0fb

---
+++ client/cas/uploader.go
@@ -0,0 +1,89 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cas
+
+import (
+	"context"
+
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/chunker"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/client"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/command"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/digest"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/filemetadata"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/tree"
+	"go.chromium.org/luci/client/isolate"
+)
+
+// Uploader provides the API to upload the files in an isolate file to CAS service.
+type Uploader struct {
+	cas       *client.Client
+	chunkSize int
+	fmCache   filemetadata.Cache
+	deduper   *ChunkerDeduper
+}
+
+// NewUploader creates a new Uploader object.
+func NewUploader(cas *client.Client) *Uploader {
+	return &Uploader{
+		cas:       cas,
+		chunkSize: chunker.DefaultChunkSize,
+		fmCache:   filemetadata.NewSingleFlightCache(),
+		deduper:   NewChunkerDeduper(),
+	}
+}
+
+func buildInputSpec(opts *isolate.ArchiveOptions) (string, *command.InputSpec, error) {
+	inputPaths, execRoot, err := isolate.ProcessIsolateForCAS(opts)
+	if err != nil {
+		return "", nil, err
+	}
+
+	inputSpec := &command.InputSpec{
+		Inputs: inputPaths,
+	}
+	if opts.IgnoredPathFilterRe != "" {
+		excl := &command.InputExclusion{
+			Regex: opts.IgnoredPathFilterRe,
+			Type:  command.UnspecifiedInputType,
+		}
+		inputSpec.InputExclusions = append(inputSpec.InputExclusions, excl)
+	}
+
+	return execRoot, inputSpec, nil
+}
+
+// Upload parses the isolate options and uploads the files it refers to to CAS. Returns the digest
+// of the root directory, if there is no error.
+func (up *Uploader) Upload(ctx context.Context, opts *isolate.ArchiveOptions) (digest.Digest, error) {
+	execRoot, inputSpec, err := buildInputSpec(opts)
+	if err != nil {
+		return digest.Empty, err
+	}
+	rootDg, chunkers, _, err := tree.ComputeMerkleTree(execRoot, inputSpec, up.chunkSize, up.fmCache)
+	if err != nil {
+		return digest.Empty, err
+	}
+	chunkers = up.deduper.Deduplicate(chunkers)
+	err = up.cas.UploadIfMissing(ctx, chunkers...)
+	if err != nil {
+		return digest.Empty, err
+	}
+	return rootDg, nil
+}
+
+// Close closes the uploader object.
+func (up *Uploader) Close() error {
+	return up.cas.Close()
+}
