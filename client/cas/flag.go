3cd2a64091782105072fc5ff780a30a84461109e
Takuto Ikuta <tikuta@chromium.org>
2020-07-29 04:04:40 +0000

    cas: move casFlags to cas package
    I will use this in isolated client too.
    
    Bug: 1110569
    Change-Id: I2deb265a03de7dfe1cad208c594eeee183272ed7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2325333
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>

---
+++ client/cas/flag.go
@@ -0,0 +1,60 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cas
+
+import (
+	"flag"
+	"regexp"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+type Flags struct {
+	Instance   string
+	DigestJSON string
+}
+
+func (c *Flags) Init(f *flag.FlagSet) {
+	f.StringVar(&c.Instance, "cas-instance", "", "CAS instance (GCP). Format is either a project ID, or \"projects/<project_id>/instances/<instance_id>\"")
+	f.StringVar(&c.DigestJSON, "digest-json", "", "Outputs a JSON file to store the CAS root digest")
+}
+
+func (c *Flags) Parse() error {
+	if c.Instance == "" {
+		// -cas-instance is optional for now
+		return nil
+	}
+	ins, err := parseCASInstance(c.Instance)
+	if err != nil {
+		return err
+	}
+	c.Instance = ins
+	return nil
+}
+
+func parseCASInstance(ins string) (string, error) {
+	// GCP project ID format: https://cloud.google.com/resource-manager/docs/creating-managing-projects
+	// Not the most accurate regexp, but let's just assume most people know what they are doing...
+	projectRe := regexp.MustCompile(`^[a-z0-9\-]+$`)
+	instanceRe := regexp.MustCompile(`^projects/[a-z0-9\-]+/instances/[^/]+$`)
+
+	if projectRe.MatchString(ins) {
+		return "projects/" + ins + "/instances/default_instance", nil
+	}
+	if instanceRe.MatchString(ins) {
+		return ins, nil
+	}
+	return "", errors.Reason("invalid CAS instance: %s", ins).Err()
+}
---
+++ client/cas/flag_test.go
@@ -0,0 +1,46 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cas
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestParseCASInstance(t *testing.T) {
+	t.Parallel()
+
+	Convey(`Basic`, t, func() {
+		ins, err := parseCASInstance("abc-123")
+		So(err, ShouldBeNil)
+		So(ins, ShouldResemble, "projects/abc-123/instances/default_instance")
+
+		ins, err = parseCASInstance("projects/foo/instances/bar")
+		So(err, ShouldBeNil)
+		So(ins, ShouldResemble, "projects/foo/instances/bar")
+	})
+
+	Convey(`Invalid`, t, func() {
+		_, err := parseCASInstance("ABC")
+		So(err, ShouldNotBeNil)
+
+		_, err = parseCASInstance("projects/foo")
+		So(err, ShouldNotBeNil)
+
+		_, err = parseCASInstance("projects/foo/instances/bar/42")
+		So(err, ShouldNotBeNil)
+	})
+}
