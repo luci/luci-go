20e1e2d6c0aa35e50bae35ddc467197bc181b953
Takuto Ikuta <tikuta@chromium.org>
2020-07-29 11:30:53 +0900

    cas: move casFlags to cas package
    I will use this in isolated client too.
    
    Bug: 1110569
    Change-Id: I2deb265a03de7dfe1cad208c594eeee183272ed7

--- client/cmd/isolate/lib/common.go
+++ client/cmd/isolate/lib/common.go
@@ -21,7 +21,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"regexp"
 	"sort"
 	"strings"
 
@@ -216,42 +215,4 @@ func recreateTree(outDir string, rootDir string, deps []string) error {
 		}
 	}
 	return nil
-}
-
-type casFlags struct {
-	instance   string
-	digestJSON string
-}
-
-func (c *casFlags) Init(f *flag.FlagSet) {
-	f.StringVar(&c.instance, "cas-instance", "", "CAS instance (GCP). Format is either a project ID, or \"projects/<project_id>/instances/<instance_id>\"")
-	f.StringVar(&c.digestJSON, "digest-json", "", "Outputs a JSON file to store the CAS root digest")
-}
-
-func (c *casFlags) Parse() error {
-	if c.instance == "" {
-		// -cas-instance is optional for now
-		return nil
-	}
-	ins, err := parseCASInstance(c.instance)
-	if err != nil {
-		return err
-	}
-	c.instance = ins
-	return nil
-}
-
-func parseCASInstance(ins string) (string, error) {
-	// GCP project ID format: https://cloud.google.com/resource-manager/docs/creating-managing-projects
-	// Not the most accurate regexp, but let's just assume most people know what they are doing...
-	projectRe := regexp.MustCompile(`^[a-z0-9\-]+$`)
-	instanceRe := regexp.MustCompile(`^projects/[a-z0-9\-]+/instances/[^/]+$`)
-
-	if projectRe.MatchString(ins) {
-		return "projects/" + ins + "/instances/default_instance", nil
-	}
-	if instanceRe.MatchString(ins) {
-		return ins, nil
-	}
-	return "", errors.Reason("invalid CAS instance: %s", ins).Err()
 }
