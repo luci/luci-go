26b896bd3429d23338d6977c7f8dce2fd5151bbf
Vadim Shtayura <vadimsh@chromium.org>
2020-07-29 23:44:03 -0700

    [tq] Implement AddTask.
    Pure Cloud Tasks case, no transactional support, no local fake.
    
    R=tandrii@chromium.org
    
    Change-Id: I9891d83e3d1c44bb914d94cfcad6978f45be7173

---
+++ server/tq/submitter.go
@@ -0,0 +1,33 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tq
+
+import (
+	"context"
+
+	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
+	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2"
+)
+
+// CloudTaskSubmitter implements Submitter on top of Cloud Tasks client.
+type CloudTaskSubmitter struct {
+	Client *cloudtasks.Client
+}
+
+// CreateTask creates a task, returning a gRPC status.
+func (s *CloudTaskSubmitter) CreateTask(ctx context.Context, req *taskspb.CreateTaskRequest) error {
+	_, err := s.Client.CreateTask(ctx, req)
+	return err
+}
