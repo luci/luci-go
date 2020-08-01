1951ad26507f2b8c39d26f8636fd38fd64516561
Vadim Shtayura <vadimsh@chromium.org>
2020-08-01 23:48:22 +0000

    [tq] Add local Cloud Tasks fake.
    Hook it up for now to "real" server when it runs on localhost.
    Later it will also be used by unit tests.
    
    R=tandrii@chromium.org
    
    Change-Id: I6db980a7d71c0fbd7dff0346e99440f4d5322816
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2333615
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

---
+++ server/tq/tqtesting/loopback.go
@@ -0,0 +1,90 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tqtesting
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+
+	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2"
+
+	"go.chromium.org/luci/common/logging"
+)
+
+// LoopbackHTTPExecutor is an Executor that executes tasks by calling the given
+// HTTP handler.
+type LoopbackHTTPExecutor struct {
+	Handler http.Handler
+}
+
+// Execute dispatches the task to the HTTP handler in a dedicated goroutine.
+//
+// Marks the task as failed if the response status code is outside of
+// range [200-299].
+func (e *LoopbackHTTPExecutor) Execute(ctx context.Context, t *Task, done func(retry bool)) {
+	go func() {
+		success := false
+		defer func() {
+			done(!success)
+		}()
+
+		var method taskspb.HttpMethod
+		var requestURL string
+		var headers map[string]string
+		var body []byte
+
+		switch mt := t.Task.MessageType.(type) {
+		case *taskspb.Task_HttpRequest:
+			method = mt.HttpRequest.HttpMethod
+			requestURL = mt.HttpRequest.Url
+			headers = mt.HttpRequest.Headers
+			body = mt.HttpRequest.Body
+		case *taskspb.Task_AppEngineHttpRequest:
+			method = mt.AppEngineHttpRequest.HttpMethod
+			requestURL = mt.AppEngineHttpRequest.RelativeUri
+			headers = mt.AppEngineHttpRequest.Headers
+			body = mt.AppEngineHttpRequest.Body
+		default:
+			logging.Errorf(ctx, "Bad task, no payload: %q", t.Task)
+			return
+		}
+
+		parsedURL, err := url.Parse(requestURL)
+		if err != nil {
+			logging.Errorf(ctx, "Bad task URL %q", requestURL)
+			return
+		}
+		host := parsedURL.Host
+
+		// Make the URL relative.
+		parsedURL.Scheme = ""
+		parsedURL.Host = ""
+		requestURL = parsedURL.String()
+
+		req := httptest.NewRequest(method.String(), requestURL, bytes.NewReader(body))
+		req.Host = host
+		for k, v := range headers {
+			req.Header.Add(k, v)
+		}
+
+		rr := httptest.NewRecorder()
+		e.Handler.ServeHTTP(rr, req)
+		status := rr.Result().StatusCode
+		success = status >= 200 && status <= 299
+	}()
+}
---
+++ server/tq/tqtesting/scheduler_test.go
@@ -0,0 +1,228 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tqtesting
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"testing"
+	"time"
+
+	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/clock/testclock"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestScheduler(t *testing.T) {
+	t.Parallel()
+
+	Convey("With scheduler", t, func() {
+		ctx, tc := testclock.UseTime(context.Background(), testclock.TestRecentTimeUTC)
+		tc.SetTimerCallback(func(d time.Duration, t clock.Timer) {
+			if testclock.HasTags(t, ClockTag) {
+				tc.Add(d)
+			}
+		})
+
+		sched := Scheduler{}
+		exec := testExecutor{
+			ctx: ctx,
+			ch:  make(chan *Task, 1000), // ~= infinite buffer
+		}
+
+		run := func(untilCount int) {
+			ctx, cancel := context.WithCancel(ctx)
+
+			done := make(chan struct{})
+			go func() {
+				defer close(done)
+				sched.Run(ctx, &exec)
+			}()
+
+			exec.waitForTasks(untilCount)
+			cancel()
+			<-done
+
+			So(sched.Tasks(), ShouldBeEmpty)
+		}
+
+		enqueue := func(payload, name string, eta time.Time) codes.Code {
+			req := &taskspb.CreateTaskRequest{
+				Parent: "projects/zzz/locations/zzz/queues/zzz",
+				Task: &taskspb.Task{
+					MessageType: &taskspb.Task_HttpRequest{
+						HttpRequest: &taskspb.HttpRequest{
+							Url: payload,
+						},
+					},
+				},
+			}
+			if name != "" {
+				req.Task.Name = req.Parent + "/tasks/" + name
+			}
+			if !eta.IsZero() {
+				req.Task.ScheduleTime = timestamppb.New(eta)
+			}
+			return status.Code(sched.CreateTask(ctx, req))
+		}
+
+		Convey("One by one tasks", func() {
+			So(enqueue("1", "name", time.Time{}), ShouldEqual, codes.OK)
+			So(enqueue("2", "name", time.Time{}), ShouldEqual, codes.AlreadyExists)
+			So(enqueue("3", "", time.Time{}), ShouldEqual, codes.OK)
+			So(enqueue("4", "", time.Time{}), ShouldEqual, codes.OK)
+
+			run(3)
+
+			So(orderByPayload(exec.tasks), ShouldResemble, []string{"1", "3", "4"})
+		})
+
+		Convey("Task chain", func() {
+			exec.execute = func(payload string, t *Task) bool {
+				if len(payload) < 3 {
+					enqueue(payload+".", "", time.Time{})
+				}
+				return true
+			}
+			enqueue(".", "", time.Time{})
+			run(3)
+			So(orderByPayload(exec.tasks), ShouldResemble, []string{".", "..", "..."})
+		})
+
+		Convey("Tasks with ETA", func() {
+			now := clock.Now(ctx)
+			for i := 2; i >= 0; i-- {
+				enqueue(fmt.Sprintf("B %d", i), fmt.Sprintf("B %d", i), now.Add(time.Duration(i)*time.Millisecond))
+				enqueue(fmt.Sprintf("A %d", i), fmt.Sprintf("A %d", i), now.Add(time.Duration(i)*time.Millisecond))
+			}
+			run(6)
+			So(payloads(exec.tasks), ShouldResemble, []string{"A 0", "B 0", "A 1", "B 1", "A 2", "B 2"})
+		})
+
+		Convey("Retries", func() {
+			var capturedTask *Task
+			sched.TaskSucceeded = func(_ context.Context, t *Task) {
+				capturedTask = t
+			}
+
+			exec.execute = func(payload string, t *Task) bool {
+				return t.Attempts == 4
+			}
+
+			enqueue(".", "", time.Time{})
+			run(4)
+			So(payloads(exec.tasks), ShouldHaveLength, 4)
+
+			So(capturedTask, ShouldNotBeNil)
+			So(capturedTask.Attempts, ShouldEqual, 4)
+		})
+
+		Convey("Fails after multiple attempts", func() {
+			sched.MaxAttempts = 10
+
+			var capturedTask *Task
+			sched.TaskFailed = func(_ context.Context, t *Task) {
+				capturedTask = t
+			}
+
+			exec.execute = func(payload string, t *Task) bool {
+				return false
+			}
+
+			enqueue(".", "", time.Time{})
+			run(10)
+			So(payloads(exec.tasks), ShouldHaveLength, 10)
+
+			So(capturedTask, ShouldNotBeNil)
+			So(capturedTask.Attempts, ShouldEqual, 10)
+		})
+
+		Convey("State capture", func() {
+			var captured []*Task
+
+			exec.execute = func(payload string, t *Task) bool {
+				if payload == "A 1" {
+					captured = sched.Tasks()
+				}
+				return true
+			}
+
+			now := clock.Now(ctx)
+			for i := 2; i >= 0; i-- {
+				enqueue(fmt.Sprintf("B %d", i), fmt.Sprintf("B %d", i), now.Add(time.Duration(i)*time.Millisecond))
+				enqueue(fmt.Sprintf("A %d", i), fmt.Sprintf("A %d", i), now.Add(time.Duration(i)*time.Millisecond))
+			}
+			run(6)
+			So(payloads(exec.tasks), ShouldResemble, []string{"A 0", "B 0", "A 1", "B 1", "A 2", "B 2"})
+
+			So(payloads(captured), ShouldResemble, []string{"A 1", "B 1", "A 2", "B 2"})
+			So(captured[0].Executing, ShouldBeTrue)
+			So(captured[1].Executing, ShouldBeFalse)
+		})
+	})
+}
+
+type testExecutor struct {
+	ctx     context.Context
+	execute func(payload string, t *Task) bool
+	tasks   []*Task
+	ch      chan *Task
+}
+
+func (exe *testExecutor) Execute(ctx context.Context, t *Task, done func(retry bool)) {
+	t = t.Copy()
+
+	success := true
+	if exe.execute != nil {
+		success = exe.execute(t.Task.GetHttpRequest().Url, t)
+	}
+
+	exe.tasks = append(exe.tasks, t)
+	exe.ch <- t
+	done(!success)
+}
+
+func (exe *testExecutor) waitForTasks(n int) {
+	for ; n > 0; n-- {
+		select {
+		case <-exe.ch:
+		case <-exe.ctx.Done():
+			So("the scheduler is stuck", ShouldBeNil)
+		}
+	}
+}
+
+func payloads(tasks []*Task) []string {
+	payloads := make([]string, len(tasks))
+	for i, t := range tasks {
+		payloads[i] = t.Task.GetHttpRequest().Url
+	}
+	return payloads
+}
+
+func orderByPayload(tasks []*Task) []string {
+	sort.Slice(tasks, func(i, j int) bool {
+		l, r := tasks[i].Task, tasks[j].Task
+		return l.GetHttpRequest().Url < r.GetHttpRequest().Url
+	})
+	return payloads(tasks)
+}
