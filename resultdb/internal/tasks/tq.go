e33a59582a4f5d0b8421cdccd4eb9e37ecefbaa2
Nodir Turakulov <nodir@chromium.org>
2020-08-02 15:46:39 -0700

    [resultdb] Minimize TestResults query
    Turns out, Spanner query optimizer does NOT optimize away truish
    conditions like REGEXP_CONTAINS(tr.TestId, @testIdRegexp) for
    testIdRegexp = ".*", and
    (@variantHashEquals IS NULL OR tr.VariantHash = @variantHashEquals).
    Including these conditions in the SQL statement conditionally
    reduces the latency 10x!
    
    Generate the SQL statement with text/template package.
    Compared to squirell, the code is less compact, as opposed to scattered
    across a file.
    
    With useless conditions removed, just using IN operator with a list
    of test variants compiles to a CrossApply operator, which too slow.
    Force HashJoin using a hint.
    
    Bug: 1078999
    Change-Id: Iebd060d552ef4b242de533d96ce051a51503bc12

---
+++ resultdb/internal/tasks/tq.go
@@ -0,0 +1,48 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tasks
+
+import (
+	"context"
+
+	"go.chromium.org/luci/server/tq"
+
+	"go.chromium.org/luci/resultdb/internal/invocations"
+	"go.chromium.org/luci/resultdb/internal/tasks/taskspb"
+	pb "go.chromium.org/luci/resultdb/proto/v1"
+)
+
+// Finalization describes how to route finalization tasks.
+//
+// The handler is implemented in internal/services/finalizer.
+var Finalization = tq.RegisterTaskClass(tq.TaskClass{
+	ID:            "try-finalize-inv",
+	Prototype:     &taskspb.TryFinalizeInvocation{},
+	Queue:         "finalizer",
+	RoutingPrefix: "/internal/tasks/finalizer",
+})
+
+// EnqueueFinalization enqueues a task to finalize an invocation.
+//
+// TODO(vadimsh): Doesn't support transactions yet, and thus should not be used.
+func EnqueueFinalization(ctx context.Context, id invocations.ID) error {
+	return tq.AddTask(ctx, &tq.Task{
+		Payload: &taskspb.TryFinalizeInvocation{
+			InvocationId: string(id),
+			State:        pb.Invocation_FINALIZING,
+		},
+		Title: string(id),
+	})
+}
