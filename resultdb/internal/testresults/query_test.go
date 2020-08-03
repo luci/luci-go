7b5f9ed3e2746d09ff29d3b732b261e60195929d
Nodir Turakulov <nodir@chromium.org>
2020-08-02 23:30:23 -0700

    [resultdb] Minimize TestResults query
    Turns out, Spanner query optimizer does NOT optimize away truish
    conditions like REGEXP_CONTAINS(tr.TestId, @testIdRegexp) for
    testIdRegexp = ".*", and
    (@variantHashEquals IS NULL OR tr.VariantHash = @variantHashEquals)
    for @variantHashEquals = NULL.
    Including these conditions in the SQL statement *conditionally*
    reduces the latency by up to 10x!
    
    Generate the SQL statement with text/template package.
    Compared to https://godoc.org/github.com/Masterminds/squirrel,
    the code is more compact, as opposed to scattered across a file.
    
    With useless conditions removed, just using IN operator with a list
    of test variants compiles to a CrossApply operator, which too slow.
    Force HashJoin using a hint.
    
    If expectancy filter is VARIANTS_WITH_UNEXPECTED_RESULTS,
    then filter by TestID and Variant in the test variant query,
    as opposed to in the main query.
    Update the UnexpectedTestResults index to store Variant, to avoid
    lookups in the main table.
    
    Bug: 1078999
    Change-Id: Iebd060d552ef4b242de533d96ce051a51503bc12

--- resultdb/internal/testresults/query_test.go
+++ resultdb/internal/testresults/query_test.go
@@ -104,6 +104,9 @@ func TestQueryTestResults(t *testing.T) {
 		})
 
 		Convey(`Expectancy filter`, func() {
+			q.Predicate.Expectancy = pb.TestResultPredicate_VARIANTS_WITH_UNEXPECTED_RESULTS
+			q.InvocationIDs = invocations.NewIDSet("inv0", "inv1")
+
 			testutil.MustApply(ctx, insert.Invocation("inv0", pb.Invocation_ACTIVE, nil))
 			testutil.MustApply(ctx, testutil.CombineMutations(
 				insert.TestResults("inv0", "T1", nil, pb.TestStatus_PASS, pb.TestStatus_FAIL),
@@ -111,21 +114,36 @@ func TestQueryTestResults(t *testing.T) {
 				insert.TestResults("inv1", "T1", nil, pb.TestStatus_PASS),
 				insert.TestResults("inv1", "T2", nil, pb.TestStatus_FAIL),
 				insert.TestResults("inv1", "T3", nil, pb.TestStatus_PASS),
-				insert.TestResults("inv1", "T4", nil, pb.TestStatus_FAIL),
+				insert.TestResults("inv1", "T4", pbutil.Variant("a", "b"), pb.TestStatus_FAIL),
 			)...)
 
-			q.InvocationIDs = invocations.NewIDSet("inv0", "inv1")
-			q.Predicate.Expectancy = pb.TestResultPredicate_VARIANTS_WITH_UNEXPECTED_RESULTS
-			actual, _ := mustFetch(q)
-			pbutil.NormalizeTestResultSlice(actual)
+			Convey(`Works`, func() {
+				So(mustFetchNames(q), ShouldResemble, []string{
+					"invocations/inv0/tests/T1/results/0",
+					"invocations/inv0/tests/T1/results/1",
+					"invocations/inv0/tests/T2/results/0",
+					"invocations/inv1/tests/T1/results/0",
+					"invocations/inv1/tests/T2/results/0",
+					"invocations/inv1/tests/T4/results/0",
+				})
+			})
 
-			So(mustFetchNames(q), ShouldResemble, []string{
-				"invocations/inv0/tests/T1/results/0",
-				"invocations/inv0/tests/T1/results/1",
-				"invocations/inv0/tests/T2/results/0",
-				"invocations/inv1/tests/T1/results/0",
-				"invocations/inv1/tests/T2/results/0",
-				"invocations/inv1/tests/T4/results/0",
+			Convey(`TestID filter`, func() {
+				q.Predicate.TestIdRegexp = "T4"
+				So(mustFetchNames(q), ShouldResemble, []string{
+					"invocations/inv1/tests/T4/results/0",
+				})
+			})
+
+			Convey(`Variant filter`, func() {
+				q.Predicate.Variant = &pb.VariantPredicate{
+					Predicate: &pb.VariantPredicate_Equals{
+						Equals: pbutil.Variant("a", "b"),
+					},
+				}
+				So(mustFetchNames(q), ShouldResemble, []string{
+					"invocations/inv1/tests/T4/results/0",
+				})
 			})
 		})
 
