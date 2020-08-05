608dba4fea3791bfe26bca0a0504c936b2618720
Vadim Shtayura <vadimsh@chromium.org>
2020-08-05 15:24:25 -0700

    [server/tq] Add detailed GAEv2 example.
    Also do some minor adjustment and fixes to make it really work:
      1. Log important options when starting to give a clue what
         cron.yaml and queue.yaml might be.
      2. Ask for a dedicated queue for the sweeper by default. Would be
         easier to troubleshoot it this way.
      3. The magic header name is X-Appengine-Queuename.
    
    R=tandrii@chromium.org
    
    Change-Id: Ife3a2765823a8755cc558b5ed8a16eb3592fc8ce

---
+++ server/tq/doc.go
@@ -0,0 +1,32 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package tq provides a task queue implementation on top of Cloud Tasks.
+//
+// It exposes a high-level API that operates with proto messages and hides
+// gory details such as serialization, routing, authentication, etc.
+//
+// Enabling it on Appengine
+//
+// All default options should just work, except you still need to setup
+// the sweeper cron and the sweeper queue.
+//
+// In cron.yaml:
+//   - url: /internal/tasks/c/sweep
+//     schedule: every 1 minutes
+//
+// In queue.yaml:
+//   - name: tq-sweep
+//     rate: 500/s
+package tq
