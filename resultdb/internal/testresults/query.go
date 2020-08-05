e9a77eb2732328b3b4c43d9492df08d3e569ffa5
Nodir Turakulov <nodir@chromium.org>
2020-08-05 04:21:04 +0000

    [resultdb] Minimize TestResults query
    Turns out, Spanner query optimizer does NOT optimize away truish
    conditions like REGEXP_CONTAINS(tr.TestId, @testIdRegexp) for
    testIdRegexp = ".*", and
    (@variantHashEquals IS NULL OR tr.VariantHash = @variantHashEquals)
    for @variantHashEquals = NULL.
    Including these conditions in the SQL statement *conditionally*
    reduces the latency by up to 10x!
    
    Generate the SQL statement with text/template package.
    Compared to https://godoc.org/github.com/Masterminds/squirrel,
    the code is more compact, as opposed to scattered across a file.
    
    With useless conditions removed, just using IN operator with a list
    of test variants compiles to a CrossApply operator, which too slow.
    Force HashJoin using a hint.
    
    If expectancy filter is VARIANTS_WITH_UNEXPECTED_RESULTS,
    then filter by TestID and Variant in the test variant query,
    as opposed to in the main query.
    Update the UnexpectedTestResults index to store Variant, to avoid
    lookups in the main table.
    
    Bug: 1078999
    Change-Id: Iebd060d552ef4b242de533d96ce051a51503bc12
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2334393
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Chan Li <chanli@chromium.org>

--- resultdb/internal/testresults/query.go
+++ resultdb/internal/testresults/query.go
@@ -15,10 +15,12 @@
 package testresults
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"strings"
 	"sync"
+	"text/template"
 
 	"cloud.google.com/go/spanner"
 	"golang.org/x/sync/errgroup"
@@ -74,73 +76,40 @@ type Query struct {
 
 func (q *Query) run(ctx context.Context, f func(*pb.TestResult) error) (err error) {
 	ctx, ts := trace.StartSpan(ctx, "testresults.Query.run")
+	ts.Attribute("cr.dev/invocations", len(q.InvocationIDs))
 	defer func() { ts.End(err) }()
 
 	if q.PageSize < 0 {
 		panic("PageSize < 0")
 	}
 
-	limit := ""
-	if q.PageSize > 0 {
-		limit = `LIMIT @limit`
-	}
+	columns, parser := q.selectClause()
+	params := q.baseParams()
+	params["limit"] = q.PageSize
 
-	selectSQL, parser := q.selectClause()
-
-	st := spanner.NewStatement(fmt.Sprintf(`
-		@{USE_ADDITIONAL_PARALLELISM=TRUE}
-		%s
-		FROM TestResults tr
-		WHERE InvocationId IN UNNEST(@invIDs)
-			# Skip test results after the one specified in the page token.
-			AND (
-				(tr.InvocationId > @afterInvocationId) OR
-				(tr.InvocationId = @afterInvocationId AND tr.TestId > @afterTestId) OR
-				(tr.InvocationId = @afterInvocationId AND tr.TestId = @afterTestId AND tr.ResultId > @afterResultId)
-			)
-			AND (@testVariants IS NULL OR STRUCT(tr.TestId, tr.VariantHash) IN UNNEST(@testVariants))
-			AND REGEXP_CONTAINS(tr.TestId, @TestIdRegexp)
-			AND (@variantHashEquals IS NULL OR tr.VariantHash = @variantHashEquals)
-			AND (@variantContains IS NULL
-				OR ARRAY_LENGTH(@variantContains) = 0
-				OR (SELECT LOGICAL_AND(kv IN UNNEST(tr.Variant)) FROM UNNEST(@variantContains) kv)
-			)
-		ORDER BY InvocationId, TestId, ResultId
-		%s
-	`, selectSQL, limit))
-	st.Params["invIDs"] = q.InvocationIDs
-	st.Params["limit"] = q.PageSize
-	st.Params["testVariants"] = []*testVariant(nil)
-
-	// Filter by expectency.
+	// Filter by expectancy.
 	if q.Predicate.GetExpectancy() == pb.TestResultPredicate_VARIANTS_WITH_UNEXPECTED_RESULTS {
 		switch testVariants, err := q.fetchVariantsWithUnexpectedResults(ctx); {
 		case err != nil:
 			return errors.Annotate(err, "failed to fetch variants with unexpected results").Err()
 		case len(testVariants) == 0:
+			// No test variant to match.
 			return nil
 		default:
-			st.Params["testVariants"] = testVariants
+			params["testVariants"] = testVariants
 		}
 	}
 
-	// Filter by test id.
-	testIDRegexp := q.Predicate.GetTestIdRegexp()
-	if testIDRegexp == "" {
-		testIDRegexp = ".*"
-	}
-	st.Params["TestIdRegexp"] = fmt.Sprintf("^%s$", testIDRegexp)
-
-	// Filter by variant.
-	PopulateVariantParams(&st, q.Predicate.GetVariant())
-
-	// Apply page token.
-	err = invocations.TokenToMap(q.PageToken, st.Params, "afterInvocationId", "afterTestId", "afterResultId")
+	err = invocations.TokenToMap(q.PageToken, params, "afterInvocationId", "afterTestId", "afterResultId")
 	if err != nil {
 		return err
 	}
 
-	// Read the results.
+	// Execute the query.
+	st := q.genStatement("testResults", map[string]interface{}{
+		"params":  params,
+		"columns": strings.Join(columns, ", "),
+	})
 	return spanutil.Query(ctx, st, func(row *spanner.Row) error {
 		tr, err := parser(row)
 		if err != nil {
@@ -154,23 +123,23 @@ func (q *Query) run(ctx context.Context, f func(*pb.TestResult) error) (err erro
 // as well as a function to convert a Spanner row to a TestResult message.
 // The returned SELECT clause assumes that the TestResults has alias "tr".
 // The returned parser is stateful and must not be called concurrently.
-func (q *Query) selectClause() (sql string, parser func(*spanner.Row) (*pb.TestResult, error)) {
-	sql = `SELECT
-		tr.InvocationId,
-		tr.TestId,
-		tr.ResultId,
-		tr.Variant,
-		tr.VariantHash,
-		tr.IsUnexpected,
-		tr.Status,
-		tr.StartTime,
-		tr.RunDurationUsec,
-		tr.TestLocationFileName,
-		tr.TestLocationLine,
-	`
+func (q *Query) selectClause() (columns []string, parser func(*spanner.Row) (*pb.TestResult, error)) {
+	columns = []string{
+		"InvocationId",
+		"TestId",
+		"ResultId",
+		"Variant",
+		"VariantHash",
+		"IsUnexpected",
+		"Status",
+		"StartTime",
+		"RunDurationUsec",
+		"TestLocationFileName",
+		"TestLocationLine",
+	}
 
 	// Select extra columns depending on the mask.
-	var extraSelect []string
+	var extraColumns []string
 	readMask := q.Mask
 	if readMask.IsEmpty() {
 		readMask = defaultListMask
@@ -180,12 +149,12 @@ func (q *Query) selectClause() (sql string, parser func(*spanner.Row) (*pb.TestR
 		case err != nil:
 			panic(err)
 		case inc != mask.Exclude:
-			extraSelect = append(extraSelect, column)
+			extraColumns = append(extraColumns, column)
+			columns = append(columns, column)
 		}
 	}
-	selectIfIncluded("tr.SummaryHtml", "summary_html")
-	selectIfIncluded("tr.Tags", "tags")
-	sql += strings.Join(extraSelect, ",")
+	selectIfIncluded("SummaryHtml", "summary_html")
+	selectIfIncluded("Tags", "tags")
 
 	// Build a parser function.
 	var b spanutil.Buffer
@@ -212,11 +181,11 @@ func (q *Query) selectClause() (sql string, parser func(*spanner.Row) (*pb.TestR
 			&testLocationLine,
 		}
 
-		for _, v := range extraSelect {
+		for _, v := range extraColumns {
 			switch v {
-			case "tr.SummaryHtml":
+			case "SummaryHtml":
 				ptrs = append(ptrs, &summaryHTML)
-			case "tr.Tags":
+			case "Tags":
 				ptrs = append(ptrs, &tr.Tags)
 			default:
 				panic("impossible")
@@ -258,13 +227,10 @@ func (q *Query) fetchVariantsWithUnexpectedResults(ctx context.Context) (testVar
 	set := map[testVariant]struct{}{}
 	var mu sync.Mutex
 
-	st := spanner.NewStatement(`
-		@{USE_ADDITIONAL_PARALLELISM=TRUE}
-		SELECT DISTINCT TestId, VariantHash
-		FROM TestResults@{FORCE_INDEX=UnexpectedTestResults}
-		WHERE IsUnexpected AND InvocationId IN UNNEST(@invIDs)
-	`)
-	st.Params["invIDs"] = q.InvocationIDs
+	params := q.baseParams()
+	st := q.genStatement("variantsWithUnexpectedResults", map[string]interface{}{
+		"params": params,
+	})
 	subStmts := invocations.ShardStatement(st, "invIDs")
 
 	eg, ctx := errgroup.WithContext(ctx)
@@ -293,6 +259,7 @@ func (q *Query) fetchVariantsWithUnexpectedResults(ctx context.Context) (testVar
 		testVariants = append(testVariants, tv)
 	}
 
+	ts.Attribute("cr.dev/count", len(testVariants))
 	return testVariants, nil
 }
 
@@ -333,19 +300,104 @@ func (q *Query) Run(ctx context.Context, f func(*pb.TestResult) error) error {
 	return q.run(ctx, f)
 }
 
+func (q *Query) baseParams() map[string]interface{} {
+	params := map[string]interface{}{
+		"invIDs": q.InvocationIDs,
+	}
+
+	if re := q.Predicate.GetTestIdRegexp(); re != "" && re != ".*" {
+		params["testIdRegexp"] = fmt.Sprintf("^%s$", re)
+	}
+
+	PopulateVariantParams(params, q.Predicate.GetVariant())
+
+	return params
+}
+
 // PopulateVariantParams populates variantHashEquals and variantContains
 // parameters based on the predicate.
-func PopulateVariantParams(st *spanner.Statement, variantPredicate *pb.VariantPredicate) {
-	st.Params["variantHashEquals"] = spanner.NullString{}
-	st.Params["variantContains"] = []string(nil)
+func PopulateVariantParams(params map[string]interface{}, variantPredicate *pb.VariantPredicate) {
 	switch p := variantPredicate.GetPredicate().(type) {
 	case *pb.VariantPredicate_Equals:
-		st.Params["variantHashEquals"] = pbutil.VariantHash(p.Equals)
+		params["variantHashEquals"] = pbutil.VariantHash(p.Equals)
 	case *pb.VariantPredicate_Contains:
-		st.Params["variantContains"] = pbutil.VariantToStrings(p.Contains)
+		params["variantContains"] = pbutil.VariantToStrings(p.Contains)
 	case nil:
 		// No filter.
 	default:
 		panic(errors.Reason("unexpected variant predicate %q", variantPredicate).Err())
 	}
+}
+
+// queryTmpl is a set of templates that generate the SQL statements used
+// by Query type.
+// Two main templates are "testResults" and "variantsWithUnexpectedResults"
+var queryTmpl = template.Must(template.New("").Parse(`
+	{{define "testResults"}}
+		@{USE_ADDITIONAL_PARALLELISM=TRUE}
+		SELECT {{.columns}}
+		FROM TestResults tr
+		WHERE InvocationId IN UNNEST(@invIDs)
+
+		{{/* Apply the page token */}}
+		{{if .params.afterInvocationId}}
+			AND (
+				(InvocationId > @afterInvocationId) OR
+				(InvocationId = @afterInvocationId AND TestId > @afterTestId) OR
+				(InvocationId = @afterInvocationId AND TestId = @afterTestId AND ResultId > @afterResultId)
+			)
+		{{end}}
+
+		{{if .params.testVariants}}
+			{{/*
+				Use HashJoin algorithm, as opposed to CrossApply, otherwise
+				Spanner will try to run O(invocations * test_variants) sub-queries.
+				Note that IN operator for UNNEST rejects hints, so we trick Spanner
+				by using SELECT .. FROM UNNEST.
+			*/}}
+			AND STRUCT(TestId, VariantHash) IN@{JOIN_TYPE=HASH_JOIN} (SELECT tr FROM UNNEST(@testVariants) tr)
+		{{else}}
+			{{/*
+				Apply TestId and Variant filters only if we don't filter by
+				@testVariants, because @testVariants are already filtered by TestID
+				and Variant.
+			*/}}
+			{{template "testIDAndVariantFilter" .}}
+		{{end}}
+
+		ORDER BY InvocationId, TestId, ResultId
+		{{if .params.limit}}LIMIT @limit{{end}}
+	{{end}}
+
+	{{define "variantsWithUnexpectedResults"}}
+		@{USE_ADDITIONAL_PARALLELISM=TRUE}
+		SELECT DISTINCT TestId, VariantHash
+		FROM TestResults@{FORCE_INDEX=UnexpectedTestResults}
+		WHERE IsUnexpected AND InvocationId IN UNNEST(@invIDs)
+		{{template "testIDAndVariantFilter" .}}
+	{{end}}
+
+	{{define "testIDAndVariantFilter"}}
+		{{/* Filter by Test ID */}}
+		{{if .params.testIdRegexp}}
+			AND REGEXP_CONTAINS(TestId, @testIdRegexp)
+		{{end}}
+
+		{{/* Filter by Variant */}}
+		{{if .params.variantHashEquals}}
+			AND VariantHash = @variantHashEquals
+		{{end}}
+		{{if .params.variantContains }}
+			AND (SELECT LOGICAL_AND(kv IN UNNEST(Variant)) FROM UNNEST(@variantContains) kv)
+		{{end}}
+	{{end}}
+`))
+
+func (*Query) genStatement(templateName string, input map[string]interface{}) spanner.Statement {
+	var sql bytes.Buffer
+	err := queryTmpl.ExecuteTemplate(&sql, templateName, input)
+	if err != nil {
+		panic(fmt.Sprintf("failed to generate a SQL statement: %s", err))
+	}
+	return spanner.Statement{SQL: sql.String(), Params: input["params"].(map[string]interface{})}
 }
