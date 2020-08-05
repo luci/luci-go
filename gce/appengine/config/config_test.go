a1da4b6a6d910f5fbdddaf25c420d615cb390c10
Andrii Shyshkalov <tandrii@google.com>
2020-08-05 10:25:38 -0700

    [gce] stop forgetting overridden currentAmount.
    This CL undos prior attempt to fix this https://crrev.com/c/2309591,
    which is not sufficient for autoscaled VM prefixes.
    
    The bug was in internal-only Config.Ensure RPC. The root cause is that it
    overwrote whatever CurrentAmount might have stored there before.
    
    Previous fix correctly ensured that Ensure RPC, when called, would have
    the same CurrentAmount set as the one computed in ExpandConfig task.
    
    This CL ensures that CurrentAmount isn't modified at all by Ensure RPC.
    
    R=smut
    
    Bug: 1106945
    Change-Id: I9dbec989ea0fc072ebca441b9b51ce711c281d5d

--- gce/appengine/config/config_test.go
+++ gce/appengine/config/config_test.go
@@ -17,12 +17,8 @@ package config
 import (
 	"context"
 	"testing"
-	"time"
-
-	"google.golang.org/genproto/googleapis/type/dayofweek"
 
 	gae "go.chromium.org/gae/impl/memory"
-	"go.chromium.org/luci/common/clock/testclock"
 	"go.chromium.org/luci/config"
 	"go.chromium.org/luci/config/cfgclient"
 	"go.chromium.org/luci/config/impl/memory"
@@ -230,10 +226,6 @@ func TestNormalize(t *testing.T) {
 												Duration: "1h",
 											},
 										},
-										Start: &gce.TimeOfDay{
-											Day:  dayofweek.DayOfWeek_MONDAY,
-											Time: "1:00",
-										},
 									},
 								},
 							},
@@ -243,52 +235,6 @@ func TestNormalize(t *testing.T) {
 			}
 			So(normalize(c, cfg), ShouldBeNil)
 			So(cfg.VMs.GetVms()[0].Amount.Change[0].Length.GetSeconds(), ShouldEqual, 3600)
-		})
-
-		Convey("current amount", func() {
-			cfg := &Config{
-				VMs: &gce.Configs{
-					Vms: []*gce.Config{
-						{
-							Amount: &gce.Amount{
-								Max: 2,
-								Min: 2,
-								Change: []*gce.Schedule{
-									{
-										Min: 5,
-										Max: 5,
-										Length: &gce.TimePeriod{
-											Time: &gce.TimePeriod_Duration{
-												Duration: "1h",
-											},
-										},
-										Start: &gce.TimeOfDay{
-											Day:  dayofweek.DayOfWeek_MONDAY,
-											Time: "1:00",
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			}
-
-			Convey("default", func() {
-				now := time.Time{}
-				So(now.Weekday(), ShouldEqual, time.Monday)
-				c, _ = testclock.UseTime(c, now)
-				So(normalize(c, cfg), ShouldBeNil)
-				So(cfg.VMs.GetVms()[0].CurrentAmount, ShouldEqual, 2)
-			})
-
-			Convey("scheduled", func() {
-				now := time.Time{}.Add(time.Hour)
-				So(now.Weekday(), ShouldEqual, time.Monday)
-				c, _ = testclock.UseTime(c, now)
-				So(normalize(c, cfg), ShouldBeNil)
-				So(cfg.VMs.GetVms()[0].CurrentAmount, ShouldEqual, 5)
-			})
 		})
 
 		Convey("lifetime", func() {
