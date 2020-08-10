b5261dc13149b2c6b61a58823f898950c415e28d
Vadim Shtayura <vadimsh@chromium.org>
2020-08-10 19:38:31 +0000

    [server/tq] Add a helper to batch-process reminders.
    R=tandrii@chromium.org
    
    Change-Id: I34679dbb2509f34ec56b7d6d3a8323156cbaf3bb
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2345463
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

---
+++ server/tq/internal/sweep/batching.go
@@ -0,0 +1,108 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sweep
+
+import (
+	"context"
+	"sync/atomic"
+	"time"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/sync/dispatcher"
+	"go.chromium.org/luci/common/sync/dispatcher/buffer"
+
+	"go.chromium.org/luci/server/tq/internal"
+	"go.chromium.org/luci/server/tq/internal/db"
+	"go.chromium.org/luci/server/tq/internal/reminder"
+)
+
+// BatchProcessor handles reminders in batches.
+type BatchProcessor struct {
+	Context   context.Context    // the context to use for processing
+	DB        db.DB              // DB to use to fetch reminders from
+	Submitter internal.Submitter // knows how to submit tasks
+
+	BatchSize         int // max size of a single reminder batch
+	ConcurrentBatches int // how many concurrent batches to process
+
+	ch        dispatcher.Channel
+	processed int32 // total reminders successfully processed
+}
+
+// Start launches background processor goroutines.
+func (p *BatchProcessor) Start() error {
+	var err error
+	p.ch, err = dispatcher.NewChannel(
+		p.Context,
+		&dispatcher.Options{
+			Buffer: buffer.Options{
+				MaxLeases: p.ConcurrentBatches,
+				BatchSize: p.BatchSize,
+				// Max waiting time to fill the batch.
+				BatchDuration: 10 * time.Millisecond,
+				FullBehavior: &buffer.BlockNewItems{
+					// If all workers are busy, block Enqueue.
+					MaxItems: p.ConcurrentBatches * p.BatchSize,
+				},
+			},
+		},
+		p.processBatch,
+	)
+	if err != nil {
+		return errors.Annotate(err, "invalid sweeper configuration").Err()
+	}
+	return nil
+}
+
+// Stop waits until all enqueues reminders are processed and then stops the
+// processor.
+//
+// Returns the total number of successfully processed reminders.
+func (p *BatchProcessor) Stop() int {
+	p.ch.Close()
+	<-p.ch.DrainC
+	return int(atomic.LoadInt32(&p.processed))
+}
+
+// Enqueue adds reminder to the to-be-processed queue.
+//
+// Must be called only between Start and Stop. Drops reminders on the floor if
+// the context is canceled.
+func (p *BatchProcessor) Enqueue(ctx context.Context, r []*reminder.Reminder) {
+	for _, rem := range r {
+		select {
+		case p.ch.C <- rem:
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+// processBatch called concurrently to handle a single batch of items.
+//
+// Logs errors inside, doesn't return them.
+func (p *BatchProcessor) processBatch(data *buffer.Batch) error {
+	batch := make([]*reminder.Reminder, len(data.Data))
+	for i, d := range data.Data {
+		batch[i] = d.(*reminder.Reminder)
+	}
+	count, err := internal.SubmitBatch(p.Context, p.Submitter, p.DB, batch)
+	if err != nil {
+		logging.Errorf(p.Context, "Processed only %d/%d reminders: %s", count, len(batch), err)
+	}
+	atomic.AddInt32(&p.processed, int32(count))
+	return nil
+}
---
+++ server/tq/internal/sweep/batching_test.go
@@ -0,0 +1,129 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sweep
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+	"testing"
+
+	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+
+	"go.chromium.org/luci/server/tq/internal/reminder"
+	"go.chromium.org/luci/server/tq/internal/testutil"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestBatching(t *testing.T) {
+	t.Parallel()
+
+	Convey("With BatchProcessor", t, func() {
+		ctx := context.Background()
+		db := &testutil.FakeDB{}
+		sub := &submitter{}
+
+		p := BatchProcessor{
+			Context:           ctx,
+			DB:                db,
+			Submitter:         sub,
+			BatchSize:         3,
+			ConcurrentBatches: 20,
+		}
+
+		makeRem := func(id string) *reminder.Reminder {
+			r := &reminder.Reminder{ID: id}
+			r.Payload, _ = proto.Marshal(&taskspb.CreateTaskRequest{
+				Parent: id + " body",
+			})
+			So(db.SaveReminder(ctx, r), ShouldBeNil)
+			return r
+		}
+
+		var r []*reminder.Reminder
+		for i := 0; i < 100; i++ {
+			r = append(r, makeRem(fmt.Sprintf("rem-%d", i)))
+		}
+		So(db.AllReminders(), ShouldHaveLength, len(r))
+
+		Convey("Works", func() {
+			So(p.Start(), ShouldBeNil)
+			p.Enqueue(ctx, r)
+			So(p.Stop(), ShouldEqual, len(r))
+			So(sub.req, ShouldHaveLength, len(r))
+			So(db.AllReminders(), ShouldHaveLength, 0)
+		})
+
+		Convey("Waits for processing to finish", func() {
+			p.BatchSize = 1000 // to make sure Enqueue doesn't block
+
+			var stopped int32
+
+			ch := make(chan struct{})
+			sub.cb = func(*taskspb.CreateTaskRequest) error {
+				if atomic.LoadInt32(&stopped) == 1 {
+					panic("processing while stopped")
+				}
+				ch <- struct{}{}
+				return nil
+			}
+
+			So(p.Start(), ShouldBeNil)
+			p.Enqueue(ctx, r)
+
+			result := make(chan int, 1)
+			go func() {
+				result <- p.Stop()
+				atomic.StoreInt32(&stopped, 1)
+			}()
+
+			for i := 0; i < len(r); i++ {
+				<-ch
+			}
+			So(<-result, ShouldEqual, len(r))
+		})
+
+		Convey("Context cancel", func() {
+			ctx, cancel := context.WithCancel(ctx)
+			p.Context = ctx
+
+			var stopped int32
+			var count int32
+			sub.cb = func(*taskspb.CreateTaskRequest) error {
+				if atomic.LoadInt32(&stopped) == 1 {
+					panic("processing while stopped")
+				}
+				if count := atomic.AddInt32(&count, 1); count >= 50 {
+					if count > 50 {
+						return status.Errorf(codes.Canceled, "boo")
+					}
+					cancel()
+				}
+				return nil
+			}
+
+			p.Start()
+			p.Enqueue(ctx, r)
+			p.Stop()
+			atomic.StoreInt32(&stopped, 1)
+
+			So(sub.req, ShouldHaveLength, 50)
+		})
+	})
+}
