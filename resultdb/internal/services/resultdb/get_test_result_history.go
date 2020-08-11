cd2ea95bd456099c7d583b1c766c2d1ff9494773
Roberto Carrillo <robertocn@google.com>
2020-08-11 13:04:44 -0700

    [resultdb] Implement basic features of GetTestResultHistory()
    This first iteration is intended to only serve the first page of test
    results matching the predicate, retrieving all invocations and test
    results in the range, and filtering them service-side.
    
    Later iterations will implement paging support and optimizations to the
    query and graph traversal.
    
    R=chanli,nodir
    
    Bug: 1074407
    Change-Id: Id1386419e6268f7a31019153d2d106b1ad096d20

--- resultdb/internal/services/resultdb/get_test_result_history.go
+++ resultdb/internal/services/resultdb/get_test_result_history.go
@@ -23,5 +23,41 @@ import (
 
 // GetTestResultHistory implements pb.ResultDBServer.
 func (s *resultDBServer) GetTestResultHistory(ctx context.Context, in *pb.GetTestResultHistoryRequest) (*pb.GetTestResultHistoryResponse, error) {
+	// Read realm, check permission
+	// validate request
+	// TODO(): Support token field
+	// tl_invocations, order, more_tl_inv, err := getTopLevelInvocationsForRequest(ctx, in)
+	// invocations, more_inv := getClosure(tl_invocations, in.Predicate, after_inv, page_size)
+	// test_results, more_results := getMatchingResultsForInvocations(invocations, in.Predicate, after_test, after_result, page_size)o
 	return nil, errors.Reason("Not yet implemented").Err()
+}
+
+func getTopLevelInvocationsForRequest(ctx context.Context, in *pb.GetTestResultHistoryRequest) ([]*pb.Invocation, bool, pb.GetTestResultHistoryResponse_Order, error) {
+	cpRange := in.GetCpRange()
+	timeRange := in.GetTimeRange()
+	var invocationsByCp, invocationsByTimestamp []*pb.invocation
+	var moreByCp, moreByTimestamp bool
+	// TODO(): Add predicate filter to query.
+	if cpRange != nil || timeRange == nil {
+		invocationsByCp, moreByCp, err := invocations.InOrdinalRange(ctx, in.Realm, cpRange.earliest, cpRange.latest)
+		if err != nil {
+			return nil, 0, err
+		}
+	}
+	if timeRange != nil || cpRange == nil {
+		invocationsByTimestamp, moreByTimestamp, err := invocations.InTimestampRange(ctx, in.Realm, timeRange.earliest, timeRange.latest)
+		if err != nil {
+			return nil, 0, err
+		}
+	}
+	if invocationsByTimestamp == nil || len(invocationsByTimestamp) == 0{
+		return invocationsByCp, pb.GetTestResultHistoryResponse_COMMIT_POSITION, nil
+	}
+	if invocationsByCp == nil || len(invocationsByCp) == 0{
+		return invocationsByTimestamp, pb.GetTestResultHistoryResponse_INVOCATION_TIMESTAMP, nil
+	}
+	if invocationsByCp[0].CreateTime > invocationsByTimestamp[0].CreateTime {
+		return invocationsByCp, pb.GetTestResultHistoryResponse_COMMIT_POSITION, nil
+	}
+	return invocationsByTimestamp, pb.GetTestResultHistoryResponse_INVOCATION_TIMESTAMP, nil
 }
