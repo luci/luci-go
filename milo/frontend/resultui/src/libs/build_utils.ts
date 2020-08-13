6e2ea83931dd0fd14288c9b73c44f8e131e41e3f
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2020-08-13 14:28:04 +1000

    [BuildPage][Related Builds] Add css & fix some minor stuffs
    Bug:1112228
    Change-Id: I5145e485cf281e793e5eeddd6fc4d6a043a8fe4c

---
+++ milo/frontend/resultui/src/libs/build_utils.ts
@@ -0,0 +1,57 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { router } from '../routes';
+import { Build, BuildStatus } from '../services/buildbucket';
+
+export function getURLForBuild(build: Build): string {
+    return router.urlForName(
+        'build-test-results',
+        {
+        'project': build.builder.project,
+        'bucket': build.builder.bucket,
+        'builder': build.builder.builder,
+        'build_num_or_id': 'b' + build.id,
+        },
+    );
+}
+
+export function getURLForBuilder(build: Build): string {
+    return `/p/${build.builder.project}/builders/${build.builder.bucket}/${build.builder.builder}`
+}
+
+export function getDisplayNameForStatus(s: BuildStatus): string {
+    switch (s) {
+        case BuildStatus.Scheduled: {
+            return "Scheduled";
+        }
+        case BuildStatus.Started: {
+            return "Running";
+        }
+        case BuildStatus.Success: {
+            return "Success";
+        }
+        case BuildStatus.Failure: {
+            return "Failure";
+        }
+        case BuildStatus.InfraFailure: {
+            return "Infra Failure";
+        }
+        case BuildStatus.Canceled: {
+            return "Canceled";
+        }
+        default: // Should not fall over to the default case
+            return "Unknown";
+    }
+}
