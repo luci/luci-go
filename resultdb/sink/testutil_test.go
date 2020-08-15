2c2736ea2125eea81aa038453f2da75c92508dc1
Scott Lee <ddoman@chromium.org>
2020-08-15 15:24:38 -0700

    [resultdb] change MaxWarmUpDuration to be a constant
    it was declared under ServerConfig so that it becomes configurable, but
    I don't find how useful it would be that it is configurable.
    
    This CL makes it just a constant.
    
    Bug: 1113089
    Change-Id: I08e9b283b32306569b41b12752a6634adbcdcd15
    R=chanli@chromium.org,iannucci@chromium.org

--- resultdb/sink/testutil_test.go
+++ resultdb/sink/testutil_test.go
@@ -62,14 +62,13 @@ func reportTestResults(ctx context.Context, host, authToken string, in *sinkpb.R
 
 func testServerConfig(ctl *gomock.Controller, addr, tk string) ServerConfig {
 	return ServerConfig{
-		Address:           addr,
-		AuthToken:         tk,
-		ArtifactUploader:  &ArtifactUploader{Client: &http.Client{}, Host: "example.org"},
-		Recorder:          pb.NewMockRecorderClient(ctl),
-		Invocation:        "invocations/u-foo-1587421194_893166206",
-		invocationID:      "u-foo-1587421194_893166206",
-		UpdateToken:       "UpdateToken-ABC",
-		MaxWarmUpDuration: 3 * time.Second,
+		Address:          addr,
+		AuthToken:        tk,
+		ArtifactUploader: &ArtifactUploader{Client: &http.Client{}, Host: "example.org"},
+		Recorder:         pb.NewMockRecorderClient(ctl),
+		Invocation:       "invocations/u-foo-1587421194_893166206",
+		invocationID:     "u-foo-1587421194_893166206",
+		UpdateToken:      "UpdateToken-ABC",
 	}
 }
 
