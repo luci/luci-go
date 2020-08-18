26e8623edd50678496b575d8221505ddd42fc2b7
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2020-08-18 17:13:12 +1000

    [BuildPage][Timeline] Load data into vis
    Bug:1115838
    Change-Id: Ie03e221e291514560ffbe58a7fe1871a4124ed94

--- milo/frontend/resultui/src/pages/build_page/timeline_tab.ts
+++ milo/frontend/resultui/src/pages/build_page/timeline_tab.ts
@@ -15,24 +15,95 @@
 
 import { MobxLitElement } from '@adobe/lit-mobx';
 import { customElement, html } from 'lit-element';
-import { observable} from 'mobx';
+import { computed, observable, when } from 'mobx';
 
 import { AppState, consumeAppState } from '../../context/app_state/app_state';
 import { BuildState, consumeBuildState } from '../../context/build_state/build_state';
+import { DataSet } from 'vis-data/peer';
+import { Timeline } from 'vis-timeline/peer';
 
 export class TimelineTabElement extends MobxLitElement {
   @observable.ref appState!: AppState;
   @observable.ref buildState!: BuildState;
-
+  private disposers: Array<() => void> = [];
+  @observable.ref rendered: boolean = false;
+ 
   connectedCallback() {
     super.connectedCallback();
     this.appState.selectedTabId = 'timeline';
+    this.rendered = false;
+    this.disposers.push(
+      when(
+        () => this.shouldRenderedTimeline,
+        () => this.renderTimeline()
+      )
+    );
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    for (const disposer of this.disposers) {
+      disposer();
+    }
+  }
+
+  @computed
+  get shouldRenderedTimeline() {
+    return  this.buildState.buildPageData != null && this.rendered;
+  }
+
+  private renderTimeline() {
+    var container = this.shadowRoot!.getElementById('timeline')!;
+
+   function groupTemplater(group: any, _: any, __: any) {
+      return `
+        <div>
+          <span class="title">${group.data.label}</span>
+          <span class="duration">( ${group.data.duration} )</span>
+        </div>`;
+    }
+
+    const options = {
+      clickToUse: false,
+      groupTemplate: groupTemplater,
+      multiselect: false,
+      orientation: {
+        axis: 'both',
+        item: 'top',
+      },
+      zoomable: false,
+    };
+ 
+    // Create a Timeline
+    const timelineData = JSON.parse(this.buildState.buildPageData!.timeline);
+
+    const timeline = new Timeline(
+      container,
+      new DataSet(timelineData.items),
+      new DataSet(timelineData.groups),
+      options);
+
+    timeline.on('select', function(props: any) {
+      const item = timelineData.items[props.items[0]];
+      if (!item) {
+        return;
+      }
+      if (item.data && item.data.logUrl) {
+        window.open(item.data.logUrl, '_blank');
+      }
+    });
   }
 
   protected render() {
     return html`
-      <div>This is the Timeline Tab<div>
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/vis/4.16.1/vis.css">
+      <div id="timeline"></div>
     `;
+  }
+
+  // updated is called when the DOM has been rendered.
+  protected updated(_: any) {
+    this.rendered = true;
   }
 }
 
