aa9223966dd9afd9b32c289e0072169d6812b1d6
Scott Lee <ddoman@chromium.org>
2020-08-18 22:12:30 -0700

    [buildbucket] validate build.summary_markdown
    Change-Id: I756877918e80ebf55b3a6bb8720d01aa78e95b77
    
    R=smut@google.com,iannucci@chromium.org
    CC=nodir@chromium.org
    
    Bug: 1110990
    Change-Id: I756877918e80ebf55b3a6bb8720d01aa78e95b77

--- buildbucket/appengine/rpc/update_build.go
+++ buildbucket/appengine/rpc/update_build.go
@@ -58,7 +58,7 @@ func validateUpdate(req *pb.UpdateBuildRequest) error {
 	}
 
 	for _, p := range req.GetUpdateMask().GetPaths() {
-		// TODO(1110990): validate gitiles_commit, summary_markdown, and steps
+		// TODO(1110990): validate gitiles_commit, and steps
 		switch p {
 		case "build.output":
 		case "build.output.properties":
@@ -70,6 +70,9 @@ func validateUpdate(req *pb.UpdateBuildRequest) error {
 		case "build.status_details":
 		case "build.steps":
 		case "build.summary_markdown":
+			if err := validateSummaryMarkdown(req.Build.SummaryMarkdown); err != nil {
+				return errors.Annotate(err, "build.summary_markdown").Err()
+			}
 		case "build.tags":
 			if err := validateTags(req.Build.Tags, TagAppend); err != nil {
 				return errors.Annotate(err, "build.tags").Err()
