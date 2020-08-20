256aa79a9082a2e501386d1c750e6feb5160f23c
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2020-08-20 04:15:57 +0000

    [BuildPage][Timeline] Load data into vis-timeline
    Bug:1115838
    Change-Id: Ie03e221e291514560ffbe58a7fe1871a4124ed94
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2362002
    Commit-Queue: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Reviewed-by: Weiwei Lin <weiweilin@google.com>

--- milo/frontend/resultui/src/pages/build_page/timeline_tab.ts
+++ milo/frontend/resultui/src/pages/build_page/timeline_tab.ts
@@ -14,8 +14,10 @@
 
 
 import { MobxLitElement } from '@adobe/lit-mobx';
-import { customElement, html } from 'lit-element';
-import { observable } from 'mobx';
+import { css, customElement, html } from 'lit-element';
+import { autorun, observable } from 'mobx';
+import { DataSet } from 'vis-data/peer';
+import { Timeline } from 'vis-timeline/peer';
 
 import { AppState, consumeAppState } from '../../context/app_state/app_state';
 import { BuildState, consumeBuildState } from '../../context/build_state/build_state';
@@ -23,17 +25,89 @@ import { BuildState, consumeBuildState } from '../../context/build_state/build_s
 export class TimelineTabElement extends MobxLitElement {
   @observable.ref appState!: AppState;
   @observable.ref buildState!: BuildState;
+  @observable.ref rendered  = false;
+  private disposers: Array<() => void> = [];
 
   connectedCallback() {
     super.connectedCallback();
     this.appState.selectedTabId = 'timeline';
+    this.rendered = false;
+    this.disposers.push(
+      autorun(
+        () => this.renderTimeline(),
+      ),
+    );
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    for (const disposer of this.disposers) {
+      disposer();
+    }
+  }
+
+  private renderTimeline() {
+    if (this.buildState.buildPageData === null || !this.rendered) {
+      return;
+    }
+
+    const container = this.shadowRoot!.getElementById('timeline')!;
+    function groupTemplater(group: { data: { label: string; duration: string; } }): string {
+      return `
+        <div>
+          <span class="title">${group.data.label}</span>
+          <span class="duration">( ${group.data.duration} )</span>
+        </div>
+      `;
+    }
+
+    const options = {
+      clickToUse: false,
+      groupTemplate: groupTemplater,
+      multiselect: false,
+      orientation: {
+        axis: 'both',
+        item: 'top',
+      },
+      zoomable: false,
+    };
+
+    // Create a Timeline
+    const timelineData = JSON.parse(this.buildState.buildPageData!.timeline);
+    const timeline = new Timeline(
+      container,
+      new DataSet(timelineData.items),
+      new DataSet(timelineData.groups),
+      options,
+    );
+
+    timeline.on('select', (props) => {
+      const item = timelineData.items[props.items[0]];
+      if (!item) {
+        return;
+      }
+      if (item.data && item.data.logUrl) {
+        window.open(item.data.logUrl, '_blank');
+      }
+    });
   }
 
   protected render() {
     return html`
-      <div>This is the Timeline Tab<div>
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/vis/4.16.1/vis.css">
+      <div id="timeline"></div>
     `;
   }
+
+  protected firstUpdated() {
+    this.rendered = true;
+  }
+
+  static styles = css`
+    .vis-range {
+      cursor: pointer;
+    }
+  `;
 }
 
 customElement('milo-timeline-tab')(
