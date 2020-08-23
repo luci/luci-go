c7d24a2958400a08fac53ce0f6c1c3367f48f39c
Weiwei Lin <weiweilin@google.com>
2020-08-23 23:48:22 +0000

    [resultui] add milo-ace-editor
    This will be used to display input/output properties.
    
    Bug: 1116824
    Change-Id: Ib8ce720f11631a78bc9174591850c7393a85c0bd
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2367415
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Quang Minh Tuan Nguyen <nqmtuan@google.com>

---
+++ milo/frontend/resultui/src/components/ace_editor.ts
@@ -0,0 +1,54 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as ace from 'ace-builds';
+import 'ace-builds/src-noconflict/mode-json';
+import { css, customElement, LitElement, property } from 'lit-element';
+import { html } from 'lit-html';
+
+ace.config.setModuleUrl('ace/mode/json_worker', require('file-loader!ace-builds/src-noconflict/worker-json').default);
+
+/**
+ * A lit-element wrapper of ace-editor.
+ */
+@customElement('milo-ace-editor')
+export class AceEditorEntryElement extends LitElement {
+  @property() options?: Partial<ace.Ace.EditorOptions>;
+
+  private editor!: ace.Ace.Editor;
+
+  protected render() {
+    return html`<div id="editor"></div>`;
+  }
+
+  protected firstUpdated() {
+    this.editor = ace.edit(this.shadowRoot!.getElementById('editor')!);
+    this.editor.renderer.attachToShadowRoot();
+  }
+
+  protected updated() {
+    this.editor.setOptions(this.options || {});
+  }
+
+  static styles = css`
+    :host {
+      display: block;
+      border-radius: 4px;
+      border: 1px solid #ccc;
+    }
+    #editor {
+      position: relative;
+    }
+  `;
+}
