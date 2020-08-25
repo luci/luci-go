74309a6088b56de3d3981c52a2c06e0f909ef02b
Junji Watanabe <jwata@google.com>
2020-08-26 02:11:30 +0900

    [cas-viewer] add scaffolds of handlers
    `casviewer/appengine` only contains GAE specific code.
    `routes.go` will implement application logics.
    
    Bug: 1121471, 1114030
    Change-Id: I5bfa66a35e6b54cd2cb5be9116d844cf4505a027

---
+++ casviewer/handlers.go
@@ -0,0 +1,43 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package casviewer
+
+import (
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/server/router"
+)
+
+// InstallHandlers install CAS Viewer handlers to the router.
+func InstallHandlers(r *router.Router) {
+	mw := router.MiddlewareChain{}
+
+	r.GET("/", mw, rootHanlder)
+	r.GET("/projects/:proj/instances/:inst/blobs/:hash/:size/tree", mw, treeHandler)
+	r.GET("/projects/:proj/instances/:inst/blobs/:hash/:size/", mw, getHandler)
+}
+
+func rootHanlder(c *router.Context) {
+	// TODO(crbug.com/1121471): Add top page.
+	logging.Debugf(c.Context, "Hello world")
+	c.Writer.Write([]byte("Hello, world. This is CAS Viewer."))
+}
+
+func treeHandler(c *router.Context) {
+	// TODO(crbug.com/1121471): implement me.
+}
+
+func getHandler(c *router.Context) {
+	// TODO(crbug.com/1121471): implement me.
+}
