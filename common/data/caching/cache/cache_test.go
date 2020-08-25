daf059c437a6d540167bd0642637c739a1bb6c4f
Takuto Ikuta <tikuta@chromium.org>
2020-08-25 15:31:21 +0900

    NOT FOR SUBMIT: benchmark disk access (hardlink)
    https://chromium-swarm-dev.appspot.com/task?id=4e3d111cfe3bcd10
    
    Change-Id: I9eaa7fc1d140d71b05bb3963a728bd0a5fc9f60a

--- common/data/caching/cache/cache_test.go
+++ common/data/caching/cache/cache_test.go
@@ -16,10 +16,15 @@ package cache
 
 import (
 	"bytes"
+	"crypto"
+	"encoding/hex"
+	"fmt"
 	"io/ioutil"
 	"math"
 	"os"
 	"path/filepath"
+	"strconv"
+	"sync"
 	"testing"
 
 	"go.chromium.org/luci/common/isolated"
@@ -236,4 +241,127 @@ func TestNew(t *testing.T) {
 		So(ioutil.WriteFile(empty2, nil, 0600), ShouldBeNil)
 		So(c.AddFileWithoutValidation(emptyHash, empty2), ShouldBeNil)
 	})
+}
+
+func hash(i int) string {
+	return hex.EncodeToString(crypto.SHA1.New().Sum([]byte(strconv.Itoa(i))))
+}
+
+func BenchmarkDisk(b *testing.B) {
+	tdir := b.TempDir()
+
+	for _, n := range []int{16, 256, 65536, 65536 * 4} {
+		n := n
+		var hashes []string
+		for i := 0; i < n; i++ {
+			hashes = append(hashes, hash(i))
+		}
+		b.Run(fmt.Sprintf("with %d files", n), func(b *testing.B) {
+			src := filepath.Join(tdir, "src")
+
+			if err := os.MkdirAll(src, 0700); err != nil {
+				b.Errorf("failed to create dir: %v", err)
+			}
+
+			for j := 0; j < n; j++ {
+				if err := ioutil.WriteFile(filepath.Join(src, hashes[j]), nil, 0600); err != nil {
+					b.Errorf("failed to write file: %v", err)
+				}
+			}
+
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+				cache := filepath.Join(tdir, "cache")
+				if err := os.MkdirAll(cache, 0700); err != nil {
+					b.Errorf("failed to create dir: %v", err)
+				}
+				b.StartTimer()
+
+				for j := 0; j < n; j++ {
+					h := hashes[j]
+					if err := os.Link(filepath.Join(src, h), filepath.Join(cache, h)); err != nil {
+						b.Errorf("failed to write file: %v", err)
+					}
+				}
+
+				b.StopTimer()
+				if err := os.RemoveAll(cache); err != nil {
+					b.Errorf("failed to remove dir: %v", err)
+				}
+				b.StartTimer()
+			}
+
+			b.StopTimer()
+			if err := os.RemoveAll(src); err != nil {
+				b.Errorf("failed to remove: %v", src)
+			}
+		})
+	}
+}
+
+func BenchmarkDiskParallel(b *testing.B) {
+	tdir := b.TempDir()
+
+	for _, n := range []int{16, 256, 65536, 65536 * 4} {
+		n := n
+		var hashes []string
+		for i := 0; i < n; i++ {
+			hashes = append(hashes, hash(i))
+		}
+		b.Run(fmt.Sprintf("with %d files", n), func(b *testing.B) {
+			src := filepath.Join(tdir, "src")
+
+			if err := os.MkdirAll(src, 0700); err != nil {
+				b.Errorf("failed to create dir: %v", err)
+			}
+
+			for j := 0; j < n; j++ {
+				if err := ioutil.WriteFile(filepath.Join(src, hashes[j]), nil, 0600); err != nil {
+					b.Errorf("failed to write file: %v", err)
+				}
+			}
+
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+				cache := filepath.Join(tdir, "cache")
+				if err := os.MkdirAll(cache, 0700); err != nil {
+					b.Errorf("failed to create dir: %v", err)
+				}
+
+				c := make(chan struct{}, 16)
+				b.StartTimer()
+				var wg sync.WaitGroup
+
+				for j := 0; j < n; j++ {
+					c <- struct{}{}
+					j := j
+					wg.Add(1)
+					go func() {
+						defer wg.Done()
+						defer func() { <-c }()
+						h := hashes[j]
+						if err := os.Link(filepath.Join(src, h), filepath.Join(cache, h)); err != nil {
+							b.Errorf("failed to link file: %v", err)
+						}
+					}()
+				}
+				wg.Wait()
+
+				b.StopTimer()
+				if err := os.RemoveAll(cache); err != nil {
+					b.Errorf("failed to remove dir: %v", err)
+				}
+				b.StartTimer()
+			}
+
+			b.StopTimer()
+			if err := os.RemoveAll(src); err != nil {
+				b.Errorf("failed to remove: %v", src)
+			}
+		})
+	}
 }
