5d635ac9fb70810b435dfe2efced58d148dc547d
Junji Watanabe <jwata@google.com>
2020-08-27 16:02:59 +0900

    [casviewer] implement ClientCache to reuse a Client for the same instance.
    CacheClient keeps Clients, one per instance.
    A client will be reused for the same instance.
    
    Bug: 1121471
    Change-Id: I0b741a4f9a137b29ae3f7060090658ae620997d4

--- casviewer/client.go
+++ casviewer/client.go
@@ -16,12 +16,101 @@ package casviewer
 
 import (
 	"context"
+	"sync"
 
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/client"
 
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/router"
 )
+
+// clientCacheKey is a context key type for ClientCache value.
+type clientCacheKey struct{}
+
+// ccKey is a context key for ClientCache value.
+var ccKey = &clientCacheKey{}
+
+// ClientCache caches CAS clients, one per an instance.
+type ClientCache struct {
+	lock    sync.RWMutex
+	clients map[string]*client.Client
+	ctx     context.Context
+}
+
+// NewClientCache initializes ClientCache.
+func NewClientCache(ctx context.Context) *ClientCache {
+	return &ClientCache{
+		clients: make(map[string]*client.Client),
+		ctx:     ctx,
+	}
+}
+
+// Get returns a Client by loading it from cache or creating a new one.
+func (cc *ClientCache) Get(instance string) (*client.Client, error) {
+	// Load Client from cache.
+	cc.lock.RLock()
+	cl, ok := cc.clients[instance]
+	cc.lock.RUnlock()
+
+	if ok {
+		return cl, nil
+	}
+
+	cc.lock.Lock()
+	defer cc.lock.Unlock()
+
+	// Somebody may have already set a client for the same instance.
+	cl, ok = cc.clients[instance]
+	if ok {
+		return cl, nil
+	}
+
+	cl, err := NewClient(cc.ctx, instance)
+	if err != nil {
+		return nil, err
+	}
+
+	// Cache the client.
+	cc.clients[instance] = cl
+	return cl, nil
+}
+
+// Clear closes Clients gracefully, and removes them from cache.
+func (cc *ClientCache) Clear() {
+	cc.lock.Lock()
+	defer cc.lock.Unlock()
+	for inst, cl := range cc.clients {
+		cl.Close()
+		delete(cc.clients, inst)
+	}
+}
+
+// withClientCacheMW creates a middleware that injects the ClientCache to context.
+func withClientCacheMW(cc *ClientCache) router.Middleware {
+	return func(c *router.Context, next router.Handler) {
+		c.Context = context.WithValue(c.Context, ccKey, cc)
+		next(c)
+	}
+}
+
+// GetClient returns a Client by loading it from cache or creating a new one.
+func GetClient(c context.Context, instance string) (*client.Client, error) {
+	cc, err := clientCache(c)
+	if err != nil {
+		return nil, err
+	}
+	return cc.Get(instance)
+}
+
+// clientCache returns ClientCache by retrieving it from the context.
+func clientCache(c context.Context) (*ClientCache, error) {
+	cc, ok := c.Value(ccKey).(*ClientCache)
+	if !ok {
+		return nil, errors.New("ClientCache not installed in the context")
+	}
+	return cc, nil
+}
 
 // NewClient connects to the instance of remote execution service, and returns a client.
 func NewClient(ctx context.Context, instance string) (*client.Client, error) {
---
+++ casviewer/client_test.go
@@ -0,0 +1,85 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package casviewer
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"go.chromium.org/luci/server/auth/authtest"
+	"go.chromium.org/luci/server/router"
+)
+
+func TestClient(t *testing.T) {
+	t.Parallel()
+
+	Convey("ClientCache", t, func() {
+		Convey("ForInstance", func() {
+			c := newContext()
+			inst1 := "projects/test-proj/instances/inst1"
+			inst2 := "projects/test-proj/instances/inst2"
+
+			// First time, it creates a new client.
+			cl1, err := GetClient(c.Context, inst1)
+			So(err, ShouldBeNil)
+			So(cl1, ShouldNotBeNil)
+
+			// The client should be reused for the same instance.
+			cl2, err := GetClient(c.Context, inst1)
+			So(err, ShouldBeNil)
+			So(cl2, ShouldEqual, cl1)
+
+			// A new client for a different instance will be created.
+			cl3, err := GetClient(c.Context, inst2)
+			So(err, ShouldBeNil)
+			So(cl3, ShouldNotBeNil)
+			So(cl3, ShouldNotEqual, cl1)
+		})
+
+		Convey("Clear", func() {
+			c := newContext()
+			inst1 := "projects/test-proj/instances/inst1"
+			inst2 := "projects/test-proj/instances/inst2"
+
+			// Create clients.
+			var err error
+			_, err = GetClient(c.Context, inst1)
+			So(err, ShouldBeNil)
+			_, err = GetClient(c.Context, inst2)
+			So(err, ShouldBeNil)
+
+			cc, err := clientCache(c.Context)
+			So(err, ShouldBeNil)
+			cc.Clear()
+
+			So(cc.clients, ShouldBeEmpty)
+		})
+	})
+}
+
+// newContext creats a fake context.
+func newContext() *router.Context {
+	cc := NewClientCache(context.Background())
+
+	ctx := context.Background()
+	authtest.MockAuthConfig(ctx)
+	c := &router.Context{
+		Context: ctx,
+	}
+	withClientCacheMW(cc)(c, func(_ *router.Context) {})
+	return c
+}
