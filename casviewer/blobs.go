0eaecd946f2cb92636f367ddd7010c18ac5d39de
Junji Watanabe <jwata@google.com>
2020-08-28 23:46:03 +0900

    retrieve blob
    Change-Id: Id099343e8bdced66e4cd4926c2a52acc2dc6f130

---
+++ casviewer/blobs.go
@@ -0,0 +1,100 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package casviewer
+
+import (
+	"fmt"
+	"net/http"
+
+	repb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/server/router"
+)
+
+func treeHandler(c *router.Context) {
+	b, err := readBlob(c)
+	if err != nil {
+		// Error response/logs have already been written in readBlob.
+		return
+	}
+
+	d := &repb.Directory{}
+	err = proto.Unmarshal(b, d)
+	if err != nil {
+		// TODO(crbug.com/1121471): render 400 html.
+		http.Error(c.Writer, "Error: Blob must be Directory", http.StatusBadRequest)
+		return
+	}
+
+	renderDirectory(c, d)
+}
+
+func getHandler(c *router.Context) {
+	b, err := readBlob(c)
+	if err != nil {
+		// Error response/logs have already been written in readBlob.
+		return
+	}
+
+	// TODO(crbug.com/1121471): append appropriate headers.
+	c.Writer.Write(b)
+}
+
+func readBlob(c *router.Context) ([]byte, error) {
+	bd, err := blobDigest(c.Params)
+	if err != nil {
+		http.Error(c.Writer, "Error: Invalid blob size", http.StatusBadRequest)
+		return nil, err
+	}
+	cl, err := GetClient(c.Context, fullInstName(c.Params))
+	code := status.Code(err)
+	if err != nil {
+		logging.Errorf(c.Context, "failed to get CAS client: %s", err)
+		// TODO(crbug.com/1121471): render 500 html.
+		errMsg := fmt.Sprintf("Error: %s", code.String())
+		http.Error(c.Writer, errMsg, http.StatusInternalServerError)
+		return nil, err
+	}
+	b, err := cl.ReadBlob(c.Context, *bd)
+	code = status.Code(err)
+	if code == codes.NotFound {
+		// TODO(crbug.com/1121471): render 404 html.
+		http.Error(c.Writer, "Error: Not Found", http.StatusNotFound)
+		return nil, err
+	}
+	if err != nil {
+		logging.Errorf(c.Context, "failed to read blob: %s", err)
+		// TODO(crbug.com/1121471): render 500 html.
+		errMsg := fmt.Sprintf("Error: %s", code.String())
+		http.Error(c.Writer, errMsg, http.StatusInternalServerError)
+		return nil, err
+	}
+	return b, nil
+}
+
+func renderDirectory(c *router.Context, d *repb.Directory) {
+	// TODO(crbug.com/1121471): render html.
+	dirs := d.GetDirectories()
+	c.Writer.Write([]byte(fmt.Sprintf("dirs: %v", dirs)))
+	files := d.GetFiles()
+	c.Writer.Write([]byte(fmt.Sprintf("files: %v", files)))
+
+	links := d.GetSymlinks()
+	c.Writer.Write([]byte(fmt.Sprintf("symlinks %v", links)))
+}
