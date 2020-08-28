6948e7a4f32c6e923e7d487038645a545d4911b7
Junji Watanabe <jwata@google.com>
2020-08-29 00:37:25 +0900

    retrieve blob
    Change-Id: Id099343e8bdced66e4cd4926c2a52acc2dc6f130

---
+++ casviewer/blobs.go
@@ -0,0 +1,122 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package casviewer
+
+import (
+	"fmt"
+	"net/http"
+
+	repb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/server/router"
+)
+
+// getHandler renders the requeted directory, or renders an error page.
+func treeHandler(c *router.Context) {
+	b, err := readBlob(c)
+	if err != nil {
+		// Error response/logs have already been written in readBlob.
+		return
+	}
+
+	d := &repb.Directory{}
+	err = proto.Unmarshal(b, d)
+	if err != nil {
+		renderBadRequest(c, "Blob must be Directory")
+		return
+	}
+
+	renderDirectory(c, d)
+}
+
+// getHandler writes the requeted blob to response, or renders an error page.
+func getHandler(c *router.Context) {
+	b, err := readBlob(c)
+	if err != nil {
+		// Error response/logs have already been written in readBlob.
+		return
+	}
+
+	// TODO(crbug.com/1121471): append appropriate headers.
+	c.Writer.Write(b)
+}
+
+// readBlob returns a blob from CAS, or renders an error page.
+func readBlob(c *router.Context) ([]byte, error) {
+	bd, err := blobDigest(c.Params)
+	if err != nil {
+		renderBadRequest(c, "Digest size must be number")
+		return nil, err
+	}
+	cl, err := GetClient(c.Context, fullInstName(c.Params))
+	code := status.Code(err)
+	if err != nil {
+		logging.Errorf(c.Context, "failed to get CAS client: %s", err)
+		renderInternalServerError(c, code.String())
+		return nil, err
+	}
+	b, err := cl.ReadBlob(c.Context, *bd)
+	code = status.Code(err)
+	if code == codes.NotFound {
+		renderNotFound(c)
+		return nil, err
+	}
+	if code == codes.InvalidArgument {
+		renderBadRequest(c, "Invalid blob digest")
+		return nil, err
+	}
+	if err != nil {
+		logging.Errorf(c.Context, "failed to read blob: %s", err)
+		renderInternalServerError(c, code.String())
+		return nil, err
+	}
+	return b, nil
+}
+
+// renderDirectory renders a directory page.
+func renderDirectory(c *router.Context, d *repb.Directory) {
+	// TODO(crbug.com/1121471): render html.
+
+	dirs := d.GetDirectories()
+	c.Writer.Write([]byte(fmt.Sprintf("dirs: %v\n", dirs)))
+	files := d.GetFiles()
+	c.Writer.Write([]byte(fmt.Sprintf("files: %v\n", files)))
+	links := d.GetSymlinks()
+	c.Writer.Write([]byte(fmt.Sprintf("symlinks %v\n", links)))
+}
+
+// renderNotFound renders 400 BadRequest page.
+func renderBadRequest(c *router.Context, errMsg string) {
+	// TODO(crbug.com/1121471): render 404 html.
+	m := fmt.Sprintf("Error: Bad Request. %s", errMsg)
+	http.Error(c.Writer, m, http.StatusBadRequest)
+}
+
+// renderNotFound renders 404 NotFound page.
+func renderNotFound(c *router.Context) {
+	// TODO(crbug.com/1121471): render 400 html.
+	http.Error(c.Writer, "Error: Not Found", http.StatusNotFound)
+}
+
+// renderInternalServerError renders 500 InternalServerError page.
+func renderInternalServerError(c *router.Context, errMsg string) {
+	// TODO(crbug.com/1121471): render 500 html.
+	m := fmt.Sprintf("Error: %s", errMsg)
+	http.Error(c.Writer, m, http.StatusInternalServerError)
+}
--- casviewer/handlers.go
+++ casviewer/handlers.go
@@ -16,10 +16,11 @@ package casviewer
 
 import (
 	"fmt"
-	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/digest"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/server/router"
 )
@@ -34,45 +35,32 @@ func InstallHandlers(r *router.Router, cc *ClientCache) {
 
 	r.GET("/", baseMW, rootHanlder)
 	r.GET("/projects/:project/instances/:instance/blobs/:hash/:size/tree", blobMW, treeHandler)
-	r.GET("/projects/:project/instances/:instance/blobs/:hash/:size/", blobMW, getHandler)
+	r.GET("/projects/:project/instances/:instance/blobs/:hash/:size", blobMW, getHandler)
+}
+
+// fullInstName construcs a full instance name from the URL parameters.
+func fullInstName(p httprouter.Params) string {
+	return fmt.Sprintf(
+		"projects/%s/instances/%s", p.ByName("project"), p.ByName("instance"))
+}
+
+// blobDigest construcs a Digest from the URL parameters.
+func blobDigest(p httprouter.Params) (*digest.Digest, error) {
+	size, err := strconv.ParseInt(p.ByName("size"), 10, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	d := &digest.Digest{
+		Hash: p.ByName("hash"),
+		Size: size,
+	}
+
+	return d, nil
 }
 
 func rootHanlder(c *router.Context) {
 	// TODO(crbug.com/1121471): Add top page.
 	logging.Debugf(c.Context, "Hello world")
 	c.Writer.Write([]byte("Hello, world. This is CAS Viewer."))
-}
-
-func treeHandler(c *router.Context) {
-	_, err := GetClient(c.Context, fullInstanceName(c.Params))
-	if err != nil {
-		errMsg := "failed to initialize CAS client"
-		logging.Errorf(c.Context, "%s: %s", errMsg, err)
-		http.Error(c.Writer, errMsg, http.StatusInternalServerError)
-		return
-	}
-
-	// TODO(crbug.com/1121471): retrieve blob and render html.
-}
-
-func getHandler(c *router.Context) {
-	_, err := GetClient(c.Context, fullInstanceName(c.Params))
-	if err != nil {
-		errMsg := "failed to initialize CAS client"
-		logging.Errorf(c.Context, "%s: %s", errMsg, err)
-		http.Error(c.Writer, errMsg, http.StatusInternalServerError)
-		return
-	}
-
-	// TODO(crbug.com/1121471): retrieve blob.
-}
-
-func fullInstanceName(p httprouter.Params) string {
-	return fmt.Sprintf(
-		"projects/%s/instances/%s", p.ByName(":project"), p.ByName(":instance"))
-}
-
-func fullResourceName(p httprouter.Params) string {
-	return fmt.Sprintf(
-		"%s/blobs/%s/%s", fullInstanceName(p), p.ByName(":hash"), p.ByName(":size"))
 }
