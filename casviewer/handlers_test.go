124bcbe001090df9262e77b0cd2b9597218d7b68
Junji Watanabe <jwata@google.com>
2020-09-02 11:26:36 +0900

    [cas-viewer] Integrate with IAP auth method.
    Use IAPAuthMethod to validate and parse IAP's JWTs.
    https://pkg.go.dev/go.chromium.org/luci/server/auth/iap
    
    auth.CurrentUser() will be available.
    
    Bug: 1121471
    Change-Id: I81781c1c230da9b34cdc271dca10be0fd66d57bf

--- casviewer/handlers_test.go
+++ casviewer/handlers_test.go
@@ -16,53 +16,72 @@ package casviewer
 
 import (
 	"context"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"go.chromium.org/luci/server/router"
-
 	. "github.com/smartystreets/goconvey/convey"
+
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/auth/authtest"
+	"go.chromium.org/luci/server/router"
 )
 
 func TestHandlers(t *testing.T) {
 	t.Parallel()
 
 	Convey("InstallHandlers", t, func() {
+		// Install handlers with fake auth settings.
 		r := router.New()
-		InstallHandlers(r, nil)
-
-		srv := httptest.NewServer(r)
-
-		resp, err := http.Get(srv.URL)
-		So(err, ShouldBeNil)
-		defer resp.Body.Close()
-		So(resp.StatusCode, ShouldEqual, http.StatusOK)
-	})
-
-	Convey("treeHandler", t, func() {
-		r := router.New()
+		r.Use(router.NewMiddlewareChain(func(c *router.Context, next router.Handler) {
+			c.Context = authtest.MockAuthConfig(c.Context)
+			db := authtest.NewFakeDB()
+			c.Context = db.Use(c.Context)
+			next(c)
+		}))
 		cc := NewClientCache(context.Background())
-		InstallHandlers(r, cc)
+		t.Cleanup(cc.Clear)
 
+		InstallHandlers(r, cc, fakeAuthMiddleware())
 		srv := httptest.NewServer(r)
+		t.Cleanup(srv.Close)
 
-		resp, err := http.Get(srv.URL + "/projects/test-proj/instances/default_instance/blobs/12345/6/tree")
-		So(err, ShouldBeNil)
-		defer resp.Body.Close()
-		So(resp.StatusCode, ShouldEqual, http.StatusOK)
+		Convey("rootHanlder", func() {
+			resp, err := http.Get(srv.URL)
+			So(err, ShouldBeNil)
+			defer resp.Body.Close()
+
+			So(resp.StatusCode, ShouldEqual, http.StatusOK)
+			// Body should contain user email address.
+			body, err := ioutil.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+			So(string(body), ShouldContainSubstring, "user@example.com")
+		})
+
+		Convey("treeHandler", func() {
+			resp, err := http.Get(
+				srv.URL + "/projects/test-proj/instances/default_instance/blobs/12345/6/tree")
+			So(err, ShouldBeNil)
+			defer resp.Body.Close()
+			So(resp.StatusCode, ShouldEqual, http.StatusOK)
+		})
+
+		Convey("getHandler", func() {
+			resp, err := http.Get(
+				srv.URL + "/projects/test-proj/instances/default_instance/blobs/12345/6")
+			So(err, ShouldBeNil)
+			defer resp.Body.Close()
+			So(resp.StatusCode, ShouldEqual, http.StatusOK)
+		})
 	})
 
-	Convey("getHandler", t, func() {
-		r := router.New()
-		cc := NewClientCache(context.Background())
-		InstallHandlers(r, cc)
+}
 
-		srv := httptest.NewServer(r)
-
-		resp, err := http.Get(srv.URL + "/projects/test-proj/instances/default_instance/blobs/12345/6")
-		So(err, ShouldBeNil)
-		defer resp.Body.Close()
-		So(resp.StatusCode, ShouldEqual, http.StatusOK)
-	})
+func fakeAuthMiddleware() router.Middleware {
+	return auth.Authenticate(&authtest.FakeAuth{
+		User: &auth.User{
+			Identity: "user:user@example.com",
+			Email:    "user@example.com",
+		}})
 }
