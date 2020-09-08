4a3076c54a7854422894040548c40d2cbbdee767
Weiwei Lin <weiweilin@google.com>
2020-09-09 09:48:59 +1000

    [resultui] enable dev-server on development mode only
    dev-server depends on files in dev-configs, which are not checked in.
    This breaks builds on bots.
    Limit dev-server to local dev mode only so prod builds do not rely on
    those files.
    
    Change-Id: I34954205f627bb599dcaa01a4cb09024ad07b5f3

--- milo/frontend/resultui/webpack.dev.ts
+++ milo/frontend/resultui/webpack.dev.ts
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import fs from 'fs';
+import path from 'path';
+
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import webpack from 'webpack';
 import merge from 'webpack-merge';
 
@@ -20,6 +24,33 @@ import common from './webpack.common';
 const config: webpack.Configuration = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
+
+  devServer: {
+    contentBase: path.join(__dirname, './out/'),
+    historyApiFallback: true,
+    https: {
+      key: fs.readFileSync(path.join(__dirname, 'dev-configs/cert.key')),
+      cert: fs.readFileSync(path.join(__dirname, 'dev-configs/cert.pem')),
+    },
+    before: (app) => {
+      const appConfigs = require('./dev-configs/configs.json');
+      app.get('/configs.js', async (_req, res) => {
+        res.set('context-type', 'application/javascript');
+        const configsTemplate = fs.readFileSync('./configs.template.js', 'utf8');
+        const config = configsTemplate
+          .replace('{{.ResultDB.Host}}', appConfigs.RESULT_DB.HOST)
+          .replace('{{.Buildbucket.Host}}', appConfigs.BUILDBUCKET.HOST)
+          .replace('{{.OAuth2.ClientID}}', appConfigs.OAUTH2.CLIENT_ID);
+        res.send(config);
+      });
+
+      const localDevConfigs = require('./dev-configs/local-dev-configs.json');
+      app.use(/^(?!\/(ui|static\/(dist|style))\/).*/, createProxyMiddleware({
+        target: localDevConfigs.milo.url,
+        changeOrigin: true,
+      }));
+    },
+  },
 });
 
 // Default export is required by webpack.
