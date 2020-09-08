4dc8cb3a1ae950ef640cb3cdedc460dbeb3fda65
Ye Kuang <yekuang@google.com>
2020-09-08 15:52:33 +0900

    [isolate] Fork the tests for cas.Uploader to `isolate` lib
    Uploader tests are in https://source.chromium.org/chromium/infra/infra/+/master:go/src/go.chromium.org/luci/client/cas/uploader_test.go
    
    I added a very naive uploadToCASCore() function so that we can write some tests
    against it. Later on I'd like to move the part in cas that is isolate-specific
    into this function. Especially, I'm talking about cas's buildInputSpec:
    https://source.chromium.org/chromium/infra/infra/+/master:go/src/go.chromium.org/luci/client/cas/uploader.go;l=62-81;drc=c03fd238720f595234abeee4f1fd50f115ba6013
    This is to decouple the cas pacakge from isolate
    
    Bug: 924625
    Change-Id: Id5a2f7963cb5ee6e38f395291bd6aa624d4e1888

--- client/cmd/isolate/lib/common_test.go
+++ client/cmd/isolate/lib/common_test.go
@@ -15,9 +15,20 @@
 package lib
 
 import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
 	"testing"
 
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/digest"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/fakes"
+	repb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
+	"github.com/golang/protobuf/proto"
 	. "github.com/smartystreets/goconvey/convey"
+
+	"go.chromium.org/luci/client/isolate"
 )
 
 func TestElideNestedPaths(t *testing.T) {
@@ -85,4 +96,175 @@ func TestElideNestedPaths(t *testing.T) {
 		// Make sure "a/b" elides neither "a/bc" nor "a/bcd"
 		So(doElision(deps), ShouldResemble, []string{"a/b", "a/bc", "a/bcd", "a/c"})
 	})
+}
+
+func TestUploadToCAS(t *testing.T) {
+	fooContent := []byte("foo")
+	barContent := []byte("bar")
+	bazContent := []byte("baz")
+	fooDg := digest.NewFromBlob(fooContent)
+	barDg := digest.NewFromBlob(barContent)
+	bazDg := digest.NewFromBlob(bazContent)
+
+	t.Parallel()
+	Convey(`Basic`, t, func() {
+		tmpDir := t.TempDir()
+		isol1Content := `{
+		  'variables': {
+		    'files': [
+		      'foo',
+		      'bar',
+		    ],
+		  },
+		}`
+		isol2Content := `{
+		  'variables': {
+		    'files': [
+		      'foo2',
+		      'baz',
+		    ],
+		  },
+		}`
+
+		writeFile(tmpDir, "foo", fooContent)
+		writeFile(tmpDir, "foo2", fooContent)
+		writeFile(tmpDir, "bar", barContent)
+		writeFile(tmpDir, "baz", bazContent)
+		isol1Path := writeFile(tmpDir, "isol1.isolate", []byte(isol1Content))
+		isol2Path := writeFile(tmpDir, "isol2.isolate", []byte(isol2Content))
+
+		e, cleanup := fakes.NewTestEnv(t)
+		defer cleanup()
+		dgs, err := uploadToCASCore(context.Background(), e.Client.GrpcClient, &isolate.ArchiveOptions{
+			Isolate: isol1Path,
+		}, &isolate.ArchiveOptions{
+			Isolate: isol2Path,
+		})
+		So(err, ShouldBeNil)
+		So(dgs, ShouldHaveLength, 2)
+
+		cas := e.Server.CAS
+
+		isol1Dir := &repb.Directory{Files: []*repb.FileNode{
+			{Name: "bar", Digest: barDg.ToProto()},
+			{Name: "foo", Digest: fooDg.ToProto()},
+		}}
+		isol2Dir := &repb.Directory{Files: []*repb.FileNode{
+			{Name: "baz", Digest: bazDg.ToProto()},
+			{Name: "foo2", Digest: fooDg.ToProto()},
+		}}
+		blob, ok := cas.Get(dgs[0])
+		So(ok, ShouldBeTrue)
+		So(blob, ShouldResemble, mustMarshal(isol1Dir))
+		blob, ok = cas.Get(dgs[1])
+		So(ok, ShouldBeTrue)
+		So(blob, ShouldResemble, mustMarshal(isol2Dir))
+
+		So(cas.BlobWrites(fooDg), ShouldEqual, 1)
+		So(cas.BlobWrites(barDg), ShouldEqual, 1)
+		So(cas.BlobWrites(bazDg), ShouldEqual, 1)
+	})
+
+	Convey(`No upload if already on the server`, t, func() {
+		tmpDir := t.TempDir()
+		isol1Content := `{
+		  'variables': {
+		    'files': [
+		      'foo',
+		      'bar',
+		      'baz',
+		    ],
+		  },
+		}`
+
+		writeFile(tmpDir, "foo", fooContent)
+		writeFile(tmpDir, "bar", barContent)
+		writeFile(tmpDir, "baz", bazContent)
+		isol1Path := writeFile(tmpDir, "isol1.isolate", []byte(isol1Content))
+
+		e, cleanup := fakes.NewTestEnv(t)
+		defer cleanup()
+		// Upload `foo` and `bar` to the server
+		cas := e.Server.CAS
+		cas.Put(fooContent)
+		cas.Put(barContent)
+
+		dgs, err := uploadToCASCore(context.Background(), e.Client.GrpcClient, &isolate.ArchiveOptions{
+			Isolate: isol1Path,
+		})
+		So(err, ShouldBeNil)
+		So(dgs, ShouldHaveLength, 1)
+
+		isol1Dir := &repb.Directory{Files: []*repb.FileNode{
+			{Name: "bar", Digest: barDg.ToProto()},
+			{Name: "baz", Digest: bazDg.ToProto()},
+			{Name: "foo", Digest: fooDg.ToProto()},
+		}}
+		blob, ok := cas.Get(dgs[0])
+		So(ok, ShouldBeTrue)
+		So(blob, ShouldResemble, mustMarshal(isol1Dir))
+
+		So(cas.BlobWrites(fooDg), ShouldEqual, 0)
+		So(cas.BlobWrites(barDg), ShouldEqual, 0)
+		So(cas.BlobWrites(bazDg), ShouldEqual, 1)
+	})
+
+	Convey(`Filter files`, t, func() {
+		tmpDir := t.TempDir()
+		isol1Content := `{
+		  'variables': {
+		    'files': [
+		      'filtered/foo',
+		      'filtered/foo2',
+		      'bar',
+		    ],
+		  },
+		}`
+
+		filteredDir := filepath.Join(tmpDir, "filtered")
+		So(os.Mkdir(filteredDir, 0700), ShouldBeNil)
+		writeFile(filteredDir, "foo", fooContent)
+		writeFile(filteredDir, "foo2", fooContent)
+		writeFile(tmpDir, "bar", barContent)
+		isol1Path := writeFile(tmpDir, "isol1.isolate", []byte(isol1Content))
+
+		e, cleanup := fakes.NewTestEnv(t)
+		defer cleanup()
+
+		filteredRe := filepath.Join("filtered", "foo")
+		if runtime.GOOS == "windows" {
+			// Need to escape `\` on Windows
+			filteredRe = `filtered\\foo`
+		}
+		dgs, err := uploadToCASCore(context.Background(), e.Client.GrpcClient, &isolate.ArchiveOptions{
+			Isolate:             isol1Path,
+			IgnoredPathFilterRe: filteredRe,
+		})
+		So(err, ShouldBeNil)
+		So(dgs, ShouldHaveLength, 1)
+
+		// `foo*` files are filtered away
+		isol1Dir := &repb.Directory{Files: []*repb.FileNode{
+			{Name: "bar", Digest: barDg.ToProto()},
+		}}
+		cas := e.Server.CAS
+		blob, ok := cas.Get(dgs[0])
+		So(ok, ShouldBeTrue)
+		So(blob, ShouldResemble, mustMarshal(isol1Dir))
+
+		So(cas.BlobWrites(fooDg), ShouldEqual, 0)
+		So(cas.BlobWrites(barDg), ShouldEqual, 1)
+	})
+}
+
+func mustMarshal(p proto.Message) []byte {
+	b, err := proto.Marshal(p)
+	So(err, ShouldBeNil)
+	return b
+}
+
+func writeFile(dir, name string, content []byte) string {
+	p := filepath.Join(dir, name)
+	So(ioutil.WriteFile(p, content, 0600), ShouldBeNil)
+	return p
 }
