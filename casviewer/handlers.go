a2023ce9ee05e3996822d40c637c08ccf85fd472
Junji Watanabe <jwata@google.com>
2020-09-09 06:23:42 +0000

    [cas-viewer] render home page using templates
    It will look like this.
    https://7088-c03fd23-tainted-jwata-use-template-dot-cas-viewer-dev.appspot.com/
    
    No style is applied, yet.
    
    Bug: 1121471
    Change-Id: Id809b35c8a7fed418f40efd102085b4504638651
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2397024
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Auto-Submit: Junji Watanabe <jwata@google.com>

--- casviewer/handlers.go
+++ casviewer/handlers.go
@@ -19,31 +19,52 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/digest"
 	"github.com/julienschmidt/httprouter"
+	"google.golang.org/appengine"
 
 	"go.chromium.org/luci/common/errors"
-	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/grpc/grpcutil"
 	"go.chromium.org/luci/server/auth"
 	"go.chromium.org/luci/server/auth/realms"
 	"go.chromium.org/luci/server/router"
+	"go.chromium.org/luci/server/templates"
 )
+
+// Path to the templates dir from the executable.
+const templatePath = "templates"
 
 var permMintToken = realms.RegisterPermission("luci.serviceAccounts.mintToken")
 
 // InstallHandlers install CAS Viewer handlers to the router.
 func InstallHandlers(r *router.Router, cc *ClientCache) {
-	baseMW := router.NewMiddlewareChain()
+	baseMW := router.NewMiddlewareChain(
+		templates.WithTemplates(getTemplateBundle()),
+	)
 	blobMW := baseMW.Extend(
 		checkPermission,
 		withClientCacheMW(cc),
 	)
 
-	r.GET("/", baseMW, rootHanlder)
+	r.GET("/", baseMW, rootHandler)
 	r.GET("/projects/:project/instances/:instance/blobs/:hash/:size/tree", blobMW, treeHandler)
 	r.GET("/projects/:project/instances/:instance/blobs/:hash/:size", blobMW, getHandler)
+}
+
+// getTemplateBundles returns template Bundle with base args.
+func getTemplateBundle() *templates.Bundle {
+	return &templates.Bundle{
+		Loader:          templates.FileSystemLoader(templatePath),
+		DefaultTemplate: "base",
+		DefaultArgs: func(c context.Context, e *templates.Extra) (templates.Args, error) {
+			return templates.Args{
+				"AppVersion": strings.Split(appengine.VersionID(c), ".")[0],
+				"User":       auth.CurrentUser(c),
+			}, nil
+		},
+	}
 }
 
 // checkPermission checks if the user has permission to read the blob.
@@ -59,12 +80,9 @@ func checkPermission(c *router.Context, next router.Handler) {
 	}
 }
 
-func rootHanlder(c *router.Context) {
-	// TODO(crbug.com/1121471): Add top page.
-	logging.Debugf(c.Context, "Hello world")
-	hello := fmt.Sprintf(
-		"Hello, world. This is CAS Viewer. Identity: %v", auth.CurrentIdentity(c.Context))
-	c.Writer.Write([]byte(hello))
+// rootHandler renders top page.
+func rootHandler(c *router.Context) {
+	templates.MustRender(c.Context, c.Writer, "pages/index.html", nil)
 }
 
 func treeHandler(c *router.Context) {
