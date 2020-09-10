cda2bb56b7fcfcd6861ed72412a033c2d84a46fa
Takuto Ikuta <tikuta@chromium.org>
2020-09-10 14:53:22 +0900

    [led] specify user agent for isolate/swarming client
    This is for https://crbug.com/1074755#c15
    
    Bug: 1074755
    Change-Id: Icdc77acc2ab2591d2d0de206e23951b271dca3cf

--- led/ledcmd/clients.go
+++ led/ledcmd/clients.go
@@ -37,6 +37,17 @@ import (
 	api "go.chromium.org/luci/swarming/proto/api"
 )
 
+// ledUserAgent stores the user agent name for this CLI.
+var ledUserAgent = "led"
+
+func init() {
+	if info, err := version.GetCurrentVersion(); err != nil {
+		ledUserAgent = fmt.Sprintf("led, %s @ %s", info.PackageName, info.InstanceID)
+	} else {
+		ledUserAgent = "led, <unknown version>"
+	}
+}
+
 var isolateTestIfaceKey = "holds an isoClientIface during tests"
 
 type pendingItem interface {
@@ -81,6 +92,7 @@ func mkIsoClient(ctx context.Context, authClient *http.Client, tree *api.CASTree
 		tree.Server, isolatedclient.WithAuthClient(authClient),
 		isolatedclient.WithNamespace(tree.Namespace),
 		isolatedclient.WithRetryFactory(retry.Default),
+		isolatedclient.WithUserAgent(ledUserAgent),
 	)
 	// The archiver is pretty noisy at Info level, so we skip giving it
 	// a logging-enabled context unless the user actually requseted verbose.
@@ -115,6 +127,7 @@ func newSwarmClient(authClient *http.Client, host string) *swarming.Service {
 		panic(err)
 	}
 	swarm.BasePath = fmt.Sprintf("https://%s/_ah/api/swarming/v1/", host)
+	swarm.UserAgent = ledUserAgent
 	return swarm
 }
 
@@ -125,17 +138,13 @@ func newSwarmbucketClient(authClient *http.Client, host string) *swarmbucket.Ser
 		panic(err)
 	}
 	sbucket.BasePath = fmt.Sprintf("https://%s/_ah/api/swarmbucket/v1/", host)
+	sbucket.UserAgent = ledUserAgent
 	return sbucket
 }
 
 func newBuildbucketClient(authClient *http.Client, host string) bbpb.BuildsClient {
 	rpcOpts := prpc.DefaultOptions()
-
-	if info, err := version.GetCurrentVersion(); err != nil {
-		rpcOpts.UserAgent = fmt.Sprintf("led, %s @ %s", info.PackageName, info.InstanceID)
-	} else {
-		rpcOpts.UserAgent = "led, <unknown version>"
-	}
+	rpcOpts.UserAgent = ledUserAgent
 
 	return bbpb.NewBuildsPRPCClient(&prpc.Client{
 		C:       authClient,
