55efdbd3bae65a60d0621d1da84a35faaf816101
Weiwei Lin <weiweilin@google.com>
2020-09-10 02:12:59 +0000

    [resultui] support crbug link in commit description
    Bug: 1113995
    Change-Id: I5583b6b27e74980eef1563072231b05f9646e2fe
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2400639
    Reviewed-by: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Commit-Queue: Weiwei Lin <weiweilin@google.com>

---
+++ milo/frontend/resultui/src/libs/markdown_it_plugins/crbug_link.ts
@@ -0,0 +1,35 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import MarkdownIt from 'markdown-it';
+
+const RE_CRBUG = /\bcrbug(\.com)?(?<path>([:/]\w+)?[:/]\d+)\b/i;
+const RE_CRBUG_TAIL = /^(\.com)?([:/]\w+)?[:/]\d+\b/i;
+
+/**
+ * Support converting crbug link texts (e.g. 'crbug/123', 'crbug.com/234',
+ * 'crbug.com:proj:123') to links.
+ *
+ * You MUST enable linkify for this to work.
+ */
+export function crbugLink(md: MarkdownIt) {
+  md.linkify.add('crbug', {
+    validate: RE_CRBUG_TAIL,
+    normalize: (match) => {
+      const reMatch = RE_CRBUG.exec(match.raw)!;
+      const path = reMatch.groups!['path'].replace(':', '/');
+      match.url = 'https://crbug.com' + path;
+    },
+  });
+}
---
+++ milo/frontend/resultui/src/libs/markdown_it_plugins/crbug_link_test.ts
@@ -0,0 +1,53 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { fixture } from '@open-wc/testing';
+import { assert } from 'chai';
+import MarkdownIt from 'markdown-it';
+
+import { crbugLink } from './crbug_link';
+
+const crbugLinks = `
+crbug/123 crbug.com/234 not a link CRBUG.COM/345
+crbug/proj/456 crbug/proj/657not_a_link
+`;
+
+const md = MarkdownIt('zero', {linkify: true})
+  .enable('linkify')
+  .use(crbugLink);
+
+describe('crbug_link', () => {
+  it('can render links correctly', async () => {
+    const ele = await fixture(md.render(crbugLinks));
+
+    const anchors = ele.querySelectorAll('a');
+    assert.equal(anchors.length, 4);
+
+    const anchor1 =  anchors.item(0);
+    assert.equal(anchor1.href, 'https://crbug.com/123');
+    assert.equal(anchor1.text, 'crbug/123');
+
+    const anchor2 =  anchors.item(1);
+    assert.equal(anchor2.href, 'https://crbug.com/234');
+    assert.equal(anchor2.text, 'crbug.com/234');
+
+    const anchor3 =  anchors.item(2);
+    assert.equal(anchor3.href, 'https://crbug.com/345');
+    assert.equal(anchor3.text, 'CRBUG.COM/345');
+
+    const anchor4 =  anchors.item(3);
+    assert.equal(anchor4.href, 'https://crbug.com/proj/456');
+    assert.equal(anchor4.text, 'crbug/proj/456');
+  });
+});
