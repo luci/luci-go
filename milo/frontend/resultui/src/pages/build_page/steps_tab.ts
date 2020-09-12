70971590b13c0e29f9906fe25999ac8ef64fb493
Weiwei Lin <weiweilin@google.com>
2020-09-12 14:22:18 +1000

    [resultui] handle steps of different statuses correctly
    1. Use different icons/colors for different statuses.
    2. Add titiles to status icons.
    3. Render duration correctly.
    
    Bug: 1123362
    Change-Id: I51ab65b5f36404944e9f37a3562b0e14a36289ce

--- milo/frontend/resultui/src/pages/build_page/steps_tab.ts
+++ milo/frontend/resultui/src/pages/build_page/steps_tab.ts
@@ -42,9 +42,9 @@ export class StepsTabElement extends MobxLitElement {
 
   @computed private get noDisplayedStep() {
     if (this.showPassed) {
-      return !this.buildState.buildPageData?.steps.length;
+      return !this.buildState.buildPageData?.steps?.length;
     }
-    return !this.buildState.buildPageData?.steps.find((s) => s.status !== BuildStatus.Success);
+    return !this.buildState.buildPageData?.steps?.find((s) => s.status !== BuildStatus.Success);
   }
 
   protected render() {
@@ -82,7 +82,7 @@ export class StepsTabElement extends MobxLitElement {
         </div>
       </div>
       <div id="main">
-        ${this.buildState.buildPageData?.steps.map((step, i) => html`
+        ${this.buildState.buildPageData?.steps?.map((step, i) => html`
         <milo-build-step-entry
           style=${styleMap({'display': step.status !== BuildStatus.Success || this.showPassed ? '' : 'none'})}
           .expanded=${true}
