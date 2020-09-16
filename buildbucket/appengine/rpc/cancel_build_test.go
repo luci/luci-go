fee0fbe8eea5921b4c5b59e1d93ef3bb30366527
smut <smut@google.com>
2020-09-16 10:38:44 -0700

    [buildbucket] Export cancelled builds to BigQuery
    BigQuery export, along with Pub/Sub notification, should occur even if
    there is no Swarming task being cancelled.
    
    Bug: 1042991
    Change-Id: I9a1dc0bf89d010b6fb3dfee0a9d69acba0f53b20

--- buildbucket/appengine/rpc/cancel_build_test.go
+++ buildbucket/appengine/rpc/cancel_build_test.go
@@ -164,7 +164,7 @@ func TestCancelBuild(t *testing.T) {
 					Input:  &pb.Build_Input{},
 					Status: pb.Status_CANCELED,
 				})
-				So(sch.Tasks(), ShouldBeEmpty)
+				So(sch.Tasks(), ShouldHaveLength, 2)
 			})
 
 			Convey("ended", func() {
@@ -273,7 +273,7 @@ func TestCancelBuild(t *testing.T) {
 					Input:  &pb.Build_Input{},
 					Status: pb.Status_CANCELED,
 				})
-				So(sch.Tasks(), ShouldHaveLength, 2)
+				So(sch.Tasks(), ShouldHaveLength, 3)
 			})
 		})
 	})
