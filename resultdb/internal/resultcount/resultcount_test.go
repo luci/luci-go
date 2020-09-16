4e707b8f995c06f12e7d998e41fe525e7662f373
Chan Li <chanli@chromium.org>
2020-09-16 11:19:17 -0700

    [ResultDB] QueryTestResultStatistics: query test result counts from TestResultCount table
    Also remove the logic of updating/query Invocations.TestResultCount
    
    Bug: 1123807
    Change-Id: I318b95b0d279fb729a600bc4868d3fabe372cfcb

--- resultdb/internal/resultcount/resultcount_test.go
+++ resultdb/internal/resultcount/resultcount_test.go
@@ -26,6 +26,7 @@ import (
 	pb "go.chromium.org/luci/resultdb/proto/v1"
 
 	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
 )
 
 func TestTestResultCount(t *testing.T) {
@@ -50,5 +51,16 @@ func TestTestResultCount(t *testing.T) {
 		count, err := ReadTestResultCount(span.Single(ctx), invocations.NewIDSet(invID))
 		So(count, ShouldEqual, 30)
 		So(err, ShouldBeNil)
+	})
+
+	Convey(`No counters`, t, func() {
+		ctx := testutil.SpannerTestContext(t)
+		testutil.MustApply(ctx, insert.Invocation("inv", pb.Invocation_FINALIZED, nil))
+
+		invID := invocations.ID("inv")
+
+		count, err := ReadTestResultCount(span.Single(ctx), invocations.NewIDSet(invID))
+		So(count, ShouldEqual, 0)
+		So(err, ShouldErrLike, "no counters found")
 	})
 }
