2815c566602a89af85154d05a9d15c6bcdf55ab0
Roberto Carrillo <robertocn@chromium.org>
2020-09-17 14:20:28 -0700

    [resultdb] Implement basic features of GetTestResultHistory()
    This first iteration is intended to only serve the first page of test
    results in the given realm, matching the test predicate, and in the
    specified range of timestamps.
    
    Later iterations will implement paging and commit position support
    as well as performance optimizations.
    
    R=chanli,nodir
    
    Bug: 1074407
    Change-Id: Id1386419e6268f7a31019153d2d106b1ad096d20

--- resultdb/internal/services/resultdb/get_test_result_history.go
+++ resultdb/internal/services/resultdb/get_test_result_history.go
@@ -17,12 +17,172 @@ package resultdb
 import (
 	"context"
 
-	"go.chromium.org/luci/common/errors"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc/codes"
 
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/grpc/appstatus"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/span"
+
+	"go.chromium.org/luci/resultdb/internal/invocations"
+	"go.chromium.org/luci/resultdb/internal/testresults"
+	"go.chromium.org/luci/resultdb/pbutil"
 	pb "go.chromium.org/luci/resultdb/proto/v1"
+)
+
+const (
+	historyDefaultPageSize = 100
 )
 
 // GetTestResultHistory implements pb.ResultDBServer.
 func (s *resultDBServer) GetTestResultHistory(ctx context.Context, in *pb.GetTestResultHistoryRequest) (*pb.GetTestResultHistoryResponse, error) {
-	return nil, errors.Reason("Not yet implemented").Err()
+	if err := verifyGetTestResultHistoryPermission(ctx, in.GetRealm()); err != nil {
+		return nil, err
+	}
+	if err := validateGetTestResultHistoryRequest(in); err != nil {
+		return nil, appstatus.BadRequest(err)
+	}
+	if in.PageSize == 0 {
+		in.PageSize = historyDefaultPageSize
+	}
+	ctx, cancelTx := span.ReadOnlyTransaction(ctx)
+	defer cancelTx()
+
+	workerCtx, cancelWorker := context.WithCancel(ctx)
+	defer cancelWorker()
+	// Ignore the derived context as we have a single worker.
+	// The closure of the results channel is sufficient to know that the work
+	// is done.
+	eg, _ := errgroup.WithContext(workerCtx)
+	results := make(chan *pb.GetTestResultHistoryResponse_Entry)
+	eg.Go(func() error {
+		defer close(results)
+		// TODO(crbug.com/1074407): Instead of getting a single fixed-length
+		// page of results here, pass a callback to get as many results as needed.
+		idxInvs, err := invocations.ByTimestamp(workerCtx, in.Realm, in.GetTimeRange())
+		if err != nil {
+			return err
+		}
+
+		// TODO(crbug.com/1107678): Parallelize the following loop.
+		for _, idxInv := range idxInvs {
+			select {
+			case <-workerCtx.Done():
+				return workerCtx.Err()
+			default:
+				if err := matchingResultsInInvTree(workerCtx, in, idxInv, results); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	})
+
+	ret := &pb.GetTestResultHistoryResponse{
+		Entries: make([]*pb.GetTestResultHistoryResponse_Entry, 0, in.PageSize),
+	}
+	// Collect results sent over the results channel until either:
+	//  - The page of results is full,
+	//  - The context is Done,
+	//  - Or the worker is done with its work (and closes the resutls channel).
+	//
+	// TODO(crbug.com/1074407): Implement ordering of the results indexed
+	// together by (TestID, VariantHash) .
+	//
+	// NB: The results in the channel are already ordered by indexed timestamp.
+	//
+	// TODO(crbug.com/1074407): Implement paging support.
+ResultsLoop:
+	for {
+		select {
+		case entry, ok := <-results:
+			if !ok {
+				break ResultsLoop
+			}
+			ret.Entries = append(ret.Entries, entry)
+			if len(ret.Entries) == int(in.PageSize) {
+				cancelWorker()
+				// Ignore the cancellation returned by the worker.
+				if err := eg.Wait(); err != nil && err != context.Canceled {
+					return ret, err
+				}
+				return ret, nil
+			}
+		case <-ctx.Done():
+			break ResultsLoop
+		}
+	}
+	return ret, eg.Wait()
+}
+
+// verifyGetTestResultHistoryPermission checks that the caller has permission to
+// get test results from the specified realm.
+func verifyGetTestResultHistoryPermission(ctx context.Context, realm string) error {
+	if realm == "" {
+		return appstatus.BadRequest(errors.Reason("realm is required").Err())
+	}
+	switch allowed, err := auth.HasPermission(ctx, permListTestResults, realm); {
+	case err != nil:
+		return err
+	case !allowed:
+		return appstatus.Errorf(codes.PermissionDenied, `caller does not have permission %s in realm %q`, permListTestResults, realm)
+	}
+	return nil
+}
+
+// validateGetTestResultHistoryRequest checks that the required fields are set,
+// and that field values are valid.
+func validateGetTestResultHistoryRequest(in *pb.GetTestResultHistoryRequest) error {
+	if in.GetRealm() == "" {
+		return errors.Reason("realm is required").Err()
+	}
+	// TODO(crbug.com/1107680): Add support for commit position ranges.
+	tr := in.GetTimeRange()
+	if tr == nil {
+		return errors.Reason("time_range must be specified").Err()
+	}
+	if in.GetPageSize() < 0 {
+		return errors.Reason("page_size, if specified, must be a positive integer").Err()
+	}
+	if in.GetVariantPredicate() != nil {
+		if err := pbutil.ValidateVariantPredicate(in.GetVariantPredicate()); err != nil {
+			return errors.Annotate(err, "variant_predicate").Err()
+		}
+	}
+	return nil
+}
+
+// matchingResultsInInvTree gets the matching results reachable from a given
+// invocation, and streams them over the given channel.
+func matchingResultsInInvTree(ctx context.Context, in *pb.GetTestResultHistoryRequest, idxInv *invocations.Historical, ret chan<- *pb.GetTestResultHistoryResponse_Entry) error {
+	reachableInvs, err := invocations.Reachable(ctx, invocations.NewIDSet(idxInv.ID))
+	if err != nil {
+		return err
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	for _, batch := range reachableInvs.Batches() {
+		batch := batch
+		eg.Go(func() error {
+			// TODO(crbug.com/1107678): Implement support for FieldMask to return
+			// only a subset of each result.
+			query := testresults.Query{
+				InvocationIDs: batch,
+				Predicate: &pb.TestResultPredicate{
+					Variant:      in.VariantPredicate,
+					TestIdRegexp: in.TestIdRegexp,
+				},
+			}
+			return query.Run(ctx, func(r *pb.TestResult) error {
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				case ret <- &pb.GetTestResultHistoryResponse_Entry{Result: r, InvocationTimestamp: idxInv.IndexTimestamp}:
+					return nil
+				}
+			})
+		})
+	}
+	return eg.Wait()
 }
---
+++ resultdb/internal/services/resultdb/get_test_result_history_test.go
@@ -0,0 +1,223 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resultdb
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/spanner"
+	"github.com/golang/protobuf/ptypes"
+	"google.golang.org/grpc/codes"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/clock/testclock"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/auth/authtest"
+
+	"go.chromium.org/luci/resultdb/internal/invocations"
+	"go.chromium.org/luci/resultdb/internal/spanutil"
+	"go.chromium.org/luci/resultdb/internal/testutil"
+	"go.chromium.org/luci/resultdb/internal/testutil/insert"
+	"go.chromium.org/luci/resultdb/pbutil"
+	pb "go.chromium.org/luci/resultdb/proto/v1"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestValidateGetTestResultHistoryRequest(t *testing.T) {
+	t.Parallel()
+
+	Convey(`ValidateGetTestResultHistoryRequest`, t, func() {
+		recently, _ := ptypes.TimestampProto(testclock.TestRecentTimeUTC)
+		earlier, _ := ptypes.TimestampProto(testclock.TestRecentTimeUTC.Add(-24 * time.Hour))
+
+		Convey(`valid`, func() {
+			req := &pb.GetTestResultHistoryRequest{
+				Realm: "testproject:testrealm",
+				Range: &pb.GetTestResultHistoryRequest_TimeRange{
+					TimeRange: &pb.TimeRange{
+						Earliest: earlier,
+						Latest:   recently,
+					},
+				},
+				PageSize:     10,
+				TestIdRegexp: "ninja://:blink_web_tests/fast/.*",
+				VariantPredicate: &pb.VariantPredicate{
+					Predicate: &pb.VariantPredicate_Contains{
+						Contains: pbutil.Variant("os", "Mac-10.15"),
+					},
+				},
+			}
+			So(validateGetTestResultHistoryRequest(req), ShouldBeNil)
+		})
+
+		Convey(`missing realm`, func() {
+			req := &pb.GetTestResultHistoryRequest{
+				Range: &pb.GetTestResultHistoryRequest_TimeRange{
+					TimeRange: &pb.TimeRange{},
+				},
+			}
+			So(validateGetTestResultHistoryRequest(req), ShouldErrLike, "realm is required")
+		})
+
+		Convey(`bad pageSize`, func() {
+			req := &pb.GetTestResultHistoryRequest{
+				Realm: "testproject:testrealm",
+				Range: &pb.GetTestResultHistoryRequest_TimeRange{
+					TimeRange: &pb.TimeRange{},
+				},
+				PageSize: -10,
+			}
+			So(validateGetTestResultHistoryRequest(req), ShouldErrLike, "page_size, if specified, must be a positive integer")
+		})
+
+		Convey(`bad variant`, func() {
+			req := &pb.GetTestResultHistoryRequest{
+				Realm: "testproject:testrealm",
+				Range: &pb.GetTestResultHistoryRequest_TimeRange{
+					TimeRange: &pb.TimeRange{},
+				},
+				VariantPredicate: &pb.VariantPredicate{
+					Predicate: &pb.VariantPredicate_Contains{
+						Contains: pbutil.Variant("$os", "Mac-10.15"),
+					},
+				},
+			}
+			So(validateGetTestResultHistoryRequest(req), ShouldErrLike, "variant_predicate")
+		})
+	})
+}
+
+func TestGetTestResultHistory(t *testing.T) {
+	Convey(`TestGetTestResultHistoryRequest`, t, func() {
+		ctx := testutil.SpannerTestContext(t)
+		ctx = clock.Set(ctx, testclock.New(testclock.TestRecentTimeUTC.Add(time.Hour)))
+		ctx = auth.WithState(
+			ctx,
+			&authtest.FakeState{
+				Identity: "user:someone@example.com",
+				IdentityPermissions: []authtest.RealmPermission{
+					{Realm: "testproject:testrealm", Permission: permListTestResults},
+				},
+			})
+		srv := newTestResultDBService()
+
+		// Insert 3 indexed invocations one minute apart,
+		// with 3 unindexed child invocations each,
+		// and with 3 test results in each child invocation.
+		// 12 invocations + 9 inclusions + 27 results = 48 mutations
+		ms := make([]*spanner.Mutation, 0, 48)
+		ms = insertResultHistoryData(ms, "some-invocation", time.Duration(0), 3, 3)
+		ms = insertResultHistoryData(ms, "some-other-invocation", 2*time.Minute, 3, 3)
+		ms = insertResultHistoryData(ms, "yet-another-invocation", 2*time.Minute, 3, 3)
+		testutil.MustApply(ctx, ms...)
+
+		earliest, _ := ptypes.TimestampProto(testclock.TestRecentTimeUTC)
+		latest, _ := ptypes.TimestampProto(testclock.TestRecentTimeUTC.Add(2 * time.Minute))
+
+		req := &pb.GetTestResultHistoryRequest{
+			Range: &pb.GetTestResultHistoryRequest_TimeRange{
+				TimeRange: &pb.TimeRange{
+					Earliest: earliest,
+					Latest:   latest,
+				},
+			},
+			VariantPredicate: &pb.VariantPredicate{
+				Predicate: &pb.VariantPredicate_Contains{
+					Contains: pbutil.Variant("dummy", "true"),
+				},
+			},
+		}
+
+		Convey(`no realm`, func() {
+			res, err := srv.GetTestResultHistory(ctx, req)
+			So(err, ShouldHaveAppStatus, codes.InvalidArgument)
+			So(err, ShouldErrLike, "realm is required")
+			So(res, ShouldBeNil)
+		})
+
+		Convey(`unauthorized`, func() {
+			req.Realm = "testproject:secretrealm"
+			res, err := srv.GetTestResultHistory(ctx, req)
+			So(err, ShouldHaveAppStatus, codes.PermissionDenied)
+			So(res, ShouldBeNil)
+		})
+
+		Convey(`truncated`, func() {
+			req.Realm = "testproject:testrealm"
+			req.PageSize = 5
+			res, err := srv.GetTestResultHistory(ctx, req)
+			So(err, ShouldBeNil)
+			So(res.Entries, ShouldHaveLength, 5)
+		})
+
+		Convey(`all results`, func() {
+			req.Realm = "testproject:testrealm"
+			res, err := srv.GetTestResultHistory(ctx, req)
+			So(err, ShouldBeNil)
+			So(res.Entries, ShouldHaveLength, 27)
+			for i := 0; i < 9; i++ {
+				So(res.Entries[i].InvocationTimestamp, ShouldResembleProto, latest)
+			}
+			for i := 19; i < 27; i++ {
+				So(res.Entries[i].InvocationTimestamp, ShouldResembleProto, earliest)
+			}
+		})
+	})
+}
+
+func insertResultHistoryData(ms []*spanner.Mutation, id string, offset time.Duration, subInvs, results int) []*spanner.Mutation {
+	// Insert indexed invocation with subinvocations and results.
+	ms = insertOneResultsInv(ms, id, testclock.TestRecentTimeUTC.Add(offset), true, 0)
+	for i := 0; i < subInvs; i++ {
+		// Insert result-containing invocations contained by the above.
+		idSub := fmt.Sprintf("%s-%d", id, i+1)
+		ms = insertOneResultsInv(ms, idSub, testclock.TestRecentTimeUTC, false, results)
+		ms = append(ms, insert.Inclusion(invocations.ID(id), invocations.ID(idSub)))
+	}
+	return ms
+}
+
+// insertOneResultsInv inserts one invocation,
+// optionally indexes it by timestamp,
+// and, also optionally, adds some test results contained by it.
+func insertOneResultsInv(ms []*spanner.Mutation, id string, ts time.Time, indexed bool, results int) []*spanner.Mutation {
+	extraArgs := map[string]interface{}{
+		"CreateTime": ts,
+		"Realm":      "testproject:testrealm",
+	}
+	if indexed {
+		extraArgs["HistoryTime"] = ts
+	}
+	invID := invocations.ID(id)
+	ms = append(ms, insert.Invocation(invID, pb.Invocation_ACTIVE, extraArgs))
+	for i := 0; i < results; i++ {
+		ms = append(ms,
+			spanutil.InsertMap("TestResults", map[string]interface{}{
+				"InvocationId":    invID,
+				"TestId":          "ninja://chrome/test:foo_tests/BarTest.DoBaz",
+				"ResultId":        fmt.Sprintf("result_%d_within_inv_%s", i, id),
+				"Variant":         pbutil.Variant("result_index", fmt.Sprintf("%d", i), "dummy", "true"),
+				"VariantHash":     "deadbeef",
+				"CommitTimestamp": spanner.CommitTimestamp,
+				"Status":          pb.TestStatus_PASS,
+				"RunDurationUsec": 1534567,
+			}))
+	}
+	return ms
+}
