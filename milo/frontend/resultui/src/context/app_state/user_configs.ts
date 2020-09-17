cd886c6838ee34e69ff774ec857c06c630fb4200
Weiwei Lin <weiweilin@google.com>
2020-09-17 16:53:33 +1000

    [resultui] persist user settings
    1. Add a utility class that persists user settings to local storage.
    2. Provide user settings from PageLayoutElement.
    3. Update filter handling code accordingly.
    
    R=nqmtuan@google.com, orodley@google.com
    
    Change-Id: I6e119e76aca745c038f7ad3a770119bc060f05b6
    Bug: 1127769

---
+++ milo/frontend/resultui/src/context/app_state/user_configs.ts
@@ -0,0 +1,63 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import merge from 'lodash-es/merge';
+import { observable, reaction } from 'mobx';
+
+import { consumeContext, provideContext } from '../../libs/context';
+
+// Backward incompatible changes are not allowed.
+export interface UserConfigs {
+  showSucceededSteps: boolean;
+  showDebugLogs: boolean;
+  testFilters: {
+    showExpected: boolean;
+    showExonerated: boolean;
+    showFlaky: boolean;
+  };
+}
+
+const DEFAULT_USER_CONFIGS = Object.freeze<UserConfigs>({
+  showSucceededSteps: true,
+  showDebugLogs: false,
+  testFilters: Object.freeze({
+    showExpected: false,
+    showExonerated: true,
+    showFlaky: true,
+  }),
+});
+
+export class UserConfigsStoreV1 {
+  private static readonly KEY = 'user-configs-v1';
+
+  @observable readonly userConfigs = merge<{}, UserConfigs>({}, DEFAULT_USER_CONFIGS);
+
+  private disposer = () => {};
+
+  init() {
+    const storedConfigsStr = window.localStorage.getItem(UserConfigsStoreV1.KEY) || '{}';
+    merge(this.userConfigs, JSON.parse(storedConfigsStr));
+    this.disposer = reaction(
+      () => JSON.stringify(this.userConfigs),
+      (configsStr) => window.localStorage.setItem(UserConfigsStoreV1.KEY, configsStr),
+    );
+  }
+
+  dispose() {
+    this.disposer();
+  }
+}
+
+export const consumeUserConfigs = consumeContext<'userConfigs', UserConfigs>('userConfigs');
+export const provideUserConfigs = provideContext<'userConfigs', UserConfigs>('userConfigs');
