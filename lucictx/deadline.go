08f4445c87830872f921f09c34888752955d02c1
Robert Iannucci <iannucci@chromium.org>
2020-09-17 17:41:47 -0700

    [lucictx] Add Deadline section.
    This will allow LUCI_CONTEXT-aware applications to understand the
    requirements surrounding their impending doom.
    
    In particular, grace_period will allow applications to intelligently
    signal and wait for their own resources before forcibly shutting them
    down.
    
    I'll be adjusting the Swarming bot code to set these values in
    LUCI_CONTEXT and also making a compatible reader/writer for the
    values in luci-py.
    
    R=jwata, maruel, tikuta, vadimsh
    
    Bug: 1127089
    Change-Id: I1c9456d942624eb6c26f8af4aeb788aa451f1240

---
+++ lucictx/deadline.go
@@ -0,0 +1,176 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lucictx
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"time"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/system/signals"
+	"google.golang.org/protobuf/types/known/durationpb"
+)
+
+// AsTime returns a time.Time from this *Timestamp.
+//
+// If t is nil, this returns a "zero" time.
+func (t *Timestamp) AsTime() time.Time {
+	if t == nil {
+		return time.Time{}
+	}
+	return time.Unix(t.Seconds, int64(t.Nanos)).UTC()
+}
+
+// NewTimestamp constructs and returns a Timestamp proto message.
+//
+// If t is Zero, this returns nil.
+func NewTimestamp(t time.Time) *Timestamp {
+	if t.IsZero() {
+		return nil
+	}
+	return &Timestamp{Seconds: int64(t.Unix()), Nanos: int32(t.Nanosecond())}
+}
+
+// AdjustDeadline does a couple things:
+//   * Reads Deadline information from LUCI_CONTEXT
+//   * Adjusts Deadline.deadline down by `atLeastCleanup`
+//   * Adjusts Deadline.grace_period down by `atLeastCleanup`
+//   * Sets the adjusted deadline in the returned context
+//   * Sets the adjusted Deadline object in the returned context
+//
+// Installs a signal handler for SIGTERM/os.Interrupt to:
+//   * close `cleanup` on signal
+//   * cancel newCtx after the adjusted grace period.
+//
+// Example:
+//
+//    func MainFunc(ctx context.Context) {
+//      // deadline     = t0+5:00
+//      // grace_period = 30s
+//      cctx, cleanup, cancel := lucictx.AdjustDeadline(ctx, 5*time.Second)
+//      ScopedFunction(cctx, cleanup)
+//
+//      // Assuming ScopedFunction exits in a timely fashion on ctx.Done, you
+//      // have ~5s to do stuff here. Alternately you could wait on `<-cleanup`
+//      // and have ~30s to do stuff, but ScopedFunction may not have finished
+//      // yet.
+//    }
+//
+//    func ScopedFunction(ctx context.Context, cleanup <-chan struct{}) {
+//      // deadline     = t0+4:55
+//      // grace_period = 25s
+//      go func() {
+//        // cleanup is closed at SIGTERM or $deadline-$grace_period, whichever
+//        // is first.
+//        <-cleanup
+//        // have 25 seconds to do something (say, send SIGTERM to a child)
+//        // before ctx.Done().
+//      }()
+//    }
+//
+// NOTE: In the event that `ctx` is canceled, `cleanup` will not be closed.
+func AdjustDeadline(ctx context.Context, atLeastCleanup time.Duration) (newCtx context.Context, cleanup <-chan struct{}, cancel func()) {
+	if atLeastCleanup < 0 {
+		panic(errors.Reason("atLeastCleanup < 0: %s", atLeastCleanup).Err())
+	}
+
+	t := Deadline{}
+	_, err := Lookup(ctx, "deadline", &t)
+	if err != nil {
+		panic(err)
+	}
+
+	newCtx = ctx
+	cancel = func() {}
+
+	// first, figure out the new grace period
+	curGrace := t.GracePeriod.AsDuration()
+	newGrace := curGrace - atLeastCleanup
+	if newGrace < 0 {
+		newGrace = 0
+	}
+	t.GracePeriod = durationpb.New(newGrace)
+
+	curDeadline, _ := ctx.Deadline()
+	luciDeadline := t.Deadline.AsTime()
+
+	var adjustedDeadline time.Time
+
+	switch {
+	case curDeadline.IsZero() && !luciDeadline.IsZero():
+		adjustedDeadline = luciDeadline
+	case !curDeadline.IsZero() && luciDeadline.IsZero():
+		adjustedDeadline = curDeadline
+	case !curDeadline.IsZero() && !luciDeadline.IsZero():
+		if curDeadline.Before(luciDeadline) {
+			adjustedDeadline = curDeadline
+		} else {
+			adjustedDeadline = luciDeadline
+		}
+	}
+	if !adjustedDeadline.IsZero() {
+		adjustedDeadline = adjustedDeadline.Add(-atLeastCleanup)
+		newCtx, cancel = context.WithDeadline(newCtx, adjustedDeadline)
+		t.Deadline = NewTimestamp(adjustedDeadline)
+	}
+
+	newCtx = Set(newCtx, "deadline", &t)
+
+	cleanup = runMonitor(newCtx, cancel, newGrace)
+
+	return
+}
+
+func runMonitor(ctx context.Context, cancel func(), gracePeriod time.Duration) <-chan struct{} {
+	cleanupCh := make(chan struct{})
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, signals.Interrupts()...)
+	cleanupTimer := clock.NewTimer(ctx)
+	if deadline, ok := ctx.Deadline(); ok {
+		cleanupTimer.Reset(clock.Until(ctx, deadline) - gracePeriod)
+	}
+
+	go func() {
+		defer cancel()
+
+		doSleep := false
+
+		select {
+		case <-ctx.Done():
+		case <-cleanupTimer.GetC():
+		case <-sigCh:
+			doSleep = true
+		}
+		signal.Stop(sigCh)
+		close(cleanupCh)
+		if !cleanupTimer.Stop() {
+			<-cleanupTimer.GetC()
+		}
+
+		// If we woke up due to a signal, we'll need to cancel the context after
+		// gracePeriod. Otherwise the context's Deadline will do this for us.
+		if doSleep {
+			cleanupTimer.Reset(gracePeriod)
+			select {
+			case <-cleanupTimer.GetC():
+			case <-ctx.Done():
+			}
+		}
+	}()
+	return cleanupCh
+}
