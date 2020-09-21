8bb1d88e8963771709c576a2c649120bb4bed215
Nodir Turakulov <nodir@chromium.org>
2020-09-21 15:04:41 -0700

    [rts] Fetch rejected patchsets with tests
    Implement fetching of patchsets rejected due to test failures,
    along with the tests that caused the failure.
    
    The implementation is Chromium-specific, so keep the implementation
    in the chromium package. Fetch the patchsets and test failures
    using BigQuery, and cache them locally. Cache key is one day (in UTC),
    and the time window is aligned with day boundaries.
    
    On cold cache, it takes ~3min to execute the query and download ~300 MB
    of data (for the window of 1w), but subsequent evaluations on the same
    day are instantaneous.
    
    Bug: 1112125
    Change-Id: Iece46d6a1f9230fa6e0a740508c6619d9dc802a1

--- rts/presubmit/eval/rejected.go
+++ rts/presubmit/eval/rejected.go
@@ -16,10 +16,17 @@ package eval
 
 import (
 	"context"
+	"fmt"
+	"path/filepath"
+	"sort"
 	"time"
+
+	"go.chromium.org/luci/auth"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
 )
 
-// RejectedPatchSet is a patchset rejected by CQ.
+// RejectedPatchSet is a patchset rejected due to test failures.
 type RejectedPatchSet struct {
 	Patchset  GerritPatchset `json:"patchset"`
 	Timestamp time.Time      `json:"timestamp"`
@@ -28,13 +35,106 @@ type RejectedPatchSet struct {
 	FailedTests []*Test `json:"failedTests"`
 }
 
+// RejectedPatchSetRequest is a request to retrieve all patchsets
+// rejected due to test failures within the given time range,
+// along with tests that caused the rejection.
+type RejectedPatchSetRequest struct {
+	Context       context.Context
+	Authenticator *auth.Authenticator
+	StartTime     time.Time
+	EndTime       time.Time
+}
+
+// RejectedPatchSetProvider retrieves patchsets rejected due to test failures.
+type RejectedPatchSetProvider func(RejectedPatchSetRequest) ([]*RejectedPatchSet, error)
+
 // rejectedPatchSetSource retrieves rejected patchsets and the tests that caused
 // the rejection.
 type rejectedPatchSetSource struct {
 	*evalRun
 }
 
+// Read returns patchsets rejected due to test failures, along with tests
+// that caused the rejection.
 func (s *rejectedPatchSetSource) Read(ctx context.Context) ([]*RejectedPatchSet, error) {
-	// TODO(crbug.com/1112125): implement.
-	panic("not implemented")
+	switch {
+	case !isUTCDayStart(s.startTime):
+		return nil, errors.New("StartTime is not a UTC day start")
+	case !isUTCDayStart(s.endTime):
+		return nil, errors.New("EndTime is not a UTC day start")
+	case !s.startTime.Before(s.endTime):
+		return nil, errors.New("StartTime must be before EndTime")
+	}
+
+	ret, start := s.readCache(ctx)
+	if !start.Before(s.endTime) {
+		return ret, nil
+	}
+
+	// Fetch the rest of rejected patchsets.
+	logging.Infof(ctx, "Fetching rejected patchsets between %s and %s. It may take a couple of minutes...", start.Format(dayLayout), s.endTime.Format(dayLayout))
+	fetched, err := s.RejectedPatchSetProvider(RejectedPatchSetRequest{
+		Context:       ctx,
+		Authenticator: s.auth,
+		StartTime:     start,
+		EndTime:       s.endTime,
+	})
+	if err != nil {
+		return nil, err
+	}
+	logging.Infof(ctx, "Fetched %d patchsets", len(fetched))
+	ret = append(ret, fetched...)
+
+	// Cache results.
+	byDay := map[time.Time][]*RejectedPatchSet{}
+	for _, rp := range ret {
+		aDay := rp.Timestamp.Truncate(day)
+		byDay[aDay] = append(byDay[aDay], rp)
+	}
+	s.writeCache(ctx, byDay)
+
+	return ret, nil
+}
+
+func (s *rejectedPatchSetSource) readCache(ctx context.Context) (fps []*RejectedPatchSet, newStartTime time.Time) {
+	start := s.startTime
+	var cached []*RejectedPatchSet
+	for !start.After(s.endTime) {
+		cached = cached[:0]
+		if !s.cacheFile(start).TryRead(ctx, &cached) {
+			// We might have a gap in days, so read the rest from BigQuery.
+			break
+		}
+
+		fps = append(fps, cached...)
+		start = start.Add(day)
+	}
+
+	return fps, start
+}
+
+func (s *rejectedPatchSetSource) writeCache(ctx context.Context, byDay map[time.Time][]*RejectedPatchSet) {
+	// Sort days.
+	days := make([]time.Time, 0, len(byDay))
+	for day := range byDay {
+		days = append(days, day)
+	}
+	sort.Slice(days, func(i, j int) bool {
+		return days[i].Before(days[j])
+	})
+
+	// Write a file per day.
+	for _, day := range days {
+		s.cacheFile(day).TryWrite(ctx, byDay[day])
+	}
+}
+
+func (s *rejectedPatchSetSource) cacheFile(t time.Time) cacheFile {
+	return cacheFile(filepath.Join(s.CacheDir, fmt.Sprintf("rejected-patch-sets/%s", t.Format(dayLayout))))
+}
+
+const dayLayout = "2006-01-02"
+
+func isUTCDayStart(t time.Time) bool {
+	return t.Location() == time.UTC && t.Truncate(day).Equal(t)
 }
