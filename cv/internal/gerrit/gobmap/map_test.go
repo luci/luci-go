026bb91ddfc002510dfe59f39db2121554fc2686
Quinten Yearsley <qyearsley@chromium.org>
2020-09-22 13:47:10 -0700

    cv: Add gobmap test skeleton
    This CL adds the start of the test file but does not
    include implementation or actual tests.
    
    Bug: 1123642
    Change-Id: Ic483a60d6ec03cf988fed467e29ac43cda236aae

---
+++ cv/internal/gerrit/gobmap/map_test.go
@@ -0,0 +1,53 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gobmap
+
+import (
+	"context"
+	"testing"
+	// "time"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestParse(t *testing.T) {
+	t.Parallel()
+
+	Convey("UpdateProjectConfig", t, func() {
+		ctx := context.Background()
+
+		Convey("not implemented", func() {
+			So(
+				UpdateProjectConfig(ctx, "project-foo"),
+				ShouldErrLike, "not implemented")
+		})
+	})
+}
+
+func TestAppend(t *testing.T) {
+	t.Parallel()
+
+	Convey("Lookup", t, func() {
+		ctx := context.Background()
+
+		Convey("not implemented", func() {
+			ids, err := Lookup(
+				ctx, "foo-review.googlesource.com", "re/po", "main")
+			So(ids, ShouldBeNil)
+			So(err, ShouldErrLike, "not implemented")
+		})
+	})
+}
