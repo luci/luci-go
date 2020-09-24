796b1ac9c0bd7160a878f685fb3f5567fc2bc4a0
Yiwei Zhang <yiwzhang@google.com>
2020-09-23 22:49:09 -0700

    [cv] Setup cronjob and install handlers for config refresh
    Bug: 1129716
    Change-Id: Ib1eef5de902d8d10560f5ffc69bb609160d3535b

--- cv/appengine/main.go
+++ cv/appengine/main.go
@@ -15,18 +15,46 @@
 package main
 
 import (
-	"go.chromium.org/luci/common/logging"
+	"context"
+	"fmt"
+	"time"
+
+	"go.chromium.org/luci/appengine/gaemiddleware"
+	"go.chromium.org/luci/config/server/cfgmodule"
+	"go.chromium.org/luci/cv/appengine/internal/config"
 	"go.chromium.org/luci/server"
+	"go.chromium.org/luci/server/gaeemulation"
+	"go.chromium.org/luci/server/module"
 	"go.chromium.org/luci/server/router"
+	"go.chromium.org/luci/server/tq"
 )
 
 func main() {
-	server.Main(nil, nil, func(srv *server.Server) error {
+	mods := []module.Module{
+		cfgmodule.NewModuleFromFlags(),
+		gaeemulation.NewModuleFromFlags(),
+		tq.NewModuleFromFlags(),
+	}
 
-		srv.Routes.GET("/", router.MiddlewareChain{}, func(c *router.Context) {
-			logging.Debugf(c.Context, "Hello world")
-			c.Writer.Write([]byte("Hello, world. This is LUCI Change Verifier."))
-		})
+	server.Main(nil, mods, func(srv *server.Server) error {
+		srv.Routes.POST("/internal/cron/refresh-config",
+			router.NewMiddlewareChain(gaemiddleware.RequireCron),
+			func(rc *router.Context) {
+				// The cron job interval is 1 minute.
+				ctx, cancel := context.WithTimeout(rc.Context, 1*time.Minute)
+				defer cancel()
+				code := 200
+				body := "Successfully Submitted Refresh Tasks"
+				if err := config.SubmitRefreshTasks(ctx); err != nil {
+					// Use 202 for return code as there's no need to retry on error for
+					// cron job.
+					code = 202
+					body = err.Error()
+				}
+				rc.Writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+				rc.Writer.WriteHeader(code)
+				fmt.Fprintln(rc.Writer, body)
+			})
 
 		return nil
 	})
