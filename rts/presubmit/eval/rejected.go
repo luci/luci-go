06657aca962ad39b75aee3abcf7452f59f233dfd
Nodir Turakulov <nodir@chromium.org>
2020-09-27 20:48:46 -0700

    [rts] Fetch rejected patchsets with tests
    Implement fetching of patchsets rejected due to test failures,
    along with the tests that caused the failure.
    
    The implementation is Chromium-specific, so keep the implementation
    in the chromium package. Fetch the patchsets and test failures
    using BigQuery, and cache them locally. Cache key is one day (in UTC),
    and the time window is aligned with day boundaries.
    
    On cold cache, it takes ~3min to execute the query and download ~300 MB
    of data (for the window of 1w), but subsequent evaluations on the same
    day are instantaneous.
    
    Bug: 1112125
    Change-Id: Iece46d6a1f9230fa6e0a740508c6619d9dc802a1

--- rts/presubmit/eval/rejected.go
+++ rts/presubmit/eval/rejected.go
@@ -16,17 +16,12 @@ package eval
 
 import (
 	"context"
+	"path/filepath"
 	"time"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
 )
-
-// RejectedPatchSet is a patchset rejected by CQ.
-type RejectedPatchSet struct {
-	Patchset  GerritPatchset `json:"patchset"`
-	Timestamp time.Time      `json:"timestamp"`
-
-	// FailedTests are the tests that caused the rejection.
-	FailedTests []*Test `json:"failedTests"`
-}
 
 // rejectedPatchSetSource retrieves rejected patchsets and the tests that caused
 // the rejection.
@@ -34,7 +29,84 @@ type rejectedPatchSetSource struct {
 	*evalRun
 }
 
+// Read returns patchsets rejected due to test failures, along with tests
+// that caused the rejection.
 func (s *rejectedPatchSetSource) Read(ctx context.Context) ([]*RejectedPatchSet, error) {
-	// TODO(crbug.com/1112125): implement.
-	panic("not implemented")
+	switch {
+	case !isUTCDayStart(s.startTime):
+		return nil, errors.New("StartTime is not a UTC day start")
+	case !isUTCDayStart(s.endTime):
+		return nil, errors.New("EndTime is not a UTC day start")
+	case !s.startTime.Before(s.endTime):
+		return nil, errors.New("StartTime must be before EndTime")
+	}
+
+	// Read what we can from cache.
+	ret, start := s.readCache(ctx)
+
+	// Fetch the rest if needed.
+	if start.Before(s.endTime) {
+		logging.Infof(ctx, "Fetching rejected patchsets between %s and %s. It may take a couple of minutes...", start.Format(dayLayout), s.endTime.Format(dayLayout))
+		fetched, err := s.Backend.RejectedPatchSets(RejectedPatchSetsRequest{
+			Context:       ctx,
+			Authenticator: s.auth,
+			StartTime:     start,
+			EndTime:       s.endTime,
+		})
+		if err != nil {
+			return nil, err
+		}
+		logging.Infof(ctx, "Fetched %d patchsets", len(fetched))
+		s.writeCache(ctx, fetched)
+		ret = append(ret, fetched...)
+	}
+
+	return ret, nil
+}
+
+// readCache reads rejected patchsets from the on-disk cache.
+//
+// The returned newStartTime is the date after which the cache is missing
+// data. The caller should fetch the rest.
+func (s *rejectedPatchSetSource) readCache(ctx context.Context) (rps []*RejectedPatchSet, newStartTime time.Time) {
+	start := s.startTime
+	for !start.After(s.endTime) {
+		var cached []*RejectedPatchSet
+		if !s.cacheFile(start).TryRead(ctx, &cached) {
+			// We might have a gap in days, so fetch the rest.
+			break
+		}
+
+		rps = append(rps, cached...)
+		start = start.Add(day)
+	}
+
+	return rps, start
+}
+
+// writeCache puts patchSets to the on-disk cache.
+//
+// If a patchSet for day D is present, then all patchSets for D must be also
+// present.
+func (s *rejectedPatchSetSource) writeCache(ctx context.Context, patchSets []*RejectedPatchSet) {
+	// Group by day.
+	byDay := map[time.Time][]*RejectedPatchSet{}
+	for _, rp := range patchSets {
+		psDay := rp.Timestamp.Truncate(day)
+		byDay[psDay] = append(byDay[psDay], rp)
+	}
+
+	for day, rps := range byDay {
+		s.cacheFile(day).TryWrite(ctx, rps)
+	}
+}
+
+func (s *rejectedPatchSetSource) cacheFile(t time.Time) cacheFile {
+	return cacheFile(filepath.Join(s.CacheDir, "rejected-patch-sets", s.Backend.Name(), t.Format(dayLayout)))
+}
+
+const dayLayout = "2006-01-02"
+
+func isUTCDayStart(t time.Time) bool {
+	return t.Location() == time.UTC && t.Truncate(day).Equal(t)
 }
