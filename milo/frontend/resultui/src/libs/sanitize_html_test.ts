23ea7a05c6a5fa81c796451b5e68e33d46fea8d9
Weiwei Lin <weiweilin@google.com>
2020-09-29 05:46:59 +0000

    [resultui] fix target attributes gets sanitised
    1. Allow target attribute to be set.
    2. When target attribute is set, makes sure rel="noopener".
    3. Add unit tests for sanitize_html.ts
    
    R=nqmtuan@google.com, orodley@google.com
    
    Change-Id: I8dd4490695ba7a4df69795266f72b46b0a1a3626
    Bug: 1133069
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2434119
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>

---
+++ milo/frontend/resultui/src/libs/sanitize_html_test.ts
@@ -0,0 +1,69 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+import { fixture, html } from '@open-wc/testing';
+import { assert } from 'chai';
+
+import { sanitizeHTML } from './sanitize_html';
+
+const DIRTY_HTML = `
+<div>
+  <a href="https://www.google.com" target="_blank"></a>
+  <a href="https://www.google.com"></a>
+  <a href="https://www.google.com" target="_self"></a>
+  <a href="https://www.google.com" target="_blank" rel="nofollow"></a>
+  <a href="https://www.google.com" target="_blank" rel="noopener nofollow"></a>
+  <form target="_blank"></form>
+  <area target="_blank"></area>
+</div>
+`;
+
+describe('sanitize_html', async () => {
+  const root = await fixture(html`${sanitizeHTML(DIRTY_HTML)}`);
+  const anchors = root.querySelectorAll('a');
+
+  it('should set rel="noopener" when target attribute is set to _blank', () => {
+    const anchor = anchors.item(0);
+    assert.equal(anchor.getAttribute('rel'), 'noopener');
+  });
+
+  it('should set rel="noopener" when target attribute is not set', () => {
+    const anchor = anchors.item(1);
+    assert.equal(anchor.getAttribute('rel'), 'noopener');
+  });
+
+  it('should not set rel="noopener" when target attribute is set but not _blank', () => {
+    const anchor = anchors.item(2);
+    assert.equal(anchor.getAttribute('rel'), null);
+  });
+
+  it('should append to the existing rel attribute', () => {
+    const anchor = anchors.item(3);
+    assert.equal(anchor.getAttribute('rel'), 'nofollow noopener');
+  });
+
+  it('should not set rel="noopener" when it is already present', () => {
+    const anchor = anchors.item(4);
+    assert.equal(anchor.getAttribute('rel'), 'noopener nofollow');
+  });
+
+  it('should set rel="noopener" on <form> and <area> as well', () => {
+    const form = root.querySelector('form')!;
+    assert.equal(form.getAttribute('rel'), 'noopener');
+
+    const area = root.querySelector('area')!;
+    assert.equal(area.getAttribute('rel'), 'noopener');
+  });
+});
