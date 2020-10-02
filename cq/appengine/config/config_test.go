aaad9b8aabb4b5c2e138789f44f25d18bacb015a
Quinten Yearsley <qyearsley@chromium.org>
2020-10-02 15:33:16 -0700

    Remove references to buildbot in luci-go cq config validator
    This removes some validation that shouldn't be necessary any more.
    
    Doing this as part of triaging bugs as secondary trooper, it turns
    out there's still some more buildbot stuff to delete.
    
    Bug: 1008605
    Change-Id: I40de099ef2c73d503b55a14a51cd8ddaa8594d94

--- cq/appengine/config/config_test.go
+++ cq/appengine/config/config_test.go
@@ -465,10 +465,6 @@ func TestTryjobValidation(t *testing.T) {
 			`), ShouldErrLike, "duplicate")
 
 			So(validate(`
-			  builders {name: "*/master/c"}
-			`), ShouldErrLike, "Buildbot")
-
-			So(validate(`
 				builders {name: "m/n/o"}
 			  builders {name: "a/b/c" equivalent_to {name: "x/y/z"}}
 			`), ShouldBeNil)
