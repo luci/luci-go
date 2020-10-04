fe8889b69b4d8d3669c3a38a723b7e79ca598975
Nodir Turakulov <nodir@chromium.org>
2020-10-04 22:05:33 +0000

    [flag] Add flag.Date
    flag.Date() converts a date string like "2020-01-02" to time.Time.
    
    R=tandrii@chromium.org
    
    Change-Id: I658033cc441a2b770162730b78d339d2240ec176
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2448130
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Auto-Submit: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

---
+++ common/flag/timelayout.go
@@ -0,0 +1,54 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package flag
+
+import (
+	"flag"
+	"time"
+)
+
+// Date returns a flag.Getter which parses the flag value as a UTC date.
+// The string must be a time with layout "2006-01-24".
+func Date(t *time.Time) flag.Getter {
+	return TimeLayout(t, "2006-01-02")
+}
+
+// TimeLayout returns a flag.Getter which parses the flag value as a UTC time,
+// based on the given layout.
+func TimeLayout(t *time.Time, layout string) flag.Getter {
+	return &timeLayoutFlag{ptr: t, layout: layout}
+}
+
+type timeLayoutFlag struct {
+	ptr    *time.Time
+	layout string
+}
+
+func (f *timeLayoutFlag) String() string {
+	return f.ptr.Format(f.layout)
+}
+
+func (f *timeLayoutFlag) Get() interface{} {
+	return *f.ptr
+}
+
+func (f *timeLayoutFlag) Set(v string) error {
+	t, err := time.Parse(f.layout, v)
+	if err != nil {
+		return err
+	}
+	*f.ptr = t
+	return nil
+}
