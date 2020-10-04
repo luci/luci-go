0940a461f5338413dea9620e215171414dc74388
Nodir Turakulov <nodir@chromium.org>
2020-10-04 01:30:52 -0700

    [rts] Add rts-chromium command
    Command rts-chromium is Chromium-specific portion of RTS, packaged
    as a separate binary. It can fetch historical records for RTS
    evaluation.
    
    Also exclude Gerrit host and project from the BigQuery query because
    they are the same for all Chromium CLs.
    
    Bug: 1112125
    Change-Id: Ib7083ea05c055395b272c4162c436ff9158fa622

---
+++ rts/cmd/rts-chromium/main.go
@@ -0,0 +1,74 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/maruel/subcommands"
+
+	"go.chromium.org/luci/auth/client/authcli"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+	"go.chromium.org/luci/common/flag/fixflagpos"
+	"go.chromium.org/luci/common/logging/gologger"
+	"go.chromium.org/luci/hardcoded/chromeinfra"
+)
+
+var logCfg = gologger.LoggerConfig{
+	Format: `%{message}`,
+	Out:    os.Stderr,
+}
+
+func main() {
+	mathrand.SeedRandomly()
+	authOpt := chromeinfra.DefaultAuthOptions()
+	authOpt.Scopes = append(authOpt.Scopes, bigquery.Scope)
+	app := &cli.Application{
+		Name:  "rts-chromium",
+		Title: "RTS for Chromium.",
+		Context: func(ctx context.Context) context.Context {
+			return logCfg.Use(ctx)
+		},
+		Commands: []*subcommands.Command{
+			cmdPresubmitHistory(&authOpt),
+
+			{}, // a separator
+			authcli.SubcommandLogin(authOpt, "auth-login", false),
+			authcli.SubcommandLogout(authOpt, "auth-logout", false),
+			authcli.SubcommandInfo(authOpt, "auth-info", false),
+
+			{}, // a separator
+			subcommands.CmdHelp,
+		},
+	}
+
+	os.Exit(subcommands.Run(app, fixflagpos.FixSubcommands(os.Args[1:])))
+}
+
+type baseCommandRun struct {
+	subcommands.CommandRunBase
+}
+
+func (r *baseCommandRun) done(err error) int {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	return 0
+}
