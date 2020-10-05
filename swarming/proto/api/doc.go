0b725284137f8054e84436953257ecb825b0d0c0
Takuto Ikuta <tikuta@chromium.org>
2020-10-05 13:35:54 +0900

    [swarming/proto/api] add doc.go
    Change-Id: I784c00db13a77644baaa0c7844cefc177a675577

---
+++ swarming/proto/api/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2020 The LUCI Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package apipb provides swarming's proto api definition.
+package apipb
