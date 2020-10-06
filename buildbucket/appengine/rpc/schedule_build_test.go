073092d956600134086839a4df19e45d56904e27
smut <smut@google.com>
2020-10-06 14:42:49 -0700

    [buildbucket] Partially validate ScheduleBuild requests
    Bug: 1042991
    Change-Id: Ibf72eedced89ae17a3baa85c31b2dea354213a92

---
+++ buildbucket/appengine/rpc/schedule_build_test.go
@@ -0,0 +1,105 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"context"
+	"testing"
+
+	"go.chromium.org/luci/gae/impl/memory"
+	"go.chromium.org/luci/gae/service/datastore"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/auth/authtest"
+
+	pb "go.chromium.org/luci/buildbucket/proto"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestScheduleBuild(t *testing.T) {
+	t.Parallel()
+
+	Convey("ScheduleBuild", t, func() {
+		srv := &Builds{}
+		ctx := memory.Use(context.Background())
+		datastore.GetTestable(ctx).AutoIndex(true)
+		datastore.GetTestable(ctx).Consistent(true)
+
+		ctx = auth.WithState(ctx, &authtest.FakeState{
+			Identity: "user:caller@example.com",
+		})
+
+		Convey("ok", func() {
+			req := &pb.ScheduleBuildRequest{
+				RequestId:       "id",
+				TemplateBuildId: 1,
+			}
+			rsp, err := srv.ScheduleBuild(ctx, req)
+			So(err, ShouldBeNil)
+			So(rsp, ShouldBeNil)
+		})
+	})
+
+	Convey("validateSchedule", t, func() {
+		Convey("nil", func() {
+			err := validateSchedule(nil)
+			So(err, ShouldErrLike, "builder or template_build_id is required")
+		})
+
+		Convey("empty", func() {
+			req := &pb.ScheduleBuildRequest{}
+			err := validateSchedule(req)
+			So(err, ShouldErrLike, "builder or template_build_id is required")
+		})
+
+		Convey("request ID", func() {
+			req := &pb.ScheduleBuildRequest{
+				RequestId:       "request/id",
+				TemplateBuildId: 1,
+			}
+			err := validateSchedule(req)
+			So(err, ShouldErrLike, "request_id cannot contain")
+		})
+
+		Convey("priority", func() {
+			Convey("negative", func() {
+				req := &pb.ScheduleBuildRequest{
+					Priority:        -1,
+					TemplateBuildId: 1,
+				}
+				err := validateSchedule(req)
+				So(err, ShouldErrLike, "priority must be in")
+			})
+
+			Convey("excessive", func() {
+				req := &pb.ScheduleBuildRequest{
+					Priority:        256,
+					TemplateBuildId: 1,
+				}
+				err := validateSchedule(req)
+				So(err, ShouldErrLike, "priority must be in")
+			})
+		})
+
+		Convey("builder ID", func() {
+			req := &pb.ScheduleBuildRequest{
+				Builder: &pb.BuilderID{},
+			}
+			err := validateSchedule(req)
+			So(err, ShouldErrLike, "project must match")
+		})
+	})
+}
