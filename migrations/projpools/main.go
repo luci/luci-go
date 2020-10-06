1d21553f0dfb205b73dadad237092f50e5ee6a3a
Vadim Shtayura <vadimsh@chromium.org>
2020-10-05 18:59:24 -0700

    [migrations] A tool to extract project -> pool association.
    Greps for "pool" dimension in cr-buildbucket.cfg.
    
    Change-Id: I810df320973187d2e54518321cbdd21291950a9b

---
+++ migrations/projpools/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"sync"
+
+	"go.chromium.org/luci/auth"
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/logging/gologger"
+	"go.chromium.org/luci/common/sync/parallel"
+	"go.chromium.org/luci/config"
+	"go.chromium.org/luci/config/cfgclient"
+	"go.chromium.org/luci/config/impl/remote"
+	"go.chromium.org/luci/hardcoded/chromeinfra"
+)
+
+type projectState struct {
+	project string
+	pools   []string
+}
+
+func (s *projectState) report() {
+	if len(s.pools) == 0 {
+		return
+	}
+	fmt.Printf("%s\n", s.project)
+	for _, pool := range s.pools {
+		fmt.Printf("  %s\n", pool)
+	}
+	fmt.Printf("\n")
+}
+
+func main() {
+	ctx := context.Background()
+	ctx = gologger.StdConfig.Use(ctx)
+	if err := run(ctx); err != nil {
+		errors.Log(ctx, err)
+		os.Exit(1)
+	}
+}
+
+func withConfigClient(ctx context.Context) (context.Context, error) {
+	auth := auth.NewAuthenticator(ctx, auth.SilentLogin, chromeinfra.DefaultAuthOptions())
+	client, err := auth.Client()
+	if err != nil {
+		return nil, err
+	}
+	return cfgclient.Use(ctx, remote.New("luci-config.appspot.com", false, func(context.Context) (*http.Client, error) {
+		return client, nil
+	})), nil
+}
+
+func run(ctx context.Context) error {
+	ctx, err := withConfigClient(ctx)
+	if err != nil {
+		return err
+	}
+	projects, err := cfgclient.ProjectsWithConfig(ctx, "cr-buildbucket.cfg")
+	if err != nil {
+		return err
+	}
+	var res []*projectState
+	var m sync.Mutex
+	err = parallel.FanOutIn(func(work chan<- func() error) {
+		for _, proj := range projects {
+			proj := proj
+			work <- func() error {
+				state, err := processProject(ctx, proj)
+				if err != nil {
+					logging.Errorf(ctx, "Failed when processing %s: %s", proj, err)
+					return err
+				}
+				state.project = proj
+				m.Lock()
+				logging.Infof(ctx, "Done with %s...", proj)
+				res = append(res, state)
+				m.Unlock()
+				return nil
+			}
+		}
+	})
+	if err != nil {
+		return err
+	}
+	sort.Slice(res, func(i, j int) bool { return res[i].project < res[j].project })
+	for _, s := range res {
+		s.report()
+	}
+	return nil
+}
+
+func processProject(ctx context.Context, proj string) (*projectState, error) {
+	var bb buildbucketpb.BuildbucketCfg
+	var meta config.Meta
+	err := cfgclient.Get(ctx,
+		config.Set("projects/"+proj),
+		"cr-buildbucket.cfg",
+		cfgclient.ProtoText(&bb),
+		&meta,
+	)
+	if err != nil {
+		return nil, err
+	}
+	pools := stringset.New(0)
+	visit := func(b *buildbucketpb.Builder) {
+		if b == nil {
+			return
+		}
+		for _, dim := range b.Dimensions {
+			chunks := strings.Split(dim, ":")
+			if chunks[0] == "pool" {
+				pools.Add(chunks[1])
+			}
+		}
+	}
+	for _, buck := range bb.Buckets {
+		visit(buck.GetSwarming().GetBuilderDefaults())
+		for _, b := range buck.GetSwarming().GetBuilders() {
+			visit(b)
+		}
+	}
+	for _, m := range bb.GetBuilderMixins() {
+		visit(m)
+	}
+	return &projectState{
+		pools: pools.ToSortedSlice(),
+	}, nil
+}
