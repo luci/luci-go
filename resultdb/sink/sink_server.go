fc6b37b07ba62c4759ffc184419b3683a7cfeeab
Scott Lee <ddoman@chromium.org>
2020-10-07 08:10:33 +0000

    [resultdb] add -coerce-negative-duration
    R=chanli@chromium.org,nodir@chromium.org
    CC=robertma@chromium.org
    
    Change-Id: I47d21b3ac40a25e0373a41cdb7de1f332140eed9
    Bug: 1135892

--- resultdb/sink/sink_server.go
+++ resultdb/sink/sink_server.go
@@ -23,6 +23,7 @@ import (
 	"sync/atomic"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
@@ -122,14 +123,27 @@ func (s *sinkServer) ReportTestResults(ctx context.Context, in *sinkpb.ReportTes
 		if tr.ResultId == "" {
 			tr.ResultId = fmt.Sprintf("%s-%.5d", s.resultIDBase, atomic.AddUint32(&s.resultCounter, 1))
 		}
-
 		if tr.GetTestLocation().GetFileName() != "" && s.cfg.TestLocationBase != "" && !strings.HasPrefix(tr.GetTestLocation().GetFileName(), "//") {
 			tr.TestLocation.FileName = "/" + path.Join(s.cfg.TestLocationBase, tr.TestLocation.FileName)
 		}
-
 		for _, a := range tr.GetArtifacts() {
 			updateArtifactContentType(a)
 		}
+		// The system-clock of GCE machines may get updated by ntp while a test is running.
+		// It can possibly cause a negative duration produced, because most test harnesses
+		// use system-clock to calculate the run time of a test. For more info, visit
+		// crbug.com/1135892.
+		if s.cfg.CoerceNegativeDuration {
+			// If a negative duration was reported, remove the duration.
+			if duration := tr.GetDuration(); duration != nil {
+				d, err := ptypes.Duration(duration)
+				if err == nil && d < 0 {
+					logging.Warningf(ctx, "Negative duration(%d) was reported; setting nil", d)
+					tr.Duration = nil
+				}
+			}
+		}
+
 		if err := validateTestResult(now, tr); err != nil {
 			return nil, status.Errorf(codes.InvalidArgument, "bad request: %s", err)
 		}
