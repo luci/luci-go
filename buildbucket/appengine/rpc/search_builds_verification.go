c9df2dfea167e5930b308629b57206ddcb3d350a
Yuanjun Huang <yuanjunh@google.com>
2020-10-07 20:52:07 -0700

    [buildbucket] Implement search API verification tool
    Bug:1090540
    Change-Id: Ie3cf7d5dea2ead169e21c455781efdd8cc060635

---
+++ buildbucket/appengine/rpc/search_builds_verification.go
@@ -0,0 +1,137 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// TODO(crbug/1090540): remove after verification is done.
+package rpc
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/grpc/appstatus"
+	"go.chromium.org/luci/grpc/prpc"
+	"go.chromium.org/luci/server/auth"
+
+	"go.chromium.org/luci/buildbucket/appengine/internal/search"
+	"go.chromium.org/luci/buildbucket/appengine/model"
+
+	pb "go.chromium.org/luci/buildbucket/proto"
+
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+// bbClientErr is tagged into errors caused by bb client creation.
+var bbClientErr = errors.BoolTag{Key: errors.NewTagKey("BB client")}
+
+type pyBuildBucketClient struct {
+	client pb.BuildsClient
+}
+
+type responseBundle struct {
+	res *pb.SearchBuildsResponse
+	err error
+}
+
+// newPyBBClient constructs a BuildBucket python client.
+func newPyBBClient(ctx context.Context, host string) (*pyBuildBucketClient, error) {
+	t, err := auth.GetRPCTransport(ctx, auth.AsCredentialsForwarder)
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to get RPC transport to BB default service").Tag(bbClientErr).Err()
+	}
+	pClient := pb.NewBuildsPRPCClient(&prpc.Client{
+		C:    &http.Client{Transport: t},
+		Host: host,
+	})
+	return &pyBuildBucketClient{client: pClient}, nil
+}
+
+// verifySearch makes RPC calls to Python service, compares py and go results and returns py results.
+func verifySearch(ctx context.Context, req *pb.SearchBuildsRequest) (*pb.SearchBuildsResponse, error) {
+	// create py BB client.
+	pyHost := "default-dot-cr-buildbucket-dev.appspot.com"
+	if ctx.Value("env") == "Prod" {
+		pyHost = "default-dot-cr-buildbucket.appspot.com"
+	}
+	pyClient, err := newPyBBClient(ctx, pyHost)
+	if err != nil {
+		return nil, err
+	}
+
+	// get Go results.
+	goResCh := make(chan *responseBundle)
+	go func() {
+		goRes, goErr := searchBuildsGoPath(ctx, req)
+		goResCh <- &responseBundle{res: goRes, err: goErr}
+	}()
+
+	// get Py results.
+	logging.Debugf(ctx, "search verification: calling pyHost:%s", pyHost)
+	pyRes, pyErr := pyClient.client.SearchBuilds(ctx, req)
+	if pyErr != nil {
+		logging.Errorf(ctx, "search verification: Python response error - %v", pyErr)
+	}
+	logging.Debugf(ctx, "search verification: successfully get Python response")
+
+	// compare.
+	select {
+	case goResBundle := <-goResCh:
+		compare(ctx, goResBundle, &responseBundle{res: pyRes, err: pyErr})
+	case <-time.After(5 * time.Second):
+		logging.Debugf(ctx, "search verification: timeout in waiting Go results")
+	}
+	return pyRes, pyErr
+}
+
+// compare is to compare py and go responses for SearchBuilds.
+func compare(ctx context.Context, goResBundle *responseBundle, pyResBundle *responseBundle) {
+	if goResBundle == nil {
+		logging.Warningf(ctx, "search verification: diff found - goResBundle is nil, while pyResBundle %v", pyResBundle)
+		return
+	}
+	if goResBundle.err != pyResBundle.err {
+		logging.Warningf(ctx, "search verification: diff found - err is not equal\n go - %v\n py - %v", goResBundle.err, pyResBundle.err)
+		return
+	}
+	diff := ShouldResembleProto(goResBundle.res.Builds, pyResBundle.res.Builds)
+	if diff == "" {
+		logging.Debugf(ctx, "search verification: no diff")
+	} else {
+		logging.Warningf(ctx, "search verification: diff found - %s", diff)
+	}
+}
+
+// searchBuildsGoPath executes the normal Go search path.
+// All code is copied from `SearchBuilds` function.
+func searchBuildsGoPath(ctx context.Context, req *pb.SearchBuildsRequest) (*pb.SearchBuildsResponse, error) {
+	logging.Debugf(ctx, "search verification: searching in Go")
+	if err := validateSearch(req); err != nil {
+		return nil, appstatus.BadRequest(err)
+	}
+	mask, err := getBuildsSubMask(req.GetFields())
+	if err != nil {
+		return nil, appstatus.BadRequest(errors.Annotate(err, "fields").Err())
+	}
+
+	rsp, err := search.NewQuery(req).Fetch(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if err = model.LoadBuildBundles(ctx, rsp.Builds, mask); err != nil {
+		return nil, err
+	}
+	return rsp, nil
+}
