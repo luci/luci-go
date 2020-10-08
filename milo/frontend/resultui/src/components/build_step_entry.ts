fbbd0abc3a3def0dbafa29c3475b1c391244df2e
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2020-10-08 14:24:12 +1100

    [Milo][BuildPage] Fix wrong substep
    Bug:1135708
    Change-Id: I638ee8d35321640c92ca8f5366f6c781b67d3525

--- milo/frontend/resultui/src/components/build_step_entry.ts
+++ milo/frontend/resultui/src/components/build_step_entry.ts
@@ -67,7 +67,13 @@ export class BuildStepEntryElement extends MobxLitElement implements OnEnterList
     this.prerender = false;
   }
 
-  @computed private get shortName() { return this.step.name.split('|')[0] || 'ERROR: Empty Name'; }
+  @computed private get shortName() {
+    if (!this.step.name) {
+      return 'ERROR: Empty Name';
+    }
+    const parts = this.step.name.split('|');
+    return parts[parts.length - 1];
+  }
 
   @computed private get duration() {
     const start = DateTime.fromISO(this.step.interval.start);
