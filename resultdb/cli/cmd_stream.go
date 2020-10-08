c9bbb53ce2824cb70b5b3553f936f5ca51639a4d
Chan Li <chanli@chromium.org>
2020-10-07 21:58:07 -0700

    [ResultSink] produce dir metadata tags for each test results
    - add a new flag "-dir-metadata" to get the path to the dir metadata file
    - For each test result, if it has test location, try to look for the directory of the location in the file
    - If found, produce "monorail_component", "monorail_os" and "team" tags and attach them to the test results
    
    Bug:1103287
    Change-Id: If6f237f08606649dd9bf01964aa6d1fd224d7149

--- resultdb/cli/cmd_stream.go
+++ resultdb/cli/cmd_stream.go
@@ -100,6 +100,10 @@ func cmdStream(p Params) *subcommands.Command {
 				Tag to add to every test result in "key:value" format.
 				A key can be repeated.
 			`))
+			r.Flags.StringVar(&r.dirMDFile, "dir-metadata", "", text.Doc(`
+				Path to the file that contains dirs maps from a directory in chromium/src.git to its metadata. See
+				https://source.chromium.org/chromium/infra/infra/+/master:go/src/infra/tools/dirmd/proto/mapping.proto;l=14.
+			`))
 			return r
 		},
 	}
@@ -118,6 +122,7 @@ type streamRun struct {
 	trChannelMaxLeases   uint
 	tags                 strpair.Map
 	pbTags               []*pb.StringPair
+	dirMDFile            string
 	// TODO(ddoman): add flags
 	// - invocation-tag
 	// - log-file
@@ -231,6 +236,7 @@ func (r *streamRun) runTestCmd(ctx context.Context, args []string) error {
 		TestResultChannelMaxLeases: r.trChannelMaxLeases,
 		TestLocationBase:           r.testTestLocationBase,
 		BaseTags:                   pbutil.FromStrpairMap(r.tags),
+		DirMDFile:                  r.dirMDFile,
 	}
 	return sink.Run(ctx, cfg, func(ctx context.Context, cfg sink.ServerConfig) error {
 		exported, err := lucictx.Export(ctx)
