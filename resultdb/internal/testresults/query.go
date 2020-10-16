7e8775c7826719658a30210842a3bbd610d9eb0b
Chan Li <chanli@chromium.org>
2020-10-15 23:21:57 -0700

    [ResultDB] support query TestMetadata
    Bug: 1137513
    Change-Id: I8e4a1750a911e883706189da57ac555b36abd89e

--- resultdb/internal/testresults/query.go
+++ resultdb/internal/testresults/query.go
@@ -163,10 +163,12 @@ func (q *Query) selectClause() (columns []string, parser func(*spanner.Row) (*pb
 	}
 	selectIfIncluded("SummaryHtml", "summary_html")
 	selectIfIncluded("Tags", "tags")
+	selectIfIncluded("TestMetadata", "test_metadata")
 
 	// Build a parser function.
 	var b spanutil.Buffer
 	var summaryHTML spanutil.Compressed
+	var tmd spanutil.Compressed
 	parser = func(row *spanner.Row) (*pb.TestResult, error) {
 		var invID invocations.ID
 		var maybeUnexpected spanner.NullBool
@@ -195,6 +197,8 @@ func (q *Query) selectClause() (columns []string, parser func(*spanner.Row) (*pb
 				ptrs = append(ptrs, &summaryHTML)
 			case "Tags":
 				ptrs = append(ptrs, &tr.Tags)
+			case "TestMetadata":
+				ptrs = append(ptrs, &tmd)
 			default:
 				panic("impossible")
 			}
@@ -212,8 +216,11 @@ func (q *Query) selectClause() (columns []string, parser func(*spanner.Row) (*pb
 		populateExpectedField(tr, maybeUnexpected)
 		populateDurationField(tr, micros)
 		populateTestLocation(tr, testLocationFileName, testLocationLine)
+		if err := populateTestMetadata(tr, tmd); err != nil {
+			return nil, errors.Annotate(err, "error unmarshalling test_metadata for %s", trName).Err()
+		}
 		if err := q.Mask.Trim(tr); err != nil {
-			return nil, errors.Annotate(err, "error trimming fields for %s", tr.Name).Err()
+			return nil, errors.Annotate(err, "error trimming fields for %s", trName).Err()
 		}
 		// Always include name in tr because name is needed to calculate
 		// page token.
