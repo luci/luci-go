fc1e7823093c8adfd8955cda8ba9a7a6eebd7de4
Andrii Shyshkalov <tandrii@google.com>
2020-10-19 20:06:48 +0200

    cv: make migration API summarize to log the received Run(s).
    R=qyearsley, yiwzhang
    
    Bug: 1139324
    Change-Id: Idefe4bda151b09f5ea0ea7c31086a5c06915e3b9

--- cv/internal/migration/migration.go
+++ cv/internal/migration/migration.go
@@ -15,9 +15,15 @@
 package migration
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 
 	"github.com/golang/protobuf/ptypes/empty"
+
+	"go.chromium.org/luci/common/logging"
+
+	cvbqpb "go.chromium.org/luci/cv/api/bigquery/v1"
 	migrationpb "go.chromium.org/luci/cv/api/migration"
 )
 
@@ -28,10 +34,47 @@ type MigrationServer struct{}
 // Used to determine whether CV's view of the world matches that of CQDaemon.
 // Initially, this is just FYI for CV.
 func (m *MigrationServer) ReportRuns(ctx context.Context, req *migrationpb.ReportRunsRequest) (*empty.Empty, error) {
-	panic("not implemented") // TODO: Implement
+	cls := 0
+	// TODO(tandrii): grab project ID from auth context.
+	project := "<UNKNOWN>"
+	for _, r := range req.Runs {
+		project = r.Attempt.LuciProject
+		cls += len(r.Attempt.GerritChanges)
+	}
+	logging.Infof(ctx, "CQD[%s] is working on %d attempts %d CLs right now", project, len(req.Runs), cls)
+	return &empty.Empty{}, nil
 }
 
 // ReportFinishedRun notifies CV of the Run CQDaemon has just finalized.
 func (m *MigrationServer) ReportFinishedRun(ctx context.Context, req *migrationpb.ReportFinishedRunRequest) (*empty.Empty, error) {
-	panic("not implemented") // TODO: Implement
+	a := req.Run.Attempt
+	logging.Infof(ctx, "CQD[%s] finished working on %s (%s) attempt with %s", a.LuciProject, a.Key, clsOf(a), a.Status.String())
+	return &empty.Empty{}, nil
+}
+
+// clsOf emits CL of the Attempt (aka Run) preserving the order but avoiding
+// duplicating hostnames.
+func clsOf(a *cvbqpb.Attempt) string {
+	if len(a.GerritChanges) == 0 {
+		return "NO CLS"
+	}
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "%d CLs:", len(a.GerritChanges))
+	priorIdx := 0
+	emit := func(excluding int) {
+		fmt.Fprintf(&buf, " [%s", a.GerritChanges[priorIdx].Host)
+		for i := priorIdx; i < excluding; i++ {
+			cl := a.GerritChanges[i]
+			fmt.Fprintf(&buf, " %d/%d", cl.Change, cl.Patchset)
+		}
+		buf.WriteString("]")
+		priorIdx = excluding
+	}
+	for j, cl := range a.GerritChanges {
+		if a.GerritChanges[priorIdx].Host != cl.Host {
+			emit(j)
+		}
+	}
+	emit(len(a.GerritChanges))
+	return buf.String()
 }
