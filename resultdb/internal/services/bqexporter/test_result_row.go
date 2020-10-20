d98f42fe165725b9c8669a6d7eba0d20f720ef2e
Chan Li <chanli@chromium.org>
2020-10-20 00:00:47 +0000

    [ResultDB] use bq/schema to create/update bq schema
    Bug: 1137513
    Change-Id: I9a785e378846c58332215b12b6d1350b9a36dfb0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2477747
    Commit-Queue: Chan Li <chanli@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- resultdb/internal/services/bqexporter/test_result_row.go
+++ resultdb/internal/services/bqexporter/test_result_row.go
@@ -20,19 +20,50 @@ import (
 	"time"
 
 	"cloud.google.com/go/bigquery"
+	"github.com/golang/protobuf/descriptor"
+	desc "github.com/golang/protobuf/protoc-gen-go/descriptor"
+
+	"go.chromium.org/luci/common/bq"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/proto/google/descutil"
 
 	"go.chromium.org/luci/resultdb/internal/invocations"
 	"go.chromium.org/luci/resultdb/pbutil"
+	bqpb "go.chromium.org/luci/resultdb/proto/bq"
 	pb "go.chromium.org/luci/resultdb/proto/v1"
 )
 
 var testResultRowSchema bigquery.Schema
 
+var testResultRowMessage = "luci.resultdb.bq.TestResultRow"
+
 func init() {
 	var err error
-	if testResultRowSchema, err = bigquery.InferSchema(&TestResultRow{}); err != nil {
+	if testResultRowSchema, err = generateSchema(); err != nil {
 		panic(err)
 	}
+}
+
+func generateSchema() (schema bigquery.Schema, err error) {
+	fd, _ := descriptor.MessageDescriptorProto(&bqpb.TestResultRow{})
+	// We also need to get FileDescriptorProto for StringPair and TestMetadata
+	// because they are defined in different files.
+	fdsp, _ := descriptor.MessageDescriptorProto(&pb.StringPair{})
+	fdtmd, _ := descriptor.MessageDescriptorProto(&pb.TestMetadata{})
+	fdset := &desc.FileDescriptorSet{
+		File: []*desc.FileDescriptorProto{fd, fdsp, fdtmd}}
+	conv := bq.SchemaConverter{
+		Desc:           fdset,
+		SourceCodeInfo: make(map[*desc.FileDescriptorProto]bq.SourceCodeInfoMap, len(fdset.File)),
+	}
+	for _, f := range fdset.File {
+		conv.SourceCodeInfo[f], err = descutil.IndexSourceCodeInfo(f)
+		if err != nil {
+			return nil, errors.Annotate(err, "failed to index source code info in file %q", fd.GetName()).Err()
+		}
+	}
+	schema, _, err = conv.Schema(testResultRowMessage)
+	return schema, err
 }
 
 // Row size limit is 1Mib according to
