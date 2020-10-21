e8c8b775e2a6bcde4cfe82c1f39c55119aad84ad
Vadim Shtayura <vadimsh@chromium.org>
2020-10-20 17:15:51 -0700

    [cipd] Regenerated pb.go.
    Just result of running "go generate ./...".
    
    R=tandrii@chromium.org, yiwzhang@google.com
    
    Change-Id: I15912305f08dfbc6b2a8604ab687f4e87b3cbdf0

--- cipd/api/admin/v1/pb.discovery.go
+++ cipd/api/admin/v1/pb.discovery.go
@@ -2,9 +2,9 @@
 
 package api
 
-import discovery "go.chromium.org/luci/grpc/discovery"
+import "go.chromium.org/luci/grpc/discovery"
 
-import "github.com/golang/protobuf/protoc-gen-go/descriptor"
+import "google.golang.org/protobuf/types/descriptorpb"
 
 func init() {
 	discovery.RegisterDescriptorSetCompressed(
@@ -483,7 +483,7 @@ func init() {
 // Will not return nil.
 //
 // Do NOT modify the returned descriptor.
-func FileDescriptorSet() *descriptor.FileDescriptorSet {
+func FileDescriptorSet() *descriptorpb.FileDescriptorSet {
 	// We just need ONE of the service names to look up the FileDescriptorSet.
 	ret, err := discovery.GetDescriptorSet("cipd.Admin")
 	if err != nil {
--- cipd/appengine/impl/repo/tasks/gen.go
+++ cipd/appengine/impl/repo/tasks/gen.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate cproto
+//go:generate cproto -discovery=false
 
 // Package tasks contains task queue tasks definitions.
 package tasks
