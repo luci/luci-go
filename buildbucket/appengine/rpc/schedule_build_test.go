d41f4cf690ffcbb7bc8c6095a66a95384b6381b1
smut <smut@google.com>
2020-10-20 22:27:34 -0700

    [buildbucket] Load template builds
    Bug: 1042991
    Change-Id: I89dac80dcedd0c51f663dff14887a1c303671b4f

--- buildbucket/appengine/rpc/schedule_build_test.go
+++ buildbucket/appengine/rpc/schedule_build_test.go
@@ -18,6 +18,8 @@ import (
 	"context"
 	"testing"
 
+	structpb "github.com/golang/protobuf/ptypes/struct"
+
 	"go.chromium.org/luci/gae/impl/memory"
 	"go.chromium.org/luci/gae/service/datastore"
 	"go.chromium.org/luci/server/auth"
@@ -33,12 +35,318 @@ import (
 func TestScheduleBuild(t *testing.T) {
 	t.Parallel()
 
-	Convey("ScheduleBuild", t, func() {
-		srv := &Builds{}
+	Convey("scheduleRequestFromTemplate", t, func() {
 		ctx := memory.Use(context.Background())
 		datastore.GetTestable(ctx).AutoIndex(true)
 		datastore.GetTestable(ctx).Consistent(true)
+		ctx = auth.WithState(ctx, &authtest.FakeState{
+			Identity: "user:caller@example.com",
+		})
 
+		So(datastore.Put(ctx, &model.Bucket{
+			ID:     "bucket",
+			Parent: model.ProjectKey(ctx, "project"),
+			Proto: pb.Bucket{
+				Acls: []*pb.Acl{
+					{
+						Identity: "user:caller@example.com",
+						Role:     pb.Acl_READER,
+					},
+				},
+			},
+		}), ShouldBeNil)
+
+		Convey("nil", func() {
+			ret, err := scheduleRequestFromTemplate(ctx, nil)
+			So(err, ShouldBeNil)
+			So(ret, ShouldBeNil)
+		})
+
+		Convey("empty", func() {
+			req := &pb.ScheduleBuildRequest{}
+			ret, err := scheduleRequestFromTemplate(ctx, req)
+			So(err, ShouldBeNil)
+			So(req, ShouldResembleProto, &pb.ScheduleBuildRequest{})
+			So(ret, ShouldResembleProto, &pb.ScheduleBuildRequest{})
+		})
+
+		Convey("not found", func() {
+			req := &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			}
+			ret, err := scheduleRequestFromTemplate(ctx, req)
+			So(err, ShouldErrLike, "not found")
+			So(req, ShouldResembleProto, &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			})
+			So(ret, ShouldBeNil)
+		})
+
+		Convey("permission denied", func() {
+			ctx = auth.WithState(ctx, &authtest.FakeState{
+				Identity: "user:unauthorized@example.com",
+			})
+			So(datastore.Put(ctx, &model.Build{
+				Proto: pb.Build{
+					Id: 1,
+					Builder: &pb.BuilderID{
+						Project: "project",
+						Bucket:  "bucket",
+						Builder: "builder",
+					},
+				},
+			}), ShouldBeNil)
+			req := &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			}
+			ret, err := scheduleRequestFromTemplate(ctx, req)
+			So(err, ShouldErrLike, "not found")
+			So(req, ShouldResembleProto, &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			})
+			So(ret, ShouldBeNil)
+		})
+
+		Convey("gerrit changes", func() {
+			So(datastore.Put(ctx, &model.Build{
+				Proto: pb.Build{
+					Id: 1,
+					Builder: &pb.BuilderID{
+						Project: "project",
+						Bucket:  "bucket",
+						Builder: "builder",
+					},
+					Input: &pb.Build_Input{
+						GerritChanges: []*pb.GerritChange{
+							{
+								Host:     "example.com",
+								Project:  "project",
+								Change:   1,
+								Patchset: 1,
+							},
+						},
+					},
+				},
+			}), ShouldBeNil)
+			req := &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			}
+			ret, err := scheduleRequestFromTemplate(ctx, req)
+			So(err, ShouldBeNil)
+			So(req, ShouldResembleProto, &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			})
+			So(ret, ShouldResembleProto, &pb.ScheduleBuildRequest{
+				Builder: &pb.BuilderID{
+					Project: "project",
+					Bucket:  "bucket",
+					Builder: "builder",
+				},
+				GerritChanges: []*pb.GerritChange{
+					{
+						Host:     "example.com",
+						Project:  "project",
+						Change:   1,
+						Patchset: 1,
+					},
+				},
+				Properties: &structpb.Struct{},
+			})
+		})
+
+		Convey("gitiles commit", func() {
+			So(datastore.Put(ctx, &model.Build{
+				Proto: pb.Build{
+					Id: 1,
+					Builder: &pb.BuilderID{
+						Project: "project",
+						Bucket:  "bucket",
+						Builder: "builder",
+					},
+					Input: &pb.Build_Input{
+						GitilesCommit: &pb.GitilesCommit{
+							Host:    "example.com",
+							Project: "project",
+							Ref:     "refs/heads/master",
+						},
+					},
+				},
+			}), ShouldBeNil)
+			req := &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			}
+			ret, err := scheduleRequestFromTemplate(ctx, req)
+			So(err, ShouldBeNil)
+			So(req, ShouldResembleProto, &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			})
+			So(ret, ShouldResembleProto, &pb.ScheduleBuildRequest{
+				Builder: &pb.BuilderID{
+					Project: "project",
+					Bucket:  "bucket",
+					Builder: "builder",
+				},
+				GitilesCommit: &pb.GitilesCommit{
+					Host:    "example.com",
+					Project: "project",
+					Ref:     "refs/heads/master",
+				},
+				Properties: &structpb.Struct{},
+			})
+		})
+
+		Convey("input properties", func() {
+			So(datastore.Put(ctx, &model.Build{
+				Proto: pb.Build{
+					Id: 1,
+					Builder: &pb.BuilderID{
+						Project: "project",
+						Bucket:  "bucket",
+						Builder: "builder",
+					},
+				},
+			}), ShouldBeNil)
+
+			Convey("empty", func() {
+				So(datastore.Put(ctx, &model.BuildInputProperties{
+					ID:    1,
+					Build: datastore.MakeKey(ctx, "Build", 1),
+				}), ShouldBeNil)
+				req := &pb.ScheduleBuildRequest{
+					TemplateBuildId: 1,
+				}
+				ret, err := scheduleRequestFromTemplate(ctx, req)
+				So(err, ShouldBeNil)
+				So(req, ShouldResembleProto, &pb.ScheduleBuildRequest{
+					TemplateBuildId: 1,
+				})
+				So(ret, ShouldResembleProto, &pb.ScheduleBuildRequest{
+					Builder: &pb.BuilderID{
+						Project: "project",
+						Bucket:  "bucket",
+						Builder: "builder",
+					},
+					Properties: &structpb.Struct{},
+				})
+			})
+
+			Convey("non-empty", func() {
+				So(datastore.Put(ctx, &model.BuildInputProperties{
+					ID:    1,
+					Build: datastore.MakeKey(ctx, "Build", 1),
+					Proto: model.DSStruct{
+						Struct: structpb.Struct{
+							Fields: map[string]*structpb.Value{
+								"input": {
+									Kind: &structpb.Value_StringValue{
+										StringValue: "input value",
+									},
+								},
+							},
+						},
+					},
+				}), ShouldBeNil)
+				req := &pb.ScheduleBuildRequest{
+					TemplateBuildId: 1,
+				}
+				ret, err := scheduleRequestFromTemplate(ctx, req)
+				So(err, ShouldBeNil)
+				So(req, ShouldResembleProto, &pb.ScheduleBuildRequest{
+					TemplateBuildId: 1,
+				})
+				So(ret, ShouldResembleProto, &pb.ScheduleBuildRequest{
+					Builder: &pb.BuilderID{
+						Project: "project",
+						Bucket:  "bucket",
+						Builder: "builder",
+					},
+					Properties: &structpb.Struct{
+						Fields: map[string]*structpb.Value{
+							"input": {
+								Kind: &structpb.Value_StringValue{
+									StringValue: "input value",
+								},
+							},
+						},
+					},
+				})
+			})
+		})
+
+		Convey("tags", func() {
+			So(datastore.Put(ctx, &model.Build{
+				Proto: pb.Build{
+					Id: 1,
+					Builder: &pb.BuilderID{
+						Project: "project",
+						Bucket:  "bucket",
+						Builder: "builder",
+					},
+				},
+				Tags: []string{
+					"key:value",
+				},
+			}), ShouldBeNil)
+			req := &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			}
+			ret, err := scheduleRequestFromTemplate(ctx, req)
+			So(err, ShouldBeNil)
+			So(req, ShouldResembleProto, &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			})
+			So(ret, ShouldResembleProto, &pb.ScheduleBuildRequest{
+				Builder: &pb.BuilderID{
+					Project: "project",
+					Bucket:  "bucket",
+					Builder: "builder",
+				},
+				Properties: &structpb.Struct{},
+				Tags: []*pb.StringPair{
+					{
+						Key:   "key",
+						Value: "value",
+					},
+				},
+			})
+		})
+
+		Convey("ok", func() {
+			So(datastore.Put(ctx, &model.Build{
+				Proto: pb.Build{
+					Id: 1,
+					Builder: &pb.BuilderID{
+						Project: "project",
+						Bucket:  "bucket",
+						Builder: "builder",
+					},
+				},
+			}), ShouldBeNil)
+			req := &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			}
+			ret, err := scheduleRequestFromTemplate(ctx, req)
+			So(err, ShouldBeNil)
+			So(req, ShouldResembleProto, &pb.ScheduleBuildRequest{
+				TemplateBuildId: 1,
+			})
+			So(ret, ShouldResembleProto, &pb.ScheduleBuildRequest{
+				Builder: &pb.BuilderID{
+					Project: "project",
+					Bucket:  "bucket",
+					Builder: "builder",
+				},
+				Properties: &structpb.Struct{},
+			})
+		})
+	})
+
+	Convey("ScheduleBuild", t, func() {
+		srv := &Builds{}
+
+		ctx := memory.Use(context.Background())
+		datastore.GetTestable(ctx).AutoIndex(true)
+		datastore.GetTestable(ctx).Consistent(true)
 		ctx = auth.WithState(ctx, &authtest.FakeState{
 			Identity: "user:caller@example.com",
 		})
@@ -109,6 +417,67 @@ func TestScheduleBuild(t *testing.T) {
 						Bucket:  "bucket",
 						Builder: "builder",
 					},
+				}
+				rsp, err := srv.ScheduleBuild(ctx, req)
+				So(err, ShouldBeNil)
+				So(rsp, ShouldBeNil)
+			})
+		})
+
+		Convey("template build ID", func() {
+			Convey("not found", func() {
+				req := &pb.ScheduleBuildRequest{
+					TemplateBuildId: 1,
+				}
+				rsp, err := srv.ScheduleBuild(ctx, req)
+				So(err, ShouldErrLike, "not found")
+				So(rsp, ShouldBeNil)
+			})
+
+			Convey("permission denied", func() {
+				So(datastore.Put(ctx, &model.Build{
+					Proto: pb.Build{
+						Id: 1,
+						Builder: &pb.BuilderID{
+							Project: "project",
+							Bucket:  "bucket",
+							Builder: "builder",
+						},
+					},
+				}), ShouldBeNil)
+				req := &pb.ScheduleBuildRequest{
+					TemplateBuildId: 1,
+				}
+				rsp, err := srv.ScheduleBuild(ctx, req)
+				So(err, ShouldErrLike, "not found")
+				So(rsp, ShouldBeNil)
+			})
+
+			Convey("ok", func() {
+				So(datastore.Put(ctx, &model.Bucket{
+					ID:     "bucket",
+					Parent: model.ProjectKey(ctx, "project"),
+					Proto: pb.Bucket{
+						Acls: []*pb.Acl{
+							{
+								Identity: "user:caller@example.com",
+								Role:     pb.Acl_SCHEDULER,
+							},
+						},
+					},
+				}), ShouldBeNil)
+				So(datastore.Put(ctx, &model.Build{
+					Proto: pb.Build{
+						Id: 1,
+						Builder: &pb.BuilderID{
+							Project: "project",
+							Bucket:  "bucket",
+							Builder: "builder",
+						},
+					},
+				}), ShouldBeNil)
+				req := &pb.ScheduleBuildRequest{
+					TemplateBuildId: 1,
 				}
 				rsp, err := srv.ScheduleBuild(ctx, req)
 				So(err, ShouldBeNil)
