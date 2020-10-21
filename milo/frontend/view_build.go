61f6cccae8ad9855537c0272248f8d762e2c0bba
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2020-10-21 00:16:18 +0000

    [Milo] Dogfood build page
    Change-Id: Ie40fc7660e7074aaa1b00764b4fd95253738131b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2488821
    Auto-Submit: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Reviewed-by: Weiwei Lin <weiweilin@google.com>
    Commit-Queue: Quang Minh Tuan Nguyen <nqmtuan@google.com>

--- milo/frontend/view_build.go
+++ milo/frontend/view_build.go
@@ -17,7 +17,6 @@ package frontend
 import (
 	"encoding/json"
 	"fmt"
-	"html/template"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -30,7 +29,6 @@ import (
 	"go.chromium.org/luci/grpc/grpcutil"
 	"go.chromium.org/luci/milo/buildsource/buildbucket"
 	"go.chromium.org/luci/milo/frontend/ui"
-	"go.chromium.org/luci/server/auth"
 	"go.chromium.org/luci/server/auth/xsrf"
 	"go.chromium.org/luci/server/router"
 	"go.chromium.org/luci/server/templates"
@@ -142,22 +140,11 @@ func renderBuild(c *router.Context, bp *ui.BuildPage, err error) error {
 	bp.StepDisplayPref = getStepDisplayPrefCookie(c)
 	bp.ShowDebugLogsPref = getShowDebugLogsPrefCookie(c)
 
-	resultUIFishfood, err := auth.IsMember(c.Context, "milo-resultui-fishfood")
-	if err != nil {
-		return err
-	}
-	bannerHTML := template.HTML("")
-	if resultUIFishfood {
-		bannerHTML = bp.NewBuildPageOptInHTML()
-	} else {
-		bannerHTML = bp.TestResultsOptInHTML()
-	}
-
 	templates.MustRender(c.Context, c.Writer, "pages/build.html", templates.Args{
 		"BuildPage":      bp,
 		"RetryRequestID": rand.Int31(),
 		"XsrfTokenField": xsrf.TokenField(c.Context),
-		"BannerHTML":     bannerHTML,
+		"BannerHTML":     bp.NewBuildPageOptInHTML(),
 	})
 	return nil
 }
