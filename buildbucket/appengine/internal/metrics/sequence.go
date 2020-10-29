8951c79d7669fc28b774f7fba332b54cf89d9dad
Scott Lee <ddoman@chromium.org>
2020-10-29 03:16:57 +0000

    [buildbucket] add build and sequence metrics in go
    R=tandrii@chromium.org,nodir@chromium.org,smut@google.com
    
    Bug: 1142984
    Change-Id: Ia4fef27f362d04d61b79f487864c912341fb78ec

---
+++ buildbucket/appengine/internal/metrics/sequence.go
@@ -0,0 +1,35 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metrics
+
+import (
+	"math"
+
+	"go.chromium.org/luci/common/tsmon/distribution"
+	"go.chromium.org/luci/common/tsmon/field"
+	"go.chromium.org/luci/common/tsmon/metric"
+	"go.chromium.org/luci/common/tsmon/types"
+)
+
+var (
+	SeqNumGenDuration = metric.NewCumulativeDistribution(
+		"buildbucket/sequence_number/gen_duration",
+		"Duration of a sequence number generation in ms",
+		&types.MetricMetadata{Units: types.Milliseconds},
+		// Bucketer for 1ms..5s range
+		distribution.GeometricBucketer(math.Pow(10, 0.0374), 100),
+		field.String("sequence"),
+	)
+)
