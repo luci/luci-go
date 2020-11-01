3b7668c86f90cbc9eee8aaa3c49925bc12d7fc73
Nodir Turakulov <nodir@chromium.org>
2020-10-31 21:34:00 -0700

    [filegraph] Initial commit
    The initial commit of the retrieval and traversal of a git-based graph
    of file associations. It adds a binary with a subcommand, but without
    an implementation. The order of landing will be top-bottom.
    
    Bug: 1136280
    Change-Id: I66d6125da8611268e5608a27096ea9cdc6407baf

---
+++ rts/filegraph/cli/cmd_query.go
@@ -0,0 +1,49 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cli
+
+import (
+	"github.com/maruel/subcommands"
+
+	"go.chromium.org/luci/common/data/text"
+)
+
+var cmdQuery = &subcommands.Command{
+	UsageLine: `query [flags] [PATH [PATH...]]`,
+	ShortDesc: "retrieve files in the descending order of distance",
+	LongDesc: text.Doc(`
+		Retrieve files in the descending order of distance from the given one(s).
+
+		Each PATH must be a file in a git repository.
+		All files must be in the same repository.
+
+		Prints files in the order from closest to farthest.
+		Each line has format "<distance> <filename>",
+		where the filename is relative to the repo root, e.g. "foo/bar.cpp".
+		Does not print unreachable files.
+	`),
+	CommandRun: func() subcommands.CommandRun {
+		r := &queryRun{}
+		return r
+	},
+}
+
+type queryRun struct {
+	baseCommandRun
+}
+
+func (r *queryRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	panic("not implemented")
+}
