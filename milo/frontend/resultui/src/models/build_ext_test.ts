b5a01ed59583b777ebe407f97de43e2c79356cf4
Weiwei Lin <weiweilin@google.com>
2020-11-02 04:37:46 +0000

    [resultui] add BuildExt class
    Add a BuildExt class to wrap Build objects with helper methods/properties.
    This is a step towards migrating from querying BuildData endpoint to
    querying buildbucket directly.
    
    R=jsca@google.com, nqmtuan@google.com
    
    Bug: 1108200
    Change-Id: I3f27ffbc0247856c907e467d5636eefbf6f0f130
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2513671
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Joey Scarr <jsca@chromium.org>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>

---
+++ milo/frontend/resultui/src/models/build_ext_test.ts
@@ -0,0 +1,91 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { assert } from 'chai';
+
+import { chaiRecursiveDeepInclude } from '../libs/test_utils/chai_recursive_deep_include';
+import { Build } from '../services/buildbucket';
+import { BuildExt } from './build_ext';
+import { StepExt } from './step_ext';
+
+chai.use(chaiRecursiveDeepInclude);
+
+describe('BuildExt', () => {
+  it('should build step-tree correctly', async () => {
+    const time = {seconds: 1000, nanos: 0};
+    const build = new BuildExt({
+      createTime: time,
+      updateTime: time,
+      steps: [
+        {name: 'root1', startTime: time},
+        {name: 'root2', startTime: time},
+        {name: 'root2|parent1', startTime: time},
+        {name: 'root2|parent1|child1', startTime: time},
+        {name: 'root2|parent1|child2', startTime: time},
+        {name: 'root3', startTime: time},
+        {name: 'root3|parent1', startTime: time},
+        {name: 'root3|parent2', startTime: time},
+        {name: 'root3|parent2|child1', startTime: time},
+        {name: 'root3|parent2|child2', startTime: time},
+      ],
+    } as Build);
+
+    assert.recursiveDeepInclude(build.rootSteps, [
+      {
+        name: 'root1',
+        children: [] as StepExt[],
+      },
+      {
+        name: 'root2',
+        children: [
+          {
+            name: 'root2|parent1',
+            children: [
+              {
+                name: 'root2|parent1|child1',
+                children: [],
+              },
+              {
+                name: 'root2|parent1|child2',
+                children: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'root3',
+        children: [
+          {
+            name: 'root3|parent1',
+            children: [],
+          },
+          {
+            name: 'root3|parent2',
+            children: [
+              {
+                name: 'root3|parent2|child1',
+                children: [],
+              },
+              {
+                name: 'root3|parent2|child2',
+                children: [],
+              },
+            ],
+          },
+        ],
+      },
+    ] as StepExt[]);
+  });
+});
