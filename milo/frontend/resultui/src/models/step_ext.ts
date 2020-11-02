c93d1c4443555abad51286d146afb77bc6705afe
Weiwei Lin <weiweilin@google.com>
2020-11-02 13:16:23 +1100

    [resultui] add StepExt class
    Add a StepExt class to wrap Step objects with helper methods/properties.
    This is a step towards migrating from querying BuildData endpoint to
    querying buildbucket directly.
    
    R=jsca@google.com, nqmtuan@google.com
    
    Bug: 1108200
    Change-Id: I1cbc1c30cc47b1f7c17e36dae21cac6be594c8e9

---
+++ milo/frontend/resultui/src/models/step_ext.ts
@@ -0,0 +1,72 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// tslint:disable: variable-name
+
+import { DateTime } from 'luxon';
+import { computed, IObservableValue, observable } from 'mobx';
+
+import { datetimeFromTimestamp } from '../libs/time_utils';
+import { BuildStatus, Log, Step } from '../services/buildbucket';
+
+/**
+ * Contains all fields of the Step object with added helper methods and
+ * properties.
+ */
+export class StepExt {
+  readonly name: string;
+  readonly startTime: DateTime;
+  readonly endTime: DateTime | null;
+  readonly status: BuildStatus;
+  readonly logs?: Log[] | undefined;
+  readonly summaryMarkdown?: string | undefined;
+
+  readonly selfName: string;
+  readonly parentName: string | null;
+  readonly children: StepExt[] = [];
+  readonly renderTime: IObservableValue<DateTime>;
+
+  constructor(step: Step, renderTime?: IObservableValue<DateTime>) {
+    if (!step.name) {
+      throw new Error('Step name can not be empty');
+    }
+
+    this.name = step.name;
+    this.startTime = datetimeFromTimestamp(step.startTime);
+    this.endTime = step.endTime ? datetimeFromTimestamp(step.endTime): null;
+    this.status = step.status;
+    this.logs = step.logs;
+    this.summaryMarkdown = step.summaryMarkdown;
+
+    const lastSeparatorIndex = step.name.lastIndexOf('|');
+    this.selfName = step.name.slice(lastSeparatorIndex + 1);
+    this.parentName = lastSeparatorIndex === -1 ? null : step.name.slice(0, lastSeparatorIndex);
+
+    this.renderTime = renderTime || observable.box(DateTime.local());
+  }
+
+  /**
+   * true if and only if the step and all of its descendants succeeded.
+   */
+  @computed get succeededRecursively(): boolean {
+    if (this.status !== BuildStatus.Success) {
+      return false;
+    }
+    return this.children.every((child) => child.succeededRecursively);
+  }
+
+  @computed get duration() {
+    return (this.endTime || this.renderTime.get()).diff(this.startTime);
+  }
+}
---
+++ milo/frontend/resultui/src/models/step_ext_test.ts
@@ -0,0 +1,89 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { assert } from 'chai';
+
+import { BuildStatus } from '../services/buildbucket';
+import { StepExt } from './step_ext';
+
+describe('StepExt', () => {
+  function createStep(name: string, status = BuildStatus.Success) {
+    const startTime = {seconds: 100, nanos: 0};
+    return new StepExt({
+      name,
+      startTime,
+      status,
+    });
+  }
+
+  describe('should compute parent/child names correctly', async () => {
+    it('for root step', () => {
+      const step = createStep('child');
+      assert.strictEqual(step.parentName, null);
+      assert.strictEqual(step.selfName, 'child');
+    });
+
+    it('for step with a parent', () => {
+      const step = createStep('parent|child');
+      assert.strictEqual(step.parentName, 'parent');
+      assert.strictEqual(step.selfName, 'child');
+    });
+
+    it('for step with a grandparent', () => {
+      const step3 = createStep('grand-parent|parent|child');
+      assert.strictEqual(step3.parentName, 'grand-parent|parent');
+      assert.strictEqual(step3.selfName, 'child');
+    });
+  });
+
+  describe('succeededRecursively', () => {
+    it('succeeded step with no children should return true', async () => {
+      const step = createStep('child', BuildStatus.Success);
+      assert.isTrue(step.succeededRecursively);
+    });
+
+    it('succeeded step with no succeeded children should return true', async () => {
+      const step = createStep('child', BuildStatus.Success);
+      step.children.push(createStep('parent|child1', BuildStatus.Success));
+      step.children.push(createStep('parent|child2', BuildStatus.Success));
+      assert.isTrue(step.succeededRecursively);
+    });
+
+    it('succeeded step with failed child should return false', async () => {
+      const step = createStep('parent', BuildStatus.Success);
+      step.children.push(createStep('parent|child1', BuildStatus.Success));
+      step.children.push(createStep('parent|child2', BuildStatus.Failure));
+      assert.isFalse(step.succeededRecursively);
+    });
+
+    it('failed step with no children should return false', async () => {
+      const step = createStep('child', BuildStatus.Failure);
+      assert.isFalse(step.succeededRecursively);
+    });
+
+    it('failed step with succeeded children should return false', async () => {
+      const step = createStep('parent', BuildStatus.Failure);
+      step.children.push(createStep('parent|child1', BuildStatus.Success));
+      step.children.push(createStep('parent|child2', BuildStatus.Success));
+      assert.isFalse(step.succeededRecursively);
+    });
+
+    it('failed step with failed children should return false', async () => {
+      const step = createStep('parent', BuildStatus.Failure);
+      step.children.push(createStep('parent|child1', BuildStatus.Success));
+      step.children.push(createStep('parent|child2', BuildStatus.Failure));
+      assert.isFalse(step.succeededRecursively);
+    });
+  });
+});
