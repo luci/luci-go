5ae084117b913fbf3e76feb89a0536fa30ba6707
Nodir Turakulov <nodir@chromium.org>
2020-11-01 18:54:07 -0800

    [filegraph] Initial commit
    The initial commit of the retrieval and traversal of a git-based graph
    of file associations. It adds a binary with a subcommand, but without
    an implementation. The order of landing will be top-bottom.
    
    Bug: 1136280
    Change-Id: I66d6125da8611268e5608a27096ea9cdc6407baf

---
+++ rts/filegraph/doc.go
@@ -0,0 +1,19 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package filegraph implements a directed weighted graph of files, where the
+// weight of edge (x, y), called distance, represents how much y is relevant
+// to x. Such graph provides distance between any two files, and can order files
+// by distance from/to a given file.
+package filegraph
