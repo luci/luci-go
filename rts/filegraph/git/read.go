8240e10ec0f61116a5ec43e164b6c85bb1321155
Nodir Turakulov <nodir@chromium.org>
2020-11-03 12:29:25 -0800

    [filegraph] Add git package
    Change-Id: If95f52737583764cd7cb5f71b3a0a4988bebe25f

---
+++ rts/filegraph/git/read.go
@@ -0,0 +1,182 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package git
+
+import (
+	"bufio"
+	"encoding/binary"
+	"io"
+	"strconv"
+	"strings"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+const fileHeader = 54
+
+// Read reads the graph from r.
+// It is the opposite of (*Graph).Write().
+func (g *Graph) Read(r *bufio.Reader) error {
+	return (&reader{r: r}).readGraph(g)
+}
+
+type reader struct {
+	r        *bufio.Reader
+	ordered  []*node
+	buf      []byte
+	textMode bool
+}
+
+func (r *reader) readGraph(g *Graph) error {
+	// Verify header.
+	switch header, err := r.readInt(); {
+	case err != nil:
+		return err
+	case header != fileHeader:
+		return errors.Reason("unexpected header").Err()
+	}
+
+	// Read version.
+	switch ver, err := r.readInt(); {
+	case err != nil:
+		return err
+	case ver != 0:
+		return errors.Reason("unexpected version %d; expected 0", ver).Err()
+	}
+
+	// Read the commit.
+	var err error
+	if g.Commit, err = r.readString(); err != nil {
+		return err
+	}
+
+	// Read the nodes.
+	r.ordered = r.ordered[:0]
+	g.root.name = "//"
+	if err := r.readNode(&g.root); err != nil {
+		return err
+	}
+
+	// Read the edges.
+	for _, n := range r.ordered {
+		if err := r.readEdges(n); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *reader) readNode(n *node) error {
+	r.ordered = append(r.ordered, n)
+
+	var err error
+	if n.commits, err = r.readInt(); err != nil {
+		return err
+	}
+
+	childCount, err := r.readInt()
+	switch {
+	case err != nil:
+		return err
+	case childCount == 0:
+		return nil
+	}
+
+	n.children = make(map[string]*node, childCount)
+	for i := 0; i < childCount; i++ {
+		childBaseName, err := r.readString()
+		if err != nil {
+			return err
+		}
+		child := &node{}
+		if n.name == "//" {
+			child.name = n.name + childBaseName
+		} else {
+			child.name = n.name + "/" + childBaseName
+		}
+		n.children[childBaseName] = child
+		if err := r.readNode(child); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *reader) readEdges(n *node) error {
+	count, err := r.readInt()
+	switch {
+	case err != nil:
+		return err
+	case count == 0:
+		return nil
+	}
+
+	n.edges = make([]edge, count)
+	for i := range n.edges {
+		index, err := r.readInt()
+		if err != nil {
+			return err
+		}
+		n.edges[i].other = r.ordered[index]
+
+		if n.edges[i].commonCommits, err = r.readInt(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *reader) readString() (string, error) {
+	if r.textMode {
+		return r.readLine()
+	}
+
+	length, err := r.readInt()
+	if err != nil {
+		return "", err
+	}
+
+	if cap(r.buf) < length {
+		r.buf = make([]byte, length)
+	}
+	r.buf = r.buf[:length]
+	if _, err := io.ReadFull(r.r, r.buf); err != nil {
+		return "", err
+	}
+	return string(r.buf), nil
+}
+
+func (r *reader) readInt() (int, error) {
+	if r.textMode {
+		s, err := r.readLine()
+		if err != nil {
+			return 0, err
+		}
+		return strconv.Atoi(s)
+	}
+
+	n, err := binary.ReadVarint(r.r)
+	return int(n), err
+}
+
+func (r *reader) readLine() (string, error) {
+	if !r.textMode {
+		panic("not text mode")
+	}
+	s, err := r.r.ReadString('\n')
+	return strings.TrimSuffix(s, "\n"), err
+}
---
+++ rts/filegraph/git/write.go
@@ -0,0 +1,170 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package git
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"strings"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+// Write writes the graph to w.
+// It is the opposite of (*Graph).Read().
+//
+// Spec:
+//  graph = header version git-commit-hash root root-edges
+//  header = 54
+//  version = 0
+//
+//  root = node
+//  node = number-of-commits number-of-children children-sorted-by-base-name
+//  children-sorted-by-base-name = child*
+//  child = base-name node
+//
+//  root-edges = node-edges
+//  node-edges = number-of-edges edge*
+//  edge =
+//    index-of-the-adjacent-node-as-found-in-the-file
+//    number-of-common-commits
+//    edges-of-children-sorted-by-base-name
+//  edges-of-children-sorted-by-base-name = edge*
+//
+// where
+//  all integer types are uvarint
+//  all strings are length-prefixed utf8
+func (g *Graph) Write(w io.Writer) error {
+	g.ensureInitialized()
+	return (&writer{w: w}).writeGraph(g)
+}
+
+type writer struct {
+	w         io.Writer
+	varintBuf [binary.MaxVarintLen64]byte
+	indices   map[*node]int
+
+	textMode bool
+}
+
+func (w *writer) writeGraph(g *Graph) error {
+	// Write the header.
+	if err := w.writeInt(fileHeader); err != nil {
+		return err
+	}
+
+	// Write version.
+	if err := w.writeInt(0); err != nil {
+		return err
+	}
+
+	// Write commit.
+	if err := w.writeString(g.Commit); err != nil {
+		return err
+	}
+
+	// Write nodes.
+	w.indices = map[*node]int{}
+	if err := w.writeNode(&g.root); err != nil {
+		return err
+	}
+
+	// Write edges.
+	return w.writeEdges(&g.root)
+}
+
+func (w *writer) writeNode(n *node) error {
+	w.indices[n] = len(w.indices)
+
+	// TODO(nodir): consider not writing the number of
+	// commits if this is a directory, since today only
+	// individual files can have commits.
+
+	// Write the number of commits.
+	if err := w.writeInt(n.commits); err != nil {
+		return err
+	}
+
+	// Write the number of direct children.
+	if err := w.writeInt(len(n.children)); err != nil {
+		return err
+	}
+
+	// Write the descendants.
+	for _, key := range n.sortedChildKeys() {
+		if err := w.writeString(key); err != nil {
+			return err
+		}
+		if err := w.writeNode(n.children[key]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (w *writer) writeEdges(n *node) error {
+	// TODO(nodir): consider changing writing edges only for
+	// nodes that have them. Note that only files have edges,
+	// unlike directories.
+
+	// Write the edges.
+	if err := w.writeInt(len(n.edges)); err != nil {
+		return err
+	}
+	for _, e := range n.edges {
+		if err := w.writeInt(w.indices[e.other]); err != nil {
+			return err
+		}
+		if err := w.writeInt(e.commonCommits); err != nil {
+			return err
+		}
+	}
+
+	// Write the edges of descendants.
+	for _, key := range n.sortedChildKeys() {
+		if err := w.writeEdges(n.children[key]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (w *writer) writeString(s string) error {
+	if w.textMode {
+		if strings.Contains(s, "\n") {
+			return errors.Reason("linebreak is not supported in text mode").Err()
+		}
+		_, err := fmt.Fprintln(w.w, s)
+		return err
+	}
+
+	if err := w.writeInt(len(s)); err != nil {
+		return err
+	}
+	_, err := io.WriteString(w.w, s)
+	return err
+}
+
+func (w *writer) writeInt(n int) error {
+	if w.textMode {
+		_, err := fmt.Fprintln(w.w, n)
+		return err
+	}
+
+	length := binary.PutVarint(w.varintBuf[:], int64(n))
+	_, err := w.w.Write(w.varintBuf[:length])
+	return err
+}
