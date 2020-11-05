8d92f47ee1206c90803c525c2e4cc08467f093ea
Nodir Turakulov <nodir@chromium.org>
2020-11-05 05:37:57 +0000

    [filegraph] Implement Graph interface in git.Graph
    This CL implements filegraph.Graph interface in git.Graph and
    adds the data structure.
    
    R=guterman@google.com, tandrii@chromium.org
    
    Bug: 1136280
    Change-Id: Iaab393987f2645254d40d504ba3884d21204c12a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2518754
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Gregory Guterman <guterman@google.com>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

--- rts/filegraph/git/graph.go
+++ rts/filegraph/git/graph.go
@@ -15,6 +15,11 @@
 package git
 
 import (
+	"math"
+	"sort"
+	"strings"
+	"sync"
+
 	"go.chromium.org/luci/rts/filegraph"
 )
 
@@ -22,10 +27,97 @@ import (
 type Graph struct {
 	// Commit is the git commit that the graph state corresponds to.
 	Commit string
+
+	root node
+	init sync.Once
+}
+
+// node is simultaneously a distance graph node (see edges) and a filesystem
+// tree node (see children).
+// It implements filegraph.Node.
+type node struct {
+	// name is the node name, e.g. "//foo/bar.cc"
+	// See also filegraph.Node.Name().
+	name string
+
+	// commits is the number of commits that touched this node.
+	commits int
+
+	// edges are outgoing edges.
+	// Note: this data structure is optimized for the Dijkstra's algorithm
+	// and loading from disk. None of them need random-access.
+	edges []edge
+
+	// children are files and subdirectories of the current node, which itself
+	// is a directory.
+	children map[string]*node
+}
+
+type edge struct {
+	to            *node
+	commonCommits int
+}
+
+func (g *Graph) ensureInitialized() {
+	g.init.Do(func() {
+		g.root.name = "//"
+	})
 }
 
 // Node implements filegraph.Graph.
 func (g *Graph) Node(name string) filegraph.Node {
-	// TODO(crbug.com/1136280): implement.
-	panic("not implemented")
+	g.ensureInitialized()
+	return g.node(name)
+}
+
+// node retrieves a graph node by name. Returns nil if it doesn't exist.
+func (g *Graph) node(name string) *node {
+	cur := &g.root
+	for _, component := range splitName(name) {
+		if cur = cur.children[component]; cur == nil {
+			return nil
+		}
+	}
+	return cur
+}
+
+func (n *node) Name() string {
+	return n.name
+}
+
+func (n *node) IsFile() bool {
+	return len(n.children) == 0 && n.name != "//"
+}
+
+func (n *node) Outgoing(callback func(other filegraph.Node, distance float64) (keepGoing bool)) {
+	for _, e := range n.edges {
+		// TODO(nodir): consider using multiplication in filegraph.Query instead of
+		// calling log2, because the latter is expensive.
+		distance := -math.Log2(float64(e.commonCommits) / float64(n.commits))
+		if !callback(e.to, distance) {
+			return
+		}
+	}
+}
+
+func (n *node) sortedChildKeys() []string {
+	if len(n.children) == 0 {
+		return nil
+	}
+	keys := make([]string, 0, len(n.children))
+	for name := range n.children {
+		keys = append(keys, name)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+// splitName splits a node name into components,
+// e.g. "//foo/bar.cc" -> ["foo", "bar.cc"].
+func splitName(name string) []string {
+	name = strings.TrimPrefix(name, "//")
+	if name == "" {
+		return nil
+	}
+	return strings.Split(name, "/")
 }
---
+++ rts/filegraph/git/graph_test.go
@@ -0,0 +1,106 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package git
+
+import (
+	"testing"
+
+	"go.chromium.org/luci/rts/filegraph"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGraph(t *testing.T) {
+	t.Parallel()
+
+	Convey(`Graph`, t, func() {
+		Convey(`Root of zero value`, func() {
+			g := &Graph{}
+			root := g.Node("//")
+			So(root, ShouldNotBeNil)
+			So(root.Name(), ShouldEqual, "//")
+			So(root.IsFile(), ShouldBeFalse)
+		})
+
+		Convey(`node()`, func() {
+			g := &Graph{
+				root: node{
+					children: map[string]*node{
+						"dir": {
+							children: map[string]*node{
+								"foo": {},
+							},
+						},
+					},
+				},
+			}
+
+			Convey(`//`, func() {
+				So(g.node("//"), ShouldEqual, &g.root)
+			})
+
+			Convey(`//dir`, func() {
+				So(g.node("//dir"), ShouldEqual, g.root.children["dir"])
+			})
+			Convey(`//dir/foo`, func() {
+				So(g.node("//dir/foo"), ShouldEqual, g.root.children["dir"].children["foo"])
+			})
+			Convey(`//dir/bar`, func() {
+				So(g.node("//dir/bar"), ShouldBeNil)
+			})
+		})
+
+		Convey(`sortedChildKeys()`, func() {
+			node := &node{
+				children: map[string]*node{
+					"foo": {},
+					"bar": {},
+				},
+			}
+			So(node.sortedChildKeys(), ShouldResemble, []string{"bar", "foo"})
+		})
+
+		Convey(`Outgoing`, func() {
+			bar := &node{commits: 2}
+			foo := &node{
+				commits: 2,
+				edges:   []edge{{to: bar, commonCommits: 1}},
+			}
+
+			type outgoingEdge struct {
+				other    filegraph.Node
+				distance float64
+			}
+			var actual []outgoingEdge
+			foo.Outgoing(func(other filegraph.Node, distance float64) bool {
+				actual = append(actual, outgoingEdge{other: other, distance: distance})
+				return true
+			})
+			So(actual, ShouldResemble, []outgoingEdge{{
+				other:    bar,
+				distance: 1,
+			}})
+		})
+
+		Convey(`splitName`, func() {
+			Convey("//foo/bar.cc", func() {
+				So(splitName("//foo/bar.cc"), ShouldResemble, []string{"foo", "bar.cc"})
+			})
+			Convey("//", func() {
+				So(splitName("//"), ShouldResemble, []string(nil))
+			})
+		})
+	})
+}
