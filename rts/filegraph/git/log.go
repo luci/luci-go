e566a7c5f5a2ba941abe4cc41f176a3811a1dd38
Nodir Turakulov <nodir@chromium.org>
2020-11-07 01:22:00 -0800

    [filegraph] Implement readCommits
    This CL implements reading of git commits, and primarily, the parser
    of git-log output. With this CL, filegraph command is functional.
    
    Bug: 1136280
    Change-Id: I0070f6b4f9d215acb174336c4f3ce6c78b2b8658

--- rts/filegraph/git/log.go
+++ rts/filegraph/git/log.go
@@ -15,7 +15,13 @@
 package git
 
 import (
+	"bufio"
+	"bytes"
 	"context"
+	"io"
+	"os/exec"
+
+	"go.chromium.org/luci/common/errors"
 )
 
 type commit struct {
@@ -35,6 +41,211 @@ type fileChange struct {
 // reachable from `exclude`.
 // Returns only first-parents, using --first-parent git flag.
 func readLog(ctx context.Context, repoDir, exclude, rev string, callback func(commit) error) (err error) {
-	// TODO(crbug.com/1136280): implement
-	panic("not implemented")
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	args := []string{
+		"-C", repoDir,
+		"log",
+		"--format=format:%H",
+		"--raw",
+		"-z",
+		"--reverse",
+		"--first-parent",
+	}
+	if exclude != "" {
+		args = append(args, exclude+"...")
+	}
+	cmd := exec.CommandContext(ctx, "git", args...)
+
+	stderr := &bytes.Buffer{}
+	cmd.Stderr = stderr
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	defer func() {
+		cancel()
+		werr := cmd.Wait()
+		if err == nil && werr != nil {
+			err = errors.Reason("git log failed: %s. stderr: %s", werr, stderr).Err()
+		}
+	}()
+
+	log := newLogReader(stdout)
+	return log.ReadCommits(callback)
+}
+
+type logParser struct {
+	r       *bufio.Reader
+	hashBuf [40]byte
+	err     error
+	sep     byte
+}
+
+func newLogReader(r io.Reader) *logParser {
+	return &logParser{r: bufio.NewReader(r)}
+}
+
+func (r *logParser) read(delim byte) (string, error) {
+	ret, err := r.r.ReadString(delim)
+	if err != nil {
+		return "", err
+	}
+
+	ret = ret[:len(ret)-1]
+	return ret, nil
+}
+
+func (r *logParser) peek() (byte, error) {
+	ret, err := r.r.ReadByte()
+	if err == nil {
+		err = r.r.UnreadByte()
+	}
+	return ret, err
+}
+
+func (r *logParser) readRawBlock() (file fileChange, err error) {
+	// Format doc: https://git-scm.com/docs/git-diff#_raw_output_format
+
+	// Skip 4 sub-blocks separated by space.
+	for i := 0; i < 4; i++ {
+		if _, err = r.read(' '); err != nil {
+			return
+		}
+	}
+
+	// Read status
+	var statusString string
+	if statusString, err = r.read(r.sep); err != nil {
+		return
+	}
+	// For renames, it might look like R90. We need only the first char.
+	file.Status = statusString[0]
+
+	if file.Path, err = r.read(r.sep); err != nil {
+		return
+	}
+
+	// If status is rename or copy, then read the second path.
+	if file.Status == 'R' || file.Status == 'C' {
+		if file.Path2, err = r.read(r.sep); err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func (r *logParser) expect(expected byte) error {
+	// Assert NUL
+	switch actual, err := r.r.ReadByte(); {
+	case err != nil:
+		return err
+	case actual != expected:
+		return errors.Reason("expected %d byte, got %d", expected, actual).Err()
+	default:
+		return nil
+	}
+}
+
+func (r *logParser) ReadCommit() (commit, error) {
+	c := commit{}
+	// If a commit is empty, it is not followed by \n.
+	// Read exactly 40 characters.
+	if _, err := io.ReadFull(r.r, r.hashBuf[:]); err != nil {
+		return c, err
+	}
+	c.Hash = string(r.hashBuf[:])
+	if !lookLikeHash(c.Hash) {
+		return c, errors.Reason("expected a hash; got %q", c.Hash).Err()
+	}
+
+	// Skip delimeter
+	switch b, err := r.r.ReadByte(); {
+	case err != nil:
+		return c, err
+	case b == r.sep:
+		// This is an empty commit
+		return c, nil
+	case b == '\n':
+		// expected
+	default:
+		return c, errors.Reason("expected \\n or separator; got %d byte", b).Err()
+	}
+
+	for {
+		next, err := r.peek()
+		if err != nil {
+			return c, err
+		}
+		if next != ':' {
+			// The raw block has ended.
+			break
+		}
+
+		file, err := r.readRawBlock()
+		if err != nil {
+			return c, err
+		}
+		c.Files = append(c.Files, file)
+	}
+
+	// // Now read num stats for each file we've read, in the same order.
+	// for _, f := range c.Files {
+	// 	if err := r.readNumStat(f); err != nil {
+	// 		return c, err
+	// 	}
+	// }
+
+	return c, nil
+}
+
+func (r *logParser) ReadCommits(fn func(commit) error) error {
+	for {
+		c, err := r.ReadCommit()
+		switch {
+		case err == io.EOF:
+			return nil
+		case err != nil:
+			return err
+		}
+
+		if err := fn(c); err != nil {
+			return err
+		}
+
+		switch b, err := r.r.ReadByte(); {
+		case err == io.EOF:
+			return nil
+		case err != nil:
+			return err
+		case b != r.sep:
+			if err := r.r.UnreadByte(); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func lookLikeHash(s string) bool {
+	if len(s) != 40 {
+		return false
+	}
+	for _, c := range s {
+		switch {
+		case c >= '0' && c <= '9':
+			// yes
+		case c >= 'a' && c <= 'f':
+			// yes
+		default:
+			return false
+		}
+	}
+	return true
 }
