f198b7877f56088544e3f5dfd1f5908804164943
Andrii Shyshkalov <tandrii@google.com>
2020-11-09 19:26:24 +0000

    cv: add EquivalentPatchsetRange.function.
    The code is defensive in case Gerrit changes something s.t.
    CV maintainers can notice this faster.
    
    R=qyearsley, yiwzhang
    
    Bug: 1146015
    Change-Id: I0936f2c5777f16c6b147c92dd00a2ffa7be2291b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2526307
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Auto-Submit: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>

---
+++ cv/internal/gerrit/equi.go
@@ -0,0 +1,73 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gerrit
+
+import (
+	"sort"
+
+	"go.chromium.org/luci/common/errors"
+	gerritpb "go.chromium.org/luci/common/proto/gerrit"
+)
+
+// EquivalentPatchsetRange computes range of patchsets code-wise equivalent to
+// the current patchset.
+//
+// Gerrit categorises each new patchset (aka Revision) according to difference
+// from prior one. The rebases are counted as equivalent, even though
+// dependencies may have changed. Thus, only REWORK changes code.
+//
+// Generally, all patchsets are numbered 1,2,3,...n without gaps. But this
+// function doesn't assume this, thus Gerrit might potentially support wiping
+// out individual patchsets, creating gaps without affecting CV.
+func EquivalentPatchsetRange(info *gerritpb.ChangeInfo) (minEquiPatchset, currentPatchset int, err error) {
+	if len(info.Revisions) == 0 {
+		err = errors.Reason("ChangeInfo must have all revisions populated").Err()
+		return
+	}
+	revs := make([]*gerritpb.RevisionInfo, 0, len(info.Revisions))
+	for _, rev := range info.Revisions {
+		revs = append(revs, rev)
+	}
+	sort.Slice(revs, func(i, j int) bool {
+		return revs[i].Number > revs[j].Number // largest patchset first.
+	})
+
+	// Validate ChangeInfo to avoid problems later.
+	switch rev, ok := info.Revisions[info.CurrentRevision]; {
+	case !ok:
+		err = errors.Reason("ChangeInfo must have current_revision populated").Err()
+		return
+	case rev != revs[0]:
+		err = errors.Reason("ChangeInfo.currentPatchset %v doesn't have largest patchset %v", rev, revs[0]).Err()
+		return
+	}
+
+	currentPatchset = int(revs[0].Number)
+	minEquiPatchset = currentPatchset
+	for i, rev := range revs[:len(revs)-1] {
+		switch rev.Kind {
+		case gerritpb.RevisionInfo_REWORK:
+			return
+		case gerritpb.RevisionInfo_NO_CHANGE,
+			gerritpb.RevisionInfo_NO_CODE_CHANGE,
+			gerritpb.RevisionInfo_TRIVIAL_REBASE:
+			minEquiPatchset = int(revs[i+1].Number)
+		default:
+			err = errors.Reason("Unknown revision kind %d %s", rev.Kind, rev.Kind).Err()
+			return
+		}
+	}
+	return
+}
---
+++ cv/internal/gerrit/equi_test.go
@@ -0,0 +1,83 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gerrit
+
+import (
+	"encoding/hex"
+	"testing"
+
+	gerritpb "go.chromium.org/luci/common/proto/gerrit"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestEquivalentPatchsetRange(t *testing.T) {
+	t.Parallel()
+
+	Convey("EquivalentPatchsetRange", t, func() {
+
+		Convey("No revisions", func() {
+			_, _, err := EquivalentPatchsetRange(makeCI())
+			So(err, ShouldErrLike, "must have all revisions populated")
+		})
+
+		Convey("Wrong CurrentRevision", func() {
+			ci := makeCI(gerritpb.RevisionInfo_REWORK)
+			ci.CurrentRevision = ""
+			_, _, err := EquivalentPatchsetRange(ci)
+			So(err, ShouldErrLike, "must have current_revision populated")
+		})
+
+		Convey("Wrong Kind", func() {
+			_, _, err := EquivalentPatchsetRange(makeCI(
+				gerritpb.RevisionInfo_TRIVIAL_REBASE,
+				gerritpb.RevisionInfo_Kind(199)))
+			So(err, ShouldErrLike, "Unknown revision kind 199")
+		})
+
+		Convey("works", func() {
+			m, p, err := EquivalentPatchsetRange(makeCI(gerritpb.RevisionInfo_REWORK))
+			So(err, ShouldBeNil)
+			So(m, ShouldEqual, 1)
+			So(p, ShouldEqual, 1)
+
+			m, p, err = EquivalentPatchsetRange(makeCI(
+				gerritpb.RevisionInfo_REWORK,
+				gerritpb.RevisionInfo_TRIVIAL_REBASE,
+				gerritpb.RevisionInfo_REWORK,
+				gerritpb.RevisionInfo_NO_CHANGE,
+				gerritpb.RevisionInfo_NO_CODE_CHANGE,
+				gerritpb.RevisionInfo_TRIVIAL_REBASE,
+			))
+			So(err, ShouldBeNil)
+			So(m, ShouldEqual, 3)
+			So(p, ShouldEqual, 6)
+		})
+	})
+}
+
+func makeCI(kinds ...gerritpb.RevisionInfo_Kind) *gerritpb.ChangeInfo {
+	ci := &gerritpb.ChangeInfo{
+		Revisions: make(map[string]*gerritpb.RevisionInfo, len(kinds)),
+	}
+	var rev string
+	for i, k := range kinds {
+		rev = hex.EncodeToString([]byte{byte(i ^ 37), byte(k ^ 7)})
+		ci.Revisions[rev] = &gerritpb.RevisionInfo{Number: int32(i + 1), Kind: k}
+	}
+	ci.CurrentRevision = rev
+	return ci
+}
