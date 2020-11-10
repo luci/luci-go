ecb74119e9b5ecd7fd1c9397acc3c9c9670389c5
Nodir Turakulov <nodir@chromium.org>
2020-11-09 18:21:41 -0800

    [filegraph] Implement git.Updater.
    It reads new commits and updates the graph accordingly.
    
    Bug: 1136280
    Change-Id: I9cd715df901d7a6d2117cb49280548327ce8e0cd

--- rts/filegraph/git/graph.go
+++ rts/filegraph/git/graph.go
@@ -24,6 +24,13 @@ import (
 )
 
 // Graph is a file graph based on the git history.
+//
+// The graph represents aggregated history of all file changes in the repo,
+// rather than the state of the repo at a single point of time.
+// In particular, the graph may include nodes for files that no longer exist.
+// It is generally not possible to tell if a node is a file or a directory,
+// because it might have been a file at one point of time, and a directory at
+// another. Therefore the graph is append-only.
 type Graph struct {
 	// Commit is the git commit that the graph state corresponds to.
 	Commit string
@@ -35,26 +42,47 @@ type Graph struct {
 // node is simultaneously a distance graph node (see edges) and a filesystem
 // tree node (see children).
 // It implements filegraph.Node.
+//
+// A node represents aggregated change history of a single node path.
+// It is never excluded from the graph because the past is immutable.
+// If a node was a file at one point, and a directory at another, it has
+// both children and edges.
 type node struct {
 	// name is the node name, e.g. "//foo/bar.cc"
 	// See also filegraph.Node.Name().
 	name string
 
-	// commits is the number of commits that touched this node.
+	// commits is the number of commits that touched this file.
+	// Note that if "//foo/bar.cc" is touched, foo's commit count is
+	// not incremented.
 	commits int
 
 	// edges are outgoing edges.
+	// If an edge exists from x to y, then it must also exist from y to x and must
+	// have the same edge.commonCommits.
+	//
 	// Note: this data structure is optimized for the Dijkstra's algorithm
 	// and loading from disk. None of them need random-access.
 	edges []edge
+
 	// copyEdgesOnAppend indicates that edges must be copied before appending.
 	copyEdgesOnAppend bool
 
-	// children are files and subdirectories of the current node, which itself
-	// is a directory.
+	// children are files and subdirectories of the this directory.
 	children map[string]*node
 }
 
+// edge is directed edge.
+//
+// If an edge exists from x to y, then a counterpart edge from y to x must also
+// exist and have the same commonCommits.
+//
+// A special kind of edges is called "alias edge". It is indicated by
+// commonCommits == 0. If edge (x, y) is an alias, then distance(x, y) is 0.
+// Alias edges are used for file renames: the old and the new file are aliases
+// of each other.
+// Alias edges are never downgraded to regular edges - they stay alias because
+// distance 0 is the minimal possible distance.
 type edge struct {
 	to            *node
 	commonCommits int
@@ -83,15 +111,70 @@ func (g *Graph) node(name string) *node {
 	return cur
 }
 
+// ensureNode creates the node if it doesn't exist, and returns it.
+// Creates the node's ancestors if needed.
+// Assumes the name is valid.
+func (g *Graph) ensureNode(name string) *node {
+	if name == "//" {
+		return &g.root
+	}
+
+	cur := &g.root
+
+	child := func(baseName, name string) *node {
+		if ret, ok := cur.children[baseName]; ok {
+			return ret
+		}
+
+		ret := &node{name: name}
+		if cur.children == nil {
+			cur.children = map[string]*node{}
+		}
+		cur.children[baseName] = ret
+		return ret
+	}
+
+	// Note: this loop does not allocate new strings.
+	startAt := 2 // skip the "//" prefix
+	for {
+		sep := strings.Index(name[startAt:], "/")
+		if sep == -1 {
+			return child(name[startAt:], name)
+		}
+		sep += startAt
+
+		cur = child(name[startAt:sep], name[:sep])
+		startAt = sep + 1
+	}
+}
+
+// ensureAlias ensures there is an alias edge from n to `to`.
+func (n *node) ensureAlias(to *node) {
+	for i := range n.edges {
+		if n.edges[i].to == to {
+			n.edges[i].commonCommits = 0
+			return
+		}
+	}
+
+	n.prepareToAppendEdges()
+	n.edges = append(n.edges, edge{to: to})
+}
+
 func (n *node) Name() string {
 	return n.name
 }
 
-func (n *node) Outgoing(callback func(other filegraph.Node, distance float64) (keepGoing bool)) {
+func (n *node) Outgoing(callback func(to filegraph.Node, distance float64) (keepGoing bool)) {
 	for _, e := range n.edges {
-		// TODO(nodir): consider using multiplication in filegraph.Query instead of
-		// calling log2, because the latter is expensive.
-		distance := -math.Log2(float64(e.commonCommits) / float64(n.commits))
+		distance := 0.0
+		if e.commonCommits == 0 {
+			// e.to is alias of n. The distance is 0.
+		} else {
+			// TODO(nodir): consider using multiplication in filegraph.Query instead of
+			// calling log2, because the latter is expensive.
+			distance = -math.Log2(float64(e.commonCommits) / float64(n.commits))
+		}
 		if !callback(e.to, distance) {
 			return
 		}
@@ -121,6 +204,22 @@ func (n *node) sortedChildKeys() []string {
 	}
 	sort.Strings(keys)
 	return keys
+}
+
+// prepareToAppendEdges copies n.edges if n.copyEdgesOnAppend is true.
+func (n *node) prepareToAppendEdges() {
+	if !n.copyEdgesOnAppend {
+		return
+	}
+
+	if len(n.edges) > 0 {
+		// Double the capacity in preparation for the append.
+		edges := make([]edge, len(n.edges), len(n.edges)*2)
+		copy(edges, n.edges)
+		n.edges = edges
+	}
+
+	n.copyEdgesOnAppend = false
 }
 
 // splitName splits a node name into components,
