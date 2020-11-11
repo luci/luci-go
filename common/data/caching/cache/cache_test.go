9529d6690adc60d0983789b1c799edb118a4c4ee
Takuto Ikuta <tikuta@chromium.org>
2020-11-11 13:42:20 +0900

    bench write a file in dir having many files
    PS 9:
    https://chromium-swarm.appspot.com/task?id=4fce64ac8fde7210
    ```
    goos: windows
    goarch: amd64
    pkg: go.chromium.org/luci/common/data/caching/cache
    BenchmarkManyFilesInDir/files_100         	    5342	    304181 ns/op
    BenchmarkManyFilesInDir/files_1000        	    5342	    295236 ns/op
    BenchmarkManyFilesInDir/files_10000       	    5334	    309776 ns/op
    BenchmarkManyFilesInDir/files_100000      	    4389	   1040423 ns/op
    PASS
    ```
    
    https://chromium-swarm.appspot.com/task?id=4fce64e9729a7b10
    ```
    goos: linux
    goarch: amd64
    pkg: go.chromium.org/luci/common/data/caching/cache
    BenchmarkManyFilesInDir/files_100         	   55904	     26803 ns/op
    BenchmarkManyFilesInDir/files_1000        	   53628	     27270 ns/op
    BenchmarkManyFilesInDir/files_10000       	   51045	     27093 ns/op
    BenchmarkManyFilesInDir/files_100000      	   38142	     30479 ns/op
    PASS
    ```
    
    Change-Id: I677ea1840a31113722d0739cc0eb7e8899ca32b8

--- common/data/caching/cache/cache_test.go
+++ common/data/caching/cache/cache_test.go
@@ -16,10 +16,14 @@ package cache
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"io/ioutil"
 	"math"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
 
 	"go.chromium.org/luci/common/isolated"
@@ -250,4 +254,42 @@ func TestNew(t *testing.T) {
 		So(ioutil.WriteFile(empty2, nil, 0600), ShouldBeNil)
 		So(c.AddFileWithoutValidation(emptyHash, empty2), ShouldBeNil)
 	})
+}
+
+func panicIfErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func sha256sum(s string) string {
+	h := sha256.Sum256([]byte(s))
+	return hex.EncodeToString(h[:])
+}
+
+func benchmarkManyFilesInDir(b *testing.B, files int) {
+	tmp, err := ioutil.TempDir(os.TempDir(), "")
+	panicIfErr(err)
+
+	for i := 0; i < files; i++ {
+		name := sha256sum(strconv.Itoa(i))
+		panicIfErr(ioutil.WriteFile(filepath.Join(tmp, name), nil, 0666))
+	}
+
+	buf := make([]byte, 4*1024)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fpath := filepath.Join(tmp, sha256sum(strconv.Itoa(i+files)))
+		panicIfErr(ioutil.WriteFile(fpath, buf, 0666))
+		panicIfErr(os.Remove(fpath))
+	}
+}
+
+func BenchmarkManyFilesInDir(b *testing.B) {
+	for _, i := range []int{100, 1000, 10000, 100000} {
+		b.Run(fmt.Sprintf("files %d", i), func(b *testing.B) {
+			benchmarkManyFilesInDir(b, i)
+		})
+	}
 }
