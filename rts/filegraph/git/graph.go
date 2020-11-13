b0156f9078facd33b464950b6b7d8eff436d2a9c
Nodir Turakulov <nodir@chromium.org>
2020-11-13 07:23:16 +0000

    [filegraph] Update git.Graph semantics.
    The initial implementation of filegraph kept the graph synchronized with
    the git tree: a file in the graph exists if and only if the file exists
    in the git tree of the most-recently-synchronized commit.
    This means we can incrementally update the graph by following commits in
    the chronological order. In particular, if a file was moved/removed
    in the git commit, and it is moved/removed in the graph.
    Here the update operation requires the state of the graph for the
    parent commit.
    
    However, this fails short on commits that have multiple parents.
    So the initial implementation passed --first-parent to git-log, with
    a TODO to fix it.
    
    Stepping back, the graph was trying to represent two things at the same
    time: 1) the state of the file tree and 2) aggregated changes to the
    file tree. To support for non-first parents of a commit, this CL
    removes (1), and keeps (2) which is what we need.
    In particular, now a file in the graph is never removed/moved,
    and the graph is now append-only.
    
    Primarily, this means the update operation no longer requires the
    state of the graph for the parent commit, and in fact no longer depends
    on the order of commits. This means we no longer have to pass
    --first-parent.
    
    This does mean that the graph has nodes for no-longer existing files.
    But it also has a nice property that the history is never lost.
    For example, if a file was removed, but then the change was reverted,
    this information is not lost when processing the removal.
    
    Still, we need an alternative solution for file renames.
    Introduce "alias edges", which are edges with distance 0.
    Reuse edge struct for memory efficiency.
    
    R=guterman@google.com, tandrii@chromium.org
    
    Bug: 1136280
    Change-Id: Id2cd775a9c61e4fe8375a8946812adf393ecb721
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2528753
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

--- rts/filegraph/git/graph.go
+++ rts/filegraph/git/graph.go
@@ -24,6 +24,15 @@ import (
 )
 
 // Graph is a file graph based on the git history.
+//
+// The graph represents aggregated history of all file changes in the repo,
+// rather than the state of the repo at a single point of time.
+// In particular, the graph may include nodes for files that no longer exist.
+// It is generally not possible to tell if a node is a file or a directory,
+// because it might have been a file at one point of time, and a directory at
+// another.
+//
+// TODO(nodir): introduce a decay function to remove old nodes/edges.
 type Graph struct {
 	// Commit is the git commit that the graph state corresponds to.
 	Commit string
@@ -35,26 +44,48 @@ type Graph struct {
 // node is simultaneously a distance graph node (see edges) and a filesystem
 // tree node (see children).
 // It implements filegraph.Node.
+//
+// A node represents aggregated change history of a single node path.
+// It is never excluded from the graph because the past is immutable.
+// If a node was a file at one point, and a directory at another, it has
+// both children and edges.
 type node struct {
 	// name is the node name, e.g. "//foo/bar.cc"
 	// See also filegraph.Node.Name().
 	name string
 
-	// commits is the number of commits that touched this node.
+	// commits is the number of commits that touched this file.
+	// Note that if "//foo/bar.cc" is touched, foo's commit count is
+	// not incremented.
 	commits int
 
 	// edges are outgoing edges.
+	// If an edge exists from x to y, then it must also exist from y to x and must
+	// have the same edge.commonCommits.
+	//
 	// Note: this data structure is optimized for the Dijkstra's algorithm
 	// and loading from disk. None of them need random-access.
 	edges []edge
+
 	// copyEdgesOnAppend indicates that edges must be copied before appending.
 	copyEdgesOnAppend bool
 
-	// children are files and subdirectories of the current node, which itself
-	// is a directory.
+	// children are files and subdirectories of the this directory.
+	// TODO(nodir): consider a sorted list instead.
 	children map[string]*node
 }
 
+// edge is directed edge.
+//
+// If an edge exists from x to y, then a counterpart edge from y to x must also
+// exist and have the same commonCommits.
+//
+// A special kind of edges is called "alias edge". It is indicated by
+// commonCommits == 0. If edge (x, y) is an alias, then distance(x, y) is 0.
+// Alias edges are used for file renames: the old and the new file are aliases
+// of each other.
+// Alias edges are never downgraded to regular edges - they stay alias because
+// distance 0 is the minimal possible distance.
 type edge struct {
 	to            *node
 	commonCommits int
@@ -87,11 +118,16 @@ func (n *node) Name() string {
 	return n.name
 }
 
-func (n *node) Outgoing(callback func(other filegraph.Node, distance float64) (keepGoing bool)) {
+func (n *node) Outgoing(callback func(to filegraph.Node, distance float64) (keepGoing bool)) {
 	for _, e := range n.edges {
-		// TODO(nodir): consider using multiplication in filegraph.Query instead of
-		// calling log2, because the latter is expensive.
-		distance := -math.Log2(float64(e.commonCommits) / float64(n.commits))
+		distance := 0.0
+		if e.commonCommits == 0 {
+			// e.to is alias of n. The distance is 0.
+		} else {
+			// TODO(nodir): consider using multiplication in filegraph.Query instead of
+			// calling log2, because the latter is expensive.
+			distance = -math.Log2(float64(e.commonCommits) / float64(n.commits))
+		}
 		if !callback(e.to, distance) {
 			return
 		}
