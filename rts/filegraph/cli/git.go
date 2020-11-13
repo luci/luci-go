68b9c2a3d132c92e201678a72173b8e56be2d642
Nodir Turakulov <nodir@chromium.org>
2020-11-13 10:58:12 -0800

    [filegraph] Implement readLog
    This CL implements reading of git commits, and primarily, the parser
    of git-log output. With this CL, filegraph command is functional.
    
    R=guterman@google.com, tandrii@chromium.org
    
    Bug: 1136280
    Change-Id: I0070f6b4f9d215acb174336c4f3ce6c78b2b8658

--- rts/filegraph/cli/git.go
+++ rts/filegraph/cli/git.go
@@ -32,6 +32,7 @@ import (
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/sync/parallel"
+	"go.chromium.org/luci/common/system/signals"
 
 	"go.chromium.org/luci/rts/filegraph"
 	"go.chromium.org/luci/rts/filegraph/git"
@@ -116,22 +117,26 @@ func (g *gitGraph) loadSyncedNodes(ctx context.Context, filePaths ...string) ([]
 // the latest commit in the ref, and caches the result on the file system.
 // repoDir is a local path to a local checkout of the git repo.
 func (g *gitGraph) loadUpdatedGraph(ctx context.Context, repoDir string) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	defer signals.HandleInterrupt(cancel)
+
 	gitDir, err := execGit(repoDir)("rev-parse", "--absolute-git-dir")
 	if err != nil {
 		return err
 	}
 
 	// Read/write the graph from/to a file under .git directory, named after the ref.
-	fileName := filepath.Join(
-		gitDir,
-		"filegraph",
-		filepath.FromSlash(g.ref),
-		fmt.Sprintf("fg.max-commit-size-%d.v0", g.maxCommitSize),
+	f, err := os.OpenFile(
+		filepath.Join(
+			gitDir,
+			"filegraph",
+			filepath.FromSlash(g.ref),
+			fmt.Sprintf("fg.max-commit-size-%d.v0", g.maxCommitSize),
+		),
+		os.O_RDWR|os.O_CREATE,
+		0777,
 	)
-	if err := os.MkdirAll(filepath.Dir(fileName), 0777); err != nil {
-		return err
-	}
-	f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0777)
 	if err != nil {
 		return err
 	}
