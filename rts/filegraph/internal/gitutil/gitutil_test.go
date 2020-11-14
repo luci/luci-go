bc2540c604986bd842e029c1d5b6fb0a73ed2022
Nodir Turakulov <nodir@chromium.org>
2020-11-14 10:09:42 -0800

    [filegraph] Add git.Load() function.
    loadUpdatedGraph() function is generic enough to be also needed
    in RTS code. Move it from the cli package to git package, and
    call it Load().
    
    Move common git functions package internal/gitutil.
    
    New/changed code is primarily in load.go.
    The code there is very similar to previous version, but also
    refactored.
    Code in other files is largely the same.
    
    Bug: 1136280
    Change-Id: I15b1232c7a8b70c6756c9f1a1d8e3594c89d30c4

--- rts/filegraph/internal/gitutil/gitutil_test.go
+++ rts/filegraph/internal/gitutil/gitutil_test.go
@@ -12,18 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cli
+package gitutil
 
 import (
-	"context"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
-
-	"go.chromium.org/luci/common/logging"
-	"go.chromium.org/luci/common/logging/memlogger"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -48,7 +44,7 @@ func TestGit(t *testing.T) {
 
 		git := func(context string) func(args ...string) string {
 			return func(args ...string) string {
-				out, err := execGit(context)(args...)
+				out, err := Exec(context)(args...)
 				So(err, ShouldBeNil)
 				return out
 			}
@@ -67,34 +63,8 @@ func TestGit(t *testing.T) {
 		out := git(fooPath)("status")
 		So(out, ShouldContainSubstring, "working tree clean")
 
-		repoDir, err := ensureSameRepo(tmpd, fooPath)
+		repoDir, err := EnsureSameRepo(tmpd, fooPath)
 		So(err, ShouldBeNil)
 		So(repoDir, ShouldEqual, tmpd)
-	})
-}
-
-func TestGitGraph(t *testing.T) {
-	t.Parallel()
-
-	Convey(`tryReadingCache`, t, func() {
-		ctx := context.Background()
-		ctx = memlogger.Use(ctx)
-
-		Convey(`empty file`, func() {
-			tmpd, err := ioutil.TempDir("", "filegraph_git")
-			So(err, ShouldBeNil)
-			defer os.RemoveAll(tmpd)
-
-			f, err := os.Create(filepath.Join(tmpd, "empty"))
-			So(err, ShouldBeNil)
-			defer f.Close()
-
-			g := gitGraph{}
-			err = g.tryReadingCache(ctx, f)
-			So(err, ShouldBeNil)
-
-			log := logging.Get(ctx).(*memlogger.MemLogger)
-			So(log, memlogger.ShouldHaveLog, logging.Info, "populating cache")
-		})
 	})
 }
