b1a8c378e063ebb72e149fb6133779e42e0f5175
Nodir Turakulov <nodir@chromium.org>
2020-11-14 15:04:00 -0800

    [filegraph] Add -reversed flag
    -reversed flag indicates that the traversal must follow incoming
    edges, instead of outgoing. In other words, it traverses the transposed
    file graph.
    
    Bug: 1136280
    Change-Id: Ifd2b918937c6cbb0f4cd82256a380d8534ed5685

--- rts/filegraph/query.go
+++ rts/filegraph/query.go
@@ -30,7 +30,9 @@ type Query struct {
 	// Nodes further than this are considered unreachable.
 	MaxDistance float64
 
-	// TODO(crbug.com/1136280): add Backwards
+	// Reversed instructs to follow incoming edges instead of outgoing.
+	Reversed bool
+
 	// TODO(crbug.com/1136280): add SilbingDistance
 
 	heap spHeap
@@ -114,7 +116,11 @@ func (q *Query) Run(callback func(*ShortestPath) (keepGoing bool)) {
 			return true
 		}
 
-		cur.Node.Outgoing(consider)
+		if q.Reversed {
+			cur.Node.Incoming(consider)
+		} else {
+			cur.Node.Outgoing(consider)
+		}
 
 		// TODO(crbug.com/1136280): use q.SiblingDistance and call consider()
 	}
