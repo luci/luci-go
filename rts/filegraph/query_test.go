943ab25e71fbb0f64357cfc2d04e50c109672902
Nodir Turakulov <nodir@chromium.org>
2020-11-14 14:26:25 -0800

    [filegraph] Add path subcommand
    filegraph-path prints the shortest path from node A to node B,
    along with distances. It is particularly useful to diagnose distances
    that are shorter/longer than expected.
    
    R=guterman@google.com, tandrii@chromium.org
    
    Bug: 1136280
    Change-Id: I7f86dbc55e25078db2d8eff9bec56842f071f1c1

--- rts/filegraph/query_test.go
+++ rts/filegraph/query_test.go
@@ -91,51 +91,105 @@ func TestQuery(t *testing.T) {
 	t.Parallel()
 
 	Convey(`Query`, t, func() {
-		Convey(`Works`, func() {
+		Convey(`Run`, func() {
+			Convey(`Works`, func() {
+				g := initGraph(
+					testEdge{from: "//a", to: "//b/1", distance: 1},
+					testEdge{from: "//a", to: "//b/2", distance: 2},
+					testEdge{from: "//b/1", to: "//c", distance: 3},
+					testEdge{from: "//b/2", to: "//c", distance: 3},
+				)
+
+				sps := g.query("//a")
+				So(sps, ShouldResemble, map[string]*ShortestPath{
+					"//a": {
+						Node:     g.node("//a"),
+						Distance: 0,
+					},
+					"//b/1": {
+						Prev:     sps["//a"],
+						Node:     g.node("//b/1"),
+						Distance: 1,
+					},
+					"//b/2": {
+						Prev:     sps["//a"],
+						Node:     g.node("//b/2"),
+						Distance: 2,
+					},
+					"//c": {
+						Prev:     sps["//b/1"],
+						Node:     g.node("//c"),
+						Distance: 4,
+					},
+				})
+			})
+
+			Convey(`Unreachable`, func() {
+				g := initGraph(
+					testEdge{from: "//a", to: "//b", distance: 1},
+					testEdge{from: "//c", to: "//d"},
+				)
+
+				sps := g.query("//a")
+				So(sps, ShouldResemble, map[string]*ShortestPath{
+					"//a": {
+						Node:     g.node("//a"),
+						Distance: 0,
+					},
+					"//b": {
+						Prev:     sps["//a"],
+						Node:     g.node("//b"),
+						Distance: 1,
+					},
+				})
+			})
+
+			Convey(`Visiting the same node multiple times`, func() {
+				g := initGraph(
+					testEdge{from: "//a", to: "//b", distance: 1},
+					testEdge{from: "//a", to: "//c", distance: 10},
+					testEdge{from: "//b", to: "//c", distance: 1},
+				)
+				g.query("//a") // asserts that each node is reported once
+			})
+			Convey(`Duplicate sources`, func() {
+				g := initGraph(
+					testEdge{from: "//a", to: "//b", distance: 1},
+				)
+				g.query("//a", "//a") // asserts that each node is reported once
+			})
+		})
+
+		Convey(`ShortestPath`, func() {
 			g := initGraph(
 				testEdge{from: "//a", to: "//b/1", distance: 1},
 				testEdge{from: "//a", to: "//b/2", distance: 2},
 				testEdge{from: "//b/1", to: "//c", distance: 3},
 				testEdge{from: "//b/2", to: "//c", distance: 3},
+				testEdge{from: "//unreachable/1", to: "//unreachable/2"},
 			)
+			q := &Query{Sources: []Node{g.node("//a")}}
 
-			sps := g.query("//a")
-			So(sps, ShouldResemble, map[string]*ShortestPath{
-				"//a": {
-					Node:     g.node("//a"),
-					Distance: 0,
-				},
-				"//b/1": {
-					Prev:     sps["//a"],
-					Node:     g.node("//b/1"),
-					Distance: 1,
-				},
-				"//b/2": {
-					Prev:     sps["//a"],
-					Node:     g.node("//b/2"),
-					Distance: 2,
-				},
-				"//c": {
-					Prev:     sps["//b/1"],
+			Convey(`Works`, func() {
+				sp := q.ShortestPath(g.node("//c"))
+				So(sp, ShouldResemble, &ShortestPath{
 					Node:     g.node("//c"),
 					Distance: 4,
-				},
+					Prev: &ShortestPath{
+						Node:     g.node("//b/1"),
+						Distance: 1,
+						Prev: &ShortestPath{
+							Node:     g.node("//a"),
+							Distance: 0,
+						},
+					},
+				})
+				So(sp.Path(), ShouldResemble, []*ShortestPath{sp.Prev.Prev, sp.Prev, sp})
 			})
-		})
 
-		Convey(`Visiting the same node multiple times`, func() {
-			g := initGraph(
-				testEdge{from: "//a", to: "//b", distance: 1},
-				testEdge{from: "//a", to: "//c", distance: 10},
-				testEdge{from: "//b", to: "//c", distance: 1},
-			)
-			g.query("//a") // asserts that each node is reported once
-		})
-		Convey(`Duplicate sources`, func() {
-			g := initGraph(
-				testEdge{from: "//a", to: "//b", distance: 1},
-			)
-			g.query("//a", "//a") // asserts that each node is reported once
+			Convey(`Unreachable`, func() {
+				So(q.ShortestPath(g.node("//unreachable/1")), ShouldBeNil)
+			})
 		})
 	})
 }
