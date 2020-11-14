5140fd05c5cc636dbec343fefa64ffdc6bdb9f7d
Nodir Turakulov <nodir@chromium.org>
2020-11-14 01:19:42 -0800

    [filegraph] Add an end-to-end benchmark
    Add an end-to-end benchmark that will help assess optimizations.
    
    One a ThinkPad X1,
    - it processes luci-go.git changes in ~1300ms allocating 470MB in ~600k allocations
    - writes the graph in ~300ms
    - reads in ~180ms
    - traverses various top-level files in 300-400ms.
    
      goos: linux
      goarch: amd64
      pkg: go.chromium.org/luci/rts/filegraph/git
      BenchmarkE2E/Build-8         	       1	1257395796 ns/op	471307968 B/op	  594591 allocs/op
      BenchmarkE2E/Write-8         	       4	 284611381 ns/op
      BenchmarkE2E/Read-8          	       6	 181843447 ns/op
      BenchmarkE2E/Query-.luci-deploytool.cfg-8         	       3	 335687834 ns/op
      BenchmarkE2E/Query-README.md-8                    	       3	 339029138 ns/op
      BenchmarkE2E/Query-LICENSE-8                      	       3	 337827007 ns/op
      BenchmarkE2E/Query-WATCHLISTS-8                   	       3	 335946119 ns/op
      BenchmarkE2E/Query-CONTRIBUTORS-8                 	       3	 335825700 ns/op
      BenchmarkE2E/Query-CONTRIBUTING.md-8              	       3	 335385618 ns/op
      BenchmarkE2E/Query-pre-commit-go.yml-8            	       3	 345585953 ns/op
      BenchmarkE2E/Query-AUTHORS-8                      	       3	 344841791 ns/op
      BenchmarkE2E/Query-OWNERS-8                       	       3	 345018292 ns/op
      BenchmarkE2E/Query-.travis.yml-8                  	       3	 343372051 ns/op
      BenchmarkE2E/Query-codereview.settings-8          	       3	 348240569 ns/op
      BenchmarkE2E/Query-PRESUBMIT.py-8                 	       3	 352913733 ns/op
      BenchmarkE2E/Query-luci-deploy.cfg-8              	       3	 338246207 ns/op
      PASS
      ok  	go.chromium.org/luci/rts/filegraph/git	31.167s
    
    R=guterman@google.com, tandrii@chromium.org
    
    Bug: 1136280
    Change-Id: I012704cb9e3c795f8a630a85a47a200676904fbc

---
+++ rts/filegraph/git/e2e_test.go
@@ -0,0 +1,106 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package git
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"os"
+	"strings"
+	"testing"
+
+	"go.chromium.org/luci/rts/filegraph"
+	"go.chromium.org/luci/rts/filegraph/internal/gitutil"
+)
+
+// BenchmarkE2E measures performance of this package end to end:
+//  - Builds the graph from a git checkout
+//  - Serializes it.
+//  - Deserializes it.
+//  - Runs queries from some files
+//
+// Uses the git checkout at path in the FILEGRAPH_BENCH_CHECKOUT environment
+// variable. If it is not defined, then uses current repo (luci-go.git).
+func BenchmarkE2E(b *testing.B) {
+	ctx := context.Background()
+	repoDir := benchRepoDir(b)
+
+	var g *Graph
+
+	// First, build the graph from scratch.
+	b.Run("Build", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			g = &Graph{}
+			if err := g.Update(ctx, repoDir, "refs/heads/master", UpdateOptions{}); err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+
+	// Serialize it.
+	buf := &bytes.Buffer{}
+	b.Run("Write", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			buf.Reset()
+			if err := g.Write(buf); err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+
+	// Deserialize it.
+	b.Run("Read", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			r := bufio.NewReader(bytes.NewReader(buf.Bytes()))
+			if err := g.Read(r); err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+
+	// Run queries for each top-level file.
+	for _, n := range g.root.children {
+		n := n
+		if len(n.children) > 0 {
+			continue
+		}
+		b.Run("Query-"+strings.TrimPrefix(n.name, "//"), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				q := filegraph.Query{Sources: []filegraph.Node{n}}
+				q.Run(func(*filegraph.ShortestPath) bool {
+					return true
+				})
+			}
+		})
+	}
+}
+
+func benchRepoDir(b *testing.B) string {
+	if repoDir := os.Getenv("FILEGRAPH_BENCH_CHECKOUT"); repoDir != "" {
+		return repoDir
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		b.Fatal(err)
+	}
+	repoDir, err := gitutil.TopLevel(cwd)
+	if err != nil {
+		b.Fatal(err)
+	}
+	return repoDir
+}
