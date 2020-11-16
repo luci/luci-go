6e1cf661ac2ad5e23dd47a31efc665702b1a7a51
Andrii Shyshkalov <tandrii@google.com>
2020-11-16 21:38:47 +0100

    cv: define CL.ApplicableConfigs.
    I decided to properly handle subtle cases due to different LUCI
    projects potentially watching the same Gerrit Host/Project/Ref.
    
    In CQDaemon, it wasn't handled at all -- 2 CQDaemon processes
    (for each LUCI project) would independently handle the same CL.
    
    For CV, each CL in datastore is annotated with latest
    ApplicableConfigs, which is updated either together with the Snapshot
    (e.g. in Gerrit CL Updater) or separately (e.g. by a Run or
    a Project Manager). Normally, there will be exactly 1 applicable
    config and a matching Snapshot.Project. And if these don't match,
    then respective CV component can decide on the best course of action,
    for example:
     * Gerrit CL updater should force-update CL iff applicable config's
       LUCI Project changes.
     * ProjectManager should update its waiting for stabilization
       to-be-created Runs if CL's applicable config group changes
     * Run Manager should stop iff CL is no longer owned by the same
       LUCI Project, and post an appropriate error message.
    
    R=qyearsley, yiwzhang
    
    Bug: 1146015
    Change-Id: Idb5d64c4c72e472009daf98e58f6ca6f5c4f371a

--- cv/internal/changelist/model_test.go
+++ cv/internal/changelist/model_test.go
@@ -122,7 +122,8 @@ func TestUpdateSnapshot(t *testing.T) {
 
 		Convey("new CL is created", func() {
 			snap := makeSnapshot(epoch)
-			err := UpdateSnapshot(ctx, eid, 0 /* unknown CLID */, snap)
+			acfg := makeApplicableConfig(epoch)
+			err := Update(ctx, eid, 0 /* unknown CLID */, snap, acfg)
 			So(err, ShouldBeNil)
 			cl, err := eid.Get(ctx)
 			So(err, ShouldBeNil)
@@ -131,11 +132,15 @@ func TestUpdateSnapshot(t *testing.T) {
 		})
 
 		Convey("update existing", func() {
-			cl, err := eid.GetOrInsert(ctx, func(*CL) { /* no snapshot attached yet*/ })
+			acfg := makeApplicableConfig(epoch)
+			cl, err := eid.GetOrInsert(ctx, func(cl *CL) {
+				// no snapshot attached yet
+				cl.ApplicableConfig = acfg
+			})
 			So(err, ShouldBeNil)
 
 			snap := makeSnapshot(epoch)
-			err = UpdateSnapshot(ctx, eid, 0 /* unknown CLID */, snap)
+			err = Update(ctx, eid, 0 /* unknown CLID */, snap, nil /*don't change acfg*/)
 			So(err, ShouldBeNil)
 
 			cl2, err := eid.Get(ctx)
@@ -143,22 +148,25 @@ func TestUpdateSnapshot(t *testing.T) {
 			So(cl2.ID, ShouldEqual, cl.ID)
 			So(cl2.EVersion, ShouldEqual, 2)
 			So(cl2.Snapshot, ShouldResembleProto, snap)
+			So(cl2.ApplicableConfig, ShouldResembleProto, makeApplicableConfig(epoch))
 
 			Convey("with known CLID", func() {
-				snap2 := makeSnapshot(epoch.Add(time.Minute))
-				err = UpdateSnapshot(ctx, "" /*unspecified externalID*/, cl.ID, snap2)
+				acfg2 := makeApplicableConfig(epoch.Add(time.Minute))
+				err = Update(ctx, "" /*unspecified externalID*/, cl.ID, nil /*don't change snapshot*/, acfg2)
 				So(err, ShouldBeNil)
 
 				cl3, err := eid.Get(ctx)
 				So(err, ShouldBeNil)
 				So(cl3.ID, ShouldEqual, cl.ID)
 				So(cl3.EVersion, ShouldEqual, 3)
-				So(cl3.Snapshot, ShouldResembleProto, snap2)
+				So(cl3.Snapshot, ShouldResembleProto, snap)
+				So(cl3.ApplicableConfig, ShouldResembleProto, acfg2)
 			})
 
 			Convey("skip if not newer", func() {
-				snap2 := makeSnapshot(epoch.Add(-time.Minute))
-				err = UpdateSnapshot(ctx, "", cl.ID, snap2)
+				snapOld := makeSnapshot(epoch.Add(-time.Minute))
+				acfgOld := makeApplicableConfig(epoch.Add(-time.Minute))
+				err = Update(ctx, "", cl.ID, snapOld, acfgOld)
 				So(err, ShouldBeNil)
 
 				cl3, err := eid.Get(ctx)
@@ -166,6 +174,7 @@ func TestUpdateSnapshot(t *testing.T) {
 				So(cl3.ID, ShouldEqual, cl.ID)
 				So(cl3.EVersion, ShouldEqual, 2)
 				So(cl3.Snapshot, ShouldResembleProto, snap)
+				So(cl3.ApplicableConfig, ShouldResembleProto, acfg)
 			})
 		})
 	})
@@ -207,13 +216,17 @@ func TestConcurrentUpdateSnapshot(t *testing.T) {
 		wg.Add(N)
 		delays := mathrand.Perm(ctx, N) // 0,1,...,N-1
 		for _, d := range delays {
-			d := d
+			// Simulate opposing Snapshot and ApplicableConfig timestamps for better
+			// test coverage.
+			snapTS := epoch.Add(time.Minute * time.Duration(d))
+			acfgTS := epoch.Add(time.Minute * time.Duration(N-1-d))
 			go func() {
 				defer wg.Done()
-				snap := makeSnapshot(epoch.Add(time.Minute * time.Duration(d)))
+				snap := makeSnapshot(snapTS)
+				acfg := makeApplicableConfig(acfgTS)
 				var err error
 				for i := 0; i < R; i++ {
-					if err = UpdateSnapshot(ctx, eid, 0, snap); err == nil {
+					if err = Update(ctx, eid, 0, snap, acfg); err == nil {
 						t.Logf("succeeded after %d tries", i)
 						return
 					}
@@ -232,8 +245,9 @@ func TestConcurrentUpdateSnapshot(t *testing.T) {
 		So(err, ShouldBeNil)
 		// Since all workers have succeded, the latest snapshot
 		// (by ExternalUpdateTime) must be the current snapshot in datastore.
-		latest := makeSnapshot(epoch.Add((N - 1) * time.Minute))
-		So(cl.Snapshot, ShouldResembleProto, latest)
+		latestTS := epoch.Add((N - 1) * time.Minute)
+		So(cl.Snapshot, ShouldResembleProto, makeSnapshot(latestTS))
+		So(cl.ApplicableConfig, ShouldResembleProto, makeApplicableConfig(latestTS))
 		// Furthermore, there must have been at most N non-noop UpdateSnapshot
 		// calls (one per worker, iff they did exactly in the increasing order of
 		// the ExternalUpdateTime).
@@ -256,5 +270,14 @@ func makeSnapshot(updatedTime time.Time) *Snapshot {
 				},
 			},
 		}},
+	}
+}
+
+func makeApplicableConfig(updatedTime time.Time) *ApplicableConfig {
+	return &ApplicableConfig{
+		UpdateTime: timestamppb.New(updatedTime),
+		Projects: []*ApplicableConfig_Project{
+			{Name: "luci_project", ConfigGroupIds: []string{"blah"}},
+		},
 	}
 }
