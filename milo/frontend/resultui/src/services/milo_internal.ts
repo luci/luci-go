276cbe6edfa83717064a64e156685cd86c781a43
Joey Scarr <jsca@google.com>
2020-11-16 15:36:04 +1100

    [resultui] Add a blamelist summary and Gitiles link.
    This also adds a duplicate 'Load more' button to the top of the page.
    
    Bug: 1149222
    Change-Id: Ia7fd24bdd336f97acec2c813fdd2f30c19513f59

--- milo/frontend/resultui/src/services/milo_internal.ts
+++ milo/frontend/resultui/src/services/milo_internal.ts
@@ -66,6 +66,7 @@ export interface GitCommit {
 export interface QueryBlamelistResponse {
   readonly commits: GitCommit[];
   readonly nextPageToken?: string;
+  readonly precedingCommit: GitCommit;
 }
 
 const SERVICE = 'luci.milo.v1.MiloInternal';
