350308754200129464672001635b7091fe3ea885
Andrii Shyshkalov <tandrii@google.com>
2020-11-17 20:32:28 +0100

    cv: implement scaffolding for updating CL by querying Gerrit.
    Follow ups will add:
     * Handling 404. Eventual consistency makes life hard.
     * Handling 403. A bit better, but sometimes caused by 404 above.
     * Fetching file list.
     * Fetching dependencies.
     * Initializing dep CLs and creating TQ tasks to fetch deps.
    
    R=qyearsley, yiwzhang
    
    Bug: 1146015
    Change-Id: Id16dd52a860f28d917f18c65f6890ae5a6cbe6e8

---
+++ cv/internal/changelist/util.go
@@ -0,0 +1,73 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package changelist
+
+import (
+	"time"
+)
+
+// IsUpToDate returns whether stored ApplicableConfig is at least as recent as
+// given time.
+func (s *ApplicableConfig) IsUpToDate(t time.Time) bool {
+	switch {
+	case s == nil:
+		return false
+	case t.After(s.UpdateTime.AsTime()):
+		return false
+	default:
+		return true
+	}
+}
+
+// IsUpToDate returns whether stored snapshot is at least as recent as given
+// time and was done for a matching LUCI project.
+func (s *Snapshot) IsUpToDate(luciProject string, t time.Time) bool {
+	switch {
+	case s == nil:
+		return false
+	case t.After(s.ExternalUpdateTime.AsTime()):
+		return false
+	case s.LuciProject != luciProject:
+		return false
+	default:
+		return true
+	}
+}
+
+// IsOnlyProject returns true iff ApplicableConfig contains only the given
+// project, regardless of the number of applicable config groups it may contain.
+func (a *ApplicableConfig) IsOnlyProject(luciProject string) bool {
+	switch {
+	case a == nil:
+		return false
+	case len(a.Projects) != 1:
+		return false
+	case a.Projects[0].Name == luciProject:
+		return false
+	default:
+		return true
+	}
+}
+
+// ContainsProject returns true whether ApplicableConfig contains the given
+// project, possibly among other projects.
+func (a *ApplicableConfig) ContainsProject(luciProject string) bool {
+	for _, p := range a.GetProjects() {
+		if p.Name == luciProject {
+			return true
+		}
+	}
+	return false
+}
