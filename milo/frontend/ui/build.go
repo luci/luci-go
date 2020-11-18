b2cbd8b79ca308f7d0ea4fb90ec8c01ca13ec078
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2020-11-18 04:20:20 +0000

    [BuildPage] Update banner for switch day
    Bug:1150146
    Change-Id: I1e65e9b4ba3d31ae6a75b7dacd7fd88f0e629d89
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2545906
    Commit-Queue: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Auto-Submit: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Reviewed-by: Weiwei Lin <weiweilin@google.com>

--- milo/frontend/ui/build.go
+++ milo/frontend/ui/build.go
@@ -632,6 +632,7 @@ var (
 							href="/ui/p/{{.Builder.Project}}/builders/{{.Builder.Bucket}}/{{.Builder.Builder}}/b{{.Id}}"
 						{{end}}
 					>Switch to the new build page!</a>
+					(Note: from 1 Dec 2020, this page will redirect by default.)
 				</div>`))
 )
 
