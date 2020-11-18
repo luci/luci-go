d54333e76f2939d6e1cd8033e4a48c66be1500d7
Roberto Carrillo <robertocn@chromium.org>
2020-11-18 15:33:19 -0800

    [resultdb] Add benchmark for result history retrieval.
    Benchmark results, 12-core-macbook off corp
    ```
    $ INTEGRATION_TESTS=1 go test -benchmem -run=^$ go.chromium.org/luci/resultdb/internal/history -bench BenchmarkGetTestResultsHistory -timeout 2h
    created a temporary Spanner database projects/chops-spanner-testing/instances/testing/databases/tmp20201116_4136163547 in 4.791444707s
    goos: darwin
    goarch: amd64
    pkg: go.chromium.org/luci/resultdb/internal/history
    BenchmarkGetTestResultsHistory/SpecificTest-25600-invocations-409600-results-12                        1        95295857155 ns/op       715261384 B/op  11266907 allocs/op
    BenchmarkGetTestResultsHistory/SpecificTestVariant-25600-invocations-409600-results-12                 1        56367387365 ns/op       484383424 B/op   7640516 allocs/op
    BenchmarkGetTestResultsHistory/SpecificTest-256000-invocations-4096000-results-12                      1        812161802765 ns/op      7883834528 B/op 124474504 allocs/op
    BenchmarkGetTestResultsHistory/SpecificTestVariant-256000-invocations-4096000-results-12                       1        449016854746 ns/op      4506527800 B/op 71157669 allocs/op
    PASS
    ok      go.chromium.org/luci/resultdb/internal/history  4794.298s
    ```
    
    Bug:1074407
    Change-Id: I17cc4ed38a1653cdf5e4db4cf3e1cc423a3cf9a2
    R=nodir,chanli

---
+++ resultdb/internal/history/history_benchmark_test.go
@@ -0,0 +1,164 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package history
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/spanner"
+	"github.com/golang/protobuf/ptypes"
+
+	"go.chromium.org/luci/common/clock/testclock"
+	"go.chromium.org/luci/server/span"
+
+	"go.chromium.org/luci/resultdb/internal/invocations"
+	"go.chromium.org/luci/resultdb/internal/spanutil"
+	"go.chromium.org/luci/resultdb/internal/testutil"
+	"go.chromium.org/luci/resultdb/internal/testutil/insert"
+	"go.chromium.org/luci/resultdb/pbutil"
+	pb "go.chromium.org/luci/resultdb/proto/v1"
+)
+
+var useRedis = os.Getenv(testutil.RedisTestEnvVar) == "1"
+
+type benchmarkDataParams struct {
+	nOSes, nVariants, nBuilds, nSteps, nShards, nTests, nResults int
+}
+
+func insertBenchmarkData(ctx context.Context, p benchmarkDataParams, buildOffset int) {
+
+	start := testclock.TestRecentTimeUTC
+	ms := make([]*spanner.Mutation, 0)
+	for os := 0; os < p.nOSes; os++ {
+		for v := 0; v < p.nVariants; v++ {
+			builder := fmt.Sprintf("OS%d_Variant%d", os, v)
+			for build := buildOffset; build < buildOffset+p.nBuilds; build++ {
+				buildInvID := invocations.ID(fmt.Sprintf("%s-build%d", builder, build))
+				buildTS := start.Add(time.Duration(os*v) * time.Minute) // + build hours + ov minutes
+				ms = append(ms, insert.Invocation(buildInvID, pb.Invocation_ACTIVE, map[string]interface{}{
+					"HistoryTime": buildTS,
+					"Realm":       "luci:benchmark",
+				}))
+				reachables := invocations.NewIDSet()
+				for step := 0; step < p.nSteps; step++ {
+					stepInvID := invocations.ID(fmt.Sprintf("%s-build%d-step%d", builder, build, step))
+					ms = append(ms, insert.Invocation(stepInvID, pb.Invocation_ACTIVE, nil))
+					ms = append(ms, insert.Inclusion(buildInvID, stepInvID))
+					reachables.Add(stepInvID)
+					for shard := 0; shard < p.nShards; shard++ {
+						shardInvID := invocations.ID(fmt.Sprintf("%s-build%d-step%d-shard%d", builder, build, step, shard))
+						ms = append(ms, insert.Invocation(shardInvID, pb.Invocation_ACTIVE, nil))
+						ms = append(ms, insert.Inclusion(stepInvID, shardInvID))
+						reachables.Add(shardInvID)
+						for test := 0; test < p.nTests; test++ {
+							testID := fmt.Sprintf("step%d-shard%d-test%d", step, shard, test)
+							for result := 0; result < p.nResults; result++ {
+								resultID := fmt.Sprint(result)
+								ms = append(ms,
+									spanutil.InsertMap("TestResults", map[string]interface{}{
+										"InvocationId":    shardInvID,
+										"TestId":          testID,
+										"ResultId":        resultID,
+										"Variant":         pbutil.Variant("os", fmt.Sprint(os), "variant", fmt.Sprint(v)),
+										"VariantHash":     fmt.Sprintf("deadbeef%d%d", os, v),
+										"CommitTimestamp": spanner.CommitTimestamp,
+										"Status":          pb.TestStatus_PASS,
+										"RunDurationUsec": 1534567,
+									}))
+
+							}
+						}
+					}
+				}
+				if useRedis {
+					invocations.ReachCache(buildInvID).TryWrite(ctx, reachables)
+				}
+
+			}
+		}
+	}
+	maxSpannerBatch := 2000
+	for len(ms) > maxSpannerBatch {
+		if _, err := span.Apply(ctx, ms[:maxSpannerBatch]); err != nil {
+			panic(err)
+		}
+		ms = ms[maxSpannerBatch:]
+	}
+
+	if _, err := span.Apply(ctx, ms); err != nil {
+		panic(err)
+	}
+}
+
+func BenchmarkGetTestResultsHistory(b *testing.B) {
+	runQueryBenchmark := func(ctx context.Context, b *testing.B, name string, q *Query) {
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				iCtx, cancel := span.ReadOnlyTransaction(ctx)
+				if _, _, err := q.Execute(iCtx); err != nil {
+					panic(err)
+				}
+				cancel()
+			}
+		})
+	}
+
+	past, _ := ptypes.TimestampProto(testclock.TestRecentTimeUTC)
+	q := &Query{
+		Request: &pb.GetTestResultHistoryRequest{
+			Range: &pb.GetTestResultHistoryRequest_TimeRange{
+				TimeRange: &pb.TimeRange{
+					Earliest: past,
+					Latest:   ptypes.TimestampNow(),
+				},
+			},
+			Realm: "luci:benchmark",
+		},
+		ignoreDeadline: true,
+	}
+
+	p := benchmarkDataParams{4, 4, 4, 4, 4, 4, 4}
+	ctx := testutil.SpannerTestContext(b)
+	lastBuild := 0
+
+	// 16 builders, each with (100, 1000) builds, where each build-level
+	// invocation contains 20 sub-invocations and 256 results.
+	for _, p.nBuilds = range []int{100, 1000} {
+
+		insertBenchmarkData(ctx, p, lastBuild)
+		lastBuild += p.nBuilds
+		totalInvocations := p.nOSes * p.nVariants * p.nBuilds * p.nSteps * p.nShards
+		totalResults := totalInvocations * p.nTests * p.nResults
+		suffix := "-no-redis"
+		if useRedis {
+			suffix = "-local-redis"
+		}
+
+		q.Request.TestIdRegexp = "step0-shard0-step0"
+		runQueryBenchmark(ctx, b, fmt.Sprintf("SpecificTest-%d-invocations-%d-results%s", totalInvocations, totalResults, suffix), q)
+
+		q.Request.TestIdRegexp = "step0-shard0-test0"
+		q.Request.VariantPredicate = &pb.VariantPredicate{
+			Predicate: &pb.VariantPredicate_Contains{
+				Contains: pbutil.Variant("os", "0", "variant", "0"),
+			},
+		}
+		runQueryBenchmark(ctx, b, fmt.Sprintf("SpecificTestVariant-%d-invocations-%d-results%s", totalInvocations, totalResults, suffix), q)
+	}
+}
