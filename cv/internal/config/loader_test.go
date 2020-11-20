fcda7cca51c9b7e4929c51404b6bf284dc9b1337
Andrii Shyshkalov <tandrii@google.com>
2020-11-20 23:28:09 +0000

    cv: add loading functions for config.
    R=qyearsley, yiwzhang
    
    Bug: 1129716
    Change-Id: I2b027531729326aeb94eeb6dd25a71b1f4e4580c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2550667
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>

---
+++ cv/internal/config/loader_test.go
@@ -0,0 +1,174 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"context"
+	"testing"
+
+	gaememory "go.chromium.org/luci/gae/impl/memory"
+	"go.chromium.org/luci/gae/service/datastore"
+
+	pb "go.chromium.org/luci/cv/api/config/v2"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestLoadingConfigs(t *testing.T) {
+	t.Parallel()
+	Convey("Load project config works", t, func() {
+		ctx := gaememory.Use(context.Background())
+		datastore.GetTestable(ctx).AutoIndex(true)
+		datastore.GetTestable(ctx).Consistent(true)
+
+		tc := TestController{}
+		const project = "chromium"
+
+		Convey("Not existing project", func() {
+			tc.MustNotExist(ctx, project)
+			m, err := GetLatestMeta(ctx, project)
+			So(err, ShouldBeNil)
+			So(m.Exists(), ShouldBeFalse)
+			So(m.EVersion, ShouldEqual, 0)
+			So(func() { m.Hash() }, ShouldPanic)
+		})
+
+		cfg := &pb.Config{
+			ConfigGroups: []*pb.ConfigGroup{
+				{
+					Name: "branch_m100",
+					Gerrit: []*pb.ConfigGroup_Gerrit{
+						{
+							Url: "https://chromium-review.googlesource.com/",
+							Projects: []*pb.ConfigGroup_Gerrit_Project{
+								{
+									Name:      "chromium/src",
+									RefRegexp: []string{"refs/heads/branch_m100"},
+								},
+							},
+						},
+					},
+				},
+				{
+					Fallback: pb.Toggle_YES,
+					Gerrit: []*pb.ConfigGroup_Gerrit{
+						{
+							Url: "https://chromium-review.googlesource.com/",
+							Projects: []*pb.ConfigGroup_Gerrit_Project{
+								{
+									Name:      "chromium/src",
+									RefRegexp: []string{"refs/heads/main"},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		tc.Create(ctx, project, cfg)
+		Convey("Enabled project", func() {
+			tc.MustExist(ctx, project)
+			m, err := GetLatestMeta(ctx, project)
+			So(err, ShouldBeNil)
+			So(m.Exists(), ShouldBeTrue)
+			So(m.EVersion, ShouldEqual, 1)
+			h := m.Hash()
+			So(h, ShouldStartWith, "sha256:")
+			So(m.ConfigGroupIDs, ShouldResemble, []ConfigGroupID{
+				ConfigGroupID(h + "/branch_m100"),
+				ConfigGroupID(h + "/index#1"),
+			})
+			cgs, err := m.GetConfigGroups(ctx)
+			So(err, ShouldBeNil)
+			So(len(cgs), ShouldEqual, 2)
+			So(cgs[0].Content, ShouldResembleProto, cfg.ConfigGroups[0])
+			So(cgs[1].Content, ShouldResembleProto, cfg.ConfigGroups[1])
+		})
+
+		cfg.ConfigGroups = append(cfg.ConfigGroups, &pb.ConfigGroup{
+			Name: "branch_m200",
+			Gerrit: []*pb.ConfigGroup_Gerrit{
+				{
+					Url: "https://chromium-review.googlesource.com/",
+					Projects: []*pb.ConfigGroup_Gerrit_Project{
+						{
+							Name:      "chromium/src",
+							RefRegexp: []string{"refs/heads/branch_m200"},
+						},
+					},
+				},
+			},
+		})
+		tc.Update(ctx, project, cfg)
+		Convey("Updated project", func() {
+			tc.MustExist(ctx, project)
+			m, err := GetLatestMeta(ctx, project)
+			So(err, ShouldBeNil)
+			So(m.Exists(), ShouldBeTrue)
+			So(m.EVersion, ShouldEqual, 2)
+			h := m.Hash()
+			So(h, ShouldStartWith, "sha256:")
+			So(m.ConfigGroupIDs, ShouldResemble, []ConfigGroupID{
+				ConfigGroupID(h + "/branch_m100"),
+				ConfigGroupID(h + "/index#1"),
+				ConfigGroupID(h + "/branch_m200"),
+			})
+			cgs, err := m.GetConfigGroups(ctx)
+			So(err, ShouldBeNil)
+			So(len(cgs), ShouldEqual, 3)
+
+			Convey("reading ConfigGroup directly works", func() {
+				cg, err := GetConfigGroup(ctx, project, m.ConfigGroupIDs[2])
+				So(err, ShouldBeNil)
+				So(cg.Content, ShouldResembleProto, cfg.ConfigGroups[2])
+			})
+		})
+
+		tc.Disable(ctx, project)
+		Convey("Disabled project", func() {
+			tc.MustExist(ctx, project)
+			m, err := GetLatestMeta(ctx, project)
+			So(err, ShouldBeNil)
+			So(m.Exists(), ShouldBeTrue)
+			So(m.Status, ShouldEqual, StatusDisabled)
+			So(m.EVersion, ShouldEqual, 3)
+			So(len(m.ConfigGroupIDs), ShouldEqual, 3)
+			cgs, err := m.GetConfigGroups(ctx)
+			So(err, ShouldBeNil)
+			So(len(cgs), ShouldEqual, 3)
+
+			Convey("reading partially deleted project", func() {
+				So(datastore.Delete(ctx, cgs[1]), ShouldBeNil)
+				_, err := m.GetConfigGroups(ctx)
+				So(err, ShouldErrLike, "ConfigGroups for")
+				So(err, ShouldErrLike, "not found")
+				So(datastore.IsErrNoSuchEntity(err), ShouldBeTrue)
+
+				// Can still read individual ConfigGroups.
+				cg, err := GetConfigGroup(ctx, project, m.ConfigGroupIDs[0])
+				So(err, ShouldBeNil)
+				So(cg.Content, ShouldResembleProto, cfg.ConfigGroups[0])
+				cg, err = GetConfigGroup(ctx, project, m.ConfigGroupIDs[2])
+				So(err, ShouldBeNil)
+				So(cg.Content, ShouldResembleProto, cfg.ConfigGroups[2])
+				// ... except the deleted one.
+				cg, err = GetConfigGroup(ctx, project, m.ConfigGroupIDs[1])
+				So(datastore.IsErrNoSuchEntity(err), ShouldBeTrue)
+			})
+		})
+		// TODO(tandrii): implement enable & delete.
+	})
+}
