3dc9ada7f76cb8664f49e26091ed3c3a179d21a1
Andrii Shyshkalov <tandrii@google.com>
2020-11-23 20:37:12 +0100

    cv: implement gerritfake GetRelatedChanges.
    This finally add tests, because code isn't trivial.
    I think this is the hardest part of gerritfake.
    
    R=qyearsley, yiwzhang
    
    Bug: 1150185
    Change-Id: Icb0e7610fe418b5001d528bb595c899369862efc

--- cv/internal/gerrit/gerritfake/client.go
+++ cv/internal/gerrit/gerritfake/client.go
@@ -18,7 +18,10 @@ import (
 	"context"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
+	"go.chromium.org/luci/common/data/stringset"
 	gerritpb "go.chromium.org/luci/common/proto/gerrit"
 
 	"go.chromium.org/luci/cv/internal/gerrit"
@@ -49,8 +52,64 @@ func (c *Client) GetChange(ctx context.Context, in *gerritpb.GetChangeRequest, o
 // the revision.
 //
 // https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#get-related-changes
-func (c *Client) GetRelatedChanges(ctx context.Context, in *gerritpb.GetRelatedChangesRequest, opts ...grpc.CallOption) (*gerritpb.GetRelatedChangesResponse, error) {
-	panic("not implemented") // TODO: Implement
+func (client *Client) GetRelatedChanges(ctx context.Context, in *gerritpb.GetRelatedChangesRequest, opts ...grpc.CallOption) (*gerritpb.GetRelatedChangesResponse, error) {
+	client.f.m.Lock()
+	defer client.f.m.Unlock()
+
+	change, found := client.f.cs[key(client.host, int(in.GetNumber()))]
+	if !found {
+		return nil, status.Errorf(codes.NotFound, "change %s/%d not found", client.host, in.GetNumber())
+	}
+	if status := change.ACLs(OpRead, client.luciProject); status.Code() != codes.OK {
+		return nil, status.Err()
+	}
+	ps, _, err := change.resolveRevision(in.GetRevisionId())
+	if err != nil {
+		return nil, err
+	}
+	start := psKey(client.host, int(in.GetNumber()), ps)
+
+	res := &gerritpb.GetRelatedChangesResponse{}
+	added := stringset.New(10)
+	add := func(psk string) error {
+		if !added.Add(psk) {
+			return nil
+		}
+		change, rev, ri, err := client.f.resolvePSKeyLocked(psk)
+		if err != nil {
+			return err
+		}
+		cc := &gerritpb.GetRelatedChangesResponse_ChangeAndCommit{
+			Project:         change.Info.GetProject(),
+			Number:          change.Info.GetNumber(),
+			Patchset:        int64(ri.GetNumber()),
+			CurrentPatchset: int64(change.Info.GetRevisions()[change.Info.GetCurrentRevision()].GetNumber()),
+			Commit:          &gerritpb.CommitInfo{Id: rev},
+		}
+		for _, parentPSkey := range client.f.parentsOf[psk] {
+			_, parentRev, _, err := client.f.resolvePSKeyLocked(parentPSkey)
+			if err != nil {
+				return err
+			}
+			cc.GetCommit().Parents = append(cc.GetCommit().Parents, &gerritpb.CommitInfo_Parent{Id: parentRev})
+		}
+		res.Changes = append(res.Changes, cc)
+		return nil
+	}
+	// NOTE: Gerrit actually guarantees specific order. For simplicity, this fake
+	// doesn't. We just recurse in direction of both child->parent and
+	// parent->child and add visited changes to the list.
+	if err := visitNodesDFS(start, client.f.childrenOf, add); err != nil {
+		return nil, err
+	}
+	if err := visitNodesDFS(start, client.f.parentsOf, add); err != nil {
+		return nil, err
+	}
+	if len(res.GetChanges()) == 1 {
+		// Just the starting change itself, emulate Gerrit by returning empty list.
+		res.Changes = nil
+	}
+	return res, nil
 }
 
 // Lists the files that were modified, added or deleted in a revision.
@@ -58,4 +117,27 @@ func (c *Client) GetRelatedChanges(ctx context.Context, in *gerritpb.GetRelatedC
 // https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#list-files
 func (c *Client) ListFiles(ctx context.Context, in *gerritpb.ListFilesRequest, opts ...grpc.CallOption) (*gerritpb.ListFilesResponse, error) {
 	panic("not implemented") // TODO: Implement
+}
+
+// visitNodesDFS visits all nodes reachable from the current node via depth
+// first search.
+//
+// Calls clbk for each node visited. If clbk returns error, visitNodesDFS aborts
+// immediatey and returns the same error.
+func visitNodesDFS(node string, edges map[string][]string, clbk func(node string) error) error {
+	visited := stringset.New(1)
+
+	var visit func(n string) error
+	visit = func(n string) error {
+		if !visited.Add(n) {
+			return nil
+		}
+		for _, m := range edges[n] {
+			if err := visit(m); err != nil {
+				return err
+			}
+		}
+		return clbk(n)
+	}
+	return visit(node)
 }
