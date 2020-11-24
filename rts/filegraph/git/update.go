1776d510578e6540a473049ff51b37d1148f1ec9
Nodir Turakulov <nodir@chromium.org>
2020-11-23 17:56:08 -0800

    [filegraph] Improve probability formula
    Take into account the number of files changed in a commit.
    Small commits provide a stronger signal, and large commits provide a
    weaker one. Update the probability formula, see doc.go.
    
    This improves rts-chromium evaluation scores
    from
      97.41% change recall of linux-rel rejections in October
      25.56% savings on linux-rel duration data on 2020-10-30
      using max-distance 4
    to
      97.63% change recall
      38.19% savings
      using max-distance 14
    
    October data without data durations:
      Change recall:
        Score: 97.63%
        Eligible rejections:                       1353
        Eligible rejections preserved by this RTS: 1321
      Test recall:
        Score: 97.64%
        Eligible test failures:                       849727
        Eligible test failures preserved by this RTS: 829647
      Efficiency:
        Evaluation failed: no test results with duration
      Total records: 858838
    
    2020-10-30 with data durations:
      Change recall:
        Score: 94.95%
        Eligible rejections:                       99
        Eligible rejections preserved by this RTS: 94
      Test recall:
        Score: 94.17%
        Eligible test failures:                       62594
        Eligible test failures preserved by this RTS: 58946
      Efficiency:
        Saved: 38.19%
        Test results analyzed: 15968
        Compute time in the sample: 9h56m15.700738854s
        Forecasted compute time:    6h8m32.097689461s
      Total records: 78709
    
    Bug: 1136280
    Change-Id: I00f20560a7dc31db0603470b33fe84f9c04798e7

--- rts/filegraph/git/update.go
+++ rts/filegraph/git/update.go
@@ -16,6 +16,7 @@ package git
 
 import (
 	"context"
+	"math"
 
 	"go.chromium.org/luci/common/errors"
 )
@@ -102,16 +103,22 @@ func (g *Graph) apply(fileChanges []fileChange) error {
 	// Create edges between each file pair.
 	// This is O(FILES * (FILES + EDGES_PER_FILE))
 
+	// Skip this commit if there is only one file to process,
+	// since it does not provide any signal.
+	if len(files) <= 1 {
+		return nil
+	}
+
+	// For any file in |files|, compute the probability of picking
+	// any other file.
+	p := 1.0 / float64(len(files)-1)
+
 	fileSet := make(map[*node]struct{}, len(files))
 	for _, f := range files {
 		fileSet[f] = struct{}{}
 	}
 	for _, file := range files {
-		file.commits++
-
-		// TODO(nodir): take the commit size into account of the distance.
-		// Smaller commits provide stronger signal of file relatedness.
-		// Specifically, consider incrementing commonCommits by 1/(len(files) - 1).
+		file.probSumDenominator++
 
 		updated := make(map[*node]struct{}, len(files)-1)
 		// Increment the commit count in file's edges that point to other files.
@@ -119,9 +126,10 @@ func (g *Graph) apply(fileChanges []fileChange) error {
 			if _, ok := fileSet[e.to]; ok {
 				updated[e.to] = struct{}{}
 
-				// Increment the common commit count unless it is an alias edge.
-				if e.commonCommits != 0 {
-					file.edges[i].commonCommits++
+				// Add the probability of this file being selected from this comment,
+				// unless it is an alias edge.
+				if !math.IsNaN(e.probSum) {
+					file.edges[i].probSum += p
 				}
 			}
 		}
@@ -131,7 +139,7 @@ func (g *Graph) apply(fileChanges []fileChange) error {
 			if to != file {
 				if _, ok := updated[to]; !ok {
 					file.prepareToAppendEdges()
-					file.edges = append(file.edges, edge{to: to, commonCommits: 1})
+					file.edges = append(file.edges, edge{to: to, probSum: p})
 				}
 			}
 		}
