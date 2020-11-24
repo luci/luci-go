68d2dc74b0953af6711a260df5b0dba7f69ac726
Andrii Shyshkalov <tandrii@google.com>
2020-11-24 00:47:16 +0000

    cv: fix package name after config package move.
    R=qyearsley, yiwzhang
    
    Bug: 1129716
    Change-Id: I6fb2a8d17f588d4cec9006cd3d1f56931217b245
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2552798
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>

--- cv/internal/config/tasks.pb.go
+++ cv/internal/config/tasks.pb.go
@@ -16,7 +16,7 @@
 // versions:
 // 	protoc-gen-go v1.25.0-devel
 // 	protoc        v3.12.1
-// source: go.chromium.org/luci/cv/appengine/internal/config/tasks.proto
+// source: go.chromium.org/luci/cv/internal/config/tasks.proto
 
 package config
 
@@ -50,7 +50,7 @@ type RefreshProjectConfigTask struct {
 func (x *RefreshProjectConfigTask) Reset() {
 	*x = RefreshProjectConfigTask{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_msgTypes[0]
+		mi := &file_go_chromium_org_luci_cv_internal_config_tasks_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -63,7 +63,7 @@ func (x *RefreshProjectConfigTask) String() string {
 func (*RefreshProjectConfigTask) ProtoMessage() {}
 
 func (x *RefreshProjectConfigTask) ProtoReflect() protoreflect.Message {
-	mi := &file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_msgTypes[0]
+	mi := &file_go_chromium_org_luci_cv_internal_config_tasks_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -76,7 +76,7 @@ func (x *RefreshProjectConfigTask) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use RefreshProjectConfigTask.ProtoReflect.Descriptor instead.
 func (*RefreshProjectConfigTask) Descriptor() ([]byte, []int) {
-	return file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDescGZIP(), []int{0}
+	return file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *RefreshProjectConfigTask) GetProject() string {
@@ -93,43 +93,41 @@ func (x *RefreshProjectConfigTask) GetDisable() bool {
 	return false
 }
 
-var File_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto protoreflect.FileDescriptor
+var File_go_chromium_org_luci_cv_internal_config_tasks_proto protoreflect.FileDescriptor
 
-var file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDesc = []byte{
-	0x0a, 0x3d, 0x67, 0x6f, 0x2e, 0x63, 0x68, 0x72, 0x6f, 0x6d, 0x69, 0x75, 0x6d, 0x2e, 0x6f, 0x72,
-	0x67, 0x2f, 0x6c, 0x75, 0x63, 0x69, 0x2f, 0x63, 0x76, 0x2f, 0x61, 0x70, 0x70, 0x65, 0x6e, 0x67,
-	0x69, 0x6e, 0x65, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x63, 0x6f, 0x6e,
-	0x66, 0x69, 0x67, 0x2f, 0x74, 0x61, 0x73, 0x6b, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
-	0x0f, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2e, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67,
-	0x22, 0x4e, 0x0a, 0x18, 0x52, 0x65, 0x66, 0x72, 0x65, 0x73, 0x68, 0x50, 0x72, 0x6f, 0x6a, 0x65,
-	0x63, 0x74, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x54, 0x61, 0x73, 0x6b, 0x12, 0x18, 0x0a, 0x07,
-	0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x70,
-	0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x12, 0x18, 0x0a, 0x07, 0x64, 0x69, 0x73, 0x61, 0x62, 0x6c,
-	0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x08, 0x52, 0x07, 0x64, 0x69, 0x73, 0x61, 0x62, 0x6c, 0x65,
-	0x42, 0x3a, 0x5a, 0x38, 0x67, 0x6f, 0x2e, 0x63, 0x68, 0x72, 0x6f, 0x6d, 0x69, 0x75, 0x6d, 0x2e,
-	0x6f, 0x72, 0x67, 0x2f, 0x6c, 0x75, 0x63, 0x69, 0x2f, 0x63, 0x76, 0x2f, 0x61, 0x70, 0x70, 0x65,
-	0x6e, 0x67, 0x69, 0x6e, 0x65, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x63,
-	0x6f, 0x6e, 0x66, 0x69, 0x67, 0x3b, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+var file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDesc = []byte{
+	0x0a, 0x33, 0x67, 0x6f, 0x2e, 0x63, 0x68, 0x72, 0x6f, 0x6d, 0x69, 0x75, 0x6d, 0x2e, 0x6f, 0x72,
+	0x67, 0x2f, 0x6c, 0x75, 0x63, 0x69, 0x2f, 0x63, 0x76, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e,
+	0x61, 0x6c, 0x2f, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x2f, 0x74, 0x61, 0x73, 0x6b, 0x73, 0x2e,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0f, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2e,
+	0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x22, 0x4e, 0x0a, 0x18, 0x52, 0x65, 0x66, 0x72, 0x65, 0x73,
+	0x68, 0x50, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x54, 0x61,
+	0x73, 0x6b, 0x12, 0x18, 0x0a, 0x07, 0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x18, 0x01, 0x20,
+	0x01, 0x28, 0x09, 0x52, 0x07, 0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x12, 0x18, 0x0a, 0x07,
+	0x64, 0x69, 0x73, 0x61, 0x62, 0x6c, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x08, 0x52, 0x07, 0x64,
+	0x69, 0x73, 0x61, 0x62, 0x6c, 0x65, 0x42, 0x30, 0x5a, 0x2e, 0x67, 0x6f, 0x2e, 0x63, 0x68, 0x72,
+	0x6f, 0x6d, 0x69, 0x75, 0x6d, 0x2e, 0x6f, 0x72, 0x67, 0x2f, 0x6c, 0x75, 0x63, 0x69, 0x2f, 0x63,
+	0x76, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x63, 0x6f, 0x6e, 0x66, 0x69,
+	0x67, 0x3b, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDescOnce sync.Once
-	file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDescData = file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDesc
+	file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDescOnce sync.Once
+	file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDescData = file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDesc
 )
 
-func file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDescGZIP() []byte {
-	file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDescOnce.Do(func() {
-		file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDescData = protoimpl.X.CompressGZIP(file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDescData)
+func file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDescGZIP() []byte {
+	file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDescOnce.Do(func() {
+		file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDescData = protoimpl.X.CompressGZIP(file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDescData)
 	})
-	return file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDescData
+	return file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDescData
 }
 
-var file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_goTypes = []interface{}{
+var file_go_chromium_org_luci_cv_internal_config_tasks_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_go_chromium_org_luci_cv_internal_config_tasks_proto_goTypes = []interface{}{
 	(*RefreshProjectConfigTask)(nil), // 0: internal.config.RefreshProjectConfigTask
 }
-var file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_depIdxs = []int32{
+var file_go_chromium_org_luci_cv_internal_config_tasks_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -137,13 +135,13 @@ var file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_depIdxs =
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_init() }
-func file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_init() {
-	if File_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto != nil {
+func init() { file_go_chromium_org_luci_cv_internal_config_tasks_proto_init() }
+func file_go_chromium_org_luci_cv_internal_config_tasks_proto_init() {
+	if File_go_chromium_org_luci_cv_internal_config_tasks_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_go_chromium_org_luci_cv_internal_config_tasks_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*RefreshProjectConfigTask); i {
 			case 0:
 				return &v.state
@@ -160,18 +158,18 @@ func file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDesc,
+			RawDescriptor: file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_goTypes,
-		DependencyIndexes: file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_depIdxs,
-		MessageInfos:      file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_msgTypes,
+		GoTypes:           file_go_chromium_org_luci_cv_internal_config_tasks_proto_goTypes,
+		DependencyIndexes: file_go_chromium_org_luci_cv_internal_config_tasks_proto_depIdxs,
+		MessageInfos:      file_go_chromium_org_luci_cv_internal_config_tasks_proto_msgTypes,
 	}.Build()
-	File_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto = out.File
-	file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_rawDesc = nil
-	file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_goTypes = nil
-	file_go_chromium_org_luci_cv_appengine_internal_config_tasks_proto_depIdxs = nil
+	File_go_chromium_org_luci_cv_internal_config_tasks_proto = out.File
+	file_go_chromium_org_luci_cv_internal_config_tasks_proto_rawDesc = nil
+	file_go_chromium_org_luci_cv_internal_config_tasks_proto_goTypes = nil
+	file_go_chromium_org_luci_cv_internal_config_tasks_proto_depIdxs = nil
 }
