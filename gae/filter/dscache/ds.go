c9c45888a4a77026d9ca528da95342c54d70c9ff
Vadim Shtayura <vadimsh@chromium.org>
2020-11-25 13:10:27 -0800

    [dscache] Extract memcache-related part from dscache.
    So we can use Redis instead where necessary.
    
    R=tandrii@chromium.org
    BUG=1140747
    
    Change-Id: I9b53b4ee0c118f8a5eeac86d89174a38d0761395

--- gae/filter/dscache/ds.go
+++ gae/filter/dscache/ds.go
@@ -15,15 +15,12 @@
 package dscache
 
 import (
+	"context"
 	"time"
 
+	"go.chromium.org/luci/common/logging"
+
 	ds "go.chromium.org/luci/gae/service/datastore"
-	mc "go.chromium.org/luci/gae/service/memcache"
-
-	"go.chromium.org/luci/common/errors"
-	log "go.chromium.org/luci/common/logging"
-
-	"golang.org/x/net/context"
 )
 
 type dsCache struct {
@@ -47,28 +44,24 @@ func (d *dsCache) PutMulti(keys []*ds.Key, vals []ds.PropertyMap, cb ds.NewKeyCB
 }
 
 func (d *dsCache) GetMulti(keys []*ds.Key, metas ds.MultiMetaGetter, cb ds.GetMultiCB) error {
-	lockItems, nonce := d.mkRandLockItems(keys, metas)
-	if len(lockItems) == 0 {
+	itemKeys := d.mkRandKeys(keys, metas)
+	if len(itemKeys) == 0 {
 		return d.RawInterface.GetMulti(keys, metas, cb)
 	}
 
-	if err := mc.Add(d.c, lockItems...); err != nil {
-		// Ignore this error. Either we couldn't add them because they exist
-		// (so, not an issue), or because memcache is having sad times (in which
-		// case we'll see so in the Get which immediately follows this).
-	}
-	if err := errors.Filter(mc.Get(d.c, lockItems...), mc.ErrCacheMiss); err != nil {
-		(log.Fields{log.ErrorKey: err}).Debugf(
-			d.c, "dscache: GetMulti: memcache.Get")
+	nonce := d.generateNonce()
+	lockItems, err := d.impl.TryLockAndFetch(d.c, itemKeys, nonce, RefreshLockTimeout)
+	if err != nil {
+		logging.WithError(err).Debugf(d.c, "dscache: GetMulti: TryLockAndFetch")
 	}
 
-	p := d.makeFetchPlan(&facts{keys, metas, lockItems, nonce})
+	p := d.makeFetchPlan(facts{keys, metas, lockItems, nonce})
 
 	if !p.empty() {
 		// looks like we have something to pull from datastore, and maybe some work
 		// to save stuff back to memcache.
 
-		toCas := []mc.Item{}
+		var toCas []CacheItem
 		err := d.RawInterface.GetMulti(p.toGet, p.toGetMeta, func(j int, pm ds.PropertyMap, err error) {
 			i := p.idxMap[j]
 			toSave := p.toSave[j]
@@ -84,7 +77,7 @@ func (d *dsCache) GetMulti(keys []*ds.Key, metas ds.MultiMetaGetter, cb ds.GetMu
 					data = encodeItemValue(pm)
 					if len(data) > internalValueSizeLimit {
 						shouldSave = false
-						log.Warningf(
+						logging.Warningf(
 							d.c, "dscache: encoded entity too big (%d/%d)!",
 							len(data), internalValueSizeLimit)
 					}
@@ -97,35 +90,35 @@ func (d *dsCache) GetMulti(keys []*ds.Key, metas ds.MultiMetaGetter, cb ds.GetMu
 			}
 
 			if toSave != nil {
-				if shouldSave { // save
+				if shouldSave {
+					// The item was successfully encoded and should be able to fit into
+					// the cache.
 					mg := metas.GetSingle(i)
-					expSecs := ds.GetMetaDefault(mg, CacheExpirationMeta, CacheTimeSeconds).(int64)
-					toSave.SetFlags(uint32(ItemHasData))
-					toSave.SetExpiration(time.Duration(expSecs) * time.Second)
-					toSave.SetValue(data)
+					expSecs := ds.GetMetaDefault(mg, CacheExpirationMeta, int64(CacheDuration.Seconds())).(int64)
+					toSave.PromoteToData(data, time.Duration(expSecs)*time.Second)
 				} else {
-					// Set a lock with an infinite timeout. No one else should try to
-					// serialize this item to memcache until something Put/Delete's it.
-					toSave.SetFlags(uint32(ItemHasLock))
-					toSave.SetExpiration(0)
-					toSave.SetValue(nil)
+					// The item is most likely too big to be cached. Set a lock with an
+					// infinite timeout. No one else should try to serialize this item to
+					// memcache until something Put/Delete's it.
+					toSave.PromoteToIndefiniteLock()
 				}
 				toCas = append(toCas, toSave)
 			}
 		})
 		if err != nil {
+			// TODO(vadimsh): Should we drop locks owned by us?
 			return err
 		}
 		if len(toCas) > 0 {
-			// we have entries to save back to memcache.
-			if err := mc.CompareAndSwap(d.c, toCas...); err != nil {
-				(log.Fields{log.ErrorKey: err}).Debugf(
-					d.c, "dscache: GetMulti: memcache.CompareAndSwap")
+			// Store stuff we fetched back into memcache unless someone (like
+			// a concurrent Put) deleted our locks already.
+			if err := d.impl.CompareAndSwap(d.c, toCas); err != nil {
+				logging.WithError(err).Debugf(d.c, "dscache: GetMulti: CompareAndSwap")
 			}
 		}
 	}
 
-	// finally, run the callback for all of the decoded items and the errors,
+	// Finally, run the callback for all of the decoded items and the errors,
 	// if any.
 	for i, dec := range p.decoded {
 		cb(i, dec, p.lme.GetOne(i))
@@ -144,6 +137,8 @@ func (d *dsCache) RunInTransaction(f func(context.Context) error, opts *ds.Trans
 		}
 		return err
 	}, opts)
+	// Note: the transaction can *eventually* succeed even if `err` is non-nil
+	// here. So on errors we pessimistically keep the locks until they expire.
 	if err == nil {
 		txnState.release(d.supportContext)
 	}
