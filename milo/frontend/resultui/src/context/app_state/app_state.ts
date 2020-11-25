87e07a9082f86515dfa725782b3a17c60d3deb64
Weiwei Lin <weiweilin@google.com>
2020-11-25 14:35:20 +1100

    [resultui] Do not require users to login to see the page.
    1. Make services available when the user is not logged in.
    2. Do not redirect the user to the login page when they are not logged
    in.
    3. Consistently use `Promise.race([])` to construct promises that never
    resolve.
    4. Remove unnecessary type assertions.
    
    R=jsca@google.com, nqmtuan@google.com
    
    Bug: 1150436
    Change-Id: Idbb448425e3b3db908701dfea79f73b4943f1ff5

--- milo/frontend/resultui/src/context/app_state/app_state.ts
+++ milo/frontend/resultui/src/context/app_state/app_state.ts
@@ -23,12 +23,15 @@ import { ResultDb } from '../../services/resultdb';
  * Records the app-level state.
  */
 export class AppState {
-  @observable.ref accessToken = '';
+  // null means the access token is uninitialized (i.e. we don't know whether
+  // the user is logged in or not).
+  // '' means there's no access token (i.e. the user is not logged in).
+  @observable.ref accessToken: string | null = null;
   @observable.ref selectedTabId = '';
 
   @computed
   get resultDb(): ResultDb | null {
-    if (!this.accessToken) {
+    if (this.accessToken === null) {
       return null;
     }
     return new ResultDb(CONFIGS.RESULT_DB.HOST, this.accessToken);
@@ -36,7 +39,7 @@ export class AppState {
 
   @computed
   get milo(): MiloInternal | null {
-    if (!this.accessToken) {
+    if (this.accessToken === null) {
       return null;
     }
     return new MiloInternal(this.accessToken);
@@ -44,7 +47,7 @@ export class AppState {
 
   @computed
   get buildsService(): BuildsService | null {
-    if (!this.accessToken) {
+    if (this.accessToken === null) {
       return null;
     }
     return new BuildsService(CONFIGS.BUILDBUCKET.HOST, this.accessToken);
