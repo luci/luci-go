b64892e7fb290b3fbb5c0cb0f3450a14738c5715
Andrii Shyshkalov <tandrii@google.com>
2020-11-25 15:52:51 +0100

    cv: add helper GerritHost function.
    Unfortunate choice of Gerrit.URL in CQ v2 config means constant
    conversion of a host.
    
    R=qyearsley, yiwzhang
    
    Change-Id: I5e7adcb705faddb33d182be0646bcfc46a6ee4f5

---
+++ cv/internal/config/util_test.go
@@ -0,0 +1,35 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"testing"
+
+	pb "go.chromium.org/luci/cv/api/config/v2"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGerritHost(t *testing.T) {
+	t.Parallel()
+
+	Convey("GerritHost", t, func() {
+		So(GerritHost(&pb.ConfigGroup_Gerrit{Url: "https://o.k"}), ShouldEqual, "o.k")
+		So(GerritHost(&pb.ConfigGroup_Gerrit{Url: "https://strip.sla.shes/"}), ShouldEqual, "strip.sla.shes")
+
+		So(func() { GerritHost(&pb.ConfigGroup_Gerrit{}) }, ShouldPanic)
+		So(func() { GerritHost(&pb.ConfigGroup_Gerrit{Url: "no.scheme"}) }, ShouldPanic)
+	})
+}
