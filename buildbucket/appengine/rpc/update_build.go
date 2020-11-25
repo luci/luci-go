3b9d31e4f8e22551c01b4b546c8cb61b783b7c42
Scott Lee <ddoman@chromium.org>
2020-11-25 01:39:05 +0000

    [buildbucket] validate build token for UpdateBuild
    It simply checks if it is the same token stored in the build entity.
    
    Bug: 1110990
    Change-Id: I26208f43b8b10cc6f7190a2d8d8b62a571c6cc80

--- buildbucket/appengine/rpc/update_build.go
+++ buildbucket/appengine/rpc/update_build.go
@@ -55,6 +55,7 @@ var (
 	}
 )
 
+// validateUpdate validates the given request.
 func validateUpdate(req *pb.UpdateBuildRequest, bs *model.BuildSteps) error {
 	if req.GetBuild().GetId() == 0 {
 		return errors.Reason("build.id: required").Err()
@@ -274,7 +275,13 @@ func (*Builds) UpdateBuild(ctx context.Context, req *pb.UpdateBuildRequest) (*pb
 	updatePaths := stringset.NewFromSlice(req.UpdateMask.GetPaths()...)
 
 	// pre-check if the build can be updated before updating it with a transaction.
-	if _, err = getBuildForUpdate(ctx, updatePaths, bm, req); err != nil {
+	b, err := getBuildForUpdate(ctx, updatePaths, bm, req)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO(crbug.com/1152628) - Use Cloud Secret Manager to validate build update tokens.
+	if err := validateBuildToken(ctx, b); err != nil {
 		return nil, err
 	}
 
