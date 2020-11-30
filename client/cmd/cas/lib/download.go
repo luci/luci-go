2d3229a2d84d4f84894e1085dee935b0a0a2abac
Takuto Ikuta <tikuta@chromium.org>
2020-11-30 15:00:35 +0900

    [cas] use embeddedkvs to cache small files
    Linux:
    https://chromium-swarm.appspot.com/task?id=50309119b2bb4c10
    took 22.1313109398s without cache hit
    took 33.9301249981 with cache hit
    
    macOS:
    https://chromium-swarm.appspot.com/task?id=50309195026d7a10
    took 93.609126091s without cache hit
    took 51.5998740196s with cache hit
    
    Windows:
    https://chromium-swarm.appspot.com/task?id=503088494ed40f10
    took 62.376999855 without cache hit
    took 63.7460000515 with cache hit
    
    Bug: 1152663
    Change-Id: I4bd3ab87c5a32e4bb503f2520bdcdcc4dfcfa2c5

--- client/cmd/cas/lib/download.go
+++ client/cmd/cas/lib/download.go
@@ -15,13 +15,17 @@
 package lib
 
 import (
+	"bytes"
 	"context"
 	"crypto"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
 	"sort"
+	"sync"
 	"time"
 
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/client"
@@ -33,6 +37,7 @@ import (
 	"go.chromium.org/luci/auth"
 	"go.chromium.org/luci/common/cli"
 	"go.chromium.org/luci/common/data/caching/cache"
+	"go.chromium.org/luci/common/data/embeddedkvs"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/isolated"
 	isol "go.chromium.org/luci/common/isolated"
@@ -57,6 +62,7 @@ Tree is referenced by their digest "<digest hash>/<size bytes>"`,
 			c.Flags.StringVar(&c.digest, "digest", "", `Digest of root directory proto "<digest hash>/<size bytes>".`)
 			c.Flags.StringVar(&c.dir, "dir", "", "Directory to download tree.")
 			c.Flags.StringVar(&c.dumpStatsJSON, "dump-stats-json", "", "Dump download stats to json file.")
+			c.Flags.StringVar(&c.kvs, "small-files-cache", "", "Cache file for small files.")
 			return &c
 		},
 	}
@@ -70,6 +76,8 @@ type downloadRun struct {
 
 	cacheDir      string
 	cachePolicies cache.Policies
+
+	kvs string
 }
 
 func (r *downloadRun) parse(a subcommands.Application, args []string) error {
@@ -82,6 +90,10 @@ func (r *downloadRun) parse(a subcommands.Application, args []string) error {
 
 	if r.cacheDir == "" && !r.cachePolicies.IsDefault() {
 		return errors.New("cache-dir is necessary when cache-max-size, cache-max-items or cache-min-free-space are specified")
+	}
+
+	if r.kvs != "" && r.cacheDir == "" {
+		return errors.New("if small-files-cache is set, cache-dir should be set")
 	}
 
 	r.dir = filepath.Clean(r.dir)
@@ -217,6 +229,15 @@ func (r *downloadRun) doDownload(ctx context.Context) error {
 		defer diskcache.Close()
 	}
 
+	var kvs *embeddedkvs.KVS
+	if r.kvs != "" {
+		kvs, err = embeddedkvs.New(r.kvs)
+		if err != nil {
+			return err
+		}
+		defer kvs.Close()
+	}
+
 	if err := createDirectories(ctx, r.dir, outputs); err != nil {
 		return err
 	}
@@ -227,6 +248,9 @@ func (r *downloadRun) doDownload(ctx context.Context) error {
 	// copy duplicates files later.
 	var dups []*client.TreeOutput
 
+	smallFiles := make(map[string][]*client.TreeOutput)
+	const smallFileThreshold = 128 * 1024 // 128KiB
+
 	for path, output := range outputs {
 		if output.IsEmptyDirectory {
 			continue
@@ -236,6 +260,11 @@ func (r *downloadRun) doDownload(ctx context.Context) error {
 			if err := os.Symlink(output.SymlinkTarget, path); err != nil {
 				return errors.Annotate(err, "failed to create symlink").Err()
 			}
+			continue
+		}
+
+		if kvs != nil && output.Digest.Size <= smallFileThreshold {
+			smallFiles[output.Digest.Hash] = append(smallFiles[output.Digest.Hash], output)
 			continue
 		}
 
@@ -260,6 +289,63 @@ func (r *downloadRun) doDownload(ctx context.Context) error {
 	logger.Infof("finished copy from cache (if any), dups: %d, to: %d, took %s", len(dups), len(to), time.Since(start))
 
 	start = time.Now()
+
+	smallFileHashes := make([]string, 0, len(smallFiles))
+	for smallFile := range smallFiles {
+		smallFileHashes = append(smallFileHashes, smallFile)
+	}
+
+	var mu sync.Mutex
+
+	// limit the number of concurrent I/O operations.
+	ch := make(chan struct{}, runtime.NumCPU())
+
+	// Sort hashes by one of corresponding file path.
+	sort.Slice(smallFileHashes, func(i, j int) bool {
+		filei := smallFiles[smallFileHashes[i]][0]
+		filej := smallFiles[smallFileHashes[j]][0]
+		return filei.Path < filej.Path
+	})
+
+	// Extract small files from kvs.
+	if err := kvs.GetMulti(smallFileHashes, func(key string, value []byte) error {
+		ch <- struct{}{}
+		defer func() { <-ch }()
+
+		mu.Lock()
+		files := smallFiles[key]
+		delete(smallFiles, key)
+		mu.Unlock()
+
+		for _, file := range files {
+			mode := 0o600
+			if file.IsExecutable {
+				mode = 0o700
+			}
+			if err := ioutil.WriteFile(file.Path, value, os.FileMode(mode)); err != nil {
+				return errors.Annotate(err, "failed to write file").Err()
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	// Process non-cached files.
+	for _, files := range smallFiles {
+		for _, file := range files {
+			if _, ok := to[file.Digest]; ok {
+				dups = append(dups, file)
+			} else {
+				to[file.Digest] = file
+			}
+		}
+	}
+
+	logger.Infof("finished copy small files from cache (if any), smallFiles: %d, to: %d, took %s", len(smallFileHashes), len(to), time.Since(start))
+
+	start = time.Now()
 	if err := c.DownloadFiles(ctx, "", to); err != nil {
 		return errors.Annotate(err, "failed to download files").Err()
 	}
@@ -277,12 +363,58 @@ func (r *downloadRun) doDownload(ctx context.Context) error {
 			return outputs[i].Path < outputs[j].Path
 		})
 
+		var eg errgroup.Group
+
+		bufferPool := sync.Pool{
+			New: func() interface{} {
+				return &bytes.Buffer{}
+			},
+		}
+
 		for _, output := range outputs {
+			if kvs != nil && output.Digest.Size <= smallFileThreshold {
+				output := output
+
+				eg.Go(func() error {
+					buf := bufferPool.Get().(*bytes.Buffer)
+					buf.Reset()
+					defer bufferPool.Put(buf)
+
+					b, err := func() ([]byte, error) {
+						ch <- struct{}{}
+						defer func() { <-ch }()
+
+						f, err := os.Open(output.Path)
+						if err != nil {
+							return nil, errors.Annotate(err, "failed to open file").Err()
+						}
+						defer f.Close()
+
+						if _, err := io.Copy(buf, f); err != nil {
+							return nil, errors.Annotate(err, "failed to read file").Err()
+						}
+
+						return buf.Bytes(), nil
+					}()
+
+					if err != nil {
+						return err
+					}
+					return kvs.Set(output.Digest.Hash, b)
+				})
+				continue
+			}
+
 			if err := diskcache.AddFileWithoutValidation(
 				isolated.HexDigest(output.Digest.Hash), output.Path); err != nil {
 				return errors.Annotate(err, "failed to add cache; path=%s digest=%s", output.Path, output.Digest).Err()
 			}
 		}
+
+		if err := eg.Wait(); err != nil {
+			return err
+		}
+
 		logger.Infof("finished cache addition, took %s", time.Since(start))
 	}
 
