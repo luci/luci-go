e8822335ad0c01d97398387bc9203dc4da85fcef
Takuto Ikuta <tikuta@chromium.org>
2020-12-01 15:20:00 +0900

    [embeddedkvs] add bbolt based embeddedkvs package
    This is a thin wrapper for bbolt.
    
    I will use this to cache small files in cas client.
    
    Bug: 1152663
    Change-Id: Ida5b11931d0d951d929a4e59e2db5bed4103b695

---
+++ common/data/embeddedkvs/cache_test.go
@@ -0,0 +1,58 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package embeddedkvs
+
+import (
+	"path/filepath"
+	"sort"
+	"sync"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCache(t *testing.T) {
+	t.Parallel()
+
+	Convey("basic", t, func() {
+		path := filepath.Join(t.TempDir(), "db")
+		k, err := New(path)
+		So(err, ShouldBeNil)
+
+		So(k.Set("key1", []byte("value1")), ShouldBeNil)
+		So(k.Set("key2", []byte("value2")), ShouldBeNil)
+		So(k.Set("key3", []byte("value3")), ShouldBeNil)
+
+		var mu sync.Mutex
+		var keys []string
+		var values []string
+
+		k.GetMulti([]string{"key1", "key2", "key4"}, func(key string, value []byte) error {
+			mu.Lock()
+			keys = append(keys, key)
+			values = append(values, string(value))
+			mu.Unlock()
+			return nil
+		})
+
+		sort.Strings(keys)
+		sort.Strings(values)
+
+		So(keys, ShouldResemble, []string{"key1", "key2"})
+		So(values, ShouldResemble, []string{"value1", "value2"})
+
+		So(k.Close(), ShouldBeNil)
+	})
+}
