bdbfc2660e6152af964cc46a5ac421aa64459f68
Yiwei Zhang <yiwzhang@google.com>
2020-12-02 22:11:15 -0800

    [mask] Explicitly handle nil message when merge
    R=ddoman, nodir
    
    Bug: 964590
    Change-Id: I383d2cf08bd36050899b69c140ed744f64d3c62c

--- common/proto/mask/mask.go
+++ common/proto/mask/mask.go
@@ -398,13 +398,21 @@ func (m *Mask) mergeImpl(src, dest protoreflect.Message) {
 			})
 			dest.Set(fieldDesc, newField)
 
-		case len(submask.children) > 0 && fieldDesc.Kind() == protoreflect.MessageKind:
-			// only singular message field can be merged partially
-			submask.mergeImpl(srcVal.Message(), dest.Mutable(fieldDesc).Message())
+		case fieldDesc.Kind() == protoreflect.MessageKind:
+			switch srcNil := !srcVal.Message().IsValid(); {
+			case srcNil && !dest.Get(fieldDesc).Message().IsValid():
+				// dest is also nil message. No need to proceed merging.
+			case srcNil && len(submask.children) == 0:
+				dest.Clear(fieldDesc)
+			case len(submask.children) == 0:
+				dest.Set(fieldDesc, cloneValue(srcVal, fieldDesc.Kind()))
+			default:
+				// only singular message field can be merged partially
+				submask.mergeImpl(srcVal.Message(), dest.Mutable(fieldDesc).Message())
+			}
 
 		default:
-			// scalar value
-			dest.Set(fieldDesc, cloneValue(srcVal, kind))
+			dest.Set(fieldDesc, srcVal) // scalar value
 		}
 	}
 }
