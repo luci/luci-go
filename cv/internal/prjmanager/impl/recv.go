45bf50c047b480212180a31038a09b2656421ae1
Andrii Shyshkalov <tandrii@google.com>
2020-12-07 03:35:25 +0100

    WIP. ProjectManager Mailbox structure.
    R=qyearsley, yiwzhang
    
    Bug: TODO.
    Change-Id: I392e02a272a70cf2dc76fc0113f990ff80158322

---
+++ cv/internal/prjmanager/impl/recv.go
@@ -0,0 +1,34 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package impl
+
+import (
+	"context"
+
+	"google.golang.org/protobuf/proto"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/server/tq"
+
+	"go.chromium.org/luci/cv/internal/prjmanager"
+)
+
+func init() {
+	prjmanager.PokePMTaskRef.AttachHandler(
+		func(ctx context.Context, payload proto.Message) error {
+			return errors.Reason("TODO(tandrii): implement").Tag(tq.Fatal).Err()
+		},
+	)
+}
