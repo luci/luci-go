64898e78f11c3bc8f88bbd5e2bad5e831bb9ec82
Andrii Shyshkalov <tandrii@google.com>
2020-12-07 19:58:36 +0100

    cv: implement ProjectManager mailbox structure.
    This CL is ~65% boilerplate, but contains working pipeline
    of sending messages to and receiving them by project manager.
    
    I thought of a making it a library. In theory it's doable, even without
    generics:
     * Payload proto message (see internal/events.proto). This could be
       re-used, but we'd need to stick both Run and Project events together.
     * Task type proto messages (internal/tasks.proto). These could be
       re-used by specifying RunManager or ProjectManager as a target,
       but then we'd need to share 1 TQ queue, which isn't good.
     * Datastore kind of event entity. Strictly speaking could be re-used,
       but this would be confusing, so ideally we should set kind on the fly,
       while re-using the struct. This is actually possible using datastore
       MetaGetter interface.
    
    Another way is to generate the boilerplaty code from scratch.
    But since we only need this one more time, I think it's easier to
    copy-paste this by hand to RunManager.
    
    R=qyearsley, yiwzhang
    
    Bug: 1156241
    Change-Id: I392e02a272a70cf2dc76fc0113f990ff80158322

---
+++ cv/internal/prjmanager/impl/recv_test.go
@@ -0,0 +1,64 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package impl
+
+import (
+	"context"
+	"testing"
+
+	"go.chromium.org/luci/gae/service/datastore"
+	"go.chromium.org/luci/server/tq/tqtesting"
+
+	"go.chromium.org/luci/cv/internal/cvtesting"
+	"go.chromium.org/luci/cv/internal/prjmanager"
+	"go.chromium.org/luci/cv/internal/prjmanager/internal"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestPokePM(t *testing.T) {
+	t.Parallel()
+
+	Convey("Event pipeline works", t, func() {
+		ct := cvtesting.Test{}
+		ctx, cancel := ct.SetUp()
+		defer cancel()
+
+		const lProject = "infra"
+
+		test := func(trans bool) {
+			// TODO(tandrii): expand test to actually create a config.
+			if trans {
+				So(datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+					return prjmanager.UpdateConfig(ctx, lProject)
+				}, nil), ShouldBeNil)
+			} else {
+				So(prjmanager.UpdateConfig(ctx, lProject), ShouldBeNil)
+			}
+			So(ct.TQ.Tasks().Payloads(), ShouldHaveLength, 1)
+			ct.TQ.Run(ctx, tqtesting.StopAfterTask("poke-pm-task"))
+			So(ct.TQ.Tasks().Payloads(), ShouldHaveLength, 0)
+			events, err := internal.Peek(ctx, lProject, 100)
+			So(err, ShouldBeNil)
+			So(events, ShouldHaveLength, 0)
+		}
+		Convey("Non-Transactional", func() {
+			test(false)
+		})
+		Convey("Transactional", func() {
+			test(true)
+		})
+	})
+}
---
+++ cv/internal/prjmanager/internal/model.go
@@ -0,0 +1,110 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/retry/transient"
+	"go.chromium.org/luci/gae/service/datastore"
+)
+
+const (
+	// ProjectDSKind should match that of actual impl.Project entities
+	// for maximal performance since they'd be located next to each other.
+	ProjectDSKind = "Project"
+	// eventDSKind is kind of `Event` entities.
+	eventDSKind = "ProjectEvent"
+)
+
+// Event is an immutable entity in Datastore representing incoming events.
+type Event struct {
+	_kind string `gae:"$kind,ProjectEvent"`
+
+	// Parent refers to the recipient LUCI Project.
+	//
+	// Make one using ProjectKey().
+	Parent *datastore.Key `gae:"$parent"`
+
+	// ID is UUID-generated one.
+	// We want to ensure IDs are extremely unlikely to be re-used.
+	ID string `gae:"$id"`
+
+	// Payload stores actual event details.
+	Payload *Payload
+
+	// CreateTime is exact time of when this entity was created.
+	//
+	// It's not indexed to avoid hot areas in the index.
+	CreateTime time.Time `gae:",noindex"`
+}
+
+// Send "sends" event to a Project's mailbox by storing in Datastore.
+func Send(ctx context.Context, luciProject string, p *Payload) error {
+	e := Event{
+		ID:         uuid.New().String(),
+		Payload:    p,
+		CreateTime: clock.Now(ctx).UTC(),
+		Parent:     datastore.MakeKey(ctx, ProjectDSKind, luciProject),
+	}
+	if err := datastore.Put(ctx, &e); err != nil {
+		return errors.Annotate(err, "failed to write Event entity").Tag(transient.Tag).Err()
+	}
+	return nil
+}
+
+// Peek reads up to `limit` outstanding events in no particular order.
+//
+// Must not be called from a transaction because the events are immutable.
+func Peek(ctx context.Context, luciProject string, limit int) (ret []*Event, err error) {
+	if datastore.CurrentTransaction(ctx) != nil {
+		panic("must be called outside a transaction")
+	}
+	q := datastore.NewQuery(eventDSKind).Limit(int32(limit)).Ancestor(
+		datastore.MakeKey(ctx, ProjectDSKind, luciProject))
+	if err = datastore.GetAll(ctx, q, &ret); err != nil {
+		err = errors.Annotate(err, "failed to read Events of %q", luciProject).Tag(transient.Tag).Err()
+	}
+	return
+}
+
+// delete deletes events returned by Peek. Idempotent.
+//
+// Should be called in a transaction with the necessary mutation which saves
+// result of processing of the event.
+//
+// May be called outside a transaction iff the event has definitely been
+// processed already.
+func Delete(ctx context.Context, events []*Event) error {
+	const batchSize = 100
+	var batch []*Event
+	for {
+		switch l := len(events); {
+		case l == 0:
+			return nil
+		case l < batchSize:
+			batch, events = events, nil
+		default:
+			batch, events = events[:batchSize], events[batchSize:]
+		}
+		if err := datastore.Delete(ctx, batch); err != nil {
+			return errors.Annotate(err, "failed to delete %d Events", len(batch)).Tag(transient.Tag).Err()
+		}
+	}
+}
