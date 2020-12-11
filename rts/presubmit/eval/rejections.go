457856c5d2bd3e05a14fb251346f7c61da5cfeee
Nodir Turakulov <nodir@chromium.org>
2020-12-11 12:36:24 -0800

    [rts] Extract rejection analysis
    This CL is another preparation for a large refactor.
    Extract rejection analysis to a reusable function.
    
    Bug: 1112125
    Change-Id: Ia8701955fefb05b4d3a641ff63e776245214c613

---
+++ rts/presubmit/eval/rejections.go
@@ -0,0 +1,85 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package eval
+
+import (
+	"context"
+
+	"go.chromium.org/luci/common/errors"
+
+	evalpb "go.chromium.org/luci/rts/presubmit/eval/proto"
+)
+
+type analyzedRejection struct {
+	*evalpb.Rejection
+	Eligible bool
+	// same order as Rejection.TestVariants.
+	Affectedness AffectednessSlice
+	Closest      Affectedness
+}
+
+func analyzeRejections(ctx context.Context, rejC <-chan *evalpb.Rejection, strategy Strategy, callback func(analyzedRejection) error) error {
+	var out Output
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		case rej, ok := <-rejC:
+			if !ok {
+				return nil
+			}
+
+			// TODO(crbug.com/1112125): add support for CL stacks.
+			// This call returns only files modified in the particular patchset and
+			// ignores possible parent CLs that were also tested.
+
+			// TODO(crbug.com/1112125): skip the patchset if it has a ton of failed tests.
+			// Most selection strategies would reject such a patchset, so it represents noise.
+
+			// Select tests.
+			in := Input{
+				TestVariants: rej.FailedTestVariants,
+			}
+			in.ensureChangedFilesInclude(rej.Patchsets...)
+			if cap(out.TestVariantAffectedness) < len(in.TestVariants) {
+				out.TestVariantAffectedness = make([]Affectedness, len(in.TestVariants))
+			} else {
+				out.TestVariantAffectedness = out.TestVariantAffectedness[:len(in.TestVariants)]
+				for i := range out.TestVariantAffectedness {
+					out.TestVariantAffectedness[i] = Affectedness{}
+				}
+			}
+			if err := strategy(ctx, in, &out); err != nil {
+				return errors.Annotate(err, "the selection strategy failed").Err()
+			}
+
+			// The rejection is preserved if at least one test ran, so find the closest test.
+			closest, err := out.TestVariantAffectedness.closest()
+			if err != nil {
+				return err
+			}
+
+			err = callback(analyzedRejection{
+				Rejection:    rej,
+				Affectedness: out.TestVariantAffectedness,
+				Closest:      out.TestVariantAffectedness[closest],
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
