541d073d6af5c49e56d2359e3144c1a3d999eec0
Robert Iannucci <iannucci@chromium.org>
2020-12-12 01:22:55 +0000

    [luciexe/build] Add skeleton for start/end functions.
    R=tandrii, vadimsh, yiwzhang
    
    Bug: 1157581
    Change-Id: I358679a7a609b2c39dc8c31e90ca11bf5f3a1aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2587202
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

--- luciexe/build/step.go
+++ luciexe/build/step.go
@@ -14,7 +14,68 @@
 
 package build
 
+import (
+	"context"
+)
+
 // StepState represents the state of a single step.
 //
 // This is properly initialized by the Step and ScheduleStep functions.
 type StepState struct{}
+
+// Step adds a new step to the build.
+//
+// The step will have a "RUNNING" status with a StartTime.
+//
+// You MUST call StepState.End; Failure to do so is generally undefined, but in
+// a LUCI build context, this will result in the step having a status of
+// CANCELED and the SummaryMarkdown being overwritten with a message informing
+// you that the step was not ended correctly. Don't rely on this behavior, just
+// call End in a defer.
+//
+// The returned context is updated so that calling Step/ScheduleStep on it will create sub-steps.
+//
+// If `name` contains `|` this function will panic, since this is a reserved
+// character for delimiting hierarchy in steps.
+//
+// Duplicate step names will be disambiguated by appending " (N)" for the 2nd,
+// 3rd, etc. duplicate.
+//
+// The returned context will have `name` embedded in it; Calling Step or
+// ScheduleStep with this context will generate a sub-step.
+//
+// Use like:
+//    var err error
+//    ctx, step := build.Step(ctx, "Step name")
+//    defer step.End(err)
+//
+//    err = opThatErrsOrPanics(ctx)
+func Step(ctx context.Context, name string) (*StepState, context.Context) {
+	panic("not implemented")
+}
+
+// ScheduleStep is like Step, except that it leaves the new step in the
+// SCHEDULED status, and does not set a StartTime.
+//
+// The step will move to RUNNING when calling any other methods on
+// the StepState, when creating a sub-Step, or if you explicitly call
+// StepState.Start().
+func ScheduleStep(ctx context.Context, name string) (*StepState, context.Context) {
+	panic("not implemented")
+}
+
+// End sets the step's final status, according to `err` (See GetStatus).
+//
+// End will also be able to set INFRA_FAILURE status and log additional
+// information if the program is panic'ing.
+//
+// End must be invoked like:
+//
+//    var err error
+//    ctx, step := build.Step(ctx, ...)  // or build.ScheduleStep
+//    defer step.End(err)
+//
+//    err = opThatErrsOrPanics()
+func (*StepState) End(err error) {
+	panic("not implemented")
+}
