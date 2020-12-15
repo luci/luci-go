d4246383820f77b4a434b0bef59c5f00cefa8929
Robert Iannucci <iannucci@chromium.org>
2020-12-15 19:42:36 +0000

    [luciexe/build] Add skeleton for start options.
    R=tandrii, vadimsh, yiwzhang
    
    Bug: 1157581
    Change-Id: Id2c9411dcd6195c8dfb841f8049b4409df4905ca
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2590713
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

--- luciexe/build/start_options.go
+++ luciexe/build/start_options.go
@@ -14,9 +14,57 @@
 
 package build
 
+import (
+	"golang.org/x/time/rate"
+
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/logdog/client/butlerlib/streamclient"
+)
+
 // StartOption is an object which can be passed to the Start function, and
 // modifies the behavior of the luciexe/build library.
 //
 // StartOptions are exclusively constructed from the Opt* functions in this
 // package.
+//
+// StartOptions are all unique per Start (i.e. you can only pass one of a kind
+// per option to Start).
 type StartOption func(*State)
+
+// OptLogsink allows you to associate a streamclient with the started build.
+//
+// See `streamclient.New` and `streamclient.NewFake` for how to create a client
+// suitable to your needs (note that this includes a local filesystem option).
+//
+// If a logsink is configured, it will be used as the output destination for the
+// go.chromium.org/luci/common/logging library, and will recieve all data
+// written via the Loggable interface.
+//
+// If no logsink is configured, the go.chromium.org/luci/common/logging library
+// will be unaffected, and data written to the Loggable interface will go to
+// an ioutil.NopWriteCloser.
+func OptLogsink(*streamclient.Client) StartOption {
+	panic("not implemented")
+}
+
+// OptSend allows you to get a callback when the state of the underlying Build
+// changes.
+//
+// This callback will be called at most as frequently as `rate` allows, up to
+// once per Build change, and is called with a copy of Build. Only one
+// outstanding invocation of this callback can occur at once.
+//
+// If new updates come in while this callback is blocking, they will apply
+// silently in the background, and as soon as the callback returns (and rate
+// allows), it will be invoked again with the current Build state.
+func OptSend(rate.Limit, func(*bbpb.Build)) StartOption {
+	panic("not implemented")
+}
+
+// OptSuppressExit will supress the call to os.Exit from Start in the event that
+// a terminal command-line flag was passed (like `--help`).
+//
+// Instead, Start will return a nil `*State` and an unmodified context.
+func OptSuppressExit() StartOption {
+	panic("not implemented")
+}
