21a4833a82dbbfb5a25ed17fc0b3e39250922a13
Yiwei Zhang <yiwzhang@google.com>
2020-12-16 12:03:55 -0800

    [cv] Implement run state and skeleton for event processor
    R=qyearsley, tandrii
    
    Bug: 1156779
    Change-Id: I646c0b63e97155c08f70deda06507c962232c777

--- cv/internal/run/impl/manager.go
+++ cv/internal/run/impl/manager.go
@@ -20,6 +20,7 @@ import (
 
 	"google.golang.org/protobuf/proto"
 
+	"go.chromium.org/luci/common/clock"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/retry/transient"
@@ -53,21 +54,70 @@ func init() {
 
 func pokeRunTask(ctx context.Context, runID run.ID) error {
 	ctx = logging.SetField(ctx, "run", runID)
-	recipient := datastore.MakeKey(ctx, run.RunKind, runID)
-	return eventbox.ProcessBatch(ctx, recipient, &runManager{id: runID})
+	recipient := datastore.MakeKey(ctx, run.RunKind, string(runID))
+	return eventbox.ProcessBatch(ctx, recipient, &runManager{runID: runID})
 }
 
 // runManager implements eventbox.Processor.
 type runManager struct {
-	id run.ID
+	runID run.ID
+}
+
+// state represents the current state of a Run.
+//
+// It consists of the Run entity and its child entities (could be partial
+// depending on the event received).
+type state struct {
+	Run run.Run
+	// TODO(yiwzhang): add RunOwner, []RunCL, []RunTryjob.
+}
+
+func (s *state) deepCopy() *state {
+	if s == nil {
+		return nil
+	}
+	return &state{
+		Run: run.Run{
+			ID:            s.Run.ID,
+			Mode:          s.Run.Mode,
+			Status:        s.Run.Status,
+			CreateTime:    s.Run.CreateTime,
+			StartTime:     s.Run.StartTime,
+			EndTime:       s.Run.EndTime,
+			UpdateTime:    s.Run.UpdateTime,
+			Owner:         s.Run.Owner,
+			ConfigGroupID: s.Run.ConfigGroupID,
+		},
+	}
+}
+
+func (s *state) shallowCopy() *state {
+	if s == nil {
+		return nil
+	}
+	ret := &state{
+		Run: s.Run,
+	}
+	return ret
 }
 
 var _ eventbox.Processor = (*runManager)(nil)
 
 // LoadState is called to load the state before a transaction.
 func (rm *runManager) LoadState(ctx context.Context) (eventbox.State, eventbox.EVersion, error) {
-	panic("not implemented")
-
+	r := run.Run{ID: rm.runID}
+	switch err := datastore.Get(ctx, &r); {
+	case err == datastore.ErrNoSuchEntity:
+		err = errors.Reason("CRITICAL: requested run entity %q is missing in datastore.", rm.runID).Err()
+		errors.Log(ctx, err)
+		panic(err)
+	case err != nil:
+		return nil, 0, errors.Annotate(err, "failed to get Run %q", rm.runID).Tag(transient.Tag).Err()
+	}
+	s := &state{
+		Run: r,
+	}
+	return s, eventbox.EVersion(r.EVersion), nil
 }
 
 // Mutate is called before a transaction to compute transitions based on a
@@ -76,7 +126,11 @@ func (rm *runManager) LoadState(ctx context.Context) (eventbox.State, eventbox.E
 // All actions that must be done atomically with updating state must be
 // encapsulated inside Transition.SideEffectFn callback.
 func (rm *runManager) Mutate(ctx context.Context, events eventbox.Events, s eventbox.State) ([]eventbox.Transition, error) {
-	panic("not implemented")
+	tr := &triageResult{}
+	for _, e := range events {
+		tr.triage(ctx, e)
+	}
+	return rm.processTriageResults(ctx, tr, s.(*state))
 }
 
 // FetchEVersion is called at the beginning of a transaction.
@@ -85,7 +139,13 @@ func (rm *runManager) Mutate(ctx context.Context, events eventbox.Events, s even
 // loaded via GetState. If different, the transaction is aborted and new state
 // isn't saved.
 func (rm *runManager) FetchEVersion(ctx context.Context) (eventbox.EVersion, error) {
-	panic("not implemented")
+	r := &run.Run{ID: rm.runID}
+	if err := datastore.Get(ctx, r); err != nil {
+		return 0, errors.Annotate(err, "failed to get %q", rm.runID).Tag(transient.Tag).Err()
+	}
+
+	return eventbox.EVersion(r.EVersion), nil
+
 }
 
 // SaveState is called in a transaction to save the state if it has changed.
@@ -93,7 +153,13 @@ func (rm *runManager) FetchEVersion(ctx context.Context) (eventbox.EVersion, err
 // The passed eversion is incremented value of eversion of what GetState
 // returned before.
 func (rm *runManager) SaveState(ctx context.Context, st eventbox.State, ev eventbox.EVersion) error {
-	panic("not implemented")
+	s := st.(*state)
+	s.Run.EVersion = int(ev)
+	s.Run.UpdateTime = clock.Now(ctx).UTC()
+	if err := datastore.Put(ctx, &(s.Run)); err != nil {
+		return errors.Annotate(err, "failed to put Run %q", s.Run.ID).Tag(transient.Tag).Err()
+	}
+	return nil
 }
 
 // triageResult is the result of the triage of the incoming events.
@@ -118,4 +184,8 @@ func (tr *triageResult) triage(ctx context.Context, item eventbox.Event) {
 	default:
 		panic(fmt.Errorf("unknown event: %T [id=%q]", e.GetEvent(), item.ID))
 	}
+}
+
+func (rm *runManager) processTriageResults(ctx context.Context, tr *triageResult, s *state) (ret []eventbox.Transition, err error) {
+	panic("not implemented")
 }
