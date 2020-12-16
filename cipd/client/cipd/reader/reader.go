193eb858f9c360ca42e6c4eedc9a32443cacc4e7
Gary Miguel <garymm@google.com>
2020-12-16 02:00:48 +0000

    [cipd] use klauspost/compress/{flate,zip} instead of stdlib
    These are significantly faster.
    Reproducing the `cipd ensure` that happens for a full checkout of
    Fuchsia on my HP Z840 Workstation:
    time cipd ensure -ensure-file /tmp/cipd.ensure -root /tmp/cipd-root -max-threads 0 -log-level debug > /tmp/cipd-ensure.txt 2>&1
    
    Before:
    474.90s user 72.76s system 172% cpu 5:17.15 total
    
    After:
    366.01s user 72.09s system 153% cpu 4:45.19 total
    
    Diff:
    Absolute: 285s - 317s = -32s
    Relative: 285s / 317s = 0.899 (or 10.1% speedup).
    
    Doing a `cipd pkg-build` from a directory containing 306MiB of Go
    binaries spread across 23 files:
    
    Before:
    10.74s user 0.33s system 100% cpu 11.022 total
    
    output size: 150M
    
    After:
    5.44s user 0.56s system 109% cpu 5.480 total
    
    output size: 154M
    
    Diff:
    Time:
    Absolute: 5.44s - 10.74s = -5.3s
    Relative: 5.44s/ 10.74s = 0.507 (or a 49.3% speedup).
    
    Output size:
    Absolute: 154M - 150M = 4M
    Relative: 154M / 150M = 1.027 (or a 2.7% increase).
    
    Bug: fuchsia:65339
    Change-Id: I2879a3a6195e38f9657c66066f2c8ec9588c1aec
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2594178
    Commit-Queue: Gary Miguel <garymm@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

--- cipd/client/cipd/reader/reader.go
+++ cipd/client/cipd/reader/reader.go
@@ -16,9 +16,7 @@
 package reader
 
 import (
-	"archive/zip"
 	"bytes"
-	"compress/flate"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -31,6 +29,9 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/klauspost/compress/flate"
+	"github.com/klauspost/compress/zip"
 
 	"go.chromium.org/luci/common/clock"
 	"go.chromium.org/luci/common/errors"
