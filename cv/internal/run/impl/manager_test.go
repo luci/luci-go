e66190cf9b98d19f51268ffb5b1c1f9b1c78e288
Yiwei Zhang <yiwzhang@google.com>
2020-12-17 04:57:52 +0000

    [cv] Implement basic StartRun functionality
    R=qyearsley, tandrii
    
    Bug: 1156779
    Change-Id: I4b52b78e28fe3336220a54bf0a8cb214cc5bc5bd
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2593239
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

---
+++ cv/internal/run/impl/manager_test.go
@@ -0,0 +1,107 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package impl
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"go.chromium.org/luci/gae/service/datastore"
+	"go.chromium.org/luci/server/tq/tqtesting"
+
+	"go.chromium.org/luci/cv/internal/cvtesting"
+	"go.chromium.org/luci/cv/internal/eventbox"
+	"go.chromium.org/luci/cv/internal/run"
+	"go.chromium.org/luci/cv/internal/run/runtest"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestStartRun(t *testing.T) {
+	t.Parallel()
+
+	Convey("StartRun", t, func() {
+		ct := cvtesting.Test{}
+		ctx, cancel := ct.SetUp()
+		defer cancel()
+		const runID = "chromium/1111111111111/deadbeef"
+		runKey := datastore.MakeKey(ctx, run.RunKind, runID)
+		initialEversion := 10
+
+		Convey("Starts when Run is PENDING", func() {
+			err := datastore.Put(ctx, &run.Run{
+				ID:       runID,
+				Status:   run.Status_PENDING,
+				EVersion: initialEversion,
+			})
+			So(err, ShouldBeNil)
+			So(run.StartRun(ctx, runID), ShouldBeNil)
+			So(runtest.Runs(ct.TQ.Tasks()), ShouldResemble, []run.ID{runID})
+			ct.TQ.Run(ctx, tqtesting.StopAfterTask("poke-run-task"))
+			assertEventBoxSize(ctx, runKey, 0)
+
+			r := run.Run{ID: runID}
+			So(datastore.Get(ctx, &r), ShouldBeNil)
+			So(r, ShouldResemble, run.Run{
+				ID:         runID,
+				Status:     run.Status_RUNNING,
+				EVersion:   initialEversion + 1,
+				StartTime:  datastore.RoundTime(ct.Clock.Now().UTC()),
+				UpdateTime: datastore.RoundTime(ct.Clock.Now().UTC()),
+			})
+		})
+
+		Convey("Panic when Run Status is not specified", func() {
+			err := datastore.Put(ctx, &run.Run{
+				ID:       runID,
+				EVersion: initialEversion,
+			})
+			So(err, ShouldBeNil)
+			So(run.StartRun(ctx, runID), ShouldBeNil)
+			So(func() { ct.TQ.Run(ctx, tqtesting.StopAfterTask("poke-run-task")) }, ShouldPanic)
+		})
+
+		statuses := []run.Status{
+			run.Status_RUNNING,
+			run.Status_FINALIZING,
+			run.Status_SUCCEEDED,
+			run.Status_FAILED,
+			run.Status_CANCELLED,
+		}
+		for _, status := range statuses {
+			Convey(fmt.Sprintf("Noop when Run is %s", status), func() {
+				err := datastore.Put(ctx, &run.Run{
+					ID:       runID,
+					Status:   status,
+					EVersion: initialEversion,
+				})
+				So(err, ShouldBeNil)
+				So(run.StartRun(ctx, runID), ShouldBeNil)
+				ct.TQ.Run(ctx, tqtesting.StopAfterTask("poke-run-task"))
+
+				r := run.Run{ID: runID}
+				So(datastore.Get(ctx, &r), ShouldBeNil)
+				So(r.EVersion, ShouldEqual, initialEversion)
+			})
+		}
+	})
+}
+
+func assertEventBoxSize(ctx context.Context, recipient *datastore.Key, expectedSize int) {
+	events, err := eventbox.List(ctx, recipient)
+	So(err, ShouldBeNil)
+	So(events, ShouldHaveLength, expectedSize)
+}
