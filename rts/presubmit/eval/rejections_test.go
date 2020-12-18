e68edb78a8321f80d6b6b19d68a0893319109dbc
Nodir Turakulov <nodir@chromium.org>
2020-12-18 09:31:13 -0800

    [rts] Switch to the threshold grid
    This is the 2nd out of two CLs fully described in
    https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2588088
    
    There is a minor departure.
    
    The second history file is no longer used for safety evaluation.
    We have much more test durations than rejections. Normally
    the processed rejection history file contains all rejections in a
    month or more, whereas duration history file includes 1% of one day or
    one week. The second file covers a too small period to have
    enough rejections.
    
    The split into training and eval sets is important in ML applications
    because their model is derived from the training set. In our case,
    the model does not depend on the data. Only thresholds are chosen based
    on data. The need for training|eval set split is not as obvious as in
    traditional ML applications. Regardless, we can add it later.
    
    Rename -training-set flag to -rejections. Use the same file to
    first choose the thresholds, and then evaluate each threshold
    combinations.
    
    Rename -eval-set to -durations. Use the file to evaluate efficiency.
    
    After processing all data, print a table depicting the
    Savings=f(ChangeRecall) function.
    Drop -max-distance and -max-rank flags. They are no longer needed.
    
    Drop periodic updates for now, but print results in the end.
    Periodic updates were important to stop evaluation early on when it was
    obvious that the manually provided threshold is bad. We don't have that
    problem anymore.
    
    Overall, evaluation code is now <300 LOC.
    
    Bug: 1112125
    Change-Id: I77d5246a8e40b6dd6da1620b6f15971d213c70fb

--- rts/presubmit/eval/rejections_test.go
+++ rts/presubmit/eval/rejections_test.go
@@ -16,94 +16,13 @@ package eval
 
 import (
 	"bytes"
-	"context"
-	"strconv"
 	"strings"
 	"testing"
 
 	evalpb "go.chromium.org/luci/rts/presubmit/eval/proto"
-	"golang.org/x/sync/errgroup"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
-
-func TestAnalyzeRejections(t *testing.T) {
-	t.Parallel()
-	Convey(`AnalyzeRejections`, t, func() {
-		ctx := context.Background()
-		rejections := []*evalpb.Rejection{
-			{
-				FailedTestVariants: []*evalpb.TestVariant{
-					{Id: "1"},
-					{Id: "2"},
-				},
-			},
-			{
-				FailedTestVariants: []*evalpb.TestVariant{
-					{Id: "3"},
-				},
-			},
-			{
-				FailedTestVariants: []*evalpb.TestVariant{
-					{Id: "4"},
-					{Id: "1"},
-				},
-			},
-		}
-
-		strategy := func(ctx context.Context, in Input, out *Output) error {
-			for i, tv := range in.TestVariants {
-				rank, err := strconv.Atoi(tv.Id)
-				if err != nil {
-					return err
-				}
-				out.TestVariantAffectedness[i].Distance = float64(rank)
-				out.TestVariantAffectedness[i].Rank = rank
-			}
-			return nil
-		}
-
-		eg, ctx := errgroup.WithContext(ctx)
-
-		rejC := make(chan *evalpb.Rejection)
-		var actual []analyzedRejection
-		eg.Go(func() error {
-			return analyzeRejections(ctx, rejC, strategy, func(ctx context.Context, rej analyzedRejection) error {
-				actual = append(actual, rej)
-				return nil
-			})
-		})
-
-		eg.Go(func() error {
-			defer close(rejC)
-			for _, r := range rejections {
-				rejC <- r
-			}
-			return nil
-		})
-
-		So(eg.Wait(), ShouldBeNil)
-		So(actual, ShouldHaveLength, 3)
-
-		So(actual[0].Rejection, ShouldResemble, rejections[0])
-		So(actual[0].Affectedness, ShouldResemble, AffectednessSlice{
-			{Distance: 1, Rank: 1},
-			{Distance: 2, Rank: 2},
-		})
-		So(actual[0].Closest, ShouldResemble, Affectedness{Distance: 1, Rank: 1})
-
-		So(actual[1].Rejection, ShouldResemble, rejections[1])
-		So(actual[1].Affectedness, ShouldResemble, AffectednessSlice{{Distance: 3, Rank: 3}})
-		So(actual[1].Closest, ShouldResemble, Affectedness{Distance: 3, Rank: 3})
-
-		So(actual[2].Rejection, ShouldResemble, rejections[2])
-		So(actual[2].Affectedness, ShouldResemble, AffectednessSlice{
-			{Distance: 4, Rank: 4},
-			{Distance: 1, Rank: 1},
-		})
-		So(actual[2].Closest, ShouldResemble, Affectedness{Distance: 1, Rank: 1})
-	})
-}
 
 func TestPrintLostRejection(t *testing.T) {
 	t.Parallel()
