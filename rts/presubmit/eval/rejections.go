315e29b138b912e2bb75e2603721d3323d8485e2
Nodir Turakulov <nodir@chromium.org>
2020-12-18 20:32:35 -0800

    [rts] Switch to the threshold grid
    This is the 2nd out of two CLs fully described in
    https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2588088
    
    There is a minor departure.
    
    The second history file is no longer used for safety evaluation.
    We have much more test durations than rejections. Normally
    the processed rejection history file contains all rejections in a
    month or more, whereas duration history file includes 1% of one day or
    one week. The second file covers a too small period to have
    enough rejections.
    
    The split into training and eval sets is important in ML applications
    because their model is derived from the training set. In our case,
    the model does not depend on the data. Only thresholds are chosen based
    on data. The need for training|eval set split is not as obvious as in
    traditional ML applications. Regardless, we can add it later.
    
    Rename -training-set flag to -rejections. Use the same file to
    first choose the thresholds, and then evaluate each threshold
    combinations.
    
    Rename -eval-set to -durations. Use the file to evaluate efficiency.
    
    After processing all data, print a table depicting the
    Savings=f(ChangeRecall) function.
    Drop -max-distance and -max-rank flags. They are no longer needed.
    
    Drop periodic updates for now, but print results in the end.
    Periodic updates were important to stop evaluation early on when it was
    obvious that the manually provided threshold is bad. We don't have that
    problem anymore.
    
    Overall, evaluation code is ~300 LOC now.
    
    Bug: 1112125
    Change-Id: I77d5246a8e40b6dd6da1620b6f15971d213c70fb

--- rts/presubmit/eval/rejections.go
+++ rts/presubmit/eval/rejections.go
@@ -15,66 +15,10 @@
 package eval
 
 import (
-	"context"
 	"sort"
-
-	"go.chromium.org/luci/common/errors"
 
 	evalpb "go.chromium.org/luci/rts/presubmit/eval/proto"
 )
-
-type analyzedRejection struct {
-	*evalpb.Rejection
-	// Affectedness are affectedness for each element of Rejection.FailedTestVariants,
-	// in the same order.
-	Affectedness AffectednessSlice
-	// Closest is the closest element in Affectedness.
-	// See also AffectednessSlice.closest().
-	Closest Affectedness
-}
-
-func analyzeRejections(ctx context.Context, rejC <-chan *evalpb.Rejection, strategy Strategy, callback func(context.Context, analyzedRejection) error) error {
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-
-		case rej, ok := <-rejC:
-			if !ok {
-				return nil
-			}
-
-			// TODO(crbug.com/1112125): skip the patchset if it has a ton of failed tests.
-			// Most selection strategies would reject such a patchset, so it represents noise.
-
-			// Select tests.
-			in := Input{
-				TestVariants: rej.FailedTestVariants,
-			}
-			in.ensureChangedFilesInclude(rej.Patchsets...)
-			out := &Output{
-				TestVariantAffectedness: make(AffectednessSlice, len(in.TestVariants)),
-			}
-			if err := strategy(ctx, in, out); err != nil {
-				return errors.Annotate(err, "the selection strategy failed").Err()
-			}
-
-			closest, err := out.TestVariantAffectedness.closest()
-			if err != nil {
-				return err
-			}
-
-			err = callback(ctx, analyzedRejection{
-				Rejection:    rej,
-				Affectedness: out.TestVariantAffectedness,
-				Closest:      out.TestVariantAffectedness[closest],
-			})
-			if err != nil {
-				return err
-			}
-		}
-	}
-}
 
 type rejectionPrinter struct {
 	*printer
