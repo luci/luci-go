aed57e44847c7c825289358ab512b2fe50acdb23
Nodir Turakulov <nodir@chromium.org>
2020-12-28 17:58:34 -0800

    [rts] Extract generic part of filegraph-based RTS
    The graph querying implementation in rts-chromium is mostly generic.
    Extract the generic bits to filegraph/rts.go.
    Add a couple of TODOs with more ideas.
    
    Move key strategy logic from eval.go to strategy.go.
    This function signature is specific to evaluation framework and
    the next CL adds another version of the function, suitable for
    "production" code. These two functions must be synchronized and thus
    must live close to each other, i.e. same file.
    
    Tested by re-evaluating the evaluation strategy and getting same scores.
    
    Bug: 1136280
    Change-Id: I48c3945fde6e4e4f35e78ad3951353b3babfbfff

---
+++ rts/cmd/rts-chromium/strategy.go
@@ -0,0 +1,56 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"strings"
+
+	"go.chromium.org/luci/common/errors"
+
+	"go.chromium.org/luci/rts/presubmit/eval"
+)
+
+func (r *evalRun) selectTests(ctx context.Context, in eval.Input, out *eval.Output) error {
+	for _, f := range in.ChangedFiles {
+		switch {
+		case f.Repo != "https://chromium-review.googlesource.com/chromium/src":
+			return errors.Reason("unexpected repo %q", f.Repo).Err()
+		case requiresAllTests(f.Path):
+			return nil
+		}
+	}
+
+	return r.fg.EvalStrategy(ctx, in, out)
+}
+
+// requiresAllTests returns true if changedFile requires running all tests.
+// If a CL changes such a file, RTS gets disabled.
+func requiresAllTests(changedFile string) bool {
+	switch {
+	case strings.HasPrefix(changedFile, "//testing/"):
+		// This CL changes the way tests run or their configurations.
+		// Run all tests.
+		return true
+
+	case changedFile == "//DEPS":
+		// The full list of modified files is not available, and the
+		// graph does not include DEPSed file changes anyway.
+		return true
+
+	default:
+		return false
+	}
+}
