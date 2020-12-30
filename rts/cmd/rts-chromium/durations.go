618363a33db20897e8447a8668d5222265fc55e4
Nodir Turakulov <nodir@chromium.org>
2020-12-30 14:28:50 -0800

    [rts] Improve test duration data format.
    Currently there are the following problems around history files:
    
    1. Downloading history is very slow because BigQuery querying API is
       not designed to extract large volumes of data.
       It appears that specifically paging through a table is not optimized.
    2. Efficiency analysis is currently very slow because each duration
       is analyzed individually. In evaluation analysis, all tests of the
       same patchset are analyzed in a batch.
    3. The history file format supports having both durations and rejections,
       but the same file is never expected to contain both.
       This is a historical artifact.
    4. The binary format of history files is hard to work with.
       It is difficult to do ad-hoc analysis of history files, e.g.
       elementary record counting in a file is impossible without doing
       full-on analysis or writing new code/tools.
       The format will be an adoption barrier for other teams.
    5. The list of changed files is still fetched via Gerrit API.
    
    Solve all these problems for test durations:
    
    1. Simplify the file format by switching to GZIP-encoded JSON Lines.
       The new format is easy to analyze with gunzip+jq.
    2. Speed up data fetching by 3x (anecdotally). First extract the results
       to GCS and then download those files in parallel and without any
       interpretation. The extraction process on BigQuery side takes care of
       the conversion from tabular format to the destination file format,
       and so remove the existing conversion code.
    3. Speed up efficiency analysis by 7x (anecdotally).
       Group test durations by patchset.
    4. Switch to affected_files output property. We can do so for
       durations because sufficient amount of time has passed since
       affected_files property was introduced.
       Rejections need a wider time window.
    
    Also extract duration fetching to a separate command "fetch-durations",
    to complete the divorce between durations and rejections.
    
    These changes should be done for rejections as well, but not enough time
    has passed to start using affected_files there. Thus, these changes
    are made only for durations for now.
    
    This CL only changes file format and fetching implementation.
    The next CL will update the evaluation code to use the new format.
    
    To simplify the review process, the existing code is modified minimally.
    In particular, the "history" subcommand is mostly left intact, except
    it no longer can fetch durations in the old format.
    
    Bug: 1090668
    Change-Id: I0f471420182f7a33d2e376d8d3d1e4d04b3bbe18

--- rts/cmd/rts-chromium/durations.go
+++ rts/cmd/rts-chromium/durations.go
@@ -15,115 +15,97 @@
 package main
 
 import (
-	"context"
 	"time"
 
 	"cloud.google.com/go/bigquery"
-	"github.com/golang/protobuf/ptypes"
-	"golang.org/x/sync/errgroup"
-	"google.golang.org/api/iterator"
+	"github.com/maruel/subcommands"
 
-	evalpb "go.chromium.org/luci/rts/presubmit/eval/proto"
+	"go.chromium.org/luci/auth"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/errors"
 )
 
-// durations calls the callback for each found test duration.
-func (r *presubmitHistoryRun) durations(ctx context.Context, callback func(*evalpb.TestDuration) error) error {
-	q, err := r.bqQuery(ctx, testDurationsSQL)
-	if err != nil {
-		return err
+func cmdFetchDurations(authOpt *auth.Options) *subcommands.Command {
+	return &subcommands.Command{
+		UsageLine: `fetch-durations`,
+		ShortDesc: "fetch test duration data",
+		LongDesc:  "Fetch test duration data, suitable for selection strategy evaluation",
+		CommandRun: func() subcommands.CommandRun {
+			r := &fetchDurationsRun{}
+			r.authOpt = authOpt
+			r.RegisterBaseFlags(&r.Flags)
+			r.Flags.Float64Var(&r.frac, "frac", 0.001, "Fraction of the data to fetch")
+			r.Flags.DurationVar(&r.minDuration, "min-duration", time.Second, "Minimum duration to fetch")
+			return r
+		},
+	}
+}
+
+type fetchDurationsRun struct {
+	baseCommandRun
+	baseHistoryRun
+	frac        float64
+	minDuration time.Duration
+}
+
+func (r *fetchDurationsRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	ctx := cli.GetContext(a, r, env)
+	if len(args) != 0 {
+		return r.done(errors.New("unexpected positional arguments"))
 	}
 
-	q.Parameters = append(q.Parameters,
+	if err := r.baseHistoryRun.Init(ctx); err != nil {
+		return r.done(err)
+	}
+
+	return r.done(r.runAndFetchResults(
+		ctx,
+		testDurationsSQL,
 		bigquery.QueryParameter{
 			Name:  "frac",
-			Value: r.durationDataFrac,
+			Value: r.frac,
 		},
 		bigquery.QueryParameter{
 			Name:  "minDuration",
 			Value: r.minDuration.Seconds(),
 		},
-	)
-
-	it, err := q.Read(ctx)
-	if err != nil {
-		return err
-	}
-
-	eg, ctx := errgroup.WithContext(ctx)
-
-	withoutChangedFiles := make(chan *evalpb.TestDuration)
-	eg.Go(func() error {
-		defer close(withoutChangedFiles)
-		for {
-			var row durationRow
-			switch err := it.Next(&row); {
-			case err == iterator.Done:
-				return nil
-			case err != nil:
-				return err
-			}
-
-			select {
-			case withoutChangedFiles <- row.proto():
-			case <-ctx.Done():
-				return ctx.Err()
-			}
-		}
-	})
-
-	for i := 0; i < 100; i++ {
-		eg.Go(func() error {
-			for td := range withoutChangedFiles {
-				switch err := r.populateChangedFiles(ctx, td.Patchsets[0]); {
-				case err == errPatchsetDeleted:
-					continue
-				case err != nil:
-					return err
-				}
-				if err := callback(td); err != nil {
-					return err
-				}
-			}
-			return nil
-		})
-	}
-
-	return eg.Wait()
-}
-
-type durationRow struct {
-	Change      int
-	Patchset    int
-	TestVariant *evalpb.TestVariant
-	Duration    float64
-}
-
-func (r *durationRow) proto() *evalpb.TestDuration {
-	return &evalpb.TestDuration{
-		Patchsets: []*evalpb.GerritPatchset{
-			{
-				Change: &evalpb.GerritChange{
-					// Assume all Chromium source code is in
-					// https://chromium.googlesource.com/chromium/src
-					// TOOD(nodir): make it fail if it is not.
-					Host:    "chromium-review.googlesource.com",
-					Project: "chromium/src",
-					Number:  int64(r.Change),
-				},
-				Patchset: int64(r.Patchset),
-			},
-		},
-		TestVariant: r.TestVariant,
-		Duration:    ptypes.DurationProto(time.Duration(r.Duration * 1e9)),
-	}
+	))
 }
 
 const testDurationsSQL = `
 WITH
-	tryjobs AS (
+	affected_files AS (
 		SELECT
-			b.id,
-			ps,
+			ps.change,
+			ps.patchset,
+			ARRAY(
+					SELECT REGEXP_REPLACE(JSON_EXTRACT_SCALAR(file_name_json, "$"), "^src/", "//")
+					FROM UNNEST(JSON_EXTRACT_ARRAY(b.output.properties, "$.affected_files.first_100")) file_name_json
+			) AS files,
+		FROM cr-buildbucket.chromium.builds b, b.input.gerrit_changes ps
+		WHERE create_time BETWEEN @startTime and TIMESTAMP_ADD(@endTime, INTERVAL 1 DAY)
+			-- Skip patchsets that modified >100 files, because we don't have the full
+			-- list.
+			AND CAST(JSON_EXTRACT(b.output.properties, "$.affected_files.total_count") as FLOAT64) <= 100
+			AND NOT EXISTS (
+				SELECT 0
+				FROM UNNEST(JSON_EXTRACT_ARRAY(b.output.properties, "$.affected_files.first_100")) f
+				-- Must start with '"src/"', otherwise it is not an src.git change.
+				-- The leading quote is there because it is a JSON string.
+				WHERE f NOT LIKE "\"src/%"
+			)
+	),
+	ps_affected_files AS (
+		-- Choose the longest file list.
+		-- Lists for the same patchset can be different if the parent CL landed
+		-- between bot_updates of different tryjobs.
+		SELECT change, patchset, ARRAY_AGG(af ORDER BY ARRAY_LENGTH(files) DESC)[OFFSET(0)].files
+		FROM affected_files af
+		GROUP BY change, patchset
+	),
+
+	tryjobs AS (
+		SELECT b.id, ps,
 		FROM commit-queue.chromium.attempts a, a.gerrit_changes ps, a.builds b
 		WHERE partition_time BETWEEN @startTime AND TIMESTAMP_ADD(@endTime, INTERVAL 1 DAY)
 	),
@@ -136,27 +118,40 @@ WITH
 			duration,
 		FROM luci-resultdb.chromium.try_test_results tr
 		WHERE partition_time BETWEEN @startTime and @endTime
-			AND RAND() <= @frac
+		  AND RAND() <= @frac
 			AND (@test_id_regexp = '' OR REGEXP_CONTAINS(test_id, @test_id_regexp))
 			AND (@builder_regexp = '' OR EXISTS (SELECT 0 FROM tr.variant WHERE key='builder' AND REGEXP_CONTAINS(value, @builder_regexp)))
 			AND duration > @minDuration
-
 			-- Exclude broken test locations.
-			-- TODO(nodir): remove this after crbug.com/1130425 is fixed.
-			AND REGEXP_CONTAINS(IFNULL(test_location.file_name, ''), r'(?i)^(|.*\.(cc|html|m|c|cpp))$')
-			-- Exclude broken prefixes.
-			-- TODO(nodir): remove after crbug.com/1017288 is fixed.
-			AND (test_id NOT LIKE 'ninja://:blink_web_tests/%' OR test_location.file_name LIKE '//third_party/%')
+			-- TODO(nodir): replace with simple "not empty" check after crbug.com/1130425 is fixed.
+			AND REGEXP_CONTAINS(test_location.file_name, r'(?i)\.(cc|html|m|c|cpp)$')
 	)
+
+-- Join all tables and produce the rows in the TestDurationRecord protojson format.
 SELECT
-	ps.change as Change,
-	ps.patchset as Patchset,
-	STRUCT(
-		test_id as ID,
-		ARRAY(SELECT FORMAT("%s:%s", key, value) FROM UNNEST(variant)) as Variant,
-		file_name as FileName
-	) as TestVariant,
-	duration as Duration
+	[STRUCT(
+		STRUCT(
+			"chromium-review.googlsource.com" AS host,
+			"src" AS project,
+			ps.change AS number
+		) AS change,
+		ps.patchset,
+		ANY_VALUE(ARRAY(
+			SELECT AS STRUCT
+				"https://chromium.googlsource.com/src" AS repo,
+				f as path
+			FROM UNNEST(af.files) f
+		)) AS changedFiles
+	)] AS patchsets,
+	ARRAY_AGG(STRUCT(
+		STRUCT(
+			test_id AS id,
+			ARRAY((SELECT FORMAT("%s:%s", key, value) FROM tr.variant)) as variant,
+			file_name as fileName
+		) as testVariant,
+		FORMAT("%fs", duration) as duration
+	)) AS testDurations,
 FROM tryjobs t
 JOIN test_results tr ON t.id = tr.build_id
-`
+JOIN ps_affected_files af ON ps.change = af.change AND ps.patchset = af.patchset
+GROUP BY ps.change, ps.patchset`
