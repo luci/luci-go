f7715274a54b4cf8afac127aff1b3fb3266a4734
Andrii Shyshkalov <tandrii@google.com>
2021-01-06 17:03:29 +0000

    cv: complete run creation code.
    R=qyearsley, yiwzhang
    
    Change-Id: I79a7404bd2e8ff48b9a7733f85214b7eb1bcbaa0
    Bug: 1156241
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2606330
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>

--- cv/internal/prjmanager/run.go
+++ cv/internal/prjmanager/run.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"sort"
 	"strconv"
+	"time"
 
 	"go.chromium.org/luci/auth/identity"
 	"go.chromium.org/luci/common/clock"
@@ -257,9 +258,78 @@ func (rb *RunBuilder) checkCLsUnchanged(ctx context.Context) {
 	}
 }
 
+// save saves all modified/created Datastore entities.
+//
+// It may be retried multiple times on failure.
 func (rb *RunBuilder) save(ctx context.Context) error {
-	// TODO(tandrii): implement.
-	return nil
+	rb.dsBatcher.reset()
+	// Keep .CreateTime and | .UpdateTime entities the same across all saved
+	// entities. Do pre-emptive rounding before Datastore layer does it such
+	// rb.run entityRun entity has exactly values as what would be read from
+	// Datastore later.
+	now := datastore.RoundTime(clock.Now(ctx).UTC())
+	rb.registerSaveRun(ctx, now)
+	for i := range rb.InputCLs {
+		rb.registerSaveRunCL(ctx, i)
+		rb.registerSaveCL(ctx, i, now)
+	}
+	// TODO(tandrii): consider also using rb.dsBatcher for PM notification,
+	// or at least doing this concurrently with rb.dsBatcher.put.
+	if err := rb.savePMNotification(ctx); err != nil {
+		return err
+	}
+	return rb.dsBatcher.put(ctx)
+}
+
+func (rb *RunBuilder) registerSaveRun(ctx context.Context, now time.Time) {
+	ids := make([]common.CLID, len(rb.InputCLs))
+	for i, cl := range rb.InputCLs {
+		ids[i] = cl.ID
+	}
+	rb.run = &run.Run{
+		ID:                  rb.run.ID,
+		EVersion:            1,
+		CreationOperationID: rb.OperationID,
+		CreateTime:          now,
+		UpdateTime:          now,
+		// EndTime & StartTime intentionally left unset.
+
+		CLs:           ids,
+		ConfigGroupID: rb.ConfigGroupID,
+		Mode:          rb.Mode,
+		Status:        run.Status_PENDING,
+		Owner:         rb.Owner,
+	}
+	rb.dsBatcher.register(rb.run, func(err error) error {
+		return errors.Annotate(err, "failed to save Run").Tag(transient.Tag).Err()
+	})
+}
+
+func (rb *RunBuilder) registerSaveRunCL(ctx context.Context, index int) {
+	inputCL := rb.InputCLs[index]
+	entity := &run.RunCL{
+		Run:     datastore.MakeKey(ctx, run.RunKind, string(rb.run.ID)),
+		ID:      inputCL.ID,
+		Trigger: inputCL.TriggerInfo,
+		Detail:  rb.cls[index].Snapshot,
+	}
+	rb.dsBatcher.register(entity, func(err error) error {
+		return errors.Annotate(err, "failed to save RunCL %d", inputCL.ID).Tag(transient.Tag).Err()
+	})
+}
+
+func (rb *RunBuilder) registerSaveCL(ctx context.Context, index int, now time.Time) {
+	cl := rb.cls[index]
+	cl.EVersion++
+	cl.UpdateTime = now
+	cl.IncompleteRuns.InsertSorted(rb.runID)
+	rb.dsBatcher.register(cl, func(err error) error {
+		return errors.Annotate(err, "failed to save CL %d", cl.ID).Tag(transient.Tag).Err()
+	})
+}
+
+func (rb *RunBuilder) savePMNotification(ctx context.Context) error {
+	return runCreated(ctx, rb.runID)
 }
 
 // computeCLsDigest populates `.runIDBuilder` for use by computeRunID.
--- cv/internal/prjmanager/send.go
+++ cv/internal/prjmanager/send.go
@@ -62,6 +62,32 @@ func CLUpdated(ctx context.Context, luciProject string, clid common.CLID, eversi
 	})
 }
 
+// runCreated is sent by ProjectManager to itself within a Run creation
+// transaction.
+//
+// Unlike other event-sending funcs, this only creates an event and doesn't
+// create a task. This is fine because:
+//   * if Run creation transaction fails, then this event isn't actually
+//     created anyways.
+//   * if ProjectManager observes the Run creation success, then it'll act as if
+//     this event was received in the upcoming state transition. Yes, it won't
+//     process this event immediately, but at this point the event is a noop,
+//     so it'll be cleared out from the eventbox upon next invocation of
+//     ProjectManager. So there is no need to create a TQ task.
+//   * else, namely Run creation succeeds but ProjectManager sees it as a
+//     failure OR ProjectManager fails at any point before it can act on
+//     RunCreation, then the existing TQ task running ProjectManager will be
+//     retried. So once again there is no need to create a TQ task.
+func runCreated(ctx context.Context, runID common.RunID) error {
+	return sendWithoutDispatch(ctx, runID.LUCIProject(), &internal.Event{
+		Event: &internal.Event_RunCreated{
+			RunCreated: &internal.RunCreated{
+				RunId: string(runID),
+			},
+		},
+	})
+}
+
 // RunFinished tells ProjectManager that a run has finalized its state.
 func RunFinished(ctx context.Context, runID common.RunID) error {
 	return send(ctx, runID.LUCIProject(), &internal.Event{
@@ -74,13 +100,17 @@ func RunFinished(ctx context.Context, runID common.RunID) error {
 }
 
 func send(ctx context.Context, luciProject string, e *internal.Event) error {
+	if err := sendWithoutDispatch(ctx, luciProject, e); err != nil {
+		return err
+	}
+	return internal.Dispatch(ctx, luciProject, time.Time{} /*asap*/)
+}
+
+func sendWithoutDispatch(ctx context.Context, luciProject string, e *internal.Event) error {
 	value, err := proto.Marshal(e)
 	if err != nil {
 		return errors.Annotate(err, "failed to marshal").Err()
 	}
 	to := datastore.MakeKey(ctx, ProjectKind, luciProject)
-	if err := eventbox.Emit(ctx, value, to); err != nil {
-		return err
-	}
-	return internal.Dispatch(ctx, luciProject, time.Time{} /*asap*/)
+	return eventbox.Emit(ctx, value, to)
 }
