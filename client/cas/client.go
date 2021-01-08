fd00cac545ed2e9e04b36143f87ba5cd21b994f9
Junji Watanabe <jwata@google.com>
2021-01-08 12:46:08 +0900

    [client][cas] specify client.Opts at NewClient
    CAS client options should be specified at initialization
    
    Change-Id: I9ba50ee222d319e8462a0973ba080f9f126b4c06

--- client/cas/client.go
+++ client/cas/client.go
@@ -59,24 +59,29 @@ func NewClient(ctx context.Context, instance string, opts auth.Options, readOnly
 		casConcurrency = runtime.NumCPU()
 	}
 
+	clOpts := []client.Opt{
+		&client.PerRPCCreds{Creds: creds},
+		client.CASConcurrency(casConcurrency),
+		client.UtilizeLocality(true),
+		&client.TreeSymlinkOpts{Preserved: true, FollowsTarget: false},
+		client.DirMode(0700),
+		client.ExecutableMode(0700),
+		client.RegularMode(0600),
+	}
 	cl, err := client.NewClient(ctx, instance,
 		client.DialParams{
 			Service:            "remotebuildexecution.googleapis.com:443",
 			TransportCredsOnly: true,
-		}, &client.PerRPCCreds{Creds: creds},
-		client.CASConcurrency(casConcurrency))
+		}, clOpts...)
 	if err != nil {
 		return nil, errors.Annotate(err, "failed to create client").Err()
 	}
 
 	// Set restricted permission for written files.
+	// TODO(jwata): define DirMode and ExecutableMode, and RegularMode in remote-apis-sdks client package.
 	cl.DirMode = 0700
 	cl.ExecutableMode = 0700
 	cl.RegularMode = 0600
-	cl.UtilizeLocality = true
-	cl.TreeSymlinkOpts = client.DefaultTreeSymlinkOpts()
-	cl.TreeSymlinkOpts.Preserved = true
-	cl.TreeSymlinkOpts.FollowsTarget = false
 
 	return cl, nil
 }
