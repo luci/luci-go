9086e7b8a3f530d0b4927ac8e0dcf29dcd077962
Andrii Shyshkalov <tandrii@google.com>
2021-01-20 18:20:27 +0000

    cv: add copyonewrite template.
    It turns out during modification of proto PState I constantly had to do
    something like this. Oh how much I want generics :(
    Till then, here is the ugly interface approach.
    
    R=yiwzhang
    
    Bug: 1156241
    Change-Id: Id8f899f997d538ecd7d2020f6453090f15fd37c7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2633274
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>

---
+++ cv/internal/prjmanager/internal/copyonwrite/cow.go
@@ -0,0 +1,156 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package copyonwrite
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Slice abstracts out copy-on-write slices of pointers to objects.
+type Slice interface {
+	Len() int
+	At(index int) interface{}
+	Append(v interface{}) Slice
+	// CloneShallow returns new slice of the given capacity with elements copied
+	// from [:length] of this slice.
+	CloneShallow(length, capacity int) Slice
+}
+
+// SortedSlice is a sorted Slice.
+type SortedSlice interface {
+	Slice
+	sort.Interface
+	// LessElements returns true if element `a` must be before `b`.
+	//
+	// Unlike sort.Interface's Less, this compares arbitrary elements.
+	//
+	// Why not use sort.Interface's Less?
+	// Because during merge phase of (newly created, updated) slices, it's
+	// necessary to compare elements from different slices. OK, OK, it's possible
+	// to move both elements to one slice and re-use Less. For example, a temp
+	// slice of 2 elements can be always allocated for this very purpose, or one
+	// can smartly re-use resulting slice for this. However, either way it's
+	// slower and more complicated than requiring boilerplate in each SortedSlice
+	// type, which can be re-used in Less() implementation.
+	LessElements(a, b interface{}) bool
+}
+
+// Deletion is special return value for Modifier to imply deletion.
+var Deletion = deletion{}
+
+type deletion struct{}
+
+// Modifier takes a pointer to an existing COW object and returns:
+// * special DeleteMe, if this object should be deleted from Slice;
+// * the same object, if there are no modifications;
+// * new object, if there were modifications. If working with SortedSlice,
+//   the new object must have the same sorting key.
+type Modifier func(interface{}) interface{}
+
+// Update modifies existing elements and adds new ones to Slice.
+//
+// If given Slice implements SortedSlice, assumes and preserves its sorted
+// order.
+//
+// COWModifier, if not nil, is called on each existing object to modify or
+// possible delete it.
+//
+// Sorts toAdd slice if using SortedSlice.
+//
+// Returns (new slice, true) if it was modified.
+// Returns (old slice, false) otherwise.
+func Update(in Slice, modifier Modifier, toAdd Slice) (Slice, bool) {
+	inLen := 0
+	if in != nil {
+		inLen = in.Len()
+	}
+	if modifier == nil {
+		modifier = noopModifier
+	}
+	cLen := 0
+	if toAdd != nil {
+		cLen = toAdd.Len()
+	}
+	_, isSorted := in.(SortedSlice)
+	if isSorted && cLen > 0 {
+		sortable, ok := toAdd.(SortedSlice)
+		if !ok {
+			panic(fmt.Errorf("Different types for in and toAdd slices: %T vs %T", in, toAdd))
+		}
+		sort.Sort(sortable)
+	}
+	switch {
+	case inLen == 0 && cLen == 0:
+		return in, false
+	case inLen == 0:
+		return toAdd, true
+	case cLen == 0:
+		return modifyRemaining(in, 0, modifier, nil)
+	case !isSorted:
+		return modifyRemaining(in, 0, modifier, toAdd)
+	}
+
+	// Merge two sorted sequences into `out` while modifying `in` elements at the
+	// same time.
+	less := in.(SortedSlice).LessElements
+	out := in.CloneShallow(0, inLen+cLen)
+	i, c := 0, 0
+	for {
+		if c == cLen {
+			return modifyRemaining(in, i, modifier, out)
+		}
+		if i == inLen {
+			for ; c < cLen; c++ {
+				out = out.Append(toAdd.At(c))
+			}
+			return out, true
+		}
+		iOld := in.At(i)
+		cNew := toAdd.At(c)
+		if less(cNew, iOld) {
+			c++
+			out = out.Append(cNew)
+			continue
+		}
+		i++
+		if iNew := modifier(iOld); iNew != Deletion {
+			out = out.Append(iNew)
+		}
+	}
+}
+
+func noopModifier(v interface{}) interface{} { return v }
+
+func modifyRemaining(in Slice, i int, modifier Modifier, out Slice) (Slice, bool) {
+	l := in.Len()
+	for ; i < l; i++ {
+		before := in.At(i)
+		after := modifier(before)
+		if out == nil && after == before {
+			continue // no changes so far
+		}
+		if out == nil {
+			out = in.CloneShallow(i, l)
+		}
+		if after != Deletion {
+			out = out.Append(after)
+		}
+	}
+	if out == nil {
+		return in, false
+	}
+	return out, true
+}
---
+++ cv/internal/prjmanager/internal/copyonwrite/cow_test.go
@@ -0,0 +1,185 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package copyonwrite
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+type el struct {
+	id, val int
+}
+
+// evenCubedOddDeleted deletes `el`s with odd IDs and sets val to id^3 for even
+// ones.
+func evenCubedOddDeleted(v interface{}) interface{} {
+	in := v.(*el)
+	if in.id&1 == 1 {
+		return Deletion
+	}
+	c := in.id * in.id * in.id
+	if in.val == c {
+		return in
+	}
+	return &el{in.id, c}
+}
+
+func TestUpdate(t *testing.T) {
+	t.Parallel()
+
+	mustNoop := func(in Slice, m Modifier, add Slice) {
+		out, u := Update(in, m, add)
+		So(u, ShouldBeFalse)
+		So(out, ShouldResemble, in)
+	}
+
+	Convey("Update noops", t, func() {
+		Convey("empty", func() {
+			mustNoop(nil, nil, nil)
+			mustNoop(elSlice{}, nil, nil)
+			mustNoop(nil, evenCubedOddDeleted, nil)
+			mustNoop(nil, evenCubedOddDeleted, elSlice{})
+		})
+		Convey("no changes", func() {
+			mustNoop(elSlice{{0, 0}, {2, 8}, {4, 64}}, nil, nil)
+			mustNoop(elSlice{{0, 0}, {2, 8}, {4, 64}}, evenCubedOddDeleted, nil)
+			mustNoop(elSlice{{0, 0}, {2, 8}, {4, 64}}, evenCubedOddDeleted, elSlice{})
+		})
+	})
+
+	Convey("Update works on Slice", t, func() {
+		Convey("deletes", func() {
+			res, u := Update(elSlice{{1, 0}, {4, 64}, {3, 0}, {0, 0}}, evenCubedOddDeleted, nil)
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSlice{{4, 64}, {0, 0}})
+		})
+		Convey("modifies", func() {
+			res, u := Update(elSlice{{2, 8}, {4, 0}}, evenCubedOddDeleted, nil)
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSlice{{2, 8}, {4, 64}})
+		})
+		Convey("modifies and deletes", func() {
+			res, u := Update(elSlice{{1, 0}, {2, 8}, {3, 0}, {4, 0}}, evenCubedOddDeleted, nil)
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSlice{{2, 8}, {4, 64}})
+		})
+		Convey("creates on empty", func() {
+			res, u := Update(nil, nil, elSlice{{6, 0}, {5, 0}})
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSlice{{6, 0}, {5, 0}})
+		})
+		Convey("creates", func() {
+			res, u := Update(elSlice{{1, 0}}, nil, elSlice{{6, 0}, {5, 0}})
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSlice{{6, 0}, {5, 0}, {1, 0}})
+		})
+		Convey("creates, modifies and deletes", func() {
+			res, u := Update(elSlice{{1, 0}, {2, 8}, {3, 0}, {4, 0}}, evenCubedOddDeleted, elSlice{{5, 25}, {0, 0}})
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSlice{{5, 25}, {0, 0}, {2, 8}, {4, 64}})
+		})
+	})
+
+	Convey("Update works on SortedSlice", t, func() {
+		Convey("panics if toAdd is not sorted", func() {
+			So(func() { Update(elSortedSlice{}, nil, elSlice{{3, 8}}) },
+				ShouldPanicLike, "Different types for in and toAdd slices")
+		})
+		Convey("creates sorted", func() {
+			res, u := Update(elSortedSlice{}, nil, elSortedSlice{{3, 8}, {1, 8}, {4, 1}, {2, 0}})
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSortedSlice{{1, 8}, {2, 0}, {3, 8}, {4, 1}})
+		})
+		Convey("modifies and deletes", func() {
+			res, u := Update(elSortedSlice{{1, 0}, {2, 8}, {3, 0}, {4, 0}}, evenCubedOddDeleted, nil)
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSortedSlice{{2, 8}, {4, 64}})
+		})
+		Convey("deletes everything", func() {
+			res, u := Update(elSortedSlice{{1, 0}, {3, 0}}, evenCubedOddDeleted, nil)
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSortedSlice{})
+		})
+		Convey("creates, modifies and deletes", func() {
+			in := elSortedSlice{{1, 0}, {2, 8}, {3, 0}, {4, 0}, {6, 1}, {7, 3}}
+			res, u := Update(in, evenCubedOddDeleted, elSortedSlice{{10, 100}, {0, 0}, {5, 25}})
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSortedSlice{{0, 0}, {2, 8}, {4, 64}, {5, 25}, {6, 216}, {10, 100}})
+
+			res, u = Update(in, evenCubedOddDeleted, elSortedSlice{{3, 3}})
+			So(u, ShouldBeTrue)
+			So(res, ShouldResemble, elSortedSlice{{2, 8}, {3, 3}, {4, 64}, {6, 216}})
+		})
+	})
+}
+
+type elSlice []*el
+
+var _ Slice = elSlice(nil)
+
+func (e elSlice) Len() int {
+	return len(e)
+}
+
+func (e elSlice) At(index int) interface{} {
+	return e[index]
+}
+
+func (e elSlice) Append(v interface{}) Slice {
+	return append(e, v.(*el))
+}
+
+func (e elSlice) CloneShallow(length int, capacity int) Slice {
+	r := make(elSlice, length, capacity)
+	copy(r, e[:length])
+	return r
+}
+
+type elSortedSlice []*el
+
+var _ SortedSlice = elSortedSlice(nil)
+
+func (e elSortedSlice) Len() int {
+	return len(e)
+}
+
+func (e elSortedSlice) At(index int) interface{} {
+	return e[index]
+}
+
+func (e elSortedSlice) Append(v interface{}) Slice {
+	return append(e, v.(*el))
+}
+
+func (e elSortedSlice) CloneShallow(length int, capacity int) Slice {
+	r := make(elSortedSlice, length, capacity)
+	copy(r, e[:length])
+	return r
+}
+
+func (e elSortedSlice) Less(i int, j int) bool {
+	return e[i].id < e[j].id
+}
+
+func (e elSortedSlice) Swap(i int, j int) {
+	e[i], e[j] = e[j], e[i]
+}
+
+func (_ elSortedSlice) LessElements(a interface{}, b interface{}) bool {
+	return a.(*el).id < b.(*el).id
+}
