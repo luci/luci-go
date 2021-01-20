2e9f8d70269ac81f98bc97f17c52991b9bbfeb59
Joey Scarr <jsca@google.com>
2021-01-20 01:27:36 +0000

    [resultui] Fix a runtime error when there are only expected results.
    Change-Id: I2663530ecaf2523cd39b7988285a948eaa7b7ee5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2637038
    Auto-Submit: Joey Scarr <jsca@chromium.org>
    Reviewed-by: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Commit-Queue: Weiwei Lin <weiweilin@google.com>

--- milo/frontend/resultui/src/models/test_loader_test.ts
+++ milo/frontend/resultui/src/models/test_loader_test.ts
@@ -94,114 +94,140 @@ describe('test_loader', () => {
     let testLoader: TestLoader;
     let stub = sinon.stub();
     const req = {invocations: ['invocation'], pageSize: 4};
-    beforeEach(() => {
-      addTestSpy = sinon.spy();
-      stub = sinon.stub();
-      stub.onCall(0).resolves({testVariants: [variant1, variant2, variant3, variant4], nextPageToken: 'page2'});
-      stub.onCall(1).resolves({testVariants: [variant5, variant6, variant7, variant8], nextPageToken: 'page3'});
-      stub.onCall(2).resolves({testVariants: [variant9], nextPageToken: undefined});
-      testLoader = new TestLoader(
-        {addTestId: addTestSpy} as Partial<TestNode> as TestNode,
-        req,
-        {queryTestVariants: stub} as Partial<UISpecificService> as UISpecificService,
-      );
+    describe('when first page contains variants', () => {
+      beforeEach(() => {
+        addTestSpy = sinon.spy();
+        stub = sinon.stub();
+        stub.onCall(0).resolves({testVariants: [variant1, variant2, variant3, variant4], nextPageToken: 'page2'});
+        stub.onCall(1).resolves({testVariants: [variant5, variant6, variant7, variant8], nextPageToken: 'page3'});
+        stub.onCall(2).resolves({testVariants: [variant9], nextPageToken: undefined});
+        testLoader = new TestLoader(
+          {addTestId: addTestSpy} as Partial<TestNode> as TestNode,
+          req,
+          {queryTestVariants: stub} as Partial<UISpecificService> as UISpecificService,
+        );
+      });
+
+      it('should load tests to test node on request', async () => {
+        assert.strictEqual(addTestSpy.callCount, 0);
+        assert.strictEqual(stub.callCount, 0);
+        await testLoader.loadNextPage();
+        assert.strictEqual(addTestSpy.callCount, 4);
+        assert.strictEqual(stub.callCount, 1);
+        assert.deepEqual(stub.getCall(0).args[0], {...req, pageToken: ''});
+
+        assert.strictEqual(addTestSpy.getCall(0).args[0], variant1.testId);
+        assert.strictEqual(addTestSpy.getCall(1).args[0], variant2.testId);
+        assert.strictEqual(addTestSpy.getCall(2).args[0], variant3.testId);
+        assert.strictEqual(addTestSpy.getCall(3).args[0], variant4.testId);
+      });
+
+      it('should preserve loading progress', async () => {
+        assert.strictEqual(testLoader.stage, LoadingStage.LoadingUnexpected);
+        assert.strictEqual(stub.callCount, 0);
+
+        await testLoader.loadNextPage();
+        assert.strictEqual(addTestSpy.callCount, 4);
+        assert.strictEqual(addTestSpy.getCall(0).args[0], variant1.testId);
+        assert.strictEqual(addTestSpy.getCall(1).args[0], variant2.testId);
+        assert.strictEqual(addTestSpy.getCall(2).args[0], variant3.testId);
+        assert.strictEqual(addTestSpy.getCall(3).args[0], variant4.testId);
+        assert.strictEqual(testLoader.stage, LoadingStage.LoadingFlaky);
+        assert.strictEqual(stub.callCount, 1);
+        assert.deepEqual(stub.getCall(0).args[0], {...req, pageToken: ''});
+
+        await testLoader.loadNextPage();
+        assert.strictEqual(addTestSpy.callCount, 8);
+        assert.strictEqual(addTestSpy.getCall(4).args[0], variant5.testId);
+        assert.strictEqual(addTestSpy.getCall(5).args[0], variant6.testId);
+        assert.strictEqual(addTestSpy.getCall(6).args[0], variant7.testId);
+        assert.strictEqual(addTestSpy.getCall(7).args[0], variant8.testId);
+        assert.strictEqual(testLoader.stage, LoadingStage.LoadingExpected);
+        assert.strictEqual(stub.callCount, 2);
+        assert.deepEqual(stub.getCall(1).args[0], {...req, pageToken: 'page2'});
+
+        await testLoader.loadNextPage();
+        assert.strictEqual(addTestSpy.callCount, 9);
+        assert.strictEqual(addTestSpy.getCall(8).args[0], variant9.testId);
+        assert.strictEqual(testLoader.stage, LoadingStage.Done);
+        assert.strictEqual(stub.callCount, 3);
+        assert.deepEqual(stub.getCall(2).args[0], {...req, pageToken: 'page3'});
+
+        // Should not load when the iterator is exhausted.
+        await testLoader.loadNextPage();
+        assert.strictEqual(addTestSpy.callCount, 9);
+        assert.strictEqual(testLoader.stage, LoadingStage.Done);
+        assert.strictEqual(stub.callCount, 3);
+      });
+
+      it('should handle concurrent loadNextPage calls correctly', async () => {
+        assert.strictEqual(testLoader.stage, LoadingStage.LoadingUnexpected);
+
+        const loadReq1 = testLoader.loadNextPage();
+        const loadReq2 = testLoader.loadNextPage();
+        const loadReq3 = testLoader.loadNextPage();
+        const loadReq4 = testLoader.loadNextPage();
+        assert.isTrue(testLoader.isLoading);
+        assert.strictEqual(testLoader.stage, LoadingStage.LoadingUnexpected);
+
+        await loadReq1;
+        assert.strictEqual(addTestSpy.callCount, 4);
+        // loadReq2 has not finished loading yet.
+        assert.isTrue(testLoader.isLoading);
+        assert.strictEqual(testLoader.stage, LoadingStage.LoadingFlaky);
+
+        await loadReq2;
+        assert.strictEqual(addTestSpy.callCount, 8);
+        // loadReq3 has not finished loading yet.
+        assert.isTrue(testLoader.isLoading);
+        assert.strictEqual(testLoader.stage, LoadingStage.LoadingExpected);
+
+        await loadReq3;
+        assert.strictEqual(addTestSpy.callCount, 9);
+        // The list is exhausted, loadReq4 should not change the loading state.
+        assert.isFalse(testLoader.isLoading);
+        assert.strictEqual(testLoader.stage, LoadingStage.Done);
+
+        await loadReq4;
+        assert.strictEqual(addTestSpy.callCount, 9);
+        assert.isFalse(testLoader.isLoading);
+        assert.strictEqual(testLoader.stage, LoadingStage.Done);
+
+        assert.strictEqual(stub.callCount, 3);
+        assert.deepEqual(stub.getCall(0).args[0], {...req, pageToken: ''});
+        assert.deepEqual(stub.getCall(1).args[0], {...req, pageToken: 'page2'});
+        assert.deepEqual(stub.getCall(2).args[0], {...req, pageToken: 'page3'});
+      });
+
+      it('should correctly set firstRequestSent', async () => {
+        assert.isFalse(testLoader.firstRequestSent);
+        testLoader.loadNextPage();
+        assert.isTrue(testLoader.firstRequestSent);
+      });
     });
 
-    it('should load tests to test node on request', async () => {
-      assert.strictEqual(addTestSpy.callCount, 0);
-      assert.strictEqual(stub.callCount, 0);
-      await testLoader.loadNextPage();
-      assert.strictEqual(addTestSpy.callCount, 4);
-      assert.strictEqual(stub.callCount, 1);
-      assert.deepEqual(stub.getCall(0).args[0], {...req, pageToken: ''});
+    describe('when first page contains no variants', () => {
+      beforeEach(() => {
+        addTestSpy = sinon.spy();
+        stub = sinon.stub();
+        stub.onCall(0).resolves({nextPageToken: 'page2'});
+        stub.onCall(1).resolves({testVariants: [variant1], nextPageToken: undefined});
+        testLoader = new TestLoader(
+          {addTestId: addTestSpy} as Partial<TestNode> as TestNode,
+          req,
+          {queryTestVariants: stub} as Partial<UISpecificService> as UISpecificService,
+        );
+      });
 
-      assert.strictEqual(addTestSpy.getCall(0).args[0], variant1.testId);
-      assert.strictEqual(addTestSpy.getCall(1).args[0], variant2.testId);
-      assert.strictEqual(addTestSpy.getCall(2).args[0], variant3.testId);
-      assert.strictEqual(addTestSpy.getCall(3).args[0], variant4.testId);
-    });
+      it('should correctly handle a response with 0 variants', async () => {
+        assert.strictEqual(stub.callCount, 0);
 
-    it('should preserve loading progress', async () => {
-      assert.strictEqual(testLoader.stage, LoadingStage.LoadingUnexpected);
-      assert.strictEqual(stub.callCount, 0);
+        await testLoader.loadNextPage();
+        assert.strictEqual(addTestSpy.callCount, 0);
 
-      await testLoader.loadNextPage();
-      assert.strictEqual(addTestSpy.callCount, 4);
-      assert.strictEqual(addTestSpy.getCall(0).args[0], variant1.testId);
-      assert.strictEqual(addTestSpy.getCall(1).args[0], variant2.testId);
-      assert.strictEqual(addTestSpy.getCall(2).args[0], variant3.testId);
-      assert.strictEqual(addTestSpy.getCall(3).args[0], variant4.testId);
-      assert.strictEqual(testLoader.stage, LoadingStage.LoadingFlaky);
-      assert.strictEqual(stub.callCount, 1);
-      assert.deepEqual(stub.getCall(0).args[0], {...req, pageToken: ''});
-
-      await testLoader.loadNextPage();
-      assert.strictEqual(addTestSpy.callCount, 8);
-      assert.strictEqual(addTestSpy.getCall(4).args[0], variant5.testId);
-      assert.strictEqual(addTestSpy.getCall(5).args[0], variant6.testId);
-      assert.strictEqual(addTestSpy.getCall(6).args[0], variant7.testId);
-      assert.strictEqual(addTestSpy.getCall(7).args[0], variant8.testId);
-      assert.strictEqual(testLoader.stage, LoadingStage.LoadingExpected);
-      assert.strictEqual(stub.callCount, 2);
-      assert.deepEqual(stub.getCall(1).args[0], {...req, pageToken: 'page2'});
-
-      await testLoader.loadNextPage();
-      assert.strictEqual(addTestSpy.callCount, 9);
-      assert.strictEqual(addTestSpy.getCall(8).args[0], variant9.testId);
-      assert.strictEqual(testLoader.stage, LoadingStage.Done);
-      assert.strictEqual(stub.callCount, 3);
-      assert.deepEqual(stub.getCall(2).args[0], {...req, pageToken: 'page3'});
-
-      // Should not load when the iterator is exhausted.
-      await testLoader.loadNextPage();
-      assert.strictEqual(addTestSpy.callCount, 9);
-      assert.strictEqual(testLoader.stage, LoadingStage.Done);
-      assert.strictEqual(stub.callCount, 3);
-    });
-
-    it('should handle concurrent loadNextPage calls correctly', async () => {
-      assert.strictEqual(testLoader.stage, LoadingStage.LoadingUnexpected);
-
-      const loadReq1 = testLoader.loadNextPage();
-      const loadReq2 = testLoader.loadNextPage();
-      const loadReq3 = testLoader.loadNextPage();
-      const loadReq4 = testLoader.loadNextPage();
-      assert.isTrue(testLoader.isLoading);
-      assert.strictEqual(testLoader.stage, LoadingStage.LoadingUnexpected);
-
-      await loadReq1;
-      assert.strictEqual(addTestSpy.callCount, 4);
-      // loadReq2 has not finished loading yet.
-      assert.isTrue(testLoader.isLoading);
-      assert.strictEqual(testLoader.stage, LoadingStage.LoadingFlaky);
-
-      await loadReq2;
-      assert.strictEqual(addTestSpy.callCount, 8);
-      // loadReq3 has not finished loading yet.
-      assert.isTrue(testLoader.isLoading);
-      assert.strictEqual(testLoader.stage, LoadingStage.LoadingExpected);
-
-      await loadReq3;
-      assert.strictEqual(addTestSpy.callCount, 9);
-      // The list is exhausted, loadReq4 should not change the loading state.
-      assert.isFalse(testLoader.isLoading);
-      assert.strictEqual(testLoader.stage, LoadingStage.Done);
-
-      await loadReq4;
-      assert.strictEqual(addTestSpy.callCount, 9);
-      assert.isFalse(testLoader.isLoading);
-      assert.strictEqual(testLoader.stage, LoadingStage.Done);
-
-      assert.strictEqual(stub.callCount, 3);
-      assert.deepEqual(stub.getCall(0).args[0], {...req, pageToken: ''});
-      assert.deepEqual(stub.getCall(1).args[0], {...req, pageToken: 'page2'});
-      assert.deepEqual(stub.getCall(2).args[0], {...req, pageToken: 'page3'});
-    });
-
-    it('should correctly set firstRequestSent', async () => {
-      assert.isFalse(testLoader.firstRequestSent);
-      testLoader.loadNextPage();
-      assert.isTrue(testLoader.firstRequestSent);
+        await testLoader.loadNextPage();
+        assert.strictEqual(addTestSpy.callCount, 1);
+      });
     });
   });
 });
