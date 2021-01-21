c108235462bafb0611bb957be500e9bd8c62e439
Yuanjun Huang <yuanjunh@google.com>
2021-01-21 22:01:44 +0000

    [buildbucket] Enable 50% batch traffic in Dev
    Bug:1144958
    Change-Id: I228e2bf5c8bab0c210db274f43fc6bdb80a06b08
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2639610
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: smut <smut@google.com>
    Commit-Queue: Yuanjun Huang <yuanjunh@google.com>

--- buildbucket/appengine/rpc/builds.go
+++ buildbucket/appengine/rpc/builds.go
@@ -79,7 +79,7 @@ func getBuildsSubMask(fields *field_mask.FieldMask) (*mask.Mask, error) {
 // TODO(crbug/1042991): Remove once methods are implemented.
 func buildsServicePostlude(ctx context.Context, methodName string, rsp proto.Message, err error) error {
 	err = commonPostlude(ctx, methodName, rsp, err)
-	if methodName == "GetBuild" || methodName == "SearchBuilds" {
+	if methodName == "GetBuild" || methodName == "SearchBuilds" || methodName == "Batch" {
 		return err
 	}
 	logging.Debugf(ctx, "%q would have returned %q with response %s", methodName, err, proto.MarshalTextString(rsp))
