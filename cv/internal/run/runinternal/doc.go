0767315b462d2fd0dab7348718dc5eaa59f74b38
Andrii Shyshkalov <tandrii@google.com>
2021-01-21 18:02:51 +0100

    cv: rename run's internal to runinternal package.
    R=yiwzhang
    
    Bug: 1169137
    Change-Id: I867e50bfef2f3daf1bc5ae68d29f774a54aa0ea1

--- cv/internal/run/runinternal/doc.go
+++ cv/internal/run/runinternal/doc.go
@@ -12,6 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package internal
-
-//go:generate cproto
+// Package runinternal stores protos for event processing of RunManager.
+package runinternal
--- cv/internal/run/runinternal/gen.go
+++ cv/internal/run/runinternal/gen.go
@@ -12,5 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package internal stores protos for event processing of RunManager.
-package internal
+package runinternal
+
+//go:generate cproto
