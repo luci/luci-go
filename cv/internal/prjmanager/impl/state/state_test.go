690ebc8941156173cbf21b803c77602903611851
Andrii Shyshkalov <tandrii@google.com>
2021-01-21 17:40:51 +0100

    cv: PM loads all missing CLs before re-partitioning.
    Changes OnUpdatedCLs:
      previously, it ignored CLs not in Datastore.
      now, it inserts such CLs into PCLs with DELETED status.
    
    Rational: simplicity.
      Now UpdateConfig, OnUpdatedCLs, and newly added
      loadActiveIntoPCLs all work the same.
    
    R=yiwzhang
    
    Bug: 1156241
    Change-Id: I2e4db973929504ea2daa22aba8a1343b062a5b6b

--- cv/internal/prjmanager/impl/state/state_test.go
+++ cv/internal/prjmanager/impl/state/state_test.go
@@ -17,6 +17,7 @@ package state
 import (
 	"context"
 	"fmt"
+	"sort"
 	"strings"
 	"testing"
 
@@ -24,6 +25,7 @@ import (
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	gerritpb "go.chromium.org/luci/common/proto/gerrit"
 	"go.chromium.org/luci/gae/service/datastore"
 	"go.chromium.org/luci/server/tq/tqtesting"
 
@@ -53,8 +55,12 @@ type ctest struct {
 }
 
 func (ct ctest) runCLUpdater(ctx context.Context, change int64) *changelist.CL {
+	return ct.runCLUpdaterAs(ctx, change, ct.lProject)
+}
+
+func (ct ctest) runCLUpdaterAs(ctx context.Context, change int64, lProject string) *changelist.CL {
 	So(updater.Schedule(ctx, &updater.RefreshGerritCL{
-		LuciProject: ct.lProject,
+		LuciProject: lProject,
 		Host:        ct.gHost,
 		Change:      change,
 	}), ShouldBeNil)
@@ -549,6 +555,11 @@ func TestOnCLsUpdated(t *testing.T) {
 						Trigger:            trigger.Find(ci203),
 						Deps:               []*changelist.Dep{{Clid: int64(cl202.ID), Kind: changelist.DepKind_HARD}},
 					},
+					{
+						Clid:     404,
+						Eversion: 0,
+						Status:   internal.PCL_DELETED,
+					},
 				},
 				DirtyComponents: true,
 			})
@@ -748,6 +759,272 @@ func TestRunsCreatedAndFinished(t *testing.T) {
 	})
 }
 
+func TestLoadActiveIntoPCLs(t *testing.T) {
+	t.Parallel()
+
+	Convey("loadActiveIntoPCLs works", t, func() {
+		ct := ctest{
+			lProject: "test",
+			gHost:    "c-review.example.com",
+		}
+		ctx, cancel := ct.SetUp()
+		defer cancel()
+
+		cfg := &cfgpb.Config{}
+		So(prototext.Unmarshal([]byte(cfgText1), cfg), ShouldBeNil)
+		ct.Cfg.Create(ctx, ct.lProject, cfg)
+		meta := ct.Cfg.MustExist(ctx, ct.lProject)
+		gobmap.Update(ctx, ct.lProject)
+
+		// Simulate existence of "test-b" project watching the same Gerrit host but
+		// diff repo.
+		const lProjectB = "test-b"
+		cfgTextB := strings.ReplaceAll(cfgText1, "repo/a", "repo/b")
+		cfgB := &cfgpb.Config{}
+		So(prototext.Unmarshal([]byte(cfgTextB), cfgB), ShouldBeNil)
+		ct.Cfg.Create(ctx, lProjectB, cfgB)
+		gobmap.Update(ctx, lProjectB)
+
+		cis := make(map[int]*gerritpb.ChangeInfo, 20)
+		makeCI := func(i int, project string, cq int, extra ...gf.CIModifier) {
+			mods := []gf.CIModifier{
+				gf.Ref("refs/heads/main"),
+				gf.Project(project),
+				gf.Updated(ct.Clock.Now()),
+			}
+			if cq > 0 {
+				mods = append(mods, gf.CQ(cq, ct.Clock.Now(), gf.U("user-1")))
+			}
+			mods = append(mods, extra...)
+			cis[i] = gf.CI(i, mods...)
+			ct.GFake.CreateChange(&gf.Change{Host: ct.gHost, ACLs: gf.ACLPublic(), Info: cis[i]})
+		}
+		makeStack := func(ids []int, project string, cq int) {
+			for i, child := range ids {
+				makeCI(child, project, cq)
+				for _, parent := range ids[:i] {
+					ct.GFake.SetDependsOn(ct.gHost, cis[child], cis[parent])
+				}
+			}
+		}
+		// Simulate the following CLs state in Gerrit:
+		//   In this project:
+		//     CQ+1
+		//       1 <- 2       form a stack (2 depends on 1)
+		//       3            depends on 2 via Cq-Depend.
+		//     CQ+2
+		//       4            standalone
+		//       5 <- 6       form a stack (6 depends on 5)
+		//       7 <- 8 <- 9  form a stack (9 depends on 7,8)
+		//       13           CQ-Depend on 11 (diff project) and 12 (not existing).
+		//   In another project:
+		//     CQ+1
+		//       10 <- 11     form a stack (11 depends on 10)
+		makeStack([]int{1, 2}, "repo/a", +1)
+		makeCI(3, "repo/a", +1, gf.Desc("T\n\nCq-Depend: 2"))
+		makeStack([]int{7, 8, 9}, "repo/a", +2)
+		makeStack([]int{5, 6}, "repo/a", +2)
+		makeCI(4, "repo/a", +2)
+		makeCI(13, "repo/a", +2, gf.Desc("T\n\nCq-Depend: 11,12"))
+		makeStack([]int{10, 11}, "repo/b", +1)
+
+		// Import into DS all CLs in their respective LUCI projects.
+		// Do this in-order such that they match auto-assigned CLIDs by fake
+		// Datastore as this helps test readability. Note that importing CL 13 would
+		// create CL entity for dep #12 before creating CL 13th own entity.
+		cls := make(map[int]*changelist.CL, 20)
+		for i := 1; i < 14; i++ {
+			if i == 12 {
+				continue // skipped. will be done after 13
+			}
+			pr := ct.lProject
+			if i == 10 || i == 11 {
+				pr = lProjectB
+			}
+			cls[i] = ct.runCLUpdaterAs(ctx, int64(i), pr)
+			// If Datastore fake changes, this check and comment above can be removed,
+			// but the rest of the test will remain valid.
+			So(cls[i].ID, ShouldEqual, i)
+		}
+		// This will get 404 from Gerrit.
+		cls[12] = ct.runCLUpdater(ctx, 12)
+
+		run4 := &run.Run{
+			ID:  common.RunID(ct.lProject + "/1-a"),
+			CLs: []common.CLID{cls[4].ID},
+		}
+		run56 := &run.Run{
+			ID:  common.RunID(ct.lProject + "/56-bb"),
+			CLs: []common.CLID{cls[5].ID, cls[6].ID},
+		}
+		run789 := &run.Run{
+			ID:  common.RunID(ct.lProject + "/789-ccc"),
+			CLs: []common.CLID{cls[9].ID, cls[7].ID, cls[8].ID},
+		}
+		So(datastore.Put(ctx, run4, run56, run789), ShouldBeNil)
+
+		state := NewExisting(prjmanager.Status_STARTED, &internal.PState{
+			LuciProject:      ct.lProject,
+			ConfigHash:       meta.Hash(),
+			ConfigGroupNames: []string{"g0", "g1"},
+			DirtyComponents:  true,
+		})
+
+		Convey("just categorization", func() {
+			state.PB.Pcls = sortPCLs([]*internal.PCL{
+				defaultPCL(cls[5]),
+				defaultPCL(cls[6]),
+				defaultPCL(cls[7]),
+				defaultPCL(cls[8]),
+				defaultPCL(cls[9]),
+				{Clid: 12, Eversion: 1, Status: internal.PCL_UNKNOWN},
+			})
+			state.PB.Components = []*internal.Component{
+				{
+					Clids: i64sorted(cls[5].ID, cls[6].ID),
+					Pruns: []*internal.PRun{internal.MakePRun(run56)},
+				},
+				// Simulate 9 previously not depending on 7, 8.
+				{Clids: i64sorted(cls[7].ID, cls[8].ID)},
+				{Clids: i64s(cls[9].ID)},
+			}
+			// 789 doesn't match any 1 component, even though 7,8,9 CLs are in PCLs.
+			state.PB.CreatedPruns = []*internal.PRun{internal.MakePRun(run789)}
+			pbBefore := backupPB(state)
+
+			cat := state.categorizeCLs()
+			So(state.loadActiveIntoPCLs(ctx, cat), ShouldBeNil)
+			So(cat, ShouldResemble, &categorizedCLs{
+				active:   mkClidsSet(cls[5], cls[6], cls[7], cls[8], cls[9]),
+				deps:     clidsSet{},
+				unused:   mkClidsSet(cls[12]),
+				unloaded: clidsSet{},
+			})
+			So(state.PB, ShouldResembleProto, pbBefore)
+		})
+
+		Convey("loads unloaded dependencies and active CLs without recursion", func() {
+			state.PB.Pcls = []*internal.PCL{
+				defaultPCL(cls[3]), // depends on 2, which in turns depends on 1.
+			}
+			state.PB.CreatedPruns = []*internal.PRun{internal.MakePRun(run56)}
+			pb := backupPB(state)
+
+			cat := state.categorizeCLs()
+			So(cat, ShouldResemble, &categorizedCLs{
+				active:   mkClidsSet(cls[3], cls[5], cls[6]),
+				deps:     mkClidsSet(cls[2]),
+				unused:   clidsSet{},
+				unloaded: mkClidsSet(cls[2], cls[5], cls[6]),
+			})
+			So(state.loadActiveIntoPCLs(ctx, cat), ShouldBeNil)
+			So(cat, ShouldResemble, &categorizedCLs{
+				active:   mkClidsSet(cls[3], cls[2], cls[5], cls[6]),
+				deps:     mkClidsSet(cls[1]),
+				unused:   clidsSet{},
+				unloaded: mkClidsSet(cls[1]),
+			})
+			pb.Pcls = sortPCLs([]*internal.PCL{
+				defaultPCL(cls[2]),
+				defaultPCL(cls[3]),
+				defaultPCL(cls[5]),
+				defaultPCL(cls[6]),
+			})
+			So(state.PB, ShouldResembleProto, pb)
+		})
+
+		Convey("loads incomplete Run with unloaded deps", func() {
+			// This case shouldn't normally happen in practice. This case simulates a
+			// runStale created a while ago of just (11, 13), presumably when current
+			// project had CL #11 in scope.
+			// Now, 11 and 13 depend on 10 and 12, respectively, and 10 and 11 are no
+			// longer watched by current project.
+			runStale := &run.Run{
+				ID:  common.RunID(ct.lProject + "/111-s"),
+				CLs: []common.CLID{cls[13].ID, cls[11].ID},
+			}
+			So(datastore.Put(ctx, runStale), ShouldBeNil)
+			state.PB.CreatedPruns = []*internal.PRun{internal.MakePRun(runStale)}
+			pb := backupPB(state)
+
+			cat := state.categorizeCLs()
+			So(cat, ShouldResemble, &categorizedCLs{
+				active:   mkClidsSet(cls[11], cls[13]),
+				deps:     clidsSet{},
+				unused:   clidsSet{},
+				unloaded: mkClidsSet(cls[11], cls[13]),
+			})
+			So(state.loadActiveIntoPCLs(ctx, cat), ShouldBeNil)
+			So(cat, ShouldResemble, &categorizedCLs{
+				active: mkClidsSet(cls[11], cls[13]),
+				// 10 isn't in deps because this project has no visibility into CL 11.
+				deps:     mkClidsSet(cls[12]),
+				unused:   clidsSet{},
+				unloaded: mkClidsSet(cls[12]),
+			})
+			pb.Pcls = sortPCLs([]*internal.PCL{
+				defaultPCL(cls[13]),
+				{
+					Clid:     int64(cls[11].ID),
+					Eversion: int64(cls[11].EVersion),
+					Status:   internal.PCL_UNWATCHED,
+					Deps:     nil, // not visible to this project
+					Trigger:  nil, // not visible to this project
+				},
+			})
+			So(state.PB, ShouldResembleProto, pb)
+		})
+
+		Convey("loads incomplete Run with non-existent CLs", func() {
+			// This case shouldn't happen in practice, but it can't be ruled out.
+			// In order to incorporate just added .CreatedRun into State,
+			// Run's CLs must have PCL entries.
+			runStale := &run.Run{
+				ID:  common.RunID(ct.lProject + "/404-s"),
+				CLs: []common.CLID{cls[4].ID, 404},
+			}
+			So(datastore.Put(ctx, runStale), ShouldBeNil)
+			state.PB.CreatedPruns = []*internal.PRun{internal.MakePRun(runStale)}
+			pb := backupPB(state)
+
+			cat := state.categorizeCLs()
+			So(cat, ShouldResemble, &categorizedCLs{
+				active:   clidsSet{cls[4].ID: struct{}{}, 404: struct{}{}},
+				deps:     clidsSet{},
+				unused:   clidsSet{},
+				unloaded: clidsSet{cls[4].ID: struct{}{}, 404: struct{}{}},
+			})
+			So(state.loadActiveIntoPCLs(ctx, cat), ShouldBeNil)
+			So(cat, ShouldResemble, &categorizedCLs{
+				active:   clidsSet{cls[4].ID: struct{}{}, 404: struct{}{}},
+				deps:     clidsSet{},
+				unused:   clidsSet{},
+				unloaded: clidsSet{},
+			})
+			pb.Pcls = sortPCLs([]*internal.PCL{
+				defaultPCL(cls[4]),
+				{
+					Clid:     404,
+					Eversion: 0,
+					Status:   internal.PCL_DELETED,
+				},
+			})
+			So(state.PB, ShouldResembleProto, pb)
+		})
+
+		Convey("noop", func() {
+			cat := state.categorizeCLs()
+			So(state.loadActiveIntoPCLs(ctx, cat), ShouldBeNil)
+			So(cat, ShouldResemble, &categorizedCLs{
+				active:   clidsSet{},
+				deps:     clidsSet{},
+				unused:   clidsSet{},
+				unloaded: clidsSet{},
+			})
+		})
+	})
+}
+
 // backupPB returns a deep copy of State.PB for future assertion that State
 // wasn't modified.
 func backupPB(s *State) *internal.PState {
@@ -762,4 +1039,66 @@ func bumpEVersion(ctx context.Context, cl *changelist.CL, desired int) {
 	}
 	cl.EVersion = desired
 	So(datastore.Put(ctx, cl), ShouldBeNil)
+}
+
+func defaultPCL(cl *changelist.CL) *internal.PCL {
+	p := &internal.PCL{
+		Clid:               int64(cl.ID),
+		Eversion:           int64(cl.EVersion),
+		ConfigGroupIndexes: []int32{0},
+		Status:             internal.PCL_OK,
+		Deps:               cl.Snapshot.GetDeps(),
+	}
+	ci := cl.Snapshot.GetGerrit().GetInfo()
+	if ci != nil {
+		p.Trigger = trigger.Find(ci)
+	}
+	return p
+}
+
+func customPCL(cl *changelist.CL, override *internal.PCL) *internal.PCL {
+	p := defaultPCL(cl)
+	proto.Merge(p, override)
+	return p
+}
+
+func i64s(vs ...interface{}) []int64 {
+	res := make([]int64, len(vs))
+	for i, v := range vs {
+		switch x := v.(type) {
+		case int64:
+			res[i] = x
+		case common.CLID:
+			res[i] = int64(x)
+		case int:
+			res[i] = int64(x)
+		default:
+			panic(fmt.Errorf("unknown type: %T %v", v, v))
+		}
+	}
+	return res
+}
+
+func i64sorted(vs ...interface{}) []int64 {
+	res := i64s(vs...)
+	sort.Slice(res, func(i, j int) bool { return res[i] < res[j] })
+	return res
+}
+
+func sortPCLs(vs []*internal.PCL) []*internal.PCL {
+	sort.Slice(vs, func(i, j int) bool { return vs[i].GetClid() < vs[j].GetClid() })
+	return vs
+}
+
+func mkClidsSet(cls ...*changelist.CL) clidsSet {
+	res := make(clidsSet, len(cls))
+	for _, cl := range cls {
+		res[cl.ID] = struct{}{}
+	}
+	return res
+}
+
+func sortByFirstCL(cs []*internal.Component) []*internal.Component {
+	sort.Slice(cs, func(i, j int) bool { return cs[i].GetClids()[0] < cs[j].GetClids()[0] })
+	return cs
 }
