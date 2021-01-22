7339552cf5379a2a729cadb934bc51fa0db32528
Nodir Turakulov <nodir@chromium.org>
2021-01-21 22:07:31 -0800

    [prpc] Refactor accept.go
    Chrome always sends "Accept-Encoding: gzip, deflate, br", even if
    the JavaScript code explicitly specifies "Accept-Encoding: gzip".
    This means requiring "Accept-Encoding: gzip" in the request isn't
    enough to support JS code running in Chrome.
    
    Refactor accept.go to be suitable for Accept-Encoding parsing.
    Also simplify the code.
    
    Bug: 1166257
    Change-Id: Ibac0fc0e744bca74cdeee80b5db51576f2a08441

--- grpc/prpc/accepts_test.go
+++ grpc/prpc/accepts_test.go
@@ -26,9 +26,7 @@ func TestAccept(t *testing.T) {
 	t.Parallel()
 
 	Convey("parseAccept", t, func() {
-		type params map[string]string
-
-		test := func(value string, expectedErr interface{}, expectedTypes ...acceptType) {
+		test := func(value string, expectedErr interface{}, expectedTypes ...acceptItem) {
 			Convey("mediaType="+value, func() {
 				actual, err := parseAccept(value)
 				So(err, ShouldErrLike, expectedErr)
@@ -36,153 +34,129 @@ func TestAccept(t *testing.T) {
 			})
 		}
 		test("", nil)
-		qf1 := func(mediaType string, p params) acceptType {
-			return acceptType{
-				MediaType:       mediaType,
-				MediaTypeParams: p,
-				QualityFactor:   1.0,
+		qf1 := func(value string) acceptItem {
+			return acceptItem{
+				Value:         value,
+				QualityFactor: 1.0,
 			}
 		}
 
 		test("text/html", nil,
-			qf1("text/html", params{}),
+			qf1("text/html"),
 		)
 
 		test("text/html; level=1", nil,
-			qf1("text/html", params{"level": "1"}),
+			qf1("text/html; level=1"),
 		)
 		test("TEXT/HTML; LEVEL=1", nil,
-			qf1("text/html", params{"level": "1"}),
+			qf1("TEXT/HTML; LEVEL=1"),
 		)
 
 		test("text/html, application/json", nil,
-			qf1("text/html", params{}),
-			qf1("application/json", params{}),
+			qf1("text/html"),
+			qf1("application/json"),
 		)
 
 		test("text/html; level=1, application/json", nil,
-			qf1("text/html", params{"level": "1"}),
-			qf1("application/json", params{}),
+			qf1("text/html; level=1"),
+			qf1("application/json"),
 		)
 
 		test("text/html; level=1, application/json; foo=bar", nil,
-			qf1("text/html", params{"level": "1"}),
-			qf1("application/json", params{"foo": "bar"}),
+			qf1("text/html; level=1"),
+			qf1("application/json; foo=bar"),
 		)
 
 		test("text/html; level=1; q=0.5", nil,
-			acceptType{
-				MediaType:       "text/html",
-				MediaTypeParams: params{"level": "1"},
-				QualityFactor:   0.5,
+			acceptItem{
+				Value:         "text/html; level=1",
+				QualityFactor: 0.5,
 			},
 		)
 		test("text/html; level=1; q=0.5; a=1", nil,
-			acceptType{
-				MediaType:       "text/html",
-				MediaTypeParams: params{"level": "1"},
-				QualityFactor:   0.5,
-				// AcceptParams is not parsed.
+			acceptItem{
+				Value:         "text/html; level=1",
+				QualityFactor: 0.5,
 			},
 		)
 		test("TEXT/HTML; LEVEL=1; Q=0.5", nil,
-			acceptType{
-				MediaType:       "text/html",
-				MediaTypeParams: params{"level": "1"},
-				QualityFactor:   0.5,
+			acceptItem{
+				Value:         "TEXT/HTML; LEVEL=1",
+				QualityFactor: 0.5,
 			},
 		)
 		test("text/html; level=1; q=0.5, application/json", nil,
-			acceptType{
-				MediaType:       "text/html",
-				MediaTypeParams: params{"level": "1"},
-				QualityFactor:   0.5,
+			acceptItem{
+				Value:         "text/html; level=1",
+				QualityFactor: 0.5,
 			},
-			qf1("application/json", params{}),
+			qf1("application/json"),
 		)
 		test("text/html; level=1; q=0.5, application/json; a=b", nil,
-			acceptType{
-				MediaType:       "text/html",
-				MediaTypeParams: params{"level": "1"},
-				QualityFactor:   0.5,
+			acceptItem{
+				Value:         "text/html; level=1",
+				QualityFactor: 0.5,
 			},
-			qf1("application/json", params{"a": "b"}),
+			qf1("application/json; a=b"),
 		)
 		test("text/html; level=1; q=0.5, */*; q=0.1", nil,
-			acceptType{
-				MediaType:       "text/html",
-				MediaTypeParams: params{"level": "1"},
-				QualityFactor:   0.5,
+			acceptItem{
+				Value:         "text/html; level=1",
+				QualityFactor: 0.5,
 			},
-			acceptType{
-				MediaType:       "*/*",
-				MediaTypeParams: params{},
-				QualityFactor:   0.1,
+			acceptItem{
+				Value:         "*/*",
+				QualityFactor: 0.1,
 			},
 		)
 		test("text/html; level=1; q=0.5, */*; x=3;q=0.1; y=5", nil,
-			acceptType{
-				MediaType:       "text/html",
-				MediaTypeParams: params{"level": "1"},
-				QualityFactor:   0.5,
+			acceptItem{
+				Value:         "text/html; level=1",
+				QualityFactor: 0.5,
 			},
-			acceptType{
-				MediaType:       "*/*",
-				MediaTypeParams: params{"x": "3"},
-				QualityFactor:   0.1,
-				// AcceptType is not parsed.
+			acceptItem{
+				Value:         "*/*; x=3",
+				QualityFactor: 0.1,
 			},
 		)
-
-		test("text/ html", "expected token after slash")
-		test("text / html", "expected slash after first token")
-		test("text/html, ", "no media type")
-		test("text/html;;;;", "invalid media parameter")
 		test("text/html;q=q", "q parameter: expected a floating-point number")
-		test("text/html,a=q", "expected slash after first token")
 	})
 
 	Convey("qParamSplit", t, func() {
 		type params map[string]string
 
-		test := func(value, mediaType, qValue, acceptParams string) {
-			Convey("value="+value, func() {
-				actualMediaType, actualQValue, actualAcceptParams := qParamSplit(value)
+		test := func(item, mediaType, qValue string) {
+			Convey(item, func() {
+				actualMediaType, actualQValue := qParamSplit(item)
 				So(actualMediaType, ShouldEqual, mediaType)
 				So(actualQValue, ShouldEqual, qValue)
-				So(actualAcceptParams, ShouldEqual, acceptParams)
 			})
 		}
 
-		test("", "", "", "")
-		test("a/b", "a/b", "", "")
+		test("", "", "")
+		test("a/b", "a/b", "")
 
-		test("a/b;mtp1=1", "a/b;mtp1=1", "", "")
-		test("a/b;q=1", "a/b", "1", "")
-		test("a/b; q=1", "a/b", "1", "")
-		test("a/b;mtp1=1;q=1", "a/b;mtp1=1", "1", "")
-		test("a/b;mtp1=1;q=1;", "a/b;mtp1=1", "1", "")
-		test("a/b;mtp1=1; q=1", "a/b;mtp1=1", "1", "")
-		test("a/b;mtp1=1;q =1", "a/b;mtp1=1", "1", "")
-		test("a/b;mtp1=1;q  =1", "a/b;mtp1=1", "1", "")
-		test("a/b;mtp1=1;q= 1", "a/b;mtp1=1", "1", "")
-		test("a/b;mtp1=1;q=1 ", "a/b;mtp1=1", "1", "")
+		test("a/b;mtp1=1", "a/b;mtp1=1", "")
+		test("a/b;q=1", "a/b", "1")
+		test("a/b; q=1", "a/b", "1")
+		test("a/b;mtp1=1;q=1", "a/b;mtp1=1", "1")
+		test("a/b;mtp1=1;q=1;", "a/b;mtp1=1", "1")
+		test("a/b;mtp1=1; q=1", "a/b;mtp1=1", "1")
+		test("a/b;mtp1=1;q =1", "a/b;mtp1=1", "1")
+		test("a/b;mtp1=1;q  =1", "a/b;mtp1=1", "1")
+		test("a/b;mtp1=1;q= 1", "a/b;mtp1=1", "1")
+		test("a/b;mtp1=1;q=1 ", "a/b;mtp1=1", "1")
 
-		test("a/b;mtp1=1;Q=1", "a/b;mtp1=1", "1", "")
+		test("a/b;mtp1=1;Q=1", "a/b;mtp1=1", "1")
 
-		test("a/b;mtp1=1;q=1.0", "a/b;mtp1=1", "1.0", "")
-		test("a/b;mtp1=1;q=1.0;", "a/b;mtp1=1", "1.0", "")
+		test("a/b;mtp1=1;q=1.0", "a/b;mtp1=1", "1.0")
+		test("a/b;mtp1=1;q=1.0;", "a/b;mtp1=1", "1.0")
 
-		test("a/b;mtp1=1;q=1;at1=1", "a/b;mtp1=1", "1", "at1=1")
-		test("a/b;mtp1=1;q=1; at1=1", "a/b;mtp1=1", "1", "at1=1")
-		test("a/b;mtp1=1;q=1;at1=1;", "a/b;mtp1=1", "1", "at1=1;")
-		test("a/b;mtp1=1;  q  =  1.0  ;at1=1;", "a/b;mtp1=1", "1.0", "at1=1;")
+		test("a/b;mtp1=1;  q  =  foo", "a/b;mtp1=1", "foo")
 
-		test("a/b;mtp1=1;  q  =  foo", "a/b;mtp1=1", "foo", "")
-
-		test("a/b;c=q", "a/b;c=q", "", "")
-		test("a/b;mtp1=1;  q", "a/b;mtp1=1;  q", "", "")
-		test("a/b;mtp1=1;  q=", "a/b;mtp1=1;  q=", "", "")
+		test("a/b;c=q", "a/b;c=q", "")
+		test("a/b;mtp1=1;  q", "a/b;mtp1=1;  q", "")
+		test("a/b;mtp1=1;  q=", "a/b;mtp1=1;  q=", "")
 	})
 }
 
