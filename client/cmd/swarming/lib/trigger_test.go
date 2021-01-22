f8e4e231ef274181659939a661aa23b04b2ef9e8
Takuto Ikuta <tikuta@chromium.org>
2021-01-22 18:35:58 +0900

    swarming: remove deprecated raw-cmd flag
    Change-Id: I7cbe7ac91e6b37128f77096baf095f1e39f16b2b

--- client/cmd/swarming/lib/trigger_test.go
+++ client/cmd/swarming/lib/trigger_test.go
@@ -242,7 +242,6 @@ func TestTriggerParse_RawNoArgs(t *testing.T) {
 			"-server", "http://localhost:9050",
 			"-dimension", "os=Ubuntu",
 			"-isolated", "0123456789012345678901234567890123456789",
-			"-raw-cmd",
 		})
 
 		err = c.Parse([]string(nil))
@@ -259,7 +258,6 @@ func TestTriggerParse_RawArgs(t *testing.T) {
 			"-server", "http://localhost:9050",
 			"-dimension", "os=Ubuntu",
 			"-isolated", "0123456789012345678901234567890123456789",
-			"-raw-cmd",
 		})
 
 		err = c.Parse([]string{"arg1", "arg2"})
