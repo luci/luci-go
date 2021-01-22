22d54e017ff10e0e0dc538f11e908b56d39fd158
Yuanjun Huang <yuanjunh@google.com>
2021-01-22 20:57:09 +0000

    [buildbucket] Enable 1% traffic for Go Batch in Prod
    Bug:1144958
    Change-Id: If718aa9cdc506320a92a33e4f940b2b20c4b28c4
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2645242
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Commit-Queue: Yuanjun Huang <yuanjunh@google.com>

--- buildbucket/appengine/frontend/main.go
+++ buildbucket/appengine/frontend/main.go
@@ -125,7 +125,7 @@ func main() {
 		srv.PRPC.HackFixFieldMasksForJSON = true
 
 		// makeOverride(prod % -> Go, dev % -> Go).
-		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "Batch", makeOverride(0, 50))
+		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "Batch", makeOverride(1, 50))
 		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "CancelBuild", makeOverride(0, 0))
 		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "ScheduleBuild", makeOverride(0, 0))
 		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "UpdateBuild", makeOverride(0, 0))
