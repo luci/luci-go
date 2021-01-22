90434c8306d238f3c64dea68c837bb3b8306f321
Andrii Shyshkalov <tandrii@google.com>
2021-01-22 03:06:31 +0100

    cv: implement diagnostic GetCL API.
    Regexp is adapted from `bb`:
      https://source.chromium.org/chromium/infra/infra/+/master:go/src/go.chromium.org/luci/buildbucket/cli/cl.go?q=buildbucket%2Fcli%2Fcl.go&ss=chromium
    Unlike `bb`, this CL accepts "https://host/change" URL.
    
    Because the matching code uses m[$largeIndex], I've added a quick test
    for sanity. Note however that this isn't a production code, so I don't
    particular care if some edge case isn't covered.
    
    R=yiwzhang
    
    Bug: 1169206
    Change-Id: I2b1b1208bc70427db43e3454c52b6c5f4adfcea7

---
+++ cv/internal/diagnostic/server_test.go
@@ -0,0 +1,47 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package diagnostic
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestParseGerritURL(t *testing.T) {
+	t.Parallel()
+
+	Convey("parseGerritURL works", t, func() {
+		eid, err := parseGerritURL("https://crrev.com/i/12/34")
+		So(err, ShouldBeNil)
+		So(string(eid), ShouldEqual, "gerrit/chrome-internal-review.googlesource.com/12")
+
+		eid, err = parseGerritURL("https://crrev.com/c/12")
+		So(err, ShouldBeNil)
+		So(string(eid), ShouldEqual, "gerrit/chromium-review.googlesource.com/12")
+
+		eid, err = parseGerritURL("https://chromium-review.googlesource.com/1541677")
+		So(err, ShouldBeNil)
+		So(string(eid), ShouldEqual, "gerrit/chromium-review.googlesource.com/1541677")
+
+		eid, err = parseGerritURL("https://pdfium-review.googlesource.com/c/33")
+		So(err, ShouldBeNil)
+		So(string(eid), ShouldEqual, "gerrit/pdfium-review.googlesource.com/33")
+
+		eid, err = parseGerritURL("https://chromium-review.googlesource.com/#/c/infra/luci/luci-go/+/1541677/7")
+		So(err, ShouldBeNil)
+		So(string(eid), ShouldEqual, "gerrit/chromium-review.googlesource.com/1541677")
+	})
+}
