8db41114f010d4c4c4122f9597bc528f48336728
Yuanjun Huang <yuanjunh@google.com>
2021-01-26 14:07:36 -0800

    [buildbucket] Remove occurence of non-inclusive words
    Bug:1117911
    Change-Id: Iaa03ffc7dd99a3ac859dfad1c64eb77eb79f1f50

--- buildbucket/proto/builds_service.pb.go
+++ buildbucket/proto/builds_service.pb.go
@@ -1946,8 +1946,8 @@ type BuildsClient interface {
 	//
 	// Buildbot: if the specified build is a buildbot build, converts it to Build
 	// message with the following rules:
-	// * bucket names are full, e.g. "master.chromium". Note that LUCI buckets
-	//   in v2 are shortened, e.g. "ci".
+	// * bucket names are full, e.g. "luci.infra.try". Note that LUCI buckets
+	//   in v2 are shortened, e.g. "try".
 	// * if a v2 Build field does not make sense in V1, it is unset/empty.
 	// * step support is not implemented for Buildbot builds.
 	// Note that it does not support getting a buildbot build by build number.
@@ -1957,18 +1957,7 @@ type BuildsClient interface {
 	// Buildbot: can return Buildbot builds, see GetBuild for conversion rules.
 	// For example, response may include a mix of LUCI and Buildbot builds if the
 	// predicate is a CL.
-	// Cannot search in a buildbot bucket or buildbot builder, e.g.
-	// {
-	//   "predicate": {
-	//     "builder": {
-	//       "project": "chromium",
-	//       "bucket": "master.chromium",
-	//       "builder": "linux-rel"
-	//     }
-	//   }
-	// }
-	// will look for builds in "master.chromium" LUCI bucket which probably does
-	// not exist.
+	// Cannot search in a buildbot bucket or builder via predicate.builder field.
 	SearchBuilds(ctx context.Context, in *SearchBuildsRequest, opts ...grpc.CallOption) (*SearchBuildsResponse, error)
 	// Updates a build. Returns an empty build.
 	//
@@ -2121,8 +2110,8 @@ type BuildsServer interface {
 	//
 	// Buildbot: if the specified build is a buildbot build, converts it to Build
 	// message with the following rules:
-	// * bucket names are full, e.g. "master.chromium". Note that LUCI buckets
-	//   in v2 are shortened, e.g. "ci".
+	// * bucket names are full, e.g. "luci.infra.try". Note that LUCI buckets
+	//   in v2 are shortened, e.g. "try".
 	// * if a v2 Build field does not make sense in V1, it is unset/empty.
 	// * step support is not implemented for Buildbot builds.
 	// Note that it does not support getting a buildbot build by build number.
@@ -2132,18 +2121,7 @@ type BuildsServer interface {
 	// Buildbot: can return Buildbot builds, see GetBuild for conversion rules.
 	// For example, response may include a mix of LUCI and Buildbot builds if the
 	// predicate is a CL.
-	// Cannot search in a buildbot bucket or buildbot builder, e.g.
-	// {
-	//   "predicate": {
-	//     "builder": {
-	//       "project": "chromium",
-	//       "bucket": "master.chromium",
-	//       "builder": "linux-rel"
-	//     }
-	//   }
-	// }
-	// will look for builds in "master.chromium" LUCI bucket which probably does
-	// not exist.
+	// Cannot search in a buildbot bucket or builder via predicate.builder field.
 	SearchBuilds(context.Context, *SearchBuildsRequest) (*SearchBuildsResponse, error)
 	// Updates a build. Returns an empty build.
 	//
