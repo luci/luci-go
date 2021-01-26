b1dab7fb6520208534cae9bf787a588f778c84f9
Yiwei Zhang <yiwzhang@google.com>
2021-01-25 21:49:02 -0800

    [cv] Introduce ProcessAfter for event processing
    Also implemented RunManager re-enqueue a task to poke itself after
    5 minutes using this feature.
    
    R=tandrii
    
    Bug: 1168615
    Change-Id: Icfd109234c1cb58928b8ef3a3998252e62cb2cea

--- cv/internal/run/runtest/runtest.go
+++ cv/internal/run/runtest/runtest.go
@@ -16,12 +16,20 @@
 package runtest
 
 import (
+	"context"
 	"sort"
 
+	"google.golang.org/protobuf/proto"
+
+	"go.chromium.org/luci/gae/service/datastore"
 	"go.chromium.org/luci/server/tq/tqtesting"
 
 	"go.chromium.org/luci/cv/internal/common"
+	"go.chromium.org/luci/cv/internal/eventbox"
+	"go.chromium.org/luci/cv/internal/run"
 	"go.chromium.org/luci/cv/internal/run/eventpb"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 // Runs returns list of runs from tasks for Run Manager.
@@ -42,4 +50,69 @@ func SortedRuns(in tqtesting.TaskList) common.RunIDs {
 	runs := Runs(in)
 	sort.Sort(runs)
 	return runs
+}
+
+// Tasks returns all Run tasks sorted by ETA and then task class name.
+func Tasks(in tqtesting.TaskList) []proto.Message {
+	tasks := in[:]
+	sort.SliceStable(tasks, func(i, j int) bool {
+		l, r := tasks[i], tasks[j]
+		if l.ETA.Equal(r.ETA) {
+			return l.Class < r.Class
+		}
+		return l.ETA.Before(r.ETA)
+	})
+
+	ret := make([]proto.Message, 0, len(tasks))
+	for _, t := range tasks {
+		switch v := t.Payload.(type) {
+		case *eventpb.PokeRunTask, *eventpb.KickPokeRunTask:
+			ret = append(ret, v)
+		}
+	}
+	return ret
+}
+
+func iterEventBox(ctx context.Context, runID common.RunID, cb func(*eventpb.Event)) {
+	runKey := datastore.MakeKey(ctx, run.RunKind, string(runID))
+	events, err := eventbox.List(ctx, runKey)
+	So(err, ShouldBeNil)
+	for _, item := range events {
+		evt := &eventpb.Event{}
+		So(proto.Unmarshal(item.Value, evt), ShouldBeNil)
+		cb(evt)
+	}
+}
+
+// AssertNotInEventbox asserts none of the target events exists in the Eventbox.
+func AssertNotInEventbox(ctx context.Context, runID common.RunID, targets ...*eventpb.Event) {
+	var hits []*eventpb.Event
+	iterEventBox(ctx, runID, func(evt *eventpb.Event) {
+		for i, target := range targets {
+			if proto.Equal(evt, target) {
+				hits = append(hits, target)
+				targets[i] = targets[len(targets)-1]
+				targets[len(targets)-1] = nil
+				targets = targets[:len(targets)-1]
+				return
+			}
+		}
+	})
+	So(hits, ShouldBeEmpty)
+}
+
+// AssertInEventbox asserts all target events exist in the Eventbox.
+func AssertInEventbox(ctx context.Context, runID common.RunID, targets ...*eventpb.Event) {
+	targets = targets[:]
+	iterEventBox(ctx, runID, func(evt *eventpb.Event) {
+		for i, target := range targets {
+			if proto.Equal(evt, target) {
+				targets[i] = targets[len(targets)-1]
+				targets[len(targets)-1] = nil
+				targets = targets[:len(targets)-1]
+				return
+			}
+		}
+	})
+	So(targets, ShouldBeEmpty)
 }
