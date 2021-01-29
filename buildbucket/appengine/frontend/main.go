af320029ea6ebb44d5327880068d7ed6a9cc5eca
Yuanjun Huang <yuanjunh@google.com>
2021-01-28 16:59:06 -0800

    Remove the traffic override method for Batch
    Bug:1144958
    Change-Id: I464306f43bec7c16f17f8a37c12d9f2a5563e494

--- buildbucket/appengine/frontend/main.go
+++ buildbucket/appengine/frontend/main.go
@@ -125,7 +125,6 @@ func main() {
 		srv.PRPC.HackFixFieldMasksForJSON = true
 
 		// makeOverride(prod % -> Go, dev % -> Go).
-		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "Batch", makeOverride(100, 100))
 		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "CancelBuild", makeOverride(0, 0))
 		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "ScheduleBuild", makeOverride(0, 0))
 		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "UpdateBuild", makeOverride(0, 0))
