12dda29eff59a3ee61d53c8ef87789f9aa73f730
Chan Li <chanli@chromium.org>
2021-01-29 21:57:24 +0000

    [ResultDB] Remove deriver code
    Bug: 1030191
    Change-Id: I8fe9dde618aaa66128741ca1ddc2a731a876380f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2648267
    Commit-Queue: Chan Li <chanli@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- resultdb/internal/services/resultdb/resultdb.go
+++ resultdb/internal/services/resultdb/resultdb.go
@@ -99,7 +99,6 @@ func InitServer(srv *server.Server, opts Options) error {
 	// dynamically fetches discovery documents from other deployments and
 	// returns their union.
 	pb.RegisterRecorderServer(srv.PRPC, nil)
-	pb.RegisterDeriverServer(srv.PRPC, nil)
 
 	srv.PRPC.AccessControl = prpc.AllowOriginAll
 	// TODO(crbug/1082369): Remove this workaround once field masks can be decoded.
--- resultdb/proto/v1/gen.go
+++ resultdb/proto/v1/gen.go
@@ -17,8 +17,6 @@ package resultpb
 //go:generate cproto
 //go:generate svcdec -type RecorderServer
 //go:generate svcdec -type ResultDBServer
-//go:generate svcdec -type DeriverServer
 //go:generate mockgen -source resultdb.pb.go -destination resultdb.mock.pb.go -package resultpb -write_package_comment=false
 //go:generate mockgen -source recorder.pb.go -destination recorder.mock.pb.go -package resultpb -write_package_comment=false
-//go:generate mockgen -source deriver.pb.go -destination deriver.mock.pb.go -package resultpb -write_package_comment=false
-//go:generate goimports -w resultdb.mock.pb.go recorder.mock.pb.go deriver.mock.pb.go
+//go:generate goimports -w resultdb.mock.pb.go recorder.mock.pb.go
