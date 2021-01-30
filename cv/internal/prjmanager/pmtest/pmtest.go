d524d6dba6ea76e004ed31b1cbb21b50101b2f02
Yiwei Zhang <yiwzhang@google.com>
2021-01-29 20:34:10 -0800

    [cv] Notify ProjectManager on Run Cancellation
    R=qyearsley, tandrii
    
    Bug: 1168619
    Change-Id: I8e8968c030f8fccb9519668750705168f1840b72

--- cv/internal/prjmanager/pmtest/pmtest.go
+++ cv/internal/prjmanager/pmtest/pmtest.go
@@ -16,8 +16,19 @@
 package pmtest
 
 import (
-	"go.chromium.org/luci/cv/internal/prjmanager/prjpb"
+	"context"
+
+	"google.golang.org/protobuf/proto"
+
+	"go.chromium.org/luci/gae/service/datastore"
 	"go.chromium.org/luci/server/tq/tqtesting"
+
+	"go.chromium.org/luci/cv/internal/common"
+	"go.chromium.org/luci/cv/internal/eventbox"
+	"go.chromium.org/luci/cv/internal/prjmanager"
+	"go.chromium.org/luci/cv/internal/prjmanager/prjpb"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 // Projects returns list of projects from tasks for PM.
@@ -31,4 +42,57 @@ func Projects(in tqtesting.TaskList) (projects []string) {
 		}
 	}
 	return projects
+}
+
+func iterEventBox(ctx context.Context, project string, cb func(*prjpb.Event)) {
+	projKey := datastore.MakeKey(ctx, prjmanager.ProjectKind, project)
+	events, err := eventbox.List(ctx, projKey)
+	So(err, ShouldBeNil)
+	for _, item := range events {
+		evt := &prjpb.Event{}
+		So(proto.Unmarshal(item.Value, evt), ShouldBeNil)
+		cb(evt)
+	}
+}
+
+func matchEventBox(ctx context.Context, project string, targets []*prjpb.Event) (matched, remaining []*prjpb.Event) {
+	remaining = make([]*prjpb.Event, len(targets))
+	copy(remaining, targets)
+	iterEventBox(ctx, project, func(evt *prjpb.Event) {
+		for i, r := range remaining {
+			if proto.Equal(evt, r) {
+				matched = append(matched, r)
+				remaining[i] = remaining[len(remaining)-1]
+				remaining[len(remaining)-1] = nil
+				remaining = remaining[:len(remaining)-1]
+				return
+			}
+		}
+	})
+	return
+}
+
+// AssertNotInEventbox asserts none of the events exists in the project
+// Eventbox.
+func AssertNotInEventbox(ctx context.Context, project string, targets ...*prjpb.Event) {
+	matched, _ := matchEventBox(ctx, project, targets)
+	So(matched, ShouldBeEmpty)
+}
+
+// AssertInEventbox asserts all events exist in the project Eventbox.
+func AssertInEventbox(ctx context.Context, project string, targets ...*prjpb.Event) {
+	_, remaining := matchEventBox(ctx, project, targets)
+	So(remaining, ShouldBeEmpty)
+}
+
+// AssertReceivedRunFinished asserts a RunFinished event has been delivered
+// tor project's eventbox for the given Run.
+func AssertReceivedRunFinished(ctx context.Context, runID common.RunID) {
+	AssertInEventbox(ctx, runID.LUCIProject(), &prjpb.Event{
+		Event: &prjpb.Event_RunFinished{
+			RunFinished: &prjpb.RunFinished{
+				RunId: string(runID),
+			},
+		},
+	})
 }
