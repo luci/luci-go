b3c96ddef578e0a0a96b2240462bb6768d14d97b
Junji Watanabe <jwata@google.com>
2021-02-01 15:28:31 +0900

    remove dependency on luci-auth from swarming/lib
    Change-Id: Ic4ae613e02f3773d78f3a0a122d7ddb5eb5142e2

--- client/cmd/swarming/lib/tasks.go
+++ client/cmd/swarming/lib/tasks.go
@@ -25,21 +25,21 @@ import (
 
 	"github.com/maruel/subcommands"
 
-	"go.chromium.org/luci/auth"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/flag"
 	"go.chromium.org/luci/common/system/signals"
 )
 
 // CmdTasks returns an object for the `tasks` subcommand.
-func CmdTasks(defaultAuthOpts auth.Options) *subcommands.Command {
+func CmdTasks(authFlgas AuthFlags) *subcommands.Command {
 	return &subcommands.Command{
 		UsageLine: "tasks <options>",
 		ShortDesc: "lists tasks",
 		LongDesc:  "List tasks matching the given options.",
 		CommandRun: func() subcommands.CommandRun {
 			r := &tasksRun{}
-			r.Init(defaultAuthOpts)
+			r.commonFlags.Init(authFlgas)
+			r.Init()
 			return r
 		},
 	}
@@ -54,9 +54,7 @@ type tasksRun struct {
 	fields  []googleapi.Field
 }
 
-func (t *tasksRun) Init(defaultAuthOpts auth.Options) {
-	t.commonFlags.Init(defaultAuthOpts)
-
+func (t *tasksRun) Init() {
 	t.Flags.StringVar(&t.outfile, "json", "", "Path to output JSON results. Implies quiet.")
 	t.Flags.Int64Var(&t.limit, "limit", 200, "Maximum number of tasks to retrieve.")
 	t.Flags.StringVar(&t.state, "state", "ALL", "Only include tasks in the specified state.")
