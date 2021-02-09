6b9d6ed6cfa0d9080ac67148a3ce12a41e420351
Yuanjun Huang <yuanjunh@google.com>
2021-02-09 00:22:09 +0000

    [buildbucket] Propagate retryable RPC errors for Batch
    Shouldn’t wrap up Py service errors into BatchResponse_Response_Error.
    Instead, all errors should be made top level error such that
    naive callers can retry it, notably `bb` cli which doesn't retry
    individual ops inside a batch.
    
    Bug: 1174310
    Change-Id: Iabb42bd1cb903e099ad2f489d815252ae54b176a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2677113
    Commit-Queue: Yuanjun Huang <yuanjunh@google.com>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

--- buildbucket/appengine/rpc/batch_test.go
+++ buildbucket/appengine/rpc/batch_test.go
@@ -21,6 +21,8 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/golang/protobuf/jsonpb"
 	spb "google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/grpc/codes"
+	grpcStatus "google.golang.org/grpc/status"
 
 	"go.chromium.org/luci/common/testing/mock"
 	"go.chromium.org/luci/gae/impl/memory"
@@ -332,6 +334,50 @@ func TestBatch(t *testing.T) {
 			}
 			So(err, ShouldBeNil)
 			So(actualRes, ShouldResembleProto, expectedRes)
+		})
+
+		Convey("py service error", func() {
+			req := &pb.BatchRequest{}
+			err := jsonpb.UnmarshalString(`{
+				"requests": [
+					{"cancelBuild": {}}
+				]
+			}`, req)
+			So(err, ShouldBeNil)
+			mockPyBBClient.EXPECT().Batch(ctx, mock.EqProto(req)).Return(nil, grpcStatus.Error(codes.Unavailable, "unavailable"))
+			actualRes, err := srv.Batch(ctx, req)
+			So(actualRes, ShouldBeNil)
+			So(err, ShouldErrLike, "rpc error: code = Unavailable desc = unavailable")
+		})
+
+		Convey("py timout error", func() {
+			req := &pb.BatchRequest{}
+			err := jsonpb.UnmarshalString(`{
+				"requests": [
+					{"cancelBuild": {}}
+				]
+			}`, req)
+			So(err, ShouldBeNil)
+			mockPyBBClient.EXPECT().Batch(ctx, mock.EqProto(req)).Return(nil, grpcStatus.Error(codes.DeadlineExceeded, "timeout"))
+			actualRes, err := srv.Batch(ctx, req)
+			So(actualRes, ShouldBeNil)
+			So(err, ShouldErrLike, "rpc error: code = Internal desc = timeout")
+		})
+
+		Convey("transport error", func() {
+			ctx := memory.Use(context.Background())
+			ctx = context.WithValue(ctx, &testFakeTransportError, grpcStatus.Error(codes.Internal, "failed to get Py BB RPC transport"))
+			srv := &Builds{}
+			req := &pb.BatchRequest{}
+			err := jsonpb.UnmarshalString(`{
+				"requests": [
+					{"scheduleBuild": {}}
+				]
+			}`, req)
+			So(err, ShouldBeNil)
+			actualRes, err := srv.Batch(ctx, req)
+			So(actualRes, ShouldBeNil)
+			So(err, ShouldErrLike, "code = Internal desc = failed to get Py BB RPC transport")
 		})
 	})
 }
