21bce947668889ff446e467db8f777037e2c4f5e
Joey Scarr <jsca@google.com>
2021-02-09 17:19:50 +1100

    [resultui] Show all steps by default if there are no failing steps.
    This change only affects the steps tab.
    
    Change-Id: I9af6b9ce283139c6948c5de8899864226d080cff

--- milo/frontend/resultui/src/pages/build_page/index.ts
+++ milo/frontend/resultui/src/pages/build_page/index.ts
@@ -115,6 +115,12 @@ export class BuildPageElement extends MobxLitElement implements BeforeEnterObser
         ?.slice('invocations/'.length) || '';
       this.invocationState.initialized = true;
 
+      // If the build has only succeeded steps, show all steps in the steps tab by
+      // default (even if the user's preference is to hide succeeded steps).
+      if (build.rootSteps.every((s) => s.status === BuildStatus.Success)) {
+        this.configsStore.userConfigs.steps.showSucceededSteps = true;
+      }
+
       // If the input gitiles commit is in the blamelist pins, select it.
       // Otherwise, select the first blamelist pin.
       const buildInputCommitRepo = build.input.gitilesCommit
