db580f3e3418a558bde86178c3791531bd06baa0
Quinten Yearsley <qyearsley@chromium.org>
2021-02-10 11:53:37 -0800

    Add cv/internal/bq for BQ-row-sending code
    Bug: 1173168
    Change-Id: I5cd1df22a9cb00dbbaef07e627c60802f133b431

---
+++ cv/internal/bq/bq.go
@@ -0,0 +1,515 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package bq handles sending rows to BigQuery.
+
+package bq
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.chromium.org/luci/gae/service/datastore"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/retry/transient"
+)
+
+// batchSize is total number of items to pass to PutMulti or DeleteMulti RPCs.
+const batchSize = 500
+
+// Set holds a set of Items and uses tombstones to achieve idempotency of Add.
+//
+// Producers just call Add(...).
+//
+// The consumer must run more elaborate algorithm that ensures atomicity of
+// 'Pop' and takes care of cleaning up of the garbage. This requires a mix of
+// transactional and non-transactional actions:
+//
+//   listing, err := set.List(ctx)
+//   if err != nil {
+//     return err
+//   }
+//
+//   if err := dsset.CleanupGarbage(ctx, listing.Garbage); err != nil {
+//     return err
+//   }
+//
+//   ... Fetch any additional info associated with 'listing.Items' ...
+//
+//   err = datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+//     op, err := set.BeginPop(ctx, listing)
+//     if err != nil {
+//       return err
+//     }
+//     for _, itm := range listing.items {
+//       if op.Pop(item.ID) {
+//         // The item was indeed in the set and we've just removed it!
+//       } else {
+//         // Some other transaction has popped it already.
+//       }
+//     }
+//     return dsset.FinishPop(ctx, op)
+//   }, nil)
+//   return err
+type Set struct {
+	// Parent points to the datastore owning the set.
+	//
+	// Set's Datastore entities will be placed with this parent.
+	Parent *datastore.Key
+	// TombstonesDelay is how long to keep tombstones in the set.
+	TombstonesDelay time.Duration
+}
+
+// Item is what's stored in the set.
+type Item struct {
+	ID    string // unique in time identifier of the item
+	Value []byte // arbitrary value (<1 MB, but preferably much smaller)
+}
+
+// Garbage is a list of tombstones to cleanup.
+type Garbage []*tombstone
+
+// Listing is returned by 'List' call.
+//
+// It contains actual listing of items in the set, as well as a bunch of service
+// information used by other operations ('CleanupGarbage' and 'Pop') to keep
+// the set in a garbage-free and consistent state.
+//
+// The only way to construct a correct Listing is to call 'List' method.
+//
+// See comments for Set struct and List method for more info.
+type Listing struct {
+	Items   []Item  // all items in the set, in arbitrary order
+	Garbage Garbage // tombstones that can be cleaned up now
+
+	parent     *datastore.Key            // set's parent.
+	producedAt time.Time                 // when 'List' call was initiated
+	idToKey    map[string]*datastore.Key // ID -> datastore key to cleanup
+}
+
+// tombstone is a reference to a deleted item that still lingers in the set.
+//
+// Tombstones exist to make sure recently popped items do not reappear in the
+// set if producers attempt to re-add them.
+type tombstone struct {
+	id        string         // deleted item ID
+	storage   *datastore.Key // itemEntity to delete in 'CleanupGarbage'
+	old       bool           // true if tombstone should be popped in 'Pop'
+	cleanedUp bool           // true if 'CleanupGarbage' processed the tombstone
+}
+
+// Add idempotently adds a bunch of items to the set.
+//
+// If items with given keys are already in the set, or have been deleted
+// recently, they won't be re-added. No error is returned in this case. When
+// retrying the call like that, the caller is responsible to pass exact same
+// Item.Value, otherwise 'List' may return random variant of the added item.
+//
+// If called outside of a transaction and the call fails, may add only some
+// subset of items. Running inside a transaction makes this operation atomic.
+//
+// Returns only transient errors.
+func (s *Set) Add(c context.Context, items []Item) error {
+	// If added items have been popped already (they have tombstones), 'List' will
+	// omit them as well.
+	entities := make([]itemEntity, len(items))
+	for i, itm := range items {
+		entities[i] = itemEntity{
+			ID:     itm.ID,
+			Parent: s.Parent,
+			Value:  itm.Value,
+		}
+	}
+	return transient.Tag.Apply(batchOp(len(entities), func(start, end int) error {
+		return datastore.Put(c, entities[start:end])
+	}))
+}
+
+// List returns all items that are currently in the set (in arbitrary order),
+// as well as a set of tombstones that points to items that were previously
+// popped and can be cleaned up now.
+//
+// Must be called outside of transactions (panics otherwise).
+//
+// The set of tombstones to cleanup should be passed to 'CleanupGarbage', and
+// later to 'BeginPop' (via Listing), in that order. Not doing so will lead to
+// accumulation of a garbage in the set that will slow down 'List' and 'Pop'.
+//
+// Returns only transient errors.
+func (s *Set) List(ctx context.Context) (*Listing, error) {
+	if datastore.CurrentTransaction(ctx) != nil {
+		panic("dsset.Set.List must be called outside of a transaction")
+	}
+	now := clock.Now(ctx).UTC()
+
+	// Fetch all items and all tombstones.
+	tombsEntity := tombstonesEntity{Parent: s.Parent}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		err := datastore.Get(ctx, &tombsEntity)
+		if err != nil && err != datastore.ErrNoSuchEntity {
+			return err
+		}
+		return nil
+	})
+
+	var entities []*itemEntity
+	eg.Go(func() error {
+		q := datastore.NewQuery("dsset.Item").Ancestor(s.Parent)
+		return datastore.GetAll(ctx, q, &entities)
+	})
+	if err := eg.Wait(); err != nil {
+		return nil, transient.Tag.Apply(err)
+	}
+
+	// Mapping "item ID" => "entity to delete to remove it". This is eventually
+	// used by 'CleanupGarbage'.
+	idToKey := map[string]*datastore.Key{}
+	for _, e := range entities {
+		idToKey[e.ID] = datastore.KeyForObj(ctx, e)
+	}
+
+	// A set of items we pretend not to see. Initially all tombstoned ones.
+	//
+	// Since we are iterating over tombstone list anyway, find all sufficiently
+	// old tombstones or tombstones that still have storage associated with them.
+	// We return them to the caller, so they can be cleaned up:
+	//   * 'CleanupGarbage' makes sure 'storage' entities are deleted.
+	//   * 'BeginPop' completely erases old tombstones.
+	var tombs Garbage
+	ignore := stringset.New(len(tombsEntity.Tombstones))
+	for _, t := range tombsEntity.Tombstones {
+		ignore.Add(t.ID)
+		old := now.Sub(t.Tombstoned) > s.TombstonesDelay
+		if storage, ok := idToKey[t.ID]; ok || old {
+			tombs = append(tombs, &tombstone{
+				id:      t.ID,
+				storage: storage,
+				old:     old, // if true, BeginPop will delete this tombstone
+			})
+		}
+	}
+
+	// Throw away tombstoned items.
+	var items []Item
+	for _, e := range entities {
+		if !ignore.Has(e.ID) {
+			items = append(items, Item{
+				ID:    e.ID,
+				Value: e.Value,
+			})
+			ignore.Add(e.ID)
+		}
+	}
+
+	return &Listing{
+		Items:      items,
+		Garbage:    tombs,
+		parent:     s.Parent,
+		producedAt: now,
+		idToKey:    idToKey,
+	}, nil
+}
+
+// PopOp is an in-progress 'Pop' operation.
+//
+// See BeginPop.
+type PopOp struct {
+	ctx      context.Context           // datastore context to use for this op
+	txn      datastore.Transaction     // a transaction that started BeginPop
+	now      time.Time                 // popping time for all popped items
+	dirty    bool                      // true if the tombstone map was modified
+	finished bool                      // true if finished already
+	entity   *tombstonesEntity         // entity with tombstones
+	tombs    map[string]time.Time      // entity.Tombstones in a map form
+	idToKey  map[string]*datastore.Key // ID -> datastore key to cleanup
+	popped   Garbage                   // new tombstones for popped items
+}
+
+// BeginPop initiates 'Pop' operation.
+//
+// Pop operation is used to transactionally remove items from the set, as well
+// as cleanup old tombstones. It must be finished with 'dsset.FinishPop', even
+// if no items have been popped: the internal state still can change in this
+// case, since 'BeginPop' cleans up old tombstones. Even more, it is necessary
+// to do 'Pop' if listing contains non-empty set of tombstones (regardless of
+// whether the caller wants to actually pop any items from the set). This is
+// part of the required set maintenance.
+//
+// Requires a transaction. Modifies Tombstone entity.
+// Requires a txndefer to be installed in context. This is already done by
+// default in luci/server.
+//
+// Returns only transient errors. Such errors usually mean that the entire pop
+// sequence ('List' + 'Pop') should be retried.
+func (s *Set) BeginPop(c context.Context, listing *Listing) (*PopOp, error) {
+	if listing.parent != s.Parent {
+		panic("passed Listing from another set")
+	}
+	txn := datastore.CurrentTransaction(c)
+	if txn == nil {
+		panic("dsset.Set.BeginPop must be called inside a transaction")
+	}
+
+	now := clock.Now(c).UTC()
+	if age := now.Sub(listing.producedAt); age > s.TombstonesDelay {
+		return nil, transient.Tag.Apply(fmt.Errorf("the listing is stale (%s > %s)", age, s.TombstonesDelay))
+	}
+
+	entity := &tombstonesEntity{Parent: s.Parent}
+	if err := datastore.Get(c, entity); err != nil && err != datastore.ErrNoSuchEntity {
+		return nil, transient.Tag.Apply(err)
+	}
+
+	// The data in tombstonesEntity, in map form.
+	tombs := make(map[string]time.Time, len(entity.Tombstones))
+	for _, t := range entity.Tombstones {
+		tombs[t.ID] = t.Tombstoned
+	}
+
+	// Throw away old tombstones right away.
+	dirty := false
+	for _, tomb := range listing.Garbage {
+		if tomb.old {
+			if !tomb.cleanedUp {
+				panic("trying to remove Tombstone that wasn't cleaned up")
+			}
+			if _, hasTomb := tombs[tomb.id]; hasTomb {
+				delete(tombs, tomb.id)
+				dirty = true
+			}
+		}
+	}
+
+	return &PopOp{
+		ctx:     c,
+		txn:     txn,
+		now:     now,
+		dirty:   dirty,
+		entity:  entity,
+		tombs:   tombs,
+		idToKey: listing.idToKey,
+	}, nil
+}
+
+// CanPop returns true if the given item can be popped from the set.
+//
+// Returns false if this item has been popped before (perhaps in another
+// transaction), or it's not in the the listing passed to BeginPop.
+func (p *PopOp) CanPop(id string) bool {
+	if _, hasTomb := p.tombs[id]; hasTomb {
+		return false // already popped by someone else
+	}
+	if _, present := p.idToKey[id]; present {
+		return true // listed in the set
+	}
+	return false
+}
+
+// Pop removed the item from the set and returns true if it was there.
+//
+// Returns false if this item has been popped before (perhaps in another
+// transaction), or it's not in the the listing passed to BeginPop.
+func (p *PopOp) Pop(id string) bool {
+	if p.finished {
+		panic("the operation has already been finished")
+	}
+	if !p.CanPop(id) {
+		return false
+	}
+	p.tombs[id] = p.now
+	p.popped = append(p.popped, &tombstone{
+		id:      id,
+		storage: p.idToKey[id],
+	})
+	p.dirty = true
+	return true
+}
+
+// makeTombstonesEntity is used internally by FinishPop.
+func (p *PopOp) makeTombstonesEntity() *tombstonesEntity {
+	p.entity.Tombstones = p.entity.Tombstones[:0]
+	for id, ts := range p.tombs {
+		p.entity.Tombstones = append(p.entity.Tombstones, struct {
+			ID         string
+			Tombstoned time.Time
+		}{id, ts})
+	}
+	return p.entity
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// FinishPop completes one or more pop operations (for different sets) by
+// submitting changes to datastore.
+//
+// Must be called within the same transaction that called BeginPop.
+//
+// Returns only transient errors.
+func FinishPop(ctx context.Context, ops ...*PopOp) error {
+	txn := datastore.CurrentTransaction(ctx)
+
+	entities := []*tombstonesEntity{}
+	tombsCount := 0
+	for _, op := range ops {
+		if op.finished {
+			panic("the operation has already been finished")
+		}
+		if op.txn != txn {
+			panic("wrong transaction")
+		}
+		if op.dirty {
+			entities = append(entities, op.makeTombstonesEntity())
+			tombsCount += len(op.popped)
+		}
+	}
+
+	if err := datastore.Put(ctx, entities); err != nil {
+		return transient.Tag.Apply(err)
+	}
+
+	var tombs Garbage
+	if tombsCount != 0 {
+		tombs = make(Garbage, 0, tombsCount)
+	}
+	for _, op := range ops {
+		tombs = append(tombs, op.popped...)
+		op.finished = true
+	}
+	txndefer.Defer(ctx, func(ctx context.Context) {
+		CleanupGarbage(ctx, tombs) // best-effort cleanup
+	})
+	return nil
+}
+
+// CleanupGarbage deletes entities used to store items under given tombstones.
+//
+// This is datastore's MultiDelete RPC in disguise.
+// Must be called outside of transactions. Idempotent.
+//
+// Can handle tombstones from multiple different sets at once. This is preferred
+// over calling 'CleanupGarbage' multiple times (once per set), since it
+// collapses multiple datastore RPCs into one.
+//
+// This MUST be called before tombstones returned by 'List' are removed in
+// 'Pop'. Failure to do so will make items reappear in the set.
+//
+// Returns only transient errors. There's no way to know which items were
+// removed and which weren't in case of an error.
+func CleanupGarbage(c context.Context, cleanup ...Garbage) error {
+	if datastore.CurrentTransaction(c) != nil {
+		panic("dsset.CleanupGarbage must be called outside of a transaction")
+	}
+
+	keys := []*datastore.Key{}
+	for _, tombs := range cleanup {
+		for _, tomb := range tombs {
+			if tomb.storage != nil {
+				keys = append(keys, tomb.storage)
+			}
+		}
+	}
+
+	err := batchOp(len(keys), func(start, end int) error {
+		return datastore.Delete(c, keys[start:end])
+	})
+	if err != nil {
+		return transient.Tag.Apply(err)
+	}
+
+	for _, tombs := range cleanup {
+		for _, tomb := range tombs {
+			tomb.cleanedUp = true
+			tomb.storage = nil
+		}
+	}
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type itemEntity struct {
+	_kind string `gae:"$kind,dsset.Item"`
+
+	ID     string         `gae:"$id"`
+	Parent *datastore.Key `gae:"$parent"`
+	Value  []byte         `gae:",noindex"`
+}
+
+type tombstonesEntity struct {
+	_kind string `gae:"$kind,dsset.Tombstones"`
+
+	ID     string         `gae:"$id,const"` // Always the same ID.
+	Parent *datastore.Key `gae:"$parent"`
+
+	// Tombstones is unordered list of pairs <item ID, when it was popped>.
+	Tombstones []struct {
+		ID         string
+		Tombstoned time.Time
+	} `gae:",noindex"`
+}
+
+// batchOp splits 'total' into batches and calls 'op' in parallel.
+//
+// Doesn't preserve order of returned errors! Don't try to deconstruct the
+// returned multi error, the position of individual errors there does not
+// correlate with the original array.
+func batchOp(total int, op func(start, end int) error) error {
+	switch {
+	case total == 0:
+		return nil
+	case total <= batchSize:
+		return op(0, total)
+	}
+
+	errs := make(chan error)
+	ops := 0
+	offset := 0
+	for total > 0 {
+		count := batchSize
+		if count > total {
+			count = total
+		}
+		go func(start, end int) {
+			errs <- op(start, end)
+		}(offset, offset+count)
+		offset += count
+		total -= count
+		ops++
+	}
+
+	var all errors.MultiError
+	for i := 0; i < ops; i++ {
+		err := <-errs
+		if merr, yep := err.(errors.MultiError); yep {
+			for _, e := range merr {
+				if e != nil {
+					all = append(all, e)
+				}
+			}
+		} else if err != nil {
+			all = append(all, err)
+		}
+	}
+
+	if len(all) == 0 {
+		return nil
+	}
+	return all
+}
---
+++ cv/internal/bq/bq_test.go
@@ -0,0 +1,261 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dsset
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+
+	"go.chromium.org/luci/gae/filter/txndefer"
+	"go.chromium.org/luci/gae/impl/memory"
+	"go.chromium.org/luci/gae/service/datastore"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/clock/testclock"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+	"go.chromium.org/luci/common/data/stringset"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func testingContext() context.Context {
+	c := txndefer.FilterRDS(memory.Use(context.Background()))
+	datastore.GetTestable(c).AutoIndex(true)
+	datastore.GetTestable(c).Consistent(true)
+	c = clock.Set(c, testclock.New(time.Unix(1442270520, 0).UTC()))
+	c = mathrand.Set(c, rand.New(rand.NewSource(1000)))
+	return c
+}
+
+// pop pops a bunch of items from the set and returns items that were popped.
+func pop(c context.Context, s *Set, listing *Listing, ids []string) (popped []string, err error) {
+	op, err := s.BeginPop(c, listing)
+	if err != nil {
+		return nil, err
+	}
+	for _, id := range ids {
+		if op.Pop(id) {
+			popped = append(popped, id)
+		}
+	}
+	if err = FinishPop(c, op); err != nil {
+		return nil, err
+	}
+	return popped, nil
+}
+
+func TestSet(t *testing.T) {
+	t.Parallel()
+
+	Convey("item one lifecycle", t, func() {
+		c := testingContext()
+
+		set := Set{
+			Parent:          datastore.NewKey(c, "Parent", "parent", 0, nil),
+			TombstonesDelay: time.Minute,
+		}
+
+		// Add one item.
+		So(set.Add(c, []Item{{ID: "abc"}}), ShouldBeNil)
+
+		// The item is returned by the listing.
+		listing, err := set.List(c)
+		So(err, ShouldBeNil)
+		So(listing.Items, ShouldResemble, []Item{{ID: "abc"}})
+		So(listing.Garbage, ShouldBeNil)
+
+		// Pop it!
+		err = datastore.RunInTransaction(c, func(c context.Context) error {
+			popped, err := pop(c, &set, listing, []string{"abc"})
+			So(err, ShouldBeNil)
+			So(popped, ShouldResemble, []string{"abc"})
+			return nil
+		}, nil)
+		So(err, ShouldBeNil)
+
+		// The listing no longer returns it.
+		listing, err = set.List(c)
+		So(err, ShouldBeNil)
+		So(listing.Items, ShouldBeNil)
+
+		// The listing no longer returns the item, and there's no tombstones to
+		// cleanup.
+		listing, err = set.List(c)
+		So(err, ShouldBeNil)
+		So(listing.Items, ShouldBeNil)
+		So(listing.Garbage, ShouldBeNil)
+
+		// Attempt to add it back (should be ignored).
+		So(set.Add(c, []Item{{ID: "abc"}}), ShouldBeNil)
+
+		// The listing still doesn't return it, but we now have a tombstone to
+		// cleanup (again).
+		listing, err = set.List(c)
+		So(err, ShouldBeNil)
+		So(listing.Items, ShouldBeNil)
+		So(len(listing.Garbage), ShouldEqual, 1)
+		So(listing.Garbage[0].old, ShouldBeFalse)
+		So(listing.Garbage[0].storage, ShouldNotBeNil)
+
+		// Popping it again doesn't work either.
+		err = datastore.RunInTransaction(c, func(c context.Context) error {
+			popped, err := pop(c, &set, listing, []string{"abc"})
+			So(err, ShouldBeNil)
+			So(popped, ShouldBeNil)
+			return nil
+		}, nil)
+		So(err, ShouldBeNil)
+
+		// Cleaning up the storage, again. This should make List stop returning
+		// the tombstone (since it has no storage items associated with it and it's
+		// not ready to be evicted yet).
+		So(CleanupGarbage(c, listing.Garbage), ShouldBeNil)
+		listing, err = set.List(c)
+		So(err, ShouldBeNil)
+		So(listing.Items, ShouldBeNil)
+		So(listing.Garbage, ShouldBeNil)
+
+		// Time passes, tombstone expires.
+		clock.Get(c).(testclock.TestClock).Add(2 * time.Minute)
+
+		// Listing now returns expired tombstone.
+		listing, err = set.List(c)
+		So(err, ShouldBeNil)
+		So(listing.Items, ShouldBeNil)
+		So(len(listing.Garbage), ShouldEqual, 1)
+		So(listing.Garbage[0].storage, ShouldBeNil) // cleaned already
+
+		// Cleanup storage keys.
+		So(CleanupGarbage(c, listing.Garbage), ShouldBeNil)
+
+		// Cleanup the tombstones themselves.
+		err = datastore.RunInTransaction(c, func(c context.Context) error {
+			popped, err := pop(c, &set, listing, nil)
+			So(err, ShouldBeNil)
+			So(popped, ShouldBeNil)
+			return nil
+		}, nil)
+		So(err, ShouldBeNil)
+
+		// No tombstones returned any longer.
+		listing, err = set.List(c)
+		So(err, ShouldBeNil)
+		So(listing.Items, ShouldBeNil)
+		So(listing.Garbage, ShouldBeNil)
+
+		// And the item can be added back now, since no trace of it is left.
+		So(set.Add(c, []Item{{ID: "abc"}}), ShouldBeNil)
+
+		// Yep, it is there.
+		listing, err = set.List(c)
+		So(err, ShouldBeNil)
+		So(listing.Items, ShouldResemble, []Item{{ID: "abc"}})
+		So(listing.Garbage, ShouldBeNil)
+	})
+}
+
+func TestStress(t *testing.T) {
+	t.Parallel()
+
+	Convey("stress", t, func() {
+		// Add 1000 items in parallel from N goroutines, and (also in parallel),
+		// run N instances of "List and pop all", collecting the result in single
+		// list. There should be no duplicates in the final list!
+		c := testingContext()
+
+		set := Set{
+			Parent:          datastore.NewKey(c, "Parent", "parent", 0, nil),
+			TombstonesDelay: time.Minute,
+		}
+
+		producers := 3
+		consumers := 5
+		items := 100
+
+		wakeups := make(chan string)
+
+		lock := sync.Mutex{}
+		var consumed []string
+
+		for i := 0; i < producers; i++ {
+			go func() {
+				for j := 0; j < items; j++ {
+					set.Add(c, []Item{{ID: fmt.Sprintf("%d", j)}})
+					// Wake up 3 consumers, so they "fight".
+					wakeups <- "wake"
+					wakeups <- "wake"
+					wakeups <- "wake"
+				}
+				for i := 0; i < consumers; i++ {
+					wakeups <- "done"
+				}
+			}()
+		}
+
+		consume := func() {
+			listing, err := set.List(c)
+			if err != nil || len(listing.Items) == 0 {
+				return
+			}
+
+			keys := make([]string, len(listing.Items))
+			for i, itm := range listing.Items {
+				keys[i] = itm.ID
+			}
+
+			// Try to pop all.
+			var popped []string
+			err = datastore.RunInTransaction(c, func(c context.Context) error {
+				var err error
+				popped, err = pop(c, &set, listing, keys)
+				return err
+			}, nil)
+
+			// Consider items consumed only if transaction has landed.
+			if err == nil && len(popped) != 0 {
+				lock.Lock()
+				consumed = append(consumed, popped...)
+				lock.Unlock()
+			}
+		}
+
+		wg := sync.WaitGroup{}
+		wg.Add(consumers)
+		for i := 0; i < consumers; i++ {
+			go func() {
+				defer wg.Done()
+				done := false
+				for !done {
+					done = (<-wakeups) == "done"
+					consume()
+				}
+			}()
+		}
+
+		wg.Wait() // this waits for completion of the entire pipeline
+
+		// Make sure 'consumed' is the initially produced set.
+		dedup := stringset.New(len(consumed))
+		for _, itm := range consumed {
+			dedup.Add(itm)
+		}
+		So(dedup.Len(), ShouldEqual, len(consumed)) // no dups
+		So(len(consumed), ShouldEqual, items)       // all are accounted for
+	})
+}
