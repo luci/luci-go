5251b34e0175b6c34314e9c07529e7fbefdf9a0b
Chan Li <chanli@chromium.org>
2021-02-10 23:53:53 -0800

    [ResultDB] QueryTestVariant: support UNEXPECTEDLY_SKIPPED
    Bug: 1175634
    Change-Id: I2dfbad7226132cbf247ba334cb4d244ddeed592e

--- resultdb/internal/testvariants/query_test.go
+++ resultdb/internal/testvariants/query_test.go
@@ -96,6 +96,8 @@ func TestQueryTestVariants(t *testing.T) {
 				pb.TestStatus_FAIL, pb.TestStatus_FAIL, pb.TestStatus_FAIL,
 				pb.TestStatus_FAIL, pb.TestStatus_FAIL, pb.TestStatus_FAIL,
 			),
+			insert.TestResults("inv1", "Tx", nil, pb.TestStatus_SKIP),
+			insert.TestResults("inv1", "Tz", nil, pb.TestStatus_SKIP, pb.TestStatus_SKIP),
 
 			insert.TestExonerations("inv0", "T1", nil, 1),
 		)...)
@@ -125,6 +127,22 @@ func TestQueryTestVariants(t *testing.T) {
 			}),
 		)
 
+		// Tx has an expected skip so it should be FLAKY instead of UNEXPECTEDLY_SKIPPED.
+		testutil.MustApply(ctx,
+			spanutil.InsertMap("TestResults", map[string]interface{}{
+				"InvocationId":    invocations.ID("inv1"),
+				"TestId":          "Tx",
+				"ResultId":        "1",
+				"Variant":         nil,
+				"VariantHash":     pbutil.VariantHash(nil),
+				"CommitTimestamp": spanner.CommitTimestamp,
+				"IsUnexpected":    false,
+				"Status":          pb.TestStatus_SKIP,
+				"RunDurationUsec": pbutil.MustDuration(duration).Microseconds(),
+				"StartTime":       startTime,
+			}),
+		)
+
 		Convey(`Unexpected works`, func() {
 			tvs, _ := mustFetch(q)
 			tvStrings := getTVStrings(tvs)
@@ -135,7 +153,9 @@ func TestQueryTestVariants(t *testing.T) {
 				"2/T2/e3b0c44298fc1c14",
 				"2/T5/c467ccce5a16dc72",
 				"2/T8/e3b0c44298fc1c14",
+				"2/Tx/e3b0c44298fc1c14",
 				"3/T1/e3b0c44298fc1c14",
+				"4/Tz/e3b0c44298fc1c14",
 			})
 
 			So(tvs[0].Results, ShouldResemble, []*uipb.TestResultBundle{
@@ -152,7 +172,7 @@ func TestQueryTestVariants(t *testing.T) {
 					},
 				},
 			})
-			So(tvs[6].Exonerations[0], ShouldResemble, &pb.TestExoneration{
+			So(tvs[7].Exonerations[0], ShouldResemble, &pb.TestExoneration{
 				ExplanationHtml: "explanation 0",
 			})
 			So(len(tvs[2].Results), ShouldEqual, 10)
@@ -179,14 +199,16 @@ func TestQueryTestVariants(t *testing.T) {
 			}
 
 			q.PageSize = 15
-			nextToken := page("", 6, []string{
+			nextToken := page("", 8, []string{
 				"1/T4/c467ccce5a16dc72",
 				"1/T5/e3b0c44298fc1c14",
 				"1/Ty/e3b0c44298fc1c14",
 				"2/T2/e3b0c44298fc1c14",
 				"2/T5/c467ccce5a16dc72",
 				"2/T8/e3b0c44298fc1c14",
+				"2/Tx/e3b0c44298fc1c14",
 				"3/T1/e3b0c44298fc1c14",
+				"4/Tz/e3b0c44298fc1c14",
 			})
 			So(nextToken, ShouldEqual, pagination.Token("EXPECTED", "", ""))
 
@@ -202,6 +224,9 @@ func TestQueryTestVariants(t *testing.T) {
 			So(nextToken, ShouldEqual, pagination.Token("EXPECTED", "T8", "e3b0c44298fc1c14"))
 
 			nextToken = page(nextToken, 1, []string{"16/T9/e3b0c44298fc1c14"})
+			So(nextToken, ShouldEqual, "CghFWFBFQ1RFRAoCVHoKEGUzYjBjNDQyOThmYzFjMTQ=")
+
+			nextToken = page(nextToken, 0, []string{})
 			So(nextToken, ShouldEqual, "")
 		})
 
