e048db8a764c663c76ac129d772098da569524ef
Scott Lee <ddoman@chromium.org>
2021-02-12 09:06:24 +0000

    [resultdb] validate BatchUploadArtifactsRequest
    Bug:1149662
    Change-Id: I5c46e09a320b4dbc19bb9eb70ceeffa992341f2c

--- resultdb/internal/services/recorder/batch_create_artifacts.go
+++ resultdb/internal/services/recorder/batch_create_artifacts.go
@@ -19,12 +19,83 @@ import (
 
 	"google.golang.org/grpc/codes"
 
+	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/grpc/appstatus"
 
+	"go.chromium.org/luci/resultdb/internal/artifacts"
+	"go.chromium.org/luci/resultdb/internal/invocations"
+	"go.chromium.org/luci/resultdb/pbutil"
 	pb "go.chromium.org/luci/resultdb/proto/v1"
 )
 
+// TODO(crbug.com/1177213) - make this less-fragile.
+const MaxBatchUploadArtifactSize = 10 * 1024 * 1024
+
+func parseParent(parent string) (invID, testID, resultID string, err error) {
+	invID, testID, resultID, err = pbutil.ParseTestResultName(parent)
+	if err != nil {
+		if invID, err = pbutil.ParseInvocationName(parent); err != nil {
+			err = errors.Reason("parent: neither valid invocation name nor valid test result name").Err()
+			return
+		}
+	}
+	return
+}
+
+func parseBatchCreateArtifactsRequest(in *pb.BatchCreateArtifactsRequest) ([]*artifactCreator, error) {
+	var ret []*artifactCreator
+	var tSize int64
+
+	for i, req := range in.Requests {
+		if req == nil || req.Artifact == nil {
+			continue
+		}
+		if err := pbutil.ValidateArtifactID(req.Artifact.ArtifactId); err != nil {
+			return nil, errors.Annotate(err, "requests[%d]: artifact_id", i).Err()
+		}
+
+		invID, testID, resultID, err := parseParent(req.Parent)
+		switch {
+		case err != nil:
+			return nil, errors.Annotate(err, "requests[%d]", i).Err()
+		case i > 0 && invocations.ID(invID) != ret[i-1].invID:
+			return nil, errors.Reason("requests[%d]: parent: the invocation ID(%q) is different to the previous invocation ID(%q)", i, invID, ret[i-1].invID).Err()
+		}
+
+		cSize := int64(len(req.Artifact.Contents))
+		tSize += cSize
+		if tSize > MaxBatchUploadArtifactSize {
+			return nil, errors.Reason("the total size of artifact contents exceeded %d", MaxBatchUploadArtifactSize).Err()
+		}
+		ret = append(ret, &artifactCreator{
+			invID:         invocations.ID(invID),
+			artifactID:    req.Artifact.ArtifactId,
+			localParentID: artifacts.ParentID(testID, resultID),
+			contentType:   req.Artifact.ContentType,
+			size:          cSize,
+		})
+	}
+	return ret, nil
+}
+
 // BatchCreateArtifacts implements pb.RecorderServer.
 func (s *recorderServer) BatchCreateArtifacts(ctx context.Context, in *pb.BatchCreateArtifactsRequest) (*pb.BatchCreateArtifactsResponse, error) {
-	return nil, appstatus.Errorf(codes.Unimplemented, "method not implemented")
+	token, err := extractUpdateToken(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	arts, err := parseBatchCreateArtifactsRequest(in)
+	if err != nil {
+		return nil, appstatus.BadRequest(err)
+	}
+	if len(arts) == 0 {
+		return nil, nil
+	}
+
+	if err := validateInvocationToken(ctx, token, arts[0].invID); err != nil {
+		return nil, appstatus.Errorf(codes.PermissionDenied, "invalid update token")
+	}
+
+	return nil, nil
 }
