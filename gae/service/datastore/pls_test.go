0395f6e3268b6c705d467da30568cd4dd80d8453
Andrii Shyshkalov <tandrii@google.com>
2021-02-16 23:19:03 +0100

    [gae] bail if struct tag has missing quotes.
    Bug: 1177190
    Change-Id: I82085c4682d87646cda643869f89e55b1be837c9

--- gae/service/datastore/pls_test.go
+++ gae/service/datastore/pls_test.go
@@ -281,6 +281,10 @@ type InvalidTagged5 struct {
 	V []InvalidTaggedSub
 }
 
+type InvalidTagged6 struct {
+	I int `json:"f" gae:f,noindex`
+}
+
 type Inner1 struct {
 	W int32
 	X string
@@ -1204,6 +1208,11 @@ var testCases = []testCase{
 		desc:   "invalid tagged5",
 		src:    &InvalidTagged5{I: 19, V: []InvalidTaggedSub{{1}}},
 		plsErr: `struct tag has repeated property name: "V.I"`,
+	},
+	{
+		desc:   "invalid tagged6",
+		src:    &InvalidTagged6{I: 19},
+		plsErr: `struct tag is invalid: "json:\"f\" gae:f,noindex" (did you mean` + " `gae:\"...\"`?)",
 	},
 	{
 		desc: "doubler",
