b4c8dca05f9fa5f3cf535c6d6105f22cb63dcbc0
Takuto Ikuta <tikuta@chromium.org>
2021-02-19 09:13:47 +0900

    regenerate everything
    Change-Id: If6b3a8e8c6e233965573c7bc4929a4223595b5e4

--- common/proto/config/project_config.pb.go
+++ common/proto/config/project_config.pb.go
@@ -13,7 +13,6 @@
 package config
 
 import (
-	proto "github.com/golang/protobuf/proto"
 	_ "go.chromium.org/luci/common/proto"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
@@ -27,10 +26,6 @@ const (
 	// Verify that runtime/protoimpl is sufficiently up-to-date.
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
-
-// This is a compile-time assertion that a sufficiently up-to-date version
-// of the legacy proto package is being used.
-const _ = proto.ProtoPackageIsVersion4
 
 // Schema for project.cfg
 type ProjectCfg struct {
