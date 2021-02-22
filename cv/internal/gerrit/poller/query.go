f9afc2cf47eba09675ec1c01d7ca01e6c118058c
Andrii Shyshkalov <tandrii@google.com>
2021-02-22 23:49:34 +0100

    [cv] more efficient full polls.
    This has been working on -dev for 12 hours with great results.
    There are still spikes in of refresh-gerrit-cl tasks,
    but I'll address this later.
    
    Basic idea is to send a batch of notifications to PM whenever
    possible instead of via indirect, individual, and possibly redundant,
    CL updater tasks.
    
    R=qyearsley, yiwzhang
    
    Bug: 1180675
    Change-Id: Iad937a1f621d297e9352c4914e5b14b1de805724

--- cv/internal/gerrit/poller/query.go
+++ cv/internal/gerrit/poller/query.go
@@ -28,12 +28,16 @@ import (
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 	gerritpb "go.chromium.org/luci/common/proto/gerrit"
+	"go.chromium.org/luci/common/retry/transient"
 	"go.chromium.org/luci/common/sync/parallel"
+	"go.chromium.org/luci/gae/service/datastore"
 	"go.chromium.org/luci/grpc/grpcutil"
 
+	"go.chromium.org/luci/cv/internal/changelist"
 	"go.chromium.org/luci/cv/internal/common"
 	"go.chromium.org/luci/cv/internal/gerrit"
 	"go.chromium.org/luci/cv/internal/gerrit/updater"
+	"go.chromium.org/luci/cv/internal/prjmanager"
 )
 
 const (
@@ -192,49 +196,123 @@ func (q *singleQuery) fetch(ctx context.Context, after time.Time, query string) 
 	}
 }
 
+// maxLoadCLBatchSize limits how many CL entities are loaded at once for
+// notifying PM.
+const maxLoadCLBatchSize = 100
+
 func (q *singleQuery) scheduleTasks(ctx context.Context, changes []*gerritpb.ChangeInfo, forceNotifyPM bool) error {
 	// TODO(tandrii): optimize by checking if CV is interested in the
 	// (host,project,ref) these changes come from before triggering tasks.
 	logging.Debugf(ctx, "scheduling %d CLUpdate tasks", len(changes))
+
+	var clids []common.CLID
+	if forceNotifyPM {
+		// Objective: make PM aware of all CLs.
+		// Optimization: avoid RefreshGerritCL with forceNotifyPM=true if possible,
+		// as this removes ability to de-duplicate.
+		var err error
+		eids := make([]changelist.ExternalID, len(changes))
+		for i, c := range changes {
+			if eids[i], err = changelist.GobID(q.sp.GetHost(), c.GetNumber()); err != nil {
+				return err
+			}
+		}
+		clids, err = changelist.Lookup(ctx, eids)
+		if err != nil {
+			return err
+		}
+		if err := notifyPMifKnown(ctx, q.luciProject, clids, maxLoadCLBatchSize); err != nil {
+			return err
+		}
+	}
+
 	errs := parallel.WorkPool(10, func(work chan<- func() error) {
-		for _, c := range changes {
-			c := c
+		for i, c := range changes {
+			payload := &updater.RefreshGerritCL{
+				LuciProject:   q.luciProject,
+				Host:          q.sp.GetHost(),
+				Change:        c.GetNumber(),
+				UpdatedHint:   c.GetUpdated(),
+				ForceNotifyPm: forceNotifyPM && (clids[i] == 0),
+			}
 			work <- func() error {
-				payload := &updater.RefreshGerritCL{
-					LuciProject:   q.luciProject,
-					Host:          q.sp.GetHost(),
-					Change:        c.GetNumber(),
-					UpdatedHint:   c.GetUpdated(),
-					ForceNotifyPm: forceNotifyPM,
-				}
 				return updater.Schedule(ctx, payload)
 			}
 		}
 	})
-	if errs != nil {
-		return common.MostSevereError(errs.(errors.MultiError))
-	}
-	return nil
+	return common.MostSevereError(errs)
 }
 
 func scheduleRefreshTasks(ctx context.Context, luciProject, host string, changes []int64) error {
 	logging.Debugf(ctx, "scheduling %d CLUpdate tasks for no longer matched CLs", len(changes))
+	var err error
+	eids := make([]changelist.ExternalID, len(changes))
+	for i, c := range changes {
+		if eids[i], err = changelist.GobID(host, c); err != nil {
+			return err
+		}
+	}
+	// Objective: make PM aware of all CLs.
+	// Optimization: avoid RefreshGerritCL with forceNotifyPM=true if possible,
+	// as this removes ability to de-duplicate.
+	// Since all no longer matched CLs are typically already stored in the
+	// Datastore, get their internal CL IDs and notify PM directly.
+	clids, err := changelist.Lookup(ctx, eids)
+	if err != nil {
+		return err
+	}
+
+	if err := notifyPMifKnown(ctx, luciProject, clids, maxLoadCLBatchSize); err != nil {
+		return err
+	}
+
 	errs := parallel.WorkPool(10, func(work chan<- func() error) {
-		for _, c := range changes {
-			c := c
+		for i, c := range changes {
+			payload := &updater.RefreshGerritCL{
+				LuciProject:   luciProject,
+				Host:          host,
+				Change:        c,
+				ForceNotifyPm: 0 == clids[i], // notify iff CL ID isn't yet known.
+			}
 			work <- func() error {
-				payload := &updater.RefreshGerritCL{
-					LuciProject:   luciProject,
-					Host:          host,
-					Change:        c,
-					ForceNotifyPm: true,
-				}
 				return updater.Schedule(ctx, payload)
 			}
 		}
 	})
-	if errs != nil {
-		return common.MostSevereError(errs.(errors.MultiError))
+	return common.MostSevereError(errs)
+}
+
+// notifyPMifKnown notifies PM to update its CLs for each non-0 CLID.
+//
+// Obtains EVersion of each CL before notify a PM. Unfortunately, this loads a
+// lot of information we don't need, such as Snapshot. So, loads CLs in batches
+// to avoid large memory footprint.
+//
+// In practice, most of these CLs would be already dscache-ed, so loading them
+// is fast.
+func notifyPMifKnown(ctx context.Context, luciProject string, clids []common.CLID, maxBatchSize int) error {
+	cls := make([]*changelist.CL, 0, maxBatchSize)
+	flush := func() error {
+		if err := datastore.Get(ctx, cls); err != nil {
+			return errors.Annotate(common.MostSevereError(err), "failed to load CLs").Tag(transient.Tag).Err()
+		}
+		return prjmanager.NotifyCLsUpdated(ctx, luciProject, cls)
+	}
+
+	for _, clid := range clids {
+		switch l := len(cls); {
+		case clid == 0:
+		case l == maxBatchSize:
+			if err := flush(); err != nil {
+				return err
+			}
+			cls = cls[:0]
+		default:
+			cls = append(cls, &changelist.CL{ID: clid})
+		}
+	}
+	if len(cls) > 0 {
+		return flush()
 	}
 	return nil
 }
