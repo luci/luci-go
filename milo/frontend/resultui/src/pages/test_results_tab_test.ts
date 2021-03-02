f96694f5dda5bc10b4d5afd26904873f3ba19d6d
Weiwei Lin <weiweilin@google.com>
2021-03-02 12:48:49 +1100

    [resultui] always load at least one test variant
    Ensure that the test result tab always load at least one test variant
    when loading new test variants.
    
    R=nodir@chromium.org, nqmtuan@google.com
    
    Bug: 1174882
    Change-Id: I29f0ce87e4fea701c6a9a094a58d98e2c96f641c

--- milo/frontend/resultui/src/pages/test_results_tab_test.ts
+++ milo/frontend/resultui/src/pages/test_results_tab_test.ts
@@ -15,7 +15,7 @@
 import { fixture, fixtureCleanup } from '@open-wc/testing/index-no-side-effects';
 import { assert } from 'chai';
 import { customElement, html, LitElement, property } from 'lit-element';
-import sinon from 'sinon';
+import sinon, { SinonStub } from 'sinon';
 
 import { AppState, provideAppState } from '../context/app_state';
 import { InvocationState, provideInvocationState } from '../context/invocation_state';
@@ -107,5 +107,110 @@ describe('Test Results Tab', () => {
 
     assert.isFalse(invocationState.testLoader?.isLoading);
     assert.strictEqual(queryTestVariantsStub.callCount, 1);
+  });
+
+  describe('loadNextTestVariants', () => {
+    let queryTestVariantsStub: SinonStub<[QueryTestVariantsRequest], Promise<QueryTestVariantsResponse>>;
+    let appState: AppState;
+    let configsStore: UserConfigsStore;
+    let invocationState: InvocationState;
+    let tab: TestResultsTabElement;
+
+    beforeEach(async () => {
+      queryTestVariantsStub = sinon.stub<[QueryTestVariantsRequest], Promise<QueryTestVariantsResponse>>();
+      queryTestVariantsStub.onCall(0).resolves({testVariants: [variant1], nextPageToken: 'next0'});
+      queryTestVariantsStub.onCall(1).resolves({testVariants: [variant2], nextPageToken: 'next1'});
+      queryTestVariantsStub.onCall(2).resolves({testVariants: [variant3], nextPageToken: 'next2'});
+      queryTestVariantsStub.onCall(3).resolves({testVariants: [variant4], nextPageToken: 'next3'});
+      queryTestVariantsStub.onCall(4).resolves({testVariants: [variant5]});
+      appState = {
+        selectedTabId: '',
+        uiSpecificService: {
+          queryTestVariants: queryTestVariantsStub as typeof UISpecificService.prototype.queryTestVariants,
+        },
+      } as AppState;
+      configsStore = new UserConfigsStore();
+
+      invocationState = new InvocationState(appState);
+      invocationState.invocationId = 'invocation-id';
+
+      const provider = await fixture<ContextProvider>(html`
+        <milo-test-context-provider
+          .appState=${appState}
+          .configsStore=${configsStore}
+          .invocationState=${invocationState}
+        >
+          <milo-test-results-tab></milo-test-results-tab>
+        </milo-test-context-provider>
+      `);
+      tab = provider.querySelector<TestResultsTabElement>('milo-test-results-tab')!;
+    });
+
+    afterEach(() => {
+      invocationState.dispose();
+      const url = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
+      window.history.replaceState({path: url}, '', url);
+      fixtureCleanup();
+    });
+
+    it('should load at least one test variant', async () => {
+      invocationState.searchText = 'ID:b';
+
+      await tab.loadNextTestVariants();
+      assert.isFalse(invocationState.testLoader?.isLoading);
+      assert.strictEqual(queryTestVariantsStub.callCount, 4);
+    });
+
+    it('display setting should not cause more tests to be loaded', async () => {
+      invocationState.showUnexpectedVariants = false;
+      invocationState.showUnexpectedlySkippedVariants = false;
+      invocationState.showFlakyVariants = false;
+      invocationState.showExoneratedVariants = false;
+      invocationState.showExpectedVariants = false;
+
+      await tab.loadNextTestVariants();
+      assert.isFalse(invocationState.testLoader?.isLoading);
+      assert.strictEqual(queryTestVariantsStub.callCount, 2);
+    });
+
+    it('should stop loading when the final page is reached', async () => {
+      // Throw test loader.
+      const oldLoadNextPage = invocationState.testLoader!.loadNextPage.bind(invocationState.testLoader!);
+      let callCount = 0;
+      invocationState.testLoader!.loadNextPage = (...params) => {
+        callCount++;
+        if (callCount > 10) {
+          throw new Error('too many load next page calls');
+        }
+        return oldLoadNextPage(...params);
+      };
+
+      invocationState.searchText = 'ID:does-not-exist';
+      await tab.loadNextTestVariants();
+
+      assert.isFalse(invocationState.testLoader?.isLoading);
+      assert.strictEqual(queryTestVariantsStub.callCount, 5);
+    });
+
+    it('should trigger automatic loading when visiting the tab for the first time', async () => {
+      assert.isFalse(invocationState.testLoader?.isLoading);
+      assert.strictEqual(queryTestVariantsStub.callCount, 1);
+
+      // Disconnect, then reload the tab.
+      tab.disconnectedCallback();
+      await fixture<ContextProvider>(html`
+        <milo-test-context-provider
+          .appState=${appState}
+          .configsStore=${configsStore}
+          .invocationState=${invocationState}
+        >
+          <milo-test-results-tab></milo-test-results-tab>
+        </milo-test-context-provider>
+      `);
+
+      // Should not trigger loading agin.
+      assert.isFalse(invocationState.testLoader?.isLoading);
+      assert.strictEqual(queryTestVariantsStub.callCount, 1);
+    });
   });
 });
