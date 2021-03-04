ebfcddfc6964efd7d5ec766c7bbe55cb4b0b1d29
Weiwei Lin <weiweilin@google.com>
2021-03-04 15:49:29 +1100

    [resultui] improve the alignment for step entries
    R=chanli@google.com, nqmtuan@google.com
    
    Bug: 1159216
    Change-Id: If1843c1ba7e8369141eced1e92fab249741e7508

--- milo/frontend/resultui/src/libs/time_utils.ts
+++ milo/frontend/resultui/src/libs/time_utils.ts
@@ -40,3 +40,20 @@ export function displayDuration(duration: Duration) {
   }
   return parts.join(' ');
 }
+
+export function displayCompactDuration(duration: Duration) {
+  const shifted = duration.shiftTo('days', 'hours', 'minutes', 'seconds', 'milliseconds');
+  if (shifted.days >= 1) {
+    return `${(shifted.days + shifted.hours / 24).toFixed(1)}d`;
+  }
+  if (shifted.hours >= 1) {
+    return `${(shifted.hours + shifted.minutes / 60).toFixed(1)}h`;
+  }
+  if (shifted.minutes >= 1) {
+    return `${(shifted.minutes + shifted.seconds / 60).toFixed(1)}m`;
+  }
+  if (shifted.seconds >= 1) {
+    return `${shifted.seconds}s`;
+  }
+  return `${shifted.milliseconds}ms`;
+}
