52315770328a23325842d347dfacf222f4c3e45d
Chan Li <chanli@google.com>
2021-03-04 22:48:02 -0800

    [ResultDB] Run tests using Cloud Spanner Emulator - mac version
    Also in this CL:
    - Refactor common/spantest to reuse common code between versions
    - Updated README to add guidance on setting up mac environment.
    
    Bug: 1066993
    Change-Id: Idad9f81115c85849c715cb8effa6f4227e72c60c

---
+++ common/spantest/spantest_notlinuxmac.go
@@ -0,0 +1,60 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !darwin
+// +build !linux
+
+package spantest
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"syscall"
+	"time"
+
+	"cloud.google.com/go/spanner"
+	spandb "cloud.google.com/go/spanner/admin/database/apiv1"
+	spanins "cloud.google.com/go/spanner/admin/instance/apiv1"
+	"google.golang.org/api/option"
+	dbpb "google.golang.org/genproto/googleapis/spanner/admin/database/v1"
+	inspb "google.golang.org/genproto/googleapis/spanner/admin/instance/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"go.chromium.org/luci/auth"
+	"go.chromium.org/luci/common/errors"
+
+	"go.chromium.org/luci/common/system/filesystem"
+	"go.chromium.org/luci/common/system/port"
+	"go.chromium.org/luci/hardcoded/chromeinfra"
+)
+
+// StartEmulator starts a Cloud Spanner Emulator instance.
+func StartEmulator(ctx context.Context) (*Emulator, error) {
+	return nil, fmt.Errorf("can only run spanner tests on linux or mac.")
+}
+
+// Stop kills the emulator process and removes the temporary gcloud config directory.
+func (e *Emulator) Stop() error {
+	return fmt.Errorf("can only run spanner tests on linux or mac.")
+}
+
+
