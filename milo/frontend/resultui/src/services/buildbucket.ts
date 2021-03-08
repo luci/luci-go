e006898a231f95c31b9ef016f2e262e64c142d82
Weiwei Lin <weiweilin@google.com>
2021-03-09 10:55:09 +1100

    [resultui] cache RPC responses at service level
    1. Add cached_fn to help with caching results.
    2. Replace existing caching mechanisms in services with cached_fn.
    3. Add unit tests.
    4. Remove an unused method in milo_internal.
    
    Bug: 1156131
    Change-Id: I68862c1e875b702836162bef5e3168a87b6a8b86

--- milo/frontend/resultui/src/services/buildbucket.ts
+++ milo/frontend/resultui/src/services/buildbucket.ts
@@ -14,6 +14,8 @@
 
 import { PrpcClient } from '@chopsui/prpc-client';
 
+import { cached, CacheOption } from '../libs/cached_fn';
+
 /**
  * Manually coded type definition and classes for buildbucket services.
  * TODO(weiweilin): To be replaced by code generated version once we have one.
@@ -258,45 +260,45 @@ export interface ScheduleBuildRequest {
 const BUILDS_SERVICE = 'buildbucket.v2.Builds';
 
 export class BuildsService {
-  private prpcClient: PrpcClient;
+  private readonly cachedCallFn: (opt: CacheOption, method: string, message: object) => Promise<unknown>;
 
   constructor(readonly host: string, accessToken: string) {
-    this.prpcClient = new PrpcClient({host, accessToken});
+    const client = new PrpcClient({host, accessToken});
+    this.cachedCallFn = cached(
+      (method: string, message: object) => client.call(BUILDS_SERVICE, method, message),
+      {key: (method, message) => `${method}-${JSON.stringify(message)}`},
+    );
   }
 
-  async getBuild(req: GetBuildRequest) {
-    return await this.call(
+  async getBuild(req: GetBuildRequest, cacheOpt = CacheOption.Cached) {
+    return await this.cachedCallFn(
+      cacheOpt,
       'GetBuild',
       req,
     ) as Build;
   }
 
-  async searchBuilds(req: SearchBuildsRequest) {
-    return await this.call(
+  async searchBuilds(req: SearchBuildsRequest, cacheOpt = CacheOption.Cached) {
+    return await this.cachedCallFn(
+      cacheOpt,
       'SearchBuilds',
       req,
     ) as SearchBuildsResponse;
   }
 
   async cancelBuild(req: CancelBuildRequest) {
-    return await this.call(
+    return await this.cachedCallFn(
+      CacheOption.NoCache,
       'CancelBuild',
       req,
     ) as Build;
   }
 
   async scheduleBuild(req: ScheduleBuildRequest) {
-    return await this.call(
+    return await this.cachedCallFn(
+      CacheOption.NoCache,
       'ScheduleBuild',
       req,
     ) as Build;
-  }
-
-  private call(method: string, message: object) {
-    return this.prpcClient.call(
-      BUILDS_SERVICE,
-      method,
-      message,
-    );
   }
 }
