66997e16f021a90b1e5a0e5dc86a38c163ba7f1e
Andrii Shyshkalov <tandrii@google.com>
2021-03-12 21:54:35 +0100

    [cv][pm] implement single CL Run creation.
    `combo` struct is used to share code with multi-CL Run
    creation in the future.
    
    To avoid accumulating Runs before we implement Run cancelation fully,
    PM will only create runs on projects which CV is managing.
    
    Also, I've deleted the e2e test inside state/ package,
    because making it realistic requires doing what entire PM does
    (e.g. creating Datastore entitiy for ProjectStateOffload, which is checked
    during Run creation).
    
    Instead, I've added e2e test in impl/.
    
    R=qyearsley, yiwzhang
    
    Bug: 1156241
    Change-Id: I1db04b300be8036ad34a52a979df5529c28149fc

--- cv/internal/prjmanager/impl/state/componentactor/stageruns.go
+++ cv/internal/prjmanager/impl/state/componentactor/stageruns.go
@@ -16,9 +16,22 @@ package componentactor
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"time"
 
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/retry/transient"
+	"go.chromium.org/luci/gae/service/datastore"
+
+	"go.chromium.org/luci/cv/internal/changelist"
+	"go.chromium.org/luci/cv/internal/common"
 	"go.chromium.org/luci/cv/internal/config"
+	"go.chromium.org/luci/cv/internal/prjmanager"
+	"go.chromium.org/luci/cv/internal/run"
 )
 
 // stageNewRuns sets .runBuilders if new Runs have to be created.
@@ -29,13 +42,13 @@ import (
 //
 // Guarantees that a CL can be in at most 1 RunBuilder. This avoids conflicts
 // whereby 2+ Runs need to modify the same CL entity.
-func (a *Actor) stageNewRuns(ctx context.Context) (time.Time, error) {
+func (a *Actor) stageNewRuns(ctx context.Context, now time.Time) (time.Time, error) {
 	a.visitedCLs = map[int64]struct{}{}
 	defer func() { a.visitedCLs = nil }() // won't be useful afterwards.
 
 	var t time.Time
 	for clid, info := range a.cls {
-		switch nt, err := a.stageNewRunsFrom(ctx, clid, info); {
+		switch nt, err := a.stageNewRunsFrom(ctx, now, clid, info); {
 		case err != nil:
 			return t, err
 		case !nt.IsZero() && (t.IsZero() || nt.Before(t)):
@@ -45,26 +58,102 @@ func (a *Actor) stageNewRuns(ctx context.Context) (time.Time, error) {
 	return t, nil
 }
 
-func (a *Actor) stageNewRunsFrom(ctx context.Context, clid int64, info *clInfo) (time.Time, error) {
+func (a *Actor) stageNewRunsFrom(ctx context.Context, now time.Time, clid int64, info *clInfo) (time.Time, error) {
 	if !a.markVisited(clid) || !info.ready {
 		return time.Time{}, nil
 	}
 	cgIndex := info.pcl.GetConfigGroupIndexes()[0]
 	cg := a.s.ConfigGroup(cgIndex)
 	if cg.Content.GetCombineCls() == nil {
-		return a.stageNewRunsSingle(ctx, info, cg)
+		return a.stageNewRunsSingle(ctx, now, info, cg)
 	}
-	return a.stageNewRunsCombo(ctx, info, cg)
+	return a.stageNewRunsCombo(ctx, now, info, cg)
 }
 
-func (a *Actor) stageNewRunsSingle(ctx context.Context, info *clInfo, cg *config.ConfigGroup) (time.Time, error) {
+func (a *Actor) stageNewRunsSingle(ctx context.Context, now time.Time, info *clInfo, cg *config.ConfigGroup) (time.Time, error) {
+	if len(info.runIndexes) > 0 {
+		// Singular case today doesn't support concurrent runs.
+		return time.Time{}, nil
+	}
+	if len(info.deps.notYetLoaded) > 0 {
+		return a.postponeDueNotYetLoadedDeps(ctx, info)
+	}
+
+	combo := combo{}
+	combo.add(info)
+	rb, err := a.makeRunBuilder(ctx, &combo, cg)
+	if err != nil {
+		return time.Time{}, err
+	}
+	a.runBuilders = append(a.runBuilders, rb)
+	return clock.Now(ctx), nil
+}
+
+func (a *Actor) stageNewRunsCombo(ctx context.Context, now time.Time, info *clInfo, cg *config.ConfigGroup) (time.Time, error) {
 	//TODO(tandrii): implement
 	return time.Time{}, nil
 }
 
-func (a *Actor) stageNewRunsCombo(ctx context.Context, info *clInfo, cg *config.ConfigGroup) (time.Time, error) {
-	//TODO(tandrii): implement
+func (a *Actor) postponeDueNotYetLoadedDeps(ctx context.Context, info *clInfo) (time.Time, error) {
+	// TODO(tandrii): for safety, this should not wait forever.
+	sb := strings.Builder{}
+	fmt.Fprintf(&sb, "combo with %v waiting on %d deps:", info, len(info.deps.notYetLoaded))
+	for _, d := range info.deps.notYetLoaded {
+		fmt.Fprintf(&sb, " %d", d.GetClid())
+	}
+	logging.Warningf(ctx, sb.String())
 	return time.Time{}, nil
+}
+
+func (a *Actor) makeRunBuilder(ctx context.Context, combo *combo, cg *config.ConfigGroup) (*prjmanager.RunBuilder, error) {
+	latestIndex := -1
+	cls := make([]*changelist.CL, len(combo.all))
+	for i, info := range combo.all {
+		cls[i] = &changelist.CL{ID: common.CLID(info.pcl.GetClid())}
+		if info == combo.latestTriggered {
+			latestIndex = i
+		}
+	}
+	if err := datastore.Get(ctx, cls); err != nil {
+		// Even if one of errors is EntityNotFound, this is a temporary situation as
+		// such CL(s) should be removed from PM state soon.
+		return nil, errors.Annotate(err, "failed to load CLs").Tag(transient.Tag).Err()
+	}
+	for i, cl := range cls {
+		exp, act := combo.all[i].pcl.GetEversion(), int64(cl.EVersion)
+		if exp != act {
+			return nil, errors.Reason("CL %d EVersion changed %d => %d", cl.ID, exp, act).Tag(transient.Tag).Err()
+		}
+	}
+
+	// Run's owner is whoever owns the latest triggered CL.
+	// It's guaranteed to be set because otherwise CL would have been sent for
+	// pruning and nor marked as ready.
+	owner, err := cls[latestIndex].Snapshot.OwnerIdentity()
+	if err != nil {
+		return nil, errors.Annotate(err, "failed to get OwnerIdentity of %d", cls[latestIndex].ID).Err()
+	}
+
+	bcls := make([]prjmanager.RunBuilderCL, len(cls))
+	for i, cl := range cls {
+		pcl := combo.all[i].pcl
+		bcls[i] = prjmanager.RunBuilderCL{
+			ID:               common.CLID(pcl.GetClid()),
+			ExpectedEVersion: int(pcl.GetEversion()),
+			TriggerInfo:      pcl.GetTrigger(),
+			Snapshot:         cl.Snapshot,
+		}
+	}
+
+	return &prjmanager.RunBuilder{
+		ConfigGroupID:            cg.ID,
+		LUCIProject:              cg.ProjectString(),
+		Mode:                     run.Mode(combo.latestTriggered.pcl.GetTrigger().GetMode()),
+		Owner:                    owner,
+		ExpectedIncompleteRunIDs: nil, // no Run is expected
+		OperationID:              fmt.Sprintf("PM-%d", mathrand.Int63(ctx)),
+		InputCLs:                 bcls,
+	}, nil
 }
 
 // markVisited makes CL visited if not already and returns if action was taken.
@@ -74,4 +163,41 @@ func (a *Actor) markVisited(clid int64) bool {
 	}
 	a.visitedCLs[clid] = struct{}{}
 	return true
+}
+
+// combo is a set of related CLs that will together form a new Run.
+//
+// The CLs in a combo are a subset of those from the component.
+type combo struct {
+	all                  []*clInfo
+	clids                map[int64]struct{}
+	notReady             []*clInfo
+	withNotYetLoadedDeps *clInfo // nil if none; any one otherwise.
+	latestTriggered      *clInfo
+	maxTriggeredTime     time.Time
+}
+
+func (c *combo) add(info *clInfo) {
+	c.all = append(c.all, info)
+	if c.clids == nil {
+		c.clids = map[int64]struct{}{info.pcl.GetClid(): struct{}{}}
+	} else {
+		c.clids[info.pcl.GetClid()] = struct{}{}
+	}
+
+	if !info.ready {
+		c.notReady = append(c.notReady, info)
+	}
+
+	if info.deps != nil && len(info.deps.notYetLoaded) > 0 {
+		c.withNotYetLoadedDeps = info
+	}
+
+	if pb := info.pcl.GetTrigger().GetTime(); pb != nil {
+		t := pb.AsTime()
+		if c.maxTriggeredTime.IsZero() || t.After(c.maxTriggeredTime) {
+			c.maxTriggeredTime = t
+			c.latestTriggered = info
+		}
+	}
 }
