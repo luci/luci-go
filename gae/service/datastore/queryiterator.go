855f7fd69708ba85373f7ef9444d432f236b2869
Yuanjun Huang <yuanjunh@google.com>
2021-03-12 18:41:04 -0800

    [datastore] Revise the StartQueryIterator flaky test
    A flaky test which blocks CQ occasionally. Change it to use a waitgroup
    to check if this goroutine (http://shortn/_40N31YGdtC) has exited after
    `ctx.cancel()` was called.
    
    Bug: 1142317
    Change-Id: Ic590a50bb662a6d0bf8569e364173315e66fce5c

--- gae/service/datastore/queryiterator.go
+++ gae/service/datastore/queryiterator.go
@@ -18,9 +18,12 @@ import (
 	"bytes"
 	"context"
 	"sort"
+	"sync"
 
 	"go.chromium.org/luci/common/data/cmpbin"
 )
+
+var testWg *sync.WaitGroup // will be set only in tests to assert if all goroutines exits.
 
 // queryIterator is an iterator for datastore query results.
 type queryIterator struct {
@@ -37,8 +40,14 @@ func startQueryIterator(ctx context.Context, fq *FinalizedQuery) *queryIterator 
 		itemCh: make(chan *rawQueryResult),
 	}
 
-	go func() {
+	if testWg != nil {
+		testWg.Add(1)
+	}
+	go func(testWg *sync.WaitGroup) {
 		defer close(qi.itemCh)
+		if testWg != nil {
+			defer testWg.Done()
+		}
 		err := Raw(ctx).Run(fq, func(k *Key, pm PropertyMap, _ CursorCB) error {
 			select {
 			case <-ctx.Done():
@@ -58,7 +67,7 @@ func startQueryIterator(ctx context.Context, fq *FinalizedQuery) *queryIterator 
 				err: err,
 			}
 		}
-	}()
+	}(testWg)
 
 	return qi
 }
