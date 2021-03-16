38b2c649f62fac9a1f656341ec46956210ddb1bb
Andrii Shyshkalov <tandrii@google.com>
2021-03-16 01:40:44 +0000

    [cv][e2e] set up end to end test package.
    Move first 3 tests from PM to that package.
    
    R=qyearsley, yiwzhang
    
    Bug: 1156241
    Change-Id: I336f64deb3a1168701659993535685e6b19a8961
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2758017
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Auto-Submit: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>

---
+++ cv/internal/cvtesting/e2e/e2e.go
@@ -0,0 +1,210 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/gae/service/datastore"
+	"go.chromium.org/luci/server/tq/tqtesting"
+
+	cfgpb "go.chromium.org/luci/cv/api/config/v2"
+	diagnosticpb "go.chromium.org/luci/cv/api/diagnostic"
+	migrationpb "go.chromium.org/luci/cv/api/migration"
+	"go.chromium.org/luci/cv/internal/common"
+	"go.chromium.org/luci/cv/internal/cvtesting"
+	"go.chromium.org/luci/cv/internal/diagnostic"
+	"go.chromium.org/luci/cv/internal/gerrit/poller/pollertest"
+	pollertask "go.chromium.org/luci/cv/internal/gerrit/poller/task"
+	"go.chromium.org/luci/cv/internal/migration"
+	"go.chromium.org/luci/cv/internal/prjmanager"
+	"go.chromium.org/luci/cv/internal/prjmanager/pmtest"
+	"go.chromium.org/luci/cv/internal/prjmanager/prjpb"
+	"go.chromium.org/luci/cv/internal/run"
+	"go.chromium.org/luci/cv/internal/run/eventpb"
+	"go.chromium.org/luci/cv/internal/run/runtest"
+
+	// Import all modules with server/tq handler additions in init() calls, which
+	// are otherwise not imported directly or transitively via imports above.
+	_ "go.chromium.org/luci/cv/internal/prjmanager/impl"
+	_ "go.chromium.org/luci/cv/internal/run/impl"
+)
+
+// Test encapsulates e2e setup for a CV test.
+//
+// Embeds cvtesting.Test, which sets CV's dependencies and some simple CV
+// components (e.g. TreeClient), while this Test focuses on setup of CV's own
+// components.
+//
+// Typical use:
+//   ct := Test{CVDev: true}
+//   ctx, cancel := ct.SetUp()
+//   defer cancel()
+type Test struct {
+	*cvtesting.Test // auto-initialized if nil
+	// CVDev if true sets e2e test to use `cv-dev` GAE app.
+	// Defaults to `cv` GAE app.
+	CVDev bool
+
+	DiagnosticServer diagnosticpb.DiagnosticServer
+	MigrationServer  migrationpb.MigrationServer
+	// TODO(tandrii): add CQD fake.
+}
+
+func (t *Test) SetUp() (ctx context.Context, deferme func()) {
+	switch {
+	case t.Test == nil:
+		t.Test = &cvtesting.Test{}
+	case t.Test.AppID != "":
+		panic("overriding cvtesting.Test{AppID} in e2e not supported")
+	}
+	switch t.CVDev {
+	case true:
+		t.Test.AppID = "cv-dev"
+	case false:
+		t.Test.AppID = "cv"
+	}
+
+	// Delegate most setup to cvtesting.Test.
+	ctx, cancel := t.Test.SetUp()
+
+	t.MigrationServer = &migration.MigrationServer{}
+	t.DiagnosticServer = &diagnostic.DiagnosticServer{}
+	return ctx, cancel
+}
+
+// Now returns test clock time in UTC.
+func (t *Test) Now() time.Time {
+	return t.Clock.Now().UTC()
+}
+
+// RunAtLeastOncePM runs at least 1 PM task, possibly more or other tasks.
+func (t *Test) RunAtLeastOncePM(ctx context.Context) {
+	t.TQ.Run(ctx, tqtesting.StopAfterTask(prjpb.ManageProjectTaskClass))
+}
+
+// RunAtLeastOnceRun runs at least 1 Run task, possibly more or other tasks.
+func (t *Test) RunAtLeastOnceRun(ctx context.Context) {
+	t.TQ.Run(ctx, tqtesting.StopAfterTask(eventpb.ManageRunTaskClassID))
+}
+
+// RunAtLeastOncePoller runs at least 1 Poller task, possibly more or other
+// tasks.
+func (t *Test) RunAtLeastOncePoller(ctx context.Context) {
+	t.TQ.Run(ctx, tqtesting.StopAfterTask(pollertask.ClassID))
+}
+
+// RunUntil runs TQ tasks, while stopIf returns false.
+func (t *Test) RunUntil(ctx context.Context, stopIf func() bool) {
+	for i := 0; i < 1000; i++ {
+		if stopIf() {
+			logging.Debugf(ctx, "RunUntil ran %d iterations", i)
+			return
+		}
+		// TODO(tandrii): support running tasks in parallel.
+		switch ts := t.TQ.Tasks().Pending(); {
+		case len(pollertest.Projects(ts)) > 0:
+			t.RunAtLeastOncePoller(ctx)
+		case len(pmtest.Projects(ts)) > 0:
+			t.RunAtLeastOncePM(ctx)
+		case len(runtest.Runs(ts)) > 0:
+			t.RunAtLeastOnceRun(ctx)
+		}
+	}
+	panic("RunUntil ran for too long!")
+}
+
+// LoadProject returns Project entity or nil if not exists.
+func (t *Test) LoadProject(ctx context.Context, lProject string) *prjmanager.Project {
+	p, err := prjmanager.Load(ctx, lProject)
+	if err != nil {
+		panic(err)
+	}
+	return p
+}
+
+// LoadRun returns Run entity or nil if not exists.
+func (t *Test) LoadRun(ctx context.Context, id common.RunID) *run.Run {
+	r := &run.Run{ID: id}
+	switch err := datastore.Get(ctx, r); {
+	case err == datastore.ErrNoSuchEntity:
+		return nil
+	case err != nil:
+		panic(err)
+	default:
+		return r
+	}
+}
+
+// LoadRunsOf loads all Runs of a project from Datastore.
+func (t *Test) LoadRunsOf(ctx context.Context, lProject string) []*run.Run {
+	var res []*run.Run
+	err := datastore.GetAll(ctx, run.NewQueryWithLUCIProject(ctx, lProject), &res)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+// EarliestCreatedRun returns the earliest created Run in a project.
+//
+// If there are several such runs, may return any one of them.
+//
+// Returns nil if there are no Runs.
+func (t *Test) EarliestCreatedRunOf(ctx context.Context, lProject string) *run.Run {
+	var earliest *run.Run
+	for _, r := range t.LoadRunsOf(ctx, lProject) {
+		if earliest == nil || earliest.CreateTime.After(r.CreateTime) {
+			earliest = r
+		}
+	}
+	return earliest
+}
+
+// LogPhase emits easy to recognize log like
+// ===========================
+// PHASE: ....
+// ===========================
+func (t *Test) LogPhase(ctx context.Context, format string, args ...interface{}) {
+	line := strings.Repeat("=", 80)
+	format = fmt.Sprintf("\n%s\nPHASE: %s\n%s", line, format, line)
+	logging.Debugf(ctx, format, args...)
+}
+
+// MakeCfgSingular return project config with a single ConfigGroup.
+func MakeCfgSingular(cgName, gHost, gRepo, gRef string) *cfgpb.Config {
+	return &cfgpb.Config{
+		ConfigGroups: []*cfgpb.ConfigGroup{
+			{
+				Name: cgName,
+				Gerrit: []*cfgpb.ConfigGroup_Gerrit{
+					{
+						Url: "https://" + gHost + "/",
+						Projects: []*cfgpb.ConfigGroup_Gerrit_Project{
+							{
+								Name:      gRepo,
+								RefRegexp: []string{gRef},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
