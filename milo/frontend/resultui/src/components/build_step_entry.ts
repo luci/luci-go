95ac7dfee3395f1b569f6273f096e98b01977305
Weiwei Lin <weiweilin@google.com>
2021-03-16 16:22:49 +1100

    [resultui] hide pin/copy icons by default to reduce noise
    R=nodir@google.com, nqmtuan@google.com
    
    Change-Id: Ia0af8643131b11cb5c64f925bebe348d02821ca4
    Bug: 1153690,1183972

--- milo/frontend/resultui/src/components/build_step_entry.ts
+++ milo/frontend/resultui/src/components/build_step_entry.ts
@@ -233,6 +233,8 @@ export class BuildStepEntryElement extends MobxLitElement implements OnEnterList
           <milo-pin-toggle
             .pinned=${this.isPinned}
             title="Pin/unpin the step."
+            class="hidden-icon"
+            style=${styleMap({'visibility': this.isPinned ? 'visible' : ''})}
             @click=${(e: Event) => {
               this.configsStore.setStepPin(this.step.name, !this.isPinned);
               e.stopPropagation();
@@ -242,6 +244,7 @@ export class BuildStepEntryElement extends MobxLitElement implements OnEnterList
           <milo-copy-to-clipboard
             .textToCopy=${this.step.name}
             title="Copy the step name."
+            class="hidden-icon"
             @click=${(e: Event) => e.stopPropagation()}
           ></milo-copy-to-clipboard>
           <span id="header-markdown">${renderMarkdown(this.step.header)}</span>
@@ -258,6 +261,19 @@ export class BuildStepEntryElement extends MobxLitElement implements OnEnterList
 
     #place-holder {
       height: 24px;
+    }
+
+    #header {
+      display: inline-block;
+      width: 100%;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    .hidden-icon {
+      visibility: hidden;
+    }
+    #header:hover .hidden-icon {
+      visibility: visible;
     }
 
     #status-indicator {
