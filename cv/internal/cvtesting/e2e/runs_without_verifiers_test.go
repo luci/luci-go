f75cd33ad5c92a502758833fdb49485f95660aee
Andrii Shyshkalov <tandrii@google.com>
2021-03-16 19:08:23 +0100

    [cv][e2e] cover case of singular config + CL with deps.
    Also, file the bug below to fix the identified problem.
    
    R=qyearsley, yiwzhang
    
    Bug: 1188763
    Change-Id: I15021f300a436458e7e2a685d18d01ae671168c6

--- cv/internal/cvtesting/e2e/runs_without_verifiers_test.go
+++ cv/internal/cvtesting/e2e/runs_without_verifiers_test.go
@@ -16,7 +16,9 @@ package e2e
 
 import (
 	"testing"
+	"time"
 
+	"go.chromium.org/luci/cv/internal/common"
 	gf "go.chromium.org/luci/cv/internal/gerrit/gerritfake"
 	"go.chromium.org/luci/cv/internal/prjmanager"
 	"go.chromium.org/luci/cv/internal/run"
@@ -82,5 +84,76 @@ func TestCreatesSingularRun(t *testing.T) {
 		So(ct.LoadGerritCL(ctx, gHost, gChange).IncompleteRuns.ContainsSorted(r.ID), ShouldBeFalse)
 		So(ct.LoadProject(ctx, lProject).State.GetPcls(), ShouldBeEmpty)
 		So(ct.LoadProject(ctx, lProject).State.GetComponents(), ShouldBeEmpty)
+	})
+}
+
+func TestCreatesSingularRunWithDeps(t *testing.T) {
+	Convey("CV creates Run in singular config in presence of Git dependencies.", t, func() {
+		/////////////////////////    Setup   ////////////////////////////////
+		ct := Test{}
+		ctx, cancel := ct.SetUp()
+		defer cancel()
+
+		const lProject = "infra"
+		const gHost = "g-review"
+		const gRepo = "re/po"
+		const gRef = "refs/heads/main"
+
+		// TODO(tandrii): remove this once Run creation is not conditional on CV
+		// managing Runs for a project.
+		ct.EnableCVRunManagement(ctx, lProject)
+		cfg := MakeCfgSingular("cg0", gHost, gRepo, gRef)
+		ct.Cfg.Create(ctx, lProject, cfg)
+
+		tStart := ct.Clock.Now()
+
+		ct.LogPhase(ctx, "Set Git chain of 13 depends on 12, and vote CQ+1 on 13")
+		ct.GFake.AddFrom(gf.WithCIs(gHost, gf.ACLRestricted(lProject), gf.CI(
+			12, gf.Project(gRepo), gf.Ref(gRef), gf.PS(2),
+		)))
+		ct.GFake.AddFrom(gf.WithCIs(gHost, gf.ACLRestricted(lProject), gf.CI(
+			13, gf.Project(gRepo), gf.Ref(gRef), gf.PS(3),
+			gf.Updated(tStart),
+			gf.CQ(+1, tStart, gf.U("user-1")),
+		)))
+		ct.GFake.SetDependsOn(gHost, "13_3", "12_2")
+
+		ct.LogPhase(ctx, "CV starts dry Run on just the 13")
+		So(prjmanager.UpdateConfig(ctx, lProject), ShouldBeNil)
+		var r13 *run.Run
+		ct.RunUntil(ctx, func() bool {
+			r13 = ct.EarliestCreatedRunOf(ctx, lProject)
+			return r13 != nil && r13.Status == run.Status_RUNNING
+		})
+		So(r13.Mode, ShouldResemble, run.DryRun)
+		So(r13.CLs, ShouldResemble, []common.CLID{ct.LoadGerritCL(ctx, gHost, 13).ID})
+
+		ct.LogPhase(ctx, "User votes CQ+2 on 12, CV starts Run on just 12 and doesn't touch 13")
+		ct.Clock.Add(time.Minute)
+		ct.GFake.MutateChange(gHost, 12, func(c *gf.Change) {
+			gf.CQ(+2, ct.Clock.Now(), "user-1")(c.Info)
+			gf.Updated(ct.Clock.Now())(c.Info)
+		})
+		ct.RunUntil(ctx, func() bool {
+			return len(ct.LoadGerritCL(ctx, gHost, 12).IncompleteRuns) > 0
+		})
+		r12 := ct.LoadRun(ctx, ct.LoadGerritCL(ctx, gHost, 12).IncompleteRuns[0])
+		So(r12.Mode, ShouldResemble, run.FullRun)
+		So(r12.CLs, ShouldResemble, []common.CLID{ct.LoadGerritCL(ctx, gHost, 12).ID})
+		So(ct.LoadRun(ctx, r13.ID).Status, ShouldEqual, run.Status_RUNNING)
+		So(r12.CreateTime, ShouldHappenAfter, r13.CreateTime)
+
+		ct.LogPhase(ctx, "User upgrades to CQ+2 on 13, which terminates old Run.\n"+
+			"Due to 13 depending on not yet submitted 12, CV purges 13 by removing CQ+2 vote")
+		ct.Clock.Add(time.Minute)
+		ct.GFake.MutateChange(gHost, 13, func(c *gf.Change) {
+			gf.CQ(+2, ct.Clock.Now(), "user-1")(c.Info)
+			gf.Updated(ct.Clock.Now())(c.Info)
+		})
+		// TODO(crbug/1188763): fix Run Manager to cancel since trigger has changed.
+		// ct.RunUntil(ctx, func() bool {
+		// 	// CQ+2 vote removed.
+		// 	return trigger.Find(ct.GFake.GetChange(gHost, 13).Info) == nil
+		// })
 	})
 }
