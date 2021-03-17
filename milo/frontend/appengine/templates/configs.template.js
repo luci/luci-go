ab05e2daf38c7134ee902a61c1d184598fd4e753
Weiwei Lin <weiweilin@google.com>
2021-03-17 01:19:47 +0000

    [resultui] update directory structure
    1. Move all generated files with content hash to out/immutable/
    2. Move all other static resources and templates to assets/
    3. Use CopyWebpackPlugin to copy static resources to out/
    4. Update symlinks in appengine/
    5. Update app.yaml and .gitignore
    
    R=nodir@google.com, nqmtuan@google.com
    
    Change-Id: Ia19763f09527910618feab50066034c1d7c7a610
    Bug: 1187775
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2764162
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- milo/frontend/appengine/templates/configs.template.js
+++ milo/frontend/appengine/templates/configs.template.js
@@ -1,1 +1,11 @@
-../../resultui/configs.template.js
+var CONFIGS = Object.freeze({
+  RESULT_DB: Object.freeze({
+    HOST: "{{.ResultDB.Host}}",
+  }),
+  BUILDBUCKET: Object.freeze({
+    HOST: "{{.Buildbucket.Host}}",
+  }),
+  OAUTH2: Object.freeze({
+    CLIENT_ID: "{{.OAuth2.ClientID}}",
+  }),
+});
