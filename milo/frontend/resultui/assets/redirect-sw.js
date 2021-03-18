8060851edd608c211554f56d3bd9c4f5818e8145
Weiwei Lin <weiweilin@google.com>
2021-03-18 12:57:21 +1100

    [resultui] add redirect service worker
    1. Add redirect-sw.js to redirect old build page links to the new build
    page in via service worker.
    2. Host redirect-sw.js at root scope.
    3. Register redirect-sw.js at root scope in both the new and old build
    page.
    4. Unregister redirect-sw.js when switching back to the old build page.
    
    After this CL, redirecting to the new build page no longer takes an
    additional 300ms.
    
    Bug: 1187775
    Change-Id: I36e5f5cae5929a2b11c25d44b66bcd1aa9098528

---
+++ milo/frontend/resultui/assets/redirect-sw.js
@@ -0,0 +1,39 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview
+ * This service worker redirects old build page links to the new build page.
+ * This is 200-400ms faster than redirecting on the server side.
+ */
+
+// TODO(crbug/1108198): we don't need this after removing the /ui prefix.
+
+// Eslint isn't able to type-check webworker scripts.
+/* eslint-disable */
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  const isOldBuildPage =
+    // Short build link.
+    url.pathname.match(/^\/b\//) ||
+    // Long build link.
+    url.pathname.match(/^\/p\/[^/]+\/builders\/[^/]+\/[^/]+\//);
+  if (isOldBuildPage) {
+    url.pathname = '/ui' + url.pathname;
+    event.respondWith(Response.redirect(url.toString()));
+    return;
+  }
+  event.respondWith(fetch(event.request));
+});
