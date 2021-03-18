8689edbe7dd61b4c5a9133826c79a24d38e97017
Takuto Ikuta <tikuta@chromium.org>
2021-03-18 10:12:50 +0900

    proto/google: remove LoadDuration
    Change-Id: Id127e3c6cd56c200ce07d903d997cdc012837857

--- logdog/common/archive/source.go
+++ logdog/common/archive/source.go
@@ -24,6 +24,7 @@ import (
 	"go.chromium.org/luci/common/proto/google"
 	"go.chromium.org/luci/logdog/api/logpb"
 	"go.chromium.org/luci/logdog/common/renderer"
+	"google.golang.org/protobuf/types/known/durationpb"
 )
 
 // safeLogEntrySource wraps a renderer.Source and guarantees that all emitted
@@ -79,7 +80,7 @@ func (s *safeLogEntrySource) normalizeLogEntry(le *logpb.LogEntry) error {
 		s.logger().Warningf("Adjusting out-of-order timestamp (%s < %s) for log stream entry %d.",
 			timeOffset, s.lastTimeOffset, le.StreamIndex)
 
-		le.TimeOffset = google.LoadDuration(le.TimeOffset, s.lastTimeOffset)
+		le.TimeOffset = durationpb.New(s.lastTimeOffset)
 	} else {
 		s.lastTimeOffset = timeOffset
 	}
