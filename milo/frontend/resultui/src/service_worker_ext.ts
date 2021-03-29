9e659dab2c74a42f30989dbbb109adfbb1134804
Weiwei Lin <weiweilin@google.com>
2021-03-30 10:08:18 +1100

    [resultui] use cached access token
    1. Add service_worker_ext.ts to save/restore cached access tokens.
    2. Add cached-auth-state.js as a fallback.
    3. Set up workbox wrapper to enable communication to SW.
    4. Send auth state to service worker in milo-page-layout.
    5. Use cached auth state in milo-page-layout.
    6. Update various configs.
    
    When the cached access token is not expired, this improves the rendering
    time by ~200ms.
    
    R=nodir@google.com, nqmtuan@google.com
    
    Change-Id: Ib4a058ba1b6eba48fb2140e78be50edca4c213d8
    Bug: 1187775

---
+++ milo/frontend/resultui/src/service_worker_ext.ts
@@ -0,0 +1,52 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { get as kvGet, set as kvSet } from 'idb-keyval';
+
+// TSC isn't able to determine the scope properly.
+// Perform manual casting to fix typing.
+const _self = (self as unknown) as ServiceWorkerGlobalScope;
+
+const AUTH_STATE_KEY = 'auth-state';
+
+export interface SetAuthStateEventData {
+  type: 'SET_AUTH_STATE';
+  authState: AuthState | null;
+}
+
+_self.addEventListener('message', async (e) => {
+  switch (e.data.type) {
+    case 'SET_AUTH_STATE': {
+      const data = e.data as SetAuthStateEventData;
+      await kvSet(AUTH_STATE_KEY, data.authState);
+      break;
+    }
+    default:
+      console.warn('unexpected message type', e.data.type, e.data, e);
+  }
+});
+
+_self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.pathname === '/ui/cached-auth-state.js') {
+    e.respondWith(
+      (async () => {
+        const authState = (await kvGet<AuthState | null>(AUTH_STATE_KEY)) || null;
+        return new Response(`CACHED_AUTH_STATE=${JSON.stringify(authState)};`, {
+          headers: { 'content-type': 'application/javascript' },
+        });
+      })()
+    );
+  }
+});
