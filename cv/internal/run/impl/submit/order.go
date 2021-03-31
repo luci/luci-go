262408d2ec000ed246bb3e81aacb5d2ba65f915b
Yiwei Zhang <yiwzhang@google.com>
2021-03-31 05:42:12 +0000

    cv: Merge go.chromium.org/luci/cv/internal/submission into run submit package
    R=qyearsley, tandrii
    
    Bug: 1179284
    Change-Id: Ifc39524cc78b127b21d1c98e45376cf27f22a1fc
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2792889
    Auto-Submit: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>

--- cv/internal/run/impl/submit/order.go
+++ cv/internal/run/impl/submit/order.go
@@ -12,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO(yiwzhang): move this functionality to its own package or a package
-// that has much narrower scope.
-
-package submission
+package submit
 
 import (
 	"fmt"
