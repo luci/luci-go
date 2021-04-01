17fe415e9fea462a6d62655998c5ef1d520a10a3
Weiwei Lin <weiweilin@google.com>
2021-04-01 13:44:30 +1100

    [resultui] support configuring column width
    1. Implement <milo-drag-tracker> to track dragging event.
    2. Move columnWiths state to invocationState.
    3. Support resizing columns in <milo-tvt-column-header>
    
    R=chanli@google.com, nqmtuan@google.com
    
    Change-Id: Idfe2cd85ff5b45bc3891dec91936db10f59c28f4
    Bug: 1178662

---
+++ milo/frontend/resultui/src/components/drag_tracker.ts
@@ -0,0 +1,66 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { customElement, LitElement } from 'lit-element';
+import { html } from 'lit-html';
+
+export interface DragEventDetails {
+  // relative to the start.
+  dx: number;
+  dy: number;
+}
+
+export type DragEvent = CustomEvent<DragEventDetails>;
+
+/**
+ * A element that tracks the dragging event.
+ * Comparing to the native HTML drag event, this has higher resolution, and
+ * fires dragend event consistently when dragging stopped.
+ */
+@customElement('milo-drag-tracker')
+export class DragTrackerElement extends LitElement {
+  onmousedown = (startEvent: MouseEvent) => {
+    const x = startEvent.pageX;
+    const y = startEvent.pageY;
+
+    const dispatchEvent = (e: MouseEvent, type: string) => {
+      const dx = e.pageX - x;
+      const dy = e.pageY - y;
+      this.dispatchEvent(
+        new CustomEvent<DragEventDetails>(type, { detail: { dx, dy } })
+      );
+    };
+
+    const onMouseMove = (dragEvent: MouseEvent) => {
+      dragEvent.preventDefault();
+      dispatchEvent(dragEvent, 'drag');
+    };
+    document.addEventListener('mousemove', onMouseMove);
+
+    document.addEventListener(
+      'mouseup',
+      (endEvent: MouseEvent) => {
+        dispatchEvent(endEvent, 'dragend');
+        document.removeEventListener('mousemove', onMouseMove);
+      },
+      { once: true }
+    );
+
+    dispatchEvent(startEvent, 'dragstart');
+  };
+
+  protected render() {
+    return html``;
+  }
+}
