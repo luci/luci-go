0d74a26b4cf0fd472e976b8338e3dc6e0c55b516
Yiwei Zhang <yiwzhang@google.com>
2021-04-01 00:27:03 +0000

    cv: Order CLs in submission order when creating Run Submission
    R=qyearsley, tandrii
    
    Bug: 1179284
    Change-Id: I65dcf86cdc987c8e7cecd972430aa207a8095f49
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2795960
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Auto-Submit: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

--- cv/internal/run/impl/handler/submit.go
+++ cv/internal/run/impl/handler/submit.go
@@ -16,10 +16,18 @@ package handler
 
 import (
 	"context"
+	"time"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/retry/transient"
+	"go.chromium.org/luci/gae/service/datastore"
 
 	"go.chromium.org/luci/cv/internal/common"
+	"go.chromium.org/luci/cv/internal/run"
 	"go.chromium.org/luci/cv/internal/run/eventpb"
 	"go.chromium.org/luci/cv/internal/run/impl/state"
+	"go.chromium.org/luci/cv/internal/run/impl/submit"
 )
 
 // OnReadyForSubmission implements Handler interface.
@@ -36,4 +44,69 @@ func (*Impl) OnCLSubmitted(ctx context.Context, rs *state.RunState, clids common
 // OnSubmissionCompleted implements Handler interface.
 func (*Impl) OnSubmissionCompleted(ctx context.Context, rs *state.RunState, sr eventpb.SubmissionResult, attempt int32) (*Result, error) {
 	panic("implement")
+}
+
+func acquireSubmitQueue(ctx context.Context, rs *state.RunState) (waitlisted bool, err error) {
+	cg, err := rs.LoadConfigGroup(ctx)
+	if err != nil {
+		return false, err
+	}
+	now := clock.Now(ctx).UTC()
+	rid := rs.Run.ID
+	var innerErr error
+	err = datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+		waitlisted, innerErr = submit.TryAcquire(ctx, rid, cg.SubmitOptions)
+		switch {
+		case innerErr != nil:
+			return innerErr
+		case !waitlisted:
+			// If not waitlisted, RM will proceed as if ReadyForSubmission event is
+			// received. Sends a ReadyForSubmission event 10 seconds later in case
+			// the event processing has failed in the middle.
+			return run.NotifyReadyForSubmission(ctx, rid, now.Add(10*time.Second))
+		default:
+			return nil
+		}
+	}, nil)
+	switch {
+	case innerErr != nil:
+		return false, innerErr
+	case err != nil:
+		return false, errors.Annotate(err, "failed to run the transaction to acquire submit queue").Tag(transient.Tag).Err()
+	default:
+		return waitlisted, nil
+	}
+}
+
+func orderCLIDsInSubmissionOrder(ctx context.Context, clids common.CLIDs, runID common.RunID, sub *run.Submission) ([]int64, error) {
+	cls := make([]*run.RunCL, len(clids))
+	runKey := datastore.MakeKey(ctx, run.RunKind, string(runID))
+	for i, clID := range clids {
+		cls[i] = &run.RunCL{
+			ID:  clID,
+			Run: runKey,
+		}
+	}
+	err := datastore.Get(ctx, cls)
+	switch merr, ok := err.(errors.MultiError); {
+	case ok:
+		for i, err := range merr {
+			if err == datastore.ErrNoSuchEntity {
+				return nil, errors.Reason("RunCL %d not found in Datastore", cls[i].ID).Err()
+			}
+		}
+		count, err := merr.Summary()
+		return nil, errors.Annotate(err, "failed to load %d out of %d RunCLs", count, len(cls)).Tag(transient.Tag).Err()
+	case err != nil:
+		return nil, errors.Annotate(err, "failed to load %d RunCLs", len(cls)).Tag(transient.Tag).Err()
+	}
+	cls, err = submit.ComputeOrder(cls)
+	if err != nil {
+		return nil, err
+	}
+	ret := make([]int64, len(cls))
+	for i, cl := range cls {
+		ret[i] = int64(cl.ID)
+	}
+	return ret, nil
 }
