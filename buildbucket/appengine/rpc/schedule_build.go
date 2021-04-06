07d8d471e58623cff636d27c457fee6120ab0b21
Robert Iannucci <iannucci@chromium.org>
2021-04-05 17:01:13 -0700

    [buildbucket] Add experiment validation to ScheduleBuild.
    R=smut, yiwzhang, yuanjunh
    
    Bug: 1093970
    Change-Id: I9397682bac7e485d84ff4abc30e286a8a56422eb

--- buildbucket/appengine/rpc/schedule_build.go
+++ buildbucket/appengine/rpc/schedule_build.go
@@ -17,6 +17,7 @@ package rpc
 import (
 	"context"
 	"fmt"
+	"regexp"
 	"sort"
 	"strings"
 	"time"
@@ -80,7 +81,25 @@ func validateSchedule(req *pb.ScheduleBuildRequest) error {
 		return errors.Reason("priority must be in [0, 255]").Err()
 	}
 
+	for expName := range req.Experiments {
+		if err := validateExperimentName(expName); err != nil {
+			return errors.Annotate(err, "experiment %q", expName).Err()
+		}
+	}
+
 	// TODO(crbug/1042991): Validate Properties, Gerrit Changes, Dimensions, Notify.
+	return nil
+}
+
+var experimentNameRE = regexp.MustCompile(`^[a-z][a-z0-9_]*(?:\.[a-z][a-z0-9_]*)*$`)
+
+func validateExperimentName(expName string) error {
+	switch {
+	case !experimentNameRE.MatchString(expName):
+		return errors.Reason("does not match %q", experimentNameRE).Err()
+	case strings.HasPrefix(expName, "luci.") && !bb.WellKnownExperiments.Has(expName):
+		return errors.New(`unknown experiment has reserved prefix "luci."`)
+	}
 	return nil
 }
 
@@ -227,7 +246,7 @@ func setExperiments(ctx context.Context, req *pb.ScheduleBuildRequest, cfg *pb.B
 
 	// All well-known experiments need to show up in the model.Build, so
 	// initialize them all to false.
-	for _, wke := range bb.WellKnownExperiments {
+	for wke := range bb.WellKnownExperiments {
 		exps[wke] = false
 	}
 
