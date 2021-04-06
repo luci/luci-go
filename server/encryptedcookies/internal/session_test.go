18187f6cf072984686d0f35114038c97fbe80bfd
Vadim Shtayura <vadimsh@chromium.org>
2021-04-05 17:06:30 -0700

    [auth] Implement encryptedcookies sign-in flow.
    R=tandrii@chromium.org
    BUG=712506
    
    Change-Id: If3c3f340446b1a528b3602ce367baadc5af797ee

---
+++ server/encryptedcookies/internal/session_test.go
@@ -0,0 +1,61 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/google/tink/go/aead"
+	"github.com/google/tink/go/keyset"
+
+	"go.chromium.org/luci/server/encryptedcookies/session"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSession(t *testing.T) {
+	t.Parallel()
+
+	Convey("Works", t, func() {
+		kh, err := keyset.NewHandle(aead.AES256GCMKeyTemplate())
+		So(err, ShouldBeNil)
+		primaryAEAD, err := aead.New(kh)
+		So(err, ShouldBeNil)
+
+		cookie, sessionAEAD := NewSessionCookie(session.GenerateID())
+		httpCookie, err := EncryptSessionCookie(primaryAEAD, cookie)
+		So(err, ShouldBeNil)
+
+		enc, err := sessionAEAD.Encrypt([]byte("blah"), nil)
+		So(err, ShouldBeNil)
+
+		req, _ := http.NewRequest("GET", "http://example.com", nil)
+		req.Header.Add("Cookie", httpCookie.String())
+
+		httpCookieFromReq := EncryptedSessionCookie(req)
+		So(httpCookieFromReq, ShouldNotBeNil)
+
+		cookieFromReq, err := DecryptSessionCookie(primaryAEAD, httpCookieFromReq)
+		So(err, ShouldBeNil)
+
+		sessionAEADFromReq, err := PerSessionAEAD(cookieFromReq)
+		So(err, ShouldBeNil)
+
+		dec, err := sessionAEADFromReq.Decrypt(enc, nil)
+		So(err, ShouldBeNil)
+		So(string(dec), ShouldEqual, "blah")
+	})
+}
