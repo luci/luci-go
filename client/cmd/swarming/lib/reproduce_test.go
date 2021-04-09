fa4e0f3cbf623685b696dc9093bd8198b42f88d8
Joanna Wang <jojwang@google.com>
2021-04-08 19:36:07 -0700

    [swarming] reproduce: Fetch task request and execute command.
    Bug:1188473
    Change-Id: Idd5637e81b9098a566dd7bdc869f724f56070785

--- client/cmd/swarming/lib/reproduce_test.go
+++ client/cmd/swarming/lib/reproduce_test.go
@@ -15,10 +15,16 @@
 package lib
 
 import (
+	"bytes"
+	"context"
 	"os"
+	"os/exec"
+	"runtime"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
+
+	"go.chromium.org/luci/common/api/swarming/swarming/v1"
 	. "go.chromium.org/luci/common/testing/assertions"
 )
 
@@ -32,7 +38,7 @@ func TestReproduceParse_NoArgs(t *testing.T) {
 	t.Parallel()
 	Convey(`Make sure Parse works with no arguments.`, t, func() {
 		c := reproduceRun{}
-		c.Init(&testAuthFlags{})
+		c.init(&testAuthFlags{})
 
 		err := c.parse([]string(nil))
 		So(err, ShouldErrLike, "must provide -server")
@@ -43,11 +49,73 @@ func TestReproduceParse_NoTaskID(t *testing.T) {
 	t.Parallel()
 	Convey(`Make sure Parse works with with no task ID.`, t, func() {
 		c := reproduceRun{}
-		c.Init(&testAuthFlags{})
+		c.init(&testAuthFlags{})
 
 		err := c.GetFlags().Parse([]string{"-server", "http://localhost:9050"})
 
 		err = c.parse([]string(nil))
 		So(err, ShouldErrLike, "must specify exactly one task id.")
+	})
+}
+
+func TestCreateTaskRequestCommand(t *testing.T) {
+	t.Parallel()
+	Convey(`Make sure we can create the correct Cmd from a fetched task's properties.`, t, func() {
+		c := reproduceRun{}
+		c.init(&testAuthFlags{})
+		// Use TempDir, which creates a temp directory, to return a unique directory name
+		// that createTaskRequestCommand() will remove and recreate (via prepareDir()).
+		c.work = t.TempDir()
+		ctx := context.Background()
+		service := &testService{
+			getTaskRequest: func(_ context.Context, _ string) (*swarming.SwarmingRpcsTaskRequest, error) {
+				return &swarming.SwarmingRpcsTaskRequest{
+					TaskSlices: []*swarming.SwarmingRpcsTaskSlice{
+						&swarming.SwarmingRpcsTaskSlice{
+							Properties: &swarming.SwarmingRpcsTaskProperties{
+								Command: []string{"rbd", "stream", "-test-id-prefix", "chicken://cow_cow/"},
+							},
+						},
+						&swarming.SwarmingRpcsTaskSlice{
+							Properties: &swarming.SwarmingRpcsTaskProperties{
+								Command: []string{"rbd", "stream", "-test-id-prefix", "chicken://chicken_chicken/"},
+							},
+						},
+					},
+				}, nil
+			},
+		}
+		cmd, err := c.createTaskRequestCommand(ctx, "task-123", service)
+		So(err, ShouldBeNil)
+		expected := exec.CommandContext(ctx, "rbd", "stream", "-test-id-prefix", "chicken://chicken_chicken/")
+		expected.Dir = c.work
+		So(cmd, ShouldResemble, expected)
+	})
+}
+
+func TestReproduceTaskRequestCommand(t *testing.T) {
+	t.Parallel()
+	Convey(`Make sure we can execute commands.`, t, func() {
+		c := reproduceRun{}
+		c.init(&testAuthFlags{})
+		ctx := context.Background()
+		var cmd *exec.Cmd
+		if runtime.GOOS == "windows" {
+			cmd = exec.CommandContext(ctx, "cmd", "/c", "echo", "chicken")
+		} else {
+			cmd = exec.CommandContext(ctx, "echo", "chicken")
+		}
+
+		var stdBuffer bytes.Buffer
+		cmd.Stdout = &stdBuffer
+		cmd.Stderr = &stdBuffer
+		err := c.executeTaskRequestCommand(cmd)
+		So(err, ShouldBeNil)
+		if runtime.GOOS == "windows" {
+			So(stdBuffer.String(), ShouldEqual, "chicken\r\n")
+		} else {
+			So(stdBuffer.String(), ShouldEqual, "chicken\n")
+		}
+
 	})
 }
