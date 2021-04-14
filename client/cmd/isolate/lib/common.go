ba452d8c2b53b8489ccebc39a9e5163f065f1aa7
Nodir Turakulov <nodir@chromium.org>
2021-04-14 04:58:51 +0000

    [isolate] Open file later
    Minimize the duraiton of time when a file is open.
    
    R=tikuta@chromium.org
    
    Change-Id: I2d4991d4e169b6af670e446cf477cb0bb76dd9d7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2825128
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Auto-Submit: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>

--- client/cmd/isolate/lib/common.go
+++ client/cmd/isolate/lib/common.go
@@ -438,15 +438,14 @@ func (r *baseCommandRun) uploadToCAS(ctx context.Context, dumpJSON string, authO
 		return rootDgs, nil
 	}
 
+	m := make(map[string]string, len(opts))
+	for i, o := range opts {
+		m[filesystem.GetFilenameNoExt(o.Isolate)] = rootDgs[i].String()
+	}
 	f, err := os.Create(dumpJSON)
 	if err != nil {
 		return rootDgs, err
 	}
 	defer f.Close()
-
-	m := make(map[string]string, len(opts))
-	for i, o := range opts {
-		m[filesystem.GetFilenameNoExt(o.Isolate)] = rootDgs[i].String()
-	}
 	return rootDgs, json.NewEncoder(f).Encode(m)
 }
