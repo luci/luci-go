1ff3eae996d8507284a7f76a973cbe786267b9f6
Vadim Shtayura <vadimsh@chromium.org>
2021-04-14 22:02:02 -0700

    [server] Refactor server/secrets package.
    Introduce SecretManagerStore which will implement secret.Store using
    Google Secret Manager. Implement RandomSecret(...) in it using the
    same logic as we had before:
      * Load a root key from GSM.
      * Use it with DerivedStore to derive RandomSecret(...).
      * Periodically reload it.
    
    Previously this logic was part of Module implementation, making it
    hard to reuse. It is now in SecretManagerStore, available for reuse
    when implementing StoredSecret(...) in the next CL.
    
    Remove unnecessary public APIs: secrets.Source and its implementations.
    
    Remove loading the root secret from a local file. It was unused and
    it is always better to use GSM in production and phony devsecret://
    in development.
    
    Introduce "devsecret-text://<plain-text>" for secrets that are
    already ASCII text (e.g. passwords). Keep "devsecret://<base64>"
    as well, since it is used.
    
    R=tandrii@chromium.org
    BUG=712506
    
    Change-Id: Ib77775da97c689bae135ed912fba4787bbc16fa4

--- server/secrets/gsm_test.go
+++ server/secrets/gsm_test.go
@@ -17,153 +17,200 @@ package secrets
 import (
 	"context"
 	"fmt"
+	"math/rand"
+	"strings"
 	"testing"
+	"time"
 
 	gax "github.com/googleapis/gax-go/v2"
-	"golang.org/x/oauth2"
-	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1beta1"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"go.chromium.org/luci/common/clock/testclock"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+
 	. "github.com/smartystreets/goconvey/convey"
-	. "go.chromium.org/luci/common/testing/assertions"
 )
 
 func TestSecretManagerSource(t *testing.T) {
 	t.Parallel()
 
-	ctx := context.Background()
-	fakeTS := oauth2.StaticTokenSource(&oauth2.Token{})
+	Convey("With SecretManagerStore", t, func() {
+		ctx := mathrand.Set(context.Background(), rand.New(rand.NewSource(123)))
+		ctx, tc := testclock.UseTime(ctx, testclock.TestRecentTimeUTC)
 
-	Convey("Constructor", t, func() {
-		Convey("Works", func() {
-			s, err := NewSecretManagerSource(ctx, "sm://project/secret", fakeTS)
+		gsm := secretManagerMock{}
+
+		sm := SecretManagerStore{
+			CloudProject:        "fake-project",
+			AccessSecretVersion: gsm.AccessSecretVersion,
+		}
+
+		Convey("normalizeName sm://<secret>", func() {
+			name, err := sm.normalizeName("sm://secret")
 			So(err, ShouldBeNil)
-			So(s.project, ShouldEqual, "project")
-			So(s.secret, ShouldEqual, "secret")
+			So(name, ShouldEqual, "sm://fake-project/secret")
 		})
 
-		Convey("Not sm://", func() {
-			_, err := NewSecretManagerSource(ctx, "zz://project/secret", fakeTS)
-			So(err, ShouldErrLike, "not a sm://... URL")
+		Convey("readSecret devsecret", func() {
+			s, err := sm.readSecret(ctx, "devsecret://YWJj", false)
+			So(err, ShouldBeNil)
+			So(s, ShouldResemble, &trackedSecret{
+				name: "devsecret://YWJj",
+				value: Secret{
+					Current: []byte("abc"),
+				},
+			})
 		})
 
-		Convey("Wrong sm:// format", func() {
-			_, err := NewSecretManagerSource(ctx, "sm://project", fakeTS)
-			So(err, ShouldErrLike, "should have form")
-			_, err = NewSecretManagerSource(ctx, "sm://project/secret/zzz", fakeTS)
-			So(err, ShouldErrLike, "should have form")
+		Convey("readSecret devsecret-text", func() {
+			s, err := sm.readSecret(ctx, "devsecret-text://abc", false)
+			So(err, ShouldBeNil)
+			So(s, ShouldResemble, &trackedSecret{
+				name: "devsecret-text://abc",
+				value: Secret{
+					Current: []byte("abc"),
+				},
+			})
+		})
+
+		Convey("readSecret sm://<project>/<secret>", func() {
+			gsm.createVersion("project", "secret", "zzz")
+
+			s, err := sm.readSecret(ctx, "sm://project/secret", false)
+			So(err, ShouldBeNil)
+			So(s.name, ShouldEqual, "sm://project/secret")
+			So(s.value, ShouldResemble, Secret{Current: []byte("zzz")})
+			So(s.versions, ShouldEqual, [2]int64{1, 0})
+			So(s.nextReload.IsZero(), ShouldBeFalse)
+		})
+
+		Convey("readSecret with prev version", func() {
+			gsm.createVersion("project", "secret", "old")
+			gsm.createVersion("project", "secret", "new")
+
+			s, err := sm.readSecret(ctx, "sm://project/secret", true)
+			So(err, ShouldBeNil)
+			So(s.value, ShouldResemble, Secret{
+				Current: []byte("new"),
+				Previous: [][]byte{
+					[]byte("old"),
+				},
+			})
+			So(s.versions, ShouldEqual, [2]int64{2, 1})
+		})
+
+		Convey("readSecret with prev version disabled", func() {
+			ref := gsm.createVersion("project", "secret", "old")
+			gsm.createVersion("project", "secret", "new")
+			gsm.disableVersion(ref)
+
+			s, err := sm.readSecret(ctx, "sm://project/secret", true)
+			So(err, ShouldBeNil)
+			So(s.value, ShouldResemble, Secret{Current: []byte("new")})
+			So(s.versions, ShouldEqual, [2]int64{2, 0})
+		})
+
+		Convey("readSecret with prev version deleted", func() {
+			ref := gsm.createVersion("project", "secret", "old")
+			gsm.createVersion("project", "secret", "new")
+			gsm.deleteVersion(ref)
+
+			s, err := sm.readSecret(ctx, "sm://project/secret", true)
+			So(err, ShouldBeNil)
+			So(s.value, ShouldResemble, Secret{Current: []byte("new")})
+			So(s.versions, ShouldEqual, [2]int64{2, 0})
+		})
+
+		Convey("Derived secrets work", func() {
+			var (
+				ver1 = []byte{56, 113, 147, 97, 153, 240, 138, 213, 51, 101, 163, 53, 195, 45, 143, 253}
+				ver2 = []byte{246, 39, 65, 71, 93, 43, 95, 59, 139, 134, 84, 40, 226, 44, 2, 47}
+			)
+
+			gsm.createVersion("project", "secret", "zzz")
+
+			So(sm.LoadRootSecret(ctx, "sm://project/secret"), ShouldBeNil)
+
+			s1, err := sm.RandomSecret(ctx, "name")
+			So(err, ShouldBeNil)
+			So(s1, ShouldResemble, Secret{Current: ver1})
+
+			rotated := make(chan struct{})
+			sm.AddRotationHandler(ctx, "sm://project/secret", func(context.Context, Secret) {
+				close(rotated)
+			})
+
+			// Rotate the secret and make sure the change is picked up.
+			gsm.createVersion("project", "secret", "xxx")
+			tc.Add(reloadIntervalMax + time.Second)
+			bctx, cancel := context.WithCancel(ctx)
+			go sm.MaintenanceLoop(bctx)
+			defer cancel()
+			<-rotated
+
+			// Random secrets are rotated too.
+			s2, err := sm.RandomSecret(ctx, "name")
+			So(err, ShouldBeNil)
+			So(s2, ShouldResemble, Secret{
+				Current:  ver2,
+				Previous: [][]byte{ver1},
+			})
 		})
 	})
-
-	Convey("ReadSecret", t, func() {
-		client := mockedSMClient{
-			project:  "project",
-			secret:   "secret",
-			versions: map[string]*mockedVersion{},
-		}
-
-		src := &SecretManagerSource{
-			client:    &client,
-			secretURL: "sm://project/secret",
-			project:   "project",
-			secret:    "secret",
-		}
-
-		// Reading the latest version and one previous one.
-		client.setVersion(1, []byte{1, 2, 3})
-		client.setVersion(2, []byte{4, 5, 6})
-		secret, err := src.ReadSecret(ctx)
-		So(err, ShouldBeNil)
-		So(secret, ShouldResemble, &Secret{
-			Current: []byte{4, 5, 6},
-			Previous: [][]byte{
-				{1, 2, 3},
-			},
-		})
-
-		// A new version is added as latest.
-		client.setVersion(3, []byte{7, 8, 9})
-		secret, err = src.ReadSecret(ctx)
-		So(err, ShouldBeNil)
-		So(secret, ShouldResemble, &Secret{
-			Current: []byte{7, 8, 9},
-			Previous: [][]byte{
-				{4, 5, 6},
-			},
-		})
-
-		// An older version is disabled.
-		client.disableVersion(2)
-		secret, err = src.ReadSecret(ctx)
-		So(err, ShouldBeNil)
-		So(secret, ShouldResemble, &Secret{
-			Current: []byte{7, 8, 9},
-		})
-
-		// An older version is deleted.
-		client.deleteVersion(2)
-		secret, err = src.ReadSecret(ctx)
-		So(err, ShouldBeNil)
-		So(secret, ShouldResemble, &Secret{
-			Current: []byte{7, 8, 9},
-		})
-
-		// The current version is disabled as well, it breaks the secret.
-		client.disableVersion(3)
-		_, err = src.ReadSecret(ctx)
-		So(err, ShouldNotBeNil)
-	})
 }
 
-type mockedSMClient struct {
-	project string
-	secret  string
+type secretManagerMock struct {
+	calls []string
 
-	versions map[string]*mockedVersion
+	versions map[string]string // full ref => a value or "" if disabled
+	latest   map[string]int    // latest ref => version number
 }
 
-type mockedVersion struct {
-	name     string
-	data     []byte
-	disabled bool
-}
-
-func (c *mockedSMClient) versionID(ver interface{}) string {
-	return fmt.Sprintf("projects/%s/secrets/%s/versions/%v", c.project, c.secret, ver)
-}
-
-func (c *mockedSMClient) setVersion(num int, val []byte) {
-	mv := &mockedVersion{
-		name: c.versionID(num),
-		data: val,
+func (sm *secretManagerMock) createVersion(project, name, value string) string {
+	if sm.versions == nil {
+		sm.versions = make(map[string]string, 1)
+		sm.latest = make(map[string]int, 1)
 	}
-	c.versions[c.versionID(num)] = mv
-	c.versions[c.versionID("latest")] = mv
+
+	latestRef := fmt.Sprintf("projects/%s/secrets/%s/versions/latest", project, name)
+
+	nextVer := sm.latest[latestRef] + 1
+	versionRef := fmt.Sprintf("projects/%s/secrets/%s/versions/%d", project, name, nextVer)
+
+	sm.versions[versionRef] = value
+	sm.latest[latestRef] = nextVer
+
+	return versionRef
 }
 
-func (c *mockedSMClient) disableVersion(num int) {
-	c.versions[c.versionID(num)].disabled = true
+func (sm *secretManagerMock) disableVersion(ref string) {
+	sm.versions[ref] = ""
 }
 
-func (c *mockedSMClient) deleteVersion(num int) {
-	delete(c.versions, c.versionID(num))
+func (sm *secretManagerMock) deleteVersion(ref string) {
+	delete(sm.versions, ref)
 }
 
-func (c *mockedSMClient) AccessSecretVersion(ctx context.Context, req *secretmanagerpb.AccessSecretVersionRequest, opts ...gax.CallOption) (*secretmanagerpb.AccessSecretVersionResponse, error) {
-	switch ver := c.versions[req.Name]; {
-	case ver == nil:
+func (sm *secretManagerMock) AccessSecretVersion(_ context.Context, req *secretmanagerpb.AccessSecretVersionRequest, _ ...gax.CallOption) (*secretmanagerpb.AccessSecretVersionResponse, error) {
+	sm.calls = append(sm.calls, req.Name)
+
+	// Recognize ".../versions/latest" aliases.
+	versionRef := req.Name
+	if ver := sm.latest[versionRef]; ver != 0 {
+		versionRef = strings.ReplaceAll(versionRef, "/versions/latest", fmt.Sprintf("/versions/%d", ver))
+	}
+
+	switch val, ok := sm.versions[versionRef]; {
+	case !ok:
 		return nil, status.Errorf(codes.NotFound, "no such version")
-	case ver.disabled:
-		return nil, status.Errorf(codes.FailedPrecondition, "disabled version")
+	case val == "":
+		return nil, status.Errorf(codes.FailedPrecondition, "the version is disabled")
 	default:
 		return &secretmanagerpb.AccessSecretVersionResponse{
-			Name:    ver.name,
-			Payload: &secretmanagerpb.SecretPayload{Data: ver.data},
+			Name:    versionRef,
+			Payload: &secretmanagerpb.SecretPayload{Data: []byte(val)},
 		}, nil
 	}
-}
-
-func (c *mockedSMClient) Close() error {
-	return nil
 }
