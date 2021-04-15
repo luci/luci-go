9a85753a45d2659c0576909392ec1c290127da25
Joanna Wang <jojwang@google.com>
2021-04-14 19:49:35 -0700

    [swarming]Move ProcessCommand and ReplaceParameter to shared package.
    Bug:1188473
    Change-Id: I14f5ce3e79125e9b5f955a0399887a1a27159405

--- cmdrunner/command_test.go
+++ cmdrunner/command_test.go
@@ -31,62 +31,6 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestReplaceParameters(t *testing.T) {
-	t.Parallel()
-
-	Convey("replaceParameters", t, func() {
-		ctx := context.Background()
-
-		Convey("test EXECUTABLE_SUFFIX", func() {
-			arg, err := replaceParameters(ctx, "program${EXECUTABLE_SUFFIX}", "", "")
-			So(err, ShouldBeNil)
-			if runtime.GOOS == "windows" {
-				So(arg, ShouldEqual, "program.exe")
-			} else {
-				So(arg, ShouldEqual, "program")
-			}
-		})
-
-		Convey("test ISOLATED_OUTDIR", func() {
-			arg, err := replaceParameters(ctx, "${ISOLATED_OUTDIR}/result.txt", "out", "")
-			So(err, ShouldBeNil)
-			So(arg, ShouldEqual, filepath.Join("out", "result.txt"))
-		})
-
-		Convey("test SWARMING_BOT_FILE", func() {
-			arg, err := replaceParameters(ctx, "${SWARMING_BOT_FILE}/config", "", "cfgdir")
-			So(err, ShouldBeNil)
-			So(arg, ShouldEqual, filepath.Join("cfgdir", "config"))
-		})
-	})
-}
-
-func TestProcessCommand(t *testing.T) {
-	t.Parallel()
-
-	Convey("processCommand", t, func() {
-		ctx := context.Background()
-		args, err := processCommand(ctx, []string{
-			"program${EXECUTABLE_SUFFIX}",
-			"${ISOLATED_OUTDIR}/result.txt",
-			"${SWARMING_BOT_FILE}/config",
-		}, "out", "cfgdir")
-
-		So(err, ShouldBeNil)
-
-		executableSuffix := ""
-		if runtime.GOOS == "windows" {
-			executableSuffix = ".exe"
-		}
-
-		So(args, ShouldResemble, []string{
-			"program" + executableSuffix,
-			filepath.Join("out", "result.txt"),
-			filepath.Join("cfgdir", "config"),
-		})
-	})
-}
-
 func TestGetCommandEnv(t *testing.T) {
 	t.Parallel()
 	originalEnvironSystem := environSystem
