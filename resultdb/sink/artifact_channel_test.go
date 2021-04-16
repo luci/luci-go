e89c051cddc525cef6f1ca944870a4f797b085f3
Scott Lee <ddoman@chromium.org>
2021-04-16 18:42:47 +0000

    [resultdb] add newUploadTask
    newUploadTask() calls stat() to find the size of the artifact content,
    and returns an uploadTask with the artifact information.
    
    newUploadTask() is called inside sinkServer.ReportTestResults() and
    sinkServer.ReportInvocationLevelArtifacts(). It means that
    Sink will return an error to the client if Sink fails to obtain
    the information of the given artifact files,
    when the request was received.
    
    Change-Id: Icfc85c2e4c7fbbb62ab1a8806c6c2dc4a734a429
    Bug: 1149662

--- resultdb/sink/artifact_channel_test.go
+++ resultdb/sink/artifact_channel_test.go
@@ -18,15 +18,20 @@ import (
 	"context"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+	pb "go.chromium.org/luci/resultdb/proto/v1"
+	sinkpb "go.chromium.org/luci/resultdb/sink/proto/v1"
 )
 
 func TestArtifactChannel(t *testing.T) {
 	t.Parallel()
 
 	Convey("schedule", t, func() {
+		name := "invocations/inv/artifacts/art1"
 		ctx := context.Background()
 		cfg := testServerConfig("127.0.0.1:123", "secret")
 		reqCh := make(chan *http.Request, 1)
@@ -36,10 +41,12 @@ func TestArtifactChannel(t *testing.T) {
 				return &http.Response{StatusCode: http.StatusNoContent}, nil
 			},
 		)
-		ac := newArtifactChannel(ctx, &cfg)
 
-		name := "invocations/inv/artifacts/art1"
-		ac.schedule(&uploadTask{name, testArtifactWithContents([]byte("content"))})
+		task, err := newUploadTask(name, testArtifactWithContents([]byte("content")))
+		So(err, ShouldBeNil)
+
+		ac := newArtifactChannel(ctx, &cfg)
+		ac.schedule(task)
 		ac.closeAndDrain(ctx)
 		req := <-reqCh
 		So(req, ShouldNotBeNil)
@@ -49,5 +56,78 @@ func TestArtifactChannel(t *testing.T) {
 		body, err := ioutil.ReadAll(req.Body)
 		So(err, ShouldBeNil)
 		So(body, ShouldResemble, []byte("content"))
+	})
+}
+
+func TestUploadTask(t *testing.T) {
+	t.Parallel()
+
+	Convey("newUploadTask", t, func() {
+		name := "invocations/inv/artifacts/art1"
+		fArt := testArtifactWithFile(func(f *os.File) {
+			_, err := f.Write([]byte("content"))
+			So(err, ShouldBeNil)
+		})
+		fArt.ContentType = "plain/text"
+		defer os.Remove(fArt.GetFilePath())
+
+		Convey("works", func() {
+			t, err := newUploadTask(name, fArt)
+			So(err, ShouldBeNil)
+			So(t, ShouldResemble, &uploadTask{art: fArt, artName: name, size: int64(len("content"))})
+		})
+
+		Convey("fails", func() {
+			// stat error
+			So(os.Remove(fArt.GetFilePath()), ShouldBeNil)
+			_, err := newUploadTask(name, fArt)
+			So(err, ShouldErrLike, "querying file info")
+
+			// is a directory
+			path, err := ioutil.TempDir("", "foo")
+			So(err, ShouldBeNil)
+			defer os.RemoveAll(path)
+			fArt.Body.(*sinkpb.Artifact_FilePath).FilePath = path
+			_, err = newUploadTask(name, fArt)
+			So(err, ShouldErrLike, "is a directory")
+		})
+	})
+
+	Convey("CreateRequest", t, func() {
+		name := "invocations/inv/tests/t1/results/r1/artifacts/a1"
+		fArt := testArtifactWithFile(func(f *os.File) {
+			_, err := f.Write([]byte("content"))
+			So(err, ShouldBeNil)
+		})
+		fArt.ContentType = "plain/text"
+		defer os.Remove(fArt.GetFilePath())
+		ut, err := newUploadTask(name, fArt)
+		So(err, ShouldBeNil)
+
+		Convey("works", func() {
+			req, err := ut.CreateRequest()
+			So(err, ShouldBeNil)
+			So(req, ShouldResembleProto, &pb.CreateArtifactRequest{
+				Parent: "invocations/inv/tests/t1/results/r1",
+				Artifact: &pb.Artifact{
+					ArtifactId:  "a1",
+					ContentType: "plain/text",
+					SizeBytes:   int64(len("content")),
+					Contents:    []byte("content"),
+				},
+			})
+		})
+
+		Convey("fails", func() {
+			// the artifact content changed.
+			So(ioutil.WriteFile(fArt.GetFilePath(), []byte("surprise!!"), 0), ShouldBeNil)
+			_, err := ut.CreateRequest()
+			So(err, ShouldErrLike, "the size of the artifact contents changed")
+
+			// the file no longer exists.
+			So(os.Remove(fArt.GetFilePath()), ShouldBeNil)
+			_, err = ut.CreateRequest()
+			So(err, ShouldErrLike, "open "+fArt.GetFilePath()) // no such file or directory
+		})
 	})
 }
