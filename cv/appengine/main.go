448c694c08eceeeb0f23a2fc348309ba883a0de2
Andrii Shyshkalov <tandrii@google.com>
2021-04-18 20:22:16 +0200

    Reland "[cv] cache Gerrit clients."
    This is a reland of 5d910a52ee66ba82a7c3a6254526298572e23abd
    This fixes a bug in original CL and also re-uses base transport
    across all clients by creating it at factory creation itself,
    which happens at server startup.
    
    Original change's description:
    > [cv] cache Gerrit clients.
    >
    > This is a minor optimization to reduce memory churn.
    >
    > R=qyearsley, yiwzhang
    >
    > Change-Id: I6f2b4bafcaa3cc6684c1c9422d0c1a4d4dfedda2
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2826049
    > Auto-Submit: Andrii Shyshkalov <tandrii@google.com>
    > Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    > Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    > Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    
    Change-Id: Ic607a5e3106bd5d17d7651f1e002da1b689ca4cc

--- cv/appengine/main.go
+++ cv/appengine/main.go
@@ -62,7 +62,14 @@ func main() {
 		if srv.Options.CloudProject == "luci-change-verifier-dev" {
 			srv.Context = common.SetDev(srv.Context)
 		}
-		srv.Context = gerrit.UseProd(srv.Context)
+
+		switch ctx, err := gerrit.UseProd(srv.Context); {
+		case err != nil:
+			return nil
+		default:
+			srv.Context = ctx
+		}
+
 		var err error
 		if srv.Context, err = tree.InstallProd(srv.Context); err != nil {
 			return err
