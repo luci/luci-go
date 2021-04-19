4f8e799d08694df63a93f004c7bfb0bc817490cf
Weiwei Lin <weiweilin@google.com>
2021-04-19 11:43:45 +1000

    [resultui] implement a standalone error handling component
    R=bryner, nodir
    
    Bug: 1191159,1199597
    Change-Id: I15f7756887e99ac2ca3dcea36c58c288af73dfe1

---
+++ milo/frontend/resultui/src/components/error_handler.ts
@@ -0,0 +1,175 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview
+ * This files contains utilities for handling errors originated from an element.
+ *
+ * To use this component, you need to
+ * 1. Wrap the element that may throw an error in <milo-error-handler />. The
+ *   element doesn't need to be the direct child of <milo-error-handler /> and
+ *   can be in shadow DOM.
+ * 2. Wrap the function that may throw in reportError or reportErrorAsync.
+ *
+ * For example:
+ * ```typescript
+ * @customElement('some-component-that-may-throw')
+ * export class SomeComponentThatMayThrow extends LitElement {
+ *   protected render = reportError.bind(this)(() => {
+ *     // Rendering code goes here.
+ *   });
+ * }
+ *
+ * @customElement('parent-element')
+ * export class ParentElement extends LitElement {
+ *   protected render() {
+ *     return html`
+ *       <milo-error-handler>
+ *         <some-component-that-may-throw></some-component-that-may-throw>
+ *       </milo-error-handler>
+ *     `;
+ *   }
+ * }
+ * ```
+ *
+ * You can also specify a fallback function:
+ * ```typescript
+ * @customElement('some-component-that-may-throw')
+ * export class SomeComponentThatMayThrow extends LitElement {
+ *   protected render = reportError.bind(this)(
+ *     () => {
+ *     // Rendering code goes here.
+ *     },
+ *     () => html`there was an error`
+ *   );
+ * }
+ * ```
+ *
+ * If an element have multiple <milo-error-handler> ancestors, only the closest
+ * <milo-error-handler> ancestor will receive the error event.
+ */
+
+import { css, customElement, html, LitElement, property } from 'lit-element';
+
+import commonStyle from '../styles/common_style.css';
+
+const EVENT_TYPE = 'error-' + Math.random().toString();
+
+/**
+ * Wraps the specified fn. Whenever fn throws, dispatches an error event from
+ * `this` element.
+ */
+export function reportError<T extends unknown[], V>(
+  this: Element,
+  fn: (...params: T) => V,
+  defaultFn?: (err: unknown, ...params: T) => V
+): (...params: T) => V {
+  return (...params: T) => {
+    try {
+      return fn(...params);
+    } catch (e) {
+      this.dispatchEvent(
+        new ErrorEvent(EVENT_TYPE, {
+          error: e,
+          message: e.toString(),
+          composed: true,
+          bubbles: true,
+        })
+      );
+      if (defaultFn) {
+        return defaultFn(e, ...params);
+      }
+      throw e;
+    }
+  };
+}
+
+/**
+ * Wraps the specified async fn. Whenever fn throws, dispatches an error event
+ * from `this` element.
+ */
+export function reportErrorAsync<T extends unknown[], V>(
+  this: Element,
+  fn: (...params: T) => Promise<V>,
+  defaultFn?: (err: unknown, ...params: T) => Promise<V>
+): (...params: T) => Promise<V> {
+  return async (...params: T) => {
+    try {
+      // Don't elide await here or immediate errors won't be cached.
+      return await fn(...params);
+    } catch (e) {
+      this.dispatchEvent(
+        new ErrorEvent(EVENT_TYPE, {
+          error: e,
+          message: e.toString(),
+          composed: true,
+          bubbles: true,
+        })
+      );
+      if (defaultFn) {
+        return defaultFn(e, ...params);
+      }
+      throw e;
+    }
+  };
+}
+
+/**
+ * Handles error events from the decedents.
+ */
+@customElement('milo-error-handler')
+export class ErrorHandlerElement extends LitElement {
+  @property() errorMsg: string | null = null;
+
+  private errorHandler = (e: Event) => {
+    const event = e as ErrorEvent;
+    event.stopPropagation();
+    this.errorMsg = event.message;
+  };
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.addEventListener(EVENT_TYPE, this.errorHandler);
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener(EVENT_TYPE, this.errorHandler);
+    super.disconnectedCallback();
+  }
+
+  protected render() {
+    return this.errorMsg === null
+      ? html`<slot></slot>`
+      : html`
+          <div id="error-label">An error occurred:</div>
+          <pre id="error-message">${this.errorMsg}</pre>
+        `;
+  }
+
+  static styles = [
+    commonStyle,
+    css`
+      #error-label {
+        margin: 8px 16px;
+      }
+
+      #error-message {
+        white-space: pre-wrap;
+        margin: 8px 16px;
+        background-color: var(--block-background-color);
+        padding: 5px;
+      }
+    `,
+  ];
+}
