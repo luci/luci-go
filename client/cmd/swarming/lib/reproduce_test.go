40a27a6d56c5a8d578fd4536b6094e069a1e5c9d
Joanna Wang <jojwang@google.com>
2021-04-20 14:59:04 -0700

    fix isolate.
    Change-Id: Ia41b95936283b3602f3cce89c6e2757f2af7e382

--- client/cmd/swarming/lib/reproduce_test.go
+++ client/cmd/swarming/lib/reproduce_test.go
@@ -132,6 +132,10 @@ func TestPrepareTaskRequestEnvironment(t *testing.T) {
 										Value: []string{"egg", "rooster"},
 									},
 								},
+								InputsRef: &swarming.SwarmingRpcsFilesRef{
+									Isolatedserver: "server-url",
+									Namespace:      "chicken-files",
+								},
 								CasInputRoot: &swarming.SwarmingRpcsCASReference{
 									CasInstance: "CAS-instance",
 								},
