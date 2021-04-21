3e09a7947d70f34c543a886e7ed32255b37b84b8
Scott Lee <ddoman@chromium.org>
2021-04-21 18:15:54 +0000

    [resultdb] upload small artifacts in batches
    Bug: 1149662
    Change-Id: I07a9fe533b97a8d3d69779bf8bd8af6ef0600517

--- resultdb/sink/sink.go
+++ resultdb/sink/sink.go
@@ -131,6 +131,12 @@ type ServerConfig struct {
 	// ancestor directory) in the map and append the directory's tags to the
 	// test results' tags.
 	LocationTags *sinkpb.LocationTags
+
+	// MaxBatchableArtifactSize is the maximum size of an artifact that can be added to
+	// batchChannel.
+	//
+	// Must be < 10MiB, and NewServer panics, otherwise.
+	MaxBatchableArtifactSize int64
 }
 
 // Validate validates all the config fields.
@@ -196,6 +202,14 @@ func NewServer(ctx context.Context, cfg ServerConfig) (*Server, error) {
 	}
 	if cfg.TestResultChannelMaxLeases == 0 {
 		cfg.TestResultChannelMaxLeases = DefaultTestResultChannelMaxLeases
+	}
+	if cfg.MaxBatchableArtifactSize == 0 {
+		cfg.MaxBatchableArtifactSize = 2 * 1024 * 1024
+	} else if cfg.MaxBatchableArtifactSize > 10*1024*1024 {
+		panic(fmt.Sprintf(
+			"NewServer: ServerConfig.MaxBatchableArtifactSize is greater than 10MiB: %d",
+			cfg.MaxBatchableArtifactSize,
+		))
 	}
 
 	// extract the invocation ID from cfg.Invocation so that other modules don't need to
