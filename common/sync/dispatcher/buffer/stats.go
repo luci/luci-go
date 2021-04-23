400441d78773e262c69a2f042d79a3be5ba06ac3
Robert Iannucci <iannucci@chromium.org>
2021-04-23 16:55:54 -0700

    [dispatcher/buffer] Abstract stats updating code.
    This is in preparation for adding item byte size restrictions to the
    buffer.
    
    R=ddoman, tandrii
    
    Change-Id: Ib0d81341a546199d20dee6b7d1196dc1b320a679

---
+++ common/sync/dispatcher/buffer/stats.go
@@ -0,0 +1,120 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buffer
+
+import "fmt"
+
+// Stats is a block of information about the Buffer's present state.
+type Stats struct {
+	// UnleasedItemCount is the total number of items (i.e. objects passed to
+	// AddNoBlock) which are currently owned by the Buffer but are not currently
+	// leased. This includes:
+	//    * Items buffered, but not yet cut into a Batch.
+	//    * Items in unleased Batches.
+	//
+	// UnleasedItemSizeBytes is the size in bytes of the same items.
+	UnleasedItemCount int
+
+	// LeasedItemCount is the total number of items (i.e. objects passed to
+	// AddNoBlock) which are currently owned by the Buffer and are in active
+	// leases.
+	//
+	// LeasedItemSizeBytes is the size in bytes of the same items.
+	LeasedItemCount int
+
+	// DroppedLeasedItemCount is the total number of items (i.e. objects passed to
+	// AddNoBlock) which were part of leases, but where those leases have been
+	// dropped (due to FullBehavior policy).
+	//
+	// DroppedLeasedItemSizeBytes is the size in bytes of the same items.
+	DroppedLeasedItemCount int
+}
+
+// Empty returns true iff the Buffer is totally empty (has zero user-provided
+// items).
+func (s Stats) Empty() bool {
+	return s.Total() == 0
+}
+
+// Total returns the total number of items currently referenced by the Buffer.
+func (s Stats) Total() int {
+	return s.UnleasedItemCount + s.LeasedItemCount + s.DroppedLeasedItemCount
+}
+
+type category int
+
+const (
+	categoryUnleased category = iota
+	categoryLeased
+	categoryDropped
+)
+
+type action int
+
+const (
+	actionAdd action = iota
+	actionDel
+	actionMove
+)
+
+func (s *Stats) getCategoryVars(cat category) (count *int) {
+	switch cat {
+	case categoryUnleased:
+		count = &s.UnleasedItemCount
+	case categoryLeased:
+		count = &s.LeasedItemCount
+	case categoryDropped:
+		count = &s.DroppedLeasedItemCount
+	default:
+		panic(fmt.Errorf("unknown category %d", cat))
+	}
+	return
+}
+
+func (s *Stats) addOneUnleased() {
+	s.update(1, actionAdd, categoryUnleased)
+}
+
+func (s *Stats) add(b *Batch, to category) {
+	s.update(b.countedSize, actionAdd, to)
+}
+
+func (s *Stats) del(b *Batch, to category) {
+	s.update(b.countedSize, actionDel, to)
+}
+
+func (s *Stats) mv(b *Batch, from, to category) {
+	s.update(b.countedSize, actionMove, from, to)
+}
+
+func (s *Stats) update(items int, act action, from category, to ...category) {
+	switch {
+	case act == actionAdd:
+		count := s.getCategoryVars(from)
+		*count += items
+
+	case act == actionDel || act == actionMove:
+		count := s.getCategoryVars(from)
+		*count -= items
+
+		if act == actionMove {
+			count = s.getCategoryVars(to[0])
+			*count += items
+		}
+
+	default:
+		panic(fmt.Errorf("unknown action %d", act))
+	}
+}
