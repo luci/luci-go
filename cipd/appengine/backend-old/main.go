0c0d8f95a63901c998ce0ff70c001356f967e3b8
Vadim Shtayura <vadimsh@chromium.org>
2021-04-22 17:10:46 -0700

    [cipd] Setup "backend-old" module.
    It is the exact copy of "backend" now. Later "backend" will be
    converted to a go113 module, but "backend-old" will stay as it.
    
    Update cron.yaml and dispatch.yaml accordingly.
    
    R=tandrii@chromium.org
    BUG=1201436
    
    Change-Id: Ic7c49089744f0a47eed0ca4cdffd50c0ed9818cf

---
+++ cipd/appengine/backend-old/main.go
@@ -0,0 +1,57 @@
+// Copyright 2017 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Binary backend implements HTTP server that handles task queues and crons.
+package main
+
+import (
+	"net/http"
+
+	"google.golang.org/appengine"
+
+	"go.chromium.org/luci/appengine/gaemiddleware"
+	"go.chromium.org/luci/appengine/gaemiddleware/standard"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/server/router"
+
+	"go.chromium.org/luci/cipd/appengine/impl"
+	"go.chromium.org/luci/cipd/appengine/impl/monitoring"
+)
+
+func main() {
+	r := router.New()
+	base := standard.Base()
+	cron := base.Extend(gaemiddleware.RequireCron)
+
+	standard.InstallHandlers(r)
+	impl.InitForGAE1(r, base)
+
+	r.GET("/internal/cron/bqlog/events-flush", cron,
+		func(c *router.Context) {
+			impl.FlushEventsToBQGAE1(c.Context)
+			c.Writer.WriteHeader(http.StatusOK)
+		},
+	)
+	r.GET("/internal/cron/import-config", cron,
+		func(c *router.Context) {
+			if err := monitoring.ImportConfig(c.Context); err != nil {
+				errors.Log(c.Context, err)
+			}
+			c.Writer.WriteHeader(http.StatusOK)
+		},
+	)
+
+	http.DefaultServeMux.Handle("/", r)
+	appengine.Main()
+}
