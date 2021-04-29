c82ea39693d3a50cef0a203983fac05b88e46648
Weiwei Lin <weiweilin@google.com>
2021-04-29 10:09:35 +1000

    [resultui] separate out step list to a standalone component
    The step list will contain more filtering logic in the future.
    This allows us to reuse the step list in the overview tab in a future
    CL.
    
    R=gavinmak@google.com, nodir
    
    Change-Id: I439f8e05d4264933e13a081f38da464bbb9736f2
    Bug: 1186402, 1189929, 1194093, 1197348

--- milo/frontend/resultui/src/components/build_step_list/build_step_entry.ts
+++ milo/frontend/resultui/src/components/build_step_list/build_step_entry.ts
@@ -19,19 +19,19 @@ import { html, render } from 'lit-html';
 import { styleMap } from 'lit-html/directives/style-map';
 import { computed, observable } from 'mobx';
 
-import './copy_to_clipboard';
-import './expandable_entry';
-import './log';
-import './pin_toggle';
-import { consumeConfigsStore, UserConfigsStore } from '../context/user_configs';
-import { GA_ACTIONS, GA_CATEGORIES, trackEvent } from '../libs/analytics_utils';
-import { BUILD_STATUS_CLASS_MAP, BUILD_STATUS_DISPLAY_MAP, BUILD_STATUS_ICON_MAP } from '../libs/constants';
-import { lazyRendering, RenderPlaceHolder } from '../libs/enter_view_observer';
-import { displayCompactDuration, displayDuration, NUMERIC_TIME_FORMAT } from '../libs/time_utils';
-import { StepExt } from '../models/step_ext';
-import colorClasses from '../styles/color_classes.css';
-import commonStyle from '../styles/common_style.css';
-import { HideTooltipEventDetail, ShowTooltipEventDetail } from './tooltip';
+import '../copy_to_clipboard';
+import '../expandable_entry';
+import '../log';
+import '../pin_toggle';
+import { consumeConfigsStore, UserConfigsStore } from '../../context/user_configs';
+import { GA_ACTIONS, GA_CATEGORIES, trackEvent } from '../../libs/analytics_utils';
+import { BUILD_STATUS_CLASS_MAP, BUILD_STATUS_DISPLAY_MAP, BUILD_STATUS_ICON_MAP } from '../../libs/constants';
+import { lazyRendering, RenderPlaceHolder } from '../../libs/enter_view_observer';
+import { displayCompactDuration, displayDuration, NUMERIC_TIME_FORMAT } from '../../libs/time_utils';
+import { StepExt } from '../../models/step_ext';
+import colorClasses from '../../styles/color_classes.css';
+import commonStyle from '../../styles/common_style.css';
+import { HideTooltipEventDetail, ShowTooltipEventDetail } from '../tooltip';
 
 /**
  * Renders a step.
