af6d3b79b8c57684b6d8aeb00964ba02b63f4862
Weiwei Lin <weiweilin@google.com>
2021-05-06 10:02:18 +1000

    [resultui] display num of test failures next to the results tab chip
    1. Modify tab_bar to support adding additional widgets.
    2. Add test-count-indicator to show num of most severe test failures.
    3. Add test-count-indicator to the invocation page and build page.
    4. Fix broken tests.
    
    R=gavinmak@google.com
    
    Bug: 1203806
    Change-Id: I492bd07f17fa7f0b495e9faf4dec0c79516caaeb

---
+++ milo/frontend/resultui/src/components/test_count_indicator.ts
@@ -0,0 +1,151 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { css, customElement, html } from 'lit-element';
+import { reaction } from 'mobx';
+
+import './dot_spinner';
+import { consumeInvocationState, InvocationState } from '../context/invocation_state';
+import { consumer } from '../libs/context';
+import { errorHandler, forwardWithoutMsg, reportErrorAsync } from '../libs/error_handler';
+import { LoadingStage } from '../models/test_loader';
+import { MiloBaseElement } from './milo_base';
+
+/**
+ * Format number with a cap.
+ */
+function formatNum(num: number, hasMore: boolean, cap?: number) {
+  if (cap && num > cap) {
+    return `${cap}+`;
+  } else if (hasMore) {
+    return `${num}+`;
+  }
+  return `${num}`;
+}
+
+/**
+ * Renders the number of most severe test failures in a badge.
+ */
+@customElement('milo-test-count-indicator')
+@errorHandler(forwardWithoutMsg, () => html``)
+@consumer
+export class TestCountIndicatorElement extends MiloBaseElement {
+  @consumeInvocationState invState!: InvocationState;
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    // When a new test loader is received, load the first page.
+    this.addDisposer(
+      reaction(
+        () => this.invState.testLoader,
+        (testLoader) => reportErrorAsync.bind(this)(async () => testLoader?.loadFirstPageOfTestVariants())(),
+        { fireImmediately: true }
+      )
+    );
+  }
+
+  protected render() {
+    const testLoader = this.invState.testLoader;
+    if (!testLoader?.firstPageLoaded) {
+      return html`<milo-dot-spinner></milo-dot-spinner>`;
+    }
+
+    if (testLoader.unfilteredUnexpectedVariantsCount > 0) {
+      return html`<div
+        id="unexpected"
+        title=${formatNum(testLoader.unfilteredUnexpectedVariantsCount, !testLoader.loadedAllUnexpectedVariants) +
+        ' unexpected tests'}
+      >
+        ${formatNum(testLoader.unfilteredUnexpectedVariantsCount, !testLoader.loadedAllUnexpectedVariants, 99)}
+      </div>`;
+    }
+
+    if (testLoader.unfilteredUnexpectedlySkippedVariantsCount > 0) {
+      return html`<div
+        id="unexpectedly-skipped"
+        title=${formatNum(
+          testLoader.unfilteredUnexpectedlySkippedVariantsCount,
+          !testLoader.loadedAllUnexpectedVariants
+        ) + ' unexpectedly skipped tests'}
+      >
+        ${formatNum(
+          testLoader.unfilteredUnexpectedlySkippedVariantsCount,
+          testLoader.stage < LoadingStage.LoadingUnexpectedlySkipped
+        )}
+      </div>`;
+    }
+
+    if (testLoader.unfilteredFlakyVariantsCount > 0) {
+      return html`<div
+        id="flaky"
+        title=${formatNum(testLoader.unfilteredFlakyVariantsCount, !testLoader.loadedAllUnexpectedVariants) +
+        ' flaky tests'}
+      >
+        ${formatNum(testLoader.unfilteredFlakyVariantsCount, testLoader.stage < LoadingStage.LoadingFlaky)}
+      </div>`;
+    }
+
+    return html`<mwc-icon id="expected" title="all tests passed">check_circle</mwc-icon>`;
+  }
+
+  static styles = css`
+    :host {
+      display: inline-block;
+      width: 30px;
+    }
+
+    milo-dot-spinner {
+      color: var(--active-color);
+      width: 30px;
+      font-size: 13px;
+    }
+
+    div {
+      color: white;
+      display: inline-block;
+      padding: 0.1em;
+      padding-top: 0.2em;
+      font-size: 75%;
+      font-weight: 700;
+      line-height: 13px;
+      text-align: center;
+      white-space: nowrap;
+      vertical-align: bottom;
+      border-radius: 0.25rem;
+      margin-bottom: 2px;
+      box-sizing: border-box;
+      width: 30px;
+    }
+    #unexpected {
+      background-color: var(--failure-color);
+    }
+    #expectedly-skipped {
+      background-color: var(--critical-failure-color);
+    }
+    #flaky {
+      background-color: var(--warning-color);
+    }
+
+    mwc-icon {
+      --mdc-icon-size: 18px;
+      vertical-align: bottom;
+      width: 20px;
+      margin-right: 10px;
+    }
+    #expected {
+      color: var(--success-color);
+    }
+  `;
+}
