6f82746881ab1632d9d069a1d6ed9d772cc3954a
Andrii Shyshkalov <tandrii@google.com>
2021-05-07 21:09:40 +0000

    [cv][cqdfake] implement candidate fetching of a CQDFake.
    R=qyearsley, yiwzhang
    
    Bug: 1160019
    Change-Id: I224c4e8e249240101d1721edab8e3fe595ad0082
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2878717
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>

--- cv/internal/migration/cqdfake/cqd_test.go
+++ cv/internal/migration/cqdfake/cqd_test.go
@@ -15,13 +15,16 @@
 package cqdfake
 
 import (
-	"context"
+	"fmt"
+	"strings"
 	"testing"
+	"time"
 
-	"go.chromium.org/luci/common/clock/testclock"
-
+	"go.chromium.org/luci/common/clock"
 	bqpb "go.chromium.org/luci/cv/api/bigquery/v1"
+	cvbqpb "go.chromium.org/luci/cv/api/bigquery/v1"
 	migrationpb "go.chromium.org/luci/cv/api/migration"
+	"go.chromium.org/luci/cv/internal/cvtesting"
 	"go.chromium.org/luci/cv/internal/migration"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -30,25 +33,86 @@ import (
 func TestCQDFake(t *testing.T) {
 	t.Parallel()
 
-	Convey("Smoke test for CQDFake", t, func() {
-		ctx := context.Background()
-		ctx, _ = testclock.UseTime(ctx, testclock.TestRecentTimeUTC)
+	Convey("CQDFake processes candidates in CQDaemon active mode", t, func() {
+		ct := cvtesting.Test{}
+		ctx, cancel := ct.SetUp()
+		defer cancel()
 
+		const lProject = "test"
+		const gHost = "gerrit.example.com"
 		cqd := CQDFake{
-			LUCIProject: "test",
+			LUCIProject: lProject,
 			CV:          &migration.MigrationServer{},
+			GFake:       ct.GFake,
 		}
+		ct.DisableCVRunManagement(ctx)
+
+		done4iterations := make(chan struct{})
+		var progress []string
+
+		candidatesCalls := 0
 		cqd.SetCandidatesClbk(func() []*migrationpb.Run {
-			return []*migrationpb.Run{
-				{
-					Attempt: &bqpb.Attempt{
-						Key: "beef01",
+			var res []*migrationpb.Run
+			candidatesCalls++
+			switch candidatesCalls {
+			case 1:
+				res = []*migrationpb.Run{
+					{
+						Attempt: &bqpb.Attempt{
+							Key:           "beef01",
+							GerritChanges: []*cvbqpb.GerritChange{{Host: gHost, Change: 1}},
+						},
 					},
-				},
+				}
+			case 2:
+				res = []*migrationpb.Run{
+					{
+						Attempt: &bqpb.Attempt{
+							Key:           "beef02",
+							GerritChanges: []*cvbqpb.GerritChange{{Host: gHost, Change: 2}},
+						},
+					},
+				}
+			case 3:
+			case 4:
+				close(done4iterations)
+			default:
+				// Don't record results of >4th iterations, which may occur since there
+				// is a race between closing done4iterations channel and cqd.Close()
+				// taking effect.
+				return nil
 			}
+			keys := strings.Join(cqd.ActiveAttemptKeys(), " ")
+			progress = append(progress, fmt.Sprintf("candidates #%d (prior: [%s])", candidatesCalls, keys))
+			return res
 		})
 
+		cqd.SetVerifyClbk(func(r *migrationpb.Run, _ bool) *migrationpb.Run {
+			progress = append(progress, fmt.Sprintf("verify %s", r.GetAttempt().GetKey()))
+			return r
+		})
+
+		// Override cvtesting timer callback to immediately unblock waiting CQDFake.
+		ct.Clock.SetTimerCallback(func(dur time.Duration, timer clock.Timer) {
+			ct.Clock.Add(dur)
+		})
+
+		// Start & wait for 4 iterations.
 		cqd.Start(ctx)
-		defer cqd.Close()
+		select {
+		case <-done4iterations:
+		case <-ctx.Done(): // avoid hanging forever.
+			panic(ctx.Err())
+		}
+		cqd.Close()
+
+		So(progress, ShouldResemble, []string{
+			`candidates #1 (prior: [])`,
+			`verify beef01`,
+			`candidates #2 (prior: [beef01])`,
+			`verify beef02`,
+			`candidates #3 (prior: [beef02])`,
+			`candidates #4 (prior: [])`,
+		})
 	})
 }
