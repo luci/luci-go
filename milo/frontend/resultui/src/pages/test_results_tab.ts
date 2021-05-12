9ee53e2d8b8399b27bbf93c67ed2fb56cea4be45
Weiwei Lin <weiweilin@google.com>
2021-05-12 13:05:14 +1000

    [resultui] show a warning when some tests could be missing
    R=gavinmak@google.com, nodir
    
    Bug: 1203806
    Change-Id: Idc30bac941d34b233495181124d83f77c83d490c

--- milo/frontend/resultui/src/pages/test_results_tab.ts
+++ milo/frontend/resultui/src/pages/test_results_tab.ts
@@ -133,6 +133,7 @@ export class TestResultsTabElement extends MiloBaseElement {
     }
 
     return html`
+      ${this.invocationState.warning ? html`<div id="warning">${this.invocationState.warning}</div>` : ''}
       <milo-test-variants-table
         style="--columns: ${this.invocationState.columnWidths.map((width) => width + 'px').join(' ')}"
       ></milo-test-variants-table>
@@ -184,6 +185,14 @@ export class TestResultsTabElement extends MiloBaseElement {
         border-left: 1px solid var(--divider-color);
         width: 0px;
         height: 100%;
+      }
+
+      #warning {
+        background-color: var(--warning-color);
+        border-bottom: 1px solid var(--divider-color);
+        height: 24px;
+        line-height: 24px;
+        text-align: center;
       }
     `,
   ];
