011284fdc2b5d19d455ee293f57a925bc6d64e2d
Weiwei Lin <weiweilin@google.com>
2021-05-19 01:33:18 +0000

    [resultui] implement dynamic time interval
    R=gavinmak@google.com, nodir
    
    Bug: 908892,1179820,1066400,1013061,1103078
    Change-Id: I02b4aca826620ee241bd57612e0477c435a29c24
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2896702
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- milo/frontend/resultui/src/pages/build_page/timeline_tab.ts
+++ milo/frontend/resultui/src/pages/build_page/timeline_tab.ts
@@ -50,6 +50,40 @@ const LIST_ITEM_HEIGHT = 16;
 const LIST_ITEM_X_OFFSET = STEP_MARGIN + TEXT_MARGIN + BORDER_SIZE;
 const LIST_ITEM_Y_OFFSET = STEP_MARGIN + (STEP_HEIGHT - LIST_ITEM_HEIGHT) / 2;
 
+const V_GRID_LINE_MAX_GAP = 80;
+const PREDEFINED_TIME_INTERVALS = [
+  // Values that can divide 1 day.
+  86400000, // 24hr
+  43200000, // 12hr
+  28800000, // 8hr
+  // Values that can divide 12 hours.
+  21600000, // 6hr
+  14400000, // 4hr
+  10800000, // 3hr
+  7200000, // 2hr
+  3600000, // 1hr
+  // Values that can divide 1 hour.
+  1800000, // 30min
+  1200000, // 20min
+  900000, // 15min
+  600000, // 10min
+  // Values that can divide 15 minutes.
+  300000, // 5min
+  180000, // 3min
+  120000, // 2min
+  60000, // 1min
+  // Values that can divide 1 minute.
+  30000, // 30s
+  20000, // 20s
+  15000, // 15s
+  10000, // 10s
+  // Values that can divide 15 seconds.
+  5000, // 5s
+  3000, // 3s
+  2000, // 2s
+  1000, // 1s
+];
+
 /**
  * A utility function that helps assigning appropriate list numbers to steps.
  * For example, if a step is the 1st child of the 2nd root step, the list number
@@ -92,6 +126,7 @@ export class TimelineTabElement extends MiloBaseElement {
   private bodyHeight!: number;
   private scaleTime!: d3.ScaleTime<number, number, never>;
   private scaleStep!: d3.ScaleLinear<number, number, never>;
+  private timeInterval!: d3.TimeInterval;
   private readonly now = Date.now();
 
   connectedCallback() {
@@ -140,6 +175,21 @@ export class TimelineTabElement extends MiloBaseElement {
       // Ensure the top and bottom borders are not rendered.
       .range([-HALF_BORDER_SIZE, this.bodyHeight + HALF_BORDER_SIZE]);
 
+    const maxInterval = (endTime - startTime + 2 * padding) / (this.bodyWidth / V_GRID_LINE_MAX_GAP);
+
+    // Assign a default value here to make TSC happy.
+    let interval = PREDEFINED_TIME_INTERVALS[0];
+
+    // Find the largest interval that is no larger than the maximum interval.
+    // Use linear search because the array is relatively short.
+    for (const predefined of PREDEFINED_TIME_INTERVALS) {
+      interval = predefined;
+      if (maxInterval >= predefined) {
+        break;
+      }
+    }
+    this.timeInterval = d3.timeMillisecond.every(interval)!;
+
     // Render each component.
     this.renderHeader();
     this.renderFooter();
@@ -157,7 +207,7 @@ export class TimelineTabElement extends MiloBaseElement {
     const headerRootGroup = svg
       .append('g')
       .attr('transform', `translate(${SIDE_PANEL_WIDTH}, ${AXIS_HEIGHT - HALF_BORDER_SIZE})`);
-    const axisTop = d3.axisTop(this.scaleTime).ticks(d3.timeMinute.every(5));
+    const axisTop = d3.axisTop(this.scaleTime).ticks(this.timeInterval);
     headerRootGroup.call(axisTop);
 
     // Top border for the side panel.
@@ -172,7 +222,7 @@ export class TimelineTabElement extends MiloBaseElement {
       .append('svg')
       .attr('viewport', `0 0 ${this.totalWidth} ${AXIS_HEIGHT}`);
     const footerRootGroup = svg.append('g').attr('transform', `translate(${SIDE_PANEL_WIDTH}, ${HALF_BORDER_SIZE})`);
-    const axisBottom = d3.axisBottom(this.scaleTime).ticks(d3.timeMinute.every(5));
+    const axisBottom = d3.axisBottom(this.scaleTime).ticks(this.timeInterval);
     footerRootGroup.call(axisBottom);
 
     // Bottom border for the side panel.
@@ -264,7 +314,7 @@ export class TimelineTabElement extends MiloBaseElement {
     // Grid lines
     const verticalGridLines = d3
       .axisTop(this.scaleTime)
-      .ticks(d3.timeMinute.every(5))
+      .ticks(this.timeInterval)
       .tickSize(-this.bodyHeight)
       .tickFormat(() => '');
     svg.append('g').attr('class', 'grid').call(verticalGridLines);
