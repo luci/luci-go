e72aed2697bdd7bc696cd06b5fe6e6adf71a1801
Weiwei Lin <weiweilin@google.com>
2021-05-19 11:12:53 +1000

    [resultui] add tests for the new timeline tab
    R=gavinmak@google.com, nodir
    
    Bug: 908892,1179820,1066400,1013061,1103078
    Change-Id: I9b6f6be10943a59a521d6cad8646d7d9b742856b

---
+++ milo/frontend/resultui/cypress/integration/timeline_page.spec.ts
@@ -0,0 +1,26 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+describe('Timeline Tab', () => {
+  it('should render timeline correctly', () => {
+    cy.visit('/p/chromium/builders/ci/android-marshmallow-arm64-rel-swarming/12479/timeline');
+    cy.get('#timeline', { includeShadowDom: true });
+    cy.matchImageSnapshot('timeline-tab');
+  });
+
+  it('can handle builds with no steps', () => {
+    cy.visit('/p/chromium/builders/ci/linux-rel-swarming/14569/timeline');
+    cy.get('#no-steps', { includeShadowDom: true }).contains('No steps were run.');
+  });
+});
