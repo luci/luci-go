f3c821a0b180729d333798b382bb2bcd5d5476ce
Weiwei Lin <weiweilin@google.com>
2021-05-25 16:40:46 +1000

    [resultui] fix unexpected cache miss after refactoring.
    1. Move module-level functions and variables in prefetch.ts to
    Prefetcher class, so they can be unit tested.
    2. Move cache resolution code to Prefetcher.respondWithPrefetched
    3. Removed additional cachedFetch declaration in service_worker_ext.ts
    that caused cache miss.
    4. Add unit tests.
    
    R=gavinmak@google.com, nodir
    
    Bug: 1187775,1209355
    Change-Id: Id5a66e0920bbd81e7924a53159383a74b22560de

---
+++ milo/frontend/resultui/src/prefetch_test.ts
@@ -0,0 +1,153 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { aTimeout } from '@open-wc/testing/index-no-side-effects';
+import { assert } from 'chai';
+import * as sinon from 'sinon';
+
+import { setAuthState } from './auth_state';
+import { PrpcClientExt } from './libs/prpc_client_ext';
+import { Prefetcher } from './prefetch';
+import { BUILD_FIELD_MASK, BuildsService } from './services/buildbucket';
+import { ResultDb } from './services/resultdb';
+
+describe('prefetch', () => {
+  let fetchStub: sinon.SinonStub<[RequestInfo, RequestInit | undefined], Promise<Response>>;
+  let respondWithStub: sinon.SinonStub<[Response | Promise<Response>], void>;
+  let prefetcher: Prefetcher;
+
+  // Helps generate fetch requests that are identical to the ones generated
+  // by the pRPC Clients.
+  let fetchInterceptor: sinon.SinonStub<[RequestInfo, RequestInit | undefined], Promise<Response>>;
+  let buildsService: BuildsService;
+  let resultdb: ResultDb;
+
+  beforeEach(async () => {
+    await setAuthState({ accessToken: 'access-token', userId: 'user-id', expiresAt: Infinity });
+
+    fetchStub = sinon.stub<[RequestInfo, RequestInit | undefined], Promise<Response>>();
+    respondWithStub = sinon.stub<[Response | Promise<Response>], void>();
+    prefetcher = new Prefetcher(CONFIGS, fetchStub);
+
+    fetchInterceptor = sinon.stub<[RequestInfo, RequestInit | undefined], Promise<Response>>();
+    buildsService = new BuildsService(
+      new PrpcClientExt({ host: CONFIGS.BUILDBUCKET.HOST, fetchImpl: fetchInterceptor }, () => 'access-token')
+    );
+    resultdb = new ResultDb(
+      new PrpcClientExt({ host: CONFIGS.RESULT_DB.HOST, fetchImpl: fetchInterceptor }, () => 'access-token')
+    );
+  });
+
+  it('prefetches build page resources', async () => {
+    const buildResponse = new Response(JSON.stringify({ buildId: '123456789' }));
+    fetchStub.onCall(0).resolves(buildResponse);
+
+    await prefetcher.prefetchResources(
+      new URL('https://luci-milo-dev.appspot.com/ui/p/chromium/builders/ci/Win7%20Tests%20(1)/116372')
+    );
+
+    await aTimeout(100);
+
+    const requestedUrls = fetchStub.getCalls().map((c) => new Request(...c.args).url);
+    assert.strictEqual(requestedUrls.length, 1);
+    assert.includeMembers(requestedUrls, [`https://${CONFIGS.BUILDBUCKET.HOST}/prpc/buildbucket.v2.Builds/GetBuild`]);
+
+    buildsService.getBuild({
+      builder: {
+        project: 'chromium',
+        bucket: 'ci',
+        builder: 'Win7 Tests (1)',
+      },
+      buildNumber: 116372,
+      fields: BUILD_FIELD_MASK,
+    });
+
+    const cacheHit = prefetcher.respondWithPrefetched(({
+      request: new Request(...fetchInterceptor.getCall(0).args),
+      respondWith: respondWithStub,
+    } as Partial<FetchEvent>) as FetchEvent);
+    const cachedRes = await respondWithStub.getCall(0).args[0];
+
+    assert.isTrue(cacheHit);
+    assert.strictEqual(fetchStub.callCount, 1);
+    assert.strictEqual(cachedRes, buildResponse);
+  });
+
+  it('prefetches build page resources when visiting a short build page url', async () => {
+    const buildResponse = new Response(JSON.stringify({ buildId: '123456789' }));
+    fetchStub.onCall(0).resolves(buildResponse);
+
+    await prefetcher.prefetchResources(new URL('https://luci-milo-dev.appspot.com/ui/b/123456789'));
+
+    await aTimeout(100);
+
+    const requestedUrls = fetchStub.getCalls().map((c) => new Request(...c.args).url);
+    assert.strictEqual(requestedUrls.length, 1);
+    assert.includeMembers(requestedUrls, [`https://${CONFIGS.BUILDBUCKET.HOST}/prpc/buildbucket.v2.Builds/GetBuild`]);
+
+    buildsService.getBuild({
+      id: '123456789',
+      fields: BUILD_FIELD_MASK,
+    });
+
+    const cacheHit = prefetcher.respondWithPrefetched(({
+      request: new Request(...fetchInterceptor.getCall(0).args),
+      respondWith: respondWithStub,
+    } as Partial<FetchEvent>) as FetchEvent);
+    const cachedRes = await respondWithStub.getCall(0).args[0];
+
+    assert.isTrue(cacheHit);
+    assert.strictEqual(fetchStub.callCount, 1);
+    assert.strictEqual(cachedRes, buildResponse);
+  });
+
+  it('prefetches artifact page resources', async () => {
+    const artifactResponse = new Response(
+      JSON.stringify({
+        name: 'invocations/inv-id/tests/test-id/results/result-id/artifacts/artifact-id',
+        artifactId: 'artifact-id',
+      })
+    );
+    fetchStub.onCall(0).resolves(artifactResponse);
+
+    await prefetcher.prefetchResources(
+      new URL(
+        // eslint-disable-next-line max-len
+        'https://luci-milo-dev.appspot.com/ui/artifact/raw/invocations/inv-id/tests/test-id/results/result-id/artifacts/artifact-id'
+      )
+    );
+
+    await aTimeout(100);
+
+    const requestedUrls = fetchStub.getCalls().map((c) => new Request(...c.args).url);
+    assert.strictEqual(requestedUrls.length, 1);
+    assert.includeMembers(requestedUrls, [
+      `https://${CONFIGS.RESULT_DB.HOST}/prpc/luci.resultdb.v1.ResultDB/GetArtifact`,
+    ]);
+
+    resultdb.getArtifact({
+      name: 'invocations/inv-id/tests/test-id/results/result-id/artifacts/artifact-id',
+    });
+
+    const cacheHit = prefetcher.respondWithPrefetched(({
+      request: new Request(...fetchInterceptor.getCall(0).args),
+      respondWith: respondWithStub,
+    } as Partial<FetchEvent>) as FetchEvent);
+    const cachedRes = await respondWithStub.getCall(0).args[0];
+
+    assert.isTrue(cacheHit);
+    assert.strictEqual(fetchStub.callCount, 1);
+    assert.strictEqual(cachedRes, artifactResponse);
+  });
+});
