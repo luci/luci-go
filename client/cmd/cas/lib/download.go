49862a38eaaafc5b50dec571497691725290e9ac
Takuto Ikuta <tikuta@chromium.org>
2021-06-01 08:00:57 +0000

    Revert "cas: log kvs files"
    This reverts commit 959b34cf7b896d81b232d6913e900e14d0e82173.
    
    Reason for revert:
    This is not necessary and too verbose now.
    
    Original change's description:
    > cas: log kvs files
    >
    > This is to see the size of db files when download is slow.
    >
    > Bug: 1172879
    > Change-Id: Id128efc5997f5542ee69f4fc9e2beca7b3064637
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2827445
    > Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@google.com>
    > Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    
    Bug: 1172879
    Change-Id: If800babb545abc4a33a9be051e433ebaf1da3db7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2929802
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>

--- client/cmd/cas/lib/download.go
+++ client/cmd/cas/lib/download.go
@@ -357,19 +357,6 @@ func (r *downloadRun) doDownload(ctx context.Context) (rerr error) {
 
 	var kvs smallFileCache
 	if r.kvs != "" {
-		if err := filepath.Walk(r.kvs, func(path string, info os.FileInfo, err error) error {
-			if os.IsNotExist(err) {
-				return nil
-			}
-
-			if info.IsDir() {
-				return nil
-			}
-			logger.Infof("kvs file %s: size %d", path, info.Size())
-			return nil
-		}); err != nil {
-			return errors.Annotate(err, "failed to walk dir: %s", r.kvs).Err()
-		}
 		kvs, err = newSmallFileCache(ctx, r.kvs)
 		if err != nil {
 			return err
