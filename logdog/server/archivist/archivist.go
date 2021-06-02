24adbb6343c7cff23f5de2bfebebfa81ad5b8259
Scott Lee <ddoman@chromium.org>
2021-06-02 08:15:43 +0000

    [logdog] Manifest export Logs to CloudLogging in the archive process
    Bug: 1164124
    Change-Id: Icb4207dddd47327cd7489c32dc791691a09cc594

--- logdog/server/archivist/archivist.go
+++ logdog/server/archivist/archivist.go
@@ -680,6 +680,7 @@ func (sa *stagedArchival) stage(c context.Context) (err error) {
 	}
 
 	m := archive.Manifest{
+		LUCIProject:      sa.project,
 		Desc:             &sa.desc,
 		Source:           &ss,
 		LogWriter:        streamWriter,
