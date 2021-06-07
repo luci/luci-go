6cfb92000f88923d2e56a7e7824fe59081e865c3
Roberto Carrillo <robertocn@chromium.org>
2021-06-07 19:09:06 +0000

    Fix "com.docker.cli" not found in $PATH
    This happens when running tests with the spanner emulator in Mac.
    The previously available workaround (disabling 'cloud experience' in
    Docker Desktop) no longer works as of docker desktop 3.3.3
    
    I found that allowing the cmd running `docker run` have access to PATH
    fixes it for me.
    
    My understanding is that `docker` now requires that `com.docker.cli` be
    findable in its PATH, but exec.Command() only resolves the path to
    `docker` and does not leak the whole contents of PATH to the process.
    
    Also, this now uses a random name for the container and calls `docker
    stop` after killing the `docker run` command. Killing the latter via
    context cancellation does not seem to stop the container anymore.
    
    R=chanli,ddoman,tandrii
    
    Change-Id: I0560cd150545e2f141312d9cc4758d5a4d0b765d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2939581
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>
    Auto-Submit: Roberto Carrillo <robertocn@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Chan Li <chanli@chromium.org>

--- common/spantest/spantest_docker.go
+++ common/spantest/spantest_docker.go
@@ -19,8 +19,10 @@ package spantest
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"os"
 	"os/exec"
+	"time"
 
 	"go.chromium.org/luci/common/errors"
 
@@ -30,6 +32,8 @@ import (
 
 // StartEmulator starts a Cloud Spanner Emulator instance.
 func StartEmulator(ctx context.Context) (*Emulator, error) {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	p, err := port.PickUnusedPort()
 	if err != nil {
 		return nil, errors.Annotate(err, "picking port").Err()
@@ -37,7 +41,8 @@ func StartEmulator(ctx context.Context) (*Emulator, error) {
 
 	hostport := fmt.Sprintf("localhost:%d", p)
 	e := &Emulator{
-		hostport: hostport,
+		hostport:      hostport,
+		containerName: fmt.Sprintf("%d", rand.Int63()),
 	}
 
 	if e.cfgDir, err = e.createSpannerEmulatorConfig(); err != nil {
@@ -45,8 +50,13 @@ func StartEmulator(ctx context.Context) (*Emulator, error) {
 	}
 
 	ctx, e.cancel = context.WithCancel(ctx)
-	e.cmd = exec.CommandContext(ctx, "docker", "run", "-p", fmt.Sprintf("127.0.0.1:%d:9010", p), "gcr.io/cloud-spanner-emulator/emulator")
-	e.cmd.Env = append(e.cmd.Env, fmt.Sprintf("SPANNER_EMULATOR_HOST=%s", e.hostport), fmt.Sprintf("CLOUDSDK_CONFIG=%s", e.cfgDir))
+	e.cmd = exec.CommandContext(
+		ctx, "docker", "run", "-p", fmt.Sprintf("127.0.0.1:%d:9010", p), "--name", e.containerName, "gcr.io/cloud-spanner-emulator/emulator")
+	e.cmd.Env = append(e.cmd.Env,
+		fmt.Sprintf("SPANNER_EMULATOR_HOST=%s", e.hostport),
+		fmt.Sprintf("CLOUDSDK_CONFIG=%s", e.cfgDir),
+		fmt.Sprintf("PATH=%s", os.Getenv("PATH")))
+
 	e.cmd.Stdout = os.Stdout
 	e.cmd.Stderr = os.Stderr
 
@@ -65,6 +75,12 @@ func (e *Emulator) Stop() error {
 		// }
 		e.cancel()
 		e.cmd = nil
+
+		// Try to explicitly stop the container, because killing `docker run`
+		// via context cancellation may not do it.
+		stopCmd := exec.Command("docker", "stop", e.containerName)
+		stopCmd.Start()
+		return stopCmd.Wait()
 	}
 
 	if e.cfgDir != "" {
