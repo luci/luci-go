38303f0fc359a354f91eec482fc0cf3585022de8
Vadim Shtayura <vadimsh@chromium.org>
2021-06-07 20:24:12 -0700

    [cipd] Log all RPCs to BigQuery.
    This will be useful to answer questions like "when was the last time
    this package was accessed".
    
    R=iannucci@chromium.org, tandrii@chromium.org
    
    Change-Id: I52d0632f852e64c49e70b53c83faa1423e38cd87

---
+++ cipd/appengine/impl/accesslog/interceptor.go
@@ -0,0 +1,169 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package accesslog implements an gRPC interceptor that logs calls to a
+// BigQuery table.
+package accesslog
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	codepb "google.golang.org/genproto/googleapis/rpc/code"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/trace"
+	"go.chromium.org/luci/server"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/auth/authdb"
+	"go.chromium.org/luci/server/bqlog"
+
+	cipdpb "go.chromium.org/luci/cipd/api/cipd/v1"
+	"go.chromium.org/luci/cipd/common"
+)
+
+func init() {
+	bqlog.RegisterSink(bqlog.Sink{
+		Prototype: &cipdpb.AccessLogEntry{},
+		Table:     "access",
+	})
+}
+
+// NewUnaryServerInterceptor returns an interceptor that logs requests.
+func NewUnaryServerInterceptor(opts *server.Options) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		if !strings.HasPrefix(info.FullMethod, "/cipd.") {
+			return handler(ctx, req)
+		}
+
+		start := clock.Now(ctx)
+		state := auth.GetState(ctx)
+
+		entry := &cipdpb.AccessLogEntry{
+			Method:         info.FullMethod,
+			Timestamp:      start.UnixNano() / 1000,
+			CallIdentity:   string(state.User().Identity),
+			PeerIdentity:   string(state.PeerIdentity()),
+			PeerIp:         state.PeerIP().String(),
+			ServiceVersion: opts.ContainerImageID,
+			ProcessId:      opts.Hostname,
+			RequestId:      trace.SpanContext(ctx),
+			AuthDbRev:      authdb.Revision(state.DB()),
+		}
+		if md, _ := metadata.FromIncomingContext(ctx); len(md["user-agent"]) > 0 {
+			entry.UserAgent = strings.Join(md["user-agent"], " ")
+		}
+
+		// Extract interesting information from recognized request body types.
+		extractFieldsFromRequest(entry, req)
+
+		panicking := true
+		defer func() {
+			entry.ResponseTimeUsec = clock.Since(ctx, start).Microseconds()
+			switch {
+			case panicking:
+				entry.ResponseCode = "INTERNAL"
+				entry.ResponseErr = "panic"
+			case err == nil:
+				entry.ResponseCode = "OK"
+			default:
+				entry.ResponseCode = codepb.Code_name[int32(status.Code(err))]
+				entry.ResponseErr = err.Error()
+			}
+			bqlog.Log(ctx, entry)
+		}()
+
+		resp, err = handler(ctx, req)
+		panicking = false
+		return
+	}
+}
+
+func extractFieldsFromRequest(entry *cipdpb.AccessLogEntry, req interface{}) {
+	if x, ok := req.(interface{ GetPackage() string }); ok {
+		entry.Package = x.GetPackage()
+	} else if x, ok := req.(interface{ GetPrefix() string }); ok {
+		entry.Package = x.GetPrefix()
+	}
+
+	if x, ok := req.(interface{ GetInstance() *cipdpb.ObjectRef }); ok {
+		entry.Instance = instanceID(x.GetInstance())
+	} else if x, ok := req.(interface{ GetObject() *cipdpb.ObjectRef }); ok {
+		entry.Instance = instanceID(x.GetObject())
+	}
+
+	if x, ok := req.(interface{ GetVersion() string }); ok {
+		entry.Version = x.GetVersion()
+	}
+
+	if x, ok := req.(interface{ GetTags() []*cipdpb.Tag }); ok {
+		entry.Tags = tagList(x.GetTags())
+	}
+
+	if x, ok := req.(interface {
+		GetMetadata() []*cipdpb.InstanceMetadata
+	}); ok {
+		entry.Metadata = metadataKeys(x.GetMetadata())
+	}
+
+	// Few one offs that do not follow the pattern.
+	switch r := req.(type) {
+	case *cipdpb.Ref:
+		entry.Version = r.Name
+	case *cipdpb.DeleteRefRequest:
+		entry.Version = r.Name
+	case *cipdpb.ListMetadataRequest:
+		entry.Metadata = r.Keys
+	case *cipdpb.DescribeInstanceRequest:
+		if r.DescribeRefs {
+			entry.Flags = append(entry.Flags, "refs")
+		}
+		if r.DescribeTags {
+			entry.Flags = append(entry.Flags, "tags")
+		}
+		if r.DescribeProcessors {
+			entry.Flags = append(entry.Flags, "processors")
+		}
+	}
+}
+
+func instanceID(ref *cipdpb.ObjectRef) string {
+	if ref == nil {
+		return ""
+	}
+	if err := common.ValidateObjectRef(ref, common.AnyHash); err != nil {
+		return fmt.Sprintf("INVALID:%s", err)
+	}
+	return common.ObjectRefToInstanceID(ref)
+}
+
+func tagList(tags []*cipdpb.Tag) []string {
+	out := make([]string, len(tags))
+	for i, t := range tags {
+		out[i] = common.JoinInstanceTag(t)
+	}
+	return out
+}
+
+func metadataKeys(md []*cipdpb.InstanceMetadata) []string {
+	out := make([]string, len(md))
+	for i, d := range md {
+		out[i] = d.Key
+	}
+	return out
+}
---
+++ cipd/appengine/impl/accesslog/interceptor_test.go
@@ -0,0 +1,159 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package accesslog
+
+import (
+	"strings"
+	"testing"
+
+	"google.golang.org/protobuf/proto"
+
+	cipdpb "go.chromium.org/luci/cipd/api/cipd/v1"
+	"go.chromium.org/luci/cipd/common"
+)
+
+func TestFields(t *testing.T) {
+	t.Parallel()
+
+	obj := &cipdpb.ObjectRef{
+		HashAlgo:  cipdpb.HashAlgo_SHA256,
+		HexDigest: strings.Repeat("a", 64),
+	}
+	iid := common.ObjectRefToInstanceID(obj)
+	pfx := "some/prefix"
+	pkg := "some/package"
+
+	tags := []*cipdpb.Tag{
+		{Key: "k1", Value: "v1"},
+		{Key: "k2", Value: "v2"},
+	}
+	tagsStr := []string{"k1:v1", "k2:v2"}
+
+	ref := "some-ref"
+
+	md := []*cipdpb.InstanceMetadata{
+		{Key: "mk1", Value: []byte("zzz")},
+		{Key: "mk2", Value: []byte("zzz")},
+	}
+	mdStr := []string{"mk1", "mk2"}
+
+	cases := []struct {
+		req interface{}
+		exp *cipdpb.AccessLogEntry
+	}{
+		{
+			&cipdpb.GetObjectURLRequest{Object: obj},
+			&cipdpb.AccessLogEntry{Instance: iid},
+		},
+		{
+			&cipdpb.BeginUploadRequest{Object: obj},
+			&cipdpb.AccessLogEntry{Instance: iid},
+		},
+		{
+			&cipdpb.PrefixRequest{Prefix: pfx},
+			&cipdpb.AccessLogEntry{Package: pfx},
+		},
+		{
+			&cipdpb.PrefixMetadata{Prefix: pfx},
+			&cipdpb.AccessLogEntry{Package: pfx},
+		},
+		{
+			&cipdpb.ListPrefixRequest{Prefix: pfx},
+			&cipdpb.AccessLogEntry{Package: pfx},
+		},
+		{
+			&cipdpb.PackageRequest{Package: pkg},
+			&cipdpb.AccessLogEntry{Package: pkg},
+		},
+		{
+			&cipdpb.Instance{Package: pkg, Instance: obj},
+			&cipdpb.AccessLogEntry{Package: pkg, Instance: iid},
+		},
+		{
+			&cipdpb.ListInstancesRequest{Package: pkg},
+			&cipdpb.AccessLogEntry{Package: pkg},
+		},
+		{
+			&cipdpb.SearchInstancesRequest{Package: pkg, Tags: tags},
+			&cipdpb.AccessLogEntry{Package: pkg, Tags: tagsStr},
+		},
+		{
+			&cipdpb.Ref{Package: pkg, Instance: obj, Name: ref},
+			&cipdpb.AccessLogEntry{Package: pkg, Instance: iid, Version: ref},
+		},
+		{
+			&cipdpb.DeleteRefRequest{Package: pkg, Name: ref},
+			&cipdpb.AccessLogEntry{Package: pkg, Version: ref},
+		},
+		{
+			&cipdpb.ListRefsRequest{Package: pkg},
+			&cipdpb.AccessLogEntry{Package: pkg},
+		},
+		{
+			&cipdpb.AttachTagsRequest{Package: pkg, Instance: obj, Tags: tags},
+			&cipdpb.AccessLogEntry{Package: pkg, Instance: iid, Tags: tagsStr},
+		},
+		{
+			&cipdpb.DetachTagsRequest{Package: pkg, Instance: obj, Tags: tags},
+			&cipdpb.AccessLogEntry{Package: pkg, Instance: iid, Tags: tagsStr},
+		},
+		{
+			&cipdpb.AttachMetadataRequest{Package: pkg, Instance: obj, Metadata: md},
+			&cipdpb.AccessLogEntry{Package: pkg, Instance: iid, Metadata: mdStr},
+		},
+		{
+			&cipdpb.DetachMetadataRequest{Package: pkg, Instance: obj, Metadata: md},
+			&cipdpb.AccessLogEntry{Package: pkg, Instance: iid, Metadata: mdStr},
+		},
+		{
+			&cipdpb.ListMetadataRequest{Package: pkg, Instance: obj, Keys: mdStr},
+			&cipdpb.AccessLogEntry{Package: pkg, Instance: iid, Metadata: mdStr},
+		},
+		{
+			&cipdpb.ResolveVersionRequest{Package: pkg, Version: ref},
+			&cipdpb.AccessLogEntry{Package: pkg, Version: ref},
+		},
+		{
+			&cipdpb.GetInstanceURLRequest{Package: pkg, Instance: obj},
+			&cipdpb.AccessLogEntry{Package: pkg, Instance: iid},
+		},
+		{
+			&cipdpb.DescribeInstanceRequest{
+				Package:            pkg,
+				Instance:           obj,
+				DescribeRefs:       true,
+				DescribeTags:       true,
+				DescribeProcessors: true,
+			},
+			&cipdpb.AccessLogEntry{
+				Package:  pkg,
+				Instance: iid,
+				Flags:    []string{"refs", "tags", "processors"},
+			},
+		},
+		{
+			&cipdpb.DescribeClientRequest{Package: pkg, Instance: obj},
+			&cipdpb.AccessLogEntry{Package: pkg, Instance: iid},
+		},
+	}
+
+	for _, c := range cases {
+		got := &cipdpb.AccessLogEntry{}
+		extractFieldsFromRequest(got, c.req)
+		if !proto.Equal(got, c.exp) {
+			t.Errorf("%s: %s != %s", c.req, got, c.exp)
+		}
+	}
+}
