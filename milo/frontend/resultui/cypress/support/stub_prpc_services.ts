116ec1285fa584b333ff4960d07cdec8b3debcb2
Weiwei Lin <weiweilin@google.com>
2021-06-09 14:44:59 +1000

    [resultui] Add cypress utilities to stub pRPC requests
    1. Implement stubRequests command.
    2. Implement stubPrpcServices command.
    3. Rewrite build_page.spec.ts with stubPrpcServices.
    4. Run tests and generate cypress/http_cache/build_page.spec.ts/*.
    
    R=bryner, gavinmak@google.com
    
    Change-Id: If6e740cf142a67de819badc71438afd242f62d63

---
+++ milo/frontend/resultui/cypress/support/stub_prpc_services.ts
@@ -0,0 +1,48 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Stubs all pRPC requests to buildbucket, resultdb, and Milo with
+       * stubRequests command. Cache names are set to 'buildbucket', 'resultdb',
+       * and 'milo'.
+       */
+      stubPrpcServices: typeof stubPrpcServices;
+    }
+  }
+}
+
+const STUB_REQUEST_OPTIONS = {
+  matchHeaders: ['host', 'accept', 'content-type', 'origin', 'authorization'],
+};
+
+/**
+ * Stubs all pRPC requests to buildbucket, resultdb, and Milo.
+ */
+function stubPrpcServices() {
+  // TODO(weiweilin): read host names from configs.
+  cy.stubRequests('https://cr-buildbucket-dev.appspot.com/prpc/**', 'buildbucket', STUB_REQUEST_OPTIONS);
+  cy.stubRequests('https://staging.results.api.cr.dev/prpc/**', 'resultdb', STUB_REQUEST_OPTIONS);
+  cy.stubRequests('https://localhost:8080/prpc/**', 'milo', STUB_REQUEST_OPTIONS);
+}
+
+/**
+ * Adds stubPrpcServices command to Cypress.
+ */
+export function addStubPrpcServicesCommand() {
+  Cypress.Commands.add('stubPrpcServices', stubPrpcServices);
+}
