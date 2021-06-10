0ec952c367497a4b26b94e508e7705a87c2e414e
Weiwei Lin <weiweilin@google.com>
2021-06-10 14:41:04 +1000

    [resultui] replace build page unit tests with integration tests
    Build page unit tests are fragile due to increasingly complex mocking
    requirements. Integration tests require less mocking therefore are less
    likely to break.
    
    R=bryner, gavinmak@google.com
    
    Change-Id: Iac90d2c39fab4d65fd87a993901f693f1b37f997

--- milo/frontend/resultui/cypress/integration/build_page.spec.ts
+++ milo/frontend/resultui/cypress/integration/build_page.spec.ts
@@ -12,15 +12,54 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { STUB_REQUEST_OPTIONS } from '../support/stub_prpc_services';
+
 describe('Build Page', () => {
   it('should navigate to the default tab', () => {
     cy.visit('/p/chromium/builders/ci/linux-rel-swarming/15252');
-    cy.url().should('include', '/overview');
+    cy.location('pathname').should('equal', '/ui/p/chromium/builders/ci/linux-rel-swarming/15252/overview');
   });
 
   it('should initiate the signin flow if the page is 404 and the user is not logged in', () => {
     cy.visit('/p/not-bound-project/builders/not-bound-bucket/not-found-builder/12479');
     cy.on('uncaught:exception', () => false);
-    cy.location('pathname').should('include', '/ui/login');
+    cy.location('pathname').should('equal', '/ui/login');
+  });
+
+  it('should compute invocation ID from buildNum in URL', () => {
+    cy.visit('/p/chromium/builders/ci/linux-rel-swarming/15252');
+    cy.get('milo-build-page')
+      .invoke('prop', 'buildState')
+      .its('invocationId')
+      .should('eq', 'build-70535a5a746775ce83281f4e4e318b2b7b239d1e7eb7c8f790bf570a14cf61fe-15252');
+  });
+
+  it('should compute invocation ID from build ID in URL', () => {
+    cy.visit('/p/chromium/builders/ci/linux-rel-swarming/b8845866669318458401');
+    cy.get('milo-build-page')
+      .invoke('prop', 'buildState')
+      .its('invocationId')
+      .should('eq', 'build-8845866669318458401');
+  });
+
+  it('should fallback to invocation ID from buildbucket when invocation is not found', () => {
+    // modified-resultdb is manually modified to respond 404 to queries with
+    // computed invocation IDs.
+    cy.stubRequests(
+      { url: 'https://staging.results.api.cr.dev/prpc/**', method: 'POST' },
+      'modified-resultdb',
+      STUB_REQUEST_OPTIONS
+    );
+    cy.visit('/p/chromium/builders/ci/linux-rel-swarming/15252');
+    cy.on('uncaught:exception', () => false);
+    cy.get('milo-build-page')
+      .invoke('prop', 'buildState')
+      .its('invocationId')
+      .should('eq', 'build-8845866669318458401');
+  });
+
+  it('should redirect to a long link when visited via a short link', () => {
+    cy.visit('/b/8845866669318458401');
+    cy.location('pathname').should('equal', '/ui/p/chromium/builders/ci/linux-rel-swarming/15252/overview');
   });
 });
