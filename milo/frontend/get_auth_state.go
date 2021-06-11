d216cdd4f4ca898cbff266326248962387b5cd06
Weiwei Lin <weiweilin@google.com>
2021-06-11 16:59:48 +1000

    [milo] Add auth-state endpoint
    1. Add auth-state endpoint so the browser can get the access token.
    2. Remove GetCurrentUser RPC because it's now redundant.
    
    R=bryner, vadimsh
    Bug: 1187775, 1155411
    Change-Id: I07aaaf812e502d456ce7c67705b5ee2a6f0a38bf

---
+++ milo/frontend/get_auth_state.go
@@ -0,0 +1,73 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package frontend
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"go.chromium.org/luci/auth/identity"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/router"
+)
+
+type authState struct {
+	// Identity string of the user (anonymous:anonymous if the user is not
+	// logged in).
+	Identity string `json:"identity,omitempty"`
+	// The email of the user. Optional, default "".
+	Email string `json:"email,omitempty"`
+	// The URL of the user avatar. Optional, default "".
+	Picture string `son:"picture,omitempty"`
+	// The token that authorizes and authenticates the requests.
+	AccessToken string `json:"accessToken,omitempty"`
+	// Expiration time (unix timestamp) of the access token.
+	//
+	// If zero, the access token does not expire.
+	AccessTokenExpiry int64 `json:"accessTokenExpiry,omitempty"`
+}
+
+// getAuthState returns data about the current user and the access token
+// associated with the current session.
+func getAuthState(c *router.Context) error {
+	user := auth.CurrentUser(c.Context)
+	var state *authState
+	if user.Identity == identity.AnonymousIdentity {
+		state = &authState{
+			Identity: string(user.Identity),
+		}
+	} else {
+		token, err := auth.GetState(c.Context).Session().AccessToken(c.Context)
+		if err != nil {
+			return err
+		}
+		state = &authState{
+			Identity:          string(user.Identity),
+			Email:             user.Email,
+			Picture:           user.Picture,
+			AccessToken:       token.AccessToken,
+			AccessTokenExpiry: token.Expiry.Unix(),
+		}
+	}
+	if err := json.NewEncoder(c.Writer).Encode(state); err != nil {
+		logging.Errorf(c.Context, "Failed to JSON encode output - %s", err)
+		return err
+	}
+
+	c.Writer.Header().Add("Content-Type", "application/json")
+	c.Writer.WriteHeader(http.StatusOK)
+	return nil
+}
