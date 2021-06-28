1ca9ca6962968e2905af6ccacaa452acf3cd079f
Andrii Shyshkalov <tandrii@google.com>
2021-06-28 02:43:48 +0200

    [cv][refactor] move Send* functions into RM/PM Notifiers.
    Now they can reference RunKind and ProjectKind w/o circular
    dependency.
    
    R=yiwzhang
    
    Change-Id: I67014a762aa70c88a400c26e6e677e93cb93816a

--- cv/internal/common/errors_test.go
+++ cv/internal/common/errors_test.go
@@ -102,17 +102,16 @@ func TestTQifyError(t *testing.T) {
 					So(tq.Fatal.In(err), ShouldBeFalse)
 					assertLoggedStack()
 				})
-				Convey("non-transient -> tq.Fatal", func() {
+				Convey("non-transient -> tq.Fatal and ", func() {
 					err := tqify.Error(ctx, errRare)
 					So(tq.Fatal.In(err), ShouldBeTrue)
 					assertLoggedStack()
 				})
 			})
 
-			Convey("KnownFatal => tq.Ignore, log as warning", func() {
+			Convey("KnownFatal => tq.Fatal, log as warning", func() {
 				err := tqify.Error(ctx, errWrapOops)
-				So(tq.Ignore.In(err), ShouldBeTrue)
-				So(tq.Fatal.In(err), ShouldBeFalse)
+				So(tq.Fatal.In(err), ShouldBeTrue)
 				assertLoggedAt(logging.Warning)
 			})
 			Convey("KnownRetry => non-transient, non-Fatal, log as warning", func() {
