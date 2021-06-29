e00873037ee10e9256b18f23d920d662878d5421
smut <smut@google.com>
2021-06-29 08:26:27 -0700

    [buildbucket] Remove traffic splitter (serve 100% of Builds RPC calls)
    Bug: 1042991,1144958
    Change-Id: I0b49fd86ff0fc64bf7936f6b69978dd07cf804f0

--- buildbucket/appengine/frontend/main.go
+++ buildbucket/appengine/frontend/main.go
@@ -16,15 +16,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"strings"
-
-	"go.chromium.org/luci/common/data/rand/mathrand"
-	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/proto/access"
 	"go.chromium.org/luci/config/server/cfgmodule"
 	"go.chromium.org/luci/grpc/prpc"
@@ -32,7 +23,6 @@ import (
 	"go.chromium.org/luci/server/cron"
 	"go.chromium.org/luci/server/gaeemulation"
 	"go.chromium.org/luci/server/module"
-	"go.chromium.org/luci/server/router"
 	"go.chromium.org/luci/server/tq"
 
 	// Enable datastore transactional tasks support.
@@ -43,16 +33,6 @@ import (
 	pb "go.chromium.org/luci/buildbucket/proto"
 )
 
-// isBeefy returns whether the request was intended for the beefy service.
-func isBeefy(req *http.Request) bool {
-	return strings.Contains(req.Host, "beefy")
-}
-
-// isDev returns whether the request was intended for the dev instance.
-func isDev(req *http.Request) bool {
-	return strings.HasSuffix(req.Host, "-dev.appspot.com")
-}
-
 func main() {
 	mods := []module.Module{
 		cfgmodule.NewModuleFromFlags(),
@@ -62,76 +42,15 @@ func main() {
 	}
 
 	server.Main(nil, mods, func(srv *server.Server) error {
-		// Proxy buildbucket.v2.Builds pRPC requests back to the Python
-		// service in order to achieve a programmatic traffic split.
-		// Because of the way dispatch routes work, requests are proxied
-		// to a copy of the Python service hosted at a different path.
-		// TODO(crbug/1042991): Remove the proxy once the go service handles all traffic.
-		pythonURL, err := url.Parse(fmt.Sprintf("https://default-dot-%s.appspot.com/python", srv.Options.CloudProject))
-		if err != nil {
-			panic(err)
-		}
-		beefyURL, err := url.Parse(fmt.Sprintf("https://beefy-dot-%s.appspot.com/python", srv.Options.CloudProject))
-		if err != nil {
-			panic(err)
-		}
-		prx := httputil.NewSingleHostReverseProxy(pythonURL)
-		prx.Director = func(req *http.Request) {
-			target := pythonURL
-			if isBeefy(req) {
-				target = beefyURL
-			}
-			// According to net.Request documentation, setting Host is unnecessary
-			// because URL.Host is supposed to be used for outbound requests.
-			// However, on GAE, it seems that req.Host is incorrectly used.
-			req.Host = target.Host
-			req.URL.Scheme = target.Scheme
-			req.URL.Host = target.Host
-			req.URL.Path = fmt.Sprintf("%s%s", target.Path, req.URL.Path)
-		}
-		// makeOverride returns a prpc.Override which allows the given percentage of requests
-		// through to this service, proxying the remainder to Python.
-		makeOverride := func(prodPct, devPct int) func(*router.Context) bool {
-			return func(ctx *router.Context) bool {
-				// TODO(crbug/1090540): remove env k-v
-				ctx.Context = context.WithValue(ctx.Context, "env", "Prod")
-				pct := prodPct
-				if isDev(ctx.Request) {
-					pct = devPct
-					// TODO(crbug/1090540): remove env k-v
-					ctx.Context = context.WithValue(ctx.Context, "env", "Dev")
-				}
-				switch val := ctx.Request.Header.Get("Should-Proxy"); val {
-				case "true":
-					pct = 0
-					logging.Debugf(ctx.Context, "request demanded to be proxied")
-				case "false":
-					pct = 100
-					logging.Debugf(ctx.Context, "request demanded not to be proxied")
-				}
-				ctx.Context = rpc.WithTrafficSplit(ctx.Context, pct)
-				if mathrand.Intn(ctx.Context, 100) < pct {
-					return false
-				}
-				target := pythonURL
-				if isBeefy(ctx.Request) {
-					target = beefyURL
-				}
-				logging.Debugf(ctx.Context, "proxying request to %s", target)
-				prx.ServeHTTP(ctx.Writer, ctx.Request)
-				return true
-			}
-		}
-
+		// TODO(crbug/1082369): Remove this workaround once field masks can be decoded.
+		srv.PRPC.HackFixFieldMasksForJSON = true
 		srv.PRPC.AccessControl = prpc.AllowOriginAll
+
 		access.RegisterAccessServer(srv.PRPC, &access.UnimplementedAccessServer{})
 		pb.RegisterBuildsServer(srv.PRPC, rpc.NewBuilds())
 		pb.RegisterBuildersServer(srv.PRPC, rpc.NewBuilders())
 		// TODO(crbug/1082369): Remove this workaround once field masks can be decoded.
 		srv.PRPC.HackFixFieldMasksForJSON = true
-
-		// makeOverride(prod % -> Go, dev % -> Go).
-		srv.PRPC.RegisterOverride("buildbucket.v2.Builds", "ScheduleBuild", makeOverride(75, 100))
 
 		cron.RegisterHandler("update_config", config.UpdateSettingsCfg)
 		return nil
--- buildbucket/appengine/rpc/batch.go
+++ buildbucket/appengine/rpc/batch.go
@@ -17,19 +17,14 @@ package rpc
 import (
 	"context"
 	"fmt"
-	"net/http"
 
 	"google.golang.org/grpc/codes"
 	grpcStatus "google.golang.org/grpc/status"
 
-	"go.chromium.org/luci/common/data/rand/mathrand"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
-	"go.chromium.org/luci/common/retry"
 	"go.chromium.org/luci/common/sync/parallel"
 	"go.chromium.org/luci/grpc/appstatus"
-	"go.chromium.org/luci/grpc/prpc"
-	"go.chromium.org/luci/server/auth"
 
 	pb "go.chromium.org/luci/buildbucket/proto"
 )
@@ -39,93 +34,35 @@ const (
 	writeReqsSizeLimit = 200
 )
 
-var testFakeTransportError = "used in tests only to mock the transport error"
-
-// pyBatchResponse captures the BatchResponse from Py service.
-type pyBatchResponse struct {
-	res *pb.BatchResponse
-	err error
-}
-
-// pctKey is the key to a traffic split percentage in [0, 100] in the context.
-var pctKey = "pct"
-
-// WithTrafficSplit returns a new context with the given traffic split.
-func WithTrafficSplit(c context.Context, pct int) context.Context {
-	return context.WithValue(c, &pctKey, pct)
-}
-
-// getTrafficSplit returns the traffic split percentage in the given context.
-func getTrafficSplit(c context.Context) int {
-	pct := c.Value(&pctKey)
-	if pct == nil {
-		return 0
-	}
-	return pct.(int)
-}
-
 // Batch handles a batch request. Implements pb.BuildsServer.
 func (b *Builds) Batch(ctx context.Context, req *pb.BatchRequest) (*pb.BatchResponse, error) {
-	pct := getTrafficSplit(ctx)
 	res := &pb.BatchResponse{}
 	if len(req.GetRequests()) == 0 {
 		return res, nil
 	}
 	res.Responses = make([]*pb.BatchResponse_Response, len(req.Requests))
 
-	// schedule and cancel requests are sent to Py service for now.
-	pyBatchReq := &pb.BatchRequest{}
-	var goBatchReq []*pb.BatchRequest_Request
-
-	// record the mapping of indices in py/goBatchReq to indices in original req.
-	pyIndices := make([]int, 0, len(req.Requests))
-	goIndices := make([]int, 0, len(req.Requests))
-	for i, r := range req.Requests {
+	scheduleReqs := 0
+	for _, r := range req.Requests {
 		switch r.Request.(type) {
 		case *pb.BatchRequest_Request_ScheduleBuild:
-			if mathrand.Intn(ctx, 100) < pct {
-				logDetails(ctx, "Batch (ScheduleBuild)", r)
-				goIndices = append(goIndices, i)
-				goBatchReq = append(goBatchReq, r)
-			} else {
-				pyIndices = append(pyIndices, i)
-				pyBatchReq.Requests = append(pyBatchReq.Requests, r)
-			}
+			scheduleReqs++
 		case *pb.BatchRequest_Request_GetBuild, *pb.BatchRequest_Request_SearchBuilds, *pb.BatchRequest_Request_CancelBuild:
-			goIndices = append(goIndices, i)
-			goBatchReq = append(goBatchReq, r)
 		default:
 			return nil, appstatus.BadRequest(errors.New("request includes an unsupported type"))
 		}
 	}
 
 	// validate request count
-	// TODO(crbug.com/1144958): refactor code to correctly calculate read and write req count after ScheduleBuild is done.
-	// pyBatchReq current only contains ScheduleBuild requests.
-	if len(pyBatchReq.Requests) > writeReqsSizeLimit {
-		return nil, appstatus.BadRequest(errors.Reason("the maximum allowed write request count in Batch is %d.", writeReqsSizeLimit).Err())
+	if scheduleReqs > writeReqsSizeLimit {
+		return nil, appstatus.BadRequest(errors.Reason("the maximum allowed schedule request count in Batch is %d.", writeReqsSizeLimit).Err())
 	}
-	if len(goBatchReq) > readReqsSizeLimit {
-		return nil, appstatus.BadRequest(errors.Reason("the maximum allowed read request count in Batch is %d.", readReqsSizeLimit).Err())
-	}
-
-	// TODO(crbug.com/1144958): remove calling py after ScheduleBuild is done.
-	pyResC := make(chan *pyBatchResponse)
-	if len(pyBatchReq.Requests) != 0 {
-		go func() {
-			pyClient, err := b.newPyBBClient(ctx)
-			if err != nil {
-				pyResC <- &pyBatchResponse{res: nil, err: err}
-				return
-			}
-			logging.Debugf(ctx, "Batch: calling python service")
-			res, err := pyClient.Batch(ctx, pyBatchReq)
-			pyResC <- &pyBatchResponse{res: res, err: err}
-		}()
+	if len(req.Requests)-scheduleReqs > readReqsSizeLimit {
+		return nil, appstatus.BadRequest(errors.Reason("the maximum allowed get+search+cancel request count in Batch is %d.", readReqsSizeLimit).Err())
 	}
 
 	err := parallel.WorkPool(64, func(c chan<- func() error) {
-		for i, r := range goBatchReq {
+		for i, r := range req.Requests {
 			i, r := i, r
 			c <- func() error {
 				response := &pb.BatchResponse_Response{}
@@ -157,28 +94,13 @@ func (b *Builds) Batch(ctx context.Context, req *pb.BatchRequest) (*pb.BatchResp
 					}
 					response.Response = toBatchResponseError(ctx, err)
 				}
-				res.Responses[goIndices[i]] = response
+				res.Responses[i] = response
 				return nil
 			}
 		}
 	})
 	if err != nil {
 		return nil, err
-	}
-
-	if len(pyBatchReq.Requests) == 0 {
-		return res, nil
-	}
-
-	pyRes := <-pyResC
-	if pyRes.err != nil {
-		logging.Warningf(ctx, "Error from Python service: %s", pyRes.err)
-		gStatus, _ := convertGRPCError(pyRes.err)
-		return nil, appstatus.Error(gStatus.Code(), gStatus.Message())
-	}
-
-	for i, idx := range pyIndices {
-		res.Responses[idx] = pyRes.res.Responses[i]
 	}
 
 	return res, nil
@@ -210,33 +132,4 @@ func toBatchResponseError(ctx context.Context, err error) *pb.BatchResponse_Resp
 		return &pb.BatchResponse_Response_Error{Error: grpcStatus.New(codes.Internal, "Internal server error").Proto()}
 	}
 	return &pb.BatchResponse_Response_Error{Error: st.Proto()}
-}
-
-// newPyBBClient constructs a BuildBucket python client.
-func (b *Builds) newPyBBClient(ctx context.Context) (pb.BuildsClient, error) {
-	switch fakeErr, ok := ctx.Value(&testFakeTransportError).(error); {
-	case ok:
-		return nil, fakeErr
-	case b.testPyBuildsClient != nil:
-		return b.testPyBuildsClient, nil
-	}
-
-	pyHost := "default-dot-cr-buildbucket.appspot.com"
-	if ctx.Value("env") == "Dev" {
-		pyHost = "default-dot-cr-buildbucket-dev.appspot.com"
-	}
-	t, err := auth.GetRPCTransport(ctx, auth.AsCredentialsForwarder)
-	if err != nil {
-		logging.Errorf(ctx, "failed to get Py BB RPC transport: %s", err)
-		return nil, grpcStatus.Error(codes.Internal, "failed to get Py BB RPC transport")
-	}
-	pClient := &prpc.Client{
-		C:          &http.Client{Transport: t},
-		Host:       pyHost,
-		PathPrefix: "/python/prpc",
-		Options: &prpc.Options{
-			Retry: retry.None,
-		},
-	}
-	return pb.NewBuildsPRPCClient(pClient), nil
 }
