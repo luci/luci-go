989caf88eed494f45580f56005fb472bc8da49d9
Scott Lee <ddoman@chromium.org>
2021-06-29 00:37:18 +0000

    [grpcmon] implement a stats.Handler with tsmon
    ClientRPCStatsMonitor implements stats.Handler.
    It captures Unary and Stream RPC calls, and update the tsmon
    metrics based on the RPC stats.
    
    Bug: 1121009
    Change-Id: Ib8ad1d40945e63740c4186172c84be27f1368a41
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/2988704
    Commit-Queue: Scott Lee <ddoman@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

--- grpc/grpcmon/client.go
+++ grpc/grpcmon/client.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The LUCI Authors.
+// Copyright 2021 The LUCI Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import (
 
 	gcode "google.golang.org/genproto/googleapis/rpc/code"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/stats"
 	"google.golang.org/grpc/status"
 
 	"go.chromium.org/luci/common/clock"
@@ -44,6 +45,8 @@ var (
 		distribution.DefaultBucketer,
 		field.String("method"),         // full name of the grpc method
 		field.String("canonical_code")) // grpc.Code of the result in string
+
+	rtKey = "Holds the current rpc tag"
 )
 
 // NewUnaryClientInterceptor returns an interceptor that gathers RPC call
@@ -81,4 +84,55 @@ func reportClientRPCMetrics(ctx context.Context, method string, err error, dur t
 	grpcClientCount.Add(ctx, 1, method, canon)
 	// TODO(tandrii): use dur.Milliseconds() once all GAE apps are >go1.11.
 	grpcClientDuration.Add(ctx, dur.Seconds()*1e3, method, canon)
+}
+
+// ClientRPCStatsMonitor implements stats.Handler to update tsmon metrics with
+// RPC stats.
+type ClientRPCStatsMonitor struct{}
+
+// TagRPC creates a context for the RPC.
+//
+// The context used for the rest lifetime of the RPC will be derived
+// from the returned context.
+//
+// Can be passed to a gRPC client via WithStatsHandler(...) dial option.
+func (m *ClientRPCStatsMonitor) TagRPC(ctx context.Context, tag *stats.RPCTagInfo) context.Context {
+	return context.WithValue(ctx, &rtKey, tag)
+}
+
+// handleRPCEnd updates the metrics for an RPC completion.
+func (m *ClientRPCStatsMonitor) handleRPCEnd(ctx context.Context, e *stats.End) {
+	rt, ok := ctx.Value(&rtKey).(*stats.RPCTagInfo)
+	if !ok {
+		// This should never happen.
+		panic("handleRPCEnd: missing rpc-tag")
+	}
+	reportClientRPCMetrics(ctx, rt.FullMethodName, e.Error, e.EndTime.Sub(e.BeginTime))
+}
+
+// HandleRPC processes the RPC stats.
+func (m *ClientRPCStatsMonitor) HandleRPC(ctx context.Context, s stats.RPCStats) {
+	switch event := s.(type) {
+	case *stats.End:
+		m.handleRPCEnd(ctx, event)
+	default:
+		// do nothing
+		//
+		// TODO(ddoman): handle InPayload and OutPayload to report
+		// sent_msg, sent_msg_bytes, recv_msg, and recv_msg_bytes.
+	}
+}
+
+// TagConn creates a context for the connection.
+//
+// The context passed to HandleConn will be derived from the returned context.
+// The context passed to HandleRPC will NOT be derived from the returned context.
+func (m *ClientRPCStatsMonitor) TagConn(ctx context.Context, t *stats.ConnTagInfo) context.Context {
+	// do nothing
+	return ctx
+}
+
+// HandleConn processes the Conn stats.
+func (m *ClientRPCStatsMonitor) HandleConn(context.Context, stats.ConnStats) {
+	// do nothing
 }
