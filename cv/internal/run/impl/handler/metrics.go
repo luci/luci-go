415972e03f8f419252c0c30c4fe245884e255925
Andrii Shyshkalov <tandrii@google.com>
2021-07-02 19:13:35 +0200

    [cv][metrics] export pickup latency metrics.
    The name "Pickup latency" originates from CQDaemon.
    
    First, this CL creates
    "/chrome/infra/cv/pickup/latency" of CV is the same as
    "/chrome/infra/commit_queue/pickup_latency/durations" of CQDaemon.
    This is what the user actually experiences.
    
    In addition, this CL adds
    "/chrome/infra/cv/pickup/latency_adjusted"
    which subtracts stabilization delay if any is configured for the Run.
    This essentially tracks overhead of CV & Gerrit, and may be alerted on,
    say if median exceeds 10s (poll interval).
    
    R=yiwzhang
    
    Bug: 1225809
    Change-Id: Ie629a623c7aa3dfe6466521e74dc062e4f4b9379

---
+++ cv/internal/run/impl/handler/metrics.go
@@ -0,0 +1,49 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package handler
+
+import (
+	"math"
+
+	"go.chromium.org/luci/common/tsmon/distribution"
+	"go.chromium.org/luci/common/tsmon/field"
+	"go.chromium.org/luci/common/tsmon/metric"
+	"go.chromium.org/luci/common/tsmon/types"
+)
+
+var (
+	// metricPickupLatencyS replicates CQDaemon's metric
+	// "/chrome/infra/commit_queue/pickup_latency/durations"
+	//
+	// TODO(crbug/1225047): remove mention of legacy metric.
+	metricPickupLatencyS = metric.NewCumulativeDistribution(
+		"cv/pickup/latency",
+		"Time between triggering CV and when CV processing actually starts",
+		&types.MetricMetadata{Units: types.Seconds},
+		// Bucketer for 1s..2.5h range.
+		//
+		// $ python3 -c "print(((10**0.04)**100)/60.0/60.0)"
+		// 2.7777777777777977
+		distribution.GeometricBucketer(math.Pow(10, 0.04), 100),
+		field.String("project"),
+	)
+	metricPickupLatencyAdjustedS = metric.NewCumulativeDistribution(
+		"cv/pickup/latency_adjusted",
+		"Time between triggering CV and when CV processing actually starts but adjusted by the configured stabilization delay",
+		&types.MetricMetadata{Units: types.Seconds},
+		distribution.GeometricBucketer(math.Pow(10, 0.04), 100),
+		field.String("project"),
+	)
+)
