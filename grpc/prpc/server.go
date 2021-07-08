03639e3bb3b0bb9a8ceda5035bb137ae379b2e78
Takuto Ikuta <tikuta@chromium.org>
2021-07-08 14:29:15 +0900

    ctx
    prpc: use ctx for context.Context
    Change-Id: I42db3bae82a5802df0f5b8ba3a98636f177ba2c9

--- grpc/prpc/server.go
+++ grpc/prpc/server.go
@@ -73,7 +73,7 @@ const (
 // AllowOriginAll returns AccessAllowWithCredentials unconditionally.
 // It can be used as Server.AccessControl.
 // It must NOT be used in combination with cookie-based authentication.
-func AllowOriginAll(c context.Context, origin string) AccessControlDecision {
+func AllowOriginAll(ctx context.Context, origin string) AccessControlDecision {
 	return AccessAllowWithCredentials
 }
 
@@ -108,7 +108,7 @@ type Server struct {
 	// by the client. If nil, no headers will be written.
 	// Otherwise, see AccessControlDecision for what access control headers will be
 	// included in the response for the specified origin.
-	AccessControl func(c context.Context, origin string) AccessControlDecision
+	AccessControl func(ctx context.Context, origin string) AccessControlDecision
 
 	// HackFixFieldMasksForJSON indicates whether to attempt a workaround for
 	// https://github.com/golang/protobuf/issues/745 when the request has
