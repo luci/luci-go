8fdd3faa73a7dd9f115f92a43c4e2fe971a8747a
Andrii Shyshkalov <tandrii@google.com>
2021-08-10 20:27:29 +0000

    [cv][try] record Tryjobs state in Run entity and emit RunLog(s).
    This should complete the work on CQD -> CV Tryjob pipeline
    in order to kill CQ status app.
    
    Note that CV Run manager should still emit more events
    to fully replace CQ status app, such as events around
    Run verification and submission.
    
    R=robertocn, yiwzhang
    
    Bug: 1232158
    Change-Id: I590b87bfef5e0c87c2ba978f1019dc05c44b76f5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3087342
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Roberto Carrillo <robertocn@chromium.org>

--- cv/internal/run/impl/handler/tryjobs_updated.go
+++ cv/internal/run/impl/handler/tryjobs_updated.go
@@ -16,15 +16,23 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 
+	cvbqpb "go.chromium.org/luci/cv/api/bigquery/v1"
 	commonpb "go.chromium.org/luci/cv/api/common/v1"
+	migrationpb "go.chromium.org/luci/cv/api/migration"
 	"go.chromium.org/luci/cv/internal/migration"
 	"go.chromium.org/luci/cv/internal/run"
 	"go.chromium.org/luci/cv/internal/run/impl/state"
+	"go.chromium.org/luci/cv/internal/tryjob"
 )
 
 // OnCQDTryjobsUpdated implements Handler interface.
@@ -40,17 +48,129 @@ func (impl *Impl) OnCQDTryjobsUpdated(ctx context.Context, rs *state.RunState) (
 		return nil, errors.Reason("expected RUNNING status, got %s", status).Err()
 	}
 
+	var lastSeen time.Time
+	if t := rs.Run.Tryjobs.GetCqdUpdateTime(); t != nil {
+		lastSeen = t.AsTime()
+	}
+
 	// Limit loading reported tryjobs to 128 latest only.
 	// If there is a malfunction in CQDaemon, ignoring earlier reports is fine.
-	switch latest, err := migration.ListReportedTryjobs(ctx, rs.Run.ID, time.Time{}, 128); {
+	switch latest, err := migration.ListReportedTryjobs(ctx, rs.Run.ID, lastSeen, 128); {
 	case err != nil:
 		return nil, errors.Annotate(err, "failed to load latest reported Tryjobs").Err()
 	case len(latest) == 0:
-		logging.Errorf(ctx, "received CQDTryjobsUpdated event, but couldn't find any reported tryjobs")
+		logging.Errorf(ctx, "received CQDTryjobsUpdated event, but couldn't find any new reports")
 		return &Result{State: rs}, nil
 	default:
 		logging.Debugf(ctx, "received CQDTryjobsUpdated event, read %d latest tryjob reports", len(latest))
-		// TODO(crbug/1232158): implement.
+		rs = rs.ShallowCopy()
+		if rs.Run.Tryjobs == nil {
+			rs.Run.Tryjobs = &run.Tryjobs{}
+		} else {
+			rs.Run.Tryjobs = proto.Clone(rs.Run.Tryjobs).(*run.Tryjobs)
+		}
+		// `latest` are ordered newest to oldest, so process them in reverse order
+		// such the newest report is ultimately stored in the Run.Tryjobs.
+		for i := len(latest) - 1; i > -1; i-- {
+			updateTryjobsFromCQD(ctx, rs, latest[i])
+		}
 		return &Result{State: rs}, nil
 	}
+}
+
+func updateTryjobsFromCQD(ctx context.Context, rs *state.RunState, rep *migration.ReportedTryjobs) {
+	before := rs.Run.Tryjobs.GetTryjobs()
+	after := make([]*run.Tryjob, 0, len(rep.Payload.GetTryjobs()))
+	for _, cqd := range rep.Payload.GetTryjobs() {
+		if cqd.GetBuilder() == nil {
+			logging.Warningf(ctx, "Skipping old Tryjob report from CQD without builder information: %q", rep.ID)
+			return
+		}
+		switch t, err := toRunTryjob(cqd); {
+		case err != nil:
+			logging.Errorf(ctx, "Failed to convert CQD tryjob to run.Tryjob: %s\nTryjob details:\n%s", err, cqd)
+		default:
+			after = append(after, t)
+		}
+	}
+	run.SortTryjobs(after)
+	rs.Run.Tryjobs.CqdUpdateTime = timestamppb.New(rep.ReportTime())
+	rs.Run.Tryjobs.Tryjobs = after
+	if updated := run.DiffTryjobsForReport(before, after); len(updated) > 0 {
+		rs.LogEntries = append(rs.LogEntries, &run.LogEntry{
+			Time: timestamppb.New(rep.ReportTime()),
+			Kind: &run.LogEntry_TryjobsUpdated_{
+				TryjobsUpdated: &run.LogEntry_TryjobsUpdated{
+					Tryjobs: updated,
+				},
+			},
+		})
+	}
+}
+
+// toRunTryjob constructs CV representation of a Tryjob based on CQDaemon's
+// input.
+//
+// Best effort. Some information loss expected. Intended use for UI, not for
+// decision making.
+func toRunTryjob(in *migrationpb.Tryjob) (*run.Tryjob, error) {
+	eid, err := tryjob.BuildbucketID(in.GetBuild().GetHost(), in.GetBuild().GetId())
+	if err != nil {
+		return nil, err
+	}
+
+	tjStatus := tryjob.Status_PENDING
+	resStatus := tryjob.Result_RESULT_STATUS_UNSPECIFIED
+	bbStatus := buildbucketpb.Status_STATUS_UNSPECIFIED
+	switch in.GetStatus() {
+	case migrationpb.TryjobStatus_NOT_TRIGGERED:
+		tjStatus = tryjob.Status_PENDING
+	case migrationpb.TryjobStatus_PENDING:
+		// CQD's PENDING means build is scheduled but isn't yet running,
+		// but CV doesn't differentiate them.
+		tjStatus = tryjob.Status_TRIGGERED
+		bbStatus = buildbucketpb.Status_SCHEDULED
+	case migrationpb.TryjobStatus_RUNNING:
+		tjStatus = tryjob.Status_TRIGGERED
+		bbStatus = buildbucketpb.Status_STARTED
+	case migrationpb.TryjobStatus_FAILED:
+		tjStatus = tryjob.Status_ENDED
+		resStatus = tryjob.Result_FAILED_PERMANENTLY
+		bbStatus = buildbucketpb.Status_FAILURE
+	case migrationpb.TryjobStatus_SUCCEEDED:
+		tjStatus = tryjob.Status_ENDED
+		resStatus = tryjob.Result_SUCCEEDED
+		bbStatus = buildbucketpb.Status_SUCCESS
+	case migrationpb.TryjobStatus_TIMED_OUT:
+		tjStatus = tryjob.Status_ENDED
+		resStatus = tryjob.Result_FAILED_TRANSIENTLY
+		bbStatus = buildbucketpb.Status_INFRA_FAILURE
+	default:
+		panic(fmt.Errorf("unhandled CQD status %s", in.GetStatus()))
+	}
+
+	return &run.Tryjob{
+		Id:         0,
+		ExternalId: string(eid),
+		Eversion:   0,
+		Definition: &tryjob.Definition{
+			Backend: &tryjob.Definition_Buildbucket_{Buildbucket: &tryjob.Definition_Buildbucket{
+				Host:    in.GetBuild().GetHost(),
+				Builder: in.GetBuilder(),
+			}},
+		},
+		Reused: in.GetBuild().GetOrigin() == cvbqpb.Build_REUSED,
+		Status: tjStatus,
+		Result: &tryjob.Result{
+			Status:     resStatus,
+			CreateTime: in.GetCreateTime(),
+			UpdateTime: nil, // CQD doesn't track this.
+			Output:     nil, // recipe Output is not relevant to the UI.
+			Backend: &tryjob.Result_Buildbucket_{Buildbucket: &tryjob.Result_Buildbucket{
+				Id:     in.GetBuild().GetId(),
+				Status: bbStatus,
+			}},
+		},
+		CqdDerived: true,
+	}, nil
 }
---
+++ cv/internal/run/impl/handler/tryjobs_updated_test.go
@@ -0,0 +1,240 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package handler
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"go.chromium.org/luci/auth/identity"
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/auth/authtest"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	cvbqpb "go.chromium.org/luci/cv/api/bigquery/v1"
+	commonpb "go.chromium.org/luci/cv/api/common/v1"
+	migrationpb "go.chromium.org/luci/cv/api/migration"
+	"go.chromium.org/luci/cv/internal/common"
+	"go.chromium.org/luci/cv/internal/cvtesting"
+	"go.chromium.org/luci/cv/internal/migration"
+	"go.chromium.org/luci/cv/internal/run"
+	"go.chromium.org/luci/cv/internal/run/impl/state"
+	"go.chromium.org/luci/cv/internal/tryjob"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestOnCQDTryjobsUpdated(t *testing.T) {
+	t.Parallel()
+
+	Convey("OnCQDTryjobsUpdated", t, func() {
+		ct := cvtesting.Test{}
+		ctx, cancel := ct.SetUp()
+		defer cancel()
+
+		const (
+			lProject = "infra"
+			bbHost   = "bb.example.com"
+		)
+		bbTryjobDef := func(projSlashBucketSlashBuilder string) *tryjob.Definition {
+			parts := strings.Split(projSlashBucketSlashBuilder, "/")
+			if len(parts) != 3 {
+				panic(fmt.Errorf(`invalid bbTryjobDef(%q): must be like "project/bucket/builder"`, projSlashBucketSlashBuilder))
+			}
+			return &tryjob.Definition{
+				Backend: &tryjob.Definition_Buildbucket_{
+					Buildbucket: &tryjob.Definition_Buildbucket{
+						Host: bbHost,
+						Builder: &buildbucketpb.BuilderID{
+							Project: parts[0],
+							Bucket:  parts[1],
+							Builder: parts[2],
+						},
+					},
+				},
+			}
+		}
+
+		rid := common.MakeRunID(lProject, ct.Clock.Now(), 1, []byte("deadbeef"))
+		rs := &state.RunState{
+			Run: run.Run{
+				ID:         rid,
+				Status:     commonpb.Run_RUNNING,
+				CreateTime: ct.Clock.Now().UTC().Add(-2 * time.Minute),
+				StartTime:  ct.Clock.Now().UTC().Add(-1 * time.Minute),
+				CLs:        common.CLIDs{1},
+			},
+		}
+		h, _, rn, _ := makeTestImpl(&ct)
+
+		mig := migration.MigrationServer{RunNotifier: rn}
+		report := func(ts ...*migrationpb.Tryjob) {
+			ctx := auth.WithState(ctx, &authtest.FakeState{
+				Identity:             identity.Identity("project:infra"),
+				PeerIdentityOverride: "user:cqdaemon@example.com",
+			})
+			_, err := mig.ReportTryjobs(ctx, &migrationpb.ReportTryjobsRequest{
+				ConfigRevision: "doesn't matter",
+				RunId:          string(rid),
+				Tryjobs:        ts,
+			})
+			So(err, ShouldBeNil)
+		}
+
+		tryjobCreateTime := timestamppb.New(ct.Clock.Now().Add(-time.Hour))
+		makeTryjob := func(id int64, s migrationpb.TryjobStatus, o cvbqpb.Build_Origin) *migrationpb.Tryjob {
+			t := &migrationpb.Tryjob{
+				Builder:    bbTryjobDef(fmt.Sprintf("%s/try/bldr-%d", lProject, id)).GetBuildbucket().GetBuilder(),
+				CreateTime: tryjobCreateTime,
+				Status:     s,
+				Build: &cvbqpb.Build{
+					Host:     bbHost,
+					Id:       id,
+					Origin:   o,
+					Critical: true,
+				},
+			}
+			return t
+		}
+
+		Convey("Update state when Run is RUNNING", func() {
+			// Send the first report.
+			ct.Clock.Add(time.Minute)
+			reported1001 := makeTryjob(1001, migrationpb.TryjobStatus_RUNNING, cvbqpb.Build_REUSED)
+			report(reported1001)
+			expected1001 := &run.Tryjob{
+				Definition: bbTryjobDef(fmt.Sprintf("%s/try/bldr-1001", lProject)),
+				CqdDerived: true,
+				Id:         0,
+				Eversion:   0,
+				ExternalId: fmt.Sprintf("buildbucket/%s/1001", bbHost),
+				Reused:     true,
+				Status:     tryjob.Status_TRIGGERED,
+				Result: &tryjob.Result{
+					CreateTime: tryjobCreateTime,
+					Backend: &tryjob.Result_Buildbucket_{Buildbucket: &tryjob.Result_Buildbucket{
+						Id:     1001,
+						Status: buildbucketpb.Status_STARTED,
+					}},
+				},
+			}
+			// Send the second report.
+			ct.Clock.Add(time.Minute)
+			reported1002 := makeTryjob(1002, migrationpb.TryjobStatus_PENDING, cvbqpb.Build_NOT_REUSED)
+			report(reported1001, reported1002)
+			expected1002 := &run.Tryjob{
+				Definition: bbTryjobDef(fmt.Sprintf("%s/try/bldr-1002", lProject)),
+				CqdDerived: true,
+				Id:         0,
+				Eversion:   0,
+				ExternalId: fmt.Sprintf("buildbucket/%s/1002", bbHost),
+				Reused:     false,
+				Status:     tryjob.Status_TRIGGERED,
+				Result: &tryjob.Result{
+					CreateTime: tryjobCreateTime,
+					Backend: &tryjob.Result_Buildbucket_{Buildbucket: &tryjob.Result_Buildbucket{
+						Id:     1002,
+						Status: buildbucketpb.Status_SCHEDULED,
+					}},
+				},
+			}
+
+			// First import processes all available reports.
+			res, err := h.OnCQDTryjobsUpdated(ctx, rs)
+			So(err, ShouldBeNil)
+			So(res.SideEffectFn, ShouldBeNil)
+			So(res.PreserveEvents, ShouldBeFalse)
+			So(res.State.Run.Tryjobs.GetTryjobs(), ShouldHaveLength, 2)
+			// For easier to debug test, compare individual tryjobs first.
+			So(res.State.Run.Tryjobs.GetTryjobs()[0], ShouldResembleProto, expected1001)
+			So(res.State.Run.Tryjobs.GetTryjobs()[1], ShouldResembleProto, expected1002)
+			// And now compare entire Tryjobs state.
+			So(res.State.Run.Tryjobs, ShouldResembleProto, &run.Tryjobs{
+				CqdUpdateTime: timestamppb.New(ct.Clock.Now()),
+				Tryjobs:       []*run.Tryjob{expected1001, expected1002},
+				Requirement:   nil, // not important in this test
+			})
+			// Finally, there must be 2 diffs reported to RunLog.
+			So(res.State.LogEntries, ShouldHaveLength, 2)
+
+			// Follow up imports only not yet read reports.
+			priorState := res.State
+			backup := proto.Clone(priorState.Run.Tryjobs).(*run.Tryjobs)
+			ct.Clock.Add(time.Minute)
+			reported1001.Status = migrationpb.TryjobStatus_FAILED
+			reported1002.Status = migrationpb.TryjobStatus_SUCCEEDED
+			report(reported1001, reported1002)
+			res, err = h.OnCQDTryjobsUpdated(ctx, &state.RunState{Run: priorState.Run})
+			So(err, ShouldBeNil)
+			// Prior state must not be mutated by accident.
+			So(priorState.Run.Tryjobs, ShouldResembleProto, backup)
+			// And there should be exactly 1 log entry.
+			So(res.State.LogEntries, ShouldHaveLength, 1)
+			// There must still be 2 tryjobs, but with diff statuses now.
+			So(res.State.Run.Tryjobs.GetTryjobs(), ShouldHaveLength, 2)
+			So(res.State.Run.Tryjobs.GetTryjobs()[0].GetStatus(), ShouldEqual, tryjob.Status_ENDED)
+			So(res.State.Run.Tryjobs.GetTryjobs()[0].GetResult().GetStatus(), ShouldEqual, tryjob.Result_FAILED_PERMANENTLY)
+			So(res.State.Run.Tryjobs.GetTryjobs()[1].GetStatus(), ShouldEqual, tryjob.Status_ENDED)
+			So(res.State.Run.Tryjobs.GetTryjobs()[1].GetResult().GetStatus(), ShouldEqual, tryjob.Result_SUCCEEDED)
+
+			// Follow up report with the same tryjobs doesn't result in new Run Log
+			// entry.
+			ct.Clock.Add(time.Minute)
+			report(reported1001, reported1002)
+			res, err = h.OnCQDTryjobsUpdated(ctx, &state.RunState{Run: res.State.Run})
+			So(err, ShouldBeNil)
+			So(res.State.LogEntries, ShouldBeEmpty)
+
+			// Follow up report may drop tryjobs.
+			// This can happen iff a tryjob was reused from prior Run, but ultimately
+			// failed, at which point CQDaemon would stop tracking it.
+			ct.Clock.Add(time.Minute)
+			report(reported1002)
+			res, err = h.OnCQDTryjobsUpdated(ctx, &state.RunState{Run: res.State.Run})
+			So(err, ShouldBeNil)
+			So(res.State.Run.Tryjobs.GetTryjobs(), ShouldHaveLength, 1)
+			So(res.State.Run.Tryjobs.GetTryjobs()[0].GetExternalId(), ShouldResemble, expected1002.GetExternalId())
+			So(res.State.LogEntries, ShouldBeEmpty)
+		})
+
+		statuses := []commonpb.Run_Status{
+			commonpb.Run_SUCCEEDED,
+			commonpb.Run_FAILED,
+			commonpb.Run_CANCELLED,
+			commonpb.Run_WAITING_FOR_SUBMISSION,
+			commonpb.Run_SUBMITTING,
+		}
+		for _, status := range statuses {
+			Convey(fmt.Sprintf("Noop when Run is %s", status), func() {
+				rs.Run.Status = status
+				res, err := h.OnCQDTryjobsUpdated(ctx, rs)
+				So(err, ShouldBeNil)
+				So(res.State, ShouldEqual, rs)
+				So(res.SideEffectFn, ShouldBeNil)
+				switch {
+				case run.IsEnded(status):
+					So(res.PreserveEvents, ShouldBeFalse)
+				default:
+					So(res.PreserveEvents, ShouldBeTrue)
+				}
+			})
+		}
+	})
+}
