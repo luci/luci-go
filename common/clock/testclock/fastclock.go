acdb2646c7e3e79386eec31ebd521ab5ea9a006e
Andrii Shyshkalov <tandrii@google.com>
2021-08-18 22:27:45 +0000

    [testclock] impl. FastClock running X times faster than system clock.
    Motivation: use in CV integration tests which need to run
    multiple concurrent TQ tasks as well as simulate delays / retries
    in Gerrit polling. Previously, CV did this via hacks manually
    pushing test clock forward in a few locations, notably in timer
    callback. However, such pushing forward proved very unrealistic.
    
    In the follow up CL, I'll integrate this clock behind a flag
    for these CV tests. I was successfully able to run them at 100x to 500x
    rate, which still takes <10s in total and I've already
    discovered one bug.
    
    R=vadimsh, yiwzhang
    
    Change-Id: I70a62d1c00d0ba25f90f9dd62f62453f991bb4e3
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3100152
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

---
+++ common/clock/testclock/fastclock.go
@@ -0,0 +1,189 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testclock
+
+import (
+	"container/heap"
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"go.chromium.org/luci/common/clock"
+)
+
+// FastClock mimics faster physical clock in tests.
+//
+// Its API is exactly the same as that of TestClock and can be used in-place.
+// However, unlike TestClock, the time in this clock moves forward as
+// a normal wall clock would, but at an arbitrarily faster rate.
+//
+// It's useful for integration tests simulating a large system over long period
+// of (wall clock) time where adjusting each indiviudal timeout/delay/timer via
+// testclock API isn't feasible.
+type FastClock struct {
+	mutex          sync.Mutex
+	initSysTime    time.Time
+	initFastTime   time.Time
+	fastToSysRatio int
+	sysTimerSleep  time.Duration
+	done           chan struct{}
+	pendingTimers  pendingTimerHeap
+	timerCallback  TimerCallback
+	systemNow      func() time.Time // mocked in tests.
+}
+
+// NewFastClock creates a new FastClock running faster than a system clock.
+//
+// You SHOULD call .Close() on the returned object after use to avoid leaks.
+func NewFastClock(now time.Time, ratio int) *FastClock {
+	f := &FastClock{
+		initFastTime:   now,
+		initSysTime:    time.Now(),
+		fastToSysRatio: ratio,
+		sysTimerSleep:  10 * time.Microsecond,
+		done:           make(chan struct{}),
+		systemNow:      time.Now,
+	}
+	go f.periodicTimerNotify()
+	return f
+}
+
+// periodicTimerNotify follows system (wall) clock and wakes us timers as
+// necessary.
+func (f *FastClock) periodicTimerNotify() {
+	notify := func() {
+		f.mutex.Lock()
+		defer f.mutex.Unlock()
+		triggerTimersLocked(f.Now(), &f.pendingTimers)
+	}
+
+	// TODO(tandrii): wait until the earliest timer for efficiency.
+	sysTimer := time.NewTimer(f.sysTimerSleep)
+	for {
+		select {
+		case <-f.done:
+			return
+		case <-sysTimer.C:
+			notify()
+			sysTimer.Reset(f.sysTimerSleep)
+		}
+	}
+}
+
+// Close frees clock resources.
+func (f *FastClock) Close() {
+	close(f.done)
+}
+
+// Now returns the current time (see time.Now).
+func (f *FastClock) Now() time.Time {
+	_, fNow := f.now()
+	return fNow
+}
+
+// now returns system (wall) clock time and this clock's time.
+func (f *FastClock) now() (time.Time, time.Time) {
+	sNow := f.systemNow()
+	fNow := f.initFastTime.Add(sNow.Sub(f.initSysTime) * time.Duration(f.fastToSysRatio))
+	return sNow, fNow
+}
+
+// Sleep sleeps the current goroutine (see time.Sleep).
+//
+// Sleep will return a TimerResult containing the time when it was awakened
+// and detailing its execution. If the sleep terminated prematurely from
+// cancellation, the TimerResult's Incomplete() method will return true.
+func (f *FastClock) Sleep(ctx context.Context, d time.Duration) clock.TimerResult {
+	t := f.NewTimer(ctx)
+	t.Reset(d)
+	return <-t.GetC()
+}
+
+// NewTimer creates a new Timer instance, bound to this Clock.
+//
+// If the supplied Context is canceled, the timer will expire immediately.
+func (f *FastClock) NewTimer(ctx context.Context) clock.Timer {
+	return newTimer(ctx, f)
+}
+
+// Set sets the test clock's time.
+func (f *FastClock) Set(fNew time.Time) {
+	f.mutex.Lock()
+	defer f.mutex.Unlock()
+
+	sNow, fBefore := f.now()
+	if fNew.Before(fBefore) {
+		panic(fmt.Errorf("cannot go backwards in time. You're not Doc Brown.\nNow: %s\nTarget:%s", fBefore, fNew))
+	}
+	f.initSysTime = sNow
+	f.initFastTime = fNew
+
+	triggerTimersLocked(fNew, &f.pendingTimers)
+}
+
+// Add advances the test clock's time.
+func (f *FastClock) Add(d time.Duration) {
+	f.mutex.Lock()
+	defer f.mutex.Unlock()
+
+	sNow, fBefore := f.now()
+	if d < 0 {
+		panic(fmt.Errorf("cannot go backwards in time. You're not Doc Brown.\nDelta: %s", d))
+	}
+	f.initSysTime = sNow
+	f.initFastTime = fBefore.Add(d)
+	triggerTimersLocked(f.initFastTime, &f.pendingTimers)
+}
+
+// SetTimerCallback is a goroutine-safe method to set an instance-wide
+// callback that is invoked when any timer begins.
+func (f *FastClock) SetTimerCallback(clbk TimerCallback) {
+	f.mutex.Lock()
+	f.timerCallback = clbk
+	f.mutex.Unlock()
+}
+
+func (f *FastClock) addPendingTimer(t *timer, d time.Duration, triggerC chan<- time.Time) {
+	deadline := f.Now().Add(d)
+	if callback := f.timerCallback; callback != nil {
+		callback(d, t)
+	}
+
+	f.mutex.Lock()
+	defer f.mutex.Unlock()
+
+	heap.Push(&f.pendingTimers, &pendingTimer{
+		timer:    t,
+		deadline: deadline,
+		triggerC: triggerC,
+	})
+	_, now := f.now()
+	triggerTimersLocked(now, &f.pendingTimers)
+}
+
+func (f *FastClock) clearPendingTimer(t *timer) {
+	f.mutex.Lock()
+	defer f.mutex.Unlock()
+
+	for i := 0; i < len(f.pendingTimers); {
+		if e := f.pendingTimers[0]; e.timer == t {
+			heap.Remove(&f.pendingTimers, i)
+			close(e.triggerC)
+		} else {
+			i++
+		}
+	}
+}
---
+++ common/clock/testclock/fastclock_test.go
@@ -0,0 +1,162 @@
+// Copyright 2015 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testclock
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"go.chromium.org/luci/common/clock"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func ExampleFastClock() {
+	// New clock running 1M times faster than wall clock.
+	clk := NewFastClock(TestRecentTimeUTC, 1000*1000)
+	defer clk.Close()
+	ctx := clock.Set(context.Background(), clk)
+	wallStart := time.Now()
+	t := clock.NewTimer(ctx)
+	t.Reset(time.Hour)
+	<-t.GetC()
+	if wallTook := time.Since(wallStart); wallTook <= 10*time.Second {
+		fmt.Printf("took less than 10 seconds\n")
+		// Output: took less than 10 seconds
+	} else {
+		fmt.Printf("took %s, either due to a bug or CPU starvation\n", wallTook)
+	}
+}
+
+func TestFastClock(t *testing.T) {
+	t.Parallel()
+
+	Convey(`A FastClock instance`, t, func() {
+		epoch := time.Date(2015, 01, 01, 00, 00, 00, 00, time.UTC)
+		Convey(`Tied to a wall clock but running 1000 times faster`, func() {
+			fc := NewFastClock(epoch, 1000)
+			defer fc.Close()
+			ctx := clock.Set(context.Background(), fc)
+
+			Convey(`Returns the current time.`, func() {
+				wall0, fast0 := fc.now()
+				fast1 := fc.Now()
+				wall2, fast2 := fc.now()
+				// Check assumptions of the wall clock, which should be monotonically
+				// non-decreasing.
+				So(wall2, ShouldHappenOnOrAfter, wall0)
+				// Ditto for the FastClock.
+				So(fast0, ShouldHappenOnOrAfter, epoch)
+				So(fast1, ShouldHappenOnOrAfter, fast0)
+				So(fast2, ShouldHappenOnOrAfter, fast1)
+
+				Convey(`Runs faster than the wall clock.`, func() {
+					So(fast2.Sub(fast0), ShouldAlmostEqual, wall2.Sub(wall0)*1000)
+				})
+			})
+
+			Convey(`When sleeping with a time of zero, awakens quickly.`, func() {
+				wall0, _ := fc.now()
+				fc.Sleep(ctx, 0)
+				wall1, _ := fc.now()
+				// The smaller, the better, but allow for some CPU starvation.
+				So(wall1.Sub(wall0), ShouldBeLessThan, time.Minute)
+			})
+
+			Convey(`Will panic if going backwards in time.`, func() {
+				So(func() { fc.Add(-1 * time.Second) }, ShouldPanic)
+				So(func() { fc.Set(fc.Now().Add(-1 * time.Second)) }, ShouldPanic)
+			})
+
+			Convey(`When sleeping, awakens if canceled.`, func() {
+				ctx, cancelFunc := context.WithCancel(ctx)
+
+				fc.SetTimerCallback(func(_ time.Duration, _ clock.Timer) {
+					cancelFunc()
+				})
+
+				So(fc.Sleep(ctx, time.Hour).Incomplete(), ShouldBeTrue)
+			})
+
+			Convey(`Awakens after a period of time while manipulated.`, func() {
+				t0 := fc.Now()
+				afterC := clock.After(ctx, time.Second)
+				t1 := fc.Now()
+				fc.Set(t0.Add(time.Second)) // too early
+				fc.Set(t1.Add(time.Second)) // more than enough
+				res := <-afterC
+				So(res.Time, ShouldHappenOnOrBetween, t0.Add(time.Second), t1.Add(time.Second))
+			})
+
+			Convey(`Awakens quickly without being manipulated due to passage of wall clock time.`, func() {
+				const wallDelay = time.Millisecond
+				const fastDelay = time.Second // 1000x faster
+
+				wall0, fast0 := fc.now()
+				fastAfter := clock.After(ctx, fastDelay)
+
+				wallAfter := time.After(wallDelay)
+				<-wallAfter
+				wall1, _ := fc.now()
+				So(wall1.Sub(wall0), ShouldBeGreaterThan, wallDelay)
+
+				fastAwoken := <-fastAfter
+				fastOverhead := (fastAwoken.Sub(fast0) - fastDelay)
+				wallOverhead := fastOverhead / 1000
+				_, _ = Println(" overhead:", "Wall", wallOverhead, "FastClock", fastOverhead)
+				So(wallOverhead, ShouldBeGreaterThan, time.Duration(0)) // check sanity
+				// The smaller, the better, but allow for some CPU starvation.
+				So(wallOverhead, ShouldBeLessThan, time.Minute)
+			})
+		})
+
+		Convey(`Tied to a wall clock mock`, func() {
+			wallEpoch := time.Date(2020, 01, 01, 00, 00, 00, 00, time.UTC)
+			fc := &FastClock{
+				fastToSysRatio: 1000,
+				initFastTime:   epoch,
+				initSysTime:    wallEpoch,
+				systemNow:      func() time.Time { return wallEpoch },
+			}
+			ctx := clock.Set(context.Background(), fc)
+
+			Convey(`When sleeping with a time of zero, awakens immediately.`, func() {
+				before := fc.Now()
+				fc.Sleep(ctx, 0)
+				So(fc.Now(), ShouldEqual, before)
+			})
+
+			Convey(`When sleeping for a period of time, awakens when signalled.`, func() {
+				sleepingC := make(chan struct{})
+				fc.SetTimerCallback(func(_ time.Duration, _ clock.Timer) {
+					close(sleepingC)
+				})
+
+				awakeC := make(chan time.Time)
+				go func() {
+					fc.Sleep(ctx, 2*time.Second)
+					awakeC <- fc.Now()
+				}()
+
+				<-sleepingC
+				fc.Set(epoch.Add(1 * time.Second))
+				fc.Set(epoch.Add(2 * time.Second))
+				So(<-awakeC, ShouldResemble, epoch.Add(2*time.Second))
+			})
+		})
+	})
+}
