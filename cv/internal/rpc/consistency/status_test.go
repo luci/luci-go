8a4616a0c123f3555920494bc65fe9abfdd52596
Andrii Shyshkalov <tandrii@google.com>
2021-09-08 19:16:33 +0000

    [cv] add a test to avoid disparity between the Run status protos.
    R=ddoman, yiwzhang
    
    Bug: 1247080
    Change-Id: I59c505ca411d8801856d8639242b9b7d33032f15
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3144054
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>

---
+++ cv/internal/rpc/consistency/status_test.go
@@ -0,0 +1,50 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package consistency
+
+import (
+	"testing"
+
+	commonpb "go.chromium.org/luci/cv/api/common/v1"
+	"go.chromium.org/luci/cv/internal/run"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestStatusV1(t *testing.T) {
+	t.Parallel()
+
+	Convey("Ensure CV API v1 Run Status matches the internal Status.", t, func() {
+		// v1 -> internal.
+		for name, val := range commonpb.Run_Status_value {
+			name, v1 := name, val
+			Convey("v1."+name, func() {
+				internal, exists := run.Status_value[name]
+				So(exists, ShouldBeTrue)
+				So(internal, ShouldEqual, v1)
+			})
+		}
+
+		// Internal -> v1.
+		for name, val := range run.Status_value {
+			name, v1 := name, val
+			Convey("internal."+name, func() {
+				internal, exists := commonpb.Run_Status_value[name]
+				So(exists, ShouldBeTrue)
+				So(internal, ShouldEqual, v1)
+			})
+		}
+	})
+}
