6d04b397d53aaa547fabfe68170f575480d7c8b3
Scott Lee <ddoman@chromium.org>
2021-09-15 01:01:04 +0000

    [buildbucket] separate out metricss defs from build_events
    V2 metrics will be added, and each of metrics_$version.go will contain
    - a list of the metrics for the version, and
    - utility functions that convert an internal field into an equivalent
    in the version.
    
    build_events.go will be used to connect build events with metric
    and pubsub events.
    
    Bug: 1249664
    Change-Id: Ieba25b0918934295d930a592827cad6162083166
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3161030
    Commit-Queue: Scott Lee <ddoman@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

--- buildbucket/appengine/rpc/build_events.go
+++ buildbucket/appengine/rpc/build_events.go
@@ -17,15 +17,10 @@ package rpc
 import (
 	"context"
 	"fmt"
-	"math"
 
 	"go.chromium.org/luci/common/data/strpair"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/sync/parallel"
-	"go.chromium.org/luci/common/tsmon/distribution"
-	"go.chromium.org/luci/common/tsmon/field"
-	"go.chromium.org/luci/common/tsmon/metric"
-	"go.chromium.org/luci/common/tsmon/types"
 	"go.chromium.org/luci/server/auth"
 
 	"go.chromium.org/luci/buildbucket/appengine/model"
@@ -33,98 +28,6 @@ import (
 	taskdefs "go.chromium.org/luci/buildbucket/appengine/tasks/defs"
 	pb "go.chromium.org/luci/buildbucket/proto"
 )
-
-var (
-	// A common set of field definitions for build metrics.
-	fieldDefs = map[string]field.Field{
-		"bucket":             field.String("bucket"),
-		"builder":            field.String("builder"),
-		"canary":             field.Bool("canary"),
-		"cancelation_reason": field.String("cancelation_reason"),
-		"failure_reason":     field.String("failure_reason"),
-		"result":             field.String("result"),
-		"user_agent":         field.String("user_agent"),
-	}
-
-	buildCountCreated = metric.NewCounter(
-		"buildbucket/builds/created",
-		"Build creation", nil,
-		bFields("user_agent")...,
-	)
-	buildCountStarted = metric.NewCounter(
-		"buildbucket/builds/started",
-		"Build start", nil,
-		bFields("canary")...,
-	)
-	buildCountCompleted = metric.NewCounter(
-		"buildbucket/builds/completed",
-		"Build completion, including success, failure and cancellation", nil,
-		bFields("result", "failure_reason", "cancelation_reason", "canary")...,
-	)
-
-	buildDurationCycle = newbuildDurationMetric(
-		"buildbucket/builds/cycle_durations",
-		"Duration between build creation and completion",
-	)
-	buildDurationRun = newbuildDurationMetric(
-		"buildbucket/builds/run_durations",
-		"Duration between build start and completion",
-	)
-	buildDurationScheduling = newbuildDurationMetric(
-		"buildbucket/builds/scheduling_durations",
-		"Duration between build creation and start",
-	)
-)
-
-func bFields(extraFields ...string) []field.Field {
-	fs := make([]field.Field, 2+len(extraFields))
-	fs[0], fs[1] = fieldDefs["bucket"], fieldDefs["builder"]
-	for i, n := range extraFields {
-		f, ok := fieldDefs[n]
-		if !ok {
-			panic(fmt.Sprintf("unknown build field %q", n))
-		}
-		fs[i+2] = f
-	}
-	return fs
-}
-
-func newbuildDurationMetric(name, description string, extraFields ...string) metric.CumulativeDistribution {
-	fs := []string{"result", "failure_reason", "cancelation_reason", "canary"}
-	return metric.NewCumulativeDistribution(
-		name, description, &types.MetricMetadata{Units: types.Seconds},
-		// Bucketer for 1s..48h range
-		distribution.GeometricBucketer(math.Pow(10, 0.053), 100),
-		bFields(append(fs, extraFields...)...)...,
-	)
-}
-
-func getLegacyMetricFields(b *model.Build) (result, failureR, cancelationR string) {
-	// The default values are "" instead of UNSET for backwards compatibility.
-	switch b.Status {
-	case pb.Status_SCHEDULED:
-	case pb.Status_STARTED:
-	case pb.Status_SUCCESS:
-		result = model.Success.String()
-	case pb.Status_FAILURE:
-		result = model.Failure.String()
-		failureR = model.BuildFailure.String()
-	case pb.Status_INFRA_FAILURE:
-		if b.Proto.StatusDetails.GetTimeout() != nil {
-			result = model.Canceled.String()
-			cancelationR = model.TimeoutCanceled.String()
-		} else {
-			result = model.Failure.String()
-			failureR = model.InfraFailure.String()
-		}
-	case pb.Status_CANCELED:
-		result = model.Canceled.String()
-		cancelationR = model.ExplicitlyCanceled.String()
-	default:
-		panic(fmt.Sprintf("getLegacyMetricFields: invalid status %q", b.Status))
-	}
-	return
-}
 
 func buildCreated(ctx context.Context, b *model.Build) {
 	var ua string
---
+++ buildbucket/appengine/rpc/metrics_v1.go
@@ -0,0 +1,119 @@
+// Copyright 2020 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"fmt"
+	"math"
+
+	"go.chromium.org/luci/buildbucket/appengine/model"
+	pb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/tsmon/distribution"
+	"go.chromium.org/luci/common/tsmon/field"
+	"go.chromium.org/luci/common/tsmon/metric"
+	"go.chromium.org/luci/common/tsmon/types"
+)
+
+var (
+	// A common set of field definitions for build metrics.
+	fieldDefs = map[string]field.Field{
+		"bucket":             field.String("bucket"),
+		"builder":            field.String("builder"),
+		"canary":             field.Bool("canary"),
+		"cancelation_reason": field.String("cancelation_reason"),
+		"failure_reason":     field.String("failure_reason"),
+		"result":             field.String("result"),
+		"user_agent":         field.String("user_agent"),
+	}
+
+	buildCountCreated = metric.NewCounter(
+		"buildbucket/builds/created",
+		"Build creation", nil,
+		bFields("user_agent")...,
+	)
+	buildCountStarted = metric.NewCounter(
+		"buildbucket/builds/started",
+		"Build start", nil,
+		bFields("canary")...,
+	)
+	buildCountCompleted = metric.NewCounter(
+		"buildbucket/builds/completed",
+		"Build completion, including success, failure and cancellation", nil,
+		bFields("result", "failure_reason", "cancelation_reason", "canary")...,
+	)
+
+	buildDurationCycle = newbuildDurationMetric(
+		"buildbucket/builds/cycle_durations",
+		"Duration between build creation and completion",
+	)
+	buildDurationRun = newbuildDurationMetric(
+		"buildbucket/builds/run_durations",
+		"Duration between build start and completion",
+	)
+	buildDurationScheduling = newbuildDurationMetric(
+		"buildbucket/builds/scheduling_durations",
+		"Duration between build creation and start",
+	)
+)
+
+func bFields(extraFields ...string) []field.Field {
+	fs := make([]field.Field, 2+len(extraFields))
+	fs[0], fs[1] = fieldDefs["bucket"], fieldDefs["builder"]
+	for i, n := range extraFields {
+		f, ok := fieldDefs[n]
+		if !ok {
+			panic(fmt.Sprintf("unknown build field %q", n))
+		}
+		fs[i+2] = f
+	}
+	return fs
+}
+
+func newbuildDurationMetric(name, description string, extraFields ...string) metric.CumulativeDistribution {
+	fs := []string{"result", "failure_reason", "cancelation_reason", "canary"}
+	return metric.NewCumulativeDistribution(
+		name, description, &types.MetricMetadata{Units: types.Seconds},
+		// Bucketer for 1s..48h range
+		distribution.GeometricBucketer(math.Pow(10, 0.053), 100),
+		bFields(append(fs, extraFields...)...)...,
+	)
+}
+
+func getLegacyMetricFields(b *model.Build) (result, failureR, cancelationR string) {
+	// The default values are "" instead of UNSET for backwards compatibility.
+	switch b.Status {
+	case pb.Status_SCHEDULED:
+	case pb.Status_STARTED:
+	case pb.Status_SUCCESS:
+		result = model.Success.String()
+	case pb.Status_FAILURE:
+		result = model.Failure.String()
+		failureR = model.BuildFailure.String()
+	case pb.Status_INFRA_FAILURE:
+		if b.Proto.StatusDetails.GetTimeout() != nil {
+			result = model.Canceled.String()
+			cancelationR = model.TimeoutCanceled.String()
+		} else {
+			result = model.Failure.String()
+			failureR = model.InfraFailure.String()
+		}
+	case pb.Status_CANCELED:
+		result = model.Canceled.String()
+		cancelationR = model.ExplicitlyCanceled.String()
+	default:
+		panic(fmt.Sprintf("getLegacyMetricFields: invalid status %q", b.Status))
+	}
+	return
+}
