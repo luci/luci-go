8ffa903c6e3b1411f0adac031ca10265788ed853
Vadim Shtayura <vadimsh@chromium.org>
2021-09-27 18:22:53 +0000

    [buildbucket] Implement BuildMask.
    R=iannucci@chromium.org, tandrii@chromium.org
    BUG=1219676
    
    Change-Id: I349124983bb9b6513c63197657f8f8aefcf263ce
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3180049
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

--- buildbucket/appengine/model/mask.go
+++ buildbucket/appengine/model/mask.go
@@ -17,63 +17,155 @@ package model
 import (
 	"fmt"
 
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 
 	pb "go.chromium.org/luci/buildbucket/proto"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/proto/mask"
+	"go.chromium.org/luci/common/proto/structmask"
+)
+
+// The default field mask to use for read requests.
+var defaultFieldMask = fieldmaskpb.FieldMask{
+	Paths: []string{
+		"builder",
+		"canary",
+		"create_time",
+		"created_by",
+		"critical",
+		"end_time",
+		"id",
+		"input.experimental",
+		"input.gerrit_changes",
+		"input.gitiles_commit",
+		"number",
+		"start_time",
+		"status",
+		"status_details",
+		"update_time",
+	},
+}
+
+// Used just for their type information.
+var (
+	buildPrototype       = pb.Build{}
+	searchBuildPrototype = pb.SearchBuildsResponse{}
 )
 
 // NoopBuildMask selects all fields.
-var NoopBuildMask = &BuildMask{mask.All(&pb.Build{})}
+var NoopBuildMask = &BuildMask{m: mask.All(&buildPrototype)}
 
 // DefaultBuildMask is the default mask to use for read requests.
-var DefaultBuildMask = HardcodedBuildMask(
-	"builder",
-	"canary",
-	"create_time",
-	"created_by",
-	"critical",
-	"end_time",
-	"id",
-	"input.experimental",
-	"input.gerrit_changes",
-	"input.gitiles_commit",
-	"number",
-	"start_time",
-	"status",
-	"status_details",
-	"update_time",
-)
+var DefaultBuildMask = HardcodedBuildMask(defaultFieldMask.Paths...)
 
 // BuildMask knows how to filter pb.Build proto messages.
 type BuildMask struct {
-	m *mask.Mask
+	m   *mask.Mask         // the overall field mask
+	in  *structmask.Filter // "input.properties" filter
+	out *structmask.Filter // "output.properties" filter
+	req *structmask.Filter // "infra.buildbucket.requested_properties" filter
 }
 
 // NewBuildMask constructs a build mask either using a legacy `fields` FieldMask
-// or new `mask` BuildMask.
+// or new `mask` BuildMask (but not both at the same time, pick one).
 //
 // legacyPrefix is usually "", but can be "builds" to trim "builds." from
 // the legacy field mask (used by SearchBuilds API).
 //
 // If the mask is empty, returns DefaultBuildMask.
-//
-// TODO(crbug.com/1219676): Recognize and use `buildMask`.
-func NewBuildMask(legacyPrefix string, legacy *fieldmaskpb.FieldMask, buildMask *pb.BuildMask) (*BuildMask, error) {
-	if buildMask != nil {
-		return nil, errors.Reason("`mask` field is not implemented yet").Err()
+func NewBuildMask(legacyPrefix string, legacy *fieldmaskpb.FieldMask, bm *pb.BuildMask) (*BuildMask, error) {
+	switch {
+	case legacy == nil && bm == nil:
+		return DefaultBuildMask, nil
+	case legacy != nil && bm != nil:
+		return nil, errors.Reason("`mask` and `fields` can't be used together, prefer `mask` since `fields` is deprecated").Err()
+	case legacy != nil:
+		return newLegacyBuildMask(legacyPrefix, legacy)
 	}
-	if len(legacy.GetPaths()) == 0 {
+
+	fm := bm.Fields
+	if len(fm.GetPaths()) == 0 {
+		fm = &defaultFieldMask
+	}
+
+	var cloned bool
+	structFilter := func(path string, structMask []*structmask.StructMask) (*structmask.Filter, error) {
+		if len(structMask) == 0 {
+			return nil, nil
+		}
+		// Implicitly include struct-valued fields when their masks are present.
+		// Make sure not to accidentally override the original FieldMask
+		// (in particular when it is &defaultFieldMask).
+		if !cloned {
+			fm = proto.Clone(fm).(*fieldmaskpb.FieldMask)
+			cloned = true
+		}
+		fm.Paths = append(fm.Paths, path)
+		return structmask.NewFilter(structMask)
+	}
+
+	// Parse struct masks. This also mutates `fm` to include corresponding fields.
+	in, err := structFilter("input.properties", bm.InputProperties)
+	if err != nil {
+		return nil, errors.Annotate(err, `bad "input_properties" struct mask`).Err()
+	}
+	out, err := structFilter("output.properties", bm.OutputProperties)
+	if err != nil {
+		return nil, errors.Annotate(err, `bad "output_properties" struct mask`).Err()
+	}
+	req, err := structFilter("infra.buildbucket.requested_properties", bm.RequestedProperties)
+	if err != nil {
+		return nil, errors.Annotate(err, `bad "requested_properties" struct mask`).Err()
+	}
+
+	// Construct the overall pb.Build mask.
+	var m *mask.Mask
+	if fm == &defaultFieldMask {
+		// An optimization for the common case, to avoid constructing mask.Mask all
+		// the time.
+		m = DefaultBuildMask.m
+	} else {
+		var err error
+		if m, err = mask.FromFieldMask(fm, &buildPrototype, false, false); err != nil {
+			return nil, err
+		}
+	}
+
+	// We want to support only field masks compatible with Go protobuf library.
+	// Note that "go.chromium.org/luci/common/proto/mask" implements a superset
+	// of this functionality. It also returns detailed errors. So we used it first
+	// to reject obviously invalid masks (e.g. referring to unknown fields) with
+	// nice error messages, and use a blunt IsValid check below to reject no
+	// longer supported non-protobuf compatible masks.
+	if !fm.IsValid(&buildPrototype) {
+		return nil, errors.Reason(
+			"the extended field mask syntax is no longer supported, " +
+				"use the standard one: " +
+				"https://pkg.go.dev/google.golang.org/protobuf/types/known/fieldmaskpb#FieldMask",
+		).Err()
+	}
+
+	return &BuildMask{
+		m:   m,
+		in:  in,
+		out: out,
+		req: req,
+	}, nil
+}
+
+// newLegacyBuildMask constructs BuildMask from legacy `fields` field.
+func newLegacyBuildMask(legacyPrefix string, fields *fieldmaskpb.FieldMask) (*BuildMask, error) {
+	if len(fields.GetPaths()) == 0 {
 		return DefaultBuildMask, nil
 	}
 	var m *mask.Mask
 	var err error
 	switch legacyPrefix {
 	case "":
-		m, err = mask.FromFieldMask(legacy, &pb.Build{}, false, false)
+		m, err = mask.FromFieldMask(fields, &buildPrototype, false, false)
 	case "builds":
-		m, err = mask.FromFieldMask(legacy, &pb.SearchBuildsResponse{}, false, false)
+		m, err = mask.FromFieldMask(fields, &searchBuildPrototype, false, false)
 		if err == nil {
 			m, err = m.Submask("builds.*")
 		}
@@ -83,7 +175,7 @@ func NewBuildMask(legacyPrefix string, legacy *fieldmaskpb.FieldMask, buildMask 
 	if err != nil {
 		return nil, err
 	}
-	return &BuildMask{m}, nil
+	return &BuildMask{m: m}, nil
 }
 
 // HardcodedBuildMask returns a build mask with given fields.
@@ -91,7 +183,7 @@ func NewBuildMask(legacyPrefix string, legacy *fieldmaskpb.FieldMask, buildMask 
 // Panics if some of them are invalid. Intended to be used to initialize
 // constants or in tests.
 func HardcodedBuildMask(fields ...string) *BuildMask {
-	return &BuildMask{mask.MustFromReadMask(&pb.Build{}, fields...)}
+	return &BuildMask{m: mask.MustFromReadMask(&buildPrototype, fields...)}
 }
 
 // Includes returns true if the given field path is included in the mask
@@ -108,5 +200,17 @@ func (m *BuildMask) Includes(fieldPath string) bool {
 
 // Trim applies the mask to the build in-place.
 func (m *BuildMask) Trim(b *pb.Build) error {
-	return m.m.Trim(b)
+	if err := m.m.Trim(b); err != nil {
+		return err
+	}
+	if m.in != nil && b.Input != nil {
+		b.Input.Properties = m.in.Apply(b.Input.Properties)
+	}
+	if m.out != nil && b.Output != nil {
+		b.Output.Properties = m.out.Apply(b.Output.Properties)
+	}
+	if m.req != nil && b.Infra != nil && b.Infra.Buildbucket != nil {
+		b.Infra.Buildbucket.RequestedProperties = m.req.Apply(b.Infra.Buildbucket.RequestedProperties)
+	}
+	return nil
 }
---
+++ buildbucket/appengine/model/mask_test.go
@@ -0,0 +1,351 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package model
+
+import (
+	"encoding/json"
+	"testing"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+	"google.golang.org/protobuf/types/known/structpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	pb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/proto/structmask"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestBuildMask(t *testing.T) {
+	t.Parallel()
+
+	type inner struct {
+		FieldOne string `json:"field_one,omitempty"`
+		FieldTwo string `json:"field_two,omitempty"`
+	}
+
+	type testStruct struct {
+		Str  string            `json:"str,omitempty"`
+		Int  int               `json:"int,omitempty"`
+		Map  map[string]string `json:"map,omitempty"`
+		List []inner           `json:"list,omitempty"`
+	}
+
+	build := pb.Build{
+		Id: 12345,
+		Builder: &pb.BuilderID{
+			Project: "project",
+			Bucket:  "bucket",
+			Builder: "builder",
+		},
+		Number:          7777,
+		Canary:          true,
+		CreatedBy:       "created_by",
+		CanceledBy:      "canceled_by",
+		CreateTime:      &timestamppb.Timestamp{Nanos: 11111111},
+		StartTime:       &timestamppb.Timestamp{Nanos: 22222222},
+		EndTime:         &timestamppb.Timestamp{Nanos: 33333333},
+		UpdateTime:      &timestamppb.Timestamp{Nanos: 44444444},
+		Status:          pb.Status_SUCCESS,
+		SummaryMarkdown: "summary_markdown",
+		Critical:        pb.Trinary_YES,
+		StatusDetails:   &pb.StatusDetails{Timeout: &pb.StatusDetails_Timeout{}},
+		Input: &pb.Build_Input{
+			Properties: asStructPb(testStruct{
+				Str: "input",
+				Int: 123,
+				Map: map[string]string{
+					"ik1": "iv1",
+					"ik2": "iv2",
+				},
+				List: []inner{
+					{"11", "11"},
+					{"21", "22"},
+				},
+			}),
+			GerritChanges: []*pb.GerritChange{
+				{Host: "h1"},
+				{Host: "h2"},
+			},
+			GitilesCommit: &pb.GitilesCommit{Host: "ihost"},
+			Experimental:  true,
+		},
+		Output: &pb.Build_Output{
+			Properties: asStructPb(testStruct{
+				Str: "output",
+				Int: 123,
+				Map: map[string]string{
+					"ok1": "ov1",
+					"ok2": "ov2",
+				},
+				List: []inner{
+					{"11", "11"},
+					{"21", "22"},
+				},
+			}),
+			GitilesCommit: &pb.GitilesCommit{Host: "ohost"},
+		},
+		Steps: []*pb.Step{
+			{Name: "s1", SummaryMarkdown: "md1"},
+			{Name: "s2", SummaryMarkdown: "md2"},
+		},
+		Infra: &pb.BuildInfra{
+			Buildbucket: &pb.BuildInfra_Buildbucket{
+				Hostname: "bb-host",
+				RequestedProperties: asStructPb(testStruct{
+					Str: "requested",
+					Int: 123,
+					Map: map[string]string{
+						"rk1": "rv1",
+						"rk2": "rv2",
+					},
+					List: []inner{
+						{"11", "11"},
+						{"21", "22"},
+					},
+				}),
+			},
+			Logdog: &pb.BuildInfra_LogDog{Hostname: "logdog-host"},
+		},
+		Tags: []*pb.StringPair{
+			{Key: "k1", Value: "v1"},
+			{Key: "k2", Value: "v2"},
+		},
+		Exe:               &pb.Executable{},
+		SchedulingTimeout: &durationpb.Duration{Seconds: 111},
+		ExecutionTimeout:  &durationpb.Duration{Seconds: 222},
+		GracePeriod:       &durationpb.Duration{Seconds: 333},
+	}
+
+	afterDefaultMask := &pb.Build{
+		Builder:    build.Builder,
+		Canary:     build.Canary,
+		CreateTime: build.CreateTime,
+		CreatedBy:  build.CreatedBy,
+		Critical:   build.Critical,
+		EndTime:    build.EndTime,
+		Id:         build.Id,
+		Input: &pb.Build_Input{
+			Experimental:  build.Input.Experimental,
+			GerritChanges: build.Input.GerritChanges,
+			GitilesCommit: build.Input.GitilesCommit,
+		},
+		Number:        build.Number,
+		StartTime:     build.StartTime,
+		Status:        build.Status,
+		StatusDetails: build.StatusDetails,
+		UpdateTime:    build.UpdateTime,
+	}
+
+	apply := func(m *BuildMask) (*pb.Build, error) {
+		b := proto.Clone(&build).(*pb.Build)
+		err := m.Trim(b)
+		return b, err
+	}
+
+	Convey("Default", t, func() {
+		Convey("No masks at all", func() {
+			m, err := NewBuildMask("", nil, nil)
+			So(err, ShouldBeNil)
+			b, err := apply(m)
+			So(err, ShouldBeNil)
+			So(b, ShouldResembleProto, afterDefaultMask)
+		})
+
+		Convey("Legacy", func() {
+			m, err := NewBuildMask("", &fieldmaskpb.FieldMask{}, nil)
+			So(err, ShouldBeNil)
+			b, err := apply(m)
+			So(err, ShouldBeNil)
+			So(b, ShouldResembleProto, afterDefaultMask)
+		})
+
+		Convey("BuildMask", func() {
+			m, err := NewBuildMask("", nil, &pb.BuildMask{})
+			So(err, ShouldBeNil)
+			b, err := apply(m)
+			So(err, ShouldBeNil)
+			So(b, ShouldResembleProto, afterDefaultMask)
+		})
+	})
+
+	Convey("Legacy", t, func() {
+		m, err := NewBuildMask("", &fieldmaskpb.FieldMask{
+			Paths: []string{
+				"builder",
+				"input.properties",
+				"steps.*.name", // note: extended syntax
+			},
+		}, nil)
+		So(err, ShouldBeNil)
+
+		b, err := apply(m)
+		So(err, ShouldBeNil)
+		So(b, ShouldResembleProto, &pb.Build{
+			Builder: build.Builder,
+			Input: &pb.Build_Input{
+				Properties: build.Input.Properties,
+			},
+			Steps: []*pb.Step{
+				{Name: "s1"},
+				{Name: "s2"},
+			},
+		})
+	})
+
+	Convey("Simple build mask", t, func() {
+		m, err := NewBuildMask("", nil, &pb.BuildMask{
+			Fields: &fieldmaskpb.FieldMask{
+				Paths: []string{
+					"builder",
+					"steps",
+					"input.properties", // will be returned unfiltered
+				},
+			},
+		})
+		So(err, ShouldBeNil)
+
+		b, err := apply(m)
+		So(err, ShouldBeNil)
+		So(b, ShouldResembleProto, &pb.Build{
+			Builder: build.Builder,
+			Steps: []*pb.Step{
+				{Name: "s1", SummaryMarkdown: "md1"},
+				{Name: "s2", SummaryMarkdown: "md2"},
+			},
+			Input: &pb.Build_Input{
+				Properties: build.Input.Properties,
+			},
+		})
+	})
+
+	Convey("Struct filters", t, func() {
+		m, err := NewBuildMask("", nil, &pb.BuildMask{
+			Fields: &fieldmaskpb.FieldMask{
+				Paths: []string{
+					"builder",
+					"input.gerrit_changes",
+					"output.properties", // will be filtered, since we have a mask below
+				},
+			},
+			InputProperties: []*structmask.StructMask{
+				{Path: []string{"str"}},
+				{Path: []string{"map", "ik1"}},
+				{Path: []string{"list", "*", "field_two"}},
+				{Path: []string{"unknown"}},
+			},
+			OutputProperties: []*structmask.StructMask{
+				{Path: []string{"str"}},
+			},
+			RequestedProperties: []*structmask.StructMask{
+				{Path: []string{"unknown"}},
+			},
+		})
+		So(err, ShouldBeNil)
+
+		b, err := apply(m)
+		So(err, ShouldBeNil)
+		So(b, ShouldResembleProto, &pb.Build{
+			Builder: build.Builder,
+			Input: &pb.Build_Input{
+				GerritChanges: build.Input.GerritChanges,
+				Properties: asStructPb(testStruct{
+					Str: "input",
+					Map: map[string]string{"ik1": "iv1"},
+					List: []inner{
+						{"", "11"},
+						{"", "22"},
+					},
+				}),
+			},
+			Output: &pb.Build_Output{
+				Properties: asStructPb(testStruct{
+					Str: "output",
+				}),
+			},
+			Infra: &pb.BuildInfra{
+				Buildbucket: &pb.BuildInfra_Buildbucket{
+					RequestedProperties: &structpb.Struct{}, // all was filtered out
+				},
+			},
+		})
+	})
+
+	Convey("Struct filters and default mask", t, func() {
+		m, err := NewBuildMask("", nil, &pb.BuildMask{
+			OutputProperties: []*structmask.StructMask{
+				{Path: []string{"str"}},
+			},
+		})
+		So(err, ShouldBeNil)
+
+		b, err := apply(m)
+		So(err, ShouldBeNil)
+
+		expected := proto.Clone(afterDefaultMask).(*pb.Build)
+		expected.Output = &pb.Build_Output{
+			Properties: asStructPb(testStruct{Str: "output"}),
+		}
+		So(b, ShouldResembleProto, expected)
+	})
+
+	Convey("Unknown mask paths", t, func() {
+		_, err := NewBuildMask("", nil, &pb.BuildMask{
+			Fields: &fieldmaskpb.FieldMask{
+				Paths: []string{"builderzzz"},
+			},
+		})
+		So(err, ShouldErrLike, `field "builderzzz" does not exist in message Build`)
+	})
+
+	Convey("Bad struct mask", t, func() {
+		_, err := NewBuildMask("", nil, &pb.BuildMask{
+			InputProperties: []*structmask.StructMask{
+				{Path: []string{"'unbalanced"}},
+			},
+		})
+		So(err, ShouldErrLike, `bad "input_properties" struct mask: bad element "'unbalanced" in the mask`)
+	})
+
+	Convey("Unsupported extended syntax", t, func() {
+		_, err := NewBuildMask("", nil, &pb.BuildMask{
+			Fields: &fieldmaskpb.FieldMask{
+				Paths: []string{"steps.*.name"},
+			},
+		})
+		So(err, ShouldErrLike, "no longer supported")
+	})
+
+	Convey("Legacy and new at the same time are not allowed", t, func() {
+		_, err := NewBuildMask("", &fieldmaskpb.FieldMask{}, &pb.BuildMask{})
+		So(err, ShouldErrLike, "can't be used together")
+	})
+}
+
+func asStructPb(v interface{}) *structpb.Struct {
+	blob, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	s := &structpb.Struct{}
+	if err := (protojson.UnmarshalOptions{}).Unmarshal(blob, s); err != nil {
+		panic(err)
+	}
+	return s
+}
