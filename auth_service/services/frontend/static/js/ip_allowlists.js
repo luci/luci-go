7e6364e0e1127d6276caab5da27e87ebecf49478
Camilo Jacomet <cjacomet@google.com>
2021-10-13 22:56:34 +0000

    [auth-service]: Populate allowlist pane with content.
    Set up AllowlistPane class and populate pane with selected allowlist.
    Populate with default first Allowlist selected.
    
    Bug:1112832
    Change-Id: I34b171daae51320ad9eec582e79f46ce0e4f633e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3198975
    Commit-Queue: Camilo Jacomet <cjacomet@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

--- auth_service/services/frontend/static/js/ip_allowlists.js
+++ auth_service/services/frontend/static/js/ip_allowlists.js
@@ -18,35 +18,103 @@ class Selector {
   constructor(element) {
     // Root DOM element.
     this.element = document.getElementById(element);
-  }
-
-  reloadAllowlists() {
-    let defer = api.ipAllowlists();
-    defer
-      .then((response) => {
-        this.populate(response.allowlists);
-      })
-      .catch((err) => {
-        console.log(err);
+    // Map of allowlists.
+    this.allowlistMap = {};
+    // Setup event listener for when selection changes.
+    this.element.addEventListener('change', (event) => {
+      // TODO(cjacomet): Improve this to not have to iterate over all options.
+      // Change which option has the selected attribute.
+      this.element.childNodes.forEach((option) => {
+        if (!option.dataset.selected && option.text === event.target.value) {
+          option.setAttribute('data-selected', 'selected');
+        } else {
+          option.setAttribute('data-selected', '');
+        }
       });
-    return defer;
+      this.onSelectionChanged(event.target.value);
+    });
   }
 
   populate(allowlists) {
+    // Empty the element before populating.
+    this.element.innerHTML = '';
+    let selected = null;
+
     let addToSelector = (name) => {
       let option = document.createElement('option');
       option.text = name;
+      option.setAttribute('data-allowlist-name', name);
       this.element.appendChild(option);
+      if (selected === null) {
+        selected = option;
+      }
     };
 
-    // All allowlists
+    // All allowlists, populate the selector element.
     allowlists.map((list) => {
+      this.allowlistMap[list.name] = list;
       addToSelector(list.name);
     });
+
+    selected.setAttribute('data-selected', 'selected');
+    this.onSelectionChanged();
+  }
+
+  onSelectionChanged() {
+    let selectedOption = this.element.querySelector(
+      '[data-selected="selected"]'
+    );
+    const allowlistSelectedEvent = new CustomEvent('allowlistSelected', {
+      bubble: true,
+      detail: {
+        ip_allowlist: selectedOption.text,
+      },
+    });
+
+    this.element.dispatchEvent(allowlistSelectedEvent);
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// AllowlistPane is the panel with information about some selected IP allowlist.
+class AllowlistPane {
+  constructor() {
+    this.descriptionBox = document.getElementById('description-box');
+    this.subnetBox = document.getElementById('subnet-box');
+  }
+
+  // Fills in the form with details about some IP allowlist.
+  populate(ipAllowlist) {
+    this.descriptionBox.innerHTML = '';
+    this.subnetBox.innerHTML = '';
+    this.descriptionBox.value = ipAllowlist.description;
+    this.subnetBox.textContent = (ipAllowlist.subnets || []).join('\n');
+  }
+}
+
+const reloadAllowlists = (selector) => {
+  let defer = api.ipAllowlists();
+  defer
+    .then((response) => {
+      selector.populate(response.allowlists);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return defer;
+};
+
 window.onload = () => {
   let selector = new Selector('allowlist-selector');
-  selector.reloadAllowlists();
+  let allowlistPane = new AllowlistPane();
+
+  reloadAllowlists(selector);
+
+  selector.element.addEventListener('allowlistSelected', (event) => {
+    if (event.detail.ip_allowlist === null) {
+      console.log('allowlist is empty');
+    } else {
+      allowlistPane.populate(selector.allowlistMap[event.detail.ip_allowlist]);
+    }
+  });
 };
