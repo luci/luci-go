5bec7d4c2dbaf56251111208759e94bee67c27af
Scott Lee <ddoman@chromium.org>
2021-12-06 21:41:38 +0000

    [buildbucket] add grpcMonitor
    Bug: 1223977
    Change-Id: I8b10700a466787b8666fb4818f01a1cfcf462fce
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3315259
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Scott Lee <ddoman@chromium.org>

---
+++ common/tsmon/monitor/grpc.go
@@ -0,0 +1,75 @@
+// Copyright 2021 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package monitor
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/logging"
+
+	pb "go.chromium.org/luci/common/tsmon/ts_mon_proto"
+	"go.chromium.org/luci/common/tsmon/types"
+)
+
+type grpcMonitor struct {
+	client    pb.MonitoringServiceClient
+	chunkSize int
+}
+
+// NewGRPCMonitor creates a new Monitor object that sends metric by gRPC.
+func NewGRPCMonitor(ctx context.Context, chunkSize, conn *grpc.ClientConn) Monitor {
+	return &grpcMonitor{
+		client:    pb.NewMonitoringServiceClient(conn),
+		chunkSize: 500,
+	}
+}
+
+// NewGRPCMonitorWithChunkSize creates a new GRPCMonitor with a given chunk size.
+func NewGRPCMonitorWithChunkSize(ctx context.Context, chunkSize int, conn *grpc.ClientConn) Monitor {
+	return &grpcMonitor{
+		client:    pb.NewMonitoringServiceClient(conn),
+		chunkSize: chunkSize,
+	}
+}
+
+func (m *grpcMonitor) ChunkSize() int {
+	return m.chunkSize
+}
+
+func (m *grpcMonitor) Send(ctx context.Context, cells []types.Cell) error {
+	// Don't waste time on serialization if we are already too late.
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+	startTime := clock.Now(ctx)
+	_, err := m.client.Insert(ctx, &pb.MonitoringInsertRequest{
+		Payload: &pb.MetricsPayload{
+			MetricsCollection: SerializeCells(cells, startTime),
+		},
+	})
+	if err != nil {
+		logging.Errorf(ctx, "tsmon: failed to send some of %d cells - %s", len(cells), err)
+		return err
+	}
+	logging.Debugf(ctx, "tsmon: sent %d cells in %s", len(cells), clock.Now(ctx).Sub(startTime))
+	return nil
+}
+
+func (m *grpcMonitor) Close() error {
+	return nil
+}
