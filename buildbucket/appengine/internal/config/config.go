c1c0cceb61b0f3053be21a7b6f020a9231cc21dc
Yuanjun Huang <yuanjunh@google.com>
2021-12-08 09:58:56 +0000

    [buildbucket] Validate global settings.cfg
    Port the config validation code from Python version to Go.
    Also validate the bbagent package suffix and new experiments fields.
    
    Py counterpart - https://chromium.googlesource.com/infra/infra/+/99f106971d2f6e7cb54d9febc68690366c8b91ce/appengine/cr-buildbucket/config.py#162
    
    Bug:1277854
    Change-Id: I7db12b234e391a0122f92c6815215a0b5f9110b9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3318695
    Reviewed-by: smut <smut@google.com>
    Reviewed-by: Chan Li <chanli@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Yuanjun Huang <yuanjunh@google.com>

--- buildbucket/appengine/internal/config/config.go
+++ buildbucket/appengine/internal/config/config.go
@@ -16,18 +16,131 @@ package config
 
 import (
 	"context"
+	"regexp"
+	"strings"
+
+	"google.golang.org/protobuf/encoding/prototext"
 
 	"go.chromium.org/luci/config"
 	"go.chromium.org/luci/config/server/cfgcache"
+	"go.chromium.org/luci/config/validation"
 
 	pb "go.chromium.org/luci/buildbucket/proto"
 )
 
+const settingsCfgFilename = "settings.cfg"
+
 // Cached settings config.
 var cachedSettingsCfg = cfgcache.Register(&cfgcache.Entry{
-	Path: "settings.cfg",
+	Path: settingsCfgFilename,
 	Type: (*pb.SettingsCfg)(nil),
 })
+
+// init registers validation rules.
+func init() {
+	validation.Rules.Add("services/${appid}", settingsCfgFilename, validateSettingsCfg)
+}
+
+// validateSettingsCfg implements validation.Func and validates the content of
+// the settings file.
+//
+// Validation errors are returned via validation.Context. An error directly
+// returned by this function means a bug in the code.
+func validateSettingsCfg(ctx *validation.Context, configSet, path string, content []byte) error {
+	cfg := pb.SettingsCfg{}
+	if err := prototext.Unmarshal(content, &cfg); err != nil {
+		ctx.Errorf("invalid SettingsCfg proto message: %s", err)
+		return nil
+	}
+	if s := cfg.Swarming; s != nil {
+		ctx.Enter("swarming")
+		validateSwarmingSettings(ctx, s)
+		ctx.Exit()
+	}
+
+	for i, exp := range cfg.Experiment.GetExperiments() {
+		ctx.Enter("experiment.experiments #%d", i)
+		validateExperiment(ctx, exp)
+		ctx.Exit()
+	}
+
+	validateHostname(ctx, "logdog.hostname", cfg.Logdog.GetHostname())
+	validateHostname(ctx, "resultdb.hostname", cfg.Resultdb.GetHostname())
+	return nil
+}
+
+func validateSwarmingSettings(ctx *validation.Context, s *pb.SwarmingSettings) {
+	validateHostname(ctx, "milo_hostname", s.MiloHostname)
+	for i, pkg := range s.UserPackages {
+		ctx.Enter("user_packages #%d", i)
+		validatePackage(ctx, pkg)
+		ctx.Exit()
+	}
+
+	if bbPkg := s.BbagentPackage; bbPkg != nil {
+		ctx.Enter("bbagent_package")
+		validatePackage(ctx, bbPkg)
+		if !strings.HasSuffix(bbPkg.PackageName, "/${platform}") {
+			ctx.Errorf("package_name must end with '/${platform}'")
+		}
+		ctx.Exit()
+	}
+
+	if kitchen := s.KitchenPackage; kitchen != nil {
+		ctx.Enter("kitchen_package")
+		validatePackage(ctx, kitchen)
+		ctx.Exit()
+	}
+}
+
+func validateHostname(ctx *validation.Context, field string, host string) {
+	if host == "" {
+		ctx.Errorf("%s unspecified", field)
+	}
+	if strings.Contains(host, "://") {
+		ctx.Errorf("%s must not contain '://'", field)
+	}
+}
+
+func validatePackage(ctx *validation.Context, pkg *pb.SwarmingSettings_Package) {
+	if pkg.PackageName == "" {
+		ctx.Errorf("package_name is required")
+	}
+	if pkg.Version == "" {
+		ctx.Errorf("version is required")
+	}
+	if pkg.Builders != nil {
+		validateRegex(ctx, "builders.regex", pkg.Builders.Regex)
+		validateRegex(ctx, "builders.regex_exclude", pkg.Builders.RegexExclude)
+	}
+}
+
+func validateExperiment(ctx *validation.Context, exp *pb.ExperimentSettings_Experiment) {
+	if exp.Name == "" {
+		ctx.Errorf("name is required")
+	}
+	if exp.DefaultValue < 0 || exp.DefaultValue > 100 {
+		ctx.Errorf("default_value must be in [0,100]")
+	}
+	if exp.MinimumValue < exp.DefaultValue || exp.MinimumValue > 100 {
+		ctx.Errorf("minimum_value must be in [${default_value},100]")
+	}
+	if exp.Inactive && (exp.DefaultValue != 0 || exp.MinimumValue != 0) {
+		ctx.Errorf("default_value and minimum_value must both be 0 when inactive is true")
+	}
+	if exp.Builders != nil {
+		validateRegex(ctx, "builders.regex", exp.Builders.Regex)
+		validateRegex(ctx, "builders.regex_exclude", exp.Builders.RegexExclude)
+	}
+}
+
+func validateRegex(ctx *validation.Context, field string, patterns []string) {
+	for _, p := range patterns {
+		if _, err := regexp.Compile(p); err != nil {
+			ctx.Errorf("%s %q: invalid regex", field, p)
+		}
+	}
+}
 
 // UpdateSettingsCfg is called from a cron periodically to import settings.cfg into datastore.
 func UpdateSettingsCfg(ctx context.Context) error {
--- buildbucket/appengine/internal/config/config_test.go
+++ buildbucket/appengine/internal/config/config_test.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"testing"
 
+	"go.chromium.org/luci/config/validation"
 	"go.chromium.org/luci/gae/impl/memory"
 
 	// TODO(crbug/1242998): Remove once safe get becomes datastore default.
@@ -38,5 +39,168 @@ func TestConfig(t *testing.T) {
 		cfg, err := GetSettingsCfg(ctx)
 		So(err, ShouldBeNil)
 		So(cfg, ShouldResembleProto, settingsCfg)
+	})
+
+	Convey("validate settings.cfg", t, func() {
+		vctx := &validation.Context{
+			Context: context.Background(),
+		}
+		configSet := "services/${appid}"
+		path := "settings.cfg"
+
+		Convey("bad proto", func() {
+			content := []byte(` bad: "bad" `)
+			So(validateSettingsCfg(vctx, configSet, path, content), ShouldBeNil)
+			So(vctx.Finalize().Error(), ShouldContainSubstring, "invalid SettingsCfg proto message")
+		})
+
+		Convey("empty settings.cfg", func() {
+			content := []byte(` `)
+			So(validateSettingsCfg(vctx, configSet, path, content), ShouldBeNil)
+			So(vctx.Finalize().Error(), ShouldContainSubstring, "logdog.hostname unspecified")
+		})
+
+		Convey("no swarming cfg", func() {
+			content := []byte(`
+				logdog {
+					hostname: "logs.chromium.org"
+				}
+				resultdb {
+					hostname: "results.api.cr.dev"
+				}
+			`)
+			So(validateSettingsCfg(vctx, configSet, path, content), ShouldBeNil)
+			So(vctx.Finalize(), ShouldBeNil)
+		})
+
+		Convey("no milo", func() {
+			content := []byte(`swarming{}`)
+			So(validateSettingsCfg(vctx, configSet, path, content), ShouldBeNil)
+			So(vctx.Finalize().Error(), ShouldContainSubstring, "milo_hostname unspecified")
+		})
+
+		Convey("invalid user_packages", func() {
+			content := []byte(`
+				swarming {
+					milo_hostname: "ci.chromium.org"
+					user_packages {
+						package_name: ""
+						version: "git_revision:c84736ceb5ddcc3f6e6d1e6c4d602bb024ceb1b2"
+					}
+				}
+			`)
+			So(validateSettingsCfg(vctx, configSet, path, content), ShouldBeNil)
+			So(vctx.Finalize().Error(), ShouldContainSubstring, "(swarming / user_packages #0): package_name is required")
+		})
+
+		Convey("no /${platform} in bbagent", func() {
+			content := []byte(`
+				swarming {
+					milo_hostname: "ci.chromium.org"
+					bbagent_package {
+						package_name: "infra/tools/luci/bbagent"
+						version: "git_revision:60be805bf35a766cdf7d80bdf0a066dce30691e8"
+					}
+				}
+			`)
+			So(validateSettingsCfg(vctx, configSet, path, content), ShouldBeNil)
+			So(vctx.Finalize().Error(), ShouldContainSubstring, "(swarming / bbagent_package): package_name must end with '/${platform}'")
+		})
+
+		Convey("invalid builders.regex in experiments", func() {
+			content := []byte(`
+				experiment {
+					experiments {
+						name: "luci.buildbucket.bbagent_getbuild"
+						builders {
+							regex: "(no right parenthesis"
+						}
+					}
+				}
+			`)
+			So(validateSettingsCfg(vctx, configSet, path, content), ShouldBeNil)
+			So(vctx.Finalize().Error(), ShouldContainSubstring, "(experiment.experiments #0): builders.regex \"(no right parenthesis\": invalid regex")
+		})
+
+		Convey("wrong default_value in experiments", func() {
+			content := []byte(`
+				experiment {
+					experiments {
+						name: "luci.buildbucket.bbagent_getbuild"
+						default_value: 101
+					}
+				}
+			`)
+			So(validateSettingsCfg(vctx, configSet, path, content), ShouldBeNil)
+			So(vctx.Finalize().Error(), ShouldContainSubstring, "default_value must be in [0,100]")
+		})
+
+		Convey("wrong minimum_value in experiments", func() {
+			content := []byte(`
+				experiment {
+					experiments {
+						name: "luci.buildbucket.bbagent_getbuild"
+						default_value: 10
+						minimum_value: 5
+					}
+				}
+			`)
+			So(validateSettingsCfg(vctx, configSet, path, content), ShouldBeNil)
+			So(vctx.Finalize().Error(), ShouldContainSubstring, "minimum_value must be in [${default_value},100]")
+		})
+
+		Convey("invalid inactive in experiments", func() {
+			content := []byte(`
+				experiment {
+					experiments {
+						name: "luci.buildbucket.bbagent_getbuild"
+						minimum_value: 5
+						inactive: true
+					}
+				}
+			`)
+			So(validateSettingsCfg(vctx, configSet, path, content), ShouldBeNil)
+			So(vctx.Finalize().Error(), ShouldContainSubstring, "default_value and minimum_value must both be 0 when inactive is true")
+		})
+
+		Convey("OK", func() {
+			var okCfg = `
+				swarming {
+					milo_hostname: "ci.chromium.org"
+					global_caches {
+						path: "git"
+					}
+					bbagent_package {
+						package_name: "infra/tools/luci/bbagent/${platform}"
+						version: "git_revision:60be805bf35a766cdf7d80bdf0a066dce30691e8"
+					}
+					kitchen_package {
+						package_name: "infra/tools/luci/kitchen/${platform}"
+						version: "git_revision:63874080a20260642c8df82d4f4885ff30b33fb6"
+					}
+					user_packages {
+						package_name: "infra/3pp/tools/git/${platform}"
+						version: "version:2@2.33.0.chromium.6"
+					}
+				}
+				logdog {
+					hostname: "logs.chromium.org"
+				}
+				resultdb {
+					hostname: "results.api.cr.dev"
+				}
+				known_public_gerrit_hosts: "swiftshader.googlesource.com"
+				known_public_gerrit_hosts: "webrtc.googlesource.com"
+				experiment {
+					experiments {
+						name: "luci.buildbucket.bbagent_getbuild"
+					}
+					# inactive experiments
+					experiments { name: "luci.use_realms" inactive: true }
+				}
+			`
+			So(validateSettingsCfg(vctx, configSet, path, []byte(okCfg)), ShouldBeNil)
+			So(vctx.Finalize(), ShouldBeNil)
+		})
 	})
 }
