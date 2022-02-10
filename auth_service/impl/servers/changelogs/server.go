e4bed955ea9fd6c792da07513e66036176a340f3
Xinyu Yang <xinyuoffline@google.com>
2022-02-10 07:02:11 +0000

    [auth-service] ListChangeLogs pRPC implementation.
    Bug:1112832
    Change-Id: I4fd708fc8666563b8bb8f1f94e961b25d41bc294
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3430248
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Camilo Jacomet <cjacomet@google.com>
    Commit-Queue: Xinyu Yang <xinyuoffline@google.com>

---
+++ auth_service/impl/servers/changelogs/server.go
@@ -0,0 +1,49 @@
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package changelogs contains ChangeLogs server implementation.
+package changelogs
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.chromium.org/luci/auth_service/api/rpcpb"
+	"go.chromium.org/luci/auth_service/impl/model"
+)
+
+// Server implements ChangeLogs server.
+type Server struct {
+	rpcpb.UnimplementedChangeLogsServer
+}
+
+// ListChangeLogs implements the corresponding RPC method.
+func (*Server) ListChangeLogs(ctx context.Context, req *rpcpb.ListChangeLogsRequest) (*rpcpb.ListChangeLogsResponse, error) {
+	changes, nextPageToken, err := model.GetAllAuthDBChange(ctx, req.GetTarget(), req.GetAuthDbRev(), req.GetPageSize(), req.GetPageToken())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to fetch change logs: %s", err)
+	}
+
+	changeList := make([]*rpcpb.AuthDBChange, len(changes))
+	for idx, entity := range changes {
+		changeList[idx] = entity.ToProto()
+	}
+
+	return &rpcpb.ListChangeLogsResponse{
+		Changes:       changeList,
+		NextPageToken: nextPageToken,
+	}, nil
+}
---
+++ auth_service/impl/servers/changelogs/server_test.go
@@ -0,0 +1,166 @@
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package changelogs
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"go.chromium.org/luci/auth_service/api/rpcpb"
+	"go.chromium.org/luci/auth_service/impl/model"
+	. "go.chromium.org/luci/common/testing/assertions"
+	"go.chromium.org/luci/gae/impl/memory"
+	"go.chromium.org/luci/gae/service/datastore"
+	_ "go.chromium.org/luci/gae/service/datastore/crbug1242998safeget"
+)
+
+func TestChangeLogsServer(t *testing.T) {
+	t.Parallel()
+	srv := Server{}
+
+	Convey("ListChangeLogs RPC call", t, func() {
+		ctx := memory.Use(context.Background())
+		datastore.GetTestable(ctx).AutoIndex(true)
+		datastore.GetTestable(ctx).Consistent(true)
+
+		So(datastore.Put(ctx,
+			&model.AuthDBChange{
+				Kind:           "AuthDBChange",
+				ID:             "AuthIPWhitelist$a!3000",
+				Parent:         model.ChangeLogRevisionKey(ctx, 10000),
+				Class:          []string{"AuthDBChange", "AuthDBIPWhitelistChange"},
+				ChangeType:     3000,
+				Comment:        "comment",
+				Description:    "description",
+				OldDescription: "",
+				Target:         "AuthIPWhitelist$a",
+				When:           time.Date(2021, time.December, 12, 1, 0, 0, 0, time.UTC),
+				Who:            "user:test@example.com",
+				AppVersion:     "123-45abc",
+			},
+			&model.AuthDBChange{
+				Kind:              "AuthDBChange",
+				ID:                "AuthGlobalConfig$test!7000",
+				Parent:            model.ChangeLogRevisionKey(ctx, 10000),
+				Class:             []string{"AuthDBChange", "AuthDBConfigChange"},
+				ChangeType:        7000,
+				Comment:           "comment",
+				OauthClientID:     "123.test.example.com",
+				OauthClientSecret: "aBcD",
+				Target:            "AuthGlobalConfig$test",
+				When:              time.Date(2019, time.December, 11, 1, 0, 0, 0, time.UTC),
+				Who:               "user:test@example.com",
+				AppVersion:        "123-45abc",
+			},
+			&model.AuthDBChange{
+				Kind:             "AuthDBChange",
+				ID:               "AuthRealmsGlobals$globals!9000",
+				Parent:           model.ChangeLogRevisionKey(ctx, 10020),
+				Class:            []string{"AuthDBChange", "AuthRealmsGlobalsChange"},
+				ChangeType:       9000,
+				Comment:          "comment",
+				PermissionsAdded: []string{"a.existInRealm"},
+				Target:           "AuthRealmsGlobals$globals",
+				When:             time.Date(2021, time.January, 11, 1, 0, 0, 0, time.UTC),
+				Who:              "user:test@example.com",
+				AppVersion:       "123-45abc",
+			},
+		), ShouldBeNil)
+
+		Convey("List all", func() {
+			res, err := srv.ListChangeLogs(ctx, &rpcpb.ListChangeLogsRequest{
+				PageSize: 2,
+			})
+			So(err, ShouldBeNil)
+			So(res.NextPageToken, ShouldNotBeEmpty)
+			So(len(res.Changes), ShouldEqual, 2)
+			So(res.Changes[0], ShouldResembleProto, &rpcpb.AuthDBChange{
+				ChangeType:       "REALMS_GLOBALS_CHANGED",
+				Comment:          "comment",
+				PermissionsAdded: []string{"a.existInRealm"},
+				Target:           "AuthRealmsGlobals$globals",
+				When:             timestamppb.New(time.Date(2021, time.January, 11, 1, 0, 0, 0, time.UTC)),
+				Who:              "user:test@example.com",
+				AppVersion:       "123-45abc",
+			})
+			So(res.Changes[1], ShouldResembleProto, &rpcpb.AuthDBChange{
+				ChangeType:     "IPWL_CREATED",
+				Comment:        "comment",
+				Description:    "description",
+				OldDescription: "",
+				Target:         "AuthIPWhitelist$a",
+				When:           timestamppb.New(time.Date(2021, time.December, 12, 1, 0, 0, 0, time.UTC)),
+				Who:            "user:test@example.com",
+				AppVersion:     "123-45abc",
+			})
+
+			res, err = srv.ListChangeLogs(ctx, &rpcpb.ListChangeLogsRequest{
+				PageToken: res.NextPageToken,
+				PageSize:  2,
+			})
+			So(err, ShouldBeNil)
+			So(res.NextPageToken, ShouldBeEmpty)
+			So(len(res.Changes), ShouldEqual, 1)
+			So(res.Changes[0], ShouldResembleProto, &rpcpb.AuthDBChange{
+				ChangeType:        "CONF_OAUTH_CLIENT_CHANGED",
+				Comment:           "comment",
+				OauthClientId:     "123.test.example.com",
+				OauthClientSecret: "aBcD",
+				Target:            "AuthGlobalConfig$test",
+				When:              timestamppb.New(time.Date(2019, time.December, 11, 1, 0, 0, 0, time.UTC)),
+				Who:               "user:test@example.com",
+				AppVersion:        "123-45abc",
+			})
+		})
+		Convey("Filter by target", func() {
+			res, err := srv.ListChangeLogs(ctx, &rpcpb.ListChangeLogsRequest{
+				Target:   "AuthRealmsGlobals$globals",
+				PageSize: 3,
+			})
+			So(err, ShouldBeNil)
+			So(len(res.Changes), ShouldEqual, 1)
+			So(res.Changes[0], ShouldResembleProto, &rpcpb.AuthDBChange{
+				ChangeType:       "REALMS_GLOBALS_CHANGED",
+				Comment:          "comment",
+				PermissionsAdded: []string{"a.existInRealm"},
+				Target:           "AuthRealmsGlobals$globals",
+				When:             timestamppb.New(time.Date(2021, time.January, 11, 1, 0, 0, 0, time.UTC)),
+				Who:              "user:test@example.com",
+				AppVersion:       "123-45abc",
+			})
+		})
+		Convey("Filter by AuthDBRev", func() {
+			res, err := srv.ListChangeLogs(ctx, &rpcpb.ListChangeLogsRequest{
+				AuthDbRev: 10020,
+				PageSize:  3,
+			})
+			So(err, ShouldBeNil)
+			So(len(res.Changes), ShouldEqual, 1)
+			So(res.Changes[0], ShouldResembleProto, &rpcpb.AuthDBChange{
+				ChangeType:       "REALMS_GLOBALS_CHANGED",
+				Comment:          "comment",
+				PermissionsAdded: []string{"a.existInRealm"},
+				Target:           "AuthRealmsGlobals$globals",
+				When:             timestamppb.New(time.Date(2021, time.January, 11, 1, 0, 0, 0, time.UTC)),
+				Who:              "user:test@example.com",
+				AppVersion:       "123-45abc",
+			})
+		})
+	})
+}
