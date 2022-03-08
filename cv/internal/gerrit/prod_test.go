68d6bac8202c553a0febaeb75adb65cab0f18b81
Yiwei Zhang <yiwzhang@google.com>
2022-03-08 00:57:50 +0000

    Revert "cv: introduce pssa migration gerrit client"
    This reverts commit 7a49cc3a4f207a2fa74a578cce6a0be8ff5207eb.
    
    Reason for revert: causing error in LUCI CV. Sample message: 
    
    ```
    original error: rpc error: code = Internal desc = failed to execute GET HTTP request: Get "https://chromium-review.googlesource.com/a/changes/chromiumos%2Fplatform%2Ftast-tests~3508778?o=ALL_REVISIONS&o=CURRENT_COMMIT&o=DETAILED_LABELS&o=DETAILED_ACCOUNTS&o=MESSAGES&o=SUBMITTABLE&o=SKIP_MERGEABLE": LUCI project: "chromeos": Project token is empty
    ```
    
    Original change's description:
    > cv: introduce pssa migration gerrit client
    >
    > R=​ddoman
    >
    > Bug: 824492
    > Change-Id: Ic2a6a181f142aa0c0984a59b59fe13d62fba7fc0
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3504116
    > Reviewed-by: Scott Lee <ddoman@chromium.org>
    > Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    
    Bug: 824492
    Change-Id: I822b7fe923cea4ef7f0b4f62243ef119f8b27315
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3508528
    Auto-Submit: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Scott Lee <ddoman@chromium.org>
    Commit-Queue: Scott Lee <ddoman@chromium.org>

--- cv/internal/gerrit/prod_test.go
+++ cv/internal/gerrit/prod_test.go
@@ -16,6 +16,7 @@ package gerrit
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -26,6 +27,7 @@ import (
 	"go.chromium.org/luci/common/clock/testclock"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/proto/gerrit"
+	"go.chromium.org/luci/gae/impl/memory"
 	"go.chromium.org/luci/gae/service/datastore"
 	"go.chromium.org/luci/server/auth"
 	"go.chromium.org/luci/server/auth/authtest"
@@ -37,39 +39,58 @@ import (
 func TestMakeClient(t *testing.T) {
 	t.Parallel()
 
-	Convey("With mocked token server", t, func() {
+	Convey("With mocked token server and legacy netrc in datastore", t, func() {
+		ctx := memory.Use(context.Background())
 		epoch := datastore.RoundTime(testclock.TestRecentTimeUTC)
-		ctx, tclock := testclock.UseTime(context.Background(), testclock.TestRecentTimeUTC)
+		ctx, tclock := testclock.UseTime(ctx, testclock.TestRecentTimeUTC)
 		ctx = authtest.MockAuthConfig(ctx)
 
 		const gHost = "first.example.com"
 
+		So(datastore.Put(ctx, &netrcToken{gHost, "legacy-1"}), ShouldBeNil)
 		f, err := newProd(ctx)
 		So(err, ShouldBeNil)
 
 		Convey("factory.token", func() {
+
 			Convey("works", func() {
-				f.mockMintProjectToken = func(context.Context, auth.ProjectTokenParams) (*auth.Token, error) {
-					return &auth.Token{Token: "tok-1", Expiry: epoch.Add(2 * time.Minute)}, nil
-				}
-				t, err := f.token(ctx, gHost, "lProject")
-				So(err, ShouldBeNil)
-				So(t.AccessToken, ShouldEqual, "tok-1")
-				So(t.TokenType, ShouldEqual, "Bearer")
+				Convey("fallback to legacy", func() {
+					f.mockMintProjectToken = func(context.Context, auth.ProjectTokenParams) (*auth.Token, error) {
+						return nil, nil
+					}
+					t, err := f.token(ctx, gHost, "not-migrated")
+					So(err, ShouldBeNil)
+					So(t.AccessToken, ShouldEqual, "bGVnYWN5LTE=") // base64 of "legacy-1"
+					So(t.TokenType, ShouldEqual, "Basic")
+				})
+
+				Convey("project-scoped account", func() {
+					f.mockMintProjectToken = func(context.Context, auth.ProjectTokenParams) (*auth.Token, error) {
+						return &auth.Token{Token: "modern-1", Expiry: epoch.Add(2 * time.Minute)}, nil
+					}
+					t, err := f.token(ctx, gHost, "modern")
+					So(err, ShouldBeNil)
+					So(t.AccessToken, ShouldEqual, "modern-1")
+					So(t.TokenType, ShouldEqual, "Bearer")
+				})
 			})
-			Convey("return errEmptyProjectToken when minted token is nil", func() {
-				f.mockMintProjectToken = func(context.Context, auth.ProjectTokenParams) (*auth.Token, error) {
-					return nil, nil
-				}
-				_, err := f.token(ctx, gHost, "lProject-1")
-				So(err, ShouldErrLike, errEmptyProjectToken)
-			})
-			Convey("error", func() {
-				f.mockMintProjectToken = func(context.Context, auth.ProjectTokenParams) (*auth.Token, error) {
-					return nil, errors.New("flake")
-				}
-				_, err := f.token(ctx, gHost, "lProject")
-				So(err, ShouldErrLike, "flake")
+
+			Convey("not works", func() {
+				Convey("no legacy host", func() {
+					f.mockMintProjectToken = func(context.Context, auth.ProjectTokenParams) (*auth.Token, error) {
+						return nil, nil
+					}
+					_, err := f.token(ctx, "second.example.com", "not-migrated")
+					So(err, ShouldErrLike, "No legacy credentials")
+				})
+
+				Convey("modern errors out", func() {
+					f.mockMintProjectToken = func(context.Context, auth.ProjectTokenParams) (*auth.Token, error) {
+						return nil, errors.New("flake")
+					}
+					_, err := f.token(ctx, gHost, "modern")
+					So(err, ShouldErrLike, "flake")
+				})
 			})
 		})
 
@@ -86,6 +107,7 @@ func TestMakeClient(t *testing.T) {
 
 			u, err := url.Parse(srv.URL)
 			So(err, ShouldBeNil)
+			So(datastore.Put(ctx, &netrcToken{u.Host, "legacy-2"}), ShouldBeNil)
 
 			limitedCtx, limitedCancel := context.WithTimeout(ctx, time.Minute)
 			defer limitedCancel()
@@ -96,17 +118,17 @@ func TestMakeClient(t *testing.T) {
 					So(ctx.Err(), ShouldBeNil) // must not be expired.
 					tokenCnt++
 					return &auth.Token{
-						Token:  fmt.Sprintf("tok-%d", tokenCnt),
+						Token:  fmt.Sprintf("modern-%d", tokenCnt),
 						Expiry: epoch.Add(2 * time.Minute),
 					}, nil
 				}
 
-				c, err := f.MakeClient(limitedCtx, u.Host, "lProject")
+				c, err := f.MakeClient(limitedCtx, u.Host, "modern")
 				So(err, ShouldBeNil)
 				_, err = c.ListChanges(limitedCtx, &gerrit.ListChangesRequest{})
 				So(err, ShouldBeNil)
 				So(requests, ShouldHaveLength, 1)
-				So(requests[0].Header["Authorization"], ShouldResemble, []string{"Bearer tok-1"})
+				So(requests[0].Header["Authorization"], ShouldResemble, []string{"Bearer modern-1"})
 
 				// Ensure client can be used even if context of its creation expires.
 				limitedCancel()
@@ -116,7 +138,28 @@ func TestMakeClient(t *testing.T) {
 				_, err = c.ListChanges(ctx, &gerrit.ListChangesRequest{})
 				So(err, ShouldBeNil)
 				So(requests, ShouldHaveLength, 2)
-				So(requests[1].Header["Authorization"], ShouldResemble, []string{"Bearer tok-2"})
+				So(requests[1].Header["Authorization"], ShouldResemble, []string{"Bearer modern-2"})
+			})
+
+			Convey("legacy", func() {
+				f.mockMintProjectToken = func(context.Context, auth.ProjectTokenParams) (*auth.Token, error) {
+					return nil, nil
+				}
+				c, err := f.MakeClient(limitedCtx, u.Host, "not-migrated")
+				So(err, ShouldBeNil)
+				_, err = c.ListChanges(limitedCtx, &gerrit.ListChangesRequest{})
+				So(err, ShouldBeNil)
+				So(requests, ShouldHaveLength, 1)
+				tokenB64 := base64.StdEncoding.EncodeToString([]byte("legacy-2"))
+				So(requests[0].Header["Authorization"], ShouldResemble, []string{"Basic " + tokenB64})
+
+				// Ensure client can be used even if context of its creation expires.
+				limitedCancel()
+				So(limitedCtx.Err(), ShouldNotBeNil)
+				_, err = c.ListChanges(ctx, &gerrit.ListChangesRequest{})
+				So(err, ShouldBeNil)
+				So(requests, ShouldHaveLength, 2)
+				So(requests[1].Header["Authorization"], ShouldResemble, []string{"Basic " + tokenB64})
 			})
 		})
 	})
