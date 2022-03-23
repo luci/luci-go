2eb1f14bc3b51e486b6f096955f2bcde68ab924d
Yiwei Zhang <yiwzhang@google.com>
2022-03-23 18:07:53 +0000

    cv: implement skeleton for tryjob executor
    R=robertocn
    
    Bug: 1227363
    Change-Id: I1710a1de1bc131bb7224eb4ea9ee70b296e16d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3538845
    Auto-Submit: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Roberto Carrillo <robertocn@chromium.org>
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>

--- cv/internal/tryjob/execute/execute.go
+++ cv/internal/tryjob/execute/execute.go
@@ -16,12 +16,117 @@ package execute
 
 import (
 	"context"
+	"fmt"
 
 	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/retry/transient"
+	"go.chromium.org/luci/gae/service/datastore"
+
+	"go.chromium.org/luci/cv/internal/common"
 	"go.chromium.org/luci/cv/internal/run"
+	"go.chromium.org/luci/cv/internal/tryjob"
 )
 
+type state struct {
+	_kind string         `gae:"$kind,TryjobExecutionState"`
+	_id   int64          `gae:"$id,1"`
+	Run   *datastore.Key `gae:"$parent"`
+	// EVersion is the version of this state. Start with 1.
+	EVersion int64
+	State    *tryjob.ExecutionState
+}
+
+func loadExecutionState(ctx context.Context, rid common.RunID) (*tryjob.ExecutionState, int64, error) {
+	s := &state{Run: datastore.MakeKey(ctx, run.RunKind, string(rid))}
+	switch err := datastore.Get(ctx, s); {
+	case err == datastore.ErrNoSuchEntity:
+		return nil, 0, nil
+	case err != nil:
+		return nil, 0, errors.Annotate(err, "failed to load tryjob execution state of run %q", rid).Err()
+	default:
+		return s.State, s.EVersion, nil
+	}
+}
+
 // Do executes the tryjob requirement for a run.
-func Do(ctx context.Context, run *run.Run, shouldStop func() bool) error {
-	return errors.New("not implemented")
+//
+// This function is idempotent so it is safe to retry.
+func Do(ctx context.Context, r *run.Run, shouldStop func() bool) error {
+	reqmt := r.Tryjobs.GetRequirement()
+	switch {
+	case reqmt == nil:
+		return errors.New("tryjob executor is invoked without requirement")
+	case len(reqmt.GetDefinitions()) == 0:
+		return errors.New("tryjob executor is invoked without any definitions in requirement")
+	}
+
+	execState, stateVer, err := loadExecutionState(ctx, r.ID)
+	switch {
+	case err != nil:
+		return err
+	case execState.GetRequirement().GetVersion() > reqmt.GetVersion():
+		panic(fmt.Errorf("impossible; tryjob executor is executing requirement with version %d which is larger than the requirement version in run %d",
+			execState.GetRequirement().GetVersion(), reqmt.GetVersion()))
+	case execState.GetRequirement().GetVersion() == reqmt.GetVersion():
+		logging.Warningf(ctx, "tryjob executor is already executing requirement v%d which is current in run", reqmt.GetVersion())
+		return nil
+	case execState == nil:
+		execState = &tryjob.ExecutionState{}
+	}
+
+	plan := prepExecutionPlan(reqmt, execState)
+	if err := plan.execute(ctx, r, execState); err != nil {
+		return err
+	}
+
+	var innerErr error
+	err = datastore.RunInTransaction(ctx, func(c context.Context) (err error) {
+		defer func() { innerErr = err }()
+		switch _, latestStateVer, err := loadExecutionState(ctx, r.ID); {
+		case err != nil:
+			return err
+		case latestStateVer != stateVer:
+			return errors.Reason("execution state has changed. before: %d, current: %d", stateVer, latestStateVer).Tag(transient.Tag).Err()
+		default:
+			s := &state{
+				Run:      datastore.MakeKey(ctx, run.RunKind, string(r.ID)),
+				EVersion: latestStateVer + 1,
+				State:    execState,
+			}
+			return errors.Annotate(datastore.Put(ctx, s), "failed to save execution state").Tag(transient.Tag).Err()
+		}
+	}, nil)
+
+	switch {
+	case innerErr != nil:
+		return innerErr
+	case err != nil:
+		return errors.Annotate(err, "failed to commit transaction").Tag(transient.Tag).Err()
+	default:
+		return nil
+	}
+}
+
+type plan struct {
+	// New tryjob to launch.
+	launchNew []*tryjob.Definition
+
+	// The indices of the tryjob execution in the existing execution state that
+	// should be discarded.
+	//
+	// This is typically due to the builder is deleted in the Project Config.
+	discardIndices intSet
+	// TODO(yiwzhang): Consider supporting the use case where a builder changes
+	// `disable_reuse` from false to true, then if this builder is currently
+	// reusing a build, it should stop reusing it and launch a new build.
+}
+
+func prepExecutionPlan(newReqmt *tryjob.Requirement, execState *tryjob.ExecutionState) plan {
+	panic("implement")
+}
+
+// execute executes the plan and mutate the state.
+func (p plan) execute(ctx context.Context, r *run.Run, execState *tryjob.ExecutionState) error {
+	panic("implement")
 }
