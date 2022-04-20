415e8d782cc5484d63a3bb01132c857d4cf887c5
Yiwei Zhang <yiwzhang@google.com>
2022-04-20 19:42:36 +0000

    cv: implement SearchBuild for buildbucket fake
    R=robertocn
    
    Bug: 1312255
    Change-Id: I57f8032ef71f75825aa0f2f8ea31eac975e26175
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3591033
    Reviewed-by: Roberto Carrillo <robertocn@chromium.org>
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>

--- cv/internal/buildbucket/fake/client.go
+++ cv/internal/buildbucket/fake/client.go
@@ -16,10 +16,20 @@ package bbfake
 
 import (
 	"context"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
 
+	"go.chromium.org/luci/buildbucket/appengine/model"
 	bbpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/grpc/appstatus"
 
 	"go.chromium.org/luci/cv/internal/buildbucket"
 )
@@ -50,9 +60,99 @@ func (c *Client) GetBuild(ctx context.Context, in *bbpb.GetBuildRequest, opts ..
 	panic("not implemented")
 }
 
+var supportedPredicates = stringset.NewFromSlice(
+	"gerrit_changes",
+	"include_experimental",
+)
+
+const defaultSearchPageSize = 5
+
 // SearchBuilds implements buildbucket.Client.
+//
+// Support paging and the following predicates:
+//  * gerrit_changes
+//  * include_experimental
+//
+// Use `defaultSearchPageSize` if page size is not specified in the input.
 func (c *Client) SearchBuilds(ctx context.Context, in *bbpb.SearchBuildsRequest, opts ...grpc.CallOption) (*bbpb.SearchBuildsResponse, error) {
-	panic("not implemented")
+	if in.GetPredicate() != nil {
+		var notSupportedPredicates []string
+		in.GetPredicate().ProtoReflect().Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+			if v.IsValid() && !supportedPredicates.Has(string(fd.Name())) {
+				notSupportedPredicates = append(notSupportedPredicates, string(fd.Name()))
+			}
+			return true
+		})
+		if len(notSupportedPredicates) > 0 {
+			return nil, appstatus.Errorf(codes.InvalidArgument, "predicates [%s] are not supported", strings.Join(notSupportedPredicates, ", "))
+		}
+	}
+	var lastReturnedBuildID int64
+	if token := in.GetPageToken(); token != "" {
+		var err error
+		lastReturnedBuildID, err = strconv.ParseInt(token, 10, 64)
+		if err != nil {
+			return nil, appstatus.Errorf(codes.InvalidArgument, "invalid token %q, expecting a build ID", token)
+		}
+	}
+	c.fake.storeMu.RLock()
+	candidates := make([]*bbpb.Build, 0, len(c.fake.store))
+	for id, b := range c.fake.store {
+		if id.host == c.bbHost {
+			candidates = append(candidates, b)
+		}
+	}
+	c.fake.storeMu.RUnlock()
+	sort.Slice(candidates, func(i, j int) bool {
+		return candidates[i].Id < candidates[j].Id
+	})
+	pageSize := in.GetPageSize()
+	if pageSize == 0 {
+		pageSize = defaultSearchPageSize
+	}
+	resBuilds := make([]*bbpb.Build, 0, pageSize)
+	for _, b := range candidates {
+		if c.shouldIncludeBuild(ctx, b, in.GetPredicate(), lastReturnedBuildID) {
+			clone := proto.Clone(b).(*bbpb.Build)
+			mask, err := model.NewBuildMask("", nil, in.GetMask())
+			if err != nil {
+				return nil, appstatus.Errorf(codes.Internal, "error while constructing BuildMask: %s", err)
+			}
+			if err := mask.Trim(clone); err != nil {
+				return nil, appstatus.Errorf(codes.Internal, "error while applying field mask: %s", err)
+			}
+			resBuilds = append(resBuilds, clone)
+			if len(resBuilds) == int(pageSize) {
+				return &bbpb.SearchBuildsResponse{
+					Builds:        resBuilds,
+					NextPageToken: strconv.FormatInt(b.Id, 10),
+				}, nil
+			}
+		}
+	}
+	return &bbpb.SearchBuildsResponse{Builds: resBuilds}, nil
+}
+
+func (c *Client) shouldIncludeBuild(ctx context.Context, b *bbpb.Build, pred *bbpb.BuildPredicate, lastReturnedBuildID int64) bool {
+	switch {
+	case b.GetId() <= lastReturnedBuildID:
+		return false
+	case !c.canAccessBuild(ctx, b):
+		return false
+	case !pred.GetIncludeExperimental() && b.GetInput().GetExperimental():
+		return false
+	case len(pred.GetGerritChanges()) > 0:
+		gcs := stringset.New(len(b.GetInput().GetGerritChanges()))
+		for _, gc := range b.GetInput().GetGerritChanges() {
+			gcs.Add(fmt.Sprintf("%s/%s/%d/%d", gc.GetHost(), gc.GetProject(), gc.GetChange(), gc.GetPatchset()))
+		}
+		for _, gc := range pred.GetGerritChanges() {
+			if !gcs.Has(fmt.Sprintf("%s/%s/%d/%d", gc.GetHost(), gc.GetProject(), gc.GetChange(), gc.GetPatchset())) {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 // ScheduleBuild implements buildbucket.Client.
@@ -63,4 +163,9 @@ func (c *Client) ScheduleBuild(ctx context.Context, in *bbpb.ScheduleBuildReques
 // CancelBuild implements buildbucket.Client.
 func (c *Client) CancelBuild(ctx context.Context, in *bbpb.CancelBuildRequest, opts ...grpc.CallOption) (*bbpb.Build, error) {
 	panic("not implemented")
+}
+
+func (c *Client) canAccessBuild(ctx context.Context, build *bbpb.Build) bool {
+	// TODO(yiwzhang): implement proper ACL
+	return c.luciProject == build.GetBuilder().GetProject()
 }
---
+++ cv/internal/buildbucket/fake/client_test.go
@@ -0,0 +1,243 @@
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bbfake
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+
+	"go.chromium.org/luci/buildbucket/appengine/model"
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/clock"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestSearchBuild(t *testing.T) {
+	Convey("Search", t, func() {
+		fake := &Fake{}
+		ctx := context.Background()
+		const (
+			bbHost   = "buildbucket.example.com"
+			lProject = "testProj"
+		)
+
+		client, err := fake.NewClientFactory().MakeClient(ctx, bbHost, lProject)
+		So(err, ShouldBeNil)
+
+		const (
+			buildID          = 12344
+			gHost            = "example-review.googlesource.com"
+			gRepo            = "repo/example"
+			gChangeNum       = 546
+			gPatchset        = 6
+			gMinEquiPatchset = 2
+		)
+		epoch := clock.Now(ctx)
+		builderID := &bbpb.BuilderID{
+			Project: lProject,
+			Bucket:  "testBucket",
+			Builder: "testBuilder",
+		}
+		gc := &bbpb.GerritChange{
+			Host:     gHost,
+			Project:  gRepo,
+			Change:   gChangeNum,
+			Patchset: gPatchset,
+		}
+		build := NewBuildConstructor().
+			WithID(buildID).
+			WithHost(bbHost).
+			WithBuilderID(builderID).
+			WithStatus(bbpb.Status_SUCCESS).
+			WithCreateTime(epoch).
+			WithStartTime(epoch.Add(1 * time.Minute)).
+			WithEndTime(epoch.Add(2 * time.Minute)).
+			AppendGerritChanges(gc).
+			Construct()
+
+		Convey("Empty Predicate", func() {
+			fake.AddBuild(build)
+			res, err := client.SearchBuilds(ctx, &bbpb.SearchBuildsRequest{})
+			So(err, ShouldBeNil)
+			So(res, ShouldResembleProto, &bbpb.SearchBuildsResponse{
+				Builds: []*bbpb.Build{trimmedBuildWithDefaultMask(build)},
+			})
+		})
+
+		Convey("Error on not allowed predicate", func() {
+			_, err := client.SearchBuilds(ctx, &bbpb.SearchBuildsRequest{
+				Predicate: &bbpb.BuildPredicate{
+					CreatedBy: "foo",
+				},
+			})
+			So(err, ShouldHaveAppStatus, codes.InvalidArgument)
+		})
+
+		Convey("Can apply Gerrit Change predicate", func() {
+			fake.AddBuild(build)
+			Convey("Match", func() {
+				res, err := client.SearchBuilds(ctx, &bbpb.SearchBuildsRequest{
+					Predicate: &bbpb.BuildPredicate{
+						GerritChanges: []*bbpb.GerritChange{gc},
+					},
+				})
+				So(err, ShouldBeNil)
+				So(res, ShouldResembleProto, &bbpb.SearchBuildsResponse{
+					Builds: []*bbpb.Build{trimmedBuildWithDefaultMask(build)},
+				})
+			})
+			Convey("Mismatch change", func() {
+				gc := proto.Clone(gc).(*bbpb.GerritChange)
+				gc.Change += 2
+				res, err := client.SearchBuilds(ctx, &bbpb.SearchBuildsRequest{
+					Predicate: &bbpb.BuildPredicate{
+						GerritChanges: []*bbpb.GerritChange{gc},
+					},
+				})
+				So(err, ShouldBeNil)
+				So(res.GetBuilds(), ShouldBeEmpty)
+				So(res.GetNextPageToken(), ShouldBeEmpty)
+			})
+			Convey("Extra change", func() {
+				anotherGC := proto.Clone(gc).(*bbpb.GerritChange)
+				anotherGC.Change += 2
+				res, err := client.SearchBuilds(ctx, &bbpb.SearchBuildsRequest{
+					Predicate: &bbpb.BuildPredicate{
+						GerritChanges: []*bbpb.GerritChange{gc, anotherGC},
+					},
+				})
+				So(err, ShouldBeNil)
+				So(res.GetBuilds(), ShouldBeEmpty)
+				So(res.GetNextPageToken(), ShouldBeEmpty)
+			})
+		})
+
+		Convey("Can apply experimental predicate", func() {
+			build := NewConstructorFromBuild(build).
+				WithExperimental(true).
+				Construct()
+			fake.AddBuild(build)
+			Convey("Include experimental", func() {
+				res, err := client.SearchBuilds(ctx, &bbpb.SearchBuildsRequest{
+					Predicate: &bbpb.BuildPredicate{
+						IncludeExperimental: true,
+					},
+				})
+				So(err, ShouldBeNil)
+				So(res, ShouldResembleProto, &bbpb.SearchBuildsResponse{
+					Builds: []*bbpb.Build{trimmedBuildWithDefaultMask(build)},
+				})
+			})
+			Convey("Not include experimental", func() {
+				res, err := client.SearchBuilds(ctx, &bbpb.SearchBuildsRequest{
+					Predicate: &bbpb.BuildPredicate{},
+				})
+				So(err, ShouldBeNil)
+				So(res.GetBuilds(), ShouldBeEmpty)
+				So(res.GetNextPageToken(), ShouldBeEmpty)
+			})
+		})
+
+		Convey("Different host", func() {
+			build := NewConstructorFromBuild(build).
+				WithHost("another-bb.example.com").
+				Construct()
+			fake.AddBuild(build)
+			res, err := client.SearchBuilds(ctx, &bbpb.SearchBuildsRequest{})
+			So(err, ShouldBeNil)
+			So(res.GetBuilds(), ShouldBeEmpty)
+			So(res.GetNextPageToken(), ShouldBeEmpty)
+		})
+
+		Convey("Different project", func() {
+			build := NewConstructorFromBuild(build).
+				WithBuilderID(&bbpb.BuilderID{
+					Project: "anotherProj",
+					Bucket:  "someBucket",
+					Builder: "someBuilder",
+				}).
+				Construct()
+			fake.AddBuild(build)
+			res, err := client.SearchBuilds(ctx, &bbpb.SearchBuildsRequest{})
+			So(err, ShouldBeNil)
+			So(res.GetBuilds(), ShouldBeEmpty)
+			So(res.GetNextPageToken(), ShouldBeEmpty)
+		})
+
+		Convey("Apply customized mask", func() {
+			fake.AddBuild(build)
+			fields, err := fieldmaskpb.New(build, "id")
+			So(err, ShouldBeNil)
+			res, err := client.SearchBuilds(ctx, &bbpb.SearchBuildsRequest{
+				Mask: &bbpb.BuildMask{
+					Fields: fields,
+				},
+			})
+			So(err, ShouldBeNil)
+			So(res, ShouldResembleProto, &bbpb.SearchBuildsResponse{
+				Builds: []*bbpb.Build{{Id: build.Id}},
+			})
+		})
+
+		Convey("Paging", func() {
+			for i := 1; i <= 50; i++ {
+				build := NewConstructorFromBuild(build).WithID(int64(i)).Construct()
+				fake.AddBuild(build)
+			}
+
+			for _, pageSize := range []int{0, 20} { // 0 means  default size
+				Convey(fmt.Sprintf("With size %d", pageSize), func() {
+					actualPageSize := pageSize
+					if actualPageSize == 0 {
+						actualPageSize = defaultSearchPageSize
+					}
+					req := &bbpb.SearchBuildsRequest{
+						PageSize: int32(pageSize),
+					}
+					expectedID := int64(1)
+					for {
+						res, err := client.SearchBuilds(ctx, req)
+						So(err, ShouldBeNil)
+						So(len(res.GetBuilds()), ShouldBeLessThanOrEqualTo, actualPageSize)
+						for _, b := range res.GetBuilds() {
+							So(b.Id, ShouldEqual, expectedID)
+							expectedID++
+						}
+						if res.NextPageToken == "" {
+							break
+						}
+						req.PageToken = res.NextPageToken
+					}
+				})
+			}
+		})
+	})
+}
+
+func trimmedBuildWithDefaultMask(b *bbpb.Build) *bbpb.Build {
+	mask, err := model.NewBuildMask("", nil, nil)
+	So(err, ShouldBeNil)
+	ret := proto.Clone(b).(*bbpb.Build)
+	So(mask.Trim(ret), ShouldBeNil)
+	return ret
+}
