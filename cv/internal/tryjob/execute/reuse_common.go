43ea84dd36c534f9ad8dfe437c3856abf192cb75
Yiwei Zhang <yiwzhang@google.com>
2022-05-23 23:54:40 +0000

    cv: refactor the functionality to decide whehter a tryjob is reusable
    R=robertocn
    
    Bug: 1312255
    Change-Id: I9eeb56e518a3b4eb03ca577d83811a8916432199
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3658056
    Reviewed-by: Roberto Carrillo <robertocn@chromium.org>
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>

---
+++ cv/internal/tryjob/execute/reuse_common.go
@@ -0,0 +1,125 @@
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package execute
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"sort"
+	"time"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/logging"
+
+	"go.chromium.org/luci/cv/internal/run"
+	"go.chromium.org/luci/cv/internal/tryjob"
+)
+
+// staleTryjobAge is the age after which a Tryjob is too old to reuse.
+const staleTryjobAge time.Duration = 24 * time.Hour
+
+type reusability int
+
+const (
+	reuseAllowed reusability = iota + 1
+	reuseMaybe
+	reuseDenied
+)
+
+// canReuseTryjob checks if a given Tryjob can be reused.
+//
+// Tryjob *can* be reused iff the Tryjob ends successfully and is fresh
+// enough (i.e created within `staleTryjobAge`). Tryjob *may* be reused
+// if the tryjob is fresh enough and still running or hasn't started yet.
+func canReuseTryjob(ctx context.Context, tj *tryjob.Tryjob, mode run.Mode) reusability {
+	switch status := tj.Status; {
+	case status == tryjob.Status_STATUS_UNSPECIFIED:
+		panic(fmt.Errorf("unspecified status for tryjob %d", tj.ID))
+	case status == tryjob.Status_PENDING:
+		return reuseMaybe
+	case status == tryjob.Status_TRIGGERED && isTryjobStale(ctx, tj):
+		return reuseDenied
+	case status == tryjob.Status_TRIGGERED:
+		// tryjob output may change anytime that changes the reusability
+		return reuseMaybe
+	case status == tryjob.Status_ENDED && canReuseResult(ctx, tj, mode):
+		return reuseAllowed
+	case status == tryjob.Status_ENDED:
+		return reuseDenied
+	case status == tryjob.Status_CANCELLED:
+		return reuseDenied
+	case status == tryjob.Status_UNTRIGGERED:
+		return reuseDenied
+	default:
+		panic(fmt.Errorf("unknown status %s for tryjob %d", tj.Status, tj.ID))
+	}
+}
+
+// canReuseResult checks if the result of the tryjob can be reused.
+func canReuseResult(ctx context.Context, tj *tryjob.Tryjob, mode run.Mode) bool {
+	switch result := tj.Result; {
+	case tj.Status != tryjob.Status_ENDED:
+		panic(fmt.Errorf("canReuseResult must be called when tryjob status is ended. got %s", tj.Status))
+	case result == nil:
+		logging.Errorf(ctx, "tryjob %d has nil result but it has ended already", tj.ID)
+		return false
+	case isTryjobStale(ctx, tj):
+		return false
+	case result.GetStatus() != tryjob.Result_SUCCEEDED:
+		return false // only succeeded tryjob can be reused.
+	case isModeAllowed(mode, result.GetOutput().GetReusability().GetModeAllowlist()):
+		return true
+	}
+	return false
+}
+
+func isTryjobStale(ctx context.Context, tj *tryjob.Tryjob) bool {
+	createTime := tj.Result.GetCreateTime()
+	if createTime == nil {
+		logging.Errorf(ctx, "Tryjob %d has nil create time when checking whether tryjob is stale", tj.ID)
+		return true // Be defensive. Consider tryjob stale.
+	}
+	return clock.Now(ctx).Sub(createTime.AsTime()) >= staleTryjobAge
+}
+
+func isModeAllowed(mode run.Mode, allowlist []string) bool {
+	if len(allowlist) == 0 { // empty list means allowing all modes
+		return true
+	}
+	for _, allowed := range allowlist {
+		if string(mode) == allowed {
+			return true
+		}
+	}
+	return false
+}
+
+// computeReuseKey computes the reuse key for a tryjob based on the CLs it
+// involves.
+func computeReuseKey(cls []*run.RunCL) string {
+	clPatchsets := make([][]byte, len(cls))
+	for i, cl := range cls {
+		clPatchsets[i] = []byte(fmt.Sprintf("%d/%d", cl.ID, cl.Detail.GetMinEquivalentPatchset()))
+	}
+	sort.Slice(clPatchsets, func(i, j int) bool {
+		return bytes.Compare(clPatchsets[i], clPatchsets[j]) < 0
+	})
+	h := sha256.New()
+	h.Write(bytes.Join(clPatchsets, []byte{0}))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
---
+++ cv/internal/tryjob/execute/reuse_common_test.go
@@ -0,0 +1,169 @@
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package execute
+
+import (
+	"fmt"
+	"testing"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	recipe "go.chromium.org/luci/cv/api/recipe/v1"
+	"go.chromium.org/luci/cv/internal/changelist"
+	"go.chromium.org/luci/cv/internal/cvtesting"
+	"go.chromium.org/luci/cv/internal/run"
+	"go.chromium.org/luci/cv/internal/tryjob"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCanReuse(t *testing.T) {
+	t.Parallel()
+
+	Convey("canReuseTryjob works", t, func() {
+		ct := cvtesting.Test{}
+		ctx, cancel := ct.SetUp()
+		defer cancel()
+
+		Convey("reuse allowed", func() {
+			Convey("empty mode allowlist", func() {
+				tj := &tryjob.Tryjob{
+					Status: tryjob.Status_ENDED,
+					Result: &tryjob.Result{
+						CreateTime: timestamppb.New(ct.Clock.Now().Add(-staleTryjobAge / 2)),
+						Status:     tryjob.Result_SUCCEEDED,
+					},
+				}
+				So(canReuseTryjob(ctx, tj, run.FullRun), ShouldEqual, reuseAllowed)
+			})
+
+			Convey("explicitly allowed in mode allowlist", func() {
+				tj := &tryjob.Tryjob{
+					Status: tryjob.Status_ENDED,
+					Result: &tryjob.Result{
+						CreateTime: timestamppb.New(ct.Clock.Now().Add(-staleTryjobAge / 2)),
+						Status:     tryjob.Result_SUCCEEDED,
+						Output: &recipe.Output{
+							Reusability: &recipe.Output_Reusability{
+								ModeAllowlist: []string{string(run.DryRun), string(run.FullRun)},
+							},
+						},
+					},
+				}
+				So(canReuseTryjob(ctx, tj, run.FullRun), ShouldEqual, reuseAllowed)
+			})
+		})
+
+		Convey("reuse maybe", func() {
+			Convey("triggered fresh tryjob", func() {
+				tj := &tryjob.Tryjob{
+					Status: tryjob.Status_TRIGGERED,
+					Result: &tryjob.Result{
+						CreateTime: timestamppb.New(ct.Clock.Now().Add(-staleTryjobAge / 2)),
+					},
+				}
+				So(canReuseTryjob(ctx, tj, run.FullRun), ShouldEqual, reuseMaybe)
+			})
+
+			Convey("pending tryjob", func() {
+				tj := &tryjob.Tryjob{
+					Status: tryjob.Status_PENDING,
+				}
+				So(canReuseTryjob(ctx, tj, run.FullRun), ShouldEqual, reuseMaybe)
+			})
+		})
+
+		Convey("reuse denied", func() {
+			Convey("triggered stale tryjob", func() {
+				tj := &tryjob.Tryjob{
+					Status: tryjob.Status_TRIGGERED,
+					Result: &tryjob.Result{
+						CreateTime: timestamppb.New(ct.Clock.Now().Add(-staleTryjobAge * 2)),
+					},
+				}
+				So(canReuseTryjob(ctx, tj, run.FullRun), ShouldEqual, reuseDenied)
+			})
+
+			Convey("successfully ended tryjob but stale", func() {
+				tj := &tryjob.Tryjob{
+					Status: tryjob.Status_ENDED,
+					Result: &tryjob.Result{
+						CreateTime: timestamppb.New(ct.Clock.Now().Add(-staleTryjobAge * 2)),
+						Status:     tryjob.Result_SUCCEEDED,
+					},
+				}
+				So(canReuseTryjob(ctx, tj, run.FullRun), ShouldEqual, reuseDenied)
+			})
+
+			Convey("failed tryjob", func() {
+				tj := &tryjob.Tryjob{
+					Status: tryjob.Status_ENDED,
+					Result: &tryjob.Result{
+						CreateTime: timestamppb.New(ct.Clock.Now().Add(-staleTryjobAge * 2)),
+						Status:     tryjob.Result_FAILED_PERMANENTLY,
+					},
+				}
+				So(canReuseTryjob(ctx, tj, run.FullRun), ShouldEqual, reuseDenied)
+			})
+
+			Convey("not in the mode allowlist", func() {
+				tj := &tryjob.Tryjob{
+					Status: tryjob.Status_ENDED,
+					Result: &tryjob.Result{
+						CreateTime: timestamppb.New(ct.Clock.Now().Add(-staleTryjobAge * 2)),
+						Status:     tryjob.Result_SUCCEEDED,
+						Output: &recipe.Output{
+							Reusability: &recipe.Output_Reusability{
+								ModeAllowlist: []string{string(run.DryRun)},
+							},
+						},
+					},
+				}
+				So(canReuseTryjob(ctx, tj, run.FullRun), ShouldEqual, reuseDenied)
+			})
+
+			for _, st := range []tryjob.Status{tryjob.Status_CANCELLED, tryjob.Status_UNTRIGGERED} {
+				Convey(fmt.Sprintf("status is %s", st), func() {
+					tj := &tryjob.Tryjob{Status: st}
+					So(canReuseTryjob(ctx, tj, run.FullRun), ShouldEqual, reuseDenied)
+				})
+			}
+		})
+	})
+}
+
+func TestComputeReuseKey(t *testing.T) {
+	t.Parallel()
+
+	Convey("computeReuseKey works", t, func() {
+		cls := []*run.RunCL{
+			{
+				ID: 22222,
+				Detail: &changelist.Snapshot{
+					MinEquivalentPatchset: 22,
+				},
+			},
+			{
+				ID: 11111,
+				Detail: &changelist.Snapshot{
+					MinEquivalentPatchset: 11,
+				},
+			},
+		}
+		// Should yield the same result as
+		// > python3 -c 'import base64;from hashlib import sha256;print(base64.b64encode(sha256(b"\0".join(sorted(b"%d/%d"%(x[0], x[1]) for x in [[22222,22],[11111,11]]))).digest()))'
+		So(computeReuseKey(cls), ShouldEqual, "2Yh+hI8zJZFe8ac1TrrFjATWGjhiV9aXsKjNJIhzATk=")
+	})
+}
