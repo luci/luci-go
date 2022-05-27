2fd60496236a3e6cc057f76c5263500bb4e491d7
Weiwei Lin <weiweilin@google.com>
2022-05-27 01:18:33 +0000

    [milo] fix typo in CSS
    R=mdraz@google.com, nqmtuan@google.com
    
    Change-Id: I7da5a81b1d52bc47e8b1d0d229ca1111c9fc1e96
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3670658
    Reviewed-by: Tuan Nguyen <nqmtuan@google.com>
    Commit-Queue: Tuan Nguyen <nqmtuan@google.com>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>

--- milo/frontend/resultui/src/components/page_layout.ts
+++ milo/frontend/resultui/src/components/page_layout.ts
@@ -233,7 +233,7 @@ export class PageLayoutElement extends MiloBaseElement implements BeforeEnterObs
       }
       .banner-container {
         width: 100%;
-        bix-sizing: border-box;
+        box-sizing: border-box;
         background-color: #feb;
         padding: 3px;
         text-align: center;
