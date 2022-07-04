e74f8f68337ee619d400e0b6607cbe9df37f78ee
Brian Ryner <bryner@google.com>
2022-07-04 19:01:43 +0000

    Fix so that Python inherits file handles again on Windows.
    The behavior was changed in go 1.17 so that only explicitly-specified
    handles are inherited by subprocesses. Unfortunately, we have no
    special knowledge of what handles the caller is attempting to pass to
    the Python process, so we want to transparently pass everything
    through.
    
    This reverts to the previous behavior, mostly by copying the
    1.16 implementation.
    
    Bug: 1340653
    Change-Id: I30302ffe2d661aa625e559385d5cfed7af731ef9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3739699
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Brian Ryner <bryner@google.com>
    Reviewed-by: Chenlin Fan <fancl@chromium.org>

--- vpython/system_windows.go
+++ vpython/system_windows.go
@@ -19,13 +19,17 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"path/filepath"
+	"syscall"
+	"unicode/utf16"
+	"unsafe"
 
+	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/vpython/python"
 	"go.chromium.org/luci/vpython/venv"
 
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/system/environ"
-	"go.chromium.org/luci/common/system/exitcode"
 )
 
 // systemSpecificLaunch launches the process described by "cmd" while ensuring
@@ -44,15 +48,84 @@ func systemSpecificLaunch(c context.Context, ve *venv.Env, cl *python.CommandLin
 	return Exec(c, ve.Interpreter(), cl, env, dir, nil)
 }
 
+// Copied from https://github.com/golang/go/blob/go1.16.15/src/syscall/exec_windows.go
+// createEnvBlock converts an array of environment strings into
+// the representation required by CreateProcess: a sequence of NUL
+// terminated strings followed by a nil.
+// Last bytes are two UCS-2 NULs, or four NUL bytes.
+func createEnvBlock(envv []string) *uint16 {
+	if len(envv) == 0 {
+		return &utf16.Encode([]rune("\x00\x00"))[0]
+	}
+	length := 0
+	for _, s := range envv {
+		length += len(s) + 1
+	}
+	length += 1
+
+	b := make([]byte, length)
+	i := 0
+	for _, s := range envv {
+		l := len(s)
+		copy(b[i:i+l], []byte(s))
+		copy(b[i+l:i+l+1], []byte{0})
+		i = i + l + 1
+	}
+	copy(b[i:i+1], []byte{0})
+
+	return &utf16.Encode([]rune(string(b)))[0]
+}
+
 func execImpl(c context.Context, argv []string, env environ.Env, dir string, setupFn func() error) error {
-	cmd := exec.Cmd{
-		Path:   argv[0],
-		Args:   argv,
-		Env:    env.Sorted(),
-		Dir:    dir,
-		Stdin:  os.Stdin,
-		Stdout: os.Stdout,
-		Stderr: os.Stderr,
+	// As of go 1.17, handles to be passed to subprocesses via Cmd.Run must be explicitly
+	// specified. To keep the expected behavior of letting Python inherit all inheritable
+	// file handles, we instead use syscall directly, based on the Go 1.16 implementation
+	// of cmd.Run and syscall.StartProcess.
+	// Tracked in https://github.com/golang/go/issues/53652
+	resolvedPath, err := exec.LookPath(argv[0])
+	if err != nil {
+		return errors.Annotate(err, "Could not locate executable for %v", argv[0]).Err()
+	}
+	resolvedPath, err = filepath.Abs(resolvedPath)
+	if err != nil {
+		return err
+	}
+
+	sys := new(syscall.SysProcAttr)
+	procAttr := syscall.ProcAttr{
+		Dir:   dir,
+		Env:   env.Sorted(),
+		Files: []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()},
+		Sys:   sys,
+	}
+
+	argv0p, err := syscall.UTF16PtrFromString(resolvedPath)
+	if err != nil {
+		return err
+	}
+
+	var cmdline string
+	for _, arg := range argv {
+		if len(cmdline) > 0 {
+			cmdline += " "
+		}
+		cmdline += syscall.EscapeArg(arg)
+	}
+
+	var argvp *uint16
+	if len(cmdline) != 0 {
+		argvp, err = syscall.UTF16PtrFromString(cmdline)
+		if err != nil {
+			return err
+		}
+	}
+
+	var dirp *uint16
+	if len(procAttr.Dir) != 0 {
+		dirp, err = syscall.UTF16PtrFromString(procAttr.Dir)
+		if err != nil {
+			return err
+		}
 	}
 
 	// At this point, ANY ERROR will be fatal (panic). We assume that each
@@ -75,12 +148,61 @@ func execImpl(c context.Context, argv []string, env environ.Env, dir string, set
 		// signal (since `cmd` would have gotten it as well).
 	}()
 
-	err := cmd.Run()
-	if rc, has := exitcode.Get(err); has {
-		// The process had an exit code (includes err==nil, 0).
-		logging.Debugf(c, "Python subprocess has terminated: %v", err)
-		os.Exit(rc)
-		panic("must not return")
+	// Acquire the fork lock so that no other threads
+	// create new fds that are not yet close-on-exec
+	// before we fork.
+	syscall.ForkLock.Lock()
+	defer syscall.ForkLock.Unlock()
+
+	p, _ := syscall.GetCurrentProcess()
+	fd := make([]syscall.Handle, len(procAttr.Files))
+	for i := range procAttr.Files {
+		if procAttr.Files[i] > 0 {
+			err := syscall.DuplicateHandle(p, syscall.Handle(procAttr.Files[i]), p, &fd[i], 0, true, syscall.DUPLICATE_SAME_ACCESS)
+			if err != nil {
+				panic(err)
+			}
+			defer syscall.CloseHandle(syscall.Handle(fd[i]))
+		}
 	}
-	panic(err)
+	si := new(syscall.StartupInfo)
+	si.Cb = uint32(unsafe.Sizeof(*si))
+	si.Flags = syscall.STARTF_USESTDHANDLES
+	if sys.HideWindow {
+		si.Flags |= syscall.STARTF_USESHOWWINDOW
+		si.ShowWindow = syscall.SW_HIDE
+	}
+	si.StdInput = fd[0]
+	si.StdOutput = fd[1]
+	si.StdErr = fd[2]
+
+	pi := new(syscall.ProcessInformation)
+
+	flags := sys.CreationFlags | syscall.CREATE_UNICODE_ENVIRONMENT
+	err = syscall.CreateProcess(argv0p, argvp, sys.ProcessAttributes, sys.ThreadAttributes, !sys.NoInheritHandles, flags, createEnvBlock(procAttr.Env), dirp, si, pi)
+	if err != nil {
+		panic(err)
+	}
+	defer syscall.CloseHandle(syscall.Handle(pi.Thread))
+
+	handle := uintptr(pi.Process)
+	s, err := syscall.WaitForSingleObject(syscall.Handle(handle), syscall.INFINITE)
+	switch s {
+	case syscall.WAIT_OBJECT_0:
+		break
+	case syscall.WAIT_FAILED:
+		panic("WaitForSingleObject failed")
+	default:
+		panic("Unexpected result from WaitForSingleObject")
+	}
+
+	var rc uint32
+	if err = syscall.GetExitCodeProcess(syscall.Handle(handle), &rc); err != nil {
+		panic(err)
+	}
+
+	// The process had an exit code (includes err==nil, 0).
+	logging.Debugf(c, "Python subprocess has terminated: %v", err)
+	os.Exit(int(rc))
+	panic("must not return")
 }
