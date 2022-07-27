luci/luci-go

commit f016d59e2b693d130377427aefbd40dfd3716870

[server] Cancel the request context when client drops.
Do it by making http.Server use our root context and inherit
stuff from it in rootMiddleware (instead of trampling over it).

That way http.Server has opportunity to hook up cancellation
machinery in between the root server context with various
root values, and the per-request context in rootMiddleware.

R=iannucci@chromium.org

Change-Id: Ie2e7ae397d2222889922ec632724f4d95d008888
Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3763518
Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

server/server.go
@@ -1245,6 +1245,8 @@ func (s *Server) healthResponse(c context.Context) string {
 //
 // Basically srv.ListenAndServe with some testing helpers.
 func (s *Server) serveLoop(srv *http.Server) error {
+	// Make sure http.Request inherits our root context.
+	srv.BaseContext = func(net.Listener) context.Context { return s.Context }
 	// If not running tests, let http.Server bind the socket as usual.
 	if s.Options.testListeners == nil {
 		return srv.ListenAndServe()
@@ -1353,11 +1355,14 @@ var cloudTraceFormat = propagation.HTTPFormat{}
 
 // rootMiddleware prepares the per-request context.
 func (s *Server) rootMiddleware(c *router.Context, next router.Handler) {
-	started := clock.Now(s.Context)
+	// The request context is derived from s.Context (see serveLoop) and has
+	// various server systems injected into it already. Its only difference from
+	// s.Context is that http.Server cancels it when the client disconnects,
+	// which we want.
+	ctx := c.Request.Context()
 
 	// If running on GAE, initialize the per-request API tickets needed to make
 	// RPCs to the GAE service bridge.
-	ctx := s.Context
 	if s.Options.GAE {
 		ctx = gae.WithTickets(ctx, gae.RequestTickets(c.Request.Header))
 	}
@@ -1372,6 +1377,7 @@ func (s *Server) rootMiddleware(c *router.Context, next router.Handler) {
 	ctx, span := s.startRequestSpan(ctx, c.Request, healthCheck)
 
 	// This is used in waitUntilNotServing.
+	started := clock.Now(ctx)
 	if !healthCheck {
 		s.lastReqTime.Store(started)
 	}
@@ -1410,7 +1416,7 @@ func (s *Server) rootMiddleware(c *router.Context, next router.Handler) {
 			}
 			// Emit a warning if the health check is slow, this likely indicates
 			// high CPU load.
-			logging.Warningf(c.Context, "Health check is slow: %s > %s", latency, healthTimeLogThreshold)
+			logging.Warningf(ctx, "Health check is slow: %s > %s", latency, healthTimeLogThreshold)
 		}
 
 		// When running behind Envoy, log its request IDs to simplify debugging.
@@ -1496,7 +1502,10 @@ func (s *Server) rootMiddleware(c *router.Context, next router.Handler) {
 		}, annotateWithSpan))
 	}
 
-	c.Context = caching.WithRequestCache(ctx)
+	ctx = caching.WithRequestCache(ctx)
+
+	c.Context = ctx
+	c.Request = c.Request.WithContext(ctx)
 	next(c)
 }
 

server/server_test.go
@@ -184,6 +184,30 @@ func TestServer(t *testing.T) {
 			So(err, ShouldBeNil)
 		})
 
+		Convey("Context cancellation on client timeout", func() {
+			cancelled := make(chan struct{})
+			srv.Routes.GET("/request", nil, func(c *router.Context) {
+				select {
+				case <-c.Context.Done():
+					close(cancelled)
+				case <-time.After(time.Minute):
+				}
+				http.Error(c.Writer, "This should basically be ignored", 500)
+			})
+			srv.ServeInBackground()
+
+			_, err := srv.GetMainWithTimeout("/request", nil, time.Second)
+			So(err, ShouldNotBeNil)
+
+			wasCancelled := false
+			select {
+			case <-cancelled:
+				wasCancelled = true
+			case <-time.After(time.Minute):
+			}
+			So(wasCancelled, ShouldBeTrue)
+		})
+
 		Convey("Warmup and cleanup callbacks", func() {
 			var warmups []string
 			var cleanups []string
@@ -716,23 +740,34 @@ func (s *testServer) StopBackgroundServing() error {
 // GetMain makes a blocking request to the main serving port, aborting it if
 // the server dies.
 func (s *testServer) GetMain(uri string, headers map[string]string) (string, error) {
-	return s.get(s.mainAddr+uri, headers)
+	return s.get(s.mainAddr+uri, headers, 0)
+}
+
+// GetMain makes a blocking request with timeout to the main serving port,
+// aborting it if the server dies.
+func (s *testServer) GetMainWithTimeout(uri string, headers map[string]string, timeout time.Duration) (string, error) {
+	return s.get(s.mainAddr+uri, headers, timeout)
 }
 
 // GetAdmin makes a blocking request to the admin port, aborting it if
 // the server dies.
 func (s *testServer) GetAdmin(uri string, headers map[string]string) (string, error) {
-	return s.get(s.adminAddr+uri, headers)
+	return s.get(s.adminAddr+uri, headers, 0)
 }
 
 // get makes a blocking request, aborting it if the server dies.
-func (s *testServer) get(uri string, headers map[string]string) (resp string, err error) {
+func (s *testServer) get(uri string, headers map[string]string, timeout time.Duration) (resp string, err error) {
 	done := make(chan struct{})
 	go func() {
 		defer close(done)
 		var req *http.Request
 		if req, err = http.NewRequest("GET", uri, nil); err != nil {
 			return
+		}
+		if timeout != 0 {
+			ctx, cancel := context.WithTimeout(context.Background(), timeout)
+			defer cancel()
+			req = req.WithContext(ctx)
 		}
 		for k, v := range headers {
 			req.Header.Set(k, v)

commit 08be067ea5b8ab3dcf23f694960f86db9edd6e61

[server] Bind ports eagerly, expose the listening address.
Commit https://chromium.googlesource.com/infra/luci/luci-go/+/f016d59
broke server/tq local loopback executor because when dispatching
tasks it was cheating and was skipping http.Server{...} network code,
instead calling http.Handler directly. It worked before because
we didn't rely on anything vital that was done in http.Server{...}.
But since that commit landed, we now rely on http.Server{...} setting
up the root context in the incoming http.Request.

By skipping http.Server{...} and calling srv.Routes().ServeHTTP(...)
directly we are missing on the root context initialization now done
inside the http.Server.

We should change tqtesting.LoopbackHTTPExecutor to do honest HTTP
request and go through http.Server{...} code to pick up the context
modifications. But to do that we need to know where to send requests
to (host and port). A server can be started with port ":0", which
means "let the OS pick an available port". But we need to know this
port already when launching server/tq module. Thus this CL.

Note that this bug affects only running server/tq locally in
dev mode. Unit tests use different code path (not involving LUCI
server or network calls at all).

R=iannucci@chromium.org

Change-Id: I83003fd1b0e84bb31a4b8865af674e1b7de545f2
Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3784333
Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

server/module/module.go
@@ -18,6 +18,7 @@ package module
 
 import (
 	"context"
+	"net"
 
 	"google.golang.org/grpc"
 
@@ -81,6 +82,11 @@ type Host interface {
 	// ServiceRegistrar is a registrar that can be used to register gRPC services.
 	ServiceRegistrar() grpc.ServiceRegistrar
 
+	// HTTPAddr is the address the main HTTP port is bound to.
+	//
+	// May be nil if the server doesn't expose the main port.
+	HTTPAddr() net.Addr
+
 	// Routes returns a router that servers HTTP requests hitting the main port.
 	//
 	// The module can use it to register additional request handlers.
@@ -105,8 +111,8 @@ type Host interface {
 	// the goroutine will exit soon after the context is canceled.
 	RunInBackground(activity string, f func(context.Context))
 
-	// RegisterWarmup registers a callback that is run in server's ListenAndServe
-	// right before the serving loop.
+	// RegisterWarmup registers a callback that is run in server's Serve right
+	// before the serving loop.
 	//
 	// It receives the global server context (including all customizations made
 	// by the user code in server.Main). Intended for best-effort warmups: there's
@@ -117,8 +123,8 @@ type Host interface {
 	// in server.Main and it can't depend on modifications done there.
 	RegisterWarmup(cb func(context.Context))
 
-	// RegisterCleanup registers a callback that is run in server's ListenAndServe
-	// after the server exits the serving loop.
+	// RegisterCleanup registers a callback that is run in server's Serve after
+	// the server exits the serving loop.
 	//
 	// It receives the global server context.
 	RegisterCleanup(cb func(context.Context))

server/port.go
@@ -16,6 +16,7 @@ package server
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"sync"
 
@@ -40,11 +41,12 @@ type Port struct {
 	// specially registered per-host routers (see VirtualHost). Normally, there
 	// are no such per-host routers, so usually Routes is used for all requests.
 	//
-	// Should be populated before Server's ListenAndServe call.
+	// Should be populated before Server's Serve loop.
 	Routes *router.Router
 
-	parent *Server     // the owning server
-	opts   PortOptions // options passed to AddPort
+	parent   *Server      // the owning server
+	opts     PortOptions  // options passed to AddPort
+	listener net.Listener // listening socket if ListenAddr != "-"
 
 	mu      sync.Mutex
 	srv     *http.Server              // lazy-initialized in httpServer()
@@ -58,7 +60,7 @@ type Port struct {
 // reach the default router (port.Routes), even if the virtual host router
 // doesn't have a route for them. Such requests finish with HTTP 404.
 //
-// Should be called before Server's ListenAndServe (panics otherwise).
+// Should be called before Server's Serve loop (panics otherwise).
 func (p *Port) VirtualHost(host string) *router.Router {
 	p.mu.Lock()
 	defer p.mu.Unlock()
@@ -82,10 +84,10 @@ func (p *Port) VirtualHost(host string) *router.Router {
 // nameForLog returns a string to identify this port in the server logs.
 func (p *Port) nameForLog() string {
 	var pfx string
-	if p.opts.ListenAddr == "-" {
-		pfx = "-"
+	if p.listener != nil {
+		pfx = "http://" + p.listener.Addr().String()
 	} else {
-		pfx = "http://" + p.opts.ListenAddr
+		pfx = "-"
 	}
 	if p.opts.Name != "" {
 		return fmt.Sprintf("%s [%s]", pfx, p.opts.Name)
@@ -95,7 +97,7 @@ func (p *Port) nameForLog() string {
 
 // httpServer lazy-initializes and returns http.Server for this port.
 //
-// Called from Server's ListenAndServe.
+// Called from Server's Serve.
 //
 // Once this function is called, no more virtual hosts can be added to the
 // server (an attempt to do so causes a panic).
@@ -103,13 +105,13 @@ func (p *Port) httpServer() *http.Server {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 
-	if p.opts.ListenAddr == "-" {
-		panic("httpServer must not be used with dummy ports")
+	if p.listener == nil {
+		panic("httpServer must not be used with uninitialized ports")
 	}
 
 	if p.srv == nil {
 		p.srv = &http.Server{
-			Addr:     p.opts.ListenAddr,
+			Addr:     p.listener.Addr().String(),
 			Handler:  p.initHandlerLocked(),
 			ErrorLog: nil, // TODO(vadimsh): Log via 'logging' package.
 		}

server/server.go
@@ -321,7 +321,7 @@ func Main(opts *Options, mods []module.Module, init func(srv *Server) error) {
 			srv.Fatal(err)
 		}
 	}
-	if err = srv.ListenAndServe(); err != nil {
+	if err = srv.Serve(); err != nil {
 		srv.Fatal(err)
 	}
 }
@@ -636,8 +636,8 @@ func (o *Options) hostOptions() module.HostOptions {
 type Server struct {
 	// Context is the root context used by all requests and background activities.
 	//
-	// Can be replaced (by a derived context) before ListenAndServe call, for
-	// example to inject values accessible to all request handlers.
+	// Can be replaced (by a derived context) before Serve call, for example to
+	// inject values accessible to all request handlers.
 	Context context.Context
 
 	// Routes is a router for requests hitting HTTPAddr port.
@@ -649,12 +649,12 @@ type Server struct {
 	// This router is also accessible to the server modules and they can install
 	// routes into it.
 	//
-	// Should be populated before ListenAndServe call.
+	// Should be populated before Serve call.
 	Routes *router.Router
 
 	// PRPC is pRPC server with APIs exposed on HTTPAddr port via Routes router.
 	//
-	// Should be populated before ListenAndServe call.
+	// Should be populated before Serve call.
 	PRPC *prpc.Server
 
 	// CookieAuth is an authentication method implemented via cookies.
@@ -677,12 +677,12 @@ type Server struct {
 
 	mu      sync.Mutex    // protects fields below
 	ports   []*Port       // all non-dummy ports (each one hosts an HTTP server)
-	started bool          // true inside and after ListenAndServe
+	started bool          // true inside and after Serve
 	stopped bool          // true inside and after Shutdown
 	ready   chan struct{} // closed right before starting the serving loop
 	done    chan struct{} // closed after Shutdown returns
 
-	// See RegisterUnaryServerInterceptor and ListenAndServe.
+	// See RegisterUnaryServerInterceptor and Serve.
 	unaryInterceptors []grpc.UnaryServerInterceptor
 
 	rndM sync.Mutex // protects rnd
@@ -728,6 +728,14 @@ func (h *moduleHostImpl) panicIfInvalid() {
 func (h *moduleHostImpl) ServiceRegistrar() grpc.ServiceRegistrar {
 	h.panicIfInvalid()
 	return h.srv.PRPC
+}
+
+func (h *moduleHostImpl) HTTPAddr() net.Addr {
+	h.panicIfInvalid()
+	if h.srv.mainPort.listener != nil {
+		return h.srv.mainPort.listener.Addr()
+	}
+	return nil
 }
 
 func (h *moduleHostImpl) Routes() *router.Router {
@@ -922,7 +930,7 @@ func New(ctx context.Context, opts Options, mods []module.Module) (srv *Server, 
 	return srv, nil
 }
 
-// AddPort prepares an additional serving HTTP port.
+// AddPort prepares and binds an additional serving HTTP port.
 //
 // Can be used to open more listening HTTP ports (in addition to opts.HTTPAddr
 // and opts.AdminAddr). The returned Port object can be used to populate the
@@ -932,8 +940,8 @@ func New(ctx context.Context, opts Options, mods []module.Module) (srv *Server, 
 // object, but it is not actually exposed as a listening TCP socket. This is
 // useful to disable listening ports without changing any code.
 //
-// Should be called before ListenAndServe (panics otherwise).
-func (s *Server) AddPort(opts PortOptions) *Port {
+// Should be called before Serve (panics otherwise).
+func (s *Server) AddPort(opts PortOptions) (*Port, error) {
 	port := &Port{
 		Routes: s.newRouter(opts),
 		parent: s,
@@ -945,10 +953,27 @@ func (s *Server) AddPort(opts PortOptions) *Port {
 	if s.started {
 		s.Fatal(errors.Reason("the server has already been started").Err())
 	}
+
 	if opts.ListenAddr != "-" {
+		// If not running tests, bind the socket as usual.
+		if s.Options.testListeners == nil {
+			var err error
+			port.listener, err = net.Listen("tcp", opts.ListenAddr)
+			if err != nil {
+				return nil, errors.Annotate(err, "failed to bind the listening port for %q at %q", opts.Name, opts.ListenAddr).Err()
+			}
+		} else {
+			// In test mode the listener MUST be prepared already.
+			port.listener = s.Options.testListeners[opts.ListenAddr]
+			if port.listener == nil {
+				return nil, errors.Reason("test listener for %q at %q is not set", opts.Name, opts.ListenAddr).Err()
+			}
+		}
+		// Add to the list of ports that actually have sockets listening.
 		s.ports = append(s.ports, port)
 	}
-	return port
+
+	return port, nil
 }
 
 // VirtualHost returns a router (registering it if necessary) used for requests
@@ -972,7 +997,7 @@ func (s *Server) AddPort(opts PortOptions) *Port {
 // directly into server.Routes instead, using VirtualHost only for routes that
 // critically depend on Host header.
 //
-// Should be called before ListenAndServe (panics otherwise).
+// Should be called before Serve (panics otherwise).
 func (s *Server) VirtualHost(host string) *router.Router {
 	return s.mainPort.VirtualHost(host)
 }
@@ -1067,9 +1092,9 @@ func (s *Server) RunInBackground(activity string, f func(context.Context)) {
 //
 // An interceptor set in server.PRPC.UnaryServerInterceptor (if any) is
 // automatically registered as the last (innermost) one right before the server
-// starts listening for requests in ListenAndServe.
+// starts serving requests in Serve.
 //
-// Should be called before ListenAndServe (panics otherwise).
+// Should be called before Serve (panics otherwise).
 func (s *Server) RegisterUnaryServerInterceptor(intr grpc.UnaryServerInterceptor) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -1079,7 +1104,7 @@ func (s *Server) RegisterUnaryServerInterceptor(intr grpc.UnaryServerInterceptor
 	s.unaryInterceptors = append(s.unaryInterceptors, intr)
 }
 
-// ListenAndServe launches the serving loop.
+// Serve launches the serving loop.
 //
 // Blocks forever or until the server is stopped via Shutdown (from another
 // goroutine or from a SIGTERM handler). Returns nil if the server was shutdown
@@ -1087,7 +1112,7 @@ func (s *Server) RegisterUnaryServerInterceptor(intr grpc.UnaryServerInterceptor
 // is logged inside.
 //
 // Should be called only once. Panics otherwise.
-func (s *Server) ListenAndServe() error {
+func (s *Server) Serve() error {
 	s.mu.Lock()
 	if s.started {
 		s.mu.Unlock()
@@ -1149,7 +1174,9 @@ func (s *Server) ListenAndServe() error {
 		port := port
 		go func() {
 			defer wg.Done()
-			if err := s.serveLoop(port.httpServer()); err != http.ErrServerClosed {
+			srv := port.httpServer()
+			srv.BaseContext = func(net.Listener) context.Context { return s.Context }
+			if err := srv.Serve(port.listener); err != http.ErrServerClosed {
 				logging.WithError(err).Errorf(s.Context, "Server %s failed", port.nameForLog())
 				errs[i] = err
 				s.Shutdown() // close all other servers
@@ -1204,7 +1231,7 @@ func (s *Server) Shutdown() {
 	// Wait for all background goroutines to stop.
 	s.bgrWg.Wait()
 
-	// Notify ListenAndServe that it can exit now.
+	// Notify Serve that it can exit now.
 	s.stopped = true
 	close(s.done)
 }
@@ -1239,23 +1266,6 @@ func (s *Server) healthResponse(c context.Context) string {
 		"host:    " + s.Options.Hostname,
 		"",
 	}, "\n")
-}
-
-// serveLoop binds the socket and launches the serving loop.
-//
-// Basically srv.ListenAndServe with some testing helpers.
-func (s *Server) serveLoop(srv *http.Server) error {
-	// Make sure http.Request inherits our root context.
-	srv.BaseContext = func(net.Listener) context.Context { return s.Context }
-	// If not running tests, let http.Server bind the socket as usual.
-	if s.Options.testListeners == nil {
-		return srv.ListenAndServe()
-	}
-	// In test mode the listener MUST be prepared already.
-	if l, _ := s.Options.testListeners[srv.Addr]; l != nil {
-		return srv.Serve(l)
-	}
-	return errors.Reason("test listener is not set").Err()
 }
 
 // waitUntilNotServing is called during the graceful shutdown and it tries to
@@ -1295,8 +1305,8 @@ func (s *Server) waitUntilNotServing() {
 	}
 }
 
-// RegisterWarmup registers a callback that is run in server's ListenAndServe
-// right before the serving loop.
+// RegisterWarmup registers a callback that is run in server's Serve right
+// before the serving loop.
 //
 // It receives the global server context (including all customizations made
 // by the user code in server.Main). Intended for best-effort warmups: there's
@@ -1321,8 +1331,8 @@ func (s *Server) runWarmup() {
 	}
 }
 
-// RegisterCleanup registers a callback that is run in ListenAndServe after the
-// server has exited the serving loop.
+// RegisterCleanup registers a callback that is run in Serve after the server
+// has exited the serving loop.
 //
 // Registering a new cleanup callback from within a cleanup causes a deadlock,
 // don't do that.
@@ -1355,10 +1365,9 @@ var cloudTraceFormat = propagation.HTTPFormat{}
 
 // rootMiddleware prepares the per-request context.
 func (s *Server) rootMiddleware(c *router.Context, next router.Handler) {
-	// The request context is derived from s.Context (see serveLoop) and has
-	// various server systems injected into it already. Its only difference from
-	// s.Context is that http.Server cancels it when the client disconnects,
-	// which we want.
+	// The request context is derived from s.Context (see Serve) and has various
+	// server systems injected into it already. Its only difference from s.Context
+	// is that http.Server cancels it when the client disconnects, which we want.
 	ctx := c.Request.Context()
 
 	// If running on GAE, initialize the per-request API tickets needed to make
@@ -2056,17 +2065,21 @@ func (s *Server) getServiceID() string {
 
 // initMainPort initializes the server on options.HTTPAddr port.
 func (s *Server) initMainPort() error {
-	s.mainPort = s.AddPort(PortOptions{
+	var err error
+	s.mainPort, err = s.AddPort(PortOptions{
 		Name:       "main",
 		ListenAddr: s.Options.HTTPAddr,
 	})
+	if err != nil {
+		return err
+	}
 	s.Routes = s.mainPort.Routes
 
 	// Install auth info handlers (under "/auth/api/v1/server/").
 	auth.InstallHandlers(s.Routes, nil)
 
-	// Expose public pRPC endpoints (see also ListenAndServe where we put the
-	// final interceptors).
+	// Expose public pRPC endpoints (see also Serve where we put the final
+	// interceptors).
 	s.PRPC = &prpc.Server{
 		Authenticator: &auth.Authenticator{
 			Methods: []auth.Method{
@@ -2110,11 +2123,14 @@ func (s *Server) initAdminPort() error {
 	})
 
 	// Install endpoints accessible through the admin port only.
-	adminPort := s.AddPort(PortOptions{
+	adminPort, err := s.AddPort(PortOptions{
 		Name:           "admin",
 		ListenAddr:     s.Options.AdminAddr,
 		DisableMetrics: true, // do not pollute HTTP metrics with admin-only routes
 	})
+	if err != nil {
+		return err
+	}
 	routes := adminPort.Routes
 
 	routes.GET("/", nil, func(c *router.Context) {

resultdb/internal/integrationtests/testapp_test.go
@@ -221,13 +221,13 @@ func (t *testApp) initServers(ctx context.Context) error {
 	return nil
 }
 
-func (t *testApp) ListenAndServe() error {
+func (t *testApp) Serve() error {
 	eg := errgroup.Group{}
 	for _, s := range t.servers {
 		s := s
 		eg.Go(func() error {
 			defer t.Shutdown()
-			return s.ListenAndServe()
+			return s.Serve()
 		})
 	}
 	return eg.Wait()
@@ -238,7 +238,7 @@ func (t *testApp) ListenAndServe() error {
 func (t *testApp) Start(ctx context.Context) error {
 	errC := make(chan error, 1)
 	go func() {
-		errC <- t.ListenAndServe()
+		errC <- t.Serve()
 	}()
 
 	// Give servers 5s to start.

server/server_test.go
@@ -716,7 +716,7 @@ func newTestServer(ctx context.Context, o *Options) (srv *testServer, err error)
 }
 
 func (s *testServer) ServeInBackground() {
-	go func() { s.serveErr.Set(s.ListenAndServe()) }()
+	go func() { s.serveErr.Set(s.Serve()) }()
 
 	// Wait until both HTTP endpoints are serving before returning.
 	if _, err := s.GetMain(healthEndpoint, nil); err != nil {