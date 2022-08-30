bc1a8910c43ec435d5d83d53294f1eaf4c0b16fc
Yiwei Zhang <yiwzhang@google.com>
2022-08-30 23:28:33 +0000

    cv: introduce RunWithBuilderTarget
    This will be used to report metrics using Builder target
    
    R=ddoman
    
    Bug: 1254783
    Change-Id: I2ba40370b8acb77b56c055561011cda8c79b632f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3863631
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Scott Lee <ddoman@chromium.org>

---
+++ cv/internal/metrics/builder.go
@@ -0,0 +1,46 @@
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metrics
+
+import (
+	"context"
+
+	bbmetrics "go.chromium.org/luci/buildbucket/metrics"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/tsmon/target"
+
+	"go.chromium.org/luci/cv/internal/common"
+	"go.chromium.org/luci/cv/internal/tryjob"
+)
+
+// RunWithBuilderTarget executes `cb` under the context of a Builder target.
+//
+// It is normally used to report metrics using Builder Target.
+func RunWithBuilderTarget(ctx context.Context, env *common.Env, def *tryjob.Definition, cb func(context.Context)) {
+	if def.GetBuildbucket() != nil {
+		panic(errors.Reason("only Buildbucket backend is supported. Got %T", def.GetBackend()))
+	}
+	builder := def.GetBuildbucket().GetBuilder()
+	cctx := target.Set(ctx, &bbmetrics.BuilderTarget{
+		Project: builder.GetProject(),
+		Bucket:  builder.GetBucket(),
+		Builder: builder.GetBuilder(),
+
+		ServiceName: env.GAEInfo.CloudProject,
+		JobName:     env.GAEInfo.ServiceName,
+		InstanceID:  env.GAEInfo.InstanceID,
+	})
+	cb(cctx)
+}
