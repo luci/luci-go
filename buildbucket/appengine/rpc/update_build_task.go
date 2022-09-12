ae2ffe3ee3d8b4552788d9a876c33867b93bc232
randymaldonado <randymaldonado@google.com>
2022-09-12 19:01:59 +0000

    [buildbucket] adding validation for task update request
    Change-Id: I1afa6302e4ce83db5bb391cf49c5530c8f818a8d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3879932
    Commit-Queue: Randy Maldonado <randymaldonado@google.com>
    Reviewed-by: Chan Li <chanli@chromium.org>

--- buildbucket/appengine/rpc/update_build_task.go
+++ buildbucket/appengine/rpc/update_build_task.go
@@ -16,14 +16,55 @@ package rpc
 
 import (
 	"context"
+	"strings"
+
+	"google.golang.org/grpc/codes"
 
 	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/proto/protowalk"
 	"go.chromium.org/luci/grpc/appstatus"
 
 	pb "go.chromium.org/luci/buildbucket/proto"
 )
 
+func validateTaskStatus(taskStatus pb.Status) error {
+	switch taskStatus {
+	case pb.Status_SCHEDULED,
+		pb.Status_ENDED_MASK,
+		pb.Status_STATUS_UNSPECIFIED:
+		return errors.Reason("task.status: invalid status %s for UpdateBuildTask", taskStatus).Err()
+	}
+
+	if _, ok := pb.Status_value[taskStatus.String()]; !ok {
+		return errors.Reason("task.status: invalid status %s for UpdateBuildTask", taskStatus).Err()
+	}
+	return nil
+}
+
+func validateUpdateBuildTaskRequest(ctx context.Context, req *pb.UpdateBuildTaskRequest) error {
+	if procRes := protowalk.Fields(req, &protowalk.RequiredProcessor{}); procRes != nil {
+		if resStrs := procRes.Strings(); len(resStrs) > 0 {
+			return errors.Reason(strings.Join(resStrs, ". ")).Err()
+		}
+	}
+	if req.GetTask().GetId().GetId() == "" {
+		return errors.Reason("task.id: required").Err()
+	}
+	if err := validateTaskStatus(req.Task.Status); err != nil {
+		return errors.Annotate(err, "task.Status").Err()
+	}
+	detailsInKb := float64(len(req.Task.GetDetails().String()) / 1024)
+	if detailsInKb > 10 {
+		return errors.Reason("task.details is greater than 10 kb").Err()
+	}
+	return nil
+}
+
 func (*Builds) UpdateBuildTask(ctx context.Context, req *pb.UpdateBuildTaskRequest) (*pb.Task, error) {
+	if err := validateUpdateBuildTaskRequest(ctx, req); err != nil {
+		return nil, appstatus.Errorf(codes.InvalidArgument, "%s", err)
+	}
+
 	_, _, err := validateBuildTaskToken(ctx, req.GetBuildId())
 	if err != nil {
 		return nil, appstatus.BadRequest(errors.Annotate(err, "invalid build").Err())
--- buildbucket/appengine/rpc/update_build_task_test.go
+++ buildbucket/appengine/rpc/update_build_task_test.go
@@ -16,9 +16,11 @@ package rpc
 
 import (
 	"context"
+	"strconv"
 	"testing"
 
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/types/known/structpb"
 
 	"go.chromium.org/luci/buildbucket"
 	"go.chromium.org/luci/common/clock/testclock"
@@ -37,16 +39,133 @@ import (
 	. "go.chromium.org/luci/common/testing/assertions"
 )
 
+func TestValidateTaskUpdate(t *testing.T) {
+	t.Parallel()
+
+	Convey("ValidateTaskUpdate", t, func() {
+		ctx := memory.Use(context.Background())
+
+		Convey("is valid task", func() {
+			req := &pb.UpdateBuildTaskRequest{
+				BuildId: "1",
+				Task: &pb.Task{
+					Status: pb.Status_STARTED,
+					Id: &pb.TaskID{
+						Id:     "one",
+						Target: "swarming",
+					},
+				},
+			}
+			So(validateUpdateBuildTaskRequest(ctx, req), ShouldBeNil)
+		})
+		Convey("is missing task", func() {
+			req := &pb.UpdateBuildTaskRequest{
+				BuildId: "1",
+			}
+			So(validateUpdateBuildTaskRequest(ctx, req), ShouldBeError)
+		})
+		Convey("is missing build id", func() {
+			req := &pb.UpdateBuildTaskRequest{
+				Task: &pb.Task{
+					Status: pb.Status_STARTED,
+					Id: &pb.TaskID{
+						Id:     "one",
+						Target: "swarming",
+					},
+				},
+			}
+			So(validateUpdateBuildTaskRequest(ctx, req), ShouldBeError)
+		})
+		Convey("is missing task id", func() {
+			req := &pb.UpdateBuildTaskRequest{
+				BuildId: "1",
+				Task: &pb.Task{
+					Status: pb.Status_STARTED,
+				},
+			}
+			So(validateUpdateBuildTaskRequest(ctx, req), ShouldBeError)
+		})
+		Convey("is invalid task status: SCHEDULED", func() {
+			req := &pb.UpdateBuildTaskRequest{
+				BuildId: "1",
+				Task: &pb.Task{
+					Status: pb.Status_SCHEDULED,
+					Id: &pb.TaskID{
+						Id:     "one",
+						Target: "swarming",
+					},
+				},
+			}
+			So(validateUpdateBuildTaskRequest(ctx, req), ShouldBeError)
+		})
+		Convey("is invalid task status: ENDED_MASK", func() {
+			req := &pb.UpdateBuildTaskRequest{
+				BuildId: "1",
+				Task: &pb.Task{
+					Status: pb.Status_ENDED_MASK,
+					Id: &pb.TaskID{
+						Id:     "one",
+						Target: "swarming",
+					},
+				},
+			}
+			So(validateUpdateBuildTaskRequest(ctx, req), ShouldBeError)
+		})
+		Convey("is invalid task status: STATUS_UNSPECIFIED", func() {
+			req := &pb.UpdateBuildTaskRequest{
+				BuildId: "1",
+				Task: &pb.Task{
+					Status: pb.Status_STATUS_UNSPECIFIED,
+					Id: &pb.TaskID{
+						Id:     "one",
+						Target: "swarming",
+					},
+				},
+			}
+			So(validateUpdateBuildTaskRequest(ctx, req), ShouldBeError)
+		})
+		Convey("is invalid task detail", func() {
+
+			details := make(map[string]*structpb.Value)
+			for i := 0; i < 10000; i++ {
+				v, _ := structpb.NewValue("my really long detail, but it's not that long.")
+				details[strconv.Itoa(i)] = v
+			}
+			req := &pb.UpdateBuildTaskRequest{
+				BuildId: "1",
+				Task: &pb.Task{
+					Status: pb.Status_STARTED,
+					Details: &structpb.Struct{
+						Fields: details,
+					},
+					Id: &pb.TaskID{
+						Id:     "one",
+						Target: "swarming",
+					},
+				},
+			}
+			So(validateUpdateBuildTaskRequest(ctx, req), ShouldBeError)
+		})
+	})
+}
+
 func TestUpdateBuildTask(t *testing.T) {
 	t.Parallel()
 
-	tk, _ := buildtoken.GenerateToken(1, pb.TokenBody_TASK)
+	var tk string
+
+	updateContextForNewBuildToken := func(ctx context.Context, buildID int64) (string, context.Context) {
+		newToken, _ := buildtoken.GenerateToken(buildID, pb.TokenBody_TASK)
+		ctx = metadata.NewIncomingContext(ctx, metadata.Pairs(buildbucket.BuildbucketBackendTokenHeader, newToken))
+		return newToken, ctx
+	}
 
 	Convey("UpdateBuildTask", t, func() {
+
 		srv := &Builds{}
 		ctx := memory.Use(context.Background())
 		ctx = metrics.WithServiceInfo(ctx, "svc", "job", "ins")
-		ctx = metadata.NewIncomingContext(ctx, metadata.Pairs(buildbucket.BuildbucketBackendTokenHeader, tk))
+		tk, ctx = updateContextForNewBuildToken(ctx, 1)
 		datastore.GetTestable(ctx).AutoIndex(true)
 		datastore.GetTestable(ctx).Consistent(true)
 		ctx, _ = tsmon.WithDummyInMemory(ctx)
@@ -95,14 +214,26 @@ func TestUpdateBuildTask(t *testing.T) {
 			Convey("buildID matches token", func() {
 				req := &pb.UpdateBuildTaskRequest{
 					BuildId: "1",
-					Task:    &pb.Task{},
+					Task: &pb.Task{
+						Status: pb.Status_STARTED,
+						Id: &pb.TaskID{
+							Id:     "one",
+							Target: "swarming",
+						},
+					},
 				}
 				So(updateBuildTask(ctx, req), ShouldBeRPCOK)
 			})
 			Convey("buildID does not match token", func() {
 				req := &pb.UpdateBuildTaskRequest{
-					BuildId: "one",
-					Task:    &pb.Task{},
+					BuildId: "2",
+					Task: &pb.Task{
+						Status: pb.Status_STARTED,
+						Id: &pb.TaskID{
+							Id:     "two",
+							Target: "swarming",
+						},
+					},
 				}
 				So(updateBuildTask(ctx, req), ShouldBeRPCUnknown)
 			})
