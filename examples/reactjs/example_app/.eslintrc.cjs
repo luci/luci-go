6fdf47ed99896d073e958c75f54df2e09ed9ee17
Mostafa Draz <mdraz@google.com>
2022-11-10 01:00:42 +0000

    [LUCI] Updated imports and added ESLint config to support path imports.
    Change-Id: I984f4850920324af24d37b30c37bf7dc98b09007
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4015403
    Reviewed-by: Matthew Warton <mwarton@google.com>
    Auto-Submit: Mostafa Draz <mdraz@google.com>
    Commit-Queue: Mostafa Draz <mdraz@google.com>

--- examples/reactjs/example_app/.eslintrc.cjs
+++ examples/reactjs/example_app/.eslintrc.cjs
@@ -1,6 +1,16 @@
-// Copyright 2022 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 
 // eslint-disable-next-line no-undef
@@ -16,7 +26,6 @@ module.exports = {
     'prettier',
     'google',
     'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
     'plugin:jsx-a11y/recommended',
@@ -25,7 +34,25 @@ module.exports = {
     'react': {
       'version': 'detect',
     },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      'typescript': {},
+    },
   },
+  'overrides': [
+    {
+      'files': ['src/**/*.test.tsx'],
+      'plugins': ['jest'],
+      'extends': ['plugin:jest/recommended'],
+    },
+    {
+      'files': ['cypress/**'],
+      'plugins': ['cypress'],
+      'extends': ['plugin:cypress/recommended'],
+    },
+  ],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'ecmaFeatures': {
@@ -38,15 +65,23 @@ module.exports = {
     'react',
     '@typescript-eslint',
     'prettier',
-    'jest',
     'jsx-a11y',
+    'import',
   ],
   'rules': {
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
     'object-curly-spacing': ['error', 'always', { 'objectsInObjects': true }],
     'require-jsdoc': 0,
-    'import/order': ['error'],
+    'import/order': ['error', {
+      'pathGroups': [
+        {
+          'pattern': '@/**',
+          'group': 'external',
+          'position': 'after',
+        },
+      ],
+    }],
     'no-trailing-spaces': 'error',
     'no-console': ['error', { allow: ['error'] }],
     'eol-last': ['error', 'always'],
--- examples/reactjs/example_app/src/components/example_react_query/example_react_query.tsx
+++ examples/reactjs/example_app/src/components/example_react_query/example_react_query.tsx
@@ -1,13 +1,23 @@
-// Copyright 2022 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import { useQuery } from 'react-query';
 
 import Grid from '@mui/material/Grid';
 import CircularProgress from '@mui/material/CircularProgress';
 import Alert from '@mui/material/Alert';
-import { getData } from '../../services/example_service';
+import { getData } from '@/services/example_service';
 
 const ExampleReactQuery = () => {
   const { data, error, isLoading } = useQuery('example', () => getData());
