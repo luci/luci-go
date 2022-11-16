020c3aceeb53f0f2abb2faa6f454c8f67ff5f38f
Camilo Jacomet <cjacomet@google.com>
2022-11-16 21:19:11 +0000

    [auth-service]: Proto + Config for imports.cfg.
    Copied the proto definition from https://chromium.googlesource.com/infra/luci/luci-py/+/HEAD/appengine/auth_service/proto/config.proto#51.
    This cl defines the operations for fetching the config and storing it in context for Auth Service to use.
    
    Bug:1336136
    Change-Id: Ibfbbcfab3ccedb74ef4d5f667e6da0861811f603
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4028740
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Commit-Queue: Camilo Jacomet <cjacomet@google.com>

---
+++ auth_service/internal/configs/srvcfg/importscfg/config_test.go
@@ -0,0 +1,81 @@
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package importscfg
+
+import (
+	"context"
+	"testing"
+
+	"go.chromium.org/luci/auth_service/api/configspb"
+
+	"go.chromium.org/luci/gae/impl/memory"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestConfigContext(t *testing.T) {
+	t.Parallel()
+	ctx := memory.Use(context.Background())
+	importsCfg := &configspb.GroupImporterConfig{
+		Tarball: []*configspb.GroupImporterConfig_TarballEntry{
+			{
+				Url: "some-url",
+				OauthScopes: []string{
+					"example-oauth",
+				},
+				Domain: "example.com",
+				Systems: []string{
+					"groups",
+				},
+				Groups: []string{
+					"committers",
+					"testers",
+				},
+			},
+		},
+		Plainlist: []*configspb.GroupImporterConfig_PlainlistEntry{
+			{
+				Url: "plainlistexampleurl",
+				OauthScopes: []string{
+					"example-oauth-scope",
+				},
+				Domain: "example.com",
+				Group:  "plain-group/example",
+			},
+		},
+		TarballUpload: []*configspb.GroupImporterConfig_TarballUploadEntry{
+			{
+				Name: "example-tarball.tz",
+				AuthorizedUploader: []string{
+					"test-service-account@example.com",
+				},
+				Domain: "example.com",
+				Systems: []string{
+					"groups",
+				},
+				Groups: []string{
+					"committers",
+				},
+			},
+		},
+	}
+	Convey("Testing basic config operations", t, func() {
+		So(SetConfig(ctx, importsCfg), ShouldBeNil)
+		cfgFromGet, err := Get(ctx)
+		So(err, ShouldBeNil)
+		So(cfgFromGet, ShouldResembleProto, importsCfg)
+	})
+}
