9f934ac29bc0f460cdd0a8603dbb6750f50f22c3
randymaldonado <randymaldonado@google.com>
2022-11-16 02:07:02 +0000

    [luciexe] allowing step tags to be added
    Bug:1353978
    Change-Id: Id8f7f49977d40507633128e61b439ea5edd46771
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/3998151
    Commit-Queue: Randy Maldonado <randymaldonado@google.com>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Auto-Submit: Randy Maldonado <randymaldonado@google.com>

--- luciexe/build/step_view.go
+++ luciexe/build/step_view.go
@@ -15,6 +15,10 @@
 package build
 
 import (
+	"sort"
+
+	pb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/data/sortby"
 	"go.chromium.org/luci/common/logging"
 )
 
@@ -23,6 +27,7 @@ import (
 // You can obtain/manipulate this with the Step.Modify method.
 type StepView struct {
 	SummaryMarkdown string
+	Tags            map[string][]string
 }
 
 // Start will change the status of this Step from SCHEDULED to STARTED and
@@ -47,20 +52,77 @@ func (s *Step) Start() {
 // This starts the step if it's still SCHEDULED.
 func (s *Step) Modify(cb func(*StepView)) {
 	logSM := ""
+	logTags := []*pb.StringPair{}
 	s.mutate(func() bool {
-		oldView := StepView{s.stepPb.SummaryMarkdown}
-		newView := oldView // shallow copy
-		cb(&newView)
-		if oldView.SummaryMarkdown != newView.SummaryMarkdown {
-			logSM = newView.SummaryMarkdown
-			s.stepPb.SummaryMarkdown = newView.SummaryMarkdown
+		// Creating a deep copy of tags
+		// note: we keep pb.Tags sorted at all times
+		tags := convertPbTagsToMap(s.stepPb.Tags)
+		view := StepView{s.stepPb.SummaryMarkdown, tags}
+		cb(&view)
+
+		// callback w/ tags
+		tagOut := convertMapTagsToPb(view.Tags)
+
+		isTagsMod := cmpPbTags(s.stepPb.Tags, view.Tags)
+		modified := false
+
+		if len(s.stepPb.SummaryMarkdown) != len(view.SummaryMarkdown) {
+			logSM = view.SummaryMarkdown
+			s.stepPb.SummaryMarkdown = view.SummaryMarkdown
+			modified = true
+		}
+		if isTagsMod {
+			// TODO: @randymaldonado change this to a diff between
+			// tagOut and s.stepPb.Tags.
+			logTags = tagOut
+			s.stepPb.Tags = tagOut
+			modified = true
+		}
+		return modified
+	})
+	if s.logsink() == nil {
+		if len(logSM) > 0 {
+			logging.Debugf(s.ctx, "changed SummaryMarkdown: %s", logSM)
+		}
+		if len(logTags) > 0 {
+			logging.Debugf(s.ctx, "changed Tags: %s", logTags)
+		}
+	}
+}
+
+func convertMapTagsToPb(tags map[string][]string) []*pb.StringPair {
+	tagOut := make([]*pb.StringPair, 0, len(tags))
+	for key, vals := range tags {
+		for _, val := range vals {
+			tagOut = append(tagOut, &pb.StringPair{Key: key, Value: val})
+		}
+	}
+	sort.Slice(tagOut, sortby.Chain{
+		func(i, j int) bool { return tagOut[i].Key < tagOut[j].Key },
+		func(i, j int) bool { return tagOut[i].Value < tagOut[j].Value },
+	}.Use)
+	return tagOut
+}
+
+func convertPbTagsToMap(tags []*pb.StringPair) map[string][]string {
+	tagsMap := make(map[string][]string, len(tags))
+	for _, pair := range tags {
+		tagsMap[pair.Key] = append(tagsMap[pair.Key], pair.Value)
+	}
+	return tagsMap
+}
+
+func cmpPbTags(oldTagsPb []*pb.StringPair, newTagsMap map[string][]string) bool {
+	tags := convertPbTagsToMap(oldTagsPb)
+	if len(tags) != len(newTagsMap) {
+		return true
+	}
+	for key, vals := range newTagsMap {
+		if len(vals) != len(tags[key]) {
 			return true
 		}
-		return false
-	})
-	if s.logsink() == nil && len(logSM) > 0 {
-		logging.Debugf(s.ctx, "changed SummaryMarkdown: %s", logSM)
 	}
+	return false
 }
 
 // SetSummaryMarkdown atomically sets the step's SummaryMarkdown field.
@@ -68,4 +130,19 @@ func (s *Step) SetSummaryMarkdown(summaryMarkdown string) {
 	s.Modify(func(v *StepView) {
 		v.SummaryMarkdown = summaryMarkdown
 	})
+}
+
+// AddTagValue sets the step's tag field by appending the new tag to the existing list.
+func (s *Step) AddTagValue(key, value string) {
+	if key != "" && value != "" {
+		s.Modify(func(v *StepView) {
+			vals := v.Tags[key]
+			for _, val := range vals {
+				if val == value {
+					return // already there
+				}
+			}
+			v.Tags[key] = append(vals, value)
+		})
+	}
 }
---
+++ luciexe/build/step_view_test.go
@@ -0,0 +1,119 @@
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package build
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	pb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/logging/memlogger"
+
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestStepTags(t *testing.T) {
+	ctx := memlogger.Use(context.Background())
+
+	Convey(`no tags added`, t, func() {
+		step := Step{
+			ctx:    ctx,
+			stepPb: &pb.Step{},
+		}
+		step.AddTagValue("", "")
+		actualTags := step.stepPb.Tags
+
+		expectedTags := []*pb.StringPair{}
+		So(actualTags, ShouldResembleProto, expectedTags)
+	})
+
+	Convey(`Add step tags`, t, func() {
+		step := Step{
+			ctx:    ctx,
+			stepPb: &pb.Step{},
+		}
+		step.AddTagValue("build.testing.key", "value")
+		actualTags := step.stepPb.Tags
+
+		expectedTags := []*pb.StringPair{}
+		expectedTags = append(expectedTags, &pb.StringPair{
+			Key:   "build.testing.key",
+			Value: "value",
+		})
+		So(actualTags, ShouldResembleProto, expectedTags)
+	})
+
+	Convey(`Add step tag with existing key`, t, func() {
+		// One key may have multiple values
+		tags := []*pb.StringPair{}
+		tags = append(tags, &pb.StringPair{
+			Key:   "build.testing.key",
+			Value: "a",
+		})
+		tags = append(tags, &pb.StringPair{
+			Key:   "build.testing.key",
+			Value: "g",
+		})
+		step := Step{
+			ctx: ctx,
+			stepPb: &pb.Step{
+				Tags: tags,
+			},
+		}
+		step.AddTagValue("build.testing.key", "d")
+		actualTags := step.stepPb.Tags
+
+		expectedTags := []*pb.StringPair{}
+		expectedTags = append(expectedTags, &pb.StringPair{
+			Key:   "build.testing.key",
+			Value: "a",
+		})
+		expectedTags = append(expectedTags, &pb.StringPair{
+			Key:   "build.testing.key",
+			Value: "d",
+		})
+		expectedTags = append(expectedTags, &pb.StringPair{
+			Key:   "build.testing.key",
+			Value: "g",
+		})
+		So(actualTags, ShouldResembleProto, expectedTags)
+	})
+}
+
+func TestSummaryMarkdown(t *testing.T) {
+	ctx := memlogger.Use(context.Background())
+
+	Convey(`no summary markdown; one is added`, t, func() {
+		step := Step{
+			ctx:    ctx,
+			stepPb: &pb.Step{},
+		}
+		step.SetSummaryMarkdown("test")
+		So(step.stepPb.SummaryMarkdown, ShouldEqual, "test")
+	})
+
+	Convey(`existing summary markdown; then modified`, t, func() {
+		step := Step{
+			ctx: ctx,
+			stepPb: &pb.Step{
+				SummaryMarkdown: "test",
+			},
+		}
+		step.SetSummaryMarkdown("some_really_cool_test_string")
+		So(step.stepPb.SummaryMarkdown, ShouldEqual, "some_really_cool_test_string")
+	})
+}
