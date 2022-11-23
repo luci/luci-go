14d6fbdad3a7a81427a8444e6b00f35011673cac
Scott Lee <ddoman@chromium.org>
2022-11-23 06:23:34 +0000

    [cv] update configs/prjcfg/loader_test.go to remove dep on refresh.go
    A follow-up CL will move refresh.go to refresher/ but it causes
    an import cycle, as loader_test.go uses functions in refresh.go.
    
    This CL replaces the use of UpdateProjectConfig() and
    DisableProjectConfig() with own mock functions.
    
    1. loader.go doesn't really use any of refresh.go functions.
    They simply fetch and return the stored entities.
    loader_test.go uses refresh functions for convenience.
    
    2. It's not necessary that the mocked refresh functions match
    the exact same behaviour of the original refresh functions.
    Due to the same reason described in (1). loader_test.go
    is not to test if the refresher functions update entities
    as expected.
    
    Bug: 1358208
    Change-Id: Idafb9544824638ca1a0cdf2fdfd87d6059a91b3f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4048785
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Commit-Queue: Scott Lee <ddoman@chromium.org>

--- cv/internal/configs/prjcfg/loader_test.go
+++ cv/internal/configs/prjcfg/loader_test.go
@@ -18,101 +18,134 @@ import (
 	"context"
 	"testing"
 
-	"google.golang.org/protobuf/encoding/prototext"
-
-	"go.chromium.org/luci/config"
-	"go.chromium.org/luci/config/cfgclient"
-	cfgmemory "go.chromium.org/luci/config/impl/memory"
-	gaememory "go.chromium.org/luci/gae/impl/memory"
 	"go.chromium.org/luci/gae/service/datastore"
 
 	cfgpb "go.chromium.org/luci/cv/api/config/v2"
+	"go.chromium.org/luci/cv/internal/cvtesting"
 
 	. "github.com/smartystreets/goconvey/convey"
 	. "go.chromium.org/luci/common/testing/assertions"
 )
 
+func mockUpdateProjectConfig(ctx context.Context, project string, cgs []*cfgpb.ConfigGroup) error {
+	const cfgHash = "sha256:deadbeef"
+	var cfgGroups []*ConfigGroup
+	var cgNames []string
+	for _, cg := range cgs {
+		cfgGroups = append(cfgGroups, &ConfigGroup{
+			ID:      MakeConfigGroupID(cfgHash, cg.Name),
+			Project: datastore.MakeKey(ctx, projectConfigKind, project),
+			Content: cg,
+		})
+		cgNames = append(cgNames, cg.Name)
+	}
+	pc := &ProjectConfig{
+		Project:          project,
+		Enabled:          true,
+		Hash:             cfgHash,
+		ExternalHash:     cfgHash + "external",
+		ConfigGroupNames: cgNames,
+		SchemaVersion:    schemaVersion,
+	}
+	cfgHashInfo := &ConfigHashInfo{
+		Hash:             cfgHash,
+		Project:          datastore.MakeKey(ctx, projectConfigKind, project),
+		ConfigGroupNames: cgNames,
+		GitRevision:      "abcdef123456",
+	}
+	return datastore.Put(ctx, pc, cfgHashInfo, cfgGroups)
+}
+
+func mockDisableProjectConfig(ctx context.Context, project string, cgs []*cfgpb.ConfigGroup) error {
+	const cfgHash = "sha256:deadbeef"
+	var cgNames []string
+	for _, cg := range cgs {
+		cgNames = append(cgNames, cg.Name)
+	}
+	pc := &ProjectConfig{
+		Project:          project,
+		Enabled:          false,
+		Hash:             cfgHash,
+		ExternalHash:     cfgHash + "external",
+		ConfigGroupNames: cgNames,
+		SchemaVersion:    schemaVersion,
+	}
+	return datastore.Put(ctx, pc)
+}
+
 func TestLoadingConfigs(t *testing.T) {
 	t.Parallel()
 	Convey("Load project config works", t, func() {
-		ctx := gaememory.Use(context.Background())
-		datastore.GetTestable(ctx).AutoIndex(true)
-		datastore.GetTestable(ctx).Consistent(true)
+		ct := cvtesting.Test{}
+		ctx, cancel := ct.SetUp()
+		defer cancel()
 
 		const project = "chromium"
-
-		Convey("Not existing project", func() {
-			m, err := GetLatestMeta(ctx, project)
-			So(err, ShouldBeNil)
-			So(m.Exists(), ShouldBeFalse)
-			So(m.EVersion, ShouldEqual, 0)
-			So(func() { m.Hash() }, ShouldPanic)
-		})
-
-		cfg := &cfgpb.Config{
-			ConfigGroups: []*cfgpb.ConfigGroup{
-				{
-					Name: "branch_m100",
-					Gerrit: []*cfgpb.ConfigGroup_Gerrit{
-						{
-							Url: "https://chromium-review.googlesource.com/",
-							Projects: []*cfgpb.ConfigGroup_Gerrit_Project{
-								{
-									Name:      "chromium/src",
-									RefRegexp: []string{"refs/heads/branch_m100"},
-								},
+		cfgGroups := []*cfgpb.ConfigGroup{
+			{
+				Name: "branch_m100",
+				Gerrit: []*cfgpb.ConfigGroup_Gerrit{
+					{
+						Url: "https://chromium-review.googlesource.com/",
+						Projects: []*cfgpb.ConfigGroup_Gerrit_Project{
+							{
+								Name:      "chromium/src",
+								RefRegexp: []string{"refs/heads/branch_m100"},
 							},
 						},
 					},
 				},
-				{
-					Fallback: cfgpb.Toggle_YES,
-					Name:     "catch_all",
-					Gerrit: []*cfgpb.ConfigGroup_Gerrit{
-						{
-							Url: "https://chromium-review.googlesource.com/",
-							Projects: []*cfgpb.ConfigGroup_Gerrit_Project{
-								{
-									Name:      "chromium/src",
-									RefRegexp: []string{"refs/heads/main"},
-								},
+			},
+			{
+				Name: "main",
+				Gerrit: []*cfgpb.ConfigGroup_Gerrit{
+					{
+						Url: "https://chromium-review.googlesource.com/",
+						Projects: []*cfgpb.ConfigGroup_Gerrit_Project{
+							{
+								Name:      "chromium/src",
+								RefRegexp: []string{"refs/heads/main"},
 							},
 						},
 					},
 				},
 			},
 		}
-		ctx = cfgclient.Use(ctx, cfgmemory.New(map[config.Set]cfgmemory.Files{
-			config.ProjectSet(project): {ConfigFileName: prototext.Format(cfg)},
-		}))
-		So(UpdateProject(ctx, project, func(context.Context) error { return nil }), ShouldBeNil)
+
+		So(mockUpdateProjectConfig(ctx, project, cfgGroups), ShouldBeNil)
+
+		Convey("Not existing project", func() {
+			m, err := GetLatestMeta(ctx, "not_chromium")
+			So(err, ShouldBeNil)
+			So(m.Exists(), ShouldBeFalse)
+			So(func() { m.Hash() }, ShouldPanic)
+		})
 
 		Convey("Enabled project", func() {
 			m, err := GetLatestMeta(ctx, project)
 			So(err, ShouldBeNil)
 			So(m.Exists(), ShouldBeTrue)
 			So(m.Status, ShouldEqual, StatusEnabled)
-			So(m.EVersion, ShouldEqual, 1)
-			So(m.ConfigGroupNames, ShouldResemble, []string{"branch_m100", "catch_all"})
-			h := m.Hash()
-			So(h, ShouldStartWith, "sha256:")
+			So(m.ConfigGroupNames, ShouldResemble, []string{cfgGroups[0].Name, cfgGroups[1].Name})
 			So(m.ConfigGroupIDs, ShouldResemble, []ConfigGroupID{
-				ConfigGroupID(h + "/branch_m100"),
-				ConfigGroupID(h + "/catch_all"),
+				ConfigGroupID(m.Hash() + "/" + cfgGroups[0].Name),
+				ConfigGroupID(m.Hash() + "/" + cfgGroups[1].Name),
 			})
 
-			m2, err := GetHashMeta(ctx, project, h)
+			m2, err := GetHashMeta(ctx, project, m.Hash())
 			So(err, ShouldBeNil)
 			So(m2, ShouldResemble, m)
 
 			cgs, err := m.GetConfigGroups(ctx)
 			So(err, ShouldBeNil)
 			So(len(cgs), ShouldEqual, 2)
-			So(cgs[0].Content, ShouldResembleProto, cfg.ConfigGroups[0])
-			So(cgs[1].Content, ShouldResembleProto, cfg.ConfigGroups[1])
+			So(cgs[0].Project.StringID(), ShouldEqual, project)
+			So(cgs[0].Content, ShouldResembleProto, cfgGroups[0])
+			So(cgs[1].Project.StringID(), ShouldEqual, project)
+			So(cgs[1].Content, ShouldResembleProto, cfgGroups[1])
 		})
 
-		cfg.ConfigGroups = append(cfg.ConfigGroups, &cfgpb.ConfigGroup{
+		cfgGroups = append(cfgGroups, &cfgpb.ConfigGroup{
 			Name: "branch_m200",
 			Gerrit: []*cfgpb.ConfigGroup_Gerrit{
 				{
@@ -126,24 +159,19 @@ func TestLoadingConfigs(t *testing.T) {
 				},
 			},
 		})
-
-		ctx = cfgclient.Use(ctx, cfgmemory.New(map[config.Set]cfgmemory.Files{
-			config.ProjectSet(project): {ConfigFileName: prototext.Format(cfg)},
-		}))
-		So(UpdateProject(ctx, project, func(context.Context) error { return nil }), ShouldBeNil)
+		So(mockUpdateProjectConfig(ctx, project, cfgGroups), ShouldBeNil)
 
 		Convey("Updated project", func() {
 			m, err := GetLatestMeta(ctx, project)
 			So(err, ShouldBeNil)
 			So(m.Exists(), ShouldBeTrue)
 			So(m.Status, ShouldEqual, StatusEnabled)
-			So(m.EVersion, ShouldEqual, 2)
 			h := m.Hash()
 			So(h, ShouldStartWith, "sha256:")
 			So(m.ConfigGroupIDs, ShouldResemble, []ConfigGroupID{
-				ConfigGroupID(h + "/branch_m100"),
-				ConfigGroupID(h + "/catch_all"),
-				ConfigGroupID(h + "/branch_m200"),
+				ConfigGroupID(h + "/" + cfgGroups[0].Name),
+				ConfigGroupID(h + "/" + cfgGroups[1].Name),
+				ConfigGroupID(h + "/" + cfgGroups[2].Name),
 			})
 			cgs, err := m.GetConfigGroups(ctx)
 			So(err, ShouldBeNil)
@@ -152,17 +180,17 @@ func TestLoadingConfigs(t *testing.T) {
 			Convey("reading ConfigGroup directly works", func() {
 				cg, err := GetConfigGroup(ctx, project, m.ConfigGroupIDs[2])
 				So(err, ShouldBeNil)
-				So(cg.Content, ShouldResembleProto, cfg.ConfigGroups[2])
+				So(cg.Content, ShouldResembleProto, cfgGroups[2])
 			})
 		})
 
-		So(DisableProject(ctx, project, func(context.Context) error { return nil }), ShouldBeNil)
+		So(mockDisableProjectConfig(ctx, project, cfgGroups), ShouldBeNil)
+
 		Convey("Disabled project", func() {
 			m, err := GetLatestMeta(ctx, project)
 			So(err, ShouldBeNil)
 			So(m.Exists(), ShouldBeTrue)
 			So(m.Status, ShouldEqual, StatusDisabled)
-			So(m.EVersion, ShouldEqual, 3)
 			So(len(m.ConfigGroupIDs), ShouldEqual, 3)
 			cgs, err := m.GetConfigGroups(ctx)
 			So(err, ShouldBeNil)
@@ -170,7 +198,8 @@ func TestLoadingConfigs(t *testing.T) {
 		})
 
 		// Re-enable the project.
-		So(UpdateProject(ctx, project, func(context.Context) error { return nil }), ShouldBeNil)
+		So(mockUpdateProjectConfig(ctx, project, cfgGroups), ShouldBeNil)
+
 		Convey("Re-enabled project", func() {
 			m, err := GetLatestMeta(ctx, project)
 			So(err, ShouldBeNil)
@@ -201,10 +230,10 @@ func TestLoadingConfigs(t *testing.T) {
 			// Can still read individual ConfigGroups.
 			cg, err := GetConfigGroup(ctx, project, m.ConfigGroupIDs[0])
 			So(err, ShouldBeNil)
-			So(cg.Content, ShouldResembleProto, cfg.ConfigGroups[0])
+			So(cg.Content, ShouldResembleProto, cfgGroups[0])
 			cg, err = GetConfigGroup(ctx, project, m.ConfigGroupIDs[2])
 			So(err, ShouldBeNil)
-			So(cg.Content, ShouldResembleProto, cfg.ConfigGroups[2])
+			So(cg.Content, ShouldResembleProto, cfgGroups[2])
 			// ... except the deleted one.
 			_, err = GetConfigGroup(ctx, project, m.ConfigGroupIDs[1])
 			So(datastore.IsErrNoSuchEntity(err), ShouldBeTrue)
