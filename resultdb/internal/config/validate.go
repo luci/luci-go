5100eb64a39f0e9d994d6e7285bde89bf9d9a1c2
Jonathan Ramirez <jlcruz@google.com>
2022-12-01 18:44:29 +0000

    [ResultDB] Add project-config validation
    Also add project-config template
    
    Bug: b/229888940
    Change-Id: Ic4491d9f2e1f8693a0bfb38bd2899f918f7766a5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4053354
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Commit-Queue: Jonathan Ramirez <jlcruz@google.com>
    Reviewed-by: Jason Kusuma <jkusuma@google.com>

---
+++ resultdb/internal/config/validate.go
@@ -0,0 +1,90 @@
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"go.chromium.org/luci/config/validation"
+	"go.chromium.org/luci/server/auth/realms"
+
+	configpb "go.chromium.org/luci/resultdb/proto/config"
+)
+
+var GCSBucketRE = regexp.MustCompile(`^[a-z0-9_\.\-]{3,222}$`)
+
+func validateStringConfig(ctx *validation.Context, name, cfg string, re *regexp.Regexp) {
+	ctx.Enter(name)
+	defer ctx.Exit()
+	if cfg == "" {
+		ctx.Errorf("empty %s is not allowed", name)
+		return
+	}
+	if !re.MatchString(cfg) {
+		ctx.Errorf("invalid %s: %q", name, cfg)
+	}
+}
+
+// Validates according to https://cloud.google.com/storage/docs/objects#naming
+func validateGCSBucketPrefix(ctx *validation.Context, name string, prefix string) {
+	ctx.Enter(name)
+	defer ctx.Exit()
+
+	prefixLen := len(prefix)
+	if prefixLen < 1 || prefixLen > 1024 {
+		ctx.Errorf("prefix: %q should have length between 1 and 1024 bytes", prefix)
+	}
+	if strings.HasPrefix(prefix, ".well-known/acme-challenge/") {
+		ctx.Errorf("prefix: %q is not allowed", prefix)
+	}
+	if prefix == "." || prefix == ".." {
+		ctx.Errorf("prefix: %q is not allowed, use '*' as wildcard to allow full access", prefix)
+	}
+	notAllowedChars, _ := strconv.Unquote(`"\u000a\u000b\u000c\u000d\u0085\u2028\u2029"`)
+	if strings.ContainsAny(prefix, notAllowedChars) {
+		ctx.Errorf("prefix: %q contains carriage return or line feed characters, which is not allowed", prefix)
+	}
+}
+
+func validateRealmGCSAllowlist(ctx *validation.Context, name string, allowList *configpb.RealmGcsAllowList) {
+	ctx.Enter(name)
+	defer ctx.Exit()
+
+	err := realms.ValidateRealmName(allowList.Realm, realms.ProjectScope)
+	if err != nil {
+		ctx.Errorf(err.Error())
+	}
+	for _, prefixes := range allowList.GcsBucketPrefixes {
+		validateStringConfig(ctx, "bucket", prefixes.Bucket, GCSBucketRE)
+		for i, prefix := range prefixes.AllowedPrefixes {
+			validateGCSBucketPrefix(ctx, fmt.Sprintf("allowed_prefixes[%d]", i), prefix)
+		}
+	}
+}
+
+func validateProjectConfig(ctx *validation.Context, cfg *configpb.ProjectConfig) {
+	realms := make(map[string]bool, len(cfg.RealmGcsAllowlist))
+	for i, allowList := range cfg.RealmGcsAllowlist {
+		realm := allowList.Realm
+		if _, ok := realms[realm]; ok {
+			ctx.Errorf("realm: %s is configured more than once", realm)
+		}
+		realms[realm] = true
+		validateRealmGCSAllowlist(ctx, fmt.Sprintf("realm_gcs_allowlist[%d]", i), allowList)
+	}
+}
---
+++ resultdb/internal/config/validate_test.go
@@ -0,0 +1,137 @@
+// Copyright 2022 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"go.chromium.org/luci/config/validation"
+	"google.golang.org/protobuf/encoding/prototext"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+
+	configpb "go.chromium.org/luci/resultdb/proto/config"
+)
+
+func TestProjectConfigValidator(t *testing.T) {
+	t.Parallel()
+
+	validate := func(cfg *configpb.ProjectConfig) error {
+		c := validation.Context{Context: context.Background()}
+		validateProjectConfig(&c, cfg)
+		return c.Finalize()
+	}
+
+	Convey("config template is valid", t, func() {
+		content, err := os.ReadFile(
+			"../../configs/projects/chromeos/luci-resultdb-dev-template.cfg",
+		)
+		So(err, ShouldBeNil)
+		cfg := &configpb.ProjectConfig{}
+		So(prototext.Unmarshal(content, cfg), ShouldBeNil)
+		So(validate(cfg), ShouldBeNil)
+	})
+
+	Convey("valid config is valid", t, func() {
+		cfg := createPlaceholderProjectConfig()
+		So(validate(cfg), ShouldBeNil)
+	})
+
+	Convey("invalid config", t, func() {
+		cfg := createPlaceholderProjectConfig()
+		Convey("realm is specified more than once", func() {
+			cfg.RealmGcsAllowlist = append(cfg.RealmGcsAllowlist,
+				&configpb.RealmGcsAllowList{
+					Realm: "a",
+				},
+				&configpb.RealmGcsAllowList{
+					Realm: "a",
+				})
+			So(validate(cfg), ShouldErrLike, "realm: a is configured more than once")
+		})
+	})
+
+	Convey("realm GCS allow list", t, func() {
+		cfg := createPlaceholderProjectConfig()
+		So(cfg.RealmGcsAllowlist, ShouldNotBeNil)
+		So(len(cfg.RealmGcsAllowlist), ShouldEqual, 1)
+		So(len(cfg.RealmGcsAllowlist[0].GcsBucketPrefixes), ShouldEqual, 1)
+		So(len(cfg.RealmGcsAllowlist[0].GcsBucketPrefixes[0].AllowedPrefixes), ShouldEqual, 1)
+		realmGCSAllowlist := cfg.RealmGcsAllowlist[0]
+
+		Convey("realm", func() {
+			Convey("must be specified", func() {
+				realmGCSAllowlist.Realm = ""
+				So(validate(cfg), ShouldNotBeNil)
+			})
+			Convey("invalid", func() {
+				realmGCSAllowlist.Realm = "a:b"
+				So(validate(cfg), ShouldNotBeNil)
+			})
+			Convey("valid", func() {
+				realmGCSAllowlist.Realm = "a"
+				So(validate(cfg), ShouldBeNil)
+			})
+		})
+
+		Convey("GCS bucket prefixes", func() {
+			bucketPrefix := realmGCSAllowlist.GcsBucketPrefixes[0]
+
+			Convey("bucket", func() {
+				Convey("must be specified", func() {
+					bucketPrefix.Bucket = ""
+					So(validate(cfg), ShouldErrLike, "empty bucket is not allowed")
+				})
+				Convey("invalid", func() {
+					bucketPrefix.Bucket = "b"
+					So(validate(cfg), ShouldErrLike, `invalid bucket: "b"`)
+				})
+				Convey("valid", func() {
+					bucketPrefix.Bucket = "bucket"
+					So(validate(cfg), ShouldBeNil)
+				})
+			})
+			Convey("allowed prefixes", func() {
+				Convey("invalid len", func() {
+					bucketPrefix.AllowedPrefixes[0] = ""
+					So(validate(cfg), ShouldErrLike, "prefix: \"\" should have length between 1 and 1024")
+				})
+				Convey("invalid prefix", func() {
+					bucketPrefix.AllowedPrefixes[0] = ".well-known/acme-challenge/a/b/c"
+					So(validate(cfg), ShouldErrLike, "prefix: \".well-known/acme-challenge/a/b/c\" is not allowed")
+				})
+				Convey("invalid value", func() {
+					bucketPrefix.AllowedPrefixes[0] = "."
+					So(validate(cfg), ShouldErrLike, "prefix: \".\" is not allowed, use '*' as wildcard to allow full access")
+				})
+				Convey("invalid char", func() {
+					bucketPrefix.AllowedPrefixes[0] = "\n"
+					So(validate(cfg), ShouldErrLike, "prefix: \"\\n\" contains carriage return or line feed characters, which is not allowed")
+				})
+				Convey("valid", func() {
+					bucketPrefix.AllowedPrefixes[0] = "a/b/c"
+					So(validate(cfg), ShouldBeNil)
+				})
+				Convey("valid wildcard", func() {
+					bucketPrefix.AllowedPrefixes[0] = "*"
+					So(validate(cfg), ShouldBeNil)
+				})
+			})
+		})
+	})
+}
