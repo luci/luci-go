42e9d3461f0a34e4ab56deaea74b1b3298314cec
Michael Anthony Knyszek <mknyszek@google.com>
2023-01-20 19:41:57 +0000

    [luciexe] Add support for LUCIEXE_FAKEBUILD
    This change adds support for LUCIEXE_FAKEBUILD to luciexe binaries. It
    accepts a path to a jsonpb Build proto and sets up the luciexe binary
    according to the LUCI User Code Contract.
    
    It also adds the --working-dir flag to luciexes to specify which
    directory to run out of; this is useful for the fake build case because
    it lets us clean up the temporary directory it runs in by default.
    
    The purpose of this feature is to make it easier to run luciexe binaries
    locally.
    
    Change-Id: I4bd12093c0087ceb1ad19a44d20cb9214dfb4e36
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4098465
    Commit-Queue: Michael Knyszek <mknyszek@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

--- luciexe/build/main.go
+++ luciexe/build/main.go
@@ -18,9 +18,12 @@ import (
 	"bytes"
 	"compress/zlib"
 	"context"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"runtime/debug"
 	"time"
 
@@ -34,6 +37,7 @@ import (
 	"go.chromium.org/luci/common/system/environ"
 	"go.chromium.org/luci/logdog/client/butlerlib/bootstrap"
 	"go.chromium.org/luci/logdog/client/butlerlib/streamclient"
+	"go.chromium.org/luci/lucictx"
 	"go.chromium.org/luci/luciexe"
 )
 
@@ -42,13 +46,13 @@ var errNonSuccess = errors.New("build state != SUCCESS")
 // Main implements all the 'command-line' behaviors of the luciexe 'exe'
 // protocol, including:
 //
-//   * parsing command line for "--output", "--help", etc.
-//   * parsing stdin (as appropriate) for the incoming Build message
-//   * creating and configuring a logdog client to send State evolutions.
-//   * Configuring a logdog client from the environment.
-//   * Writing Build state updates to the logdog "build.proto" stream.
-//   * Start'ing the build in this process.
-//   * End'ing the build with the returned error from your function
+//   - parsing command line for "--output", "--help", etc.
+//   - parsing stdin (as appropriate) for the incoming Build message
+//   - creating and configuring a logdog client to send State evolutions.
+//   - Configuring a logdog client from the environment.
+//   - Writing Build state updates to the logdog "build.proto" stream.
+//   - Start'ing the build in this process.
+//   - End'ing the build with the returned error from your function
 //
 // If `inputMsg` is nil, the top-level properties will be ignored.
 //
@@ -56,28 +60,37 @@ var errNonSuccess = errors.New("build state != SUCCESS")
 // they work as they would for MakePropertyModifier.
 //
 // CLI Arguments parsed:
-//   * -h / --help : Print help for this binary (including input/output
+//   - -h / --help : Print help for this binary (including input/output
 //     property type info)
-//   * --strict-input : Enable strict property parsing (see OptStrictInputProperties)
-//   * --output : luciexe "output" flag; See
+//   - --strict-input : Enable strict property parsing (see OptStrictInputProperties)
+//   - --output : luciexe "output" flag; See
 //     https://pkg.go.dev/go.chromium.org/luci/luciexe#hdr-Recursive_Invocation
-//   * -- : Any extra arguments after a "--" token are passed to your callback
+//   - --working-dir : The working directory to run from; Default is $PWD unless
+//     LUCIEXE_FAKEBUILD is set, in which case a temp dir is used and cleaned up after.
+//   - -- : Any extra arguments after a "--" token are passed to your callback
 //     as-is.
 //
 // Example:
 //
-//    func main() {
-//      input := *MyInputProps{}
-//      var writeOutputProps func(*MyOutputProps)
-//      var mergeOutputProps func(*MyOutputProps)
+//	func main() {
+//	  input := *MyInputProps{}
+//	  var writeOutputProps func(*MyOutputProps)
+//	  var mergeOutputProps func(*MyOutputProps)
 //
-//      Main(input, &writeOutputProps, &mergeOutputProps, func(ctx context.Context, args []string, st *build.State) error {
-//        // actual build code here, build is already Start'd
-//        // input was parsed from build.Input.Properties
-//        writeOutputProps(&MyOutputProps{...})
-//        return nil // will mark the Build as SUCCESS
-//      })
-//    }
+//	  Main(input, &writeOutputProps, &mergeOutputProps, func(ctx context.Context, args []string, st *build.State) error {
+//	    // actual build code here, build is already Start'd
+//	    // input was parsed from build.Input.Properties
+//	    writeOutputProps(&MyOutputProps{...})
+//	    return nil // will mark the Build as SUCCESS
+//	  })
+//	}
+//
+// Main also supports running a luciexe build locally by specifying the
+// LUCIEXE_FAKEBUILD environment variable. The value of the variable should
+// be a path to a file containing a JSON-encoded Build proto.
+//
+// TODO(iannucci): LUCIEXE_FAKEBUILD does not support nested invocations.
+// It should set up bbagent and butler in order to aggregate logs.
 //
 // NOTE: These types are pretty bad; There's significant opportunity to improve
 // them with Go2 generics.
@@ -96,7 +109,7 @@ func Main(inputMsg proto.Message, writeFnptr, mergeFnptr interface{}, cb func(co
 	}
 }
 
-func main(ctx context.Context, args []string, stdin io.Reader, inputMsg proto.Message, writeFnptr, mergeFnptr interface{}, cb func(context.Context, []string, *State) error) error {
+func main(ctx context.Context, args []string, stdin io.Reader, inputMsg proto.Message, writeFnptr, mergeFnptr interface{}, cb func(context.Context, []string, *State) error) (err error) {
 	args = append([]string(nil), args...)
 	var userArgs []string
 	for i, a := range args {
@@ -114,9 +127,14 @@ func main(ctx context.Context, args []string, stdin io.Reader, inputMsg proto.Me
 		opts = append(opts, OptOutputProperties(writeFnptr, mergeFnptr))
 	}
 
-	outputFile, strict, help := parseArgs(args)
+	outputFile, wd, strict, help := parseArgs(args)
 	if strict {
 		opts = append(opts, OptStrictInputProperties())
+	}
+	if wd != "" {
+		if err := os.Chdir(wd); err != nil {
+			return err
+		}
 	}
 
 	var initial *bbpb.Build
@@ -130,15 +148,21 @@ func main(ctx context.Context, args []string, stdin io.Reader, inputMsg proto.Me
 		}
 	}()
 
-	var err error
-
 	if !help {
-		var moreOpts []StartOption
-		initial, moreOpts, err = prepOptsFromLuciexeEnv(ctx, stdin, &lastBuild)
-		if err != nil {
-			return err
+		if path := environ.FromCtx(ctx).Get(luciexeFakeVar); path != "" {
+			var cleanup func(*error)
+			ctx, initial, cleanup, err = prepFromFakeLuciexeEnv(ctx, wd, path)
+			if cleanup != nil {
+				defer cleanup(&err)
+			}
+		} else {
+			var moreOpts []StartOption
+			initial, moreOpts, err = prepOptsFromLuciexeEnv(ctx, stdin, &lastBuild)
+			if err != nil {
+				return err
+			}
+			opts = append(opts, moreOpts...)
 		}
-		opts = append(opts, moreOpts...)
 	}
 
 	state, ictx, err := Start(ctx, initial, opts...)
@@ -165,6 +189,68 @@ func main(ctx context.Context, args []string, stdin io.Reader, inputMsg proto.Me
 
 // overridden in tests
 var mainSendRate = rate.Every(time.Second)
+
+const luciexeFakeVar = "LUCIEXE_FAKEBUILD"
+
+func prepFromFakeLuciexeEnv(ctx context.Context, wd, buildPath string) (newCtx context.Context, initial *bbpb.Build, cleanup func(*error), err error) {
+	// This is a fake build.
+	logging.Infof(ctx, "Running fake build because %s is set. Build proto path: %s", luciexeFakeVar, buildPath)
+
+	// Pull the Build proto from the path in the environment variable and
+	// set up the environment according to the LUCI User Code Contract.
+	newCtx = ctx
+	env := environ.FromCtx(ctx)
+
+	// Defensively clear the environment variable so it doesn't propagate to subtasks.
+	env.Remove(luciexeFakeVar)
+
+	// Read the Build proto.
+	initial, err = readLuciexeFakeBuild(buildPath)
+	if err != nil {
+		return
+	}
+
+	if wd == "" {
+		// Create working directory heirarchy in a new temporary directory.
+		wd, err = os.MkdirTemp("", fmt.Sprintf("luciexe-fakebuild-%s-", filepath.Base(os.Args[0])))
+		if err != nil {
+			return
+		}
+		err = os.Chdir(wd)
+		if err != nil {
+			return
+		}
+		cleanup = func(err *error) {
+			logging.Infof(ctx, "Cleaning up working directory: %s", wd)
+			if r := os.RemoveAll(wd); r != nil && *err == nil {
+				*err = r
+			} else if r != nil {
+				logging.Errorf(ctx, "Failed to clean up working directory: %s", wd)
+			}
+		}
+	}
+	logging.Infof(ctx, "Working directory: %s", wd)
+
+	tmpDir := filepath.Join(wd, "tmp")
+	if err = os.MkdirAll(tmpDir, os.ModePerm); err != nil {
+		return
+	}
+	cacheDir := filepath.Join(wd, "cache")
+	if err = os.MkdirAll(cacheDir, os.ModePerm); err != nil {
+		return
+	}
+
+	// Set up environment and LUCI_CONTEXT.
+	for _, key := range luciexe.TempDirEnvVars {
+		env.Set(key, tmpDir)
+	}
+	newCtx = env.SetInCtx(newCtx)
+	newCtx = lucictx.SetLUCIExe(newCtx, &lucictx.LUCIExe{
+		CacheDir: cacheDir,
+	})
+
+	return
+}
 
 func prepOptsFromLuciexeEnv(ctx context.Context, stdin io.Reader, lastBuild **bbpb.Build) (initial *bbpb.Build, opts []StartOption, err error) {
 	initial, err = readStdinBuild(stdin)
@@ -237,9 +323,21 @@ func readStdinBuild(stdin io.Reader) (*bbpb.Build, error) {
 	return ret, err
 }
 
-func parseArgs(args []string) (output string, strict bool, help bool) {
+func readLuciexeFakeBuild(filename string) (*bbpb.Build, error) {
+	// N.B. This proto is JSON-encoded.
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, errors.Annotate(err, "reading *Build from %s=%s", luciexeFakeVar, filename).Err()
+	}
+	ret := &bbpb.Build{}
+	err = json.Unmarshal(data, ret)
+	return ret, errors.Annotate(err, "decoding *Build from %s=%s", luciexeFakeVar, filename).Err()
+}
+
+func parseArgs(args []string) (output, wd string, strict, help bool) {
 	fs := flag.FlagSet{}
 	fs.BoolVar(&strict, "strict-input", false, "Strict input parsing; Input properties supplied which aren't understood by this program will print an error and quit.")
+	fs.StringVar(&wd, "working-dir", "", "The working directory to run from; Default is $PWD unless LUCIEXE_FAKEBUILD is set, in which case a temp dir is used and cleaned up after.")
 	fs.StringVar(&output, "output", "", "Output final Build message to this path. Must end with {.json,.pb,.textpb}")
 	fs.BoolVar(&help, "help", false, "Print help for this executable")
 	fs.BoolVar(&help, "h", false, "Print help for this executable")
