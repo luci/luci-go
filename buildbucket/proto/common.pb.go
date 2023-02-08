c670fb67ab469ece93b2cc52fb23c821ca788839
randymaldonado <randymaldonado@google.com>
2023-02-08 21:40:31 +0000

    [buildbucket] moving CacheEntry to common proto
    Change-Id: Ib9e2f54aca8eac81aa27e5d411063ed8376c018b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4228097
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Randy Maldonado <randymaldonado@google.com>
    Reviewed-by: Yuanjun Huang <yuanjunh@google.com>

--- buildbucket/proto/common.pb.go
+++ buildbucket/proto/common.pb.go
@@ -850,6 +850,156 @@ func (x *RequestedDimension) GetExpiration() *durationpb.Duration {
 	return nil
 }
 
+// This message is a duplicate of Build.Infra.Swarming.CacheEntry,
+// however we will be moving from hardcoded swarming -> task backends.
+// This message will remain as the desired CacheEntry and eventually
+// Build.Infra.Swarming will be deperecated, so this will remain.
+//
+// Describes a cache directory persisted on a bot.
+//
+// If a build requested a cache, the cache directory is available on build
+// startup. If the cache was present on the bot, the directory contains
+// files from the previous run on that bot.
+// The build can read/write to the cache directory while it runs.
+// After build completes, the cache directory is persisted.
+// The next time another build requests the same cache and runs on the same
+// bot, the files will still be there (unless the cache was evicted,
+// perhaps due to disk space reasons).
+//
+// One bot can keep multiple caches at the same time and one build can request
+// multiple different caches.
+// A cache is identified by its name and mapped to a path.
+//
+// If the bot is running out of space, caches are evicted in LRU manner
+// before the next build on this bot starts.
+//
+// Buildbucket implicitly declares cache
+//
+//	{"name": "<hash(project/bucket/builder)>", "path": "builder"}.
+//
+// This means that any LUCI builder has a "personal disk space" on the bot.
+// Builder cache is often a good start before customizing caching.
+// In recipes, it is available at api.buildbucket.builder_cache_path.
+//
+// To share a builder cache among multiple builders, it can be overridden:
+//
+//	builders {
+//	  name: "a"
+//	  caches {
+//	    path: "builder"
+//	    name: "my_shared_cache"
+//	  }
+//	}
+//	builders {
+//	  name: "b"
+//	  caches {
+//	    path: "builder"
+//	    name: "my_shared_cache"
+//	  }
+//	}
+//
+// Builders "a" and "b" share their builder cache. If an "a" build ran on a
+// bot and left some files in the builder cache and then a "b" build runs on
+// the same bot, the same files will be available in the builder cache.
+type CacheEntry struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Identifier of the cache. Required. Length is limited to 128.
+	// Must be unique in the build.
+	//
+	// If the pool of swarming bots is shared among multiple LUCI projects and
+	// projects use same cache name, the cache will be shared across projects.
+	// To avoid affecting and being affected by other projects, prefix the
+	// cache name with something project-specific, e.g. "v8-".
+	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	// Relative path where the cache in mapped into. Required.
+	//
+	// Must use POSIX format (forward slashes).
+	// In most cases, it does not need slashes at all.
+	//
+	// In recipes, use api.path['cache'].join(path) to get absolute path.
+	//
+	// Must be unique in the build.
+	Path string `protobuf:"bytes,2,opt,name=path,proto3" json:"path,omitempty"`
+	// Duration to wait for a bot with a warm cache to pick up the
+	// task, before falling back to a bot with a cold (non-existent) cache.
+	//
+	// The default is 0, which means that no preference will be chosen for a
+	// bot with this or without this cache, and a bot without this cache may
+	// be chosen instead.
+	//
+	// If no bot has this cache warm, the task will skip this wait and will
+	// immediately fallback to a cold cache request.
+	//
+	// The value must be multiples of 60 seconds.
+	WaitForWarmCache *durationpb.Duration `protobuf:"bytes,3,opt,name=wait_for_warm_cache,json=waitForWarmCache,proto3" json:"wait_for_warm_cache,omitempty"`
+	// Environment variable with this name will be set to the path to the cache
+	// directory.
+	EnvVar string `protobuf:"bytes,4,opt,name=env_var,json=envVar,proto3" json:"env_var,omitempty"`
+}
+
+func (x *CacheEntry) Reset() {
+	*x = CacheEntry{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[8]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *CacheEntry) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*CacheEntry) ProtoMessage() {}
+
+func (x *CacheEntry) ProtoReflect() protoreflect.Message {
+	mi := &file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[8]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use CacheEntry.ProtoReflect.Descriptor instead.
+func (*CacheEntry) Descriptor() ([]byte, []int) {
+	return file_go_chromium_org_luci_buildbucket_proto_common_proto_rawDescGZIP(), []int{8}
+}
+
+func (x *CacheEntry) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *CacheEntry) GetPath() string {
+	if x != nil {
+		return x.Path
+	}
+	return ""
+}
+
+func (x *CacheEntry) GetWaitForWarmCache() *durationpb.Duration {
+	if x != nil {
+		return x.WaitForWarmCache
+	}
+	return nil
+}
+
+func (x *CacheEntry) GetEnvVar() string {
+	if x != nil {
+		return x.EnvVar
+	}
+	return ""
+}
+
 type StatusDetails_ResourceExhaustion struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -859,7 +1009,7 @@ type StatusDetails_ResourceExhaustion struct {
 func (x *StatusDetails_ResourceExhaustion) Reset() {
 	*x = StatusDetails_ResourceExhaustion{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[8]
+		mi := &file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[9]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -872,7 +1022,7 @@ func (x *StatusDetails_ResourceExhaustion) String() string {
 func (*StatusDetails_ResourceExhaustion) ProtoMessage() {}
 
 func (x *StatusDetails_ResourceExhaustion) ProtoReflect() protoreflect.Message {
-	mi := &file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[8]
+	mi := &file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[9]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -897,7 +1047,7 @@ type StatusDetails_Timeout struct {
 func (x *StatusDetails_Timeout) Reset() {
 	*x = StatusDetails_Timeout{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[9]
+		mi := &file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[10]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -910,7 +1060,7 @@ func (x *StatusDetails_Timeout) String() string {
 func (*StatusDetails_Timeout) ProtoMessage() {}
 
 func (x *StatusDetails_Timeout) ProtoReflect() protoreflect.Message {
-	mi := &file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[9]
+	mi := &file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[10]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -997,25 +1147,34 @@ var file_go_chromium_org_luci_buildbucket_proto_common_proto_rawDesc = []byte{
 	0x6c, 0x75, 0x65, 0x12, 0x39, 0x0a, 0x0a, 0x65, 0x78, 0x70, 0x69, 0x72, 0x61, 0x74, 0x69, 0x6f,
 	0x6e, 0x18, 0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x19, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65,
 	0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x44, 0x75, 0x72, 0x61, 0x74, 0x69,
-	0x6f, 0x6e, 0x52, 0x0a, 0x65, 0x78, 0x70, 0x69, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x2a, 0x87,
-	0x01, 0x0a, 0x06, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x16, 0x0a, 0x12, 0x53, 0x54, 0x41,
-	0x54, 0x55, 0x53, 0x5f, 0x55, 0x4e, 0x53, 0x50, 0x45, 0x43, 0x49, 0x46, 0x49, 0x45, 0x44, 0x10,
-	0x00, 0x12, 0x0d, 0x0a, 0x09, 0x53, 0x43, 0x48, 0x45, 0x44, 0x55, 0x4c, 0x45, 0x44, 0x10, 0x01,
-	0x12, 0x0b, 0x0a, 0x07, 0x53, 0x54, 0x41, 0x52, 0x54, 0x45, 0x44, 0x10, 0x02, 0x12, 0x0e, 0x0a,
-	0x0a, 0x45, 0x4e, 0x44, 0x45, 0x44, 0x5f, 0x4d, 0x41, 0x53, 0x4b, 0x10, 0x04, 0x12, 0x0b, 0x0a,
-	0x07, 0x53, 0x55, 0x43, 0x43, 0x45, 0x53, 0x53, 0x10, 0x0c, 0x12, 0x0b, 0x0a, 0x07, 0x46, 0x41,
-	0x49, 0x4c, 0x55, 0x52, 0x45, 0x10, 0x14, 0x12, 0x11, 0x0a, 0x0d, 0x49, 0x4e, 0x46, 0x52, 0x41,
-	0x5f, 0x46, 0x41, 0x49, 0x4c, 0x55, 0x52, 0x45, 0x10, 0x24, 0x12, 0x0c, 0x0a, 0x08, 0x43, 0x41,
-	0x4e, 0x43, 0x45, 0x4c, 0x45, 0x44, 0x10, 0x44, 0x2a, 0x25, 0x0a, 0x07, 0x54, 0x72, 0x69, 0x6e,
-	0x61, 0x72, 0x79, 0x12, 0x09, 0x0a, 0x05, 0x55, 0x4e, 0x53, 0x45, 0x54, 0x10, 0x00, 0x12, 0x07,
-	0x0a, 0x03, 0x59, 0x45, 0x53, 0x10, 0x01, 0x12, 0x06, 0x0a, 0x02, 0x4e, 0x4f, 0x10, 0x02, 0x2a,
-	0x21, 0x0a, 0x0b, 0x43, 0x6f, 0x6d, 0x70, 0x72, 0x65, 0x73, 0x73, 0x69, 0x6f, 0x6e, 0x12, 0x08,
-	0x0a, 0x04, 0x5a, 0x4c, 0x49, 0x42, 0x10, 0x00, 0x12, 0x08, 0x0a, 0x04, 0x5a, 0x53, 0x54, 0x44,
-	0x10, 0x01, 0x42, 0x36, 0x5a, 0x34, 0x67, 0x6f, 0x2e, 0x63, 0x68, 0x72, 0x6f, 0x6d, 0x69, 0x75,
-	0x6d, 0x2e, 0x6f, 0x72, 0x67, 0x2f, 0x6c, 0x75, 0x63, 0x69, 0x2f, 0x62, 0x75, 0x69, 0x6c, 0x64,
-	0x62, 0x75, 0x63, 0x6b, 0x65, 0x74, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x3b, 0x62, 0x75, 0x69,
-	0x6c, 0x64, 0x62, 0x75, 0x63, 0x6b, 0x65, 0x74, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x33,
+	0x6f, 0x6e, 0x52, 0x0a, 0x65, 0x78, 0x70, 0x69, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x22, 0x97,
+	0x01, 0x0a, 0x0a, 0x43, 0x61, 0x63, 0x68, 0x65, 0x45, 0x6e, 0x74, 0x72, 0x79, 0x12, 0x12, 0x0a,
+	0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d,
+	0x65, 0x12, 0x12, 0x0a, 0x04, 0x70, 0x61, 0x74, 0x68, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x04, 0x70, 0x61, 0x74, 0x68, 0x12, 0x48, 0x0a, 0x13, 0x77, 0x61, 0x69, 0x74, 0x5f, 0x66, 0x6f,
+	0x72, 0x5f, 0x77, 0x61, 0x72, 0x6d, 0x5f, 0x63, 0x61, 0x63, 0x68, 0x65, 0x18, 0x03, 0x20, 0x01,
+	0x28, 0x0b, 0x32, 0x19, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x62, 0x75, 0x66, 0x2e, 0x44, 0x75, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x52, 0x10, 0x77,
+	0x61, 0x69, 0x74, 0x46, 0x6f, 0x72, 0x57, 0x61, 0x72, 0x6d, 0x43, 0x61, 0x63, 0x68, 0x65, 0x12,
+	0x17, 0x0a, 0x07, 0x65, 0x6e, 0x76, 0x5f, 0x76, 0x61, 0x72, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x06, 0x65, 0x6e, 0x76, 0x56, 0x61, 0x72, 0x2a, 0x87, 0x01, 0x0a, 0x06, 0x53, 0x74, 0x61,
+	0x74, 0x75, 0x73, 0x12, 0x16, 0x0a, 0x12, 0x53, 0x54, 0x41, 0x54, 0x55, 0x53, 0x5f, 0x55, 0x4e,
+	0x53, 0x50, 0x45, 0x43, 0x49, 0x46, 0x49, 0x45, 0x44, 0x10, 0x00, 0x12, 0x0d, 0x0a, 0x09, 0x53,
+	0x43, 0x48, 0x45, 0x44, 0x55, 0x4c, 0x45, 0x44, 0x10, 0x01, 0x12, 0x0b, 0x0a, 0x07, 0x53, 0x54,
+	0x41, 0x52, 0x54, 0x45, 0x44, 0x10, 0x02, 0x12, 0x0e, 0x0a, 0x0a, 0x45, 0x4e, 0x44, 0x45, 0x44,
+	0x5f, 0x4d, 0x41, 0x53, 0x4b, 0x10, 0x04, 0x12, 0x0b, 0x0a, 0x07, 0x53, 0x55, 0x43, 0x43, 0x45,
+	0x53, 0x53, 0x10, 0x0c, 0x12, 0x0b, 0x0a, 0x07, 0x46, 0x41, 0x49, 0x4c, 0x55, 0x52, 0x45, 0x10,
+	0x14, 0x12, 0x11, 0x0a, 0x0d, 0x49, 0x4e, 0x46, 0x52, 0x41, 0x5f, 0x46, 0x41, 0x49, 0x4c, 0x55,
+	0x52, 0x45, 0x10, 0x24, 0x12, 0x0c, 0x0a, 0x08, 0x43, 0x41, 0x4e, 0x43, 0x45, 0x4c, 0x45, 0x44,
+	0x10, 0x44, 0x2a, 0x25, 0x0a, 0x07, 0x54, 0x72, 0x69, 0x6e, 0x61, 0x72, 0x79, 0x12, 0x09, 0x0a,
+	0x05, 0x55, 0x4e, 0x53, 0x45, 0x54, 0x10, 0x00, 0x12, 0x07, 0x0a, 0x03, 0x59, 0x45, 0x53, 0x10,
+	0x01, 0x12, 0x06, 0x0a, 0x02, 0x4e, 0x4f, 0x10, 0x02, 0x2a, 0x21, 0x0a, 0x0b, 0x43, 0x6f, 0x6d,
+	0x70, 0x72, 0x65, 0x73, 0x73, 0x69, 0x6f, 0x6e, 0x12, 0x08, 0x0a, 0x04, 0x5a, 0x4c, 0x49, 0x42,
+	0x10, 0x00, 0x12, 0x08, 0x0a, 0x04, 0x5a, 0x53, 0x54, 0x44, 0x10, 0x01, 0x42, 0x36, 0x5a, 0x34,
+	0x67, 0x6f, 0x2e, 0x63, 0x68, 0x72, 0x6f, 0x6d, 0x69, 0x75, 0x6d, 0x2e, 0x6f, 0x72, 0x67, 0x2f,
+	0x6c, 0x75, 0x63, 0x69, 0x2f, 0x62, 0x75, 0x69, 0x6c, 0x64, 0x62, 0x75, 0x63, 0x6b, 0x65, 0x74,
+	0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x3b, 0x62, 0x75, 0x69, 0x6c, 0x64, 0x62, 0x75, 0x63, 0x6b,
+	0x65, 0x74, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -1031,7 +1190,7 @@ func file_go_chromium_org_luci_buildbucket_proto_common_proto_rawDescGZIP() []by
 }
 
 var file_go_chromium_org_luci_buildbucket_proto_common_proto_enumTypes = make([]protoimpl.EnumInfo, 3)
-var file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
+var file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes = make([]protoimpl.MessageInfo, 11)
 var file_go_chromium_org_luci_buildbucket_proto_common_proto_goTypes = []interface{}{
 	(Status)(0),                              // 0: buildbucket.v2.Status
 	(Trinary)(0),                             // 1: buildbucket.v2.Trinary
@@ -1044,22 +1203,24 @@ var file_go_chromium_org_luci_buildbucket_proto_common_proto_goTypes = []interfa
 	(*StringPair)(nil),                       // 8: buildbucket.v2.StringPair
 	(*TimeRange)(nil),                        // 9: buildbucket.v2.TimeRange
 	(*RequestedDimension)(nil),               // 10: buildbucket.v2.RequestedDimension
-	(*StatusDetails_ResourceExhaustion)(nil), // 11: buildbucket.v2.StatusDetails.ResourceExhaustion
-	(*StatusDetails_Timeout)(nil),            // 12: buildbucket.v2.StatusDetails.Timeout
-	(*timestamppb.Timestamp)(nil),            // 13: google.protobuf.Timestamp
-	(*durationpb.Duration)(nil),              // 14: google.protobuf.Duration
+	(*CacheEntry)(nil),                       // 11: buildbucket.v2.CacheEntry
+	(*StatusDetails_ResourceExhaustion)(nil), // 12: buildbucket.v2.StatusDetails.ResourceExhaustion
+	(*StatusDetails_Timeout)(nil),            // 13: buildbucket.v2.StatusDetails.Timeout
+	(*timestamppb.Timestamp)(nil),            // 14: google.protobuf.Timestamp
+	(*durationpb.Duration)(nil),              // 15: google.protobuf.Duration
 }
 var file_go_chromium_org_luci_buildbucket_proto_common_proto_depIdxs = []int32{
-	11, // 0: buildbucket.v2.StatusDetails.resource_exhaustion:type_name -> buildbucket.v2.StatusDetails.ResourceExhaustion
-	12, // 1: buildbucket.v2.StatusDetails.timeout:type_name -> buildbucket.v2.StatusDetails.Timeout
-	13, // 2: buildbucket.v2.TimeRange.start_time:type_name -> google.protobuf.Timestamp
-	13, // 3: buildbucket.v2.TimeRange.end_time:type_name -> google.protobuf.Timestamp
-	14, // 4: buildbucket.v2.RequestedDimension.expiration:type_name -> google.protobuf.Duration
-	5,  // [5:5] is the sub-list for method output_type
-	5,  // [5:5] is the sub-list for method input_type
-	5,  // [5:5] is the sub-list for extension type_name
-	5,  // [5:5] is the sub-list for extension extendee
-	0,  // [0:5] is the sub-list for field type_name
+	12, // 0: buildbucket.v2.StatusDetails.resource_exhaustion:type_name -> buildbucket.v2.StatusDetails.ResourceExhaustion
+	13, // 1: buildbucket.v2.StatusDetails.timeout:type_name -> buildbucket.v2.StatusDetails.Timeout
+	14, // 2: buildbucket.v2.TimeRange.start_time:type_name -> google.protobuf.Timestamp
+	14, // 3: buildbucket.v2.TimeRange.end_time:type_name -> google.protobuf.Timestamp
+	15, // 4: buildbucket.v2.RequestedDimension.expiration:type_name -> google.protobuf.Duration
+	15, // 5: buildbucket.v2.CacheEntry.wait_for_warm_cache:type_name -> google.protobuf.Duration
+	6,  // [6:6] is the sub-list for method output_type
+	6,  // [6:6] is the sub-list for method input_type
+	6,  // [6:6] is the sub-list for extension type_name
+	6,  // [6:6] is the sub-list for extension extendee
+	0,  // [0:6] is the sub-list for field type_name
 }
 
 func init() { file_go_chromium_org_luci_buildbucket_proto_common_proto_init() }
@@ -1165,7 +1326,7 @@ func file_go_chromium_org_luci_buildbucket_proto_common_proto_init() {
 			}
 		}
 		file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[8].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*StatusDetails_ResourceExhaustion); i {
+			switch v := v.(*CacheEntry); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -1177,6 +1338,18 @@ func file_go_chromium_org_luci_buildbucket_proto_common_proto_init() {
 			}
 		}
 		file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[9].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*StatusDetails_ResourceExhaustion); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_go_chromium_org_luci_buildbucket_proto_common_proto_msgTypes[10].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*StatusDetails_Timeout); i {
 			case 0:
 				return &v.state
@@ -1195,7 +1368,7 @@ func file_go_chromium_org_luci_buildbucket_proto_common_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_go_chromium_org_luci_buildbucket_proto_common_proto_rawDesc,
 			NumEnums:      3,
-			NumMessages:   10,
+			NumMessages:   11,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
