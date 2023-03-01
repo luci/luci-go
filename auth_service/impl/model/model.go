6d9316172966c455aba9c1c8ddd200d4f24cf52d
Camilo Jacomet <cjacomet@google.com>
2023-03-01 19:11:58 +0000

    [auth-service]: Create PermissionsList internal Proto.
    See comment in model.go for why this is necessary. My plan is to have both Permissions and PermissionsList be present in datastore entity AuthRealmsGlobals until we teach Python to write to PermissionsList instead of Permissions and also to use Permissions.cfg. Go will use the PermissionsList field while Python will use the Permissions field for the time being. While that will take some attention to make sure they both stay in sync, the permissions don't change often.
    
    Bug:1336137
    Change-Id: I5ea6036d95a2737859126416629eb98c89533867
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4298270
    Reviewed-by: Yulan Lin <yulanlin@google.com>
    Commit-Queue: Camilo Jacomet <cjacomet@google.com>

--- auth_service/impl/model/model.go
+++ auth_service/impl/model/model.go
@@ -32,6 +32,7 @@ import (
 	"go.chromium.org/luci/auth_service/api/configspb"
 	"go.chromium.org/luci/auth_service/api/rpcpb"
 	"go.chromium.org/luci/auth_service/impl/info"
+	"go.chromium.org/luci/auth_service/internal/permissions"
 	"go.chromium.org/luci/common/clock"
 	"go.chromium.org/luci/common/data/stringset"
 	"go.chromium.org/luci/common/errors"
@@ -215,8 +216,23 @@ type AuthRealmsGlobals struct {
 
 	Parent *datastore.Key `gae:"$parent"`
 
+	// TODO(cjacomet): Remove this once Python version no longer depends on
+	// this.
+	//
+	// Permissions and PermissionsList are the same, they are just represented
+	// differently in datastore from Python to Golang. In Python lists of pointers
+	// to protobuf types would serialize automagically and store as an array of
+	// serialized strings in datastore. In Go this is not supported in the luci datastore
+	// package. It is better to define the list as repeated protobuf.Msg instead of
+	// []*protobuf.Msg. This will yield the same results in the end but with a bit different steps.
+	// Permissions will not be unmarshalled in this version, the declaration
+	// is to avoid any issues when fetching the entity from datastore.
+	//
 	// Permissions is all globally defined permissions, in alphabetical order.
 	Permissions []string `gae:"permissions,noindex"`
+
+	// PermissionsList is all globally defined permissions, in alphabetical order.
+	PermissionsList *permissions.PermissionsList `gae:"permissionslist"`
 }
 
 // AuthProjectRealms is all realms of a single LUCI project.
