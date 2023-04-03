34ec804d2ba8150fd7a005bd8ed22a307d455252
Weiwei Lin <weiweilin@google.com>
2023-04-03 04:26:10 +0000

    [milo] handle errors occurred during React rendering process
    Errors from Lit is already handled by <LitEnvProvider />
    
    R=mdraz@google.com, nqmtuan@google.com
    
    Bug: 1408955
    Change-Id: I7a9a1f7507422bd04c6b0d1e919ce7bd4e82c91e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4387656
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Mostafa Draz <mdraz@google.com>

--- milo/ui/src/layouts/base.tsx
+++ milo/ui/src/layouts/base.tsx
@@ -14,6 +14,7 @@
 
 import { Outlet } from 'react-router-dom';
 
+import { ErrorBoundary } from '../components/error_boundary';
 import { TopBar } from '../components/top_bar';
 
 /**
@@ -23,7 +24,9 @@ export function BaseLayout() {
   return (
     <>
       <TopBar />
-      <Outlet />
+      <ErrorBoundary>
+        <Outlet />
+      </ErrorBoundary>
     </>
   );
 }
