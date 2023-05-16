5045338d5686138398d6c9ee8ca74c5c67801ff4
nqmtuan <nqmtuan@google.com>
2023-05-16 03:14:43 +0000

    [Analysis] Add BQ Exporter table schema
    Bug:b/270027453
    Change-Id: I5992fa8f9a1df6df5dbc5891077927d514c5bff3
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4535437
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Commit-Queue: Tuan Nguyen <nqmtuan@google.com>

---
+++ analysis/internal/changepoints/bqexporter/schema.go
@@ -0,0 +1,91 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bqexporter
+
+import (
+	"cloud.google.com/go/bigquery"
+	"cloud.google.com/go/bigquery/storage/managedwriter/adapt"
+	"github.com/golang/protobuf/descriptor"
+	desc "github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"go.chromium.org/luci/common/bq"
+	"google.golang.org/protobuf/types/descriptorpb"
+
+	"go.chromium.org/luci/analysis/internal/bqutil"
+	bqpb "go.chromium.org/luci/analysis/proto/bq"
+	pb "go.chromium.org/luci/analysis/proto/v1"
+)
+
+// We stream test variant branch update to this table.
+const tableName = "test_variant_segment_updates"
+
+// schemaApplyer ensures BQ schema matches the row proto definitions.
+var schemaApplyer = bq.NewSchemaApplyer(bq.RegisterSchemaApplyerCache(50))
+
+const rowMessage = "luci.analysis.bq.TestVariantBranchRow"
+
+var tableMetadata *bigquery.TableMetadata
+
+// tableSchemaDescriptor is a self-contained DescriptorProto for describing
+// row protocol buffers sent to the BigQuery Write API.
+var tableSchemaDescriptor *descriptorpb.DescriptorProto
+
+func init() {
+	var err error
+	var schema bigquery.Schema
+	if schema, err = generateRowSchema(); err != nil {
+		panic(err)
+	}
+	if tableSchemaDescriptor, err = generateRowSchemaDescriptor(); err != nil {
+		panic(err)
+	}
+
+	tableMetadata = &bigquery.TableMetadata{
+		RangePartitioning: &bigquery.RangePartitioning{
+			// has_recent_unexpected_results has only 2 possible values: 0 and 1.
+			Field: "has_recent_unexpected_results",
+			Range: &bigquery.RangePartitioningRange{
+				Start: 0,
+				// End is exclusive.
+				End: 2,
+				// The width of each interval range.
+				Interval: 1,
+			},
+		},
+		Clustering: &bigquery.Clustering{
+			Fields: []string{"project", "test_id"},
+		},
+		// Relax ensures no fields are marked "required".
+		Schema: schema.Relax(),
+	}
+}
+
+func generateRowSchema() (schema bigquery.Schema, err error) {
+	fd, _ := descriptor.MessageDescriptorProto(&bqpb.TestVariantBranchRow{})
+	// We also need to get FileDescriptorProto for SourceRef and GitilesRef
+	// because they are defined in different files.
+	fdsr, _ := descriptor.MessageDescriptorProto(&pb.SourceRef{})
+	fdgr, _ := descriptor.MessageDescriptorProto(&pb.GitilesRef{})
+	fdset := &desc.FileDescriptorSet{File: []*desc.FileDescriptorProto{fd, fdsr, fdgr}}
+	return bqutil.GenerateSchema(fdset, rowMessage)
+}
+
+func generateRowSchemaDescriptor() (*desc.DescriptorProto, error) {
+	m := &bqpb.TestVariantBranchRow{}
+	descriptorProto, err := adapt.NormalizeDescriptor(m.ProtoReflect().Descriptor())
+	if err != nil {
+		return nil, err
+	}
+	return descriptorProto, nil
+}
