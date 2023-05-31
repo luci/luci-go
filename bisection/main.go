6a752a492e7627e31bf57fd354f98de21e9a39ac
Anne Redulla <aredulla@google.com>
2023-05-31 06:15:17 +0000

    [Bisection] Delete deprecated GoFindit services
    Bug: 1356025
    Change-Id: I9e84397d3173276bb9442099000fd637bd05c91e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4570320
    Reviewed-by: Tuan Nguyen <nqmtuan@google.com>
    Commit-Queue: Anne Redulla <aredulla@google.com>

--- bisection/main.go
+++ bisection/main.go
@@ -28,11 +28,9 @@ import (
 	"go.chromium.org/luci/bisection/frontend/handlers"
 	"go.chromium.org/luci/bisection/internal/config"
 	"go.chromium.org/luci/bisection/metrics"
-	gfipb "go.chromium.org/luci/bisection/proto"
 	pb "go.chromium.org/luci/bisection/proto/v1"
 	"go.chromium.org/luci/bisection/pubsub"
 	"go.chromium.org/luci/bisection/server"
-	gfiserver "go.chromium.org/luci/bisection/server/gofindit"
 
 	"github.com/golang/protobuf/proto"
 	"go.chromium.org/luci/auth/identity"
@@ -187,24 +185,6 @@ func main() {
 		// Installs gRPC service to communicate with recipes
 		pb.RegisterBotUpdatesServer(srv, &pb.DecoratedBotUpdates{
 			Service: &server.BotUpdatesServer{},
-			Prelude: checkBotAPIAccess,
-		})
-
-		// Installs soon-to-be deprecated GoFindit gRPC service.
-		// TODO (aredulla): remove this once all references to the GoFindit gRPC
-		// service have been updated to use the LUCI Bisection Analyses service.
-		gfipb.RegisterGoFinditServiceServer(srv, &gfipb.DecoratedGoFinditService{
-			Service: &gfiserver.GoFinditServer{},
-			Prelude: checkAPIAccess,
-		})
-
-		// Installs soon-to-be deprecated GoFindit gRPC service to communicate
-		// with recipes.
-		// TODO (aredulla): remove this once all references to the GoFindit gRPC
-		// bot service have been updated to use the LUCI Bisection Bot Updates
-		// service.
-		gfipb.RegisterGoFinditBotServiceServer(srv, &gfipb.DecoratedGoFinditBotService{
-			Service: &gfiserver.GoFinditBotServer{},
 			Prelude: checkBotAPIAccess,
 		})
 
