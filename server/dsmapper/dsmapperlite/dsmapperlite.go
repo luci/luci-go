5485d6c1cafd50dcb3b0189b9caafdf2668ea4a2
Vadim Shtayura <vadimsh@chromium.org>
2023-06-07 08:24:23 +0000

    [dsmapperlite] An in-memory dsmapper implementation.
    A tiny one at that.
    
    The original implementation is using task queues to distribute
    mapping operations across many machines and it can theoretically
    handle very large sets of data.
    
    This implementation distributes mapping operations across goroutines
    in a single process and doesn't scale well past some limit (maybe 1M
    entities, haven't checked). But it reuses the same datastore magic
    (`__scatter__` hidden field) as the original dsmapper to balance
    workload across all goroutines.
    
    I'm considering using it to quickly scan Swarming bot fleet entities
    to come up with aggregated statistics (number of dead bots per pool
    etc). With 64 shards, it scans 33K BotInfo entities in ~3s on my
    workstation (i.e. likely it will be even faster in the cloud).
    
    R=iannucci@chromium.org
    
    Change-Id: I819735c6bfcb6d8eb0e898943d49332624f6273c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4517797
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

---
+++ server/dsmapper/dsmapperlite/dsmapperlite.go
@@ -0,0 +1,74 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package dsmapperlite implements an in-process datastore mapper.
+//
+// Unlike its bigger sibling dsmapper, it doesn't distribute mapping
+// operations across machines, but in exchange has a very simple API. There's
+// no need to install it as a server module or setup task queue tasks etc. Just
+// use is a library.
+//
+// Useful for quickly visiting up to 100K entities.
+package dsmapperlite
+
+import (
+	"context"
+
+	"golang.org/x/sync/errgroup"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/gae/service/datastore"
+	"go.chromium.org/luci/server/dsmapper/internal/splitter"
+)
+
+// Map passes all entities matching the query to the callback, in parallel,
+// in some random order.
+//
+// Runs up to `shards` number of parallel goroutines, where each one executes
+// a datastore query and passes the resulting entities to the callback (along
+// with the shard index). Each query fetches entities in `batchSize` pages
+// before handling them. The overall memory consumption is thus
+// `O(shards * batchSize * averageEntitySize)`.
+//
+// Within a shard, the callback is called sequentially, but different shards
+// are processed in parallel. If the callback needs to parallelize entity
+// processing more, it should manage its own goroutine pool and pass entities
+// to it.
+//
+// If the callback returns an error, Map aborts the entire operation ASAP (but
+// it may take some time to wind down). When this happens, the context passed
+// to the callback is canceled.
+func Map[E any](ctx context.Context, q *datastore.Query, shards, batchSize int, cb func(ctx context.Context, shard int, entity E) error) error {
+	logging.Infof(ctx, "Calculating ranges...")
+	ranges, err := splitter.SplitIntoRanges(ctx, q, splitter.Params{
+		Shards:  shards,
+		Samples: 500,
+	})
+	if err != nil {
+		return errors.Annotate(err, "failed to do the initial __scatter__ query").Err()
+	}
+	logging.Infof(ctx, "Querying %d ranges in parallel...", len(ranges))
+	eg, ctx := errgroup.WithContext(ctx)
+	for idx, r := range ranges {
+		idx := idx
+		rangedQ := r.Apply(q)
+		eg.Go(func() error {
+			return datastore.RunBatch(ctx, int32(batchSize), rangedQ, func(e E) error {
+				return cb(ctx, idx, e)
+			})
+		})
+	}
+	return eg.Wait()
+}
---
+++ server/dsmapper/dsmapperlite/dsmapperlite_test.go
@@ -0,0 +1,81 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dsmapperlite
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"go.chromium.org/luci/gae/impl/memory"
+	"go.chromium.org/luci/gae/service/datastore"
+)
+
+type intEnt struct {
+	ID int64 `gae:"$id"`
+}
+
+func TestMap(t *testing.T) {
+	t.Parallel()
+
+	ctx := memory.Use(context.Background())
+
+	const count = 1000
+	for i := 0; i < count; i++ {
+		if err := datastore.Put(ctx, &intEnt{ID: int64(i + 1)}); err != nil {
+			t.Fatalf("Storing entity: %s", err)
+		}
+	}
+	datastore.GetTestable(ctx).CatchupIndexes()
+
+	t.Run("visits all", func(t *testing.T) {
+		m := sync.Mutex{}
+		seen := map[int64]int{}
+
+		err := Map(ctx, datastore.NewQuery("intEnt"), 10, 256, func(ctx context.Context, _ int, e *intEnt) error {
+			m.Lock()
+			seen[e.ID] += 1
+			m.Unlock()
+			return nil
+		})
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		if len(seen) != count {
+			t.Fatalf("Expected to visit %d entities, but visited %d", count, len(seen))
+		}
+		for key, visits := range seen {
+			if visits != 1 {
+				t.Fatalf("Entity %d was visited more than once: %d", key, visits)
+			}
+		}
+	})
+
+	t.Run("aborts on errors", func(t *testing.T) {
+		var visits atomic.Int64
+		err := Map(ctx, datastore.NewQuery("intEnt"), 10, 256, func(ctx context.Context, _ int, e *intEnt) error {
+			if visits.Add(1) == 100 {
+				return errors.New("boom")
+			}
+			return nil
+		})
+		if err == nil {
+			t.Fatalf("Unexpectedly succeeded")
+		}
+	})
+}
