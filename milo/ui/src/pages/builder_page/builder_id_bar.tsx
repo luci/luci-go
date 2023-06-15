38bd95daf37430ce7f71f0a450baef22f535ac2c
Weiwei Lin <weiweilin@google.com>
2023-06-15 05:44:41 +0000

    [milo] make opting out the new builder page last longer
    R=mdraz@google.com, nqmtuan@google.com
    
    Change-Id: I9829129b3c87c0fa28e185bfdbe75439bd0b4091
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4617532
    Reviewed-by: Tuan Nguyen <nqmtuan@google.com>
    Commit-Queue: Tuan Nguyen <nqmtuan@google.com>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>

--- milo/ui/src/pages/builder_page/builder_id_bar.tsx
+++ milo/ui/src/pages/builder_page/builder_id_bar.tsx
@@ -69,7 +69,8 @@ export function BuilderIdBar({ builderId }: BuilderIdBarProps) {
           }
 
           const expires = new Date(
-            Date.now() + 24 * 60 * 60 * 1000
+            // 1 week from now.
+            Date.now() + 7 * 24 * 60 * 60 * 1000
           ).toUTCString();
           document.cookie = `showNewBuilderPage=false; expires=${expires}; path=/`;
         }}
