327bcd546ce26e5f3f3e6fbd1b3c608556de479a
Yiwei Zhang <yiwzhang@google.com>
2023-06-16 04:18:43 +0000

    config: add a testutil package to facilitate testing
    R=yuanjunh
    
    Bug: 1449933
    Change-Id: I5958be226920df3ff1a5f09b74615f714b4b859d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4618956
    Reviewed-by: Yuanjun Huang <yuanjunh@google.com>
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>

---
+++ config_service/testutil/testing.go
@@ -0,0 +1,96 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package testutil reduces the boilerplate for testing in LUCI Config.
+package testutil
+
+import (
+	"context"
+	"strconv"
+	"testing"
+	"time"
+
+	"google.golang.org/protobuf/encoding/prototext"
+	"google.golang.org/protobuf/proto"
+
+	"go.chromium.org/luci/common/clock/testclock"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/logging/gologger"
+	"go.chromium.org/luci/config"
+	"go.chromium.org/luci/gae/impl/memory"
+	"go.chromium.org/luci/gae/service/datastore"
+	"go.chromium.org/luci/server/tq"
+
+	"go.chromium.org/luci/config_service/internal/model"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+const AppID = "luci-config-dev"
+
+// SetupContext sets up testing common context for LUCI Config tests.
+func SetupContext() context.Context {
+	ctx := context.Background()
+	utc := time.Date(2023, time.March, 4, 17, 30, 00, 0, time.UTC)
+	ctx, _ = testclock.UseTime(ctx, utc)
+	if testing.Verbose() {
+		ctx = logging.SetLevel(gologger.StdConfig.Use(ctx), logging.Debug)
+	} else {
+		ctx = logging.SetLevel(gologger.StdConfig.Use(ctx), logging.Info)
+	}
+
+	ctx, _ = tq.TestingContext(ctx, nil)
+	ctx = memory.UseWithAppID(ctx, "dev~"+AppID)
+	datastore.GetTestable(ctx).Consistent(true)
+	// Intentionally not enabling AutoIndex to ensure the composite index must
+	// be explicitly added
+	datastore.GetTestable(ctx).AutoIndex(false)
+	return ctx
+}
+
+// InjectConfigSet writes a new revision for the provided config set.
+//
+// The revision ID is a monotonically increasing integer.
+func InjectConfigSet(ctx context.Context, cfgSet config.Set, configs map[string]proto.Message) {
+	cs := &model.ConfigSet{
+		ID: cfgSet,
+	}
+	switch err := datastore.Get(ctx, cs); err {
+	case datastore.ErrNoSuchEntity:
+		cs.LatestRevision.ID = "1"
+	case nil:
+		prevID, err := strconv.Atoi(cs.LatestRevision.ID)
+		So(err, ShouldBeNil)
+		cs.LatestRevision.ID = strconv.Itoa(prevID + 1)
+	default:
+		So(err, ShouldBeNil)
+	}
+
+	var files []*model.File
+	for filepath, msg := range configs {
+		content, err := prototext.Marshal(msg)
+		So(err, ShouldBeNil)
+		files = append(files, &model.File{
+			Path:     filepath,
+			Revision: datastore.MakeKey(ctx, model.ConfigSetKind, string(cfgSet), model.RevisionKind, cs.LatestRevision.ID),
+			Content:  content,
+		})
+	}
+	So(datastore.Put(ctx, cs, files), ShouldBeNil)
+}
+
+// InjectSelfConfigs is a shorthand for updating LUCI Config self config.
+func InjectSelfConfigs(ctx context.Context, configs map[string]proto.Message) {
+	InjectConfigSet(ctx, config.MustServiceSet(AppID), configs)
+}
