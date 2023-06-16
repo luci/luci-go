cd38eebc4f187170283a31a34c0367db34fbc1fa
Weiwei Lin <weiweilin@google.com>
2023-06-16 00:37:16 +0000

    [milo] explain the usage of js-with-babel
    R=beining@google.com, mdraz@google.com
    
    Bug: 1429134
    Change-Id: I97e696ad221e19c2370d0ad397287eac574f601c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4617541
    Auto-Submit: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Mostafa Draz <mdraz@google.com>
    Commit-Queue: Mostafa Draz <mdraz@google.com>

--- milo/ui/jest.config.js
+++ milo/ui/jest.config.js
@@ -14,6 +14,15 @@
 
 // eslint-disable-next-line no-undef
 module.exports = {
+  // Use `js-with-babel` instead of `js-with-ts` because
+  // 1. `js-with-babel` is significantly faster than `js-with-ts`. This is
+  // likely because babel transpiles each file individually while tsc compiles
+  // the entire project as a whole.
+  // 2. The production build is also transpiled by babel. Using the same
+  // transpiler helps ensuring the code under test behaves similar to the code
+  // in production. Ideally, we should build testing code with the same
+  // toolchain as we build production code (i.e. Vite). However, there's no
+  // plugin supporting that yet.
   preset: 'ts-jest/presets/js-with-babel',
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(test).[jt]s?(x)'],
@@ -28,10 +37,8 @@ module.exports = {
       },
     ],
   },
-  /**
-   * Some modules uses `es6` modules, which is not compatible with jest, so we
-   * need to transform them.
-   */
+  // Some modules use `es6` modules, which is not compatible with jest, so we
+  // need to transform them.
   transformIgnorePatterns: ['/node_modules/?!(lodash-es|lit)'],
   setupFiles: ['./src/testing_tools/setup_env.ts'],
   moduleNameMapper: {
