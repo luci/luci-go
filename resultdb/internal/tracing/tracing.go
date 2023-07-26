88d60f5fdc1efd6c4b1019b38fdc4cd8964d67da
Jeff Yoon <jeffyoon@google.com>
2023-07-26 20:08:27 +0000

    [ResultDB] Format files
    go fmt ./...
    
    Change-Id: I4743591547a9670202020a7b657a56b6facc17da
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4706311
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Commit-Queue: Jeff Yoon <jeffyoon@google.com>

--- resultdb/internal/tracing/tracing.go
+++ resultdb/internal/tracing/tracing.go
@@ -16,12 +16,12 @@
 package tracing
 
 import (
-  "context"
+	"context"
 
-  "go.opentelemetry.io/otel"
-  "go.opentelemetry.io/otel/attribute"
-  "go.opentelemetry.io/otel/codes"
-  "go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var tracer = otel.Tracer("go.chromium.org/luci/resultdb")
@@ -30,18 +30,18 @@ var tracer = otel.Tracer("go.chromium.org/luci/resultdb")
 //
 // Finish it with End, usually like this:
 //
-//  ctx, span := tracing.Start(ctx, "..."")
-//  defer func() { tracing.End(span, err) }()
+//	ctx, span := tracing.Start(ctx, "..."")
+//	defer func() { tracing.End(span, err) }()
 func Start(ctx context.Context, name string, attrs ...attribute.KeyValue) (context.Context, trace.Span) {
-  return tracer.Start(ctx, name, trace.WithAttributes(attrs...))
+	return tracer.Start(ctx, name, trace.WithAttributes(attrs...))
 }
 
 // End records the error (if any) and closes the span.
 func End(span trace.Span, err error, attrs ...attribute.KeyValue) {
-  span.SetAttributes(attrs...)
-  if err != nil {
-    span.RecordError(err)
-    span.SetStatus(codes.Error, err.Error())
-  }
-  span.End()
+	span.SetAttributes(attrs...)
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+	}
+	span.End()
 }
