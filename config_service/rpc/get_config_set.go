284ab06fb97eac26812f74d361087091373bacf9
Yuanjun Huang <yuanjunh@google.com>
2023-07-27 01:24:23 +0000

    [luci-config] Implement GetConfigSet rpc
    Implement GetConfigSet rpc.
    
    Bug: 1465995
    Change-Id: Ie2c787f9c13554761e7ca1bd8cf8404d9d8cf407
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4721846
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Commit-Queue: Yuanjun Huang <yuanjunh@google.com>

---
+++ config_service/rpc/get_config_set.go
@@ -0,0 +1,111 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/common/proto/mask"
+	"go.chromium.org/luci/config"
+	"go.chromium.org/luci/gae/service/datastore"
+	"go.chromium.org/luci/server/auth"
+
+	"go.chromium.org/luci/config_service/internal/acl"
+	"go.chromium.org/luci/config_service/internal/model"
+	pb "go.chromium.org/luci/config_service/proto"
+)
+
+// GetConfigSet fetches a single config set. Implements pb.ConfigsServer.
+func (c Configs) GetConfigSet(ctx context.Context, req *pb.GetConfigSetRequest) (*pb.ConfigSet, error) {
+	// Validate the request.
+	if req.ConfigSet == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "config_set is not specified")
+	}
+	m, err := toConfigSetMask(req.GetFields())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid fields mask: %s", err)
+	}
+
+	ctx = logging.SetFields(ctx, logging.Fields{"ConfigSet": req.ConfigSet})
+
+	// Check read access for the config set.
+	switch hasPerm, err := acl.CanReadConfigSet(ctx, config.Set(req.ConfigSet)); {
+	case err != nil:
+		logging.Errorf(ctx, "cannot check permission for %q: %s", auth.CurrentIdentity(ctx), err)
+		return nil, status.Errorf(codes.Internal, "error while checking permission for %q", auth.CurrentIdentity(ctx))
+	case !hasPerm:
+		logging.Infof(ctx, "%q does not have the read access", auth.CurrentIdentity(ctx))
+		return nil, notFoundErr(auth.CurrentIdentity(ctx))
+	}
+
+	cfgSet := &model.ConfigSet{ID: config.Set(req.ConfigSet)}
+	var attempt *model.ImportAttempt
+	switch {
+	case m.MustIncludes("last_import_attempt") != mask.Exclude:
+		attempt = &model.ImportAttempt{ConfigSet: datastore.KeyForObj(ctx, cfgSet)}
+		// Fetch in the transaction to make sure the consistency between the
+		// ConfigSet and ImportAttempt entities.
+		err = datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+			return datastore.Get(ctx, cfgSet, attempt)
+		}, &datastore.TransactionOptions{ReadOnly: true})
+	default:
+		err = datastore.Get(ctx, cfgSet)
+	}
+	if err != nil {
+		if errors.Contains(err, datastore.ErrNoSuchEntity) {
+			logging.Warningf(ctx, "cannot find the config set or its import attempt: %s", err)
+			return nil, notFoundErr(auth.CurrentIdentity(ctx))
+		}
+		logging.Errorf(ctx, "cannot fetch config set or its import attempt: %s", err)
+		return nil, status.Errorf(codes.Internal, "error while fetching config set")
+	}
+
+	var files []*model.File
+	if m.MustIncludes("file_paths") != mask.Exclude {
+		query := datastore.NewQuery(model.FileKind).
+			Ancestor(datastore.MakeKey(ctx, model.ConfigSetKind, string(cfgSet.ID), model.RevisionKind, cfgSet.LatestRevision.ID)).
+			KeysOnly(true)
+		if err = datastore.GetAll(ctx, query, &files); err != nil {
+			logging.Errorf(ctx, "error while fetching config files: %s", err)
+			return nil, status.Errorf(codes.Internal, "error while fetching config files")
+		}
+	}
+
+	// To proto.
+	cfgSetPb := toConfigSetPb(cfgSet)
+	cfgSetPb.LastImportAttempt = toImportAttempt(attempt)
+	cfgSetPb.FilePaths = extractFilePaths(files)
+	if err := m.Trim(cfgSetPb); err != nil {
+		logging.Errorf(ctx, "cannot trim the config set proto: %s", err)
+		return nil, status.Errorf(codes.Internal, "error while constructing response")
+	}
+	return cfgSetPb, nil
+}
+
+func extractFilePaths(files []*model.File) []string {
+	if files == nil {
+		return nil
+	}
+	filePaths := make([]string, len(files))
+	for i, f := range files {
+		filePaths[i] = f.Path
+	}
+	return filePaths
+}
---
+++ config_service/rpc/get_config_set_test.go
@@ -0,0 +1,185 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"testing"
+
+	"google.golang.org/genproto/protobuf/field_mask"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"go.chromium.org/luci/auth/identity"
+	"go.chromium.org/luci/common/clock"
+	cfgcommonpb "go.chromium.org/luci/common/proto/config"
+	"go.chromium.org/luci/config"
+	"go.chromium.org/luci/gae/service/datastore"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/auth/authtest"
+
+	"go.chromium.org/luci/config_service/internal/common"
+	"go.chromium.org/luci/config_service/internal/model"
+	pb "go.chromium.org/luci/config_service/proto"
+	"go.chromium.org/luci/config_service/testutil"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestGetConfigSet(t *testing.T) {
+	t.Parallel()
+
+	Convey("GetConfigSet", t, func() {
+		ctx := testutil.SetupContext()
+		srv := &Configs{}
+
+		userID := identity.Identity("user:user@example.com")
+		fakeAuthDB := authtest.NewFakeDB()
+		testutil.InjectSelfConfigs(ctx, map[string]proto.Message{
+			common.ACLRegistryFilePath: &cfgcommonpb.AclCfg{
+				ProjectAccessGroup: "project-access-group",
+			},
+		})
+		fakeAuthDB.AddMocks(
+			authtest.MockMembership(userID, "project-access-group"),
+		)
+		ctx = auth.WithState(ctx, &authtest.FakeState{
+			Identity: userID,
+			FakeDB:   fakeAuthDB,
+		})
+
+		testutil.InjectConfigSet(ctx, config.MustProjectSet("project"), map[string]proto.Message{
+			"config1.cfg": nil,
+			"config2.cfg": nil,
+		})
+		commitTime := clock.Now(ctx)
+		So(datastore.Put(ctx, &model.ImportAttempt{
+			ConfigSet: datastore.MakeKey(ctx, model.ConfigSetKind, "projects/project"),
+			Success:   true,
+			Message:   "Up-to-date",
+			Revision: model.RevisionInfo{
+				ID:         "1",
+				CommitTime: commitTime,
+			},
+		}), ShouldBeNil)
+
+		Convey("empty req", func() {
+			res, err := srv.GetConfigSet(ctx, &pb.GetConfigSetRequest{})
+			So(res, ShouldBeNil)
+			So(err, ShouldHaveGRPCStatus, codes.InvalidArgument, "config_set is not specified")
+		})
+
+		Convey("invalid field mask", func() {
+			res, err := srv.GetConfigSet(ctx, &pb.GetConfigSetRequest{
+				ConfigSet: "projects/project",
+				Fields: &field_mask.FieldMask{
+					Paths: []string{"random"},
+				},
+			})
+			So(res, ShouldBeNil)
+			So(err, ShouldHaveGRPCStatus, codes.InvalidArgument, `invalid fields mask: field "random" does not exist in message ConfigSet`)
+		})
+
+		Convey("no permission", func() {
+			ctx = auth.WithState(ctx, &authtest.FakeState{
+				Identity: identity.Identity("user:random@example.com"),
+				FakeDB:   authtest.NewFakeDB(),
+			})
+			res, err := srv.GetConfigSet(ctx, &pb.GetConfigSetRequest{
+				ConfigSet: "projects/project",
+			})
+			So(res, ShouldBeNil)
+			So(err, ShouldHaveGRPCStatus, codes.NotFound, `requested resource not found or "user:random@example.com" dose not have permission to access it`)
+		})
+
+		Convey("ok with default mask", func() {
+			res, err := srv.GetConfigSet(ctx, &pb.GetConfigSetRequest{
+				ConfigSet: "projects/project",
+			})
+			So(err, ShouldBeNil)
+			So(res, ShouldResembleProto, &pb.ConfigSet{
+				Name: "projects/project",
+				Revision: &pb.ConfigSet_Revision{
+					Id:        "1",
+					Timestamp: timestamppb.New(commitTime),
+				},
+			})
+		})
+
+		Convey("with last_import_attempt", func() {
+			res, err := srv.GetConfigSet(ctx, &pb.GetConfigSetRequest{
+				ConfigSet: "projects/project",
+				Fields: &field_mask.FieldMask{
+					Paths: []string{"last_import_attempt", "name", "revision"},
+				},
+			})
+			So(err, ShouldBeNil)
+			So(res, ShouldResembleProto, &pb.ConfigSet{
+				Name: "projects/project",
+				Revision: &pb.ConfigSet_Revision{
+					Id:        "1",
+					Timestamp: timestamppb.New(commitTime),
+				},
+				LastImportAttempt: &pb.ConfigSet_Attempt{
+					Message: "Up-to-date",
+					Success: true,
+					Revision: &pb.ConfigSet_Revision{
+						Id:        "1",
+						Timestamp: timestamppb.New(commitTime),
+					},
+				},
+			})
+		})
+
+		Convey("with file_paths", func() {
+			res, err := srv.GetConfigSet(ctx, &pb.GetConfigSetRequest{
+				ConfigSet: "projects/project",
+				Fields: &field_mask.FieldMask{
+					Paths: []string{"file_paths", "name", "revision"},
+				},
+			})
+			So(err, ShouldBeNil)
+			So(res, ShouldResembleProto, &pb.ConfigSet{
+				Name: "projects/project",
+				Revision: &pb.ConfigSet_Revision{
+					Id:        "1",
+					Timestamp: timestamppb.New(commitTime),
+				},
+				FilePaths: []string{"config1.cfg", "config2.cfg"},
+			})
+		})
+
+		Convey("with mixed masks and sub-masks", func() {
+			res, err := srv.GetConfigSet(ctx, &pb.GetConfigSetRequest{
+				ConfigSet: "projects/project",
+				Fields: &field_mask.FieldMask{
+					Paths: []string{"file_paths", "name", "revision.id", "last_import_attempt.success"},
+				},
+			})
+			So(err, ShouldBeNil)
+			So(res, ShouldResembleProto, &pb.ConfigSet{
+				Name: "projects/project",
+				Revision: &pb.ConfigSet_Revision{
+					Id: "1",
+				},
+				FilePaths: []string{"config1.cfg", "config2.cfg"},
+				LastImportAttempt: &pb.ConfigSet_Attempt{
+					Success: true,
+				},
+			})
+		})
+	})
+}
