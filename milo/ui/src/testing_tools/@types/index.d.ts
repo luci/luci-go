e61272a1fb665c9b4748c9c74f1be04e84b4a0bc
Weiwei Lin <weiweilin@google.com>
2023-08-16 03:54:25 +0000

    [milo] add docs about relative imports in mock module utils
    R=mdraz@google.com, nqmtuan@google.com
    
    Change-Id: Ie8d8b87daecceea568a4995054ef4ffd31cabf76
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4778116
    Auto-Submit: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Mostafa Draz <mdraz@google.com>
    Commit-Queue: Mostafa Draz <mdraz@google.com>

--- milo/ui/src/testing_tools/@types/index.d.ts
+++ milo/ui/src/testing_tools/@types/index.d.ts
@@ -15,6 +15,14 @@
 /**
  * Similar to `jest.createMockFromModule` but only mock the export keys
  * specified in `keysToMock`.
+ *
+ * Relative `moduleName` is supported. A relative module name will be resolved
+ * relative to the file where `createSelectiveMockFromModule` is called (
+ * similar to a regular import statement).
+ * However, relative import should only be used when the module being mocked and
+ * the module under test are both child modules of the same parent module.
+ * In other cases, absolute imports should be favored. The same rule is applied
+ * to regular import statements.
  */
 declare function createSelectiveMockFromModule<T = unknown>(
   moduleName: string,
@@ -23,6 +31,14 @@ declare function createSelectiveMockFromModule<T = unknown>(
 
 /**
  * Spy the exported functions specified in `keysToSpy` in a module.
+ *
+ * Relative `moduleName` is supported. A relative module name will be resolved
+ * relative to the file where `createSelectiveSpiesFromModule` is called (
+ * similar to a regular import statement).
+ * However, relative import should only be used when the module being spied and
+ * the module under test are both child modules of the same parent module.
+ * In other cases, absolute imports should be favored. The same rule is applied
+ * to regular import statements.
  *
  * The spy is created via `jest.fn(actualImpl)` instead of `jest.spyOn` so
  * `jest.restoreAllMocks()` will NOT restore the spies. This is intentional.
