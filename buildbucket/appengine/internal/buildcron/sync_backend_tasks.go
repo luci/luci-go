afc8303edc621d7979c51e0705023dde989f1725
Chan Li <chanli@chromium.org>
2023-08-31 23:19:10 +0000

    [buildbucket] Implement a cron job to sync builds with backend tasks
    The cron job gets a list of pairs of (project, backen_target), then enqueues cloud tasks for each pair
    to sync builds in the project with their backend tasks on the backend.
    
    
    Bug: 1472896
    Change-Id: Ia13bbda672cb48924fdfd6b01c88cb5b7bfa0974
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4827857
    Reviewed-by: Yuanjun Huang <yuanjunh@google.com>
    Commit-Queue: Chan Li <chanli@chromium.org>

---
+++ buildbucket/appengine/internal/buildcron/sync_backend_tasks.go
@@ -0,0 +1,67 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buildcron
+
+import (
+	"context"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/gae/service/datastore"
+
+	"go.chromium.org/luci/buildbucket/appengine/model"
+	"go.chromium.org/luci/buildbucket/appengine/tasks"
+)
+
+type projectBackendPair struct {
+	// LUCI project name.
+	project string
+	// Task backend target.
+	backend string
+}
+
+// Find distinct (project, backend_target) by querying Build.
+// TODO(crbug.com/1472896): cache the (project, backend) pairs.
+func getProjectBackendPairs(ctx context.Context) ([]*projectBackendPair, error) {
+	q := datastore.NewQuery(model.BuildKind).
+		Eq("incomplete", true).Gt("backend_target", "").
+		Project("project", "backend_target").Distinct(true)
+	var blds []*model.Build
+	if err := datastore.GetAll(ctx, q, &blds); err != nil {
+		return nil, errors.Annotate(err, "failed to fetch all project keys").Err()
+	}
+
+	var pairs []*projectBackendPair
+	for _, b := range blds {
+		pairs = append(pairs, &projectBackendPair{project: b.Project, backend: b.BackendTarget})
+	}
+	return pairs, nil
+}
+
+// TriggerSyncBackendTasks finds all (project, backend_target) pairs and triggers
+// SyncBuildsWithBackendTasks tasks for each pair.
+func TriggerSyncBackendTasks(ctx context.Context) error {
+	pairs, err := getProjectBackendPairs(ctx)
+	if err != nil {
+		return err
+	}
+
+	var merr errors.MultiError
+	for _, p := range pairs {
+		if err := tasks.SyncWithBackend(ctx, p.backend, p.project); err != nil {
+			merr = append(merr, errors.Annotate(err, "failed to enqueue task to sync builds of project %s with backend %s", p.project, p.backend).Err())
+		}
+	}
+	return merr.AsError()
+}
---
+++ buildbucket/appengine/internal/buildcron/sync_backend_tasks_test.go
@@ -0,0 +1,135 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buildcron
+
+import (
+	"sort"
+	"testing"
+
+	"go.chromium.org/luci/gae/service/datastore"
+
+	"go.chromium.org/luci/buildbucket/appengine/model"
+	taskdefs "go.chromium.org/luci/buildbucket/appengine/tasks/defs"
+	pb "go.chromium.org/luci/buildbucket/proto"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestTriggerSyncBackendTasks(t *testing.T) {
+	t.Parallel()
+	Convey("TriggerSyncBackendTasks", t, func() {
+		ctx, _, sch := setUp()
+
+		b1 := &model.Build{
+			ID: 1,
+			Proto: &pb.Build{
+				Builder: &pb.BuilderID{
+					Project: "p1",
+				},
+			},
+		}
+		b2 := &model.Build{
+			ID:            1,
+			BackendTarget: "b1",
+			Proto: &pb.Build{
+				Builder: &pb.BuilderID{
+					Project: "p1",
+				},
+			},
+		}
+		b3 := &model.Build{
+			ID:            3,
+			BackendTarget: "b1",
+			Proto: &pb.Build{
+				Builder: &pb.BuilderID{
+					Project: "p1",
+				},
+			},
+		}
+		b4 := &model.Build{
+			ID:            4,
+			BackendTarget: "b2",
+			Proto: &pb.Build{
+				Builder: &pb.BuilderID{
+					Project: "p1",
+				},
+			},
+		}
+		b5 := &model.Build{
+			ID:            5,
+			BackendTarget: "b1",
+			Proto: &pb.Build{
+				Builder: &pb.BuilderID{
+					Project: "p2",
+				},
+			},
+		}
+		b6 := &model.Build{
+			ID:            6,
+			BackendTarget: "b3",
+			Proto: &pb.Build{
+				Builder: &pb.BuilderID{
+					Project: "p3",
+				},
+			},
+		}
+
+		So(datastore.Put(ctx, b1, b2, b3, b4, b5, b6), ShouldBeNil)
+		So(TriggerSyncBackendTasks(ctx), ShouldBeNil)
+		So(sch.Tasks(), ShouldHaveLength, 4)
+		pairs := make([]*projectBackendPair, 4)
+		for i, t := range sch.Tasks() {
+			switch v := t.Payload.(type) {
+			case *taskdefs.SyncBuildsWithBackendTasks:
+				pairs[i] = &projectBackendPair{
+					project: v.Project,
+					backend: v.Backend,
+				}
+			default:
+				panic("invalid task payload")
+			}
+		}
+		sort.Slice(pairs, func(i, j int) bool {
+			switch {
+			case pairs[i].project < pairs[j].project:
+				return true
+			case pairs[i].project > pairs[j].project:
+				return false
+			default:
+				return pairs[i].backend < pairs[j].backend
+			}
+		})
+
+		expected := []*projectBackendPair{
+			{
+				project: "p1",
+				backend: "b1",
+			},
+			{
+				project: "p1",
+				backend: "b2",
+			},
+			{
+				project: "p2",
+				backend: "b1",
+			},
+			{
+				project: "p3",
+				backend: "b3",
+			},
+		}
+		So(pairs, ShouldResemble, expected)
+	})
+}
