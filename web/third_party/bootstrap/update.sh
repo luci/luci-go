9f9e407a806f460e2216b8d337de3d6f04628c96
Yiwei Zhang <yiwzhang@google.com>
2023-09-11 20:44:10 +0000

    expose embed.FS that contains bootstrap js and css
    R=vadimsh, yuanjunh
    
    Bug: 1479247
    Change-Id: I12c5db4056513d60d4ab069195357d8cc4dee73e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4855636
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

--- web/third_party/bootstrap/update.sh
+++ web/third_party/bootstrap/update.sh
@@ -24,6 +24,33 @@ do
   cp ${SRC_DIR}/js/bootstrap.bundle.min.js.map ${DST_DIR}/js
   echo ${VERSION} >> ${DST_DIR}/VERSION
 
+  # Expose fs.FS that contains bootstrap js and css.
+  cat > ${DST_DIR}/embed.go << EOF
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package bootstrap exposes a fs.FS variable that contains bootstrap js and css
+package bootstrap
+
+import "embed"
+
+// FS is a file system that contains compiled bundled bootstrap js and css code
+//
+// go:embed css js
+var FS embed.FS
+EOF
+
   # Need a LICENSE.
   curl "https://raw.githubusercontent.com/twbs/bootstrap/main/LICENSE" -L -o ${DST_DIR}/LICENSE
 
---
+++ web/third_party/bootstrap/v5/embed.go
@@ -0,0 +1,23 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package bootstrap exposes a fs.FS variable that contains bootstrap js and css
+package bootstrap
+
+import "embed"
+
+// FS is a file system that contains compiled bundled bootstrap js and css code
+//
+// go:embed css js
+var FS embed.FS
