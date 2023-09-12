db2ee7abef67cdaf9cee4e8946810936cb82a09f
Mostafa Draz <mdraz@google.com>
2023-09-12 08:57:45 +0000

    luci_analysis: Prompt user to login for rule page permissions issue
    Change-Id: Id691d9d7d672f9ba5d437d718e7307ba4c6c5253
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4848576
    Auto-Submit: Mostafa Draz <mdraz@google.com>
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Commit-Queue: Patrick Meiring <meiring@google.com>

--- analysis/frontend/ui/src/views/rule/rule.tsx
+++ analysis/frontend/ui/src/views/rule/rule.tsx
@@ -19,51 +19,62 @@ import Grid from '@mui/material/Grid';
 import LinearProgress from '@mui/material/LinearProgress';
 
 import ClusterAnalysisSection from '@/components/cluster/cluster_analysis_section/cluster_analysis_section';
-import RuleTopPanel from '@/components/rule/rule_top_panel/rule_top_panel';
-import RuleArchivedMessage from '@/components/rule/rule_archived_message/rule_archived_message';
-import ErrorAlert from '@/components/error_alert/error_alert';
 import { ClusterContextProvider } from '@/components/cluster/cluster_context';
+import ErrorAlert from '@/components/error_alert/error_alert';
+import LoadErrorAlert from '@/components/load_error_alert/load_error_alert';
+import RuleArchivedMessage from '@/components/rule/rule_archived_message/rule_archived_message';
+import RuleTopPanel from '@/components/rule/rule_top_panel/rule_top_panel';
 import useFetchRule from '@/hooks/use_fetch_rule';
 
 const Rule = () => {
   const { project, id } = useParams();
+
+  const { isLoading, data: rule, error, isSuccess } = useFetchRule(id, project);
 
   if (!project || !id) {
     return (
       <ErrorAlert
         errorTitle="Project or Rule ID is not specified"
         errorText="Project or Rule ID not specified in the URL, please make sure you have the correct URL and try again."
-        showError />
-    );
-  }
-
-  const { isLoading, isError, data: rule, error } = useFetchRule(id, project);
-
-  if (isLoading) {
-    return <LinearProgress />;
-  }
-
-  if (isError) {
-    return (
-      <ErrorAlert
-        errorText={`An error occured while fetching the rule: ${error}`}
-        errorTitle="Failed to load rule"
-        showError />
+        showError
+      />
     );
   }
 
   return (
     <Container className='mt-1' maxWidth={false}>
       <Grid sx={{ mt: 1 }} container spacing={2}>
-        <Grid item xs={12}>
-          <RuleTopPanel project={project} ruleId={id} />
-        </Grid>
-        <Grid item xs={12}>
-          {rule?.isActive ?
-            <ClusterContextProvider project={project} clusterAlgorithm='rules' clusterId={id}>
-              <ClusterAnalysisSection />
-            </ClusterContextProvider> : <RuleArchivedMessage />}
-        </Grid>
+        {isLoading && (
+          <LinearProgress />
+        )}
+        {
+          error && (
+            <LoadErrorAlert
+              entityName="rule"
+              error={error}
+            />
+          )
+        }
+        {
+          isSuccess && rule && (
+            <>
+              <Grid item xs={12}>
+                <RuleTopPanel project={project} ruleId={id} />
+              </Grid>
+              <Grid item xs={12}>
+                {rule?.isActive ?
+                  (
+                    <ClusterContextProvider project={project} clusterAlgorithm='rules' clusterId={id}>
+                      <ClusterAnalysisSection />
+                    </ClusterContextProvider>
+                  ) : (
+                    <RuleArchivedMessage />
+                  )
+                }
+              </Grid>
+            </>
+          )
+        }
       </Grid>
     </Container>
   );
