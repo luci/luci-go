2db250cfe8c06903054d3e66739bb87be157eaf3
Aravind Vasudevan <aravindvasudev@google.com>
2023-10-31 18:47:23 +0000

    [cv] Implement post_gerrit_message longop
    This change implements "post_gerrit_message" longop which idempotently posts messages to gerrit. The core chunk of its implementation has been borrowed from "post_start_message" flow since they perform similar tasks. "post_gerrit_message" is implemented with the idea of it replacing "post_start_message" in a future CL.
    
    Unlike "post_start_message", "post_gerrit_message" doesn't add bot_data to the msg. This is because not all messages require bot_data and this makes it easier to deprecate bot_data when deprecating "post_start_message" (See crbug.com/1414898).
    
    Since "post_gerrit_message" doesn't have bot_data, it ensures idempotency by checking if the CL has the same message posted within the lifetime of this run. This is similar to how we ensure idempotency in the other parts of CV's gerrit message posting flow (See [1] and [2]). This makes it easier to replace those flows with this longop if necessary.
    
    [1] https://source.chromium.org/chromium/infra/infra/+/main:go/src/go.chromium.org/luci/cv/internal/gerrit/trigger/reset.go;l=507;drc=aa8dc3c12da2d195350848a2a87c21c2289ef4af
    [2] https://source.chromium.org/chromium/infra/infra/+/main:go/src/go.chromium.org/luci/cv/internal/run/impl/handler/submit.go;l=780;drc=aa8dc3c12da2d195350848a2a87c21c2289ef4af
    
    Bug: 1466346
    Change-Id: I63c3bdb4faba6f59329a2d3c945ade6a9feb91ab
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4977566
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Scott Lee <ddoman@chromium.org>
    Commit-Queue: Aravind Vasudevan <aravindvasudev@google.com>

---
+++ cv/internal/run/impl/longops/postgerritmessage.go
@@ -0,0 +1,227 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package longops
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	gerritpb "go.chromium.org/luci/common/proto/gerrit"
+	"go.chromium.org/luci/common/retry"
+	"go.chromium.org/luci/common/retry/transient"
+	"go.chromium.org/luci/common/sync/parallel"
+	"go.chromium.org/luci/cv/internal/common"
+	"go.chromium.org/luci/cv/internal/common/lease"
+	"go.chromium.org/luci/cv/internal/gerrit"
+	"go.chromium.org/luci/cv/internal/run"
+	"go.chromium.org/luci/cv/internal/run/eventpb"
+	"go.chromium.org/luci/cv/internal/run/impl/util"
+)
+
+// PostGerritMessageOp posts the given message to gerrit.
+//
+// PostGerritMessageOp is a single-use object.
+type PostGerritMessageOp struct {
+	// All public fields must be set.
+	*Base
+	GFactory gerrit.Factory
+	Env      *common.Env
+
+	// These private fields are set internally as implementation details.
+	lock           sync.Mutex
+	latestPostedAt time.Time
+}
+
+// Do actually posts the message.
+func (op *PostGerritMessageOp) Do(ctx context.Context) (*eventpb.LongOpCompleted, error) {
+	op.assertCalledOnce()
+
+	if op.IsCancelRequested() {
+		return &eventpb.LongOpCompleted{Status: eventpb.LongOpCompleted_CANCELLED}, nil
+	}
+
+	rcls, err := run.LoadRunCLs(ctx, op.Run.ID, op.Run.CLs)
+	if err != nil {
+		return nil, err
+	}
+
+	if op.Op.GetDeadline() == nil {
+		panic(errors.New("PostGerritMessageOp: missing deadline"))
+	}
+
+	errs := make(errors.MultiError, len(rcls))
+	poolError := parallel.WorkPool(min(len(rcls), 8), func(work chan<- func() error) {
+		for i, rcl := range rcls {
+			i, rcl := i, rcl
+			work <- func() error {
+				switch posted, err := op.doCL(ctx, rcl); {
+				case err != nil:
+					// all errors will be aggregated across all CLs below.
+					errs[i] = err
+				default:
+					op.lock.Lock()
+					if op.latestPostedAt.IsZero() || op.latestPostedAt.Before(posted) {
+						op.latestPostedAt = posted
+					}
+					op.lock.Unlock()
+				}
+				return nil
+			}
+		}
+	})
+	if poolError != nil {
+		panic(fmt.Errorf("unexpected WorkPool error %w", poolError))
+	}
+
+	var hasCancelled, hasFailed bool
+	for i, err := range errs {
+		switch {
+		case err == nil:
+		case errors.Unwrap(err) == errCancelHonored:
+			hasCancelled = true
+		default:
+			hasFailed = true
+			logging.Warningf(ctx, "failed to post gerrit message on CL %d %q: %s",
+				rcls[i].ID, rcls[i].ExternalID, err)
+		}
+	}
+	result := &eventpb.LongOpCompleted{
+		Result: &eventpb.LongOpCompleted_PostGerritMessage_{
+			PostGerritMessage: &eventpb.LongOpCompleted_PostGerritMessage{},
+		},
+	}
+	switch {
+	case hasFailed:
+		result.Status = eventpb.LongOpCompleted_FAILED
+	case hasCancelled:
+		result.Status = eventpb.LongOpCompleted_CANCELLED
+	default:
+		result.Status = eventpb.LongOpCompleted_SUCCEEDED
+		result.GetPostGerritMessage().Time = timestamppb.New(op.latestPostedAt)
+	}
+	// doCL() retries on transient failures until 30 secs before the op
+	// deadline. If any failure cases, this returns nil in error to prevent
+	// the TQ task from being retried.
+	return result, nil
+}
+
+func (op *PostGerritMessageOp) doCL(ctx context.Context, rcl *run.RunCL) (time.Time, error) {
+	ctx = logging.SetField(ctx, "cl", rcl.ID)
+	if rcl.Detail.GetGerrit() == nil {
+		panic(fmt.Errorf("CL %d is not a Gerrit CL", rcl.ID))
+	}
+	req, err := op.makeSetReviewReq(rcl)
+	if err != nil {
+		return notPosted, err
+	}
+
+	var lastNonDeadlineErr error
+	var postedAt time.Time
+	queryOpts := []gerritpb.QueryOption{gerritpb.QueryOption_MESSAGES}
+	err = retry.Retry(clock.Tag(ctx, common.LaunchRetryClockTag), op.makeRetryFactory(), func() error {
+		if op.IsCancelRequested() {
+			return errCancelHonored
+		}
+
+		var err error
+		switch postedAt, err = util.IsActionTakenOnGerritCL(ctx, op.GFactory, rcl, queryOpts, op.hasGerritMessagePosted); {
+		case postedAt != notPosted:
+			logging.Debugf(ctx, "PostGerritMessageOp: the CL already has this message at %s", postedAt)
+			return nil
+		case err == nil:
+		case errors.Unwrap(err) != context.DeadlineExceeded:
+			lastNonDeadlineErr = err
+			fallthrough
+		default:
+			return errors.Annotate(err, "failed to check if message was already posted").Err()
+		}
+		switch err = util.MutateGerritCL(ctx, op.GFactory, rcl, req, 2*time.Minute, "post-gerrit-message"); {
+		case err == nil:
+			// NOTE: to avoid another round-trip to Gerrit, use the CV time here even
+			// though it isn't the same as what Gerrit recorded.
+			postedAt = clock.Now(ctx).Truncate(time.Second)
+		case errors.Unwrap(err) != context.DeadlineExceeded:
+			lastNonDeadlineErr = err
+			fallthrough
+		default:
+			logging.Debugf(ctx, "PostGerritMessageOp: failed to mutate Gerrit CL: %s", err)
+		}
+		return err
+	}, nil)
+
+	switch {
+	case err == nil:
+		return postedAt, nil
+	case errors.Unwrap(err) == context.DeadlineExceeded && lastNonDeadlineErr != nil:
+		// if the deadline error occurred after retries, then returns the last
+		// error before the deadline error. It should be more informative than
+		// `context deadline exceeded`.
+		return notPosted, lastNonDeadlineErr
+	default:
+		return notPosted, err
+	}
+}
+
+// hasGerritMessagePosted returns when the gerrit message was posted on a CL or
+// zero time.
+func (op *PostGerritMessageOp) hasGerritMessagePosted(rcl *run.RunCL, ci *gerritpb.ChangeInfo) time.Time {
+	// Look at latest messages first for efficiency,
+	// and skip all messages which are too old.
+	clTriggeredAt := rcl.Trigger.Time.AsTime()
+	msg := strings.TrimSpace(op.Op.GetPostGerritMessage().GetMessage())
+	for i := len(ci.GetMessages()) - 1; i >= 0; i-- {
+		switch m := ci.GetMessages()[i]; {
+		case m.GetDate().AsTime().Before(clTriggeredAt):
+			return notPosted
+		case strings.TrimSpace(m.Message) == msg:
+			// This msg has been already posted to gerrit.
+			return m.GetDate().AsTime()
+		}
+	}
+
+	return notPosted
+}
+
+func (op *PostGerritMessageOp) makeSetReviewReq(rcl *run.RunCL) (*gerritpb.SetReviewRequest, error) {
+	return &gerritpb.SetReviewRequest{
+		Project:    rcl.Detail.GetGerrit().GetInfo().GetProject(),
+		Number:     rcl.Detail.GetGerrit().GetInfo().GetNumber(),
+		RevisionId: rcl.Detail.GetGerrit().GetInfo().GetCurrentRevision(),
+		Tag:        op.Run.Mode.GerritMessageTag(),
+		Notify:     gerritpb.Notify_NOTIFY_NONE,
+		Message:    op.Op.GetPostGerritMessage().GetMessage(),
+	}, nil
+}
+
+func (op *PostGerritMessageOp) makeRetryFactory() retry.Factory {
+	return lease.RetryIfLeased(transient.Only(func() retry.Iterator {
+		return &retry.ExponentialBackoff{
+			Limited: retry.Limited{
+				Delay:   100 * time.Millisecond,
+				Retries: -1, // unlimited
+			},
+			Multiplier: 2,
+			MaxDelay:   1 * time.Minute,
+		}
+	}))
+}
---
+++ cv/internal/run/impl/longops/postgerritmessage_test.go
@@ -0,0 +1,243 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package longops
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"go.chromium.org/luci/common/clock"
+	gerritpb "go.chromium.org/luci/common/proto/gerrit"
+	cfgpb "go.chromium.org/luci/cv/api/config/v2"
+	"go.chromium.org/luci/cv/internal/changelist"
+	"go.chromium.org/luci/cv/internal/common"
+	"go.chromium.org/luci/cv/internal/configs/prjcfg/prjcfgtest"
+	"go.chromium.org/luci/cv/internal/configs/validation"
+	"go.chromium.org/luci/cv/internal/cvtesting"
+	gf "go.chromium.org/luci/cv/internal/gerrit/gerritfake"
+	"go.chromium.org/luci/cv/internal/gerrit/trigger"
+	"go.chromium.org/luci/cv/internal/run"
+	"go.chromium.org/luci/cv/internal/run/eventpb"
+	"go.chromium.org/luci/cv/internal/run/impl/util"
+	"go.chromium.org/luci/gae/service/datastore"
+)
+
+func TestPostGerritMessage(t *testing.T) {
+	t.Parallel()
+
+	Convey("PostGerritMessageOp works", t, func() {
+		ct := cvtesting.Test{}
+		ctx, cancel := ct.SetUp(t)
+		defer cancel()
+
+		const (
+			lProject = "chromeos"
+			runID    = lProject + "/777-1-deadbeef"
+			gHost    = "g-review.example.com"
+			gChange1 = 111
+			gChange2 = 222
+		)
+
+		cfg := cfgpb.Config{
+			CqStatusHost: validation.CQStatusHostPublic,
+			ConfigGroups: []*cfgpb.ConfigGroup{
+				{Name: "test"},
+			},
+		}
+		prjcfgtest.Create(ctx, lProject, &cfg)
+
+		ensureCL := func(ci *gerritpb.ChangeInfo) (*changelist.CL, *run.RunCL) {
+			triggers := trigger.Find(&trigger.FindInput{ChangeInfo: ci, ConfigGroup: cfg.GetConfigGroups()[0]})
+			So(triggers.GetCqVoteTrigger(), ShouldNotBeNil)
+
+			if ct.GFake.Has(gHost, int(ci.GetNumber())) {
+				ct.GFake.MutateChange(gHost, int(ci.GetNumber()), func(c *gf.Change) {
+					c.Info = ci
+				})
+			} else {
+				ct.GFake.AddFrom(gf.WithCIs(gHost, gf.ACLRestricted(lProject), ci))
+			}
+
+			cl := changelist.MustGobID(gHost, ci.GetNumber()).MustCreateIfNotExists(ctx)
+			rcl := &run.RunCL{
+				ID:         cl.ID,
+				ExternalID: cl.ExternalID,
+				IndexedID:  cl.ID,
+				Trigger:    triggers.GetCqVoteTrigger(),
+				Run:        datastore.MakeKey(ctx, common.RunKind, string(runID)),
+				Detail: &changelist.Snapshot{
+					Kind: &changelist.Snapshot_Gerrit{Gerrit: &changelist.Gerrit{
+						Host: gHost,
+						Info: ci,
+					}},
+					ExternalUpdateTime: timestamppb.New(ct.Clock.Now()),
+				},
+			}
+			cl.Snapshot = rcl.Detail
+			cl.EVersion++
+			So(datastore.Put(ctx, cl, rcl), ShouldBeNil)
+			return cl, rcl
+		}
+
+		makeRunWithCLs := func(r *run.Run, cis ...*gerritpb.ChangeInfo) *run.Run {
+			if len(cis) == 0 {
+				panic(fmt.Errorf("at least one CL required"))
+			}
+			if r == nil {
+				r = &run.Run{}
+			}
+			r.ID = runID
+			r.Status = run.Status_RUNNING
+			for _, ci := range cis {
+				_, rcl := ensureCL(ci)
+				r.CLs = append(r.CLs, rcl.ID)
+			}
+			if r.Mode == "" {
+				r.Mode = run.FullRun
+			}
+			if r.ConfigGroupID == "" {
+				r.ConfigGroupID = prjcfgtest.MustExist(ctx, lProject).ConfigGroupIDs[0]
+			}
+			So(datastore.Put(ctx, r), ShouldBeNil)
+			return r
+		}
+
+		makeOp := func(r *run.Run) *PostGerritMessageOp {
+			return &PostGerritMessageOp{
+				Base: &Base{
+					Op: &run.OngoingLongOps_Op{
+						Deadline:        timestamppb.New(ct.Clock.Now().Add(10000 * time.Hour)),
+						CancelRequested: false,
+						Work: &run.OngoingLongOps_Op_PostGerritMessage_{
+							PostGerritMessage: &run.OngoingLongOps_Op_PostGerritMessage{
+								Message: "This is a test message.",
+							},
+						},
+					},
+					IsCancelRequested: func() bool { return false },
+					Run:               r,
+				},
+				Env:      ct.Env,
+				GFactory: ct.GFactory(),
+			}
+		}
+
+		Convey("Happy path", func() {
+			op := makeOp(makeRunWithCLs(nil, gf.CI(gChange1, gf.CQ(+2))))
+			res, err := op.Do(ctx)
+
+			So(err, ShouldBeNil)
+			So(res.GetStatus(), ShouldEqual, eventpb.LongOpCompleted_SUCCEEDED)
+			So(ct.GFake.GetChange(gHost, gChange1).Info, gf.ShouldLastMessageContain, "This is a test message.")
+		})
+
+		Convey("Happy path with multiple CLs", func() {
+			op := makeOp(makeRunWithCLs(
+				&run.Run{Mode: run.DryRun},
+				gf.CI(gChange1, gf.CQ(+1)),
+				gf.CI(gChange2, gf.CQ(+1)),
+			))
+			res, err := op.Do(ctx)
+			So(err, ShouldBeNil)
+			So(res.GetStatus(), ShouldEqual, eventpb.LongOpCompleted_SUCCEEDED)
+
+			for _, gChange := range []int{gChange1, gChange2} {
+				ci := ct.GFake.GetChange(gHost, gChange).Info
+				So(ci, gf.ShouldLastMessageContain, "This is a test message.")
+				// Should post exactly one message.
+				So(ci.GetMessages(), ShouldHaveLength, 1)
+
+				// Recorded timestamp must be approximately correct since both CLs are
+				// posted at around the same time.
+				So(res.GetPostGerritMessage().GetTime().AsTime(), ShouldHappenWithin, time.Second, ci.GetMessages()[0].GetDate().AsTime())
+			}
+		})
+
+		Convey("Best effort avoidance of duplicated messages", func() {
+			// Make two same PostGerritMessageOp objects, since they are single-use
+			// only.
+			opFirst := makeOp(makeRunWithCLs(nil, gf.CI(gChange1, gf.CQ(+2))))
+			opRetry := makeOp(makeRunWithCLs(nil, gf.CI(gChange1, gf.CQ(+2))))
+
+			// Simulate first try updating Gerrit, but somehow crashing before getting
+			// response from Gerrit.
+			_, err := opFirst.Do(ctx)
+			So(err, ShouldBeNil)
+			ci := ct.GFake.GetChange(gHost, gChange1).Info
+			So(ci, gf.ShouldLastMessageContain, "This is a test message.")
+			So(ci.GetMessages(), ShouldHaveLength, 1)
+
+			Convey("very quick retry leads to dups", func() {
+				ct.Clock.Add(time.Second)
+				res, err := opRetry.Do(ctx)
+				So(err, ShouldBeNil)
+				So(res.GetStatus(), ShouldEqual, eventpb.LongOpCompleted_SUCCEEDED)
+				So(ct.GFake.GetChange(gHost, gChange1).Info.GetMessages(), ShouldHaveLength, 2)
+				// And the timestamp isn't entirely right, but that's fine.
+				So(res.GetPostGerritMessage().GetTime().AsTime(), ShouldResemble, ct.Clock.Now().UTC().Truncate(time.Second))
+			})
+
+			Convey("later retry", func() {
+				ct.Clock.Add(util.StaleCLAgeThreshold)
+				res, err := opRetry.Do(ctx)
+				So(err, ShouldBeNil)
+				So(res.GetStatus(), ShouldEqual, eventpb.LongOpCompleted_SUCCEEDED)
+				// There should still be exactly 1 message.
+				ci := ct.GFake.GetChange(gHost, gChange1).Info
+				So(ci.GetMessages(), ShouldHaveLength, 1)
+				// and the timestamp must be exactly correct.
+				So(res.GetPostGerritMessage().GetTime().AsTime(), ShouldResemble, ci.GetMessages()[0].GetDate().AsTime())
+			})
+		})
+
+		Convey("Failures", func() {
+			op := makeOp(makeRunWithCLs(
+				&run.Run{Mode: run.DryRun},
+				gf.CI(gChange1, gf.CQ(+1)),
+			))
+			ctx, cancel := clock.WithDeadline(ctx, op.Op.Deadline.AsTime())
+			defer cancel()
+			ct.Clock.Set(op.Op.Deadline.AsTime().Add(-8 * time.Minute))
+
+			Convey("With a non transient failure", func() {
+				ct.GFake.MutateChange(gHost, gChange1, func(c *gf.Change) {
+					c.ACLs = func(_ gf.Operation, _ string) *status.Status {
+						return status.New(codes.PermissionDenied, "admin-is-angry-today")
+					}
+				})
+			})
+			Convey("With a transient failure", func() {
+				ct.GFake.MutateChange(gHost, gChange1, func(c *gf.Change) {
+					c.ACLs = func(_ gf.Operation, _ string) *status.Status {
+						return status.New(codes.Internal, "oops, temp error")
+					}
+				})
+			})
+			res, err := op.Do(ctx)
+			// Given any failure, the status should be set to FAILED,
+			// but the returned error is nil to prevent the TQ retry.
+			So(err, ShouldBeNil)
+			So(res.GetStatus(), ShouldEqual, eventpb.LongOpCompleted_FAILED)
+			So(res.GetPostGerritMessage().GetTime(), ShouldBeNil)
+			So(ct.GFake.GetChange(gHost, gChange1).Info.GetMessages(), ShouldHaveLength, 0)
+		})
+	})
+}
