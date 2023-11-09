019b435bd5ef458a33b96d39cce5b1155c1dfa9e
Vadim Shtayura <vadimsh@chromium.org>
2023-11-09 16:59:40 +0000

    [swarming] Improve JSON-valued properties.
    Store empty JSON maps as null, this is (very) slightly faster
    and also matches what Python does.
    
    Recognize zlib-compressed JSON blobs. They exist in older Swarming
    entities (and will continue to exist for ~1.5y until they are aged
    out).
    
    Store JSON as PTString (not PTBytes). The only difference is that
    Cloud Datastore UI will show them properly (it shows PTBytes in
    base64, which is annoying when the actual data is already readable
    text).
    
    R=jonahhooper@google.com, sfetisov@google.com
    
    Change-Id: I69da0b28f7945f6a7ea5ce25083e875f3057c5fc
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5015513
    Reviewed-by: Jonah Hooper <jonahhooper@google.com>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

--- swarming/server/model/utils.go
+++ swarming/server/model/utils.go
@@ -15,8 +15,12 @@
 package model
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+
+	"github.com/klauspost/compress/zlib"
 
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/gae/service/datastore"
@@ -37,23 +41,56 @@ func checkIsHex(s string, minLen int) error {
 }
 
 // ToJSONProperty serializes a value into a JSON blob property.
+//
+// Empty maps and lists are stored as nulls.
 func ToJSONProperty(val any) (datastore.Property, error) {
+	if val == nil {
+		return datastore.MkPropertyNI(nil), nil
+	}
 	blob, err := json.Marshal(val)
-	return datastore.MkPropertyNI(blob), err
+	if bytes.Equal(blob, []byte("{}")) || bytes.Equal(blob, []byte("[]")) {
+		return datastore.MkPropertyNI(nil), nil
+	}
+	return datastore.MkPropertyNI(string(blob)), err
 }
 
 // FromJSONProperty deserializes a JSON blob property into `val`.
 //
-// If the property is missing, `val` will be unchanged.
+// If the property is missing, `val` will be unchanged. Assumes `val` is a list
+// or a dict.
+//
+// Recognizes zlib-compressed properties for compatibility with older entities.
 func FromJSONProperty(prop datastore.Property, val any) error {
-	bytes, err := prop.Project(datastore.PTBytes)
+	propVal, err := prop.Project(datastore.PTBytes)
 	if err != nil {
 		return err
 	}
-	if blob, _ := bytes.([]byte); len(blob) != 0 {
+	blob, _ := propVal.([]byte)
+	if len(blob) == 0 {
+		return nil
+	}
+
+	// This seems to be an uncompressed JSON. Load it as is. Note that zlib
+	// compressed data always starts with 0x78 byte (part of the zlib header).
+	if blob[0] == '{' || blob[0] == '[' {
 		return json.Unmarshal(blob, val)
 	}
-	return nil
+
+	// If this doesn't look like JSON, this is likely an older zlib-compressed
+	// property. Try to uncompress and load it.
+	r, err := zlib.NewReader(bytes.NewBuffer(blob))
+	if err != nil {
+		return err
+	}
+	w := bytes.NewBuffer(nil)
+	if _, err := io.Copy(w, r); err != nil {
+		_ = r.Close()
+		return err
+	}
+	if err := r.Close(); err != nil {
+		return err
+	}
+	return json.Unmarshal(w.Bytes(), val)
 }
 
 // LegacyNullProperty is a placeholder for "recognizing" legacy properties.
---
+++ swarming/server/model/utils_test.go
@@ -0,0 +1,100 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package model
+
+import (
+	"bytes"
+	"compress/zlib"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"go.chromium.org/luci/gae/service/datastore"
+)
+
+func TestToJSONProperty(t *testing.T) {
+	t.Parallel()
+
+	Convey("With a value", t, func() {
+		p, err := ToJSONProperty(map[string]string{"a": "b"})
+		So(err, ShouldBeNil)
+		So(p.Type(), ShouldEqual, datastore.PTString)
+		So(p.Value().(string), ShouldEqual, `{"a":"b"}`)
+	})
+
+	Convey("With empty map", t, func() {
+		p, err := ToJSONProperty(map[string]string{})
+		So(err, ShouldBeNil)
+		So(p.Type(), ShouldEqual, datastore.PTNull)
+	})
+
+	Convey("With empty list", t, func() {
+		p, err := ToJSONProperty([]string{})
+		So(err, ShouldBeNil)
+		So(p.Type(), ShouldEqual, datastore.PTNull)
+	})
+
+	Convey("With nil", t, func() {
+		p, err := ToJSONProperty(nil)
+		So(err, ShouldBeNil)
+		So(p.Type(), ShouldEqual, datastore.PTNull)
+	})
+}
+
+func TestFromJSONProperty(t *testing.T) {
+	t.Parallel()
+
+	Convey("Null", t, func() {
+		var v map[string]string
+		So(FromJSONProperty(datastore.MkProperty(nil), &v), ShouldBeNil)
+		So(v, ShouldResemble, map[string]string(nil))
+	})
+
+	Convey("Empty", t, func() {
+		var v map[string]string
+		So(FromJSONProperty(datastore.MkProperty(""), &v), ShouldBeNil)
+		So(v, ShouldResemble, map[string]string(nil))
+	})
+
+	Convey("Bytes", t, func() {
+		var v map[string]string
+		So(FromJSONProperty(datastore.MkProperty([]byte(`{"a":"b"}`)), &v), ShouldBeNil)
+		So(v, ShouldResemble, map[string]string{"a": "b"})
+	})
+
+	Convey("String", t, func() {
+		var v map[string]string
+		So(FromJSONProperty(datastore.MkProperty(`{"a":"b"}`), &v), ShouldBeNil)
+		So(v, ShouldResemble, map[string]string{"a": "b"})
+	})
+
+	Convey("Compressed", t, func() {
+		var v map[string]string
+		So(FromJSONProperty(datastore.MkProperty(deflate([]byte(`{"a":"b"}`))), &v), ShouldBeNil)
+		So(v, ShouldResemble, map[string]string{"a": "b"})
+	})
+}
+
+func deflate(blob []byte) []byte {
+	out := bytes.NewBuffer(nil)
+	w := zlib.NewWriter(out)
+	if _, err := w.Write(blob); err != nil {
+		panic(err)
+	}
+	if err := w.Close(); err != nil {
+		panic(err)
+	}
+	return out.Bytes()
+}
