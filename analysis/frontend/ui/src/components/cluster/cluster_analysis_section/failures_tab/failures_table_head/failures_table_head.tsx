c584fbabf3e62cb4c786e58e534ce69ed6184b7d
Matthew Warton <mwarton@google.com>
2023-11-15 03:54:16 +0000

    analysis: Add tooltips to column headers on the recent failures table
    Change-Id: I13f633dd98be4c558b1dd2c3b258a1dfd431ce51
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5028377
    Commit-Queue: Patrick Meiring <meiring@google.com>
    Auto-Submit: Matthew Warton <mwarton@google.com>
    Reviewed-by: Patrick Meiring <meiring@google.com>

--- analysis/frontend/ui/src/components/cluster/cluster_analysis_section/failures_tab/failures_table_head/failures_table_head.tsx
+++ analysis/frontend/ui/src/components/cluster/cluster_analysis_section/failures_tab/failures_table_head/failures_table_head.tsx
@@ -19,6 +19,7 @@ import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel';
 
 import { MetricName } from '@/tools/failures_tools';
+import { Tooltip } from '@mui/material';
 
 const NarrowTableCell = styled(TableCell)(() => ({
   [`&.${tableCellClasses.root}`]: {
@@ -44,11 +45,31 @@ const FailuresTableHead = ({
           sx={{ padding: '10px', width: '40px' }}
         >
         </NarrowTableCell>
-        <NarrowTableCell sx={{ width: '160px' }}>Build</NarrowTableCell>
-        <NarrowTableCell sx={{ width: '100px' }}>Verdict</NarrowTableCell>
-        <NarrowTableCell>Variant</NarrowTableCell>
-        <NarrowTableCell sx={{ width: '80px' }}>CL(s)</NarrowTableCell>
-        <NarrowTableCell sx={{ width: '100px' }}>Presubmit Run</NarrowTableCell>
+        <NarrowTableCell sx={{ width: '160px' }}>
+          <Tooltip title="The ID of the top-level build containing this test result.  The link views this test result in the top-level build.">
+            <span>Build</span>
+          </Tooltip>
+        </NarrowTableCell>
+        <NarrowTableCell sx={{ width: '100px' }}>
+          <Tooltip title={<>The combined result of all retries of this test variant in this build.  See <a href="http://go/resultdb-concepts#test-verdict" target="_blank">go/resultdb-concepts</a> for more details.</>}>
+            <span>Verdict</span>
+          </Tooltip>
+        </NarrowTableCell>
+        <NarrowTableCell>
+          <Tooltip title={<>The key value pairs that define the configuration in which the test was run.  Any variant keys that are used for grouping will be omitted. See <a href="http://go/resultdb-concepts#test-variant" target="_blank">go/resultdb-concepts</a> for more details.</>}>
+            <span>Variant</span>
+          </Tooltip>
+        </NarrowTableCell>
+        <NarrowTableCell sx={{ width: '80px' }}>
+          <Tooltip title="The Gerrit CL ID and patchset number for each CL that was patched in when the test was run.  Will be empty if none were patched in (e.g. postsubmit tests).">
+            <span>CL(s)</span>
+          </Tooltip>
+        </NarrowTableCell>
+        <NarrowTableCell sx={{ width: '100px' }}>
+          <Tooltip title="The status of the LUCI CV presubmit run that contained this test result.  Submitted means that the run was successful and that the CL was submitted based on this run.">
+            <span>Presubmit Run</span>
+          </Tooltip>
+        </NarrowTableCell>
         <NarrowTableCell
           sortDirection={sortMetric === 'presubmitRejects' ? (isAscending ? 'asc' : 'desc') : false}
           sx={{ cursor: 'pointer', width: '80px' }}>
@@ -57,7 +78,9 @@ const FailuresTableHead = ({
             active={sortMetric === 'presubmitRejects'}
             direction={isAscending ? 'asc' : 'desc'}
             onClick={() => toggleSort('presubmitRejects')}>
-            User CLs Failed Presubmit
+            <Tooltip title="The number of user authored CLs that have been failed by these test results.  The group totals are a count of distinct CLs, so if there are multiple tests that failed a single CL, the multiple tests will only count as 1.">
+              <span>User CLs Failed Presubmit</span>
+            </Tooltip>
           </TableSortLabel>
         </NarrowTableCell>
         <NarrowTableCell
@@ -67,7 +90,9 @@ const FailuresTableHead = ({
             active={sortMetric === 'invocationFailures'}
             direction={isAscending ? 'asc' : 'desc'}
             onClick={() => toggleSort('invocationFailures')}>
-            Builds Failed
+            <Tooltip title='The number builds that have been failed by these test results.  The group totals are a count of distinct builds, so if there are multiple tests that failed a single build, the multiple tests will only count as 1.'>
+              <span>Builds Failed</span>
+            </Tooltip>
           </TableSortLabel>
         </NarrowTableCell>
         <NarrowTableCell
@@ -77,7 +102,9 @@ const FailuresTableHead = ({
             active={sortMetric === 'criticalFailuresExonerated'}
             direction={isAscending ? 'asc' : 'desc'}
             onClick={() => toggleSort('criticalFailuresExonerated')}>
-            Presubmit-Blocking Failures Exonerated
+            <Tooltip title="The number failed test results that would have blocked a presubmit run that have been exonerated.  The blocking status is independent of whether the run actually passed or failed.  The group totals are a simple sum of the individual failures.">
+              <span>Presubmit-Blocking Failures Exonerated</span>
+            </Tooltip>
           </TableSortLabel>
         </NarrowTableCell>
         <NarrowTableCell
@@ -87,7 +114,9 @@ const FailuresTableHead = ({
             active={sortMetric === 'failures'}
             direction={isAscending ? 'asc' : 'desc'}
             onClick={() => toggleSort('failures')}>
-            Total Failures
+            <Tooltip title="The total number of failed test results.  The group totals are a simple sum of the individual failures.">
+              <span>Total Failures</span>
+            </Tooltip>
           </TableSortLabel>
         </NarrowTableCell>
         <NarrowTableCell
@@ -97,7 +126,9 @@ const FailuresTableHead = ({
             active={sortMetric === 'latestFailureTime'}
             direction={isAscending ? 'asc' : 'desc'}
             onClick={() => toggleSort('latestFailureTime')}>
-            Latest Failure Time
+            <Tooltip title="The time of the latest failure that has been ingested and analyzed. The main source of latency is waiting for all of the tests in a run to be complete.">
+              <span>Latest Failure Time</span>
+            </Tooltip>
           </TableSortLabel>
         </NarrowTableCell>
       </TableRow>
