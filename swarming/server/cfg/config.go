bb056303393edd6892352eb8ab8719f63ce7e423
Vadim Shtayura <vadimsh@chromium.org>
2023-11-17 18:33:50 +0000

    [swarming] Implement config datastore caching logic.
    Configs are fetched from LUCI Config via a cron job and are
    stored in the datastore, along with their "digest" (a hash of
    content).
    
    Each backend process loads the initial copy of the configs from
    the datastore and periodically looks in the datastore at the most
    recently stored digest. If it is different, it refetches full
    configs from the datastore.
    
    Few complications:
    * Need a digest because revision can change even when configs are
      unchanged. We can theoretically use revision as well and just
      do some unnecessary reloads, but adding a digest isn't hard.
    * Need to handle cases related to new service bootstrap, when
      there's no config yet.
    
    R=jonahhooper@google.com, sfetisov@google.com
    
    Change-Id: Ib7e3ab388425fa5631c0aeefbfaf7c3543729379
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5034561
    Reviewed-by: Sergey Fetisov <sfetisov@google.com>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

--- swarming/server/cfg/config.go
+++ swarming/server/cfg/config.go
@@ -16,6 +16,45 @@ package cfg
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"regexp"
+	"sort"
+	"time"
+
+	"google.golang.org/protobuf/encoding/prototext"
+	"google.golang.org/protobuf/proto"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/config"
+	"go.chromium.org/luci/config/cfgclient"
+	"go.chromium.org/luci/config/validation"
+	"go.chromium.org/luci/gae/service/datastore"
+
+	configpb "go.chromium.org/luci/swarming/proto/config"
+	"go.chromium.org/luci/swarming/server/cfg/internalcfgpb"
+)
+
+// Individually recognized config files.
+const (
+	settingsCfg = "settings.cfg"
+	poolsCfg    = "pools.cfg"
+	botsCfg     = "bots.cfg"
+)
+
+// hookScriptRe matches paths like `scripts/hooks.py`. It intentionally doesn't
+// match subdirectories of `scripts/` since they contain unit tests the server
+// doesn't care about.
+var hookScriptRe = regexp.MustCompile(`scripts/[^/]+\.py`)
+
+const (
+	// A pseudo-revision of an empty config.
+	emptyRev = "0000000000000000000000000000000000000000"
+	// A digest of a default config (calculated in the test).
+	emptyDigest = "0NpkIis/WMci8PDKkLD3PB/t8B86nbBVjyD59iosjOM"
 )
 
 // Config is an immutable queryable representation of Swarming server configs.
@@ -25,10 +64,15 @@ import (
 type Config struct {
 	// Revision is the config repo commit the config was loaded from.
 	Revision string
-	// ViewURL is an URL to the config repo commit.
-	ViewURL string
+	// Digest is derived exclusively from the configs content.
+	Digest string
+	// Fetched is when the stored config was fetched from LUIC Config.
+	Fetched time.Time
+	// Refreshed is when the process config was fetched from the datastore.
+	Refreshed time.Time
 
-	// TODO: add the actual config
+	// TODO: Add the rest of configs.
+	settings *configpb.SettingsCfg
 }
 
 // UpdateConfigs fetches the most recent server configs from LUCI Config and
@@ -36,10 +80,204 @@ type Config struct {
 //
 // Called from a cron job once a minute.
 func UpdateConfigs(ctx context.Context) error {
-	// TODO: Fetch bots.cfg, revalidate via validateBotsCfg.
-	// TODO: Fetch pools.cfg, revalidate via validatePoolsCfg.
-	// TODO: Fetch settings.cfg, revalidate via validateSettingsCfg.
+	// Fetch known config files and everything that looks like a hooks script.
+	files, err := cfgclient.Client(ctx).GetConfigs(ctx, "services/${appid}",
+		func(path string) bool {
+			return path == settingsCfg || path == poolsCfg || path == botsCfg || hookScriptRe.MatchString(path)
+		}, false)
+	if err != nil && !errors.Is(err, config.ErrNoConfig) {
+		return errors.Annotate(err, "failed to fetch the most recent configs from LUCI Config").Err()
+	}
+
+	// Get the revision, to check if we have seen it already.
+	var revision string
+	if len(files) == 0 {
+		// This can happen in new deployments.
+		logging.Warningf(ctx, "There are no configuration files in LUCI Config")
+		revision = emptyRev
+	} else {
+		// Per GetConfigs logic, all files are at the same revision. Pick the first.
+		for _, cfg := range files {
+			revision = cfg.Revision
+			break
+		}
+	}
+
+	// No need to do anything if already processed this revision.
+	lastRev := &configBundleRev{Key: configBundleRevKey(ctx)}
+	switch err := datastore.Get(ctx, lastRev); {
+	case err == nil && lastRev.Revision == revision:
+		logging.Infof(ctx, "Configs are already up-to-date at rev %s (fetched %s ago)", lastRev.Revision, clock.Since(ctx, lastRev.Fetched).Round(time.Second))
+		return nil
+	case err == nil && lastRev.Revision != revision:
+		logging.Infof(ctx, "Configs revision changed %s => %s", lastRev.Revision, revision)
+	case errors.Is(err, datastore.ErrNoSuchEntity):
+		logging.Infof(ctx, "First config import ever at rev %s", revision)
+	case err != nil:
+		return errors.Annotate(err, "failed to fetch the latest processed revision from datastore").Err()
+	}
+
+	// Parse and re-validate.
+	bundle, err := parseAndValidateConfigs(ctx, revision, files)
+	if err != nil {
+		return errors.Annotate(err, "bad configs at rev %s", revision).Err()
+	}
+
+	// Store as the new authoritative config.
+	err = datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+		now := clock.Now(ctx).UTC()
+		return datastore.Put(ctx,
+			&configBundle{
+				Key:      configBundleKey(ctx),
+				Revision: bundle.Revision,
+				Digest:   bundle.Digest,
+				Fetched:  now,
+				Bundle:   bundle,
+			},
+			&configBundleRev{
+				Key:      configBundleRevKey(ctx),
+				Revision: bundle.Revision,
+				Digest:   bundle.Digest,
+				Fetched:  now,
+			})
+	}, nil)
+
+	if err != nil {
+		return errors.Annotate(err, "failed to store configs at rev %s in the datastore", bundle.Revision).Err()
+	}
+
+	logging.Infof(ctx, "Stored configs at rev %s (digest %s)", bundle.Revision, bundle.Digest)
 	return nil
+}
+
+// defaultConfigs returns default config protos used on an "empty" server.
+func defaultConfigs() *internalcfgpb.ConfigBundle {
+	// TODO: Figure out actually good default values.
+	return &internalcfgpb.ConfigBundle{
+		Revision: emptyRev,
+		Digest:   emptyDigest,
+		Settings: &configpb.SettingsCfg{},
+		Pools:    &configpb.PoolsCfg{},
+		Bots:     &configpb.BotsCfg{},
+		Scripts:  map[string]string{},
+	}
+}
+
+// parseAndValidateConfigs parses config files fetched from LUCI Config, if any.
+func parseAndValidateConfigs(ctx context.Context, rev string, files map[string]config.Config) (*internalcfgpb.ConfigBundle, error) {
+	bundle := defaultConfigs()
+	bundle.Revision = rev
+
+	// Parse and validate files individually.
+	if err := parseAndValidate(ctx, files, settingsCfg, bundle.Settings, validateSettingsCfg); err != nil {
+		return nil, err
+	}
+	if err := parseAndValidate(ctx, files, poolsCfg, bundle.Pools, validatePoolsCfg); err != nil {
+		return nil, err
+	}
+	if err := parseAndValidate(ctx, files, botsCfg, bundle.Bots, validateBotsCfg); err != nil {
+		return nil, err
+	}
+
+	// Make sure all referenced hook scripts are actually present. Collect them.
+	for idx, group := range bundle.Bots.BotGroup {
+		if group.BotConfigScript == "" {
+			continue
+		}
+		if _, ok := bundle.Scripts[group.BotConfigScript]; ok {
+			continue
+		}
+		body, ok := files["scripts/"+group.BotConfigScript]
+		if !ok {
+			return nil, errors.Reason("bot group #%d refers to undefined bot config script %q", idx+1, group.BotConfigScript).Err()
+		}
+		bundle.Scripts[group.BotConfigScript] = body.Content
+	}
+
+	// Derive the digest based exclusively on configs content, regardless of the
+	// revision. The revision can change even if configs are unchanged. The digest
+	// is used to know when configs are changing **for real**.
+	visit := []string{settingsCfg, poolsCfg, botsCfg}
+	for script := range bundle.Scripts {
+		visit = append(visit, "scripts/"+script)
+	}
+	sort.Strings(visit)
+	h := sha256.New()
+	_, _ = fmt.Fprintf(h, "version 1\n")
+	for _, path := range visit {
+		_, _ = fmt.Fprintf(h, "%s\n%d\n%s\n", path, len(files[path].Content), files[path].Content)
+	}
+	bundle.Digest = base64.RawStdEncoding.EncodeToString(h.Sum(nil))
+
+	return bundle, nil
+}
+
+// parseAndValidate parses and validated one text proto config file.
+func parseAndValidate[T any, TP interface {
+	*T
+	proto.Message
+}](ctx context.Context,
+	files map[string]config.Config,
+	path string,
+	cfg *T,
+	validate func(ctx *validation.Context, t *T),
+) error {
+	// Parse it if it is present. Otherwise use the default value of `cfg`.
+	if body := files[path]; body.Content != "" {
+		if err := prototext.Unmarshal([]byte(body.Content), TP(cfg)); err != nil {
+			return errors.Annotate(err, "%s", path).Err()
+		}
+	} else {
+		logging.Warningf(ctx, "There's no %s config, using default", path)
+	}
+	// Pass through the validation, abort on fatal errors, allow warnings.
+	valCtx := validation.Context{Context: ctx}
+	validate(&valCtx, cfg)
+	if err := valCtx.Finalize(); err != nil {
+		var valErr *validation.Error
+		if errors.As(err, &valErr) {
+			blocking := valErr.WithSeverity(validation.Blocking)
+			if blocking != nil {
+				return errors.Annotate(blocking, "%s", path).Err()
+			}
+		} else {
+			return errors.Annotate(err, "%s", path).Err()
+		}
+	}
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// configBundle is an entity that stores latest configs as compressed protos.
+type configBundle struct {
+	_ datastore.PropertyMap `gae:"-,extra"`
+
+	Key      *datastore.Key              `gae:"$key"`
+	Revision string                      `gae:",noindex"`
+	Digest   string                      `gae:",noindex"`
+	Fetched  time.Time                   `gae:",noindex"`
+	Bundle   *internalcfgpb.ConfigBundle `gae:",zstd"`
+}
+
+// configBundleRev just stores the metadata for faster fetches.
+type configBundleRev struct {
+	_ datastore.PropertyMap `gae:"-,extra"`
+
+	Key      *datastore.Key `gae:"$key"`
+	Revision string         `gae:",noindex"`
+	Digest   string         `gae:",noindex"`
+	Fetched  time.Time      `gae:",noindex"`
+}
+
+// configBundleKey is a key of the singleton configBundle entity.
+func configBundleKey(ctx context.Context) *datastore.Key {
+	return datastore.NewKey(ctx, "ConfigBundle", "", 1, nil)
+}
+
+// configBundleRevKey is a key of the singleton configBundleRev entity.
+func configBundleRevKey(ctx context.Context) *datastore.Key {
+	return datastore.NewKey(ctx, "ConfigBundleRev", "", 1, configBundleKey(ctx))
 }
 
 // fetchFromDatastore fetches the config from the datastore.
@@ -49,6 +287,63 @@ func UpdateConfigs(ctx context.Context) error {
 // If `cur` is not nil its (immutable) parts may be used to construct the
 // new Config in case they didn't change.
 func fetchFromDatastore(ctx context.Context, cur *Config) (*Config, error) {
+	// If already have a config, check if we really need to reload it.
+	if cur != nil {
+		rev := &configBundleRev{Key: configBundleRevKey(ctx)}
+		switch err := datastore.Get(ctx, rev); {
+		case errors.Is(err, datastore.ErrNoSuchEntity):
+			rev.Revision = emptyRev
+			rev.Digest = emptyDigest
+		case err != nil:
+			return nil, errors.Annotate(err, "fetching configBundleRev").Err()
+		}
+		if cur.Digest == rev.Digest {
+			clone := *cur
+			clone.Revision = rev.Revision
+			clone.Fetched = rev.Fetched
+			clone.Refreshed = clock.Now(ctx).UTC()
+			return &clone, nil
+		}
+	}
+
+	// Either have no config or the one in the datastore is different. Get it.
+	bundle := &configBundle{Key: configBundleKey(ctx)}
+	switch err := datastore.Get(ctx, bundle); {
+	case errors.Is(err, datastore.ErrNoSuchEntity):
+		bundle.Revision = emptyRev
+		bundle.Digest = emptyDigest
+		bundle.Bundle = defaultConfigs()
+	case err != nil:
+		return nil, errors.Annotate(err, "fetching configBundle").Err()
+	}
+
+	// Transform config protos into data structures optimized for config queries.
+	// This should never really fail, since we store only validated configs. If
+	// this fails, the entire service will eventually go offline since new
+	// processes won't be able to load initial copy of the config (while old
+	// processes will keep using last known good copies, until eventually they
+	// all terminate).
+	cfg, err := buildQueriableConfig(ctx, bundle)
+	if err != nil {
+		logging.Errorf(ctx,
+			"Broken config in the datastore at rev %s (digest %s, fetched %s ago): %s",
+			bundle.Revision, bundle.Digest, clock.Since(ctx, bundle.Fetched), err,
+		)
+		return nil, errors.Annotate(err, "broken config in the datastore").Err()
+	}
+	logging.Infof(ctx, "Loaded configs at rev %s", cfg.Revision)
+	return cfg, nil
+}
+
+// buildQueriableConfig transforms config protos into data structures optimized
+// for config queries.
+func buildQueriableConfig(ctx context.Context, ent *configBundle) (*Config, error) {
 	// TODO: Implement.
-	return &Config{}, nil
+	return &Config{
+		Revision:  ent.Revision,
+		Digest:    ent.Digest,
+		Fetched:   ent.Fetched,
+		Refreshed: clock.Now(ctx).UTC(),
+		settings:  ent.Bundle.Settings,
+	}, nil
 }
---
+++ swarming/server/cfg/config_test.go
@@ -0,0 +1,325 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cfg
+
+import (
+	"context"
+	"testing"
+
+	"go.chromium.org/luci/common/clock/testclock"
+	"go.chromium.org/luci/config"
+	"go.chromium.org/luci/config/cfgclient"
+	cfgmem "go.chromium.org/luci/config/impl/memory"
+	"go.chromium.org/luci/gae/impl/memory"
+	"go.chromium.org/luci/gae/service/datastore"
+
+	configpb "go.chromium.org/luci/swarming/proto/config"
+	"go.chromium.org/luci/swarming/server/cfg/internalcfgpb"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "go.chromium.org/luci/common/testing/assertions"
+)
+
+func TestUpdateConfigs(t *testing.T) {
+	t.Parallel()
+
+	Convey("With test context", t, func() {
+		ctx := testCtx()
+
+		call := func(files cfgmem.Files) error {
+			return UpdateConfigs(cfgclient.Use(ctx, cfgmem.New(map[config.Set]cfgmem.Files{
+				"services/${appid}": files,
+			})))
+		}
+
+		fetchDS := func() *configBundle {
+			bundle := &configBundle{Key: configBundleKey(ctx)}
+			rev := &configBundleRev{Key: configBundleRevKey(ctx)}
+			So(datastore.Get(ctx, bundle, rev), ShouldBeNil)
+			So(bundle.Revision, ShouldEqual, rev.Revision)
+			So(bundle.Digest, ShouldEqual, rev.Digest)
+			So(bundle.Fetched.Equal(rev.Fetched), ShouldBeTrue)
+			return bundle
+		}
+
+		// Start with no configs. Should store default empty config.
+		So(call(nil), ShouldBeNil)
+		So(fetchDS().Bundle, ShouldResembleProto, defaultConfigs())
+		// Call it again. Still no configs.
+		So(call(nil), ShouldBeNil)
+		So(fetchDS().Bundle, ShouldResembleProto, defaultConfigs())
+
+		configSet1 := cfgmem.Files{"bots.cfg": `trusted_dimensions: "boo"`}
+		configBundle1 := &internalcfgpb.ConfigBundle{
+			Revision: "52c28bde9e27457d3b6ef76d73c3eca72289fe81",
+			Digest:   "mdpudjzSaYvrwIy8F6v6+5FmxgaocvTSUGzFECuyFv8",
+			Settings: defaultConfigs().Settings,
+			Pools:    defaultConfigs().Pools,
+			Bots: &configpb.BotsCfg{
+				TrustedDimensions: []string{"boo"},
+			},
+		}
+
+		// A config appears. It is stored.
+		So(call(configSet1), ShouldBeNil)
+		So(fetchDS().Bundle, ShouldResembleProto, configBundle1)
+
+		Convey("Updates good configs", func() {
+			configSet2 := cfgmem.Files{"bots.cfg": `trusted_dimensions: "blah"`}
+			configBundle2 := &internalcfgpb.ConfigBundle{
+				Revision: "3c79c4e6bd845fbeb4715d6fb5d4613084a345d9",
+				Digest:   "Y/TeCiZzHF8BByQG3zBho2Mqv+L+k9SlePjOomKdpmw",
+				Settings: defaultConfigs().Settings,
+				Pools:    defaultConfigs().Pools,
+				Bots: &configpb.BotsCfg{
+					TrustedDimensions: []string{"blah"},
+				},
+			}
+
+			// Another config appears. It is store.
+			So(call(configSet2), ShouldBeNil)
+			So(fetchDS().Bundle, ShouldResembleProto, configBundle2)
+			// Call it again, the same config is there.
+			So(call(configSet1), ShouldBeNil)
+			So(fetchDS().Bundle, ShouldResembleProto, configBundle1)
+		})
+
+		Convey("Ignores bad configs", func() {
+			// A broken config appears. The old valid config is left unchanged.
+			So(call(cfgmem.Files{"bots.cfg": `what is this`}), ShouldNotBeNil)
+			So(fetchDS().Bundle, ShouldResembleProto, configBundle1)
+		})
+	})
+}
+
+func TestParseAndValidateConfigs(t *testing.T) {
+	t.Parallel()
+
+	call := func(files map[string]string) (*internalcfgpb.ConfigBundle, error) {
+		cfgs := make(map[string]config.Config, len(files))
+		for k, v := range files {
+			cfgs[k] = config.Config{Content: v}
+		}
+		return parseAndValidateConfigs(context.Background(), "rev", cfgs)
+	}
+
+	Convey("Good", t, func() {
+		bundle, err := call(map[string]string{
+			"settings.cfg": `
+				google_analytics: "boo"
+			`,
+			"pools.cfg": `
+				pool {
+					name: "blah"
+				}
+			`,
+			"bots.cfg": `
+				bot_group {
+					bot_id: "id1"
+					bot_config_script: "script1.py"
+				}
+				bot_group {
+					bot_id: "id2"
+					bot_config_script: "script1.py"
+				}
+				bot_group {
+					bot_id: "id3"
+					bot_config_script: "script2.py"
+				}
+			`,
+			"scripts/script1.py": "script1 body",
+			"scripts/script2.py": "script2 body",
+			"scripts/ignored.py": "ignored",
+		})
+		So(err, ShouldBeNil)
+		So(bundle, ShouldResembleProto, &internalcfgpb.ConfigBundle{
+			Revision: "rev",
+			Digest:   "QFraUvvnl/Ew1tL4pxwOQmnFLMlJH15UVvrtptR8kcM",
+			Settings: &configpb.SettingsCfg{
+				GoogleAnalytics: "boo",
+			},
+			Pools: &configpb.PoolsCfg{
+				Pool: []*configpb.Pool{
+					{Name: []string{"blah"}},
+				},
+			},
+			Bots: &configpb.BotsCfg{
+				BotGroup: []*configpb.BotGroup{
+					{BotId: []string{"id1"}, BotConfigScript: "script1.py"},
+					{BotId: []string{"id2"}, BotConfigScript: "script1.py"},
+					{BotId: []string{"id3"}, BotConfigScript: "script2.py"},
+				},
+			},
+			Scripts: map[string]string{
+				"script1.py": "script1 body",
+				"script2.py": "script2 body",
+			},
+		})
+	})
+
+	Convey("Empty", t, func() {
+		empty := defaultConfigs()
+		empty.Revision = "rev"
+		bundle, err := call(nil)
+		So(err, ShouldBeNil)
+		So(bundle, ShouldResembleProto, empty)
+	})
+
+	Convey("Broken", t, func() {
+		_, err := call(map[string]string{"bots.cfg": "what is this"})
+		So(err, ShouldErrLike, "bots.cfg: proto")
+	})
+
+	Convey("Missing scripts", t, func() {
+		_, err := call(map[string]string{
+			"bots.cfg": `
+				bot_group {
+					bot_id: "id1"
+					bot_config_script: "script1.py"
+				}
+				bot_group {
+					bot_id: "id2"
+					bot_config_script: "missing.py"
+				}
+			`,
+			"scripts/script1.py": "script1 body",
+			"scripts/ignored.py": "ignored",
+		})
+		So(err, ShouldErrLike, `bot group #2 refers to undefined bot config script "missing.py"`)
+	})
+}
+
+func TestFetchFromDatastore(t *testing.T) {
+	t.Parallel()
+
+	Convey("With test context", t, func() {
+		ctx := testCtx()
+
+		update := func(files cfgmem.Files) error {
+			return UpdateConfigs(cfgclient.Use(ctx, cfgmem.New(map[config.Set]cfgmem.Files{
+				"services/${appid}": files,
+			})))
+		}
+
+		fetch := func(cur *Config) (*Config, error) {
+			return fetchFromDatastore(ctx, cur)
+		}
+
+		Convey("Empty datastore", func() {
+			cfg, err := fetch(nil)
+			So(err, ShouldBeNil)
+			So(cfg.Revision, ShouldEqual, emptyRev)
+			So(cfg.Digest, ShouldEqual, emptyDigest)
+			So(cfg.settings, ShouldResembleProto, defaultConfigs().Settings)
+		})
+
+		Convey("Default configs in datastore", func() {
+			// A cron job runs and discovers no configs.
+			So(update(nil), ShouldBeNil)
+
+			// Fetches initial copy of default config.
+			cfg, err := fetch(nil)
+			So(err, ShouldBeNil)
+			So(cfg.Revision, ShouldEqual, emptyRev)
+			So(cfg.Digest, ShouldEqual, emptyDigest)
+			So(cfg.settings, ShouldResembleProto, defaultConfigs().Settings)
+
+			// Does nothing, there's still no real config.
+			cfg, err = fetch(cfg)
+			So(err, ShouldBeNil)
+			So(cfg.Revision, ShouldEqual, emptyRev)
+			So(cfg.Digest, ShouldEqual, emptyDigest)
+			So(cfg.settings, ShouldResembleProto, defaultConfigs().Settings)
+
+			// A real config appears.
+			So(update(cfgmem.Files{"settings.cfg": `google_analytics: "boo"`}), ShouldBeNil)
+
+			// It replaces the empty config when fetched.
+			cfg, err = fetch(cfg)
+			So(err, ShouldBeNil)
+			So(cfg.Revision, ShouldEqual, "bcf7460a098890cc7efc8eda1c8279658ec25eb3")
+			So(cfg.Digest, ShouldEqual, "xv7iFT37ovx5Qc9kjYK0kEa3Eq47cNNC0ZbEd61eOYQ")
+			So(cfg.settings, ShouldResembleProto, &configpb.SettingsCfg{
+				GoogleAnalytics: "boo",
+			})
+
+			// Suddenly the config is completely gone.
+			So(datastore.Delete(ctx, configBundleKey(ctx), configBundleRevKey(ctx)), ShouldBeNil)
+
+			// The default config is used again.
+			cfg, err = fetch(cfg)
+			So(err, ShouldBeNil)
+			So(cfg.Revision, ShouldEqual, emptyRev)
+			So(cfg.Digest, ShouldEqual, emptyDigest)
+			So(cfg.settings, ShouldResembleProto, defaultConfigs().Settings)
+		})
+
+		Convey("Some configs in datastore", func() {
+			// Some initial config.
+			So(update(cfgmem.Files{"settings.cfg": `google_analytics: "boo"`}), ShouldBeNil)
+
+			// Fetches initial copy of this config.
+			cfg, err := fetch(nil)
+			So(err, ShouldBeNil)
+			So(cfg.Revision, ShouldEqual, "bcf7460a098890cc7efc8eda1c8279658ec25eb3")
+			So(cfg.Digest, ShouldEqual, "xv7iFT37ovx5Qc9kjYK0kEa3Eq47cNNC0ZbEd61eOYQ")
+			So(cfg.settings, ShouldResembleProto, &configpb.SettingsCfg{
+				GoogleAnalytics: "boo",
+			})
+
+			// Nothing change. The same config is returned.
+			cfg, err = fetch(cfg)
+			So(err, ShouldBeNil)
+			So(cfg.Revision, ShouldEqual, "bcf7460a098890cc7efc8eda1c8279658ec25eb3")
+			So(cfg.Digest, ShouldEqual, "xv7iFT37ovx5Qc9kjYK0kEa3Eq47cNNC0ZbEd61eOYQ")
+			So(cfg.settings, ShouldResembleProto, &configpb.SettingsCfg{
+				GoogleAnalytics: "boo",
+			})
+
+			// A noop config change happens.
+			So(update(cfgmem.Files{
+				"settings.cfg": `google_analytics: "boo"`,
+				"unrelated":    "change",
+			}), ShouldBeNil)
+
+			// Has the new revision, but the same digest and the exact same body.
+			prev := cfg.settings
+			cfg, err = fetch(cfg)
+			So(err, ShouldBeNil)
+			So(cfg.Revision, ShouldEqual, "725455c37c09b5dfa3dc31b47c9f787d555cbfb3")
+			So(cfg.Digest, ShouldEqual, "xv7iFT37ovx5Qc9kjYK0kEa3Eq47cNNC0ZbEd61eOYQ")
+			So(cfg.settings == prev, ShouldBeTrue) // equal as pointers
+
+			// A real config change happens.
+			So(update(cfgmem.Files{"settings.cfg": `google_analytics: "blah"`}), ShouldBeNil)
+
+			// Causes the config update.
+			cfg, err = fetch(cfg)
+			So(err, ShouldBeNil)
+			So(cfg.Revision, ShouldEqual, "3a7123badfd5426f2a75932433f99b0aee8baf9b")
+			So(cfg.Digest, ShouldEqual, "+JawsgfwWonAz8wFE/iR2AcdVmMhK3OwFbNSgCjBiRo")
+			So(cfg.settings, ShouldResembleProto, &configpb.SettingsCfg{
+				GoogleAnalytics: "blah",
+			})
+		})
+	})
+}
+
+func testCtx() context.Context {
+	ctx := context.Background()
+	ctx, _ = testclock.UseTime(ctx, testclock.TestRecentTimeUTC)
+	ctx = memory.Use(ctx)
+	return ctx
+}
