170d021f16b57ae69db80b97cea03edc3b129914
Weiwei Lin <weiweilin@google.com>
2024-02-06 00:26:42 +0000

    luci-ui: migrate copy button to React
    Keep the lit-element implementation as-is for now so lit components
    using them can stay performant before they are migrated to React
    themselves.
    
    R=beining@google.com, mdraz@google.com
    
    Bug: b/40253769
    Change-Id: Ie8a393b1f550a60a20ef3753979731c507101a4e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5266526
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Mostafa Draz <mdraz@google.com>

---
+++ milo/ui/src/generic_libs/components/copy_to_clipboard/copy_to_clipboard.test.tsx
@@ -0,0 +1,70 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import copy from 'copy-to-clipboard';
+import { act } from 'react-dom/test-utils';
+
+import { CopyToClipboard } from './copy_to_clipboard';
+
+jest.mock('copy-to-clipboard');
+
+describe('<CopyToClipboard />', () => {
+  let copyMock: jest.MockedFunction<typeof copy>;
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    copyMock = jest.mocked(copy);
+  });
+
+  afterEach(() => {
+    cleanup();
+    jest.useRealTimers();
+  });
+
+  it('should copy text when clicked', () => {
+    render(<CopyToClipboard textToCopy="teststring" />);
+
+    fireEvent.click(screen.getByTestId('ContentCopyIcon'));
+    expect(copyMock).toHaveBeenCalledWith('teststring');
+  });
+
+  it('should support copying text generated from function', () => {
+    const genText = jest.fn(() => 'teststring');
+    render(<CopyToClipboard textToCopy={genText} />);
+    expect(genText).not.toHaveBeenCalled();
+
+    fireEvent.click(screen.getByTestId('ContentCopyIcon'));
+    expect(genText).toHaveBeenCalledTimes(1);
+    expect(copyMock).toHaveBeenCalledWith('teststring');
+  });
+
+  it('should display done icon for a short period when clicked', async () => {
+    render(<CopyToClipboard textToCopy="teststring" />);
+    expect(screen.getByTestId('ContentCopyIcon')).toBeInTheDocument();
+    expect(screen.queryByTestId('DoneIcon')).not.toBeInTheDocument();
+
+    fireEvent.click(screen.getByTestId('ContentCopyIcon'));
+    expect(screen.queryByTestId('ContentCopyIcon')).not.toBeInTheDocument();
+    expect(screen.getByTestId('DoneIcon')).toBeInTheDocument();
+
+    await act(() => jest.advanceTimersByTimeAsync(500));
+    expect(screen.queryByTestId('ContentCopyIcon')).not.toBeInTheDocument();
+    expect(screen.getByTestId('DoneIcon')).toBeInTheDocument();
+
+    await act(() => jest.advanceTimersByTimeAsync(1000));
+    expect(screen.getByTestId('ContentCopyIcon')).toBeInTheDocument();
+    expect(screen.queryByTestId('DoneIcon')).not.toBeInTheDocument();
+  });
+});
---
+++ milo/ui/src/generic_libs/components/copy_to_clipboard/copy_to_clipboard.tsx
@@ -0,0 +1,70 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { ContentCopy, Done } from '@mui/icons-material';
+import { Box, styled } from '@mui/material';
+import copy from 'copy-to-clipboard';
+import { useState } from 'react';
+import { useTimeoutFn } from 'react-use';
+
+const Container = styled(Box)`
+  display: inline-block;
+  cursor: pointer;
+  vertical-align: text-bottom;
+  width: 18px;
+  height: 18px;
+  border-radius: 2px;
+  padding: 2px;
+  font-size: 17px;
+  overflow: hidden;
+
+  &:hover {
+    background-color: silver;
+  }
+`;
+
+export interface CopyToClipboardProps {
+  /**
+   * Text, or a function to generated text, to be copied to the clipboard.
+   */
+  readonly textToCopy: string | (() => string);
+  readonly title?: string;
+}
+
+/**
+ * A simple icon that copies textToCopy to clipboard onclick.
+ */
+export function CopyToClipboard({ textToCopy, title }: CopyToClipboardProps) {
+  const [justCopied, setJustCopied] = useState(false);
+  const [_isReady, _cancel, reset] = useTimeoutFn(() => {
+    setJustCopied(false);
+  }, 1000);
+
+  function handleCopy() {
+    const text = typeof textToCopy === 'function' ? textToCopy() : textToCopy;
+    copy(text);
+    setJustCopied(true);
+    reset();
+  }
+
+  return (
+    <Container title={title}>
+      {justCopied ? (
+        <Done fontSize="inherit" />
+      ) : (
+        <ContentCopy fontSize="inherit" onClick={handleCopy} />
+      )}
+    </Container>
+  );
+}
