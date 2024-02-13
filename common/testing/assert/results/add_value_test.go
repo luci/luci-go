293366647c8f6e61cb990e2e27c0ffad7a8830cf
Greg Nisbet <gregorynisbet@google.com>
2024-02-13 21:37:29 +0000

    [noconvey] Add Addvalue method to result.
    Add a non-exported addvalue method to result.
    
    BUG=b:319730573
    TEST=make test
    
    Change-Id: I6ddb112de7a240f4a48b38e44dc6c41ac8ff6396
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5292998
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Reviewed-by: Bryan Ni <bniche@google.com>

---
+++ common/testing/assert/results/add_value_test.go
@@ -0,0 +1,45 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package results
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"go.chromium.org/luci/common/testing/typed"
+)
+
+// TestAddValue tests adding a value to the values of a result.
+func TestAddValue(t *testing.T) {
+	t.Parallel()
+
+	result := &Result{}
+	addValue(result, "a", 4)
+	if diff := typed.Got(result.values[0]).Want(value{name: "a", value: 4}).Options(cmpopts.EquateComparable(value{})).Diff(); diff != "" {
+		t.Errorf("unexpected diff (-want +got): %s", diff)
+	}
+}
+
+// TestAddValue tests adding a formatted value to the values of a result.
+func TestAddValuef(t *testing.T) {
+	t.Parallel()
+
+	result := &Result{}
+	addValuef(result, "a", "%d", 4)
+	if diff := typed.Got(result.values[0]).Want(value{name: "a", value: verbatimString("4")}).Options(cmpopts.EquateComparable(value{})).Diff(); diff != "" {
+		t.Errorf("unexpected diff (-want +got): %s", diff)
+	}
+}
