db399efa60d6002306186a7b3324c64e1b7ffc39
Greg Nisbet <gregorynisbet@google.com>
2024-02-13 22:14:10 +0000

    [noconvey] Implement should.Equal
    BUG=b:319730573
    TEST=make test
    
    Change-Id: I40aa6eea2e9248f1ae9f9a2f6f5cac8e80c9ed5d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5293001
    Reviewed-by: Bryan Ni <bniche@google.com>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>

---
+++ common/testing/assert/should/equal.go
@@ -0,0 +1,45 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package should
+
+import (
+	"reflect"
+
+	"go.chromium.org/luci/common/testing/assert/results"
+)
+
+// Equal checks whether two objects are equal.
+func Equal[T any](expected T) results.Comparison[any] {
+	cmpName := "should.Equal"
+	rtype := reflect.TypeOf(expected)
+
+	return func(actual any) (ret *results.Result) {
+		if reflect.TypeOf(actual) != rtype {
+			return results.NewResultBuilder().
+				SetName(cmpName, rtype).
+				Because("Type of actual does not match (is `%T`)", actual).
+				Result()
+		}
+		if reflect.DeepEqual(actual, expected) {
+			return nil
+		}
+
+		return results.NewResultBuilder().
+			SetName(cmpName, rtype).
+			Actual(actual).
+			Expected(expected).
+			Result()
+	}
+}
---
+++ common/testing/assert/should/equal_test.go
@@ -0,0 +1,53 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package should
+
+import (
+	"testing"
+
+	"go.chromium.org/luci/common/testing/typed"
+)
+
+// TestEqual checks whether two things are equal.
+func TestEqual(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name string
+		lhs  interface{}
+		rhs  interface{}
+		ok   bool
+	}{
+		{
+			name: "empty",
+			lhs:  nil,
+			rhs:  nil,
+			ok:   true,
+		},
+	}
+
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := Equal(tt.lhs)(tt.rhs) == nil
+			want := tt.ok
+			if diff := typed.Got(got).Want(want).Diff(); diff != "" {
+				t.Errorf("unexpected diff (-want +got): %s", diff)
+			}
+		})
+	}
+}
