09fae412111ce6eb636fc21a786c21d963620b08
Yiwei Zhang <yiwzhang@google.com>
2024-02-29 22:57:22 +0000

    cv: handle the failures in tryjob execution state
    the handler will evaluate the failure into messages and scheudle
    reset trigger tasks.
    
    R=ddoman
    
    Change-Id: I9ff7919ba272f2ad1ab9edda5337650d9c51dc97
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5334003
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>
    Reviewed-by: Scott Lee <ddoman@chromium.org>

--- cv/internal/run/impl/handler/tryjobs.go
+++ cv/internal/run/impl/handler/tryjobs.go
@@ -19,16 +19,21 @@ import (
 	"context"
 	"fmt"
 	"slices"
+	"sort"
+	"strconv"
+	"strings"
 	"text/template"
 	"time"
 
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	bbutil "go.chromium.org/luci/buildbucket/protoutil"
 	"go.chromium.org/luci/common/clock"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 
+	cfgpb "go.chromium.org/luci/cv/api/config/v2"
 	"go.chromium.org/luci/cv/internal/common"
 	"go.chromium.org/luci/cv/internal/run"
 	"go.chromium.org/luci/cv/internal/run/eventpb"
@@ -82,7 +87,7 @@ func (impl *Impl) OnTryjobsUpdated(ctx context.Context, rs *state.RunState, tryj
 	}
 }
 
-func (impl *Impl) onCompletedExecuteTryjobs(ctx context.Context, rs *state.RunState, op *run.OngoingLongOps_Op, opCompleted *eventpb.LongOpCompleted) (*Result, error) {
+func (impl *Impl) onCompletedExecuteTryjobs(ctx context.Context, rs *state.RunState, _ *run.OngoingLongOps_Op, opCompleted *eventpb.LongOpCompleted) (*Result, error) {
 	opID := opCompleted.GetOperationId()
 	rs = rs.ShallowCopy()
 	rs.RemoveCompletedLongOp(opID)
@@ -93,6 +98,7 @@ func (impl *Impl) onCompletedExecuteTryjobs(ctx context.Context, rs *state.RunSt
 	var (
 		runStatus       run.Status
 		msgTmpl         string
+		failures        *tryjob.ExecutionState_Failures
 		attentionReason string
 	)
 	switch opCompleted.GetStatus() {
@@ -129,7 +135,10 @@ func (impl *Impl) onCompletedExecuteTryjobs(ctx context.Context, rs *state.RunSt
 				}
 			case executionStatus == tryjob.ExecutionState_FAILED:
 				runStatus = run.Status_FAILED
-				msgTmpl = "This CL has failed the run. Reason:\n\n" + es.FailureReasonTmpl
+				if es.FailureReasonTmpl != "" {
+					msgTmpl = "This CL has failed the run. Reason:\n\n" + es.FailureReasonTmpl
+				}
+				failures = es.GetFailures()
 				attentionReason = "Tryjobs failed"
 			case executionStatus == tryjob.ExecutionState_RUNNING:
 				// Tryjobs are still running. No change to run status.
@@ -148,11 +157,43 @@ func (impl *Impl) onCompletedExecuteTryjobs(ctx context.Context, rs *state.RunSt
 		if err != nil {
 			return nil, err
 		}
+		var failedTryjobs []*tryjob.Tryjob
+		if len(failures.GetUnsuccessfulResults()) > 0 {
+			ids := make(common.TryjobIDs, len(failures.GetUnsuccessfulResults()))
+			for i, r := range failures.GetUnsuccessfulResults() {
+				ids[i] = common.TryjobID(r.GetTryjobId())
+			}
+			var err error
+			failedTryjobs, err = tryjob.LoadTryjobsByIDs(ctx, ids)
+			if err != nil {
+				return nil, err
+			}
+		}
 
 		metas := make(map[common.CLID]reviewInputMeta, len(rs.CLs))
 		for _, cl := range cls {
 			var meta reviewInputMeta
-			if msgTmpl != "" {
+			switch {
+			case runStatus == run.Status_SUCCEEDED && rs.Mode == run.NewPatchsetRun:
+				meta = reviewInputMeta{} // silent
+			case runStatus == run.Status_SUCCEEDED:
+				meta = reviewInputMeta{
+					message: "This CL has passed the run",
+					notify:  rs.Mode.GerritNotifyTargets(),
+				}
+			case runStatus == run.Status_FAILED && failures != nil:
+				meta = reviewInputMeta{
+					notify:         rs.Mode.GerritNotifyTargets(),
+					addToAttention: rs.Mode.GerritNotifyTargets(),
+					reason:         "Tryjobs failed",
+				}
+				switch {
+				case len(failures.GetUnsuccessfulResults()) > 0:
+					meta.message = "This CL has failed the run. Reason:\n\n" + composeTryjobsResultFailureReason(cl, failedTryjobs)
+				case len(failures.GetLaunchFailures()) > 0:
+					meta.message = composeLaunchFailureReason(failures.GetLaunchFailures())
+				}
+			case msgTmpl != "":
 				whoms := rs.Mode.GerritNotifyTargets()
 				var msg string
 				switch t, err := template.New("FailureReason").Parse(msgTmpl); {
@@ -175,6 +216,13 @@ func (impl *Impl) onCompletedExecuteTryjobs(ctx context.Context, rs *state.RunSt
 				if attentionReason != "" {
 					meta.addToAttention = whoms
 					meta.reason = attentionReason
+				}
+			default:
+				meta = reviewInputMeta{
+					message:        "Unexpected error when processing Tryjobs. Please retry. If retry continues to fail, please contact LUCI team.\n\n" + cvBugLink,
+					notify:         rs.Mode.GerritNotifyTargets(),
+					addToAttention: rs.Mode.GerritNotifyTargets(),
+					reason:         "Run failed",
 				}
 			}
 			metas[cl.ID] = meta
@@ -216,4 +264,157 @@ func enqueueRequirementChangedTask(ctx context.Context, rs *state.RunState) {
 			},
 		},
 	})
+}
+
+// composeTryjobsResultFailureReason make a human-readable string explaining
+// the failed Tryjob result for the given CL.
+func composeTryjobsResultFailureReason(cl *run.RunCL, tryjobs []*tryjob.Tryjob) string {
+	switch len(tryjobs) {
+	case 0:
+		panic(fmt.Errorf("composeReason called without tryjobs"))
+	case 1: // Optimize for most common case: one failed tryjob.
+		tj := tryjobs[0]
+		restricted := tj.Definition.GetResultVisibility() == cfgpb.CommentLevel_COMMENT_LEVEL_RESTRICTED
+		var sb strings.Builder
+		if restricted {
+			writeMDLink(&sb, "Tryjob", tj.ExternalID.MustURL())
+			sb.WriteString(" has failed")
+		} else {
+			sb.WriteString("Tryjob ")
+			writeMDLink(&sb, getBuilderName(tj.Definition, tj.Result), tj.ExternalID.MustURL())
+			sb.WriteString(" has failed")
+			if sm := tj.Result.GetBuildbucket().GetSummaryMarkdown(); sm != "" {
+				sb.WriteString(" with summary")
+				if cl.Detail.GetGerrit() != nil {
+					fmt.Fprintf(&sb, " ([view all results](%s?checksPatchset=%d&tab=checks))", cl.ExternalID.MustURL(), cl.Detail.GetPatchset())
+				}
+				sb.WriteString(":\n\n---\n")
+				sb.WriteString(sm)
+			}
+		}
+		return sb.String()
+	default:
+		var sb strings.Builder
+		sb.WriteString("Failed Tryjobs:")
+		// restrict the result visibility if any tryjob has restricted result
+		// visibility
+		var restricted bool
+		for _, tj := range tryjobs {
+			if tj.Definition.GetResultVisibility() == cfgpb.CommentLevel_COMMENT_LEVEL_RESTRICTED {
+				restricted = true
+				break
+			}
+		}
+		for _, tj := range tryjobs {
+			sb.WriteString("\n* ")
+			if restricted {
+				sb.WriteString(tj.ExternalID.MustURL())
+			} else {
+				writeMDLink(&sb, getBuilderName(tj.Definition, tj.Result), tj.ExternalID.MustURL())
+				if sm := tj.Result.GetBuildbucket().GetSummaryMarkdown(); sm != "" {
+					sb.WriteString(". Summary")
+					if cl.Detail.GetGerrit() != nil {
+						fmt.Fprintf(&sb, " ([view all results](%s?checksPatchset=%d&tab=checks))", cl.ExternalID.MustURL(), cl.Detail.GetPatchset())
+					}
+					sb.WriteString(":\n\n---\n")
+					sb.WriteString(sm)
+					sb.WriteString("\n\n---")
+				}
+			}
+		}
+		return sb.String()
+	}
+}
+
+// composeLaunchFailureReason makes a string explaining tryjob launch failures.
+func composeLaunchFailureReason(launchFailures []*tryjob.ExecutionState_Failures_LaunchFailure) string {
+	if len(launchFailures) == 0 {
+		panic(fmt.Errorf("expected non-empty launch failures"))
+	}
+	if len(launchFailures) == 1 { // optimize for most common case
+		for _, failure := range launchFailures {
+			switch {
+			case failure.GetDefinition().GetBuildbucket() == nil:
+				panic(fmt.Errorf("non Buildbucket backend is not supported. got %T", failure.GetDefinition().GetBackend()))
+			case failure.GetDefinition().GetResultVisibility() == cfgpb.CommentLevel_COMMENT_LEVEL_RESTRICTED:
+				// TODO(crbug/1302119): Replace terms like "Project admin" with
+				// dedicated contact sourced from Project Config.
+				return "Failed to launch one tryjob. The tryjob name can't be shown due to configuration. Please contact your Project admin for help."
+			default:
+				builderName := bbutil.FormatBuilderID(failure.GetDefinition().GetBuildbucket().GetBuilder())
+				return fmt.Sprintf("Failed to launch tryjob `%s`. Reason: %s", builderName, failure.GetReason())
+			}
+		}
+	}
+
+	var sb strings.Builder
+	sb.WriteString("Failed to launch the following tryjobs:")
+	var restrictedCnt int
+	lines := make([]string, 0, len(launchFailures))
+	for _, failure := range launchFailures {
+		if failure.GetDefinition().GetResultVisibility() == cfgpb.CommentLevel_COMMENT_LEVEL_RESTRICTED {
+			restrictedCnt++
+			continue
+		}
+		lines = append(lines, fmt.Sprintf("* `%s`; Failure reason: %s", bbutil.FormatBuilderID(failure.GetDefinition().GetBuildbucket().GetBuilder()), failure.GetReason()))
+	}
+	sort.Strings(lines) // for determinism
+	for _, l := range lines {
+		sb.WriteRune('\n')
+		sb.WriteString(l)
+	}
+
+	switch {
+	case restrictedCnt == len(launchFailures):
+		// TODO(crbug/1302119): Replace terms like "Project admin" with
+		// dedicated contact sourced from Project Config.
+		return fmt.Sprintf("Failed to launch %d tryjobs. The tryjob names can't be shown due to configuration. Please contact your Project admin for help.", restrictedCnt)
+	case restrictedCnt > 0:
+		sb.WriteString("\n\nIn addition to the tryjobs above, failed to launch ")
+		sb.WriteString(strconv.Itoa(restrictedCnt))
+		sb.WriteString(" tryjob")
+		if restrictedCnt > 1 {
+			sb.WriteString("s")
+		}
+		sb.WriteString(". But the tryjob names can't be shown due to configuration. Please contact your Project admin for help.")
+	}
+	return sb.String()
+}
+
+// getBuilderName gets the Buildbucket builder name from Tryjob result or
+// Tryjob definition.
+//
+// Tries to get builder name from the result first as it reflects actual
+// builder launched which may or may not be the main builder in the tryjob
+// definition.
+func getBuilderName(def *tryjob.Definition, result *tryjob.Result) string {
+	if result != nil && result.GetBackend() != nil {
+		switch result.GetBackend().(type) {
+		case *tryjob.Result_Buildbucket_:
+			if builder := result.GetBuildbucket().GetBuilder(); builder != nil {
+				return bbutil.FormatBuilderID(builder)
+			}
+		default:
+			panic(fmt.Errorf("non Buildbucket tryjob backend is not supported. got %T", result.GetBackend()))
+		}
+	}
+	if def != nil && def.GetBackend() != nil {
+		switch def.GetBackend().(type) {
+		case *tryjob.Definition_Buildbucket_:
+			if builder := def.GetBuildbucket().GetBuilder(); builder != nil {
+				return bbutil.FormatBuilderID(builder)
+			}
+		default:
+			panic(fmt.Errorf("non Buildbucket tryjob backend is not supported. got %T", def.GetBackend()))
+		}
+	}
+	panic(fmt.Errorf("impossible; can't get builder name from definition and result. Definition: %s; Result: %s", def, result))
+}
+
+func writeMDLink(sb *strings.Builder, text, url string) {
+	sb.WriteString("[")
+	sb.WriteString(text)
+	sb.WriteString("](")
+	sb.WriteString(url)
+	sb.WriteString(")")
 }
--- cv/internal/run/impl/handler/tryjobs_test.go
+++ cv/internal/run/impl/handler/tryjobs_test.go
@@ -20,6 +20,7 @@ import (
 	"testing"
 	"time"
 
+	bbpb "go.chromium.org/luci/buildbucket/proto"
 	"go.chromium.org/luci/common/errors"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -296,6 +297,177 @@ func TestOnCompletedExecuteTryjobs(t *testing.T) {
 			})
 
 			Convey("Tryjob execution failed", func() {
+				Convey("tryjobs result failure", func() {
+					tj := tryjob.MustBuildbucketID("example.com", 12345).MustCreateIfNotExists(ctx)
+					tj.Result = &tryjob.Result{
+						Backend: &tryjob.Result_Buildbucket_{
+							Buildbucket: &tryjob.Result_Buildbucket{
+								Builder: &bbpb.BuilderID{
+									Project: lProject,
+									Bucket:  "test",
+									Builder: "foo",
+								},
+								SummaryMarkdown: "this is the summary",
+							},
+						},
+					}
+					So(datastore.Put(ctx, tj), ShouldBeNil)
+
+					err := datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+						return tryjob.SaveExecutionState(ctx, rs.ID, &tryjob.ExecutionState{
+							Status: tryjob.ExecutionState_FAILED,
+							Failures: &tryjob.ExecutionState_Failures{
+								UnsuccessfulResults: []*tryjob.ExecutionState_Failures_UnsuccessfulResult{
+									{TryjobId: int64(tj.ID)},
+								},
+							},
+						}, 0, nil)
+					}, nil)
+					So(err, ShouldBeNil)
+					res, err := h.OnLongOpCompleted(ctx, rs, result)
+					So(err, ShouldBeNil)
+					So(res.State.Status, ShouldEqual, run.Status_RUNNING)
+					So(res.State.Tryjobs, ShouldResembleProto, &run.Tryjobs{
+						State: &tryjob.ExecutionState{
+							Status: tryjob.ExecutionState_FAILED,
+							Failures: &tryjob.ExecutionState_Failures{
+								UnsuccessfulResults: []*tryjob.ExecutionState_Failures_UnsuccessfulResult{
+									{TryjobId: int64(tj.ID)},
+								},
+							},
+						},
+					})
+					So(res.State.OngoingLongOps.GetOps(), ShouldHaveLength, 1)
+					for _, op := range res.State.OngoingLongOps.GetOps() {
+						So(op.GetResetTriggers(), ShouldNotBeNil)
+						So(op.GetResetTriggers().GetRequests(), ShouldResembleProto, []*run.OngoingLongOps_Op_ResetTriggers_Request{
+							{
+								Clid:    int64(rs.CLs[0]),
+								Message: "This CL has failed the run. Reason:\n\nTryjob [chromium/test/foo](https://example.com/build/12345) has failed with summary ([view all results](https://example-review.googlesource.com/c/101?checksPatchset=2&tab=checks)):\n\n---\nthis is the summary",
+								Notify: gerrit.Whoms{
+									gerrit.Whom_OWNER,
+									gerrit.Whom_CQ_VOTERS,
+								},
+								AddToAttention: gerrit.Whoms{
+									gerrit.Whom_OWNER,
+									gerrit.Whom_CQ_VOTERS,
+								},
+								AddToAttentionReason: "Tryjobs failed",
+							},
+						})
+						So(op.GetResetTriggers().GetRunStatusIfSucceeded(), ShouldEqual, run.Status_FAILED)
+					}
+					So(res.SideEffectFn, ShouldBeNil)
+					So(res.PreserveEvents, ShouldBeFalse)
+				})
+
+				Convey("failed to launch tryjobs", func() {
+					def := &tryjob.Definition{
+						Backend: &tryjob.Definition_Buildbucket_{
+							Buildbucket: &tryjob.Definition_Buildbucket{
+								Host: "example.com",
+								Builder: &bbpb.BuilderID{
+									Project: lProject,
+									Bucket:  "test",
+									Builder: "foo",
+								},
+							},
+						},
+					}
+					err := datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+						return tryjob.SaveExecutionState(ctx, rs.ID, &tryjob.ExecutionState{
+							Status: tryjob.ExecutionState_FAILED,
+							Failures: &tryjob.ExecutionState_Failures{
+								LaunchFailures: []*tryjob.ExecutionState_Failures_LaunchFailure{
+									{
+										Definition: def,
+										Reason:     "permission denied",
+									},
+								},
+							},
+						}, 0, nil)
+					}, nil)
+					So(err, ShouldBeNil)
+					res, err := h.OnLongOpCompleted(ctx, rs, result)
+					So(err, ShouldBeNil)
+					So(res.State.Status, ShouldEqual, run.Status_RUNNING)
+					So(res.State.Tryjobs, ShouldResembleProto, &run.Tryjobs{
+						State: &tryjob.ExecutionState{
+							Status: tryjob.ExecutionState_FAILED,
+							Failures: &tryjob.ExecutionState_Failures{
+								LaunchFailures: []*tryjob.ExecutionState_Failures_LaunchFailure{
+									{
+										Definition: def,
+										Reason:     "permission denied",
+									},
+								},
+							},
+						},
+					})
+					So(res.State.OngoingLongOps.GetOps(), ShouldHaveLength, 1)
+					for _, op := range res.State.OngoingLongOps.GetOps() {
+						So(op.GetResetTriggers(), ShouldNotBeNil)
+						So(op.GetResetTriggers().GetRequests(), ShouldResembleProto, []*run.OngoingLongOps_Op_ResetTriggers_Request{
+							{
+								Clid:    int64(rs.CLs[0]),
+								Message: "Failed to launch tryjob `chromium/test/foo`. Reason: permission denied",
+								Notify: gerrit.Whoms{
+									gerrit.Whom_OWNER,
+									gerrit.Whom_CQ_VOTERS,
+								},
+								AddToAttention: gerrit.Whoms{
+									gerrit.Whom_OWNER,
+									gerrit.Whom_CQ_VOTERS,
+								},
+								AddToAttentionReason: "Tryjobs failed",
+							},
+						})
+						So(op.GetResetTriggers().GetRunStatusIfSucceeded(), ShouldEqual, run.Status_FAILED)
+					}
+					So(res.SideEffectFn, ShouldBeNil)
+					So(res.PreserveEvents, ShouldBeFalse)
+				})
+
+				Convey("Unknown error", func() {
+					err := datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+						return tryjob.SaveExecutionState(ctx, rs.ID, &tryjob.ExecutionState{
+							Status: tryjob.ExecutionState_FAILED,
+						}, 0, nil)
+					}, nil)
+					So(err, ShouldBeNil)
+					res, err := h.OnLongOpCompleted(ctx, rs, result)
+					So(err, ShouldBeNil)
+					So(res.State.Status, ShouldEqual, run.Status_RUNNING)
+					So(res.State.Tryjobs, ShouldResembleProto, &run.Tryjobs{
+						State: &tryjob.ExecutionState{
+							Status: tryjob.ExecutionState_FAILED,
+						},
+					})
+					So(res.State.OngoingLongOps.GetOps(), ShouldHaveLength, 1)
+					for _, op := range res.State.OngoingLongOps.GetOps() {
+						So(op.GetResetTriggers(), ShouldNotBeNil)
+						So(op.GetResetTriggers().GetRequests(), ShouldResembleProto, []*run.OngoingLongOps_Op_ResetTriggers_Request{
+							{
+								Clid:    int64(rs.CLs[0]),
+								Message: "Unexpected error when processing Tryjobs. Please retry. If retry continues to fail, please contact LUCI team.\n\n" + cvBugLink,
+								Notify: gerrit.Whoms{
+									gerrit.Whom_OWNER,
+									gerrit.Whom_CQ_VOTERS,
+								},
+								AddToAttention: gerrit.Whoms{
+									gerrit.Whom_OWNER,
+									gerrit.Whom_CQ_VOTERS,
+								},
+								AddToAttentionReason: "Run failed",
+							},
+						})
+						So(op.GetResetTriggers().GetRunStatusIfSucceeded(), ShouldEqual, run.Status_FAILED)
+					}
+					So(res.SideEffectFn, ShouldBeNil)
+					So(res.PreserveEvents, ShouldBeFalse)
+				})
+
+				// TODO(yiwzhang): Delete following
 				var whoms gerrit.Whoms
 				Convey("With no failure reason placeholders", func() {
 					Convey("On CQ Vote Run", func() {
@@ -425,6 +597,240 @@ func TestOnCompletedExecuteTryjobs(t *testing.T) {
 						So(err, ShouldNotBeNil)
 					})
 				})
+			})
+		})
+	})
+}
+
+func TestComposeTryjobsFailureReason(t *testing.T) {
+	Convey("ComposeTryjobsFailureReason", t, func() {
+		cl := &run.RunCL{
+			ID:         1111,
+			ExternalID: changelist.MustGobID("example.review.com", 123),
+			Detail: &changelist.Snapshot{
+				LuciProject: "test",
+				Kind: &changelist.Snapshot_Gerrit{
+					Gerrit: &changelist.Gerrit{
+						Host: "example.review.com",
+					},
+				},
+				Patchset: 2,
+			},
+		}
+		Convey("panics", func() {
+			So(func() {
+				_ = composeTryjobsResultFailureReason(cl, nil)
+			}, ShouldPanicLike, "called without tryjobs")
+		})
+		const bbHost = "test.com"
+		builder := &bbpb.BuilderID{
+			Project: "test_proj",
+			Bucket:  "test_bucket",
+			Builder: "test_builder",
+		}
+		Convey("works", func() {
+			Convey("single", func() {
+				Convey("restricted", func() {
+					r := composeTryjobsResultFailureReason(cl, []*tryjob.Tryjob{
+						{
+							ExternalID: tryjob.MustBuildbucketID(bbHost, 123456790),
+							Definition: &tryjob.Definition{
+								Backend: &tryjob.Definition_Buildbucket_{
+									Buildbucket: &tryjob.Definition_Buildbucket{
+										Builder: builder,
+										Host:    bbHost,
+									},
+								},
+								ResultVisibility: cfgpb.CommentLevel_COMMENT_LEVEL_RESTRICTED,
+							},
+							Result: &tryjob.Result{
+								Backend: &tryjob.Result_Buildbucket_{
+									Buildbucket: &tryjob.Result_Buildbucket{
+										Builder:         builder,
+										SummaryMarkdown: "A couple\nof lines\nwith secret details",
+									},
+								},
+							},
+						},
+					})
+					So(r, ShouldEqual, "[Tryjob](https://test.com/build/123456790) has failed")
+				})
+				Convey("not restricted", func() {
+					r := composeTryjobsResultFailureReason(cl, []*tryjob.Tryjob{
+						{
+							ExternalID: tryjob.MustBuildbucketID(bbHost, 123456790),
+							Definition: &tryjob.Definition{
+								Backend: &tryjob.Definition_Buildbucket_{
+									Buildbucket: &tryjob.Definition_Buildbucket{
+										Builder: builder,
+										Host:    bbHost,
+									},
+								},
+								ResultVisibility: cfgpb.CommentLevel_COMMENT_LEVEL_FULL,
+							},
+							Result: &tryjob.Result{
+								Backend: &tryjob.Result_Buildbucket_{
+									Buildbucket: &tryjob.Result_Buildbucket{
+										Builder:         builder,
+										SummaryMarkdown: "A couple\nof lines\nwith public details",
+									},
+								},
+							},
+						},
+					})
+					So(r, ShouldEqual, "Tryjob [test_proj/test_bucket/test_builder](https://test.com/build/123456790) has failed with summary ([view all results](https://example.review.com/c/123?checksPatchset=2&tab=checks)):\n\n---\nA couple\nof lines\nwith public details")
+				})
+			})
+
+			Convey("multiple tryjobs", func() {
+				tjs := []*tryjob.Tryjob{
+					// restricted.
+					{
+						ExternalID: tryjob.MustBuildbucketID("test.com", 123456790),
+						Definition: &tryjob.Definition{
+							Backend: &tryjob.Definition_Buildbucket_{
+								Buildbucket: &tryjob.Definition_Buildbucket{
+									Builder: builder,
+									Host:    bbHost,
+								},
+							},
+							ResultVisibility: cfgpb.CommentLevel_COMMENT_LEVEL_RESTRICTED,
+						},
+						Result: &tryjob.Result{
+							Backend: &tryjob.Result_Buildbucket_{
+								Buildbucket: &tryjob.Result_Buildbucket{
+									Builder:         builder,
+									SummaryMarkdown: "A couple\nof lines\nwith secret details",
+								},
+							},
+						},
+					},
+					// un-restricted but empty summary markdown.
+					{
+						ExternalID: tryjob.MustBuildbucketID("test.com", 123456791),
+						Definition: &tryjob.Definition{
+							Backend: &tryjob.Definition_Buildbucket_{
+								Buildbucket: &tryjob.Definition_Buildbucket{
+									Builder: builder,
+									Host:    bbHost,
+								},
+							},
+							ResultVisibility: cfgpb.CommentLevel_COMMENT_LEVEL_FULL,
+						},
+						Result: &tryjob.Result{
+							Backend: &tryjob.Result_Buildbucket_{
+								Buildbucket: &tryjob.Result_Buildbucket{
+									Builder: builder,
+								},
+							},
+						},
+					},
+					// un-restricted.
+					{
+						ExternalID: tryjob.MustBuildbucketID("test.com", 123456792),
+						Definition: &tryjob.Definition{
+							Backend: &tryjob.Definition_Buildbucket_{
+								Buildbucket: &tryjob.Definition_Buildbucket{
+									Builder: builder,
+									Host:    bbHost,
+								},
+							},
+							ResultVisibility: cfgpb.CommentLevel_COMMENT_LEVEL_FULL,
+						},
+						Result: &tryjob.Result{
+							Backend: &tryjob.Result_Buildbucket_{
+								Buildbucket: &tryjob.Result_Buildbucket{
+									SummaryMarkdown: "A couple\nof lines\nwith public details",
+								},
+							},
+						},
+					},
+				}
+
+				Convey("all public visibility", func() {
+					r := composeTryjobsResultFailureReason(cl, tjs[1:])
+					So(r, ShouldEqual, "Failed Tryjobs:\n* [test_proj/test_bucket/test_builder](https://test.com/build/123456791)\n* [test_proj/test_bucket/test_builder](https://test.com/build/123456792). Summary ([view all results](https://example.review.com/c/123?checksPatchset=2&tab=checks)):\n\n---\nA couple\nof lines\nwith public details\n\n---")
+				})
+
+				Convey("with one restricted visibility", func() {
+					r := composeTryjobsResultFailureReason(cl, tjs)
+					So(r, ShouldEqual, "Failed Tryjobs:\n* https://test.com/build/123456790\n* https://test.com/build/123456791\n* https://test.com/build/123456792")
+				})
+			})
+		})
+	})
+}
+
+func TestComposeLaunchFailureReason(t *testing.T) {
+	Convey("Compose Launch Failure Reason", t, func() {
+		defFoo := &tryjob.Definition{
+			Backend: &tryjob.Definition_Buildbucket_{
+				Buildbucket: &tryjob.Definition_Buildbucket{
+					Host: "buildbucket.example.com",
+					Builder: &bbpb.BuilderID{
+						Project: "ProjectFoo",
+						Bucket:  "BucketFoo",
+						Builder: "BuilderFoo",
+					},
+				},
+			},
+		}
+		Convey("Single", func() {
+			Convey("restricted", func() {
+				defFoo.ResultVisibility = cfgpb.CommentLevel_COMMENT_LEVEL_RESTRICTED
+				reason := composeLaunchFailureReason(
+					[]*tryjob.ExecutionState_Failures_LaunchFailure{
+						{Definition: defFoo, Reason: "permission denied"},
+					})
+				So(reason, ShouldEqual, "Failed to launch one tryjob. The tryjob name can't be shown due to configuration. Please contact your Project admin for help.")
+			})
+			Convey("public", func() {
+				reason := composeLaunchFailureReason(
+					[]*tryjob.ExecutionState_Failures_LaunchFailure{
+						{Definition: defFoo, Reason: "permission denied"},
+					})
+				So(reason, ShouldEqual, "Failed to launch tryjob `ProjectFoo/BucketFoo/BuilderFoo`. Reason: permission denied")
+			})
+		})
+		defBar := &tryjob.Definition{
+			Backend: &tryjob.Definition_Buildbucket_{
+				Buildbucket: &tryjob.Definition_Buildbucket{
+					Host: "buildbucket.example.com",
+					Builder: &bbpb.BuilderID{
+						Project: "ProjectBar",
+						Bucket:  "BucketBar",
+						Builder: "BuilderBar",
+					},
+				},
+			},
+		}
+		Convey("Multiple", func() {
+			Convey("All restricted", func() {
+				defFoo.ResultVisibility = cfgpb.CommentLevel_COMMENT_LEVEL_RESTRICTED
+				defBar.ResultVisibility = cfgpb.CommentLevel_COMMENT_LEVEL_RESTRICTED
+				reason := composeLaunchFailureReason(
+					[]*tryjob.ExecutionState_Failures_LaunchFailure{
+						{Definition: defFoo, Reason: "permission denied"},
+						{Definition: defBar, Reason: "builder not found"},
+					})
+				So(reason, ShouldEqual, "Failed to launch 2 tryjobs. The tryjob names can't be shown due to configuration. Please contact your Project admin for help.")
+			})
+			Convey("Partial restricted", func() {
+				defBar.ResultVisibility = cfgpb.CommentLevel_COMMENT_LEVEL_RESTRICTED
+				reason := composeLaunchFailureReason(
+					[]*tryjob.ExecutionState_Failures_LaunchFailure{
+						{Definition: defFoo, Reason: "permission denied"},
+						{Definition: defBar, Reason: "builder not found"},
+					})
+				So(reason, ShouldEqual, "Failed to launch the following tryjobs:\n* `ProjectFoo/BucketFoo/BuilderFoo`; Failure reason: permission denied\n\nIn addition to the tryjobs above, failed to launch 1 tryjob. But the tryjob names can't be shown due to configuration. Please contact your Project admin for help.")
+			})
+			Convey("All public", func() {
+				reason := composeLaunchFailureReason(
+					[]*tryjob.ExecutionState_Failures_LaunchFailure{
+						{Definition: defFoo, Reason: "permission denied"},
+						{Definition: defBar, Reason: "builder not found"},
+					})
+				So(reason, ShouldEqual, "Failed to launch the following tryjobs:\n* `ProjectBar/BucketBar/BuilderBar`; Failure reason: builder not found\n* `ProjectFoo/BucketFoo/BuilderFoo`; Failure reason: permission denied")
 			})
 		})
 	})
