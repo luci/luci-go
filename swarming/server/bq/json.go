225e5412b9e0e51f8ea68afe6f0dd76b9c8ac5f6
Vadim Shtayura <vadimsh@chromium.org>
2024-04-26 19:26:15 +0000

    [swarming] Produce backward compatible JSON for BQ rows.
    It will be needed for PubSub pushes done by the BQ exporter.
    
    Unfortunately protobuf schema that BigQuery Write API wants
    is not compatible with schema of PubSub messages. This was not
    a problem for Python exporter because it was using deprecated
    JSON-based BigQuery API and could use custom proto => JSON
    converter. But in Go we must use the schema matching BQ rows.
    In particular, JSON-valued fields are `string` and fields
    representing durations are `double`.
    
    Convert such fields back to structpb.Struct{} and
    durationpb.Duration{} for PubSub export.
    
    R=chanli@chromium.org
    
    Change-Id: Idff389b88ff12b8add4a748bf44a5429ab101a9f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5492881
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

---
+++ swarming/server/bq/json.go
@@ -0,0 +1,194 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bq
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/known/durationpb"
+
+	"go.chromium.org/luci/common/errors"
+
+	bqpb "go.chromium.org/luci/swarming/proto/bq"
+)
+
+// fixers know how to "fix" messages of given proto types.
+var fixers map[protoreflect.MessageDescriptor]messageFixer
+
+// messageFixer knows how to "fix" JSON dicts matching particular proto message.
+type messageFixer struct {
+	// Fixers to apply recursively, keyed by JSON field name.
+	perKey map[string]func(in any) (any, error)
+}
+
+func init() {
+	// Preprocess all exported message types to know what JSON fields need to be
+	// post-processed in exportToJSON.
+	fixers = map[protoreflect.MessageDescriptor]messageFixer{}
+	for _, msg := range []proto.Message{
+		&bqpb.TaskRequest{},
+		&bqpb.BotEvent{},
+		&bqpb.TaskResult{},
+	} {
+		typ := msg.ProtoReflect().Descriptor()
+		if fixer := buildMessageFixer(typ); len(fixer.perKey) != 0 {
+			fixers[typ] = fixer
+		}
+	}
+}
+
+// exportToJSON converts a protobuf message to JSON.
+//
+// It understands `swarming.bq.marshal_to_json_as` field options.
+func exportToJSON(msg proto.Message) (string, error) {
+	// Only types known in advance are supported here.
+	fixer, ok := fixers[msg.ProtoReflect().Descriptor()]
+	if !ok {
+		panic("unexpected message type")
+	}
+
+	// Serialize to JSON using default proto encoding rules.
+	raw, err := protojson.Marshal(msg)
+	if err != nil {
+		return "", err
+	}
+
+	// Convert to a structured Go value that can be "fixed".
+	var dict map[string]any
+	if err := json.Unmarshal(raw, &dict); err != nil {
+		return "", err
+	}
+
+	// Recursively "fix" necessary parts.
+	if err := fixMsg(dict, fixer); err != nil {
+		return "", err
+	}
+
+	// Convert back to pretty JSON.
+	blob, err := json.MarshalIndent(dict, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(blob), nil
+}
+
+// fixMsg applies fixes to a JSON dict, recursively.
+func fixMsg(msg map[string]any, fixer messageFixer) error {
+	for key, val := range msg {
+		if val == nil {
+			continue // can't fix null
+		}
+		fixField, ok := fixer.perKey[key]
+		if !ok {
+			continue // no need to fix this field
+		}
+		if reflect.TypeOf(val).Kind() == reflect.Slice {
+			// This is a slice, apply the fixer to each individual element.
+			s := reflect.ValueOf(val)
+			for i := 0; i < s.Len(); i++ {
+				val := s.Index(i)
+				fixed, err := fixField(val.Interface())
+				if err != nil {
+					return errors.Annotate(err, "%q[%d]", key, i).Err()
+				}
+				val.Set(reflect.ValueOf(fixed))
+			}
+		} else {
+			// This is some elementary type or a message. Fix it directly.
+			fixed, err := fixField(val)
+			if err != nil {
+				return errors.Annotate(err, "%q", key).Err()
+			}
+			msg[key] = fixed
+		}
+	}
+	return nil
+}
+
+// buildMessageFixer traverses proto message descriptor and builds a fixer.
+func buildMessageFixer(typ protoreflect.MessageDescriptor) messageFixer {
+	fixer := messageFixer{perKey: map[string]func(in any) (any, error){}}
+	fields := typ.Fields()
+
+	for i := 0; i < fields.Len(); i++ {
+		field := fields.Get(i)
+		jsonKey := field.JSONName()
+
+		// See if this field is annotated with `marshal_to_json_as` option.
+		if opts := field.Options(); opts != nil && proto.HasExtension(opts, bqpb.E_MarshalToJsonAs) {
+			kind := proto.GetExtension(opts, bqpb.E_MarshalToJsonAs).(bqpb.MarshalKind)
+			switch kind {
+			case bqpb.MarshalKind_JSON:
+				if field.Kind() != protoreflect.StringKind {
+					panic("marshal_to_json_as=JSON can be applied only to string fields")
+				}
+				fixer.perKey[jsonKey] = jsonFixer
+			case bqpb.MarshalKind_DURATION:
+				if field.Kind() != protoreflect.DoubleKind {
+					panic("marshal_to_json_as=DURATION can be applied only to double fields")
+				}
+				fixer.perKey[jsonKey] = durationFixer
+			default:
+				panic(fmt.Sprintf("unknown marshal_to_json_as %v", kind))
+			}
+			continue
+		}
+
+		// Maybe some nested message needs fixes.
+		if field.Kind() == protoreflect.MessageKind {
+			if inner := buildMessageFixer(field.Message()); len(inner.perKey) != 0 {
+				fixer.perKey[jsonKey] = func(in any) (any, error) {
+					if err := fixMsg(in.(map[string]any), inner); err != nil {
+						return nil, err
+					}
+					return in, nil
+				}
+			}
+		}
+	}
+
+	return fixer
+}
+
+// jsonFixer knows how to replace a JSON string with a structured JSON dict.
+//
+// Broken JSON values are silently skipped and replaced with null.
+func jsonFixer(in any) (any, error) {
+	var msg map[string]any
+	if err := json.Unmarshal([]byte(in.(string)), &msg); err != nil {
+		return nil, nil
+	}
+	return msg, nil
+}
+
+// durationFixer knows how to replace a double with a proto duration encoding.
+func durationFixer(in any) (any, error) {
+	secs := in.(float64)
+	dur := durationpb.New(time.Duration(secs * float64(time.Second)))
+	blob, err := protojson.Marshal(dur)
+	if err != nil {
+		return nil, err
+	}
+	// We get JSON encoding, which is a JSON string. Get rid of `"`, they will be
+	// added later when we serialize the final result as JSON.
+	return strings.Trim(string(blob), `"`), nil
+}
---
+++ swarming/server/bq/json_test.go
@@ -0,0 +1,125 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bq
+
+import (
+	"testing"
+
+	bqpb "go.chromium.org/luci/swarming/proto/bq"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestExportToJSON(t *testing.T) {
+	t.Parallel()
+
+	Convey("JSON works", t, func() {
+		out, err := exportToJSON(&bqpb.BotEvent{
+			Bot: &bqpb.Bot{
+				BotId: "bot-id",
+				Info: &bqpb.BotInfo{
+					Supplemental: `{"k1": "v1"}`,
+					Version:      "version",
+					Host: &bqpb.PhysicalEntity{
+						Supplemental: `{"k2": "v2"}`,
+					},
+					Devices: []*bqpb.PhysicalEntity{
+						{Supplemental: `{"k3": "v3"}`},
+						{Supplemental: `{"k4": "v4"}`},
+					},
+				},
+			},
+			Event:    bqpb.BotEventType_BOT_NEW_SESSION,
+			EventMsg: "hello",
+		})
+		So(err, ShouldBeNil)
+		So(out, ShouldEqual, `{
+  "bot": {
+    "botId": "bot-id",
+    "info": {
+      "devices": [
+        {
+          "supplemental": {
+            "k3": "v3"
+          }
+        },
+        {
+          "supplemental": {
+            "k4": "v4"
+          }
+        }
+      ],
+      "host": {
+        "supplemental": {
+          "k2": "v2"
+        }
+      },
+      "supplemental": {
+        "k1": "v1"
+      },
+      "version": "version"
+    }
+  },
+  "event": "BOT_NEW_SESSION",
+  "eventMsg": "hello"
+}`)
+	})
+
+	Convey("Duration works", t, func() {
+		out, err := exportToJSON(&bqpb.TaskRequest{
+			TaskSlices: []*bqpb.TaskSlice{
+				{
+					Properties: &bqpb.TaskProperties{
+						ExecutionTimeout: 1.5,
+						IoTimeout:        2.5,
+						GracePeriod:      3.5,
+					},
+					Expiration: 4.5,
+				},
+				{
+					Properties: &bqpb.TaskProperties{
+						ExecutionTimeout: 5.5,
+						IoTimeout:        6.5,
+						GracePeriod:      7.5,
+					},
+					Expiration: 8.5,
+				},
+			},
+			BotPingTolerance: 9.5,
+		})
+		So(err, ShouldBeNil)
+		So(out, ShouldEqual, `{
+  "botPingTolerance": "9.500s",
+  "taskSlices": [
+    {
+      "expiration": "4.500s",
+      "properties": {
+        "executionTimeout": "1.500s",
+        "gracePeriod": "3.500s",
+        "ioTimeout": "2.500s"
+      }
+    },
+    {
+      "expiration": "8.500s",
+      "properties": {
+        "executionTimeout": "5.500s",
+        "gracePeriod": "7.500s",
+        "ioTimeout": "6.500s"
+      }
+    }
+  ]
+}`)
+	})
+}
