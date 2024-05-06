38c54da35c1a850eabf5890c1b3e6783b8b69de5
Greg NISBET <gregorynisbet@google.com>
2024-05-06 21:12:50 +0000

    [iputil] Add utility functions for IP addresses
    Add utility functions for IP addresses. Future CLs will add functions for getting
    sizes of ranges of IP addresses and iterating over IP addresses and other routine
    stuff that's done in UFS but isn't really specific to UFS.
    
    BUG=b:187899200
    TEST=make test
    
    Change-Id: Id250e3e4a7b044578f3c539d9ea0bcf6f2ab5dcb
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4987965
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

---
+++ common/net/iputil/iputil.go
@@ -0,0 +1,47 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package iputil is a collection of utilities for manipulating IP addresses that
+// has been extracted from UFS because it is not UFS-specific.
+package iputil
+
+import (
+	"encoding/binary"
+	"net"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+// IPv4StrToInt returns an uint32 address from the given ipv4 address string.
+//
+// IPv6 addresses or non-parseable strings will return an error.
+func IPv4StrToInt(ipAddress string) (uint32, error) {
+	ip := net.ParseIP(ipAddress)
+	if ip != nil {
+		ip = ip.To4()
+	}
+	if ip == nil {
+		return 0, errors.Reason("invalid IPv4 address %q", ipAddress).Err()
+	}
+	return binary.BigEndian.Uint32(ip), nil
+}
+
+// IPv4IntToStr returns a string ipv4 address
+//
+// Ipv6 addresses, or non-parseable strings will return an error.
+func IPv4IntToStr(ipAddress uint32) string {
+	ip := make(net.IP, 4)
+	binary.BigEndian.PutUint32(ip, ipAddress)
+	return ip.String()
+}
---
+++ common/net/iputil/iputil_test.go
@@ -0,0 +1,93 @@
+// Copyright 2023 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package iputil
+
+import (
+	"testing"
+	"testing/quick"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// TestIPv4StrToInt tests IPv4StrToInt on a few specific examples.
+func TestIPv4StrToInt(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name  string
+		input string
+		want  uint32
+		ok    bool
+	}{
+		{
+			name:  "zero",
+			input: "0.0.0.0",
+			want:  0x00_00_00_00,
+			ok:    true,
+		},
+		{
+			name:  "simple address",
+			input: "1.2.3.4",
+			want:  0x01_02_03_04,
+			ok:    true,
+		},
+		{
+			name:  "not an address",
+			input: "aaaaaaaaa",
+			want:  0,
+			ok:    false,
+		},
+	}
+
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := IPv4StrToInt(tt.input)
+			switch {
+			case tt.ok && err != nil:
+				t.Error(err)
+			case !tt.ok && err == nil:
+				t.Error("error is unexpectedly nil")
+			}
+
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("(-want +got): %s", diff)
+			}
+		})
+	}
+}
+
+// TestIPv4RoundTrip tests that converting to and from an IPv4 using the helper functions round trips.
+func TestIPv4RoundTrip(t *testing.T) {
+	t.Parallel()
+
+	roundTrip := func(ip uint32) bool {
+		ipStr := IPv4IntToStr(ip)
+		roundTripped, err := IPv4StrToInt(ipStr)
+		if err != nil {
+			return false
+		}
+		if ip != roundTripped {
+			return false
+		}
+		return true
+	}
+
+	if err := quick.Check(roundTrip, nil); err != nil {
+		t.Error(err)
+	}
+}
