1e5bd06348e94739b480e699463b2500e639247b
Robert Iannucci <iannucci@chromium.org>
2024-05-08 01:47:46 +0000

    [assert/should] Add nil/zero comparisons.
    R=gregorynisbet@google.com
    
    Change-Id: I91477470cb8c34d6ad36c4aaecb7f4f7cfd455d0
    Bug: 319730573
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/4916225
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Gregory Nisbet <gregorynisbet@google.com>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>

---
+++ common/testing/assert/should/nil.go
@@ -0,0 +1,99 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package should
+
+import (
+	"reflect"
+
+	"go.chromium.org/luci/common/testing/assert/comparison"
+)
+
+func isNil(cmpName string, actual any) (isNil bool, ret *comparison.Failure) {
+	defer func() {
+		if recover() != nil {
+			ret = comparison.NewFailureBuilder(cmpName).
+				Because("`%T` cannot be checked for nil", actual).
+				Failure
+		}
+	}()
+	isNil = reflect.ValueOf(actual).IsNil()
+	return
+}
+
+// BeNil implements comparison.Func[any] and asserts that `actual` is a nillable
+// type (like a pointer, slice, channel, etc) and is nil. This will also accept
+// untyped nils (e.g. `any(nil)`).
+//
+// Note that this is a stricter form than should.BeZero.
+func BeNil(actual any) *comparison.Failure {
+	const cmpName = "should.BeNil"
+
+	if actual == nil {
+		return nil
+	}
+	if isNil, rslt := isNil(cmpName, actual); isNil || rslt != nil {
+		return rslt
+	}
+
+	return comparison.NewFailureBuilder(cmpName).Actual(actual).Failure
+}
+
+// BeNil implements comparison.Func[any] and asserts that `actual` is a nillable
+// type (like a pointer, slice, channel, etc) and is not nil.
+//
+// Note that this is a slightly stricter form than should.NotBeZero.
+func NotBeNil(actual any) *comparison.Failure {
+	const cmpName = "should.NotBeNil"
+
+	if actual != nil {
+		if isNil, rslt := isNil(cmpName, actual); !isNil || rslt != nil {
+			return rslt
+		}
+	}
+
+	return comparison.NewFailureBuilder(cmpName).Failure
+}
+
+// BeZero implements comparison.Func[any] and asserts that `actual` is
+// a zero value (by Go's definition of zero values, e.g. empty strings, structs,
+// nil pointers, etc).
+func BeZero(actual any) *comparison.Failure {
+	const cmpName = "should.BeZero"
+
+	if actual == nil {
+		return nil
+	}
+	if reflect.ValueOf(actual).IsZero() {
+		return nil
+	}
+
+	return comparison.NewFailureBuilder(cmpName, actual).
+		Actual(actual).Failure
+}
+
+// NotBeZero implements comparison.Func[any] and asserts that `actual` is
+// a non-zero value (by Go's definition of zero values, e.g. empty strings, structs,
+// nil pointers, etc).
+func NotBeZero(actual any) *comparison.Failure {
+	const cmpName = "should.NotBeZero"
+
+	if actual != nil {
+		if !reflect.ValueOf(actual).IsZero() {
+			return nil
+		}
+	}
+
+	return comparison.NewFailureBuilder(cmpName, actual).Failure
+}
---
+++ common/testing/assert/should/nil_test.go
@@ -0,0 +1,65 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package should
+
+import (
+	"testing"
+)
+
+func TestBeNil(t *testing.T) {
+	t.Parallel()
+
+	t.Run("untyped", shouldPass(BeNil(nil)))
+	t.Run("slice", shouldPass(BeNil([]int(nil))))
+	t.Run("pointer", shouldPass(BeNil((*struct{})(nil))))
+
+	t.Run("empty string not nil", shouldFail(BeNil(""), "cannot be checked for nil"))
+	t.Run("non-empty slice", shouldFail(BeNil([]string{"yo"}), "Actual"))
+}
+
+func TestNotBeNil(t *testing.T) {
+	t.Parallel()
+
+	t.Run("non-empty slice", shouldPass(NotBeNil([]string{"yo"})))
+	t.Run("pointer", shouldPass(NotBeNil(&struct{}{})))
+
+	t.Run("untyped", shouldFail(NotBeNil(nil)))
+	t.Run("slice", shouldFail(NotBeNil([]int(nil))))
+	t.Run("channel", shouldFail(NotBeNil((chan int)(nil))))
+
+	t.Run("empty string not nil", shouldFail(NotBeNil(""), "cannot be checked for nil"))
+}
+
+func TestBeZero(t *testing.T) {
+	t.Parallel()
+
+	t.Run("nil", shouldPass(BeZero(nil)))
+	t.Run("nil slice", shouldPass(BeZero([]int(nil))))
+	t.Run("nil pointer", shouldPass(BeZero((*struct{})(nil))))
+	t.Run("empty string", shouldPass(BeZero("")))
+	t.Run("int", shouldPass(BeZero(0)))
+
+	t.Run("allocated slice", shouldFail(BeZero([]int{})))
+}
+
+func TestNotBeZero(t *testing.T) {
+	t.Parallel()
+
+	t.Run("string", shouldPass(NotBeZero("hi")))
+	t.Run("int", shouldPass(NotBeZero(100)))
+
+	t.Run("nil", shouldFail(NotBeZero(nil)))
+	t.Run("empty string", shouldFail(NotBeZero("")))
+}
