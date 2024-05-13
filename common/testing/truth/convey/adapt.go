439025c9c6715fb902ad6ddecdce99e06b7dac0a
Robert Iannucci <iannucci@chromium.org>
2024-05-13 18:08:14 +0000

    [truth/convey] Add Adapt function for old comparisons.
    R=gregorynisbet@google.com
    
    Bug: 319730573
    Change-Id: Iff58f5e958e15efe03f2911f9b729058513b5c70
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5530610
    Reviewed-by: Gregory Nisbet <gregorynisbet@google.com>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>

---
+++ common/testing/truth/convey/adapt.go
@@ -0,0 +1,54 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package convey provides a temporary symbol which converts a convey-style
+// comparison of the type:
+//
+//	func(actual any, expected ...any) string
+//
+// Into an truth compatible symbol:
+//
+//	comparison.Func[any]
+package convey
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+
+	"go.chromium.org/luci/common/testing/truth/comparison"
+)
+
+// Adapt takes an old convey-style comparison function and converts it to a
+// comparison.Func[any].
+//
+// This is intended to be a temporary tool which should only be used during the
+// transitional period where we switch away from Convey-style assertions. Code
+// using this adapter should be rewritten to exclusively use truth/comparison.
+//
+// # Example
+//
+//	assert.That(t, actualValue, convey.Adapt(myCustomShouldFunction)(expected))
+func Adapt(oldComparison func(actual any, expected ...any) string) func(expected ...any) comparison.Func[any] {
+	return func(expected ...any) comparison.Func[any] {
+		return func(actual any) *comparison.Failure {
+			text := oldComparison(actual, expected...)
+			if text == "" {
+				return nil
+			}
+			name := runtime.FuncForPC(reflect.ValueOf(oldComparison).Pointer()).Name()
+			return comparison.NewFailureBuilder(fmt.Sprintf("adapt.Convey(%s)", name)).Because(text).Failure
+		}
+	}
+}
---
+++ common/testing/truth/convey/adapt_test.go
@@ -0,0 +1,87 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package convey
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/smartystreets/goconvey/convey"
+	"go.chromium.org/luci/common/testing/truth/assert"
+	"go.chromium.org/luci/common/testing/truth/comparison"
+	"go.chromium.org/luci/common/testing/truth/should"
+)
+
+type myType struct {
+	strVal string
+	intVal int
+
+	ignoreInternalCallback func()
+}
+
+func shouldMatchMyType(actual any, expected ...any) string {
+	// NOTE: Propery convey assertions should be able to return JSON in
+	// their return value. This is incredibly tedious to implement, so most
+	// custom comparisons don't do this.
+
+	if len(expected) > 1 {
+		return convey.ShouldHaveLength(expected, 1)
+	}
+	mine, ok := actual.(myType)
+	if !ok {
+		return fmt.Sprintf("actual must be myType, got %T", actual)
+	}
+	other, ok := expected[0].(myType)
+	if !ok {
+		return fmt.Sprintf("other must be myType, got %T", expected[0])
+	}
+	if mine.strVal != other.strVal {
+		return fmt.Sprintf(
+			"actual.strVal != other.strVal: %q vs %q",
+			mine.strVal, other.strVal)
+	}
+
+	if mine.intVal != other.intVal {
+		return fmt.Sprintf(
+			"actual.intVal != other.intVal: %d vs %d",
+			mine.intVal, other.intVal)
+	}
+	return ""
+}
+
+func TestAdapt(t *testing.T) {
+	exampleMyTypeFromProdCode := myType{intVal: 100, ignoreInternalCallback: func() {}}
+
+	// have to use assert.Loosely because Adapt returns comparison.Func[any]
+	assert.Loosely(t, exampleMyTypeFromProdCode, Adapt(shouldMatchMyType)(myType{intVal: 100}))
+
+	// check for failures
+	assert.That(t, Adapt(shouldMatchMyType)(myType{intVal: 100})("smarfle"), should.Resemble(&comparison.Failure{
+		Comparison: &comparison.Failure_ComparisonFunc{
+			Name: "adapt.Convey(go.chromium.org/luci/common/testing/truth/convey.shouldMatchMyType)",
+		},
+		Findings: []*comparison.Failure_Finding{
+			{Name: "Because", Value: []string{"actual must be myType, got string"}},
+		},
+	}))
+	assert.That(t, Adapt(shouldMatchMyType)(myType{intVal: 100})(myType{strVal: "100"}), should.Resemble(&comparison.Failure{
+		Comparison: &comparison.Failure_ComparisonFunc{
+			Name: "adapt.Convey(go.chromium.org/luci/common/testing/truth/convey.shouldMatchMyType)",
+		},
+		Findings: []*comparison.Failure_Finding{
+			{Name: "Because", Value: []string{`actual.strVal != other.strVal: "100" vs ""`}},
+		},
+	}))
+}
