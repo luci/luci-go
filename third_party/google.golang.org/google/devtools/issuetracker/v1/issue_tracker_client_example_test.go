b6eee06cdcf329bbce422dfdaa856b0bbc403135
Weiwei Lin <weiweilin@google.com>
2024-05-15 05:26:25 +0000

    issuetracker: update RPC bindings to include the access_limit field
    R=mdraz@google.com
    
    Bug: b/336424075
    Change-Id: Ifa80360c49d18ca422444abc5ecb21e7147d9491
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5540832
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Patrick Meiring <meiring@google.com>

--- third_party/google.golang.org/google/devtools/issuetracker/v1/issue_tracker_client_example_test.go
+++ third_party/google.golang.org/google/devtools/issuetracker/v1/issue_tracker_client_example_test.go
@@ -332,6 +332,12 @@ func ExampleClient_ListIssueComments() {
 		}
 		// TODO: Use resp.
 		_ = resp
+
+		// If you need to access the underlying RPC response,
+		// you can do so by casting the `Response` as below.
+		// Otherwise, remove this line. Only populated after
+		// first call to Next(). Not safe for concurrent access.
+		_ = it.Response.(*issuetrackerpb.ListIssueCommentsResponse)
 	}
 }
 
@@ -388,6 +394,12 @@ func ExampleClient_ListIssueUpdates() {
 		}
 		// TODO: Use resp.
 		_ = resp
+
+		// If you need to access the underlying RPC response,
+		// you can do so by casting the `Response` as below.
+		// Otherwise, remove this line. Only populated after
+		// first call to Next(). Not safe for concurrent access.
+		_ = it.Response.(*issuetrackerpb.ListIssueUpdatesResponse)
 	}
 }
 
@@ -419,6 +431,12 @@ func ExampleClient_ListIssues() {
 		}
 		// TODO: Use resp.
 		_ = resp
+
+		// If you need to access the underlying RPC response,
+		// you can do so by casting the `Response` as below.
+		// Otherwise, remove this line. Only populated after
+		// first call to Next(). Not safe for concurrent access.
+		_ = it.Response.(*issuetrackerpb.ListIssuesResponse)
 	}
 }
 
