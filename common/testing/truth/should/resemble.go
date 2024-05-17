161efc345bf7fd02eda9f22f7d8caddc8a6bbbe9
Robert Iannucci <iannucci@chromium.org>
2024-05-17 01:46:55 +0000

    [testing/truth/should] Make should.Resemble behave like ShouldResemble.
    Goconvey's ShouldResemble comparison function always compares
    unexported fields (because this is what reflect.DeepEqual does).
    This change makes should.Resemble also have this same behavior by
    passing in a list of cmp.AllowUnexported() for every type reachable
    from the current argument's type.
    
    This implements a WORM cache for compared types, because typical
    packages end up comparing the same types with unexported fields
    multiple times per test process.
    
    This CL also marks should.Resemble as deprecated, and suggests
    the new should.Match instead, which has explicit user-controlled
    semantics around unexported fields.
    
    R=gregorynisbet@google.com
    
    Bug: 319730573
    Change-Id: Id195c8012e2c57185a6dbd53e888cfb63d0071a4
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5543796
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Gregory Nisbet <gregorynisbet@google.com>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>

--- common/testing/truth/should/resemble.go
+++ common/testing/truth/should/resemble.go
@@ -15,14 +15,146 @@
 package should
 
 import (
+	"reflect"
+	"sync"
+
 	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/proto"
 
 	"go.chromium.org/luci/common/testing/truth/comparison"
 	"go.chromium.org/luci/common/testing/truth/failure"
 	"go.chromium.org/luci/common/testing/typed"
 )
 
-// Resemble returns a Comparison which checks if the actual value 'resembles'
+// should.Resemble needs to keep a cache of type -> []cmp.Option for
+// AllowUnexporteds. This is faster in packages which do many tests over the
+// same types.
+//
+// Note that during extractAllowUnexportedFromLocked we intentionally insert
+// `nil` into resembleOptionCache so that when walking recursive types we don't
+// recurse infinitely. By the time that resembleOptionCacheMu is released, all
+// nil entries in resembleOptionCache will be replaced with []cmp.Option if
+// there were any options to add, or it will remain nil in the event that no
+// structs with unexported fields were discovered. Either way, the next call to
+// extractAllowUnexportedFrom/extractAllowUnexportedFromLocked will get a cache
+// hit.
+var resembleOptionCacheMu sync.RWMutex
+var resembleOptionCache = map[reflect.Type][]cmp.Option{}
+
+// resetOptionCache is only called from tests inside the `should` package
+// itself.
+func resetOptionCache() {
+	resembleOptionCacheMu.Lock()
+	defer resembleOptionCacheMu.Unlock()
+	resembleOptionCache = map[reflect.Type][]cmp.Option{}
+}
+
+// We want to be able to ignore any struct fields of type *Something where this
+// is a proto message.
+var protoMessageType = reflect.TypeFor[proto.Message]()
+
+// extractAllowUnexportedFrom returns a list of cmp.Option which include cmp.AllowUnexported
+// options for all struct types which include unexported fields which are
+// reachable from `typ`.
+//
+// The result of this function is cached behind an RWMutex, so the second call
+// to this for the same type should be fast.
+func extractAllowUnexportedFrom(typ reflect.Type) []cmp.Option {
+	resembleOptionCacheMu.RLock()
+	val, ok := resembleOptionCache[typ]
+	resembleOptionCacheMu.RUnlock()
+	if ok {
+		return val
+	}
+
+	resembleOptionCacheMu.Lock()
+	defer resembleOptionCacheMu.Unlock()
+	return extractAllowUnexportedFromLocked(typ)
+}
+
+// extractAllowUnexportedFromLocked checks the cache for a set of
+// cmp.AllowUnexported in the cache, and if it's missing, computes it.
+func extractAllowUnexportedFromLocked(typ reflect.Type) []cmp.Option {
+	// First, check the cache.
+	if val, ok := resembleOptionCache[typ]; ok {
+		// could be returning []cmp.Option or nil, if something higher in the stack
+		// is already working on `typ`, in the case of a recursive type.
+		return val
+	}
+
+	switch typ.Kind() {
+	case reflect.Struct:
+		// only structs may have unexported fields. cmp.AllowUnexported will fail
+		// for any types that are not Struct.
+		var ret []cmp.Option
+
+		// mark this entry in the cache as being worked on - this allows recursive
+		// types to compute successfully.
+		resembleOptionCache[typ] = nil
+
+		hasUnexported := false
+		for i := 0; i < typ.NumField(); i++ {
+			field := typ.Field(i)
+			if !hasUnexported && !field.IsExported() {
+				// It looks like this struct, itself, has unexported fields, so we at
+				// least need an AllowUnexported for ourself.
+				ret = append(ret, cmp.AllowUnexported(reflect.New(typ).Elem().Interface()))
+				hasUnexported = true
+			}
+			// We also will want to include AllowUnexporteds for any sub-types implied
+			// by this field.
+			ret = append(ret, extractAllowUnexportedFromLocked(field.Type)...)
+		}
+
+		// actually populate the entry now
+		resembleOptionCache[typ] = ret
+		return ret
+
+	case reflect.Pointer:
+		// proto messages are explicitly handled by the default cmp.Transform from
+		// the protocmp package so we do not want to recurse here.
+		if typ.Implements(protoMessageType) {
+			return nil
+		}
+
+		fallthrough
+	case reflect.Slice, reflect.Array, reflect.Chan:
+		// Chan is included here for completness, but I doubt that you can
+		// cmp.Diff one of them with any manner of success.
+		return extractAllowUnexportedFromLocked(typ.Elem())
+
+	case reflect.Map:
+		// note that key types can be `struct` so we need to explore them as well.
+		return append(extractAllowUnexportedFromLocked(typ.Key()), extractAllowUnexportedFromLocked(typ.Elem())...)
+	}
+
+	// all other types don't need any special treatment
+	return nil
+}
+
+// Resemble returns a comparison.Func which checks if the actual value 'resembles'
+// `expected`, but implicitly compares unexported fields for compatibility with
+// goconvey's ShouldResemble check.
+//
+// Deprecated: Use should.Match instead, which does not automatically
+// add AllowUnexported(expected). should.Match also allows specifying your own
+// auxilliary cmp.Options globally and locally, for more advanced interaction
+// with cmp.Diff.
+//
+// Semblance is computed with "github.com/google/go-cmp/cmp", and this
+// function automatically adds the following cmp.Options to match behavior of
+// coconvey:
+//   - "google.golang.org/protobuf/testing/protocmp".Transform()
+//   - "github.com/google/go-cmp/cmp".AllowUnexported(expected) (if `expected`
+//     has an underlying struct type after peeling off slices, pointers, etc.)
+//
+// It is recommended that you use should.Equal when comparing primitive types.
+func Resemble[T any](expected T) comparison.Func[T] {
+	return matchImpl(
+		"should.Resemble", expected, extractAllowUnexportedFrom(reflect.TypeOf(expected)))
+}
+
+// Match returns a comparison.Func which checks if the actual value matches
 // `expected`.
 //
 // Semblance is computed with "github.com/google/go-cmp/cmp", and this
@@ -34,14 +166,21 @@ import (
 //
 // This is done via the go.chromium.org/luci/common/testing/registry package,
 // which also allows process-wide registration of additional default
-// cmp.Options, should you need it.
+// cmp.Options. It is recommended to register cmp.Options in TestMain for any
+// types that your package tests which contain unexported fields, or other
+// internal details. Note that cmp.Diff implicitly will use `X.Equal(X)`
+// methods for any types which implement them as methods, so this may be a good
+// first thing to implement.
 //
 // It is recommended that you use should.Equal when comparing primitive types.
-func Resemble[T any](expected T, opts ...cmp.Option) comparison.Func[T] {
-	cmpName := "should.Resemble"
+func Match[T any](expected T, opts ...cmp.Option) comparison.Func[T] {
+	return matchImpl("should.Match", expected, opts)
+}
 
+// matchImpl is the implementation of Match and Resemble.
+func matchImpl[T any](cmpName string, expected T, opts []cmp.Option) comparison.Func[T] {
 	return func(actual T) *failure.Summary {
-		diff := typed.Diff(expected, actual)
+		diff := typed.Diff(expected, actual, opts...)
 
 		if diff == "" {
 			return nil
--- common/testing/truth/should/resemble_test.go
+++ common/testing/truth/should/resemble_test.go
@@ -15,21 +15,24 @@
 package should
 
 import (
+	"reflect"
 	"testing"
 
 	"google.golang.org/protobuf/types/known/structpb"
 
 	"go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/common/testing/ftt"
+	"go.chromium.org/luci/common/testing/truth/assert"
 )
 
-func TestResemble(t *testing.T) {
+func TestMatch(t *testing.T) {
 	t.Parallel()
 
-	t.Run("simple", shouldPass(Resemble(100)(100)))
-	t.Run("simple fail", shouldFail(Resemble(100)(101), "Diff"))
+	t.Run("simple", shouldPass(Match(100)(100)))
+	t.Run("simple fail", shouldFail(Match(100)(101), "Diff"))
 
 	t.Run("simple proto", shouldPass(
-		Resemble(&buildbucketpb.Build{Id: 12345})(&buildbucketpb.Build{Id: 12345})))
+		Match(&buildbucketpb.Build{Id: 12345})(&buildbucketpb.Build{Id: 12345})))
 
 	props, err := structpb.NewStruct(map[string]any{
 		"heyo": 100,
@@ -38,7 +41,123 @@ func TestResemble(t *testing.T) {
 		t.Fatal("could not make struct", err)
 	}
 	t.Run("struct proto fail", shouldFail(
-		Resemble(&buildbucketpb.Build{Id: 12345, Input: &buildbucketpb.Build_Input{
+		Match(&buildbucketpb.Build{Id: 12345, Input: &buildbucketpb.Build_Input{
 			Properties: props,
 		}})(&buildbucketpb.Build{Id: 12345}), "Diff"))
+
+	t.Run("unexported fields", func(t *testing.T) {
+		type myStruct struct {
+			private string
+		}
+		mustPanicLike(t, "unexported field", func() {
+			Match(myStruct{"hi"})(myStruct{"hi"})
+		})
+	})
+}
+
+func TestResemble(t *testing.T) {
+	// Note that Resemble and Match are the same except for unexported fields.
+
+	type myStruct struct {
+		private string
+	}
+	t.Run("unexported fields", shouldPass(Resemble(myStruct{"hi"})(myStruct{"hi"})))
+}
+
+func TestResembleTypeWalking(t *testing.T) {
+	// t.Parallel() - this involves testing the resembleOptionCache, so do not run
+	// this test in parallel with anything else.
+
+	// Ensure this test never leaks option cache entries.
+	//
+	// If the option cache is working correctly, it's not a big deal to leak, but
+	// if it's not, this may help minimize blast radius from this test to others
+	// in this package.
+	defer resetOptionCache()
+
+	ftt.Run("with clean state", t, func(t *ftt.Test) {
+		// Always start with an empty cache
+		resetOptionCache()
+
+		t.Run("simple types leave no entries", func(t *ftt.Test) {
+			assert.Loosely(t, extractAllowUnexportedFrom(reflect.TypeOf("hello")), BeNil)
+			assert.Loosely(t, resembleOptionCache, BeEmpty)
+		})
+
+		t.Run("struct types with no unexported fields leaves nil entry", func(t *ftt.Test) {
+			type myStruct struct {
+				Field string
+			}
+			typ := reflect.TypeFor[myStruct]()
+			assert.Loosely(t, extractAllowUnexportedFrom(typ), BeNil)
+			assert.Loosely(t, resembleOptionCache, HaveLength(1))
+			assert.Loosely(t, resembleOptionCache[typ], BeNil)
+		})
+
+		t.Run("struct types with no exported fields leaves single entry", func(t *ftt.Test) {
+			type myStruct struct {
+				fieldA string
+				fieldB string
+			}
+			typ := reflect.TypeFor[myStruct]()
+			assert.Loosely(t, extractAllowUnexportedFrom(typ), HaveLength(1))
+			assert.Loosely(t, resembleOptionCache, HaveLength(1))
+			assert.Loosely(t, resembleOptionCache[typ], HaveLength(1))
+
+			t.Run("and we can see the cache hit in code coverage", func(t *ftt.Test) {
+				// note that extractAllowUnexportedFrom(typ) is already cached in this
+				// subtest.
+				assert.Loosely(t, extractAllowUnexportedFrom(typ), HaveLength(1))
+			})
+		})
+
+		t.Run("*struct types with no exported fields leaves single entry", func(t *ftt.Test) {
+			type myStruct struct {
+				fieldA string
+				fieldB string
+			}
+			typ := reflect.TypeFor[*myStruct]()
+			assert.Loosely(t, extractAllowUnexportedFrom(typ), HaveLength(1))
+			assert.Loosely(t, resembleOptionCache, HaveLength(1))
+			// note that myStruct is in cache, not *myStruct
+			assert.Loosely(t, resembleOptionCache[typ.Elem()], HaveLength(1))
+		})
+
+		t.Run("recursive struct types with no exported fields leaves single entry", func(t *ftt.Test) {
+			type myStruct struct {
+				fieldA string
+				fieldB string
+
+				sub *myStruct
+			}
+			typ := reflect.TypeFor[*myStruct]()
+			assert.Loosely(t, extractAllowUnexportedFrom(typ), HaveLength(1))
+			assert.Loosely(t, resembleOptionCache, HaveLength(1))
+			// note that myStruct is in cache, not *myStruct
+			assert.Loosely(t, resembleOptionCache[typ.Elem()], HaveLength(1))
+		})
+
+		t.Run("maps with struct keys work", func(t *ftt.Test) {
+			type myKey struct {
+				field string
+			}
+			type myValue struct {
+				thing int
+			}
+			assert.Loosely(t, extractAllowUnexportedFrom(reflect.TypeFor[map[myKey]*myValue]()), HaveLength(2))
+			assert.Loosely(t, resembleOptionCache, HaveLength(2))
+			assert.Loosely(t, resembleOptionCache[reflect.TypeFor[myKey]()], HaveLength(1))
+			assert.Loosely(t, resembleOptionCache[reflect.TypeFor[myValue]()], HaveLength(1))
+		})
+
+		t.Run("proto fields are ignored", func(t *ftt.Test) {
+			type myStruct struct {
+				Struct *structpb.Struct
+			}
+			assert.Loosely(t, extractAllowUnexportedFrom(reflect.TypeFor[myStruct]()), BeEmpty)
+			assert.Loosely(t, resembleOptionCache, HaveLength(1))
+			assert.Loosely(t, resembleOptionCache[reflect.TypeFor[myStruct]()], BeNil)
+		})
+
+	})
 }
