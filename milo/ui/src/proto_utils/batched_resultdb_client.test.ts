70012210d811ab876a19d6e801e1b2381d0b84de
Weiwei Lin <weiweilin@google.com>
2024-05-30 00:22:36 +0000

    luci-ui: implement BatchedResultDBClientImpl
    This allows us to batch BatchGetTestVariants RPC calls together
    automatically.
    
    R=mdraz@google.com
    
    Bug: b/321110247
    Change-Id: Id986fbd96fc08241d7b91353ce70e57e296e6780
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5570031
    Reviewed-by: Beining Chen <beining@google.com>
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>

---
+++ milo/ui/src/proto_utils/batched_resultdb_client.test.ts
@@ -0,0 +1,491 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {
+  BatchGetTestVariantsRequest,
+  BatchGetTestVariantsResponse,
+  ResultDBClientImpl,
+} from '@/proto/go.chromium.org/luci/resultdb/proto/v1/resultdb.pb';
+
+import { BatchedResultDBClientImpl } from './batched_resultdb_client';
+
+describe('BatchedResultDBClientImpl', () => {
+  let batchGetTestVariants: jest.SpiedFunction<
+    BatchedResultDBClientImpl['BatchGetTestVariants']
+  >;
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    batchGetTestVariants = jest
+      .spyOn(ResultDBClientImpl.prototype, 'BatchGetTestVariants')
+      .mockImplementation(async (batchReq) => {
+        return BatchGetTestVariantsResponse.fromPartial({
+          testVariants: Object.freeze(
+            batchReq.testVariants.map((tv) => ({
+              testId: tv.testId,
+              variantHash: tv.variantHash,
+              sourcesId: tv.variantHash.split('-')[0],
+            })),
+          ),
+          sources: Object.fromEntries(
+            batchReq.testVariants.map(
+              (tv) =>
+                [
+                  tv.variantHash.split('-')[0],
+                  {
+                    gitilesCommit: { commitHash: tv.variantHash.split('-')[0] },
+                  },
+                ] as const,
+            ),
+          ),
+        });
+      });
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    batchGetTestVariants.mockReset();
+  });
+
+  it('can batch eligible requests together', async () => {
+    const client = new BatchedResultDBClientImpl(
+      {
+        request: jest.fn(),
+      },
+      { maxBatchSize: 5 },
+    );
+
+    const call1 = client.BatchGetTestVariants(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv1',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test1', variantHash: 'source1-hash1' },
+          { testId: 'test2', variantHash: 'source2-hash1' },
+        ]),
+      }),
+    );
+    const call2 = client.BatchGetTestVariants(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv2',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test3', variantHash: 'source1-hash1' },
+          { testId: 'test4', variantHash: 'source2-hash1' },
+        ]),
+      }),
+    );
+    const call3 = client.BatchGetTestVariants(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv1',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test5', variantHash: 'source1-hash1' },
+          { testId: 'test6', variantHash: 'source2-hash1' },
+        ]),
+      }),
+    );
+    const call4 = client.BatchGetTestVariants(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv2',
+        resultLimit: 5,
+        testVariants: Object.freeze([
+          { testId: 'test7', variantHash: 'source1-hash1' },
+          { testId: 'test8', variantHash: 'source2-hash1' },
+        ]),
+      }),
+    );
+    const call5 = client.BatchGetTestVariants(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv2',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test9', variantHash: 'source1-hash1' },
+        ]),
+      }),
+    );
+
+    await jest.advanceTimersToNextTimerAsync();
+    // The batch function should be called only once for each
+    // `invocation` + `resultLimit` combination.
+    expect(batchGetTestVariants).toHaveBeenCalledTimes(3);
+    expect(batchGetTestVariants).toHaveBeenCalledWith(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv1',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test1', variantHash: 'source1-hash1' },
+          { testId: 'test2', variantHash: 'source2-hash1' },
+          { testId: 'test5', variantHash: 'source1-hash1' },
+          { testId: 'test6', variantHash: 'source2-hash1' },
+        ]),
+      }),
+    );
+    expect(batchGetTestVariants).toHaveBeenCalledWith(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv2',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test3', variantHash: 'source1-hash1' },
+          { testId: 'test4', variantHash: 'source2-hash1' },
+          { testId: 'test9', variantHash: 'source1-hash1' },
+        ]),
+      }),
+    );
+    expect(batchGetTestVariants).toHaveBeenCalledWith(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv2',
+        resultLimit: 5,
+        testVariants: Object.freeze([
+          { testId: 'test7', variantHash: 'source1-hash1' },
+          { testId: 'test8', variantHash: 'source2-hash1' },
+        ]),
+      }),
+    );
+
+    // The responses should be just like regular calls.
+    expect(await call1).toEqual(
+      BatchGetTestVariantsResponse.fromPartial({
+        testVariants: Object.freeze([
+          {
+            testId: 'test1',
+            variantHash: 'source1-hash1',
+            sourcesId: 'source1',
+          },
+          {
+            testId: 'test2',
+            variantHash: 'source2-hash1',
+            sourcesId: 'source2',
+          },
+        ]),
+        sources: {
+          source1: {
+            gitilesCommit: {
+              commitHash: 'source1',
+            },
+          },
+          source2: {
+            gitilesCommit: {
+              commitHash: 'source2',
+            },
+          },
+        },
+      }),
+    );
+    expect(await call2).toEqual(
+      BatchGetTestVariantsResponse.fromPartial({
+        testVariants: Object.freeze([
+          {
+            testId: 'test3',
+            variantHash: 'source1-hash1',
+            sourcesId: 'source1',
+          },
+          {
+            testId: 'test4',
+            variantHash: 'source2-hash1',
+            sourcesId: 'source2',
+          },
+        ]),
+        sources: {
+          source1: {
+            gitilesCommit: {
+              commitHash: 'source1',
+            },
+          },
+          source2: {
+            gitilesCommit: {
+              commitHash: 'source2',
+            },
+          },
+        },
+      }),
+    );
+    expect(await call3).toEqual(
+      BatchGetTestVariantsResponse.fromPartial({
+        testVariants: Object.freeze([
+          {
+            testId: 'test5',
+            variantHash: 'source1-hash1',
+            sourcesId: 'source1',
+          },
+          {
+            testId: 'test6',
+            variantHash: 'source2-hash1',
+            sourcesId: 'source2',
+          },
+        ]),
+        sources: {
+          source1: {
+            gitilesCommit: {
+              commitHash: 'source1',
+            },
+          },
+          source2: {
+            gitilesCommit: {
+              commitHash: 'source2',
+            },
+          },
+        },
+      }),
+    );
+    expect(await call4).toEqual(
+      BatchGetTestVariantsResponse.fromPartial({
+        testVariants: Object.freeze([
+          {
+            testId: 'test7',
+            variantHash: 'source1-hash1',
+            sourcesId: 'source1',
+          },
+          {
+            testId: 'test8',
+            variantHash: 'source2-hash1',
+            sourcesId: 'source2',
+          },
+        ]),
+        sources: {
+          source1: {
+            gitilesCommit: {
+              commitHash: 'source1',
+            },
+          },
+          source2: {
+            gitilesCommit: {
+              commitHash: 'source2',
+            },
+          },
+        },
+      }),
+    );
+    expect(await call5).toEqual(
+      BatchGetTestVariantsResponse.fromPartial({
+        testVariants: Object.freeze([
+          {
+            testId: 'test9',
+            variantHash: 'source1-hash1',
+            sourcesId: 'source1',
+          },
+        ]),
+        sources: {
+          source1: {
+            gitilesCommit: {
+              commitHash: 'source1',
+            },
+          },
+        },
+      }),
+    );
+  });
+
+  it('can handle over batching', async () => {
+    const client = new BatchedResultDBClientImpl(
+      {
+        request: jest.fn(),
+      },
+      { maxBatchSize: 5 },
+    );
+
+    const call1 = client.BatchGetTestVariants(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv1',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test1', variantHash: 'source1-hash1' },
+          { testId: 'test2', variantHash: 'source2-hash1' },
+        ]),
+      }),
+    );
+    const call2 = client.BatchGetTestVariants(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv2',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test3', variantHash: 'source1-hash1' },
+          { testId: 'test4', variantHash: 'source2-hash1' },
+        ]),
+      }),
+    );
+    const call3 = client.BatchGetTestVariants(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv1',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test5', variantHash: 'source1-hash1' },
+          { testId: 'test6', variantHash: 'source2-hash1' },
+        ]),
+      }),
+    );
+    const call4 = client.BatchGetTestVariants(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv2',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test7', variantHash: 'source1-hash1' },
+          { testId: 'test8', variantHash: 'source2-hash1' },
+        ]),
+      }),
+    );
+    const call5 = client.BatchGetTestVariants(
+      BatchGetTestVariantsRequest.fromPartial({
+        invocation: 'invocations/inv1',
+        resultLimit: 10,
+        testVariants: Object.freeze([
+          { testId: 'test9', variantHash: 'source1-hash1' },
+          { testId: 'test10', variantHash: 'source1-hash2' },
+        ]),
+      }),
+    );
+
+    await jest.advanceTimersToNextTimerAsync();
+    // The batch function should be called 3 times.
+    // 2 time2 for inv1, 1 time for inv2.
+    expect(batchGetTestVariants).toHaveBeenCalledTimes(3);
+
+    // The responses should be just like regular calls.
+    expect(await call1).toEqual(
+      BatchGetTestVariantsResponse.fromPartial({
+        testVariants: Object.freeze([
+          {
+            testId: 'test1',
+            variantHash: 'source1-hash1',
+            sourcesId: 'source1',
+          },
+          {
+            testId: 'test2',
+            variantHash: 'source2-hash1',
+            sourcesId: 'source2',
+          },
+        ]),
+        sources: {
+          source1: {
+            gitilesCommit: {
+              commitHash: 'source1',
+            },
+          },
+          source2: {
+            gitilesCommit: {
+              commitHash: 'source2',
+            },
+          },
+        },
+      }),
+    );
+    expect(await call2).toEqual(
+      BatchGetTestVariantsResponse.fromPartial({
+        testVariants: Object.freeze([
+          {
+            testId: 'test3',
+            variantHash: 'source1-hash1',
+            sourcesId: 'source1',
+          },
+          {
+            testId: 'test4',
+            variantHash: 'source2-hash1',
+            sourcesId: 'source2',
+          },
+        ]),
+        sources: {
+          source1: {
+            gitilesCommit: {
+              commitHash: 'source1',
+            },
+          },
+          source2: {
+            gitilesCommit: {
+              commitHash: 'source2',
+            },
+          },
+        },
+      }),
+    );
+    expect(await call3).toEqual(
+      BatchGetTestVariantsResponse.fromPartial({
+        testVariants: Object.freeze([
+          {
+            testId: 'test5',
+            variantHash: 'source1-hash1',
+            sourcesId: 'source1',
+          },
+          {
+            testId: 'test6',
+            variantHash: 'source2-hash1',
+            sourcesId: 'source2',
+          },
+        ]),
+        sources: {
+          source1: {
+            gitilesCommit: {
+              commitHash: 'source1',
+            },
+          },
+          source2: {
+            gitilesCommit: {
+              commitHash: 'source2',
+            },
+          },
+        },
+      }),
+    );
+    expect(await call4).toEqual(
+      BatchGetTestVariantsResponse.fromPartial({
+        testVariants: Object.freeze([
+          {
+            testId: 'test7',
+            variantHash: 'source1-hash1',
+            sourcesId: 'source1',
+          },
+          {
+            testId: 'test8',
+            variantHash: 'source2-hash1',
+            sourcesId: 'source2',
+          },
+        ]),
+        sources: {
+          source1: {
+            gitilesCommit: {
+              commitHash: 'source1',
+            },
+          },
+          source2: {
+            gitilesCommit: {
+              commitHash: 'source2',
+            },
+          },
+        },
+      }),
+    );
+    expect(await call5).toEqual(
+      BatchGetTestVariantsResponse.fromPartial({
+        testVariants: Object.freeze([
+          {
+            testId: 'test9',
+            variantHash: 'source1-hash1',
+            sourcesId: 'source1',
+          },
+          {
+            testId: 'test10',
+            variantHash: 'source1-hash2',
+            sourcesId: 'source1',
+          },
+        ]),
+        sources: {
+          source1: {
+            gitilesCommit: {
+              commitHash: 'source1',
+            },
+          },
+        },
+      }),
+    );
+  });
+});
---
+++ milo/ui/src/proto_utils/batched_resultdb_client.ts
@@ -0,0 +1,109 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { BatchOption, batched } from '@/generic_libs/tools/batched_fn';
+import { Sources } from '@/proto/go.chromium.org/luci/resultdb/proto/v1/invocation.pb';
+import {
+  BatchGetTestVariantsRequest,
+  BatchGetTestVariantsResponse,
+  ResultDBClientImpl,
+} from '@/proto/go.chromium.org/luci/resultdb/proto/v1/resultdb.pb';
+
+import { Rpc } from './types';
+
+const MAX_BATCH_SIZE = 100;
+
+export interface BatchedResultDBClientImplOpts {
+  readonly service?: string;
+  /**
+   * Maximum number of requests in a given batch request. Defaults to 100.
+   */
+  readonly maxBatchSize?: number;
+}
+
+/**
+ * The same as `ResultDBClientImpl` except that eligible RPC calls are batched
+ * automatically. Only RPC calls made via the same client instance can be
+ * batched together.
+ */
+export class BatchedResultDBClientImpl extends ResultDBClientImpl {
+  private readonly autoBatchedBatchGetTestVariants: (
+    opt: BatchOption,
+    req: BatchGetTestVariantsRequest,
+  ) => Promise<BatchGetTestVariantsResponse>;
+
+  constructor(rpc: Rpc, opts?: BatchedResultDBClientImplOpts) {
+    super(rpc, opts);
+    const maxBatchSize = opts?.maxBatchSize || MAX_BATCH_SIZE;
+
+    this.autoBatchedBatchGetTestVariants = batched({
+      fn: (req: BatchGetTestVariantsRequest) => super.BatchGetTestVariants(req),
+      shardFn: (req: BatchGetTestVariantsRequest) =>
+        `invocation/${req.invocation}/result-limit/${req.resultLimit}`,
+      combineParamSets([req1], [req2]) {
+        if (
+          req1.testVariants.length + req2.testVariants.length >
+          maxBatchSize
+        ) {
+          return {
+            ok: false,
+            value: null,
+          };
+        }
+
+        return {
+          ok: true,
+          value: [
+            {
+              invocation: req1.invocation,
+              resultLimit: req1.resultLimit,
+              testVariants: [...req1.testVariants, ...req2.testVariants],
+            },
+          ],
+        };
+      },
+      splitReturn(paramSets, ret) {
+        let pivot = 0;
+        const splitRets: BatchGetTestVariantsResponse[] = [];
+        for (const [req] of paramSets) {
+          const testVerdicts = ret.testVariants.slice(
+            pivot,
+            pivot + req.testVariants.length,
+          );
+
+          const sources: { [key: string]: Sources } = {};
+          for (const tv of testVerdicts) {
+            sources[tv.sourcesId] = ret.sources[tv.sourcesId];
+          }
+
+          splitRets.push({
+            testVariants: testVerdicts,
+            sources,
+          });
+
+          pivot += req.testVariants.length;
+        }
+
+        return splitRets;
+      },
+    });
+  }
+
+  BatchGetTestVariants(
+    request: BatchGetTestVariantsRequest,
+    opt: BatchOption = {},
+  ): Promise<BatchGetTestVariantsResponse> {
+    return this.autoBatchedBatchGetTestVariants(opt, request);
+  }
+}
