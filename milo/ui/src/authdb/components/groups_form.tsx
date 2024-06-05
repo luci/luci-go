68e6c01d84c9d3e1c4e50aa4a0acd60579e7c336
Heidi Chan <heidichan@google.com>
2024-06-05 05:42:08 +0000

    [auth-service-ui] editable groups form for frontend.
    - Button will switch between edit (textfields) and readonly text for each section.
    - editable textfield region for description/owners.
    - Enter button works on textfields.
    - can add (textfield) and remove members/subgroups from list.
    - Submit button added but not yet functional.
    - only frontend changes atm, no api calls.
    - resets to readonly mode when new group selected.
    - Changed some styling.
    Bug: b/335076325
    Change-Id: Icdfb32293f0324a3c598266c4bf15113be9c0358
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5584163
    Commit-Queue: Heidi Chan <heidichan@google.com>
    Reviewed-by: Anne Redulla <aredulla@google.com>

--- milo/ui/src/authdb/components/groups_form.tsx
+++ milo/ui/src/authdb/components/groups_form.tsx
@@ -11,26 +11,49 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
-import List from '@mui/material/List';
-import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import ClearIcon from '@mui/icons-material/Clear';
+import CancelIcon from '@mui/icons-material/Cancel';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import DoneIcon from '@mui/icons-material/Done';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { FormControl } from '@mui/material';
-import { FormLabel } from '@mui/material';
 import { useAuthServiceClient } from '@/authdb/hooks/prpc_clients';
 import { useQuery } from '@tanstack/react-query';
+import { useState, useEffect } from 'react';
 
 interface GroupsFormProps {
     name: string;
 }
+const theme = createTheme({
+  typography: {
+    h6: {
+      color: 'black',
+    },
+  },
+  components: {
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          borderBottom: 'none',
+        }
+      }
+    },
+  },
+});
 
 // Strips '<prefix>:' from a string if it starts with it.
 function stripPrefix (prefix: string, str: string) {
@@ -45,19 +68,141 @@ function stripPrefix (prefix: string, str: string) {
 };
 
 export function GroupsForm({ name } : GroupsFormProps) {
-    const client = useAuthServiceClient();
+  const [descriptionMode, setDescriptionMode] = useState<boolean>();
+  const [ownersMode, setOwnersMode] = useState<boolean>();
+  const [membersMode, setMembersMode] = useState<boolean>();
+  const [subgroupsMode, setSubgroupsMode] = useState<boolean>();
+  const [addingMember, setAddingMember] = useState<boolean>();
+  const [addingSubgroup, setAddingSubgroup] = useState<boolean>();
+  const [members, setMembers] = useState<string[]>();
+  const [subgroups, setSubgroups] = useState<string[]>();
+  const [description, setDescription] = useState<string>();
+  const [owners, setOwners] = useState<string>();
+  const [currentMember, setCurrentMember] = useState<string>();
+  const [currentSubgroup, setCurrentSubgroup] = useState<string>();
+
+  const client = useAuthServiceClient();
     const {
       isLoading,
       isError,
-      data: response,
-      error,
+      error
     } = useQuery({
-      ...client.GetGroup.query({"name": name})
+      ...client.GetGroup.query({"name": name}),
+      onSuccess: (response) => {
+          let initialMembers: string[] = (response?.members)?.map((member => stripPrefix('user', member))) || [] as string[];
+          setMembers(initialMembers);
+          let initialSubgroups: string[] = (response?.nested || []) as string[];
+          setSubgroups(initialSubgroups);
+          let initialDescription: string = response?.description || "";
+          setDescription(initialDescription);
+          let initialOwners: string = response?.owners || "";
+          setOwners(initialOwners);
+          setReadonlyMode();
+      },
     })
-    const members: readonly string[] = (response?.members)?.map((member => stripPrefix('user', member))) || [];
-    const description: string = response?.description || "";
-    const owners: string = response?.owners || "";
-    const subgroups: readonly string[] = response?.nested || [];
+
+    const setReadonlyMode = () => {
+      setDescriptionMode(false);
+      setOwnersMode(false);
+      setMembersMode(false);
+      setSubgroupsMode(false);
+    }
+    const changeDescriptionMode = () => {
+      setDescriptionMode(!descriptionMode);
+    }
+    useEffect(() => {
+      if (descriptionMode) {
+        addDescriptionEventListener();
+      }
+      if (ownersMode) {
+        addOwnersEventListener();
+      }
+    }, [descriptionMode, ownersMode]);
+    const changeOwnersMode = () => {
+      setOwnersMode(!ownersMode);
+    }
+    const changeMembersMode = () => {
+      setMembersMode(!membersMode);
+    }
+    const changeSubgroupsMode = () => {
+      setSubgroupsMode(!subgroupsMode);
+    }
+    const changeAddingMember = () => {
+      setAddingMember(!addingMember);
+    }
+    const changeAddingSubgroup = () => {
+      setAddingSubgroup(!addingSubgroup);
+    }
+
+    const membersTextfield = document.getElementById('membersTextfield');
+    if (membersTextfield) {
+      membersTextfield.addEventListener('keydown', (e) => {
+        if (e.key == 'Enter') {
+          addToMembers(currentMember);
+        }
+      });
+    }
+
+    const subgroupsTextfield = document.getElementById('subgroupsTextfield');
+    if (subgroupsTextfield) {
+      subgroupsTextfield.addEventListener('keydown', (e) => {
+        if (e.key == 'Enter') {
+          addToSubgroups(currentSubgroup);
+        }
+      });
+    }
+
+    const addDescriptionEventListener = () => {
+      const descriptionTextfield = document.getElementById('descriptionTextfield');
+      if (descriptionTextfield) {
+        descriptionTextfield.addEventListener('keydown', (e) => {
+          if (e.key == 'Enter') {
+            setDescriptionMode(false);
+          };
+        });
+      }
+    }
+
+    const addOwnersEventListener = () => {
+      const ownersTextfield = document.getElementById('ownersTextfield');
+      if (ownersTextfield) {
+        ownersTextfield.addEventListener('keydown', (e) => {
+          if (e.key == 'Enter') {
+            setOwnersMode(false);
+          }
+        });
+      }
+    }
+
+    const addToMembers = (name: string | undefined) => {
+      if (name) {
+        let newMembers = Array.from(members || []);
+        newMembers.push(name);
+        setMembers(newMembers);
+        setCurrentMember('');
+      }
+    }
+
+    const addToSubgroups = (name: string | undefined) => {
+      if (name) {
+        let newSubgroups = Array.from(subgroups || []);
+        newSubgroups.push(name);
+        setSubgroups(newSubgroups);
+        setCurrentSubgroup('');
+      }
+    }
+
+    const removeFromMembers = (index: number) => {
+      let newMembers = Array.from(members || []);
+      newMembers.splice(index, 1);
+      setMembers(newMembers);
+    }
+
+    const removeFromSubgroups = (index: number) => {
+      let newSubgroups = Array.from(subgroups || []);
+      newSubgroups.splice(index, 1);
+      setSubgroups(newSubgroups);
+    }
 
     if (isLoading) {
       return (
@@ -80,8 +225,11 @@ export function GroupsForm({ name } : GroupsFormProps) {
 
   return (
     <Paper elevation={3} sx={{minHeight:'500px', p:'20px', ml:'5px'}}>
-      <FormControl data-testid="groups-form">
-        <Typography variant="h4" sx={{pb: 1.5}}> {name} </Typography>
+      <ThemeProvider theme={theme}>
+      <FormControl data-testid="groups-form" style={{width:'100%'}}>
+        <div>
+        <Typography variant="h4"> {name} </Typography>
+        </div>
         <TableContainer>
           <Table sx={{ p: 0, width: '100%' }}>
               <TableRow>
@@ -89,7 +237,20 @@ export function GroupsForm({ name } : GroupsFormProps) {
                   <Typography variant="h6"> Description</Typography>
                 </TableCell>
                 <TableCell>
-                  <Typography variant="body1"> {description} </Typography>
+                  {descriptionMode
+                  ? <TextField value={description} style={{width: '100%', whiteSpace:'pre-wrap'}} onChange={(e) => setDescription(e.target.value)} id='descriptionTextfield'></TextField>
+                  : <Typography variant="body1"> {description} </Typography>
+                  }
+                </TableCell>
+                <TableCell align='right'>
+                {descriptionMode
+                    ? <IconButton color='success' onClick={changeDescriptionMode} sx={{p: 0}}>
+                        <DoneIcon />
+                      </IconButton>
+                    : <IconButton color='primary' onClick={changeDescriptionMode} sx={{p: 0}}>
+                        <EditIcon />
+                      </IconButton>
+                    }
                 </TableCell>
               </TableRow>
               <TableRow>
@@ -97,28 +258,129 @@ export function GroupsForm({ name } : GroupsFormProps) {
                   <Typography variant="h6"> Owners</Typography>
                 </TableCell>
                 <TableCell>
-                  <Typography variant="body1"> {owners} </Typography>
+                  {ownersMode
+                  ? <TextField value={owners} style={{width: '100%'}} onChange={(e) => setOwners(e.target.value)} id='ownersTextfield'></TextField>
+                  : <Typography variant="body1"> {owners} </Typography>
+                  }
+                </TableCell>
+                <TableCell align='right'>
+                  <IconButton color='primary' onClick={changeOwnersMode} sx={{p: 0}}>
+                    {ownersMode
+                    ? <DoneIcon />
+                    : <EditIcon />
+                    }
+                  </IconButton>
                 </TableCell>
               </TableRow>
           </Table>
         </TableContainer>
-        <div>
-          <FormLabel>
-            <Typography variant="h6"> Members</Typography>
-          </FormLabel>
-          <List disablePadding>
-            {members.map((member, index) => <ListItemText key={index}>{member}</ListItemText>)}
-          </List>
-        </div>
-        <div>
-          <FormLabel>
-            <Typography variant="h6"> Subgroups</Typography>
-          </FormLabel>
-          <List disablePadding>
-            {subgroups.map((subgroup, index) => <ListItemText key={index}>{subgroup}</ListItemText>)}
-          </List>
-        </div>
+        <TableContainer>
+          <Table sx={{ p: 0, pt: '15px', width: '100%' }}>
+            <TableRow>
+              <TableCell colSpan={2}>
+                <Typography variant="h6"> Members</Typography>
+              </TableCell>
+              <TableCell align='right'>
+                  <IconButton color='primary' onClick={changeMembersMode} sx={{p: 0}}>
+                    {membersMode
+                    ? <ClearIcon />
+                    : <EditIcon />
+                    }
+                  </IconButton>
+                </TableCell>
+            </TableRow>
+            {members && members.map((member, index) =>
+              <TableRow key={index} style={{height: '34px'}} sx={{pb: 0, pt: 0, borderBottom: '1px solid grey'}}>
+                <TableCell sx={{pb: 0, pt: 0}} colSpan={2}>{member}</TableCell>
+                {membersMode && <TableCell align='right' sx={{pb: 0, pt: 0}}>
+                  <IconButton color='error' sx={{p: 0}} onClick={() => removeFromMembers(index as number)}><CancelIcon />
+                  </IconButton>
+                </TableCell>}
+              </TableRow>
+            )}
+            {membersMode && addingMember && (
+              <TableRow>
+                <TableCell sx={{p: 0, pt: '15px', pr: '15px'}} style={{width: '94%'}}>
+                  <TextField label='New Member' style={{width: '100%'}} onChange={(e) => setCurrentMember(e.target.value)} id='membersTextfield' value={currentMember}></TextField>
+                </TableCell>
+                <TableCell align='center' style={{width: '3%'}} sx={{p: 0, pt: '15px'}}>
+                  <IconButton color='success' sx={{p: 0}} onClick={() => {addToMembers(currentMember)}}>
+                    <DoneIcon />
+                  </IconButton>
+                </TableCell>
+                <TableCell align='center' style={{width: '3%'}} sx={{p: 0, pt: '15px'}}>
+                  <IconButton color='error' sx={{p: 0}} onClick={changeAddingMember}>
+                    <ClearIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>)
+            }
+            {membersMode && !addingMember &&
+              <TableRow>
+                <Button sx={{mt: '10px'}} variant="outlined" startIcon={<AddCircleIcon />} onClick={changeAddingMember}>
+                  Add
+                </Button>
+            </TableRow>
+            }
+          </Table>
+        </TableContainer>
+        <TableContainer>
+          <Table sx={{ p: 0, pt: '15px', width: '100%' }}>
+            <TableRow>
+              <TableCell colSpan={2}>
+                <Typography variant="h6"> Subgroups</Typography>
+              </TableCell>
+              <TableCell align='right'>
+                  <IconButton color='primary' onClick={changeSubgroupsMode} sx={{p: 0}}>
+                    {subgroupsMode
+                    ? <ClearIcon />
+                    : <EditIcon />
+                    }
+                  </IconButton>
+                </TableCell>
+            </TableRow>
+            {subgroups && subgroups.map((subgroup, index) =>
+              <TableRow key={index} style={{height: '34px'}} sx={{pb: 0, pt: 0, borderBottom: '1px solid grey'}}>
+                <TableCell sx={{pb: 0, pt: 0}} colSpan={2}>{subgroup}</TableCell>
+                {subgroupsMode && <TableCell align='right' sx={{pb: 0, pt: 0}}>
+                  <IconButton color='error' sx={{p: 0}} onClick={() => removeFromSubgroups(index as number)}><CancelIcon />
+                  </IconButton>
+                </TableCell>}
+              </TableRow>
+            )}
+            {subgroupsMode && addingSubgroup && (
+              <TableRow>
+                <TableCell sx={{p: 0, pt: '15px', pr: '15px'}} style={{width: '94%'}}>
+                  <TextField label='New Subgroup' style={{width: '100%'}} onChange={(e) => setCurrentSubgroup(e.target.value)} id='subgroupsTextfield' value={currentSubgroup}></TextField>
+                </TableCell>
+                <TableCell align='center' style={{width: '3%'}} sx={{p: 0, pt: '15px'}}>
+                  <IconButton color='success' sx={{p: 0}} onClick={() => {addToSubgroups(currentSubgroup)}}>
+                    <DoneIcon />
+                  </IconButton>
+                </TableCell>
+                <TableCell align='center' style={{width: '3%'}} sx={{p: 0, pt: '15px'}}>
+                  <IconButton color='error' sx={{p: 0}} onClick={changeAddingSubgroup}>
+                    <ClearIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>)
+            }
+            {subgroupsMode && !addingSubgroup &&
+              <TableRow>
+                <Button sx={{mt: '10px'}} variant="outlined" startIcon={<AddCircleIcon />} onClick={changeAddingSubgroup}>
+                  Add
+                </Button>
+            </TableRow>
+            }
+          </Table>
+        </TableContainer>
+        {(descriptionMode || ownersMode || membersMode || subgroupsMode) &&
+          <Button variant="contained" disableElevation style={{width: '15%', marginTop: '15px'}}>
+            Submit
+          </Button>
+        }
       </FormControl>
+      </ThemeProvider>
     </Paper>
   );
 }
