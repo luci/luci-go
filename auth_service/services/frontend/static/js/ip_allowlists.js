eb31a0febff4a30a8b456ccae6e7bddc347ab393
Anne Redulla <aredulla@google.com>
2024-07-05 05:02:18 +0000

    [auth-service] Handle allowlist selection better
    Also handles empty allowlist config now (which is technically
    valid).
    
    Bug: b/302615926
    Change-Id: I88eab5ba72c477ab8430ddf92ac0b7c722360f24
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5676020
    Reviewed-by: Rachael Newitt <renewitt@google.com>
    Commit-Queue: Anne Redulla <aredulla@google.com>
    Reviewed-by: Heidi Chan <heidichan@google.com>

--- auth_service/services/frontend/static/js/ip_allowlists.js
+++ auth_service/services/frontend/static/js/ip_allowlists.js
@@ -18,60 +18,39 @@ class Selector {
   constructor(element) {
     // Root DOM element.
     this.element = document.getElementById(element);
-    // Map of allowlists.
-    this.allowlistMap = {};
-    // Setup event listener for when selection changes.
-    this.element.addEventListener('change', (event) => {
-      // TODO(cjacomet): Improve this to not have to iterate over all options.
-      // Change which option has the selected attribute.
-      this.element.childNodes.forEach((option) => {
-        if (!option.dataset.selected && option.text === event.target.value) {
-          option.setAttribute('data-selected', 'selected');
-        } else {
-          option.setAttribute('data-selected', '');
-        }
-      });
-      this.onSelectionChanged(event.target.value);
-    });
+
+    // The IP allowlist names from which to select.
+    this.options = new Set();
+  }
+
+  select(name) {
+    if (this.options.has(name)) {
+      this.element.value = name;
+      this.element.dispatchEvent(new Event('change'));
+    }
   }
 
   populate(allowlists) {
     // Empty the element before populating.
     this.element.innerHTML = '';
-    let selected = null;
+    this.options.clear();
 
     let addToSelector = (name) => {
-      let option = document.createElement('option');
-      option.text = name;
-      option.setAttribute('data-allowlist-name', name);
-      this.element.appendChild(option);
-      if (selected === null) {
-        selected = option;
+      if (this.options.has(name)) {
+        return;
       }
+
+      this.options.add(name);
+      const option = document.createElement('option');
+      option.text = name;
+      option.setAttribute('value', name);
+      this.element.appendChild(option);
     };
 
     // All allowlists, populate the selector element.
     allowlists.map((list) => {
-      this.allowlistMap[list.name] = list;
       addToSelector(list.name);
     });
-
-    selected.setAttribute('data-selected', 'selected');
-    this.onSelectionChanged();
-  }
-
-  onSelectionChanged() {
-    let selectedOption = this.element.querySelector(
-      '[data-selected="selected"]'
-    );
-    const allowlistSelectedEvent = new CustomEvent('allowlistSelected', {
-      bubble: true,
-      detail: {
-        ip_allowlist: selectedOption.text,
-      },
-    });
-
-    this.element.dispatchEvent(allowlistSelectedEvent);
   }
 }
 
@@ -119,39 +98,66 @@ class RevisionDetails {
 
 window.onload = () => {
   const loadingBox = new common.LoadingBox('#loading-box-placeholder');
+
   const allowlistsContent = new common.HidableElement('#allowlists-content', false);
+  const allowlistsConfig = new common.HidableElement('#allowlists-config', false);
+  const emptyConfigFallback = new common.HidableElement('#empty-config-fallback', false);
+
   const errorBox = new common.ErrorBox('#api-error-placeholder');
 
   const selector = new Selector('allowlist-selector');
   const allowlistPane = new AllowlistPane();
   const revisionDetails = new RevisionDetails('revision-details');
 
-  selector.element.addEventListener('allowlistSelected', (event) => {
-    if (event.detail.ip_allowlist === null) {
-      return;
+  let ipAllowlists = new Map();
+  // Add a listener to update the allowlist details based on the current
+  // selection.
+  selector.element.addEventListener('change', (event) => {
+    const target = ipAllowlists.get(event.target.value);
+    if (target) {
+      allowlistPane.populate(target);
     }
-
-    allowlistPane.populate(selector.allowlistMap[event.detail.ip_allowlist]);
   });
 
-  const reloadAllowlists = () => {
+  const loadAllowlists = () => {
     errorBox.clearError();
     loadingBox.setLoadStatus(true);
     allowlistsContent.hide();
+    allowlistsConfig.hide();
+    emptyConfigFallback.hide();
+    ipAllowlists.clear();
 
     return api.ipAllowlists()
     .then((response) => {
-      selector.populate(response.allowlists);
+      const allowlists = response.allowlists || [];
+      selector.populate(allowlists);
+
+      if (allowlists.length === 0) {
+        emptyConfigFallback.show();
+      } else {
+        // Set the IP allowlist data.
+        response.allowlists.forEach((a) => {
+          ipAllowlists.set(a.name, a);
+        });
+
+        // Select the first IP allowlist by default.
+        const count = response.allowlists ? response.allowlists.length : 0;
+        if (count > 0) {
+          selector.select(response.allowlists[0].name);
+        }
+        allowlistsConfig.show();
+      }
+
       revisionDetails.setLink(response.configViewUrl, response.configRevision);
       allowlistsContent.show();
     })
     .catch((err) => {
-      errorBox.showError("Fetching  IP allowlists failed", err.error);
+      errorBox.showError("Fetching IP allowlists failed", err.error);
     })
     .finally(() => {
       loadingBox.setLoadStatus(false);
     });
   };
 
-  reloadAllowlists();
+  loadAllowlists();
 };
