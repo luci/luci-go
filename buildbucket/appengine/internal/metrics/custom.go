9c39a6f888ec6be71fc2c0e4fdbd43bbc9d0e31e
Chan Li <chanli@chromium.org>
2024-07-10 22:41:13 +0000

    [Buildbucket] Add Flush and Report for custom metrics
    Bug: 338071541
    Change-Id: Iddfd96cc109cec5c8f8b59463e8c880b776d6a82
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5623610
    Commit-Queue: Chan Li <chanli@chromium.org>
    Reviewed-by: Scott Lee <ddoman@chromium.org>

--- buildbucket/appengine/internal/metrics/custom.go
+++ buildbucket/appengine/internal/metrics/custom.go
@@ -16,8 +16,12 @@ package metrics
 
 import (
 	"context"
+	"fmt"
+	"sync"
 
+	"go.chromium.org/luci/common/data/stringset"
 	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/tsmon"
 	"go.chromium.org/luci/common/tsmon/metric"
 	"go.chromium.org/luci/common/tsmon/registry"
@@ -30,27 +34,19 @@ import (
 
 var cmKey = "custom_metrics"
 
+var currentCustomMetrics stringset.Set
+
 // CustomMetric is an interface to report custom metrics.
 type CustomMetric interface {
-	isDisabled() bool
-	setDisabled(disabled bool)
 	getFields() []string
+	report(ctx context.Context, value any, fields []any)
 }
 
 // customeMetricInfo holds the information about a registered custom metrics.
 type customeMetricInfo struct {
-	disabled bool
 	// Metric fields.
 	// Keep it here so that we could report the correct fields with correct order.
 	fields []string
-}
-
-func (cmi *customeMetricInfo) isDisabled() bool {
-	return cmi.disabled
-}
-
-func (cmi *customeMetricInfo) setDisabled(disabled bool) {
-	cmi.disabled = disabled
 }
 
 func (cmi *customeMetricInfo) getFields() []string {
@@ -62,9 +58,25 @@ type counter struct {
 	*customeMetricInfo
 }
 
+func (c *counter) report(ctx context.Context, value any, fields []any) {
+	v, ok := value.(int64)
+	if !ok {
+		panic(fmt.Sprintf("cannot convert %q to int64", value))
+	}
+	c.Add(ctx, v, fields...)
+}
+
 type cumulativeDistribution struct {
 	metric.CumulativeDistribution
 	*customeMetricInfo
+}
+
+func (cd *cumulativeDistribution) report(ctx context.Context, value any, fields []any) {
+	v, ok := value.(float64)
+	if !ok {
+		panic(fmt.Sprintf("cannot convert %q to float64", value))
+	}
+	cd.Add(ctx, v, fields...)
 }
 
 type float struct {
@@ -72,9 +84,25 @@ type float struct {
 	*customeMetricInfo
 }
 
+func (f *float) report(ctx context.Context, value any, fields []any) {
+	v, ok := value.(float64)
+	if !ok {
+		panic(fmt.Sprintf("cannot convert %q to float64", value))
+	}
+	f.Set(ctx, v, fields...)
+}
+
 type int struct {
 	metric.Int
 	*customeMetricInfo
+}
+
+func (i *int) report(ctx context.Context, value any, fields []any) {
+	v, ok := value.(int64)
+	if !ok {
+		panic(fmt.Sprintf("cannot convert %q to int64", value))
+	}
+	i.Set(ctx, v, fields...)
 }
 
 // withCustomMetrics returns a new context with the given map of custom metrics.
@@ -97,6 +125,153 @@ func getCustomMetrics(ctx context.Context) *CustomMetrics {
 type CustomMetrics struct {
 	metrics map[pb.CustomBuildMetricBase]map[string]CustomMetric
 	state   *tsmon.State
+	buf     chan *Report
+	m       sync.RWMutex
+}
+
+type Report struct {
+	Base     pb.CustomBuildMetricBase
+	Name     string
+	FieldMap map[string]string
+	Value    any
+}
+
+// Report tries to report a metric. If it couldn't do the report directly (e.g.
+// a metric state recreation is happening), it will save the report to a buffer.
+// Returns a bool to indicate if it's a direct report or not (mostly for testing).
+func (cms *CustomMetrics) Report(ctx context.Context, r *Report) bool {
+	cms.m.RLock()
+	defer cms.m.RUnlock()
+
+	if cms.state != nil {
+		cm := cms.metrics[r.Base][r.Name]
+		if cm == nil {
+			return false
+		}
+		fields := convertFields(cm.getFields(), r.FieldMap)
+		ctx = tsmon.WithState(ctx, cms.state)
+		cm.report(ctx, r.Value, fields)
+		return true
+	}
+
+	// Updating metrics. Save the report in buffer for now.
+	select {
+	case cms.buf <- r:
+	default:
+		// This can happen only if Flush is stuck for too long for some reason.
+		logging.Errorf(ctx, "Custom metric buffer is full, dropping the report")
+	}
+
+	return false
+}
+
+func convertFields(fieldKeys []string, fieldMap map[string]string) []any {
+	fields := make([]any, len(fieldKeys))
+	for i, fk := range fieldKeys {
+		fields[i] = fieldMap[fk]
+	}
+	return fields
+}
+
+// Flush flushes all stored metrics.
+// But before flush, it also checks if there's any change in service config
+// that requires to update cms.metrics and recreate cms.state.
+// If yes, it'll reset cms.state - so the upcoming reports can be
+// buffered, then perform the updates.
+// After cms.metrics are updated, it then adds all the buffered reports back
+// to the new cms.state.
+//
+// Called by a backendground job periodically. Must not be called concurrently.
+func (cms *CustomMetrics) Flush(ctx context.Context, globalCfg *pb.SettingsCfg) error {
+	cms.m.RLock()
+	state := cms.state
+	metrics := cms.metrics
+	cms.m.RUnlock()
+
+	if state == nil {
+		panic("no state found for custom metrics")
+	}
+
+	// Updates on custom metrics in service config should be rare, check if
+	// an update is needed first.
+	newCmsInCfg := convertCustomMetricConfig(globalCfg)
+	needUpdate := checkUpdates(newCmsInCfg)
+	if !needUpdate {
+		return state.Flush(ctx, nil)
+	}
+
+	// Need to update cms.metrics and recreate cms.state.
+	// Reset cms.state for now so that we could do it safely.
+	buf := make(chan *Report, 10000)
+	cms.m.Lock()
+	if cms.state != state {
+		panic("unexpected state")
+	}
+	cms.state = nil
+	cms.buf = buf
+	cms.m.Unlock()
+
+	defer func() {
+		cms.m.Lock()
+		if cms.state != nil {
+			panic("unexpected state")
+		}
+		// Restore cms.state and cms.metrics after metrics update (and perhaps
+		// state recreation).
+		// We are inside **the exclusive** lock. It means there's no one
+		// holding the reader lock, i.e. all `cms.buf <- r` calls are done.
+		// Once we pass this section, new reports will be written to
+		// `state` directly.
+		cms.state = state
+		cms.metrics = metrics
+		cms.buf = nil
+		cms.m.Unlock()
+
+		// Wait for all reports to be added to buffer then report all of them.
+		// They will be flushed in the next Flush.
+		// By now cms.state has been either recreated or restored, so new Reports
+		// will stop using the buffer.
+		// Should be safe since Flush is called by a backendground job periodically,
+		// so there should not be multiple Flush running currenctly.
+		close(buf)
+		for rep := range buf {
+			cms.Report(ctx, rep)
+		}
+	}()
+
+	// Flush existing reports in state.
+	err := state.Flush(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	// Recreate metrics and state.
+	// It is safe to just replace cms.state, since we flushed all data in it and
+	// all new data is currently being accumulated in cms.buf.
+	// update cms.state and cms.metrics according to new config.
+	newCms, err := newCustomMetrics(globalCfg)
+	if err == nil {
+		metrics, state = newCms.metrics, newCms.state
+		currentCustomMetrics = newCmsInCfg
+	}
+	return err
+}
+
+// checkUpdates checks if there's any update for custom metrics in service config.
+func checkUpdates(new stringset.Set) bool {
+	return currentCustomMetrics.Difference(new).Len() != 0 || new.Difference(currentCustomMetrics).Len() != 0
+}
+
+// convertCustomMetricConfig converts custom metrics from globalCfg to a stringset
+// so it'll be easier to check differences of custom metrics from different
+// versions of globalCfgs.
+func convertCustomMetricConfig(globalCfg *pb.SettingsCfg) stringset.Set {
+	cmSet := stringset.New(0)
+	for _, cm := range globalCfg.CustomMetrics {
+		cmStr := fmt.Sprintf("%s:%s:%q", cm.GetMetricBase(), cm.Name, cm.Fields)
+		cmSet.Add(cmStr)
+	}
+	return cmSet
 }
 
 // WithCustomMetrics creates a CustomMetrics with dedicated tsmon.State to
@@ -108,6 +283,7 @@ func WithCustomMetrics(ctx context.Context, globalCfg *pb.SettingsCfg) (context.
 	if err != nil {
 		return ctx, err
 	}
+	currentCustomMetrics = convertCustomMetricConfig(globalCfg)
 	return withCustomMetrics(ctx, cmsInCtx), nil
 }
 
@@ -159,8 +335,7 @@ func newMetric(cm *pb.CustomMetric, registry *registry.Registry) (CustomMetric, 
 	}
 
 	info := &customeMetricInfo{
-		disabled: cm.Disabled,
-		fields:   cm.Fields,
+		fields: cm.Fields,
 	}
 
 	switch base := cm.GetMetricBase(); base {
--- buildbucket/appengine/internal/metrics/custom_test.go
+++ buildbucket/appengine/internal/metrics/custom_test.go
@@ -16,6 +16,7 @@ package metrics
 
 import (
 	"context"
+	"sync"
 	"testing"
 
 	"google.golang.org/protobuf/encoding/prototext"
@@ -69,8 +70,17 @@ func init() {
 func resetCustomMetrics(ctx context.Context) context.Context {
 	cfg := &pb.SettingsCfg{}
 	_ = prototext.Unmarshal([]byte(cfgContent), cfg)
+	ctx = WithServiceInfo(ctx, "svc", "job", "ins")
+	ctx = WithBuilder(ctx, "project", "bucket", "builder")
 	ctx, _ = WithCustomMetrics(ctx, cfg)
 	return ctx
+}
+
+func getCurrentMetricsAndState(ctx context.Context) (map[pb.CustomBuildMetricBase]map[string]CustomMetric, *tsmon.State) {
+	cms := getCustomMetrics(ctx)
+	cms.m.RLock()
+	defer cms.m.RUnlock()
+	return cms.metrics, cms.state
 }
 
 func TestWithCustomMetrics(t *testing.T) {
@@ -81,8 +91,7 @@ func TestWithCustomMetrics(t *testing.T) {
 		_ = prototext.Unmarshal([]byte(cfgContent), globalCfg)
 
 		Convey("check metrics from test context", func() {
-			cms := getCustomMetrics(ctx)
-			v2Custom := cms.metrics
+			v2Custom, _ := getCurrentMetricsAndState(ctx)
 			startM := v2Custom[pb.CustomBuildMetricBase_CUSTOM_BUILD_METRIC_BASE_STARTED]["/chrome/infra/custom/builds/started"]
 			_, ok := startM.(*counter)
 			So(ok, ShouldBeTrue)
@@ -95,6 +104,93 @@ func TestWithCustomMetrics(t *testing.T) {
 			countM := v2Custom[pb.CustomBuildMetricBase_CUSTOM_BUILD_METRIC_BASE_COUNT]["/chrome/infra/custom/builds/count"]
 			_, ok = countM.(*int)
 			So(ok, ShouldBeTrue)
+		})
+	})
+}
+
+func TestUpdateCustomMetrics(t *testing.T) {
+	ctx, _ := tsmon.WithDummyInMemory(testContext)
+
+	Convey("Flush/report", t, func() {
+		ctx = resetCustomMetrics(ctx)
+		globalCfg := &pb.SettingsCfg{}
+		_ = prototext.Unmarshal([]byte(cfgContent), globalCfg)
+		Convey("normal report", func() {
+			cms := getCustomMetrics(ctx)
+			// Normal report.
+			cms.Report(ctx, &Report{
+				Base: pb.CustomBuildMetricBase_CUSTOM_BUILD_METRIC_BASE_STARTED,
+				Name: "/chrome/infra/custom/builds/started",
+				FieldMap: map[string]string{
+					"os": "linux",
+				},
+				Value: int64(1),
+			})
+			err := cms.Flush(ctx, globalCfg)
+			So(err, ShouldBeNil)
+		})
+
+		flushAndMultiReports := func(globalCfg *pb.SettingsCfg, buffered *bool) {
+			var wg sync.WaitGroup
+			var mu sync.Mutex
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				cms := getCustomMetrics(ctx)
+				_ = cms.Flush(ctx, globalCfg)
+			}()
+
+			for i := 0; i < 10; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					cms := getCustomMetrics(ctx)
+					directUpdated := cms.Report(ctx, &Report{
+						Base: pb.CustomBuildMetricBase_CUSTOM_BUILD_METRIC_BASE_STARTED,
+						Name: "/chrome/infra/custom/builds/started",
+						FieldMap: map[string]string{
+							"experiments": "None",
+							"os":          "linux",
+						},
+						Value: int64(1),
+					})
+					mu.Lock()
+					if !directUpdated && !*buffered {
+						*buffered = true
+					}
+					mu.Unlock()
+				}()
+			}
+			wg.Wait()
+		}
+
+		Convey("normal report synced", func() {
+			oldCms := getCustomMetrics(ctx)
+			oldState := oldCms.state
+
+			buffered := false
+			flushAndMultiReports(globalCfg, &buffered)
+			So(buffered, ShouldBeFalse)
+			newCms := getCustomMetrics(ctx)
+			So(newCms.state, ShouldEqual, oldState)
+		})
+
+		Convey("with a metric updating fields", func() {
+			oldCms := getCustomMetrics(ctx)
+			oldState := oldCms.state
+
+			buffered := false
+			globalCfg.CustomMetrics[0].Fields = append(globalCfg.CustomMetrics[0].Fields, "new_field")
+			flushAndMultiReports(globalCfg, &buffered)
+			// This line makes the test flaky.
+			//So(buffered, ShouldBeTrue)
+			newCms := getCustomMetrics(ctx)
+			newCms.m.RLock()
+			So(newCms.state, ShouldNotEqual, oldState)
+			newCms.m.RUnlock()
+			So(len(oldCms.buf), ShouldEqual, 0)
+			So(len(newCms.buf), ShouldEqual, 0)
 		})
 	})
 }
