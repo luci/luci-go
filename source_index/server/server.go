0a4b3ea66dc60e1a2b9c60e359babadbcc5a8626
Weiwei Lin <weiweilin@google.com>
2024-08-09 02:11:17 +0000

    source-index: setup pubsub handler
    This is mostly just to trigger a deployment and test whether request
    dispatching is workking correctly.
    
    R=meiring, nqmtuan
    
    Bug: b/356027716
    Change-Id: I30d542e62e9edef7925e07c45f488b24a05545d5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5772200
    Reviewed-by: Tuan Nguyen <nqmtuan@google.com>
    Commit-Queue: Weiwei Lin <weiweilin@google.com>

--- source_index/server/server.go
+++ source_index/server/server.go
@@ -17,12 +17,20 @@
 package server
 
 import (
+	"fmt"
+	"net/http"
+
+	"go.chromium.org/luci/auth/identity"
+	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/config/server/cfgmodule"
 	"go.chromium.org/luci/grpc/prpc"
 	luciserver "go.chromium.org/luci/server"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/auth/openid"
 	"go.chromium.org/luci/server/cron"
 	"go.chromium.org/luci/server/gaeemulation"
 	"go.chromium.org/luci/server/module"
+	"go.chromium.org/luci/server/router"
 	spanmodule "go.chromium.org/luci/server/span"
 	"go.chromium.org/luci/server/tq"
 
@@ -68,6 +76,25 @@ func RegisterCrons(srv *luciserver.Server) error {
 
 // RegisterPubSubHandlers registers pub/sub handlers.
 func RegisterPubSubHandlers(srv *luciserver.Server) error {
+	pubsubMW := router.NewMiddlewareChain(
+		auth.Authenticate(&openid.GoogleIDTokenAuthMethod{
+			AudienceCheck: openid.AudienceMatchesHost,
+		}),
+	)
+	pusherID := identity.Identity(fmt.Sprintf("user:gitiles-pubsub@%s.iam.gserviceaccount.com", srv.Options.CloudProject))
+
+	// PubSub subscription endpoints.
+	srv.Routes.POST("/push-handlers/gitiles/:gitiles_host", pubsubMW, func(ctx *router.Context) {
+		if got := auth.CurrentIdentity(ctx.Request.Context()); got != pusherID {
+			logging.Errorf(ctx.Request.Context(), "Expecting ID token of %q, got %q", pusherID, got)
+			ctx.Writer.WriteHeader(http.StatusForbidden)
+		} else {
+			// Ignore the pubsub request for now.
+			// TODO(b/356027716): implement commit ingestion.
+			ctx.Writer.WriteHeader(http.StatusOK)
+		}
+	})
+
 	return nil
 }
 
