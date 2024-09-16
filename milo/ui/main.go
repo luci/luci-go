73f81c6cc384b66462a44294b7755af3b6265c0b
Weiwei Lin <weiweilin@google.com>
2024-09-16 03:32:14 +0000

    luci-ui: add some experimental setup for push-on-green
    This will build a new ui service to serve the UI files
    It should not affect the actual UI because the new ui service is not
    used. All UI static assets are still served by the default service.
    
    R=meiring
    
    Bug: b/367097786
    Change-Id: Ie95614c76de6dfffadcbe54bb8b04315b0b4f48e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5859523
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>

---
+++ milo/ui/main.go
@@ -0,0 +1,33 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package main contains some experimental setup for push-on-green. See
+// b/367097786.
+package main
+
+import (
+	luciserver "go.chromium.org/luci/server"
+
+	"go.chromium.org/luci/milo/server"
+
+	_ "go.chromium.org/luci/server/encryptedcookies/session/datastore"
+)
+
+// main implements the entrypoint for the UI service.
+// The UI service doesn't need anything. It only host the static files.
+func main() {
+	server.Main(func(srv *luciserver.Server) error {
+		return nil
+	})
+}
