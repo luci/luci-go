9aa34a1861644451c09ebd57bc48f4206f5ad6fe
Weiwei Lin <weiweilin@google.com>
2024-09-27 02:15:02 +0000

    luci-ui: move gitiles CORS setup instruction to g3doc
    R=beining
    
    Bug: b/369918108
    Change-Id: I23a2b64c5b615a2565cdd60eb680e7a7ad3acc9b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5892373
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Auto-Submit: Weiwei Lin <weiweilin@google.com>
    Reviewed-by: Beining Chen <beining@google.com>

--- milo/ui/src/gitiles/hooks/prpc_client.ts
+++ milo/ui/src/gitiles/hooks/prpc_client.ts
@@ -31,21 +31,8 @@ export function useSourceIndexClient() {
  * LUCI UI is allowed to send cross-origin requests to them. Otherwise, the
  * requests are sent through `MiloInternal`, which adds 200~600ms overhead.
  *
- * Follow the steps below to allow LUCI UI to send cross-origin requests to more
- * gitiles hosts:
- * 1. File a security review ([example](http://b/351915276)).
- *    1. List all LUCI UI host regexes.
- *       * `"^https://ci[.]chromium[.]org$"`
- *       * `"^https://(([A-Za-z0-9-_]+[.])?staging[.])?luci[.]app$"`
- *       * `"^https://([A-Za-z0-9-_]+-dot-)?luci-milo(-dev)?[.]appspot[.]com$"`
- *    2. List all the additional gitiles hosts we want to send cross-origin
- *       queries to.
- * 2. Filed a bug under `... > Chromium > Infra > Git > Admin` (if the gitiles
- *    hosts are managed by Chrome Ops Source) and link to the security review.
- * 3. Once the security review is approved, a git admin will process the request
- *    and add LUCI UI to the CORS allowlist.
- * 4. Add the gitiles hosts to this array so requests to them no longer need to
- *    be sent through `MiloInternal`.
+ * Follow the instruction at go/luci-ui-allow-cross-origin-requests-to-gitiles
+ * to allow LUCI UI to send cross-origin requests to more gitiles hosts.
  */
 const CORS_ENABLED_GITILES_HOSTS = Object.freeze([
   'chromium.googlesource.com',
