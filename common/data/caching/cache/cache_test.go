3ca4436095a502d8ba40044d3707f01f8e752a7b
Robert Iannucci <iannucci@chromium.org>
2024-10-14 22:57:37 +0000

    [common/data/caching/cache] Convert test to ftt/truth.
    This CL is part of the GoConvey -> Truth/FTT migration (go/noconvey-psa).
    For more information see:
    1. go/noconvey-psa
    2. go/noconvey-migration
    3. go/noconvey-proposal
    
    This one had a really interesting (and incorrectly written) helper
    function which needed to be hand-tweaked.
    
    R=gregorynisbet@google.com
    
    Bug: 319730573
    Change-Id: I7355c36dc4ca25605ef0f1f22d5c183b85440896
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/5924625
    Reviewed-by: Gregory Nisbet <gregorynisbet@google.com>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>

--- common/data/caching/cache/cache_test.go
+++ common/data/caching/cache/cache_test.go
@@ -25,185 +25,188 @@ import (
 	"testing"
 
 	"go.chromium.org/luci/common/system/filesystem"
-
-	. "github.com/smartystreets/goconvey/convey"
+	"go.chromium.org/luci/common/testing/ftt"
+	"go.chromium.org/luci/common/testing/truth"
+	"go.chromium.org/luci/common/testing/truth/assert"
+	"go.chromium.org/luci/common/testing/truth/should"
 )
 
-func testCache(t *testing.T, c *Cache) HexDigests {
+func testCache(t testing.TB, c *Cache) HexDigests {
+	t.Helper()
+
 	var expected HexDigests
-	Convey(`Common tests performed on a cache of objects.`, func() {
-		// c's policies must have MaxItems == 2 and MaxSize == 1024.
-		td := t.TempDir()
-		ctx := context.Background()
+	// c's policies must have MaxItems == 2 and MaxSize == 1024.
+	td := t.TempDir()
+	ctx := context.Background()
 
-		h := crypto.SHA1
-		fakeDigest := HexDigest("0123456789012345678901234567890123456789")
-		badDigest := HexDigest("012345678901234567890123456789012345678")
-		emptyContent := []byte{}
-		emptyDigest := HashBytes(h, emptyContent)
-		file1Content := []byte("foo")
-		file1Digest := HashBytes(h, file1Content)
-		file2Content := []byte("foo bar")
-		file2Digest := HashBytes(h, file2Content)
-		hardlinkContent := []byte("hardlink")
-		hardlinkDigest := HashBytes(h, hardlinkContent)
-		largeContent := bytes.Repeat([]byte("A"), 1023)
-		largeDigest := HashBytes(h, largeContent)
-		tooLargeContent := bytes.Repeat([]byte("A"), 1025)
-		tooLargeDigest := HashBytes(h, tooLargeContent)
+	h := crypto.SHA1
+	fakeDigest := HexDigest("0123456789012345678901234567890123456789")
+	badDigest := HexDigest("012345678901234567890123456789012345678")
+	emptyContent := []byte{}
+	emptyDigest := HashBytes(h, emptyContent)
+	file1Content := []byte("foo")
+	file1Digest := HashBytes(h, file1Content)
+	file2Content := []byte("foo bar")
+	file2Digest := HashBytes(h, file2Content)
+	hardlinkContent := []byte("hardlink")
+	hardlinkDigest := HashBytes(h, hardlinkContent)
+	largeContent := bytes.Repeat([]byte("A"), 1023)
+	largeDigest := HashBytes(h, largeContent)
+	tooLargeContent := bytes.Repeat([]byte("A"), 1025)
+	tooLargeDigest := HashBytes(h, tooLargeContent)
 
-		So(c.Keys(), ShouldResemble, HexDigests{})
+	assert.Loosely(t, c.Keys(), should.Resemble(HexDigests{}), truth.LineContext())
 
-		So(c.Touch(fakeDigest), ShouldBeFalse)
-		So(c.Touch(badDigest), ShouldBeFalse)
+	assert.Loosely(t, c.Touch(fakeDigest), should.BeFalse, truth.LineContext())
+	assert.Loosely(t, c.Touch(badDigest), should.BeFalse, truth.LineContext())
 
-		c.Evict(fakeDigest)
-		c.Evict(badDigest)
+	c.Evict(fakeDigest)
+	c.Evict(badDigest)
 
-		r, err := c.Read(fakeDigest)
-		So(r, ShouldBeNil)
-		So(err, ShouldNotBeNil)
-		r, err = c.Read(badDigest)
-		So(r, ShouldBeNil)
-		So(err, ShouldNotBeNil)
+	r, err := c.Read(fakeDigest)
+	assert.Loosely(t, r, should.BeNil, truth.LineContext())
+	assert.Loosely(t, err, should.NotBeNil, truth.LineContext())
+	r, err = c.Read(badDigest)
+	assert.Loosely(t, r, should.BeNil, truth.LineContext())
+	assert.Loosely(t, err, should.NotBeNil, truth.LineContext())
 
-		// It's too large to fit in the cache.
-		So(c.Add(ctx, tooLargeDigest, bytes.NewBuffer(tooLargeContent)), ShouldNotBeNil)
+	// It's too large to fit in the cache.
+	assert.Loosely(t, c.Add(ctx, tooLargeDigest, bytes.NewBuffer(tooLargeContent)), should.NotBeNil, truth.LineContext())
 
-		// It gets discarded because it's too large.
-		So(c.Add(ctx, largeDigest, bytes.NewBuffer(largeContent)), ShouldBeNil)
-		So(c.Add(ctx, emptyDigest, bytes.NewBuffer(emptyContent)), ShouldBeNil)
-		So(c.Add(ctx, emptyDigest, bytes.NewBuffer(emptyContent)), ShouldBeNil)
-		So(c.Keys(), ShouldResemble, HexDigests{emptyDigest, largeDigest})
-		c.Evict(emptyDigest)
-		So(c.Keys(), ShouldResemble, HexDigests{largeDigest})
-		So(c.Add(ctx, emptyDigest, bytes.NewBuffer(emptyContent)), ShouldBeNil)
+	// It gets discarded because it's too large.
+	assert.Loosely(t, c.Add(ctx, largeDigest, bytes.NewBuffer(largeContent)), should.BeNil, truth.LineContext(), truth.LineContext())
+	assert.Loosely(t, c.Add(ctx, emptyDigest, bytes.NewBuffer(emptyContent)), should.BeNil, truth.LineContext())
+	assert.Loosely(t, c.Add(ctx, emptyDigest, bytes.NewBuffer(emptyContent)), should.BeNil, truth.LineContext())
+	assert.Loosely(t, c.Keys(), should.Resemble(HexDigests{emptyDigest, largeDigest}), truth.LineContext())
+	c.Evict(emptyDigest)
+	assert.Loosely(t, c.Keys(), should.Resemble(HexDigests{largeDigest}), truth.LineContext())
+	assert.Loosely(t, c.Add(ctx, emptyDigest, bytes.NewBuffer(emptyContent)), should.BeNil, truth.LineContext())
 
-		So(c.Add(ctx, file1Digest, bytes.NewBuffer(file1Content)), ShouldBeNil)
-		So(c.Touch(emptyDigest), ShouldBeTrue)
-		So(c.Add(ctx, file2Digest, bytes.NewBuffer(file2Content)), ShouldBeNil)
+	assert.Loosely(t, c.Add(ctx, file1Digest, bytes.NewBuffer(file1Content)), should.BeNil, truth.LineContext())
+	assert.Loosely(t, c.Touch(emptyDigest), should.BeTrue, truth.LineContext())
+	assert.Loosely(t, c.Add(ctx, file2Digest, bytes.NewBuffer(file2Content)), should.BeNil, truth.LineContext())
 
-		r, err = c.Read(file1Digest)
-		So(r, ShouldBeNil)
-		So(err, ShouldNotBeNil)
-		r, err = c.Read(file2Digest)
-		So(err, ShouldBeNil)
-		actual, err := io.ReadAll(r)
-		So(r.Close(), ShouldBeNil)
-		So(err, ShouldBeNil)
-		So(actual, ShouldResemble, file2Content)
+	r, err = c.Read(file1Digest)
+	assert.Loosely(t, r, should.BeNil, truth.LineContext())
+	assert.Loosely(t, err, should.NotBeNil, truth.LineContext())
+	r, err = c.Read(file2Digest)
+	assert.Loosely(t, err, should.BeNil, truth.LineContext())
+	actual, err := io.ReadAll(r)
+	assert.Loosely(t, r.Close(), should.BeNil, truth.LineContext())
+	assert.Loosely(t, err, should.BeNil, truth.LineContext())
+	assert.Loosely(t, actual, should.Resemble(file2Content), truth.LineContext())
 
-		expected = HexDigests{file2Digest, emptyDigest}
-		So(c.Keys(), ShouldResemble, expected)
+	expected = HexDigests{file2Digest, emptyDigest}
+	assert.Loosely(t, c.Keys(), should.Resemble(expected), truth.LineContext())
 
-		dest := filepath.Join(td, "foo")
-		So(c.Hardlink(fakeDigest, dest, os.FileMode(0600)), ShouldNotBeNil)
-		So(c.Hardlink(badDigest, dest, os.FileMode(0600)), ShouldNotBeNil)
-		So(c.Hardlink(file2Digest, dest, os.FileMode(0600)), ShouldBeNil)
-		// See comment about the fact that it may or may not work.
-		_ = c.Hardlink(file2Digest, dest, os.FileMode(0600))
-		actual, err = os.ReadFile(dest)
-		So(err, ShouldBeNil)
-		So(actual, ShouldResemble, file2Content)
+	dest := filepath.Join(td, "foo")
+	assert.Loosely(t, c.Hardlink(fakeDigest, dest, os.FileMode(0600)), should.NotBeNil, truth.LineContext())
+	assert.Loosely(t, c.Hardlink(badDigest, dest, os.FileMode(0600)), should.NotBeNil, truth.LineContext())
+	assert.Loosely(t, c.Hardlink(file2Digest, dest, os.FileMode(0600)), should.BeNil, truth.LineContext())
+	// See comment about the fact that it may or may not work.
+	_ = c.Hardlink(file2Digest, dest, os.FileMode(0600))
+	actual, err = os.ReadFile(dest)
+	assert.Loosely(t, err, should.BeNil, truth.LineContext())
+	assert.Loosely(t, actual, should.Resemble(file2Content), truth.LineContext())
 
-		dest = filepath.Join(td, "hardlink")
-		So(c.AddWithHardlink(ctx, hardlinkDigest, bytes.NewBuffer(hardlinkContent), dest, os.ModePerm),
-			ShouldBeNil)
-		actual, err = os.ReadFile(dest)
-		So(err, ShouldBeNil)
-		So(actual, ShouldResemble, hardlinkContent)
+	dest = filepath.Join(td, "hardlink")
+	assert.Loosely(t, c.AddWithHardlink(ctx, hardlinkDigest, bytes.NewBuffer(hardlinkContent), dest, os.ModePerm),
+		should.BeNil, truth.LineContext())
+	actual, err = os.ReadFile(dest)
+	assert.Loosely(t, err, should.BeNil, truth.LineContext())
+	assert.Loosely(t, actual, should.Resemble(hardlinkContent), truth.LineContext())
 
-		// |emptyDigest| is evicted.
-		expected = HexDigests{hardlinkDigest, file2Digest}
+	// |emptyDigest| is evicted.
+	expected = HexDigests{hardlinkDigest, file2Digest}
 
-		So(c.Close(), ShouldBeNil)
-	})
+	assert.Loosely(t, c.Close(), should.BeNil, truth.LineContext())
+
 	return expected
 }
 
 func TestNew(t *testing.T) {
-	Convey(`Test the disk-based cache of objects.`, t, func() {
+	ftt.Run(`Test the disk-based cache of objects.`, t, func(t *ftt.Test) {
 		td := t.TempDir()
 
 		pol := Policies{MaxSize: 1024, MaxItems: 2}
 		h := crypto.SHA1
 		c, err := New(pol, td, h)
-		So(err, ShouldBeNil)
+		assert.Loosely(t, err, should.BeNil)
 		expected := testCache(t, c)
 
 		c, err = New(pol, td, h)
-		So(err, ShouldBeNil)
-		So(c.Keys(), ShouldResemble, expected)
-		So(c.Close(), ShouldBeNil)
+		assert.Loosely(t, err, should.BeNil)
+		assert.Loosely(t, c.Keys(), should.Resemble(expected))
+		assert.Loosely(t, c.Close(), should.BeNil)
 
 		curdir, err := os.Getwd()
-		So(err, ShouldBeNil)
+		assert.Loosely(t, err, should.BeNil)
 		defer func() {
-			So(os.Chdir(curdir), ShouldBeNil)
+			assert.Loosely(t, os.Chdir(curdir), should.BeNil)
 		}()
 
-		So(os.Chdir(td), ShouldBeNil)
+		assert.Loosely(t, os.Chdir(td), should.BeNil)
 
 		rel, err := filepath.Rel(td, t.TempDir())
-		So(err, ShouldBeNil)
-		So(filepath.IsAbs(rel), ShouldBeFalse)
+		assert.Loosely(t, err, should.BeNil)
+		assert.Loosely(t, filepath.IsAbs(rel), should.BeFalse)
 		_, err = New(pol, rel, h)
-		So(err, ShouldBeNil)
+		assert.Loosely(t, err, should.BeNil)
 	})
 
-	Convey(`invalid state.json`, t, func() {
+	ftt.Run(`invalid state.json`, t, func(t *ftt.Test) {
 		dir := t.TempDir()
 		state := filepath.Join(dir, "state.json")
 		invalid := filepath.Join(dir, "invalid file")
-		So(os.WriteFile(state, []byte("invalid"), os.ModePerm), ShouldBeNil)
-		So(os.WriteFile(invalid, []byte("invalid"), os.ModePerm), ShouldBeNil)
+		assert.Loosely(t, os.WriteFile(state, []byte("invalid"), os.ModePerm), should.BeNil)
+		assert.Loosely(t, os.WriteFile(invalid, []byte("invalid"), os.ModePerm), should.BeNil)
 
 		c, err := New(Policies{}, dir, crypto.SHA1)
-		So(err, ShouldNotBeNil)
+		assert.Loosely(t, err, should.NotBeNil)
 		if c == nil {
 			t.Errorf("c should not be nil: %v", err)
 		}
-		So(c, ShouldNotBeNil)
+		assert.Loosely(t, c, should.NotBeNil)
 
-		So(c.statePath(), ShouldEqual, state)
+		assert.Loosely(t, c.statePath(), should.Equal(state))
 
 		// invalid files should be removed.
 		empty, err := filesystem.IsEmptyDir(dir)
-		So(err, ShouldBeNil)
-		So(empty, ShouldBeTrue)
+		assert.Loosely(t, err, should.BeNil)
+		assert.Loosely(t, empty, should.BeTrue)
 
-		So(c.Close(), ShouldBeNil)
+		assert.Loosely(t, c.Close(), should.BeNil)
 	})
 
-	Convey(`MinFreeSpace too big`, t, func() {
+	ftt.Run(`MinFreeSpace too big`, t, func(t *ftt.Test) {
 		ctx := context.Background()
 		dir := t.TempDir()
 		h := crypto.SHA1
 		c, err := New(Policies{MaxSize: 10, MinFreeSpace: math.MaxInt64}, dir, h)
-		So(err, ShouldBeNil)
+		assert.Loosely(t, err, should.BeNil)
 
 		file1Content := []byte("foo")
 		file1Digest := HashBytes(h, file1Content)
-		So(c.Add(ctx, file1Digest, bytes.NewBuffer(file1Content)), ShouldBeNil)
+		assert.Loosely(t, c.Add(ctx, file1Digest, bytes.NewBuffer(file1Content)), should.BeNil)
 
-		So(c.Close(), ShouldBeNil)
+		assert.Loosely(t, c.Close(), should.BeNil)
 	})
 
-	Convey(`MaxSize 0`, t, func() {
+	ftt.Run(`MaxSize 0`, t, func(t *ftt.Test) {
 		ctx := context.Background()
 		dir := t.TempDir()
 		h := crypto.SHA1
 		c, err := New(Policies{MaxSize: 0, MaxItems: 1}, dir, h)
-		So(err, ShouldBeNil)
+		assert.Loosely(t, err, should.BeNil)
 
 		file1Content := []byte("foo")
 		file1Digest := HashBytes(h, file1Content)
-		So(c.Add(ctx, file1Digest, bytes.NewBuffer(file1Content)), ShouldBeNil)
-		So(c.Keys(), ShouldHaveLength, 1)
-		So(c.Close(), ShouldBeNil)
+		assert.Loosely(t, c.Add(ctx, file1Digest, bytes.NewBuffer(file1Content)), should.BeNil)
+		assert.Loosely(t, c.Keys(), should.HaveLength(1))
+		assert.Loosely(t, c.Close(), should.BeNil)
 	})
 
-	Convey(`HardLink will update used`, t, func() {
+	ftt.Run(`HardLink will update used`, t, func(t *ftt.Test) {
 		dir := t.TempDir()
 		h := crypto.SHA1
 		onDiskContent := []byte("on disk")
@@ -212,21 +215,21 @@ func TestNew(t *testing.T) {
 		notOnDiskDigest := HashBytes(h, notOnDiskContent)
 
 		c, err := New(Policies{}, dir, h)
-		defer func() { So(c.Close(), ShouldBeNil) }()
+		defer func() { assert.Loosely(t, c.Close(), should.BeNil) }()
 
-		So(err, ShouldBeNil)
-		So(c, ShouldNotBeNil)
+		assert.Loosely(t, err, should.BeNil)
+		assert.Loosely(t, c, should.NotBeNil)
 		perm := os.ModePerm
-		So(os.WriteFile(c.itemPath(onDiskDigest), onDiskContent, perm), ShouldBeNil)
+		assert.Loosely(t, os.WriteFile(c.itemPath(onDiskDigest), onDiskContent, perm), should.BeNil)
 
-		So(c.Used(), ShouldBeEmpty)
-		So(c.Hardlink(notOnDiskDigest, filepath.Join(dir, "not_on_disk"), perm), ShouldNotBeNil)
-		So(c.Used(), ShouldBeEmpty)
-		So(c.Hardlink(onDiskDigest, filepath.Join(dir, "on_disk"), perm), ShouldBeNil)
-		So(c.Used(), ShouldHaveLength, 1)
+		assert.Loosely(t, c.Used(), should.BeEmpty)
+		assert.Loosely(t, c.Hardlink(notOnDiskDigest, filepath.Join(dir, "not_on_disk"), perm), should.NotBeNil)
+		assert.Loosely(t, c.Used(), should.BeEmpty)
+		assert.Loosely(t, c.Hardlink(onDiskDigest, filepath.Join(dir, "on_disk"), perm), should.BeNil)
+		assert.Loosely(t, c.Used(), should.HaveLength(1))
 	})
 
-	Convey(`AddFileWithoutValidation`, t, func() {
+	ftt.Run(`AddFileWithoutValidation`, t, func(t *ftt.Test) {
 		ctx := context.Background()
 		dir := t.TempDir()
 		cache := filepath.Join(dir, "cache")
@@ -235,23 +238,23 @@ func TestNew(t *testing.T) {
 			MaxSize:  1,
 			MaxItems: 1,
 		}, cache, h)
-		defer func() { So(c.Close(), ShouldBeNil) }()
-		So(err, ShouldBeNil)
+		defer func() { assert.Loosely(t, c.Close(), should.BeNil) }()
+		assert.Loosely(t, err, should.BeNil)
 
 		empty := filepath.Join(dir, "empty")
-		So(os.WriteFile(empty, nil, 0600), ShouldBeNil)
+		assert.Loosely(t, os.WriteFile(empty, nil, 0600), should.BeNil)
 
 		emptyHash := HashBytes(h, nil)
 
-		So(c.AddFileWithoutValidation(ctx, emptyHash, empty), ShouldBeNil)
+		assert.Loosely(t, c.AddFileWithoutValidation(ctx, emptyHash, empty), should.BeNil)
 
-		So(c.Touch(emptyHash), ShouldBeTrue)
+		assert.Loosely(t, c.Touch(emptyHash), should.BeTrue)
 
 		// Adding already existing file is fine.
-		So(c.AddFileWithoutValidation(ctx, emptyHash, empty), ShouldBeNil)
+		assert.Loosely(t, c.AddFileWithoutValidation(ctx, emptyHash, empty), should.BeNil)
 
 		empty2 := filepath.Join(dir, "empty2")
-		So(os.WriteFile(empty2, nil, 0600), ShouldBeNil)
-		So(c.AddFileWithoutValidation(ctx, emptyHash, empty2), ShouldBeNil)
+		assert.Loosely(t, os.WriteFile(empty2, nil, 0600), should.BeNil)
+		assert.Loosely(t, c.AddFileWithoutValidation(ctx, emptyHash, empty2), should.BeNil)
 	})
 }
