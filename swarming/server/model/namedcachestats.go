840ee66a516399b42aeee8bb656fec76adaeea8b
JB Tamas-Leloup <jtamasleloup@google.com>
2024-11-20 16:03:02 +0000

    [swarming] Add NamedCacheStats entity to model.
    This is needed for updated_named_caches cron.
    
    A different entity kind is used compared to the Python implementation (NamedCacheStats vs NamedCache) to avoid conflicts.
    
    Also, the new model slightly differs from the Python model. Where the Python model has a new entity for each pool:cache:os combination, the new model has a new entity for each pool:cache combination with each entity having its own list of (OS->Size). This reduces the overall number of entities stored in Datastore.
    
    BUG:b/355013356
    Change-Id: I4d6f3c639118d6674c2461f326fe8982ed1659ec
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/6011277
    Reviewed-by: Chan Li <chanli@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: JB Tamas-Leloup <jtamasleloup@google.com>

---
+++ swarming/server/model/namedcachestats.go
@@ -0,0 +1,54 @@
+// Copyright 2024 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package model
+
+import (
+	"context"
+	"time"
+
+	"go.chromium.org/luci/gae/service/datastore"
+)
+
+// NamedCacheStats contains cache size hints for some (pool, cache) pair.
+// Datastore automatically deletes expired NamedCacheStats entities as per the TTL policy.
+type NamedCacheStats struct {
+	// Key identifies the pool and the cache, see NamedCacheStatsKey(...).
+	Key *datastore.Key `gae:"$key"`
+	// OS is per-OS entries with cache size hint for that OS.
+	OS []PerOSEntry `gae:"os,noindex"`
+	// ExpireAt is when this entity can be deleted.
+	ExpireAt time.Time `gae:"expiry,noindex"`
+	// LastUpdate is when this entity was updated the last time.
+	LastUpdate time.Time `gae:"updated,noindex"`
+	// Extra are entity properties that didn't match any declared ones above.
+	Extra datastore.PropertyMap `gae:"-,extra"`
+}
+
+// PerOSEntry contains the cache size hint for some concrete OS family.
+type PerOSEntry struct {
+	// Name is the OS family name, e.g. "Windows". See DetermineOSFamily(...).
+	Name string `gae:"name,noindex"`
+	// Size is the current estimate of the cache size for this OS in bytes.
+	Size int64 `gae:"size,noindex"`
+	// LastUpdate is when this entry was updated the last time.
+	LastUpdate time.Time `gae:"updated,noindex"`
+	// ExpireAt is when this entry can be deleted.
+	ExpireAt time.Time `gae:"expiry,noindex"`
+}
+
+// NamedCacheStatsKey returns a NamedCacheStats key given a cache and a pool.
+func NamedCacheStatsKey(ctx context.Context, pool, cache string) *datastore.Key {
+	return datastore.NewKey(ctx, "NamedCacheStats", pool+":"+cache, 0, nil)
+}
