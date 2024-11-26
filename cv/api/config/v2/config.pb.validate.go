0b2320d8a1d2413a39b177c4ea81e4b9dd535d41
Yiwei Zhang <yiwzhang@google.com>
2024-11-26 21:59:26 +0000

    cv: define skip_footers in LUCI CV builder config
    This will be used to replace Not Useful functionality in Tricium.
    
    Bug: 380944450
    Change-Id: Ibf228c01607b50bf3979fb6ef11f21f87c54037d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/6051076
    Reviewed-by: Scott Lee <ddoman@chromium.org>
    Commit-Queue: Yiwei Zhang <yiwzhang@google.com>

--- cv/api/config/v2/config.pb.validate.go
+++ cv/api/config/v2/config.pb.validate.go
@@ -3270,6 +3270,40 @@ func (m *Verifiers_Tryjob_Builder) validate(all bool) error {
 
 	}
 
+	for idx, item := range m.GetSkipFooters() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, Verifiers_Tryjob_BuilderValidationError{
+						field:  fmt.Sprintf("SkipFooters[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, Verifiers_Tryjob_BuilderValidationError{
+						field:  fmt.Sprintf("SkipFooters[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return Verifiers_Tryjob_BuilderValidationError{
+					field:  fmt.Sprintf("SkipFooters[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return Verifiers_Tryjob_BuilderMultiError(errors)
 	}
@@ -3796,6 +3830,115 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = Verifiers_Tryjob_Builder_LocationFilterValidationError{}
+
+// Validate checks the field values on Verifiers_Tryjob_Builder_SkipFooter with
+// the rules defined in the proto definition for this message. If any rules
+// are violated, the first error encountered is returned, or nil if there are
+// no violations.
+func (m *Verifiers_Tryjob_Builder_SkipFooter) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on Verifiers_Tryjob_Builder_SkipFooter
+// with the rules defined in the proto definition for this message. If any
+// rules are violated, the result is a list of violation errors wrapped in
+// Verifiers_Tryjob_Builder_SkipFooterMultiError, or nil if none found.
+func (m *Verifiers_Tryjob_Builder_SkipFooter) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *Verifiers_Tryjob_Builder_SkipFooter) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Key
+
+	// no validation rules for ValueRegexp
+
+	if len(errors) > 0 {
+		return Verifiers_Tryjob_Builder_SkipFooterMultiError(errors)
+	}
+
+	return nil
+}
+
+// Verifiers_Tryjob_Builder_SkipFooterMultiError is an error wrapping multiple
+// validation errors returned by
+// Verifiers_Tryjob_Builder_SkipFooter.ValidateAll() if the designated
+// constraints aren't met.
+type Verifiers_Tryjob_Builder_SkipFooterMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m Verifiers_Tryjob_Builder_SkipFooterMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m Verifiers_Tryjob_Builder_SkipFooterMultiError) AllErrors() []error { return m }
+
+// Verifiers_Tryjob_Builder_SkipFooterValidationError is the validation error
+// returned by Verifiers_Tryjob_Builder_SkipFooter.Validate if the designated
+// constraints aren't met.
+type Verifiers_Tryjob_Builder_SkipFooterValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e Verifiers_Tryjob_Builder_SkipFooterValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e Verifiers_Tryjob_Builder_SkipFooterValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e Verifiers_Tryjob_Builder_SkipFooterValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e Verifiers_Tryjob_Builder_SkipFooterValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e Verifiers_Tryjob_Builder_SkipFooterValidationError) ErrorName() string {
+	return "Verifiers_Tryjob_Builder_SkipFooterValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e Verifiers_Tryjob_Builder_SkipFooterValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sVerifiers_Tryjob_Builder_SkipFooter.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = Verifiers_Tryjob_Builder_SkipFooterValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = Verifiers_Tryjob_Builder_SkipFooterValidationError{}
 
 // Validate checks the field values on UserLimit_Limit with the rules defined
 // in the proto definition for this message. If any rules are violated, the
