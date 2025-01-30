a7a96f95fa6c19627bd014dbf9d9354601c3af61
Heidi Chan <heidichan@google.com>
2025-01-29 17:53:55 -0800

    AnalyzePrincipalPermissions function
    Moved relevant functions from model into new permissions file.
    AnalyzePrincipalPermissions iterates through realms object to return a mapping of principal to an array of realms permissions.
    Tests added
    
    Bug: b/350848587
    Change-Id: If55c237817e636247b75ffc0561bb45c9a22a7e7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/6191852
    Reviewed-by: Anne Redulla <aredulla@google.com>
    Commit-Queue: Heidi Chan <heidichan@google.com>

---
+++ auth_service/impl/model/permissions.go
@@ -0,0 +1,102 @@
+// Copyright 2025 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package model contains model definitions for Auth Service.
+//
+// This file contains functionality related to getting permissions
+// from the latest Realms object.
+package model
+
+import (
+	"context"
+
+	"google.golang.org/protobuf/proto"
+
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/server/auth/service/protocol"
+
+	"go.chromium.org/luci/auth_service/api/rpcpb"
+	"go.chromium.org/luci/auth_service/impl/util/zlib"
+)
+
+// processSnapshot is a helper function to get the
+// ReplicationPushRequest from the given AuthDBSnapshot.
+func processSnapshot(authDBSnapshot *AuthDBSnapshot) (*protocol.ReplicationPushRequest, error) {
+	authDBBlob, err := zlib.Decompress(authDBSnapshot.AuthDBDeflated)
+	if err != nil {
+		return nil, errors.Annotate(err, "error decompressing AuthDBDeflated").Err()
+	}
+
+	req := &protocol.ReplicationPushRequest{}
+	if err := proto.Unmarshal(authDBBlob, req); err != nil {
+		return nil, errors.Annotate(err, "error unmarshalling AuthDB blob").Err()
+	}
+
+	return req, nil
+}
+
+func getRealms(ctx context.Context) (*protocol.Realms, error) {
+	snapshotLatest, err := GetAuthDBSnapshotLatest(ctx)
+	if err != nil {
+		return nil, err
+	}
+	snapshot, err := GetAuthDBSnapshot(ctx, snapshotLatest.AuthDBRev, false)
+	if err != nil {
+		return nil, err
+	}
+	// Decompress and unmarshal snapshot.
+	req, err := processSnapshot(snapshot)
+	if err != nil {
+		return nil, err
+	}
+
+	if req.AuthDb == nil {
+		return nil, errors.New("Snapshot had no AuthDB")
+	}
+
+	return req.AuthDb.Realms, nil
+}
+
+// AnalyzePrincipalPermissions analyzes the realms for the permissions granted to each principal in a given realm.
+// It returns a mapping of principal -> collection of realm permissions, the permissions that principal has in that specific realm.
+func AnalyzePrincipalPermissions(realms *protocol.Realms) (map[string][]*rpcpb.RealmPermissions, error) {
+	result := make(map[string][]*rpcpb.RealmPermissions)
+	for _, realm := range realms.Realms {
+		principalToPermissions := make(map[string]stringset.Set)
+		// Maintain map of principal to permissions set.
+		for _, binding := range realm.Bindings {
+			permissionNames := stringset.New(len(binding.Permissions))
+			for _, permIndex := range binding.Permissions {
+				permissionNames.Add(realms.Permissions[permIndex].Name)
+			}
+			// For each principal look up its current permission set & extend it by these permission names.
+			permissions := permissionNames.ToSlice()
+			for _, principal := range binding.Principals {
+				if _, ok := principalToPermissions[principal]; !ok {
+					principalToPermissions[principal] = stringset.New(0)
+				}
+				principalToPermissions[principal].AddAll(permissions)
+			}
+		}
+		for principal, permissions := range principalToPermissions {
+			realmPermissions := &rpcpb.RealmPermissions{
+				Name:        realm.Name,
+				Permissions: permissions.ToSortedSlice(),
+			}
+			result[principal] = append(result[principal], realmPermissions)
+		}
+	}
+	return result, nil
+}
---
+++ auth_service/impl/model/permissions_test.go
@@ -0,0 +1,177 @@
+// Copyright 2025 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package model
+
+import (
+	"context"
+	"testing"
+
+	"google.golang.org/protobuf/proto"
+
+	"go.chromium.org/luci/common/testing/ftt"
+	"go.chromium.org/luci/common/testing/truth/assert"
+	"go.chromium.org/luci/common/testing/truth/should"
+	"go.chromium.org/luci/gae/impl/memory"
+	"go.chromium.org/luci/gae/service/datastore"
+	"go.chromium.org/luci/server/auth/service/protocol"
+
+	"go.chromium.org/luci/auth_service/api/rpcpb"
+)
+
+func TestGetRealms(t *testing.T) {
+	t.Parallel()
+	ftt.Run("Returns realms object", t, func(t *ftt.Test) {
+		ctx := memory.Use(context.Background())
+		const testAuthDBRev = 12345
+
+		testRealms := &protocol.Realms{
+			Permissions: []*protocol.Permission{
+				{Name: "luci.dev.p1"},
+				{Name: "luci.dev.p2"},
+				{Name: "luci.dev.p3"},
+			},
+			Realms: []*protocol.Realm{
+				{
+					Name: "p:@root",
+				},
+				{
+					Name: "p:r",
+					Bindings: []*protocol.Binding{
+						{
+							Permissions: []uint32{0, 1},
+							Principals:  []string{"group:gr1"},
+						},
+						{
+							Permissions: []uint32{0, 1, 2},
+							Principals:  []string{"group:gr3", "group:gr4"},
+						},
+						{
+							Permissions: []uint32{1, 2},
+							Principals:  []string{"group:gr2"},
+						},
+					},
+				},
+			},
+		}
+		testRequest := &protocol.ReplicationPushRequest{
+			Revision: &protocol.AuthDBRevision{
+				AuthDbRev: testAuthDBRev,
+			},
+			AuthDb: &protocol.AuthDB{
+				Realms: testRealms,
+			},
+		}
+		blob, err := proto.Marshal(testRequest)
+		assert.Loosely(t, err, should.BeNil)
+
+		testReplicationState := &AuthReplicationState{
+			AuthDBRev: testAuthDBRev,
+		}
+		err = StoreAuthDBSnapshot(ctx, testReplicationState, blob)
+		assert.Loosely(t, err, should.BeNil)
+
+		authDBSnapshotLatest := &AuthDBSnapshotLatest{
+			Kind:         "AuthDBSnapshotLatest",
+			ID:           "latest",
+			AuthDBRev:    testAuthDBRev,
+			AuthDBSha256: "test-sha-256",
+			ModifiedTS:   testModifiedTS,
+		}
+		err = datastore.Put(ctx, authDBSnapshotLatest)
+		assert.Loosely(t, err, should.BeNil)
+
+		actual, err := getRealms(ctx)
+		assert.Loosely(t, err, should.BeNil)
+		assert.Loosely(t, actual, should.Match(testRealms))
+	})
+}
+
+func TestAnalyzePrincipalPermissions(t *testing.T) {
+	t.Parallel()
+	ftt.Run("Returns correct permissions on valid group", t, func(t *ftt.Test) {
+		testRealms := &protocol.Realms{
+			Permissions: []*protocol.Permission{
+				{Name: "luci.dev.p1"},
+				{Name: "luci.dev.p2"},
+				{Name: "luci.dev.p3"},
+				{Name: "luci.dev.p4"},
+			},
+			Realms: []*protocol.Realm{
+				{
+					Name: "p:@root",
+				},
+				{
+					Name: "p:r",
+					Bindings: []*protocol.Binding{
+						{
+							Permissions: []uint32{0, 1},
+							Principals:  []string{"group:gr1"},
+						},
+						{
+							Permissions: []uint32{0, 1, 2},
+							Principals:  []string{"group:gr3", "group:gr4"},
+						},
+						{
+							Permissions: []uint32{1, 2},
+							Principals:  []string{"group:gr1", "group:gr2"},
+						},
+					},
+				},
+				{
+					Name: "p:r2",
+					Bindings: []*protocol.Binding{
+						{
+							Permissions: []uint32{2},
+							Principals:  []string{"group:gr1"},
+						},
+					},
+				},
+			},
+		}
+		expectedResult := map[string][]*rpcpb.RealmPermissions{
+			"group:gr1": []*rpcpb.RealmPermissions{
+				{
+					Name:        "p:r",
+					Permissions: []string{"luci.dev.p1", "luci.dev.p2", "luci.dev.p3"},
+				},
+				{
+					Name:        "p:r2",
+					Permissions: []string{"luci.dev.p3"},
+				},
+			},
+			"group:gr2": []*rpcpb.RealmPermissions{
+				{
+					Name:        "p:r",
+					Permissions: []string{"luci.dev.p2", "luci.dev.p3"},
+				},
+			},
+			"group:gr3": []*rpcpb.RealmPermissions{
+				{
+					Name:        "p:r",
+					Permissions: []string{"luci.dev.p1", "luci.dev.p2", "luci.dev.p3"},
+				},
+			},
+			"group:gr4": []*rpcpb.RealmPermissions{
+				{
+					Name:        "p:r",
+					Permissions: []string{"luci.dev.p1", "luci.dev.p2", "luci.dev.p3"},
+				},
+			},
+		}
+		actualPermissions, err := AnalyzePrincipalPermissions(testRealms)
+		assert.Loosely(t, actualPermissions, should.Match(expectedResult))
+		assert.Loosely(t, err, should.BeNil)
+	})
+}
