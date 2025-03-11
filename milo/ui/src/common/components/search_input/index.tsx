2cda7f90b540042905118932037e6d7455352827
Greg NISBET <gregorynisbet@google.com>
2025-03-11 14:22:35 -0700

    Fix up an export *, eliminate an eslint warning in index.tsx
    One down, 54 to go.
    
    BUG=None
    TEST=make format && make build
    
    Change-Id: I49712e0a94f54c1198f15201d2a779051236b548
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/6344912
    Reviewed-by: Chan Li <chanli@chromium.org>
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>

--- milo/ui/src/common/components/search_input/index.tsx
+++ milo/ui/src/common/components/search_input/index.tsx
@@ -12,4 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export * from './search_input';
+export type { SearchInputProps } from './search_input';
+export { SearchInput } from './search_input';
