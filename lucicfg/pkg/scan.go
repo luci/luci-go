543a9d624a8fac17af3ed9f0e9e11491ffa3d568
Vadim Shtayura <vadimsh@chromium.org>
2025-03-12 13:20:37 -0700

    [lucicfg] Add a function to "discover" packages on disk.
    Will be used by "fmt" and "lint" implementations.
    
    Uses presence of PACKAGE.star or .lucicfgfmtrc or repo root
    (in that order) to find a package root directory. This should
    be good enough for all existing code.
    
    R=iannucci@chromium.org
    
    Change-Id: I42abb74342ec0978bc5d91ffd8711480a507bccf
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/6344048
    Reviewed-by: Robbie Iannucci <iannucci@google.com>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

---
+++ lucicfg/pkg/scan.go
@@ -0,0 +1,203 @@
+// Copyright 2025 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pkg
+
+import (
+	"fmt"
+	"io/fs"
+	"iter"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+)
+
+const legacyConfig = ".lucicfgfmtrc"
+
+// ScanResult is one of the discovered root directories.
+type ScanResult struct {
+	// Root is the absolute path to the discovered root.
+	Root string
+
+	// IsPackage is true if this root contains PACKAGE.star and thus contains
+	// a real lucicfg package.
+	//
+	// It is false for other roots that contain "legacy" packages. This exists for
+	// compatibility with the code that isn't using PACKAGE.star yet.
+	IsPackage bool
+
+	// Files are absolute paths to *.star files under this root.
+	//
+	// This is a subset of files passed to ScanForRoots (i.e. NOT all files in
+	// the package). Guaranteed to have at least one entry.
+	Files []string
+}
+
+// RelFiles returns slash-separated paths relative to the root.
+//
+// This is the same set as s.Files, just in a more normalized form suitable
+// for passing to a interpreter.Loader.
+func (s *ScanResult) RelFiles() []string {
+	out := make([]string, len(s.Files))
+	for i, p := range s.Files {
+		rel, err := filepath.Rel(s.Root, p)
+		if err != nil {
+			panic(fmt.Sprintf("filepath.Rel(%q, %q): %s", s.Root, p, err))
+		}
+		out[i] = filepath.ToSlash(rel)
+	}
+	return out
+}
+
+// ScanForRoots takes a list of paths and groups Starlark files in them by
+// the package they belong to.
+//
+// Paths can either point to concrete Starlark files or to directories (which
+// will be recursively traversed to find all Starlark files in them).
+//
+// If `paths` is empty, will scan the current directory.
+//
+// Returns two kinds of entries:
+//   - Entries representing discovered packages with PACKAGE.star file.
+//   - Entries representing "guessed" packages, for compatibility with code
+//     without PACKAGE.star. Uses presence of ".lucicfgfmtrc" to "guess" the
+//     root, falling back to a repository boundary otherwise.
+//
+// Does not attempt to interpret PACKAGE.star (use PackageOnDisk for that).
+func ScanForRoots(paths []string) ([]*ScanResult, error) {
+	results := []*ScanResult{}              // all discovered roots
+	perDirRoots := map[string]*ScanResult{} // abs dir path => closest root
+	allRoots := map[string]*ScanResult{}    // abs root path => *ScanResult for it
+	seen := stringset.New(len(paths))       // all visited leaf files
+
+	findPkgRoot := func(dir string) (*ScanResult, error) {
+		if found := perDirRoots[dir]; found != nil {
+			return found, nil
+		}
+
+		root, marker, err := findAnyRoot(dir)
+		if err != nil {
+			return nil, err
+		}
+		if res := allRoots[root]; res != nil {
+			// Already seen this root. Cache `findAnyRoot` resolution.
+			perDirRoots[dir] = res
+			return res, nil
+		}
+
+		// Found a new root!
+		res := &ScanResult{
+			Root:      root,
+			IsPackage: marker == PackageScript,
+		}
+		results = append(results, res)
+		allRoots[root] = res
+		perDirRoots[dir] = res
+		return res, nil
+	}
+
+	for path, err := range expandDirs(paths) {
+		if err != nil {
+			return nil, err
+		}
+		if !seen.Add(path) {
+			continue
+		}
+		pkgRes, err := findPkgRoot(filepath.Dir(path))
+		if err != nil {
+			return nil, err
+		}
+		pkgRes.Files = append(pkgRes.Files, path)
+	}
+
+	slices.SortFunc(results, func(a, b *ScanResult) int {
+		return strings.Compare(a.Root, b.Root)
+	})
+	for _, r := range results {
+		slices.Sort(r.Files)
+	}
+
+	return results, nil
+}
+
+// findAnyRoot finds either a root with PACKAGE.star or a legacy config or
+// a repo root.
+//
+// `marker` is either "PACKAGE.star" or ".lucicfgfmtrc" or "" depending on what
+// kind of root was found.
+func findAnyRoot(dir string) (root, marker string, err error) {
+	switch root, found, err := findRoot(dir, PackageScript); {
+	case err != nil:
+		return "", "", err
+	case found:
+		return root, PackageScript, nil
+	}
+	switch root, found, err := findRoot(dir, legacyConfig); {
+	case err != nil:
+		return "", "", err
+	case found:
+		return root, legacyConfig, nil
+	default:
+		return root, "", nil // repo root
+	}
+}
+
+// expandDirs iterates over recursive traversal of given paths.
+//
+// Paths can either be files (will be yielded as is) or directories (will be
+// recursively traversed in search for *.star files).
+func expandDirs(paths []string) iter.Seq2[string, error] {
+	if len(paths) == 0 {
+		paths = []string{"."}
+	}
+
+	return func(yield func(string, error) bool) {
+		for _, p := range paths {
+			p, err := filepath.Abs(p)
+			if err != nil {
+				yield(p, errors.Annotate(err, "could not absolutize %q", p).Err())
+				return
+			}
+
+			switch info, err := os.Stat(p); {
+			case err != nil:
+				yield(p, err)
+				return
+
+			case !info.IsDir():
+				if !yield(p, nil) {
+					return
+				}
+
+			default:
+				err := filepath.WalkDir(p, func(path string, entry fs.DirEntry, err error) error {
+					if err == nil && !entry.IsDir() && strings.HasSuffix(path, ".star") {
+						if !yield(path, nil) {
+							return fs.SkipAll
+						}
+					}
+					return err
+				})
+				if err != nil {
+					yield(p, err)
+					return
+				}
+			}
+		}
+	}
+}
---
+++ lucicfg/pkg/scan_test.go
@@ -0,0 +1,168 @@
+// Copyright 2025 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pkg
+
+import (
+	"path/filepath"
+	"testing"
+
+	"go.chromium.org/luci/common/testing/truth/assert"
+	"go.chromium.org/luci/common/testing/truth/should"
+)
+
+func TestScanForRoots(t *testing.T) {
+	t.Parallel()
+
+	tmp := prepDisk(t, map[string]string{
+		"repo a/.git/config":                "",
+		"repo a/file.star":                  "",
+		"repo a/deeper/file.star":           "",
+		"repo a/inner/.git/config":          "",
+		"repo a/inner/file.star":            "",
+		"repo b/pkg 1/PACKAGE.star":         "",
+		"repo b/pkg 1/deeper/.lucicfgfmtrc": "",
+		"repo b/pkg 1/deeper/file.star":     "",
+		"repo b/pkg 1/inner/PACKAGE.star":   "",
+		"repo b/pkg 2/.lucicfgfmtrc":        "",
+		"repo b/pkg 2/file.star":            "",
+		"repo b/pkg 2/deeper/.lucicfgfmtrc": "",
+		"repo b/pkg 2/deeper/file.star":     "",
+		"repo c/.git/config":                "",
+		"repo c/ignored/.lucicfgfmtrc":      "",
+		"repo c/ignored/file":               "",
+	})
+
+	abs := func(p string) string {
+		return filepath.Join(tmp, filepath.FromSlash(p))
+	}
+
+	rebasePath := func(p string) string {
+		var err error
+		p, err = filepath.Rel(tmp, p)
+		if err != nil {
+			panic(err)
+		}
+		return filepath.ToSlash(p)
+	}
+
+	rebaseRes := func(res []*ScanResult) []*ScanResult {
+		for _, r := range res {
+			r.Root = rebasePath(r.Root)
+			for i, f := range r.Files {
+				r.Files[i] = rebasePath(f)
+			}
+		}
+		return res
+	}
+
+	expectedAll := []*ScanResult{
+		{
+			Root:  "repo a",
+			Files: []string{"repo a/deeper/file.star", "repo a/file.star"},
+		},
+		{
+			Root:  "repo a/inner",
+			Files: []string{"repo a/inner/file.star"},
+		},
+		{
+			Root:      "repo b/pkg 1",
+			IsPackage: true,
+			Files:     []string{"repo b/pkg 1/PACKAGE.star", "repo b/pkg 1/deeper/file.star"},
+		},
+		{
+			Root:      "repo b/pkg 1/inner",
+			IsPackage: true,
+			Files:     []string{"repo b/pkg 1/inner/PACKAGE.star"},
+		},
+		{
+			Root:  "repo b/pkg 2",
+			Files: []string{"repo b/pkg 2/file.star"},
+		},
+		{
+			Root:  "repo b/pkg 2/deeper",
+			Files: []string{"repo b/pkg 2/deeper/file.star"},
+		},
+	}
+
+	t.Run("Recursive all", func(t *testing.T) {
+		res, err := ScanForRoots([]string{tmp})
+		assert.NoErr(t, err)
+		assert.That(t, rebaseRes(res), should.Match(expectedAll))
+	})
+
+	t.Run("Individual files", func(t *testing.T) {
+		res, err := ScanForRoots([]string{
+			abs("repo a/file.star"),
+			abs("repo a/deeper/file.star"),
+			abs("repo a/inner/file.star"),
+			abs("repo b/pkg 1/PACKAGE.star"),
+			abs("repo b/pkg 1/deeper/file.star"),
+			abs("repo b/pkg 1/inner/PACKAGE.star"),
+			abs("repo b/pkg 2/file.star"),
+			abs("repo b/pkg 2/deeper/file.star"),
+		})
+		assert.NoErr(t, err)
+		assert.That(t, rebaseRes(res), should.Match(expectedAll))
+	})
+
+	t.Run("Individual dirs", func(t *testing.T) {
+		res, err := ScanForRoots([]string{
+			abs("repo a"),
+			abs("repo a/deeper"),
+			abs("repo b/pkg 1"),
+			abs("repo b/pkg 1/deeper"),
+			abs("repo b/pkg 1/inner"),
+			abs("repo b/pkg 2"),
+			abs("repo b/pkg 2/deeper"),
+		})
+		assert.NoErr(t, err)
+		assert.That(t, rebaseRes(res), should.Match(expectedAll))
+	})
+
+	t.Run("Few leaves", func(t *testing.T) {
+		res, err := ScanForRoots([]string{
+			abs("repo a/deeper/file.star"),
+			abs("repo b/pkg 1/deeper/file.star"),
+			abs("repo b/pkg 2/deeper/file.star"),
+		})
+		assert.NoErr(t, err)
+		assert.That(t, rebaseRes(res), should.Match([]*ScanResult{
+			{
+				Root: "repo a", Files: []string{"repo a/deeper/file.star"},
+			},
+			{
+				Root:      "repo b/pkg 1",
+				IsPackage: true,
+				Files:     []string{"repo b/pkg 1/deeper/file.star"},
+			},
+			{
+				Root:  "repo b/pkg 2/deeper",
+				Files: []string{"repo b/pkg 2/deeper/file.star"},
+			},
+		}))
+	})
+
+	t.Run("RelFiles", func(t *testing.T) {
+		res, err := ScanForRoots([]string{
+			abs("repo a"),
+		})
+		assert.NoErr(t, err)
+		assert.That(t, res[0].Root, should.Equal(abs("repo a")))
+		assert.That(t, res[0].RelFiles(), should.Match([]string{
+			"deeper/file.star",
+			"file.star",
+		}))
+	})
+}
