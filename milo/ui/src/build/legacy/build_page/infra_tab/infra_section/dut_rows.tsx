334c1bb2ed6b9f2eb4ef44ea4c1c39862cc82c3f
Pietro Scuttari <pietroscutta@google.com>
2025-06-17 10:37:34 -0700

    Hide "DUT running task" if missing in the fleet-console
    https://screenshot.googleplex.com/4pbRRxRhB5qdL6T
    
    Change-Id: I01e72b01ba7a3b433a3159307717d70375418d25
    Bug: b/424603213
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/6650789
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Commit-Queue: Tiffany Zhang <zhangtiff@chromium.org>
    Auto-Submit: Pietro Scuttari <pietroscutta@google.com>

--- milo/ui/src/build/legacy/build_page/infra_tab/infra_section/dut_rows.tsx
+++ milo/ui/src/build/legacy/build_page/infra_tab/infra_section/dut_rows.tsx
@@ -16,6 +16,7 @@ import { Link } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 
 import { OutputBuildInfra_Backend } from '@/build/types';
+import { useDeviceData } from '@/fleet/pages/device_details_page/use_device_data';
 import { DEVICE_TASKS_SWARMING_HOST } from '@/fleet/utils/builds';
 import { getDutName } from '@/fleet/utils/swarming';
 import { useBotsClient } from '@/swarming/hooks/prpc_clients';
@@ -41,7 +42,15 @@ export function DutRows({ backend }: DutRowsProps) {
     enabled: botId !== undefined,
   });
 
+  // We can't call hooks conditionally, so we need to provide a failing ID
+  // when the query is not successful. This ensures that the hook is always
+  // called, even if the query fails.
+  const deviceQuery = useDeviceData(
+    dutNameQuery.isSuccess ? dutNameQuery.data : 'failing id',
+  );
+
   if (!dutNameQuery.isSuccess) return null;
+  if (!deviceQuery.device) return null;
 
   return (
     <tr>
