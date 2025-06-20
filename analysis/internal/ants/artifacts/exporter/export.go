3deb87caa8dedb285c95fcdb7f3a8e72694b0d1d
Beining Chen <beining@google.com>
2025-06-19 23:26:04 -0700

    [analysis] set up export client for ants artifact export
    Bug: b:408309587
    
    Change-Id: I6c4ae56385329a45572dc5938aa18ab776ac2e40
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/6653462
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Commit-Queue: Beining Chen <beining@google.com>

---
+++ analysis/internal/ants/artifacts/exporter/export.go
@@ -0,0 +1,87 @@
+// Copyright 2025 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package exporter
+
+import (
+	"context"
+
+	"go.chromium.org/luci/common/errors"
+	rdbpbutil "go.chromium.org/luci/resultdb/pbutil"
+	rdbpb "go.chromium.org/luci/resultdb/proto/v1"
+
+	bqpb "go.chromium.org/luci/analysis/proto/bq/legacy"
+)
+
+// InsertClient defines an interface for inserting rows into BigQuery.
+type InsertClient interface {
+	// Insert inserts the given rows into BigQuery.
+	Insert(ctx context.Context, rows []*bqpb.AntsArtifactRow) error
+}
+
+// Exporter provides methods to stream rows into BigQuery.
+type Exporter struct {
+	client InsertClient
+}
+
+// NewExporter instantiates a new Exporter. The given client is used
+// to insert rows into BigQuery.
+func NewExporter(client InsertClient) *Exporter {
+	return &Exporter{client: client}
+}
+
+// ExportOptions captures context which will be exported
+// alongside the test artifacts.
+type ExportOptions struct {
+	Invocation *rdbpb.Invocation
+}
+
+// Export inserts the provided artifacts into BigQuery.
+func (e *Exporter) Export(ctx context.Context, artifacts []*rdbpb.Artifact, opts ExportOptions) error {
+
+	exportRow, err := prepareExportRow(artifacts, opts)
+	if err != nil {
+		return errors.Fmt("prepare row: %w", err)
+	}
+
+	if err := e.client.Insert(ctx, exportRow); err != nil {
+		return errors.Fmt("insert rows: %w", err)
+	}
+	return nil
+}
+
+// prepareExportRow converts a ResultDB Artifact proto to an AntsArtifactRow BigQuery proto.
+func prepareExportRow(artifacts []*rdbpb.Artifact, opts ExportOptions) ([]*bqpb.AntsArtifactRow, error) {
+	invocationID, err := rdbpbutil.ParseInvocationName(opts.Invocation.Name)
+	if err != nil {
+		return nil, errors.Fmt("invalid invocation name %q: %w", invocationID, err)
+	}
+
+	results := make([]*bqpb.AntsArtifactRow, 0, len(artifacts))
+	for _, artifact := range artifacts {
+
+		artifactRow := &bqpb.AntsArtifactRow{
+			InvocationId:   invocationID,
+			WorkUnitId:     "",
+			TestResultId:   artifact.ResultId,
+			Name:           artifact.ArtifactId,
+			Size:           artifact.SizeBytes,
+			ContentType:    artifact.ContentType,
+			ArtifactType:   "",
+			CompletionTime: opts.Invocation.FinalizeTime,
+		}
+		results = append(results, artifactRow)
+	}
+	return results, nil
+}
---
+++ analysis/internal/ants/artifacts/exporter/schema.go
@@ -0,0 +1,84 @@
+// Copyright 2025 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package exporter
+
+import (
+	"time"
+
+	"cloud.google.com/go/bigquery"
+	"cloud.google.com/go/bigquery/storage/managedwriter/adapt"
+	"github.com/golang/protobuf/descriptor"
+	desc "github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"google.golang.org/protobuf/types/descriptorpb"
+
+	"go.chromium.org/luci/common/bq"
+
+	bqpb "go.chromium.org/luci/analysis/proto/bq/legacy"
+)
+
+// tableName is the name of the exported BigQuery table.
+const tableName = "ants_test_artifacts"
+
+// Only keep the data for 30 days, since all the data in the table will be exported to placer soon after they arrive.
+const partitionExpirationTime = 30 * 24 * time.Hour
+
+const rowMessage = "luci.analysis.bq.legacy.AntsArtifactRow"
+
+var tableMetadata *bigquery.TableMetadata
+
+// tableSchemaDescriptor is a self-contained DescriptorProto for describing
+// row protocol buffers sent to the BigQuery Write API.
+var tableSchemaDescriptor *descriptorpb.DescriptorProto
+
+func init() {
+	var err error
+	var schema bigquery.Schema
+	if schema, err = generateRowSchema(); err != nil {
+		panic(err)
+	}
+	if tableSchemaDescriptor, err = generateRowSchemaDescriptor(); err != nil {
+		panic(err)
+	}
+
+	tableMetadata = &bigquery.TableMetadata{
+		TimePartitioning: &bigquery.TimePartitioning{
+			Type:       bigquery.DayPartitioningType,
+			Expiration: partitionExpirationTime,
+			Field:      "completion_time",
+		},
+		Description: "Contains test artifacts produced by Android.",
+		// Relax ensures no fields are marked "required".
+		Schema: bq.RelaxSchema(schema),
+		Labels: map[string]string{bq.MetadataVersionKey: "1"},
+	}
+}
+
+func generateRowSchema() (schema bigquery.Schema, err error) {
+	fd, _ := descriptor.MessageDescriptorProto(&bqpb.AntsArtifactRow{})
+	// We also need to get FileDescriptorProto for other referenced protos
+	// because they are defined in different files.
+	fdats, _ := descriptor.MessageDescriptorProto(&bqpb.AntsTestResultRow{})
+	fdset := &desc.FileDescriptorSet{File: []*desc.FileDescriptorProto{fd, fdats}}
+	return bq.GenerateSchema(fdset, rowMessage)
+}
+
+func generateRowSchemaDescriptor() (*desc.DescriptorProto, error) {
+	m := &bqpb.AntsArtifactRow{}
+	descriptorProto, err := adapt.NormalizeDescriptor(m.ProtoReflect().Descriptor())
+	if err != nil {
+		return nil, err
+	}
+	return descriptorProto, nil
+}
