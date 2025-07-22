f7c6b611ba6ebefb4aa6a7a01a194b30c2166aa7
Beining Chen <beining@google.com>
2025-07-22 00:52:01 -0700

    [resultdb] implement BatchCreateWorkUnits RPC
    Bug: b:422274371
    
    Change-Id: I45f1723d6b0d9a9c14f83964be9eaab89224fe42
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/6773724
    Reviewed-by: Patrick Meiring <meiring@google.com>
    Commit-Queue: Beining Chen <beining@google.com>

--- resultdb/internal/services/recorder/batch_create_work_units.go
+++ resultdb/internal/services/recorder/batch_create_work_units.go
@@ -17,12 +17,19 @@ package recorder
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"cloud.google.com/go/spanner"
 	"google.golang.org/grpc/codes"
 
+	"go.chromium.org/luci/common/clock"
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/grpc/appstatus"
+	"go.chromium.org/luci/server/auth"
+	"go.chromium.org/luci/server/span"
 
+	"go.chromium.org/luci/resultdb/internal/masking"
+	"go.chromium.org/luci/resultdb/internal/permissions"
 	"go.chromium.org/luci/resultdb/internal/rootinvocations"
 	"go.chromium.org/luci/resultdb/internal/workunits"
 	"go.chromium.org/luci/resultdb/pbutil"
@@ -37,9 +44,108 @@ func (s *recorderServer) BatchCreateWorkUnits(ctx context.Context, in *pb.BatchC
 	if err := validateBatchCreateWorkUnitsRequest(in); err != nil {
 		return nil, appstatus.BadRequest(err)
 	}
-	// TODO: verify update tokens.
+	if err := createWorkUnitsIdempotent(ctx, in, s.ExpectedResultsExpiration); err != nil {
+		return nil, err
+	}
 
-	return nil, appstatus.Error(codes.Unimplemented, "not yet implemented")
+	// TODO: The response is read in a separate transaction
+	// from the creation. This means a duplicate request may not receive an
+	// identical response if the invocation was updated in the meantime.
+	// While AIP-155 (https://google.aip.dev/155#stale-success-responses)
+	// permits returning the most current data, we could instead construct the
+	// response from the request data for better consistency.
+	ids := make([]workunits.ID, 0, len(in.Requests))
+	for _, r := range in.Requests {
+		ids = append(ids, extractWorkUnitIDFromRequest(r))
+	}
+	wuRows, err := workunits.ReadBatch(span.Single(ctx), ids, workunits.AllFields)
+	if err != nil {
+		return nil, err
+	}
+	workUnits := make([]*pb.WorkUnit, len(in.Requests))
+	updateTokens := make([]string, len(in.Requests))
+	for i, id := range ids {
+		token, err := generateWorkUnitToken(ctx, id)
+		if err != nil {
+			return nil, err
+		}
+		updateTokens[i] = token
+		inputs := masking.WorkUnitFields{
+			Row: wuRows[i],
+			// TODO:populate child work unit.
+			// There should be no child invocation at this point.
+		}
+		workUnits[i] = masking.WorkUnit(inputs, permissions.FullAccess, pb.WorkUnitView_WORK_UNIT_VIEW_FULL)
+	}
+	return &pb.BatchCreateWorkUnitsResponse{
+		WorkUnits:    workUnits,
+		UpdateTokens: updateTokens,
+	}, nil
+}
+
+// createWorkUnitsIdempotent atomically creates the work units in the request.
+// The operation is idempotent: if the work units already exist with the same
+// request ID, the creation is skipped.
+func createWorkUnitsIdempotent(
+	ctx context.Context,
+	in *pb.BatchCreateWorkUnitsRequest,
+	uninterestingTestVerdictsExpirationTime time.Duration,
+) error {
+	now := clock.Now(ctx)
+	createdBy := string(auth.CurrentIdentity(ctx))
+
+	ids := make([]workunits.ID, 0, len(in.Requests))
+	parentIDs := make([]workunits.ID, 0, len(in.Requests))
+	for _, r := range in.Requests {
+		ids = append(ids, extractWorkUnitIDFromRequest(r))
+		parentIDs = append(parentIDs, workunits.MustParseName(r.Parent))
+	}
+	_, err := mutateWorkUnitsForCreate(ctx, parentIDs, ids, func(ctx context.Context) error {
+		dedup, err := deduplicateCreateWorkUnits(ctx, ids, in.RequestId, createdBy)
+		if err != nil {
+			return err
+		}
+		if dedup {
+			// This call should be deduplicated, do not write to database.
+			return nil
+		}
+		for i, r := range in.Requests {
+			wu := r.WorkUnit
+			state := wu.State
+			if state == pb.WorkUnit_STATE_UNSPECIFIED {
+				state = pb.WorkUnit_ACTIVE
+			}
+			deadline := wu.Deadline.AsTime()
+			if wu.Deadline == nil {
+				deadline = now.Add(defaultDeadlineDuration)
+			}
+
+			wuRow := &workunits.WorkUnitRow{
+				ID:                 ids[i],
+				ParentWorkUnitID:   spanner.NullString{Valid: true, StringVal: parentIDs[i].WorkUnitID},
+				State:              state,
+				Realm:              wu.Realm,
+				CreatedBy:          createdBy,
+				Deadline:           deadline,
+				CreateRequestID:    in.RequestId,
+				ProducerResource:   wu.ProducerResource,
+				Tags:               wu.Tags,
+				Properties:         wu.Properties,
+				Instructions:       wu.Instructions,
+				ExtendedProperties: wu.ExtendedProperties,
+			}
+			legacyCreateOpts := workunits.LegacyCreateOptions{
+				ExpectedTestResultsExpirationTime: now.Add(uninterestingTestVerdictsExpirationTime),
+			}
+			span.BufferWrite(ctx, workunits.Create(wuRow, legacyCreateOpts)...)
+		}
+		// TODO: add inclusions
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func validateBatchCreateWorkUnitsPermissions(ctx context.Context, req *pb.BatchCreateWorkUnitsRequest) error {
@@ -60,6 +166,99 @@ func validateBatchCreateWorkUnitsPermissions(ctx context.Context, req *pb.BatchC
 		}
 	}
 	return nil
+}
+
+// mutateWorkUnitsForCreate provides a transactional wrapper for creating work units.
+//
+// It ensures that modifications happen only if the parent work unit is ACTIVE and
+// the caller provides a valid update token. For parents created in the same batch
+// request, the state check is skipped as they don't exist in the database yet.
+//
+// Both the parentIDs and newIDs slices must be corresponding the BatchCreateWorkUnits.requests
+// (i.e. parentIDs[i] and  newIDs[i] are the parent ID and work unit ID in BatchCreateWorkUnits.requests[i])
+//
+// On success, it returns the Spanner commit timestamp.
+func mutateWorkUnitsForCreate(ctx context.Context, parentIDs []workunits.ID, newIDs []workunits.ID, f func(context.Context) error) (time.Time, error) {
+	// Per AIP-211, authorization should precede validation. We intentionally deviate
+	// from that pattern by performing update-token authorization inside `mutateWorkUnitsForCreate`,
+	// after initial request validation. This make sure data modification is always
+	// performed with authorization check.
+	//
+	// Because the update-token is not yet validated, logic
+	// before the `mutateWorkUnitsForCreate` call MUST NOT access or return any protected
+	// resource data. All preceding checks should operate only on the incoming
+	// request parameters.
+	token, err := extractUpdateToken(ctx)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	if err := validateSameUpdateTokenState(parentIDs); err != nil {
+		return time.Time{}, appstatus.BadRequest(err)
+	}
+
+	// We have already check all parentIDs have the same update token, so we just need to check against one of them.
+	if err := validateWorkUnitToken(ctx, token, parentIDs[0]); err != nil {
+		return time.Time{}, appstatus.Errorf(codes.PermissionDenied, "invalid update token")
+	}
+
+	newIDSet := workunits.NewIDSet(newIDs...)
+	parentsToCheckSet := workunits.NewIDSet(parentIDs...)
+	// Only check parents that are not being created in this batch.
+	parentsToCheckSet.RemoveAll(newIDSet)
+	parentsToCheck := parentsToCheckSet.ToSlice()
+
+	commitTimestamp, err := span.ReadWriteTransaction(ctx, func(ctx context.Context) error {
+		states, err := workunits.ReadStates(ctx, parentsToCheck)
+		if err != nil {
+			return err
+		}
+		for i, st := range states {
+			if st != pb.WorkUnit_ACTIVE {
+				return appstatus.Errorf(codes.FailedPrecondition, "parent %q is not active", parentsToCheck[i].Name())
+			}
+		}
+		return f(ctx)
+	})
+	if err != nil {
+		return time.Time{}, err
+	}
+	return commitTimestamp, nil
+}
+
+func deduplicateCreateWorkUnits(ctx context.Context, ids []workunits.ID, requestID, createdBy string) (shouldDedup bool, err error) {
+	results, err := workunits.ReadRequestIDsAndCreatedBys(ctx, ids)
+	if err != nil {
+		return false, err
+	}
+	var exampleExistWorkUnit *workunits.ID
+	existCount := 0
+	for i := range ids {
+		if results[i] == nil {
+			// Work unit ID doesn't exist yet.
+			continue
+		}
+		if exampleExistWorkUnit == nil {
+			exampleExistWorkUnit = &ids[i]
+		}
+		existCount += 1
+		if results[i].RequestID != requestID || results[i].CreatedBy != createdBy {
+			// Work unit with the same id exist, and is not created with the same requestID and creator.
+			return false, appstatus.Errorf(codes.AlreadyExists, "%q already exists with different requestID or creator", ids[i].Name())
+		}
+	}
+	if existCount == 0 {
+		// Do not deduplicate, none of the id exists.
+		return false, nil
+	}
+	if existCount != len(ids) {
+		// some ids already exist, but some doesn't exist.
+		// Could happen if someone sent two different but overlapping batch create
+		// requests, but reused the request_id.
+		return false, appstatus.Errorf(codes.AlreadyExists, "some work units already exist (eg. %q)", exampleExistWorkUnit.Name())
+	}
+	// All id exist, deduplicate this call.
+	return true, nil
 }
 
 func validateBatchCreateWorkUnitsRequest(req *pb.BatchCreateWorkUnitsRequest) error {
@@ -119,7 +318,7 @@ func validateRequestOrdering(req *pb.BatchCreateWorkUnitsRequest) error {
 		parentID := workunits.MustParseName(r.Parent)
 		idx, ok := idToIndex[parentID]
 		if ok && idx >= i {
-			return errors.Fmt("requests[%d]: parent: cannot refer to work unit created in the later request requests[%d], please order requests to match expected creation order.", i, idx)
+			return errors.Fmt("requests[%d]: parent: cannot refer to work unit created in the later request requests[%d], please order requests to match expected creation order", i, idx)
 		}
 	}
 	return nil
@@ -131,4 +330,15 @@ func extractWorkUnitIDFromRequest(r *pb.CreateWorkUnitRequest) workunits.ID {
 		RootInvocationID: parentID.RootInvocationID,
 		WorkUnitID:       r.WorkUnitId,
 	}
+}
+
+// validate only a single update token is required.
+func validateSameUpdateTokenState(parents []workunits.ID) error {
+	s := workUnitTokenState(parents[0])
+	for i, p := range parents {
+		if s != workUnitTokenState(p) {
+			return errors.Fmt("requests[%d]: parent %q requires a different update token to requests[0].parent %q, but this RPC only accepts one update token", i, p.Name(), parents[0].Name())
+		}
+	}
+	return nil
 }
--- resultdb/internal/services/recorder/batch_create_work_units_test.go
+++ resultdb/internal/services/recorder/batch_create_work_units_test.go
@@ -18,16 +18,32 @@ import (
 	"context"
 	"testing"
 
+	"cloud.google.com/go/spanner"
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/types/known/structpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
+	"go.chromium.org/luci/common/clock/testclock"
 	"go.chromium.org/luci/common/testing/ftt"
+	"go.chromium.org/luci/common/testing/prpctest"
 	"go.chromium.org/luci/common/testing/truth/assert"
 	"go.chromium.org/luci/common/testing/truth/should"
 	"go.chromium.org/luci/grpc/appstatus"
+	"go.chromium.org/luci/grpc/grpcutil/testing/grpccode"
 	"go.chromium.org/luci/server/auth"
 	"go.chromium.org/luci/server/auth/authtest"
+	"go.chromium.org/luci/server/span"
 
+	"go.chromium.org/luci/resultdb/internal/instructionutil"
+	"go.chromium.org/luci/resultdb/internal/invocations"
+	"go.chromium.org/luci/resultdb/internal/rootinvocations"
+	"go.chromium.org/luci/resultdb/internal/spanutil"
+	"go.chromium.org/luci/resultdb/internal/testutil"
+	"go.chromium.org/luci/resultdb/internal/testutil/insert"
 	"go.chromium.org/luci/resultdb/internal/workunits"
+	"go.chromium.org/luci/resultdb/pbutil"
 	pb "go.chromium.org/luci/resultdb/proto/v1"
 )
 
@@ -226,6 +242,358 @@ func TestValidateBatchCreateWorkUnitsRequest(t *testing.T) {
 
 			err := validateBatchCreateWorkUnitsRequest(req)
 			assert.That(t, err, should.ErrLike("all requests must be for creations in the same root invocation"))
+		})
+	})
+}
+
+func TestBatchCreateWorkUnits(t *testing.T) {
+	ftt.Run(`TestBatchCreateWorkUnits`, t, func(t *ftt.Test) {
+		ctx := testutil.SpannerTestContext(t)
+		ctx = auth.WithState(ctx, &authtest.FakeState{
+			Identity: "user:someone@example.com",
+			IdentityPermissions: []authtest.RealmPermission{
+				{Realm: "testproject:testrealm", Permission: permCreateWorkUnit},
+				{Realm: "testproject:testrealm", Permission: permIncludeWorkUnit},
+				{Realm: "testproject:@root", Permission: permCreateWorkUnitWithReservedID},
+				{Realm: "testproject:@root", Permission: permSetWorkUnitProducerResource},
+			},
+		})
+
+		// Set test clock.
+		start := testclock.TestRecentTimeUTC
+		ctx, _ = testclock.UseTime(ctx, start)
+
+		// Setup a full HTTP server.
+		server := &prpctest.Server{}
+		pb.RegisterRecorderServer(server, newTestRecorderServer())
+		server.Start(ctx)
+		defer server.Close()
+		client, err := server.NewClient()
+		assert.Loosely(t, err, should.BeNil)
+		recorder := pb.NewRecorderPRPCClient(client)
+
+		rootInvID := rootinvocations.ID("root-inv-id")
+		parentWorkUnitID := workunits.ID{
+			RootInvocationID: rootInvID,
+			WorkUnitID:       "wu-parent",
+		}
+		workUnitID1 := workunits.ID{
+			RootInvocationID: rootInvID,
+			WorkUnitID:       "wu-parent:wu-new-1",
+		}
+		workUnitID11 := workunits.ID{
+			RootInvocationID: rootInvID,
+			WorkUnitID:       "wu-new-11",
+		}
+		workUnitID2 := workunits.ID{
+			RootInvocationID: rootInvID,
+			WorkUnitID:       "wu-new-2",
+		}
+
+		// Insert a root invocation and the parent work unit.
+		rootInv := rootinvocations.NewBuilder(rootInvID).WithRealm("testproject:testrealm").Build()
+		parentWu := workunits.NewBuilder(rootInvID, parentWorkUnitID.WorkUnitID).WithState(pb.WorkUnit_ACTIVE).Build()
+		testutil.MustApply(ctx, t, insert.RootInvocationWithRootWorkUnit(rootInv)...)
+		testutil.MustApply(ctx, t, insert.WorkUnit(parentWu)...)
+
+		// Generate an update token for the parent work unit.
+		parentUpdateToken, err := generateWorkUnitToken(ctx, parentWorkUnitID)
+		assert.Loosely(t, err, should.BeNil)
+		ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs(pb.UpdateTokenMetadataKey, parentUpdateToken))
+
+		// A basic valid request.
+		// parent
+		//  |---wu-parent:wu-new-1
+		//  |          |--- wu-new-11
+		//  |---wu-new-2
+		req := &pb.BatchCreateWorkUnitsRequest{
+			RequestId: "test-request-id",
+			Requests: []*pb.CreateWorkUnitRequest{
+				{
+					Parent:     parentWorkUnitID.Name(),
+					WorkUnitId: workUnitID1.WorkUnitID,
+					WorkUnit:   &pb.WorkUnit{Realm: "testproject:testrealm"},
+				},
+				{
+					Parent:     workUnitID1.Name(),
+					WorkUnitId: workUnitID11.WorkUnitID,
+					WorkUnit:   &pb.WorkUnit{Realm: "testproject:testrealm"},
+				},
+				{
+					Parent:     parentWorkUnitID.Name(),
+					WorkUnitId: workUnitID2.WorkUnitID,
+					WorkUnit:   &pb.WorkUnit{Realm: "testproject:testrealm"},
+				},
+			},
+		}
+
+		t.Run("invalid request", func(t *ftt.Test) {
+			// Full validation is tested in validateBatchCreateWorkUnitsRequest tests.
+			// These tests just ensure that validation is being called.
+			t.Run("missing top-level request id", func(t *ftt.Test) {
+				req.RequestId = ""
+
+				_, err := recorder.BatchCreateWorkUnits(ctx, req)
+				assert.That(t, err, grpccode.ShouldBe(codes.InvalidArgument))
+				assert.That(t, err, should.ErrLike("request_id: unspecified"))
+			})
+		})
+
+		t.Run("permission denied", func(t *ftt.Test) {
+			// Permission checks are tested exhaustively in TestvalidateBatchCreateWorkUnitsPermissions.
+			// This test just ensures they are called.
+			t.Run("no create work unit permission", func(t *ftt.Test) {
+				req.Requests[0].WorkUnit.Realm = "secretproject:testrealm"
+
+				_, err := recorder.BatchCreateWorkUnits(ctx, req)
+				assert.That(t, err, grpccode.ShouldBe(codes.PermissionDenied))
+				assert.That(t, err, should.ErrLike(`requests[0]: caller does not have permission "resultdb.workUnits.create"`))
+			})
+
+			t.Run("update token", func(t *ftt.Test) {
+				// Update token validation is not inside validateBatchCreateWorkUnitsPermissions, this need to be tested exhaustively here.
+				t.Run("invalid update token", func(t *ftt.Test) {
+					ctx := metadata.NewOutgoingContext(ctx, metadata.Pairs(pb.UpdateTokenMetadataKey, "invalid-token"))
+
+					_, err := recorder.BatchCreateWorkUnits(ctx, req)
+					assert.That(t, err, grpccode.ShouldBe(codes.PermissionDenied))
+					assert.That(t, err, should.ErrLike(`invalid update token`))
+				})
+
+				t.Run("missing update token", func(t *ftt.Test) {
+					ctx := metadata.NewOutgoingContext(ctx, metadata.MD{})
+
+					_, err := recorder.BatchCreateWorkUnits(ctx, req)
+					assert.That(t, err, grpccode.ShouldBe(codes.Unauthenticated))
+					assert.That(t, err, should.ErrLike(`missing update-token metadata value in the request`))
+				})
+
+				t.Run("parent requires a different update token", func(t *ftt.Test) {
+					anotherParent := workunits.ID{
+						RootInvocationID: rootInvID,
+						WorkUnitID:       "another-parent",
+					}
+					req.Requests[1].Parent = anotherParent.Name()
+
+					_, err := recorder.BatchCreateWorkUnits(ctx, req)
+					assert.That(t, err, grpccode.ShouldBe(codes.InvalidArgument))
+					assert.That(t, err, should.ErrLike(`parent "rootInvocations/root-inv-id/workUnits/another-parent" requires a different update token`))
+				})
+			})
+		})
+
+		t.Run("parent", func(t *ftt.Test) {
+			t.Run("parent not active", func(t *ftt.Test) {
+				testutil.MustApply(ctx, t, spanutil.UpdateMap("WorkUnits", map[string]any{
+					"RootInvocationShardId": parentWorkUnitID.RootInvocationShardID(),
+					"WorkUnitId":            parentWorkUnitID.WorkUnitID,
+					"State":                 pb.WorkUnit_FINALIZING,
+				}))
+
+				_, err = recorder.BatchCreateWorkUnits(ctx, req)
+				assert.That(t, err, grpccode.ShouldBe(codes.FailedPrecondition))
+				assert.That(t, err, should.ErrLike("parent \"rootInvocations/root-inv-id/workUnits/wu-parent\" is not active"))
+			})
+
+			t.Run("parent does not exist and not created in the request", func(t *ftt.Test) {
+				testutil.MustApply(ctx, t, spanner.Delete("WorkUnits", parentWorkUnitID.Key()))
+
+				_, err = recorder.BatchCreateWorkUnits(ctx, req)
+				assert.That(t, err, grpccode.ShouldBe(codes.NotFound))
+				assert.That(t, err, should.ErrLike("not found"))
+			})
+		})
+		t.Run("already exists", func(t *ftt.Test) {
+			t.Run("exist with different request id", func(t *ftt.Test) {
+				// Create just one of the work units to be created.
+				wu := workunits.NewBuilder(rootInvID, workUnitID11.WorkUnitID).WithCreateRequestID("different-request-id").Build()
+				testutil.MustApply(ctx, t, insert.WorkUnit(wu)...)
+
+				_, err := recorder.BatchCreateWorkUnits(ctx, req)
+				assert.That(t, err, grpccode.ShouldBe(codes.AlreadyExists))
+				assert.That(t, err, should.ErrLike("\"rootInvocations/root-inv-id/workUnits/wu-new-11\" already exists with different requestID or creator"))
+			})
+			t.Run("partial exist with the same request id", func(t *ftt.Test) {
+				// Create all work units to be created, but with a different request ID.
+				wu := workunits.NewBuilder(rootInvID, workUnitID2.WorkUnitID).WithCreateRequestID("test-request-id").WithCreatedBy("user:someone@example.com").Build()
+				testutil.MustApply(ctx, t, insert.WorkUnit(wu)...)
+
+				_, err := recorder.BatchCreateWorkUnits(ctx, req)
+				assert.That(t, err, grpccode.ShouldBe(codes.AlreadyExists))
+				// The error message depends on which row is read first, so we just check for the general error string.
+				assert.That(t, err, should.ErrLike("some work units already exist (eg. \"rootInvocations/root-inv-id/workUnits/wu-new-2\")"))
+			})
+		})
+
+		t.Run("create is idempotent", func(t *ftt.Test) {
+			res1, err := recorder.BatchCreateWorkUnits(ctx, req)
+			assert.Loosely(t, err, should.BeNil)
+
+			// Send the exact same request again.
+			res2, err := recorder.BatchCreateWorkUnits(ctx, req)
+			assert.Loosely(t, err, should.BeNil)
+			assert.That(t, res2, should.Match(res1))
+
+			// Check the database has the expected number of entries.
+			readCtx, cancel := span.ReadOnlyTransaction(ctx)
+			defer cancel()
+			rows, err := workunits.ReadBatch(readCtx, []workunits.ID{workUnitID1, workUnitID11, workUnitID2}, workunits.AllFields)
+			assert.Loosely(t, err, should.BeNil)
+			assert.Loosely(t, rows, should.HaveLength(3))
+		})
+
+		t.Run("end to end success", func(t *ftt.Test) {
+			wuProperties := &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"@type": structpb.NewStringValue("foo.bar.com/x/some.package.MyMessage"),
+					"key":   structpb.NewStringValue("workunit"),
+				},
+			}
+			instructions := testutil.TestInstructions()
+			extendedProperties := map[string]*structpb.Struct{
+				"mykey": {
+					Fields: map[string]*structpb.Value{
+						"@type":       structpb.NewStringValue("foo.bar.com/x/some.package.MyMessage"),
+						"child_key_1": structpb.NewStringValue("child_value_1"),
+					},
+				},
+			}
+
+			// Fill all fields to enhance test coverage.
+			req.Requests[0].WorkUnit = &pb.WorkUnit{
+				Realm:              "testproject:testrealm",
+				State:              pb.WorkUnit_FINALIZING,
+				ProducerResource:   "//producer.example.com/builds/123",
+				Tags:               pbutil.StringPairs("e2e_key", "e2e_value"),
+				Properties:         wuProperties,
+				ExtendedProperties: extendedProperties,
+				Instructions:       instructions,
+			}
+
+			// Expected work units in the response.
+			expectedWU1 := proto.Clone(req.Requests[0].WorkUnit).(*pb.WorkUnit)
+			proto.Merge(expectedWU1, &pb.WorkUnit{
+				Name:       workUnitID1.Name(),
+				Parent:     parentWorkUnitID.Name(),
+				WorkUnitId: workUnitID1.WorkUnitID,
+				Creator:    "user:someone@example.com",
+				Deadline:   timestamppb.New(start.Add(defaultDeadlineDuration)),
+			})
+			expectedWU1.Instructions = instructionutil.InstructionsWithNames(instructions, workUnitID1.Name())
+
+			expectedWU11 := proto.Clone(req.Requests[1].WorkUnit).(*pb.WorkUnit)
+			proto.Merge(expectedWU11, &pb.WorkUnit{
+				Name:       workUnitID11.Name(),
+				Parent:     workUnitID1.Name(),
+				WorkUnitId: workUnitID11.WorkUnitID,
+				State:      pb.WorkUnit_ACTIVE, // Default state is ACTIVE.
+				Creator:    "user:someone@example.com",
+				Deadline:   timestamppb.New(start.Add(defaultDeadlineDuration)),
+			})
+
+			expectedWU2 := proto.Clone(req.Requests[2].WorkUnit).(*pb.WorkUnit)
+			proto.Merge(expectedWU2, &pb.WorkUnit{
+				Name:       workUnitID2.Name(),
+				Parent:     parentWorkUnitID.Name(),
+				WorkUnitId: workUnitID2.WorkUnitID,
+				State:      pb.WorkUnit_ACTIVE, // Default state is ACTIVE.
+				Creator:    "user:someone@example.com",
+				Deadline:   timestamppb.New(start.Add(defaultDeadlineDuration)),
+			})
+
+			expectWURow1 := &workunits.WorkUnitRow{
+				ID:                 workUnitID1,
+				ParentWorkUnitID:   spanner.NullString{Valid: true, StringVal: parentWorkUnitID.WorkUnitID},
+				State:              pb.WorkUnit_FINALIZING,
+				Realm:              "testproject:testrealm",
+				CreatedBy:          "user:someone@example.com",
+				FinalizeStartTime:  spanner.NullTime{},
+				FinalizeTime:       spanner.NullTime{},
+				Deadline:           start.Add(defaultDeadlineDuration),
+				CreateRequestID:    "test-request-id",
+				ProducerResource:   "//producer.example.com/builds/123",
+				Tags:               pbutil.StringPairs("e2e_key", "e2e_value"),
+				Properties:         wuProperties,
+				Instructions:       instructionutil.InstructionsWithNames(instructions, workUnitID1.Name()),
+				ExtendedProperties: extendedProperties,
+			}
+
+			expectWURow11 := &workunits.WorkUnitRow{
+				ID:                workUnitID11,
+				ParentWorkUnitID:  spanner.NullString{Valid: true, StringVal: workUnitID1.WorkUnitID},
+				State:             pb.WorkUnit_ACTIVE,
+				Realm:             "testproject:testrealm",
+				CreatedBy:         "user:someone@example.com",
+				FinalizeStartTime: spanner.NullTime{},
+				FinalizeTime:      spanner.NullTime{},
+				Deadline:          start.Add(defaultDeadlineDuration),
+				CreateRequestID:   "test-request-id",
+			}
+
+			expectWURow2 := &workunits.WorkUnitRow{
+				ID:                workUnitID2,
+				ParentWorkUnitID:  spanner.NullString{Valid: true, StringVal: parentWorkUnitID.WorkUnitID},
+				State:             pb.WorkUnit_ACTIVE,
+				Realm:             "testproject:testrealm",
+				CreatedBy:         "user:someone@example.com",
+				FinalizeStartTime: spanner.NullTime{},
+				FinalizeTime:      spanner.NullTime{},
+				Deadline:          start.Add(defaultDeadlineDuration),
+				CreateRequestID:   "test-request-id",
+			}
+
+			// Expected update token
+			workUnitID11ExpectedUpdateToken, err := generateWorkUnitToken(ctx, workUnitID11)
+			assert.Loosely(t, err, should.BeNil)
+			workUnitID2ExpectedUpdateToken, err := generateWorkUnitToken(ctx, workUnitID2)
+			assert.Loosely(t, err, should.BeNil)
+
+			res, err := recorder.BatchCreateWorkUnits(ctx, req)
+			assert.Loosely(t, err, should.BeNil)
+			expectedWU1.CreateTime = res.WorkUnits[0].CreateTime
+			expectedWU1.FinalizeStartTime = res.WorkUnits[0].FinalizeStartTime
+			expectedWU11.CreateTime = res.WorkUnits[1].CreateTime
+			expectedWU2.CreateTime = res.WorkUnits[2].CreateTime
+			assert.That(t, res, should.Match(
+				&pb.BatchCreateWorkUnitsResponse{
+					WorkUnits: []*pb.WorkUnit{expectedWU1, expectedWU11, expectedWU2},
+					UpdateTokens: []string{
+						parentUpdateToken,
+						workUnitID11ExpectedUpdateToken,
+						workUnitID2ExpectedUpdateToken,
+					},
+				},
+			))
+			assert.Loosely(t, res.WorkUnits[0].FinalizeStartTime, should.NotBeNil)
+
+			// Check the database.
+			readCtx, cancel := span.ReadOnlyTransaction(ctx)
+			defer cancel()
+			row, err := workunits.Read(readCtx, workUnitID1, workunits.AllFields)
+			assert.Loosely(t, err, should.BeNil)
+			expectWURow1.SecondaryIndexShardID = row.SecondaryIndexShardID
+			expectWURow1.CreateTime = row.CreateTime
+			expectWURow1.FinalizeStartTime = row.FinalizeStartTime
+			assert.That(t, row, should.Match(expectWURow1))
+
+			row, err = workunits.Read(readCtx, workUnitID2, workunits.AllFields)
+			assert.Loosely(t, err, should.BeNil)
+			expectWURow2.SecondaryIndexShardID = row.SecondaryIndexShardID
+			expectWURow2.CreateTime = row.CreateTime
+			expectWURow2.Tags = []*pb.StringPair{}
+			assert.That(t, row, should.Match(expectWURow2))
+
+			row, err = workunits.Read(readCtx, workUnitID11, workunits.AllFields)
+			assert.Loosely(t, err, should.BeNil)
+			expectWURow11.SecondaryIndexShardID = row.SecondaryIndexShardID
+			expectWURow11.CreateTime = row.CreateTime
+			expectWURow11.Tags = []*pb.StringPair{}
+			assert.That(t, row, should.Match(expectWURow11))
+
+			// Check the legacy invocation is inserted.
+			legacyInvs, err := invocations.ReadBatch(readCtx, invocations.NewIDSet(workUnitID1.LegacyInvocationID(), workUnitID11.LegacyInvocationID(), workUnitID2.LegacyInvocationID()), invocations.AllFields)
+			assert.Loosely(t, err, should.BeNil)
+			assert.Loosely(t, legacyInvs, should.HaveLength(3))
 		})
 	})
 }
