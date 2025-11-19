7d0f4279ece413aff4bbc017f5f4f33c6ddfceef
Vadim Shtayura <vadimsh@chromium.org>
2025-11-18 17:23:30 -0800

    [cr-buildbucket] Add scheduleBuildOp helper.
    It represents an operation of creation of a batch of builds.
    It carries the request, request-scoped data (like the bucket
    cache) and accumulates errors.
    
    Will be used inside refactored scheduleBuild(...) implementation,
    in particular to avoid hard-to-read indirection through index maps.
    
    Note that it doesn't itself implement the logic of scheduling builds.
    There will be a function that does that, but it will be relatively
    high-level. This simplifies introducing a new code path for
    Turbo CI builders.
    
    R=chanli@chromium.org
    BUG=b/454194663
    
    Change-Id: I62f1be7986c8d413eafa4d9bd58fc7fd3d9dcb7c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/7170173
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

---
+++ buildbucket/appengine/rpc/schedule_build_op.go
@@ -0,0 +1,219 @@
+// Copyright 2025 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"context"
+	"fmt"
+	"iter"
+	"maps"
+	"slices"
+
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc/codes"
+
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/grpc/appstatus"
+
+	"go.chromium.org/luci/buildbucket/appengine/internal/config"
+	"go.chromium.org/luci/buildbucket/appengine/model"
+	pb "go.chromium.org/luci/buildbucket/proto"
+	"go.chromium.org/luci/buildbucket/protoutil"
+)
+
+// scheduleBuildOp is a batch of ScheduleBuildRequest processed in parallel.
+//
+// It carries the request state (being mutated during its processing) as well as
+// a cache of entities used by different parts of the request processing.
+//
+// Construct it with newScheduleBuildOp.
+type scheduleBuildOp struct {
+	// Reqs are the requests being processed.
+	Reqs []*pb.ScheduleBuildRequest
+	// DryRun is true if this batch will not actually be submitted.
+	DryRun bool
+
+	// Errs accumulates per-request errors.
+	Errs errors.MultiError
+	// Builds are the created builds (may be incomplete if DryRun is true).
+	Builds []*model.Build
+	// Masks are masks to apply to the returned build protos.
+	Masks []*model.BuildMask
+
+	// Parents describes parent builds mentioned by the batch.
+	Parents *parentsMap
+	// Buckets are buckets containing builders requested by the batch.
+	Buckets *bucketsMap
+	// GlobalCfg is the global config used to handle this batch.
+	GlobalCfg *pb.SettingsCfg
+	// WellKnownExperiments are known 'global' experiments from the GlobalCfg.
+	WellKnownExperiments stringset.Set
+
+	// reqMap maps *pb.ScheduleBuildRequest to its index in Reqs.
+	reqMap map[*pb.ScheduleBuildRequest]int
+}
+
+// newScheduleBuildOp initializes a scheduleBuildOp for a batch of requests.
+//
+// It prepares it to be used for detailed validation and eventual submission of
+// the given requests by prefetching their parent builds, bucket configs and
+// global service configs.
+//
+// It doesn't yet validate requests themselves.
+//
+// An error here means the entire batch has failed. It is always an appstatus
+// error.
+func newScheduleBuildOp(ctx context.Context, reqs []*pb.ScheduleBuildRequest) (*scheduleBuildOp, error) {
+	if len(reqs) == 0 {
+		return nil, appstatus.BadRequest(errors.New("at least one ScheduleBuildRequest is required"))
+	}
+
+	dryRun := reqs[0].DryRun
+	for _, req := range reqs {
+		if req.DryRun != dryRun {
+			return nil, appstatus.BadRequest(errors.New("all requests must have the same dry_run value"))
+		}
+	}
+
+	// Validate parents and fetch parent builds.
+	pIDSet := make(map[int64]struct{})
+	for _, req := range reqs {
+		if req.ParentBuildId != 0 {
+			pIDSet[req.ParentBuildId] = struct{}{}
+		}
+	}
+	parents, err := validateParents(ctx, slices.Collect(maps.Keys(pIDSet)))
+	if err != nil {
+		return nil, err
+	}
+
+	// Prefetch bucket configs to check shadowing settings.
+	buckets, err := prefetchBuckets(ctx, reqs)
+	if err != nil {
+		return nil, err
+	}
+
+	// Need the config to lookup experiments to validate them in the request.
+	globalCfg, err := config.GetSettingsCfg(ctx)
+	if err != nil {
+		return nil, appstatus.Errorf(codes.Internal, "error fetching service config: %s", err)
+	}
+
+	op := &scheduleBuildOp{
+		Reqs:                 reqs,
+		DryRun:               dryRun,
+		Errs:                 make(errors.MultiError, len(reqs)),
+		Masks:                make([]*model.BuildMask, len(reqs)),
+		Builds:               make([]*model.Build, len(reqs)),
+		Parents:              parents,
+		Buckets:              buckets,
+		GlobalCfg:            globalCfg,
+		WellKnownExperiments: protoutil.WellKnownExperiments(globalCfg),
+	}
+	op.UpdateReqMap()
+	return op, nil
+}
+
+// reqIdx returns the index of *pb.ScheduleBuildRequest in Reqs or panic.
+func (op *scheduleBuildOp) reqIdx(req *pb.ScheduleBuildRequest) int {
+	idx, ok := op.reqMap[req]
+	if !ok {
+		panic("scheduleBuildOp is given an unknown *ScheduleBuildRequest")
+	}
+	return idx
+}
+
+// UpdateReqMap updates mapping from *pb.ScheduleBuildRequest to its index.
+//
+// Must be called in case Reqs is updated.
+func (op *scheduleBuildOp) UpdateReqMap() {
+	op.reqMap = make(map[*pb.ScheduleBuildRequest]int, len(op.Reqs))
+	for idx, req := range op.Reqs {
+		if _, exists := op.reqMap[req]; exists {
+			panic("scheduleBuildOp is given identical *ScheduleBuildRequest")
+		}
+		op.reqMap[req] = idx
+	}
+}
+
+// Pending iterates over unresolved (not yet failed) requests.
+func (op *scheduleBuildOp) Pending() iter.Seq2[int, *pb.ScheduleBuildRequest] {
+	return func(yield func(int, *pb.ScheduleBuildRequest) bool) {
+		idx := 0
+		for i, req := range op.Reqs {
+			if op.Errs[i] == nil {
+				if !yield(idx, req) {
+					return
+				}
+				idx++
+			}
+		}
+	}
+}
+
+// Fail marks a single request as failed.
+func (op *scheduleBuildOp) Fail(req *pb.ScheduleBuildRequest, err error) {
+	idx := op.reqIdx(req)
+	op.Errs[idx] = err
+	op.Builds[idx] = nil
+}
+
+// SetBuild associates a pending request with a build entity it produced.
+func (op *scheduleBuildOp) SetBuild(req *pb.ScheduleBuildRequest, build *model.Build) {
+	idx := op.reqIdx(req)
+	if op.Errs[idx] != nil {
+		panic(fmt.Sprintf("request #%d was already marked as failed", idx))
+	}
+	op.Builds[idx] = build
+}
+
+// Finalize converts build entities to protos, applying the mask.
+//
+// May do datastore fetches to fetch build details if they are requested by
+// the mask.
+//
+// Returns the final result of this batch operation.
+func (op *scheduleBuildOp) Finalize(ctx context.Context) ([]*pb.Build, errors.MultiError) {
+	out := make([]*pb.Build, len(op.Reqs))
+
+	eg, _ := errgroup.WithContext(ctx)
+	eg.SetLimit(64)
+	for i, build := range op.Builds {
+		if op.Errs[i] != nil {
+			continue
+		}
+		if build == nil {
+			op.Errs[i] = appstatus.Errorf(codes.Internal, "build #%d/%d was left unprocessed, this should not be possible", i, len(op.Builds))
+			continue
+		}
+		eg.Go(func() error {
+			// Note: We don't redact the Build response here because we expect any
+			// user with BuildsAdd permission should also have BuildsGet.
+			//
+			// TODO(crbug/1042991): Don't re-read freshly written entities
+			// (see ToProto).
+			if op.DryRun {
+				out[i], op.Errs[i] = build.ToDryRunProto(ctx, op.Masks[i])
+			} else {
+				out[i], op.Errs[i] = build.ToProto(ctx, op.Masks[i], nil)
+			}
+			return nil
+		})
+	}
+	_ = eg.Wait()
+
+	return out, op.Errs
+}
---
+++ buildbucket/appengine/rpc/schedule_build_op_test.go
@@ -0,0 +1,135 @@
+// Copyright 2025 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpc
+
+import (
+	"testing"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/testing/truth/assert"
+	"go.chromium.org/luci/common/testing/truth/should"
+	"go.chromium.org/luci/gae/impl/memory"
+	"go.chromium.org/luci/gae/service/datastore"
+
+	"go.chromium.org/luci/buildbucket/appengine/internal/config"
+	"go.chromium.org/luci/buildbucket/appengine/model"
+	pb "go.chromium.org/luci/buildbucket/proto"
+)
+
+func TestScheduleBuildOp(t *testing.T) {
+	t.Parallel()
+
+	ctx := memory.Use(t.Context())
+
+	assert.NoErr(t, config.SetTestSettingsCfg(ctx, &pb.SettingsCfg{
+		Experiment: &pb.ExperimentSettings{
+			Experiments: []*pb.ExperimentSettings_Experiment{
+				{Name: "some-experiment"},
+			},
+		},
+	}))
+
+	bucketEntity := func(proj, buck string) *model.Bucket {
+		return &model.Bucket{
+			Parent: model.ProjectKey(ctx, proj),
+			ID:     buck,
+			Proto:  &pb.Bucket{},
+		}
+	}
+
+	buildEntity := func(id int64) *model.Build {
+		return &model.Build{
+			ID: id,
+			Proto: &pb.Build{
+				Builder: &pb.BuilderID{
+					Project: "ignored-project",
+					Bucket:  "ignored-bucket",
+					Builder: "ignored-builder",
+				},
+			},
+		}
+	}
+
+	buildInfraEntity := func(id int64) *model.BuildInfra {
+		return &model.BuildInfra{
+			Build: datastore.KeyForObj(ctx, buildEntity(id)),
+			Proto: &pb.BuildInfra{},
+		}
+	}
+
+	scheduleReq := func(proj, buck, builder string, parentBuildID int64) *pb.ScheduleBuildRequest {
+		return &pb.ScheduleBuildRequest{
+			Builder: &pb.BuilderID{
+				Project: proj,
+				Bucket:  buck,
+				Builder: builder,
+			},
+			ParentBuildId: parentBuildID,
+			DryRun:        true,
+		}
+	}
+
+	const parentBuildID = 123
+
+	assert.NoErr(t, datastore.Put(ctx,
+		bucketEntity("proj1", "bucket1"),
+		bucketEntity("proj1", "bucket2"),
+		buildEntity(parentBuildID),
+		buildInfraEntity(parentBuildID),
+	))
+
+	op, err := newScheduleBuildOp(ctx, []*pb.ScheduleBuildRequest{
+		scheduleReq("proj1", "bucket1", "builder", 0),
+		scheduleReq("proj1", "bucket1", "builder", parentBuildID),
+		scheduleReq("proj1", "bucket2", "builder", 0),
+	})
+	assert.NoErr(t, err)
+
+	assert.Loosely(t, op.Reqs, should.HaveLength(3))
+	assert.Loosely(t, op.Errs, should.HaveLength(3))
+	assert.Loosely(t, op.Builds, should.HaveLength(3))
+
+	assert.That(t, op.WellKnownExperiments.ToSortedSlice(), should.Match([]string{"some-experiment"}))
+
+	pending := 0
+	for range op.Pending() {
+		pending++
+	}
+	assert.That(t, pending, should.Equal(3))
+
+	op.Fail(op.Reqs[0], errors.New("boom"))
+	pending = 0
+	for range op.Pending() {
+		pending++
+	}
+	assert.That(t, pending, should.Equal(2))
+
+	op.SetBuild(op.Reqs[1], &model.Build{
+		Proto: &pb.Build{Id: 666},
+	})
+
+	out, errs := op.Finalize(ctx)
+	assert.Loosely(t, out, should.HaveLength(3))
+	assert.Loosely(t, errs, should.HaveLength(3))
+
+	assert.That(t, errs[0], should.ErrLike("boom"))
+	assert.Loosely(t, out[0], should.BeNil)
+
+	assert.NoErr(t, errs[1])
+	assert.That(t, out[1], should.Match(&pb.Build{Id: 666}))
+
+	assert.That(t, errs[2], should.ErrLike("was left unprocessed"))
+	assert.Loosely(t, out[2], should.BeNil)
+}
