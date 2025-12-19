ddf002613baf45661670fb8bd5eb54ad8cc56688
Patrick Meiring <meiring@google.com>
2025-12-18 17:12:50 -0800

    [ResultDB] Add TestAggregationPredicate.contains_test_results_filter.
    Extend the predicate used by QueryTestAggregations to support
    filtering to aggregations that contain particular test results.
    
    The semantics of the AIP-160 filter is defined using a schema
    object in the testresultsv2 package.
    
    The new filters are not yet integrated in the QueryTestAggregations RPC,
    this will occur in a follow-up.
    
    BUG=b:461911366
    TEST=None
    
    Change-Id: I1d5721c1719f7eeb7ef7508d5d2d9080e212adcf
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/7235195
    Commit-Queue: Patrick Meiring <meiring@google.com>
    Reviewed-by: Allen Xie <zhihuixie@google.com>

---
+++ resultdb/internal/testresultsv2/filters.go
@@ -0,0 +1,84 @@
+// Copyright 2025 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testresultsv2
+
+import (
+	"go.chromium.org/luci/common/data/aip160"
+
+	pb "go.chromium.org/luci/resultdb/proto/v1"
+)
+
+var statusEnumDef = aip160.NewEnumDefinition("pb.TestResult.Status", pb.TestResult_Status_value, int32(pb.TestResult_STATUS_UNSPECIFIED))
+
+// Security: filters on a column that a user does not have access to can leak information. With
+// certain filter types, this can be done rather quickly.
+//
+// E.g. if "module_variant.key:s" matches, they could try "module_variant.key:sa", "module_variant.key:sb",
+// until they find a filter that matches, say "module_variant.key:se". With repetition, this allows shifting out
+// the full value, "module_variant.key:secret". This attack works in ~N*A queries, where N is the length of the
+// value and A is the size of the alphabet.
+//
+// To prevent this, we required implementers to define a custom columns on the TestResultsV2 table that masks
+// out columns if the user does not have access to it. The columns required are:
+// - ModuleVariantMasked
+// - TagsMasked
+// - TestMetadataNameMasked
+// - TestMetadataLocationRepoMasked
+// - TestMetadataLocationFileNameMasked
+//
+// The columns are named differently to the original table columns to avoid
+// accidental security bugs if this comment is not followed, as the filters
+// will simply not work.
+var filterSchema = aip160.NewDatabaseTable().WithFields(
+	aip160.NewField().WithFieldPath("test_id_structured", "module_name").WithBackend(aip160.NewStringColumn("ModuleName")).FilterableImplicitly().Build(),
+	aip160.NewField().WithFieldPath("test_id_structured", "module_scheme").WithBackend(aip160.NewStringColumn("ModuleScheme")).Filterable().Build(),
+	aip160.NewField().WithFieldPath("test_id_structured", "module_variant").WithBackend(aip160.NewKeyValueColumn("ModuleVariantMasked").WithStringArray().Build()).Filterable().Build(),
+	aip160.NewField().WithFieldPath("test_id_structured", "module_variant_hash").WithBackend(aip160.NewStringColumn("ModuleVariantHash")).Filterable().Build(),
+	aip160.NewField().WithFieldPath("test_id_structured", "coarse_name").WithBackend(aip160.NewStringColumn("T1CoarseName")).FilterableImplicitly().Build(),
+	aip160.NewField().WithFieldPath("test_id_structured", "fine_name").WithBackend(aip160.NewStringColumn("T2FineName")).FilterableImplicitly().Build(),
+	aip160.NewField().WithFieldPath("test_id_structured", "case_name").WithBackend(aip160.NewStringColumn("T3CaseName")).FilterableImplicitly().Build(),
+	aip160.NewField().WithFieldPath("tags").WithBackend(aip160.NewKeyValueColumn("TagsMasked").WithStringArray().Build()).Filterable().Build(),
+	aip160.NewField().WithFieldPath("test_metadata", "name").WithBackend(aip160.NewStringColumn("TestMetadataNameMasked")).FilterableImplicitly().Build(),
+	aip160.NewField().WithFieldPath("test_metadata", "location", "repo").WithBackend(aip160.NewStringColumn("TestMetadataLocationRepoMasked")).Filterable().Build(),
+	aip160.NewField().WithFieldPath("test_metadata", "location", "file_name").WithBackend(aip160.NewStringColumn("TestMetadataLocationFileNameMasked")).FilterableImplicitly().Build(),
+	aip160.NewField().WithFieldPath("duration").WithBackend(aip160.NewDurationColumn("RunDurationNanos").Build()).Filterable().Build(),
+	aip160.NewField().WithFieldPath("status").WithBackend(aip160.NewEnumColumn("StatusV2").WithDefinition(statusEnumDef).Build()).Filterable().Build(),
+	// Future extensions: in the old Test Results tab in MILO, we also supported (test result) status and duration.
+	// As at writing, this requires extending the aip160 library to support enums and durations (stored as INT64 nanoseconds).
+).Build()
+
+// ValidateFilter validates an AIP-160 filter string.
+func ValidateFilter(filter string) error {
+	f, err := aip160.ParseFilter(filter)
+	if err != nil {
+		return err
+	}
+	// Trial generate the filter SQL. This will pick up any errors not found
+	// at parse time.
+	_, _, err = filterSchema.WhereClause(f, "", "trf_")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// WhereClause generates a WHERE clause for the given AIP-160 filter string.
+func WhereClause(filter, tableAlias, parameterPrefix string) (string, []aip160.SqlQueryParameter, error) {
+	f, err := aip160.ParseFilter(filter)
+	if err != nil {
+		return "", nil, err
+	}
+	return filterSchema.WhereClause(f, tableAlias, parameterPrefix)
+}
---
+++ resultdb/internal/testresultsv2/filters_test.go
@@ -0,0 +1,73 @@
+// Copyright 2025 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testresultsv2
+
+import (
+	"testing"
+
+	"go.chromium.org/luci/common/testing/ftt"
+	"go.chromium.org/luci/common/testing/truth/assert"
+	"go.chromium.org/luci/common/testing/truth/should"
+)
+
+func TestWhereClause(t *testing.T) {
+	ftt.Run("WhereClause", t, func(t *ftt.Test) {
+		t.Run("Empty filter", func(t *ftt.Test) {
+			result, _, err := WhereClause("", "T", "tr")
+			assert.Loosely(t, err, should.BeNil)
+			assert.Loosely(t, result, should.Equal("(TRUE)"))
+		})
+		t.Run("Test ID structured", func(t *ftt.Test) {
+			filter := `test_id_structured.module_name="modulename"` +
+				` AND test_id_structured.module_scheme="modulescheme"` +
+				` AND test_id_structured.module_variant.key="variantvalue"` +
+				` AND test_id_structured.module_variant_hash="modulevarianthash"` +
+				` AND test_id_structured.coarse_name="coarsename"` +
+				` AND test_id_structured.fine_name="finename"` +
+				` AND test_id_structured.case_name="casename"`
+			result, _, err := WhereClause(filter, "T", "tr")
+			assert.Loosely(t, err, should.BeNil)
+			assert.Loosely(t, result, should.Equal(`((T.ModuleName = @tr0) AND (T.ModuleScheme = @tr1)`+
+				` AND (@tr2 IN UNNEST(T.ModuleVariantMasked)) AND (T.ModuleVariantHash = @tr3)`+
+				` AND (T.T1CoarseName = @tr4) AND (T.T2FineName = @tr5) AND (T.T3CaseName = @tr6))`))
+		})
+		t.Run("Tags", func(t *ftt.Test) {
+			filter := `tags.key="tagvalue"`
+			result, _, err := WhereClause(filter, "T", "tr")
+			assert.Loosely(t, err, should.BeNil)
+			assert.Loosely(t, result, should.Equal(`(@tr0 IN UNNEST(T.TagsMasked))`))
+		})
+		t.Run("Test Metadata", func(t *ftt.Test) {
+			filter := `test_metadata.name="testmetadataname"` +
+				` AND test_metadata.location.repo="testmetadatalocationrepo"` +
+				` AND test_metadata.location.file_name="testmetadatalocationfilename"`
+			result, _, err := WhereClause(filter, "T", "tr")
+			assert.Loosely(t, err, should.BeNil)
+			assert.Loosely(t, result, should.Equal(`((T.TestMetadataNameMasked = @tr0) AND (T.TestMetadataLocationRepoMasked = @tr1) AND (T.TestMetadataLocationFileNameMasked = @tr2))`))
+		})
+		t.Run("Duration", func(t *ftt.Test) {
+			filter := `duration>100s`
+			result, _, err := WhereClause(filter, "T", "tr")
+			assert.Loosely(t, err, should.BeNil)
+			assert.Loosely(t, result, should.Equal(`(T.RunDurationNanos > 100000000000)`))
+		})
+		t.Run("Status", func(t *ftt.Test) {
+			filter := `status=EXECUTION_ERRORED`
+			result, _, err := WhereClause(filter, "T", "tr")
+			assert.Loosely(t, err, should.BeNil)
+			assert.Loosely(t, result, should.Equal(`(T.StatusV2 = 4)`))
+		})
+	})
+}
