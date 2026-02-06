df6330c29b43ef61329de45ca9655a57a3c55c62
Yang Yang <echoyang@google.com>
2026-02-06 09:24:47 -0800

    [server/tq] Add a binary that runs TQ DataStore sweeps.
    This is a copy of tq-sweeper-spanner/
    
    We'll test this in staging.
    
    Bug: b:433981550
    Test: TODO in dev environment
    Change-Id: I172c1652f06d87a2617e038d0748cdf41123b44a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/6824524
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Yang Yang <echoyang@chromium.org>

---
+++ server/cmd/tq-sweeper-datastore/main.go
@@ -0,0 +1,80 @@
+// Copyright 2026 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Executable tq-sweeper-datastore runs an inproc sweeping driver that scans
+// DataStore database for TQ transactional tasks reminders.
+//
+// It should run as a single replica per DataStore DB. It is OK if it temporarily
+// goes offline or runs on multiple replicas: this will just delay the task
+// reminders sweeping.
+package main
+
+import (
+	"context"
+	"flag"
+	"time"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/logging"
+
+	"go.chromium.org/luci/server"
+	"go.chromium.org/luci/server/gaeemulation"
+	"go.chromium.org/luci/server/module"
+	"go.chromium.org/luci/server/tq"
+
+	// Sweep DataStore DB.
+	_ "go.chromium.org/luci/server/tq/txn/datastore"
+)
+
+func main() {
+	sweepInterval := flag.Duration("tq-sweep-interval", time.Minute, "How often to run full sweeps")
+
+	opts := &tq.ModuleOptions{
+		SweepMode:               "inproc",
+		ServingPrefix:           "-", // not serving any tasks
+		SweepInitiationEndpoint: "-", // sweeps are launched by the timer only
+	}
+	opts.Register(flag.CommandLine)
+
+	modules := []module.Module{
+		gaeemulation.NewModuleFromFlags(),
+		tq.NewModule(opts),
+	}
+
+	server.Main(nil, modules, func(srv *server.Server) error {
+		if *sweepInterval < 0 {
+			*sweepInterval = 0
+		}
+
+		srv.RunInBackground("", func(ctx context.Context) {
+			count := 0
+
+			for ctx.Err() == nil {
+				count++
+				start := clock.Now(ctx)
+
+				logging.Infof(ctx, "--------------- Starting the sweep #%d ------------------", count)
+				tq.Default.Sweep(ctx)
+				logging.Infof(ctx, "--------------- Sweep #%d done in %s ---------------", count, clock.Now(ctx).Sub(start))
+
+				if sleep := start.Add(*sweepInterval).Sub(clock.Now(ctx)); sleep > 0 && ctx.Err() == nil {
+					logging.Infof(ctx, "Waiting %s until the next sweep", sleep)
+					clock.Sleep(ctx, sleep)
+				}
+			}
+		})
+
+		return nil
+	})
+}
