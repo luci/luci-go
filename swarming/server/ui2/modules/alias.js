6c82e5b2f3862584afc336c45a624c5e43c1fde8
Ryan Macnak <rmacnak@google.com>
2026-03-18 12:32:48 -0700

    Add UI alias for Ubuntu 26.04 (Resolute Raccoon).
    Change-Id: I44482c416aa337aa5bfcaf87bc65fb6b3999322f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/7679760
    Commit-Queue: Ryan Macnak <rmacnak@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>

--- swarming/server/ui2/modules/alias.js
+++ swarming/server/ui2/modules/alias.js
@@ -1,6 +1,16 @@
-// Copyright 2018 The LUCI Authors. All rights reserved.
-// Use of this source code is governed under the Apache License, Version 2.0
-// that can be found in the LICENSE file.
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // applyAlias will transform 'value' based on what 'key' is.
 // This aliasing should be used for human-displayable only,
@@ -186,6 +196,7 @@ const OS_ALIASES = {
   "Ubuntu-20.04": "Ubuntu 20.04 Focal Fossa",
   "Ubuntu-22.04": "Ubuntu 22.04 Jammy Jellyfish",
   "Ubuntu-24.04": "Ubuntu 24.04 Noble Numbat",
+  "Ubuntu-26.04": "Ubuntu 26.04 Resolute Raccoon",
   "Windows-10-10240": "Windows 10 version 1507",
   "Windows-10-10586": "Windows 10 version 1511",
   "Windows-10-14393": "Windows 10 version 1607",
