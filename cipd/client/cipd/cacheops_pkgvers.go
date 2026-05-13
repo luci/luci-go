12b904b0b4923a74ef27ad00eed38885ff2f2935
Robert Iannucci <iannucci@google.com>
2026-05-13 15:24:26 -0700

    [CIPD] Add abstraction for dealing with partially-resolved ensure files.
    Bug: 500385111
    Change-Id: Icae3fc019b5a077a947ebddf5570b62d6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/7818039
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@google.com>
    Reviewed-by: Brian Ryner <bryner@google.com>

---
+++ cipd/client/cipd/cacheops_pkgvers.go
@@ -0,0 +1,186 @@
+// Copyright 2026 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cipd
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+
+	"go.chromium.org/luci/cipd/client/cipd/ensure"
+	"go.chromium.org/luci/cipd/client/cipd/internal"
+	"go.chromium.org/luci/cipd/client/cipd/template"
+	"go.chromium.org/luci/cipd/client/cipd/ui"
+	"go.chromium.org/luci/cipd/common"
+)
+
+// PackageVersion describes a possibly-unresolved version of a package, and the
+// Service which is supposed to have it.
+type PackageVersion struct {
+	// Service is JUST the hostname of the service.
+	Service     string
+	PackageName string
+	Version     string
+}
+
+// PackageVersionSet is a set of [PackageVersion]s.
+type PackageVersionSet map[PackageVersion]struct{}
+
+// Add ensures that the given triple is in the set.
+func (s PackageVersionSet) Add(service, pkg, version string) {
+	s[PackageVersion{service, pkg, version}] = struct{}{}
+}
+
+type resolver func(ctx context.Context, service, pkg, version string) (common.Pin, error)
+
+// resolve will attempt to resolve ALL pins, filling them into `vc`.
+//
+// If they were all successfully resolved, they are returned in a new
+// PackageVersionSet where all `versions` are instance IDs. This resolved map
+// will be deduplicated (if multiple tags/refs point to the same instance, they
+// will be merged into a single entry).
+func (m PackageVersionSet) resolve(ctx context.Context, vc *internal.VersionCache, resolveFn resolver) (ret PackageVersionSet, err error) {
+	now := clock.Now(ctx)
+
+	total := int64(len(m))
+	ctx, done := ui.NewActivity(ctx, nil, "resolving")
+	defer func() {
+		if err != nil {
+			logging.Errorf(ctx, "Failed to resolve all versions")
+		} else {
+			logging.Infof(ctx, "Resolved %d pins in %.1fs", total, clock.Now(ctx).Sub(now).Seconds())
+		}
+		done()
+	}()
+
+	var mu sync.Mutex
+	pins := make(PackageVersionSet, len(m))
+	var errs []error
+	var progress int64
+
+	curAct := ui.CurrentActivity(ctx)
+	// report either reports a pin or an error.
+	report := func(service string, pin common.Pin, err error) {
+		mu.Lock()
+		defer mu.Unlock()
+		if err == nil {
+			pins.Add(service, pin.PackageName, pin.InstanceID)
+		} else {
+			errs = append(errs, err)
+		}
+		progress++
+		curAct.Progress(ctx, "resolving", "versions", progress, total)
+	}
+
+	var eg errgroup.Group
+	// Note: there is no serious computation which is happening here; 99% of the
+	// time is waiting for small RPCs from the server.
+	eg.SetLimit(64)
+
+	for vers := range m {
+		pin := common.Pin{PackageName: vers.PackageName}
+		if common.ValidateInstanceID(vers.Version, common.AnyHash) == nil {
+			pin.InstanceID = vers.Version
+			report(vers.Service, pin, nil)
+			continue
+		}
+
+		eg.Go(func() error {
+			rslv, err := resolveFn(ctx, vers.Service, vers.PackageName, vers.Version)
+			if err != nil {
+				report(vers.Service, pin, err)
+				return nil
+			}
+
+			pin.InstanceID = rslv.InstanceID
+			vc.AddVersion(ctx, vers.Service, pin, vers.Version)
+			report(vers.Service, pin, nil)
+			return nil
+		})
+	}
+	_ = eg.Wait()
+
+	if len(errs) > 0 {
+		for _, err := range errs {
+			logging.Errorf(ctx, "%s", err)
+		}
+		return nil, errors.Join(errs...)
+	}
+
+	return pins, nil
+}
+
+// fakeHash is a syntactically valid nonsense hash for [OfflineResolve].
+var fakeHash string
+
+func init() {
+	h := common.MustNewHash(common.DefaultHashAlgo)
+	h.Write([]byte("cacheops"))
+	fakeHash = common.ObjectRefToInstanceID(common.ObjectRefFromHash(h))
+}
+
+// OfflineResolve transforms an ensure.File into an PackageVersionSet for use
+// with [Client.CachePrepare].
+//
+// This does not make any network access, but instead resolves as much as
+// possible with `vf` (typically parsed from the ensure.File itself), but
+// leaving all other versions unresolved.
+func OfflineResolve(ef *ensure.File, vf ensure.VersionsFile) (PackageVersionSet, error) {
+	var mu sync.Mutex
+	ret := PackageVersionSet{}
+
+	platforms := ef.VerifyPlatforms
+	if len(platforms) == 0 {
+		platforms = []template.Platform{template.DefaultTemplate()}
+	}
+
+	var service string
+	if ef.ServiceURL != "" {
+		parsed, err := url.Parse(ef.ServiceURL)
+		if err != nil {
+			return nil, fmt.Errorf("parsing ensurefile URL: %w", err)
+		}
+		service = parsed.Host
+	}
+
+	for _, plat := range platforms {
+		_, err := ef.Resolve(func(pkg, vers string) (common.Pin, error) {
+			mu.Lock()
+			defer mu.Unlock()
+
+			if pin, err := vf.ResolveVersion(pkg, vers); err == nil {
+				ret.Add(service, pkg, pin.InstanceID)
+			} else {
+				ret.Add(service, pkg, vers)
+			}
+
+			// We don't care about the resolved pins from file.Resolve - they just
+			// need to validate.
+			return common.Pin{PackageName: pkg, InstanceID: fakeHash}, nil
+		}, plat.Expander())
+		if err != nil {
+			return nil, errors.Fmt("resolving platform %q: %w", plat, err)
+		}
+	}
+
+	return ret, nil
+}
---
+++ cipd/client/cipd/cacheops_pkgvers_test.go
@@ -0,0 +1,129 @@
+// Copyright 2026 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cipd
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"go.chromium.org/luci/common/data/stringset"
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/testing/truth/assert"
+	"go.chromium.org/luci/common/testing/truth/should"
+
+	"go.chromium.org/luci/cipd/client/cipd/ensure"
+	"go.chromium.org/luci/cipd/client/cipd/internal"
+	"go.chromium.org/luci/cipd/common"
+)
+
+var set = stringset.NewFromSlice
+
+func TestPackageVersionSet(t *testing.T) {
+	t.Parallel()
+
+	m := PackageVersionSet{}
+
+	srv1 := "some.server.example.com"
+	srv2 := "other.server.example.com"
+
+	aIID := strings.Repeat("a", 40)
+
+	m.Add(srv1, "some/pkg", "latest")
+	m.Add(srv1, "some/pkg", "greatest")
+	m.Add(srv1, "some/pkg", "most/fantastic")
+	m.Add(srv2, "some/pkg", "tag:123")
+	m.Add(srv1, "some/pkg", aIID)
+
+	assert.That(t, m, should.Match(PackageVersionSet{
+		{srv1, "some/pkg", "latest"}:         {},
+		{srv1, "some/pkg", "greatest"}:       {},
+		{srv1, "some/pkg", "most/fantastic"}: {},
+		{srv2, "some/pkg", "tag:123"}:        {},
+		{srv1, "some/pkg", aIID}:             {},
+	}))
+
+	vc := &internal.VersionCache{
+		Tags:           internal.Disabled,
+		FileObjectRefs: internal.Disabled,
+		Refs:           internal.Disabled,
+	}
+
+	// We have a tag and ref in here; we should get an error.
+	_, err := m.resolve(t.Context(), vc, func(ctx context.Context, service, pkg, version string) (common.Pin, error) {
+		return common.Pin{}, errors.New("no resolution")
+	})
+	assert.ErrIsLike(t, err, "no resolution")
+
+	bIID := strings.Repeat("b", 40)
+
+	// Give it a fake resolver which always resolves to "bbb...bbb"
+	pins, err := m.resolve(t.Context(), vc, func(ctx context.Context, service, pkg, version string) (common.Pin, error) {
+		return common.Pin{
+			PackageName: pkg,
+			InstanceID:  bIID,
+		}, nil
+	})
+	assert.NoErr(t, err)
+	assert.That(t, pins, should.Match(PackageVersionSet{
+		{srv1, "some/pkg", strings.Repeat("a", 40)}: {},
+		{srv1, "some/pkg", strings.Repeat("b", 40)}: {},
+		{srv2, "some/pkg", strings.Repeat("b", 40)}: {},
+	}))
+
+	// And our version cache contains all resolved versions.
+	for _, ref := range []string{"latest", "greatest", "most/fantastic"} {
+		pin, err := vc.ResolveRef(t.Context(), srv1, "some/pkg", ref)
+		assert.NoErr(t, err)
+		assert.That(t, pin, should.Match(common.Pin{
+			PackageName: "some/pkg",
+			InstanceID:  bIID,
+		}))
+	}
+	pin, err := vc.ResolveTag(t.Context(), srv2, "some/pkg", "tag:123")
+	assert.NoErr(t, err)
+	assert.That(t, pin, should.Match(common.Pin{
+		PackageName: "some/pkg",
+		InstanceID:  bIID,
+	}))
+}
+
+func TestOfflineResolve(t *testing.T) {
+	t.Parallel()
+
+	ef, err := ensure.ParseFile(strings.NewReader(`
+$ServiceURL https://server.example.com/
+$VerifiedPlatform linux-amd64 linux-arm64 macos-arm64
+
+some/package/${platform} tag:123
+some/other/package latest
+`))
+	assert.NoErr(t, err)
+
+	// Prepare a version file with only two resolved versions.
+	vf := ensure.VersionsFile{}
+	vf.AddVersion("some/package/linux-amd64", "tag:123", strings.Repeat("a", 40))
+	vf.AddVersion("some/other/package", "latest", strings.Repeat("b", 40))
+
+	m, err := OfflineResolve(ef, vf)
+	assert.NoErr(t, err)
+
+	assert.That(t, m, should.Match(PackageVersionSet{
+		{"server.example.com", "some/other/package", strings.Repeat("b", 40)}:       {},
+		{"server.example.com", "some/package/linux-amd64", strings.Repeat("a", 40)}: {},
+		{"server.example.com", "some/package/linux-arm64", "tag:123"}:               {},
+		{"server.example.com", "some/package/macos-arm64", "tag:123"}:               {},
+	}))
+}
