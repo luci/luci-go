5a84dddd1f64edda82e57b0cdfe98cedf65d5dbc
Greg NISBET <gregorynisbet@google.com>
2026-06-08 15:14:13 -0700

    auth/integration/gcemeta: remove FTT from gcemeta_test.go
    Refactors gcemeta_test.go to split TestServer into individual top-level
    test functions following standard Test<Concept>_<Behavior> naming conventions
    and extracts metadata server setup and t.Setenv into a helper function.
    
    TAG=agy
    
    Change-Id: Ic97f87af060278fa4a9573cbea7bc3d11ab38a23
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/7853930
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Reviewed-by: Allen Li <ayatane@chromium.org>

--- auth/integration/gcemeta/gcemeta_test.go
+++ auth/integration/gcemeta/gcemeta_test.go
@@ -17,7 +17,6 @@ package gcemeta
 import (
 	"context"
 	"net/http"
-	"os"
 	"testing"
 	"time"
 
@@ -25,7 +24,6 @@ import (
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 
-	"go.chromium.org/luci/common/testing/ftt"
 	"go.chromium.org/luci/common/testing/truth/assert"
 	"go.chromium.org/luci/common/testing/truth/should"
 )
@@ -45,107 +43,126 @@ func (f *fakeGenerator) GenerateIDToken(ctx context.Context, audience string, li
 	return &oauth2.Token{AccessToken: f.idToken}, nil
 }
 
-func TestServer(t *testing.T) {
+type gcemetaTestContext struct {
+	srv             *Server
+	gen             *fakeGenerator
+	fakeAccessToken *oauth2.Token
+	fakeIDToken     string
+}
+
+func setupGCEMetaTest(t *testing.T, ctx context.Context) *gcemetaTestContext {
 	fakeAccessToken := &oauth2.Token{
 		AccessToken: "fake_access_token",
 		Expiry:      time.Now().Add(time.Hour),
 	}
 	fakeIDToken := "fake_id_token"
 
-	ctx := context.Background()
 	gen := &fakeGenerator{fakeAccessToken, fakeIDToken, nil}
-	srv := Server{
+	srv := &Server{
 		Generator:        gen,
 		Email:            "fake@example.com",
 		Scopes:           []string{"scope1", "scope2"},
 		MinTokenLifetime: 2 * time.Minute,
 	}
 
-	// Need to set GCE_METADATA_HOST once before all tests because 'metadata'
-	// package caches results of metadata fetches.
 	addr, err := srv.Start(ctx)
 	if err != nil {
 		t.Fatalf("Failed to start: %s", err)
 	}
-	defer srv.Stop(ctx)
+	t.Cleanup(func() { srv.Stop(ctx) })
 
-	os.Setenv("GCE_METADATA_HOST", addr)
+	t.Setenv("GCE_METADATA_HOST", addr)
 
-	ftt.Run("Works", t, func(c *ftt.Test) {
-		assert.Loosely(c, metadata.OnGCE(), should.BeTrue)
+	return &gcemetaTestContext{
+		srv:             srv,
+		gen:             gen,
+		fakeAccessToken: fakeAccessToken,
+		fakeIDToken:     fakeIDToken,
+	}
+}
 
-		c.Run("Metadata client works", func(c *ftt.Test) {
-			cl := metadata.NewClient(http.DefaultClient)
+func TestServer_OnGCE(t *testing.T) {
+	ctx := t.Context()
+	setupGCEMetaTest(t, ctx)
+	assert.Loosely(t, metadata.OnGCE(), should.BeTrue)
+}
 
-			num, err := cl.NumericProjectIDWithContext(ctx)
-			assert.Loosely(c, err, should.BeNil)
-			assert.Loosely(c, num, should.Equal("0"))
+func TestServer_MetadataClient(t *testing.T) {
+	ctx := t.Context()
+	setupGCEMetaTest(t, ctx)
+	cl := metadata.NewClient(http.DefaultClient)
 
-			pid, err := cl.ProjectIDWithContext(ctx)
-			assert.Loosely(c, err, should.BeNil)
-			assert.Loosely(c, pid, should.Equal("none"))
+	num, err := cl.NumericProjectIDWithContext(ctx)
+	assert.Loosely(t, err, should.BeNil)
+	assert.Loosely(t, num, should.Equal("0"))
 
-			accounts, err := cl.GetWithContext(ctx, "instance/service-accounts/")
-			assert.Loosely(c, err, should.BeNil)
-			assert.Loosely(c, accounts, should.Equal("default/\nfake@example.com/\n"))
+	pid, err := cl.ProjectIDWithContext(ctx)
+	assert.Loosely(t, err, should.BeNil)
+	assert.Loosely(t, pid, should.Equal("none"))
 
-			for _, acc := range []string{"fake@example.com", "default"} {
-				info, err := cl.GetWithContext(ctx, "instance/service-accounts/"+acc+"/?recursive=true")
-				assert.Loosely(c, err, should.BeNil)
-				assert.Loosely(c, info, should.Equal(
-					`{"aliases":["default"],"email":"fake@example.com","scopes":["scope1","scope2"]}`))
+	accounts, err := cl.GetWithContext(ctx, "instance/service-accounts/")
+	assert.Loosely(t, err, should.BeNil)
+	assert.Loosely(t, accounts, should.Equal("default/\nfake@example.com/\n"))
 
-				email, err := cl.GetWithContext(ctx, "instance/service-accounts/"+acc+"/email")
-				assert.Loosely(c, err, should.BeNil)
-				assert.Loosely(c, email, should.Equal("fake@example.com"))
+	for _, acc := range []string{"fake@example.com", "default"} {
+		info, err := cl.GetWithContext(ctx, "instance/service-accounts/"+acc+"/?recursive=true")
+		assert.Loosely(t, err, should.BeNil)
+		assert.Loosely(t, info, should.Equal(
+			`{"aliases":["default"],"email":"fake@example.com","scopes":["scope1","scope2"]}`))
 
-				scopes, err := cl.ScopesWithContext(ctx, acc)
-				assert.Loosely(c, err, should.BeNil)
-				assert.Loosely(c, scopes, should.Match([]string{"scope1", "scope2"}))
-			}
-		})
+		email, err := cl.GetWithContext(ctx, "instance/service-accounts/"+acc+"/email")
+		assert.Loosely(t, err, should.BeNil)
+		assert.Loosely(t, email, should.Equal("fake@example.com"))
 
-		c.Run("OAuth2 token source works", func(c *ftt.Test) {
-			c.Run("Default scopes", func(c *ftt.Test) {
-				ts := google.ComputeTokenSource("default")
-				tok, err := ts.Token()
-				assert.Loosely(c, err, should.BeNil)
-				// Do not put tokens into logs, in case we somehow accidentally hit real
-				// metadata server with real tokens.
-				if tok.AccessToken != fakeAccessToken.AccessToken {
-					panic("Bad token")
-				}
-				assert.Loosely(c, time.Until(tok.Expiry), should.BeGreaterThan(55*time.Minute))
-				assert.Loosely(c, gen.lastScopes, should.Match([]string{"scope1", "scope2"}))
-			})
+		scopes, err := cl.ScopesWithContext(ctx, acc)
+		assert.Loosely(t, err, should.BeNil)
+		assert.Loosely(t, scopes, should.Match([]string{"scope1", "scope2"}))
+	}
+}
 
-			c.Run("Custom scopes", func(c *ftt.Test) {
-				ts := google.ComputeTokenSource("default", "custom1", "custom1", "custom2")
-				tok, err := ts.Token()
-				assert.Loosely(c, err, should.BeNil)
-				// Do not put tokens into logs, in case we somehow accidentally hit real
-				// metadata server with real tokens.
-				if tok.AccessToken != fakeAccessToken.AccessToken {
-					panic("Bad token")
-				}
-				assert.Loosely(c, time.Until(tok.Expiry), should.BeGreaterThan(55*time.Minute))
-				assert.Loosely(c, gen.lastScopes, should.Match([]string{"custom1", "custom2"}))
-			})
-		})
+func TestServer_OAuth2TokenSource_DefaultScopes(t *testing.T) {
+	ctx := t.Context()
+	tc := setupGCEMetaTest(t, ctx)
 
-		c.Run("ID token fetch works", func(c *ftt.Test) {
-			reply, err := metadata.GetWithContext(ctx, "instance/service-accounts/default/identity?audience=boo&format=ignored")
-			assert.Loosely(c, err, should.BeNil)
-			// Do not put tokens into logs, in case we somehow accidentally hit real
-			// metadata server with real tokens.
-			if reply != fakeIDToken {
-				panic("Bad token")
-			}
-		})
+	ts := google.ComputeTokenSource("default")
+	tok, err := ts.Token()
+	assert.Loosely(t, err, should.BeNil)
+	if tok.AccessToken != tc.fakeAccessToken.AccessToken {
+		panic("Bad token")
+	}
+	assert.Loosely(t, time.Until(tok.Expiry), should.BeGreaterThan(55*time.Minute))
+	assert.Loosely(t, tc.gen.lastScopes, should.Match([]string{"scope1", "scope2"}))
+}
 
-		c.Run("Unsupported metadata call", func(c *ftt.Test) {
-			_, err := metadata.ExternalIPWithContext(ctx)
-			assert.Loosely(c, err.Error(), should.Equal(`metadata: GCE metadata "instance/network-interfaces/0/access-configs/0/external-ip" not defined`))
-		})
-	})
+func TestServer_OAuth2TokenSource_CustomScopes(t *testing.T) {
+	ctx := t.Context()
+	tc := setupGCEMetaTest(t, ctx)
+
+	ts := google.ComputeTokenSource("default", "custom1", "custom1", "custom2")
+	tok, err := ts.Token()
+	assert.Loosely(t, err, should.BeNil)
+	if tok.AccessToken != tc.fakeAccessToken.AccessToken {
+		panic("Bad token")
+	}
+	assert.Loosely(t, time.Until(tok.Expiry), should.BeGreaterThan(55*time.Minute))
+	assert.Loosely(t, tc.gen.lastScopes, should.Match([]string{"custom1", "custom2"}))
+}
+
+func TestServer_IDTokenFetch(t *testing.T) {
+	ctx := t.Context()
+	tc := setupGCEMetaTest(t, ctx)
+
+	reply, err := metadata.GetWithContext(ctx, "instance/service-accounts/default/identity?audience=boo&format=ignored")
+	assert.Loosely(t, err, should.BeNil)
+	if reply != tc.fakeIDToken {
+		panic("Bad token")
+	}
+}
+
+func TestServer_UnsupportedMetadataCall(t *testing.T) {
+	ctx := t.Context()
+	setupGCEMetaTest(t, ctx)
+
+	_, err := metadata.ExternalIPWithContext(ctx)
+	assert.Loosely(t, err.Error(), should.Equal(`metadata: GCE metadata "instance/network-interfaces/0/access-configs/0/external-ip" not defined`))
 }
