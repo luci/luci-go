7693f00a4d83b4e38f856837530e9a222e1d0165
Alex Ovsienko <ovsienko@google.com>
2026-06-30 22:54:47 -0700

    Revert "vpython: fix proxy for python."
    This reverts commit b91d616f4e6e4fe6651680c34e22ffabf469233f.
    
    Reason for revert: Causes internal proxies propagation through the new variables. We will remove native proxies for Python. If users need proxy for their operation, they will have to work on their own infra.
    
    Failure Link: https://fusion2.corp.google.com/ci/kokoro/prod:chrome-secure-shell%2Fgcp_ubuntu%2Frelease/activity/
    
    Bug: b/529822095
    
    Original change's description:
    > vpython: fix proxy for python.
    >
    > The vpython forgot the env variables for proxies behind as we are running hermetic environment. Previously CIPD could just take over all variables, howevere we cannot do this now. So, pass over common proxy env variables to python.
    >
    > Bug: b/491263752
    > Change-Id: I47feb351353390fd5641fa227cbb1bcc6a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/8018402
    > Commit-Queue: Alex Ovsienko <ovsienko@google.com>
    > Reviewed-by: Fumitoshi Ukai <ukai@google.com>
    
    Bug: b/491263752
    Change-Id: I9e113a4aa2311033725a0253595f95bd2fc884b6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/8029400
    Commit-Queue: Alex Ovsienko <ovsienko@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>

--- vpython/python/python.go
+++ vpython/python/python.go
@@ -130,7 +130,6 @@ func (e *Environment) WithWheels(wheels generators.Generator) generators.Generat
 	if reportURL := os.Getenv(common.EnvVpythonReportMissingUrl); reportURL != "" {
 		env.Set(common.EnvVpythonReportMissingUrl, reportURL)
 	}
-	passProxyEnv(env)
 	return &workflow.Generator{
 		Name: "python_venv",
 		Args: []string{
@@ -251,7 +250,6 @@ func (e *Environment) WithUV(uv generators.Generator, spec *standard.ProjectSpec
 	if reportURL := os.Getenv(common.EnvVpythonReportMissingUrl); reportURL != "" {
 		env.Set(common.EnvVpythonReportMissingUrl, reportURL)
 	}
-	passProxyEnv(env)
 
 	reqGen := &SpecRequirementsGenerator{Spec: spec}
 
@@ -277,15 +275,5 @@ func (e *Environment) WithUV(uv generators.Generator, spec *standard.ProjectSpec
 			{Type: generators.DepsHostTarget, Generator: reqGen},
 			{Type: generators.DepsHostTarget, Generator: bootstrapGen},
 		},
-	}
-}
-
-func passProxyEnv(dst environ.Env) {
-	for _, env := range os.Environ() {
-		key, value, _ := strings.Cut(env, "=")
-		switch strings.ToLower(key) {
-		case "http_proxy", "https_proxy", "all_proxy", "no_proxy":
-			dst.Set(key, value)
-		}
 	}
 }
