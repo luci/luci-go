769c558b7d1c65c8fffe73c173afcc97e55a1de8
Robert Iannucci <iannucci@google.com>
2026-09-09 17:51:34 -0700

    [swarming] Fix the version link at the top.
    Change-Id: Ic3c74427a96e496252f5b535c70e0b766a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-go/+/8384976
    Auto-Submit: Robbie Iannucci <iannucci@google.com>
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@google.com>

--- swarming/server/ui2/modules/swarming-app/swarming-app.js
+++ swarming/server/ui2/modules/swarming-app/swarming-app.js
@@ -1,6 +1,16 @@
-// Copyright 2018 The LUCI Authors. All rights reserved.
-// Use of this source code is governed under the Apache License, Version 2.0
-// that can be found in the LICENSE file.
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 /** @module swarming-ui/modules/swarming-app
  * @description <h2><code>swarming-app</code></h2>
@@ -90,7 +100,7 @@ function gitLink(details) {
     return "";
   }
   const version = split.length == 2 ? split[1] : split[0];
-  return html`<a href=https://chromium.googlesource.com/infra/luci/luci-py/+/${version}>${version}</a>`;
+  return html`<a href=https://chromium.googlesource.com/infra/luci/luci-go/+/${version}>${version}</a>`;
 }
 
 const dynamicContentTemplate = (ele) =>
